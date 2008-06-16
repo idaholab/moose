6644d0611ffb148ef0ea7160c969012e4a163642
Derek Gaston <derek.gaston@inl.gov>
2008-06-16 15:44:58 +0000

    Dummy commit
    r129

--- framework/src/Kernel.C
+++ framework/src/Kernel.C
@@ -4,6 +4,6 @@ void
 Kernel::computeResidual(DenseVector<Number> & Re, Elem * elem)
 {
   dof_map.dof_indices(elem, dof_indices);
-  fe->reinit(elem);
+  fe->reinit(elem); 
   Re.resize(dof_indices.size());
 }
