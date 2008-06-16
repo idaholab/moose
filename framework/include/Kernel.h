43cb6628f514466dc53f0a2a8397c52f1e2cdc50
Derek Gaston <derek.gaston@inl.gov>
2008-06-16 13:26:26 +0000

    Add new Kernel constructor
    r127

--- framework/include/Kernel.h
+++ framework/include/Kernel.h
@@ -1,3 +1,5 @@
+#include "system.h"
+
 /** 
  * The Kernel class is responsible for calculating the residuals for various
  * physics.
@@ -7,6 +9,18 @@ class Kernel
 {
 public:
   Kernel(){};
+
+  /** 
+   * This constructor should be used most often.  It initializes all internal
+   * references needed for residual computation.
+   * 
+   * @param system The system this variable is in
+   * @param var_name The variable this Kernel is going to compute a residual for.
+   */
+  Kernel(System * system, std::string var_name);
+
   virtual ~Kernel(){};
+
+  computeResidual();
 };
 
