2ba382c95dddcc04eb2aabcc7e011c6cca506e54
Derek Gaston <derek.gaston@inl.gov>
2008-06-16 15:12:20 +0000

    #23 Kernel.h first take
    r128

--- framework/include/Kernel.h
+++ framework/include/Kernel.h
@@ -1,4 +1,12 @@
-#include "system.h"
+// libMesh includes
+#include "equation_systems.h"
+#include "mesh_base.h"
+#include "nonlinear_implicit_system.h"
+#include "fe_base.h"
+#include "quadrature_gauss.h"
+
+//Forward Declarations
+class Elem;
 
 /** 
  * The Kernel class is responsible for calculating the residuals for various
@@ -8,8 +16,6 @@
 class Kernel
 {
 public:
-  Kernel(){};
-
   /** 
    * This constructor should be used most often.  It initializes all internal
    * references needed for residual computation.
@@ -17,10 +23,73 @@ public:
    * @param system The system this variable is in
    * @param var_name The variable this Kernel is going to compute a residual for.
    */
-  Kernel(System * system, std::string var_name);
+  Kernel(EquationSystems * es, std::string var_name);
 
   virtual ~Kernel(){};
 
-  computeResidual();
+  /** 
+   * Computes the residual for the current element.
+   * 
+   * @param Re Local residual vector.
+   * @param elem Current element.
+   */
+  void computeResidual(DenseVector<Number> & Re, Elem * elem);
+
+protected:
+  
+  /** 
+   * This is the virtual that derived classes should override.
+   * 
+   * @param Re 
+   */
+  virtual void computeElemResidual(DenseVector<Number> & Re);
+
+  EquationSystems & _es;
+  MeshBase & _mesh;
+  unsigned int _dimension;
+
+  NonlinearImplicitSystem & _system;
+  const DofMap & _dof_map;
+
+  /**
+   * FE Type to be used.
+   */
+  FEType _fe_type;
+
+  /**
+   * Interior finite element.
+   */
+  AutoPtr<FEBase> _fe;
+
+  /**
+   * Interior quadrature rule.
+   */
+  QGauss _qrule;
+
+  /**
+   * Boundary finite element. 
+   */
+  AutoPtr<FEBase> _fe_face;
+
+  /**
+   * Boundary quadrature rule.
+   */
+  QGauss _qface;
+
+  /**
+   * Interior Jacobian pre-multiplied by the weight at every quadrature point.
+   */
+  const std::vector<Real> & _JxW;
+
+  /**
+   * Interior shape function.
+   */
+  const std::vector<std::vector<Real> > & _phi;
+
+  /**
+   * Gradient of interior shape function.
+   */
+  const std::vector<std::vector<RealGradient> > & _dphi;
+  
 };
 
--- framework/src/Kernel.C
+++ framework/src/Kernel.C
@@ -1,1 +1,9 @@
 #include"Kernel.h"
+
+void
+Kernel::computeResidual(DenseVector<Number> & Re, Elem * elem)
+{
+  dof_map.dof_indices(elem, dof_indices);
+  fe->reinit(elem);
+  Re.resize(dof_indices.size());
+}
