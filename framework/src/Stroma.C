9a3f92b39b63f6e22b67fdd8f9cdbaca0c26f0f6
Derek Gaston <derek.gaston@inl.gov>
2008-07-14 14:06:34 +0000

    missed file
    r204

---
+++ framework/src/Stroma.C
@@ -0,0 +1,9 @@
+#include "Stroma.h"
+
+void
+Stroma::registerObjects()
+{
+  BCFactory::instance()->registerBC<DirichletBC>("DirichletBC");
+  BCFactory::instance()->registerBC<NeumannBC>("NeumannBC");
+  BCFactory::instance()->registerBC<VectorNeumannBC>("VectorNeumannBC");
+}
