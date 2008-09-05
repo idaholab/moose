f5f5baacc94f080cddaf323bb312364b833bbe5d
Ryosuke Park <ryosuke.park@inl.gov>
2008-09-05 11:50:05 +0000

    trivial change in comment of Vacuum BC
    r253

--- framework/include/VacuumBC.h
+++ framework/include/VacuumBC.h
@@ -9,7 +9,7 @@ class VacuumBC;
 /**
  * Implements a simple Vacuum BC for neutron diffusion on the boundary.
  * Vacuum BC is defined as \f$ D\frac{du}{dn}+\frac{u}{2} = 0\f$, where u is neutron flux.
- * Hence, \f$ D\frac{du}{dn}=-\frac{u}{2} \f$ and \f$ -\frac{u}{2} \f$ is substitute into 
+ * Hence, \f$ D\frac{du}{dn}=-\frac{u}{2} \f$ and \f$ -\frac{u}{2} \f$ is substituted into 
  * the Neumann BC term produced from integrating the diffusion operator by parts.
  */
 class VacuumBC : public BoundaryCondition
