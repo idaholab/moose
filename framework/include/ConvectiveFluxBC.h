bd11814fef7ad0ea31303193524aae1c5417cd23
Cody Permann <cody.permann@inl.gov>
2009-04-23 09:44:29 +0000

    Convective Flux Sin ramp
    r679

--- framework/include/ConvectiveFluxBC.h
+++ framework/include/ConvectiveFluxBC.h
@@ -9,12 +9,6 @@ class ConvectiveFluxBC;
 template<>
 Parameters valid_params<ConvectiveFluxBC>();
 
-/**
- * Implements a simple Vacuum BC for neutron diffusion on the boundary.
- * Vacuum BC is defined as \f$ D\frac{du}{dn}+\frac{u}{2} = 0\f$, where u is neutron flux.
- * Hence, \f$ D\frac{du}{dn}=-\frac{u}{2} \f$ and \f$ -\frac{u}{2} \f$ is substituted into 
- * the Neumann BC term produced from integrating the diffusion operator by parts.
- */
 class ConvectiveFluxBC : public BoundaryCondition
 {
 public:
@@ -25,9 +19,11 @@ public:
    */
   ConvectiveFluxBC(std::string name, Parameters parameters, std::string var_name, unsigned int boundary_id, std::vector<std::string> coupled_to, std::vector<std::string> coupled_as)
     :BoundaryCondition(name, parameters, var_name, true, boundary_id, coupled_to, coupled_as),
-    _rate(_parameters.get<Real>("rate")),
-    _value(_parameters.get<Real>("value"))
-  {}
+     _initial(_parameters.get<Real>("initial")),
+     _final(_parameters.get<Real>("final")),
+     _rate(_parameters.get<Real>("rate")),
+     _duration(_parameters.get<Real>("duration"))
+    {}
 
   virtual ~ConvectiveFluxBC(){}
 
@@ -39,8 +35,10 @@ private:
   /**
    * Ratio of u to du/dn
    */
+  Real _initial;
+  Real _final;
   Real _rate;
-  Real _value;
+  Real _duration;
 };
 
 #endif //CONVECTIVEFLUXBC_H
--- framework/src/ConvectiveFluxBC.C
+++ framework/src/ConvectiveFluxBC.C
@@ -5,16 +5,32 @@ Parameters valid_params<ConvectiveFluxBC>()
 {
   Parameters params;
   params.set<Real>("rate")=7500;
-  params.set<Real>("value")=500;
+  params.set<Real>("initial")=500;
+  params.set<Real>("final")=500;
+  params.set<Real>("duration")=0.0;
   return params;
 }
 
 Real ConvectiveFluxBC::computeQpResidual()
 {
-  return -(_phi_face[_i][_qp]*_rate*(_value - _u_face[_qp]));
+  Real value;
+
+  if(_t < _duration)
+    value = _initial + (_final-_initial) * std::sin((0.5/_duration) * libMesh::pi * _t);
+  else
+    value = _final;
+    
+  return -(_phi_face[_i][_qp]*_rate*(value - _u_face[_qp]));
 }
   
 Real ConvectiveFluxBC::computeQpJacobian()
 {
-  return -(_phi_face[_i][_qp]*_rate*(_value - _phi_face[_j][_qp]));
+  Real value;
+
+  if(_t < _duration)
+    value = _initial + (_final-_initial) * std::sin((0.5/_duration) * libMesh::pi * _t);
+  else
+    value = _final;
+  
+  return -(_phi_face[_i][_qp]*_rate*(value - _phi_face[_j][_qp]));
 }
