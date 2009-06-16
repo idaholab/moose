1fde5cf6e772b664f36298180c9aee10c01e2aca
Derek Gaston <derek.gaston@inl.gov>
2009-06-16 15:47:06 +0000

    ifdef tbb stuff
    r773

--- framework/include/ParallelUniqueId.h
+++ framework/include/ParallelUniqueId.h
@@ -3,7 +3,9 @@
 
 #include "libmesh_common.h"
 
+#ifdef LIBMESH_HAVE_TBB_API
 #include "tbb/concurrent_queue.h"
+#endif
 
 class ParallelUniqueId
 {
@@ -11,24 +13,34 @@ public:
 
   ParallelUniqueId()
     {
+      #ifdef LIBMESH_HAVE_TBB_API
       ids.pop(id);
+      #else
+      id = 0;
+      #endif
     }
 
   ~ParallelUniqueId()
     {
+      #ifdef LIBMESH_HAVE_TBB_API
       ids.push(id);
+      #endif
     }
 
   static void initialize()
     {
+      #ifdef LIBMESH_HAVE_TBB_API
       for(unsigned int i=0; i<libMesh::n_threads(); ++i)
         ids.push(i);
+      #endif
     }
   
   unsigned int id;
 
 protected:
+#ifdef LIBMESH_HAVE_TBB_API
   static tbb::concurrent_queue<unsigned int> ids;
+#endif
 };
 
 #endif // PARALLELUNIQUEID_H
