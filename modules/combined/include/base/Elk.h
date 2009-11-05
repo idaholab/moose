9798c053450b52087d88faf3d90c00e2493c2226
Cody Permann <cody.permann@inl.gov>
2009-11-05 07:56:22 +0000

    Adding the Extended Library of Kernels framework
    r1250

---
+++ modules/combined/include/base/Elk.h
@@ -0,0 +1,12 @@
+#ifndef ELK_H
+#define ELK_H
+
+namespace Moose
+{
+  /**
+   * Registers the Kernels, BCs, and Materials provided in Elk.
+   */
+  void registerObjects();
+}
+
+#endif //ELK_H
---
+++ modules/combined/src/base/Elk.C
@@ -0,0 +1,11 @@
+#include "KernelFactory.h"
+#include "BCFactory.h"
+#include "AuxFactory.h"
+#include "MaterialFactory.h"
+
+#include "Elk.h"
+
+void
+Elk::registerObjects()
+{
+}
