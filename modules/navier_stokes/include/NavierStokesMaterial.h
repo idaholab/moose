a9e80918351421b4eb8594221a8ead28b58ba113
Derek Gaston <derek.gaston@inl.gov>
2009-11-05 15:34:04 +0000

    #254 NS material
    r1265

---
+++ modules/navier_stokes/include/NavierStokesMaterial.h
@@ -0,0 +1,75 @@
+#ifndef NAVIERSTOKESMATERIAL_H
+#define NAVIERSTOKESMATERIAL_H
+
+#include "Material.h"
+
+
+//Forward Declarations
+class NavierStokesMaterial;
+
+template<>
+Parameters valid_params<NavierStokesMaterial>();
+
+/**
+ * This is the base class all materials should use if you are trying to use the Navier-Stokes Kernels.
+ *
+ * Note that the derived class just needs to compute dynamic_viscocity then call this class's
+ * computeProperties() function.
+ *
+ * Also make sure that the derived class's valid_params function just adds to this class's valid_params.
+ *
+ * Finally, note that this Material _isn't_ registered with the MaterialFactory.  The reason is that by
+ * itself this material doesn't work!  You _must_ derive from this material and compute dynamic_viscocity!
+ */
+class NavierStokesMaterial : public Material
+{
+public:
+NavierStokesMaterial(std::string name,
+      Parameters parameters,
+      unsigned int block_id,
+      std::vector<std::string> coupled_to,
+      std::vector<std::string> coupled_as);
+  
+protected:
+
+  /**
+   * Must be called _after_ the child class computes dynamic_viscocity.
+   */
+  virtual void computeProperties();
+
+  bool _has_u;
+  std::vector<Real> & _u;
+  std::vector<RealGradient> & _grad_u;
+
+  bool _has_v;
+  std::vector<Real> & _v;
+  std::vector<RealGradient> & _grad_v;
+
+  bool _has_w;
+  std::vector<Real> & _w;
+  std::vector<RealGradient> & _grad_w;
+
+  bool _has_pe;
+  std::vector<Real> & _pe;
+  std::vector<RealGradient> & _grad_pe;
+
+  std::vector<RealTensorValue> & _viscous_stress_tensor;
+  std::vector<Real> & _thermal_conductivity;
+  std::vector<Real> & _pressure;
+
+  Real & _gamma;
+  Real & _c_v;
+  Real & _c_p;
+  Real & _R;
+  Real & _Pr;
+
+  std::vector<Real> & _dynamic_viscocity;
+  
+  Real _R_param;
+  Real _gamma_param;
+  Real _Pr_param;
+
+  std::vector<std::vector<RealGradient> *> _vel_grads;
+};
+
+#endif //NAVIERSTOKESMATERIAL_H
---
+++ modules/navier_stokes/src/NavierStokesMaterial.C
@@ -0,0 +1,107 @@
+#include "NavierStokesMaterial.h"
+
+template<>
+Parameters valid_params<NavierStokesMaterial>()
+{
+  Parameters params;
+
+  //Default is Air
+  params.set<Real>("R")=287.04; // J/kgK
+  params.set<Real>("gamma")=1.405;
+  params.set<Real>("Pr")=0.71;
+  
+  return params;
+}
+
+NavierStokesMaterial::NavierStokesMaterial(std::string name,
+      Parameters parameters,
+      unsigned int block_id,
+      std::vector<std::string> coupled_to,
+      std::vector<std::string> coupled_as)
+  :Material(name,parameters,block_id,coupled_to,coupled_as),
+    _has_u(isCoupled("u")),
+
+    _u(coupledVal("u")),
+    _grad_u(coupledGrad("u")),
+    
+    _has_v(isCoupled("v")),
+    _v(coupledVal("v")),
+    _grad_v(coupledGrad("v")),
+    
+    _has_w(isCoupled("w")),
+    _w(_has_w ? coupledVal("w") : _zero),
+    _grad_w(_has_w ? coupledGrad("w") : _grad_zero),
+    
+    _has_pe(isCoupled("pe")),
+    _pe(coupledVal("pe")),
+    _grad_pe(coupledGrad("pe")),
+
+    _viscous_stress_tensor(declareTensorProperty("viscous_stress_tensor")),
+    _thermal_conductivity(declareRealProperty("thermal_conductivity")),
+    _pressure(declareRealProperty("pressure")),
+
+    _gamma(declareConstantRealProperty("gamma")),
+    _c_v(declareConstantRealProperty("c_v")),
+    _c_p(declareConstantRealProperty("c_p")),
+    _R(declareConstantRealProperty("R")),
+    _Pr(declareConstantRealProperty("Pr")),
+
+    //Declared here but _not_ calculated here
+    _dynamic_viscocity(declareRealProperty("dynamic_viscocity")),
+    
+    _R_param(parameters.get<Real>("R")),
+    _gamma_param(parameters.get<Real>("gamma")),
+    _Pr_param(parameters.get<Real>("Pr"))
+  {
+    _gamma = _gamma_param;
+    _R = _R_param;
+    _Pr = _Pr_param;
+    
+    _c_v = _R / (_gamma_param - 1);
+
+    _c_p = _gamma_param * _c_v;
+
+    //Load these up in a vector for convenience
+    _vel_grads.resize(3);
+
+    _vel_grads[0] = &_grad_u;
+    _vel_grads[1] = &_grad_v;
+    _vel_grads[2] = &_grad_w;
+  }
+
+
+/**
+ * Must be called _after_ the child class computes dynamic_viscocity.
+ */
+void
+NavierStokesMaterial::computeProperties()
+{  
+  for(unsigned int qp=0; qp<_qrule->n_points(); qp++)
+  {  
+    /******* Viscous Stress Tensor *******/
+    //Technically... this _is_ the transpose (since we are loading these by rows)
+    //But it doesn't matter....
+    RealTensorValue grad_outter_u(_grad_u[qp],_grad_v[qp],_grad_w[qp]);
+
+    grad_outter_u += grad_outter_u.transpose();
+
+    Real div_vel = 0;
+    for(unsigned int i=0;i<3;i++)
+      div_vel += (*_vel_grads[i])[qp](i);
+
+    //Add diagonal terms
+    for(unsigned int i=0;i<3;i++)
+      grad_outter_u(i,i) -= (2.0/3.0) * div_vel;
+
+    grad_outter_u *= _dynamic_viscocity[qp];
+    
+    _viscous_stress_tensor[qp] = grad_outter_u;
+
+    
+    /******* Pressure *******/
+    _pressure[qp] = (_gamma_param - 1)*(_pe[qp] - (0.5 * (_u[qp]*_u[qp] + _v[qp]*_v[qp] + _w[qp]*_w[qp])));
+      
+    _thermal_conductivity[qp] = (_c_p * _dynamic_viscocity[qp]) / _Pr;
+  }
+}
+
