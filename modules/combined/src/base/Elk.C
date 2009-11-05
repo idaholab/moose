9bc102a8aa3704103006dfa271079ecda81c9323
Cody Permann <cody.permann@inl.gov>
2009-11-05 08:32:39 +0000

    Adding the first kernel to Elk
    r1254

--- modules/combined/src/base/Elk.C
+++ modules/combined/src/base/Elk.C
@@ -1,6 +1,10 @@
 #include "KernelFactory.h"
+#include "CoefDiffusion.h"
+
 #include "BCFactory.h"
+
 #include "AuxFactory.h"
+
 #include "MaterialFactory.h"
 
 #include "Elk.h"
@@ -8,4 +12,5 @@
 void
 Elk::registerObjects()
 {
+  KernelFactory::instance()->registerKernel<CoefDiffusion>("CoefDiffusion");
 }
