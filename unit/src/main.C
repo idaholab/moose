92ca7b451afe2095f418c022ee961272345af04b
Cody Permann <cody.permann@inl.gov>
2009-12-17 16:55:38 +0000

    Dumping XML directly
    r3131

--- unit/src/main.C
+++ unit/src/main.C
@@ -11,6 +11,8 @@
 #include "ColumnMajorMatrixTest.h"
 #include "Moose.h"
 
+#include <fstream>
+
 PerfLog Moose::perf_log("CppUnit");
 
 int main(int argc, char **argv)
@@ -21,12 +23,16 @@ int main(int argc, char **argv)
   runner.addTest(suite);
 
   /* Uncomment the following line for bitten testing */
-  runner.setOutputter ( new CppUnit::XmlOutputter( &runner.result(), std::cout ) );
+  std::ofstream out("test_results.xml");
+  
+  runner.setOutputter ( new CppUnit::XmlOutputter( &runner.result(), out ) );
   
   /* Uncomment the following line for CLI testing */
   //runner.setOutputter ( new CppUnit::CompilerOutputter( &runner.result(), std::cerr ) );
 
   bool wasSucessful = runner.run("", false, true, false);
+
+  out.close();
   
   return wasSucessful ? 0 : 1;
 }
