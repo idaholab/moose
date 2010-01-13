f23d0588e46c33f400f125079d08ad26d818927c
Michael Tonks <michael.tonks@inl.gov>
2010-01-13 16:35:26 +0000

    modified the phase field files using an approximation of the log
    r1438

--- modules/phase_field/include/utils/MathUtils.h
+++ modules/phase_field/include/utils/MathUtils.h
@@ -7,7 +7,9 @@ namespace MathUtils
 {
   inline Real badLog(Real x)
   {
-    return std::log(x);
+    Real y = (x - 1.) / (x + 1.);
+    Real sqy= y*y;
+    return 2*y*( 1. + sqy*(1./3. + sqy*(1./5. + sqy*(1./7. + sqy*(1./9. + sqy*(1./11. + sqy*(1./13. + sqy*1./15)))))));
   };
 };
 
