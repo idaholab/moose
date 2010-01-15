0dd0884ef436e497b31c64683298f7161e17c515
Michael Tonks <michael.tonks@inl.gov>
2010-01-15 12:36:01 +0000

    fixed various files
    r1440

--- modules/phase_field/include/utils/MathUtils.h
+++ modules/phase_field/include/utils/MathUtils.h
@@ -11,6 +11,20 @@ namespace MathUtils
     Real sqy= y*y;
     return 2*y*( 1. + sqy*(1./3. + sqy*(1./5. + sqy*(1./7. + sqy*(1./9. + sqy*(1./11. + sqy*(1./13. + sqy*1./15)))))));
   };
+
+  inline Real polyLog(Real x)
+  {
+    Real tol=1.0e-4;
+    Real c1=2.0e12;
+    Real c2=-3.0e8;
+    Real c3=3.0e4;
+    Real c4=-1.104367370530952e+01;
+
+    if(x<tol)
+      return c1*tol*tol*tol/6.0 + c2*tol*tol/2.0 + c3*tol + c4;
+    else
+      return std::log(x);
+  };  
 };
 
 #endif //MATHUTILS_H
