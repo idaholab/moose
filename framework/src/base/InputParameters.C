d3dd282a86a1843bd9d60a8f49c117b77da04f64
Derek Gaston <derek.gaston@inl.gov>
2010-03-01 17:45:00 +0000

    add += operator
    r1606

--- framework/src/base/InputParameters.C
+++ framework/src/base/InputParameters.C
@@ -21,6 +21,24 @@ InputParameters::operator=(const InputParameters &rhs)
   return *this;
 }
 
+InputParameters &
+InputParameters::operator+=(const InputParameters &rhs)
+{
+  Parameters::operator+=(rhs);
+
+  for(std::map<std::string, std::string>::const_iterator it = rhs._doc_string.begin();
+      it!=rhs._doc_string.end();
+      ++it)
+    this->_doc_string[it->first] = it->second;
+
+  for(std::set<std::string>::const_iterator it = rhs._required_params.begin();
+      it!=rhs._required_params.end();
+      ++it)
+    this->_required_params.insert(*it);
+
+  return *this;
+}
+
 void
 InputParameters::seenInInputFile(const std::string &name)
 {
