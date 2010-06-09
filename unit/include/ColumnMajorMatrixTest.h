f345673cee0fa788e16c7b0346a6f69a399a5a3a
Philip Jagielski <philip.jagielski@inl.gov>
2010-06-09 16:41:57 +0000

    Test every function of ColumnMajorMatrix, does not test assertions.
    r3135

--- unit/include/ColumnMajorMatrixTest.h
+++ unit/include/ColumnMajorMatrixTest.h
@@ -10,27 +10,79 @@
 class ColumnMajorMatrixTest : public CppUnit::TestFixture 
 {
   CPPUNIT_TEST_SUITE( ColumnMajorMatrixTest );
-  CPPUNIT_TEST( addMatrixScalar );
-  CPPUNIT_TEST( addMatrixScalarEquals );
+
+  CPPUNIT_TEST( rowColConstructor );
+  CPPUNIT_TEST( copyConstructor );
+  CPPUNIT_TEST( tensorConstructor );
+  CPPUNIT_TEST( ThreeColConstructor );
+  CPPUNIT_TEST( numEntries );
+  CPPUNIT_TEST( reshapeMatrix );
+  CPPUNIT_TEST( accessMatrix );
+  CPPUNIT_TEST( print );
+  CPPUNIT_TEST( fillMatrix );
+  CPPUNIT_TEST( transposeMatrix );
+  CPPUNIT_TEST( setDiagMatrix );
+  CPPUNIT_TEST( trMatrix );
+  CPPUNIT_TEST( zeroMatrix );
+  CPPUNIT_TEST( identityMatrix );
+  CPPUNIT_TEST( contractionMatrix );
+  CPPUNIT_TEST( normMatrix );
+  CPPUNIT_TEST( tensorAssignOperator );
+  CPPUNIT_TEST( matrixAssignOperator );
   CPPUNIT_TEST( multMatrixScalar );
-  CPPUNIT_TEST( multMatrixScalarEquals );
-  CPPUNIT_TEST( multMatrixMatrix );
   CPPUNIT_TEST( multMatrixVec );
+  CPPUNIT_TEST( multMatrixMatrix );
+  CPPUNIT_TEST( addMatrixMatrix );
+  CPPUNIT_TEST( addMatrixMatrixEquals );
+  CPPUNIT_TEST( subMatrixMatrixEquals );
+  CPPUNIT_TEST( addMatrixScalar );
+  CPPUNIT_TEST( divideMatrixScalarEquals );
+  CPPUNIT_TEST( multMatrixScalarEquals );
+  CPPUNIT_TEST( addMatrixScalarEquals );
+  CPPUNIT_TEST( equalMatrix );
+  CPPUNIT_TEST( notEqualMatrix );
+
   CPPUNIT_TEST_SUITE_END();
   
 public:
   void setUp();
   void tearDown();
   
-  void addMatrixScalar();
-  void addMatrixScalarEquals();
+  void rowColConstructor();
+  void copyConstructor();
+  void tensorConstructor();
+  void ThreeColConstructor();
+  void numEntries();
+  void reshapeMatrix();
+  void accessMatrix();
+  void print();
+  void fillMatrix();
+  void transposeMatrix();
+  void setDiagMatrix();
+  void trMatrix();
+  void zeroMatrix();
+  void identityMatrix();
+  void contractionMatrix();
+  void normMatrix();
+  void tensorAssignOperator();
+  void matrixAssignOperator();
   void multMatrixScalar();
-  void multMatrixScalarEquals();
-  void multMatrixMatrix();
   void multMatrixVec();
+  void multMatrixMatrix();
+  void addMatrixMatrix();
+  void addMatrixMatrixEquals();
+  void subMatrixMatrixEquals();
+  void addMatrixScalar();
+  void divideMatrixScalarEquals();
+  void multMatrixScalarEquals();
+  void addMatrixScalarEquals();
+  void equalMatrix();
+  void notEqualMatrix();
 
 private:
-  ColumnMajorMatrix *a, *t; 
+  ColumnMajorMatrix *a, *t, *two_mat; 
+  ColumnMajorMatrix *add, *add_solution;
+  ColumnMajorMatrix *sub, *sub_solution;
 };
 
 #endif  // COLUMNMAJORMATRIXTEST_H
--- unit/src/ColumnMajorMatrixTest.C
+++ unit/src/ColumnMajorMatrixTest.C
@@ -5,8 +5,9 @@ CPPUNIT_TEST_SUITE_REGISTRATION( ColumnMajorMatrixTest );
 void
 ColumnMajorMatrixTest::setUp()
 {
+  // Define commonly used matrices for testing
   a = new ColumnMajorMatrix(3, 3);
-  ColumnMajorMatrix &a_ref = *a;
+  ColumnMajorMatrix & a_ref = *a;
   
   a_ref(0, 0) = 1;
   a_ref(1, 0) = 2;
@@ -19,7 +20,7 @@ ColumnMajorMatrixTest::setUp()
   a_ref(2, 2) = 9;
 
   t = new ColumnMajorMatrix(3, 2);
-  ColumnMajorMatrix &t_ref = *t;
+  ColumnMajorMatrix & t_ref = *t;
 
   t_ref(0, 0) = 1;
   t_ref(1, 0) = 2;
@@ -27,6 +28,38 @@ ColumnMajorMatrixTest::setUp()
   t_ref(0, 1) = 4;
   t_ref(1, 1) = 5;
   t_ref(2, 1) = 6;
+
+  two_mat = new ColumnMajorMatrix(2, 2);
+  ColumnMajorMatrix & mat = *two_mat;
+
+  mat(0, 0) = 1;
+  mat(1, 0) = 2;
+  mat(0, 1) = 3;
+  mat(1, 1) = 4;
+
+  add = new ColumnMajorMatrix(3, 2);
+  add_solution = new ColumnMajorMatrix(3, 2);
+  ColumnMajorMatrix & add_ref = *add;
+  ColumnMajorMatrix & a_sol_ref = *add_solution;
+
+  add_ref(0, 0) = 6; a_sol_ref(0, 0) = 7;
+  add_ref(1, 0) = 5; a_sol_ref(1, 0) = 7;
+  add_ref(2, 0) = 4; a_sol_ref(2, 0) = 7;
+  add_ref(0, 1) = 1; a_sol_ref(0, 1) = 5;
+  add_ref(1, 1) = 1; a_sol_ref(1, 1) = 6;
+  add_ref(2, 1) = 1; a_sol_ref(2, 1) = 7;
+
+  sub = new ColumnMajorMatrix(3, 2);
+  sub_solution = new ColumnMajorMatrix(3, 2);
+  ColumnMajorMatrix & sub_ref = *sub;
+  ColumnMajorMatrix & s_sol_ref = *sub_solution;
+
+  sub_ref(0, 0) = 0; s_sol_ref(0, 0) = 1;
+  sub_ref(1, 0) = 1; s_sol_ref(1, 0) = 1;
+  sub_ref(2, 0) = 2; s_sol_ref(2, 0) = 1;
+  sub_ref(0, 1) = 1; s_sol_ref(0, 1) = 3;
+  sub_ref(1, 1) = 1; s_sol_ref(1, 1) = 4;
+  sub_ref(2, 1) = 1; s_sol_ref(2, 1) = 5;
 }
 
 void
@@ -34,6 +67,11 @@ ColumnMajorMatrixTest::tearDown()
 {
   delete a;
   delete t;
+  delete add;
+  delete add_solution;
+  delete sub;
+  delete sub_solution;
+  delete two_mat;
 }
 
 void
@@ -52,6 +90,23 @@ ColumnMajorMatrixTest::addMatrixScalar()
   add_solution(2, 2) = 19;
 
   CPPUNIT_ASSERT(add_solution == *a + 10);
+}
+
+void
+ColumnMajorMatrixTest::divideMatrixScalarEquals()
+{
+  ColumnMajorMatrix divide(2, 2);
+
+  divide(0, 0) = 2;
+  divide(1, 0) = 4;
+  divide(0, 1) = 6;
+  divide(1, 1) = 8;
+
+  divide /= 2;
+  CPPUNIT_ASSERT( divide(0,0) == 1 );
+  CPPUNIT_ASSERT( divide(1,0) == 2 );
+  CPPUNIT_ASSERT( divide(0,1) == 3 );
+  CPPUNIT_ASSERT( divide(1,1) == 4 );
 }
 
 void
@@ -145,4 +200,299 @@ ColumnMajorMatrixTest::multMatrixVec()
   mult_solution(2, 0) = 42;
 
   CPPUNIT_ASSERT(mult_solution == *a * vec);
+}
+
+void
+ColumnMajorMatrixTest::reshapeMatrix()
+{
+  ColumnMajorMatrix mat(3, 2);
+
+  mat(0, 0) = 1;
+  mat(1, 0) = 2;
+  mat(2, 0) = 3;
+  mat(0, 1) = 4;
+  mat(1, 1) = 5;
+  mat(2, 1) = 6;
+  mat.reshape(2, 3);
+
+  /** from:
+  * 1 4 
+  * 2 5 
+  * 3 6 
+  *
+  * to:
+  *
+  * 1 3 5 
+  * 2 4 6 
+  */
+
+  CPPUNIT_ASSERT( mat(0,1) == 3 );
+  CPPUNIT_ASSERT( mat(1,2) == 6 );
+
+  mat.reshape(1, 6);
+
+  CPPUNIT_ASSERT( mat(0,1) == 2 );
+  CPPUNIT_ASSERT( mat(0,5) == 6 );
+}
+
+void
+ColumnMajorMatrixTest::setDiagMatrix()
+{
+  ColumnMajorMatrix mat = *two_mat;
+  mat.setDiag( 9 );
+
+  CPPUNIT_ASSERT( mat(0,0) == 9 );
+  CPPUNIT_ASSERT( mat(1,0) == 2 );
+  CPPUNIT_ASSERT( mat(0,1) == 3 );
+  CPPUNIT_ASSERT( mat(1,1) == 9 );
+}
+
+void
+ColumnMajorMatrixTest::trMatrix()
+{
+  CPPUNIT_ASSERT( a->tr() == 15 );
+  CPPUNIT_ASSERT( two_mat->tr() == 5 );
+}
+
+void
+ColumnMajorMatrixTest::zeroMatrix()
+{
+  ColumnMajorMatrix mat = *two_mat;
+  mat.zero();
+
+  CPPUNIT_ASSERT( mat(0,0) == 0 );
+  CPPUNIT_ASSERT( mat(1,0) == 0 );
+  CPPUNIT_ASSERT( mat(0,1) == 0 );
+  CPPUNIT_ASSERT( mat(1,1) == 0 );
+}
+
+void
+ColumnMajorMatrixTest::identityMatrix()
+{
+  ColumnMajorMatrix mat = *two_mat;
+  mat.identity();
+
+  CPPUNIT_ASSERT( mat(0,0) == 1 );
+  CPPUNIT_ASSERT( mat(1,0) == 0 );
+  CPPUNIT_ASSERT( mat(0,1) == 0 );
+  CPPUNIT_ASSERT( mat(1,1) == 1 );
+}
+
+void
+ColumnMajorMatrixTest::contractionMatrix()
+{
+  ColumnMajorMatrix mat = *two_mat;
+  ColumnMajorMatrix sec(2, 2);
+
+  sec(0, 0) = 4;
+  sec(1, 0) = 3;
+  sec(0, 1) = 2;
+  sec(1, 1) = 1;
+
+  CPPUNIT_ASSERT( mat.doubleContraction( sec ) == 21 );
+}
+
+void
+ColumnMajorMatrixTest::normMatrix()
+{
+  ColumnMajorMatrix mat(2, 2);
+  mat(0, 0) = 1;
+  mat(1, 0) = 2;
+  mat(0, 1) = 2;
+  mat(1, 1) = 4;
+
+  CPPUNIT_ASSERT( mat.norm() == 5 );
+}
+
+void
+ColumnMajorMatrixTest::transposeMatrix()
+{
+  ColumnMajorMatrix mat = *two_mat;
+  ColumnMajorMatrix test = mat.transpose();
+
+  CPPUNIT_ASSERT( test(0,0) == 1 );
+  CPPUNIT_ASSERT( test(1,0) == 3 );
+  CPPUNIT_ASSERT( test(0,1) == 2 );
+  CPPUNIT_ASSERT( test(1,1) == 4 );
+}
+
+void
+ColumnMajorMatrixTest::rowColConstructor()
+{
+  ColumnMajorMatrix mat = *two_mat;
+
+  CPPUNIT_ASSERT( mat(0,0) == 1 );
+  CPPUNIT_ASSERT( mat(1,0) == 2 );
+  CPPUNIT_ASSERT( mat(0,1) == 3 );
+  CPPUNIT_ASSERT( mat(1,1) == 4 );
+}
+
+void
+ColumnMajorMatrixTest::copyConstructor()
+{
+  ColumnMajorMatrix mat = *two_mat;
+  ColumnMajorMatrix test = mat;
+  test(1, 1) = 9;
+
+  CPPUNIT_ASSERT( test(0,0) == 1 );
+  CPPUNIT_ASSERT( test(1,0) == 2 );
+  CPPUNIT_ASSERT( test(0,1) == 3 );
+  CPPUNIT_ASSERT( test(1,1) == 9 );
+  CPPUNIT_ASSERT( mat(1,1) == 4 );
+
+  CPPUNIT_ASSERT( test.numEntries() == 4 );
+}
+
+void
+ColumnMajorMatrixTest::tensorConstructor()
+{
+  TensorValue<Real> tensor( 1, 4, 7,
+                            2, 5, 8,
+                            3, 6, 9 );
+  ColumnMajorMatrix test( tensor );
+
+  CPPUNIT_ASSERT( test == *a );
+  CPPUNIT_ASSERT( test.numEntries() == 9 );
+}
+
+void
+ColumnMajorMatrixTest::ThreeColConstructor()
+{
+  VectorValue<Real> col1( 1, 2, 3 );
+  VectorValue<Real> col2( 4, 5, 6 );
+  VectorValue<Real> col3( 7, 8, 9 );
+
+  ColumnMajorMatrix test( col1, col2, col3 );
+
+  CPPUNIT_ASSERT( test == *a );
+  CPPUNIT_ASSERT( test.numEntries() == 9 );
+}
+
+void
+ColumnMajorMatrixTest::numEntries()
+{
+  //numEntries is tested in other functions, like after different
+  //constructors to make sure the number of entries copied over correctly
+  ColumnMajorMatrix mat = *two_mat;
+  ColumnMajorMatrix mat2(3, 4);
+
+  CPPUNIT_ASSERT( mat.numEntries() == 4 );
+  CPPUNIT_ASSERT( mat2.numEntries() == 12 );
+}
+
+void
+ColumnMajorMatrixTest::accessMatrix()
+{
+  //tests operator()
+  ColumnMajorMatrix mat = *two_mat;
+
+  CPPUNIT_ASSERT( mat(0,0) == 1 );
+  CPPUNIT_ASSERT( mat(1,0) == 2 );
+  CPPUNIT_ASSERT( mat(0,1) == 3 );
+  CPPUNIT_ASSERT( mat(1,1) == 4 );
+}
+
+void
+ColumnMajorMatrixTest::print()
+{
+  //TODO?
+}
+
+void
+ColumnMajorMatrixTest::fillMatrix()
+{
+  TensorValue<Real> tensor( 0, 0, 0, 0, 0, 0, 0, 0, 0 );
+  ColumnMajorMatrix & a_ref = *a;
+  a_ref.fill( tensor );
+
+  CPPUNIT_ASSERT( tensor(0, 0) == a_ref(0, 0) );
+  CPPUNIT_ASSERT( tensor(1, 0) == a_ref(1, 0) );
+  CPPUNIT_ASSERT( tensor(2, 0) == a_ref(2, 0) );
+  CPPUNIT_ASSERT( tensor(0, 1) == a_ref(0, 1) );
+  CPPUNIT_ASSERT( tensor(1, 1) == a_ref(1, 1) );
+  CPPUNIT_ASSERT( tensor(2, 1) == a_ref(2, 1) );
+  CPPUNIT_ASSERT( tensor(0, 2) == a_ref(0, 2) );
+  CPPUNIT_ASSERT( tensor(1, 2) == a_ref(1, 2) );
+  CPPUNIT_ASSERT( tensor(2, 2) == a_ref(2, 2) );
+}
+
+void
+ColumnMajorMatrixTest::tensorAssignOperator()
+{
+  ColumnMajorMatrix test(3, 3);
+  TensorValue<Real> tensor( 1, 4, 7,
+                            2, 5, 8,
+                            3, 6, 9 );
+  test = tensor;
+
+  CPPUNIT_ASSERT( test == *a );
+  CPPUNIT_ASSERT( test.numEntries() == 9 );
+}
+
+void
+ColumnMajorMatrixTest::matrixAssignOperator()
+{
+  ColumnMajorMatrix mat = *two_mat;
+  ColumnMajorMatrix test(1, 2);
+
+  test = mat;
+  test(1, 1) = 9;
+
+  CPPUNIT_ASSERT( test(0,0) == 1 );
+  CPPUNIT_ASSERT( test(1,0) == 2 );
+  CPPUNIT_ASSERT( test(0,1) == 3 );
+  CPPUNIT_ASSERT( test(1,1) == 9 );
+  CPPUNIT_ASSERT( mat(1,1) == 4 );
+
+  CPPUNIT_ASSERT( test.numEntries() == 4 );
+}
+
+void
+ColumnMajorMatrixTest::addMatrixMatrix()
+{
+  CPPUNIT_ASSERT( *t + *add == *add_solution );
+}
+
+void
+ColumnMajorMatrixTest::addMatrixMatrixEquals()
+{
+  ColumnMajorMatrix mat = *t;
+  mat += *add;
+
+  CPPUNIT_ASSERT( mat == *add_solution );
+}
+
+void
+ColumnMajorMatrixTest::subMatrixMatrixEquals()
+{
+  ColumnMajorMatrix mat = *t;
+  mat -= *sub;
+
+  CPPUNIT_ASSERT( mat == *sub_solution );
+}
+
+void
+ColumnMajorMatrixTest::equalMatrix()
+{
+  ColumnMajorMatrix mat(1, 1);
+  ColumnMajorMatrix mat1(1, 1);
+
+  mat(0, 0) = 2;
+  mat1(0, 0) = 2;
+
+  CPPUNIT_ASSERT( !(*a == *t) );
+  CPPUNIT_ASSERT( mat == mat1 );
+}
+
+void
+ColumnMajorMatrixTest::notEqualMatrix()
+{
+  ColumnMajorMatrix mat(1, 1);
+  ColumnMajorMatrix mat1(1, 1);
+
+  mat(0, 0) = 2;
+  mat1(0, 0) = 2;
+
+  CPPUNIT_ASSERT( *a != *t );
+  CPPUNIT_ASSERT( !(mat != mat1) );
 }
