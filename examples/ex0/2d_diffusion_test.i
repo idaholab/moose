a226db8c9e16fdfb119d276ad821231f9144ffc4
Cody Permann <cody.permann@inl.gov>
2010-06-09 18:04:41 +0000

    Adding smallest MOOSE based application
    r1914

---
+++ examples/ex0/2d_diffusion_test.i
@@ -0,0 +1,71 @@
+[Mesh]
+  dim = 2
+  file = square.e
+  uniform_refine = 4
+[]
+
+[Variables]
+  active = 'u'
+
+  [./u]
+    order = FIRST
+    family = LAGRANGE
+  [../]
+
+# This variable is not active in the list above 
+# therefore it is not used in the simulation
+  [./v]
+    order = FIRST
+    family = LAGRANGE
+  [../]
+[]
+
+[Kernels]
+  active = 'diff'
+
+  [./diff]
+    type = Diffusion
+    variable = u
+  [../]
+[]
+
+[BCs]
+  active = 'left right'
+
+  [./left]
+    type = DirichletBC
+    variable = u
+    boundary = '1 3'
+    value = 0
+  [../]
+
+  [./right]
+    type = DirichletBC
+    variable = u
+    boundary = '2 4'
+    value = 1
+  [../]
+[]
+
+[Materials]
+  active = empty
+
+  [./empty]
+    type = EmptyMaterial
+    block = 1
+  [../]
+[]
+
+[Executioner]
+	type = Steady
+	perf_log = true
+	petsc_options = '-snes_mf_operator'
+[]
+
+[Output]
+  file_base = out
+  interval = 1
+  exodus = true
+[]
+   
+    
---
+++ examples/ex0/src/main.C
@@ -0,0 +1,53 @@
+//Moose Includes
+#include "Parser.h"
+#include "Executioner.h"
+#include "MooseSystem.h"
+
+// C++ include files
+#include <iostream>
+
+// libMesh includes
+#include "perf_log.h"
+
+// Create a performance log
+PerfLog Moose::perf_log("Example 0");
+
+ // Begin the main program.
+int main (int argc, char** argv)
+{
+  // Create a MooseInit Object
+  MooseInit init (argc, argv);
+
+  // Create a single MooseSystem which can hold
+  // a single nonlinear system and single auxillary system
+  MooseSystem moose_system;
+
+  // Register a bunch of common objects that exist inside of Moose.
+  // You will generally create a registerObjects method of your own
+  // to register modules that you create in your own application where
+  // you will generally call this method.
+  Moose::registerObjects();
+
+  // Create the input file parser which takes a reference to the main
+  // MooseSystem
+  Parser p(moose_system);
+
+  // Do some bare minimum command line parsing to look for a filename
+  // to parse
+  std::string input_filename = "";
+  if ( Moose::command_line->search("-i") )
+    input_filename = Moose::command_line->next(input_filename);
+  else
+    mooseError("Must specify an input file using -i");      
+
+  // Tell the parser to parse the given file to setup the simulation and execute
+  p.parse(input_filename);
+  p.execute();
+
+  if(!Moose::executioner)
+    mooseError("Executioner not supplied!");
+
+  // Run the executioner once the problem has been setup by the parser
+  Moose::executioner->setup();
+  Moose::executioner->execute();
+}
