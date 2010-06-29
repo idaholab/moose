409940098a14e02d8630ecb250b72967ca1538e2
Cody Permann <cody.permann@inl.gov>
2010-06-29 11:37:38 +0000

    Adding a Moose Test Application
    r1972

---
+++ test/src/main.C
@@ -0,0 +1,42 @@
+/**
+ * Moose Test Application
+ */
+
+//Moose Includes
+#include "Parser.h"
+#include "Executioner.h"
+#include "MooseSystem.h"
+
+// C++ include files
+#include <iostream>
+
+// libMesh includes
+#include "perf_log.h"
+
+// Create a performance log
+PerfLog Moose::perf_log("Moose Test");
+
+ // Begin the main program.
+int main (int argc, char** argv)
+{
+  MooseInit init (argc, argv);
+  MooseSystem moose_system;
+
+  Moose::registerObjects();
+  Parser p(moose_system);
+
+  std::string input_filename = "";
+  if ( Moose::command_line->search("-i") )
+    input_filename = Moose::command_line->next(input_filename);
+  else
+    mooseError("Must specify an input file using -i");
+  
+  p.parse(input_filename);
+  p.execute();
+
+  if(!Moose::executioner)
+    mooseError("Executioner not supplied!");
+  
+  Moose::executioner->setup();
+  Moose::executioner->execute();
+}
