121151a34139a6d02fb89f764bbdf4bed36b7c53
Michael Tonks <michael.tonks@inl.gov>
2010-07-20 14:34:17 +0000

    changes to several files
    r2083

--- modules/phase_field/include/utils/MathUtils.h
+++ modules/phase_field/include/utils/MathUtils.h
@@ -15,12 +15,12 @@ namespace MathUtils
   inline Real polyLog(Real x)
   {
     Real tol = 1.e-4;
-    Real c1=-1.0e8;
+    Real c1=-5.0e7;
     Real c2=2.0e4;
     Real c3=-10.71034037197618;
 
     if(x<tol)
-      return c1*x*x/2.0 + c2*x + c3;
+      return c1*x*x + c2*x + c3;
     else
       return std::log(x);
   }
@@ -28,11 +28,11 @@ namespace MathUtils
   inline Real dpolyLog(Real x)
   {
     Real tol = 1.e-4;
-    Real c1=-1.0e8;
+    Real c1=-5.0e7;
     Real c2=2.0e4;
 
     if(x<tol)
-      return c1*x + c2;
+      return 2.0*c1*x + c2;
     else
       return 1.0/x;
   }
@@ -40,10 +40,10 @@ namespace MathUtils
   inline Real d2polyLog(Real x)
   {
     Real tol = 1.e-4;
-    Real c1=-1.0e8;
+    Real c1=-5.0e7;
 
     if(x<tol)
-      return c1;
+      return 2.0*c1;
     else
       return -1.0/(x*x);
   }
@@ -51,12 +51,12 @@ namespace MathUtils
   inline Real polyLogLT(Real x)
   {
     Real tol = 1.e-2;
-    Real c1=-1.0e4;
+    Real c1=-5.0e3;
     Real c2=2.0e2;
     Real c3=-6.105170185988091;
 
     if(x<tol)
-      return c1*x*x/2.0 + c2*x + c3;
+      return c1*x*x + c2*x + c3;
     else
       return std::log(x);
   }
@@ -64,11 +64,11 @@ namespace MathUtils
   inline Real dpolyLogLT(Real x)
   {
     Real tol = 1.e-2;
-    Real c1=-1.0e4;
+    Real c1=-5.0e3;
     Real c2=2.0e2;
 
     if(x<tol)
-      return c1*x + c2;
+      return 2.0*c1*x + c2;
     else
       return 1.0/x;
   }
@@ -76,13 +76,44 @@ namespace MathUtils
   inline Real d2polyLogLT(Real x)
   {
     Real tol = 1.e-2;
-    Real c1=-1.0e4;
+    Real c1=-5.0e3;
 
     if(x<tol)
-      return c1;
+      return 2.0*c1;
     else
       return -1.0/(x*x);
   }
+
+  inline Real advPolyLog(Real x, Real tol, int order)
+  {
+    Real c1 = 1.0/(3.0*tol*tol*tol);
+    Real c2 = -3.0/(2.0*tol*tol);
+    Real c3 = 3.0/tol;
+    Real c4 = std::log(tol)-11.0/6.0;
+
+    if(order==0)
+    {
+      if(x<tol)
+        return c1*x*x*x + c2*x*x + c3*x + c4;
+      else
+        return std::log(x);
+    }
+    else if(order==1)
+    {
+      if(x<tol)
+        return 3.0*c1*x*x + 2.0*c2*x + c3;
+      else
+        return 1.0/x;
+    }
+    else if(order==2)
+    {
+      if(x<tol)
+        return 6.0*c1*x + 2.0*c2;
+      else
+        return -1.0/(x*x);
+    }
+  }
+  
 }
 
 #endif //MATHUTILS_H
