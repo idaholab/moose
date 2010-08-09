d2ad3deb0758703c42bfbf1c353aa460bc9ee04d
Alexandra Gertman <alexandra.gertman@inl.gov>
2010-08-09 09:44:32 +0000

    added a NeumannBC test to 2d_diffusion_test
    r2181

--- test/tests/2d_diffusion_test/2d_diffusion_test.py
+++ test/tests/2d_diffusion_test/2d_diffusion_test.py
@@ -2,3 +2,6 @@ import tools
 
 def test(dofs=0, np=0):
   tools.executeAppAndDiff(__file__,'2d_diffusion_test.i',['out.e'], dofs, np)
+
+def test(dofs=0, np=0):
+  tools.executeAppAndDiff(__file__,'2d_diffusion_neumannbc_test.i',['neumannbc_out.e'], dofs, np)
