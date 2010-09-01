a1d3705cee25b31f00d837e77e006dee2f91946b
Michael Tonks <michael.tonks@inl.gov>
2010-09-01 09:49:26 +0000

    r2305 dd8cd9ef-2931-0410-98ca-75ad22d19dd1

--- modules/phase_field/include/utils/MathUtils.h
+++ modules/phase_field/include/utils/MathUtils.h
@@ -5,83 +5,45 @@
 
 namespace MathUtils 
 {
-  inline Real badLog(Real x)
+
+  inline Real polyLog(Real x, Real tol, int deriv)
   {
-    Real y = (x - 1.) / (x + 1.);
-    Real sqy= y*y;
-    return 2*y*( 1. + sqy*(1./3. + sqy*(1./5. + sqy*(1./7. + sqy*(1./9. + sqy*(1./11. + sqy*(1./13. + sqy*1./15)))))));
-  }
+    Real c1=-0.5/(tol*tol);
+    Real c2=2.0/tol;
+    Real c3=std::log(tol) - 3.0/2.0;
 
-  inline Real polyLog(Real x)
-  {
-    Real tol = 1.e-4;
-    Real c1=-5.0e7;
-    Real c2=2.0e4;
-    Real c3=-10.71034037197618;
-
-    if(x<tol)
-      return c1*x*x + c2*x + c3;
-    else
-      return std::log(x);
-  }
-
-  inline Real dpolyLog(Real x)
-  {
-    Real tol = 1.e-4;
-    Real c1=-5.0e7;
-    Real c2=2.0e4;
-
-    if(x<tol)
-      return 2.0*c1*x + c2;
-    else
-      return 1.0/x;
-  }
-
-  inline Real d2polyLog(Real x)
-  {
-    Real tol = 1.e-4;
-    Real c1=-5.0e7;
-
-    if(x<tol)
-      return 2.0*c1;
-    else
-      return -1.0/(x*x);
-  }
-
-  inline Real polyLogLT(Real x)
-  {
-    Real tol = 1.e-2;
-    Real c1=-5.0e3;
-    Real c2=2.0e2;
-    Real c3=-6.105170185988091;
-
-    if(x<tol)
-      return c1*x*x + c2*x + c3;
-    else
-      return std::log(x);
-  }
-
-  inline Real dpolyLogLT(Real x)
-  {
-    Real tol = 1.e-2;
-    Real c1=-5.0e3;
-    Real c2=2.0e2;
-
-    if(x<tol)
-      return 2.0*c1*x + c2;
-    else
-      return 1.0/x;
-  }
-
-  inline Real d2polyLogLT(Real x)
-  {
-    Real tol = 1.e-2;
-    Real c1=-5.0e3;
-
-    if(x<tol)
-      return 2.0*c1;
-    else
-      return -1.0/(x*x);
+    Real value=0.0;
+      
+    if (deriv == 0)
+    {
+      if(x<tol)
+        value =  c1*x*x + c2*x + c3;
+      else
+        value =  std::log(x);
+    }
+    else if (deriv == 1)
+    {
+      if(x<tol)
+        value =  2.0*c1*x + c2;
+      else
+        value =  1.0/x;
+    }
+    else if (deriv == 2)
+    {
+      if(x<tol)
+        value =  2.0*c1;
+      else
+        value =  -1.0/(x*x);
+    }
+    else if (deriv == 3)
+    {
+      if(x<tol)
+        value =  0.0;
+      else
+        value =  2.0/(x*x*x);
+    }
+    
+    return value;
   }
 
   inline Real advPolyLog(Real x, Real tol, int order)
@@ -91,27 +53,37 @@ namespace MathUtils
     Real c3 = 3.0/tol;
     Real c4 = std::log(tol)-11.0/6.0;
 
+    Real value = 0.0;
+
     if(order==0)
     {
       if(x<tol)
-        return c1*x*x*x + c2*x*x + c3*x + c4;
+        value =  c1*x*x*x + c2*x*x + c3*x + c4;
       else
-        return std::log(x);
+        value =  std::log(x);
     }
     else if(order==1)
     {
       if(x<tol)
-        return 3.0*c1*x*x + 2.0*c2*x + c3;
+        value =  3.0*c1*x*x + 2.0*c2*x + c3;
       else
-        return 1.0/x;
+        value =  1.0/x;
     }
     else if(order==2)
     {
       if(x<tol)
-        return 6.0*c1*x + 2.0*c2;
+        value =  6.0*c1*x + 2.0*c2;
       else
-        return -1.0/(x*x);
+        value =  -1.0/(x*x);
     }
+    else if(order==3)
+    {
+      if(x<tol)
+        value =  6.0*c1;
+      else
+        value =  2.0/(x*x*x);
+    }
+    return value;
   }
   
 }
