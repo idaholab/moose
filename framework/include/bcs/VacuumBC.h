dc28d0aeedd35049f6972430a2009f33bbd681ab
Cody Permann <cody.permann@inl.gov>
2010-09-02 09:51:51 +0000

    Commiting Copyrighted version of MOOSE
    r2335

--- framework/include/bcs/VacuumBC.h
+++ framework/include/bcs/VacuumBC.h
@@ -1,3 +1,17 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Orieneted Simulation Environment */
+/*                                                              */
+/*            @ 2010 Battelle Energy Alliance, LLC              */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
 #ifndef VACUUMBC_H
 #define VACUUMBC_H
 
---
+++ framework/scripts/fixup_headers.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import os, string
+from optparse import OptionParser
+
+global_ignores = ['fparser', '.svn']
+copyright_header = \
+"""/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Orieneted Simulation Environment */
+/*                                                              */
+/*            @ 2010 Battelle Energy Alliance, LLC              */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+"""
+
+def fixupHeader():
+  for dirpath, dirnames, filenames in os.walk(os.getcwd() + "/../"):
+
+    # Don't traverse into ignored directories
+    for ignore in global_ignores:
+      if ignore in dirnames:
+        dirnames.remove(ignore)
+
+    print dirpath
+    print dirnames
+    for file in filenames:
+      suffix = os.path.splitext(file)
+      if suffix[-1] == '.C' or suffix[-1] == '.h':
+        checkAndUpdate(dirpath + '/' + file)
+          
+
+def checkAndUpdate(filename):
+  f = open(filename)
+  text = f.read()
+  f.close()
+
+  # Check (exact match only)
+  if (string.find(text, copyright_header) == -1):
+    # Update
+    f = open(filename + "~tmp", "w")
+    f.write(copyright_header)
+    f.write(text)
+    f.close()
+
+    os.rename(filename + "~tmp", filename)
+  
+if __name__ == '__main__':
+  fixupHeader()
+
+
