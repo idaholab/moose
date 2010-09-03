c48ec61d118fb65d9bb0ed028aeab124eb8a3a0d
Derek Gaston <derek.gaston@inl.gov>
2010-09-03 12:29:20 +0000

    fix test
    r3148

--- unit/src/ParsedFunctionTest.C
+++ unit/src/ParsedFunctionTest.C
@@ -16,7 +16,7 @@ ParsedFunctionTest::basicConstructor()
   //test constructor with no additional variables
   params.set<std::string>("function") = std::string("x + 1.5*y + 2 * z + t/4");
   ParsedFunction f("test", m, params);
-  CPPUNIT_ASSERT(f(4, 1, 2, 3) == 11);
+  CPPUNIT_ASSERT(f.value(4, 1, 2, 3) == 11);
 }
 
 void
@@ -34,7 +34,7 @@ ParsedFunctionTest::advancedConstructor()
 
   ParsedFunction f("test", m, params);
   f.getVarAddr("q") = 4;
-  CPPUNIT_ASSERT( f(0, 1, 2) == 7 );
+  CPPUNIT_ASSERT( f.value(0, 1, 2) == 7 );
 
   //test the constructor with three variables
   std::vector<std::string> three_vars(3);
@@ -50,7 +50,7 @@ ParsedFunctionTest::advancedConstructor()
   f2.getVarAddr("q") = 4;
   f2.getVarAddr("w") = 2;
   f2.getVarAddr("r") = 1.5;
-  CPPUNIT_ASSERT( f2(0, 2, 4) == 9 );
+  CPPUNIT_ASSERT( f2.value(0, 2, 4) == 9 );
 
   //test the constructor with one variable that's set
   std::vector<Real> one_val(1);
@@ -62,7 +62,7 @@ ParsedFunctionTest::advancedConstructor()
   params3.set<std::vector<Real> >("vals") = one_val;
 
   ParsedFunction f3("test", m, params3);
-  CPPUNIT_ASSERT( f3(0,2) == 5 );
+  CPPUNIT_ASSERT( f3.value(0,2) == 5 );
 
   //test the constructor with three variables, two that are set
   std::vector<Real> two_vals(2);
@@ -76,9 +76,9 @@ ParsedFunctionTest::advancedConstructor()
 
   ParsedFunction f4("test", m, params4);
   f4.getVarAddr("r") = 2;
-  CPPUNIT_ASSERT( f4(0, 2, 4) == 6 );
+  CPPUNIT_ASSERT( f4.value(0, 2, 4) == 6 );
   f4.getVarAddr("r") = 4;
-  CPPUNIT_ASSERT( f4(0, 2, 4) == 5 );
+  CPPUNIT_ASSERT( f4.value(0, 2, 4) == 5 );
 }
 
 void
@@ -98,11 +98,11 @@ ParsedFunctionTest::testVariables()
   ParsedFunction f("test", m, params);
   Real & q = f.getVarAddr("q");
   q = 4;
-  CPPUNIT_ASSERT( f(0, 1, 2) == 7 );
+  CPPUNIT_ASSERT( f.value(0, 1, 2) == 7 );
   q = 2;
-  CPPUNIT_ASSERT( f(0, 1, 2) == 5 );
+  CPPUNIT_ASSERT( f.value(0, 1, 2) == 5 );
   q = -4;
-  CPPUNIT_ASSERT( f(0, 1, 2) == -1 );
+  CPPUNIT_ASSERT( f.value(0, 1, 2) == -1 );
 
   //test three variables, test updating them randomly
   std::vector<std::string> three_vars(3);
@@ -119,13 +119,13 @@ ParsedFunctionTest::testVariables()
   Real & w2 = f2.getVarAddr("w");
   Real & r2 = f2.getVarAddr("r");
   q2 = 4; w2 = 2; r2 = 1.5;
-  CPPUNIT_ASSERT( f2(0, 2, 4) == 9 );
+  CPPUNIT_ASSERT( f2.value(0, 2, 4) == 9 );
   q2 = 1; w2 = 4; r2 = 2.5;
-  CPPUNIT_ASSERT( f2(0, 2, 4) == 7 );
+  CPPUNIT_ASSERT( f2.value(0, 2, 4) == 7 );
   q2 = 2;
-  CPPUNIT_ASSERT( f2(0, 2, 4) == 8 );
+  CPPUNIT_ASSERT( f2.value(0, 2, 4) == 8 );
   w2 = 3;
-  CPPUNIT_ASSERT( f2(0, 2, 6) == 9 );
+  CPPUNIT_ASSERT( f2.value(0, 2, 6) == 9 );
 }
 
 void
@@ -139,13 +139,13 @@ ParsedFunctionTest::testConstants()
   params.set<std::string>("function") = "log(e) + x";
 
   ParsedFunction f("test", m, params);
-  CPPUNIT_ASSERT_DOUBLES_EQUAL( 2, f(0,1), 0.0000001 );
+  CPPUNIT_ASSERT_DOUBLES_EQUAL( 2, f.value(0,1), 0.0000001 );
 
   InputParameters params2 = validParams<ParsedFunction>();
   params2.set<std::string>("function") = "sin(pi*x)";
 
   ParsedFunction f2("test", m, params2);
-  CPPUNIT_ASSERT_DOUBLES_EQUAL( 0, f2(0,1), 0.0000001 );
-  CPPUNIT_ASSERT_DOUBLES_EQUAL( 1, f2(0,0.5), 0.0000001 );
-  CPPUNIT_ASSERT_DOUBLES_EQUAL( -1, f2(0,1.5), 0.0000001 );
+  CPPUNIT_ASSERT_DOUBLES_EQUAL( 0, f2.value(0,1), 0.0000001 );
+  CPPUNIT_ASSERT_DOUBLES_EQUAL( 1, f2.value(0,0.5), 0.0000001 );
+  CPPUNIT_ASSERT_DOUBLES_EQUAL( -1, f2.value(0,1.5), 0.0000001 );
 }
