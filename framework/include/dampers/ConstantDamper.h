4171d59c4aeccadcd5d3938907aa49bfc640a2c3
Cody Permann <cody.permann@inl.gov>
2010-09-08 16:11:22 +0000

    Removing Duplicate headers - sorry :)
    r2384

--- framework/include/dampers/ConstantDamper.h
+++ framework/include/dampers/ConstantDamper.h
@@ -12,20 +12,6 @@
 /*            See COPYRIGHT for full restrictions               */
 /****************************************************************/
 
-/****************************************************************/
-/*               DO NOT MODIFY THIS HEADER                      */
-/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
-/*                                                              */
-/*           (c) 2010 Battelle Energy Alliance, LLC             */
-/*                   ALL RIGHTS RESERVED                        */
-/*                                                              */
-/*          Prepared by Battelle Energy Alliance, LLC           */
-/*            Under Contract No. DE-AC07-05ID14517              */
-/*            With the U. S. Department of Energy               */
-/*                                                              */
-/*            See COPYRIGHT for full restrictions               */
-/****************************************************************/
-
 #ifndef CONSTANTDAMPER_H
 #define CONSTANTDAMPER_H
 
