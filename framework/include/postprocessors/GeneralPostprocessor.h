4ff24e8910118cf5c9ba39fe8eeee4d16502c61c
Cody Permann <cody.permann@inl.gov>
2010-09-28 12:07:43 +0000

    Adding General Postprocessor Class
    r2450

---
+++ framework/include/postprocessors/GeneralPostprocessor.h
@@ -0,0 +1,39 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#ifndef GENERALPOSTPROCESSOR_H
+#define GENERALPOSTPROCESSOR_H
+
+#include "Postprocessor.h"
+#include "MooseObject.h"
+#include "PostprocessorInterface.h"
+
+//Forward Declarations
+class GeneralPostprocessor;
+
+template<>
+InputParameters validParams<GeneralPostprocessor>();
+
+/* This class is here to combine the Postprocessor interface and the
+ * base class Postprocessor object along with adding MooseObject to the inheritence tree*/
+class GeneralPostprocessor : public Postprocessor, public MooseObject, protected PostprocessorInterface
+{
+public:
+  GeneralPostprocessor(const std::string & name, MooseSystem &moose_system, InputParameters parameters);
+  
+  virtual ~GeneralPostprocessor()
+    {}
+};
+ 
+#endif
