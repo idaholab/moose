11bae33f9eaa0ac287deda69d956526b42646f20
Jason Hales <jason.hales@inl.gov>
2010-12-13 08:34:21 +0000

    Clean up unused parser parameters
    r2790

--- modules/heat_conduction/src/HeatConductionMaterial.C
+++ modules/heat_conduction/src/HeatConductionMaterial.C
@@ -6,25 +6,10 @@ InputParameters validParams<HeatConductionMaterial>()
   InputParameters params = validParams<Material>();
 
   params.addCoupledVar("temp", "Coupled Temperature");
-  
+
   params.set<Real>("thermal_conductivity")=1.0;
-  params.set<Real>("thermal_expansion")=1.0;
   params.set<Real>("specific_heat")=1.0;
   params.set<Real>("density")=1.0;
-  params.set<Real>("youngs_modulus")=1.0;
-  params.set<Real>("poissons_ratio")=1.0;
-  params.set<Real>("neutron_diffusion_coefficient")=1.0;
-  params.set<Real>("neutron_absorption_xs")=1.0;
-  params.set<Real>("neutron_fission_xs")=1.0;
-  params.set<Real>("neutron_per_fission")=1.0;
-  params.set<Real>("neutron_velocity")=1.0;
-  params.set<Real>("t_ref")=300;
-  params.set<Real>("d_1")=1.0;
-  params.set<Real>("d_2")=0.0;
-  params.set<Real>("q_1")=1.0;
-  params.set<Real>("q_2")=0.0;
-  params.set<Real>("gas_constant")=8.3143e-3;
-  params.set<Real>("lambda")=1.;
 
   return params;
 }
@@ -48,7 +33,7 @@ HeatConductionMaterial::HeatConductionMaterial(const std::string & name,
 void
 HeatConductionMaterial::computeProperties()
 {
-  for(unsigned int qp=0; qp<_n_qpoints; qp++)
+  for(unsigned int qp=0; qp<_n_qpoints; ++qp)
   {
     _density[qp] = _my_density;
     _thermal_conductivity[qp] = _my_thermal_conductivity;
