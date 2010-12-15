94de97d533227b0feb128f56fa5d4fb4db91a74d
Cody Permann <cody.permann@inl.gov>
2010-12-15 14:15:48 +0000

    Adding LineSegment class
    r2821

---
+++ framework/include/utils/LineSegment.h
@@ -0,0 +1,55 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#ifndef LINESEGMENT_H
+#define LINESEGMENT_H
+
+// libMesh includes
+#include "point.h"
+#include "plane.h"
+
+class LineSegment
+{
+ public:
+
+  LineSegment (const Point & p0, const Point & p1);
+
+  /**
+   * Returns the closest point on the LineSegment
+   * to the passed in point.  Note that the closest point may be
+   * one of the ends of the LineSegment.
+   */
+  Point closest_point (const Point & p);
+
+  /**
+   * Finds the closest point on the Line determined by the
+   * Line Segments.  Returns a boolean indicating whether
+   * that normal point is within the LineSegment or not
+   */
+  bool closest_normal_point (const Point & p, Point & closest_p);
+
+  /**
+   * Determines whether a point is in a line segment or not
+   */
+  bool contains_point (const Point & p);
+  
+  bool intersect(const Plane & pl, Point & intersect_p);
+    
+ private:
+  bool closest_point(const Point & p, bool clamp_to_segment, Point & closest_p);
+
+  Point _p0, _p1;
+};
+
+#endif // LINESEGMENT_H
---
+++ framework/src/utils/LineSegment.C
@@ -0,0 +1,100 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#include "LineSegment.h"
+#include "vector_value.h"
+
+LineSegment::LineSegment(const Point & p0, const Point & p1):
+  _p0(p0),
+  _p1(p1)
+{}
+
+bool
+LineSegment::closest_point (const Point & p, bool clamp_to_segment, Point & closest_p)
+{
+  Point p0_p  = p - _p0;
+  Point p0_p1 = _p1 - _p0;
+  Real p0_p1_2 = p0_p1.size_sq();
+  Real perp = p0_p(0)*p0_p1(0) + p0_p(1)*p0_p1(1) + p0_p(2)*p0_p1(2);
+  Real t = perp / p0_p1_2;
+  bool on_segment = true;
+
+  if (t < 0.0 || t > 1.0)
+    on_segment = false;
+
+  if (clamp_to_segment)
+  {
+    if (t < 0.0) t = 0.0;
+    else if (t > 1.0) t = 1.0;
+  }
+  
+  closest_p = _p0 + p0_p1 * t;
+  return on_segment;
+}
+
+Point
+LineSegment::closest_point (const Point & p)
+{
+  Point closest_p;
+  closest_point(p, true, closest_p);
+  return closest_p;
+}
+
+bool
+LineSegment::closest_normal_point (const Point & p, Point & closest_p)
+{
+  return closest_point(p, false, closest_p);
+}
+
+bool
+LineSegment::contains_point (const Point & p)
+{
+  Point closest_p;
+  return closest_point(p, false, closest_p);
+}
+
+bool
+LineSegment::intersect (const Plane & pl, Point & intersect_p)
+{
+  /**
+   * There are three cases in 3D for intersection of a line and a plane
+   * Case 1: The line is parallel to the plane - No intersection
+   *         Numerator = non-zero
+   *         Denominator = zero
+   *
+   * Case 2: The line is within the plane - Inf intersection
+   *         Numerator = zero
+   *         Denominator = zero
+   *
+   * Case 3: The line intersects the plane at a single point
+   *         Denominator = non-zero
+   */         
+
+  Point pl0 = pl.get_planar_point();
+  RealVectorValue N = pl.unit_normal(_p0);
+  RealVectorValue I = (_p1-_p0).unit();
+
+  Real numerator = (pl0-_p0)*N;
+  Real denominator = I*N;
+
+  // For now we will treat Case 2 as no intersection (not unique)
+  if (std::abs(denominator) < 1.e-10)
+    return false;
+
+  Real d = numerator / denominator;
+  intersect_p = d*I + _p0;
+  
+  return true;
+}
+
