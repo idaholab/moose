246505d4c07016f164fbea97fdbc62052d0f23d3
Cody Permann <cody.permann@inl.gov>
2011-01-04 07:51:14 +0000

    Removing call for need postprocessors in Plenum Pressure BC
    r2952

--- modules/solid_mechanics/src/PlenumPressureBC.C
+++ modules/solid_mechanics/src/PlenumPressureBC.C
@@ -33,7 +33,7 @@ PlenumPressureBC::PlenumPressureBC(const std::string & name, InputParameters par
    _output( getParam<std::string>("output") != "" ? &getPostprocessorValue(getParam<std::string>("output")) : NULL ),
    _my_value(0)
 {
-  _moose_system.needPostprocessorsForResiduals( true );
+  //_moose_system.needPostprocessorsForResiduals( true );
 
   if(_component < 0)
   {
