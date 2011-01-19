ea6d8bff20898b647c10e7ba947c65dcd84e1365
Cody Permann <cody.permann@inl.gov>
2011-01-19 12:54:57 +0000

    Fixing LineSegment Class and unit tests
    r3107

--- framework/src/utils/LineSegment.C
+++ framework/src/utils/LineSegment.C
@@ -64,7 +64,7 @@ bool
 LineSegment::contains_point (const Point & p) const
 {
   Point closest_p;
-  return closest_point(p, false, closest_p);
+  return closest_point(p, false, closest_p) && closest_p == p;
 }
 
 bool
@@ -91,10 +91,20 @@ LineSegment::intersect (const Plane & pl, Point & intersect_p) const
   Real numerator = (pl0-_p0)*N;
   Real denominator = I*N;
 
-  // For now we will treat Case 2 as no intersection (not unique)
+  
+  // The Line is parallel to the plane
   if (std::abs(denominator) < 1.e-10)
+  {
+    // The Line is on the plane
+    if (std::abs(numerator) < 1.e-10)
+    {
+      // The solution is not unique so we'll just pick an end point for now
+      intersect_p = _p0;
+      return true;
+    }
     return false;
-
+  }
+  
   // TODO: d can be used to determine whether the LineSegment intersects
   // the plane or if the Line determined by the Segment intersects the plane
   // d will fall between 0 and 1 if the intersection is within the line
@@ -109,7 +119,76 @@ bool
 LineSegment::intersect (const LineSegment & l, Point & intersect_p) const
 {
   /**
-   * There are three cases in 3D for intersection of two lines
+   * First check for concurance:
+   * 
+   * 
+   * | x1 y1 z1 1 |
+   * | x2 y2 z2 1 | = (x3 - x1) * [(x2-x1) x (x4-x3)] = 0
+   * | x3 y3 z3 1 |
+   * | x4 y4 z4 1 |
+   *
+   * 
+   * Solve:
+   *   x = _p0 + (_p1 - _p0)*s
+   *   x = l.p0 + (l._p1 - l.p0)*t
+   *
+   *   where
+   *   a = _p1 - _p0
+   *   b = l._p1 - l._p0
+   *   c = l._p0 - _p0
+   *   
+   *   s = (c x b) * (a x b) / | a x b |^2 
+   */
+  RealVectorValue a = _p1 - _p0;
+  RealVectorValue b = l._p1 - l._p0;
+  RealVectorValue c = l._p0 - _p0;
+  
+  RealVectorValue v = a.cross(b);
+  
+  // Check for parallel lines
+  if (std::abs(v.size()) < 1.e-10 && std::abs(c.cross(a).size()) < 1.e-10)
+  {
+    // TODO: The lines are co-linear but more work is needed to determine and intersection point
+    //       it could be the case that the line segments don't lie on the same line or overlap only
+    //       a bit
+    return true;
+  }
+
+  // Check that the lines are coplanar
+  Real concur = c * (a.cross(b));
+  if (std::abs(concur) > 1.e-10)
+    return false;
+  
+  Real s = (c.cross(b) * v) / (v*v);
+  Real t = (c.cross(a) * v) / (v*v); 
+
+  // if s and t are between 0 and 1 then the Line Segments intersect
+  // TODO: We could handle other case of clamping to the end of Line
+  //       Segements if we want to here
+
+  if (s >= 0 && s <= 1 && t >= 0 && t <= 1) 
+  {
+    intersect_p = _p0 + s*a;
+    return true;
+  }
+  return false;
+  
+  /**
+   * Parameteric Equation of lines
+   * _p0 + t(v0) = l._p0 + u(v1)
+   *
+   * Case 1: Parallel Lines
+   *         v0 x v1 == 0
+   *         
+   * Case 1a: Collinear Lines
+   *         v0 x v1 == 0
+   *         (l._p0 - _p0) x (_p1 - _p0) == 0
+   *
+   * Case 2: Intersecting Lines
+   *         0 <= t <= 1
+   *         0 <= u <= 1
+   *
+   * 
    * Case 1: The lines do not intersect
    *         vleft cross vright = non-zero
    *
@@ -120,26 +199,33 @@ LineSegment::intersect (const LineSegment & l, Point & intersect_p) const
    * Case 3: The line intersect at a single point
    *         vleft cross vright = zero
    *         vleft (Denominator) = non-zero
-   */         
   RealVectorValue v0 = _p1 - _p0;
   RealVectorValue v1 = l._p1 - l._p0;
   RealVectorValue v2 = l._p0 - _p0;
   
-  RealVectorValue vleft = v0.cross(v1);
-  RealVectorValue vright = v2.cross(v1);
+  RealVectorValue vbot = v0.cross(v1);
+  RealVectorValue vtop = v2.cross(v1);
 
-  // If vleft cross vright = zero || vleft is zero then there is not
-  // a unique intersection point
-  if (std::abs(vleft.size()) < 1.e-10 ||
-      std::abs(vleft.cross(vright).size()) > 1.e-10)
+  RealVectorValue crossed = vleft.cross(vright);
+
+  // Case 1: No intersection
+  if (std::abs(vleft.cross(vright).size()) > 1.e-10)
     return false;
 
-  //TODO: We could break out Case 2 differently if we want to detect
-  //      co-linear lines
+  // Case 2: Co-linear (just return one of the end points)
+  if (std::abs(vleft.size()) < 1.e-10)
+  {
+    intersect_p = _p0;
+    return true;
+  }
+  
+  // Case 3:
+  
   //TODO: We could detect whether the Line Segments actually overlap
   //      instead of whether the Lines are co-linear
 
   Real a = vright.size()/vleft.size();
   intersect_p = _p0 + a*v0;
   return true;
+     */         
 }
