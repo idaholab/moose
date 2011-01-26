19aa094d7bf0039015d1ca8c91fee7f7a1891687
Derek Gaston <derek.gaston@inl.gov>
2011-01-26 16:13:07 +0000

    fix test
    r3199

--- test/tests/constant_point_source_test/constant_point_source_test.py
+++ test/tests/constant_point_source_test/constant_point_source_test.py
@@ -1,7 +1,5 @@
 import tools
 
-def testdirichlet(dofs=0, np=0):
-  tools.executeAppAndDiff(__file__,'2d_diffusion_test.i',['out.e'], dofs, np)
+def test(dofs=0, np=0):
+  tools.executeAppAndDiff(__file__,'constant_point_source_test.i',['out.e'], dofs, np)
 
-def testneumann(dofs=0, np=0):
-  tools.executeAppAndDiff(__file__,'2d_diffusion_neumannbc_test.i',['neumannbc_out.e'], dofs, np)
