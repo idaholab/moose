e810025f9a0fa065f5fec30baf4b61b15d71898d
Cody Permann <cody.permann@inl.gov>
2011-02-09 14:43:42 +0000

    switch URL checkout method for stable script
    r3339

--- framework/scripts/updateStable.sh
+++ framework/scripts/updateStable.sh
@@ -7,10 +7,10 @@ then
   if [ $ARCH == '-gnu' ]
   then
     # Get the current stable version of moose
-    svn co --username moosetest --quiet https://hpcsc/svn/herd/branches/stable/moose stable-moose
+    svn co --quiet svn+ssh://hpcsc/herd/branches/stable/moose stable-moose
 
     # Merge trunk into the stable version
-    svn merge https://hpcsc/svn/herd/trunk/moose stable-moose
+    svn merge svn+ssh://hpcsc/herd/trunk/moose stable-moose
 
     # Check it in!
     svn ci --username moosetest stable-moose -m "Auto update at time `date`"
