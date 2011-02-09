be1b0f13986202276474f4e518062394cd91e490
Cody Permann <cody.permann@inl.gov>
2011-02-09 12:27:42 +0000

    Script update
    r3330

--- framework/scripts/updateStable.sh
+++ framework/scripts/updateStable.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-svn merge https://hpcsc/svn/herd/trunk/moose https://hpcsc/svn/herd/branches/stable/moose
+# Get the current stable version of moose
+svn co --quiet https://hpcsc/svn/herd/branches/stable/moose stable-moose
+
+# Find the version number
+version=`svn info stable-moose | perl -nle 'print $1 if /Last Changed Rev\D*(\d+)/'`
+
+# Merge the trunk into the local copy
+svn merge -r $version https://hpcsc/svn/herd/trunk/moose stable-moose
+
