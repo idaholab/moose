5338539c6e11470c96a6307b27ab51792ec27797
Cody Permann <cody.permann@inl.gov>
2011-02-09 13:27:25 +0000

    Update Stable script final - hopefully
    r3334

--- framework/scripts/updateStable.sh
+++ framework/scripts/updateStable.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-# Get the current stable version of moose
-svn co --quiet https://hpcsc/svn/herd/branches/stable/moose stable-moose
+hostname=`hostname -s`
 
-# Merge trunk into the stable version
-svn merge https://hpcsc/svn/herd/trunk/moose stable-moose
+if [ $hostname == 'helios' ]
+then
+  if [ $ARCH == '-gnu' ]
+  then
+    # Get the current stable version of moose
+    svn co --quiet https://hpcsc/svn/herd/branches/stable/moose stable-moose
 
-# Check it in!
-svn ci stable-moose
+    # Merge trunk into the stable version
+    svn merge https://hpcsc/svn/herd/trunk/moose stable-moose
+
+    # Check it in!
+    svn ci stable-moose -m "Auto update at time `date`"
+  fi 
+fi
+
 
