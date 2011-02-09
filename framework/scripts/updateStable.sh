69bec6de4911c39f48978c409eedd2ce61501616
Cody Permann <cody.permann@inl.gov>
2011-02-09 14:13:46 +0000

    one more update to stable script
    r3337

--- framework/scripts/updateStable.sh
+++ framework/scripts/updateStable.sh
@@ -7,13 +7,13 @@ then
   if [ $ARCH == '-gnu' ]
   then
     # Get the current stable version of moose
-    svn co --quiet https://hpcsc/svn/herd/branches/stable/moose stable-moose
+    svn co --username moosetest --quiet https://hpcsc/svn/herd/branches/stable/moose stable-moose
 
     # Merge trunk into the stable version
     svn merge https://hpcsc/svn/herd/trunk/moose stable-moose
 
     # Check it in!
-    svn ci stable-moose -m "Auto update at time `date`"
+    svn ci --username moosetest stable-moose -m "Auto update at time `date`"
   fi 
 fi
 
