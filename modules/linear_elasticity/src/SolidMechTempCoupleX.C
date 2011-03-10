814e877de2b42d5011e87464518e9967e9ad84bd
Jason Hales <jason.hales@inl.gov>
2011-03-10 08:35:34 +0000

    Whitespace
    r3562

--- modules/linear_elasticity/src/SolidMechTempCoupleX.C
+++ modules/linear_elasticity/src/SolidMechTempCoupleX.C
@@ -28,6 +28,6 @@ SolidMechTempCoupleX::computeQpOffDiagJacobian(unsigned int jvar)
 
     if(jvar == _temp_var)
       return -(_c1*(1+2*_c2)*_grad_test[_i][_qp](0)*_alpha[_qp]*_phi[_j][_qp]);
-    
+
     return 0.0;
   }
