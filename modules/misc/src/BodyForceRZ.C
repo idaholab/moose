93d2d4cd92e18648d9c7a43afbba11aec697f18f
David Andrs <david.andrs@inl.gov>
2011-03-21 16:47:34 +0000

    Removing some print statements
    r3815

--- modules/misc/src/BodyForceRZ.C
+++ modules/misc/src/BodyForceRZ.C
@@ -9,7 +9,6 @@ InputParameters validParams<BodyForceRZ>()
 
 Real BodyForceRZ::computeQpResidual()
 {
-  std::cout<<_q_point[_qp](0)<<std::endl;
   return 2 * M_PI * _q_point[_qp](0) * BodyForce::computeQpResidual();
 }
 
