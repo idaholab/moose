5a8721062a790a7a654f875f084ccf67c3c7bfd0
David Andrs <david.andrs@inl.gov>
2011-03-21 16:47:34 +0000

    fix parallel
    r3814

--- modules/misc/src/BodyForceRZ.C
+++ modules/misc/src/BodyForceRZ.C
@@ -9,6 +9,7 @@ InputParameters validParams<BodyForceRZ>()
 
 Real BodyForceRZ::computeQpResidual()
 {
+  std::cout<<_q_point[_qp](0)<<std::endl;
   return 2 * M_PI * _q_point[_qp](0) * BodyForce::computeQpResidual();
 }
 
