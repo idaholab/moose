e54bff6249a6cec657a80f8846f91adea0b97c08
David Andrs <david.andrs@inl.gov>
2011-03-21 16:47:34 +0000

    Removing some print statements
    r3815

--- framework/src/kernels/TimeDerivative.C
+++ framework/src/kernels/TimeDerivative.C
@@ -16,17 +16,11 @@ TimeDerivative::TimeDerivative(const std::string & name, InputParameters paramet
 Real
 TimeDerivative::computeQpResidual()
 {
-//  std::cout<<"u_dot: "<<_u_dot[_qp] - ((_u[_qp] - _u_old[_qp])/_dt)<<std::endl;
-//  std::cout<<"old  : "<<((_u[_qp] - _u_old[_qp])/_dt)<<std::endl;
-  
-  return ((_u[_qp] - _u_old[_qp])/_dt) * _test[_i][_qp];
-  
-//  return _test[_i][_qp]*_u_dot[_qp];
+  return _test[_i][_qp]*_u_dot[_qp];
 }
 
 Real
 TimeDerivative::computeQpJacobian()
 {
-  return ((_phi[_j][_qp])/_dt) * _test[_i][_qp];
-//  return _test[_i][_qp]*_phi[_j][_qp]*_du_dot_du[_qp];
+  return _test[_i][_qp]*_phi[_j][_qp]*_du_dot_du[_qp];
 }
