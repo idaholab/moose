dba5288ed1d32b0ed08898588edb20ce167ac128
Jason Hales <jason.hales@inl.gov>
2011-04-01 14:03:51 +0000

    Change to help convergence in the presence of rigid body modes
    r4161

--- modules/linear_elasticity/include/SolidMechImplicitEuler.h
+++ modules/linear_elasticity/include/SolidMechImplicitEuler.h
@@ -15,13 +15,17 @@ class SolidMechImplicitEuler : public SecondDerivativeImplicitEuler
 public:
 
   SolidMechImplicitEuler(const std::string & name, InputParameters parameters);
-  
+
 protected:
   virtual Real computeQpResidual();
-  
+
   virtual Real computeQpJacobian();
-  
+
+  virtual Real scaling();
+
 private:
   MaterialProperty<Real> & _density;
+  const bool _artificial_scaling_set;
+  const Real _artificial_scaling;
 };
 #endif //SOLIDMECHIMPLICITEULER_H
