295285a0a13207dac276da6e3b41e2057a7efee8
Cody Permann <cody.permann@inl.gov>
2011-04-06 06:21:32 +0000

    Verify additional steps in damping problem
    r4199

--- test/tests/constant_damper_test/constant_damper_test.py
+++ test/tests/constant_damper_test/constant_damper_test.py
@@ -1,4 +1,8 @@
 import tools
 
-def testdirichlet(dofs=0, np=0, n_threads=0):
+def testdamper(dofs=0, np=0, n_threads=0):
   tools.executeAppAndDiff(__file__,'constant_damper_test.i',['out.e'], dofs, np, n_threads)
+
+# Make sure the damping causes 8 NL steps
+def testverifydamping(dofs=0, np=0, n_threads=0):
+  tools.executeAppExpectError(__file__,'constant_damper_test.i','NL step\s+8')
