e680bfaccea531e625bcfe43690c89d2d3400a27
David Andrs <david.andrs@inl.gov>
2011-04-25 12:13:58 +0000

    Problem::_real_zero was not properly initialized and caused some troubles in threaded runs
    r4550

--- framework/src/base/Problem.C
+++ framework/src/base/Problem.C
@@ -20,7 +20,7 @@ Problem::Problem():
 {
   unsigned int n_threads = libMesh::n_threads();
 
-  _real_zero.resize(n_threads);
+  _real_zero.resize(n_threads, 0.);
   _zero.resize(n_threads);
   _grad_zero.resize(n_threads);
   _second_zero.resize(n_threads);
