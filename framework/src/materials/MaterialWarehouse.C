e1c0e48fbecaf1b134e9523fb863f1abed6457a4
Cody Permann <cody.permann@inl.gov>
2011-05-24 15:01:52 +0000

    Fixing boundary materials sort
    r4963

--- framework/src/materials/MaterialWarehouse.C
+++ framework/src/materials/MaterialWarehouse.C
@@ -42,7 +42,7 @@ void
 MaterialWarehouse::initialSetup()
 {
   sortMaterials();
-  
+
   for (std::map<int, std::vector<Material *> >::iterator j = _active_materials.begin(); j != _active_materials.end(); ++j)
     for (std::vector<Material *>::iterator k = j->second.begin(); k != j->second.end(); ++k)
       (*k)->initialSetup();
@@ -203,12 +203,25 @@ void MaterialWarehouse::addNeighborMaterial(int block_id, Material *material)
 
 void
 MaterialWarehouse::sortMaterials()
-{ 
-  for (std::map<int, std::vector<Material *> >::iterator j = _active_materials.begin(); j != _active_materials.end(); ++j)  
+{
+#ifdef DEBUG
+  // Sanity check that all three containers contain the same keys (well almost we aren't checking "every" case)
+  for (std::map<int, std::vector<Material *> >::iterator j = _active_materials.begin();
+       j != _active_materials.end(); ++j)
+  {
+    mooseAssert(_active_boundary_materials.find(j->first) != _active_boundary_materials.end(),
+                "active_boundary_materials is different then active_materials");
+
+    // TODO: Enable this check when doing DG
+    //mooseAssert(_active_neighbor_materials.find(j->first) != _active_neighbor_materials.end(),
+    //             "active_neighbor_materials is different then active_materials");
+  }
+#endif
+
+  for (std::map<int, std::vector<Material *> >::iterator j = _active_materials.begin(); j != _active_materials.end(); ++j)
   {
     DependencyResolver<Material *> resolver;
 
-    
     /**
      * For each block we have to run through the dependency list since
      * the property provided by a material can change from block to block
@@ -217,7 +230,7 @@ MaterialWarehouse::sortMaterials()
     for (std::vector<Material *>::const_iterator mat_iter=j->second.begin(); mat_iter != j->second.end(); ++mat_iter)
     {
       const std::set<std::string> & depend_props = (*mat_iter)->getPropertyDependencies();
-       
+
       for (std::set<std::string>::const_iterator prop_iter=depend_props.begin(); prop_iter != depend_props.end(); ++prop_iter)
       {
         // Ask each active material if they supply this property
@@ -225,14 +238,20 @@ MaterialWarehouse::sortMaterials()
         {
           // Don't check THIS material for a coupled property
           if (mat_iter == mat_iter2) continue;
-           
+
           if ((*mat_iter2)->have_property_name(*prop_iter))
             resolver.insertDependency(*mat_iter, *mat_iter2);
         }
-      } 
+      }
     }
-    
-    // Sort based on dependencies
-    std::sort(j->second.begin(), j->second.end(), resolver);    
+
+    // Sort based on dependencies - Can we use the same resolver for all three material data structures? I think so!
+    std::sort(j->second.begin(), j->second.end(), resolver);
+
+    std::sort(_active_boundary_materials[j->first].begin(), _active_boundary_materials[j->first].end(), resolver);
+
+    // TODO: Sort this list when doing DG
+    //std::sort(_active_neighbor_materials[j->first].begin(), _active_neighbor_materials[j->first].end(), resolver);
   }
+
 }
