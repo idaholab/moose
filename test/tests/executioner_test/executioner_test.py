7f237b6120371a47260149aacfc0a9489b757b2f
Derek Gaston <derek.gaston@inl.gov>
2011-06-13 08:56:37 +0000

    disable solution time adaptive test... its non-deterministic
    r5308

--- test/tests/executioner_test/executioner_test.py
+++ test/tests/executioner_test/executioner_test.py
@@ -10,7 +10,8 @@ def test_transient(dofs=0, np=0, n_threads=0):
   tools.executeAppAndDiff(__file__,'transient.i',['out_transient.e'], dofs, np, n_threads)
   
 def test_sln_time_adapt(dofs=0, np=0, n_threads=0):
-  tools.executeAppAndDiff(__file__,'sln-time-adapt.i',['out_sta.e'], dofs, np, n_threads)
+  pass
+#  tools.executeAppAndDiff(__file__,'sln-time-adapt.i',['out_sta.e'], dofs, np, n_threads)
 
 try:
   from options import *
@@ -25,7 +26,8 @@ try:
                     EXODIFF : ['out_transient.e'] }
 
   test_sln_time_adapt = { INPUT : 'sln-time-adapt.i',
-                       EXODIFF : ['out_sta.e'] }
+                          EXODIFF : ['out_sta.e'] ,
+                          SKIP : True }
 
 except:
   pass
