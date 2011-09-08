132ff1fe058e42836320d7fb05f2b4e9443c8d32
Wenfeng Liu <wenfeng.liu@anatech.com>
2011-09-08 15:37:39 +0000

    changes to Water_Steam_EOS to provide functions for BISON coolant channel model
    r6090

--- modules/water_steam_eos/include/Water_Steam_EOS.h
+++ modules/water_steam_eos/include/Water_Steam_EOS.h
@@ -5,7 +5,7 @@ namespace Water_Steam_EOS
   {
 
     //subroutine TSAT(p, 100D0, Ts)
-    double TSAT_( double&, double&, double&);
+    //double TSAT_( double&, double&, double&);
     
     //subroutine water_steam_prop_PH(p, h, Ts,T, Sw, Den, dTdp, dTdh, dDendp, dDendh,  ierr, dhwdp, dhsdp, dTdp)
     void water_steam_prop_ph_( double&, double&, double&, double&, double&, double&, double&, double&, double&, double&, 
@@ -21,11 +21,44 @@ namespace Water_Steam_EOS
     //subroutine  wateos_noderiv(T, p, dw)  this one is faster, no derivatives calculated
     void wateos_noderiv1_( double&, double&, double&);
 	  
-      
     //subroutine  VISW_noderiv1 (rho_s,tc,vs)  
-    double viss_noderiv1_( double&, double&, double&);  
+    void viss_noderiv1_( double&, double&, double&);  
+
+    //================================================================================
+    //
+    //
+    // Following function/subroutines definitions are in source water_steam_functions.f90 
+    //
+    //--------------------------------------------------------------------------------
+
+    void   boundary_23_(double& P, double& T, int& N);
+
+    void   saturation_(double& P, double& T, int& N, int& nerr);
+
+    void   enthalpy_density_pt_(double& H, double& D, double& P, double& T, int& nerr, int& nRegion);
+
+    double viscosity_(double& rho, double& T);
+
+    //////////////////////////////////////////////////////////////////////////////////
+    //
+    //   A  list of functions used for BISON coolant channel model
+    // 
+    //
+    //   | Function name             | source code file           |
+    //   |---------------------------+----------------------------|
+    //   | void water_steam_prop_ph_ | water_steam_phase_prop.f90 |
+    //   | void viss_noderiv1_       | VISS_noderiv1.f90          |
+    //   | void boundary_23_         | water_steam_functions.f90  |
+    //   | void saturation_          | water_steam_functions.f90  |
+    //   | void enthalpy_density_pt_ | water_steam_functions.f90  |
+    //   | double viscosity_         | water_steam_functions.f90  |
+    //   |                           |                            |
+    //
+    //
+    //////////////////////////////////////////////////////////////////////////////////
+
     
-   //    
+
   }
 
 }
