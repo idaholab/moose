8fa25fe45ce89f83fc88a997ecbe215940078200
Avery Bingham <avery.guild-bingham@inl.gov>
2012-01-10 09:27:20 +0000

    Added thermal conduction boundary condition. Heat flux boudnary assumeing a bulk coolant of temperature provided as a constant or functional value.
    r8016

---
+++ modules/heat_conduction/src/BulkCoolantBC.C
@@ -0,0 +1,43 @@
+#include "BulkCoolantBC.h"
+
+template<>
+InputParameters validParams<BulkCoolantBC>()
+{
+  InputParameters params = validParams<IntegratedBC>();
+  params.addParam<Real>("alpha",2E3, "heat transfer coefficient W/m^2-K"); //should be optionally a material property
+  params.addParam<Real>("bulk_temparature",800., "Bulk fluid temperature K");
+  params.addParam<std::string>("function", "function describing bulk temperature");
+  return params;
+}
+
+BulkCoolantBC::BulkCoolantBC(const std::string & name, InputParameters parameters)
+  :IntegratedBC(name, parameters),
+   _alpha(getParam<Real>("alpha")),
+   _tempb(getParam<Real>("bulk_temperature")),
+   _has_function(getParam<std::string>("function") != ""),
+   _function( _has_function ? &getFunction("function") : NULL ),
+   _conductivity(getMaterialProperty<Real>("thermal_conductivity")) //should give option for constant value
+
+  {}
+
+Real
+BulkCoolantBC::computeQpResidual()
+  {
+
+    Real bulk_temp( _tempb );
+
+    if ( _has_function )
+    bulk_temp *= _function->value(_t, _q_point[_qp]);
+    
+    
+    return -( _test[_i][_qp]*(_alpha)/(_conductivity[_qp])*(bulk_temp - _u[_qp] ) );
+    
+    
+  }
+
+Real
+BulkCoolantBC::computeQpJacobian()
+{
+  return -( _test[_i][_qp]*(_alpha)/(_conductivity[_qp])*(-_phi[_j][_qp]) );
+}
+
