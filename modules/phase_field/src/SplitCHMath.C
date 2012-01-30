4802369faf1fcecfe682cae1f7ef4d611a1ddb5e
Michael Tonks <michael.tonks@inl.gov>
2012-01-30 13:47:39 +0000

    added header file
    r8413

---
+++ modules/phase_field/src/SplitCHMath.C
@@ -0,0 +1,30 @@
+#include "SplitCHMath.h"
+// The couple, SplitCHMath and SplitCHWRes, splits the CH equation by replacing chemical potential with 'w'.
+template<>
+InputParameters validParams<SplitCHMath>()
+{
+  InputParameters params = validParams<SplitCHCRes>();
+
+  return params;
+}
+
+SplitCHMath::SplitCHMath(const std::string & name, InputParameters parameters)
+  :SplitCHCRes(name, parameters)
+{
+}
+
+Real
+SplitCHMath::computeDFDC(PFFunctionType type)
+{
+  switch (type)
+  {
+  case Residual:
+    return _u[_qp]*_u[_qp]*_u[_qp] - _u[_qp]; // return Residual value
+    
+  case Jacobian: 
+    return (3.0*_u[_qp]*_u[_qp] - 1.0)*_phi[_j][_qp]; //return Jacobian value
+    
+  }
+  
+  mooseError("Invalid type passed in");
+  }
