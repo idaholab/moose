c63bc92ef327da4d7f57cae840332c4bdc854d59
David Andrs <david.andrs@inl.gov>
2012-03-19 15:41:35 +0000

    Refactoring common code into PipeBase
    r9384

---
+++ modules/thm/include/components/PipeBase.h
@@ -0,0 +1,64 @@
+#ifndef PIPEBASE_H
+#define PIPEBASE_H
+
+#include "point.h"
+#include "vector_value.h"
+
+#include "RELAP7.h"
+#include "Component.h"
+#include "Model.h"
+
+class PipeBase;
+
+template<>
+InputParameters validParams<PipeBase>();
+
+/**
+ * A base class for pipe components
+ *
+ * A pipe is defined by its position, direction, length and area.
+ * Mesh: mesh is generated in such a way, that the pipe starts at the origin (0, 0, 0) and is aligned with x-axis. Its
+ * subdivided into _n_elems elements (of type EDGE2).
+ */
+class PipeBase : public Component,
+    public Model
+{
+public:
+  PipeBase(const std::string & name, InputParameters params);
+  virtual ~PipeBase();
+
+  virtual void buildMesh();
+  virtual void addVariables();
+
+  virtual Point getPosition() { return _position; }
+  virtual RealVectorValue getDirection() { return _dir; }
+
+  // Pipe specific interface ----
+  virtual Node * getBoundaryNode(RELAP7::EEndType id);
+  virtual unsigned int getBoundaryId(RELAP7::EEndType id);
+  virtual Real getArea() { return _A; }
+
+protected:
+  Point _position;                              ///< Physical position in the space
+  RealVectorValue _dir;                         ///< Direction this pipe is going to
+  Real _length;                                 ///< Length of the pipe
+  unsigned int _n_elems;                        ///< Number of elements this pipe is divided into
+  Real _A;                                      ///< Pipe area (A_i)
+  Real _aw;					///< Heat transfer surface density
+						///< Physical meaning:
+						///< _aw = A_heating_surface_per_length / A_pipe_flow_are [1/m]
+
+  Real _f;                                      ///< friction
+  Real _Hw;                                     ///< Convective heat transfer coefficient
+  Real _Tw;                                     ///< Wall temperature
+
+
+  unsigned int _subdomain_id;                   ///< Subdomain id this pipe defined
+
+  std::map<RELAP7::EEndType, Node *> _bnd_nodes;        ///< Boundary nodes of this pipe (indexing: local "node id" => Node).
+                                                        ///< Local node IDs are used by other components for connecting
+
+  std::map<RELAP7::EEndType, unsigned int> _bnd_ids;    ///< Boundary id of this pipe (indexing: local "node id" => boundary_id).
+};
+
+#endif /* PIPEBASE_H */
---
+++ modules/thm/src/components/PipeBase.C
@@ -0,0 +1,120 @@
+#include "PipeBase.h"
+#include "Simulation.h"
+#include "FEProblem.h"
+#include "Conversion.h"
+#include "R7Conversion.h"
+#include "OneDEnergyWallHeating.h"
+
+#include "edge_edge2.h"
+#include "fe_type.h"
+
+// physics
+#include "Diffusion.h"
+
+template<>
+InputParameters validParams<PipeBase>()
+{
+  InputParameters params = validParams<Component>();
+  //Input parameters [NO] default values should be given.
+  params.addRequiredParam<std::vector<Real> >("position", "Origin (start) of the pipe");
+  params.addRequiredParam<std::vector<Real> >("orientation", "Orientation vector of the pipe");
+  params.addRequiredParam<Real>("length", "Length of the pipe");
+  params.addRequiredParam<unsigned int>("n_elems", "number of element in this pipe");
+  params.addRequiredParam<Real>("A", "Area of the pipe");
+
+  //Input parameters default values could be given.
+  params.addParam<Real>("aw", 0.0, "Heating surface density");
+  params.addParam<Real>("f", 0.0, "friction");
+  params.addParam<Real>("Hw", 0.0, "Convective heat transfer coefficient");
+  params.addParam<Real>("Tw", 400, "Wall temperature");
+
+  return params;
+}
+
+
+PipeBase::PipeBase(const std::string & name, InputParameters params) :
+    Component(name, params),
+    Model(params),
+    _position(toPoint(getParam<std::vector<Real> >("position"))),
+    _length(getParam<Real>("length")),
+    _n_elems(getParam<unsigned int>("n_elems")),
+    _A(getParam<Real>("A")),
+    _aw(getParam<Real>("aw")),
+    _f(getParam<Real>("f")),
+    _Hw(getParam<Real>("Hw")),
+    _Tw(getParam<Real>("Tw"))
+{
+  const std::vector<Real> & dir = getParam<std::vector<Real> >("orientation");
+  _dir = VectorValue<Real>(dir[0], dir[1], dir[2]);
+}
+
+PipeBase::~PipeBase()
+{
+}
+
+Node *
+PipeBase::getBoundaryNode(RELAP7::EEndType id)
+{
+  std::map<RELAP7::EEndType, Node *>::iterator it = _bnd_nodes.find(id);
+  if (it != _bnd_nodes.end())
+    return it->second;
+  else
+    return NULL;
+}
+
+unsigned int
+PipeBase::getBoundaryId(RELAP7::EEndType id)
+{
+  std::map<RELAP7::EEndType, unsigned int>::iterator it = _bnd_ids.find(id);
+  if (it != _bnd_ids.end())
+    return it->second;
+  else
+    mooseError("PipeBase " << name() << " does not have this type of end defined.");
+}
+
+void
+PipeBase::buildMesh()
+{
+  std::vector<unsigned int> node_ids;
+  // points
+  Real delta_t = _length / _n_elems;
+  Point p(0, 0, 0);                      // origin
+  for (unsigned int i = 0; i <= _n_elems; i++)
+  {
+    const Node * nd = _mesh._mesh.add_point(p);
+    node_ids.push_back(nd->id());
+    p(0) += delta_t;
+  }
+
+  // elems
+  _subdomain_id = getNextSubdomainId();
+  unsigned int bc_id_in = getNextBCId();
+  unsigned int bc_id_out = getNextBCId();
+  for (unsigned int i = 0; i < _n_elems; i++)
+  {
+    Elem * elem = _mesh._mesh.add_elem (new Edge2);
+    elem->subdomain_id() = _subdomain_id;
+    elem->set_node(0) = _mesh._mesh.node_ptr(node_ids[i]);
+    elem->set_node(1) = _mesh._mesh.node_ptr(node_ids[i+1]);
+
+    // BCs
+    if (i == 0)
+    {
+      _bnd_ids[RELAP7::IN] = bc_id_in;
+      _mesh._mesh.boundary_info->add_side(elem, 0, bc_id_in);
+      _bnd_nodes[RELAP7::IN] = elem->get_node(0);                // first 0 is local bnd_id that Joints will use for connecting
+    }
+    if (i == (_n_elems - 1))
+    {
+      _bnd_ids[RELAP7::OUT] = bc_id_out;
+      _mesh._mesh.boundary_info->add_side(elem, 1, bc_id_out);
+      _bnd_nodes[RELAP7::OUT] = elem->get_node(1);               // first 1 is local bnd_id that Joints will use for connecting
+    }
+ }
+}
+
+void
+PipeBase::addVariables()
+{
+  Model::addVariables(_subdomain_id);
+}
