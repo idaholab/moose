13805c562ab3d6dd21bc92e66c0359cbee5a2f42
Jason Miller <jason.miller@inl.gov>
2012-04-11 07:29:47 +0000

    removing scale_refine from mixed_dim_test. Meshes with mixed dimension elements can not be refined.
    r9958

--- test/tests/mixed_dim_test/mixed_dim_test.py
+++ test/tests/mixed_dim_test/mixed_dim_test.py
@@ -1,13 +1,10 @@
 from options import *
 
 onedtwod_test = { INPUT : '1d_2d.i',
-                  EXODIFF : ['out.e'],
-                  SCALE_REFINE : 5
+                  EXODIFF : ['out.e']
                   }
 onedthreed_test = { INPUT : '1d_3d.i',
-                    EXODIFF : ['1d_3d_out.e'],
-                    SCALE_REFINE : 5
+                    EXODIFF : ['1d_3d_out.e']
                     }
 
-
-
+# Meshes with mixed dimension elements can not be refined
