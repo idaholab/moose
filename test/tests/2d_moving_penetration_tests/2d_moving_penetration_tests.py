fa2afafa87a342e9ce24c64a50e4028981486311
Benjamin Spencer <benjamin.spencer@inl.gov>
2012-04-25 16:42:56 +0000

    Add 2d tests of PenetrationLocator with moving contact surfaces
    These tests exercise PenetrationLocator in 2d with moving meshes in a
    variety of situations.  There are two variants of each of these
    tests: one with a zero tangential tolerance, and one with a nonzero
    tangential tolerance (tt in the name).
    
    Addresses #852
    
    r10227

---
+++ test/tests/2d_moving_penetration_tests/2d_moving_penetration_tests.py
@@ -0,0 +1,31 @@
+from options import *
+
+pl_test1 = { INPUT : 'pl_test1.i',
+             EXODIFF : ['pl_test1_out.e'] }
+
+pl_test1tt = { INPUT : 'pl_test1tt.i',
+               EXODIFF : ['pl_test1tt_out.e'] }
+
+pl_test2 = { INPUT : 'pl_test2.i',
+             EXODIFF : ['pl_test2_out.e'] }
+
+pl_test2tt = { INPUT : 'pl_test2tt.i',
+               EXODIFF : ['pl_test2tt_out.e'] }
+
+pl_test3 = { INPUT : 'pl_test3.i',
+             EXODIFF : ['pl_test3_out.e'] }
+
+pl_test3tt = { INPUT : 'pl_test3tt.i',
+               EXODIFF : ['pl_test3tt_out.e'] }
+
+pl_test4 = { INPUT : 'pl_test4.i',
+             EXODIFF : ['pl_test4_out.e'] }
+
+pl_test4tt = { INPUT : 'pl_test4tt.i',
+               EXODIFF : ['pl_test4tt_out.e'] }
+
+pl_test5 = { INPUT : 'pl_test5.i',
+             EXODIFF : ['pl_test5_out.e'] }
+
+pl_test5tt = { INPUT : 'pl_test5tt.i',
+               EXODIFF : ['pl_test5tt_out.e'] }
