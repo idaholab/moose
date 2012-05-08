9fb9304ff9795132e40261fb30c35cbde1a8f00e
Derek Gaston <derek.gaston@inl.gov>
2012-05-08 10:29:56 +0000

    add penetration locator test in parallel fixes #934
    r10465

--- test/tests/penetration_locator_test/penetration_locator_test.py
+++ test/tests/penetration_locator_test/penetration_locator_test.py
@@ -3,3 +3,8 @@ from options import *
 test = { INPUT : 'penetration_locator_test.i',
          EXODIFF : ['out.e'] }
 
+parallel_test = { INPUT : 'penetration_locator_test.i',
+                  EXODIFF : ['out.e'],
+                  PARALLEL : '4',
+                  PREREQ : 'test' }
+
