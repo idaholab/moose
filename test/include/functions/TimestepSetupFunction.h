b6574c6bf4653a3adbc9ac859db0dd22559a5a34
Derek Gaston <derek.gaston@inl.gov>
2012-06-11 15:17:58 +0000

    add missing files ref #1017
    r11088

---
+++ test/include/functions/TimestepSetupFunction.h
@@ -0,0 +1,37 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#ifndef TIMESTEPSETUPFUNCTION_H
+#define TIMESTEPSETUPFUNCTION_H
+
+#include "Function.h"
+
+class TimestepSetupFunction;
+
+template<>
+InputParameters validParams<TimestepSetupFunction>();
+
+class TimestepSetupFunction : public Function
+{
+public:
+  TimestepSetupFunction(const std::string & name, InputParameters parameters);
+
+  virtual Real value(Real t, const Point & p);
+
+  virtual void timestepSetup();
+private:
+  unsigned int _local_timestep;
+};
+
+#endif //TIMESTEPSETUPFUNCTION_H
---
+++ test/src/functions/TimestepSetupFunction.C
@@ -0,0 +1,39 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#include "TimestepSetupFunction.h"
+
+template<>
+InputParameters validParams<TimestepSetupFunction>()
+{
+  InputParameters params = validParams<Function>();
+  return params;
+}
+
+TimestepSetupFunction::TimestepSetupFunction(const std::string & name, InputParameters parameters) :
+    Function(name, parameters),
+    _local_timestep(0)
+{}
+
+void
+TimestepSetupFunction::timestepSetup()
+{
+  _local_timestep++;
+}
+
+Real
+TimestepSetupFunction::value(Real /*t*/, const Point & p)
+{
+  return _local_timestep;
+}
