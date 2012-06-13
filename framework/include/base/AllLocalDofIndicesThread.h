4fe4ef048c38d1234ae26a50384878f1b4340c77
Derek Gaston <derek.gaston@inl.gov>
2012-06-13 14:23:30 +0000

    add missing files ref #1030 #708 #722 #1150
    r11206

---
+++ framework/include/base/AllLocalDofIndicesThread.h
@@ -0,0 +1,50 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#ifndef ALLLOCALDOFINDICESTHREAD_H
+#define ALLLOCALDOFINDICESTHREAD_H
+
+#include "ParallelUniqueId.h"
+
+// libMesh includes
+#include "node_range.h"
+#include "system.h"
+
+class FEProblem;
+
+/**
+ * Grab all the local dof indices for the variables passed in, in the system passed in.
+ */
+class AllLocalDofIndicesThread
+{
+public:
+  AllLocalDofIndicesThread(System & sys, std::vector<std::string> vars);
+  // Splitting Constructor
+  AllLocalDofIndicesThread(AllLocalDofIndicesThread & x, Threads::split split);
+
+  void operator() (const ConstElemRange & range);
+
+  void join(const AllLocalDofIndicesThread & y);
+
+  std::set<unsigned int> _all_dof_indices;
+
+protected:
+  System & _sys;
+  DofMap & _dof_map;
+  std::vector<std::string> _vars;
+  std::vector<unsigned int> _var_numbers;
+  THREAD_ID _tid;
+};
+
+#endif //ALLLOCALDOFINDICESTHREAD_H
---
+++ framework/src/base/AllLocalDofIndicesThread.C
@@ -0,0 +1,69 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#include "AllLocalDofIndicesThread.h"
+
+#include "FEProblem.h"
+
+// libmesh includes
+#include "threads.h"
+#include LIBMESH_INCLUDE_UNORDERED_SET
+LIBMESH_DEFINE_HASH_POINTERS
+
+AllLocalDofIndicesThread::AllLocalDofIndicesThread(System & sys,
+                                                   std::vector<std::string> vars) :
+    _sys(sys),
+    _dof_map(sys.get_dof_map()),
+    _vars(vars)
+{
+  _var_numbers.resize(_vars.size());
+  for(unsigned int i=0; i<_vars.size(); i++)
+    _var_numbers[i] = _sys.variable_number(_vars[i]);
+}
+
+// Splitting Constructor
+AllLocalDofIndicesThread::AllLocalDofIndicesThread(AllLocalDofIndicesThread & x, Threads::split /*split*/) :
+    _sys(x._sys),
+    _dof_map(x._dof_map),
+    _vars(x._vars)
+{
+}
+
+void
+AllLocalDofIndicesThread::operator() (const ConstElemRange & range)
+{
+  ParallelUniqueId puid;
+  _tid = puid.id;
+
+  for (ConstElemRange::const_iterator elem_it = range.begin() ; elem_it != range.end(); ++elem_it)
+  {
+    const Elem * elem = *elem_it;
+
+    std::vector<unsigned int> dof_indices;
+
+    // prepare variables
+    for (unsigned int i=0; i<_var_numbers.size(); i++)
+    {
+      _dof_map.dof_indices(elem, dof_indices, _var_numbers[i]);
+      for(unsigned int j=0; j<dof_indices.size(); j++)
+        _all_dof_indices.insert(dof_indices[j]);
+    }
+  }
+}
+
+void
+AllLocalDofIndicesThread::join(const AllLocalDofIndicesThread & y)
+{
+  _all_dof_indices.insert(y._all_dof_indices.begin(), y._all_dof_indices.end());
+}
