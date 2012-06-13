53f9b85a444c844d8899241da1fcce7fd9f39f23
Jason Hales <jason.hales@inl.gov>
2012-06-13 12:47:57 +0000

    Restart test for plasticity
    r11200

--- modules/combined/tests/LinearStrainHardening/LinearStrainHardening_test.py
+++ modules/combined/tests/LinearStrainHardening/LinearStrainHardening_test.py
@@ -6,3 +6,18 @@ test = { INPUT : 'LinearStrainHardening_test.i',
          SCALE_REFINE : 2
          }
 
+testRestart1 = { INPUT : 'LinearStrainHardeningRestart1.i',
+                 EXODIFF : ['LinearStrainHardeningRestart1_out.e'],
+                 ABS_ZERO : 1e-9,
+                 PREREQ : 'test',
+                 SKIP : 'Need exodiff option'
+               }
+
+testRestart2 = { INPUT : 'LinearStrainHardeningRestart2.i',
+                 EXODIFF : ['out.e'],
+#                 EXODIFF_OPTIONS : ['-TM'],
+                 ABS_ZERO : 1e-9,
+                 PREREQ : 'testRestart1',
+                 SKIP : 'Need exodiff option'
+               }
+
