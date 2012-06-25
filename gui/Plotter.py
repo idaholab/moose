f9a828545397f534b57ab34070fd9bc2ab9d7640
schosa <schosa@dd8cd9ef-2931-0410-98ca-75ad22d19dd1>
2012-06-25 15:29:14 +0000

    added tab to peacock main gui so postprocessor data can be viewed, also made two new files that preform plotting
    r11461

---
+++ gui/Plotter.py
@@ -0,0 +1,50 @@
+#!usr/bin/python
+
+import sys, os, random
+from PyQt4 import QtGui, QtCore
+import numpy, csv
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+
+class PlotWidget(FigureCanvas):
+    """This is the canvas Widget. It allows for MPL plot embedding """
+    
+    def __init__(self, parent=None, width=7, height=4, dpi=100):
+        self.fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = self.fig.add_subplot(111)
+        # We want the axes cleared every time plot() is called
+        self.axes.hold(False)
+        
+        
+        FigureCanvas.__init__(self, self.fig)
+        self.setParent(parent)
+        
+        FigureCanvas.setSizePolicy(self,
+                                   QtGui.QSizePolicy.Fixed,
+                                   QtGui.QSizePolicy.Fixed)
+        
+        FigureCanvas.updateGeometry(self)
+
+
+class MPLPlotter(PlotWidget):
+    """This is a widget that inherites from the plotWidget class that is used to update the plot with PP data"""
+    def __init__(self, plotData, plotName):
+        PlotWidget.__init__(self)
+        
+        self.setPlotData(plotData,plotName)
+    
+    def setPlotData(self, plotData, plotName):
+        self.plotData = plotData
+        self.plotName = plotName
+        self.xData = self.plotData[0]
+        self.yData = self.plotData[1]
+        self.plotTitle = plotName + ' Postprocessor'
+        
+        
+        # MPL plots
+        self.axes.plot(self.xData, self.yData, 'r')
+        self.axes.set_xlabel('time')
+        self.axes.set_ylabel(self.plotName)
+        self.axes.set_title(self.plotTitle)
+        self.draw()
---
+++ gui/PostprocessorWidget.py
@@ -0,0 +1,152 @@
+#!usr/bin/python
+
+import sys, os, random
+from PyQt4 import QtGui, QtCore
+import numpy, csv
+from Plotter import *
+
+
+
+class PostprocessorWidget(QtGui.QWidget):
+    def __init__(self, input_file_widget, execute_widget):
+        QtGui.QWidget.__init__(self)
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+        self.input_file_widget = input_file_widget
+        self.execute_widget = execute_widget
+        
+        self.getFileName()
+        
+        # uses the postprocessor selected by the user to pass the information required for plotting
+        self.comboWidget = QtGui.QComboBox(self)
+        self.fillComboWidget()
+        self.comboWidget.activated[str].connect(self.createPlot)
+        self.plotObjectDict = {}
+        self.plotDataDict = {}
+        
+        # adds a button to the widget that will be used to clear plot selectons
+        self.clearButton = QtGui.QPushButton("Clear")
+        self.clearButton.resize(self.clearButton.sizeHint())
+        self.clearButton.clicked.connect(self.clearClick)
+
+        # adds a scroll layout to the main widget so all of the plots don't take up too much space
+        # scroll area widget contents - layout
+        self.scrollLayout = QtGui.QFormLayout()
+        self.scrollWidget = QtGui.QWidget()
+        self.scrollWidget.setLayout(self.scrollLayout)
+        self.scrollArea = QtGui.QScrollArea()
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setHorizontalScrollBarPolicy(1)
+        self.scrollArea.setWidget(self.scrollWidget)
+        
+        
+        # timer feature that updates all of the plots
+        self.timer = QtCore.QTimer(self)
+        QtCore.QObject.connect(self.execute_widget.run_button, QtCore.SIGNAL("clicked()"), self.runClicked)
+        QtCore.QObject.connect(self.timer, QtCore.SIGNAL("timeout()"), self.updatePlots)
+        self.timer.start(500)
+        
+        
+        # sets the layout for the widget
+        self.Layout = QtGui.QVBoxLayout(self)
+        self.Layout.addWidget(self.comboWidget)
+        self.Layout.addWidget(self.scrollArea)
+        self.Layout.addWidget(self.clearButton)
+
+   
+    
+    def getFileName(self):
+        
+        output_item = self.input_file_widget.tree_widget.findItems("Output", QtCore.Qt.MatchExactly)[0]
+        self.cwd = os.getcwd()
+        self.currentFile = self.cwd +'/peacock_run_tmp_out.csv'
+    
+        if output_item:
+            outputTable_data = output_item.table_data
+            
+            if 'file_base' in outputTable_data:
+                self.currentFile = self.cwd +'/'+ outputTable_data['file_base'] + '.csv'
+            
+    
+    def fillComboWidget(self):
+        
+        if os.path.exists(self.currentFile):
+        
+            self.names = open(self.currentFile,'rb').readline()
+            self.names = self.names.strip()
+            self.names = self.names.split(',')
+        
+        # the PP names are used to populate a dropdown menu 
+        # the user uses  this menu to select specific graphs to be monitored
+            for index in range(len(self.names)-1):
+                self.comboWidget.addItem(self.names[index+1])
+        
+            self.comboWidget.setCurrentIndex(-1)
+      
+    
+    
+    def createPlot(self, plotName):
+        
+        # when this method is called a new item is added to the plots dictionary
+        # then the getPlotData function is called to begin the first plot
+       
+        time = [0]
+        postData = [0]
+        plotData = [time, postData]
+        self.plotObjectDict[plotName] = MPLPlotter(plotData,plotName)
+        self.plotDataDict[plotName] = [plotData]
+        
+        
+        # adds a layout to the PPD plot
+        self.scrollLayout.addRow(self.plotObjectDict[plotName])
+        self.updatePlots()
+    
+    def updatePlots(self):
+        # this method loops through all of the plot objects in the plot dictionary
+        # and for each plot calls the getPlotData function to update every plot that has been selected
+        
+        
+        if os.path.exists(self.currentFile):
+            self.data = numpy.genfromtxt(self.currentFile,
+                                delimiter = ',' , names = True)
+            self.time = []
+        
+            for line in self.data:
+                self.time.append(line[0])
+            
+            for key in self.plotObjectDict.keys():
+                self.postData = []
+                self.getPlotData(key)
+                plotData = [self.time, self.postData]
+                self.plotDataDict[key] = plotData
+    
+            for key in self.plotDataDict:
+                self.plotObjectDict[key].setPlotData(self.plotDataDict[key],key)
+
+    
+    def getPlotData(self, plotName):
+        
+       
+        indexCol = self.names.index(plotName)
+        for line in self.data:
+            self.postData.append(line[indexCol])
+    
+    def runClicked(self):
+        self.timer.stop()
+        QtCore.QObject.connect(self.execute_widget.proc,QtCore.SIGNAL("finished(int)"),self.stopPlotting)
+        self.clearClick()
+
+    def clearClick(self):
+        for i in range(self.scrollLayout.count()):
+            self.scrollLayout.itemAt(i).widget().close()
+        self.plotObjectDict = {}
+        self.plotDataDict = {}
+        self.comboWidget.clear()
+        self.getFileName()
+        self.fillComboWidget()
+        self.comboWidget.setCurrentIndex(-1)
+        
+
+    def stopPlotting(self):
+        
+        self.updatePlots()
+        self.timer.stop()
