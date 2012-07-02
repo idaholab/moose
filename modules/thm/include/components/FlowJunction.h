4666070e856c42feec988cf44540456466e6ea28
Ling Zou <ling.zou@inl.gov>
2012-07-02 11:55:36 +0000

    1), TimeDependentVolume now can handle both barotropic and nonisothermal conditions. 2), IsoFlowJunction/ErgFlowJunction are removed. 3), FlowJunction is added to replace them, which will potentially replace branches (needs full test b4 switch). 4), Re-organized the boundary conditions, i.e., OneDMassStaticPandTBC, OneDMomentumStaticPandTBC, and OneDEnergyStaticPandTBC. 5), Added an general initial condition for scalar variable. Now we can use a vector to initialize a scalar variable.
    r11660

---
+++ modules/thm/include/components/FlowJunction.h
@@ -0,0 +1,46 @@
+#ifndef FLOWJUNCTION_H
+#define FLOWJUNCTION_H
+
+#include <string>
+
+#include "Joint.h"
+
+class FlowJunction;
+
+template<>
+InputParameters validParams<FlowJunction>();
+
+/**
+ * Flow junction (isothermal)
+ */
+class FlowJunction : public Joint
+{
+public:
+  FlowJunction(const std::string & name, InputParameters params);
+  virtual ~FlowJunction();
+
+  virtual void buildMesh();
+  virtual void addVariables();
+  virtual void addMooseObjects();
+
+protected:
+  std::vector<std::string> _inputs;
+  std::vector<std::string> _outputs;
+  std::vector<unsigned int> _bnd_id;
+
+  //std::string _density_var_name;	///< Flow flow junction maintains one scalar variable (density of this flow junction)
+  //std::string _specific_int_energy_var_name;	///< An optional variable, if non-isothermal
+
+  std::string _FlowJunction_var_name;
+
+  std::vector<Real> _k_coeffs;		///< A vector to store user inputed K loss (form loss, minor loss) coefficients.
+  Real _ref_area;					///< A reference area for this flow junction to calculate its reference velocity (User input)
+  Real _ref_volume;					///< A reference volume for this flow junction
+
+  bool _has_initial_P;				///< Is initial pressure provided from user input
+  bool _has_initial_T;				///< Is initial temperature provided from user input
+  Real _initial_P;					///< Initial pressure from user input (if provided)
+  Real _initial_T;				///< Initial temperature from user input (if provided)
+};
+
+#endif /* FLOWJUNCTION_H */
---
+++ modules/thm/src/components/FlowJunction.C
@@ -0,0 +1,184 @@
+#include "FlowJunction.h"
+#include "Conversion.h"
+#include "Simulation.h"
+#include "Pipe.h"
+#include "FEProblem.h"
+#include <sstream>
+
+#include "FlowJunctionConstraint.h"
+
+#include "Function.h"
+#include "EquationOfState.h"
+
+
+template<>
+InputParameters validParams<FlowJunction>()
+{
+  InputParameters params = validParams<Joint>();
+  params.addRequiredParam<std::vector<std::string> >("inputs", "Inputs of this joint");
+  params.addRequiredParam<std::vector<std::string> >("outputs", "Outputs of this joint");
+
+  params.addRequiredParam<std::vector<Real> >("K", "Form loss coefficients");
+  params.addRequiredParam<Real>("Area", "Reference area of this branch");
+  params.addParam<Real>("Volume", 0.0, "Reference volume of this branch");
+  //params.addRequiredParam<Real>("Initial_pressure", "Initial pressure of this branch");
+  params.addParam<Real>("initial_P", 1.e5, "Initial pressure of this branch");
+
+  params.addRequiredParam<std::string>("eos_function", "The EOS function object");
+  return params;
+}
+
+
+FlowJunction::FlowJunction(const std::string & name, InputParameters params) :
+    Joint(name, params),
+    _inputs(getParam<std::vector<std::string> >("inputs")),
+    _outputs(getParam<std::vector<std::string> >("outputs")),
+    _k_coeffs(getParam<std::vector<Real> >("K")),
+    _ref_area(getParam<Real>("Area")),
+    _ref_volume(getParam<Real>("Volume")),
+	_has_initial_P(params.wasSeenInInput("initial_P")),
+	_has_initial_T(params.wasSeenInInput("initial_T")),
+	_initial_P(_has_initial_P ? getParam<Real>("initial_P") : _sim.getParam<Real>("global_init_P")),
+	_initial_T(_has_initial_T ? getParam<Real>("initial_T") : _sim.getParam<Real>("global_init_T"))
+{
+  //Generate variable name for the pressure variable of this branch
+  //_density_var_name = genName("density_", _id, "_FlowJunction");
+  //even this is an optional variable, still gives it a name
+  //_specific_int_energy_var_name = genName("specific_int_energy_", _id, "_FlowJunction");
+
+
+	_FlowJunction_var_name = genName("variables_of_", _id, "_FlowJunction");
+}
+
+FlowJunction::~FlowJunction()
+{
+}
+
+void
+FlowJunction::buildMesh()
+{
+  // Joint does not create any mesh
+  // BUT we will figure out our connectivity here
+
+  // connected inputs
+  for (unsigned int i = 0; i < _inputs.size(); ++i)
+  {
+    std::string comp_name;
+    RELAP7::EEndType end_type;
+    RELAP7::getConnectionInfo(_inputs[i], comp_name, end_type);
+
+    Component * comp = _sim.getComponentByName(comp_name);
+    if (dynamic_cast<PipeBase *>(comp) != NULL)
+    {
+      PipeBase * pipe = dynamic_cast<PipeBase *>(comp);
+
+      // get the boundary node from the pipe
+      Node * nd = pipe->getBoundaryNode(end_type);
+      _nodes.push_back(nd->id());
+      _Areas.push_back(pipe->getArea());
+      //_normals.push_back(1.);
+      _normals.push_back(pipe->getBoundaryOutNorm(end_type));
+      _bnd_id.push_back(pipe->getBoundaryId(end_type));
+    }
+  }
+
+  // connected outputs
+  for (unsigned int i = 0; i < _outputs.size(); ++i)
+  {
+    std::string comp_name;
+    RELAP7::EEndType end_type;
+    RELAP7::getConnectionInfo(_outputs[i], comp_name, end_type);
+
+    Component * comp = _sim.getComponentByName(comp_name);
+    if (dynamic_cast<PipeBase *>(comp) != NULL)
+    {
+      PipeBase * pipe = dynamic_cast<PipeBase *>(comp);
+
+      // get the boundary node from the pipe
+      Node * nd = pipe->getBoundaryNode(end_type);
+      _nodes.push_back(nd->id());
+      _Areas.push_back(pipe->getArea());
+      //_normals.push_back(-1.);
+      _normals.push_back(pipe->getBoundaryOutNorm(end_type));
+      _bnd_id.push_back(pipe->getBoundaryId(end_type));
+    }
+  }
+}
+
+void
+FlowJunction::addVariables()
+{
+  // add scalar variable
+  // Note the physical meaning of this scalar variable is the "pressure of the branch, P_Branch"
+
+
+
+  FEProblem & feproblem = _sim.feproblem();
+  Function & func = feproblem.getFunction(getParam<std::string>("eos_function"));
+  EquationOfState& _eos = dynamic_cast<EquationOfState&>(func);
+
+  std::cout << "initial T " << _initial_T << std::endl;
+
+  Real _initial_specific_int_energy = 0.;
+  if(_model_type == Model::EQ_MODEL_3)
+  {
+	  _initial_specific_int_energy = _eos.invert_eos_internal_energy(_initial_T);
+  }
+
+  std::cout << "initial e " << _initial_specific_int_energy << std::endl;
+
+  Real _initial_rho = _eos.invert_eos_rho(_initial_P, _initial_specific_int_energy);
+
+  if(_model_type == Model::EQ_MODEL_2)
+  {
+	  _sim.addVariable(true, _FlowJunction_var_name, FEType(FIRST, SCALAR), 0, 1.);	//FIXME: is '1' the best scaling factor?
+	  _sim.addScalarInitialCondition(_FlowJunction_var_name, _initial_rho);
+  }
+  else if (_model_type == Model::EQ_MODEL_3)
+  {
+	  _sim.addVariable(true, _FlowJunction_var_name, FEType(SECOND, SCALAR), 0, 1.);	//FIXME: is '1' the best scaling factor?
+	  std::vector<Real> _initial_conditions(2, 0.);
+	  _initial_conditions[0] = _initial_rho;
+	  _initial_conditions[1] = _initial_specific_int_energy;
+	  _sim.addScalarInitialCondition(_FlowJunction_var_name, _initial_conditions);
+  }
+  else
+	  mooseError("Not implemented yet.");
+}
+
+void
+FlowJunction::addMooseObjects()
+{
+  std::vector<std::string> cv_u(1, Model::VELOCITY);
+  std::vector<std::string> cv_pressure(1, Model::PRESSURE);
+  std::vector<std::string> cv_rho(1, Model::RHO);
+  std::vector<std::string> cv_rhou(1, Model::RHOU);
+  std::vector<std::string> cv_rhoE(1, Model::RHOE);
+
+  //add constraint
+  {
+    InputParameters params = validParams<FlowJunctionConstraint>();
+    params.set<std::string>("variable") = _FlowJunction_var_name;
+    params.set<std::string>("ced_variable") = Model::RHO;
+
+    params.set<std::vector<unsigned int> >("nodes") = _nodes;
+    params.set<std::vector<Real> >("areas") = _Areas;
+    params.set<Real>("Volume") = _ref_volume;
+    params.set<std::vector<Real> >("normals") = _normals;
+    params.set<std::string>("eos_function") = getParam<std::string>("eos_function");
+
+    params.set<std::vector<Real> >("K") = _k_coeffs;
+    params.set<Real>("Area") = _ref_area;
+
+    // coupling variables
+    params.set<std::vector<std::string> >("u") = cv_u;
+    params.set<std::vector<std::string> >("rhou") = cv_rhou;
+    if(_model_type == Model::EQ_MODEL_3)
+    	params.set<std::vector<std::string> >("rhoE") = cv_rhoE;
+
+    //params.set<Model::EModelType>("model_type") = _model_type;
+
+    _sim.addScalarKernel("FlowJunctionConstraint", genName("ScalarKernel", _id, _name), params);
+  }
+
+}
