d1c5347473959ccd306b5adf754e84e888e5fa06
Derek Gaston <derek.gaston@inl.gov>
2012-07-06 14:35:58 +0000

    fixes for generated mesh
    r11792

--- gui/MeshInfo.py
+++ gui/MeshInfo.py
@@ -62,13 +62,15 @@ class ExodusIIMeshInfo(MeshInfo):
 class GeneratedMeshInfo(MeshInfo):
   def __init__(self, mesh_item_data):
     MeshInfo.__init__(self, mesh_item_data)
-    self.block_names = ['0']
-    self.sideset_names = ['left','right','0','1']
+    self.block_names = set(['0'])
+    self.sideset_names = set(['left','right','0','1'])
     if 'dim' in self.mesh_item_data:
       if int(mesh_item_data['dim']) >= 2:
-        self.sideset_names += ['bottom','top','2','3']
+        self.sideset_names |= set(['bottom','top','2','3'])
       if int(mesh_item_data['dim']) == 3:
-        self.sideset_names += ['front','back','4','5']
+        self.sideset_names |= set(['front','back','4','5'])
+
+    self.nodeset_names = set()
 
   def blockNames(self):
     return self.block_names
@@ -76,3 +78,5 @@ class GeneratedMeshInfo(MeshInfo):
   def sidesetNames(self):
     return self.sideset_names
 
+  def nodesetNames(self):
+    return self.nodeset_names
