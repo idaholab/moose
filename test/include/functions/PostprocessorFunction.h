7c41ba7699dafe66b92c6341117a0f4f0d81e34f
Derek Gaston <derek.gaston@inl.gov>
2012-07-06 00:33:23 +0000

    add missing files, ref #1199
    r11766

---
+++ test/include/functions/PostprocessorFunction.h
@@ -0,0 +1,36 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#ifndef POSTPROCESSORFUNCTION_H
+#define POSTPROCESSORFUNCTION_H
+
+#include "Function.h"
+
+class PostprocessorFunction;
+
+template<>
+InputParameters validParams<PostprocessorFunction>();
+
+class PostprocessorFunction : public Function
+{
+public:
+  PostprocessorFunction(const std::string & name, InputParameters parameters);
+
+  virtual Real value(Real t, const Point & p);
+  
+protected:
+  PostprocessorValue & _pp;
+};
+
+#endif //POSTPROCESSORFUNCTION_H
---
+++ test/src/functions/PostprocessorFunction.C
@@ -0,0 +1,36 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#include "PostprocessorFunction.h"
+
+template<>
+InputParameters validParams<PostprocessorFunction>()
+{
+  InputParameters params = validParams<Function>();
+  params.addRequiredParam<std::string>("pp", "The name of the postprocessor you are trying to get.");
+  return params;
+}
+
+PostprocessorFunction::PostprocessorFunction(const std::string & name, InputParameters parameters) :
+    Function(name, parameters),
+    _pp(getPostprocessorValue(getParam<std::string>("pp")))
+{
+  std::cout<<"Stuff!"<<std::endl;
+}
+
+Real
+PostprocessorFunction::value(Real /*t*/, const Point & /*p*/)
+{
+  return _pp;
+}
