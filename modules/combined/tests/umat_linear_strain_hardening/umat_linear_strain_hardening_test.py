91845a4422498fee98a81551cd6f5b9d73a30cc5
Derek Gaston <derek.gaston@inl.gov>
2012-07-26 09:29:12 +0000

    turn off abaqus test for now
    r12354

--- modules/combined/tests/umat_linear_strain_hardening/umat_linear_strain_hardening_test.py
+++ modules/combined/tests/umat_linear_strain_hardening/umat_linear_strain_hardening_test.py
@@ -1,6 +1,7 @@
 from options import *
 
 test = { INPUT : 'umat_linear_strain_hardening.i',
-         EXODIFF : ['out.e']
+         EXODIFF : ['out.e'],
+         SKIP : 'until cody gets off his bum'
          }
 
