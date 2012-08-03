d1f6683b095029c205cd4a0001345194fb9def1b
Paul Talbot <paul.talbot@inl.gov>
2012-08-03 09:20:07 +0000

    Adding tests for hermite family of finite element types.
    r12484

---
+++ test/tests/fe_hermite_test/fe_hermite_test.py
@@ -0,0 +1,17 @@
+from options import *
+
+test_hermite_3_1d = { INPUT : 'hermite-3-1d.i',
+                  EXODIFF : ['hermite-3-1d_out.e'],
+                  SCALE_REFINE : 5
+                  }
+
+test_hermite_3_2d = { INPUT : 'hermite-3-2d.i',
+                  EXODIFF : ['hermite-3-2d_out.e'],
+                  SCALE_REFINE : 5
+                  }
+
+test_hermite_3_3d = { INPUT : 'hermite-3-3d.i',
+                  EXODIFF : ['hermite-3-3d_out.e'],
+                  SCALE_REFINE : 5
+                  }
+
