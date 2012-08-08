8b21168736a96d9fa95649b8b40ebb26cd619c46
Cody Permann <cody.permann@inl.gov>
2012-08-08 07:56:47 +0000

    More Test Twiddling
    r12552

--- modules/combined/tests/abaqus_creep_swelling/abaqus_creep_swelling_test.py
+++ modules/combined/tests/abaqus_creep_swelling/abaqus_creep_swelling_test.py
@@ -2,7 +2,7 @@ from options import *
 
 test = { INPUT : 'abaqus_creep_swelling.i',
          EXODIFF : ['out.e'],
-         ABS_ZERO : 1e-7,
+         ABS_ZERO : 1e-6,
          LIBRARY_MODE : ['DYNAMIC']
          }
 
--- modules/combined/tests/abaqus_implicit_creep/abaqus_implicit_creep_test.py
+++ modules/combined/tests/abaqus_implicit_creep/abaqus_implicit_creep_test.py
@@ -2,7 +2,7 @@ from options import *
 
 test = { INPUT : 'abaqus_implicit_creep.i',
          EXODIFF : ['out.e'],
-         ABS_ZERO : 1e-7,
+         ABS_ZERO : 1e-6,
          LIBRARY_MODE : ['DYNAMIC']
          }
 
