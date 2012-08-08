8eb911b2f2c66bb0b17f1c769c6145c4552b6bfd
Cody Permann <cody.permann@inl.gov>
2012-08-08 07:41:26 +0000

    Loosening exodiff criteria
    r12551

--- modules/combined/tests/abaqus_creep_swelling/abaqus_creep_swelling_test.py
+++ modules/combined/tests/abaqus_creep_swelling/abaqus_creep_swelling_test.py
@@ -2,6 +2,7 @@ from options import *
 
 test = { INPUT : 'abaqus_creep_swelling.i',
          EXODIFF : ['out.e'],
+         ABS_ZERO : 1e-7,
          LIBRARY_MODE : ['DYNAMIC']
          }
 
--- modules/combined/tests/abaqus_implicit_creep/abaqus_implicit_creep_test.py
+++ modules/combined/tests/abaqus_implicit_creep/abaqus_implicit_creep_test.py
@@ -2,6 +2,7 @@ from options import *
 
 test = { INPUT : 'abaqus_implicit_creep.i',
          EXODIFF : ['out.e'],
+         ABS_ZERO : 1e-7,
          LIBRARY_MODE : ['DYNAMIC']
          }
 
