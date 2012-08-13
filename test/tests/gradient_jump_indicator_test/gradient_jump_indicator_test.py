a292f6f88a6d45e61ae96f9be528f104b988a8bf
Cody Permann <cody.permann@inl.gov>
2012-08-13 13:24:48 +0000

    Disabling gradient jump indicator test in parallel - refs #1295
    r12703

--- test/tests/gradient_jump_indicator_test/gradient_jump_indicator_test.py
+++ test/tests/gradient_jump_indicator_test/gradient_jump_indicator_test.py
@@ -2,7 +2,8 @@ from options import *
 
 test = { INPUT : 'gradient_jump_indicator_test.i',
          EXODIFF : ['gradient_jump_indicator_test_out.e'],
-         SCALE_REFINE : 2
+         SCALE_REFINE : 2,
+         MAX_PARALLEL : 1
          }
 
 
