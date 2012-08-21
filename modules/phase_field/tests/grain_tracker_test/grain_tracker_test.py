a077415ee98b7d5bcbb8d196b7be9363510475a3
Cody Permann <cody.permann@inl.gov>
2012-08-21 09:59:09 +0000

    Disabling test - not stable across platforms
    r12906

--- modules/phase_field/tests/grain_tracker_test/grain_tracker_test.py
+++ modules/phase_field/tests/grain_tracker_test/grain_tracker_test.py
@@ -1,5 +1,6 @@
 from options import *
 
 test = { INPUT : 'grain_tracker_same.i',
-         EXODIFF : ['grain_tracker_same_out.e']
+         EXODIFF : ['grain_tracker_same_out.e'],
+         SKIP: 'Not stable'
         }
