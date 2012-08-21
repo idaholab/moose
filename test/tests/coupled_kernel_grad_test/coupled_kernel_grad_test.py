e8dca0779342e332016265be048cf58b18661c88
Jason Miller <jason.miller@inl.gov>
2012-08-21 13:30:39 +0000

    adjust coupled_kernel_grad_test scale_refine settings so they do not timeout. Fixes #1325
    r12924

--- test/tests/coupled_kernel_grad_test/coupled_kernel_grad_test.py
+++ test/tests/coupled_kernel_grad_test/coupled_kernel_grad_test.py
@@ -2,6 +2,6 @@ from options import *
 
 test_coupled_kernel_grad = { INPUT : 'coupled_kernel_grad_test.i',
                   EXODIFF : ['coupled_kernel_grad_test_out.e'],
-                  SCALE_REFINE : 5,
+                  SCALE_REFINE : 2,
                   MAX_PARALLEL : 1
                   }
