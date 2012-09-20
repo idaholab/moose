33dab349e59ff8dcc9f5318653a2b7af29e7ae69
Hongbin Zhang <hongbin.zhang@inl.gov>
2012-09-20 09:39:24 +0000

    SGEOS was expended to include liquid phase and gas phase
    r13739

---
+++ modules/thm/include/utils/Numerics.h
@@ -0,0 +1,12 @@
+#ifndef NUMERICS_H
+#define NUMERICS_H
+
+#include "libmesh_common.h"
+
+namespace Numerics {
+  extern void* auxpt;
+
+Real Newton_Solver(Real x0, Real f0, Real (*fct)(Real x, Real f0), Real (*dfct)(Real x), Real tola, Real tolr, unsigned int n_max);
+}
+
+#endif  // NUMERICS_H
---
+++ modules/thm/src/utils/Numerics.C
@@ -0,0 +1,34 @@
+#include "Numerics.h"
+
+namespace Numerics {
+
+  void* auxpt = NULL;
+
+  Real Newton_Solver(Real x0, Real f0, Real (*fct)(Real x, Real f0), Real (*dfct)(Real x), Real tola, Real tolr, unsigned int n_max)
+  {
+        unsigned int n=0; // iteration index
+        if(x0==0)  x0 = tola;  // avoid setting initial guess to 0; 
+
+        Real res=(*fct)(x0,f0); // store the current residual value
+        Real xnew=x0;
+        Real xold=x0;
+
+        while ( n<= n_max)
+        {
+                if(std::fabs(res)<tola && std::fabs((xnew-xold)/xold)<tolr) {
+                        std::cout<<"  Newton iteration: total iteration "<<n<<" res= "<<res<<std::endl; //debug
+                        return xnew;
+                }
+
+                n++;
+                xold=xnew;
+                res=(*fct)(xold, f0);
+                xnew=xold-res/(*dfct)(xold);
+                std::cout<<"  Newton iteration: n= "<<n<<" xnew= "<<xnew<<" res= "<<res<<std::endl; //debug
+        }
+        std::cout << "ERROR: Newton iteration cannot converge after "<<n-1<< std::endl;
+        return 0;
+  }
+ 
+}
+
