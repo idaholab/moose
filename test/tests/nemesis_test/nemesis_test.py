2c985f592f0c94c9490292d60b85798c0c31e7ee
Cody Permann <cody.permann@inl.gov>
2012-09-24 07:38:54 +0000

    Skipping broken test until fixed in libMesh
    r13767

--- test/tests/nemesis_test/nemesis_test.py
+++ test/tests/nemesis_test/nemesis_test.py
@@ -4,5 +4,6 @@ test = { INPUT : 'nemesis_test.i',
          EXODIFF : ['out.e.4.0', 'out.e.4.1', 'out.e.4.2', 'out.e.4.3'],
          MIN_PARALLEL : 4,
          MAX_PARALLEL : 4,
-         MESH_MODE : ['PARALLEL']
+         MESH_MODE : ['PARALLEL'],
+         SKIP : 'Libmesh Bug #1365'
          }
