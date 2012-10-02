d1d91ea3caecb7c231cf00a643fb791d7a43730e
Cody Permann <cody.permann@inl.gov>
2012-10-02 10:04:33 +0000

    Adding CLANG option to TestHarness - #1438
    r14025

--- modules/combined/tests/umat_linear_strain_hardening/umat_linear_strain_hardening_test.py
+++ modules/combined/tests/umat_linear_strain_hardening/umat_linear_strain_hardening_test.py
@@ -2,6 +2,7 @@ from options import *
 
 test = { INPUT : 'umat_linear_strain_hardening.i',
          EXODIFF : ['out.e'],
-         LIBRARY_MODE : ['DYNAMIC']
+         LIBRARY_MODE : ['DYNAMIC'],
+         COMPILER : ['GCC', 'INTEL']
          }
 
