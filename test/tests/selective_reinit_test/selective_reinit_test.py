6d4c1748cd31c1525679648ab89a710711344548
Derek Gaston <derek.gaston@inl.gov>
2012-10-04 15:33:55 +0000

    dont run this test through valgrind
    r14149

--- test/tests/selective_reinit_test/selective_reinit_test.py
+++ test/tests/selective_reinit_test/selective_reinit_test.py
@@ -5,7 +5,8 @@ test = { INPUT : 'selective_reinit_test.i',
          MAX_TIME : 20,
          MAX_THREADS : 1,
          MAX_PARALLEL : 1,
-         METHOD : ['OPT']
+         METHOD : ['OPT'],
+         NO_VALGRIND : True
          }
 
 
