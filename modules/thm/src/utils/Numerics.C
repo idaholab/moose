22bc30b555db62cffd07cf2010a2e118c5f9e6be
Haihua Zhao <haihua.zhao@inl.gov>
2012-10-12 11:42:51 +0000

    fix StiffenedGasEquationOfStateLiquid::saturationPressure convergance issue at low temperature; update TwoPhaseStiffenedGasEOS: now single phase regions work, still debug two phase region
    r14271

--- modules/thm/src/utils/Numerics.C
+++ modules/thm/src/utils/Numerics.C
@@ -1,4 +1,5 @@
 #include "Numerics.h"
+#include <sstream>
 
 namespace Numerics {
 
@@ -6,28 +7,32 @@ namespace Numerics {
 
   Real Newton_Solver(Real x0, Real f0, Real (*fct)(Real x, Real f0), Real (*dfct)(Real x), Real tola, Real tolr, unsigned int n_max)
   {
-        unsigned int n=0; // iteration index
-        if(x0==0)  x0 = tola;  // avoid setting initial guess to 0; 
+    unsigned int n=0; // iteration index
+    if(x0==0)  x0 = tola;  // avoid setting initial guess to 0; 
 
-        Real res=(*fct)(x0,f0); // store the current residual value
-        Real xnew=x0;
-        Real xold=x0;
+    Real res=(*fct)(x0,f0); // store the current residual value
+    Real xnew=x0;
+    Real xold=x0;
 
-        while ( n<= n_max)
-        {
-                if(std::fabs(res)<tola && std::fabs((xnew-xold)/xold)<tolr) {
-                        //std::cout<<"  Newton iteration: total iteration "<<n<<" res= "<<res<<std::endl; //debug
-                        return xnew;
-                }
+    while ( n<= n_max)
+    {
+      if(std::fabs(res)<tola && std::fabs((xnew-xold)/xold)<tolr) 
+      {
+        //std::cout<<"  Newton iteration: total iteration "<<n<<" res= "<<res<<std::endl; //debug
+        return xnew;
+      }
 
-                n++;
-                xold=xnew;
-                res=(*fct)(xold, f0);
-                xnew=xold-res/(*dfct)(xold);
-                //std::cout<<"  Newton iteration: n= "<<n<<" xnew= "<<xnew<<" res= "<<res<<std::endl; //debug
-        }
-        std::cerr << "ERROR: Newton iteration cannot converge after "<<n-1<< std::endl;
-        return 0;
+      n++;
+      xold=xnew;
+      res=(*fct)(xold, f0);
+      xnew=xold-res/(*dfct)(xold);
+      std::cout<<"  Newton iteration: n= "<<n<<" xold= "<<xold<<" xnew= "<<xnew<<" res= "<<res<<std::endl; //debug
+    }
+    
+    std::stringstream error_message;
+    error_message << "Newton iteration cannot converge after " << (n-1)<<"; final value is "<<xnew;
+    mooseError(error_message.str() );
+    return 0.;
   }
  
 }
