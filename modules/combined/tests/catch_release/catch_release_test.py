ef2d40d1d7c8c3e9f18fae3348ee5cf1a188281a
Benjamin Spencer <benjamin.spencer@inl.gov>
2012-10-26 08:33:26 +0000

    Disable catch_release_test for older PETSc
    r14614

--- modules/combined/tests/catch_release/catch_release_test.py
+++ modules/combined/tests/catch_release/catch_release_test.py
@@ -2,6 +2,7 @@ from options import *
 
 test = { INPUT : 'catch_release.i',
          EXODIFF : ['catch_release_out.e'],
-         CUSTOM_CMP : 'catch_release.exodiff'
+         CUSTOM_CMP : 'catch_release.exodiff',
+         PETSC_VERSION : ['>=3.1']
        }
 
