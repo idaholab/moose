543c912794b19bedb9cf6cc1ef40f0fce2785dc0
Cody Permann <cody.permann@inl.gov>
2012-11-01 15:55:30 +0000

    Revert "Test Harness Refactor - refs #1405"
    This reverts commit 3b3e9fda26d2316a81e10a62a2c42cc3aaf70a6f.
    
    r14683

--- modules/combined/tests/internal_volume/internal_volume_test.py
+++ modules/combined/tests/internal_volume/internal_volume_test.py
@@ -1,28 +1,22 @@
 from options import *
 
 test = { INPUT : 'internal_volume.i',
-         CSVDIFF : ['out.csv'],
-         TYPE : 'CSVDiff' }
+         CSVDIFF : ['out.csv']}
 
 test_hex20 = { INPUT : 'internal_volume_hex20.i',
                PREREQ : ['test'],
-               CSVDIFF : ['out.csv'],
-               TYPE : 'CSVDiff' }
+               CSVDIFF : ['out.csv']}
 
 test_rz = { INPUT : 'internal_volume_rz.i',
-            CSVDIFF : ['out_rz.csv'],
-            TYPE : 'CSVDiff' }
+            CSVDIFF : ['out_rz.csv']}
 
 test_quad8_rz = { INPUT : 'internal_volume_rz_quad8.i',
                   PREREQ : ['test_rz'],
-                  CSVDIFF : ['out_rz.csv'],
-                  TYPE : 'CSVDiff' }
+                  CSVDIFF : ['out_rz.csv']}
 
 test_rz_displaced = { INPUT : 'internal_volume_rz_displaced.i',
-                      CSVDIFF : ['internal_volume_rz_displaced_out.csv'],
-                      TYPE : 'CSVDiff' }
+                      CSVDIFF : ['internal_volume_rz_displaced_out.csv']}
 
 test_rz_cone = { INPUT : 'internal_volume_rz_cone.i',
-                 CSVDIFF : ['internal_volume_rz_cone_out.csv'],
-                 TYPE : 'CSVDiff' }
+                      CSVDIFF : ['internal_volume_rz_cone_out.csv']}
 
