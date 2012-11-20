917c4b96a677c2f6b86864b6ebb54dc80b2121ef
Cody Permann <cody.permann@inl.gov>
2012-11-20 16:47:32 +0000

    Use the GrainTrackerAux kernel instead of nodal
    r15022

--- modules/phase_field/src/postprocessors/GrainTracker.C
+++ modules/phase_field/src/postprocessors/GrainTracker.C
@@ -40,18 +40,20 @@ GrainTracker::~GrainTracker()
 }
 
 Real
-GrainTracker::getNodeValue(unsigned int node_id) const
+GrainTracker::getNodeValue(unsigned int node_id, unsigned int var_idx) const
 {
+  mooseAssert(var_idx < _maps_size, "Index out of range");
+  
   if (_t_step < _tracking_step)
     return 0;
 
   // When running threaded - we won't have the complete map
   // since we don't exchange areas marked with zero.  In this
   // case we can just return zero for nodes NOT in the map
-  if (_bubble_maps[0].find(node_id) == _bubble_maps[0].end())   // Single mode ONLY
+  if (_bubble_maps[var_idx].find(node_id) == _bubble_maps[var_idx].end())
     return 0;
-
-  unsigned int region_id = _bubble_maps[0].at(node_id);
+  
+  unsigned int region_id = _bubble_maps[var_idx].at(node_id);
   return _region_to_grain.at(region_id);
 }
 
