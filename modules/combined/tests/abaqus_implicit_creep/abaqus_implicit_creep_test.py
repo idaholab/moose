09b95a5f593c851425551d2bd6b78167305788a8
Cody Permann <cody.permann@inl.gov>
2013-01-12 17:35:09 +0000

    Skipping Broken Elk Test, refs #1613
    r15793

--- modules/combined/tests/abaqus_implicit_creep/abaqus_implicit_creep_test.py
+++ modules/combined/tests/abaqus_implicit_creep/abaqus_implicit_creep_test.py
@@ -3,7 +3,7 @@ from options import *
 test = { INPUT : 'abaqus_implicit_creep.i',
          EXODIFF : ['out.e'],
          ABS_ZERO : 1e-6,
-	 PETSC_VERSION : ['>=3.1.0'],
-         LIBRARY_MODE : ['DYNAMIC']
+         LIBRARY_MODE : ['DYNAMIC'],
+	 SKIP : 'Convergence issues #1613'
          }
 
