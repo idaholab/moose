947146a6f1e4e91da11a3f6c0e68c102958e5e8c
Cody Permann <cody.permann@inl.gov>
2013-01-12 16:11:44 +0000

    Improve code coverage - refs #920
    r15791

--- modules/combined/tests/abaqus_implicit_creep/abaqus_implicit_creep_test.py
+++ modules/combined/tests/abaqus_implicit_creep/abaqus_implicit_creep_test.py
@@ -3,6 +3,7 @@ from options import *
 test = { INPUT : 'abaqus_implicit_creep.i',
          EXODIFF : ['out.e'],
          ABS_ZERO : 1e-6,
+	 PETSC_VERSION : ['>=3.1.0'],
          LIBRARY_MODE : ['DYNAMIC']
          }
 
