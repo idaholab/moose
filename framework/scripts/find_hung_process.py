cf4e31fcd5b914c23f71f2ccd9392365919465a5
Cody Permann <cody.permann@inl.gov>
2013-01-20 09:16:34 +0000

    Trimming the stacks instead of using index trickery to index into them - refs #1644
    r15988

--- framework/scripts/find_hung_process.py
+++ framework/scripts/find_hung_process.py
@@ -33,20 +33,29 @@ def main():
     hosts.append(i)
 
   unique_stack_traces = {}
-  regex = re.compile("^#0", re.M | re.S)
+  trace_regex = re.compile("^#0", re.M | re.S)
+  last_lines_regex = re.compile("(?:.*\n){" + str(num_to_keep) + "}\Z", re.M)
+
   for host in hosts:
     command = "ssh " + host + " \"ps -e | grep " + application + " | awk '{print \$1}' | xargs -I {} gdb --batch --pid={} -ex bt 2>&1 | grep '^#' \""
     p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
     output = p.communicate()[0]
 
     # Python FAIL - We have to re-glue the tokens we threw away from our split (Perl 1 : Python 0)
-    traces = ["#0" + trace for trace in regex.split(output)]
+    traces = ["#0" + trace for trace in trace_regex.split(output)]
     for trace in traces:
       if trace == '#0': # More Python FAIL - why you suck so bad?
         continue
+
+      # If the user requested to save only the last few lines, do that here
+      if num_to_keep:
+        m = last_lines_regex.search(trace)
+        if m:
+          trace = m.group(0)
+
       unique = ''
       for bt in unique_stack_traces:
-        if compare_traces(trace, bt, num_to_keep):
+        if compare_traces(trace, bt):
           unique = bt
 
       if unique == '':
@@ -59,22 +68,17 @@ def main():
   for trace, count in unique_stack_traces.iteritems():
     print "**********************************\nCount: " + str(count) + "\n" + trace
 
-def compare_traces(trace1, trace2, num_keep):
+def compare_traces(trace1, trace2):
   lines1 = trace1.split("\n")
   lines2 = trace2.split("\n")
 
-  idx_adj = 0
-  if num_keep == 0:
-    if len(lines1) != len(lines2):
-      return False
-    num_keep = len(lines1)
-  else:
-    idx_adj = len(lines1) - len(lines2)
+  if len(lines1) != len(lines2):
+    return False
 
   # Only compare the stack trace part - not the memoery addresses
-  for i in xrange(len(lines1) - num_keep, len(lines1)):
+  for i in xrange(len(lines1)):
     line1 = lines1[i].split()[2:]
-    line2 = lines2[i - idx_adj].split()[2:]
+    line2 = lines2[i].split()[2:]
 
     if line1 != line2:
       return False
