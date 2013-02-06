2eaef9e68388d594063926b265a0f33cd686a82f
Bradley Fromm <bradley.fromm@inl.gov>
2013-02-06 11:21:44 +0000

    standardized 'crys_num' variable as an unsigned integer
    r16552

---
+++ modules/phase_field/src/utils/Euler2RGB.C
@@ -0,0 +1,386 @@
+/******************************************************************************/
+/*  Filename: Euler2RGB.C                                                     */
+/*                                                                            */
+/*  Summary:  This function rotates a set of three Bunge Euler angles into    */
+/*            the standard Stereographic triangle, interpolates the RGB color */
+/*            value based on a user selected reference sample direction, and  */
+/*            outputs an integer value representing the RGB tuplet usable for */
+/*            plotting inverse pole figure colored grain maps.  The program   */
+/*            can accommodate any of the seven crystal systems.               */
+/*                                                                            */
+/*  Inputs:   1) Reference sample direction (sd) input as an integer between  */
+/*               1 and 3.  Options are [100], [010], and [001] with [001]     */
+/*               being the most common choice found in the literature. Input  */
+/*               "1" to select [100], "2" to select [010], and "3" to select  */
+/*               the [001] sample direction.                                  */
+/*            2) Set of three Euler angles (phi1, PHI, phi2) in the Bunge     */
+/*               notation (must be in radians)                                */
+/*            3) Phase number "phase" used to assign black color values to    */
+/*               voids (phase = 0)                                            */
+/*            4) Integer value of crystal class as defined by OIM Software:   */
+/*                    "43" for cubic                                          */
+/*                    "62" for hexagonal                                      */
+/*                    "42" for tetragonal                                     */
+/*                    "32" for trigonal                                       */
+/*                    "22" for orthorhombic                                   */
+/*                     "2" for monoclinic                                     */
+/*                     "1" for triclinic                                      */
+/*                     "0" for unindexed points (bad data point)              */
+/*                                                                            */
+/*  Output:  Integer value of RGB calculated from:                            */
+/*           RGB = red*256^2 + green*256 + blue (where red, green, and blue   */
+/*           are integer values between 0 and 255)                            */
+/*                                                                            */
+/******************************************************************************/
+
+// Include files
+#include "Euler2RGB.h"
+
+Real Euler2RGB(unsigned int sd, Real phi1, Real PHI, Real phi2, unsigned int phase, unsigned int sym)
+{
+  // Define Constants
+  const Real pi = 4.0 * std::atan(1.0);
+  const Real pi_x2 = 2 * pi;
+  const Real a = (std::sqrt (3)) / 2;
+
+  // Preallocate and zero variables
+  int i = 0;
+  int j =0;
+  int k = 0;
+  int index = 0;
+  int nsym = 1;
+  int RGBint = 0;
+
+  Real blue = 0.0;
+  Real chi = 0.0;
+  Real chi_min = 0.0;
+  Real chi_max = 0.0;
+  Real chi_max2 = 0.0;
+  Real color = 0.0;
+  Real eta = 0.0;
+  Real eta_min = 0.0;
+  Real eta_max = 0.0;
+  Real green = 0.0;
+  Real maxRGB = 0.0;
+  Real red = 0.0;
+  Real sum = 0.0;
+
+  int ref_dir[3] = {0, 0, 0};
+  Real g[3][3] = {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}};
+  Real hkl[3] = {0.0, 0.0, 0.0};
+  Real S[3][3] = {{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}};
+  Real (*SymOps)[3][3];
+
+  std::vector<Real> RGB (3,0.0);
+
+  // Assign reference sample direction
+  switch (sd)
+  {
+  case 1:
+    ref_dir[0] = 1;
+    ref_dir[1] = 0;
+    ref_dir[2] = 0;
+    break;
+
+  case 2:
+    ref_dir[0] = 0;
+    ref_dir[1] = 1;
+    ref_dir[2] = 0;
+    break;
+
+  case 3:
+    ref_dir[0] = 0;
+    ref_dir[1] = 0;
+    ref_dir[2] = 1;
+    break;
+  };
+
+  // Define symmetry operators for each of the seven crystal systems
+  Real SymOpsCubic [24][3][3] =
+    {
+      {{ 1,  0,  0}, { 0,  1,  0}, { 0,  0,  1}},
+      {{ 0,  0,  1}, { 1,  0,  0}, { 0,  1,  0}},
+      {{ 0,  1,  0}, { 0,  0,  1}, { 1,  0,  0}},
+      {{ 0, -1,  0}, { 0,  0,  1}, {-1,  0,  0}},
+      {{ 0, -1,  0}, { 0,  0, -1}, { 1,  0,  0}},
+      {{ 0,  1,  0}, { 0,  0, -1}, {-1,  0,  0}},
+      {{ 0,  0, -1}, { 1,  0,  0}, { 0, -1,  0}},
+      {{ 0,  0, -1}, {-1,  0,  0}, { 0,  1,  0}},
+      {{ 0,  0,  1}, {-1,  0,  0}, { 0, -1,  0}},
+      {{-1,  0,  0}, { 0,  1,  0}, { 0,  0, -1}},
+      {{-1,  0,  0}, { 0, -1,  0}, { 0,  0,  1}},
+      {{ 1,  0,  0}, { 0, -1,  0}, { 0,  0, -1}},
+      {{ 0,  0, -1}, { 0, -1,  0}, {-1,  0,  0}},
+      {{ 0,  0,  1}, { 0, -1,  0}, { 1,  0,  0}},
+      {{ 0,  0,  1}, { 0,  1,  0}, {-1,  0,  0}},
+      {{ 0,  0, -1}, { 0,  1,  0}, { 1,  0,  0}},
+      {{-1,  0,  0}, { 0,  0, -1}, { 0, -1,  0}},
+      {{ 1,  0,  0}, { 0,  0, -1}, { 0,  1,  0}},
+      {{ 1,  0,  0}, { 0,  0,  1}, { 0, -1,  0}},
+      {{-1,  0,  0}, { 0,  0,  1}, { 0,  1,  0}},
+      {{ 0, -1,  0}, {-1,  0,  0}, { 0,  0, -1}},
+      {{ 0,  1,  0}, {-1,  0,  0}, { 0,  0, -1}},
+      {{ 0,  1,  0}, { 1,  0,  0}, { 0,  0, -1}},
+      {{ 0, -1,  0}, { 1,  0,  0}, { 0,  0,  1}}
+    };
+
+  Real SymOpsHexagonal[12][3][3] =
+    {
+      {{   1,  0, 0}, { 0,    1, 0}, {0, 0,  1}},
+      {{-0.5,  a, 0}, {-a, -0.5, 0}, {0, 0,  1}},
+      {{-0.5, -a, 0}, { a, -0.5, 0}, {0, 0,  1}},
+      {{ 0.5,  a, 0}, {-a,  0.5, 0}, {0, 0,  1}},
+      {{  -1,  0, 0}, { 0,   -1, 0}, {0, 0,  1}},
+      {{ 0.5, -a, 0}, { a,  0.5, 0}, {0, 0,  1}},
+      {{-0.5, -a, 0}, {-a,  0.5, 0}, {0, 0, -1}},
+      {{   1,  0, 0}, { 0,   -1, 0}, {0, 0, -1}},
+      {{-0.5,  a, 0}, { a,  0.5, 0}, {0, 0, -1}},
+      {{ 0.5,  a, 0}, { a, -0.5, 0}, {0, 0, -1}},
+      {{  -1,  0, 0}, { 0,    1, 0}, {0, 0, -1}},
+      {{ 0.5, -a, 0}, {-a, -0.5, 0}, {0, 0, -1}}
+    };
+
+  Real SymOpsTetragonal [8][3][3] =
+    {
+      {{ 1,  0, 0}, { 0,  1, 0}, {0, 0,  1}},
+      {{-1,  0, 0}, { 0,  1, 0}, {0, 0, -1}},
+      {{ 1,  0, 0}, { 0, -1, 0}, {0, 0, -1}},
+      {{-1,  0, 0}, { 0, -1, 0}, {0, 0,  1}},
+      {{ 0,  1, 0}, {-1,  0, 0}, {0, 0,  1}},
+      {{ 0, -1, 0}, { 1,  0, 0}, {0, 0,  1}},
+      {{ 0,  1, 0}, { 1,  0, 0}, {0, 0, -1}},
+      {{ 0, -1, 0}, {-1,  0, 0}, {0, 0, -1}}
+    };
+
+  Real SymOpsTrigonal [6][3][3] =
+    {
+      {{   1,  0, 0}, { 0,    1, 0}, {0, 0,  1}},
+      {{-0.5,  a, 0}, {-a, -0.5, 0}, {0, 0,  1}},
+      {{-0.5, -a, 0}, { a, -0.5, 0}, {0, 0,  1}},
+      {{ 0.5,  a, 0}, { a, -0.5, 0}, {0, 0, -1}},
+      {{  -1,  0, 0}, { 0,    1, 0}, {0, 0,  1}},
+      {{ 0.5, -a, 0}, {-a, -0.5, 0}, {0, 0, -1}}
+    };
+
+  Real SymOpsOrthorhombic [4][3][3] =
+    {
+      {{ 1, 0, 0}, {0,  1, 0}, {0, 0,  1}},
+      {{-1, 0, 0}, {0,  1, 0}, {0, 0, -1}},
+      {{ 1, 0, 0}, {0, -1, 0}, {0, 0,  1}},
+      {{-1, 0, 0}, {0, -1, 0}, {0, 0,  1}}
+    };
+
+  Real SymOpsMonoclinic [2][3][3] =
+    {
+      {{ 1, 0, 0}, {0, 1, 0}, {0, 0,  1}},
+      {{-1, 0, 0}, {0, 1, 0}, {0, 0, -1}}
+    };
+
+  Real SymOpsTriclinic [1][3][3] =
+    {
+      {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}}
+    };
+
+  // Assign parameters based on crystal class (sym)
+  // Load cubic parameters (class 432)
+  if (sym == 43)
+  {
+    nsym = 24;
+    SymOps = SymOpsCubic;
+    eta_min =  0 * (pi / 180);
+    eta_max = 45 * (pi / 180);
+    chi_min =  0 * (pi / 180);
+    chi_max = std::acos(std::sqrt(1 / (2 + (std::tan(std::pow(eta_max, 2))))));
+  }
+
+  //  Load hexagonal parameters (class 622)
+  else if (sym == 62)
+  {
+    nsym = 12;
+    SymOps = SymOpsHexagonal;
+    eta_min =  0 * (pi / 180);
+    eta_max = 30 * (pi / 180);
+    chi_min =  0 * (pi / 180);
+    chi_max = pi / 2;
+  }
+
+  //  Load tetragonal parameters (class 422)
+  else if (sym == 42)
+  {
+    nsym = 8;
+    SymOps = SymOpsTetragonal;
+    eta_min =  0 * (pi / 180);
+    eta_max = 45 * (pi / 180);
+    chi_min =  0 * (pi / 180);
+    chi_max = pi / 2;
+  }
+
+  //  Load trigonal parameters (class 32)
+  else if (sym == 32)
+  {
+    nsym = 6;
+    SymOps = SymOpsTrigonal;
+    eta_min =  0 * (pi / 180);
+    eta_max = 60 * (pi / 180);
+    chi_min =  0 * (pi / 180);
+    chi_max = pi / 2;
+  }
+
+  //  Load orthorhombic parameters (class 22)
+  else if (sym == 22)
+  {
+    nsym = 4;
+    SymOps = SymOpsOrthorhombic;
+    eta_min =  0 * (pi / 180);
+    eta_max = 90 * (pi / 180);
+    chi_min =  0 * (pi / 180);
+    chi_max = pi / 2;
+  }
+
+  //  Load monoclinic parameters (class 2)
+  else if (sym == 2)
+  {
+    nsym = 2;
+    SymOps = SymOpsMonoclinic;
+    eta_min =   0 * (pi / 180);
+    eta_max = 180 * (pi / 180);
+    chi_min =   0 * (pi / 180);
+    chi_max =  pi / 2;
+  }
+
+  //  Load triclinic parameters (class 1)
+  else if (sym == 1)
+  {
+    nsym = 1;
+    SymOps = SymOpsTriclinic;
+    eta_min =   0 * (pi / 180);
+    eta_max = 360 * (pi / 180);
+    chi_min =   0 * (pi / 180);
+    chi_max =  pi / 2;
+  }
+
+  // Accomodate non-conforming (bad) data points
+  else
+  {
+    nsym = 0;
+  }
+
+  // Start of main routine //
+  // Assign black RGB values for bad data points (nsym = 0) or voids (phase = 0)
+  if ((nsym == 0) || (phase == 0))
+  {
+    RGB[0] = 0;
+    RGB[1] = 0;
+    RGB[2] = 0;
+  }
+
+  // Assign black RGB value for Euler angles outside of allowable range
+  else if ((phi1 > pi_x2) || (PHI > pi) || (phi2 > pi_x2))
+  {
+    RGB[0] = 0;
+    RGB[1] = 0;
+    RGB[2] = 0;
+  }
+
+  //  Routine for valid set of Euler angles
+  else
+  {
+    // Construct 3X3 orientation matrix
+    g[0][0] =  std::cos(phi1) * std::cos(phi2) - std::sin(phi1) * std::cos(PHI) * std::sin(phi2);
+    g[0][1] =  std::sin(phi1) * std::cos(phi2) + std::cos(phi1) * std::cos(PHI) * std::sin(phi2);
+    g[0][2] =  std::sin(phi2) * std::sin(PHI);
+    g[1][0] = -std::cos(phi1) * std::sin(phi2) - std::sin(phi1) * std::cos(PHI) * std::cos(phi2);
+    g[1][1] = -std::sin(phi1) * std::sin(phi2) + std::cos(phi1) * std::cos(PHI) * std::cos(phi2);
+    g[1][2] =  std::cos(phi2) * std::sin(PHI);
+    g[2][0] =  std::sin(phi1) * std::sin(PHI);
+    g[2][1] = -std::cos(phi1) * std::sin(PHI);
+    g[2][2] =  std::cos(PHI);
+
+    // Loop to sort Euler angles into standard stereographic triangle (SST)
+    index = 0;
+    bool exit_flag = false;
+    while ((exit_flag == false) && (index < nsym))
+    {
+      // Form orientation matrix
+      for (int i = 0; i < 3; i++)
+      {
+        for (int j = 0; j < 3; j++)
+        {
+          S[i][j] = 0.0;
+          for (int k = 0; k < 3; k++)
+          {
+            S[i][j] += SymOps[index][i][k] * g[k][j];
+          }
+        }
+      }
+
+      // Multiple orientation matrix by reference sample direction
+      for (int i = 0; i < 3; i++)
+      {
+        hkl[i] = 0;
+        for (int j = 0; j < 3; j++)
+        {
+          hkl[i] += S[i][j] * ref_dir[j];
+        }
+      }
+
+      // Convert to spherical coordinates (ignore "r" variable since r=1)
+      eta = std::fabs(std::atan2(hkl[1], hkl[0]));
+      chi = std::acos(std::fabs(hkl[2]));
+
+      // Continue if eta and chi values are within the SST
+      if (eta >= eta_min && eta < eta_max && chi >= eta_min && chi < chi_max)
+      {
+        exit_flag = true;
+      }
+
+      // Increment to next symmetry operator if not in SST
+      else
+      {
+        index++;
+      }
+    }
+
+    //  Adjust maximum chi value to ensure it falls within the SST (cubic materials only)
+    if (sym == 43)
+    {
+      chi_max2 = std::acos (std::sqrt (1 / (2 + (std::tan (std::pow(eta, 2))))));
+    }
+    else
+    {
+      chi_max2 = pi / 2;
+    }
+
+    //  Calculate the RGB color values and make adjustments to maximize colorspace
+    red = std::fabs(1.0 - (chi / chi_max2));
+    blue = std::fabs((eta - eta_min) / (eta_max -  eta_min));
+    green = 1.0 - blue;
+
+    blue = blue * (chi / chi_max2);
+    green = green * (chi / chi_max2);
+
+    RGB[0] = std::sqrt(red);
+    RGB[1] = std::sqrt(green);
+    RGB[2] = std::sqrt(blue);
+
+    // Find maximum value of red, green, or blue
+    maxRGB = RGB[0];
+    for (int i = 0; i < 2; i++)
+    {
+      if (RGB[i + 1] > maxRGB)
+      {
+        maxRGB = RGB[i + 1];
+      }
+    }
+
+    //  Adjust RGB values to enforce white center point instead of black
+    for (int i = 0; i < 3; i++)
+    {
+      RGB[i] = RGB[i] / maxRGB;
+    }
+
+    //  Convert RGB tuple to scalar and return integer value
+    RGBint = (round(RGB[0] * 255) * std::pow( 256.0, 2)) + (round (RGB[1] * 255) * 256) + round (RGB[2] *255);
+  }
+  return RGBint;
+}
