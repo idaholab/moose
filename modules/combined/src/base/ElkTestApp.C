0a02fa6b4a20e61678e0fc0171037910ecb8961f
Cody Permann <cody.permann@inl.gov>
2013-02-07 11:59:35 +0000

    Getting rid of one of MOOSE's many inits() - closes #1724
    r16597

--- modules/combined/src/base/ElkTestApp.C
+++ modules/combined/src/base/ElkTestApp.C
@@ -4,8 +4,10 @@
 ElkTestApp::ElkTestApp(int argc, char * argv[]) :
     MooseApp(argc, argv)
 {
-  init();
+  Moose::registerObjects(_factory);
   Elk::registerObjects(_factory);
+  
+  Moose::associateSyntax(_syntax, _action_factory);
   Elk::associateSyntax(_syntax, _action_factory);
 }
 
