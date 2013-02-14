d752fbd5936e7efdba70396fc7ee6738e8a4c4ed
Bradley Fromm <bradley.fromm@inl.gov>
2013-02-14 16:47:21 +0000

    added simple method to evolve stateful material properties
    r16847

--- modules/phase_field/tests/reconstruction_test/reconstruction_test.py
+++ modules/phase_field/tests/reconstruction_test/reconstruction_test.py
@@ -1,10 +1,5 @@
 from options import *
 
-reconstuction_test = { INPUT : 'reconstruction_test.i',
-              EXODIFF : ['out.e-s003']
-              }
-
-
 reconstruction4gr_test = { INPUT : 'reconstruction4gr_test.i',
               EXODIFF : ['4gr.e-s003']
               }
