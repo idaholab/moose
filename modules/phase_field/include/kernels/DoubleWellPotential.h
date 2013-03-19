e683c18849221771a0ad5cc2eb03b4b2d3bd1f74
Michael Pernice <michael.pernice@inl.gov>
2013-03-19 11:33:01 +0000

    Kernel for computing free energy for Cahn-Hilliard and Allen-Cahn problems
    r17533

---
+++ modules/phase_field/include/kernels/DoubleWellPotential.h
@@ -0,0 +1,26 @@
+#ifndef DoubleWellPotential_H
+#define DoubleWellPotential_H
+
+#include "ACBulk.h"
+
+//Forward Declarations
+class DoubleWellPotential;
+
+template<>
+InputParameters validParams<DoubleWellPotential>();
+
+class DoubleWellPotential : public ACBulk
+{
+public:
+
+  DoubleWellPotential(const std::string & name, InputParameters parameters);
+
+protected:
+
+  virtual Real computeDFDOP(PFFunctionType type);
+
+private:
+
+
+};
+#endif //DoubleWellPotential_H
