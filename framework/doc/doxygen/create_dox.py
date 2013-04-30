e3c393720eaf29ab2df5ee00862b832dd0a4fc86
Jason Miller <jason.miller@inl.gov>
2013-04-30 09:39:53 +0000

    Adding ability to link input files to their class. References #1213
    r18512

---
+++ framework/doc/doxygen/create_dox.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import os, sys, argparse
+
+def writeDOX(args, writefile):
+  args.file_object.write("\n/** @example " + writefile + "\n */")
+
+def createDOX(args):
+  file_dict = os.walk(args.application)
+  for dir_name, dir_list, file_list in file_dict:
+    for single_file in file_list:
+      if os.path.splitext(dir_name + '/' + single_file)[1] == '.i':
+        writeDOX(args, single_file)
+  args.file_object.close()
+  print 'Wrote to file:', args.application + '/' + args.application.split('/').pop() + '.dox'
+
+def _verifyARGs(args):
+  if os.path.exists(args.application):
+    args.file_object = open(args.application + '/' + args.application.split('/').pop() + '.dox', 'w')
+    return args
+  else:
+    print 'Path not found:', args.application
+    sys.exit(1)
+
+def _parseARGs(args=None):
+  parser = argparse.ArgumentParser(description='Build dox file for every input file. (Deletes DOX file if present)')
+  parser.add_argument('--application', required=True, help='Path to application')
+  return _verifyARGs(parser.parse_args(args))
+
+if __name__ == '__main__':
+  createDOX(_parseARGs())
