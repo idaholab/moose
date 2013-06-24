874749e91cebd137c4c74691151879d352f0cb11
David Andrs <david.andrs@inl.gov>
2013-06-24 12:36:50 +0000

    Setting _qp before computing the normal (refs #1871)
    r19646

--- framework/src/bcs/NodalNormalBC.C
+++ framework/src/bcs/NodalNormalBC.C
@@ -44,6 +44,7 @@ NodalNormalBC::~NodalNormalBC()
 void
 NodalNormalBC::computeResidual(NumericVector<Number> & residual)
 {
+  _qp = 0;
   _normal = Point(_nx[_qp], _ny[_qp], _nz[_qp]);
   NodalBC::computeResidual(residual);
 }
