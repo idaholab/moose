e5aa062fb51e50ebe927fca967c9458ab7f55ecb
John Peterson <jw.peterson@inl.gov>
2013-07-19 15:08:28 +0000

    App refactoring for ex16.
    Refs #2021.
    
    r20264

--- examples/ex16_timestepper/src/base/ExampleApp.C
+++ examples/ex16_timestepper/src/base/ExampleApp.C
@@ -1,6 +1,13 @@
 #include "ExampleApp.h"
-#include "Example.h"
 #include "Moose.h"
+#include "AppFactory.h"
+
+// Example 16 Includes
+#include "TransientHalf.h"
+#include "ExampleDiffusion.h"
+#include "Convection.h"
+#include "ExampleImplicitEuler.h"
+#include "ExampleMaterial.h"
 
 template<>
 InputParameters validParams<ExampleApp>()
@@ -15,13 +22,31 @@ ExampleApp::ExampleApp(const std::string & name, InputParameters parameters) :
   srand(libMesh::processor_id());
 
   Moose::registerObjects(_factory);
-  Example::registerObjects(_factory);
+  ExampleApp::registerObjects(_factory);
 
   Moose::associateSyntax(_syntax, _action_factory);
-  Example::associateSyntax(_syntax, _action_factory);
+  ExampleApp::associateSyntax(_syntax, _action_factory);
 }
 
 ExampleApp::~ExampleApp()
 {
 }
 
+void ExampleApp::registerApps()
+{
+  registerApp(ExampleApp);
+}
+
+void ExampleApp::registerObjects(Factory & factory)
+{
+  // Register our new executioner
+  registerExecutioner(TransientHalf);
+  registerKernel(ExampleDiffusion);
+  registerKernel(Convection);
+  registerKernel(ExampleImplicitEuler);
+  registerMaterial(ExampleMaterial);
+}
+
+void ExampleApp::associateSyntax(Syntax & syntax, ActionFactory & action_factory)
+{
+}
--- examples/ex16_timestepper/src/main.C
+++ examples/ex16_timestepper/src/main.C
@@ -20,12 +20,11 @@
  * course of the simulation
  */
 
-#include "Example.h"
-//Moose Includes
 #include "MooseInit.h"
 #include "Moose.h"
 #include "MooseApp.h"
 #include "AppFactory.h"
+#include "ExampleApp.h"
 
 // Create a performance log
 PerfLog Moose::perf_log("Example");
@@ -37,7 +36,7 @@ int main(int argc, char *argv[])
   MooseInit init(argc, argv);
 
   // Register this application's MooseApp and any it depends on
-  Example::registerApps();
+  ExampleApp::registerApps();
 
   // This creates dynamic memory that we're responsible for deleting
   MooseApp * app = AppFactory::createApp("ExampleApp", argc, argv);
