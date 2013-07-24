4e6d26b3dee6b0db3f0e1d58a1fd0457a797ae1a
David Andrs <david.andrs@inl.gov>
2013-07-24 11:08:12 +0000

    LM implementation
    r20369

--- modules/thm/include/components/FlowJunction.h
+++ modules/thm/include/components/FlowJunction.h
@@ -2,7 +2,6 @@
 #define FLOWJUNCTION_H
 
 #include <string>
-
 #include "Junction.h"
 
 class FlowJunction;
@@ -11,7 +10,7 @@ template<>
 InputParameters validParams<FlowJunction>();
 
 /**
- * Junction for flow
+ * Joint for flow
  */
 class FlowJunction : public Junction
 {
@@ -22,54 +21,8 @@ public:
   virtual void addVariables();
   virtual void addMooseObjects();
 
-  virtual std::vector<unsigned int> getIDs(std::string piece);
-  virtual std::string variableName(std::string piece);
-
 protected:
-  /// Name of the SCALAR density variable in the junction
-  std::string _junction_rho_name;
-
-  /// Name of the SCALAR momentum variable in the junction
-  std::string _junction_rhou_name;
-
-  /// Name of the SCALAR total energy variable in the junction
-  std::string _junction_rhoE_name;
-
-  /// Name of the coupled scalar aux variable for the pressure.
-  std::string _p_name;
-
-  /// Name of the coupled scalar aux variable for the temperature.
-  std::string _T_name;
-
-  /// Volume of the junction
-  Real _junction_vol;
-
-  /// Gravity in the junction
-  Real _junction_gravity;
-
-  /// Loss in the junction
-  Real _junction_loss;
-
-  /// Area of the junction
-  Real _junction_area;
-
-  /// True if initial pressure is provided in FlowJunction block
-  bool _has_initial_P;
-
-  /// True if initial velocity is provided in FlowJunction block
-  bool _has_initial_V;
-
-  /// True if initial temperature is provided in FlowJunction block
-  bool _has_initial_T;
-
-  /// Initial pressure from user input (if provided)
-  Real _initial_P;
-
-  /// Initial velocity from user input (if provided)
-  Real _initial_V;
-
-  /// Initial temperature from user input (if provided)
-  Real _initial_T;
+  std::string _lm_name;
 };
 
 #endif /* FLOWJUNCTION_H */
--- modules/thm/src/components/FlowJunction.C
+++ modules/thm/src/components/FlowJunction.C
@@ -1,9 +1,9 @@
 #include "FlowJunction.h"
+#include "Factory.h"
 #include "Conversion.h"
 #include "Simulation.h"
-#include "Pipe.h"
 #include "FEProblem.h"
-#include "Factory.h"
+#include "GeometricalComponent.h"
 #include <sstream>
 
 
@@ -11,41 +11,14 @@ template<>
 InputParameters validParams<FlowJunction>()
 {
   InputParameters params = validParams<Junction>();
-
   params.addRequiredParam<UserObjectName>("eos", "The name of equation of state object to use.");
-
-  params.addRequiredParam<Real>("junction_vol", "Volume of the junction");
-  params.addRequiredParam<Real>("junction_gravity", "Gravity in the junction");
-  params.addRequiredParam<Real>("junction_loss", "Loss in the junction");
-  params.addRequiredParam<Real>("junction_area", "Area of the junction");
-
-  // These params are not required, the junction will use global initial pressure, velocity,
-  // and temperature values if these values are not specified.
-  params.addParam<Real>("initial_P", "Initial pressure in the pipe");
-  params.addParam<Real>("initial_V", "Initial velocity in the pipe");
-  params.addParam<Real>("initial_T", "Initial temperature in the pipe");
-
   return params;
 }
 
 
 FlowJunction::FlowJunction(const std::string & name, InputParameters params) :
     Junction(name, params),
-    _junction_rho_name(genName("junction_rho", _id, "")),
-    _junction_rhou_name(genName("junction_rhou", _id, "")),
-    _junction_rhoE_name(genName("junction_rhoE", _id, "")),
-    _p_name(genName("lm", _id, "p")),
-    _T_name(genName("lm", _id, "T")),
-    _junction_vol(getParam<Real>("junction_vol")),
-    _junction_gravity(getParam<Real>("junction_gravity")),
-    _junction_loss(getParam<Real>("junction_loss")),
-    _junction_area(getParam<Real>("junction_area")),
-    _has_initial_P(params.isParamValid("initial_P")),
-    _has_initial_V(params.isParamValid("initial_V")),
-    _has_initial_T(params.isParamValid("initial_T")),
-    _initial_P(_has_initial_P ? getParam<Real>("initial_P") : _sim.getParam<Real>("global_init_P")),
-    _initial_V(_has_initial_V ? getParam<Real>("initial_V") : _sim.getParam<Real>("global_init_V")),
-    _initial_T(_has_initial_T ? getParam<Real>("initial_T") : _sim.getParam<Real>("global_init_T"))
+    _lm_name(genName("lm", _id, ""))
 {
 }
 
@@ -53,51 +26,19 @@ FlowJunction::~FlowJunction()
 {
 }
 
-
 void
 FlowJunction::addVariables()
 {
-  // add scalar variable (the conserved variables within the junction)
+  // add scalar variable (i.e. Lagrange multiplier)
   switch (_model_type)
   {
-  case FlowModel::EQ_MODEL_3:
-  {
-    // Set initial conditions for the junction variables.
-
-    // Add three separate SCALAR junction variables with individual scaling factors
-    _sim.addVariable(true, _junction_rho_name, FEType(FIRST,  SCALAR), 0, /*scale_factor=*/1.0);
-    _sim.addVariable(true, _junction_rhou_name, FEType(FIRST,  SCALAR), 0, /*scale_factor=*/1.e-4);
-    _sim.addVariable(true, _junction_rhoE_name, FEType(FIRST,  SCALAR), 0, /*scale_factor=*/1.e-6);
-
-    // Compute initial (rho, rho*u, rho*E) in the FlowJunction using the initial (P,V,T) values
-    // and the Simulation's EquationOfState object.
-    Real
-      initial_rho = 0.,
-      initial_rhou = 0.,
-      initial_rhoE = 0.;
-
-    const EquationOfState & eos = _sim.getEquationOfState(getParam<UserObjectName>("eos"));
-
-    initial_rho = eos.rho_from_p_T(_initial_P, _initial_T);
-    initial_rhou = initial_rho * _initial_V;
-    initial_rhoE = initial_rho * (eos.e_from_p_rho(_initial_P, initial_rho) + 0.5 * _initial_V * _initial_V);
-
-    // Add initial conditions for these scalar values based on either the global initial (p,V,T)
-    // values or the (p,V,T) values specified in the FlowJunction.
-    _sim.addScalarInitialCondition(_junction_rho_name, initial_rho);
-    _sim.addScalarInitialCondition(_junction_rhou_name, initial_rhou);
-    _sim.addScalarInitialCondition(_junction_rhoE_name, initial_rhoE);
-
-    // Add the SCALAR variables for temperature and pressure in the junction.
-    _sim.addVariable(false, _p_name, FEType(FIRST,  SCALAR), /*subdomain_id=*/0, /*scale_factor=*/1.);
-    _sim.addVariable(false, _T_name, FEType(FIRST,  SCALAR), /*subdomain_id=*/0, /*scale_factor=*/1.);
-
-    // Add initial conditions for the SCALAR aux variables.
-    _sim.addScalarInitialCondition(_p_name, _initial_P);
-    _sim.addScalarInitialCondition(_T_name, _initial_T);
-
+  case FlowModel::EQ_MODEL_2:
+    _sim.addVariable(true, _lm_name,  FEType(SECOND, SCALAR), 0, 1.);
     break;
-  }
+
+  case FlowModel::EQ_MODEL_3:
+    _sim.addVariable(true, _lm_name,  FEType(THIRD, SCALAR), 0, 1.e-5);
+    break;
 
   default:
     mooseError("Not implemented yet.");
@@ -108,203 +49,128 @@ FlowJunction::addVariables()
 void
 FlowJunction::addMooseObjects()
 {
+  std::vector<std::string> cv_area(1, FlowModel::AREA);
   std::vector<std::string> cv_u(1, FlowModel::VELOCITY);
   std::vector<std::string> cv_pressure(1, FlowModel::PRESSURE);
+  std::vector<std::string> cv_enthalpy(1, FlowModel::ENTHALPY);
   std::vector<std::string> cv_rho(1, FlowModel::RHO);
   std::vector<std::string> cv_rhou(1, FlowModel::RHOU);
   std::vector<std::string> cv_rhoE(1, FlowModel::RHOE);
+  std::vector<std::string> cv_lambda(1, _lm_name);
+  std::vector<Real> sf = _sim.getParam<std::vector<Real> >("scaling_factors");
 
-  // Add NumericalFluxUserObject for use with FlowJunction - see
-  // HeatStructure.C for another example of adding a UserObject
+  MooseEnum execute_options(SetupInterface::getExecuteOptions());
+  execute_options = "residual";
+
+  std::string c_pps = genName("flow", _id, "_pps");
   {
-    InputParameters params = _factory.getValidParams("NumericalFluxUserObject");
-    _sim.addUserObject("NumericalFluxUserObject", "numerical_flux", params);
+    InputParameters params = _factory.getValidParams("PressureCB");
+    params.set<std::vector<unsigned int> >("r7:boundary") = _bnd_id;
+    params.set<MooseEnum>("execute_on") = execute_options;
+    params.set<MooseEnum>("output") = "none";
+    params.set<UserObjectName>("eos")  = getParam<UserObjectName>("eos");
+    // coupling
+    params.set<std::vector<std::string> >("area") = cv_area;
+    params.set<std::vector<std::string> >("pressure") = cv_pressure;
+
+    _sim.addPostprocessor("PressureCB", c_pps, params);
   }
 
   // add BC terms
-  const std::vector<unsigned int> & boundary_ids = getBoundaryIds();
-  for (unsigned int i = 0; i < boundary_ids.size(); ++i)
+  for (unsigned int i = 0; i < _bnd_id.size(); ++i)
   {
-    std::vector<unsigned int> bnd_id(1, boundary_ids[i]);
+    std::vector<unsigned int> bnd_id(1, _bnd_id[i]);
 
     // mass
     {
-      InputParameters params = _factory.getValidParams("OneDSpecifiedFluxBC");
-
+      InputParameters params = _factory.getValidParams("OneDFreeMassBC");
       params.set<NonlinearVariableName>("variable") = FlowModel::RHO;
       params.set<std::vector<unsigned int> >("r7:boundary") = bnd_id;
       params.set<UserObjectName>("eos") = getParam<UserObjectName>("eos");
-      params.set<std::string>("eqn_name") = "CONTINUITY";
-      params.set<std::vector<std::string> >("u")    = cv_u;
-      params.set<std::vector<std::string> >("rho")  = cv_rho;
+      params.set<PostprocessorName>("c") = c_pps;
+      params.set<std::vector<Real> >("scaling_factors") = sf;
+      // coupling
+      params.set<std::vector<std::string> >("rho") = cv_rho;
       params.set<std::vector<std::string> >("rhou") = cv_rhou;
-      params.set<std::vector<std::string> >("rhoE") = cv_rhoE;
-      params.set<std::vector<std::string> >("junction_rho")  = std::vector<std::string>(1, _junction_rho_name);
-      params.set<std::vector<std::string> >("junction_rhou") = std::vector<std::string>(1, _junction_rhou_name);
-      params.set<std::vector<std::string> >("junction_rhoE") = std::vector<std::string>(1, _junction_rhoE_name);
-      params.set<UserObjectName>("numerical_flux") = "numerical_flux";
+      params.set<std::vector<std::string> >("u") = cv_u;
+      params.set<std::vector<std::string> >("area") = cv_area;
+      params.set<std::vector<std::string> >("lambda") = cv_lambda;
 
-      _sim.addBoundaryCondition("OneDSpecifiedFluxBC", genName("mass", _id, "_bc"), params);
+      if (_model_type == FlowModel::EQ_MODEL_3)
+      {
+        params.set<std::vector<std::string> >("rhoE") = cv_rhoE;
+        params.set<std::vector<std::string> >("enthalpy") = cv_enthalpy;
+      }
+
+      _sim.addBoundaryCondition("OneDFreeMassBC", genName("mass", _id, "_bc"), params);
     }
-
-
     // momentum
     {
-      InputParameters params = _factory.getValidParams("OneDSpecifiedFluxBC");
-
+      InputParameters params = _factory.getValidParams("OneDFreeMomentumBC");
       params.set<NonlinearVariableName>("variable") = FlowModel::RHOU;
       params.set<std::vector<unsigned int> >("r7:boundary") = bnd_id;
       params.set<UserObjectName>("eos") = getParam<UserObjectName>("eos");
-      params.set<std::string>("eqn_name") = "MOMENTUM";
-      params.set<std::vector<std::string> >("u")    = cv_u;
-      params.set<std::vector<std::string> >("rho")  = cv_rho;
+      params.set<PostprocessorName>("c") = c_pps;
+      params.set<std::vector<Real> >("scaling_factors") = sf;
+      // coupling
+      params.set<std::vector<std::string> >("rho") = cv_rho;
       params.set<std::vector<std::string> >("rhou") = cv_rhou;
-      params.set<std::vector<std::string> >("rhoE") = cv_rhoE;
-      params.set<std::vector<std::string> >("junction_rho")  = std::vector<std::string>(1, _junction_rho_name);
-      params.set<std::vector<std::string> >("junction_rhou") = std::vector<std::string>(1, _junction_rhou_name);
-      params.set<std::vector<std::string> >("junction_rhoE") = std::vector<std::string>(1, _junction_rhoE_name);
-      params.set<UserObjectName>("numerical_flux") = "numerical_flux";
+      params.set<std::vector<std::string> >("u") = cv_u;
+      params.set<std::vector<std::string> >("pressure") = cv_pressure;
+      params.set<std::vector<std::string> >("area") = cv_area;
+      params.set<std::vector<std::string> >("lambda") = cv_lambda;
 
-      _sim.addBoundaryCondition("OneDSpecifiedFluxBC", genName("mom", _id, "_bc"), params);
+      if (_model_type == FlowModel::EQ_MODEL_3)
+      {
+        params.set<std::vector<std::string> >("rhoE") = cv_rhoE;
+        params.set<std::vector<std::string> >("enthalpy") = cv_enthalpy;
+      }
+
+      _sim.addBoundaryCondition("OneDFreeMomentumBC", genName("mom", _id, "_bc"), params);
     }
-
-
     // energy
     if (_model_type == FlowModel::EQ_MODEL_3)
     {
-      InputParameters params = _factory.getValidParams("OneDSpecifiedFluxBC");
-
+      InputParameters params = _factory.getValidParams("OneDFreeEnergyBC");
       params.set<NonlinearVariableName>("variable") = FlowModel::RHOE;
       params.set<std::vector<unsigned int> >("r7:boundary") = bnd_id;
-      params.set<UserObjectName>("eos")  = getParam<UserObjectName>("eos");
-      params.set<std::string>("eqn_name") = "ENERGY";
-      params.set<std::vector<std::string> >("u")    = cv_u;
-      params.set<std::vector<std::string> >("rho")  = cv_rho;
-      params.set<std::vector<std::string> >("rhou") = cv_rhou;
-      params.set<std::vector<std::string> >("rhoE") = cv_rhoE;
-      params.set<std::vector<std::string> >("junction_rho")  = std::vector<std::string>(1, _junction_rho_name);
-      params.set<std::vector<std::string> >("junction_rhou") = std::vector<std::string>(1, _junction_rhou_name);
-      params.set<std::vector<std::string> >("junction_rhoE") = std::vector<std::string>(1, _junction_rhoE_name);
-      params.set<UserObjectName>("numerical_flux") = "numerical_flux";
-
-      _sim.addBoundaryCondition("OneDSpecifiedFluxBC", genName("erg", _id, "_bc"), params);
-    }
-  }
-
-  // Add the constraints - add the same kernel three times,
-  // associating it with a different variable each time.
-  {
-    // Local variables to help with the loop...
-    std::vector<std::string> var_names(3), eqn_names(3), ker_names(3);
-    var_names[0] = _junction_rho_name; var_names[1] = _junction_rhou_name; var_names[2] = _junction_rhoE_name;
-    eqn_names[0] = "CONTINUITY";       eqn_names[1] = "MOMENTUM";          eqn_names[2] = "ENERGY";
-    ker_names[0] = "rho";              ker_names[1] = "rhou";              ker_names[2] = "rhoE";
-
-    for (unsigned v=0; v<3; ++v)
-    {
-      InputParameters params = _factory.getValidParams("FlowConstraint");
-
-      params.set<NonlinearVariableName>("variable") = var_names[v];
-      params.set<std::string>("eqn_name") = eqn_names[v];
-
-      params.set<FlowModel::EModelType>("model_type") = _model_type;
-      params.set<std::vector<unsigned int> >("nodes") = _nodes;
-      params.set<std::vector<Real> >("areas") = _Areas;
-      params.set<std::vector<Real> >("normals") = _normals;
       params.set<UserObjectName>("eos") = getParam<UserObjectName>("eos");
-      params.set<UserObjectName>("numerical_flux") = "numerical_flux";
-
       // coupling
-      params.set<std::vector<std::string> >("u") = cv_u;
-      params.set<std::vector<std::string> >("p") = cv_pressure;
       params.set<std::vector<std::string> >("rho") = cv_rho;
       params.set<std::vector<std::string> >("rhou") = cv_rhou;
       params.set<std::vector<std::string> >("rhoE") = cv_rhoE;
-      params.set<std::vector<std::string> >("junction_rho")  = std::vector<std::string>(1, _junction_rho_name);
-      params.set<std::vector<std::string> >("junction_rhou") = std::vector<std::string>(1, _junction_rhou_name);
-      params.set<std::vector<std::string> >("junction_rhoE") = std::vector<std::string>(1, _junction_rhoE_name);
+      params.set<std::vector<std::string> >("u") = cv_u;
+      params.set<std::vector<std::string> >("enthalpy") = cv_enthalpy;
+      params.set<std::vector<std::string> >("area") = cv_area;
+      params.set<PostprocessorName>("c") = c_pps;
+      params.set<std::vector<Real> >("scaling_factors") = sf;
+      params.set<std::vector<std::string> >("lambda") = cv_lambda;
 
-      // junction parameters
-      params.set<Real>("junction_vol")     = _junction_vol;
-      params.set<Real>("junction_gravity") = _junction_gravity;
-      params.set<Real>("junction_loss")    = _junction_loss;
-      params.set<Real>("junction_area")    = _junction_area;
-
-      _sim.addScalarKernel("FlowConstraint", genName(ker_names[v], _id, "_c"), params);
+      _sim.addBoundaryCondition("OneDFreeEnergyBC", genName("erg", _id, "_bc"), params);
     }
   }
 
-
-
-  // Add an AuxScalarKernel for the junction pressure.
+  // add the constrains
   {
-    InputParameters params = _factory.getValidParams("ScalarPressureAux");
+    InputParameters params = _factory.getValidParams("FlowConstraint");
+    params.set<NonlinearVariableName>("variable") = _lm_name;
 
-    // The variable associated with this kernel.
-    params.set<AuxVariableName>("variable") = _p_name;
-
-    params.set<std::vector<std::string> >("junction_rho")  = std::vector<std::string>(1, _junction_rho_name);
-    params.set<std::vector<std::string> >("junction_rhou") = std::vector<std::string>(1, _junction_rhou_name);
-    params.set<std::vector<std::string> >("junction_rhoE") = std::vector<std::string>(1, _junction_rhoE_name);
-
-    // And the equation of state object that the AuxScalarKernel will use.
+    params.set<FlowModel::EModelType>("model_type") = _model_type;
+    params.set<std::vector<unsigned int> >("nodes") = _nodes;
+    params.set<std::vector<Real> >("areas") = _Areas;
+    params.set<std::vector<Real> >("normals") = _normals;
     params.set<UserObjectName>("eos") = getParam<UserObjectName>("eos");
+    // coupling
+    params.set<std::vector<std::string> >("u") = cv_u;
+    params.set<std::vector<std::string> >("p") = cv_pressure;
+    params.set<std::vector<std::string> >("rho") = cv_rho;
+    params.set<std::vector<std::string> >("rhou") = cv_rhou;
+    if (_model_type == FlowModel::EQ_MODEL_3)
+    {
+      params.set<std::vector<std::string> >("rhoE") = cv_rhoE;
+      params.set<std::vector<std::string> >("enthalpy") = cv_enthalpy;
+    }
 
-    // Add this kernel to the Simulation object
-    _sim.addAuxScalarKernel("ScalarPressureAux", genName("flow", _id, "_p"), params);
+    _sim.addScalarKernel("FlowConstraint", genName("flow", _id, "_c0"), params);
   }
-
-
-
-  // Add an AuxScalarKernel for the junction temperature.
-  {
-    InputParameters params = _factory.getValidParams("ScalarTemperatureAux");
-
-    // The variable associated with this kernel.
-    params.set<AuxVariableName>("variable") = _T_name;
-
-    params.set<std::vector<std::string> >("junction_rho")  = std::vector<std::string>(1, _junction_rho_name);
-    params.set<std::vector<std::string> >("junction_rhou") = std::vector<std::string>(1, _junction_rhou_name);
-    params.set<std::vector<std::string> >("junction_rhoE") = std::vector<std::string>(1, _junction_rhoE_name);
-
-    // And the equation of state object that the AuxScalarKernel will use.
-    params.set<UserObjectName>("eos") = getParam<UserObjectName>("eos");
-
-    // Add this kernel to the Simulation object
-    _sim.addAuxScalarKernel("ScalarTemperatureAux", genName("flow", _id, "_T"), params);
-  }
-
-
-
-  // add PPS for visualization of scalar aux variables
-  MooseEnum execute_options(SetupInterface::getExecuteOptions());
-  execute_options = "timestep";
-  {
-    InputParameters params = _factory.getValidParams("PrintScalarVariable");
-    params.set<VariableName>("variable") = _p_name;
-    params.set<MooseEnum>("output") = "file";
-    params.set<MooseEnum>("execute_on") = execute_options;
-    _sim.addPostprocessor("PrintScalarVariable", genName(name(), "p"), params);
-  }
-  {
-    InputParameters params = _factory.getValidParams("PrintScalarVariable");
-    params.set<VariableName>("variable") = _T_name;
-    params.set<MooseEnum>("execute_on") = execute_options;
-    params.set<MooseEnum>("output") = "file";
-    _sim.addPostprocessor("PrintScalarVariable", genName(name(), "T"), params);
-  }
-}
-
-std::vector<unsigned int>
-FlowJunction::getIDs(std::string /*piece*/)
-{
-  mooseError("Not implemented yet");
-  return std::vector<unsigned int>();
-}
-
-std::string
-FlowJunction::variableName(std::string /*piece*/)
-{
-  mooseError("Not implemented yet");
-  return std::string();
 }
