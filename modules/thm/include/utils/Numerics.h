49362314c2e78494c7e1ff573b0e9d934b73af73
David Andrs <david.andrs@inl.gov>
2013-08-22 08:18:32 +0000

    Adding form loss coefficient into FlowJunction
    r21069

--- modules/thm/include/utils/Numerics.h
+++ modules/thm/include/utils/Numerics.h
@@ -9,4 +9,11 @@ namespace Numerics {
 Real Newton_Solver(Real x0, Real f0, Real (*fct)(Real x, Real f0), Real (*dfct)(Real x), Real tola, Real tolr, unsigned int n_max);
 }
 
+
+template <typename T>
+int sgn(T val)
+{
+  return (T(0) < val) - (val < T(0));
+}
+
 #endif  // NUMERICS_H
