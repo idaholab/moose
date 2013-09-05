4e5e729ae04e2238bfef895f9a587a2b27994ff6
Cody Permann <cody.permann@inl.gov>
2013-09-05 12:07:08 +0000

    Modified Petsc Script to replace -snes - refs #458
    r21263

--- framework/scripts/replace_snes.py
+++ framework/scripts/replace_snes.py
@@ -28,11 +28,11 @@ def fixupHeader():
         checkAndUpdate(dirpath + '/' + file)
 
 
-re_snes = re.compile(r"^([^#]*) -snes_mf_operator \s*(.*)", re.X)
-re_ls31_option = re.compile(r"^([^#]*) -snes_type \s+ -snes_ls \s*(.*)", re.X)
-re_ls31_value = re.compile(r"^([^#]* petsc_options_value .*? ) ls \s+ (\w+) \s*(.*)", re.X)
-re_ls33_option = re.compile(r"^([^#]*) -snes_linesearch_type \s*(.*)", re.X)
-re_ls33_value = re.compile(r"^([^#]* petsc_options_value .*? ) \b(cubic | quadratic | basic | bt)\b \s*(.*)", re.X)
+re_snes = re.compile(r"^(\s*)([^#]*) -snes\b \s*(.*)", re.X)
+#re_ls31_option = re.compile(r"^([^#]*) -snes_type \s+ -snes_ls \s*(.*)", re.X)
+#re_ls31_value = re.compile(r"^([^#]* petsc_options_value .*? ) ls \s+ (\w+) \s*(.*)", re.X)
+#re_ls33_option = re.compile(r"^([^#]*) -snes_linesearch_type \s*(.*)", re.X)
+#re_ls33_value = re.compile(r"^([^#]* petsc_options_value .*? ) \b(cubic | quadratic | basic | bt)\b \s*(.*)", re.X)
 
 
 re2 = re.compile(r"(.*)^([^#]*? petsc_options \s*=\s* (?: '\s*' | \"\s*\" | \s* ) )$", re.M | re.X | re.S)
@@ -42,47 +42,48 @@ def checkAndUpdate(filename):
   lines = f.readlines()
   f.close()
 
-  print filename
+#  print filename
   replacement_made = False
   line_search_added = False
   for i, line in enumerate(lines):
 
     # Replace PETSc solver type
-    (lines[i], num_replacements) = re_snes.subn("\n  #Preconditioned JFNK (default)\n  solve_type = 'PJFNK'\n\n\g<1>\g<2>", lines[i])
+    (lines[i], num_replacements) = re_snes.subn("\n\g<1>solve_type = 'NEWTON'\n\g<1>\g<2>\g<3>", lines[i])
     if num_replacements:
       replacement_made = True
+      print filename
 
     # Replace PETSC3.1 linesearch options
-    (lines[i], num_replacements) = re_ls31_option.subn("\g<1>\g<2>", lines[i])
-    if num_replacements:
-      replacement_made = True
-
-    m = re_ls31_value.search(lines[i])
-    if m:
-      replacement_made = True
-      line_search_added = True
-      line_search_type = m.group(2)
-      if line_search_type == "basic":
-        line_search_type = "none"
-
-      (lines[i], num_replacements) = re_ls31_value.subn("\g<1>\g<3>\n\n  line_search = '" + line_search_type + "'\n", lines[i])
-
-    # Replace PETSC3.3 linesearch options
-    (lines[i], num_replacements) = re_ls33_option.subn("\g<1>\g<2>", lines[i])
-    if num_replacements:
-      replacement_made = True
-
-    m = re_ls33_value.search(lines[i])
-    if m:
-      replacement_made = True
-      line_search_type = m.group(2)
-      if line_search_type == "basic":
-        line_search_type = "none"
-
-      if line_search_added:
-        (lines[i], num_replacements) = re_ls33_value.subn("\g<1>\g<3>\n", lines[i])
-      else:
-        (lines[i], num_replacements) = re_ls33_value.subn("\g<1>\g<3>\n\n  line_search = '" + line_search_type + "'\n", lines[i])
+#    (lines[i], num_replacements) = re_ls31_option.subn("\g<1>\g<2>", lines[i])
+#    if num_replacements:
+#      replacement_made = True
+#
+#    m = re_ls31_value.search(lines[i])
+#    if m:
+#      replacement_made = True
+#      line_search_added = True
+#      line_search_type = m.group(2)
+#      if line_search_type == "basic":
+#        line_search_type = "none"
+#
+#      (lines[i], num_replacements) = re_ls31_value.subn("\g<1>\g<3>\n\n  line_search = '" + line_search_type + "'\n", lines[i])
+#
+#    # Replace PETSC3.3 linesearch options
+#    (lines[i], num_replacements) = re_ls33_option.subn("\g<1>\g<2>", lines[i])
+#    if num_replacements:
+#      replacement_made = True
+#
+#    m = re_ls33_value.search(lines[i])
+#    if m:
+#      replacement_made = True
+#      line_search_type = m.group(2)
+#      if line_search_type == "basic":
+#        line_search_type = "none"
+#
+#      if line_search_added:
+#        (lines[i], num_replacements) = re_ls33_value.subn("\g<1>\g<3>\n", lines[i])
+#      else:
+#        (lines[i], num_replacements) = re_ls33_value.subn("\g<1>\g<3>\n\n  line_search = '" + line_search_type + "'\n", lines[i])
 
     # See if we've left an empty set of options now that we've replaced the solver type, remove if necessary
     if replacement_made:
