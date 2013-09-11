57f05e0c23dde299bdacae9c99adc3167b8823dc
David Andrs <david.andrs@inl.gov>
2013-09-11 15:26:59 +0000

    Solid wall boundary condition is enforced strongly (closes idaholab/relap-7#2183)
    There was a slight change on energy hammer test case which has been re-golded.
    
    r21392

--- modules/thm/src/components/SolidWall.C
+++ modules/thm/src/components/SolidWall.C
@@ -30,42 +30,12 @@ SolidWall::addVariables()
 void
 SolidWall::addMooseObjects()
 {
-  // coupling "vectors"
-  std::vector<VariableName> cv_p(1, FlowModel::PRESSURE);
-  std::vector<VariableName> cv_rho(1, FlowModel::RHO);
-  std::vector<VariableName> cv_rhoE(1, FlowModel::RHOE);
-  // boundary id
   std::vector<unsigned int> bnd_id(1, getBoundaryId());
   {
-    InputParameters params = _factory.getValidParams("OneDMassSolidWallBC");
-    params.set<NonlinearVariableName>("variable") = FlowModel::RHO;
-    params.set<std::vector<unsigned int> >("r7:boundary") = bnd_id;
-    _sim.addBoundaryCondition("OneDMassSolidWallBC", genName("bc", _id, "rho"), params);
-  }
-  {
-    InputParameters params = _factory.getValidParams("OneDMomentumSolidWallBC");
+    InputParameters params = _factory.getValidParams("DirichletBC");
     params.set<NonlinearVariableName>("variable") = FlowModel::RHOU;
     params.set<std::vector<unsigned int> >("r7:boundary") = bnd_id;
-    params.set<UserObjectName>("eos") = getParam<UserObjectName>("eos");
-
-    // coupling
-    params.set<std::vector<VariableName> >("p") = cv_p;
-    params.set<std::vector<VariableName> >("rho") = cv_rho;
-    if (_model_type == FlowModel::EQ_MODEL_3)
-      params.set<std::vector<VariableName> >("rhoE") = cv_rhoE;
-    _sim.addBoundaryCondition("OneDMomentumSolidWallBC", genName("bc", _id, "rhou"), params);
-  }
-
-  if (_model_type == FlowModel::EQ_MODEL_3)
-  {
-    InputParameters params = _factory.getValidParams("OneDEnergySolidWallBC");
-    params.set<NonlinearVariableName>("variable") = FlowModel::RHOE;
-    params.set<std::vector<unsigned int> >("r7:boundary") = bnd_id;
-    params.set<UserObjectName>("eos") = getParam<UserObjectName>("eos");
-
-    // There is no coupling for the energy equation solid wall BC.
-    // It simply returns zero...
-
-    _sim.addBoundaryCondition("OneDEnergySolidWallBC", genName("bc", _id, "rhoE"), params);
+    params.set<Real>("value") = 0.;
+    _sim.addBoundaryCondition("DirichletBC", genName("bc", _id, "rho"), params);
   }
 }
