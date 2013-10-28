953f87107eaf719358a71dffa86681271f58d2b1
Cody Permann <cody.permann@inl.gov>
2013-10-28 12:45:45 +0000

    Uniform refine works with Exodus Meshes - closes #1721
    r22496

--- gui/vtk/RendererFactory.py
+++ gui/vtk/RendererFactory.py
@@ -3,7 +3,13 @@ from MeshOnlyRenderer import MeshOnlyRenderer
 
 ''' Factory Function '''
 def getRenderer(mesh_render_widget, mesh_item_data):
-  if ( ('type' in mesh_item_data and mesh_item_data['type'] == 'FileMesh') or 'type' not in mesh_item_data) and 'file' in mesh_item_data and '.e' in mesh_item_data['file']:
-    return ExodusRenderer(mesh_render_widget, mesh_item_data)
+  if (('type' in mesh_item_data and mesh_item_data['type'] == 'FileMesh') \
+      or 'type' not in mesh_item_data) and 'file' in mesh_item_data and '.e' in mesh_item_data['file']:
+
+    # If we we are using Uniform Refine, we'll still need to use the MeshOnlyRenderer
+    if 'uniform_refine' in mesh_item_data and int(mesh_item_data['uniform_refine']) > 0:
+      return MeshOnlyRenderer(mesh_render_widget, mesh_item_data)
+    else:
+      return ExodusRenderer(mesh_render_widget, mesh_item_data)
   else:
     return MeshOnlyRenderer(mesh_render_widget, mesh_item_data)
