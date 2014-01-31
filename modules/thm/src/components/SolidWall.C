491a192edbf096c311ac1d26afd9f461c36090e3
David Andrs <david.andrs@inl.gov>
2014-01-31 07:58:14 +0000

    SolidWall updated to VA model (refs idaholab/relap-7#1394)
    r24431

--- modules/thm/src/components/SolidWall.C
+++ modules/thm/src/components/SolidWall.C
@@ -33,7 +33,7 @@ SolidWall::addMooseObjects()
   std::vector<unsigned int> bnd_id(1, getBoundaryId());
   {
     InputParameters params = _factory.getValidParams("DirichletBC");
-    params.set<NonlinearVariableName>("variable") = FlowModel::RHOU;
+    params.set<NonlinearVariableName>("variable") = FlowModel::RHOUA;
     params.set<std::vector<unsigned int> >("r7:boundary") = bnd_id;
     params.set<Real>("value") = 0.;
     _sim.addBoundaryCondition("DirichletBC", genName("bc", _id, "rho"), params);
