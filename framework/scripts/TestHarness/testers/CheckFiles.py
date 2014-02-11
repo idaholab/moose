a67db21d5f5190e54939f233528a8111e5752d0e
Cody Permann <cody.permann@inl.gov>
2014-02-11 16:30:15 +0000

    Added ability to CheckFiles tester to search files for regex (#2443)
    r24715

--- framework/scripts/TestHarness/testers/CheckFiles.py
+++ framework/scripts/TestHarness/testers/CheckFiles.py
@@ -9,7 +9,7 @@ class CheckFiles(RunApp):
     params.addParam('check_files', [], "A list of files that MUST exist.")
     params.addParam('check_not_exists', [], "A list of files that must NOT exist.")
     params.addParam('delete_output_before_running',  True, "Delete pre-existing output files before running test. Only set to False if you know what you're doing!")
-
+    params.addParam('file_expect_out', "A regular expression that must occur in all of the check files in order for the test to be considered passing.")
     return params
   getValidParams = staticmethod(getValidParams)
 
@@ -41,5 +41,16 @@ class CheckFiles(RunApp):
        if os.path.isfile(os.path.join(self.specs['test_dir'], file)):
          reason = 'UNEXPECTED FILES'
          break
+
+     # if still no errors, check that all the files contain the file_expect_out expression
+     if reason == '':
+       if self.specs.isValid('file_expect_out'):
+         for file in self.specs['check_files']:
+           fid = open(os.path.join(self.specs['test_dir'], file), 'r')
+           contents = fid.read()
+           fid.close()
+           if not self.checkOutputForPattern(contents, self.specs['file_expect_out']):
+             reason = 'NO EXPECTED OUT IN FILE'
+             break
 
     return (reason, output)
