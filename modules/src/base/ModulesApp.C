9ecd5e7496a113143c9a6600483b012cd00d7b6b
Cody Permann <Cody.Permann@inl.gov>
2014-02-14 21:31:20 -0700

    Adding modules directory

---
+++ modules/src/base/ModulesApp.C
@@ -0,0 +1,74 @@
+#include "ModulesApp.h"
+#include "Factory.h"
+#include "ActionFactory.h"
+#include "AppFactory.h"
+
+/************************************************************
+ * New Module Step 1.                                       *
+ *         Add include for new modules here                 *
+ * #if defined(MODULENAME) || defined(ALL_MODULES)          *
+ *   #include "ModuleNameApp.h"                             *
+ * #endif                                                   *
+ ***********************************************************/
+#if defined(FOOBAR) || defined(ALL_MODULES)
+  #include "FoobarApp.h"
+#endif
+
+template<>
+InputParameters validParams<ModulesApp>()
+{
+  InputParameters params = validParams<MooseApp>();
+  return params;
+}
+
+ModulesApp::ModulesApp(const std::string & name, InputParameters parameters) :
+    MooseApp(name, parameters)
+{
+  Moose::registerObjects(_factory);
+  ModulesApp::registerObjects(_factory);
+  
+  Moose::associateSyntax(_syntax, _action_factory);
+  ModulesApp::associateSyntax(_syntax, _action_factory);
+}
+
+ModulesApp::~ModulesApp()
+{
+}
+
+void
+ModulesApp::registerApps()
+{
+  registerApp(ModulesApp);
+}
+
+void
+ModulesApp::registerObjects(Factory & factory)
+{
+  /************************************************************
+   * New Module Step 2.                                       *
+   *                Register module objects here              *
+   * ModuleNameApp::registerObjects(factory);                 * 
+   * if defined(FOOBAR) || defined(ALL_MODULES)               * 
+   *   FoobarApp::registerObjects(factory);                   * 
+   * #endif                                                   * 
+   ***********************************************************/
+#if defined(FOOBAR) || defined(ALL_MODULES) 
+  FoobarApp::registerObjects(factory);
+#endif
+}
+
+void
+ModulesApp::associateSyntax(Syntax & syntax, ActionFactory & action_factory)
+{
+  /************************************************************
+   * New Module Step 3.                                       *
+   *                  Associate syntax here                   *
+   * #if defined(FOOBAR) || defined(ALL_MODULES)              * 
+   *   FoobarApp::associateSyntax(syntax, action_factory);    * 
+   * #endif                                                   * 
+   ***********************************************************/
+#if defined(FOOBAR) || defined(ALL_MODULES) 
+  FoobarApp::associateSyntax(syntax, action_factory);
+#endif
+}
+
