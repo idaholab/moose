55995797a075ce4ae237095aaca68597add34f6e
Cody Permann <codypermann@gmail.com>
2014-02-27 12:02:52 -0700

    libmesh update (LIBMESH_DIR location change)

--- framework/build.mk
+++ framework/build.mk
@@ -3,7 +3,7 @@
 #       This can be overridden by using environment variables (MOOSE_DIR and/or FRAMEWORK_DIR)
 
 # Set LIBMESH_DIR if it is not already set in the environment (try our best to guess!)
-LIBMESH_DIR       ?= $(MOOSE_DIR)/../libmesh/installed
+LIBMESH_DIR       ?= $(MOOSE_DIR)/libmesh/installed
 
 # If the user has no environment variable
 # called METHOD, he gets optimized mode.
