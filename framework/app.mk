453fb607c5b7c031301b4d060038f6d5b85f2dcb
Cody Permann <Cody.Permann@inl.gov>
2014-03-08 08:40:56 -0700

    Fixing serialization problem in linking step - #2460

--- framework/app.mk
+++ framework/app.mk
@@ -75,12 +75,10 @@ endif
 # Target-specific Variable Values (See GNU-make manual)
 $(app_LIB): curr_objs := $(app_objects)
 $(app_LIB): curr_dir := $(APPLICATION_DIR)
-$(app_LIB): curr_other_libs := $(app_LIBS_other)
-#$(app_LIB): $(app_objects) $(app_plugin_deps) $(app_LIBS_other)
 $(app_LIB): $(app_objects) $(app_LIBS_other) $(app_plugin_deps)
 	@echo "Linking Library "$@"..."
 	@$(libmesh_LIBTOOL) --tag=CXX $(LIBTOOLFLAGS) --mode=link --quiet \
-	  $(libmesh_CXX) $(libmesh_CXXFLAGS) -o $@ $(curr_objs) $(curr_other_libs) $(libmesh_LDFLAGS) $(EXTERNAL_FLAGS) -rpath $(curr_dir)/lib
+	  $(libmesh_CXX) $(libmesh_CXXFLAGS) -o $@ $(curr_objs) $(libmesh_LDFLAGS) $(EXTERNAL_FLAGS) -rpath $(curr_dir)/lib
 	@$(libmesh_LIBTOOL) --mode=install --quiet install -c $@ $(curr_dir)/lib
 
 $(app_EXEC): $(app_LIBS) $(mesh_library) $(main_object)
