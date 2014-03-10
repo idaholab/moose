d995c72878a52db9c84b5abfe7db0960b9ddca68
John Peterson <jw.peterson@inl.gov>
2014-03-10 10:26:47 -0600

    We need to have quotes around the arguments to -name when calling find.
    The 'find foo/bar/..' part is fine.
    
    Refs #2566.

--- scripts/delete_trailing_whitespace.sh
+++ scripts/delete_trailing_whitespace.sh
@@ -2,5 +2,4 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd $SCRIPT_DIR/..
-find . \( -name *.[Chi] -or -name *.py -or \( -name "contrib" -or -name "libmesh" \) -prune -and -type f \) -print0 | xargs -0 perl -pli -e "s/\s+$//"
+find $SCRIPT_DIR/.. \( -name "*.[Chi]" -or -name "*.py" -or \( -name "contrib" -or -name "libmesh" \) -prune -and -type f \) -print0 | xargs -0 perl -pli -e "s/\s+$//"
