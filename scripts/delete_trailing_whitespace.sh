54b619344c68033adfb23d6e5ef6406a785b6c72
John Peterson <jw.peterson@inl.gov>
2014-03-10 09:58:02 -0600

    Apparently find command doesn't like '..' on Mavericks.
    Refs #2566.

--- scripts/delete_trailing_whitespace.sh
+++ scripts/delete_trailing_whitespace.sh
@@ -2,5 +2,5 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-find $SCRIPT_DIR/.. \( -name *.[Chi] -or -name *.py -or \( -name "contrib" -or -name "libmesh" \) -prune -and -type f \) -print0 | xargs -0 perl -pli -e "s/\s+$//"
-
+cd $SCRIPT_DIR/..
+find . \( -name *.[Chi] -or -name *.py -or \( -name "contrib" -or -name "libmesh" \) -prune -and -type f \) -print0 | xargs -0 perl -pli -e "s/\s+$//"
