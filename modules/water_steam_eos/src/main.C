421c06f45b8f4063b6b5d876580c6bbddb3039b8
Cody Permann <codypermann@gmail.com>
2014-03-12 16:44:54 -0600

    Fixing bug in water_steam_eos module - #2460

--- modules/water_steam_eos/src/main.C
+++ modules/water_steam_eos/src/main.C
@@ -1,11 +1,11 @@
-#include "WaterSteamEosApp.h"
+#include "WaterSteamEOSApp.h"
 #include "MooseInit.h"
 #include "Moose.h"
 #include "MooseApp.h"
 #include "AppFactory.h"
 
 // Create a performance log
-PerfLog Moose::perf_log("WaterSteamEos");
+PerfLog Moose::perf_log("WaterSteamEOS");
 
 // Begin the main program.
 int main(int argc, char *argv[])
@@ -14,10 +14,10 @@ int main(int argc, char *argv[])
   MooseInit init(argc, argv);
 
   // Register this application's MooseApp and any it depends on
-  WaterSteamEosApp::registerApps();
+  WaterSteamEOSApp::registerApps();
 
   // This creates dynamic memory that we're responsible for deleting
-  MooseApp * app = AppFactory::createApp("WaterSteamEosApp", argc, argv);
+  MooseApp * app = AppFactory::createApp("WaterSteamEOSApp", argc, argv);
 
   // Execute the application
   app->run();
