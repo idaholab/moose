684719e1bef57dfb566d78343646d2820bb33744
David Andrs <david.andrs@inl.gov>
2014-04-01 14:00:41 +0000

    Adding pressure based stabilization for single phase
    Author: David Andrs <david.andrs@inl.gov>
    Signed-off-by: Richard Martineau <richard.martineau@inl.gov>
    
    r26174

---
+++ modules/thm/include/auxkernels/SoundSpeedAux.h
@@ -0,0 +1,31 @@
+#ifndef SOUNDSPEEDAUX_H
+#define SOUNDSPEEDAUX_H
+
+#include "AuxKernel.h"
+
+class SoundSpeedAux;
+class EquationOfState;
+
+template<>
+InputParameters validParams<SoundSpeedAux>();
+
+/**
+ * Computes the sound speed, given the equation of state
+ */
+class SoundSpeedAux : public AuxKernel
+{
+public:
+  SoundSpeedAux(const std::string & name, InputParameters parameters);
+  virtual ~SoundSpeedAux();
+
+protected:
+  virtual Real computeValue();
+
+  VariableValue & _rho;
+  VariableValue & _rhou;
+  VariableValue & _rhoE;
+
+  const EquationOfState & _eos;
+};
+
+#endif /* SOUNDSPEEDAUX_H */
---
+++ modules/thm/src/auxkernels/SoundSpeedAux.C
@@ -0,0 +1,36 @@
+#include "SoundSpeedAux.h"
+#include "EquationOfState.h"
+
+template<>
+InputParameters validParams<SoundSpeedAux>()
+{
+  InputParameters params = validParams<AuxKernel>();
+  params.addRequiredCoupledVar("rho", "density");
+  params.addRequiredCoupledVar("rhou", "momentum");
+  params.addRequiredCoupledVar("rhoE", "total energy");
+  params.addRequiredParam<UserObjectName>("eos", "The name of equation of state object to use.");
+
+  return params;
+}
+
+SoundSpeedAux::SoundSpeedAux(const std::string & name, InputParameters parameters) :
+    AuxKernel(name, parameters),
+    _rho(coupledValue("rho")),
+    _rhou(coupledValue("rhou")),
+    _rhoE(coupledValue("rhoE")),
+    _eos(getUserObject<EquationOfState>("eos"))
+{
+}
+
+SoundSpeedAux::~SoundSpeedAux()
+{
+}
+
+Real
+SoundSpeedAux::computeValue()
+{
+  Real c2 = _eos.c2(_rho[_qp], _rhou[_qp], _rhoE[_qp]);
+  if (c2 < 0)
+    mooseError("Sound speed went negative. Aborting...");
+  return std::sqrt(c2);
+}
