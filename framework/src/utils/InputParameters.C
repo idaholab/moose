eea1b27446fb4d5b6e703fb6402a3caf27bb36d9
David Andrs <david.andrs@inl.gov>
2014-04-23 10:58:23 -0600

    Fixing a missing new line (closes #2957)

--- framework/src/utils/InputParameters.C
+++ framework/src/utils/InputParameters.C
@@ -269,8 +269,9 @@ InputParameters::checkParams(const std::string &prefix)
     {
       // The parameter is required but missing
       if (oss.str().empty())
-        oss << "The following required parameters are missing:\n";
-      oss << l_prefix << "/" << it->first << "\n\tDoc String: \"" + getDocString(it->first) + "\"";
+        oss << "The following required parameters are missing:" << std::endl;
+      oss << l_prefix << "/" << it->first << std::endl;
+      oss << "\tDoc String: \"" + getDocString(it->first) + "\"" << std::endl;
     }
   }
 
