f52d624235e9f5b1fc1037e69d6d4016b077ca50
Ling Zou <ling.zou@inl.gov>
2014-04-30 07:51:03 +0000

    move friction term into a common kernel, for both 3-eqn and 7-eqn models.
    In Pipe, wall friction material is moved into the common block.
    Very small difference, ~1.e-6/-5, was observed in energy_hammer test case, regold
    
    Author: Ling Zou <ling.zou@inl.gov>
    Signed-off-by: David Andrs <david.andrs@inl.gov>
    
    r26892

---
+++ modules/thm/include/kernels/OneDMomentumFriction.h
@@ -0,0 +1,38 @@
+#ifndef ONEDMOMENTUMFRICTION_H
+#define ONEDMOMENTUMFRICTION_H
+
+#include "Kernel.h"
+
+
+// Forward Declarations
+class OneDMomentumFriction;
+
+template<>
+InputParameters validParams<OneDMomentumFriction>();
+
+/**
+ * The spatial part of the 1D momentum conservation for the variable-area Euler equations.
+ */
+class OneDMomentumFriction : public Kernel
+{
+public:
+
+  OneDMomentumFriction(const std::string & name, InputParameters parameters);
+
+protected:
+  virtual Real computeQpResidual();
+  virtual Real computeQpJacobian();
+  virtual Real computeQpOffDiagJacobian(unsigned int jvar);
+
+  // Coupled variables
+  VariableValue & _u_vel;
+  VariableValue & _hydraulic_diameter;
+
+  // IDs of coupled variables (for computing Jacobians)
+  unsigned _rhoA_var_number;
+
+  // Material properties
+  MaterialProperty<Real> & _friction;
+};
+
+#endif
---
+++ modules/thm/src/kernels/OneDMomentumFriction.C
@@ -0,0 +1,46 @@
+#include "OneDMomentumFriction.h"
+#include "EquationOfState.h"
+
+template<>
+InputParameters validParams<OneDMomentumFriction>()
+{
+  InputParameters params = validParams<Kernel>();
+
+  // Required coupled variables
+  params.addRequiredCoupledVar("rhoA", "density multiplied by area");
+  params.addRequiredCoupledVar("u", "velocity");
+  params.addRequiredCoupledVar("hydraulic_diameter", "The hydraulic diameter. Depends on A(x).");
+
+  return params;
+}
+
+OneDMomentumFriction::OneDMomentumFriction(const std::string & name, InputParameters parameters) :
+    Kernel(name, parameters),
+    _u_vel(coupledValue("u")),
+    _hydraulic_diameter(coupledValue("hydraulic_diameter")),
+    _rhoA_var_number(coupled("rhoA")),
+    _friction(getMaterialProperty<Real>("friction"))
+{
+}
+
+Real
+OneDMomentumFriction::computeQpResidual()
+{
+  // Contribution due to friction.
+  return (0.5*_friction[_qp]/_hydraulic_diameter[_qp]) * _u[_qp] * std::abs(_u_vel[_qp]) * _test[_i][_qp];
+}
+
+Real
+OneDMomentumFriction::computeQpJacobian()
+{
+  return (0.5*_friction[_qp]/_hydraulic_diameter[_qp]) * 2. * std::abs(_u_vel[_qp]) * _phi[_j][_qp] * _test[_i][_qp];
+}
+
+Real
+OneDMomentumFriction::computeQpOffDiagJacobian(unsigned int jvar)
+{
+  if (jvar == _rhoA_var_number)
+    return (0.5*_friction[_qp]/_hydraulic_diameter[_qp]) * (-_u_vel[_qp]) * std::abs(_u_vel[_qp]) * _phi[_j][_qp] * _test[_i][_qp];
+  else
+    return 0.;
+}
