823c5c5f5e9cc3a88893134397d186f9efcf4739
Cody Permann <codypermann@gmail.com>
2014-05-07 09:28:09 -0600

    Fixing up HeatSource in modules (inherits from BodyForce) refs #3047

--- modules/heat_conduction/src/HeatSource.C
+++ modules/heat_conduction/src/HeatSource.C
@@ -3,9 +3,11 @@
 template<>
 InputParameters validParams<HeatSource>()
 {
-  InputParameters params = validParams<Kernel>();
-  params.addParam<Real>("value",1.0,"Value of heat source.  Multiplied by function if present.");
-  params.addParam<FunctionName>("function", "", "Function describing the volumetric heat source");
+  InputParameters params = validParams<BodyForce>();
+
+  // Override defaults and documentation, weak form is identical to BodyForce in MOOSE
+  params.addParam<Real>("value", 1.0, "Value of heat source. Multiplied by function if present.");
+  params.addParam<FunctionName>("function", "1", "Function describing the volumetric heat source");
   return params;
 }
 
