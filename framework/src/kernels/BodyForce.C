6da4d2f80b75be6dcafa8201b12dd52ea8e43f2c
Cody Permann <codypermann@gmail.com>
2014-05-07 08:39:12 -0600

    Finishing up default value work -Modifying test to demonstrate that ParsedFunctions or ConstantFunctions can be used where FunctionName parameters are requested. closes #3047

--- framework/src/kernels/BodyForce.C
+++ framework/src/kernels/BodyForce.C
@@ -22,25 +22,21 @@ InputParameters validParams<BodyForce>()
 {
   InputParameters params = validParams<Kernel>();
   params.set<Real>("value")=0.0;
-  params.addParam<FunctionName>("function", "", "A function that describes the body force");
+  // A ConstantFunction of "1" is supplied as the default
+  params.addParam<FunctionName>("function", "1", "A function that describes the body force");
   return params;
 }
 
 BodyForce::BodyForce(const std::string & name, InputParameters parameters) :
     Kernel(name, parameters),
     _value(getParam<Real>("value")),
-    _has_function(getParam<FunctionName>("function") != ""),
-    _function( _has_function ? &getFunction("function") : NULL )
+    _function(getFunction("function"))
 {
 }
 
 Real
 BodyForce::computeQpResidual()
 {
-  Real factor = _value;
-  if (_has_function)
-  {
-    factor *= _function->value(_t, _q_point[_qp]);
-  }
-  return _test[_i][_qp]*-factor;
+  Real factor = _value * _function.value(_t, _q_point[_qp]);
+  return _test[_i][_qp] * -factor;
 }
