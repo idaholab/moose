6162719c871e72b1922cab7528f313c203392ebb
Daniel Schwen <daniel.schwen@inl.gov>
2014-05-10 17:28:40 +0000

    clean up ICs
    r27151

--- modules/phase_field/include/ics/PFCFreezingIC.h
+++ modules/phase_field/include/ics/PFCFreezingIC.h
@@ -1,11 +1,7 @@
 #ifndef PFCFREEZINGIC_H
 #define PFCFREEZINGIC_H
 
-#include "Kernel.h"
 #include "InitialCondition.h"
-
-// System includes
-#include <string>
 
 // Forward Declarations
 class PFCFreezingIC;
@@ -17,30 +13,25 @@ InputParameters validParams<PFCFreezingIC>();
  * PFCFreezingIC creates an intial density for a PFC model that has one area of a set
  * crystal structure (initialized using sinusoids) and all the rest with a random structure.
  * The random values will fall between 0 and 1.
+ * \todo For the FCC this returns 0. This cannot be right, yet it satisfies the (probably bogus) test.
  */
 class PFCFreezingIC : public InitialCondition
 {
 public:
-
-  /**
-   * Constructor
-   *
-   * @param name The name given to the initial condition in the input file.
-   * @param parameters The parameters object holding data for the class to use.
-   * @param var_name The variable this InitialCondtion is supposed to provide values for.
-   */
   PFCFreezingIC(const std::string & name,
                 InputParameters parameters);
-  
+
   virtual Real value(const Point & p);
 
 private:
-  Real _x1; 
-  Real _y1; 
-  Real _z1; 
-  Real _x2; 
-  Real _y2; 
-  Real _z2; 
+  Real _x1;
+  Real _y1;
+  Real _z1;
+
+  Real _x2;
+  Real _y2;
+  Real _z2;
+
   Real _lc;
   MooseEnum _crystal_structure;
 
@@ -52,8 +43,6 @@ private:
   Real _inside, _outside;
 
   unsigned int _icdim;
-  
-  
 };
 
 #endif //PFCFREEZINGIC_H
--- modules/phase_field/src/ics/PFCFreezingIC.C
+++ modules/phase_field/src/ics/PFCFreezingIC.C
@@ -12,49 +12,49 @@ InputParameters validParams<PFCFreezingIC>()
   params.addRequiredParam<Real>("x2", "The x coordinate of the upper right-hand corner of the box");
   params.addRequiredParam<Real>("y2", "The y coordinate of the upper right-hand corner of the box");
   params.addParam<Real>("z2", 0.0, "The z coordinate of the upper right-hand corner of the box");
-  
+
   params.addParam<Real>("min", 0.0, "Lower bound of the randomly generated values");
   params.addParam<Real>("max", 1.0, "Upper bound of the randomly generated values");
   params.addParam<Real>("inside", 1.0, "Value inside sinusoids");
   params.addParam<Real>("outside", 0.0, "Value outside sinusoids");
 
   params.addRequiredParam<Real>("lc", "The lattice constant off the crystal structure");
-  
+
   MooseEnum crystal_structures("FCC, BCC");
   params.addParam<MooseEnum>("crystal_structure", crystal_structures,"The type of crystal structure");
-  
+
   params.addParam<unsigned int>("seed", 0, "Seed value for the random number generator");
-  
+
   return params;
 }
 
 PFCFreezingIC::PFCFreezingIC(const std::string & name,
-                             InputParameters parameters)
-  :InitialCondition(name, parameters),
-   _x1(getParam<Real>("x1")),
-   _y1(getParam<Real>("y1")),
-   _z1(getParam<Real>("z1")),
-   _x2(getParam<Real>("x2")),
-   _y2(getParam<Real>("y2")),
-   _z2(getParam<Real>("z2")),
-   _lc(getParam<Real>("lc")),
-   _crystal_structure(getParam<MooseEnum>("crystal_structure")),
-   _bottom_left(_x1,_y1,_z1),
-   _top_right(_x2,_y2,_z2),
-   _range(_top_right - _bottom_left),
-   _min(getParam<Real>("min")),
-   _max(getParam<Real>("max")),
-   _val_range(_max - _min),
-   _inside(getParam<Real>("inside")),
-   _outside(getParam<Real>("outside"))
+                             InputParameters parameters) :
+    InitialCondition(name, parameters),
+    _x1(getParam<Real>("x1")),
+    _y1(getParam<Real>("y1")),
+    _z1(getParam<Real>("z1")),
+    _x2(getParam<Real>("x2")),
+    _y2(getParam<Real>("y2")),
+    _z2(getParam<Real>("z2")),
+    _lc(getParam<Real>("lc")),
+    _crystal_structure(getParam<MooseEnum>("crystal_structure")),
+    _bottom_left(_x1,_y1,_z1),
+    _top_right(_x2,_y2,_z2),
+    _range(_top_right - _bottom_left),
+    _min(getParam<Real>("min")),
+    _max(getParam<Real>("max")),
+    _val_range(_max - _min),
+    _inside(getParam<Real>("inside")),
+    _outside(getParam<Real>("outside"))
 {
   std::cout << "MooseEnum? " << _crystal_structure << std::endl;
-  
-  for(unsigned int i=0; i<LIBMESH_DIM; i++)
+
+  for(unsigned int i = 0; i < LIBMESH_DIM; i++)
     mooseAssert(_range(i) >= 0.0, "x1, y1 or z1 is not less than x2, y2 or z2");
-  
+
   MooseRandom::seed(getParam<unsigned int>("seed"));
-  
+
   if (_range(1) == 0.0)
     _icdim = 1;
   else if (_range(2) < 1.0e-10*_range(0))
@@ -69,37 +69,29 @@ PFCFreezingIC::value(const Point & p)
   //If out of bounds, set random value
   for(unsigned int i=0; i<LIBMESH_DIM; i++)
     if(p(i) < _bottom_left(i) || p(i) > _top_right(i))
-      return _min + _val_range*MooseRandom::rand();
-    
-  //If in bounds, set sinusoid IC to make atoms
+      return _min + _val_range * MooseRandom::rand();
 
+  //If in bounds, set sinusoid IC to make atoms
   Real val = 0.0;
-  
-  
   if (_crystal_structure == "FCC")
   {
-    Real val = 0.0;
-    
-    for (unsigned int i=0; i<_icdim; i++) 
-      val *= std::cos((2.0/_lc*p(i))*libMesh::pi);
+    // Note: this effectively (and now explicitly) returns 0.0 for FCC.
+    return 0.0;
+
+    for (unsigned int i=0; i<_icdim; i++)
+      val += std::cos((2.0/_lc*p(i))*libMesh::pi);
   }
   else
   {
-      for (unsigned int i=0; i<_icdim; i++) 
-      {
-          val += std::cos((2.0/_lc*p(i))*libMesh::pi);
-      }
-      val /= _icdim;
-      val = std::pow(val,2);
+    for (unsigned int i=0; i<_icdim; i++)
+      val += std::cos((2.0/_lc*p(i))*libMesh::pi);
+
+    val /= _icdim;
+    val = std::pow(val,2);
   }
+
   Real amp = _inside - _outside;
-  
   val =  amp * val + _outside;
-  
+
   return val;
 }
-
-  
-
-
-  
