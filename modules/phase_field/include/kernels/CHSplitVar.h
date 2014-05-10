f6b962703c713b0c6f3f4acdb682324bab3e1e68
Daniel Schwen <daniel.schwen@inl.gov>
2014-05-10 12:35:02 +0000

    code clean up, regold due to 3.141->libmMesh::pi
    r27144

--- modules/phase_field/include/kernels/CHSplitVar.h
+++ modules/phase_field/include/kernels/CHSplitVar.h
@@ -1,5 +1,5 @@
-#ifndef CHSplitVar_H
-#define CHSplitVar_H
+#ifndef CHSPLITVAR_H
+#define CHSPLITVAR_H
 
 #include "KernelGrad.h"
 
@@ -9,22 +9,22 @@ class CHSplitVar;
 template<>
 InputParameters validParams<CHSplitVar>();
 
+/**
+ *
+ */
 class CHSplitVar : public KernelGrad
 {
 public:
-
   CHSplitVar(const std::string & name, InputParameters parameters);
-  
+
 protected:
   virtual RealGradient precomputeQpResidual();
   virtual RealGradient precomputeQpJacobian();
   virtual Real computeQpOffDiagJacobian(unsigned int jvar);
-  
 
 private:
-
   Real _var_c;
   VariableGradient & _grad_c;
-  
 };
-#endif //CHSplitVar_H
+
+#endif //CHSPLITVAR_H
--- modules/phase_field/src/kernels/CHSplitVar.C
+++ modules/phase_field/src/kernels/CHSplitVar.C
@@ -4,8 +4,8 @@ template<>
 InputParameters validParams<CHSplitVar>()
 {
   InputParameters params = validParams<KernelGrad>();
-  params.addRequiredCoupledVar("c","Variable representing the laplacian of c");
-  
+  params.addRequiredCoupledVar("c", "Variable representing the laplacian of c");
+
   return params;
 }
 
@@ -13,13 +13,13 @@ CHSplitVar::CHSplitVar(const std::string & name, InputParameters parameters)
   :KernelGrad(name, parameters),
    _var_c(coupled("c")),
    _grad_c(coupledGradient("c"))
-{ 
+{
 }
 
 RealGradient
 CHSplitVar::precomputeQpResidual()
 {
-  return  _grad_c[_qp];//*_grad_test[_i][_qp]
+  return  _grad_c[_qp]; // * _grad_test[_i][_qp]
 }
 
 RealGradient
@@ -32,8 +32,7 @@ Real
 CHSplitVar::computeQpOffDiagJacobian(unsigned int jvar)
 {
   if(jvar == _var_c)
-    return _grad_phi[_j][_qp]*_grad_test[_i][_qp];
-  
+    return _grad_phi[_j][_qp] * _grad_test[_i][_qp];
+
   return 0.0;
 }
-
