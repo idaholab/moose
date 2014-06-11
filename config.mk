2f40d1136e7ac171d852ff21e4dd286076b17380
Joshua Cogliati <joshua.cogliati@inl.gov>
2014-06-11 15:14:42 +0000

    Making config file more like r7_moose one.
    Also gating off creating the python3 crow libraries unless
    CROW_USE_PYTHON3=TRUE
    
    r27882

--- config.mk
+++ config.mk
@@ -1,10 +1,10 @@
 CROW_DIR := $(ROOT_DIR)/crow
 
-PYTHON3_HELLO := $(shell python3 -c "print('HELLO')" 2>/dev/null)
-PYTHON2_HELLO := $(shell python -c "print 'HELLO'" 2>/dev/null)
-
 SWIG_VERSION := $(shell swig -version 2>/dev/null)
 PYTHON_CONFIG_WHICH := $(shell which python-config 2>/dev/null)
+ifeq ($(CROW_USE_PYTHON3),TRUE)
+	PYTHON3_CONFIG_WHICH := $(shell which python3-config 2>/dev/null)
+endif
 
 UNAME := $(shell uname)
 
@@ -13,12 +13,12 @@ ifneq ($(PYTHON_CONFIG_WHICH),)
 	PYTHON2_LIB=$(shell python-config --ldflags)
 endif
 
-# look for numpy include directory
-#NUMPY_INCLUDE = $(shell python $(CROW_DIR)/scripts/find_numpy_include.py)
+ifneq ($(PYTHON3_CONFIG_WHICH),)
+	PYTHON3_INCLUDE = $(shell python3-config --includes) #-DPy_LIMITED_API
+	PYTHON3_LIB = $(shell python3-config --ldflags) #-DPy_LIMITED_API
+endif
 
-ifeq ($(PYTHON3_HELLO),HELLO)
-	PYTHON_INCLUDE = $(shell python3-config --includes) #-DPy_LIMITED_API
-	PYTHON_LIB = $(shell python3-config --ldflags) #-DPy_LIMITED_API
+ifneq ($(PYTHON3_CONFIG_WHICH),)
 ifeq ($(findstring SWIG Version 2,$(SWIG_VERSION)),)
 	CONTROL_MODULES =
 	PYTHON_MODULES =
@@ -27,29 +27,26 @@ else
 	CONTROL_MODULES = $(CROW_DIR)/control_modules/_distribution1D.so $(CROW_DIR)/control_modules/_crowtools.so
 	PYTHON_MODULES = $(CROW_DIR)/python_modules/_distribution1Dpy2.so $(CROW_DIR)/python_modules/_distribution1Dpy3.so $(CROW_DIR)/python_modules/_interpolationNDpy2.so $(CROW_DIR)/python_modules/_interpolationNDpy3.so
 endif #Have SWIG
+	PYTHON_INCLUDE=$(PYTHON3_INCLUDE)
+	PYTHON_LIB=$(PYTHON3_LIB)
 
 else #no Python3
-ifeq ($(PYTHON2_HELLO),HELLO)
-ifeq ($(PYTHON_CONFIG_WHICH),)
-	PYTHON_INCLUDE = -DNO_PYTHON_FOR_YOU
-	PYTHON_LIB = -DNO_PYTHON_FOR_YOU
-	CONTROL_MODULES =
-	PYTHON_MODULES =
-else #Python 2 and Python config found but not Python 3
+
+ifneq ($(PYTHON_CONFIG_WHICH),)
+#Python 2 config found but not Python 3
 	PYTHON_INCLUDE=$(PYTHON2_INCLUDE)
 	PYTHON_LIB=$(PYTHON2_LIB)
 	#CONTROL_MODULES=
 	SWIG_PY_FLAGS=
 	PYTHON_MODULES = $(CROW_DIR)/python_modules/_distribution1Dpy2.so $(CROW_DIR)/python_modules/_interpolationNDpy2.so
 	CONTROL_MODULES=$(CROW_DIR)/control_modules/_distribution1D.so $(CROW_DIR)/control_modules/_crowtools.so
-endif
-else
-#Python3 and Python2 not found.
+else #No python3 config or python2 config
 	PYTHON_INCLUDE = -DNO_PYTHON_FOR_YOU
 	PYTHON_LIB = -DNO_PYTHON_FOR_YOU
 	CONTROL_MODULES =
 	PYTHON_MODULES =
 endif
+
 endif
 
 CROW_LIB_INCLUDE_DIR := $(CROW_DIR)/contrib/include
