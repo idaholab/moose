255345b7ac5f5f00a925c2bf7d8f629ada8b5e59
David Andrs <david.andrs@inl.gov>
2014-06-16 17:12:57 -0600

    2-phase, step 3 with constant area a constant volume fraction

---
+++ modules/thm/include/kernels/OneDMassFlux.h
@@ -0,0 +1,30 @@
+#ifndef ONEDMASSFLUX_H
+#define ONEDMASSFLUX_H
+
+#include "Kernel.h"
+
+class OneDMassFlux;
+
+template<>
+InputParameters validParams<OneDMassFlux>();
+
+/**
+ * Mass flux
+ */
+class OneDMassFlux : public Kernel
+{
+public:
+  OneDMassFlux(const std::string & name, InputParameters parameters);
+  virtual ~OneDMassFlux();
+
+protected:
+  virtual Real computeQpResidual();
+  virtual Real computeQpJacobian();
+  virtual Real computeQpOffDiagJacobian(unsigned int jvar);
+
+  VariableValue & _rhouA;
+  unsigned int _rhouA_var_number;
+};
+
+
+#endif /* ONEDMASSFLUX_H */
---
+++ modules/thm/src/kernels/OneDMassFlux.C
@@ -0,0 +1,46 @@
+#include "OneDMassFlux.h"
+
+template<>
+InputParameters validParams<OneDMassFlux>()
+{
+  InputParameters params = validParams<Kernel>();
+  params.addRequiredCoupledVar("rhouA", "conserved x-momentum");
+
+  return params;
+}
+
+OneDMassFlux::OneDMassFlux(const std::string & name, InputParameters parameters) :
+    Kernel(name, parameters),
+    _rhouA(coupledValue("rhouA")),
+    _rhouA_var_number(coupled("rhouA"))
+{
+}
+
+OneDMassFlux::~OneDMassFlux()
+{
+}
+
+Real
+OneDMassFlux::computeQpResidual()
+{
+  // Note: negative sign comes from integration by parts
+  return -_rhouA[_qp] * _grad_test[_i][_qp](0);
+}
+
+Real
+OneDMassFlux::computeQpJacobian()
+{
+  // This seems weird at first glance, but this term does not depend
+  // on U0, only the conserved variable U1.  In other words the (1,1)
+  // entry of the flux Jacobian is 0.
+  return 0.;
+}
+
+Real
+OneDMassFlux::computeQpOffDiagJacobian(unsigned int jvar)
+{
+  if (jvar == _rhouA_var_number)
+    return -_phi[_j][_qp] * _grad_test[_i][_qp](0);
+  else
+    return 0.;
+}
