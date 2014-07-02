951f3275d75976e365ae3d453e59b74f286bb18d
David Andrs <david.andrs@inl.gov>
2014-07-01 19:54:51 -0600

    Adding jacobian wrt alpha_A_liquid for OneDEenrgyFlux kernel

--- modules/thm/include/kernels/OneDEnergyFlux.h
+++ modules/thm/include/kernels/OneDEnergyFlux.h
@@ -23,16 +23,27 @@ protected:
   virtual Real computeQpJacobian();
   virtual Real computeQpOffDiagJacobian(unsigned int jvar);
 
+  bool _is_liquid;
+  Real _sign;
+
   VariableValue & _rhouA;
 
+  VariableValue & _area;
   VariableValue & _rho;
   VariableValue & _rhou;
   VariableValue & _rhoE;
   VariableValue & _u_vel;
   VariableValue & _enthalpy;
+  VariableValue & _pressure;
 
   unsigned _rhoA_var_number;
   unsigned _rhouA_var_number;
+  bool _has_alpha_A;
+  unsigned int _alpha_A_liquid_var_number;
+  MaterialProperty<Real> * _dp_dalphaA_liquid;
+  Real _vf_norm_factor;
+
+  VariableValue & _alpha;
 
   const EquationOfState & _eos;
 };
--- modules/thm/src/kernels/OneDEnergyFlux.C
+++ modules/thm/src/kernels/OneDEnergyFlux.C
@@ -5,6 +5,7 @@ template<>
 InputParameters validParams<OneDEnergyFlux>()
 {
   InputParameters params = validParams<Kernel>();
+  params.addRequiredCoupledVar("area", "Cross-sectional area");
   params.addRequiredCoupledVar("rho", "density");
   params.addRequiredCoupledVar("rhou", "momentum");
   params.addRequiredCoupledVar("rhoE", "total energy");
@@ -14,6 +15,12 @@ InputParameters validParams<OneDEnergyFlux>()
 
   params.addRequiredCoupledVar("u", "velocity");
   params.addRequiredCoupledVar("enthalpy", "enthalpy");
+  params.addRequiredCoupledVar("pressure", "pressure");
+
+  params.addRequiredParam<bool>("is_liquid", "True for liquid, false for vapor");
+  params.addRequiredParam<Real>("vf_norm_factor", "Normalization factor for volume fraction equation.");
+  params.addCoupledVar("alpha", 1., "Volume fraction");
+  params.addCoupledVar("alpha_A_liquid", "Volume fraction of liquid");
 
   params.addRequiredParam<UserObjectName>("eos", "The name of equation of state object to use.");
 
@@ -22,14 +29,26 @@ InputParameters validParams<OneDEnergyFlux>()
 
 OneDEnergyFlux::OneDEnergyFlux(const std::string & name, InputParameters parameters) :
     Kernel(name, parameters),
+    _is_liquid(getParam<bool>("is_liquid")),
+    _sign(_is_liquid ? 1. : -1.),
     _rhouA(coupledValue("rhouA")),
+    _area(coupledValue("area")),
     _rho(coupledValue("rho")),
     _rhou(coupledValue("rhou")),
     _rhoE(coupledValue("rhoE")),
     _u_vel(coupledValue("u")),
     _enthalpy(coupledValue("enthalpy")),
+    _pressure(coupledValue("pressure")),
     _rhoA_var_number(coupled("rhoA")),
     _rhouA_var_number(coupled("rhouA")),
+    _has_alpha_A(isCoupled("alpha_A_liquid")),
+    _alpha_A_liquid_var_number(_has_alpha_A ? coupled("alpha_A_liquid") : libMesh::invalid_uint),
+    _dp_dalphaA_liquid(_has_alpha_A ?
+        (_is_liquid ? &getMaterialProperty<Real>("dp_L_d_alphaA_L") : &getMaterialProperty<Real>("dp_V_d_alphaA_L")) :
+        NULL),
+    // volume fraction norm factor
+    _vf_norm_factor(getParam<Real>("vf_norm_factor")),
+    _alpha(coupledValue("alpha")),
     _eos(getUserObject<EquationOfState>("eos"))
 {
 }
@@ -63,6 +82,10 @@ OneDEnergyFlux::computeQpOffDiagJacobian(unsigned int jvar)
   {
     Real A32 = _u_vel[_qp] * _eos.dp_drhou(_rho[_qp], _rhou[_qp], _rhoE[_qp]) + _enthalpy[_qp];
     return -A32 * _phi[_j][_qp] * _grad_test[_i][_qp](0);
+  }
+  else if (jvar == _alpha_A_liquid_var_number)
+  {
+    return -(_u_vel[_qp] * (_sign * _pressure[_qp] + _alpha[_qp] * _area[_qp] * (*_dp_dalphaA_liquid)[_qp])) / _vf_norm_factor * _phi[_j][_qp] * _grad_test[_i][_qp](0);
   }
   else
     return 0.;
