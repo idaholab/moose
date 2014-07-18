cf5679c9caeee13920acb51ea88e203727193d5b
Derek Gaston <derek.gaston@inl.gov>
2014-07-18 14:28:18 -0600

    add test that shows how to get normals in an AuxKernel closes #3558

---
+++ test/include/auxkernels/FluxAverageAux.h
@@ -0,0 +1,56 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#ifndef FLUXAVERAGEAUX_H
+#define FLUXAVERAGEAUX_H
+
+#include "AuxKernel.h"
+
+
+//Forward Declarations
+class FluxAverageAux;
+
+template<>
+InputParameters validParams<FluxAverageAux>();
+
+/**
+ * Computes the average "flux" of a coupled variable and puts it in an elemental field.
+ *
+ * Only works on boundaries.
+ */
+class FluxAverageAux : public AuxKernel
+{
+public:
+  FluxAverageAux(const std::string & name, InputParameters parameters);
+
+  virtual ~FluxAverageAux() {}
+
+protected:
+  virtual Real computeValue();
+
+  /// The "material" property
+  Real _diffusivity;
+
+  /// Coupled gradient
+  VariableGradient & _coupled_gradient;
+
+  /// The variable we're coupled to
+  MooseVariable & _coupled_var;
+
+  /// normals at quadrature points
+  const MooseArray<Point> & _normals;
+
+};
+
+#endif //FLUXAVERAGEAUX_H
---
+++ test/src/auxkernels/FluxAverageAux.C
@@ -0,0 +1,41 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#include "FluxAverageAux.h"
+
+template<>
+InputParameters validParams<FluxAverageAux>()
+{
+  InputParameters params = validParams<AuxKernel>();
+
+  params.addRequiredCoupledVar("coupled", "Coupled variable for calculation of the flux");
+  params.addRequiredParam<Real>("diffusivity", "Value to use as the 'diffusivity'");
+
+  return params;
+}
+
+FluxAverageAux::FluxAverageAux(const std::string & name, InputParameters parameters) :
+    AuxKernel(name, parameters),
+    _diffusivity(getParam<Real>("diffusivity")),
+    _coupled_gradient(coupledGradient("coupled")),
+    _coupled_var(*(getCoupledVars().find("coupled")->second)[0]),
+    _normals(_coupled_var.normals())
+{
+}
+
+Real
+FluxAverageAux::computeValue()
+{
+  return _diffusivity*_coupled_gradient[_qp]*_normals[_qp];
+}
