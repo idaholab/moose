1d2f490a76de3b8346344cecaf26c014d135222b
Daniel Schwen <daniel.schwen@inl.gov>
2014-07-30 08:19:30 +0000

    add Langevin noise kernel
    r28927

---
+++ modules/phase_field/include/kernels/LangevinNoise.h
@@ -0,0 +1,26 @@
+#ifndef LANGEVINNOISE_H
+#define LANGEVINNOISE_H
+
+#include "Kernel.h"
+
+//Forward Declarations
+class LangevinNoise;
+
+template<>
+InputParameters validParams<LangevinNoise>();
+
+class LangevinNoise : public Kernel
+{
+public:
+  LangevinNoise(const std::string & name, InputParameters parameters);
+
+protected:
+  virtual void residualSetup();
+  virtual Real computeQpResidual();
+
+private:
+  const Real _amplitude;
+  const MaterialProperty<Real> * _multiplier_prop;
+};
+
+#endif //LANGEVINNOISE_H
---
+++ modules/phase_field/src/kernels/LangevinNoise.C
@@ -0,0 +1,31 @@
+#include "LangevinNoise.h"
+#include "MooseRandom.h"
+
+template<>
+InputParameters validParams<LangevinNoise>()
+{
+  InputParameters params = validParams<Kernel>();
+  params.addRequiredParam<Real>("amplitude", "Amplitude"); // per sqrt(time)");
+  params.addParam<std::string>("multiplier", "Material property to multiply the random numbers with (defaults to 1.0 if omitted)");
+  return params;
+}
+LangevinNoise::LangevinNoise(const std::string & name,
+                             InputParameters parameters) :
+    Kernel(name, parameters),
+    _amplitude(getParam<Real>("amplitude")),
+    _multiplier_prop(parameters.isParamValid("multiplier") ? &getMaterialProperty<Real>(getParam<std::string>("multiplier")) : NULL)
+{
+}
+
+void
+LangevinNoise::residualSetup()
+{
+  unsigned int rseed = _t_step;
+  MooseRandom::seed(rseed);
+}
+
+Real
+LangevinNoise::computeQpResidual()
+{
+  return -_test[_i][_qp] * (2.0 * MooseRandom::rand() - 1.0) * _amplitude * (_multiplier_prop == NULL ? 1.0 : (*_multiplier_prop)[_qp]);
+}
