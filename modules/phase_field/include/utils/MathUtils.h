5c512b8d766c834aa2de6392e47c044a870b2a64
Daniel Schwen <daniel.schwen@inl.gov>
2014-08-19 14:57:19 -0600

    More code formatting (#1)

--- modules/phase_field/include/utils/MathUtils.h
+++ modules/phase_field/include/utils/MathUtils.h
@@ -2,7 +2,6 @@
 #define MATHUTILS_H
 
 #include "Moose.h"
-
 #include "libmesh/libmesh.h"
 
 namespace MathUtils
@@ -93,10 +92,10 @@ namespace MathUtils
 
   inline Real poly3Log(Real x, Real tol, int order)
   {
-    Real c1 = 1.0/(3.0*tol*tol*tol);
-    Real c2 = -3.0/(2.0*tol*tol);
-    Real c3 = 3.0/tol;
-    Real c4 = std::log(tol)-11.0/6.0;
+    Real c1 = 1.0 / (3.0 * tol*tol*tol);
+    Real c2 = -3.0 / (2.0 * tol*tol);
+    Real c3 = 3.0 / tol;
+    Real c4 = std::log(tol) - 11.0/6.0;
 
     Real value = 0.0;
 
@@ -117,16 +116,16 @@ namespace MathUtils
     else if (order == 2)
     {
       if (x < tol)
-        value = 6.0*c1*x + 2.0*c2;
+        value = 6.0 * c1 * x + 2.0 * c2;
       else
-        value = -1.0/(x*x);
+        value = -1.0 / (x*x);
     }
     else if (order == 3)
     {
       if (x < tol)
         value = 6.0*c1;
       else
-        value = 2.0/(x*x*x);
+        value = 2.0 / (x*x*x);
     }
     return value;
   }
@@ -138,46 +137,64 @@ namespace MathUtils
     if (order == 0)
     {
       if (x < tol)
-        value = log(tol)+(x-tol)/tol-(x-tol)*(x-tol)/(2.0*tol*tol)+(x-tol)*(x-tol)*(x-tol)/(3.0*tol*tol*tol)-(x-tol)*(x-tol)*(x-tol)*(x-tol)/(4.0*tol*tol*tol*tol)+(x-tol)*(x-tol)*(x-tol)*(x-tol)*(x-tol)/(5.0*tol*tol*tol*tol*tol)-(x-tol)*(x-tol)*(x-tol)*(x-tol)*(x-tol)*(x-tol)/(6*tol*tol*tol*tol*tol*tol);
+        value =   std::log(tol)
+                + (x-tol) / tol
+                - (x-tol) * (x-tol) / (2.0 * tol*tol)
+                + (x-tol) * (x-tol) * (x-tol) / (3.0 * tol*tol*tol)
+                - (x-tol) * (x-tol) * (x-tol) * (x-tol) / (4.0 * tol*tol*tol*tol)
+                + (x-tol) * (x-tol) * (x-tol) * (x-tol) * (x-tol) / (5.0 * tol*tol*tol*tol*tol)
+                - (x-tol) * (x-tol) * (x-tol) * (x-tol) * (x-tol) * (x-tol) / (6.0 * tol*tol*tol*tol*tol*tol);
       else
         value = std::log(x);
     }
     else if (order == 1)
     {
       if (x < tol)
-        value = 1/tol-2.0*(x-tol)/(2.0*tol*tol)+3.0*(x-tol)*(x-tol)/(3.0*tol*tol*tol)-4.0*(x-tol)*(x-tol)*(x-tol)/(4.0*tol*tol*tol*tol)+5.0*(x-tol)*(x-tol)*(x-tol)*(x-tol)/(5.0*tol*tol*tol*tol*tol)-6*(x-tol)*(x-tol)*(x-tol)*(x-tol)*(x-tol)/(6*tol*tol*tol*tol*tol*tol);
+        value =   1.0 / tol
+                - 2.0 * (x-tol)/(2.0*tol*tol)
+                + 3.0 * (x-tol) * (x-tol) / (3.0 * tol*tol*tol)
+                - 4.0 * (x-tol) * (x-tol) * (x-tol) / (4.0*tol*tol*tol*tol)
+                + 5.0 * (x-tol) * (x-tol) * (x-tol) * (x-tol) / (5.0 * tol*tol*tol*tol*tol)
+                - 6.0 * (x-tol) * (x-tol) * (x-tol) * (x-tol) * (x-tol) / (6.0 * tol*tol*tol*tol*tol*tol);
       else
-        value = 1.0/x;
+        value = 1.0 / x;
     }
     else if (order == 2)
     {
       if (x < tol)
-        value = -2.0*1/(2.0*tol*tol)+3.0*2.0*(x-tol)/(3.0*tol*tol*tol)-4.0*3.0*(x-tol)*(x-tol)/(4.0*tol*tol*tol*tol)+5.0*4.0*(x-tol)*(x-tol)*(x-tol)/(5.0*tol*tol*tol*tol*tol)-6*5.0*(x-tol)*(x-tol)*(x-tol)*(x-tol)/(6*tol*tol*tol*tol*tol*tol);
+        value = -2.0 * 1.0 / (2.0*tol*tol)
+                + 3.0 * 2.0 * (x-tol) / (3.0 * tol*tol*tol)
+                - 4.0 * 3.0 * (x-tol) * (x-tol) / (4.0 * tol*tol*tol*tol)
+                + 5.0 * 4.0 * (x-tol) * (x-tol) * (x-tol) / (5.0 * tol*tol*tol*tol*tol)
+                - 6.0 * 5.0 * (x-tol) * (x-tol) * (x-tol) * (x-tol) / (6.0 * tol*tol*tol*tol*tol*tol);
       else
-        value = -1.0/(x*x);
+        value = -1.0 / (x*x);
     }
     else if (order == 3)
     {
       if (x < tol)
-        value =  3.0*2.0*1/(3.0*tol*tol*tol)-4.0*3.0*2.0*(x-tol)/(4.0*tol*tol*tol*tol)+5.0*4.0*3.0*(x-tol)*(x-tol)/(5.0*tol*tol*tol*tol*tol)-6*5.0*4.0*(x-tol)*(x-tol)*(x-tol)/(6*tol*tol*tol*tol*tol*tol);
+        value =   3.0 * 2.0 * 1.0 / (3.0 * tol*tol*tol)
+                - 4.0 * 3.0 * 2.0 * (x-tol) / (4.0 * tol*tol*tol*tol)
+                + 5.0 * 4.0 * 3.0 * (x-tol) * (x-tol) / (5.0 * tol*tol*tol*tol*tol)
+                - 6.0 * 5.0 * 4.0 * (x-tol) * (x-tol) * (x-tol) / (6.0 * tol*tol*tol*tol*tol*tol);
       else
-        value =  2.0/(x*x*x);
+        value =  2.0 / (x*x*x);
     }
     return value;
   }
 
+  /// \todo This can be done without std::pow!
   inline Real TaylorLog(Real x)
   {
     Real y = (x - 1.0) / (x + 1.0);
-    Real val=1.0;
+    Real val = 1.0;
     for (unsigned int i = 0; i < 5; ++i)
     {
       Real exponent = i + 2.0;
-      val += 1.0/(2.0*(i+1.0)+1.0)*std::pow(y, exponent);
+      val += 1.0 / (2.0 * (i + 1.0) + 1.0) * std::pow(y, exponent);
     }
 
-    val *= 2.0 * y;
-    return val;
+    return val * 2.0 * y;
   }
 }
 
