d9a8a2e14bec60c49fbef2674182efb25668c53b
Andrew E Slaughter <andrew.slaughter@inl.gov>
2014-08-20 22:44:14 -0600

    Documentation and more testing for CSVIO.py

--- gui/io/CSVIO.py
+++ gui/io/CSVIO.py
@@ -5,16 +5,29 @@ import os, sys, csv
 sys.path.append(os.path.abspath('..'))
 from base import *
 
-
+##
+# A class for performing read/write operations on *.csv files
 class CSVIO(object, PeacockErrorInterface, PeacockTestInterface):
+
+  ##
+  # Class constructor
+  # @param filename The name of the csv file top open
+  # @param kwargs Optional keyword/value pairs
+  #
+  # Optional Arguments:
+  #   @see PeacockErrorInterface, PeacockTestInterface
   def __init__(self, filename, **kwargs):
     PeacockErrorInterface.__init__(self, **kwargs)
     PeacockTestInterface.__init__(self, **kwargs)
 
-
     # Initialize member variables
     self._headers = []
     self._data = dict()
+
+    # Produce an error if the file does not exist
+    if not os.path.isfile(filename):
+      self.peacockError('The file \'' + filename + '\' does not exist.')
+      return
 
     # Read the file
     with open(filename) as csvfile:
@@ -42,8 +55,15 @@ class CSVIO(object, PeacockErrorInterface, PeacockTestInterface):
           for idx in xrange(len(row)):
             self._data[self._headers[idx]].append(float(row[idx]))
 
+  ##
+  # Operator[] to allow access to the data directly via the class object (public)
+  # For example:
+  #   data = CSVIO('test.csv')
+  #   print data['temp']
+  #
   def __getitem__(self, key):
 
+    # Attempt to access the data, produce an error to the screen if it fails
     try:
       return self._data[key]
 
@@ -51,16 +71,27 @@ class CSVIO(object, PeacockErrorInterface, PeacockTestInterface):
       self.peacockError('No data for key \'' + key + '\' located', dialog=False)
       return None
 
+# tests (see PeacockTestInterface)
+  ## Test reading data
   def _testDataRead(self):
     result = data['temp'] == [1.2, 3.3, 5.5]
     fail_msg = 'Data read failed'
     return (result, fail_msg)
 
+  ## Test data access failure
   def _testDataError(self):
     data['ThisDoesNotExist']
-    result = self.getLastErrorMessage() == 'No data for key \'ThisDoesNotExist\' located'
+    result = self.testLastErrorMessage('No data for key \'ThisDoesNotExist\' located')
     fail_msg = 'Error failed to produce'
     return (result, fail_msg)
+
+  ## Test invalid file input
+  def _testInvalidInput(self):
+    io = CSVIO('bad_filename.csv', testing=True)
+    result = io.testLastErrorMessage('The file \'bad_filename.csv\' does not exist.')
+    fail_msg = 'The fake filename exists'
+    return (result, fail_msg)
+
 
 # Perform testing
 if __name__ == "__main__":
