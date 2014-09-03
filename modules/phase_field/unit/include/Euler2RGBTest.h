0192289890454bbf55e44b9eeac8b2eff260efda
Daniel Schwen <daniel.schwen@inl.gov>
2014-09-03 15:18:56 -0600

    Add Euler2RGB unit test (#27)

---
+++ modules/phase_field/unit/include/Euler2RGBTest.h
@@ -0,0 +1,38 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#ifndef MATHUTILSTEST_H
+#define MATHUTILSTEST_H
+
+//CPPUnit includes
+#include "cppunit/extensions/HelperMacros.h"
+
+class Euler2RGBTest : public CppUnit::TestFixture
+{
+  CPPUNIT_TEST_SUITE( Euler2RGBTest );
+
+  CPPUNIT_TEST( test );
+
+  CPPUNIT_TEST_SUITE_END();
+
+public:
+  void setUp();
+  void tearDown();
+
+  void test();
+
+private:
+};
+
+#endif //MATHUTILSTEST_H
