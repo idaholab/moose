03172eeef326100edb63bddd49a4d1b37bd8e184
Andrew E Slaughter <andrew.slaughter@inl.gov>
2014-09-03 13:26:57 -0600

    Working on using TestHarness to test Peacock

--- gui/tests/io/csvio.py
+++ gui/tests/io/csvio.py
@@ -6,22 +6,22 @@ import io
 
 # Test reading data
 def testDataRead():
-  csvio = io.CSVIO('test.csv', testing=True)
+  csvio = io.CSVIO('input.csv', testing=True)
   result = csvio['temp'] == [1.2, 3.3, 5.5]
   fail_msg = 'Data read failed'
   return (result, fail_msg)
 
 # Test data access failure
 def testDataError():
-  csvio = io.CSVIO('test.csv', testing=True)
+  csvio = io.CSVIO('input.csv', testing=True)
   csvio['ThisDoesNotExist']
   result = csvio.testLastErrorMessage('No data for key \'ThisDoesNotExist\' located')
   fail_msg = 'Error failed to produce'
-  return (result, fail_msg)
+  return [result, fail_msg]
 
 # Test invalid file input
 def testInvalidInput():
   csvio = io.CSVIO('bad_filename.csv', testing=True)
   result = csvio.testLastErrorMessage('The file \'bad_filename.csv\' does not exist.')
   fail_msg = 'The fake filename exists'
-  return (result, fail_msg)
+  return [result, fail_msg]
