865891e84a76b8795afdea7779100b104acc8a65
Cody Permann <codypermann@gmail.com>
2014-09-08 13:23:31 -0600

    Thermochimica makefile (#22711)
    Co-authored-by: Cody Permann <codypermann@gmail.com>
    Co-authored-by: Jason D. Hales <jason.hales@inl.gov>
    Co-authored-by: zabrax <adam.zabriskie@inl.gov>

---
+++ modules/chemical_reactions/contrib/thermochimica.mk
@@ -0,0 +1,36 @@
+# source files
+Thermochimica_srcfiles    := $(shell find $(THERMOCHIMICA_SRC) -name "*.C")
+Thermochimica_f90modfiles := $(shell find $(THERMOCHIMICA_SRC) -name "Module*.f90")
+Thermochimica_f90srcfiles := $(shell find $(THERMOCHIMICA_SRC) -name "*.f90" -not -name "Module*")
+
+# object files
+Thermochimica_objects     := $(patsubst %.C, %.$(obj-suffix), $(Thermochimica_srcfiles))
+Thermochimica_objects     += $(patsubst %.f90, %.$(obj-suffix), $(Thermochimica_f90modfiles))
+Thermochimica_objects     += $(patsubst %.f90, %.$(obj-suffix), $(Thermochimica_f90srcfiles))
+
+# dependencies (C, C++ files only)
+Thermochimica_deps := $(patsubst %.C, %.$(obj-suffix).d, $(Thermochimica_srcfiles)) \
+                      $(patsubst %.c, %.$(obj-suffix).d, $(Thermochimica_csrcfiles))
+
+# clang static analyzer files
+Thermochimica_analyzer := $(patsubst %.C, %.plist.$(obj-suffix), $(Thermochimica_srcfiles))
+
+app_LIBS       := $(THERMOCHIMICA_DIR)/lib/libThermochimica-$(METHOD).la $(app_LIBS)
+app_LIBS_other := $(filter-out $(app_LIB),$(app_LIBS))
+
+$(THERMOCHIMICA_DIR)/lib/libThermochimica-$(METHOD).la : $(Thermochimica_objects)
+	@$(shell mkdir -p $(THERMOCHIMICA_DIR)/lib)
+	@echo "Linking Library "$@"..."
+	@$(libmesh_LIBTOOL) --tag=CXX $(LIBTOOLFLAGS) --mode=link --quiet \
+	  $(libmesh_CXX) $(libmesh_CXXSHAREDFLAG) -o $@ $(Thermochimica_objects) $(libmesh_LDFLAGS) $(EXTERNAL_FLAGS) -rpath $(THERMOCHIMICA_DIR)/lib
+	@$(libmesh_LIBTOOL) --mode=install --quiet install -c $@ $(THERMOCHIMICA_DIR)/lib
+
+# Include dependencies (see note above)
+-include $(Thermochimica_deps)
+
+ADDITIONAL_LIBS += $(THERMOCHIMICA_DIR)/lib/libThermochimica-$(METHOD).la
+
+app_INCLUDES += -I$(THERMOCHIMICA_SRC) -I$(THERMOCHIMICA_SRC)/api -I$(THERMOCHIMICA_SRC)/module
+
+# F90 module dependency rules
+$(Thermochimica_f90srcfiles): $(patsubst %.f90, %.$(obj-suffix), $(Thermochimica_f90modfiles))
