dc9488c07c3ff48edb18c7ee348bf260ec9c9e1c
Andrew E Slaughter <andrew.slaughter@inl.gov>
2014-09-16 20:16:28 -0600

    Beginning to build MooseWidget Tests

--- gui/src/base/__init__.py
+++ gui/src/base/__init__.py
@@ -3,4 +3,5 @@ from PeacockTestInterface import *
 from MooseWidgetInfoBase import *
 from MooseWidget import *
 from PeacockConsoleWidget import PeacockConsoleWidget
+
 __all__ = ['PeacockConsoleWidget', 'PeacockErrorInterface', 'PeacockTestInterface', 'MooseWidget', 'MooseWidgetInfoBase']
