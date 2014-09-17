52ff6e8b507cd8e80e6c50be1e7c89db94cab728
Andrew E Slaughter <andrew.slaughter@inl.gov>
2014-09-17 08:24:28 -0600

    Added test for duplicate name

--- gui/tests/base/MooseWidget_object.py
+++ gui/tests/base/MooseWidget_object.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python
-########################################
-# Tests for MooseObject::object method #
-########################################
 import os, sys
 from PySide import QtGui
 
@@ -24,43 +21,50 @@ class TestMooseWidget(QtGui.QWidget, base.MooseWidget):
 # Create
 app  = QtGui.QApplication(sys.argv)
 main = QtGui.QMainWindow()
-test = TestMooseWidget(main=main)
-test.addObject(SubTestMooseWidget(), handle='sub_widget')
-#test.addObject(SubTestMooseWidget(), handle='another_sub_widget')
-test.setProperty('debug', True)
+_test = TestMooseWidget(main=main)
+_test.addObject(SubTestMooseWidget(), handle='sub_widget')
+_test.addObject(SubTestMooseWidget(), handle='another_sub_widget')
+_test.setProperty('debug', True)
 
 # Test call to 'object' fails when an invalid parent name is given
 def invalidParentObject():
-  obj = test.object('sub_widget', parent='invalid_parent_name')
+  obj = _test.object('sub_widget', owner='invalid_parent_name')
   result = obj == None
   fail_msg = 'Object is not None'
   return (result, fail_msg)
 
 # Test 'object' pass when a valid parent object name is given
 def validParentObject():
-  test.setProperty('debug', True)
-  obj = test.object('sub_sub_widget', parent='sub_widget')
+  _test.setProperty('debug', True)
+  obj = _test.object('sub_sub_widget', owner='sub_widget')
   result = obj.property('handle') == 'sub_sub_widget'
   fail_msg = 'Object unexpected handle'
   return (result, fail_msg)
 
 # Test that 'object' fails when an invalid child object name is given
 def invalidObject():
-  obj = test.object('invalid_widget_name')
+  obj = _test.object('invalid_widget_name')
   result = obj == None
   fail_msg = 'Object is not None'
   return (result, fail_msg)
 
 # Test that 'object' returns a valid object when an valid child object name is given
 def validChildObject():
-  obj = test.object('sub_widget')
+  obj = _test.object('sub_widget')
   result = obj.property('handle') == 'sub_widget'
   fail_msg = 'Object has invalid handle'
   return (result, fail_msg)
 
 # Test that 'object' returns a valid object when an valid grandchild object name is given
 def validGrandChildObject():
-  obj = test.object('sub_sub_widget')
+  obj = _test.object('sub_sub_widget')
   result = obj.property('handle') == 'sub_sub_widget'
   fail_msg = 'Object has invalid handle'
+  return (result, fail_msg)
+
+# Test duplicate name error message
+def addObjectDuplicateName():
+  _test.addObject(QtGui.QWidget(), handle='sub_widget')
+  result = _test.testLastErrorMessage('The handle sub_widget already exists')
+  fail_msg = 'No expected error'
   return (result, fail_msg)
