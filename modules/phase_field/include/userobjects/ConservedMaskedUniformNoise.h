85e1199c57978545f97b4b20b6b6d42938481d77
Daniel Schwen <daniel.schwen@inl.gov>
2014-09-24 16:13:24 -0600

    Add necessary and remove empty files (#38)

---
+++ modules/phase_field/include/userobjects/ConservedMaskedUniformNoise.h
@@ -0,0 +1,30 @@
+#ifndef CONSERVEDMASKEDUNIFORMNOISE_H
+#define CONSERVEDMASKEDUNIFORMNOISE_H
+
+#include "ConservedMaskedNoiseBase.h"
+#include "ConservedUniformNoiseVeneer.h"
+
+//Forward delcarations
+class ConservedMaskedUniformNoise;
+
+template<>
+InputParameters validParams<ConservedMaskedUniformNoise>()
+{
+  return validParams<ConservedMaskedNoiseBase>();
+}
+
+/**
+ * Userobject that generates a uniformly distributed random number in the interval [-1:1]
+ * once per timestep for every quadrature point in a way that the integral
+ * over all random numbers is zero.
+ *
+ * \see ConservedNoiseBase
+ */
+class ConservedMaskedUniformNoise : public ConservedUniformNoiseVeneer<ConservedMaskedNoiseBase>
+{
+public:
+  ConservedMaskedUniformNoise(const std::string & name, InputParameters parameters) :
+    ConservedUniformNoiseVeneer<ConservedMaskedNoiseBase>(name, parameters) {}
+};
+
+#endif //CONSERVEDMASKEDUNIFORMNOISE_H
