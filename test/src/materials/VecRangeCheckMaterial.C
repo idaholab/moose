0c66414dd20ed13bd0b73909bcbde68336a9272c
Daniel Schwen <daniel.schwen@inl.gov>
2014-10-04 09:28:37 -0600

    Add tests for types and enable vectors of long (#3988)

--- test/src/materials/VecRangeCheckMaterial.C
+++ test/src/materials/VecRangeCheckMaterial.C
@@ -5,9 +5,12 @@ InputParameters validParams<VecRangeCheckMaterial>()
 {
   InputParameters params = validParams<Material>();
   params.addRequiredRangeCheckedParam<std::vector<Real> >("rv3", "rv3_size = 3", "Real vector of length 3");
-  params.addRequiredRangeCheckedParam<std::vector<Real> >("iv3", "iv3_size = 3", "Int vector of length 3");
+  params.addRequiredRangeCheckedParam<std::vector<int> >("iv3", "iv3_size = 3", "Int vector of length 3");
   params.addRequiredRangeCheckedParam<std::vector<Real> >("rvp", "rvp > 0", "Real vector of all positive values");
-  params.addRequiredRangeCheckedParam<std::vector<Real> >("ivg", "ivg_0 > ivg_1", "Int vector where component 0 is bigger than component 1");
+  params.addRequiredRangeCheckedParam<std::vector<unsigned int> >("uvg", "uvg_0 > uvg_1", "Unsigned int vector where component 0 is bigger than component 1");
+  params.addRequiredRangeCheckedParam<std::vector<long> >("lvg", "lvg_0 > lvg_1", "Long vector where component 0 is bigger than component 1");
+  params.addRequiredRangeCheckedParam<std::vector<int> >("ivg", "ivg_0 > ivg_1", "Int vector where component 0 is bigger than component 1");
+  params.addRequiredRangeCheckedParam<std::vector<Real> >("rvg", "rvg_0 > rvg_1", "Real vector where component 0 is bigger than component 1");
   params.addRequiredRangeCheckedParam<std::vector<Real> >("rvl", "rvl_10 > 0", "Testing if component 10 is positive (usually we should have a size check here as well)");
   return params;
 }
--- test/tests/parser/vector_range_checking/all_pass.i
+++ test/tests/parser/vector_range_checking/all_pass.i
@@ -10,7 +10,10 @@
     rv3 = '1.1 2.2 3.3'
     iv3 = '1 2 3'
     rvp = '0.1 0.2 0.3 0.4'
+    uvg = '2 1'
+    lvg = '2 1'
     ivg = '2 1'
+    rvg = '2.0 1.0'
     rvl = '0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.1 1.2 1.3'
   [../]
 []
@@ -18,7 +21,7 @@
 [Problem]
   type = FEProblem
   solve = false
-  kernel_check = false
+  #kernel_check = false
 []
 
 [Executioner]
