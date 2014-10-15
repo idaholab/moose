2a47229c9f37c2b4b5f5bad4445b21abaece8415
Benjamin Spencer <benjamin.spencer@inl.gov>
2014-10-15 12:34:34 -0600

    Add MultiAppCopyTransfer ref #4736

---
+++ framework/include/transfers/MultiAppCopyTransfer.h
@@ -0,0 +1,45 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#ifndef MULTIAPPCOPYTRANSFER_H
+#define MULTIAPPCOPYTRANSFER_H
+
+#include "MultiAppTransfer.h"
+
+class MooseVariable;
+class MultiAppCopyTransfer;
+
+template<>
+InputParameters validParams<MultiAppCopyTransfer>();
+
+/**
+ * Copy the value to the target domain from the nearest node in the source domain.
+ */
+class MultiAppCopyTransfer :
+  public MultiAppTransfer
+{
+public:
+  MultiAppCopyTransfer(const std::string & name, InputParameters parameters);
+  virtual ~MultiAppCopyTransfer() {}
+
+  virtual void initialSetup();
+
+  virtual void execute();
+
+protected:
+  AuxVariableName _to_var_name;
+  VariableName _from_var_name;
+};
+
+#endif // MULTIAPPCOPYTRANSFER_H
