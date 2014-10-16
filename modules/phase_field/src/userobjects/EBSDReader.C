68c5d7bdca7ab975037213f46f9b92322a6eaf0a
Daniel Schwen <daniel.schwen@inl.gov>
2014-10-16 10:56:24 -0600

    Remove superfluous const qualifier (#57)

--- modules/phase_field/src/userobjects/EBSDReader.C
+++ modules/phase_field/src/userobjects/EBSDReader.C
@@ -168,13 +168,13 @@ EBSDReader::getAvgData(unsigned int phase, unsigned int grain) const
   return _avg_data[indexFromIndex(_feature_id[phase][grain])];
 }
 
-const unsigned int
+unsigned int
 EBSDReader::getGrainNum() const
 {
   return _feature_num;
 }
 
-const unsigned int
+unsigned int
 EBSDReader::getGrainNum(unsigned int phase) const
 {
   return _feature_id[phase].size();
