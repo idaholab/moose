b88390f3d5b38dd339ef5155def3b1658db87bee
David Andrs <david.andrs@inl.gov>
2014-10-23 09:20:35 -0600

    Comparisons on shared pointers need .get() called
    Refs #4042

--- framework/src/actions/CreateExecutionerAction.C
+++ framework/src/actions/CreateExecutionerAction.C
@@ -96,7 +96,7 @@ CreateExecutionerAction::act()
   MooseSharedPointer<Executioner> executioner = MooseSharedNamespace::static_pointer_cast<Executioner>(_factory.create(_type, "Executioner", _moose_object_pars));
   Moose::setup_perf_log.pop("Create Executioner","Setup");
 
-  if (_problem != NULL)
+  if (_problem.get() != NULL)
   {
     storeCommonExecutionerParams(*_problem, _pars);
 
