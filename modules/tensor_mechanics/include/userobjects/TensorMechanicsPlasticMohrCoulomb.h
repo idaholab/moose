2d895a0d06c5d63c4fceb4696cecd52e39f52666
WilkAndy <andrew.wilkins@csiro.au>
2014-10-28 09:47:28 +1000

    'Cap' smoothing added to MohrCoulomb
    Two tests illustrating the correctness.
    
    Fixes #4139

--- modules/tensor_mechanics/include/userobjects/TensorMechanicsPlasticMohrCoulomb.h
+++ modules/tensor_mechanics/include/userobjects/TensorMechanicsPlasticMohrCoulomb.h
@@ -19,8 +19,9 @@ InputParameters validParams<TensorMechanicsPlasticMohrCoulomb>();
  * that implements more realistic cohesion, friction and dilation angles,
  * and/or hardening.
  *
- * The smoothing of the tip of the yield-surface cone is described in
+ * For 'hyperbolic' smoothing, the smoothing of the tip of the yield-surface cone is described in
  * Zienkiewicz and Prande "Some useful forms of isotropic yield surfaces for soil and rock mechanics" (1977) In G Gudehus (editor) "Finite Elements in Geomechanics" Wile, Chichester, pp 179-190.
+ * For 'cap' smoothing, additional smoothing is performed.
  * The smoothing of the edges of the cone is described in
  * AJ Abbo, AV Lyamin, SW Sloan, JP Hambleton "A C2 continuous approximation to the Mohr-Coulomb yield surface" International Journal of Solids and Structures 48 (2011) 3001-3010
  *
@@ -80,8 +81,24 @@ class TensorMechanicsPlasticMohrCoulomb : public TensorMechanicsPlasticModel
 
  protected:
 
+  /**
+   * The yield function is modified to
+   * f = s_m*sinphi + sqrt(a + s_bar^2 K^2) - C*cosphi
+   * where "a" depends on the tip_scheme.  Currently _tip_scheme is
+   * 'hyperbolic', where a = _small_smoother2
+   * 'cap' where a = _small_smoother2 + (p(stress_mean - _cap_start))^2
+   *       with the function p(x)=x(1-exp(-_cap_rate*x)) for x>0, and p=0 otherwise
+   */
+  MooseEnum _tip_scheme;
+
   /// Square of tip smoothing parameter to smooth the cone at mean_stress = T
   Real _small_smoother2;
+
+  /// smoothing parameter dictating when the 'cap' will start - see doco for _tip_scheme
+  Real _cap_start;
+
+  /// dictates how quickly the 'cap' degenerates to a hemisphere - see doco for _tip_scheme
+  Real _cap_rate;
 
   /// edge smoothing parameter, in radians
   Real _tt;
@@ -106,6 +123,15 @@ class TensorMechanicsPlasticMohrCoulomb : public TensorMechanicsPlasticModel
 
   /// if secondInvariant < _lode_cutoff then set Lode angle to zero.  This is to guard against precision-loss
   Real _lode_cutoff;
+
+  /// returns the 'a' parameter - see doco for _tip_scheme
+  virtual Real smooth(const RankTwoTensor & stress) const;
+
+  /// returns the da/dstress_mean - see doco for _tip_scheme
+  virtual Real dsmooth(const RankTwoTensor & stress) const;
+
+  /// returns the d^2a/dstress_mean^2 - see doco for _tip_scheme
+  virtual Real d2smooth(const RankTwoTensor & stress) const;
 
   /// cohesion as a function of internal parameter
   virtual Real cohesion(const Real internal_param) const;
