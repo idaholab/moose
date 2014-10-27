f785b7252afcc7fb81dc01091d79d092f298da1e
WilkAndy <andrew.wilkins@csiro.au>
2014-10-27 15:39:55 +1000

    'Cap' smoothing added to TensorMechanicsPlasticTensile, along with a test of this
    Fixes #4139

--- modules/tensor_mechanics/include/userobjects/TensorMechanicsPlasticTensile.h
+++ modules/tensor_mechanics/include/userobjects/TensorMechanicsPlasticTensile.h
@@ -13,8 +13,9 @@ InputParameters validParams<TensorMechanicsPlasticTensile>();
 /**
  * FiniteStrainTensile implements rate-independent associative tensile failure
  * with no hardening/softening in the finite-strain framework.
- * The smoothing of the tip of the yield-surface cone is described in
+ * For 'hyperbolic' smoothing, the smoothing of the tip of the yield-surface cone is described in
  * Zienkiewicz and Prande "Some useful forms of isotropic yield surfaces for soil and rock mechanics" (1977) In G Gudehus (editor) "Finite Elements in Geomechanics" Wile, Chichester, pp 179-190.
+ * For 'cap' smoothing, additional smoothing is performed.
  * The smoothing of the edges of the cone is described in
  * AJ Abbo, AV Lyamin, SW Sloan, JP Hambleton "A C2 continuous approximation to the Mohr-Coulomb yield surface" International Journal of Solids and Structures 48 (2011) 3001-3010
  */
@@ -73,8 +74,24 @@ class TensorMechanicsPlasticTensile : public TensorMechanicsPlasticModel
 
  protected:
 
+  /**
+   * The yield function is modified to
+   * f = s_m + sqrt(a + s_bar^2 K^2) - tensile_strength
+   * where "a" depends on the tip_scheme.  Currently _tip_scheme is
+   * 'hyperbolic', where a = _small_smoother2
+   * 'cap' where a = _small_smoother2 + (p(stress_mean - _cap_start))^2
+   *       with the function p(x)=x(1-exp(-_cap_rate*x)) for x>0, and p=0 otherwise
+   */
+  MooseEnum _tip_scheme;
+
   /// Square of tip smoothing parameter to smooth the cone at mean_stress = T
   Real _small_smoother2;
+
+  /// smoothing parameter dictating when the 'cap' will start - see doco for _tip_scheme
+  Real _cap_start;
+
+  /// dictates how quickly the 'cap' degenerates to a hemisphere - see doco for _tip_scheme
+  Real _cap_rate;
 
   /// edge smoothing parameter, in radians
   Real _tt;
@@ -93,6 +110,15 @@ class TensorMechanicsPlasticTensile : public TensorMechanicsPlasticModel
 
   /// Abbo et al's A parameter
   Real _aaa;
+
+  /// returns the 'a' parameter - see doco for _tip_scheme
+  virtual Real smooth(const RankTwoTensor & stress) const;
+
+  /// returns the da/dstress_mean - see doco for _tip_scheme
+  virtual Real dsmooth(const RankTwoTensor & stress) const;
+
+  /// returns the d^2a/dstress_mean^2 - see doco for _tip_scheme
+  virtual Real d2smooth(const RankTwoTensor & stress) const;
 
   /// tensile strength as a function of residual value, rate, and internal_param
   virtual Real tensile_strength(const Real internal_param) const;
