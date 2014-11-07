030327e8beffb24165f806a3e59fbb33f6d2c08e
Andrew E Slaughter <andrew.slaughter@inl.gov>
2014-11-07 09:35:38 -0800

    Setting up auto exec. locator

--- gui/src/execute/ExecuteWidget.py
+++ gui/src/execute/ExecuteWidget.py
@@ -19,13 +19,8 @@ class ExecuteWidget(QtGui.QWidget, MooseWidget):
     self.addObject(ExecuteCommandControl(**kwargs), handle='ExecuteCommandControl')
     self.addObject(ExecuteWidgetConsole(**kwargs), handle='ExecuteConsole')
 
-    # Create the menu items
+    # Create the 'execute' menu items
     self.addObject(QtGui.QMenu(), handle='ExecuteMenu')
-
-    #### DEMO: One method for adding a menu action ####
-    # I would rather see each action added in the menu setup
-    #self.addObject(QtGui.QAction('&'+'Select executable', self),
-    #               handle='SelectMenuAction', parent='ExecuteMenu')
 
     # Connect the 'Run" button to the execute method
     self.connectSignal('run', self.execute)
@@ -33,68 +28,45 @@ class ExecuteWidget(QtGui.QWidget, MooseWidget):
     # Perform the setup for this object
     self.setup()
 
+
   ##
   # Method for running an executable and passing the output to the Console widget
-  # @param command The program to run
-  def execute(self, cmd):
+  # @param cmd The program to run
+  # @param args The arguments to pass to the program
+  def execute(self, cmd, args):
 
-    cmd = '/Users/slauae/projects/moose/test/moose_test-oprof'
-    args = ['-i', '/Users/slauae/projects/moose/test/tests/kernels/simple_transient_diffusion/simple_transient_diffusion.i', 'Mesh/uniform_refine=1']
+#    cmd = '/Users/slauae/projects/moose/test/moose_test-oprof'
+#    args = ['-i', '/Users/slauae/projects/moose/test/tests/kernels/simple_transient_diffusion/simple_transient_diffusion.i', 'Mesh/uniform_refine=1']
 
-    # Execute the command
-    self._debug('Command: ' + cmd)
+    # A debugging message
+    self._debug('Running Command: ' + cmd)
 
-    self.runProcess = QtCore.QProcess(self)
+    # Create the process object where the command will be executed
+    process = QtCore.QProcess(self)
 
-   # pwd = os.getcwd()
-   # os.chdir(os.path.dirname(cmd))
-   # self.runProcess.setWorkingDirectory(os.path.dirname(cmd))
-
+    # Get the object where the console information will be displayed and connect the process that will
+    # display with the standard out/error
     console_obj = self.object('ExecuteConsole')
-    self.runProcess.readyReadStandardOutput.connect(lambda: console_obj.updateConsole(self.runProcess))
+    process.readyReadStandardOutput.connect(lambda: console_obj.updateConsole(self.runProcess))
+    process.readyReadStandardError.connect(lambda: console_obj.updateConsole(self.runProcess))
 
-    self.runProcess.start('/Users/slauae/projects/moose/test/moose_test-oprof', args)
+    # Run the command with supplied arguments
+    process.start('/Users/slauae/projects/moose/test/moose_test-oprof', args)
 
-
-
-
-  def writeOutput(self):
-    while self.runProcess.canReadLine():
-         #   if self.currentProcess is None:
-         #       break
-
-      text = self.runProcess.readLine()
-      print text,
-
-#output = process.stdout.read()
-#    print output
-
-    #proc = QtCore.QProcess(self.object('Console'))
-    #proc.setProcessChannelMode(QtCore.QProcess.MergedChannels)
-    #proc.start(cmd)
-
-    # Connect the Console (display) to the process
-    #self.object('Console').connect(proc, QtCore.SIGNAL("readyReadStandardOutput()"),
-    #                               lambda: self.object('ExecuteConsole').updateConsole(proc))
-
-#private:
 
   ##
   # Setup the execute menu
-  #def _setupExecuteMenu(self, q_object):
-  #  q_object.setTitle('Execute')
-  #
-  #  # Add 'Run' menu item
-  #  action = QtGui.QAction('&'+'Run', self)
-  #  action.triggered.connect(self.callback('Run'))
-  #  action.setShortcut(QtGui.QKeySequence(QtCore.Qt.CTRL + QtCore.Qt.Key_R))
-  #  q_object.addAction(action)
-  #
-  #  # Add 'Select' menu item
-  #  #### I think we should do the same for 'Select' rather how it was done above
+  def _setupExecuteMenu(self, q_object):
+    q_object.setTitle('Execute')
 
-  ##
-  # Add the 'Select' menu action
-  #def _setupSelectMenuAction(self, q_object):
-  #  q_object.triggered.connect(self.callback('Select'))
-  #  q_object.setShortcut(QtGui.QKeySequence(QtCore.Qt.CTRL + QtCore.Qt.Key_E))
+    # Add 'Run' menu item
+    action = QtGui.QAction('&'+'Run', self)
+    action.triggered.connect(self.execute)
+    action.setShortcut(QtGui.QKeySequence(QtCore.Qt.CTRL + QtCore.Qt.Key_R))
+    q_object.addAction(action)
+
+    # Add 'Select' menu item
+    action = QtGui.QAction('&'+'Select', self)
+    action.triggered.connect(self.callback('Select'))
+    action.setShortcut(QtGui.QKeySequence(QtCore.Qt.CTRL + QtCore.Qt.Key_F))
+    q_object.addAction(action)
