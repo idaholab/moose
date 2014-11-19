79c49a41438f83c8ab364a8e6a2383ea0c593979
Andrew E Slaughter <andrew.slaughter@inl.gov>
2014-11-19 13:36:44 -0700

    Execute widget testing done

--- gui/tests/execute/ExecuteWidget.py
+++ gui/tests/execute/ExecuteWidget.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import os, sys
+import os, sys, time
 from PySide import QtGui
 
 import src.execute
@@ -7,7 +7,7 @@ from src.utils import TestObject
 
 # Create the application
 main = QtGui.QMainWindow()
-test = src.execute.ExecuteWidget(main=main, testing=True, debug=False, wait=True)
+test = src.execute.ExecuteWidget(main=main, testing=True, debug=False)
 
 class ExecuteWidget(TestObject):
   def __init__(self, **kwargs):
@@ -21,9 +21,11 @@ class ExecuteWidget(TestObject):
   # Test executing a command
   def testRun(self):
     test.object('Executable').setText(self.test_app)
+    test.object('Arguments').setText('')
     run = test.callback('Run')
     test.clear() # clear the console output
     run()
+    test.process.waitForFinished()
     result = test.object('Console').toPlainText() == 'Testing'
     test.clear() # clear the console output
     return (result, 'Failed to run test application')
@@ -35,13 +37,36 @@ class ExecuteWidget(TestObject):
     run = test.callback('Run')
     test.clear() # clear the console output
     run()
+    test.process.waitForFinished()
     result = test.object('Console').toPlainText() == 'Testing with flag'
     test.clear() # clear the console output
     return (result, 'Failed to run test application')
 
-  # Tests the clear button
+  # Tests the clear method via the callback
   def testClear(self):
     test.object('Console').setText('Remove This')
-    test.clear()
+    callback = test.callback('ClearConsole')
+    callback()
     result = test.object('Console').toPlainText() == ''
     return (result, 'Failed to clear text')
+
+  # Tests the kill method via the callback
+  def testKill(self):
+    # Setup the executeable with the --kill flag
+    test.object('Arguments').setText('--kill')
+    test.object('Executable').setText(self.test_app)
+
+    # Get the run/kill function callbacks
+    run = test.callback('Run')
+    kill = test.callback('KillConsole')
+
+    # Clear the console run and kill the process
+    test.clear() # clear the console output
+    run()
+    kill()
+
+    # If the process was killed there should be nothing printed to the console
+    test.process.waitForFinished()
+    result = test.object('Console').toPlainText() == ''
+    test.clear()
+    return (result, 'Failed to kill application')
