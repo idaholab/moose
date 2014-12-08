9f475c0b9af5e21b7eaae6abc7f63f65ce0d5ad2
Daniel Schwen <daniel.schwen@inl.gov>
2014-12-08 08:23:17 -0700

    Add switching and barrier function materials (#4355)

---
+++ modules/phase_field/src/materials/SwitchingFunctionMaterial.C
@@ -0,0 +1,42 @@
+#include "SwitchingFunctionMaterial.h"
+
+template<>
+InputParameters validParams<SwitchingFunctionMaterial>()
+{
+  InputParameters params = validParams<OrderParameterFunction>();
+  params.addClassDescription("Helper material to provide h(eta) and its derivative in one of two polynomial forms.\nSIMPLE: 3*eta^2-2*eta^3\nHIGH: eta^3*(6*eta^2-15*eta+10)");
+  MooseEnum h_order("SIMPLE HIGH");
+  params.addParam<MooseEnum>("h_order", h_order, "Polynomial order of the switching function h(eta)");
+  params.set<std::string>("function_name") = std::string("h");
+  return params;
+}
+
+SwitchingFunctionMaterial::SwitchingFunctionMaterial(const std::string & name,
+                                         InputParameters parameters) :
+    OrderParameterFunction(name, parameters),
+    _h_order(getParam<MooseEnum>("h_order"))
+{
+}
+
+void
+SwitchingFunctionMaterial::computeQpProperties()
+{
+  if (_h_order == 0) // SIMPLE
+  {
+    _prop_f[_qp] =   3.0 * _eta[_qp]*_eta[_qp]
+                   - 2.0 * _eta[_qp]*_eta[_qp]*_eta[_qp];
+    _prop_df[_qp] =  6.0 * _eta[_qp]
+                   - 6.0 * _eta[_qp]*_eta[_qp];
+    _prop_d2f[_qp] =  6.0 - 12.0 * _eta[_qp];
+  }
+  else if (_h_order == 1) // HIGH
+  {
+    _prop_f[_qp] =    _eta[_qp]*_eta[_qp]*_eta[_qp]
+                    * (6.0 * _eta[_qp]*_eta[_qp] - 15.0 * _eta[_qp] + 10.0);
+    _prop_df[_qp] =   30.0 * _eta[_qp]*_eta[_qp]
+                    * (_eta[_qp]*_eta[_qp] - 2.0 * _eta[_qp] + 1.0);
+    _prop_d2f[_qp] = _eta[_qp] * (120.0 * _eta[_qp]*_eta[_qp] - 180.0 * _eta[_qp] + 60.0);
+  }
+  else
+    mooseError("Internal error");
+}
