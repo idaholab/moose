3e411059a8ce2e6ba7b2e0b16161409ec87e2bbb
Derek Gaston <derek.gaston@inl.gov>
2015-01-16 09:13:57 -0700

    add step3 refs #4522

---
+++ tutorials/darcy_thermo_mech/step3_darcy_material/problems/step3.i
@@ -0,0 +1,63 @@
+[Mesh]
+  type = GeneratedMesh
+  dim = 2
+  nx = 100
+  ny = 10
+  xmax = 0.304 # Length of test chamber
+  ymax = 0.0257 # Half inner diameter of test chamber
+[]
+
+[Variables]
+  [./pressure]
+  [../]
+[]
+
+[Kernels]
+  [./darcy_pressure]
+    type = DarcyPressure
+    variable = pressure
+    viscosity = 7.98e-4 # (Pa*s) Water at 30 degrees C (Wikipedia)
+    permeability = 0.8451e-9 # (m^2) 1mm balls.  From paper
+  [../]
+[]
+
+[BCs]
+  [./inlet]
+    type = DirichletBC
+    variable = pressure
+    boundary = left
+    value = 205778.43 # (Pa) From Figure 2 from paper.  First dot for 1mm balls.
+  [../]
+  [./outlet]
+    type = DirichletBC
+    variable = pressure
+    boundary = right
+    value = 0 # (Pa) Gives the correct pressure drop from Figure 2 for 1mm balls
+  [../]
+[]
+
+[Materials]
+  [./column]
+    type = PackedColumn
+    block = 0
+    ball_radius = 1
+  [../]
+[]
+
+[Executioner]
+  type = Steady
+  solve_type = PJFNK
+  petsc_options_iname = '-pc_type -pc_hypre_type'
+  petsc_options_value = 'hypre boomeramg'
+[]
+
+[Outputs]
+  output_initial = true
+  exodus = true
+  [./console]
+    type = Console
+    perf_log = true
+    linear_residuals = true
+  [../]
+[]
+
---
+++ tutorials/darcy_thermo_mech/step3_darcy_material/src/base/DarcyThermoMechApp.C
@@ -0,0 +1,56 @@
+#include "DarcyThermoMechApp.h"
+#include "Moose.h"
+#include "AppFactory.h"
+#include "ModulesApp.h"
+
+// Kernels
+#include "DarcyPressure.h"
+
+// Materials
+#include "PackedColumn.h"
+
+template<>
+InputParameters validParams<DarcyThermoMechApp>()
+{
+  InputParameters params = validParams<MooseApp>();
+
+  params.set<bool>("use_legacy_uo_initialization") = false;
+  params.set<bool>("use_legacy_uo_aux_computation") = false;
+  return params;
+}
+
+DarcyThermoMechApp::DarcyThermoMechApp(const std::string & name, InputParameters parameters) :
+    MooseApp(name, parameters)
+{
+  srand(processor_id());
+
+  Moose::registerObjects(_factory);
+  ModulesApp::registerObjects(_factory);
+  DarcyThermoMechApp::registerObjects(_factory);
+
+  Moose::associateSyntax(_syntax, _action_factory);
+  ModulesApp::associateSyntax(_syntax, _action_factory);
+  DarcyThermoMechApp::associateSyntax(_syntax, _action_factory);
+}
+
+DarcyThermoMechApp::~DarcyThermoMechApp()
+{
+}
+
+void
+DarcyThermoMechApp::registerApps()
+{
+  registerApp(DarcyThermoMechApp);
+}
+
+void
+DarcyThermoMechApp::registerObjects(Factory & factory)
+{
+  registerKernel(DarcyPressure);
+  registerMaterial(PackedColumn);
+}
+
+void
+DarcyThermoMechApp::associateSyntax(Syntax & syntax, ActionFactory & action_factory)
+{
+}
