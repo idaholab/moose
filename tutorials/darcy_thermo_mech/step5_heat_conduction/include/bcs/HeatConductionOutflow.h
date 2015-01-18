a7faae22cfb2318e7a04c2fbd5b640b1d4cdd158
Derek Gaston <derek.gaston@inl.gov>
2015-01-18 12:36:52 -0700

    beginning of step 5 refs #4522

---
+++ tutorials/darcy_thermo_mech/step5_heat_conduction/include/bcs/HeatConductionOutflow.h
@@ -0,0 +1,51 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#ifndef HEATCONDUCTIONOUTFLOW_H
+#define HEATCONDUCTIONOUTFLOW_H
+
+#include "IntegratedBC.h"
+
+
+class HeatConductionOutflow;
+
+template<>
+InputParameters validParams<HeatConductionOutflow>();
+
+/**
+ * An IntegratedBC representing the "No BC" boundary condition for Heat Conduction.
+ *
+ * This is essentially -test*k*grad_u*normal... essentially completing the integration by parts.
+ * This is a well accepted practice for truncating longer domains for convection/diffusion
+ * problems as analyzed in: Griffiths, David F. "The ‘no boundary condition’outflow boundary condition." International journal for numerical methods in fluids 24.4 (1997): 393-411.
+ */
+class HeatConductionOutflow : public IntegratedBC
+{
+public:
+  HeatConductionOutflow(const std::string & name, InputParameters parameters);
+
+
+protected:
+  /// This is called to integrate the residual across the boundary
+  virtual Real computeQpResidual();
+
+  /// Optional (but recommended!) to compute the derivative of the residual with respect to _this_ variable
+  virtual Real computeQpJacobian();
+
+  /// Thermal conductivity of the material
+  MaterialProperty<Real> & _thermal_conductivity;
+};
+
+
+#endif //HEATCONDUCTIONOUTFLOW_H
