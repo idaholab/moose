f2f92375ec44f91fdf066971cecbd65e06aff2a4
John Peterson <jwpeterson@gmail.com>
2015-01-21 13:59:25 -0700

    Implement computeQpOffDiagJacobian() for MatchedValueBC.
    This test can now use Newton and converge in a single nonlinear step,
    rather than relying on PJFNK to get the correct action of the
    Jacobian.
    
    Refs #656.

--- framework/src/bcs/MatchedValueBC.C
+++ framework/src/bcs/MatchedValueBC.C
@@ -18,18 +18,28 @@ template<>
 InputParameters validParams<MatchedValueBC>()
 {
   InputParameters params = validParams<NodalBC>();
-  params.addRequiredCoupledVar("v", "");
+  params.addRequiredCoupledVar("v", "The variable whose value we are to match.");
   return params;
 }
 
 MatchedValueBC::MatchedValueBC(const std::string & name, InputParameters parameters) :
     NodalBC(name, parameters),
-    _v_face(coupledValue("v"))
+    _v(coupledValue("v")),
+    _v_num(coupled("v"))
 {
 }
 
 Real
 MatchedValueBC::computeQpResidual()
 {
-  return _u[_qp]-_v_face[_qp];
+  return _u[_qp] - _v[_qp];
+}
+
+Real
+MatchedValueBC::computeQpOffDiagJacobian(unsigned int jvar)
+{
+  if (jvar == _v_num)
+    return -1.0;
+  else
+    return 0.;
 }
