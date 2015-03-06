81477101fd9db70fef833d563bd0b11b128a734d
Daniel Schwen <daniel.schwen@inl.gov>
2015-03-06 13:33:12 -0700

    Address Cody's comments (#4708)

--- framework/src/utils/FunctionParserUtils.C
+++ framework/src/utils/FunctionParserUtils.C
@@ -1,3 +1,17 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
 #include "FunctionParserUtils.h"
 
 template<>
@@ -24,7 +38,7 @@ const char * FunctionParserUtils::_eval_error_msg[] = {
 };
 
 FunctionParserUtils::FunctionParserUtils(const std::string & name,
-                                          InputParameters parameters) :
+                                         InputParameters parameters) :
     _enable_jit(parameters.isParamValid("enable_jit") &&
                 parameters.get<bool>("enable_jit")),
     _disable_fpoptimizer(parameters.get<bool>("disable_fpoptimizer")),
