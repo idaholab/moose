adc1c759335ec0da9f0823b3fcea4b2a6a9d0304
Shane Stafford <shane.stafford@inl.gov>
2015-03-27 13:36:38 -0600

    Fixes issues with #4851 (refs #4850).
    The 4ElemTensionRelease_mechanical_constraint test passed with the
    changes in #4851 only if the Predictor was turned off. This change
    fixes that issue and reverts that test's input file.

--- modules/contact/include/MechanicalContactConstraint.h
+++ modules/contact/include/MechanicalContactConstraint.h
@@ -110,9 +110,10 @@ protected:
   RealVectorValue _vars;
 
   MooseVariable * _nodal_area_var;
-  SystemBase & _aux_system;
+  SystemBase & _area_aux_system;
 
   libMesh::VectorValue<unsigned> _contact_force_var;
+  SystemBase & _pinfo_aux_system;
 
   /// Whether to include coupling between the master and slave nodes in the Jacobian
   const bool _master_slave_jacobian;
