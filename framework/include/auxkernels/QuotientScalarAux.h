b1c8af7cce708c3306b27eb5b70a877988968e75
David Andrs <david.andrs@inl.gov>
2015-04-14 09:51:13 -0600

    Adding QuotientScalarAux

---
+++ framework/include/auxkernels/QuotientScalarAux.h
@@ -0,0 +1,43 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#ifndef QUOTIENTSCALARAUX_H
+#define QUOTIENTSCALARAUX_H
+
+#include "AuxScalarKernel.h"
+
+class QuotientScalarAux;
+
+template<>
+InputParameters validParams<QuotientScalarAux>();
+
+/**
+ * This auxiliary kernel computes its value by dividing "numerator" by
+ * "denominator.  For efficiency, it doesn't check the denominator for
+ * zero before dividing.
+ */
+class QuotientScalarAux : public AuxScalarKernel
+{
+public:
+  QuotientScalarAux(const std::string & name, InputParameters parameters);
+  virtual ~QuotientScalarAux();
+
+protected:
+  virtual Real computeValue();
+
+  VariableValue & _a;
+  VariableValue & _b;
+};
+
+#endif /* QUOTIENTSCALARAUX_H */
---
+++ framework/src/auxkernels/QuotientScalarAux.C
@@ -0,0 +1,42 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#include "QuotientScalarAux.h"
+
+template<>
+InputParameters validParams<QuotientScalarAux>()
+{
+  InputParameters params = validParams<AuxScalarKernel>();
+  params.addCoupledVar("numerator", "The upstairs of the quotient variable");
+  params.addCoupledVar("denominator", "The downstairs of the quotient variable");
+
+  return params;
+}
+
+QuotientScalarAux::QuotientScalarAux(const std::string & name, InputParameters parameters) :
+    AuxScalarKernel(name, parameters),
+    _a(coupledScalarValue("numerator")),
+    _b(coupledScalarValue("denominator"))
+{
+}
+
+QuotientScalarAux::~QuotientScalarAux()
+{
+}
+
+Real
+QuotientScalarAux::computeValue()
+{
+  return _a[0] / _b[0];
+}
