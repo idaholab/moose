d8b7b673c91e014192a195a22c7755b106986dcb
Cody Permann <codypermann@gmail.com>
2015-05-20 11:24:59 -0600

    Water Steam EOS no longer requires legacy flags refs #5086

--- modules/water_steam_eos/src/base/WaterSteamEOSApp.C
+++ modules/water_steam_eos/src/base/WaterSteamEOSApp.C
@@ -12,7 +12,7 @@ template<>
 InputParameters validParams<WaterSteamEOSApp>()
 {
   InputParameters params = validParams<MooseApp>();
-  params.set<bool>("use_legacy_uo_initialization") = true;
+  params.set<bool>("use_legacy_uo_initialization") = false;
   params.set<bool>("use_legacy_uo_aux_computation") = false;
 
   return params;
