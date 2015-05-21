8cbfb62ce2022c2db9b565de91608511f46d06f7
Cody Permann <codypermann@gmail.com>
2015-05-21 11:24:25 -0600

    Turning off legacy flags in all Examples refs #5086

--- examples/ex10_aux/src/base/ExampleApp.C
+++ examples/ex10_aux/src/base/ExampleApp.C
@@ -23,6 +23,9 @@ template<>
 InputParameters validParams<ExampleApp>()
 {
   InputParameters params = validParams<MooseApp>();
+
+  params.set<bool>("use_legacy_uo_initialization") = false;
+  params.set<bool>("use_legacy_uo_aux_computation") = false;
   return params;
 }
 
