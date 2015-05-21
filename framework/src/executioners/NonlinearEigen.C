41fea475f7c2b9c8703d02bfedb53d26d4f8af35
Yaqi Wang <Yaqi.Wang@inl.gov>
2015-05-21 17:27:56 -0600

    Small fixes from Yaqi in support of YAK (refs #4606)

--- framework/src/executioners/NonlinearEigen.C
+++ framework/src/executioners/NonlinearEigen.C
@@ -37,7 +37,7 @@ NonlinearEigen::NonlinearEigen(const std::string & name, InputParameters paramet
      _output_after_pi(getParam<bool>("output_after_power_iterations"))
 {
   _eigenvalue = getParam<Real>("k0");
-  addAttributeReporter("eigenvalue", _eigenvalue);
+  addAttributeReporter("eigenvalue", _eigenvalue, "initial timestep_end");
 }
 
 void
