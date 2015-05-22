7933294e1c0d7537f8c09e30d01c154d587b27b2
Andrew E Slaughter <andrew.slaughter@inl.gov>
2015-05-22 09:27:29 -0600

    Added tests for setting of default values in Receiver (refs #5106)

--- test/src/base/MooseTestApp.C
+++ test/src/base/MooseTestApp.C
@@ -168,7 +168,7 @@
 #include "NumElemQPs.h"
 #include "NumSideQPs.h"
 #include "ElementL2Diff.h"
-#include "GrowPP.h"
+#include "TestPostprocessor.h"
 
 // Functions
 #include "TimestepSetupFunction.h"
@@ -430,7 +430,7 @@ MooseTestApp::registerObjects(Factory & factory)
   registerPostprocessor(NumElemQPs);
   registerPostprocessor(NumSideQPs);
   registerPostprocessor(ElementL2Diff);
-  registerPostprocessor(GrowPP);
+  registerPostprocessor(TestPostprocessor);
 
   registerMarker(RandomHitMarker);
   registerMarker(QPointMarker);
