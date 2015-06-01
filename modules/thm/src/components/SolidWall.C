4f2c1c4e86b9d83eb0fa8ea06e9ae600e2286202
David Andrs <david.andrs@inl.gov>
2015-06-01 10:57:09 -0600

    Updating to use new fluid properties system

--- modules/thm/src/components/SolidWall.C
+++ modules/thm/src/components/SolidWall.C
@@ -32,7 +32,7 @@ SolidWall::addMooseObjects1Phase()
   std::vector<unsigned int> bnd_id(1, getBoundaryId());
   {
     InputParameters params = _factory.getValidParams("DirichletBC");
-    params.set<NonlinearVariableName>("variable") = RHOUA;
+    params.set<NonlinearVariableName>("variable") = FlowModel::RHOUA;
     params.set<std::vector<unsigned int> >("r7:boundary") = bnd_id;
     params.set<Real>("value") = 0.;
     _sim.addBoundaryCondition("DirichletBC", genName(name(), "rhou"), params);
@@ -45,14 +45,14 @@ SolidWall::addMooseObjects2Phase()
   std::vector<unsigned int> bnd_id(1, getBoundaryId());
   {
     InputParameters params = _factory.getValidParams("DirichletBC");
-    params.set<NonlinearVariableName>("variable") = ALPHA_RHOU_A_LIQUID;
+    params.set<NonlinearVariableName>("variable") = FlowModel::ALPHA_RHOU_A_LIQUID;
     params.set<std::vector<unsigned int> >("r7:boundary") = bnd_id;
     params.set<Real>("value") = 0.;
     _sim.addBoundaryCondition("DirichletBC", genName(name(), "alpha_rhou_A_liquid"), params);
   }
   {
     InputParameters params = _factory.getValidParams("DirichletBC");
-    params.set<NonlinearVariableName>("variable") = ALPHA_RHOU_A_VAPOR;
+    params.set<NonlinearVariableName>("variable") = FlowModel::ALPHA_RHOU_A_VAPOR;
     params.set<std::vector<unsigned int> >("r7:boundary") = bnd_id;
     params.set<Real>("value") = 0.;
     _sim.addBoundaryCondition("DirichletBC", genName(name(), "alpha_rhou_A_vapor"), params);
@@ -62,8 +62,10 @@ SolidWall::addMooseObjects2Phase()
 void
 SolidWall::addMooseObjects()
 {
-  if (isSinglePhase())
+  if (_model_type == FlowModel::EQ_MODEL_2 ||
+      _model_type == FlowModel::EQ_MODEL_3 ||
+      _model_type == FlowModel::EQ_MODEL_HEM)
     addMooseObjects1Phase();
-  else if (isTwoPhase())
+  else if (_model_type == FlowModel::EQ_MODEL_7)
     addMooseObjects2Phase();
 }
