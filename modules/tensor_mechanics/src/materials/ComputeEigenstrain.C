ff398f8a5f2d7028b0bd972a415deb20d3499233
Daniel Schwen <daniel.schwen@inl.gov>
2015-06-08 17:25:45 -0600

    Refactor Compute1PhaseEigenStrain (#5194)

---
+++ modules/tensor_mechanics/src/materials/ComputeEigenstrain.C
@@ -0,0 +1,33 @@
+/****************************************************************/
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*          All contents are licensed under LGPL V2.1           */
+/*             See LICENSE for full restrictions                */
+/****************************************************************/
+#include "ComputeEigenstrain.h"
+
+template<>
+InputParameters validParams<ComputeEigenstrain>()
+{
+  InputParameters params = validParams<ComputeStressFreeStrainBase>();
+  params.addClassDescription("Computes an Eigenstrain and its derivatives that is a function of one variable, where the scaler dependence is defined in a derivative material.");
+  params.addRequiredParam<std::vector<Real> >("eigen_base","Vector of values defining the constant base tensor for the Eigenstrain");
+  params.addParam<std::string>("prefactor", "var_dep", "Name of material defining the variable dependence");
+  return params;
+}
+
+ComputeEigenstrain::ComputeEigenstrain(const std::string & name,
+                                       InputParameters parameters) :
+    ComputeStressFreeStrainBase(name, parameters),
+    _prefactor_name(getParam<std::string>("prefactor")),
+    _prefactor(getMaterialProperty<Real>(_prefactor_name))
+{
+  _eigen_base_tensor.fillFromInputVector(getParam<std::vector<Real> >("eigen_base"));
+}
+
+void
+ComputeEigenstrain::computeQpStressFreeStrain()
+{
+  //Define Eigenstrain
+  _stress_free_strain[_qp] = _eigen_base_tensor * _prefactor[_qp];
+}
