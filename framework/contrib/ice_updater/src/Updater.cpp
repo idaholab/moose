93eb35472391d4fa065f75dfaec9a3098fa98c35
Alex McCaskey <mccaskeyaj@ornl.gov>
2015-06-22 09:08:47 -0500

    Updating the license to BSD 3 clause.
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- framework/contrib/ice_updater/src/Updater.cpp
+++ framework/contrib/ice_updater/src/Updater.cpp
@@ -1,16 +1,34 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2014 UT-Battelle, LLC.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (c) 2015, UT-Battelle, LLC.
+ * All rights reserved.
  *
- * Contributors:
- *   Initial API and implementation and/or initial documentation - Jay Jay Billings,
- *   Jordan H. Deyton, Dasha Gorin, Alexander J. McCaskey, Taylor Patterson,
- *   Claire Saunders, Matthew Wang, Anna Wojtowicz
- *******************************************************************************/
-
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+*******************************************************************************/
 #include "Updater.h"
 #include <fstream>
 #include <stdlib.h>
@@ -260,7 +278,6 @@ bool Updater::stop() {
 		//Pause this thread for sleepTime seconds
 		tbb::this_tbb_thread::sleep(tbb::tick_count::interval_t(1.0));
 
-		Moose::out << "Stopping the thread.\n";
 		// Stop the thread
 		updaterThread->stopThread();
 
@@ -361,7 +378,6 @@ tbb::task * UpdaterThread::execute() {
 
 	}
 
-	std::cout << "Stopping the Updater Thread.\n";
 	return NULL;
 }
 
