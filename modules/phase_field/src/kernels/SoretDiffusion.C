f9c76b23edc15aa98445c500202aea57b0859f25
Michael Tonks <michael.tonks@inl.gov>
2015-06-30 14:01:44 -0600

    Changed SplitCHSoret to SoretDiffusion and made split and direct tests for #5324

--- modules/phase_field/src/kernels/SoretDiffusion.C
+++ modules/phase_field/src/kernels/SoretDiffusion.C
@@ -4,9 +4,9 @@
 /*          All contents are licensed under LGPL V2.1           */
 /*             See LICENSE for full restrictions                */
 /****************************************************************/
-#include "SplitCHSoret.h"
+#include "SoretDiffusion.h"
 template<>
-InputParameters validParams<SplitCHSoret>()
+InputParameters validParams<SoretDiffusion>()
 {
   InputParameters params = validParams<Kernel>();
   params.addClassDescription("Add Soret effect to Split formulation Cahn-Hilliard Kernel");
@@ -17,7 +17,7 @@ InputParameters validParams<SplitCHSoret>()
   return params;
 }
 
-SplitCHSoret::SplitCHSoret(const std::string & name, InputParameters parameters) :
+SoretDiffusion::SoretDiffusion(const std::string & name, InputParameters parameters) :
     Kernel(name, parameters),
     _T_var(coupled("T")),
     _T(coupledValue("T")),
@@ -31,7 +31,7 @@ SplitCHSoret::SplitCHSoret(const std::string & name, InputParameters parameters)
 }
 
 Real
-SplitCHSoret::computeQpResidual()
+SoretDiffusion::computeQpResidual()
 {
   Real T_term = _D[_qp] * _Q[_qp] * _c[_qp] / (_kb * _T[_qp] * _T[_qp]);
 
@@ -39,13 +39,29 @@ SplitCHSoret::computeQpResidual()
 }
 
 Real
-SplitCHSoret::computeQpOffDiagJacobian(unsigned int jvar)
+SoretDiffusion::computeQpJacobian()
 {
-  if (_c_var == jvar)
-    return _D[_qp] * _Q[_qp] * _phi[_j][_qp] * _grad_T[_qp] / (_kb * _T[_qp] * _T[_qp]) * _grad_test[_i][_qp];
-  else if (_T_var == jvar)
+  if (_c_var == _var.number()) //Requires c jacobian
+    return computeQpCJacobian();
+
+  return 0.0;
+}
+
+Real
+SoretDiffusion::computeQpOffDiagJacobian(unsigned int jvar)
+{
+  if (_c_var == jvar) //Requires c jacobian
+    return computeQpCJacobian();
+  else if (_T_var == jvar) //Requires T jacobian
     return _D[_qp] * _Q[_qp] * _c[_qp] * _grad_test[_i][_qp] *
            (_grad_phi[_j][_qp]/(_kb * _T[_qp] * _T[_qp]) - 2.0 * _grad_T[_qp] * _phi[_j][_qp] / (_kb * _T[_qp] * _T[_qp] * _T[_qp]));
 
   return 0.0;
+}
+
+Real
+SoretDiffusion::computeQpCJacobian()
+{
+  //Calculate the Jacobian for the c variable
+  return _D[_qp] * _Q[_qp] * _phi[_j][_qp] * _grad_T[_qp] / (_kb * _T[_qp] * _T[_qp]) * _grad_test[_i][_qp];
 }
