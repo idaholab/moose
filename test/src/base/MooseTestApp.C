79599027c462d4b33828f017c3690c8a0c0f465f
Andrew E Slaughter <andrew.slaughter@inl.gov>
2015-07-30 14:13:39 -0600

    Use default registration for moose-test
    (refs #4606)

--- test/src/base/MooseTestApp.C
+++ test/src/base/MooseTestApp.C
@@ -246,11 +246,7 @@ extern "C" void MooseTestApp__registerApps() { MooseTestApp::registerApps(); }
 void
 MooseTestApp::registerApps()
 {
-#undef  registerApp
-#define registerApp(name) AppFactory::instance().reg<name>(#name)
   registerApp(MooseTestApp);
-#undef  registerApp
-#define registerApp(name) AppFactory::instance().regLegacy<name>(#name)
 }
 
 // External entry point for dynamic object registration
@@ -258,10 +254,6 @@ extern "C" void MooseTestApp__registerObjects(Factory & factory) { MooseTestApp:
 void
 MooseTestApp::registerObjects(Factory & factory)
 {
-
-#undef registerObject
-#define registerObject(name) factory.reg<name>(stringifyName(name))
-
   // Kernels
   registerKernel(CoupledConvection);
   registerKernel(ForcingFn);
@@ -452,10 +444,6 @@ MooseTestApp::registerObjects(Factory & factory)
 
   // Outputs
   registerOutput(OutputObjectTest);
-
-#undef registerObject
-#define registerObject(name) factory.regLegacy<name>(stringifyName(name))
-
 }
 
 // External entry point for dynamic syntax association
@@ -463,10 +451,6 @@ extern "C" void MooseTestApp__associateSyntax(Syntax & syntax, ActionFactory & a
 void
 MooseTestApp::associateSyntax(Syntax & syntax, ActionFactory & action_factory)
 {
-
-#undef registerAction
-#define registerAction(tplt, action) action_factory.reg<tplt>(stringifyName(tplt), action)
-
   // and add more
   registerAction(ConvDiffMetaAction, "meta_action");
   registerAction(AddLotsOfAuxVariablesAction, "meta_action");
@@ -484,7 +468,4 @@ MooseTestApp::associateSyntax(Syntax & syntax, ActionFactory & action_factory)
   syntax.registerActionSyntax("ApplyCoupledVariablesTestAction", "ApplyInputParametersTest");
 
   syntax.registerActionSyntax("AddLotsOfDiffusion", "Testing/LotsOfDiffusion/*");
-
-#undef registerAction
-#define registerAction(tplt, action) action_factory.regLegacy<tplt>(stringifyName(tplt), action)
 }
