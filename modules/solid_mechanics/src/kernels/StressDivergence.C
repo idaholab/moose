a347103669e62c6aeacf18064b274db7411181d8
sveerara <swetha.veeraraghavan@inl.gov>
2015-08-11 15:28:38 -0600

    Rayleigh damping and HHT time integration added to Solid Mechanics modules StressDivergence.C and SolidMechInertialForce.C (#5537)

--- modules/solid_mechanics/src/kernels/StressDivergence.C
+++ modules/solid_mechanics/src/kernels/StressDivergence.C
@@ -18,6 +18,8 @@ InputParameters validParams<StressDivergence>()
   params.addCoupledVar("disp_y", "The y displacement");
   params.addCoupledVar("disp_z", "The z displacement");
   params.addCoupledVar("temp", "The temperature");
+  params.addParam<Real>("zeta", 0, "zeta parameter");
+  params.addParam<Real>("alpha", 0, "zeta parameter");
   params.addParam<std::string>("appended_property_name", "", "Name appended to material properties to make them unique");
 
   params.set<bool>("use_displaced_mesh") = true;
@@ -28,6 +30,7 @@ InputParameters validParams<StressDivergence>()
 
 StressDivergence::StressDivergence(const InputParameters & parameters)
   :Kernel(parameters),
+   _stress_old(getMaterialPropertyOld<SymmTensor>("stress" + getParam<std::string>("appended_property_name"))),
    _stress(getMaterialProperty<SymmTensor>("stress" + getParam<std::string>("appended_property_name"))),
    _Jacobian_mult(getMaterialProperty<SymmElasticityTensor>("Jacobian_mult" + getParam<std::string>("appended_property_name"))),
    _d_stress_dT(getMaterialProperty<SymmTensor>("d_stress_dT"+ getParam<std::string>("appended_property_name"))),
@@ -39,19 +42,35 @@ StressDivergence::StressDivergence(const InputParameters & parameters)
    _xdisp_var(_xdisp_coupled ? coupled("disp_x") : 0),
    _ydisp_var(_ydisp_coupled ? coupled("disp_y") : 0),
    _zdisp_var(_zdisp_coupled ? coupled("disp_z") : 0),
-   _temp_var(_temp_coupled ? coupled("temp") : 0)
+   _temp_var(_temp_coupled ? coupled("temp") : 0),
+   _zeta(getParam<Real>("zeta")),
+   _alpha(getParam<Real>("alpha"))
 {}
 
 Real
 StressDivergence::computeQpResidual()
 {
-  return _stress[_qp].rowDot(_component, _grad_test[_i][_qp]);
+    if((_dt>0) && ((_zeta!=0)||(_alpha!=0)))
+    {
+	  return _stress[_qp].rowDot(_component, _grad_test[_i][_qp])*(1+_alpha+_zeta/_dt)-(_zeta/_dt+_alpha)*_stress_old[_qp].rowDot(_component, _grad_test[_i][_qp]);
+    }
+   else
+    {
+      return _stress[_qp].rowDot(_component, _grad_test[_i][_qp]);
+    }
 }
 
 Real
 StressDivergence::computeQpJacobian()
 {
+  if(_dt>0)
+ {
+  return _Jacobian_mult[_qp].stiffness( _component, _component, _grad_test[_i][_qp], _grad_phi[_j][_qp] )*(1+_alpha+_zeta/_dt);
+  }
+  else
+  {
   return _Jacobian_mult[_qp].stiffness( _component, _component, _grad_test[_i][_qp], _grad_phi[_j][_qp] );
+  }
 }
 
 Real
@@ -79,8 +98,15 @@ StressDivergence::computeQpOffDiagJacobian(unsigned int jvar)
 
   if ( active )
   {
-    return _Jacobian_mult[_qp].stiffness( _component, coupled_component,
+    if(_dt>0)
+    {
+     return _Jacobian_mult[_qp].stiffness( _component, coupled_component, _grad_test[_i][_qp], _grad_phi[_j][_qp] )*(1+_alpha+_zeta/_dt);
+    }
+    else
+    {
+    return  _Jacobian_mult[_qp].stiffness( _component, coupled_component,
                                           _grad_test[_i][_qp], _grad_phi[_j][_qp] );
+    }
   }
 
   if ( _temp_coupled && jvar == _temp_var )
