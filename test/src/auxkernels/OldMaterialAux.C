21ebded36f8ca0f815491907a34b02062066e6cc
Andrew E Slaughter <andrew.slaughter@inl.gov>
2015-08-12 10:40:22 -0600

    AuxKernel getMaterialPropertyOld/Older should return const reference
    (closes #5539)

---
+++ test/src/auxkernels/OldMaterialAux.C
@@ -0,0 +1,36 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#include "OldMaterialAux.h"
+
+template<>
+InputParameters validParams<OldMaterialAux>()
+{
+  InputParameters params = validParams<AuxKernel>();
+  params.addRequiredParam<MaterialPropertyName>("property_name", "The name of the material property to capture old and older values from");
+  return params;
+}
+
+OldMaterialAux::OldMaterialAux(const InputParameters & parameters) :
+    AuxKernel(parameters),
+    _old(getMaterialPropertyOld<Real>("property_name")),
+    _older(getMaterialPropertyOlder<Real>("property_name"))
+{
+}
+
+Real
+OldMaterialAux::computeValue()
+{
+  return _old[_qp] + _older[_qp];
+}
---
+++ test/tests/auxkernels/old_older_material_aux/old_mat_in_aux.i
@@ -0,0 +1,86 @@
+[Mesh]
+  type = GeneratedMesh
+  dim = 2
+  nx = 10
+  ny = 10
+[]
+
+[AuxVariables]
+  [./aux]
+    order = CONSTANT
+    family = MONOMIAL
+  [../]
+[]
+
+[AuxKernels]
+  [./old_mat_aux]
+    type = OldMaterialAux
+    property_name = prop
+    variable = aux
+  [../]
+[]
+
+[Variables]
+  [./u]
+  [../]
+[]
+
+[Functions]
+  [./func]
+    type = ParsedFunction
+    value = t
+  [../]
+[]
+
+[Kernels]
+  [./diff]
+    type = CoefDiffusion
+    variable = u
+    coef = 0.1
+  [../]
+  [./time]
+    type = TimeDerivative
+    variable = u
+  [../]
+[]
+
+[BCs]
+  [./left]
+    type = DirichletBC
+    variable = u
+    boundary = left
+    value = 0
+  [../]
+  [./right]
+    type = DirichletBC
+    variable = u
+    boundary = right
+    value = 1
+  [../]
+[]
+
+[Materials]
+  [./mat]
+    type = GenericFunctionMaterial
+    prop_names = prop
+    prop_values = func
+    block = 0
+    enable_stateful = true
+  [../]
+[]
+
+[Executioner]
+  type = Transient
+  num_steps = 10
+  dt = 0.1
+  solve_type = PJFNK
+  petsc_options_iname = '-pc_type -pc_hypre_type'
+  petsc_options_value = 'hypre boomeramg'
+[]
+
+[Outputs]
+  output_initial = true
+  exodus = true
+  print_linear_residuals = true
+  print_perf_log = true
+[]
