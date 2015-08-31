2b1cae7f46d0fab179246582ccb54baab6d8e4b9
David Andrs <david.andrs@inl.gov>
2015-08-31 13:26:16 -0600

    Unifying MOOSE syntax and RELAP-7 syntax
    The code does not care about the syntax now,, it is either MOOSE's or
    RELAP-7's, no special switch is needed anymore.
    
    Refs #19885

---
+++ modules/thm/include/actions/AddHeatStructureMaterialAction.h
@@ -0,0 +1,17 @@
+#ifndef ADDHEATSTRUCTUREMATERIALACTION_H
+#define ADDHEATSTRUCTUREMATERIALACTION_H
+
+#include "AddUserObjectAction.h"
+
+class AddHeatStructureMaterialAction;
+
+template<>
+InputParameters validParams<AddHeatStructureMaterialAction>();
+
+class AddHeatStructureMaterialAction : public AddUserObjectAction
+{
+public:
+  AddHeatStructureMaterialAction(InputParameters params);
+};
+
+#endif /* ADDHEATSTRUCTUREMATERIALACTION_H */
---
+++ modules/thm/src/actions/AddHeatStructureMaterialAction.C
@@ -0,0 +1,12 @@
+#include "AddHeatStructureMaterialAction.h"
+
+template<>
+InputParameters validParams<AddHeatStructureMaterialAction>()
+{
+  return validParams<AddUserObjectAction>();
+}
+
+AddHeatStructureMaterialAction::AddHeatStructureMaterialAction(InputParameters params) :
+    AddUserObjectAction(params)
+{
+}
