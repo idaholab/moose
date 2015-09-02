fdb8805b5e3ce53ce8979929f4fe7c3421fd384a
amccaskey <mccaskeyaj@ornl.gov>
2015-09-02 15:40:50 -0400

    Adding a check for the number of posts

--- python/TestHarness/testers/ICEUpdaterTester.py
+++ python/TestHarness/testers/ICEUpdaterTester.py
@@ -3,27 +3,26 @@ from util import runCommand
 import os
 import web, threading
 
-urls = ('/ice/update', 'ice_update')
-
-class ice_update:
-    def POST(self):
-      print 'HELLO'
-      data = web.data()
-      print data
+urls = ('/ice/update', 'ICEUpdaterTester')
 
 class ICEUpdaterTester(RunApp, web.application):
-    
+
+  postCounter = 0
+  
   @staticmethod
   def validParams():
     params = RunApp.validParams()
-
+    params.addRequiredParam('nPosts', "The Number of Expected Posts")
     return params
         
-  def __init__(self, name, params):
+  def __init__(self, name=None, params=None):
     RunApp.__init__(self, name, params)
     web.application.__init__(self, urls, globals())
     self.httpThread = threading.Thread(target=self.run)
-  
+    ICEUpdaterTester.postCounter += 1
+    if params is not None:
+       self.nPosts = self.getParam('nPosts')
+
   # This method is called prior to running the test.  It can be used to cleanup files
   # or do other preparations before the tester is run
   def prepare(self):
@@ -44,8 +43,15 @@ class ICEUpdaterTester(RunApp, web.application):
   # processing should happen in this method
   def processResults(self, moose_dir, retcode, options, output):
     self.stop()
-    return RunApp.processResults(self, moose_dir, retcode, options, output)
-
+    if ICEUpdaterTester.postCounter != int(self.nPosts):
+       return ("DID NOT GET CORRECT NUMBER OF POSTS", False)
+    else:
+       return RunApp.processResults(self, moose_dir, retcode, options, output)
+  
   def run(self, port=8080, *middleware):
     func = self.wsgifunc(*middleware)
     return web.httpserver.runsimple(func, ('127.0.0.1', port))
+
+  def POST(self):
+    data = web.data()
+    print data
