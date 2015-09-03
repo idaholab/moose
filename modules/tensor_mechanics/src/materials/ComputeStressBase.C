ec7fd2db6f8d17364ac4b43399d01fdcd877b9eb
sveerara <swetha.veeraraghavan@inl.gov>
2015-09-03 13:11:47 -0600

    Added Rayleigh damping and HHT time integration to Tensor mechanics module #5559

--- modules/tensor_mechanics/src/materials/ComputeStressBase.C
+++ modules/tensor_mechanics/src/materials/ComputeStressBase.C
@@ -12,6 +12,7 @@ InputParameters validParams<ComputeStressBase>()
   InputParameters params = validParams<Material>();
   params.addParam<std::vector<FunctionName> >("initial_stress", "A list of functions describing the initial stress.  If provided, there must be 9 of these, corresponding to the xx, yx, zx, xy, yy, zy, xz, yz, zz components respectively.  If not provided, all components of the initial stress will be zero");
   params.addParam<std::string>("base_name", "Optional parameter that allows the user to define multiple mechanics material systems on the same block, i.e. for multiple phases");
+  params.addParam<bool>("store_stress_old", false, "Parameter which indicates whether the old stress state, required for the HHT time integration scheme and Rayleigh damping, needs to be stored");
   return params;
 }
 
@@ -23,8 +24,17 @@ ComputeStressBase::ComputeStressBase(const InputParameters & parameters) :
     _elastic_strain(declareProperty<RankTwoTensor>(_base_name + "elastic_strain")),
     _elasticity_tensor(getMaterialPropertyByName<ElasticityTensorR4>(_base_name + "elasticity_tensor")),
     _extra_stress(getDefaultMaterialProperty<RankTwoTensor>(_base_name + "extra_stress")),
-    _Jacobian_mult(declareProperty<ElasticityTensorR4>(_base_name + "Jacobian_mult"))
+    _Jacobian_mult(declareProperty<ElasticityTensorR4>(_base_name + "Jacobian_mult")),
+    _store_stress_old(getParam<bool>("store_stress_old"))
 {
+  //Declares old stress and older stress if the parameter _store_stress_old is true. This parameter can be set from the input file using any of the child classes of ComputeStressBase.
+
+  if (_store_stress_old)
+  {
+    declarePropertyOld<RankTwoTensor>(_base_name + "stress");
+    declarePropertyOlder<RankTwoTensor>(_base_name + "stress");
+  }
+
   const std::vector<FunctionName> & fcn_names(getParam<std::vector<FunctionName> >("initial_stress"));
   const unsigned num = fcn_names.size();
 
