7affe0e1935e599af869842b4059bb2d74ca5471
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2015-09-16 16:51:41 -0600

    Python 3 compatability.

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,6 +1,10 @@
 import sys
 
-import crow_modules.distribution1Dpy2
-import crow_modules.interpolationNDpy2
+if sys.version_info.major > 2:
+  import crow_modules.distribution1Dpy3
+  import crow_modules.interpolationNDpy3
+else:
+  import crow_modules.distribution1Dpy2
+  import crow_modules.interpolationNDpy2
 
 sys.exit(0)
