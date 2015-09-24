b64e42a08076927776cf9212a4db420cce151e95
John Peterson <jwpeterson@gmail.com>
2015-09-24 15:27:51 -0600

    Remove deprecated Dirk2 class.
    Refs #5670.

--- framework/src/base/Moose.C
+++ framework/src/base/Moose.C
@@ -287,7 +287,6 @@
 #include "CrankNicolson.h"
 #include "ExplicitEuler.h"
 #include "ExplicitMidpoint.h"
-#include "Dirk.h"
 #include "LStableDirk2.h"
 #include "LStableDirk3.h"
 #include "ImplicitMidpoint.h"
@@ -678,7 +677,6 @@ registerObjects(Factory & factory)
   registerTimeIntegrator(ExplicitEuler);
   registerDeprecatedObjectName(ExplicitMidpoint, "RungeKutta2", "09/25/2015 12:00");
   registerTimeIntegrator(ExplicitMidpoint);
-  registerDeprecatedObjectName(Dirk, "Dirk", "09/22/2015 12:00");
   registerTimeIntegrator(LStableDirk2);
   registerTimeIntegrator(LStableDirk3);
   registerTimeIntegrator(ImplicitMidpoint);
