db0328997a319914803c546fdbda3e4da6b2cb5e
Andrew E Slaughter <andrew.slaughter@inl.gov>
2015-09-24 10:55:49 -0600

    Updating tutorials for new output syntax
    (refs #5603)

--- tutorials/darcy_thermo_mech/step7_adaptivity/src/base/DarcyThermoMechApp.C
+++ tutorials/darcy_thermo_mech/step7_adaptivity/src/base/DarcyThermoMechApp.C
@@ -36,6 +36,8 @@ InputParameters validParams<DarcyThermoMechApp>()
 
   params.set<bool>("use_legacy_uo_initialization") = false;
   params.set<bool>("use_legacy_uo_aux_computation") = false;
+  params.set<bool>("use_legacy_output_syntax") = false;
+
   return params;
 }
 
