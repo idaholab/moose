43ed278b7cf4877047337b38dd9e3fdb7983872f
Daniel Schwen <daniel.schwen@inl.gov>
2015-10-21 13:29:08 -0600

    Refactor CompositeMobilityTensor, use VariableName (#5850)

--- modules/phase_field/include/materials/CompositeMobilityTensor.h
+++ modules/phase_field/include/materials/CompositeMobilityTensor.h
@@ -7,55 +7,23 @@
 #ifndef COMPOSITEMOBILITYTENSOR_H
 #define COMPOSITEMOBILITYTENSOR_H
 
-#include "Material.h"
-#include "DerivativeMaterialInterface.h"
-
-class CompositeMobilityTensor;
-
-template<>
-InputParameters validParams<CompositeMobilityTensor>();
+#include "CompositeTensorBase.h"
 
 /**
  * CompositeMobilityTensor provides a simple RealTensorValue type
  * MaterialProperty that can be used as a mobility in a phase field simulation.
+ * This mobility is computes as a weighted sum of base mobilities where each weight
+ * can be a scalar material property that may depend on simulation variables.
+ * The generic logic that computes a weighted sum of tensors is located in the
+ * templated base class CompositeTensorBase.
  */
-class CompositeMobilityTensor : public DerivativeMaterialInterface<Material>
+class CompositeMobilityTensor : public CompositeTensorBase<RealTensorValue>
 {
 public:
   CompositeMobilityTensor(const InputParameters & parameters);
-
-protected:
-  virtual void computeQpProperties();
-
-  /// Name of the mobility tensor material property
-  MaterialPropertyName _M_name;
-  /// component tensor names
-  std::vector<MaterialPropertyName> _tensor_names;
-  /// component weight names
-  std::vector<MaterialPropertyName> _weight_names;
-
-  /// number of dependent variables
-  unsigned int _num_args;
-  /// number of compomemt tensors and weights
-  unsigned int _num_comp;
-
-  /// @{ Composed tensor and its derivatives
-  MaterialProperty<RealTensorValue> & _M;
-  std::vector<MaterialProperty<RealTensorValue> *> _dM;
-  std::vector<std::vector<MaterialProperty<RealTensorValue> *> > _d2M;
-  /// @}
-
-  /// @{ component tensors and their derivatives w.r.t. the args
-  std::vector<const MaterialProperty<RealTensorValue> *> _tensors;
-  std::vector<std::vector<const MaterialProperty<RealTensorValue> *> > _dtensors;
-  std::vector<std::vector<std::vector<const MaterialProperty<RealTensorValue> *> > > _d2tensors;
-  /// @}
-
-  /// @{ component weights and their derivatives w.r.t. the args
-  std::vector<const MaterialProperty<Real> *> _weights;
-  std::vector<std::vector<const MaterialProperty<Real> *> > _dweights;
-  std::vector<std::vector<std::vector<const MaterialProperty<Real> *> > > _d2weights;
-  /// @}
 };
+
+template<>
+InputParameters validParams<CompositeMobilityTensor>();
 
 #endif //COMPOSITEMOBILITYTENSOR_H
