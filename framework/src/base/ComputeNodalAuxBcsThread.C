c2677d7845b320132c402af3027887ff38c44e3e
Derek Gaston <derek.gaston@inl.gov>
2015-11-08 19:24:00 -0500

    Add ThreadedNodeLoop and switch ComputeNodalAuxBcsThread over to it refs #1287

--- framework/src/base/ComputeNodalAuxBcsThread.C
+++ framework/src/base/ComputeNodalAuxBcsThread.C
@@ -24,69 +24,57 @@
 ComputeNodalAuxBcsThread::ComputeNodalAuxBcsThread(FEProblem & fe_problem,
                                                    AuxiliarySystem & sys,
                                                    std::vector<AuxWarehouse> & auxs) :
-    _fe_problem(fe_problem),
-    _sys(sys),
+    ThreadedNodeLoop<ConstBndNodeRange, ConstBndNodeRange::const_iterator>(fe_problem, sys),
+    _aux_sys(sys),
     _auxs(auxs)
 {
 }
 
 // Splitting Constructor
-ComputeNodalAuxBcsThread::ComputeNodalAuxBcsThread(ComputeNodalAuxBcsThread & x, Threads::split /*split*/) :
-    _fe_problem(x._fe_problem),
-    _sys(x._sys),
+ComputeNodalAuxBcsThread::ComputeNodalAuxBcsThread(ComputeNodalAuxBcsThread & x, Threads::split split) :
+    ThreadedNodeLoop<ConstBndNodeRange, ConstBndNodeRange::const_iterator>(x, split),
+    _aux_sys(x._aux_sys),
     _auxs(x._auxs)
 {
 }
 
 void
-ComputeNodalAuxBcsThread::operator() (const ConstBndNodeRange & range)
+ComputeNodalAuxBcsThread::onNode(ConstBndNodeRange::const_iterator & node_it)
 {
-  ParallelUniqueId puid;
-  _tid = puid.id;
+  const BndNode * bnode = *node_it;
 
-  for (ConstBndNodeRange::const_iterator nd = range.begin() ; nd != range.end(); ++nd)
+  BoundaryID boundary_id = bnode->_bnd_id;
+
+  // prepare variables
+  for (std::map<std::string, MooseVariable *>::iterator it = _aux_sys._nodal_vars[_tid].begin(); it != _aux_sys._nodal_vars[_tid].end(); ++it)
   {
-    const BndNode * bnode = *nd;
+    MooseVariable * var = it->second;
+    var->prepareAux();
+  }
 
-    BoundaryID boundary_id = bnode->_bnd_id;
+  if (_auxs[_tid].activeBCs(boundary_id).size() > 0)
+  {
+    Node * node = bnode->_node;
 
-    // prepare variables
-    for (std::map<std::string, MooseVariable *>::iterator it = _sys._nodal_vars[_tid].begin(); it != _sys._nodal_vars[_tid].end(); ++it)
+    if (node->processor_id() == _fe_problem.processor_id())
+    {
+      _fe_problem.reinitNodeFace(node, boundary_id, _tid);
+
+      for (std::vector<AuxKernel *>::const_iterator aux_it = _auxs[_tid].activeBCs(boundary_id).begin();
+           aux_it != _auxs[_tid].activeBCs(boundary_id).end();
+           ++aux_it)
+        (*aux_it)->compute();
+    }
+  }
+
+  // We are done, so update the solution vector
+  {
+    Threads::spin_mutex::scoped_lock lock(Threads::spin_mtx);
+    // update the solution vector
+    for (std::map<std::string, MooseVariable *>::iterator it = _aux_sys._nodal_vars[_tid].begin(); it != _aux_sys._nodal_vars[_tid].end(); ++it)
     {
       MooseVariable * var = it->second;
-      var->prepareAux();
-    }
-
-    if (_auxs[_tid].activeBCs(boundary_id).size() > 0)
-    {
-      Node * node = bnode->_node;
-
-//      if (unlikely(_calculate_element_time))
-//        startNodeTiming(node.id());
-
-      if (node->processor_id() == _fe_problem.processor_id())
-      {
-        _fe_problem.reinitNodeFace(node, boundary_id, _tid);
-
-        for (std::vector<AuxKernel *>::const_iterator aux_it = _auxs[_tid].activeBCs(boundary_id).begin();
-            aux_it != _auxs[_tid].activeBCs(boundary_id).end();
-            ++aux_it)
-          (*aux_it)->compute();
-      }
-
-//      if (unlikely(_calculate_element_time))
-//        stopNodeTiming(node.id());
-    }
-
-    // We are done, so update the solution vector
-    {
-      Threads::spin_mutex::scoped_lock lock(Threads::spin_mtx);
-      // update the solution vector
-      for (std::map<std::string, MooseVariable *>::iterator it = _sys._nodal_vars[_tid].begin(); it != _sys._nodal_vars[_tid].end(); ++it)
-      {
-        MooseVariable * var = it->second;
-        var->insert(_sys.solution());
-      }
+      var->insert(_sys.solution());
     }
   }
 }
--- framework/src/base/ThreadedNodeLoop.C
+++ framework/src/base/ThreadedNodeLoop.C
@@ -12,27 +12,4 @@
 /*            See COPYRIGHT for full restrictions               */
 /****************************************************************/
 
-#ifndef COMPUTENODALAUXBCSTHREAD_H
-#define COMPUTENODALAUXBCSTHREAD_H
-
 #include "ThreadedNodeLoop.h"
-#include "AuxWarehouse.h"
-
-class ComputeNodalAuxBcsThread : public ThreadedNodeLoop<ConstBndNodeRange, ConstBndNodeRange::const_iterator>
-{
-public:
-  ComputeNodalAuxBcsThread(FEProblem & fe_problem, AuxiliarySystem & sys, std::vector<AuxWarehouse> & auxs);
-
-  // Splitting Constructor
-  ComputeNodalAuxBcsThread(ComputeNodalAuxBcsThread & x, Threads::split split);
-
-  virtual void onNode(ConstBndNodeRange::const_iterator & node_it);
-
-  void join(const ComputeNodalAuxBcsThread & /*y*/);
-
-protected:
-  AuxiliarySystem & _aux_sys;
-  std::vector<AuxWarehouse> & _auxs;
-};
-
-#endif //COMPUTENODALAUXBCSTHREAD_H
