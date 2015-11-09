120c58a03540b3aaa011f038647ba3433cf734cb
Congjian Wang <wangc@fn601604.fn.inl.gov>
2015-11-09 16:16:18 -0700

    add comments for cellProbabilityWeight

--- src/distributions/distributionNDNormal.C
+++ src/distributions/distributionNDNormal.C
@@ -389,6 +389,9 @@ double BasicMultivariateNormal::cellProbabilityWeight(std::vector<double> center
      * In the 1D case a cell region is an interval [a,b], thus the integral of the pdf in such interval is
      * calculated as CDF(b)-CDF(a). This functions perform a similar evolution but for a generic ND cell
      * This function assumes all the input variables are uncorrelated, and follows univariate normal distribution N(0,1)
+     * Input Parameters:
+     * center: a vector to store the grid coordinate, for ND grid sampler, center represents the coordinate of given grid point
+     * dx:  a vector to store the distance between given grid coordinate and its connected points, for ND grid sampler, dx represents the distance between grid_coordinate_plus_one - grid_coordinate_minus_one, where grid_coordinate_plus_one and grid_coordinate_minus_one are the shift of "center"
      */
 
   double value = 1.0;
