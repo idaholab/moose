0207ce4143273504b94450c3a892c3a80b72d7e4
Andrew E Slaughter <andrew.slaughter@inl.gov>
2015-11-20 15:37:57 -0700

    Objects must define if the 'enable' parameter is controllable
    This allows us to limit the ability to control objects such as FEProblem, etc.
    
    (refs #5680)

--- test/src/controls/TestControl.C
+++ test/src/controls/TestControl.C
@@ -21,7 +21,7 @@ InputParameters validParams<TestControl>()
 {
   InputParameters params = validParams<Control>();
 
-  MooseEnum test_type("real variable point tid_warehouse_error");
+  MooseEnum test_type("real variable point tid_warehouse_error disable_executioner");
   params.addRequiredParam<MooseEnum>("test_type", test_type, "Indicates the type of test to perform");
   params.addRequiredParam<std::string>("parameter", "The input parameter(s) to control. Specify a single parameter name and all parameters in all objects matching the name will be updated");
 
@@ -40,6 +40,9 @@ TestControl::TestControl(const InputParameters & parameters) :
 
   else if (_test_type == "tid_warehouse_error")
     _fe_problem.getControlWarehouse().initialSetup(12345);
+
+  else if (_test_type == "disable_executioner")
+    getControllableValueByName<bool>("Executioner/enable");
 
   else if (_test_type != "point")
     mooseError("Unknown test type.");
