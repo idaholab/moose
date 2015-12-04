d997f353a8189621bb1400357b8d2bdbf43afe43
Andrew E Slaughter <andrew.slaughter@inl.gov>
2015-12-04 14:56:38 -0700

    Updates MultiAppWarehouse to new design
    (refs #5680)

--- framework/src/multiapps/MultiAppWarehouse.C
+++ framework/src/multiapps/MultiAppWarehouse.C
@@ -18,7 +18,8 @@
 #include "TransientMultiApp.h"
 #include "MooseError.h"
 
-MultiAppWarehouse::MultiAppWarehouse()
+MultiAppWarehouse::MultiAppWarehouse() :
+    ExecuteMooseObjectWarehouse<MultiApp>(/*threaded=*/false)
 {
 }
 
@@ -27,53 +28,26 @@ MultiAppWarehouse::~MultiAppWarehouse()
 }
 
 void
-MultiAppWarehouse::addMultiApp(MooseSharedPointer<MultiApp> multi_app)
+MultiAppWarehouse::addObject(MooseSharedPointer<MultiApp> object, THREAD_ID /*tid*/)
 {
-  _all_ptrs.push_back(multi_app);
-  _all_objects.push_back(multi_app.get());
+  ExecuteMooseObjectWarehouse<MultiApp>::addObject(object);
 
-  MooseSharedPointer<TransientMultiApp> trans_multi_app = MooseSharedNamespace::dynamic_pointer_cast<TransientMultiApp>(multi_app);
-
-  if (trans_multi_app.get())
-    _transient_multi_apps.push_back(trans_multi_app.get());
+  // Store TranseintMultiApp objects in another containter, this is needed for calling computeDT
+  MooseSharedPointer<TransientMultiApp> trans_multi_app = MooseSharedNamespace::dynamic_pointer_cast<TransientMultiApp>(object);
+  if (trans_multi_app)
+    _transient_multi_apps.addObject(trans_multi_app);
 }
 
-bool
-MultiAppWarehouse::hasMultiApp(const std::string & multi_app_name) const
-{
-  for (std::vector<MultiApp *>::const_iterator i = _all_objects.begin(); i != _all_objects.end(); ++i)
-    if ((*i)->name() == multi_app_name)
-      return true;
-
-  return false;
-}
-
-bool
-MultiAppWarehouse::hasMultiApp() const
-{
-  return !_all_objects.empty();
-}
-
-MultiApp *
-MultiAppWarehouse::getMultiApp(const std::string & multi_app_name) const
-{
-  for (std::vector<MultiApp *>::const_iterator i = _all_objects.begin(); i != _all_objects.end(); ++i)
-    if ((*i)->name() == multi_app_name)
-      return *i;
-
-  mooseError("Unknown MultiApp: " << multi_app_name);
-}
 
 void
 MultiAppWarehouse::parentOutputPositionChanged()
 {
-  for (unsigned int i=0; i<_all_objects.size(); i++)
-    _all_objects[i]->parentOutputPositionChanged();
-}
-
-void
-MultiAppWarehouse::initialSetup()
-{
-  for (std::vector<MultiApp *>::iterator i = _all_objects.begin(); i != _all_objects.end(); ++i)
-    (*i)->initialSetup();
+  std::map<ExecFlagType, MooseObjectStorage<MultiApp> >::iterator it;
+  std::vector<MooseSharedPointer<MultiApp> >::iterator jt;
+  for (it = _execute_objects.begin(); it != _execute_objects.end(); ++it)
+  {
+    const std::vector<MooseSharedPointer<MultiApp> > & objects = it->second.getActiveObjects();
+    for (std::vector<MooseSharedPointer<MultiApp> >::const_iterator jt = objects.begin(); jt != objects.end(); ++jt)
+      (*jt)->parentOutputPositionChanged();
+  }
 }
