748777928715ceb23795bf64d5ae185c0d9bb552
Benjamin Spencer <benjamin.spencer@inl.gov>
2015-12-04 09:48:23 -0700

    Add OutOfPlanePressure kernel for generalized plane strain closes #6024

---
+++ modules/tensor_mechanics/include/kernels/OutOfPlanePressure.h
@@ -0,0 +1,41 @@
+/****************************************************************/
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*          All contents are licensed under LGPL V2.1           */
+/*             See LICENSE for full restrictions                */
+/****************************************************************/
+
+#ifndef OUTOFPLANEPRESSURE_H
+#define OUTOFPLANEPRESSURE_H
+
+#include "Kernel.h"
+
+//Forward Declarations
+class Function;
+class OutOfPlanePressure;
+
+template<>
+InputParameters validParams<OutOfPlanePressure>();
+
+/**
+ * OutOfPlanePressure is a kernel used to apply pressure in the out-of-plane direction
+ * in 2D plane stress or generalized plane strain models. Following the convention of
+ * the standard Pressure boundary condition, positive pressures are applied inward into
+ * the surface.
+ */
+
+class OutOfPlanePressure : public Kernel
+{
+public:
+
+  OutOfPlanePressure(const InputParameters & parameters);
+
+protected:
+  virtual Real computeQpResidual();
+
+private:
+  const PostprocessorValue * const _postprocessor;
+  Function & _function;
+  const Real _factor;
+};
+#endif //OUTOFPLANEPRESSURE_H
--- modules/tensor_mechanics/src/base/TensorMechanicsApp.C
+++ modules/tensor_mechanics/src/base/TensorMechanicsApp.C
@@ -26,6 +26,7 @@
 #include "InertialForce.h"
 #include "Gravity.h"
 #include "DynamicStressDivergenceTensors.h"
+#include "OutOfPlanePressure.h"
 
 #include "LinearElasticMaterial.h"
 #include "LinearElasticTruss.h"
@@ -184,6 +185,7 @@ TensorMechanicsApp::registerObjects(Factory & factory)
   registerKernel(InertialForce);
   registerKernel(Gravity);
   registerKernel(DynamicStressDivergenceTensors);
+  registerKernel(OutOfPlanePressure);
 
   registerMaterial(LinearElasticMaterial);
   registerMaterial(LinearElasticTruss);
