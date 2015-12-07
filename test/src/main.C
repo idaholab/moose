667664ac61c22f6b7fd0e8d55eb97bef17e2c1e0
Cody Permann <codypermann@gmail.com>
2015-12-07 14:49:10 -0700

    Fooling around with switching largest_id_type to char

--- test/src/main.C
+++ test/src/main.C
@@ -18,10 +18,10 @@ template <typename T>
 struct string_list_inserter
   : std::iterator<std::output_iterator_tag, T>
 {
-  explicit string_list_inserter(std::list<T *> & list) : _list(list) 
+  explicit string_list_inserter(std::list<T *> & list) : _list(list)
     {
     }
-  
+
   template <typename T2>
   void operator=(const T2 & value)
     {
@@ -52,59 +52,89 @@ namespace Parallel {
 // to handle communication of Elems
 template <>
 struct BufferType<const std::string *> {
-  typedef largest_id_type type;
+  typedef unsigned char type;
 };
 
 template<>
-unsigned int packed_size(const std::string *, std::vector<largest_id_type>::const_iterator in)
+unsigned int packed_size(const std::string *, std::vector<unsigned char>::const_iterator in)
 {
-  // String is encoded as a length followed by the content (as long long ints)
-  return in[0] + 1;;
+  std::cout << "packed size: " << ((in[0] << 24) | (in[1] << 16) | (in[2] << 8) | (in[3] << 0)) << std::endl;
+
+
+  // String is encoded as a 32-bit length followed by the content (unsigned char)
+  return (in[0] << 24) | (in[1] << 16) | (in[2] << 8) | (in[3]);
 }
 
 template<>
-unsigned int packed_size(const std::string * s, std::vector<largest_id_type>::iterator in)
+unsigned int packed_size(const std::string * s, std::vector<unsigned char>::iterator in)
 {
-  return packed_size(s, std::vector<largest_id_type>::const_iterator(in));
+  return packed_size(s, std::vector<unsigned char>::const_iterator(in));
 }
 
 
 template<>
 unsigned int packable_size(const std::string * s, const void *)
 {
-  // String is encoded as a length followed by the content (as long long ints)
-  return s->size() + 1;
+  // String is encoded as a 32-bit length followed by the content (unsigned char)
+  return s->size() + 4;
 }
- 
- 
+
+
 template <>
-void pack (const std::string * b, std::vector<largest_id_type> & data, const void *)
+void pack (const std::string * b, std::vector<unsigned char> & data, const void *)
 {
-  data.push_back(b->size());
+  uint32_t size = 4 /* 32-bit int */ + b->size();
+
+//  std::cout << "size: " << size << std::endl;
+//
+//  const unsigned char* beg = reinterpret_cast<const unsigned char*>(&size);
+//  const unsigned char* end = beg + sizeof(size);
+//  while(beg != end)
+//    std::cout << std::bitset<CHAR_BIT>(*beg++) << ' ';
+//  std::cout << '\n';
+
+
+  data.push_back(size >> 24);
+  data.push_back(size >> 16);
+  data.push_back(size >> 8);
+  data.push_back(size);
+
+//  data.push_back(static_cast<unsigned char>(size & 0x000000FF));
+//  data.reinterpret_cast<
+
+  unsigned int packed_size = data.size();
+  std::cout << "byte 0: " <<  static_cast<unsigned int>(data[packed_size-4]) << std::endl;
+  std::cout << "byte 1: " <<  static_cast<unsigned int>(data[packed_size-3]) << std::endl;
+  std::cout << "byte 2: " <<  static_cast<unsigned int>(data[packed_size-2]) << std::endl;
+  std::cout << "byte 3: " <<  static_cast<unsigned int>(data[packed_size-1]) << std::endl;
+
+
   std::copy(b->begin(), b->end(), std::back_inserter(data));
 }
- 
+
 template <>
-void unpack(std::vector<largest_id_type>::const_iterator in, std::string ** out, void *)
+void unpack(std::vector<unsigned char>::const_iterator in, std::string ** out, void *)
 {
-  largest_id_type size = in[0];
+  uint32_t size = (in[0] << 24) | (in[1] << 16) | (in[2] << 8) | (in[3] << 0);
+
+  std::cout << "Unpack: " << size << std::endl;
 
   std::ostringstream oss;
-  for (unsigned int i = 0; i < size; ++i)
-    oss << static_cast<char>(in[i+1]);
+  for (unsigned int i = 4; i < size; ++i)
+    oss << in[i];
 
-  in += size + 1;
-  
+  in += size;
+
   (*out) = new std::string(oss.str());
 }
 
 }
 }
-  
+
 
 
 int main(int argc, char *argv[])
-{ 
+{
   // Initialize MPI, solvers and MOOSE
   MooseInit init(argc, argv);
 
@@ -114,7 +144,11 @@ int main(int argc, char *argv[])
   switch (comm->rank())
   {
   case 0:
-    foo.assign("zero"); break;
+    for (unsigned int i=0; i<260; ++i)
+      foo.append("0123456789");
+    break;
+
+//    foo.assign("zero"); break;
   case 1:
     foo.assign("one"); break;
   case 2:
@@ -130,21 +164,22 @@ int main(int argc, char *argv[])
   default:
     foo.assign("some bigger number"); break;
   }
-  
+
   std::vector<std::string *> send(1);
   send[0] = &foo;
 
   std::list<std::string *> recv;
-  
+
   comm->allgather_packed_range((void *)(NULL), send.begin(), send.end(),
                                string_list_inserter<std::string>(recv));
 
+
   for (std::list<std::string *>::iterator it = recv.begin(); it != recv.end(); ++it)
     std::cout << **it << '\n';
- 
+
   // Vitally important!
   for (std::list<std::string *>::iterator it = recv.begin(); it != recv.end(); ++it)
     delete *it;
-  
+
   return 0;
 }
