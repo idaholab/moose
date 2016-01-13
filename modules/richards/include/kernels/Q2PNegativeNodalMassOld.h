81206785b61e540fbaf1d8c57cc8c8950f16cbe4
WilkAndy <andrew.wilkins@csiro.au>
2016-01-13 21:48:11 +1000

    Removed Q2PMass Postprocessor
    Split the Kernel Q2PMassChange to Q2PNodalMass and Q2PNegativeNodalMass,
    so that the former can be used to calculate nodal masses using save_in.
    
    Created a Q2PAction to simplify the input files considerably.
    
    Fixes #6141

--- modules/richards/include/kernels/Q2PNegativeNodalMassOld.h
+++ modules/richards/include/kernels/Q2PNegativeNodalMassOld.h
@@ -6,56 +6,43 @@
 /****************************************************************/
 
 
-#ifndef Q2PMASSCHANGE
-#define Q2PMASSCHANGE
+#ifndef Q2PNEGATIVENODALMASSOLD
+#define Q2PNEGATIVENODALMASSOLD
 
 #include "TimeKernel.h"
 #include "Material.h"
 #include "RichardsDensity.h"
 
 // Forward Declarations
-class Q2PMassChange;
+class Q2PNegativeNodalMassOld;
 
 template<>
-InputParameters validParams<Q2PMassChange>();
+InputParameters validParams<Q2PNegativeNodalMassOld>();
 
 /**
- * d(fluid mass in porespace)/dt with the fluid mass
+ * -fluid_mass_old/dt with the fluid mass
  * being lumped to the nodes.
  */
-class Q2PMassChange : public TimeKernel
+class Q2PNegativeNodalMassOld : public TimeKernel
 {
 public:
 
-  Q2PMassChange(const InputParameters & parameters);
+  Q2PNegativeNodalMassOld(const InputParameters & parameters);
 
 protected:
 
   virtual Real computeQpResidual();
 
-  virtual Real computeQpJacobian();
-
-  virtual Real computeQpOffDiagJacobian(unsigned int jvar);
-
   const RichardsDensity & _density;
-
-  /// the other variable (this is porepressure if the Variable is saturation)
-  VariableValue & _other_var_nodal;
 
   /// old value of the other variable (this is porepressure if the Variable is saturation)
   VariableValue & _other_var_nodal_old;
 
-  /// variable number of the other variable
-  unsigned int _other_var_num;
-
   /// whether the "other variable" is actually porepressure
   bool _var_is_pp;
-
-  /// current value of the porosity
-  const MaterialProperty<Real> & _porosity;
 
   /// value of the porosity at the start of the timestep
   const MaterialProperty<Real> & _porosity_old;
 };
 
-#endif //Q2PMASSCHANGE
+#endif //Q2PNEGATIVENODALMASSOLD
---
+++ modules/richards/include/kernels/Q2PNodalMass.h
@@ -0,0 +1,55 @@
+/****************************************************************/
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*          All contents are licensed under LGPL V2.1           */
+/*             See LICENSE for full restrictions                */
+/****************************************************************/
+
+
+#ifndef Q2PNODALMASS
+#define Q2PNODALMASS
+
+#include "TimeKernel.h"
+#include "Material.h"
+#include "RichardsDensity.h"
+
+// Forward Declarations
+class Q2PNodalMass;
+
+template<>
+InputParameters validParams<Q2PNodalMass>();
+
+/**
+ * fluid_mass/dt lumped to the nodes
+ */
+class Q2PNodalMass : public TimeKernel
+{
+public:
+
+  Q2PNodalMass(const InputParameters & parameters);
+
+protected:
+
+  virtual Real computeQpResidual();
+
+  virtual Real computeQpJacobian();
+
+  virtual Real computeQpOffDiagJacobian(unsigned int jvar);
+
+  const RichardsDensity & _density;
+
+  /// the other variable (this is porepressure if the Variable is saturation)
+  VariableValue & _other_var_nodal;
+
+  /// variable number of the other variable
+  unsigned int _other_var_num;
+
+  /// whether the "other variable" is actually porepressure
+  bool _var_is_pp;
+
+  /// current value of the porosity
+  const MaterialProperty<Real> & _porosity;
+
+};
+
+#endif //Q2PNODALMASS
