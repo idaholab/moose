ca9aff2e3f57ff412408367199f5863e78f7dd08
Yaqi Wang <Yaqi.Wang@inl.gov>
2016-01-14 11:49:55 -0700

    add a postprocessor to scale the values of other postprocessors #5954

---
+++ framework/include/postprocessors/ScalePostprocessor.h
@@ -0,0 +1,41 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+#ifndef SCALEPOSTPROCESSOR_H
+#define SCALEPOSTPROCESSOR_H
+
+#include "GeneralPostprocessor.h"
+
+class ScalePostprocessor;
+
+template<>
+InputParameters validParams<ScalePostprocessor>();
+
+/**
+ * Scale a postprocessor
+ */
+class ScalePostprocessor : public GeneralPostprocessor
+{
+public:
+  ScalePostprocessor(const InputParameters & parameters);
+
+  virtual void initialize() {}
+  virtual void execute() {}
+  virtual PostprocessorValue getValue();
+
+protected:
+  const PostprocessorValue & _value;
+  Real _scaling_factor;
+};
+
+#endif /* SCALEPOSTPROCESSOR_H */
