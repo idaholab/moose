3194acc79bcd5b5e0ce53ad5a4a11afa4fdb802b
Daniel Schwen <daniel.schwen@inl.gov>
2016-01-19 14:29:23 -0700

    Minor cleanup (#6099)

--- modules/tensor_mechanics/src/utils/RotationTensor.C
+++ modules/tensor_mechanics/src/utils/RotationTensor.C
@@ -15,30 +15,30 @@ RotationTensor::RotationTensor(const RealVectorValue & Euler_angles)
 void
 RotationTensor::update(const RealVectorValue & Euler_angles)
 {
-  Real phi_1 = Euler_angles(0)*(libMesh::pi/180.0);
-  Real Phi = Euler_angles(1)*(libMesh::pi/180.0);
-  Real phi_2 = Euler_angles(2)*(libMesh::pi/180.0);
+  const Real phi_1 = Euler_angles(0) * (libMesh::pi / 180.0);
+  const Real Phi = Euler_angles(1) * (libMesh::pi / 180.0);
+  const Real phi_2 = Euler_angles(2) * (libMesh::pi / 180.0);
 
-  Real c1 = std::cos(phi_1);
-  Real c2 = std::cos(Phi);
-  Real c3 = std::cos(phi_2);
+  const Real c1 = std::cos(phi_1);
+  const Real c2 = std::cos(Phi);
+  const Real c3 = std::cos(phi_2);
 
-  Real s1 = std::sin(phi_1);
-  Real s2 = std::sin(Phi);
-  Real s3 = std::sin(phi_2);
+  const Real s1 = std::sin(phi_1);
+  const Real s2 = std::sin(Phi);
+  const Real s3 = std::sin(phi_2);
 
-  //doing a Z1, X2, Z3 rotation
-  //RealTensorValue is formed row-wise
+  // doing a Z1, X2, Z3 rotation
+  // RealTensorValue is formed row-wise
 
-  _coords[0] = c1*c3 - c2*s1*s3; //R11
-  _coords[3] = -c1*s3 - c2*c3*s1; //R12
-  _coords[6] = s1*s2; //R13
+  _coords[0] = c1 * c3 - c2 * s1 * s3;  //R11
+  _coords[3] = -c1 * s3 - c2 * c3 * s1; //R12
+  _coords[6] = s1 * s2;                 //R13
 
-  _coords[1] = c3*s1 + c1*c2*s3; //R21
-  _coords[4] = c1*c2*c3 - s1*s3; //R22
-  _coords[7] = -c1*s2; //R23
+  _coords[1] = c3 * s1 + c1 * c2 * s3; //R21
+  _coords[4] = c1 * c2 * c3 - s1 * s3; //R22
+  _coords[7] = -c1 * s2;               //R23
 
-  _coords[2] = s2*s3; //R31
-  _coords[5] = c3*s2; //R32
-  _coords[8] = c2; //R33
+  _coords[2] = s2 * s3; //R31
+  _coords[5] = c3 * s2; //R32
+  _coords[8] = c2;      //R33
 }
