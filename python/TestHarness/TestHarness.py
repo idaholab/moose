caf01ed68c512620b0db33289ef7194a3c0fbf24
John Peterson <jwpeterson@gmail.com>
2016-01-19 11:23:46 -0700

    Add 'superlu' option to TestHarness.

--- python/TestHarness/TestHarness.py
+++ python/TestHarness/TestHarness.py
@@ -88,6 +88,7 @@ class TestHarness:
       self.checks['petsc_debug'] = set(['ALL'])
       self.checks['curl'] = set(['ALL'])
       self.checks['tbb'] = set(['ALL'])
+      self.checks['superlu'] = set(['ALL'])
     else:
       self.checks['compiler'] = getCompilers(self.libmesh_dir)
       self.checks['petsc_version'] = getPetscVersion(self.libmesh_dir)
@@ -101,6 +102,7 @@ class TestHarness:
       self.checks['petsc_debug'] = getLibMeshConfigOption(self.libmesh_dir, 'petsc_debug')
       self.checks['curl'] =  getLibMeshConfigOption(self.libmesh_dir, 'curl')
       self.checks['tbb'] =  getLibMeshConfigOption(self.libmesh_dir, 'tbb')
+      self.checks['superlu'] =  getLibMeshConfigOption(self.libmesh_dir, 'superlu')
 
     # Override the MESH_MODE option if using '--parallel-mesh' option
     if self.options.parallel_mesh == True or \
