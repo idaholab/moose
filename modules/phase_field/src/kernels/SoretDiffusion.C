69e0d21eef8009727835f44c326401c02784ab02
Daniel Schwen <daniel.schwen@inl.gov>
2016-01-26 14:57:38 -0700

    Make c coupling optional and default to variable (#6239)

--- modules/phase_field/src/kernels/SoretDiffusion.C
+++ modules/phase_field/src/kernels/SoretDiffusion.C
@@ -11,7 +11,7 @@ InputParameters validParams<SoretDiffusion>()
   InputParameters params = validParams<Kernel>();
   params.addClassDescription("Add Soret effect to Split formulation Cahn-Hilliard Kernel");
   params.addRequiredCoupledVar("T", "Temperature");
-  params.addRequiredCoupledVar("c", "Concentration");
+  params.addCoupledVar("c", "Concentration");
   params.addRequiredParam<MaterialPropertyName>("diff_name", "The diffusivity used with the kernel");
   params.addParam<MaterialPropertyName>("Q_name", "Qheat", "The material name for the heat of transport");
   return params;
@@ -22,39 +22,39 @@ SoretDiffusion::SoretDiffusion(const InputParameters & parameters) :
     _T_var(coupled("T")),
     _T(coupledValue("T")),
     _grad_T(coupledGradient("T")),
-    _c_var(coupled("c")),
-    _c(coupledValue("c")),
+    _is_coupled(isCoupled("c")),
+    _c_var(_is_coupled ? coupled("c") : _var.number()),
+    _c(_is_coupled ? coupledValue("c") : _u),
     _D(getMaterialProperty<Real>("diff_name")),
     _Q(getMaterialProperty<Real>("Q_name")),
-    _kb(8.617343e-5) // Boltzmann constant in eV/K
+    _kB(8.617343e-5) // Boltzmann constant in eV/K
 {
 }
 
 Real
 SoretDiffusion::computeQpResidual()
 {
-  Real T_term = _D[_qp] * _Q[_qp] * _c[_qp] / (_kb * _T[_qp] * _T[_qp]);
-
+  const Real T_term = _D[_qp] * _Q[_qp] * _c[_qp] / (_kB * _T[_qp] * _T[_qp]);
   return T_term * _grad_T[_qp] * _grad_test[_i][_qp];
 }
 
 Real
 SoretDiffusion::computeQpJacobian()
 {
-  if (_c_var == _var.number()) //Requires c jacobian
-    return computeQpCJacobian();
-
-  return 0.0;
+  return _is_coupled ? 0.0 : computeQpCJacobian();
 }
 
 Real
 SoretDiffusion::computeQpOffDiagJacobian(unsigned int jvar)
 {
-  if (_c_var == jvar) //Requires c jacobian
+  // c Off-Diagonal Jacobian
+  if (_c_var == jvar)
     return computeQpCJacobian();
-  else if (_T_var == jvar) //Requires T jacobian
+
+  // T Off-Diagonal Jacobian
+  if (_T_var == jvar)
     return _D[_qp] * _Q[_qp] * _c[_qp] * _grad_test[_i][_qp] *
-           (_grad_phi[_j][_qp]/(_kb * _T[_qp] * _T[_qp]) - 2.0 * _grad_T[_qp] * _phi[_j][_qp] / (_kb * _T[_qp] * _T[_qp] * _T[_qp]));
+           (_grad_phi[_j][_qp] - 2.0 * _grad_T[_qp] * _phi[_j][_qp] / _T[_qp]) / (_kB * _T[_qp] * _T[_qp]);
 
   return 0.0;
 }
@@ -62,7 +62,6 @@ SoretDiffusion::computeQpOffDiagJacobian(unsigned int jvar)
 Real
 SoretDiffusion::computeQpCJacobian()
 {
-  //Calculate the Jacobian for the c variable
-  return _D[_qp] * _Q[_qp] * _phi[_j][_qp] * _grad_T[_qp] / (_kb * _T[_qp] * _T[_qp]) * _grad_test[_i][_qp];
+  // Calculate the Jacobian for the c variable
+  return _D[_qp] * _Q[_qp] * _phi[_j][_qp] * _grad_T[_qp] / (_kB * _T[_qp] * _T[_qp]) * _grad_test[_i][_qp];
 }
-
