f719c421ff43284398a16572b54e0165e4f1608a
John Peterson <jwpeterson@gmail.com>
2016-02-18 10:02:27 -0700

    Add test of second derivatives in IntegratedBC.
    Code originally by: Wen Jiang <wen.jiang@inl.gov>
    
    Refs #6327.

---
+++ test/src/bcs/TestLapBC.C
@@ -0,0 +1,42 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#include "TestLapBC.h"
+
+template<>
+InputParameters validParams<TestLapBC>()
+{
+  InputParameters params = validParams<IntegratedBC>();
+  return params;
+}
+
+TestLapBC::TestLapBC(const InputParameters & parameters) :
+    IntegratedBC(parameters),
+    _second_u(second()),
+    _second_phi(secondPhiFace()),
+    _second_test(secondTestFace())
+{
+}
+
+Real
+TestLapBC::computeQpResidual()
+{
+  return -0.5 * _second_u[_qp].tr() * _test[_i][_qp];
+}
+
+Real
+TestLapBC::computeQpJacobian()
+{
+  return -0.5 * _second_phi[_j][_qp].tr() * _test[_i][_qp];
+}
---
+++ test/tests/bcs/second_deriv/test_lap_bc.i
@@ -0,0 +1,85 @@
+[Mesh]
+  type = GeneratedMesh
+  dim = 2
+  nx = 1
+  ny = 1
+  xmin = -1
+  xmax = 1
+  ymin = -1
+  ymax = 1
+  elem_type = QUAD9
+[]
+
+[Variables]
+  [./u]
+    order = SECOND
+    family = LAGRANGE
+  [../]
+[]
+
+[Kernels]
+  [./diff]
+    type = Diffusion
+    variable = u
+  [../]
+  [./ffn]
+    type = UserForcingFunction
+    variable = u
+    function = force_fn
+  [../]
+[]
+
+[Functions]
+  [./left_bc_func]
+    type = ParsedFunction
+    value = '1+y*y'
+  [../]
+  [./top_bc_func]
+    type = ParsedFunction
+    value = '1+x*x'
+  [../]
+  [./bottom_bc_func]
+    type = ParsedFunction
+    value = '1+x*x'
+  [../]
+  [./force_fn]
+    type = ParsedFunction
+    value = -4
+  [../]
+[]
+
+[BCs]
+  [./left]
+    type = FunctionDirichletBC
+    variable = u
+    boundary = left
+    function = left_bc_func
+  [../]
+  [./bottom]
+    type = FunctionDirichletBC
+    variable = u
+    boundary = bottom
+    function = bottom_bc_func
+  [../]
+  [./top]
+    type = FunctionDirichletBC
+    variable = u
+    boundary = top
+    function = top_bc_func
+  [../]
+  [./right_test]
+    type = TestLapBC
+    variable = u
+    boundary = right
+  [../]
+[]
+
+[Executioner]
+  type = Steady
+  solve_type = 'NEWTON'
+[]
+
+[Outputs]
+  file_base = out
+  exodus = true
+[]
