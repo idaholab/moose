a4876be6faa41cca76cb1b646832fe91ad2f1c42
Brian Alger <brian.alger@inl.gov>
2016-02-22 14:32:05 -0700

    Avoid updating timestamps on all files with delete_trailing_whitespace.sh. This also avoids the accidental "fixing" of files with no newlines before EOF.
    refs #6332

--- scripts/delete_trailing_whitespace.sh
+++ scripts/delete_trailing_whitespace.sh
@@ -7,5 +7,12 @@ REPO_DIR=${1:-"$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../"}
 if [ ! -d "$REPO_DIR" ]; then
   echo "$REPO_DIR directory does not exist";
 else
-  find $REPO_DIR -path ./contrib -prune -o -path ./libmesh -prune -o \( -name "*.[Chi]" -o -name "*.py" \) -type f -print0 | xargs -0 perl -pli -e "s/\s+$//"
+  bad_files=$(find $REPO_DIR -path ./contrib -prune -o -path ./libmesh -prune -o \( -name "*.[Chi]" -o -name "*.py" \) -type f -print0 | xargs -0 grep -l "[[:blank:]]$")
+  if [ "$bad_files" != "" ]; then
+    # do it like this to split on new lines. This preserves files with spaces in their names.
+    while read -r line; do
+      echo "Fixing: $line"
+      perl -pli -e "s/\s+$//" "$line"
+    done <<< "$bad_files"
+  fi
 fi
