7c2970b430d136662c4feeec7d36bfc8df770dd2
diego mandelli <diego.mandelli@inl.gov>
2016-02-24 10:57:46 -0700

    edit

--- src/utilities/NDspline.C
+++ src/utilities/NDspline.C
@@ -366,6 +366,9 @@ double NDSpline::phi(double t){
  if ((fabs(t))<1)
   phi_value = 4.0 - 6.0*std::pow(fabs(t),2) + 3.0*std::pow(fabs(t),3);
 
+ if (phi_value!=0.0)
+	 std::cout<< t <<" , "<< phi_value <<std::endl;
+
  return phi_value;
 }
 
