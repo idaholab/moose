a39874536c9d0bcd8e94efeea07254b199e31788
Derek Gaston <derek.gaston@inl.gov>
2016-03-04 01:43:12 -0500

    add LineFunctionSampler closes #6438

---
+++ framework/include/vectorpostprocessors/LineFunctionSampler.h
@@ -0,0 +1,77 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#ifndef LINEFUNCTIONSAMPLER_H
+#define LINEFUNCTIONSAMPLER_H
+
+#include "GeneralVectorPostprocessor.h"
+#include "SamplerBase.h"
+#include "Function.h"
+
+//Forward Declarations
+class LineFunctionSampler;
+
+template<>
+InputParameters validParams<LineFunctionSampler>();
+
+class LineFunctionSampler :
+  public GeneralVectorPostprocessor,
+  protected SamplerBase
+{
+public:
+  LineFunctionSampler(const InputParameters & parameters);
+
+  virtual ~LineFunctionSampler() {}
+
+  virtual void initialize();
+  virtual void execute();
+  virtual void finalize();
+
+  // Let the SamplerBase version of threadJoin() take part in the
+  // overload resolution process, otherwise we get warnings about
+  // overloaded virtual functions and "hiding" in debug mode.
+  using SamplerBase::threadJoin;
+
+  virtual void threadJoin(const UserObject & y);
+
+protected:
+  /// Beginning of the line
+  Point _start_point;
+
+  /// End of the line
+  Point _end_point;
+
+  /// Number of points along the line
+  unsigned int _num_points;
+
+  /// Names of the Functions
+  const std::vector<FunctionName> & _function_names;
+
+  /// Number of Functions we're sampling
+  unsigned int _num_funcs;
+
+  /// Pointers to the Functions
+  std::vector<Function *> _functions;
+
+  /// So we don't have to create and destroy this vector over and over again
+  std::vector<Real> _values;
+
+  /// The points to evaluate at
+  std::vector<Point> _points;
+
+  /// The ID to use for each point (yes, this is Real on purpose)
+  std::vector<Real> _ids;
+};
+
+#endif
---
+++ test/tests/vectorpostprocessors/line_function_sampler/line_function_sampler.i
@@ -0,0 +1,57 @@
+[Mesh]
+  type = GeneratedMesh
+  dim = 2
+  nx = 10
+  ny = 10
+[]
+
+[Variables]
+  [./u]
+  [../]
+[]
+
+[Kernels]
+  [./diff]
+    type = Diffusion
+    variable = u
+  [../]
+[]
+
+[BCs]
+  [./left]
+    type = DirichletBC
+    variable = u
+    boundary = left
+    value = 0
+  [../]
+  [./right]
+    type = DirichletBC
+    variable = u
+    boundary = right
+    value = 1
+  [../]
+[]
+
+[VectorPostprocessors]
+  [./func_vals]
+    type = LineFunctionSampler
+    functions = 'x+1 x^2+y^2'
+    start_point = '0 0 0'
+    end_point = '1 1 0'
+    num_points = 10
+    sort_by = id
+  [../]
+[]
+
+[Executioner]
+  # Preconditioned JFNK (default)
+  type = Steady
+  solve_type = PJFNK
+  petsc_options_iname = '-pc_type -pc_hypre_type'
+  petsc_options_value = 'hypre boomeramg'
+[]
+
+[Outputs]
+  exodus = true
+  csv = true
+[]
