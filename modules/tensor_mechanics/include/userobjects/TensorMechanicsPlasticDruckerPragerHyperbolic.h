c68c13bf5d861f4f7c4537405bbbd13d6edd6e0d
WilkAndy <andrew.wilkins@csiro.au>
2016-03-10 17:18:39 +1000

    Drucker Prager plasticity added to TensorMechanics
    A smoothed and non-smoothed version have been added
    The smoothed version has a custom return-map algorithm and a custom consistent tangent operator calculation
    
    Refs #6359

---
+++ modules/tensor_mechanics/include/userobjects/TensorMechanicsPlasticDruckerPragerHyperbolic.h
@@ -0,0 +1,167 @@
+/****************************************************************/
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*          All contents are licensed under LGPL V2.1           */
+/*             See LICENSE for full restrictions                */
+/****************************************************************/
+#ifndef TENSORMECHANICSPLASTICDRUCKERPRAGERHYPERBOLIC_H
+#define TENSORMECHANICSPLASTICDRUCKERPRAGERHYPERBOLIC_H
+
+#include "TensorMechanicsPlasticDruckerPrager.h"
+#include "TensorMechanicsHardeningModel.h"
+
+
+class TensorMechanicsPlasticDruckerPragerHyperbolic;
+
+
+template<>
+InputParameters validParams<TensorMechanicsPlasticDruckerPragerHyperbolic>();
+
+/**
+ * Rate-independent non-associative Drucker Prager
+ * with hardening/softening.  The cone's tip is smoothed in a hyperbolic fashion
+ * Most functions (eg flowPotential, etc) are simply inherited from
+ * TensorMechanicsPlasticDruckerPrager.  Note df_dsig is over-ridden
+ */
+class TensorMechanicsPlasticDruckerPragerHyperbolic : public TensorMechanicsPlasticDruckerPrager
+{
+ public:
+  TensorMechanicsPlasticDruckerPragerHyperbolic(const InputParameters & parameters);
+
+
+  /// Returns the model name (DruckerPragerHyperbolic)
+  virtual std::string modelName() const;
+
+  /// Returns _use_custom_returnMap
+  virtual bool useCustomReturnMap() const;
+
+  /// Returns _use_custom_cto
+  virtual bool useCustomCTO() const;
+
+ protected:
+
+  /**
+   * The yield function
+   * @param stress the stress at which to calculate the yield function
+   * @param intnl internal parameter
+   * @return the yield function
+   */
+  Real yieldFunction(const RankTwoTensor & stress, Real intnl) const;
+
+  /**
+   * The derivative of the flow potential with respect to stress
+   * @param stress the stress at which to calculate the flow potential
+   * @param intnl internal parameter
+   * @return dr_dstress(i, j, k, l) = dr(i, j)/dstress(k, l)
+   */
+  RankFourTensor dflowPotential_dstress(const RankTwoTensor & stress, Real intnl) const;
+
+
+  /// Function that's used in dyieldFunction_dstress and flowPotential
+  virtual RankTwoTensor df_dsig(const RankTwoTensor & stress, Real bbb) const;
+
+
+  /**
+    * Performs a custom return-map.
+    * You may choose to over-ride this in your
+    * derived TensorMechanicsPlasticXXXX class,
+    * and you may implement the return-map
+    * algorithm in any way that suits you.  Eg, using
+    * a Newton-Raphson approach, or a radial-return,
+    * etc.
+    * This may also be used as a quick way of ascertaining
+    * whether (trial_stress, intnl_old) is in fact admissible.
+    *
+    * For over-riding this function, please note the
+    * following.
+    *
+    * (1) Denoting the return value of the function by "successful_return",
+    * the only possible output values should be:
+    *   (A) trial_stress_inadmissible=false, successful_return=true.
+    *       That is, (trial_stress, intnl_old) is in fact admissible
+    *       (in the elastic domain).
+    *   (B) trial_stress_inadmissible=true, successful_return=false.
+    *       That is (trial_stress, intnl_old) is inadmissible
+    *       (outside the yield surface), and you didn't return
+    *       to the yield surface.
+    *   (C) trial_stress_inadmissible=true, successful_return=true.
+    *       That is (trial_stress, intnl_old) is inadmissible
+    *       (outside the yield surface), but you did return
+    *       to the yield surface.
+    * The default implementation only handles case (A) and (B):
+    * it does not attempt to do a return-map algorithm.
+    *
+    * (2) you must correctly signal "successful_return" using the
+    * return value of this function.  Don't assume the calling function
+    * will do Kuhn-Tucker checking and so forth!
+    *
+    * (3) In cases (A) and (B) you needn't set returned_stress,
+    * returned_intnl, delta_dp, or dpm.  This is for computational
+    * efficiency.
+    *
+    * (4) In cases (A) and (B), you MUST place the yield function
+    * values at (trial_stress, intnl_old) into yf so the calling
+    * function can use this information optimally.  You will have
+    * already calculated these yield function values, which can be
+    * quite expensive, and it's not very optimal for the calling
+    * function to have to re-calculate them.
+    *
+    * (5) In case (C), you need to set:
+    *   returned_stress (the returned value of stress)
+    *   returned_intnl  (the returned value of the internal variable)
+    *   delta_dp   (the change in plastic strain)
+    *   dpm (the plastic multipliers needed to bring about the return)
+    *   yf (yield function values at the returned configuration)
+    *
+    * (Note, if you over-ride returnMap, you will probably
+    * want to override consistentTangentOpertor too, otherwise
+    * it will default to E_ijkl.)
+    *
+    * @param trial_stress The trial stress
+    * @param intnl_old Value of the internal parameter
+    * @param E_ijkl Elasticity tensor
+    * @param ep_plastic_tolerance Tolerance defined by the user for the plastic strain
+    * @param[out] returned_stress In case (C): lies on the yield surface after returning and produces the correct plastic strain (normality condition).  Otherwise: not defined
+    * @param[out] returned_intnl In case (C): the value of the internal parameter after returning.  Otherwise: not defined
+    * @param[out] dpm  In case (C): the plastic multipliers needed to bring about the return.  Otherwise: not defined
+    * @param[out] delta_dp In case (C): The change in plastic strain induced by the return process.  Otherwise: not defined
+    * @param[out] yf In case (C): the yield function at (returned_stress, returned_intnl).  Otherwise: the yield function at (trial_stress, intnl_old)
+    * @param[out] trial_stress_inadmissible Should be set to false if the trial_stress is admissible, and true if the trial_stress is inadmissible.  This can be used by the calling prorgram
+    * @return true if a successful return (or a return-map not needed), false if the trial_stress is inadmissible but the return process failed
+    */
+  virtual bool returnMap(const RankTwoTensor & trial_stress, Real intnl_old, const RankFourTensor & E_ijkl,
+                         Real ep_plastic_tolerance, RankTwoTensor & returned_stress, Real & returned_intnl,
+                         std::vector<Real> & dpm, RankTwoTensor & delta_dp, std::vector<Real> & yf,
+                         bool & trial_stress_inadmissible) const;
+
+  /**
+    * Calculates a custom consistent tangent operator.
+    * You may choose to over-ride this in your
+    * derived TensorMechanicsPlasticXXXX class.
+    *
+    * @param stress_old trial stress before returning
+    * @param stress current stress state
+    * @param intnl internal parameter
+    * @param E_ijkl elasticity tensor
+    * @param cumulative_pm the cumulative plastic multipliers
+    * @return the consistent tangent operator for J2 (radial return) case
+    */
+  virtual RankFourTensor consistentTangentOperator(const RankTwoTensor & trial_stress, const RankTwoTensor & stress, Real intnl,
+                                                   const RankFourTensor & E_ijkl, const std::vector<Real> & cumulative_pm) const;
+
+ private:
+
+  /// smoothing parameter for the cone's tip
+  const Real _smoother2;
+
+  /// whether to use the custom returnMap function
+  const bool _use_custom_returnMap;
+
+  /// Whether to use the custom consistent tangent operator calculation
+  const bool _use_custom_cto;
+
+  /// max iters for custom return map loop
+  const unsigned _max_iters;
+};
+
+#endif // TENSORMECHANICSPLASTICDRUCKERPRAGERHYPERBOLIC_H
---
+++ modules/tensor_mechanics/src/userobjects/TensorMechanicsPlasticDruckerPrager.C
@@ -0,0 +1,275 @@
+/****************************************************************/
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*          All contents are licensed under LGPL V2.1           */
+/*             See LICENSE for full restrictions                */
+/****************************************************************/
+#include "TensorMechanicsPlasticDruckerPrager.h"
+
+template<>
+InputParameters validParams<TensorMechanicsPlasticDruckerPrager>()
+{
+  InputParameters params = validParams<TensorMechanicsPlasticModel>();
+  MooseEnum mc_interpolation_scheme("outer_tip=0 inner_tip=1 lode_zero=2 inner_edge=3 native=4", "lode_zero");
+  params.addParam<MooseEnum>("mc_interpolation_scheme", mc_interpolation_scheme, "Scheme by which the Drucker-Prager cohesion, friction angle and dilation angle are set from the Mohr-Coulomb parameters mc_cohesion, mc_friction_angle and mc_dilation_angle.  Consider the DP and MC yield surfaces on the devatoric (octahedral) plane.  Outer_tip: the DP circle touches the outer tips of the MC hex.  Inner_tip: the DP circle touches the inner tips of the MC hex.  Lode_zero: the DP circle intersects the MC hex at lode angle=0.  Inner_edge: the DP circle is the largest circle that wholey fits inside the MC hex.  Native: The DP cohesion, friction angle and dilation angle are set equal to the mc_ parameters entered.");
+  params.addRequiredParam<UserObjectName>("mc_cohesion", "A TensorMechanicsHardening UserObject that defines hardening of the Mohr-Coulomb cohesion.  Physically this should not be negative.");
+  params.addRequiredParam<UserObjectName>("mc_friction_angle", "A TensorMechanicsHardening UserObject that defines hardening of the Mohr-Coulomb friction angle (in radians).  Physically this should be between 0 and Pi/2.");
+  params.addRequiredParam<UserObjectName>("mc_dilation_angle", "A TensorMechanicsHardening UserObject that defines hardening of the Mohr-Coulomb dilation angle (in radians).  Usually the dilation angle is not greater than the friction angle, and it is between 0 and Pi/2.");
+  params.addClassDescription("Non-associative Drucker Prager plasticity with no smoothing of the cone tip.");
+  return params;
+}
+
+TensorMechanicsPlasticDruckerPrager::TensorMechanicsPlasticDruckerPrager(const InputParameters & parameters) :
+    TensorMechanicsPlasticModel(parameters),
+    _mc_cohesion(getUserObject<TensorMechanicsHardeningModel>("mc_cohesion")),
+    _mc_phi(getUserObject<TensorMechanicsHardeningModel>("mc_friction_angle")),
+    _mc_psi(getUserObject<TensorMechanicsHardeningModel>("mc_dilation_angle")),
+    _mc_interpolation_scheme(getParam<MooseEnum>("mc_interpolation_scheme")),
+    _zero_cohesion_hardening(_mc_cohesion.modelName().compare("Constant") == 0),
+    _zero_phi_hardening(_mc_phi.modelName().compare("Constant") == 0),
+    _zero_psi_hardening(_mc_psi.modelName().compare("Constant") == 0)
+{
+  if (_mc_phi.value(0) < 0 || _mc_psi.value(0) < 0)
+    mooseError("TensorMechanicsPlasticDruckerPrager: MC friction and dilation angles must lie in [0, Pi/2]");
+  if (_mc_phi.value(0) < _mc_psi.value(0))
+    mooseError("TensorMechanicsPlasticDruckerPrager: MC friction angle must not be less than MC dilation angle");
+  if (_mc_cohesion.value(0) < 0)
+    mooseError("TensorMechanicsPlasticDruckerPrager: MC cohesion should not be negative");
+
+  initializeAandB(0.0, _aaa, _bbb);
+  initializeB(0.0, dilation, _bbb_flow);
+}
+
+
+Real
+TensorMechanicsPlasticDruckerPrager::yieldFunction(const RankTwoTensor & stress, Real intnl) const
+{
+  Real aaa;
+  Real bbb;
+  AandB(intnl, aaa, bbb);
+  return std::sqrt(stress.secondInvariant()) + stress.trace() * bbb - aaa;
+}
+
+RankTwoTensor
+TensorMechanicsPlasticDruckerPrager::df_dsig(const RankTwoTensor & stress, Real bbb) const
+{
+  return 0.5 * stress.dsecondInvariant()/std::sqrt(stress.secondInvariant()) + stress.dtrace() * bbb;
+}
+
+RankTwoTensor
+TensorMechanicsPlasticDruckerPrager::dyieldFunction_dstress(const RankTwoTensor & stress, Real intnl) const
+{
+  Real bbb;
+  Bonly(intnl, friction, bbb);
+  return df_dsig(stress, bbb);
+}
+
+
+Real
+TensorMechanicsPlasticDruckerPrager::dyieldFunction_dintnl(const RankTwoTensor & stress, Real intnl) const
+{
+  Real daaa;
+  Real dbbb;
+  dAandB(intnl, daaa, dbbb);
+  return stress.trace() * dbbb - daaa;
+}
+
+RankTwoTensor
+TensorMechanicsPlasticDruckerPrager::flowPotential(const RankTwoTensor & stress, Real intnl) const
+{
+  Real bbb_flow;
+  Bonly(intnl, dilation, bbb_flow);
+  return df_dsig(stress, bbb_flow);
+}
+
+RankFourTensor
+TensorMechanicsPlasticDruckerPrager::dflowPotential_dstress(const RankTwoTensor & stress, Real /*intnl*/) const
+{
+  RankFourTensor dr_dstress;
+  dr_dstress = 0.5 * stress.d2secondInvariant() / std::sqrt(stress.secondInvariant());
+  dr_dstress += -0.5 * 0.5 * stress.dsecondInvariant().outerProduct(stress.dsecondInvariant()) / std::pow(stress.secondInvariant(), 1.5);
+  return dr_dstress;
+}
+
+RankTwoTensor
+TensorMechanicsPlasticDruckerPrager::dflowPotential_dintnl(const RankTwoTensor & stress, Real intnl) const
+{
+  Real dbbb;
+  dBonly(intnl, dilation, dbbb);
+  return stress.dtrace() * dbbb;
+}
+
+
+std::string
+TensorMechanicsPlasticDruckerPrager::modelName() const
+{
+  return "DruckerPrager";
+}
+
+
+void
+TensorMechanicsPlasticDruckerPrager::AandB(Real intnl, Real & aaa, Real & bbb) const
+{
+  if (_zero_cohesion_hardening && _zero_phi_hardening)
+  {
+    aaa = _aaa;
+    bbb = _bbb;
+    return;
+  }
+  initializeAandB(intnl, aaa, bbb);
+}
+
+void
+TensorMechanicsPlasticDruckerPrager::Bonly(Real intnl, int fd, Real & bbb) const
+{
+  if (_zero_phi_hardening && (fd == friction))
+  {
+    bbb = _bbb;
+    return;
+  }
+  if (_zero_psi_hardening && (fd == dilation))
+  {
+    bbb = _bbb_flow;
+    return;
+  }
+  initializeB(intnl, fd, bbb);
+}
+
+
+void
+TensorMechanicsPlasticDruckerPrager::dBonly(Real intnl, int fd, Real & dbbb) const
+{
+  if (_zero_phi_hardening && (fd == friction))
+  {
+    dbbb = 0;
+    return;
+  }
+  if (_zero_psi_hardening && (fd == dilation))
+  {
+    dbbb = 0;
+    return;
+  }
+  const Real sin = (fd == friction) ? std::sin(_mc_phi.value(intnl)) : std::sin(_mc_psi.value(intnl));
+  const Real dsin = (fd == friction) ? std::cos(_mc_phi.value(intnl)) * _mc_phi.derivative(intnl) : std::cos(_mc_psi.value(intnl)) * _mc_psi.derivative(intnl);
+  switch (_mc_interpolation_scheme)
+  {
+  case 0: // outer_tip
+    dbbb = 2.0 / std::sqrt(3.0) * (dsin / (3.0 - sin) + sin * dsin / std::pow(3.0 - sin, 2));
+    break;
+  case 1: // inner_tip
+    dbbb = 2.0 / std::sqrt(3.0) * (dsin / (3.0 + sin) - sin * dsin / std::pow(3.0 + sin, 2));
+    break;
+  case 2: // lode_zero
+    dbbb = dsin / 3.0;
+    break;
+  case 3: // inner_edge
+    dbbb = dsin / std::sqrt(9.0 + 3.0 * std::pow(sin, 2)) - 3 * sin * sin * dsin / std::pow(9.0 + 3.0 * std::pow(sin, 2), 1.5);
+    break;
+  case 4: // native
+    const Real cos = (fd == friction) ? std::cos(_mc_phi.value(intnl)) : std::cos(_mc_psi.value(intnl));
+    const Real dcos = (fd == friction) ? -std::sin(_mc_phi.value(intnl)) * _mc_phi.derivative(intnl) : -std::sin(_mc_psi.value(intnl)) * _mc_psi.derivative(intnl);
+    dbbb = dsin / cos - sin * dcos / std::pow(cos, 2);
+    break;
+  }
+}
+
+
+void
+TensorMechanicsPlasticDruckerPrager::dAandB(Real intnl, Real & daaa, Real & dbbb) const
+{
+  if (_zero_cohesion_hardening && _zero_phi_hardening)
+  {
+    daaa = 0;
+    dbbb = 0;
+    return;
+  }
+
+  const Real C = _mc_cohesion.value(intnl);
+  const Real dC = _mc_cohesion.derivative(intnl);
+  const Real cosphi = std::cos(_mc_phi.value(intnl));
+  const Real dcosphi = -std::sin(_mc_phi.value(intnl)) * _mc_phi.derivative(intnl);
+  const Real sinphi = std::sin(_mc_phi.value(intnl));
+  const Real dsinphi = std::cos(_mc_phi.value(intnl)) * _mc_phi.derivative(intnl);
+  switch (_mc_interpolation_scheme)
+  {
+  case 0: // outer_tip
+    daaa = 2.0 * std::sqrt(3.0) * (dC * cosphi / (3.0 - sinphi) + C * dcosphi / (3.0 - sinphi) + C * cosphi * dsinphi / std::pow(3.0 - sinphi, 2));
+    dbbb = 2.0 / std::sqrt(3.0) * (dsinphi / (3.0 - sinphi) + sinphi * dsinphi / std::pow(3.0 - sinphi, 2));
+    break;
+  case 1: // inner_tip
+    daaa = 2.0 * std::sqrt(3.0) * (dC * cosphi / (3.0 + sinphi) + C * dcosphi / (3.0 + sinphi) - C * cosphi * dsinphi / std::pow(3.0 + sinphi, 2));
+    dbbb = 2.0 / std::sqrt(3.0) * (dsinphi / (3.0 + sinphi) - sinphi * dsinphi / std::pow(3.0 + sinphi, 2));
+    break;
+  case 2: // lode_zero
+    daaa = dC * cosphi + C * dcosphi;
+    dbbb = dsinphi / 3.0;
+    break;
+  case 3: // inner_edge
+    daaa = 3.0 * dC * cosphi / std::sqrt(9.0 + 3.0 * std::pow(sinphi, 2)) + 3.0 * C * dcosphi / std::sqrt(9.0 + 3.0 * std::pow(sinphi, 2)) - 3.0 * C * cosphi * 3.0 * sinphi * dsinphi / std::pow(9.0 + 3.0 * std::pow(sinphi, 2), 1.5);
+    dbbb = dsinphi / std::sqrt(9.0 + 3.0 * std::pow(sinphi, 2)) - 3.0 * sinphi * sinphi * dsinphi / std::pow(9.0 + 3.0 * std::pow(sinphi, 2), 1.5);
+    break;
+  case 4: // native
+    daaa = dC;
+    dbbb = dsinphi / cosphi - sinphi * dcosphi / std::pow(cosphi, 2);
+    break;
+  }
+}
+
+
+void
+TensorMechanicsPlasticDruckerPrager::initializeAandB(Real intnl, Real & aaa, Real & bbb) const
+{
+  const Real C = _mc_cohesion.value(intnl);
+  const Real cosphi = std::cos(_mc_phi.value(intnl));
+  const Real sinphi = std::sin(_mc_phi.value(intnl));
+  switch (_mc_interpolation_scheme)
+  {
+  case 0: // outer_tip
+    aaa = 2.0 * std::sqrt(3.0) * C * cosphi / (3.0 - sinphi);
+    bbb = 2.0 * sinphi / std::sqrt(3.0) / (3.0 - sinphi);
+    break;
+  case 1: // inner_tip
+    aaa = 2.0 * std::sqrt(3.0) * C * cosphi / (3.0 + sinphi);
+    bbb = 2.0 * sinphi / std::sqrt(3.0) / (3.0 + sinphi);
+    break;
+  case 2: // lode_zero
+    aaa = C * cosphi;
+    bbb = sinphi / 3.0;
+    break;
+  case 3: // inner_edge
+    aaa = 3.0 * C * cosphi / std::sqrt(9.0 + 3.0 * std::pow(sinphi, 2));
+    bbb = sinphi / std::sqrt(9.0 + 3.0 * std::pow(sinphi, 2));
+    break;
+  case 4: // native
+    aaa = C;
+    bbb = sinphi / cosphi;
+    break;
+  }
+}
+
+void
+TensorMechanicsPlasticDruckerPrager::initializeB(Real intnl, int fd, Real & bbb) const
+{
+  const Real sin = (fd == friction) ? std::sin(_mc_phi.value(intnl)) : std::sin(_mc_psi.value(intnl));
+  switch (_mc_interpolation_scheme)
+  {
+  case 0: // outer_tip
+    bbb = 2.0 * sin / std::sqrt(3.0) / (3.0 - sin);
+    break;
+  case 1: // inner_tip
+    bbb = 2.0 * sin / std::sqrt(3.0) / (3.0 + sin);
+    break;
+  case 2: // lode_zero
+    bbb = sin / 3.0;
+    break;
+  case 3: // inner_edge
+    bbb = sin / std::sqrt(9.0 + 3.0 * std::pow(sin, 2));
+    break;
+  case 4: // native
+    const Real cos = (fd == friction) ? std::cos(_mc_phi.value(intnl)) : std::cos(_mc_psi.value(intnl));
+    bbb = sin / cos;
+    break;
+  }
+}
+
+
