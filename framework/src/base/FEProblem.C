a3a632d78b6d1c4ee17d7e2b27466861bb18e44a
Daniel Schwen <daniel.schwen@inl.gov>
2016-03-13 16:03:33 -0600

    Exempt unresticted zero properties from check (#6360)

--- framework/src/base/FEProblem.C
+++ framework/src/base/FEProblem.C
@@ -3762,8 +3762,9 @@ FEProblem::checkDependMaterialsHelper(const std::map<SubdomainID, std::vector<Mo
       }
     }
 
-    // Add zero material propertied for this block
+    // Add zero material properties specific to this block and unrestricted
     block_supplied_props.insert(_zero_block_material_props[j->first].begin(), _zero_block_material_props[j->first].end());
+    block_supplied_props.insert(_zero_block_material_props[Moose::ANY_BLOCK_ID].begin(), _zero_block_material_props[Moose::ANY_BLOCK_ID].end());
 
     // Error check to make sure all properties consumed by materials are supplied on this block
     std::set<std::string> difference;
