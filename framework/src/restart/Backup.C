1a316ca05163692e4cd1d5c590f88ba1ee09a5b7
Cody Permann <codypermann@gmail.com>
2016-04-06 08:04:41 -0600

    fixup! Fixup tests with improved error message

--- framework/src/restart/Backup.C
+++ framework/src/restart/Backup.C
@@ -26,7 +26,7 @@ Backup::Backup()
 
   _restartable_data.resize(n_threads);
 
-  for (unsigned int i=0; i < n_threads; i++)
+  for (unsigned int i = 0; i < n_threads; ++i)
     _restartable_data[i] = new std::stringstream;
 }
 
@@ -34,6 +34,6 @@ Backup::~Backup()
 {
   unsigned int n_threads = libMesh::n_threads();
 
-  for (unsigned int i=0; i < n_threads; i++)
+  for (unsigned int i = 0; i < n_threads; ++i)
     delete _restartable_data[i];
 }
