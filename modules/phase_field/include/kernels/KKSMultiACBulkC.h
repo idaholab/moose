2394a52f62ee010df058f512569e32478128cf4b
Larry Aagesen <LarryAagesen@inl.gov>
2016-04-12 17:14:43 -0600

    Add phase concentrations to KKSMultiACBulkC Ref #6705

--- modules/phase_field/include/kernels/KKSMultiACBulkC.h
+++ modules/phase_field/include/kernels/KKSMultiACBulkC.h
@@ -20,7 +20,7 @@ InputParameters validParams<KKSMultiACBulkC>();
  * \f$ \frac{dh}{d\eta}\frac{dF_a}{dc_a}(c_a-c_b) \f$
  * in the the Allen-Cahn bulk residual.
  *
- * The non-linear variable for this Kernel is the order parameter 'eta'.
+ * The non-linear variable for this Kernel is the order parameter 'eta_i'.
  */
 class KKSMultiACBulkC : public KKSACBulkBase
 {
@@ -31,31 +31,34 @@ protected:
   virtual Real computeDFDOP(PFFunctionType type);
   virtual Real computeQpOffDiagJacobian(unsigned int jvar);
 
+  const unsigned int _ncj;
+
   /// phase a concentration
-  std::string _ca_name;
-  unsigned int _ca_var;
-  const VariableValue & _ca;
+  // std::string _ca_name;
+  // unsigned int _ca_var;
+  std::vector<const VariableValue *> _vals;
+  std::vector<unsigned int> _vals_var;
 
-  /// phase b concentration
-  std::string _cb_name;
-  unsigned int _cb_var;
-  const VariableValue & _cb;
-
-  /// Value of the switching function \f$ h(\eta) \f$
-  const MaterialProperty<Real> & _prop_h;
-
-  /// Derivative of the free energy function \f$ \frac d{dc_a} F_a \f$
-  const MaterialProperty<Real> & _prop_dFadca;
-
-  /// Second derivative of the free energy function \f$ \frac {d^2}{dc_a^2} F_a \f$
-  const MaterialProperty<Real> & _prop_d2Fadca2;
-
-  /// Second derivative of the free energy function \f$ \frac {d^2}{dc_b^2} F_b \f$
-  const MaterialProperty<Real> & _prop_d2Fbdcb2;
-
-  /// Mixed partial derivatives of the free energy function wrt ca and
-  /// any other coupled variables \f$ \frac {d^2}{dc_a dq} F_a \f$
-  std::vector<const MaterialProperty<Real>* > _prop_d2Fadcadarg;
+  // /// phase b concentration
+  // std::string _cb_name;
+  // unsigned int _cb_var;
+  // const VariableValue & _cb;
+  //
+  // /// Value of the switching function \f$ h(\eta) \f$
+  // const MaterialProperty<Real> & _prop_h;
+  //
+  // /// Derivative of the free energy function \f$ \frac d{dc_a} F_a \f$
+  // const MaterialProperty<Real> & _prop_dFadca;
+  //
+  // /// Second derivative of the free energy function \f$ \frac {d^2}{dc_a^2} F_a \f$
+  // const MaterialProperty<Real> & _prop_d2Fadca2;
+  //
+  // /// Second derivative of the free energy function \f$ \frac {d^2}{dc_b^2} F_b \f$
+  // const MaterialProperty<Real> & _prop_d2Fbdcb2;
+  //
+  // /// Mixed partial derivatives of the free energy function wrt ca and
+  // /// any other coupled variables \f$ \frac {d^2}{dc_a dq} F_a \f$
+  // std::vector<const MaterialProperty<Real>* > _prop_d2Fadcadarg;
 };
 
 #endif //KKSMULTIACBULKC_H
--- modules/phase_field/src/kernels/KKSMultiACBulkC.C
+++ modules/phase_field/src/kernels/KKSMultiACBulkC.C
@@ -10,53 +10,55 @@ template<>
 InputParameters validParams<KKSMultiACBulkC>()
 {
   InputParameters params = validParams<KKSACBulkBase>();
-  params.addClassDescription("KKS model kernel (part 2 of 2) for the Bulk Allen-Cahn. This includes all terms dependent on chemical potential.");
-  params.addRequiredCoupledVar("ca", "a-phase concentration");
-  params.addRequiredCoupledVar("cb", "b-phase concentration");
+  params.addClassDescription("Multi-phase KKS model kernel (part 2 of 2) for the Bulk Allen-Cahn. This includes all terms dependent on chemical potential.");
+  params.addRequiredCoupledVar("cj", "Array of phase concentrations cj. Place in same order as Fj_names!");
   return params;
 }
 
 KKSMultiACBulkC::KKSMultiACBulkC(const InputParameters & parameters) :
     KKSACBulkBase(parameters),
-    _ca_name(getVar("ca", 0)->name()),
-    _ca_var(coupled("ca")),
-    _ca(coupledValue("ca")),
-    _cb_name(getVar("cb", 0)->name()),
-    _cb_var(coupled("cb")),
-    _cb(coupledValue("cb")),
-    _prop_h(getMaterialProperty<Real>("h")),
-    _prop_dFadca(getMaterialPropertyDerivative<Real>("fa_name", _ca_name)),
-    _prop_d2Fadca2(getMaterialPropertyDerivative<Real>("fa_name", _ca_name, _ca_name)),
-    _prop_d2Fbdcb2(getMaterialPropertyDerivative<Real>("fb_name", _cb_name, _cb_name))
+    _ncj(coupledComponents("cj")),
+    _vals(_ncj),
+    _vals_var(_ncj)
+    // _ca_name(getVar("ca", 0)->name()),
+    // _ca_var(coupled("ca")),
+    // _ca(coupledValue("ca")),
+    // _cb_name(getVar("cb", 0)->name()),
+    // _cb_var(coupled("cb")),
+    // _cb(coupledValue("cb")),
+    // _prop_h(getMaterialProperty<Real>("h")),
+    // _prop_dFadca(getMaterialPropertyDerivative<Real>("fa_name", _ca_name)),
+    // _prop_d2Fadca2(getMaterialPropertyDerivative<Real>("fa_name", _ca_name, _ca_name)),
+    // _prop_d2Fbdcb2(getMaterialPropertyDerivative<Real>("fb_name", _cb_name, _cb_name))
 {
-  //Resize to number of coupled variables (_nvar from KKSACBulkBase constructor)
-  _prop_d2Fadcadarg.resize(_nvar);
-
-  // Iterate over all coupled variables
-  for (unsigned int i = 0; i < _nvar; ++i)
-  {
-    MooseVariable *cvar = _coupled_moose_vars[i];
-
-    // get second partial derivatives wrt ca and other coupled variable
-    _prop_d2Fadcadarg[i] = &getMaterialPropertyDerivative<Real>("fa_name", _ca_name, cvar->name());
-  }
+  // //Resize to number of coupled variables (_nvar from KKSACBulkBase constructor)
+  // _prop_d2Fadcadarg.resize(_nvar);
+  //
+  // // Iterate over all coupled variables
+  // for (unsigned int i = 0; i < _nvar; ++i)
+  // {
+  //   MooseVariable *cvar = _coupled_moose_vars[i];
+  //
+  //   // get second partial derivatives wrt ca and other coupled variable
+  //   _prop_d2Fadcadarg[i] = &getMaterialPropertyDerivative<Real>("fa_name", _ca_name, cvar->name());
+  // }
 }
 
 Real
 KKSMultiACBulkC::computeDFDOP(PFFunctionType type)
 {
   Real res = 0.0;
-  Real A1 = _prop_dFadca[_qp] * (_ca[_qp] - _cb[_qp]);
+  // Real A1 = _prop_dFadca[_qp] * (_ca[_qp] - _cb[_qp]);
 
   switch (type)
   {
     case Residual:
-      return _prop_dh[_qp] * A1;
+      return res;
 
     case Jacobian:
-      res =   _prop_d2h[_qp] * A1;
-
-      return _phi[_j][_qp] * res;
+      // res =   _prop_d2h[_qp] * A1;
+      //
+      return res;
   }
 
   mooseError("Invalid type passed in");
@@ -68,32 +70,32 @@ KKSMultiACBulkC::computeQpOffDiagJacobian(unsigned int jvar)
   // first get dependence of mobility _L on other variables using parent class
   // member function
   Real res = ACBulk<Real>::computeQpOffDiagJacobian(jvar);
-  // Then add dependence of KKSACBulkF on other variables
-  // Treat ca and cb specially, as they appear in the residual
-  if (jvar == _ca_var)
-  {
-    res += _L[_qp] * _prop_dh[_qp] * (
-        (_ca[_qp] - _cb[_qp]) * _prop_d2Fadca2[_qp] + _prop_dFadca[_qp]
-        ) * _phi[_j][_qp] * _test[_i][_qp];
-
-    return res;
-  }
-
-  if (jvar == _cb_var)
-  {
-    res -= _L[_qp] * _prop_dh[_qp] * _prop_dFadca[_qp]
-              * _phi[_j][_qp] * _test[_i][_qp];
-
-    return res;
-  }
-
-  //  for all other vars get the coupled variable jvar is referring to
-  unsigned int cvar;
-  if (!mapJvarToCvar(jvar, cvar))
-    return res;
-
-  res += _L[_qp] * _prop_dh[_qp] * (*_prop_d2Fadcadarg[cvar])[_qp]
-            * (_ca[_qp] - _cb[_qp]) * _phi[_j][_qp]  * _test[_i][_qp];
-
+  // // Then add dependence of KKSACBulkF on other variables
+  // // Treat ca and cb specially, as they appear in the residual
+  // if (jvar == _ca_var)
+  // {
+  //   res += _L[_qp] * _prop_dh[_qp] * (
+  //       (_ca[_qp] - _cb[_qp]) * _prop_d2Fadca2[_qp] + _prop_dFadca[_qp]
+  //       ) * _phi[_j][_qp] * _test[_i][_qp];
+  //
+  //   return res;
+  // }
+  //
+  // if (jvar == _cb_var)
+  // {
+  //   res -= _L[_qp] * _prop_dh[_qp] * _prop_dFadca[_qp]
+  //             * _phi[_j][_qp] * _test[_i][_qp];
+  //
+  //   return res;
+  // }
+  //
+  // //  for all other vars get the coupled variable jvar is referring to
+  // unsigned int cvar;
+  // if (!mapJvarToCvar(jvar, cvar))
+  //   return res;
+  //
+  // res += _L[_qp] * _prop_dh[_qp] * (*_prop_d2Fadcadarg[cvar])[_qp]
+  //           * (_ca[_qp] - _cb[_qp]) * _phi[_j][_qp]  * _test[_i][_qp];
+  //
   return res;
 }
