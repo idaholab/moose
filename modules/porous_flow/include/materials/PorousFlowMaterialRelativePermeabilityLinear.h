2807f4ada66576db53bb1368a952273760b442b3
Chris Green <chris.green@csiro.au>
2016-04-18 09:47:19 +1000

    Create base class for relative permeability materials and update constant and linear relative permeability materials

--- modules/porous_flow/include/materials/PorousFlowMaterialRelativePermeabilityLinear.h
+++ modules/porous_flow/include/materials/PorousFlowMaterialRelativePermeabilityLinear.h
@@ -9,10 +9,7 @@
 #ifndef POROUSFLOWMATERIALRELATIVEPERMEABILITYLINEAR_H
 #define POROUSFLOWMATERIALRELATIVEPERMEABILITYLINEAR_H
 
-#include "DerivativeMaterialInterface.h"
-#include "Material.h"
-
-#include "PorousFlowDictator.h"
+#include "PorousFlowMaterialRelativePermeabilityBase.h"
 
 //Forward Declarations
 class PorousFlowMaterialRelativePermeabilityLinear;
@@ -23,7 +20,7 @@ InputParameters validParams<PorousFlowMaterialRelativePermeabilityLinear>();
 /**
  * Linear relative permeability (equal to the phase saturation)
  */
-class PorousFlowMaterialRelativePermeabilityLinear : public DerivativeMaterialInterface<Material>
+class PorousFlowMaterialRelativePermeabilityLinear : public PorousFlowMaterialRelativePermeabilityBase
 {
 public:
   PorousFlowMaterialRelativePermeabilityLinear(const InputParameters & parameters);
@@ -31,19 +28,6 @@ public:
 protected:
 
   virtual void computeQpProperties();
-
-  /// The variable names UserObject for the PorousFlow variables
-  const PorousFlowDictator & _dictator_UO;
-  /// Phase number of fluid that this relative permeability relates to
-  const unsigned int _phase_num;
-  /// Name of (dummy) saturation primary variable
-  VariableName _saturation_variable_name;
-  /// Relative permeability material property
-  MaterialProperty<Real> & _relative_permeability;
-  /// Derivative of relative permeability wrt phase saturation
-  MaterialProperty<Real> & _drelative_permeability_ds;
-  /// Saturation material property
-  const MaterialProperty<std::vector<Real> > & _saturation;
 };
 
 #endif //POROUSFLOWMATERIALRELATIVEPERMEABILITYLINEAR_H
--- modules/porous_flow/src/materials/PorousFlowMaterialRelativePermeabilityLinear.C
+++ modules/porous_flow/src/materials/PorousFlowMaterialRelativePermeabilityLinear.C
@@ -7,34 +7,25 @@
 
 
 #include "PorousFlowMaterialRelativePermeabilityLinear.h"
-#include "Conversion.h"
 
 template<>
 InputParameters validParams<PorousFlowMaterialRelativePermeabilityLinear>()
 {
-  InputParameters params = validParams<Material>();
-  params.addRequiredParam<UserObjectName>("PorousFlowDictator_UO", "The UserObject that holds the list of Porous-Flow variable names.");
-  params.addRequiredParam<unsigned int>("phase", "The phase number");
+  InputParameters params = validParams<PorousFlowMaterialRelativePermeabilityBase>();
   params.addClassDescription("This Material provides a linear relative permeability");
   return params;
 }
 
 PorousFlowMaterialRelativePermeabilityLinear::PorousFlowMaterialRelativePermeabilityLinear(const InputParameters & parameters) :
-    DerivativeMaterialInterface<Material>(parameters),
-
-  _dictator_UO(getUserObject<PorousFlowDictator>("PorousFlowDictator_UO")),
-  _phase_num(getParam<unsigned int>("phase")),
-  _saturation_variable_name("saturation_varname"),
-  _relative_permeability(declareProperty<Real>("PorousFlow_relative_permeability" + Moose::stringify(_phase_num))),
-  _drelative_permeability_ds(declarePropertyDerivative<Real>("PorousFlow_relative_permeability" + Moose::stringify(_phase_num), _saturation_variable_name)),
-  _saturation(getMaterialProperty<std::vector<Real> >("PorousFlow_saturation"))
+  PorousFlowMaterialRelativePermeabilityBase(parameters)
 {
 }
 
 void
 PorousFlowMaterialRelativePermeabilityLinear::computeQpProperties()
 {
-  /// The relative permeability is equal to the phase saturation
+  /// The relative permeability is equal to the phase saturation and its derivative
+  /// wrt saturation is 1
   _relative_permeability[_qp] = _saturation[_qp][_phase_num];
   _drelative_permeability_ds[_qp] = 1.0;
 }
