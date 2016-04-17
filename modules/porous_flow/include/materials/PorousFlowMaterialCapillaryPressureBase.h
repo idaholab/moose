eed7a3e4ff2e792dc7198ebff106e657eba52777
Chris Green <chris.green@csiro.au>
2016-04-18 09:50:25 +1000

    Add base class for capillary pressure materials, and implement constant and linear versions

---
+++ modules/porous_flow/include/materials/PorousFlowMaterialCapillaryPressureBase.h
@@ -0,0 +1,47 @@
+/****************************************************************/
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*          All contents are licensed under LGPL V2.1           */
+/*             See LICENSE for full restrictions                */
+/****************************************************************/
+
+
+#ifndef POROUSFLOWMATERIALCAPILLARYPRESSUREBASE_H
+#define POROUSFLOWMATERIALCAPILLARYPRESSUREBASE_H
+
+#include "DerivativeMaterialInterface.h"
+#include "Material.h"
+#include "PorousFlowDictator.h"
+
+class PorousFlowMaterialCapillaryPressureBase;
+
+template<>
+InputParameters validParams<PorousFlowMaterialCapillaryPressureBase>();
+
+/**
+ * Base class for relative permeability materials
+ */
+class PorousFlowMaterialCapillaryPressureBase : public DerivativeMaterialInterface<Material>
+{
+public:
+  PorousFlowMaterialCapillaryPressureBase(const InputParameters & parameters);
+
+protected:
+
+  virtual void computeQpProperties();
+
+  /// Phase number of fluid that this relative permeability relates to
+  const unsigned int _phase_num;
+  /// Saturation material property
+  const MaterialProperty<std::vector<Real> > & _saturation;
+  /// Name of (dummy) saturation primary variable
+  VariableName _saturation_variable_name;
+  /// Capillary pressure material property
+  MaterialProperty<Real> & _capillary_pressure;
+  /// Derivative of capillary pressure wrt phase saturation
+  MaterialProperty<Real> & _dcapillary_pressure_ds;
+  /// The PorousFlowDictator UserObject
+  const PorousFlowDictator & _dictator_UO;
+};
+
+#endif //POROUSFLOWMATERIALCAPILLARYPRESSUREBASE_H
