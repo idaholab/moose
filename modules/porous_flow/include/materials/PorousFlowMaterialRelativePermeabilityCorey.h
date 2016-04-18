8bbf1d7b8ce92d5d13ac5c5cc0bee976591a6a79
Chris Green <chris.green@csiro.au>
2016-04-18 15:34:14 +1000

    Update Corey relperm to use derivative material interface base class

--- modules/porous_flow/include/materials/PorousFlowMaterialRelativePermeabilityCorey.h
+++ modules/porous_flow/include/materials/PorousFlowMaterialRelativePermeabilityCorey.h
@@ -9,50 +9,28 @@
 #ifndef PORFLOWMATERIALRELATIVEPERMEABILITYCOREY_H
 #define PORFLOWMATERIALRELATIVEPERMEABILITYCOREY_H
 
-#include "DerivativeMaterialInterface.h"
-#include "Material.h"
+#include "PorousFlowMaterialRelativePermeabilityBase.h"
 
-#include "PorousFlowDictator.h"
-
-//Forward Declarations
 class PorousFlowMaterialRelativePermeabilityCorey;
 
 template<>
 InputParameters validParams<PorousFlowMaterialRelativePermeabilityCorey>();
 
 /**
- * Material designed to calculate Corey-type relative permeability of an arbitrary phase given the saturation and 
+ * Material designed to calculate Corey-type relative permeability of an arbitrary phase given the saturation and
  * Corey exponent of that phase.
  */
-class PorousFlowMaterialRelativePermeabilityCorey : public DerivativeMaterialInterface<Material>
+class PorousFlowMaterialRelativePermeabilityCorey : public PorousFlowMaterialRelativePermeabilityBase
 {
 public:
   PorousFlowMaterialRelativePermeabilityCorey(const InputParameters & parameters);
 
 protected:
 
+  virtual void computeQpProperties();
+
   /// Core exponent n_j for phase Sj
   const Real _n;
-
-  /// phase number of fluid that we're dealing with
-  const unsigned int _phase_num;
-
-  /// The variable names UserObject for the Porous-Flow variables
-  const PorousFlowDictator & _porflow_name_UO;
-
-  /// nodal Saturation of each phase
-  const MaterialProperty<std::vector<Real> > & _saturation;
-
-  /// d(nodal saturation)/d(PorousFlow variable)
-  const MaterialProperty<std::vector<std::vector<Real> > > & _dsaturation_dvar;
-
-  /// computed nodal relative permeability of phase j
-  MaterialProperty<Real> & _relative_permeability;
-
-  /// d(nodal relative permeability)/d(PorousFlow variable)
-  MaterialProperty<std::vector<Real> > & _drelative_permeability_dvar;
-
-  virtual void computeQpProperties();
 };
 
 #endif //PORFLOWMATERIALRELATIVEPERMEABILITYCOREY_H
