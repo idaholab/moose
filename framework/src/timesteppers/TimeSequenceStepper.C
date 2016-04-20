bb3323e7841354df5bd277b61673e5811c8ba62b
Daniel Schwen <daniel.schwen@inl.gov>
2016-04-20 08:47:48 -0600

    Factor out time sequence stepper base class (#6795)

--- framework/src/timesteppers/TimeSequenceStepper.C
+++ framework/src/timesteppers/TimeSequenceStepper.C
@@ -13,92 +13,18 @@
 /****************************************************************/
 
 #include "TimeSequenceStepper.h"
-#include "FEProblem.h"
-#include "Transient.h"
 
 template<>
 InputParameters validParams<TimeSequenceStepper>()
 {
-  InputParameters params = validParams<TimeStepper>();
+  InputParameters params = validParams<TimeSequenceStepperBase>();
   params.addRequiredParam<std::vector<Real> >("time_sequence", "The values of t");
   params.addClassDescription("Solves the Transient problem at a sequence of given time points.");
   return params;
 }
 
 TimeSequenceStepper::TimeSequenceStepper(const InputParameters & parameters) :
-    TimeStepper(parameters),
-    _current_step(declareRestartableData("current_step", (unsigned int) 0)),
-    _time_sequence(declareRestartableData<std::vector<Real> >("time_sequence"))
+    TimeSequenceStepperBase(parameters)
 {
-  // In case of half transient, transient's end time needs to be reset to
-  // be able to imprint TimeSequenceStepper's end time
-  if (_app.halfTransient())
-    _executioner.endTime() *= 2.0;
-
-  // only set up _time_sequence if the app is _not_ restarting or recovering
-  if (!_app.isRecovering() && ! _app.isRestarting())
-  {
-    // sync _executioner.startTime and endTime with _time_sequence
-    Real start_time = _executioner.getStartTime();
-    Real end_time = _executioner.endTime();
-
-    // make sure time sequence is in ascending order
-    std::vector<Real> times = getParam<std::vector<Real> >("time_sequence");
-    for (unsigned int j = 0; j < times.size() - 1; j++)
-      if (times[j + 1] <= times[j])
-        mooseError("time_sequence must be in ascending order.");
-
-    _time_sequence.push_back(start_time);
-    for (unsigned int j = 0; j < times.size(); j++)
-    {
-      if (times[j] > start_time && times[j] < end_time)
-        _time_sequence.push_back(times[j]);
-    }
-    _time_sequence.push_back(end_time);
-  }
-
-  if (_app.halfTransient())
-  {
-    unsigned int half = (_time_sequence.size() - 1) / 2;
-    _executioner.endTime() = _time_sequence[half];
-  }
-}
-
-void
-TimeSequenceStepper::init()
-{
-}
-
-void
-TimeSequenceStepper::step()
-{
-  TimeStepper::step();
-  if (converged())
-    _current_step++;
-}
-
-Real
-TimeSequenceStepper::computeInitialDT()
-{
-  return computeDT();
-}
-
-Real
-TimeSequenceStepper::computeDT()
-{
-  return _time_sequence[_current_step + 1] - _time_sequence[_current_step];
-}
-
-Real
-TimeSequenceStepper::computeFailedDT()
-{
-  if (computeDT() <= _dt_min)
-    mooseError("Solve failed and timestep already at or below dtmin, cannot continue!");
-
-  // cut the time step in a half if possible
-  Real dt = 0.5 * computeDT();
-  if (dt < _dt_min)
-    dt = _dt_min;
-  _time_sequence.insert(_time_sequence.begin() + _current_step + 1, _time_sequence[_current_step] + dt);
-  return computeDT();
+  setupSequence(getParam<std::vector<Real> >("time_sequence"));
 }
