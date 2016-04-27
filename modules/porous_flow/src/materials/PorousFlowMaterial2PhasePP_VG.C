d84a59ac80490915266223865f3d820016500299
WilkAndy <andrew.wilkins@csiro.au>
2016-04-27 12:26:53 +1000

    Initial porous flow module - C++ code.
    This work is still unpolished, but the functionality is good.
    It is becoming increasingly difficult to work in this MEGA BRANCH
    that we have. So i need to merge into idaholab so we can follow
    the standard incremental approach with issues+PRs.
    
    Daniel fixup regarding libmesh - thanks Daniel.
    
    Fixes #6845

---
+++ modules/porous_flow/src/materials/PorousFlowMaterial2PhasePP_VG.C
@@ -0,0 +1,50 @@
+/****************************************************************/
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*          All contents are licensed under LGPL V2.1           */
+/*             See LICENSE for full restrictions                */
+/****************************************************************/
+
+
+#include "PorousFlowMaterial2PhasePP_VG.h"
+
+template<>
+InputParameters validParams<PorousFlowMaterial2PhasePP_VG>()
+{
+  InputParameters params = validParams<PorousFlowVariableBase>();
+
+  params.addRequiredCoupledVar("phase0_porepressure", "Variable that is the porepressure of phase 0 (eg, the water phase).  It will be <= phase1_porepressure.");
+  params.addRequiredCoupledVar("phase1_porepressure", "Variable that is the porepressure of phase 1 (eg, the gas phase)");
+  params.addRequiredRangeCheckedParam<Real>("al", "al > 0", "van-Genuchten alpha parameter.  Must be positive.  effectiveSaturation = (1 + (-al*P)^(1/(1-m)))^(-m), where P = phase0_porepressure - phase1_porepressure <= 0");
+  params.addRequiredRangeCheckedParam<Real>("m", "m > 0 & m < 1", "van-Genuchten m parameter.  Must be between 0 and 1, and optimally should be set to >0.5   EffectiveSaturation = (1 + (-al*p)^(1/(1-m)))^(-m)");
+  params.addClassDescription("This Material calculates the 2 porepressures and the 2 saturations in a 2-phase isothermal situation, and derivatives of these with respect to the PorousFlowVariables.  A van-Genuchten capillary suction function is assumed");
+  return params;
+}
+
+PorousFlowMaterial2PhasePP_VG::PorousFlowMaterial2PhasePP_VG(const InputParameters & parameters) :
+    PorousFlowMaterial2PhasePP(parameters),
+
+    _al(getParam<Real>("al")),
+    _m(getParam<Real>("m"))
+{
+  if (_dictator_UO.num_phases() != 2)
+    mooseError("The Dictator announces that the number of phases is " << _dictator_UO.num_phases() << " whereas PorousFlowMaterial2PhasePP_VG can only be used for 2-phase simulation.  When you have an efficient government, you have a dictatorship.");
+}
+
+Real
+PorousFlowMaterial2PhasePP_VG::effectiveSaturation(Real pressure) const
+{
+  return PorousFlowEffectiveSaturationVG::seff(pressure, _al, _m);
+}
+
+Real
+PorousFlowMaterial2PhasePP_VG::dEffectiveSaturation_dP(Real pressure) const
+{
+  return PorousFlowEffectiveSaturationVG::dseff(pressure, _al, _m);
+}
+
+Real
+PorousFlowMaterial2PhasePP_VG::d2EffectiveSaturation_dP2(Real pressure) const
+{
+  return PorousFlowEffectiveSaturationVG::d2seff(pressure, _al, _m);
+}
---
+++ modules/porous_flow/src/materials/PorousFlowMaterialRelativePermeabilityBase.C
@@ -0,0 +1,40 @@
+/****************************************************************/
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*          All contents are licensed under LGPL V2.1           */
+/*             See LICENSE for full restrictions                */
+/****************************************************************/
+
+
+#include "PorousFlowMaterialRelativePermeabilityBase.h"
+#include "Conversion.h"
+
+template<>
+InputParameters validParams<PorousFlowMaterialRelativePermeabilityBase>()
+{
+  InputParameters params = validParams<Material>();
+  params.addRequiredParam<unsigned int>("phase", "The phase number");
+  params.addRequiredParam<UserObjectName>("PorousFlowDictator_UO", "The UserObject that holds the list of PorousFlow variable names");
+  params.addClassDescription("Base class for PorousFlow relative permeability materials");
+  return params;
+}
+
+PorousFlowMaterialRelativePermeabilityBase::PorousFlowMaterialRelativePermeabilityBase(const InputParameters & parameters) :
+    DerivativeMaterialInterface<Material>(parameters),
+
+  _phase_num(getParam<unsigned int>("phase")),
+  _dictator_UO(getUserObject<PorousFlowDictator>("PorousFlowDictator_UO")),
+  _saturation_variable_name(_dictator_UO.saturationVariableNameDummy()),
+  _saturation_nodal(getMaterialProperty<std::vector<Real> >("PorousFlow_saturation_nodal")),
+  _relative_permeability(declareProperty<Real>("PorousFlow_relative_permeability" + Moose::stringify(_phase_num))),
+  _drelative_permeability_ds(declarePropertyDerivative<Real>("PorousFlow_relative_permeability" + Moose::stringify(_phase_num), _saturation_variable_name))
+{
+  if (_phase_num >= _dictator_UO.num_phases())
+    mooseError("PorousFlowMaterialRelativePermeability: The Dictator proclaims that the number of fluid phases is " << _dictator_UO.num_phases() << " while you have foolishly entered phase = " << _phase_num << ".  Be aware that the Dictator does not tolerate mistakes.");
+}
+
+void
+PorousFlowMaterialRelativePermeabilityBase::computeQpProperties()
+{
+  mooseError("computeQpProperties() must be overriden in materials derived from PorousFlowMaterialRelativePermeabilityBase");
+}
