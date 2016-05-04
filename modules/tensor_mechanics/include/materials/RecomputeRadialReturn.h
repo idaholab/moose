00f71bb67d271e92051cb69d1778dc7d66b6ab2e
Jason Hales <jason.hales@inl.gov>
2016-05-04 15:40:36 -0600

    Rework the Recompute classes
    This is done so that the new stress is computed based on the current
    elastic strain.  This allows temperature-dependent moduli to affect
    the stress properly.
    
    Genuflected at the Throne of Whitespace.
    
    ref #5481

--- modules/tensor_mechanics/include/materials/RecomputeRadialReturn.h
+++ modules/tensor_mechanics/include/materials/RecomputeRadialReturn.h
@@ -4,42 +4,42 @@
 /*          All contents are licensed under LGPL V2.1           */
 /*             See LICENSE for full restrictions                */
 /****************************************************************/
-#ifndef RECOMPUTERADIALRETURNSTRESSINCREMENT_H
-#define RECOMPUTERADIALRETURNSTRESSINCREMENT_H
+#ifndef RECOMPUTERADIALRETURN_H
+#define RECOMPUTERADIALRETURN_H
 
-#include "RecomputeGeneralReturnStressIncrement.h"
+#include "RecomputeGeneralReturn.h"
 #include "RankTwoTensor.h"
 #include "RankFourTensor.h"
 #include "Conversion.h"
 
 //Forward declaration
-class RecomputeRadialReturnStressIncrement;
+class RecomputeRadialReturn;
 
 template<>
-InputParameters validParams<RecomputeRadialReturnStressIncrement>();
+InputParameters validParams<RecomputeRadialReturn>();
 
 /**
- * RecomputeRadialReturnStressIncrement computes the radial return stress increment for
+ * RecomputeRadialReturn computes the radial return stress increment for
  * an isotropic viscoplasticity plasticity model after interating on the difference
  * between new and old trial stress increments.  This radial return mapping class
  * acts as a base class for the radial return creep and plasticity classes / combinations
  * and inherits from DiscreteMaterial.
- * The stress increment computed by RecomputeRadialReturnStressIncrement is used by
+ * The stress increment computed by RecomputeRadialReturn is used by
  * ComputeRadialReturnMappingStress which computes the elastic stress for finite
  * strains.  This return mapping class is acceptable for finite strains.
  * This class is based on the Elasto-viscoplasticity algorithm in F. Dunne and N.
  * Petrinic's Introduction to Computational Plasticity (2004) Oxford University Press.
  */
 
-class RecomputeRadialReturnStressIncrement : public RecomputeGeneralReturnStressIncrement
+class RecomputeRadialReturn : public RecomputeGeneralReturn
 {
 public:
-  RecomputeRadialReturnStressIncrement(const InputParameters & parameters);
+  RecomputeRadialReturn(const InputParameters & parameters);
 
 protected:
   /// A radial return (J2) mapping method is defined in this material by overwritting
-  /// the computeStressIncrement method.
-  virtual void computeStressIncrement();
+  /// the computeInelasticStrainIncrement method.
+  virtual void computeInelasticStrainIncrement();
 
   virtual void computeStressInitialize(Real /*effectiveTrialStress*/){}
   virtual void iterationInitialize(Real /*scalar*/) {}
@@ -54,4 +54,4 @@ protected:
   const Real _absolute_tolerance;
 };
 
-#endif //RECOMPUTERADIALRETURNSTRESSINCREMENT_H
+#endif //RECOMPUTERADIALRETURN_H
