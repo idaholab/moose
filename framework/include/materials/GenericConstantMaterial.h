930fa423424e3ff64fcd1f5ebe924df0cea3b7c4
Cody Permann <codypermann@gmail.com>
2016-07-28 16:21:02 -0600

    Override in ics and materials
    refs #7171

--- framework/include/materials/GenericConstantMaterial.h
+++ framework/include/materials/GenericConstantMaterial.h
@@ -38,7 +38,7 @@ public:
   GenericConstantMaterial(const InputParameters & parameters);
 
 protected:
-  virtual void computeQpProperties();
+  virtual void computeQpProperties() override;
 
   std::vector<std::string> _prop_names;
   std::vector<Real> _prop_values;
