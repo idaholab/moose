a485f41edff61a1c4658b330073825b2b24cab26
Daniel Schwen <daniel.schwen@inl.gov>
2016-08-01 15:26:11 -0600

    Use new getEnum method (#7459)

--- modules/contact/src/ContactPenetrationVarAction.C
+++ modules/contact/src/ContactPenetrationVarAction.C
@@ -31,13 +31,9 @@ void
 ContactPenetrationVarAction::act()
 {
   if (!_problem->getDisplacedProblem())
-  {
     mooseError("Contact requires updated coordinates.  Use the 'displacements = ...' line in the Mesh block.");
-  }
 
   _problem->addAuxVariable("penetration",
                            FEType(Utility::string_to_enum<Order>(getParam<MooseEnum>("order")),
                                   Utility::string_to_enum<FEFamily>("LAGRANGE")));
-
 }
-
