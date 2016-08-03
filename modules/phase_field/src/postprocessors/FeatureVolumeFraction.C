bef89f215193fa0d96cc4bc0a883121c7f0af834
Daniel Schwen <daniel.schwen@inl.gov>
2016-08-03 09:47:16 -0600

    Fix headers (#7165)

--- modules/phase_field/src/postprocessors/FeatureVolumeFraction.C
+++ modules/phase_field/src/postprocessors/FeatureVolumeFraction.C
@@ -4,6 +4,7 @@
 /*          All contents are licensed under LGPL V2.1           */
 /*             See LICENSE for full restrictions                */
 /****************************************************************/
+
 #include "FeatureVolumeFraction.h"
 #include <cmath>
 
