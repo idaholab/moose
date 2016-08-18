9f83f36e72a0b4bb9f51d2a7de1aa813595ba6b8
John Peterson <jwpeterson@gmail.com>
2016-08-18 12:42:39 -0600

    Add abs_max/min option to TimeExtremeValue.
    This is useful for e.g. tracking the infinity (max) norm of a SCALAR
    ODE variable over time.
    
    Refs #5701.

--- framework/src/postprocessors/TimeExtremeValue.C
+++ framework/src/postprocessors/TimeExtremeValue.C
@@ -21,13 +21,18 @@ template<>
 InputParameters validParams<TimeExtremeValue>()
 {
   // Define the min/max enumeration
-  MooseEnum type_options("max=0 min=1", "max");
+  MooseEnum type_options("max=0 min=1 abs_max=2 abs_min=3", "max");
 
   // Define the parameters
   InputParameters params = validParams<GeneralPostprocessor>();
-  params.addParam<MooseEnum>("value_type", type_options, "Type of extreme value to return. 'max' returns the maximum value. 'min' returns the minimum value.");
+  params.addParam<MooseEnum>("value_type", type_options,
+                             "Type of extreme value to return."
+                             "'max' returns the maximum value."
+                             "'min' returns the minimum value."
+                             "'abs_max' returns the maximum absolute value."
+                             "'abs_min' returns the minimum absolute value.");
   params.addRequiredParam<PostprocessorName>("postprocessor", "The name of the postprocessor used for reporting time extreme values");
-  params.addClassDescription("A postprocessor for reporting the max/min value of another postprocessor over time.");
+  params.addClassDescription("A postprocessor for reporting the extreme value of another postprocessor over time.");
 
   return params;
 }
@@ -39,7 +44,31 @@ TimeExtremeValue::TimeExtremeValue(const InputParameters & parameters) :
     _value(declareRestartableData<Real>("value"))
 {
   if (!_app.isRecovering())
-    _value = (_type == 0) ? -std::numeric_limits<Real>::max() : std::numeric_limits<Real>::max();
+  {
+    switch (_type)
+    {
+      case MAX:
+        _value = -std::numeric_limits<Real>::max();
+        break;
+
+      case MIN:
+        _value = std::numeric_limits<Real>::max();
+        break;
+
+      case ABS_MAX:
+         // the max absolute value of anything is greater than or equal to 0
+        _value = 0;
+        break;
+
+      case ABS_MIN:
+        // the min absolute value of anything is less than this
+        _value = std::numeric_limits<Real>::max();
+        break;
+
+      default:
+        mooseError("Unrecognzed _type == " << _type);
+    }
+  }
 }
 
 void
@@ -54,6 +83,17 @@ TimeExtremeValue::execute()
     case MIN:
       _value = std::min(_value, _postprocessor);
       break;
+
+    case ABS_MAX:
+      _value = std::max(_value, std::abs(_postprocessor));
+      break;
+
+    case ABS_MIN:
+      _value = std::min(_value, std::abs(_postprocessor));
+      break;
+
+    default:
+      mooseError("Unrecognzed _type == " << _type);
   }
 }
 
