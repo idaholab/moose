7485b281a53ba868c7735f0697e41fa22573b662
Larry Aagesen <LarryAagesen@inl.gov>
2016-09-29 14:51:52 -0600

    Header and source files for KKSMultiFreeEnergy aux kernel Ref #7798

---
+++ modules/phase_field/include/auxkernels/KKSMultiFreeEnergy.h
@@ -0,0 +1,57 @@
+/****************************************************************/
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*          All contents are licensed under LGPL V2.1           */
+/*             See LICENSE for full restrictions                */
+/****************************************************************/
+#ifndef KKSMULTIFREEENERGY_H
+#define KKSMULTIFREEENERGY_H
+
+#include "TotalFreeEnergyBase.h"
+#include "Material.h"
+
+//Forward Declarations
+class KKSMultiFreeEnergy;
+
+template<>
+InputParameters validParams<KKSMultiFreeEnergy>();
+
+/**
+ * Compute the free energy in the multi-phase KKS Model
+ * \f$ F = \sum_i h_i F_i +  + wg_i + \frac{\kappa}{2}|\eta_i|^2 \f$
+ */
+class KKSMultiFreeEnergy : public TotalFreeEnergyBase
+{
+public:
+  KKSMultiFreeEnergy(const InputParameters & parameters);
+
+protected:
+  virtual Real computeValue();
+
+  /// Names of free energy functions for each phase \f$ F_j \f$
+  std::vector<MaterialPropertyName> _Fj_names;
+  unsigned int _num_j;
+
+  /// Values of the free energy functions for each phase \f$ F_j \f$
+  std::vector<const MaterialProperty<Real> *> _prop_Fj;
+
+  /// switching function names
+  std::vector<MaterialPropertyName> _hj_names;
+
+  /// Values of the switching functions for each phase \f$ h_j \f$
+  std::vector<const MaterialProperty<Real> *> _prop_hj;
+
+  /// switching function names
+  std::vector<MaterialPropertyName> _gj_names;
+
+  /// Values of the switching functions for each phase \f$ h_j \f$
+  std::vector<const MaterialProperty<Real> *> _prop_gj;
+
+  /// Barrier term height
+  const Real _w;
+
+  /// Gradient interface free energy coefficients
+  std::vector<const MaterialProperty<Real> *> _kappas;
+};
+
+#endif //KKSMULTIFREEENERGY_H
---
+++ modules/phase_field/src/auxkernels/KKSMultiFreeEnergy.C
@@ -0,0 +1,71 @@
+/****************************************************************/
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*          All contents are licensed under LGPL V2.1           */
+/*             See LICENSE for full restrictions                */
+/****************************************************************/
+#include "KKSMultiFreeEnergy.h"
+
+template<>
+InputParameters validParams<KKSMultiFreeEnergy>()
+{
+  InputParameters params = validParams<TotalFreeEnergyBase>();
+  params.addClassDescription("Total free energy in multi-phase KKS system, including chemical, barrier and gradient terms");
+  params.addRequiredParam<std::vector<MaterialPropertyName> >("Fj_names", "List of free energies for each phase. Place in same order as hj_names and gj_names!");
+  params.addRequiredParam<std::vector<MaterialPropertyName> >("hj_names", "Switching Function Materials that provide h. Place in same order as Fj_names and gj_names!");
+  params.addRequiredParam<std::vector<MaterialPropertyName> >("gj_names", "Barrier Function Materials that provide g. Place in same order as Fj_names and hj_names!");
+  params.addRequiredParam<Real>("w", "Double well height parameter");
+  params.addParam< std::vector<MaterialPropertyName> >("kappa_names", std::vector<MaterialPropertyName>(), "Vector of kappa names corresponding to each variable name in interfacial_vars in the same order.");
+  return params;
+}
+
+KKSMultiFreeEnergy::KKSMultiFreeEnergy(const InputParameters & parameters) :
+    TotalFreeEnergyBase(parameters),
+    _Fj_names(getParam<std::vector<MaterialPropertyName> >("Fj_names")),
+    _num_j(_Fj_names.size()),
+    _prop_Fj(_num_j),
+    _hj_names(getParam<std::vector<MaterialPropertyName> >("hj_names")),
+    _prop_hj(_num_j),
+    _gj_names(getParam<std::vector<MaterialPropertyName> >("gj_names")),
+    _prop_gj(_num_j),
+    _w(getParam<Real>("w")),
+    _kappas(_nkappas)
+{
+  // Check that same number of Fj, hj, and gj are passed in
+  if (_hj_names.size() != _num_j)
+    mooseError("Size of hj_names is not equal to size of Fj_names in KKSMultiFreeEnergy AuxKernel " << name());
+  if (_gj_names.size() != _num_j)
+    mooseError("Size of gj_names is not equal to size of Fj_names in KKSMultiFreeEnergy AuxKernel " << name());
+
+  // get bulk properties
+  for (unsigned int i = 0; i < _num_j; ++i)
+  {
+    _prop_Fj[i] = &getMaterialPropertyByName<Real>(_Fj_names[i]);
+    _prop_hj[i] = &getMaterialPropertyByName<Real>(_hj_names[i]);
+    _prop_gj[i] = &getMaterialPropertyByName<Real>(_gj_names[i]);
+  }
+
+  // Check to ensure size of interfacial_vars is the same as kappa_names
+  if (_nvars != _nkappas)
+    mooseError("Size of interfacial_vars is not equal to the size of kappa_names in KKSMultiFreeEnergy AuxKernel " << name());
+
+  // Assign kappa values
+  for (unsigned int i = 0; i < _nkappas; ++i)
+    _kappas[i] = &getMaterialPropertyByName<Real>(_kappa_names[i]);
+}
+
+Real
+KKSMultiFreeEnergy::computeValue()
+{
+  Real total_energy = 0.0;
+  // Include bulk energy and additional contributions
+  for (unsigned int i = 0; i < _num_j; ++i)
+    total_energy += (*_prop_hj[i])[_qp] * (*_prop_Fj[i])[_qp]
+                      + _w * (*_prop_gj[i])[_qp] + _additional_free_energy[_qp];
+
+  // Calculate interfacial energy of each variable
+  for (unsigned int i = 0; i < _nvars; ++i)
+    total_energy += (*_kappas[i])[_qp] / 2.0 * (*_grad_vars[i])[_qp].norm_sq();
+
+  return total_energy;
+}
