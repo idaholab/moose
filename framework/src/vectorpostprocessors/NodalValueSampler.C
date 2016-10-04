2e9c804ea1f9b843fa32531a4af5c7683dc4e478
John Peterson <jwpeterson@gmail.com>
2016-10-04 10:01:48 -0600

    Detect variables with different discretizations in the same NodalValueSampler.
    The NodalValueSampler currently expects there to be a nodal value
    available for each of its coupled variables at every node it
    encounters.  This won't be the case when e.g. a FIRST, LAGRANGE
    element is used on a mesh with quadratic elements.  In such cases we
    can just skip sampling at the node in question, but in case someone
    tries to construct a single NodalValueSampler with variables with
    different discretizations, we don't have a good way of handling that
    (e.g. we don't want to erroneously report 0 values for unsampled
    variables) so we detect that case and throw an error.  The fix is to
    use separate NodalValueSamplers for variables with different
    discretizations. Also fix doco typo.

--- framework/src/vectorpostprocessors/NodalValueSampler.C
+++ framework/src/vectorpostprocessors/NodalValueSampler.C
@@ -14,6 +14,9 @@
 
 #include "NodalValueSampler.h"
 
+// C++ includes
+#include <numeric>
+
 template<>
 InputParameters validParams<NodalValueSampler>()
 {
@@ -30,11 +33,12 @@ NodalValueSampler::NodalValueSampler(const InputParameters & parameters) :
 {
   std::vector<std::string> var_names(_coupled_moose_vars.size());
   _values.resize(_coupled_moose_vars.size());
+  _has_values.resize(_coupled_moose_vars.size());
 
   for (unsigned int i=0; i<_coupled_moose_vars.size(); i++)
     var_names[i] = _coupled_moose_vars[i]->name();
 
-  // Initialize the datastructions in SamplerBase
+  // Initialize the data structures in SamplerBase
   SamplerBase::setupVariables(var_names);
 }
 
@@ -47,10 +51,48 @@ NodalValueSampler::initialize()
 void
 NodalValueSampler::execute()
 {
+  // There may not be a nodal solution value at every node.  This
+  // can happen if, for instance, you have a LINEAR, LAGRANGE
+  // variable defined on a mesh with quadratic elements.
+  //
+  // We currently handle the following cases:
+  // 1.) *none* of the coupled vars have values at the current node
+  // 2.) *all* of the coupled vars have values at the current node
+  //
+  // If you have two different discretizations, you'll have to use two
+  // separate NodalValueSampler objects to get their values.
   for (unsigned int i=0; i<_coupled_moose_vars.size(); i++)
-    _values[i] = _coupled_moose_vars[i]->nodalSln()[_qp];
+  {
+    const VariableValue & nodal_solution = _coupled_moose_vars[i]->nodalSln();
 
-  SamplerBase::addSample(*_current_node, _current_node->id(), _values);
+    if (nodal_solution.size() > 0)
+    {
+      _values[i] = nodal_solution[_qp];
+      _has_values[i] = 1;
+    }
+    else
+    {
+      _values[i] = 0.; // arbitrary, will not be used
+      _has_values[i] = 0;
+    }
+  }
+
+  // Sum the number of values we had
+  unsigned int num_values = std::accumulate(_has_values.begin(),
+                                            _has_values.end(),
+                                            0,
+                                            std::plus<unsigned int>());
+
+  // If the number of values matches the number of available values,
+  // call addSample() as normal.  If there are more than zero values
+  // available but less than the number requested, throw an error.
+  // Otherwise, num_values==0, and we can skip adding the sample
+  // entirely without error.
+  if (num_values == _has_values.size())
+    SamplerBase::addSample(*_current_node, _current_node->id(), _values);
+
+  else if (num_values != 0 && num_values < _has_values.size())
+    mooseError("You must use separate NodalValueSampler objects for variables with different discretizations.");
 }
 
 void
