cdde28329333b54fe66e03f4b4c05ca40b8e8073
Chris Green <chris.green@csiro.au>
2016-10-30 09:29:08 +1100

    Add unit tests of CO2FluidProperties. Refs #7972

---
+++ unit/include/CO2FluidPropertiesTest.h
@@ -0,0 +1,132 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#ifndef CO2FLUIDPROPERTIESTEST_H
+#define CO2FLUIDPROPERTIESTEST_H
+
+//CPPUnit includes
+#include "GuardedHelperMacros.h"
+
+class MooseMesh;
+class FEProblem;
+class CO2FluidProperties;
+
+class CO2FluidPropertiesTest : public CppUnit::TestFixture
+{
+  CPPUNIT_TEST_SUITE(CO2FluidPropertiesTest);
+
+  /**
+   * Verify calculation of melting pressure using experimental data from
+   * Michels et al, The melting line of carbon dioxide up to 2800 atmospheres,
+   * Physica 9 (1942).
+   * Note that results in this reference are given in atm, but have been
+   * converted to MPa here.
+   * As we are comparing with experimental data, calculated values within 1% are
+   * considered satisfactory.
+   */
+  CPPUNIT_TEST(melting);
+
+  /**
+   * Verify calculation of sublimation pressure using data from
+   * Bedford et al., Recommended values of temperature for a selected set of
+   * secondary reference points, Metrologia 20 (1984).
+   */
+  CPPUNIT_TEST(sublimation);
+
+  /**
+   * Verify calculation of vapor pressure, vapor density and saturated liquid
+   * density using experimental data from
+   * Duschek et al., Measurement and correlation of the (pressure, density, temperature)
+   * relation of cabon dioxide II. Saturated-liquid and saturated-vapor densities and
+   * the vapor pressure along the entire coexstance curve, J. Chem. Thermo. 22 (1990).
+   * As we are comparing with experimental data, calculated values within 1% are
+   * considered satisfactory.
+   */
+  CPPUNIT_TEST(vapor);
+
+  /**
+   * Verify calculation of partial density at infinite dilution using data from
+   * Hnedkovsky et al., Volumes of aqueous solutions of CH4, CO2, H2S, and NH3
+   * at temperatures from 298.15 K to 705 K and pressures to 35 MPa,
+   * J. Chem. Thermo. 28, 1996.
+   * As we are comparing with experimental data, calculated values within 5% are
+   * considered satisfactory.
+   */
+  CPPUNIT_TEST(partialDensity);
+
+  /**
+   * Verify calculation of Henry's constant using data from
+   * Guidelines on the Henry's constant and vapour liquid distribution constant
+   * for gases in H20 and D20 at high temperatures, IAPWS (2004).
+   */
+  CPPUNIT_TEST(henry);
+
+  /**
+   * Verify calculation of thermal conductivity using data from
+   * Scalabrin et al., A Reference Multiparameter Thermal Conductivity Equation
+   * for Carbon Dioxide with an Optimized Functional Form,
+   * J. Phys. Chem. Ref. Data 35 (2006)
+   */
+  CPPUNIT_TEST(thermalConductivity);
+
+  /**
+   * Verify calculation of viscosity using data from
+   * Fenghour et al., The viscosity of carbon dioxide,
+   * J. Phys. Chem. Ref. Data, 27, 31-44 (1998)
+   */
+  CPPUNIT_TEST(viscosity);
+
+  /**
+   * Verify calculation of thermophysical properties of CO2 from the Span and
+   * Wagner EOS using verification data provided in
+   * A New Equation of State for Carbon Dioxide Covering the Fluid Region from
+   * the Triple-Point Temperature to 1100K at Pressures up to 800 MPa,
+   * J. Phys. Chem. Ref. Data, 25 (1996)
+   */
+  CPPUNIT_TEST(propertiesSW);
+
+  /**
+   * Verify calculation of the derivatives of all properties by comparing with finite
+   * differences
+   */
+  CPPUNIT_TEST(derivatives);
+
+  CPPUNIT_TEST_SUITE_END();
+
+public:
+  void registerObjects(Factory & factory);
+  void buildObjects();
+
+  void setUp();
+  void tearDown();
+
+  void melting();
+  void sublimation();
+  void vapor();
+  void partialDensity();
+  void henry();
+  void thermalConductivity();
+  void viscosity();
+  void propertiesSW();
+  void derivatives();
+
+private:
+  MooseApp * _app;
+  Factory * _factory;
+  MooseMesh * _mesh;
+  FEProblem * _fe_problem;
+  const CO2FluidProperties * _fp;
+};
+
+#endif  // CO2FLUIDPROPERTIESTEST_H
---
+++ unit/src/CO2FluidPropertiesTest.C
@@ -0,0 +1,227 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#include "MooseApp.h"
+#include "Utils.h"
+#include "CO2FluidPropertiesTest.h"
+
+#include "FEProblem.h"
+#include "AppFactory.h"
+#include "GeneratedMesh.h"
+#include "CO2FluidProperties.h"
+
+CPPUNIT_TEST_SUITE_REGISTRATION(CO2FluidPropertiesTest);
+
+void
+CO2FluidPropertiesTest::registerObjects(Factory & factory)
+{
+  registerUserObject(CO2FluidProperties);
+}
+
+void
+CO2FluidPropertiesTest::buildObjects()
+{
+  InputParameters mesh_params = _factory->getValidParams("GeneratedMesh");
+  mesh_params.set<MooseEnum>("dim") = "3";
+  mesh_params.set<std::string>("name") = "mesh";
+  mesh_params.set<std::string>("_object_name") = "name1";
+  _mesh = new GeneratedMesh(mesh_params);
+
+  InputParameters problem_params = _factory->getValidParams("FEProblem");
+  problem_params.set<MooseMesh *>("mesh") = _mesh;
+  problem_params.set<std::string>("name") = "problem";
+  problem_params.set<std::string>("_object_name") = "name2";
+  _fe_problem = new FEProblem(problem_params);
+
+  InputParameters uo_pars = _factory->getValidParams("CO2FluidProperties");
+  _fe_problem->addUserObject("CO2FluidProperties", "fp", uo_pars);
+  _fp = & _fe_problem->getUserObject<CO2FluidProperties>("fp");
+}
+
+void
+CO2FluidPropertiesTest::setUp()
+{
+  char str[] = "foo";
+  char * argv[] = { str, NULL };
+
+  _app = AppFactory::createApp("MooseUnitApp", 1, (char **) argv);
+  _factory = &_app->getFactory();
+
+  registerObjects(*_factory);
+  buildObjects();
+}
+
+void
+CO2FluidPropertiesTest::tearDown()
+{
+  delete _fe_problem;
+  delete _mesh;
+  delete _app;
+}
+
+void
+CO2FluidPropertiesTest::melting()
+{
+  REL_TEST("melting", _fp->meltingPressure(217.03), 2.57e6, 1.0e-2);
+  REL_TEST("melting", _fp->meltingPressure(235.29), 95.86e6, 1.0e-2);
+  REL_TEST("melting", _fp->meltingPressure(266.04), 286.77e6, 1.0e-2);
+}
+
+void
+CO2FluidPropertiesTest::sublimation()
+{
+  REL_TEST("sublimation", _fp->sublimationPressure(194.6857), 0.101325e6, 1.0e-4);
+}
+
+void
+CO2FluidPropertiesTest::vapor()
+{
+  // Vapor pressure
+  REL_TEST("vapor", _fp->vaporPressure(217.0), 0.52747e6, 1.0e-2);
+  REL_TEST("vapor", _fp->vaporPressure(245.0), 1.51887e6, 1.0e-2);
+  REL_TEST("vapor", _fp->vaporPressure(303.8), 7.32029e6, 1.0e-2);
+
+  // Saturated vapor density
+  REL_TEST("saturated vapor density", _fp->saturatedVaporDensity(217.0), 14.0017, 1.0e-2);
+  REL_TEST("saturated vapor density", _fp->saturatedVaporDensity(245.0), 39.5048, 1.0e-2);
+  REL_TEST("saturated vapor density", _fp->saturatedVaporDensity(303.8), 382.30, 1.0e-2);
+
+  // Saturated liquid density
+  REL_TEST("saturated liquid density", _fp->saturatedLiquidDensity(217.0), 1177.03, 1.0e-2);
+  REL_TEST("saturated liquid density", _fp->saturatedLiquidDensity(245.0), 1067.89, 1.0e-2);
+  REL_TEST("saturated liquid density", _fp->saturatedLiquidDensity(303.8), 554.14, 1.0e-2);
+}
+
+void
+CO2FluidPropertiesTest::partialDensity()
+{
+  REL_TEST("partial density", _fp->partialDensity(373.15), 1182.8, 5.0e-2);
+  REL_TEST("partial density", _fp->partialDensity(473.35), 880.0, 5.0e-2);
+  REL_TEST("partial density", _fp->partialDensity(573.15), 593.8, 5.0e-2);
+}
+
+void
+CO2FluidPropertiesTest::henry()
+{
+  REL_TEST("henry", _fp->henryConstant(300.0), 173.63e6, 1.0e-3);
+  REL_TEST("henry", _fp->henryConstant(400.0), 579.84e6, 1.0e-3);
+  REL_TEST("henry", _fp->henryConstant(500.0), 520.79e6, 1.0e-3);
+  REL_TEST("henry", _fp->henryConstant(600.0), 259.53e6, 1.0e-3);
+}
+
+void
+CO2FluidPropertiesTest::thermalConductivity()
+{
+  REL_TEST("thermal conductivity", _fp->k(23.435, 250.0), 13.45e-3, 1.0e-3);
+  REL_TEST("thermal conductivity", _fp->k(18.579, 300.0), 17.248e-3, 1.0e-3);
+  REL_TEST("thermal conductivity", _fp->k(11.899, 450.0), 29.377e-3, 1.0e-3);
+}
+
+void
+CO2FluidPropertiesTest::viscosity()
+{
+  REL_TEST("viscosity", _fp->mu(20.199, 280.0), 14.15e-6, 1.0e-3);
+  REL_TEST("viscosity", _fp->mu(15.105, 360.0), 17.94e-6, 1.0e-3);
+  REL_TEST("viscosity", _fp->mu(10.664, 500.0), 24.06e-6, 1.0e-3);
+}
+
+void
+CO2FluidPropertiesTest::propertiesSW()
+{
+  // Pressure = 1 MPa, temperature = 280 K
+  Real p = 1.0e6;
+  Real T = 280.0;
+  REL_TEST("density", _fp->rho(p, T), 20.199, 1.0e-3);
+  REL_TEST("enthalpy", _fp->h(p, T), -26.385, 1.0e-3);
+  REL_TEST("internal energy", _fp->e(p, T), -75.892, 1.0e-3);
+  REL_TEST("entropy", _fp->s(p, T), -0.51326, 1.0e-3);
+  REL_TEST("cp", _fp->cp(p, T), 0.92518, 1.0e-3);
+  REL_TEST("cv", _fp->cv(p, T), 0.67092, 1.0e-3);
+  REL_TEST("c", _fp->c(p, T), 252.33, 1.0e-3);
+
+  // Pressure = 1 MPa, temperature = 500 K
+  T = 500.0;
+  REL_TEST("density", _fp->rho(p, T), 10.664, 1.0e-3);
+  REL_TEST("enthalpy", _fp->h(p, T), 185.60, 1.0e-3);
+  REL_TEST("internal energy", _fp->e(p, T), 91.829, 1.0e-3);
+  REL_TEST("entropy", _fp->s(p, T), 0.04225, 1.0e-3);
+  REL_TEST("cp", _fp->cp(p, T), 1.0273, 1.0e-3);
+  REL_TEST("cv", _fp->cv(p, T), 0.82823, 1.0e-3);
+  REL_TEST("c", _fp->c(p, T), 339.81, 1.0e-3);
+
+  // Pressure = 10 MPa, temperature = 500 K
+  p = 10.0e6;
+  REL_TEST("density", _fp->rho(p, T), 113.07, 1.0e-3);
+  REL_TEST("enthalpy", _fp->h(p, T), 157.01, 1.0e-3);
+  REL_TEST("internal energy", _fp->e(p, T), 68.569, 1.0e-3);
+  REL_TEST("entropy", _fp->s(p, T), -0.4383, 1.0e-3);
+  REL_TEST("cp", _fp->cp(p, T), 1.1624, 1.0e-3);
+  REL_TEST("cv", _fp->cv(p, T), 0.85516, 1.0e-3);
+  REL_TEST("c", _fp->c(p, T), 337.45, 1.0e-3);
+}
+
+void
+CO2FluidPropertiesTest::derivatives()
+{
+  Real p = 1.0e6;
+  Real T = 350.0;
+
+  // Finite differencing parameters
+  Real dp = 1.0e1;
+  Real dT = 1.0e-4;
+
+  // density
+  Real drho_dp_fd = (_fp->rho(p + dp, T) - _fp->rho(p - dp, T)) / (2.0 * dp);
+  Real drho_dT_fd = (_fp->rho(p, T + dT) - _fp->rho(p, T - dT)) / (2.0 * dT);
+  Real rho = 0.0, drho_dp = 0.0, drho_dT = 0.0;
+  _fp->rho_dpT(p, T, rho, drho_dp, drho_dT);
+
+  ABS_TEST("rho", rho, _fp->rho(p, T), 1.0e-15);
+  REL_TEST("drho_dp", drho_dp, drho_dp_fd, 1.0e-6);
+  REL_TEST("drho_dT", drho_dT, drho_dT_fd, 1.0e-6);
+
+  // enthalpy
+  Real dh_dp_fd = (_fp->h(p + dp, T) - _fp->h(p - dp, T)) / (2.0 * dp);
+  Real dh_dT_fd = (_fp->h(p, T + dT) - _fp->h(p, T - dT)) / (2.0 * dT);
+  Real h = 0.0, dh_dp = 0.0, dh_dT = 0.0;
+  _fp->h_dpT(p, T, h, dh_dp, dh_dT);
+
+  ABS_TEST("h", h, _fp->h(p, T), 1.0e-15);
+  REL_TEST("dh_dp", dh_dp, dh_dp_fd, 1.0e-6);
+  REL_TEST("dh_dT", dh_dT, dh_dT_fd, 1.0e-6);
+
+  // internal energy
+  Real de_dp_fd = (_fp->e(p + dp, T) - _fp->e(p - dp, T)) / (2.0 * dp);
+  Real de_dT_fd = (_fp->e(p, T + dT) - _fp->e(p, T - dT)) / (2.0 * dT);
+  Real e = 0.0, de_dp = 0.0, de_dT = 0.0;
+  _fp->e_dpT(p, T, e, de_dp, de_dT);
+
+  ABS_TEST("e", e, _fp->e(p, T), 1.0e-15);
+  REL_TEST("de_dp", de_dp, de_dp_fd, 1.0e-6);
+  REL_TEST("de_dT", de_dT, de_dT_fd, 1.0e-6);
+
+  // Viscosity
+  rho = 15.105;
+  T = 360.0;
+  Real drho = 1.0e-4;
+
+  Real dmu_drho_fd = (_fp->mu(rho + drho, T) - _fp->mu(rho - drho, T)) / (2.0 * drho);
+  Real dmu_dT_fd = (_fp->mu(rho, T + dT) - _fp->mu(rho, T - dT)) / (2.0 * dT);
+  Real mu = 0.0, dmu_drho = 0.0, dmu_dT = 0.0;
+  _fp->mu_drhoT(rho, T, mu, dmu_drho, dmu_dT);
+
+  ABS_TEST("mu", mu, _fp->mu(rho, T), 1.0e-15);
+  REL_TEST("dmu_dp", dmu_drho, dmu_drho_fd, 1.0e-6);
+  REL_TEST("dmu_dT", dmu_dT, dmu_dT_fd, 1.0e-6);
+}
