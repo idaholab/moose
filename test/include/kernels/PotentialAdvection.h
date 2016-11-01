e8d29990c635eddc502b1100358841f235436232
Alex Lindsay <adlinds3@ncsu.edu>
2016-10-31 19:41:03 -0500

    Beginning construction of shape_side_uo test. (Issue #7980)

---
+++ test/include/kernels/PotentialAdvection.h
@@ -0,0 +1,40 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+#ifndef POTENTIALADVECTION_H_
+#define POTENTIALADVECTION_H_
+
+#include "Kernel.h"
+
+class PotentialAdvection;
+
+template<>
+InputParameters validParams<PotentialAdvection>();
+
+class PotentialAdvection : public Kernel
+{
+public:
+  PotentialAdvection(const InputParameters & parameters);
+
+protected:
+  virtual Real computeQpResidual();
+  virtual Real computeQpJacobian();
+  virtual Real computeQpOffDiagJacobian(unsigned int jvar);
+
+private:
+  const VariableGradient & _grad_potential;
+  unsigned int _potential_id;
+  Real _sgn;
+};
+
+#endif //POTENTIALADVECTION_H
