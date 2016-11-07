effa51bddfaae3d131e25f258fd19ab37485546e
Alex Lindsay <adlinds3@ncsu.edu>
2016-11-07 15:32:12 -0600

    Experimenting with templating.

--- framework/include/userobject/ShapeElementUserObject.h
+++ framework/include/userobject/ShapeElementUserObject.h
@@ -16,6 +16,7 @@
 #define SHAPEELEMENTUSEROBJECT_H
 
 #include "ElementUserObject.h"
+#include "ShapeUserObject.h"
 
 //Forward Declarations
 class ShapeElementUserObject;
@@ -31,55 +32,18 @@ InputParameters validParams<ShapeElementUserObject>();
  * \warning It is up to the user to ensure _fe_problem.currentlyComputingJacobian()
  *          returns true before utilizing the shape functions.
  */
-class ShapeElementUserObject : public ElementUserObject
+class ShapeElementUserObject : public ShapeUserObject<ElementUserObject>
 {
 public:
   ShapeElementUserObject(const InputParameters & parameters);
 
-  /// check if jacobian is to be computed in user objects
-  const bool & computeJacobianFlag() const { return _compute_jacobians; }
-
-  /**
-   * Returns the set of variables a Jacobian has been requested for
-   */
-  const std::set<MooseVariable *> & jacobianMooseVariables() const { return _jacobian_moose_variables; }
-
-  /**
-   * This function will be called with the shape functions for jvar initialized. It
-   * can be used to compute Jacobian contributions of the by implementing executeJacobian.
-   */
-  virtual void executeJacobianWrapper(unsigned int jvar, const std::vector<dof_id_type> & dof_indices);
-
-protected:
-  /**
-   * Implement this function to compute Jacobian terms for this UserObject. The
-   * shape function index _j and its corrsponding global DOF index _j_global
-   * will be provided.
-   */
-  virtual void executeJacobian(unsigned int /*jvar*/) = 0;
-
-  /**
-   * Returns the index for a coupled variable by name and requests the computation
-   * of a Jacobian w.r.t. to this variable i.e. the call to executeJacobian() with
-   * shapefunctions initialized for this variable.
-   */
-  virtual unsigned int coupled(const std::string & var_name, unsigned int comp = 0);
-
-  /// shape function values
-  const VariablePhiValue & _phi;
-
-  /// shape function gradients
-  const VariablePhiGradient & _grad_phi;
-
-  /// j-th index for enumerating the shape functions
-  unsigned int _j;
-
-  /// global DOF ID corresponding to _j
-  dof_id_type _j_global;
-
-private:
-  const bool _compute_jacobians;
-  std::set<MooseVariable *> _jacobian_moose_variables;
+/* protected: */
+/*   /\** */
+/*    * Implement this function to compute Jacobian terms for this UserObject. The */
+/*    * shape function index _j and its corrsponding global DOF index _j_global */
+/*    * will be provided. */
+/*    *\/ */
+/*   virtual void executeJacobian(unsigned int /\*jvar*\/) = 0; */
 };
 
 #endif //SHAPEELEMENTUSEROBJECT_H
--- framework/src/userobject/ShapeSideUserObject.C
+++ framework/src/userobject/ShapeSideUserObject.C
@@ -19,39 +19,12 @@ template<>
 InputParameters validParams<ShapeSideUserObject>()
 {
   InputParameters params = validParams<SideUserObject>();
-  params.addParam<bool>("compute_jacobians", true, "Compute Jacobians for coupled variables");
-  params.addParamNamesToGroup("compute_jacobians", "Advanced");
+  params += ShapeUserObject<SideUserObject>::validParams();
   return params;
 }
 
 ShapeSideUserObject::ShapeSideUserObject(const InputParameters & parameters) :
-    SideUserObject(parameters),
-    _phi(_assembly.phiFace()),
-    _grad_phi(_assembly.gradPhiFace()),
-    _compute_jacobians(getParam<bool>("compute_jacobians"))
+    // SideUserObject(parameters),
+    ShapeUserObject<SideUserObject>(parameters, _assembly.phiFace(), _assembly.gradPhiFace())
 {
-  mooseWarning("Jacobian calculation in UserObjects is an experimental capability with a potentially unstable interface.");
-}
-
-unsigned int
-ShapeSideUserObject::coupled(const std::string & var_name, unsigned int comp)
-{
-  MooseVariable * var = getVar(var_name, comp);
-
-  // add to the set of variables for which executeJacobian will be called
-  if (_compute_jacobians && var->kind() == Moose::VAR_NONLINEAR)
-    _jacobian_moose_variables.insert(var);
-
-  // return the variable number
-  return SideUserObject::coupled(var_name, comp);
-}
-
-void
-ShapeSideUserObject::executeJacobianWrapper(unsigned int jvar, const std::vector<dof_id_type> & dof_indices)
-{
-  for (_j = 0; _j < _phi.size(); ++_j)
-  {
-    _j_global = dof_indices[_j];
-    executeJacobian(jvar);
-  }
 }
