e43709abfdcffeba7a802858b171ccdbbf352247
Cody Permann <codypermann@gmail.com>
2016-11-15 13:25:14 -0700

    Remove Linear Elasticity *fingers crossed*
    closes #4863

--- modules/combined/src/base/CombinedApp.C
+++ modules/combined/src/base/CombinedApp.C
@@ -15,7 +15,6 @@
 #include "ContactApp.h"
 #include "FluidPropertiesApp.h"
 #include "HeatConductionApp.h"
-#include "LinearElasticityApp.h"
 #include "MiscApp.h"
 #include "NavierStokesApp.h"
 #include "PhaseFieldApp.h"
@@ -66,7 +65,6 @@ CombinedApp::registerObjects(Factory & factory)
   ContactApp::registerObjects(factory);
   FluidPropertiesApp::registerObjects(factory);
   HeatConductionApp::registerObjects(factory);
-  LinearElasticityApp::registerObjects(factory);
   MiscApp::registerObjects(factory);
   NavierStokesApp::registerObjects(factory);
   PhaseFieldApp::registerObjects(factory);
@@ -87,7 +85,6 @@ CombinedApp::associateSyntax(Syntax & syntax, ActionFactory & action_factory)
   ContactApp::associateSyntax(syntax, action_factory);
   FluidPropertiesApp::associateSyntax(syntax, action_factory);
   HeatConductionApp::associateSyntax(syntax, action_factory);
-  LinearElasticityApp::associateSyntax(syntax, action_factory);
   MiscApp::associateSyntax(syntax, action_factory);
   NavierStokesApp::associateSyntax(syntax, action_factory);
   PhaseFieldApp::associateSyntax(syntax, action_factory);
