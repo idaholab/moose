4c31ac92b7ed703569fe7c89a693a0c0e5969b08
Fande Kong <fdkong.jd@gmail.com>
2016-11-29 09:38:13 -0700

    Added the implementation of newAssemblyArray(),
    deleteAssemblyArray() and initNullSpaceVectors().
    
    Refs #7398

--- framework/src/base/FEProblemBase.C
+++ framework/src/base/FEProblemBase.C
@@ -200,6 +200,55 @@ FEProblemBase::FEProblemBase(const InputParameters & parameters) :
   _eq.parameters.set<FEProblemBase *>("_fe_problem_base") = this;
 }
 
+void FEProblemBase::newAssemblyArray(NonlinearSystemBase nl)
+{
+  unsigned int n_threads = libMesh::n_threads();
+
+  _assembly.resize(n_threads);
+  for (unsigned int i = 0; i < n_threads; ++i)
+    _assembly[i] = new Assembly(nl, couplingMatrix(), i);
+}
+
+void FEProblemBase::deleteAssemblyArray()
+{
+  unsigned int n_threads = libMesh::n_threads();
+  for (unsigned int i = 0; i < n_threads; i++)
+  {
+    delete _assembly[i];
+  }
+}
+
+void FEProblemBase::initNullSpaceVectors(const InputParameters & parameters, NonlinearSystemBase nl)
+{
+  unsigned int dimNullSpace = parameters.get<unsigned int>("null_space_dimension");
+  unsigned int dimTransposeNullSpace = parameters.get<unsigned int>("transpose_null_space_dimension");
+  unsigned int dimNearNullSpace = parameters.get<unsigned int>("near_null_space_dimension");
+  for (unsigned int i = 0; i < dimNullSpace; ++i)
+  {
+    std::ostringstream oss;
+    oss << "_" << i;
+    // do not project, since this will be recomputed, but make it ghosted, since the near nullspace builder might march over all nodes
+    nl.addVector("NullSpace" + oss.str(), false, GHOSTED);
+  }
+  _subspace_dim["NullSpace"] = dimNullSpace;
+  for (unsigned int i = 0; i < dimTransposeNullSpace; ++i)
+  {
+    std::ostringstream oss;
+    oss << "_" << i;
+    // do not project, since this will be recomputed, but make it ghosted, since the near nullspace builder might march over all nodes
+    nl.addVector("TransposeNullSpace" + oss.str(), false, GHOSTED);
+  }
+  _subspace_dim["TransposeNullSpace"] = dimTransposeNullSpace;
+  for (unsigned int i = 0; i < dimNearNullSpace; ++i)
+  {
+    std::ostringstream oss;
+    oss << "_" << i;
+    // do not project, since this will be recomputed, but make it ghosted, since the near-nullspace builder might march over all semilocal nodes
+    nl.addVector("NearNullSpace" + oss.str(), false, GHOSTED);
+  }
+  _subspace_dim["NearNullSpace"] = dimNearNullSpace;
+}
+
 
 FEProblemBase::~FEProblemBase()
 {
