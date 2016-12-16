82c406030713b2c0569d7790978354477e24e81c
Robert Carlsen <rwcarlsen@gmail.com>
2016-12-16 14:13:32 -0700

    working on adding moose master command

---
+++ templates/Kernel.C
@@ -0,0 +1,32 @@
+
+#include "TmplKernel.h"
+
+template<>
+InputParameters validParams<TmplKernel>()
+{
+  InputParameters params = validParams<TmplKernel>();
+  // add custom kernel parameters here
+  return params;
+}
+
+TmplKernel::TmplKernel(const InputParameters & parameters)
+    : TmplKernel(parameters)
+{
+}
+
+Real
+TmplKernel::computeQpResidual() {
+  // implement residual calculation here
+}
+
+Real
+TmplKernel::computeQpJacobian() {
+  // optionally implement jacobian calculation here
+  return 0;
+}
+
+// implement these functions only if you have a particular need to:
+Real TmplKernel::computeQpOffDiagJacobian(unsigned int /*jvar*/) { return 0; }
+void TmplKernel::precalculateResidual() { }
+void precalculateJacobian() { }
+void precalculateOffDiagJacobian(unsigned int /* jvar */) { }
---
+++ templates/Kernel.h
@@ -0,0 +1,37 @@
+
+#ifndef TMPLKERNEL_H
+#define TMPLKERNEL_H
+
+#include "TmplKernel.h"
+
+class TmplKernel;
+
+template<>
+InputParameters validParams<TmplKernel>();
+
+class TmplKernel : public Kernel
+{
+public:
+  TmplKernel(const InputParameters & parameters);
+
+protected:
+
+  /// Compute this kernel's contribution to the residual at the current
+  /// quadrature point.
+  virtual Real computeQpResidual();
+
+  /// Compute this kernel's contribution to the Jacobian at the current
+  /// quadrature point.
+  virtual Real computeQpJacobian();
+
+  /// Implement if you need to compute an off-diagonal Jacobian component.
+  virtual Real computeQpOffDiagJacobian(unsigned int jvar);
+
+  /// Following methods can be used by kernels that need to perform a
+  /// per-element calculation:
+  virtual void precalculateResidual();
+  virtual void precalculateJacobian();
+  virtual void precalculateOffDiagJacobian(unsigned int /* jvar */);
+};
+
+#endif
