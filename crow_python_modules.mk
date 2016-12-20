7b667ad35ef7bd42365f32a9ae9343e52200a49d
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2016-12-20 16:43:42 -0700

    For test = is correct, not ==

--- crow_python_modules.mk
+++ crow_python_modules.mk
@@ -38,11 +38,11 @@ $(CROW_DIR)/install/crow_modules/_distribution1Dpy2.so $(CROW_DIR)/install/crow_
 .INTERMEDIATE: setup_py2_intermediate
 
 setup_py2_intermediate : $(CROW_DIR)/crow_modules/distribution1Dpy2.i $(CROW_DIR)/crow_modules/interpolationNDpy2.i $(DISTRIBUTION_SOURCE) $(INTERPOLATION_SOURCE)
-	(cd $(CROW_DIR) && unset CXX CC && CFLAGS="$$CFLAGS $(COVERAGE_COMPILE_EXTRA)" && LDFLAGS="$$LDFLAGS $(COVERAGE_LINK_EXTRA)" && export CFLAGS LDFLAGS &&if test `uname` == Darwin; then MACOSX_DEPLOYMENT_TARGET=10.9; export MACOSX_DEPLOYMENT_TARGET; fi && source $(CROW_DIR)/scripts/setup_raven_libs && python $(CROW_DIR)/setup.py build_ext build install --install-platlib=$(CROW_DIR)/install)
+	(cd $(CROW_DIR) && unset CXX CC && CFLAGS="$$CFLAGS $(COVERAGE_COMPILE_EXTRA)" && LDFLAGS="$$LDFLAGS $(COVERAGE_LINK_EXTRA)" && export CFLAGS LDFLAGS &&if test `uname` = Darwin; then MACOSX_DEPLOYMENT_TARGET=10.9; export MACOSX_DEPLOYMENT_TARGET; fi && source $(CROW_DIR)/scripts/setup_raven_libs && python $(CROW_DIR)/setup.py build_ext build install --install-platlib=$(CROW_DIR)/install)
 
 $(CROW_DIR)/install/crow_modules/_distribution1Dpy3.so $(CROW_DIR)/install/crow_modules/_interpolationNDpy3.so : setup_py3_intermediate
 
 .INTERMEDIATE: setup_py3_intermediate
 
 setup_py3_intermediate : $(CROW_DIR)/crow_modules/distribution1Dpy3.i $(CROW_DIR)/crow_modules/interpolationNDpy3.i $(DISTRIBUTION_SOURCE) $(INTERPOLATION_SOURCE)
-	(cd $(CROW_DIR) && unset CXX CC && if test `uname` == Darwin; then MACOSX_DEPLOYMENT_TARGET=10.9; export MACOSX_DEPLOYMENT_TARGET; fi && source $(CROW_DIR)/scripts/setup_raven_libs && python3 $(CROW_DIR)/setup3.py build_ext build install --install-platlib=$(CROW_DIR)/install)
+	(cd $(CROW_DIR) && unset CXX CC && if test `uname` = Darwin; then MACOSX_DEPLOYMENT_TARGET=10.9; export MACOSX_DEPLOYMENT_TARGET; fi && source $(CROW_DIR)/scripts/setup_raven_libs && python3 $(CROW_DIR)/setup3.py build_ext build install --install-platlib=$(CROW_DIR)/install)
