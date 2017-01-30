9a0511508f3b8f9d0fabc4edeb12f242cd0f78c8
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2017-01-30 12:32:25 -0700

    Cleaning out makefiles.

--- config.mk
+++ config.mk
@@ -32,7 +32,6 @@ endif
 
 ifneq ($(PYTHON3_CONFIG_WHICH),)
 	SWIG_PY_FLAGS=-py3
-	CONTROL_MODULES = $(CROW_DIR)/control_modules/_distribution1D.so $(CROW_DIR)/control_modules/_crowtools.so
 	PYTHON_MODULES = $(CROW_DIR)/install/crow_modules/_distribution1Dpy2.so $(CROW_DIR)/install/crow_modules/_interpolationNDpy2.so $(CROW_DIR)/install/crow_modules/_distribution1Dpy3.so $(CROW_DIR)/install/crow_modules/_interpolationNDpy3.so
 	PYTHON_INCLUDE=$(PYTHON3_INCLUDE)
 	PYTHON_LIB=$(PYTHON3_LIB)
@@ -43,14 +42,11 @@ ifneq ($(PYTHON_CONFIG_WHICH),)
 #Python 2 config found but not Python 3
 	PYTHON_INCLUDE=$(PYTHON2_INCLUDE)
 	PYTHON_LIB=$(PYTHON2_LIB)
-	#CONTROL_MODULES=
 	SWIG_PY_FLAGS=
 	PYTHON_MODULES = $(CROW_DIR)/install/crow_modules/_distribution1Dpy2.so $(CROW_DIR)/install/crow_modules/_interpolationNDpy2.so
-	CONTROL_MODULES=$(CROW_DIR)/control_modules/_distribution1D.so $(CROW_DIR)/control_modules/_crowtools.so
 else #No python3 config or python2 config
 	PYTHON_INCLUDE = -DNO_PYTHON_FOR_YOU
 	PYTHON_LIB = -DNO_PYTHON_FOR_YOU
-	CONTROL_MODULES =
 	PYTHON_MODULES =
 endif
 
@@ -80,11 +76,4 @@ else
 crow_shared_ext := so
 endif
 
-HAS_DYNAMIC := $(shell $(libmesh_LIBTOOL) --config | grep build_libtool_libs | cut -d'=' -f2 )
 
-ifeq ($(HAS_DYNAMIC),no)
-ifdef CONTROL_MODULES
-$(warning CROW modules must be compiled with shared libmesh libraries)
-	CONTROL_MODULES =
-endif
-endif
--- crow_python_modules.mk
+++ crow_python_modules.mk
@@ -1,36 +1,10 @@
 
-DISTRIBUTION_COMPILE_COMMAND=$(libmesh_LIBTOOL) --tag=CXX $(LIBTOOLFLAGS) --mode=compile --quiet \
-          $(libmesh_CXX) $(libmesh_CPPFLAGS) $(libmesh_CXXFLAGS) -I$(CROW_LIB_INCLUDE_DIR) -I$(CROW_DIR)/include/distributions/ -I$(CROW_DIR)/include/utilities/  -MMD -MF $@.d -MT $@ -c $< -o $@
-
 #from setup.py
 DISTRIBUTION_SOURCE=$(CROW_DIR)/src/distributions/distribution.C $(CROW_DIR)/src/utilities/MDreader.C $(CROW_DIR)/src/utilities/inverseDistanceWeigthing.C $(CROW_DIR)/src/utilities/microSphere.C $(CROW_DIR)/src/utilities/NDspline.C $(CROW_DIR)/src/utilities/ND_Interpolation_Functions.C  $(CROW_DIR)/src/distributions/distributionNDBase.C $(CROW_DIR)/src/distributions/distributionNDNormal.C $(CROW_DIR)/src/distributions/distributionFunctions.C $(CROW_DIR)/src/distributions/DistributionContainer.C $(CROW_DIR)/src/distributions/distribution_1D.C
 
 #from setup.py
 INTERPOLATION_SOURCE=$(CROW_DIR)/src/utilities/ND_Interpolation_Functions.C $(CROW_DIR)/src/utilities/NDspline.C $(CROW_DIR)/src/utilities/microSphere.C $(CROW_DIR)/src/utilities/inverseDistanceWeigthing.C $(CROW_DIR)/src/utilities/MDreader.C
 
-$(CROW_DIR)/src/distributions/DistributionContainer.$(obj-suffix): $(CROW_DIR)/src/distributions/DistributionContainer.C
-	$(DISTRIBUTION_COMPILE_COMMAND)
-
-$(CROW_DIR)/src/distributions/distribution_1D.$(obj-suffix): $(CROW_DIR)/src/distributions/distribution_1D.C
-	$(DISTRIBUTION_COMPILE_COMMAND)
-
-$(CROW_DIR)/src/distributions/distributionFunctions.$(obj-suffix): $(CROW_DIR)/src/distributions/distributionFunctions.C
-	$(DISTRIBUTION_COMPILE_COMMAND)
-
-$(CROW_DIR)/src/distributions/distributionNDBase.$(obj-suffix): $(CROW_DIR)/src/distributions/distributionNDBase.C
-	$(DISTRIBUTION_COMPILE_COMMAND)
-
-$(CROW_DIR)/src/distributions/distributionNDNormal.$(obj-suffix): $(CROW_DIR)/src/distributions/distributionNDNormal.C
-	$(DISTRIBUTION_COMPILE_COMMAND)
-
-$(CROW_DIR)/src/utilities/ND_Interpolation_Functions.$(obj-suffix): $(CROW_DIR)/src/utilities/ND_Interpolation_Functions.C
-	$(DISTRIBUTION_COMPILE_COMMAND)
-
-$(CROW_DIR)/src/distributions/randomClass.$(obj-suffix): $(CROW_DIR)/src/distributions/randomClass.C
-	$(DISTRIBUTION_COMPILE_COMMAND)
-
-$(CROW_DIR)/src/distributions/distributionNDCartesianSpline.$(obj-suffix): $(CROW_DIR)/src/distributions/distributionNDCartesianSpline.C
-	$(DISTRIBUTION_COMPILE_COMMAND)
 
 
 $(CROW_DIR)/install/crow_modules/_distribution1Dpy2.so $(CROW_DIR)/install/crow_modules/_interpolationNDpy2.so : setup_py2_intermediate
