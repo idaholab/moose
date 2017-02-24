d67a479825b9223d787b64832d67d8287fc21630
Robert Carlsen <rwcarlsen@gmail.com>
2017-02-24 12:28:01 -0700

    cleanup dirac

--- framework/src/base/ComputeDiracThread.C
+++ framework/src/base/ComputeDiracThread.C
@@ -83,61 +83,49 @@ ComputeDiracThread::onElement(const Elem * elem)
   // Only call reinitMaterials() if one or more DiracKernels has
   // actually called getMaterialProperty().  Loop over all the
   // DiracKernels and check whether this is the case.
-  bool need_reinit_materials = false;
+  for (const auto & dirac_kernel : dkernels)
   {
-    for (const auto & dirac_kernel : dkernels)
+    // If any of the DiracKernels have had getMaterialProperty()
+    // called, we need to reinit Materials.
+    if (dirac_kernel->getMaterialPropertyCalled())
     {
-      // If any of the DiracKernels have had getMaterialProperty()
-      // called, we need to reinit Materials.
-      if (dirac_kernel->getMaterialPropertyCalled())
-      {
-        need_reinit_materials = true;
-        break;
-      }
+      _fe_problem.reinitMaterials(_subdomain, _tid, /*swap_stateful=*/false);
+      break;
     }
   }
 
-  if (need_reinit_materials)
-    _fe_problem.reinitMaterials(_subdomain, _tid, /*swap_stateful=*/false);
-
   for (const auto & dirac_kernel : dkernels)
   {
-    if (dirac_kernel->hasPointsOnElem(elem))
+    if (!dirac_kernel->hasPointsOnElem(elem))
+      continue;
+    else if (_jacobian == NULL)
     {
-      if (_jacobian == NULL)
-        dirac_kernel->computeResidual();
-      else
+      dirac_kernel->computeResidual();
+      continue;
+    }
+
+    // Get a list of coupled variables from the SubProblem
+    std::vector<std::pair<MooseVariable *, MooseVariable *> > & coupling_entries =
+      dirac_kernel->subProblem().assembly(_tid).couplingEntries();
+
+    // Loop over the list of coupled variable pairs
+    for (auto & it : coupling_entries)
+    {
+      MooseVariable * ivariable = it.first;
+      MooseVariable * jvariable = it.second;
+
+      // A variant of the check that is in
+      // ComputeFullJacobianThread::computeJacobian().  We
+      // only want to call computeOffDiagJacobian() if both
+      // variables are active on this subdomain, and the
+      // off-diagonal variable actually has dofs.
+      if (dirac_kernel->variable().number() == ivariable->number()
+          && ivariable->activeOnSubdomain(_subdomain)
+          && jvariable->activeOnSubdomain(_subdomain)
+          && (jvariable->numberOfDofs() > 0))
       {
-        // Get a list of coupled variables from the SubProblem
-        std::vector<std::pair<MooseVariable *, MooseVariable *> > & coupling_entries =
-          dirac_kernel->subProblem().assembly(_tid).couplingEntries();
-
-        // Loop over the list of coupled variable pairs
-        {
-          std::vector<std::pair<MooseVariable *, MooseVariable *> >::iterator
-            var_pair_iter = coupling_entries.begin(),
-            var_pair_end = coupling_entries.end();
-
-          for (; var_pair_iter != var_pair_end; ++var_pair_iter)
-          {
-            MooseVariable * ivariable = var_pair_iter->first;
-            MooseVariable * jvariable = var_pair_iter->second;
-
-            // A variant of the check that is in
-            // ComputeFullJacobianThread::computeJacobian().  We
-            // only want to call computeOffDiagJacobian() if both
-            // variables are active on this subdomain, and the
-            // off-diagonal variable actually has dofs.
-            if (dirac_kernel->variable().number() == ivariable->number()
-                && ivariable->activeOnSubdomain(_subdomain)
-                && jvariable->activeOnSubdomain(_subdomain)
-                && (jvariable->numberOfDofs() > 0))
-            {
-              dirac_kernel->subProblem().prepareShapes(jvariable->number(), _tid);
-              dirac_kernel->computeOffDiagJacobian(jvariable->number());
-            }
-          }
-        }
+        dirac_kernel->subProblem().prepareShapes(jvariable->number(), _tid);
+        dirac_kernel->computeOffDiagJacobian(jvariable->number());
       }
     }
   }
