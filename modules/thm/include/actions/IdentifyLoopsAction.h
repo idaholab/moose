d8080adadf41ef036f91d0dc81b2380d3628e8f1
hansje <joshua.hansel@inl.gov>
2017-03-10 07:46:58 -0700

    Added ability to identify component loops
    Closes idaholab/relap-7#387

---
+++ modules/thm/include/actions/IdentifyLoopsAction.h
@@ -0,0 +1,23 @@
+#ifndef IDENTIFYLOOPSACTION_H
+#define IDENTIFYLOOPSACTION_H
+
+#include "R7Action.h"
+
+class IdentifyLoopsAction;
+
+template<>
+InputParameters validParams<IdentifyLoopsAction>();
+
+/**
+ * Identifies the component loops.
+ */
+class IdentifyLoopsAction : public R7Action
+{
+public:
+  IdentifyLoopsAction(InputParameters parameters);
+  virtual ~IdentifyLoopsAction();
+
+  virtual void act();
+};
+
+#endif /* IDENTIFYLOOPSACTION_H */
