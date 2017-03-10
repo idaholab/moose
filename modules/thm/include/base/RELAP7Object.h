57da44f7e9043dc1b73903a6a7699b82827dd261
hansje <joshua.hansel@inl.gov>
2017-03-10 14:56:37 -0700

    Added functions to pass parameters
    Closes idaholab/relap-7#390

--- modules/thm/include/base/RELAP7Object.h
+++ modules/thm/include/base/RELAP7Object.h
@@ -16,6 +16,45 @@ class RELAP7Object : public MooseObject
 public:
   RELAP7Object(const InputParameters & parameters);
   virtual ~RELAP7Object();
+
+protected:
+  /**
+   * Passes a parameter from this object's input parameters to another set of input parameters.
+   *
+   * @tparam T  the type of the parameter to be passed
+   * @param name[in] name  the name of this object's parameter
+   * @param new_name[in] new_name  the name of the corresponding parameters in \c params
+   * @param params[in,out] params  the parameters to which the parameter will be passed
+   */
+  template <typename T>
+  void passParameter(const std::string & name, const std::string & new_name, InputParameters & params) const;
+
+  /**
+   * Passes a parameter from this object's input parameters to another set of input parameters.
+   *
+   * This version overloads the other by assuming that the parameter has the same name.
+   *
+   * @tparam T  the type of the parameter to be passed
+   * @param name[in] name  the name of the parameter
+   * @param params[in,out] params  the parameters to which the parameter will be passed
+   */
+  template <typename T>
+  void passParameter(const std::string & name, InputParameters & params) const;
 };
+
+template <typename T>
+void
+RELAP7Object::passParameter(const std::string & name, const std::string & new_name, InputParameters & params) const
+{
+  if (isParamValid(name))
+    params.set<T>(new_name) = _pars.get<T>(name);
+}
+
+template <typename T>
+void
+RELAP7Object::passParameter(const std::string & name, InputParameters & params) const
+{
+  passParameter<T>(name, name, params);
+}
 
 #endif /* RELAP7OBJECT_H */
