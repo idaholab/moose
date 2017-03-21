27ab0eb4db179531661d2a4ac08c749a5f84d4a0
Daniel Schwen <daniel.schwen@inl.gov>
2017-03-21 11:27:11 -0600

    Address comments-ish (#8770)

--- framework/scripts/compile_commands.py
+++ framework/scripts/compile_commands.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+
+# This script is used by the `make compile_commands.json` build command.
+# It reformats the raw dump of current directory, compiler command, compiler
+# options, and list of source files into a well formed JSON file that adheres to
+# the format specified in https://clang.llvm.org/docs/JSONCompilationDatabase.html
+
 import sys, os
 import json
 
