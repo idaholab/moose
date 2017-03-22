d46a24ebdfad4bf64ef2a189033797a0d342eedb
Roy H. Stogner <roystgnr@ices.utexas.edu>
2017-03-22 15:05:29 -0500

    Add missing header to modules .C file
    Not sure how I didn't get this before rebasing; I could swear the last
    commit to this file predates my branch.

--- modules/porous_flow/src/kernels/PorousFlowFullySaturatedMassTimeDerivative.C
+++ modules/porous_flow/src/kernels/PorousFlowFullySaturatedMassTimeDerivative.C
@@ -7,6 +7,9 @@
 
 #include "PorousFlowFullySaturatedMassTimeDerivative.h"
 
+// MOOSE includes
+#include "MooseVariable.h"
+
 template <>
 InputParameters
 validParams<PorousFlowFullySaturatedMassTimeDerivative>()
