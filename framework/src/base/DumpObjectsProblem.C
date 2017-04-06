c714ab0fcc88d6bd5dcad3af16d519e4b5de4a48
Daniel Schwen <daniel.schwen@inl.gov>
2017-04-06 10:58:04 -0600

    Minimal parameter set, add variables (#8875)

--- framework/src/base/DumpObjectsProblem.C
+++ framework/src/base/DumpObjectsProblem.C
@@ -12,7 +12,10 @@
 /*            See COPYRIGHT for full restrictions               */
 /****************************************************************/
 #include "DumpObjectsProblem.h"
+#include "MooseMesh.h"
 #include <sstream>
+
+#include "libmesh/string_to_enum.h"
 
 template <>
 InputParameters
@@ -34,6 +37,8 @@ DumpObjectsProblem::addVariable(const std::string & var_name,
                                 Real scale_factor,
                                 const std::set<SubdomainID> * const active_subdomains)
 {
+  dumpVariableHelper(
+      "Variables", var_name, type.family, type.order, scale_factor, active_subdomains);
   FEProblem::addVariable(var_name, type, scale_factor, active_subdomains);
 }
 
@@ -43,6 +48,7 @@ DumpObjectsProblem::addScalarVariable(const std::string & var_name,
                                       Real scale_factor,
                                       const std::set<SubdomainID> * const active_subdomains)
 {
+  dumpVariableHelper("Variables", var_name, SCALAR, order, scale_factor, active_subdomains);
   FEProblem::addScalarVariable(var_name, order, scale_factor, active_subdomains);
 }
 
@@ -51,6 +57,7 @@ DumpObjectsProblem::addAuxVariable(const std::string & var_name,
                                    const FEType & type,
                                    const std::set<SubdomainID> * const active_subdomains)
 {
+  dumpVariableHelper("AuxVariables", var_name, type.family, type.order, 1.0, active_subdomains);
   FEProblem::addAuxVariable(var_name, type, active_subdomains);
 }
 
@@ -60,6 +67,7 @@ DumpObjectsProblem::addAuxScalarVariable(const std::string & var_name,
                                          Real scale_factor,
                                          const std::set<SubdomainID> * const active_subdomains)
 {
+  dumpVariableHelper("AuxVariables", var_name, SCALAR, order, 1.0, active_subdomains);
   FEProblem::addScalarVariable(var_name, order, scale_factor, active_subdomains);
 }
 
@@ -68,8 +76,8 @@ DumpObjectsProblem::addFunction(std::string type,
                                 const std::string & name,
                                 InputParameters parameters)
 {
+  dumpObjectHelper("Functions", type, name, parameters);
   FEProblem::addFunction(type, name, parameters);
-  dumpHelper("Functions", type, name, parameters);
 }
 
 void
@@ -77,8 +85,8 @@ DumpObjectsProblem::addKernel(const std::string & type,
                               const std::string & name,
                               InputParameters parameters)
 {
+  dumpObjectHelper("Kernels", type, name, parameters);
   FEProblem::addKernel(type, name, parameters);
-  dumpHelper("Kernels", type, name, parameters);
 }
 
 void
@@ -86,8 +94,8 @@ DumpObjectsProblem::addNodalKernel(const std::string & type,
                                    const std::string & name,
                                    InputParameters parameters)
 {
+  dumpObjectHelper("NodalKernel", type, name, parameters);
   FEProblem::addNodalKernel(type, name, parameters);
-  dumpHelper("NodalKernel", type, name, parameters);
 }
 
 void
@@ -95,8 +103,8 @@ DumpObjectsProblem::addScalarKernel(const std::string & type,
                                     const std::string & name,
                                     InputParameters parameters)
 {
+  dumpObjectHelper("ScalarKernels", type, name, parameters);
   FEProblem::addScalarKernel(type, name, parameters);
-  dumpHelper("ScalarKernels", type, name, parameters);
 }
 
 void
@@ -104,8 +112,8 @@ DumpObjectsProblem::addBoundaryCondition(const std::string & type,
                                          const std::string & name,
                                          InputParameters parameters)
 {
+  dumpObjectHelper("BCs", type, name, parameters);
   FEProblem::addBoundaryCondition(type, name, parameters);
-  dumpHelper("BCs", type, name, parameters);
 }
 
 void
@@ -113,8 +121,8 @@ DumpObjectsProblem::addConstraint(const std::string & type,
                                   const std::string & name,
                                   InputParameters parameters)
 {
+  dumpObjectHelper("Constraints", type, name, parameters);
   FEProblem::addConstraint(type, name, parameters);
-  dumpHelper("Constraints", type, name, parameters);
 }
 
 void
@@ -122,8 +130,8 @@ DumpObjectsProblem::addAuxKernel(const std::string & type,
                                  const std::string & name,
                                  InputParameters parameters)
 {
+  dumpObjectHelper("AuxKernels", type, name, parameters);
   FEProblem::addAuxKernel(type, name, parameters);
-  dumpHelper("AuxKernels", type, name, parameters);
 }
 
 void
@@ -131,8 +139,8 @@ DumpObjectsProblem::addAuxScalarKernel(const std::string & type,
                                        const std::string & name,
                                        InputParameters parameters)
 {
+  dumpObjectHelper("AuxScalarKernels", type, name, parameters);
   FEProblem::addAuxScalarKernel(type, name, parameters);
-  dumpHelper("AuxScalarKernels", type, name, parameters);
 }
 
 void
@@ -140,8 +148,8 @@ DumpObjectsProblem::addDiracKernel(const std::string & type,
                                    const std::string & name,
                                    InputParameters parameters)
 {
+  dumpObjectHelper("DiracKernels", type, name, parameters);
   FEProblem::addDiracKernel(type, name, parameters);
-  dumpHelper("DiracKernels", type, name, parameters);
 }
 
 void
@@ -149,8 +157,8 @@ DumpObjectsProblem::addDGKernel(const std::string & type,
                                 const std::string & name,
                                 InputParameters parameters)
 {
+  dumpObjectHelper("DGKernels", type, name, parameters);
   FEProblem::addDGKernel(type, name, parameters);
-  dumpHelper("DGKernels", type, name, parameters);
 }
 
 void
@@ -158,8 +166,8 @@ DumpObjectsProblem::addInterfaceKernel(const std::string & type,
                                        const std::string & name,
                                        InputParameters parameters)
 {
+  dumpObjectHelper("InterfaceKernels", type, name, parameters);
   FEProblem::addInterfaceKernel(type, name, parameters);
-  dumpHelper("InterfaceKernels", type, name, parameters);
 }
 
 void
@@ -167,8 +175,8 @@ DumpObjectsProblem::addInitialCondition(const std::string & type,
                                         const std::string & name,
                                         InputParameters parameters)
 {
+  dumpObjectHelper("ICs", type, name, parameters);
   FEProblem::addInitialCondition(type, name, parameters);
-  dumpHelper("ICs", type, name, parameters);
 }
 
 void
@@ -176,32 +184,135 @@ DumpObjectsProblem::addMaterial(const std::string & type,
                                 const std::string & name,
                                 InputParameters parameters)
 {
+  dumpObjectHelper("Materials", type, name, parameters);
   FEProblem::addMaterial(type, name, parameters);
-  dumpHelper("Materials", type, name, parameters);
+}
+
+std::string
+DumpObjectsProblem::deduceNecessaryParameters(const std::string & type,
+                                              const InputParameters & parameters)
+{
+  auto factory_params = stringifyParameters(_factory.getValidParams(type));
+  auto specified_params = stringifyParameters(parameters);
+
+  std::string param_text;
+  for (auto & value_pair : specified_params)
+  {
+    // parameter name
+    const auto & param_name = value_pair.first;
+    const auto & param_value = value_pair.second;
+
+    auto factory_it = factory_params.find(param_name);
+    if (factory_it == factory_params.end() || factory_it->second != param_value)
+      param_text += "    " + param_name + " = " + param_value + '\n';
+  }
+
+  return param_text;
 }
 
 void
-DumpObjectsProblem::dumpHelper(const std::string & system,
-                               const std::string & type,
-                               const std::string & name,
-                               InputParameters parameters)
+DumpObjectsProblem::dumpObjectHelper(const std::string & system,
+                                     const std::string & type,
+                                     const std::string & name,
+                                     const InputParameters & parameters)
 {
   auto path = _app.actionWarehouse().getCurrentActionName();
+  auto param_text = deduceNecessaryParameters(type, parameters);
+
+  // clang-format off
+  _generated_syntax[path][system] +=
+        "  [./" + name + "]\n"
+      + "    type = " + type + '\n'
+      +      param_text
+      + "  [../]\n";
+  // clang-format on
+}
+
+void
+DumpObjectsProblem::dumpVariableHelper(const std::string & system,
+                                       const std::string & var_name,
+                                       FEFamily family,
+                                       Order order,
+                                       Real scale_factor,
+                                       const std::set<SubdomainID> * const active_subdomains)
+{
+  auto path = _app.actionWarehouse().getCurrentActionName();
+  std::string param_text;
+
+  if (active_subdomains)
+  {
+    std::string blocks;
+    for (auto & subdomain_id : *active_subdomains)
+    {
+      auto subdomain_name = _mesh.getMesh().subdomain_name(subdomain_id);
+      if (subdomain_name == "")
+        subdomain_name = std::to_string(subdomain_id);
+
+      if (!blocks.empty())
+        blocks += ' ';
+
+      blocks += subdomain_name;
+    }
+
+    if (active_subdomains->size() > 1)
+      blocks = "'" + blocks + "'";
+
+    param_text += "    blocks = " + blocks + '\n';
+  }
+
+  if (family != LAGRANGE)
+    param_text += "    family = " + libMesh::Utility::enum_to_string<FEFamily>(family) + '\n';
+  if (order != FIRST)
+    param_text += "    order = " + libMesh::Utility::enum_to_string<Order>(order) + '\n';
+  if (scale_factor != 1.0)
+    param_text += "    scale = " + std::to_string(scale_factor);
+
+  // clang-format off
+  _generated_syntax[path][system] +=
+        "  [./" + var_name + "]\n"
+      +      param_text
+      + "  [../]\n";
+  // clang-format on
+}
+
+void
+DumpObjectsProblem::initialSetup()
+{
+  dumpGeneratedSyntax(getParam<std::string>("dump_path"));
+  std::abort();
+}
+
+void
+DumpObjectsProblem::dumpGeneratedSyntax(const std::string path)
+{
+  auto pathit = _generated_syntax.find(path);
+  if (pathit == _generated_syntax.end())
+    return;
+
+  for (const auto & system_pair : pathit->second)
+    Moose::out << '[' << system_pair.first << "]\n" << system_pair.second << "[]\n\n";
+}
+
+std::map<std::string, std::string>
+DumpObjectsProblem::stringifyParameters(const InputParameters & parameters)
+{
+  std::map<std::string, std::string> parameter_map;
 
   std::string syntax;
-  if (isParamValid("parser_syntax"))
+  if (parameters.isParamValid("parser_syntax"))
     syntax = parameters.get<std::string>("parser_syntax");
 
-  std::string param_text;
   for (auto & value_pair : parameters)
   {
     // parameter name
     const auto & param_name = value_pair.first;
 
-    if (parameters.isParamSetByUser(param_name) && !parameters.isPrivate(param_name))
+    if (!parameters.isPrivate(param_name) && parameters.isParamValid(param_name))
     {
       if (param_name == "control_tags")
       {
+        // deal with the control tags. The current parser_syntax is automatically added to this. So
+        // we can remove the parameter if that's all there is in it
       }
       else
       {
@@ -227,34 +338,10 @@ DumpObjectsProblem::dumpHelper(const std::string & system,
         if (param_value.find_first_of(" ") != std::string::npos)
           param_value = "'" + param_value + "'";
 
-        param_text += "    " + param_name + " = " + param_value + '\n';
+        parameter_map[param_name] = param_value;
       }
     }
   }
 
-  // clang-format off
-  _generated_syntax[path][system] +=
-        "  [./" + name + "]\n"
-      + "    type = " + type + '\n'
-      +      param_text
-      + "  [../]\n";
-  // clang-format on
-}
-
-void
-DumpObjectsProblem::initialSetup()
-{
-  dumpGeneratedSyntax(getParam<std::string>("dump_path"));
-  std::abort();
-}
-
-void
-DumpObjectsProblem::dumpGeneratedSyntax(const std::string path)
-{
-  auto pathit = _generated_syntax.find(path);
-  if (pathit == _generated_syntax.end())
-    return;
-
-  for (const auto & system_pair : pathit->second)
-    Moose::out << '[' << system_pair.first << "]\n" << system_pair.second << "[]\n\n";
+  return parameter_map;
 }
