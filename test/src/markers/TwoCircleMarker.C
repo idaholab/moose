924292e33d9196e9f1fa8d03745de1cc755f3ebf
Jesse Carter <jesse.carter@gmail.com>
2017-04-07 21:44:41 -0600

    apply clang format patch

--- test/src/markers/TwoCircleMarker.C
+++ test/src/markers/TwoCircleMarker.C
@@ -25,8 +25,9 @@ validParams<TwoCircleMarker>()
   params.addRequiredParam<Point>("point2", "The center of the second circle.");
   params.addRequiredParam<Real>("radius2",
                                 "Distance from the center of the second circle to mark elements.");
-  params.addParam<Real>("shut_off_time", std::numeric_limits<Real>::max(),
-                                "Time at which the second circle becomes inactive.");
+  params.addParam<Real>("shut_off_time",
+                        std::numeric_limits<Real>::max(),
+                        "Time at which the second circle becomes inactive.");
 
   MooseEnum marker_states = Marker::markerStates();
 
@@ -55,7 +56,8 @@ TwoCircleMarker::computeElementMarker()
 {
   Point centroid = _current_elem->centroid();
 
-  if (((centroid - _p1).size() < _r1) || (((centroid - _p2).size() < _r2) && (_fe_problem.time() < _shut_off_time)))
+  if (((centroid - _p1).size() < _r1) ||
+      (((centroid - _p2).size() < _r2) && (_fe_problem.time() < _shut_off_time)))
     return _inside;
 
   return _outside;
