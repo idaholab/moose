34bf68897e280168c987c3d0e1fddd2ecfba765c
hansje <joshua.hansel@inl.gov>
2017-04-11 16:21:30 -0600

    Renamed OneD7EqnMomentumAreaGradient to OneDMomentumAreaGradient
    Since OneD7EqnMomentumAreaGradient now applies to both 1-phase and
    2-phase, it should be renamed, as per the current convention.
    
    Refs idaholab/relap-7#19688

---
+++ modules/thm/include/kernels/OneDMomentumAreaGradient.h
@@ -0,0 +1,50 @@
+#ifndef ONEDMOMENTUMAREAGRADIENT_H
+#define ONEDMOMENTUMAREAGRADIENT_H
+
+#include "Kernel.h"
+#include "DerivativeMaterialInterfaceRelap.h"
+
+class OneDMomentumAreaGradient;
+
+template <>
+InputParameters validParams<OneDMomentumAreaGradient>();
+
+/**
+ * Computes the area gradient term in the momentum equation.
+ *
+ * This kernel computes the following area gradient term in the phase-\f$k\f$ momentum equation:
+ * \f[
+ *   p_k \alpha_k \frac{\partial A}{\partial x} .
+ * \f]
+ */
+class OneDMomentumAreaGradient : public DerivativeMaterialInterfaceRelap<Kernel>
+{
+public:
+  OneDMomentumAreaGradient(const InputParameters & parameters);
+  virtual ~OneDMomentumAreaGradient();
+
+protected:
+  virtual Real computeQpResidual();
+  virtual Real computeQpJacobian();
+  virtual Real computeQpOffDiagJacobian(unsigned int jvar);
+
+  bool _is_liquid;
+  Real _sign;
+
+  const VariableValue & _alpha;
+  const VariableValue & _area;
+  const VariableGradient & _area_grad;
+
+  const MaterialProperty<Real> & _pressure;
+  const MaterialProperty<Real> * const _dp_dbeta;
+  const MaterialProperty<Real> & _dp_darhoA;
+  const MaterialProperty<Real> & _dp_darhouA;
+  const MaterialProperty<Real> * const _dp_darhoEA;
+  const MaterialProperty<Real> * const _daL_dbeta;
+
+  unsigned int _alpha_rhoA_var_number;
+  unsigned int _alpha_rhoE_var_number;
+  unsigned int _beta_var_number;
+};
+
+#endif /* ONEDMOMENTUMAREAGRADIENT_H */
---
+++ modules/thm/src/kernels/OneDMomentumAreaGradient.C
@@ -0,0 +1,75 @@
+#include "OneDMomentumAreaGradient.h"
+
+template <>
+InputParameters
+validParams<OneDMomentumAreaGradient>()
+{
+  InputParameters params = validParams<Kernel>();
+  params.addParam<bool>("is_liquid", false, "True for liquid phase, false for vapor");
+  params.addRequiredCoupledVar("alpha_rho_A", "The density of the kth phase");
+  params.addRequiredCoupledVar("alpha_rhou_A", "The momentum of the kth phase");
+  params.addCoupledVar("alpha_rhoE_A", "The total energy of the kth phase");
+  params.addRequiredCoupledVar("area", "Cross-sectional area");
+  params.addCoupledVar("alpha", 1, "The volume fraction of the kth phase");
+  params.addCoupledVar("beta", 0, "Remapped volume fraction of liquid (two-phase only)");
+  params.addRequiredParam<MaterialPropertyName>("pressure", "Pressure");
+  params.addParam<MaterialPropertyName>("daL_dbeta", "Derivative of alphaL w.r.t. beta");
+  return params;
+}
+
+OneDMomentumAreaGradient::OneDMomentumAreaGradient(const InputParameters & parameters)
+  : DerivativeMaterialInterfaceRelap<Kernel>(parameters),
+    _is_liquid(getParam<bool>("is_liquid")),
+    _sign(_is_liquid ? 1. : -1.),
+    _alpha(coupledValue("alpha")),
+    _area(coupledValue("area")),
+    _area_grad(coupledGradient("area")),
+    _pressure(getMaterialProperty<Real>("pressure")),
+    _dp_dbeta(isCoupled("beta") ? &getMaterialPropertyDerivativeRelap<Real>("pressure", "beta")
+                                : NULL),
+    _dp_darhoA(getMaterialPropertyDerivativeRelap<Real>("pressure", "alpha_rho_A")),
+    _dp_darhouA(getMaterialPropertyDerivativeRelap<Real>("pressure", "alpha_rhou_A")),
+    _dp_darhoEA(isCoupled("alpha_rhoE_A")
+                    ? &getMaterialPropertyDerivativeRelap<Real>("pressure", "alpha_rhoE_A")
+                    : nullptr),
+    _daL_dbeta(isCoupled("beta") ? &getMaterialProperty<Real>("daL_dbeta") : nullptr),
+    _alpha_rhoA_var_number(coupled("alpha_rho_A")),
+    _alpha_rhoE_var_number(isCoupled("alpha_rhoE_A") ? coupled("alpha_rhoE_A")
+                                                     : libMesh::invalid_uint),
+    _beta_var_number(isCoupled("beta") ? coupled("beta") : libMesh::invalid_uint)
+{
+}
+
+OneDMomentumAreaGradient::~OneDMomentumAreaGradient() {}
+
+Real
+OneDMomentumAreaGradient::computeQpResidual()
+{
+  return -_pressure[_qp] * _alpha[_qp] * _area_grad[_qp](0) * _test[_i][_qp];
+}
+
+Real
+OneDMomentumAreaGradient::computeQpJacobian()
+{
+  return -_dp_darhouA[_qp] * _alpha[_qp] * _area_grad[_qp](0) * _phi[_j][_qp] * _test[_i][_qp];
+}
+
+Real
+OneDMomentumAreaGradient::computeQpOffDiagJacobian(unsigned int jvar)
+{
+  if (jvar == _alpha_rhoA_var_number)
+  {
+    return -_dp_darhoA[_qp] * _alpha[_qp] * _area_grad[_qp](0) * _phi[_j][_qp] * _test[_i][_qp];
+  }
+  else if (jvar == _alpha_rhoE_var_number)
+  {
+    return -(*_dp_darhoEA)[_qp] * _alpha[_qp] * _area_grad[_qp](0) * _phi[_j][_qp] * _test[_i][_qp];
+  }
+  else if (jvar == _beta_var_number)
+  {
+    return -((*_dp_dbeta)[_qp] * _alpha[_qp] + _sign * _pressure[_qp] * (*_daL_dbeta)[_qp]) *
+           _area_grad[_qp](0) * _phi[_j][_qp] * _test[_i][_qp];
+  }
+  else
+    return 0;
+}
