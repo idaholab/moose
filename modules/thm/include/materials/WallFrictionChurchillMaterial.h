4daad1f409b886d8a7938d656a1a06691f50da04
hansje <joshua.hansel@inl.gov>
2017-05-05 13:58:02 -0600

    Renamed WallFrictionMaterial to WallFrictionChurchillMaterial
    Refs idaholab/relap-7#19688

---
+++ modules/thm/include/materials/WallFrictionChurchillMaterial.h
@@ -0,0 +1,40 @@
+#ifndef WALLFRICTIONCHURCHILLMATERIAL_H
+#define WALLFRICTIONCHURCHILLMATERIAL_H
+
+#include "Material.h"
+#include "DerivativeMaterialInterfaceRelap.h"
+
+class WallFrictionChurchillMaterial;
+
+template <>
+InputParameters validParams<WallFrictionChurchillMaterial>();
+
+/**
+ * Computes drag coefficient using the Churchill formula for Fanning friction factor
+ */
+class WallFrictionChurchillMaterial : public DerivativeMaterialInterfaceRelap<Material>
+{
+public:
+  WallFrictionChurchillMaterial(const InputParameters & parameters);
+
+protected:
+  virtual void computeQpProperties();
+
+  /// The wall friction coefficient
+  MaterialProperty<Real> & _Cw;
+  MaterialProperty<Real> & _dCw_drhoA;
+  MaterialProperty<Real> & _dCw_drhouA;
+  MaterialProperty<Real> & _dCw_drhoEA;
+  const MaterialProperty<Real> & _mu;
+
+  /// Density of the phase
+  const VariableValue & _rho;
+  /// Velocity (x-component)
+  const VariableValue & _u_vel;
+  /// Hydraulic diameter
+  const VariableValue & _D_h;
+  /// Roughness of the surface
+  const Real & _roughness;
+};
+
+#endif // WALLFRICTIONCHURCHILLMATERIAL_H
---
+++ modules/thm/src/materials/WallFrictionChurchillMaterial.C
@@ -0,0 +1,43 @@
+#include "WallFrictionChurchillMaterial.h"
+#include "WallFrictionModels.h"
+#include "Numerics.h"
+
+template <>
+InputParameters
+validParams<WallFrictionChurchillMaterial>()
+{
+  InputParameters params = validParams<Material>();
+  params.addRequiredCoupledVar("rhoA", "Mass equation variable: rho*A");
+  params.addRequiredCoupledVar("rhouA", "Momentum equation variable: rho*u*A");
+  params.addRequiredCoupledVar("rhoEA", "Total energy equation variable: rho*E*A");
+  params.addRequiredCoupledVar("rho", "density");
+  params.addRequiredCoupledVar("u_vel", "x-component of the velocity");
+  params.addRequiredCoupledVar("D_h", "hydraulic diameter");
+  params.addRequiredParam<Real>("roughness", "Surface roughness");
+  return params;
+}
+
+WallFrictionChurchillMaterial::WallFrictionChurchillMaterial(const InputParameters & parameters)
+  : DerivativeMaterialInterfaceRelap<Material>(parameters),
+    _Cw(declareProperty<Real>("Cw")),
+    _dCw_drhoA(declarePropertyDerivativeRelap<Real>("Cw", "rhoA")),
+    _dCw_drhouA(declarePropertyDerivativeRelap<Real>("Cw", "rhouA")),
+    _dCw_drhoEA(declarePropertyDerivativeRelap<Real>("Cw", "rhoEA")),
+    _mu(getMaterialProperty<Real>("mu")),
+    _rho(coupledValue("rho")),
+    _u_vel(coupledValue("u_vel")),
+    _D_h(coupledValue("D_h")),
+    _roughness(getParam<Real>("roughness"))
+{
+}
+
+void
+WallFrictionChurchillMaterial::computeQpProperties()
+{
+  Real Re = Reynolds(1, _rho[_qp], _u_vel[_qp], _D_h[_qp], _mu[_qp]);
+
+  _Cw[_qp] = WallFriction::Churchill(Re, _roughness, _D_h[_qp]) * 2. * _rho[_qp] / _D_h[_qp];
+  _dCw_drhoA[_qp] = 0;
+  _dCw_drhouA[_qp] = 0;
+  _dCw_drhoEA[_qp] = 0;
+}
