205af77518f76f6de66b2409c687d53914a07e1b
David Andrs <david.andrs@inl.gov>
2017-05-19 11:14:43 -0600

    Adding SetRealValueControl
    This block takes a control data (of type Real) and set it value in a
    component.
    
    Refs idaholab/relap-7#19889

---
+++ modules/thm/src/controls/SetRealValueControl.C
@@ -0,0 +1,30 @@
+#include "SetRealValueControl.h"
+#include "Function.h"
+
+template <>
+InputParameters
+validParams<SetRealValueControl>()
+{
+  InputParameters params = validParams<RELAP7Control>();
+  params.addRequiredParam<std::string>("component", "The name of the component to be controlled.");
+  params.addRequiredParam<std::string>(
+      "parameter", "The name of the parameter in the component to be controlled.");
+  params.addRequiredParam<std::string>("value",
+                                       "The name of control data to be set in the component.");
+  return params;
+}
+
+SetRealValueControl::SetRealValueControl(const InputParameters & parameters)
+  : RELAP7Control(parameters),
+    _component_name(getParam<std::string>("component")),
+    _param_name(getParam<std::string>("parameter")),
+    _ctrl_param_name("component/" + _component_name + "/" + _param_name),
+    _value(getControlData<Real>("value"))
+{
+}
+
+void
+SetRealValueControl::execute()
+{
+  setControllableValueByName<Real>(_ctrl_param_name, _value);
+}
