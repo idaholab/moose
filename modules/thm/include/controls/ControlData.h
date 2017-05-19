97600fbc9261d796d73d41694f35bed8aecd7a64
David Andrs <david.andrs@inl.gov>
2017-05-19 10:17:43 -0600

    Initial implementation of the new control logic system
    Each control can declare a value that will be computing via
    declareControlData API.  These have a unique name.  The
    values can be retrived within the control system via
    getControlData API.
    
    Refs idaholab/relap-7#19889

---
+++ modules/thm/include/controls/ControlData.h
@@ -0,0 +1,88 @@
+#ifndef CONTROLDATA_H
+#define CONTROLDATA_H
+
+// C++ includes
+#include <vector>
+
+// Forward declarations
+class ControlDataValue;
+
+/**
+ * Abstract definition of a ControlData value.
+ */
+class ControlDataValue
+{
+public:
+  /**
+   * Constructor
+   * @param name The full (unique) name for this piece of data.
+   */
+  ControlDataValue(std::string name) : _name(name) {}
+
+  /**
+   * Destructor.
+   */
+  virtual ~ControlDataValue() = default;
+
+  /**
+   * String identifying the type of parameter stored.
+   * Must be reimplemented in derived classes.
+   */
+  virtual std::string type() = 0;
+
+  /**
+   * The full (unique) name of this particular piece of data.
+   */
+  const std::string & name() const { return _name; }
+
+protected:
+  /// The full (unique) name of this particular piece of data.
+  const std::string _name;
+};
+
+/**
+ * Concrete definition of a parameter value
+ * for a specified type.
+ */
+template <typename T>
+class ControlData : public ControlDataValue
+{
+public:
+  /**
+   * Constructor
+   * @param name The full (unique) name for this piece of data.
+   */
+  ControlData(std::string name) : ControlDataValue(name) { _value_ptr = new T; }
+
+  virtual ~ControlData() { delete _value_ptr; }
+
+  /**
+   * @returns a read-only reference to the parameter value.
+   */
+  const T & get() { return *_value_ptr; }
+
+  /**
+   * @returns a writable reference to the parameter value.
+   */
+  T & set() { return *_value_ptr; }
+
+  /**
+   * String identifying the type of parameter stored.
+   */
+  virtual std::string type() override;
+
+private:
+  /// Stored value.
+  T * _value_ptr;
+};
+
+// ------------------------------------------------------------
+// ControlData<> class inline methods
+template <typename T>
+inline std::string
+ControlData<T>::type()
+{
+  return typeid(T).name();
+}
+
+#endif
