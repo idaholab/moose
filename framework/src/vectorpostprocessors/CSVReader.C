074d95fc626168bbd8a6fe643e35dcde2b47aaeb
Andrew E Slaughter <andrew.slaughter@inl.gov>
2017-05-23 11:19:15 -0600

    Creates VPP for reading CSV data
    (refs #9167)

---
+++ framework/src/vectorpostprocessors/CSVReader.C
@@ -0,0 +1,71 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+// STL includes
+#include <fstream>
+
+// MOOSE includes
+#include "CSVReader.h"
+#include "MooseUtils.h"
+
+template <>
+InputParameters
+validParams<CSVReader>()
+{
+  InputParameters params = validParams<GeneralVectorPostprocessor>();
+  params.addRequiredParam<FileName>("csv_file",
+                                    "The name of the CSV file to read. Currently, with "
+                                    "the exception of the header row, only numeric "
+                                    "values are supported.");
+  params.addParam<bool>(
+      "header",
+      true,
+      "When true it is assumed that the first row contains column headers, these "
+      "headers are used as the VectorPostprocessor vector names. If false the "
+      "file is assumed to contain only numbers and the vectors are named "
+      "automatically based on the VectorPostprocessor block name (e.g., 'foo_0000', 'foo_0001').");
+  params.addParam<std::string>("delimiter",
+                               ",",
+                               "The column delimiter. Despite the name this can read files "
+                               "separated by delimiter other than a comma.");
+  params.set<MultiMooseEnum>("execute_on") = "initial";
+  return params;
+}
+
+CSVReader::CSVReader(const InputParameters & params)
+  : GeneralVectorPostprocessor(params),
+    _csv_reader(getParam<FileName>("csv_file"),
+                getParam<bool>("header"),
+                getParam<std::string>("delimiter"),
+                std::make_shared<libMesh::Parallel::Communicator>(_communicator.get()))
+{
+}
+
+void
+CSVReader::initialize()
+{
+  _csv_reader.read();
+  for (auto & name : _csv_reader.getColumnNames())
+    if (_column_data.find(name) == _column_data.end())
+      _column_data[name] = &declareVector(name);
+}
+
+void
+CSVReader::execute()
+{
+  const std::vector<std::string> & names = _csv_reader.getColumnNames();
+  const std::vector<std::vector<double>> & data = _csv_reader.getColumnData();
+  for (std::size_t i = 0; i < _column_data.size(); ++i)
+    _column_data[names[i]]->assign(data[i].begin(), data[i].end());
+}
