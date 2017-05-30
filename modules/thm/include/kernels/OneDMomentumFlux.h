424f23336ecb5f8c1945c8b33e3efb20e0e193d1
hansje <joshua.hansel@inl.gov>
2017-05-30 15:42:27 -0600

    Changed OneDMomentumFlux to use materials

--- modules/thm/include/kernels/OneDMomentumFlux.h
+++ modules/thm/include/kernels/OneDMomentumFlux.h
@@ -22,22 +22,31 @@ protected:
   virtual Real computeQpJacobian();
   virtual Real computeQpOffDiagJacobian(unsigned int jvar);
 
+  const bool _has_beta;
+
+  const VariableValue & _A;
+
   const MaterialProperty<Real> & _alpha;
   const MaterialProperty<Real> * const _dalpha_dbeta;
 
-  const VariableValue & _vel;
-  const MaterialProperty<Real> & _pressure;
+  const MaterialProperty<Real> & _rho;
+  const MaterialProperty<Real> * const _drho_dbeta;
+  const MaterialProperty<Real> & _drho_darhoA;
+
+  const MaterialProperty<Real> & _vel;
+  const MaterialProperty<Real> & _dvel_darhoA;
+  const MaterialProperty<Real> & _dvel_darhouA;
+
+  const MaterialProperty<Real> & _p;
+  const MaterialProperty<Real> * const _dp_dbeta;
   const MaterialProperty<Real> & _dp_darhoA;
   const MaterialProperty<Real> & _dp_darhouA;
   const MaterialProperty<Real> & _dp_darhoEA;
-  const VariableValue & _area;
 
-  unsigned int _rhoA_var_number;
-  unsigned int _rhoEA_var_number;
-
-  const VariableValue & _beta;
-  unsigned int _beta_var_number;
-  const MaterialProperty<Real> * _dp_dbeta;
+  const unsigned int _beta_var_number;
+  const unsigned int _arhoA_var_number;
+  const unsigned int _arhouA_var_number;
+  const unsigned int _arhoEA_var_number;
 };
 
 #endif /* ONEDMOMENTUMFLUX_H */
--- modules/thm/src/kernels/OneDMomentumFlux.C
+++ modules/thm/src/kernels/OneDMomentumFlux.C
@@ -5,79 +5,85 @@ InputParameters
 validParams<OneDMomentumFlux>()
 {
   InputParameters params = validParams<Kernel>();
-  params.addCoupledVar("beta", 0, "Remapped volume fraction of liquid (two-phase only)");
-  params.addRequiredCoupledVar("rhoA", "alpha*rho*A");
-  params.addRequiredCoupledVar("rhouA", "alpha*rho*u*A");
-  params.addRequiredCoupledVar("rhoEA", "alpha*rho*E*A");
-  params.addRequiredCoupledVar("vel", "Velocity");
-  params.addRequiredCoupledVar("A", "cross-sectional area");
+
+  params.addRequiredCoupledVar("A", "Cross-sectional area");
+
+  params.addCoupledVar("beta", "Remapped volume fraction of liquid (two-phase only)");
+  params.addRequiredCoupledVar("arhoA", "alpha*rho*A");
+  params.addRequiredCoupledVar("arhouA", "alpha*rho*u*A");
+  params.addRequiredCoupledVar("arhoEA", "alpha*rho*E*A");
+
   params.addRequiredParam<MaterialPropertyName>("alpha", "Volume fraction material property");
-  params.addRequiredParam<MaterialPropertyName>("p", "Pressure");
+  params.addRequiredParam<MaterialPropertyName>("rho", "Density material property");
+  params.addRequiredParam<MaterialPropertyName>("vel", "Velocity material property");
+  params.addRequiredParam<MaterialPropertyName>("p", "Pressure material property");
+
   return params;
 }
 
 OneDMomentumFlux::OneDMomentumFlux(const InputParameters & parameters)
   : DerivativeMaterialInterfaceRelap<Kernel>(parameters),
+
+    _has_beta(isCoupled("beta")),
+
+    _A(coupledValue("A")),
+
     _alpha(getMaterialProperty<Real>("alpha")),
-    _dalpha_dbeta(isCoupled("beta") ? &getMaterialPropertyDerivativeRelap<Real>("alpha", "beta")
-                                    : nullptr),
-    _vel(coupledValue("vel")),
-    _pressure(getMaterialProperty<Real>("p")),
-    _dp_darhoA(getMaterialPropertyDerivativeRelap<Real>("p", "rhoA")),
-    _dp_darhouA(getMaterialPropertyDerivativeRelap<Real>("p", "rhouA")),
-    _dp_darhoEA(getMaterialPropertyDerivativeRelap<Real>("p", "rhoEA")),
-    _area(coupledValue("A")),
-    _rhoA_var_number(coupled("rhoA")),
-    _rhoEA_var_number(coupled("rhoEA")),
-    _beta(coupledValue("beta")),
-    _beta_var_number(isCoupled("beta") ? coupled("beta") : libMesh::invalid_uint),
-    _dp_dbeta(isCoupled("beta") ? &getMaterialPropertyDerivativeRelap<Real>("p", "beta") : nullptr)
+    _dalpha_dbeta(_has_beta ? &getMaterialPropertyDerivativeRelap<Real>("alpha", "beta") : nullptr),
+
+    _rho(getMaterialProperty<Real>("rho")),
+    _drho_dbeta(_has_beta ? &getMaterialPropertyDerivativeRelap<Real>("rho", "beta") : nullptr),
+    _drho_darhoA(getMaterialPropertyDerivativeRelap<Real>("rho", "arhoA")),
+
+    _vel(getMaterialProperty<Real>("vel")),
+    _dvel_darhoA(getMaterialPropertyDerivativeRelap<Real>("vel", "arhoA")),
+    _dvel_darhouA(getMaterialPropertyDerivativeRelap<Real>("vel", "arhouA")),
+
+    _p(getMaterialProperty<Real>("p")),
+    _dp_dbeta(_has_beta ? &getMaterialPropertyDerivativeRelap<Real>("p", "beta") : nullptr),
+    _dp_darhoA(getMaterialPropertyDerivativeRelap<Real>("p", "arhoA")),
+    _dp_darhouA(getMaterialPropertyDerivativeRelap<Real>("p", "arhouA")),
+    _dp_darhoEA(getMaterialPropertyDerivativeRelap<Real>("p", "arhoEA")),
+
+    _beta_var_number(_has_beta ? coupled("beta") : libMesh::invalid_uint),
+    _arhoA_var_number(coupled("arhoA")),
+    _arhouA_var_number(coupled("arhouA")),
+    _arhoEA_var_number(coupled("arhoEA"))
 {
 }
 
 Real
 OneDMomentumFlux::computeQpResidual()
 {
-  Real F2 = _u[_qp] * _vel[_qp] + _alpha[_qp] * _pressure[_qp] * _area[_qp];
-  // The contribution due to the convective flux.  Negative sign on
-  // the F2 term comes from integration by parts.
-  return -F2 * _grad_test[_i][_qp](0);
+  return -_alpha[_qp] * (_rho[_qp] * _vel[_qp] * _vel[_qp] + _p[_qp]) * _A[_qp] *
+         _grad_test[_i][_qp](0);
 }
 
 Real
 OneDMomentumFlux::computeQpJacobian()
 {
-  // (2,2) entry of flux Jacobian is the same as the constant area case, p_1 + 2*u
-  Real A22 = 2. * _vel[_qp] + _alpha[_qp] * _dp_darhouA[_qp] * _area[_qp];
-
-  // Negative sign comes from integration by parts
-  return -A22 * _phi[_j][_qp] * _grad_test[_i][_qp](0);
+  return -_alpha[_qp] * (_rho[_qp] * 2.0 * _vel[_qp] * _dvel_darhouA[_qp] + _dp_darhouA[_qp]) *
+         _A[_qp] * _phi[_j][_qp] * _grad_test[_i][_qp](0);
 }
 
 Real
 OneDMomentumFlux::computeQpOffDiagJacobian(unsigned int jvar)
 {
-  if (jvar == _rhoA_var_number)
+  if (jvar == _arhoA_var_number)
   {
-    // (2,1) entry of flux Jacobian is the same as the constant area case, p_0 - u^2
-    Real A21 = _alpha[_qp] * _dp_darhoA[_qp] * _area[_qp] - _vel[_qp] * _vel[_qp];
-
-    // The contribution from the convective flux term.  Negative sign comes from integration by
-    // parts.
-    return -A21 * _phi[_j][_qp] * _grad_test[_i][_qp](0);
+    return -_alpha[_qp] * (_drho_darhoA[_qp] * _vel[_qp] * _vel[_qp] +
+                           _rho[_qp] * 2.0 * _vel[_qp] * _dvel_darhoA[_qp] + _dp_darhoA[_qp]) *
+           _A[_qp] * _phi[_j][_qp] * _grad_test[_i][_qp](0);
   }
-  else if (jvar == _rhoEA_var_number)
+  else if (jvar == _arhoEA_var_number)
   {
-    // (2,3) entry of flux Jacobian is the same as the constant area case, p_2
-    Real A23 = _alpha[_qp] * _dp_darhoEA[_qp] * _area[_qp];
-
-    // Contribution due to convective flux.  Negative sign comes from integration by parts.
-    return -A23 * _phi[_j][_qp] * _grad_test[_i][_qp](0);
+    return -_alpha[_qp] * _dp_darhoEA[_qp] * _A[_qp] * _phi[_j][_qp] * _grad_test[_i][_qp](0);
   }
   else if (jvar == _beta_var_number)
   {
-    return -(_pressure[_qp] * (*_dalpha_dbeta)[_qp] + _alpha[_qp] * (*_dp_dbeta)[_qp]) *
-           _area[_qp] * _phi[_j][_qp] * _grad_test[_i][_qp](0);
+    return -((*_dalpha_dbeta)[_qp] * (_rho[_qp] * _vel[_qp] * _vel[_qp] + _p[_qp]) +
+             _alpha[_qp] * ((*_drho_dbeta)[_qp] * _vel[_qp] * _vel[_qp] + (*_dp_dbeta)[_qp])) *
+           _A[_qp] * _phi[_j][_qp] * _grad_test[_i][_qp](0);
   }
   else
     return 0.;
