228efb8f2014494660716de758d37b5ef673c3de
hansje <joshua.hansel@inl.gov>
2017-05-30 13:07:51 -0600

    Changed OneDMassFlux to use materials

--- modules/thm/include/kernels/OneDMassFlux.h
+++ modules/thm/include/kernels/OneDMassFlux.h
@@ -2,6 +2,7 @@
 #define ONEDMASSFLUX_H
 
 #include "Kernel.h"
+#include "DerivativeMaterialInterfaceRelap.h"
 
 class OneDMassFlux;
 
@@ -11,7 +12,7 @@ InputParameters validParams<OneDMassFlux>();
 /**
  * Mass flux
  */
-class OneDMassFlux : public Kernel
+class OneDMassFlux : public DerivativeMaterialInterfaceRelap<Kernel>
 {
 public:
   OneDMassFlux(const InputParameters & parameters);
@@ -21,8 +22,21 @@ protected:
   virtual Real computeQpJacobian();
   virtual Real computeQpOffDiagJacobian(unsigned int jvar);
 
-  const VariableValue & _rhouA;
-  unsigned int _rhouA_var_number;
+  const VariableValue & _A;
+
+  const MaterialProperty<Real> & _alpha;
+  const MaterialProperty<Real> * const _dalpha_dbeta;
+
+  const MaterialProperty<Real> & _rho;
+  const MaterialProperty<Real> * const _drho_dbeta;
+  const MaterialProperty<Real> & _drho_darhoA;
+
+  const MaterialProperty<Real> & _vel;
+  const MaterialProperty<Real> & _dvel_darhoA;
+  const MaterialProperty<Real> & _dvel_darhouA;
+
+  const unsigned int _beta_var_number;
+  const unsigned int _arhouA_var_number;
 };
 
 #endif /* ONEDMASSFLUX_H */
--- modules/thm/src/kernels/OneDMassFlux.C
+++ modules/thm/src/kernels/OneDMassFlux.C
@@ -5,37 +5,63 @@ InputParameters
 validParams<OneDMassFlux>()
 {
   InputParameters params = validParams<Kernel>();
-  params.addRequiredCoupledVar("rhouA", "conserved x-momentum");
+
+  params.addRequiredCoupledVar("A", "Cross-sectional area");
+  params.addCoupledVar("beta", "Volume fraction mapper");
+  params.addRequiredCoupledVar("arhoA", "alpha*rho*A");
+  params.addRequiredCoupledVar("arhouA", "alpha*rho*u*A");
+
+  params.addRequiredParam<MaterialPropertyName>("alpha", "Volume fraction property");
+  params.addRequiredParam<MaterialPropertyName>("rho", "Density property");
+  params.addRequiredParam<MaterialPropertyName>("vel", "Velocity property");
 
   return params;
 }
 
 OneDMassFlux::OneDMassFlux(const InputParameters & parameters)
-  : Kernel(parameters), _rhouA(coupledValue("rhouA")), _rhouA_var_number(coupled("rhouA"))
+  : DerivativeMaterialInterfaceRelap<Kernel>(parameters),
+    _A(coupledValue("A")),
+
+    _alpha(getMaterialProperty<Real>("alpha")),
+    _dalpha_dbeta(isCoupled("beta") ? &getMaterialPropertyDerivativeRelap<Real>("alpha", "beta")
+                                    : nullptr),
+
+    _rho(getMaterialProperty<Real>("rho")),
+    _drho_dbeta(isCoupled("beta") ? &getMaterialPropertyDerivativeRelap<Real>("rho", "beta")
+                                  : nullptr),
+    _drho_darhoA(getMaterialPropertyDerivativeRelap<Real>("rho", "arhoA")),
+
+    _vel(getMaterialProperty<Real>("vel")),
+    _dvel_darhoA(getMaterialPropertyDerivativeRelap<Real>("vel", "arhoA")),
+    _dvel_darhouA(getMaterialPropertyDerivativeRelap<Real>("vel", "arhouA")),
+
+    _beta_var_number(isCoupled("beta") ? coupled("beta") : libMesh::invalid_uint),
+    _arhouA_var_number(coupled("arhouA"))
 {
 }
 
 Real
 OneDMassFlux::computeQpResidual()
 {
-  // Note: negative sign comes from integration by parts
-  return -_rhouA[_qp] * _grad_test[_i][_qp](0);
+  return -_alpha[_qp] * _rho[_qp] * _vel[_qp] * _A[_qp] * _grad_test[_i][_qp](0);
 }
 
 Real
 OneDMassFlux::computeQpJacobian()
 {
-  // This seems weird at first glance, but this term does not depend
-  // on arhoA, only the conserved variable arhouA.  In other words the (1,1)
-  // entry of the flux Jacobian is 0.
-  return 0.;
+  return -_alpha[_qp] * (_drho_darhoA[_qp] * _vel[_qp] + _rho[_qp] * _dvel_darhoA[_qp]) * _A[_qp] *
+         _phi[_j][_qp] * _grad_test[_i][_qp](0);
 }
 
 Real
 OneDMassFlux::computeQpOffDiagJacobian(unsigned int jvar)
 {
-  if (jvar == _rhouA_var_number)
-    return -_phi[_j][_qp] * _grad_test[_i][_qp](0);
+  if (jvar == _beta_var_number)
+    return -((*_dalpha_dbeta)[_qp] * _rho[_qp] + _alpha[_qp] * (*_drho_dbeta)[_qp]) * _vel[_qp] *
+           _A[_qp] * _phi[_j][_qp] * _grad_test[_i][_qp](0);
+  else if (jvar == _arhouA_var_number)
+    return -_alpha[_qp] * _rho[_qp] * _dvel_darhouA[_qp] * _A[_qp] * _phi[_j][_qp] *
+           _grad_test[_i][_qp](0);
   else
     return 0.;
 }
