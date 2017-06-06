e07a1ca1010a437d5fbd8bff910fcd14d7ab1d87
Jason Miller <jason.miller@inl.gov>
2017-06-06 13:12:03 -0600

    Address comments
    Refs #9248

--- python/TestHarness/tests/test_Syntax.py
+++ python/TestHarness/tests/test_Syntax.py
@@ -5,5 +5,15 @@ class TestHarnessTester(TestHarnessTestCase):
         """
         Test for SYNTAX PASS status in the TestHarness
         """
+
+        # Test that the SYNTAX PASS status message properly displays
         output = self.runTests('-i', 'syntax')
         self.assertIn('SYNTAX PASS', output)
+
+        # Test that the SYNTAX PASS status message properly displays
+        output = self.runTests('--check-input', '-i', 'syntax')
+        self.assertIn('SYNTAX PASS', output)
+
+        # Check that SYNTAX PASS test was not run
+        output = self.runTests('--check-input', '-i', 'no_syntax')
+        self.assertNotIn('SYNTAX PASS', output)
