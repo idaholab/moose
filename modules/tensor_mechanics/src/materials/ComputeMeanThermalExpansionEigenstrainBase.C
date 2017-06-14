5dc24d3336cbebc8da25d928d1193529ae9ed00f
Mark C. Messner <messner@anl.gov>
2017-06-14 13:52:47 -0500

    Option to set stress free temperature with AuxVariable
    Modifies TensorMechanics thermal strain materials to allow users to set
    some non-uniform thermal strain distribution via an AuxVariable,
    initialized with an AuxKernel
    
    closes #9291

--- modules/tensor_mechanics/src/materials/ComputeMeanThermalExpansionEigenstrainBase.C
+++ modules/tensor_mechanics/src/materials/ComputeMeanThermalExpansionEigenstrainBase.C
@@ -19,21 +19,8 @@ validParams<ComputeMeanThermalExpansionEigenstrainBase>()
 
 ComputeMeanThermalExpansionEigenstrainBase::ComputeMeanThermalExpansionEigenstrainBase(
     const InputParameters & parameters)
-  : ComputeThermalExpansionEigenstrainBase(parameters),
-    _alphabar_stress_free_temperature(0.0),
-    _thexp_stress_free_temperature(0.0)
+  : ComputeThermalExpansionEigenstrainBase(parameters)
 {
-}
-
-void
-ComputeMeanThermalExpansionEigenstrainBase::initialSetup()
-{
-  _alphabar_stress_free_temperature = meanThermalExpansionCoefficient(_stress_free_temperature);
-  _thexp_stress_free_temperature =
-      _alphabar_stress_free_temperature * (_stress_free_temperature - referenceTemperature());
-
-  // Evaluate the derivative so it will error out early on if there are any issues with that
-  meanThermalExpansionCoefficientDerivative(_stress_free_temperature);
 }
 
 void
@@ -47,18 +34,31 @@ ComputeMeanThermalExpansionEigenstrainBase::computeThermalStrain(Real & thermal_
   const Real current_alphabar = meanThermalExpansionCoefficient(current_temp);
   const Real thexp_current_temp = current_alphabar * (current_temp - reference_temperature);
 
-  // Per M. Niffenegger and K. Reichlin (2012), thermal_strain should be divided by
-  // (1.0 + _thexp_stress_free_temperature) to account for the ratio of the length at the
-  // stress-free temperature to the length at the reference temperature. It can be neglected
-  // because it is very close to 1, but we include it for completeness here.
+  // Mean linear thermal expansion coefficient relative to the reference temperature
+  // evaluated at stress_free_temperature.  This is
+  // \f$\bar{\alpha} = (\delta L(T_{sf}) / L) / (T_{sf} - T_{ref})\f$
+  // where \f$T_sf\f$ is the stress-free temperature and \f$T_{ref}\f$ is the reference temperature.
+  const Real alphabar_stress_free_temperature =
+      meanThermalExpansionCoefficient(_stress_free_temperature[_qp]);
+  // Thermal expansion relative to the reference temperature evaluated at stress_free_temperature
+  // \f$(\delta L(T_sf) / L)\f$, where \f$T_sf\f$ is the stress-free temperature.
+  const Real thexp_stress_free_temperature =
+      alphabar_stress_free_temperature * (_stress_free_temperature[_qp] - referenceTemperature());
 
-  thermal_strain = (thexp_current_temp - _thexp_stress_free_temperature) /
-                   (1.0 + _thexp_stress_free_temperature);
+  // Per M. Niffenegger and K. Reichlin (2012), thermal_strain should be divided
+  // by (1.0 + thexp_stress_free_temperature) to account for the ratio of
+  // the length at the stress-free temperature to the length at the reference
+  // temperature. It can be neglected because it is very close to 1,
+  // but we include it for completeness here.
+
+  thermal_strain =
+      (thexp_current_temp - thexp_stress_free_temperature) / (1.0 + thexp_stress_free_temperature);
 
   const Real dalphabar_dT = meanThermalExpansionCoefficientDerivative(current_temp);
   const Real numerator = dalphabar_dT * (current_temp - reference_temperature) + current_alphabar;
   const Real denominator =
-      1.0 + _alphabar_stress_free_temperature * (_stress_free_temperature - reference_temperature);
+      1.0 +
+      alphabar_stress_free_temperature * (_stress_free_temperature[_qp] - reference_temperature);
   if (denominator < small)
     mooseError("Denominator too small in thermal strain calculation");
   instantaneous_cte = numerator / denominator;
