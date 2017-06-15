228937931f6f31a56ce21a19a4eb401d7e48cdf5
Giorla, Alain B <giorlaab@ornl.gov>
2017-06-15 16:22:54 -0400

    Add LogConstant time stepper
    - Added a LogConstantDT time stepper and appropriate test
    
    closes #9312

---
+++ framework/src/timesteppers/LogConstantDT.C
@@ -0,0 +1,56 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#include "LogConstantDT.h"
+
+template <>
+InputParameters
+validParams<LogConstantDT>()
+{
+  InputParameters params = validParams<TimeStepper>();
+  params.addClassDescription(
+      "TimeStepper which imposes a time step constant in the logarithmic space");
+  params.addRequiredRangeCheckedParam<Real>("log_dt", "log_dt > 0", "Time step in log10(time)");
+  params.addRequiredRangeCheckedParam<Real>(
+      "first_dt", "first_dt > 0", "Initial time step (in absolute time)");
+  params.addRangeCheckedParam<Real>(
+      "growth_factor",
+      2,
+      "growth_factor>=1",
+      "Maximum ratio of new to previous timestep sizes following a step that required the time"
+      " step to be cut due to a failed solve.");
+  return params;
+}
+
+LogConstantDT::LogConstantDT(const InputParameters & parameters)
+  : TimeStepper(parameters),
+    _log_dt(getParam<Real>("log_dt")),
+    _first_dt(getParam<Real>("first_dt")),
+    _dt_factor(std::pow(10.0, _log_dt)),
+    _growth_factor(getParam<Real>("growth_factor"))
+{
+}
+
+Real
+LogConstantDT::computeInitialDT()
+{
+  return _first_dt;
+}
+
+Real
+LogConstantDT::computeDT()
+{
+  Real next = _time * _dt_factor;
+  return std::min(next - _time, _growth_factor * getCurrentDT());
+}
