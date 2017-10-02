195bd04665dbe06a3de0538a55e4f26bd939c6c6
Chris Green <chris.green@csiro.au>
2017-10-02 13:10:23 +1100

    Change reaction parser to regex system. Refs #9972

--- modules/chemical_reactions/include/actions/AddCoupledSolidKinSpeciesAction.h
+++ modules/chemical_reactions/include/actions/AddCoupledSolidKinSpeciesAction.h
@@ -21,18 +21,40 @@ public:
 
   virtual void act() override;
 
-  /// Prints out list of kinetic reactions
-  void printReactions() const;
-
 private:
+  /// Basis set of primary species
   const std::vector<NonlinearVariableName> _primary_species;
-  const std::vector<std::string> _reactions;
+  /// Secondary species added as AuxVariables
+  const std::vector<AuxVariableName> _secondary_species;
+  /// Secondary solid species read by the parser
+  std::vector<VariableName> _solid_kinetic_species;
+  /// Primary species involved in the ith kinetic reaction
+  std::vector<std::vector<VariableName>> _primary_species_involved;
+  /// Secondary solid species involved the ith primary species
+  std::vector<std::vector<VariableName>> _kinetic_species_involved;
+  /// Stoichiometric coefficients for each primary species in each reaction
+  std::vector<std::vector<Real>> _stos;
+  /// Weight of each primary species in each reaction
+  std::vector<std::vector<Real>> _weights;
+  /// Reaction network read from input file
+  std::string _input_reactions;
+  /// Vector of parsed reactions
+  std::vector<std::string> _reactions;
+  /// Number of reactions
+  unsigned int _num_reactions;
+  /// Log10 of equilibrium constant
   const std::vector<Real> _logk;
+  /// Specific reactive surface area, m^2/L solution
   const std::vector<Real> _r_area;
+  /// Reference kinetic rate constant
   const std::vector<Real> _ref_kconst;
+  /// Activation energy
   const std::vector<Real> _e_act;
+  /// Gas constant, (Default 8.314 J/mol/K)
   const Real _gas_const;
+  /// Reference temperature
   const std::vector<Real> _ref_temp;
+  /// Actual system temperature
   const std::vector<VariableName> _sys_temp;
 };
 
