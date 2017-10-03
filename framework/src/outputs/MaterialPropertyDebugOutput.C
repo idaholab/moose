56987139dfa1f779da48c0b5015b57458978ee96
Andrew E Slaughter <andrew.slaughter@inl.gov>
2017-10-03 12:27:25 -0600

    Improve material property debug message
    (closes #10008)

--- framework/src/outputs/MaterialPropertyDebugOutput.C
+++ framework/src/outputs/MaterialPropertyDebugOutput.C
@@ -18,6 +18,7 @@
 #include "MooseApp.h"
 #include "Material.h"
 #include "ConsoleUtils.h"
+#include "MooseMesh.h"
 
 #include "libmesh/transient_system.h"
 
@@ -50,6 +51,9 @@ MaterialPropertyDebugOutput::printMaterialMap() const
   // Build output streams for block materials and block face materials
   std::stringstream active_block, active_face, active_neighbor, active_boundary;
 
+  // Reference to mesh for getting boundary/block names
+  MooseMesh & mesh = _problem_ptr->mesh();
+
   // Reference to the Material warehouse
   const MaterialWarehouse & warehouse = _problem_ptr->getMaterialWarehouse();
 
@@ -58,7 +62,8 @@ MaterialPropertyDebugOutput::printMaterialMap() const
     const auto & objects = warehouse.getBlockObjects();
     for (const auto & it : objects)
     {
-      active_block << "    Block ID " << it.first << ":\n";
+      active_block << "    Subdomain: " << mesh.getSubdomainName(it.first) << " (" << it.first
+                   << ")\n";
       printMaterialProperties(active_block, it.second);
     }
   }
@@ -68,7 +73,8 @@ MaterialPropertyDebugOutput::printMaterialMap() const
     const auto & objects = warehouse[Moose::FACE_MATERIAL_DATA].getBlockObjects();
     for (const auto & it : objects)
     {
-      active_face << "    Block ID " << it.first << ":\n";
+      active_block << "    Subdomain: " << mesh.getSubdomainName(it.first) << " (" << it.first
+                   << ")\n";
       printMaterialProperties(active_face, it.second);
     }
   }
@@ -78,7 +84,8 @@ MaterialPropertyDebugOutput::printMaterialMap() const
     const auto & objects = warehouse[Moose::NEIGHBOR_MATERIAL_DATA].getBlockObjects();
     for (const auto & it : objects)
     {
-      active_neighbor << "    Block ID " << it.first << ":\n";
+      active_block << "    Subdomain: " << mesh.getSubdomainName(it.first) << " (" << it.first
+                   << ")\n";
       printMaterialProperties(active_neighbor, it.second);
     }
   }
@@ -88,25 +95,23 @@ MaterialPropertyDebugOutput::printMaterialMap() const
     const auto & objects = warehouse.getBoundaryObjects();
     for (const auto & it : objects)
     {
-      active_boundary << "    Boundary ID " << it.first << ":\n";
+      active_boundary << "    Boundary: " << mesh.getBoundaryName(it.first) << " (" << it.first
+                      << ")\n";
       printMaterialProperties(active_boundary, it.second);
     }
   }
 
   // Write the stored strings to the ConsoleUtils output objects
-  _console << "Materials:\n";
-  _console << std::setw(ConsoleUtils::console_field_width) << "  Active Materials on Subdomain:\n";
+  _console << "\n\nActive Materials:\n";
   _console << std::setw(ConsoleUtils::console_field_width) << active_block.str() << '\n';
 
-  _console << std::setw(ConsoleUtils::console_field_width)
-           << "  Active Face Materials on Subdomain:\n";
+  _console << std::setw(ConsoleUtils::console_field_width) << "Active Face Materials:\n";
   _console << std::setw(ConsoleUtils::console_field_width) << active_face.str() << '\n';
 
-  _console << std::setw(ConsoleUtils::console_field_width)
-           << "  Active Neighboring Materials on Subdomain:\n";
+  _console << std::setw(ConsoleUtils::console_field_width) << "Active Neighboring Materials:\n";
   _console << std::setw(ConsoleUtils::console_field_width) << active_neighbor.str() << '\n';
 
-  _console << std::setw(ConsoleUtils::console_field_width) << "  Active Materials on Boundaries:\n";
+  _console << std::setw(ConsoleUtils::console_field_width) << "Active Boundary Materials:\n";
   _console << std::setw(ConsoleUtils::console_field_width) << active_boundary.str() << '\n';
 }
 
