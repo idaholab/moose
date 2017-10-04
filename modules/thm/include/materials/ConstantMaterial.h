bbeb69e2158a75348442dca6b6d1d344a4ce2aa5
Joshua Hansel <joshua.hansel@inl.gov>
2017-10-04 09:03:09 -0600

    Added derivatives to constant material
    This is necessary because the derivative material property
    interface for RELAP-7 is being made more strict; property
    derivatives must always be declared, even zero-valued ones.
    
    Refs idaholab/relap-7#477

--- modules/thm/include/materials/ConstantMaterial.h
+++ modules/thm/include/materials/ConstantMaterial.h
@@ -1,7 +1,7 @@
 #ifndef CONSTANTMATERIAL_H
 #define CONSTANTMATERIAL_H
 
-#include "Material.h"
+#include "DerivativeMaterialInterface.h"
 
 class ConstantMaterial;
 
@@ -9,9 +9,9 @@ template <>
 InputParameters validParams<ConstantMaterial>();
 
 /**
- *
+ * Constant material with zero-valued derivatives
  */
-class ConstantMaterial : public Material
+class ConstantMaterial : public DerivativeMaterialInterface<Material>
 {
 public:
   ConstantMaterial(const InputParameters & parameters);
@@ -20,7 +20,19 @@ protected:
   virtual void computeQpProperties();
 
   const Real & _value;
+
+  const MaterialPropertyName _property_name;
+
   MaterialProperty<Real> & _property;
+
+  /// Number of variables for which to create zero-valued property derivatives
+  const unsigned int _n_derivative_vars;
+
+  /// List of variables for which to create zero-valued property derivatives
+  std::vector<const VariableValue *> _derivative_vars;
+
+  /// Derivatives of material property with respect to each variable
+  std::vector<MaterialProperty<Real> *> _derivative_properties;
 };
 
 #endif /* CONSTANTMATERIAL_H */
--- modules/thm/src/materials/ConstantMaterial.C
+++ modules/thm/src/materials/ConstantMaterial.C
@@ -7,18 +7,31 @@ validParams<ConstantMaterial>()
   InputParameters params = validParams<Material>();
   params.addParam<Real>("value", 0., "Constant value being assigned into the property");
   params.addRequiredParam<std::string>("property_name", "The property name to declare");
+  params.addCoupledVar(
+      "derivative_vars",
+      "Names of variables for which to create (zero) material derivative properties");
   return params;
 }
 
 ConstantMaterial::ConstantMaterial(const InputParameters & parameters)
-  : Material(parameters),
+  : DerivativeMaterialInterface<Material>(parameters),
     _value(getParam<Real>("value")),
-    _property(declareProperty<Real>(getParam<std::string>("property_name")))
+    _property_name(getParam<std::string>("property_name")),
+    _property(declareProperty<Real>(_property_name)),
+    _n_derivative_vars(coupledComponents("derivative_vars"))
 {
+  // get references to new material property derivatives
+  _derivative_properties.resize(_n_derivative_vars);
+  for (unsigned int i = 0; i < _n_derivative_vars; ++i)
+    _derivative_properties[i] =
+        &declarePropertyDerivative<Real>(_property_name, getVar("derivative_vars", i)->name());
 }
 
 void
 ConstantMaterial::computeQpProperties()
 {
   _property[_qp] = _value;
+
+  for (unsigned int i = 0; i < _n_derivative_vars; ++i)
+    (*_derivative_properties[i])[_qp] = 0;
 }
