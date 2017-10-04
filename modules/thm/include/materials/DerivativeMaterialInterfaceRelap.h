e04e39b12539d91f0838a8dc8f7cbacae5ea01b9
Joshua Hansel <joshua.hansel@inl.gov>
2017-10-04 10:34:15 -0600

    Added a new interface for getting 2-phase material property derivatives
    This adds a function which checks the phase of the property against
    the phase of the derivative variable to determine if a zero-valued
    material property should be returned.
    
    Refs idaholab/relap-7#477

--- modules/thm/include/materials/DerivativeMaterialInterfaceRelap.h
+++ modules/thm/include/materials/DerivativeMaterialInterfaceRelap.h
@@ -12,6 +12,7 @@ public:
 protected:
   /**
    * Method for declaring derivative material properties
+   *
    * @tparam U The material property type
    * @param base The name of the property to take the derivative of
    * @param var_name The name of the coupled variable vector containing the variable to take the
@@ -25,7 +26,8 @@ protected:
                                                        const unsigned int i = 0);
 
   /**
-   * Method for retreiving derivative material properties
+   * Method for retrieving derivative material properties
+   *
    * @tparam U The material property type
    * @param base The name of the property to take the derivative of
    * @param var_name The name of the coupled variable vector containing the variable to take the
@@ -37,6 +39,32 @@ protected:
   const MaterialProperty<U> & getMaterialPropertyDerivativeRelap(const std::string & base,
                                                                  const std::string & var_name,
                                                                  const unsigned int i = 0);
+
+  /**
+   * Method for retrieving derivative material properties corresponding to phase-dependent
+   * derivatives.
+   *
+   * The intended behavior of this function is as follows. If the material property
+   * and derivative variable correspond to the same phase, then the material property
+   * is retrieved as usual. Else, a zero-valued material property is returned, as
+   * it is assumed that the material property depends only upon the variables
+   * corresponding to its phase.
+   *
+   * @tparam U The material property type
+   * @param base The name of the property to take the derivative of
+   * @param property_is_liquid  Does the property correspond to the liquid phase?
+   * @param var_name The name of the coupled variable vector containing the variable to take the
+   *   derivative with respect to
+   * @param var_is_liquid  Does the derivative variable correspond to the liquid phase?
+   * @param i The index of the variable to take the derivative with respect to within the coupled
+   *   variable vector
+   */
+  template <typename U>
+  const MaterialProperty<U> & getMaterialPropertyDerivativeRelapPhase(const std::string & base,
+                                                                      bool property_is_liquid,
+                                                                      const std::string & var_name,
+                                                                      bool var_is_liquid,
+                                                                      const unsigned int i = 0);
 };
 
 template <class T>
@@ -71,6 +99,29 @@ DerivativeMaterialInterfaceRelap<T>::getMaterialPropertyDerivativeRelap(
 
   return this->template getMaterialPropertyByName<U>(
       this->template propertyNameFirst(prop_name, der_var_name));
+}
+
+template <class T>
+template <typename U>
+const MaterialProperty<U> &
+DerivativeMaterialInterfaceRelap<T>::getMaterialPropertyDerivativeRelapPhase(
+    const std::string & base,
+    bool property_is_liquid,
+    const std::string & var_name,
+    bool var_is_liquid,
+    const unsigned int i)
+{
+  if (property_is_liquid == var_is_liquid)
+  {
+    return getMaterialPropertyDerivativeRelap<U>(base, var_name, i);
+  }
+  else
+  {
+    const std::string prop_name = this->deducePropertyName(base);
+    const std::string der_var_name = this->template getVar(var_name, i)->name();
+    const std::string der_prop_name = this->template propertyNameFirst(prop_name, der_var_name);
+    return this->template getZeroMaterialProperty<U>(der_prop_name);
+  }
 }
 
 #endif // DERIVATIVEMATERIALINTERFACERELAP_H
