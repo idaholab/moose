6dc0fd5144ef7e0f917077504a14e919e0622365
Alex Lindsay <alexlindsay239@gmail.com>
2017-10-05 08:12:35 -0600

    Commit python work

--- modules/navier_stokes/test/tests/ins/mms/supg/adv_dominated_plot_convergence.py
+++ modules/navier_stokes/test/tests/ins/mms/supg/adv_dominated_plot_convergence.py
@@ -1,8 +1,9 @@
-# To compare convergence of INS variables for SUPG stabilized and unstabilized cases
-# run the run_adv_dominated.py script and then run this script
+# Example script for plotting MMS cases
 
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
+from itertools import chain
 
 # Use fonts that match LaTeX
 from matplotlib import rcParams
@@ -17,27 +18,69 @@ rcParams['text.usetex'] = True
 fontP = FontProperties()
 fontP.set_size('x-small')
 
-h_array = np.array([.25, .125, .0625, .03125, .015625])
+h_array = np.array([.25,
+                    .125,
+                    .0625,
+                    .03125])
+                    # .015625])
+                    # .0078125,
+                    # .00390625])
 variable_names = ['L2p', 'L2vel_x', 'L2vel_y', 'L2vxx']
-strings = ["stabilized", "unstabilized"]
-mu = "1.5e-2"
+strings = ['_'.join(filter(None, ('stabilized', sys.argv[1])))]  # , '_'.join(filter(None, ('unstabilized', sys.argv[1])))]
+# strings = map(lambda x: '_'.join(('stabilized', x)),
+#               ['consistent_pspg_weighting_mu1.5e-4', 'q2q1_consistent_pspg_weighting_mu1.5e-4',
+#                'q2q1_no_pspg_mu1.5e-4', 'q2q1_no_pspg_mu1.5e1'])
+# mus = ["1.5e1", "1.5e0", "5e-1", "1.5e-1", "5e-2", "1.5e-2", "5e-3", "1.5e-3", "1.5e-4"]
+mus = ["1.5e1"]
+error_dict = {'L2p': r'e_p', 'L2vel_x': r'e_{v_x}', 'L2vel_y': r'e_{v_y}', 'L2vxx': r'\nabla e_{v_x}'}
+mu_looping = True
+
 
 for name in variable_names:
     for string in strings:
-        data_array = np.array([])
-        for h in h_array:
-            n = int(1 / h)
-            data_file = "%s_mu%s_%sx%s.csv" % (string, mu, n, n)
-            data = np.genfromtxt(data_file, dtype=float, names=True, delimiter=',')
-            data_array = np.append(data_array, data[name])
-        z = np.polyfit(np.log10(h_array), np.log10(data_array), 1)
-        p = np.poly1d(z)
-        plt.plot(np.log10(h_array), p(np.log10(h_array)), '-')
-        equation = "y=%.1fx+%.1f" % (z[0], z[1])
-        plt.scatter(np.log10(h_array), np.log10(data_array), label=r"$%s; %s$" % (string, equation))
-    plt.xlabel(r"$\log_{10} h$")
-    plt.ylabel(r"$\log_{10} L_2$")
-    plt.legend(prop=fontP)
-    plt.tight_layout()
-    plt.savefig("adv_dominated_convergence_plot_%s.eps" % name, format='eps')
-    plt.close()
+        for mu in mus:
+            data_array = np.array([])
+            for h in h_array:
+                n = int(1 / h)
+                if mu_looping:
+                    data_file = "%s_mu%s_%sx%s.csv" % (string, mu, n, n)
+                else:
+                    data_file = "%s_%sx%s.csv" % (string, n, n)
+                data = np.genfromtxt(data_file, dtype=float, names=True, delimiter=',')
+                data_array = np.append(data_array, data[name])
+            z = np.polyfit(np.log10(h_array), np.log10(data_array), 1)
+            p = np.poly1d(z)
+            plt.plot(np.log10(h_array), p(np.log10(h_array)), '-')
+            equation = "y=%.1fx+%.1f" % (z[0], z[1])
+            m = "%.1f" % z[0]
+            string_list = string.split('_')
+            if string_list[1] != "q2q1":
+                string = '_'.join([string_list[0]] + ["q1q1"] + string_list[1:])
+            if mu_looping:
+                mu_formatted = "%2.1E" % float(mu)
+                mu_dec = mu_formatted[:3]
+                mu_exp = ''.join(filter(lambda x: x != '+' and x != '0', list(mu_formatted[4:])))
+                # plt.scatter(np.log10(h_array), np.log10(data_array),
+                #         label=r"$\mu=%s\cdot 10^{%s}$; $%s$; %s" % (mu_dec, mu_exp, m, ' '.join(string.split('_')[1:])))
+                plt.scatter(np.log10(h_array), np.log10(data_array),
+                        label=r"$%s$; $\|%s\|$" % (m, error_dict[name]))
+            else:
+                plt.scatter(np.log10(h_array), np.log10(data_array),
+                        label=r"$%s$; %s; ||%s||" % (m, ' '.join(string.split('_')[1:]), error_dict[name]))
+
+            # plt.scatter(np.log10(h_array), np.log10(data_array),
+            #             label=r"$\mu=%s\cdot 10^{%s}$; $%s$; %s" % (mu_dec, mu_exp, m, ' '.join(string.split('_')[1:])))
+            # plt.scatter(np.log10(h_array), np.log10(data_array), label=r"$\mu=%s\cdot 10^{%s}$; $%s$" % (mu[:3], mu[4:], equation))
+            # plt.scatter(np.log10(h_array), np.log10(data_array), label=r"%s; %s; $%s$" % (string.split('_')[0], mu, equation))
+plt.xlabel(r"$\log_{10} h$")
+# plt.ylabel(r"$\log_{10} ||%s||$" % error_dict[name])
+plt.ylabel(r"$\log_{10} ||e||$")
+plt.legend(prop=fontP)
+plt.tight_layout()
+if mu_looping:
+    # plt.savefig('_'.join(filter(None, string.split('_')[1:] + [name, "mu" + mu])) + ".eps", format='eps')
+    plt.savefig('_'.join(filter(None, string.split('_')[1:] + ["mu" + mu])) + "_combined_names.eps", format='eps')
+else:
+    # plt.savefig('_'.join(filter(None, string.split('_')[1:] + [name])) + "_method_comparison.eps", format='eps')
+    plt.savefig('_'.join(filter(None, string.split('_')[1:])) + "_combined_names.eps", format='eps')
+plt.close()
--- modules/navier_stokes/test/tests/ins/mms/supg/run_adv_dominated.py
+++ modules/navier_stokes/test/tests/ins/mms/supg/run_adv_dominated.py
@@ -1,36 +1,38 @@
-# To compare convergence of INS variables for SUPG stabilized and unstabilized cases
-# run this script and then adv_dominated_plot_convergence.py
+# Example script for running mms cases
 
 from subprocess import call
 import numpy as np
-'''
-Function for running MMS simulation cases
-'''
-
+import sys
 
 def mms_cases(h_list, string_list, mu):
     for h in h_list:
         n = int(1/h)
         dt = h / 1.8
         for string in string_list:
-            args = ["mpirun", "-np", str(n / 4 + 8), "navier_stokes-opt", "-i", "supg_adv_dominated_mms.i",
+            args = ["mpirun", "-np", str(24), "navier_stokes-opt", "-i", "supg_pspg_adv_dominated_mms.i",
                     "Mesh/nx=%s" % n,
                     "Mesh/ny=%s" % n,
                     "Outputs/file_base=%s_mu%s_%sx%s" % (string, mu, n, n),
                     "Executioner/TimeStepper/dt=%s" % dt,
                     "Executioner/num_steps=1000000",
                     "Executioner/trans_ss_check=true",
-                    "Executioner/ss_check_tol=1e-10"]
+                    "Executioner/ss_check_tol=1e-10",
+                    "mu=%s" % mu]
 
-            if string == "unstabilized":
+            if string.split('_')[0] == "unstabilized":
                 args.append("GlobalParams/supg=false")
             call(args)
 
 
-strings = ["stabilized", "unstabilized"]
+strings = ['_'.join(filter(None, ('stabilized', sys.argv[1])))]  # , '_'.join(filter(None, ('unstabilized', sys.argv[1])))]
 h_list = np.array([.25,
                    .125,
                    .0625,
-                   .03125,
-                   .015625])
-mms_cases(h_list, strings, "1.5e-2")
+                   .03125])
+                   # .015625,
+                   # .0078125,
+                   # .00390625])
+# mus = ["1.5e1", "1.5e0", "5e-1", "1.5e-1", "5e-2", "1.5e-2", "5e-3", "1.5e-3", "1.5e-4"]
+mus = ["1.5e1"]
+for mu in mus:
+    mms_cases(h_list, strings, mu)
