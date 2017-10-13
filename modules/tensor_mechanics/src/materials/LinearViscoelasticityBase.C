4e1c6b281ed0b6f3d76ae67678e64b9514665a3e
Giorla, Alain B <giorlaab@ornl.gov>
2017-10-13 08:26:32 -0400

    clang formatting

--- modules/tensor_mechanics/src/materials/LinearViscoelasticityBase.C
+++ modules/tensor_mechanics/src/materials/LinearViscoelasticityBase.C
@@ -123,7 +123,8 @@ LinearViscoelasticityBase::initQpStatefulProperties()
   _first_elasticity_tensor[_qp].zero();
   _apparent_creep_strain[_qp].zero();
 
-  _springs_elasticity_tensors[_qp].resize((_has_longterm_dashpot && _components > 0) ? _components - 1 : _components, RankFourTensor());
+  _springs_elasticity_tensors[_qp].resize(
+      (_has_longterm_dashpot && _components > 0) ? _components - 1 : _components, RankFourTensor());
   _dashpot_viscosities[_qp].resize(_components, 0);
   _viscous_strains[_qp].resize(_components, RankTwoTensor());
 
