f3260557670203834a49823c9397783da43197aa
David Andrs <david.andrs@inl.gov>
2017-10-20 16:12:12 -0600

    Enabling custom closures for wall friction coefficient (1-phase)
    Users can plug their own custom closure for computing wall friction
    coefficient (f) for single phase flow. Users are required to:
    
    1. call registerClosureType("my-name");
    2. register the new objects via calling
       registerWallFrictionCoefficentSinglePhaseMaterial("my-name", MyClass);
    
    Then saying `closures_type = my-name` in [GlobalParams] will use the new
    closure.
    
    Refs idaholab/relap-7#263

---
+++ modules/thm/src/materials/WallFriction3EqnBaseMaterial.C
@@ -0,0 +1,36 @@
+#include "WallFriction3EqnBaseMaterial.h"
+
+template <>
+InputParameters
+validParams<WallFriction3EqnBaseMaterial>()
+{
+  InputParameters params = validParams<Material>();
+  params.addRequiredCoupledVar("rhoA", "Mass equation variable: rho*A");
+  params.addRequiredCoupledVar("rhouA", "Momentum equation variable: rho*u*A");
+  params.addRequiredCoupledVar("rhoEA", "Total energy equation variable: rho*E*A");
+  params.addRequiredCoupledVar("rho", "Density");
+  params.addRequiredCoupledVar("vel", "x-component of the velocity");
+  params.addRequiredCoupledVar("D_h", "hydraulic diameter");
+
+  params.addRequiredParam<MaterialPropertyName>("Cw", "Drag coefficient material property");
+  params.addRequiredParam<MaterialPropertyName>("mu", "Dynamic viscosity material property");
+
+  params.addRequiredParam<Real>("roughness", "Surface roughness");
+  return params;
+}
+
+WallFriction3EqnBaseMaterial::WallFriction3EqnBaseMaterial(const InputParameters & parameters)
+  : DerivativeMaterialInterfaceRelap<Material>(parameters),
+    _Cw_name(getParam<MaterialPropertyName>("Cw")),
+    _Cw(declareProperty<Real>(_Cw_name)),
+    _dCw_drhoA(declarePropertyDerivativeRelap<Real>(_Cw_name, "rhoA")),
+    _dCw_drhouA(declarePropertyDerivativeRelap<Real>(_Cw_name, "rhouA")),
+    _dCw_drhoEA(declarePropertyDerivativeRelap<Real>(_Cw_name, "rhoEA")),
+
+    _mu(getMaterialProperty<Real>("mu")),
+    _rho(coupledValue("rho")),
+    _vel(coupledValue("vel")),
+    _D_h(coupledValue("D_h")),
+    _roughness(getParam<Real>("roughness"))
+{
+}
