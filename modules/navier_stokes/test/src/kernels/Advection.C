ec285934185f9d5161783719631795013dc45fa6
John Peterson <jwpeterson@gmail.com>
2017-10-25 13:49:39 -0600

    Run clang-format on files.

--- modules/navier_stokes/test/src/kernels/Advection.C
+++ modules/navier_stokes/test/src/kernels/Advection.C
@@ -14,7 +14,8 @@ validParams<Advection>()
 {
   InputParameters params = validParams<INSBase>();
 
-  params.addClassDescription("This class solves the scalar advection equation, $\\vec{a}\\cdot\\nabla u = f$ with SUPG stabilization.");
+  params.addClassDescription("This class solves the scalar advection equation, "
+                             "$\\vec{a}\\cdot\\nabla u = f$ with SUPG stabilization.");
   params.addParam<FunctionName>("forcing_func", 0, "The forcing function, typically used for MMS.");
   MooseEnum tau_type("opt mod");
   params.addRequiredParam<MooseEnum>(
@@ -23,17 +24,19 @@ validParams<Advection>()
 }
 
 Advection::Advection(const InputParameters & parameters)
-    : INSBase(parameters),
-      _ffn(getFunction("forcing_func")),
-      _tau_type(getParam<MooseEnum>("tau_type"))
-{}
+  : INSBase(parameters),
+    _ffn(getFunction("forcing_func")),
+    _tau_type(getParam<MooseEnum>("tau_type"))
+{
+}
 
 Real
 Advection::computeQpResidual()
 {
   Real tau_val = (_tau_type == "opt" ? tauNodal() : tau());
   RealVectorValue U(_u_vel[_qp], _v_vel[_qp], _w_vel[_qp]);
-  return (_test[_i][_qp] + tau_val * (U * _grad_test[_i][_qp])) * (U * _grad_u[_qp] - _ffn.value(_t, _q_point[_qp]));
+  return (_test[_i][_qp] + tau_val * (U * _grad_test[_i][_qp])) *
+         (U * _grad_u[_qp] - _ffn.value(_t, _q_point[_qp]));
 }
 
 Real
