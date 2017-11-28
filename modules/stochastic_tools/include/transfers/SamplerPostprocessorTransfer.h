2193f145998bc4fffe62d65d0e204414dd7e97b7
Sebastian Schunert <snschune@ncsu.edu>
2017-11-28 16:41:59 -0700

    Derive SamplerPostprocessorTransfer from MultiAppVectorPostprocessorTransfer  (#10319)

--- modules/stochastic_tools/include/transfers/SamplerPostprocessorTransfer.h
+++ modules/stochastic_tools/include/transfers/SamplerPostprocessorTransfer.h
@@ -9,7 +9,7 @@
 #define SAMPLERPOSTPROCESSORTRANSFER_H
 
 // MOOSE includes
-#include "MultiAppTransfer.h"
+#include "MultiAppVectorPostprocessorTransfer.h"
 #include "Sampler.h"
 
 // Forward declarations
@@ -24,16 +24,14 @@ InputParameters validParams<SamplerPostprocessorTransfer>();
 /**
  * Transfer Postprocessor from sub-applications to the master application.
  */
-class SamplerPostprocessorTransfer : public MultiAppTransfer
+class SamplerPostprocessorTransfer : public MultiAppVectorPostprocessorTransfer
 {
 public:
   SamplerPostprocessorTransfer(const InputParameters & parameters);
-  virtual void execute() override;
   virtual void initialSetup() override;
 
 protected:
-  /// Name of VPP that will store the data
-  const VectorPostprocessorName & _results_name;
+  virtual void executeFromMultiapp() override;
 
   /// SamplerMultiApp that this transfer is working with
   SamplerMultiApp * _sampler_multi_app;
@@ -43,9 +41,6 @@ protected:
 
   /// Storage for StochasticResults object that data will be transferred to/from
   StochasticResults * _results;
-
-  /// Name of Postprocessor transferring from
-  const std::string & _sub_pp_name;
 };
 
 #endif
