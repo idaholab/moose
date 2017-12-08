3da60f68f41df2c9598eae74dbddb3741c1d11fa
kkq561 <kyoungdockim2013@u.northwestern.edu>
2017-12-07 23:15:49 -0600

    Fix compile errors #10347

--- modules/phase_field/src/kernels/MatGradSqCoupled.C
+++ modules/phase_field/src/kernels/MatGradSqCoupled.C
@@ -1,69 +1,60 @@
-// #include "JouleHeatingSource.h"
 #include "MatGradSqCoupled.h"
 
 template <>
 InputParameters
-// validParams<JouleHeatingSource>()
 validParams<MatGradSqCoupled>()
 {
-//  InputParameters params = validParams<HeatSource>();
   InputParameters params = validParams<Kernel>();
-  params.addCoupledVar("elec", "Electric potential for joule heating.");
-  params.addCoupledVar("args", "Vector of arguments of the diffusivity");
+  params.addCoupledVar("elec", "Electric field");
+  params.addCoupledVar("args", "Additional variable");
   params.addParam<MaterialPropertyName>(
-      "electrical_conductivity",
-      "electrical_conductivity",
-      "Material property providing electrical conductivity of the material.");
+      "prefactor",
+      "prefactor",
+      "Material property providing a prefactor of electric field contribution");
   return params;
 }
 
-// JouleHeatingSource::JouleHeatingSource(const InputParameters & parameters)
 MatGradSqCoupled::MatGradSqCoupled(const InputParameters & parameters)
-//  : DerivativeMaterialInterface<JvarMapKernelInterface<HeatSource>>(parameters),
   : DerivativeMaterialInterface<JvarMapKernelInterface<Kernel>>(parameters),
     _grad_elec(coupledGradient("elec")),
     _elec_var(coupled("elec")),
-    _elec_cond(getMaterialProperty<Real>("electrical_conductivity")),
-    _delec_cond_dT(getMaterialPropertyDerivative<Real>("electrical_conductivity", _var.name())),
-    _delec_cond_darg(_coupled_moose_vars.size())
+    _prefactor(getMaterialProperty<Real>("prefactor")),
+    _dprefactor_dphi(getMaterialPropertyDerivative<Real>("prefactor", _var.name())),
+    _dprefactor_darg(_coupled_moose_vars.size())
 {
-  for (unsigned int i = 0; i < _delec_cond_darg.size(); ++i)
-    _delec_cond_darg[i] = &getMaterialPropertyDerivative<Real>("electrical_conductivity",
+  for (unsigned int i = 0; i < _dprefactor_darg.size(); ++i)
+    _dprefactor_darg[i] = &getMaterialPropertyDerivative<Real>("prefactor",
                                                                _coupled_moose_vars[i]->name());
 }
 
 void
-// JouleHeatingSource::initialSetup()
 MatGradSqCoupled::initialSetup()
 {
-  validateNonlinearCoupling<Real>("electrical_conductivity");
+  validateNonlinearCoupling<Real>("prefactor");
 }
 
 Real
-// JouleHeatingSource::computeQpResidual()
 MatGradSqCoupled::computeQpResidual()
 {
-  return -_elec_cond[_qp] * _grad_elec[_qp] * _grad_elec[_qp] * _test[_i][_qp];
+  return -_prefactor[_qp] * _grad_elec[_qp] * _grad_elec[_qp] * _test[_i][_qp];
 }
 
 Real
-// JouleHeatingSource::computeQpJacobian()
 MatGradSqCoupled::computeQpJacobian()
 {
-  return -_delec_cond_dT[_qp] * _grad_elec[_qp] * _grad_elec[_qp] * _phi[_j][_qp] * _test[_i][_qp];
+  return -_dprefactor_dphi[_qp] * _grad_elec[_qp] * _grad_elec[_qp] * _phi[_j][_qp] * _test[_i][_qp];
 }
 
 Real
-// JouleHeatingSource::computeQpOffDiagJacobian(unsigned int jvar)
 MatGradSqCoupled::computeQpOffDiagJacobian(unsigned int jvar)
 {
   const unsigned int cvar = mapJvarToCvar(jvar);
 
   if (jvar == _elec_var)
-    return -2 * _elec_cond[_qp] * _grad_elec[_qp] * _grad_phi[_j][_qp] * _test[_i][_qp] -
-           (*_delec_cond_darg[cvar])[_qp] * _grad_elec[_qp] * _grad_elec[_qp] * _phi[_j][_qp] *
+    return -2 * _prefactor[_qp] * _grad_elec[_qp] * _grad_phi[_j][_qp] * _test[_i][_qp] -
+           (*_dprefactor_darg[cvar])[_qp] * _grad_elec[_qp] * _grad_elec[_qp] * _phi[_j][_qp] *
                _test[_i][_qp];
 
-  return -(*_delec_cond_darg[cvar])[_qp] * _grad_elec[_qp] * _grad_elec[_qp] * _phi[_j][_qp] *
+  return -(*_dprefactor_darg[cvar])[_qp] * _grad_elec[_qp] * _grad_elec[_qp] * _phi[_j][_qp] *
          _test[_i][_qp];
 }
