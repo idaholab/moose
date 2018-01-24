b3f917d589a59608a60dda302a3d6aa4c074a406
moose-maintenance <bounces@inl.gov>
2018-01-24 08:09:28 -0700

    Update all C++ headers
    closes #10466

--- modules/tensor_mechanics/include/materials/CappedMohrCoulombCosseratStressUpdate.h
+++ modules/tensor_mechanics/include/materials/CappedMohrCoulombCosseratStressUpdate.h
@@ -1,9 +1,12 @@
-/****************************************************************/
-/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
-/*                                                              */
-/*          All contents are licensed under LGPL V2.1           */
-/*             See LICENSE for full restrictions                */
-/****************************************************************/
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
 #ifndef CAPPEDMOHRCOULOMBCOSSERATSTRESSUPDATE_H
 #define CAPPEDMOHRCOULOMBCOSSERATSTRESSUPDATE_H
 
--- modules/xfem/include/base/XFEMFuncs.h
+++ modules/xfem/include/base/XFEMFuncs.h
@@ -1,16 +1,11 @@
-/****************************************************************/
-/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
-/*                                                              */
-/*          All contents are licensed under LGPL V2.1           */
-/*             See LICENSE for full restrictions                */
-/****************************************************************/
-
-/****************************************************************/
-/* This file also contains modified functions from libMesh and  */
-/* the geometry library of John Burkardt                        */
-/* http://people.sc.fsu.edu/~jburkardt/                         */
-/* These libraries are both distributed under the LGPL          */
-/****************************************************************/
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
 
 #ifndef XFEMFUNCS_H
 #define XFEMFUNCS_H
