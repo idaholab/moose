aecbdd8182d8a8c6ef3d2256ee25603cd00173b3
moose-maintenance <bounces@inl.gov>
2018-01-25 16:03:43 -0700

    Update all python headers
    refs #10466

--- modules/combined/test/tests/inelastic_strain/elas_plas/plot_cycled_stress.py
+++ modules/combined/test/tests/inelastic_strain/elas_plas/plot_cycled_stress.py
@@ -1,4 +1,13 @@
 #!/opt/moose/miniconda/bin/python
+#* This file is part of the MOOSE framework
+#* https://www.mooseframework.org
+#*
+#* All rights reserved, see COPYRIGHT for full restrictions
+#* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+#*
+#* Licensed under LGPL 2.1, please see LICENSE for details
+#* https://www.gnu.org/licenses/lgpl-2.1.html
+
 import matplotlib as mpl
 import numpy as np
 import matplotlib.pyplot as plt
