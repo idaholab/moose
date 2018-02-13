2f9e9dd0d461854648e194b23061d36939dcc834
kkq561 <30814421+kkq561@users.noreply.github.com>
2018-02-13 16:30:31 -0600

    Add coupled gradient square kernel (#10721)
    * Add header and source files for MatGradSqCoupled #10347
    
    * Fix compile errors #10347
    
    * Fix compile errors #10347
    
    * Fix compile errors #10347
    
    * nabla_sq #10347
    
    * Add input file for MatGradSqCoupled #10347
    
    * Fix comments #10347
    
    * Fix comments #10347
    
    * Fix errors #10347
    
    * Fix #10347

---
+++ modules/phase_field/src/kernels/MatGradSquareCoupled.C
@@ -0,0 +1,70 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "MatGradSquareCoupled.h"
+
+template <>
+InputParameters
+validParams<MatGradSquareCoupled>()
+{
+  InputParameters params = validParams<Kernel>();
+  params.addCoupledVar("elec_potential", "Electric potential");
+  params.addCoupledVar("args", "Vector of arguments to prefactor");
+  params.addParam<MaterialPropertyName>(
+      "prefactor",
+      "prefactor",
+      "Material property providing a prefactor of electric potential contribution");
+  return params;
+}
+
+MatGradSquareCoupled::MatGradSquareCoupled(const InputParameters & parameters)
+  : DerivativeMaterialInterface<JvarMapKernelInterface<Kernel>>(parameters),
+    _grad_elec_potential(coupledGradient("elec_potential")),
+    _elec_potential_var(coupled("elec_potential")),
+    _prefactor(getMaterialProperty<Real>("prefactor")),
+    _dprefactor_dphi(getMaterialPropertyDerivative<Real>("prefactor", _var.name())),
+    _dprefactor_darg(_coupled_moose_vars.size())
+{
+  for (unsigned int i = 0; i < _dprefactor_darg.size(); ++i)
+    _dprefactor_darg[i] =
+        &getMaterialPropertyDerivative<Real>("prefactor", _coupled_moose_vars[i]->name());
+}
+
+void
+MatGradSquareCoupled::initialSetup()
+{
+  validateNonlinearCoupling<Real>("prefactor");
+}
+
+Real
+MatGradSquareCoupled::computeQpResidual()
+{
+  return -_prefactor[_qp] * _grad_elec_potential[_qp] * _grad_elec_potential[_qp] * _test[_i][_qp];
+}
+
+Real
+MatGradSquareCoupled::computeQpJacobian()
+{
+  return -_dprefactor_dphi[_qp] * _grad_elec_potential[_qp] * _grad_elec_potential[_qp] *
+         _phi[_j][_qp] * _test[_i][_qp];
+}
+
+Real
+MatGradSquareCoupled::computeQpOffDiagJacobian(unsigned int jvar)
+{
+  const unsigned int cvar = mapJvarToCvar(jvar);
+
+  if (jvar == _elec_potential_var)
+    return -2 * _prefactor[_qp] * _grad_elec_potential[_qp] * _grad_phi[_j][_qp] * _test[_i][_qp] -
+           (*_dprefactor_darg[cvar])[_qp] * _grad_elec_potential[_qp] * _grad_elec_potential[_qp] *
+               _phi[_j][_qp] * _test[_i][_qp];
+
+  return -(*_dprefactor_darg[cvar])[_qp] * _grad_elec_potential[_qp] * _grad_elec_potential[_qp] *
+         _phi[_j][_qp] * _test[_i][_qp];
+}
