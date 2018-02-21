b17e73f253e4613e7ff3aad4bc7757dd2c672b0b
Derek Stucki <derek.stucki@inl.gov>
2018-02-21 09:52:54 -0700

    Added setup file to make compilation easier, esp on windows.

---
+++ framework/contrib/hit/setup.py
@@ -0,0 +1,15 @@
+try:
+    from setuptools import setup
+    from setuptools import Extension
+except ImportError:
+    from distutils.core import setup
+    from distutils.extension import Extension
+
+setup(
+    ext_modules=[
+        Extension(
+            "hit",
+            ["hit.cpp", "lex.cc", "parse.cc"],
+        )
+    ]
+)
