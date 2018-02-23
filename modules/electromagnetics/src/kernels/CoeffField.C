42bff5b4e6cfcf1453e35703850c46f00b47784b
Casey Icenhour <cticenho@ncsu.edu>
2018-02-23 16:35:27 -0700

    Edited kernels consistency with textbook strong form, added MMS test.
    Signs changed in both kernels, and CoeffField had a coefficient change
    more consistent with physics (and allows for less rounding errors...).
    
    Method of Manufactured Solutions (MMS1) input file used to test kernel
    structure - file reproduced solution successfully.

--- modules/electromagnetics/src/kernels/CoeffField.C
+++ modules/electromagnetics/src/kernels/CoeffField.C
@@ -3,7 +3,7 @@
 
 template <> InputParameters validParams<CoeffField>() {
   InputParameters params = validParams<Reaction>();
-  params.addParam<Real>("coefficient", 1.0, "Coefficient multiplier for field.");
+  params.addParam<Real>("k", 1.0, "Coefficient multiplier (to be squared) for field.");
   params.addParam<FunctionName>("func",1.0,"Function multiplier for field.");
   return params;
 }
@@ -11,16 +11,16 @@ template <> InputParameters validParams<CoeffField>() {
 CoeffField::CoeffField(const InputParameters & parameters)
     : Reaction(parameters),
 
-      _coefficient(getParam<Real>("coefficient")),
+      _coefficient(getParam<Real>("k")),
 
       _func(getFunction("func"))
 
 {}
 
 Real CoeffField::computeQpResidual() {
-  return _coefficient * _func.value(_t,_q_point[_qp]) * Reaction::computeQpResidual();
+  return -std::pow(_coefficient,2) * _func.value(_t,_q_point[_qp]) * Reaction::computeQpResidual();
 }
 
 Real CoeffField::computeQpJacobian() {
-  return _coefficient * _func.value(_t,_q_point[_qp]) * Reaction::computeQpJacobian();
+  return -std::pow(_coefficient,2) * _func.value(_t,_q_point[_qp]) * Reaction::computeQpJacobian();
 }
