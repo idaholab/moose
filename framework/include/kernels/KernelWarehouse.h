b82036cd241e7dabb082f59c5a1005679bd35882
Derek Gaston <derek.gaston@inl.gov>
2018-02-26 13:09:02 -0700

    Add some backwards compatibility closes #10366

---
+++ framework/include/kernels/KernelWarehouse.h
@@ -0,0 +1,18 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifndef KERNELWAREHOUSE_H
+#define KERNELWAREHOUSE_H
+
+#include "MooseObjectWarehouse.h"
+
+/// This is only here for backwards compatibility.  Can be removed anytime after 9/1/2018
+typedef MooseObjectWarehouse<KernelBase> KernelWarehouse;
+
+#endif
