a76ce3d814f1aaeebab2546c147b05f44862f3dc
Robert Carlsen <rwcarlsen@gmail.com>
2018-03-08 13:04:57 -0700

    doc comments

--- framework/include/base/Registry.h
+++ framework/include/base/Registry.h
@@ -18,26 +18,36 @@
 #define combineNames1(X, Y) X##Y
 #define combineNames(X, Y) combineNames1(X, Y)
 
+/// add an Action to the registry with the given app name/label as being associated with the given
+/// task (quoted string).  classname is the (unquoted) c++ class.
 #define registerMooseAction(app, classname, task)                                                  \
   static char combineNames(dummyvar_for_registering_action_##classname, __LINE__) =                \
       Registry::addAction<classname>(                                                              \
           {app, #classname, "", task, nullptr, nullptr, nullptr, __FILE__, __LINE__, "", ""})
 
+/// Add a MooseObject to the registry with the given app name/label.  classname is the (unquoted)
+/// c++ class.  Each object/class should only be registered once.
 #define registerMooseObject(app, classname)                                                        \
   static char combineNames(dummyvar_for_registering_obj_##classname, __LINE__) =                   \
       Registry::add<classname>(                                                                    \
           {app, #classname, "", "", nullptr, nullptr, nullptr, __FILE__, __LINE__, "", ""})
 
+/// Add a MooseObject to the registry with the given app name/label under an alternate alias/name
+/// (quoted string) instead of the classname.
 #define registerMooseObjectAliased(app, classname, alias)                                          \
   static char combineNames(dummyvar_for_registering_obj_##classname, __LINE__) =                   \
       Registry::add<classname>(                                                                    \
           {app, #classname, alias, "", nullptr, nullptr, nullptr, __FILE__, __LINE__, "", ""})
 
+/// Add a deprecated MooseObject to the registry with the given app name/label. time is the time
+/// the object became/becomes deprecated in "mm/dd/yyyy HH:MM" format.
 #define registerMooseObjectDeprecated(app, classname, time)                                        \
   static char combineNames(dummyvar_for_registering_obj_##classname, __LINE__) =                   \
       Registry::add<classname>(                                                                    \
           {app, #classname, "", "", nullptr, nullptr, nullptr, __FILE__, __LINE__, time, ""})
 
+/// add a deprecated MooseObject to the registry that has been replaced by another
+/// object. time is the time the object became/becomes deprecated in "mm/dd/yyyy hh:mm" format.
 #define registerMooseObjectReplaced(app, classname, time, replacement)                             \
   static char combineNames(dummyvar_for_registering_obj_##classname, __LINE__) =                   \
       Registry::add<classname>({app,                                                               \
@@ -52,6 +62,9 @@
                                 time,                                                              \
                                 #replacement})
 
+/// add a deprecated MooseObject orig_class to the registry that has been replaced by another
+/// object new_class with the same API. time is the time the object became/becomes deprecated in
+/// "mm/dd/yyyy hh:mm" format.
 #define registerMooseObjectRenamed(app, orig_class, time, new_class)                               \
   static char combineNames(dummyvar_for_registering_obj_##orig_name, __LINE__) =                   \
       Registry::add<new_class>({app,                                                               \
@@ -77,18 +90,32 @@ using paramsPtr = InputParameters (*)();
 using buildPtr = std::shared_ptr<MooseObject> (*)(const InputParameters & parameters);
 using buildActionPtr = std::shared_ptr<Action> (*)(const InputParameters & parameters);
 
+/// Holds details and meta-data info for a particular MooseObject or Action for use in the
+/// registry.
 struct RegistryEntry
 {
+  /// label (usually app name - e.g. "YourAnimalApp") that the object or action is associated with.
   std::string _label;
+  /// name of the c++ class for the object.
   std::string _classname;
+  /// an alternate name to register the object to factories under.  If unspecified, _classname is
+  /// used.
   std::string _alias;
+  /// name that the object will be registered to factories under.  If unspecified, _alias is used.
   std::string _name;
+  /// function pointer for building instances of the MooseObject (if the entry is for an object).
   buildPtr _build_ptr;
+  /// function pointer for building instances of the Action (if the entry is for an action).
   buildActionPtr _build_action_ptr;
+  /// function pointer for building InputParameters objects for the object or action.
   paramsPtr _params_ptr;
+  /// file path for the c++ file the object or action was added to the registry in.
   std::string _file;
+  /// line number in the c++ file the object or action was added to the registry on.
   int _line;
+  /// time in "mm/dd/yyyy HH:MM" format that the object is/becomes deprecated, blank otherwise.
   std::string _deprecated_time;
+  /// class name for an object that replaces this object if deprecated, blank otherwise.
   std::string _replaced_by;
 };
 
@@ -106,9 +133,21 @@ buildAct(const InputParameters & parameters)
   return std::make_shared<T>(parameters);
 }
 
+/// The registry is used as a global singleton to collect information on all available MooseObject
+/// and Action classes for use in a moose app/simulation.  It must be global because we want+need
+/// to be able to register objects in global scope during static initialization time before other
+/// parts of the moose app execution have started running.  This allows us to distribute
+/// registration across all the files that define the actual classes being registered so we don't
+/// have to have any central location with a bajillion includes that makes (especially incremental)
+/// compiles slow. The registry collects the app, name, and other information for each objects and
+/// makes it available to the moose object and action factories and others for general use.  All
+/// public functions in this class modify and return data from the global singleton.
 class Registry
 {
 public:
+  /// Adds information on a MooseObject to the registry.  The _build_ptr, _build_action_ptr, and
+  /// _params_ptr objects of the info object should all be nullptr - these are set automatically by
+  /// the add function itself using the templated type T.
   template <typename T>
   static char add(const RegistryEntry & info)
   {
@@ -119,6 +158,9 @@ public:
     return 0;
   }
 
+  /// Adds information on an Action object to the registry.  The _build_ptr, _build_action_ptr, and
+  /// _params_ptr objects of the info object should all be nullptr - these are set automatically by
+  /// the addAction function itself using the templated type T.
   template <typename T>
   static char addAction(const RegistryEntry & info)
   {
@@ -129,11 +171,26 @@ public:
     return 0;
   }
 
+  /// This registers all MooseObjects known to the registry that have the given label(s) with the
+  /// factory f.
   static void registerObjectsTo(Factory & f, const std::set<std::string> & labels);
+
+  /// This registers all Actions known to the registry that have the given label(s) with the
+  /// factory f.
   static void registerActionsTo(ActionFactory & f, const std::set<std::string> & labels);
+
+  /// This runs error checking to make sure that all objects and actions in the registry have valid
+  /// labels.  Labels are considered valid if they have been added to the list of known labels via
+  /// the addKnownLabel function or they have been used to register objects to a factory via
+  /// registerObjectsTo or registerActionsTo.
   static void checkLabels(const std::set<std::string> & known_labels = {});
+
+  /// addKnownLabel whitelists a label as valid for purposes of the checkLabels function.
   static void addKnownLabel(const std::string & label);
+
+  /// Returns a per-label keyed map of all MooseObjects in the registry.
   static const std::map<std::string, std::vector<RegistryEntry>> & allObjects();
+  /// Returns a per-label keyed map of all Actions in the registry.
   static const std::map<std::string, std::vector<RegistryEntry>> & allActions();
 
 private:
