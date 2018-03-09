3f64b5cb4ddb7136e1d9f37ed6a196a68b77baee
Casey Icenhour <cticenho@ncsu.edu>
2018-03-09 15:10:59 -0700

    Convergence test and function for helmholtz coupled field problem.

---
+++ modules/electromagnetics/include/functions/HelmholtzTestFunc.h
@@ -0,0 +1,44 @@
+#ifndef HELMHOLTZTESTFUNC_H
+#define HELMHOLTZTESTFUNC_H
+
+#include "Function.h"
+
+class HelmholtzTestFunc;
+
+template<>
+InputParameters validParams<HelmholtzTestFunc>();
+
+/**
+ *  Function of analytical solution for use in convergence testing with coupled_helmholtz test file
+ */
+class HelmholtzTestFunc : public Function
+{
+public:
+  HelmholtzTestFunc(const InputParameters & parameters);
+
+  virtual Real value(Real t, const Point & p) override;
+
+protected:
+
+  Real _L;
+
+  Real _a;
+  Real _b;
+
+  Real _g0_real;
+  Real _g0_imag;
+
+  Real _gL_real;
+  Real _gL_imag;
+
+  MooseEnum _component;
+
+  Real _C1_real;
+  Real _C1_imag;
+
+  Real _C2_real;
+  Real _C2_imag;
+
+};
+
+#endif // HELMHOLTZTESTFUNC_H
