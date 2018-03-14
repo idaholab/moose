2b40776d60a663ef65855f33fed066bd01307a0f
Joshua Hansel <joshua.hansel@inl.gov>
2018-03-14 10:58:06 -0600

    Refactored rDG BCs to all use caching
    Closes idaholab/relap-7#522

--- modules/thm/src/components/SolidWall.C
+++ modules/thm/src/components/SolidWall.C
@@ -36,30 +36,47 @@ SolidWall::addMooseObjects1Phase()
   }
   else if (_spatial_discretization == FlowModel::rDG)
   {
-    const std::string class_name = "Euler1DVarAreaWallGhostCellBC";
-    InputParameters params = _factory.getValidParams(class_name);
-    params.set<std::vector<BoundaryName>>("boundary") = getBoundaryNames();
-    params.set<std::vector<VariableName>>("A") = {FlowModelSinglePhase::AREA};
-    params.set<std::vector<VariableName>>("rhoA") = {FlowModelSinglePhase::RHOA};
-    params.set<std::vector<VariableName>>("rhouA") = {FlowModelSinglePhase::RHOUA};
-    params.set<std::vector<VariableName>>("rhoEA") = {FlowModelSinglePhase::RHOEA};
-    params.set<UserObjectName>("rdg_flux") = _rdg_flux_name;
-    params.set<bool>("implicit") = _implicit_rdg;
+    ExecFlagEnum userobject_execute_on(MooseUtils::getDefaultExecFlagEnum());
+    userobject_execute_on = {EXEC_LINEAR, EXEC_NONLINEAR};
 
-    // mass
-    params.set<NonlinearVariableName>("variable") = FlowModelSinglePhase::RHOA;
-    _sim.addBoundaryCondition(
-        class_name, Component::genName(name(), class_name, FlowModelSinglePhase::RHOA), params);
+    // boundary flux user object
+    const std::string boundary_flux_name = genName(name(), "boundary_flux");
+    {
+      const std::string class_name = "Euler1DVarAreaWallBoundaryFlux";
+      InputParameters params = _factory.getValidParams(class_name);
+      params.set<UserObjectName>("rdg_flux") = _rdg_flux_name;
+      params.set<bool>("implicit") = _implicit_rdg;
+      params.set<ExecFlagEnum>("execute_on") = userobject_execute_on;
+      _sim.addUserObject(class_name, boundary_flux_name, params);
+    }
 
-    // momentum
-    params.set<NonlinearVariableName>("variable") = FlowModelSinglePhase::RHOUA;
-    _sim.addBoundaryCondition(
-        class_name, Component::genName(name(), class_name, FlowModelSinglePhase::RHOUA), params);
+    // BCs
+    {
+      const std::string class_name = "Euler1DVarAreaBC";
+      InputParameters params = _factory.getValidParams(class_name);
+      params.set<std::vector<BoundaryName>>("boundary") = getBoundaryNames();
+      params.set<UserObjectName>("boundary_flux") = boundary_flux_name;
+      params.set<std::vector<VariableName>>("A") = {FlowModelSinglePhase::AREA};
+      params.set<std::vector<VariableName>>("rhoA") = {FlowModelSinglePhase::RHOA};
+      params.set<std::vector<VariableName>>("rhouA") = {FlowModelSinglePhase::RHOUA};
+      params.set<std::vector<VariableName>>("rhoEA") = {FlowModelSinglePhase::RHOEA};
+      params.set<bool>("implicit") = _implicit_rdg;
 
-    // energy
-    params.set<NonlinearVariableName>("variable") = FlowModelSinglePhase::RHOEA;
-    _sim.addBoundaryCondition(
-        class_name, Component::genName(name(), class_name, FlowModelSinglePhase::RHOEA), params);
+      // mass
+      params.set<NonlinearVariableName>("variable") = FlowModelSinglePhase::RHOA;
+      _sim.addBoundaryCondition(
+          class_name, genName(name(), class_name, FlowModelSinglePhase::RHOA), params);
+
+      // momentum
+      params.set<NonlinearVariableName>("variable") = FlowModelSinglePhase::RHOUA;
+      _sim.addBoundaryCondition(
+          class_name, genName(name(), class_name, FlowModelSinglePhase::RHOUA), params);
+
+      // energy
+      params.set<NonlinearVariableName>("variable") = FlowModelSinglePhase::RHOEA;
+      _sim.addBoundaryCondition(
+          class_name, genName(name(), class_name, FlowModelSinglePhase::RHOEA), params);
+    }
   }
 }
 
