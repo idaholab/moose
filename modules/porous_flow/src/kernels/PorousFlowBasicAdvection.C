4dc8045f89655c44241091cbdc5627d60a40a827
WilkAndy <andrew.wilkins@csiro.au>
2018-03-20 11:10:38 +0100

    Basic advection added to PorousFlow
    PorousFlowDarcyVelocityMaterial computes the Darcy velocity and its derivatives at the quadpoints
    PorousFlowBasicAdvection uses this Darcy velocity to implement an advective flux
    
    Fixes #11053

---
+++ modules/porous_flow/src/kernels/PorousFlowBasicAdvection.C
@@ -0,0 +1,73 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "PorousFlowBasicAdvection.h"
+
+registerMooseObject("PorousFlowApp", PorousFlowBasicAdvection);
+
+template <>
+InputParameters
+validParams<PorousFlowBasicAdvection>()
+{
+  InputParameters params = validParams<Kernel>();
+  params.addRequiredParam<UserObjectName>(
+      "PorousFlowDictator", "The UserObject that holds the list of Porous-Flow variable names.");
+  params.addParam<unsigned int>("phase", 0, "Use the Darcy velocity of this fluid phase");
+  params.addClassDescription(
+      "Advective flux of a Variable using the Darcy velocity of the fluid phase");
+  return params;
+}
+
+PorousFlowBasicAdvection::PorousFlowBasicAdvection(const InputParameters & parameters)
+  : Kernel(parameters),
+    _dictator(getUserObject<PorousFlowDictator>("PorousFlowDictator")),
+    _ph(getParam<unsigned int>("phase")),
+    _darcy_velocity(
+        getMaterialProperty<std::vector<RealVectorValue>>("PorousFlow_darcy_velocity_qp")),
+    _ddarcy_velocity_dvar(getMaterialProperty<std::vector<std::vector<RealVectorValue>>>(
+        "dPorousFlow_darcy_velocity_qp_dvar")),
+    _ddarcy_velocity_dgradvar(
+        getMaterialProperty<std::vector<std::vector<std::vector<RealVectorValue>>>>(
+            "dPorousFlow_darcy_velocity_qp_dgradvar"))
+{
+  if (_ph >= _dictator.numPhases())
+    mooseError("The Dictator proclaims that the number of phases is ",
+               _dictator.numPhases(),
+               " whereas you have provided the phase number as ",
+               _ph);
+}
+
+Real
+PorousFlowBasicAdvection::computeQpResidual()
+{
+  return -_grad_test[_i][_qp] * _darcy_velocity[_qp][_ph] * _u[_qp];
+}
+
+Real
+PorousFlowBasicAdvection::computeQpJacobian()
+{
+  const Real result = -_grad_test[_i][_qp] * _darcy_velocity[_qp][_ph] * _phi[_j][_qp];
+  return result + computeQpOffDiagJacobian(_var.number());
+}
+
+Real
+PorousFlowBasicAdvection::computeQpOffDiagJacobian(unsigned int jvar)
+{
+  if (_dictator.notPorousFlowVariable(jvar))
+    return 0.0;
+
+  const unsigned pvar = _dictator.porousFlowVariableNum(jvar);
+  Real result =
+      -_grad_test[_i][_qp] * _ddarcy_velocity_dvar[_qp][_ph][pvar] * _phi[_j][_qp] * _u[_qp];
+  for (unsigned j = 0; j < LIBMESH_DIM; ++j)
+    result -= _grad_test[_i][_qp] *
+              (_ddarcy_velocity_dgradvar[_qp][_ph][j][pvar] * _grad_phi[_j][_qp](j)) * _u[_qp];
+
+  return result;
+}
