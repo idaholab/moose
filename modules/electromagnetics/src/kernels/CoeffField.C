3bdaabb8c5b7222e41b5472ab570af796283b077
Casey Icenhour <cticenho@ncsu.edu>
2018-03-26 14:20:25 -0600

    Generalize coefficient names in CoeffField kernels.

--- modules/electromagnetics/src/kernels/CoeffField.C
+++ modules/electromagnetics/src/kernels/CoeffField.C
@@ -6,7 +6,7 @@ InputParameters
 validParams<CoeffField>()
 {
   InputParameters params = validParams<Reaction>();
-  params.addParam<Real>("k", 1.0, "Coefficient multiplier (to be squared) for field.");
+  params.addParam<Real>("coeff", 1.0, "Coefficient multiplier for field.");
   params.addParam<FunctionName>("func", 1.0, "Function multiplier for field.");
   return params;
 }
@@ -14,7 +14,7 @@ validParams<CoeffField>()
 CoeffField::CoeffField(const InputParameters & parameters)
   : Reaction(parameters),
 
-    _coefficient(getParam<Real>("k")),
+    _coefficient(getParam<Real>("coeff")),
 
     _func(getFunction("func"))
 
@@ -24,13 +24,11 @@ CoeffField::CoeffField(const InputParameters & parameters)
 Real
 CoeffField::computeQpResidual()
 {
-  return -std::pow(_coefficient, 2) * _func.value(_t, _q_point[_qp]) *
-         Reaction::computeQpResidual();
+  return -_coefficient * _func.value(_t, _q_point[_qp]) * Reaction::computeQpResidual();
 }
 
 Real
 CoeffField::computeQpJacobian()
 {
-  return -std::pow(_coefficient, 2) * _func.value(_t, _q_point[_qp]) *
-         Reaction::computeQpJacobian();
+  return -_coefficient * _func.value(_t, _q_point[_qp]) * Reaction::computeQpJacobian();
 }
