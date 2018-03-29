a7166442021b30119a5e45b97859b9bec81316ee
Benjamin Spencer <benjamin.spencer@inl.gov>
2018-03-29 14:22:13 -0600

    Framework for damage mechanics models ref #11041
    Created DamageBase, a base class for damage mechanics models.  Also created a
    stress calculator (ComputeDamageStress), which uses a model derived from
    DamageBase to compute damaged elastic stress. Also, added a very basic
    damage mechanics model derived from DamageBase (ScalarMaterialDamage), which
    uses another material model to define the evolution of damage.
    
    The longer term goal in this work is to permit the use of damage mechanics
    models together with plasticity and creep models using ComputeMultipleInelasticStress,
    but we are still working out issues with that. The simpler ComputeDamageStress
    model only has material nonlinearity due to damage, and allows us to develop
    and use damage models by themselves.

---
+++ modules/tensor_mechanics/include/materials/DamageBase.h
@@ -0,0 +1,72 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifndef DAMAGEBASE_H
+#define DAMAGEBASE_H
+
+#include "Material.h"
+#include "RankFourTensor.h"
+#include "RankTwoTensor.h"
+
+// Forward declaration
+class DamageBase;
+
+template <>
+InputParameters validParams<DamageBase>();
+
+/**
+ * DamageBase is a base class for damage models, which modify the stress tensor
+ * computed by another model based on a damage mechanics formulation.
+ * These models are designed to be called by another model, so they have
+ * compute=false set.
+ */
+class DamageBase : public Material
+{
+public:
+  DamageBase(const InputParameters & parameters);
+
+  /**
+   * Update the internal variable(s) that evolve the damage
+   */
+  virtual void updateDamage();
+
+  /**
+   * Update the current stress tensor for effects of damage.
+   * @param stress_new Undamaged stress to be modified by the damage model
+   */
+  virtual void updateStressForDamage(RankTwoTensor & stress_new) = 0;
+
+  /**
+   * Update the material constitutive matrix
+   * @param jacobian_mult Material constitutive matrix to be modified for
+   * effects of damage
+   */
+  virtual void updateJacobianMultForDamage(RankFourTensor & jacobian_mult) = 0;
+
+  /**
+   * Perform any necessary rotation of internal variables for finite
+   * strain.
+   * @param rotation_increment The finite-strain rotation increment
+   */
+  virtual void finiteStrainRotation(const RankTwoTensor & rotation_increment);
+
+  /// Sets the value of the member variable _qp for use in inheriting classes
+  void setQp(unsigned int qp);
+
+  ///@{ Retained as empty methods to avoid a warning from Material.C in framework. These methods are unused in all inheriting classes and should not be overwritten.
+  void resetQpProperties() final {}
+  void resetProperties() final {}
+  ///@}
+
+protected:
+  /// Base name optionally used as prefix to material tensor names
+  const std::string _base_name;
+};
+
+#endif // DAMAGEBASE_H
