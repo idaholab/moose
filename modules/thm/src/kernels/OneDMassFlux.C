e7faa1d49907dc218fe7bf97e9caacaecb44eff0
David Andrs <david.andrs@inl.gov>
2018-04-02 15:06:51 -0600

    1D flow in 3D space
    Changing 1D kernels to work in 3D non-displaced space.
    
    Also, changing the way gravity is computed. The projection of the gravity
    vector in the x-direction was precomputed by an aux kernel.  Now, that we
    have the pipe direction computed in a material, there is no need for
    this, we just need to bring in the global gravity vector and dot it with
    the pipe direction.

--- modules/thm/src/kernels/OneDMassFlux.C
+++ modules/thm/src/kernels/OneDMassFlux.C
@@ -13,6 +13,8 @@ validParams<OneDMassFlux>()
   params.addRequiredCoupledVar("arhoA", "alpha*rho*A");
   params.addRequiredCoupledVar("arhouA", "alpha*rho*u*A");
 
+  params.addRequiredParam<MaterialPropertyName>("direction",
+                                                "The direction of the pipe material property");
   params.addRequiredParam<MaterialPropertyName>("alpha", "Volume fraction property");
   params.addRequiredParam<MaterialPropertyName>("rho", "Density property");
   params.addRequiredParam<MaterialPropertyName>("vel", "Velocity property");
@@ -23,6 +25,8 @@ validParams<OneDMassFlux>()
 OneDMassFlux::OneDMassFlux(const InputParameters & parameters)
   : DerivativeMaterialInterfaceRelap<Kernel>(parameters),
     _A(coupledValue("A")),
+
+    _dir(getMaterialProperty<RealVectorValue>("direction")),
 
     _alpha(getMaterialProperty<Real>("alpha")),
     _dalpha_dbeta(isCoupled("beta") ? &getMaterialPropertyDerivativeRelap<Real>("alpha", "beta")
@@ -45,14 +49,14 @@ OneDMassFlux::OneDMassFlux(const InputParameters & parameters)
 Real
 OneDMassFlux::computeQpResidual()
 {
-  return -_alpha[_qp] * _rho[_qp] * _vel[_qp] * _A[_qp] * _grad_test[_i][_qp](0);
+  return -_alpha[_qp] * _rho[_qp] * _vel[_qp] * _dir[_qp] * _A[_qp] * _grad_test[_i][_qp];
 }
 
 Real
 OneDMassFlux::computeQpJacobian()
 {
-  return -_alpha[_qp] * (_drho_darhoA[_qp] * _vel[_qp] + _rho[_qp] * _dvel_darhoA[_qp]) * _A[_qp] *
-         _phi[_j][_qp] * _grad_test[_i][_qp](0);
+  return -_alpha[_qp] * (_drho_darhoA[_qp] * _vel[_qp] + _rho[_qp] * _dvel_darhoA[_qp]) *
+         _dir[_qp] * _A[_qp] * _phi[_j][_qp] * _grad_test[_i][_qp];
 }
 
 Real
@@ -60,10 +64,10 @@ OneDMassFlux::computeQpOffDiagJacobian(unsigned int jvar)
 {
   if (jvar == _beta_var_number)
     return -((*_dalpha_dbeta)[_qp] * _rho[_qp] + _alpha[_qp] * (*_drho_dbeta)[_qp]) * _vel[_qp] *
-           _A[_qp] * _phi[_j][_qp] * _grad_test[_i][_qp](0);
+           _dir[_qp] * _A[_qp] * _phi[_j][_qp] * _grad_test[_i][_qp];
   else if (jvar == _arhouA_var_number)
-    return -_alpha[_qp] * _rho[_qp] * _dvel_darhouA[_qp] * _A[_qp] * _phi[_j][_qp] *
-           _grad_test[_i][_qp](0);
+    return -_alpha[_qp] * _rho[_qp] * _dvel_darhouA[_qp] * _dir[_qp] * _A[_qp] * _phi[_j][_qp] *
+           _grad_test[_i][_qp];
   else
     return 0.;
 }
