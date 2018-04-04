4a27be3b6d4a8bd348db301269398006c3572edd
David Andrs <david.andrs@inl.gov>
2018-04-04 11:32:38 -0600

    Updating heat conduction model
    Moving from displaced space to non-displaced one.

---
+++ modules/thm/src/kernels/HeatConductionRZ.C
@@ -0,0 +1,31 @@
+#include "HeatConductionRZ.h"
+
+registerMooseObject("RELAP7App", HeatConductionRZ);
+
+template <>
+InputParameters
+validParams<HeatConductionRZ>()
+{
+  InputParameters params = validParams<HeatConductionKernel>();
+  params += validParams<RZSymmetry>();
+  return params;
+}
+
+HeatConductionRZ::HeatConductionRZ(const InputParameters & parameters)
+  : HeatConductionKernel(parameters), RZSymmetry(parameters)
+{
+}
+
+Real
+HeatConductionRZ::computeQpResidual()
+{
+  Real r = computeRadius(_q_point[_qp]);
+  return 2. * libMesh::pi * r * HeatConductionKernel::computeQpResidual();
+}
+
+Real
+HeatConductionRZ::computeQpJacobian()
+{
+  Real r = computeRadius(_q_point[_qp]);
+  return 2. * libMesh::pi * r * HeatConductionKernel::computeQpJacobian();
+}
---
+++ modules/thm/src/kernels/HeatConductionTimeDerivativeRZ.C
@@ -0,0 +1,31 @@
+#include "HeatConductionTimeDerivativeRZ.h"
+
+registerMooseObject("RELAP7App", HeatConductionTimeDerivativeRZ);
+
+template <>
+InputParameters
+validParams<HeatConductionTimeDerivativeRZ>()
+{
+  InputParameters params = validParams<HeatConductionTimeDerivative>();
+  params += validParams<RZSymmetry>();
+  return params;
+}
+
+HeatConductionTimeDerivativeRZ::HeatConductionTimeDerivativeRZ(const InputParameters & parameters)
+  : HeatConductionTimeDerivative(parameters), RZSymmetry(parameters)
+{
+}
+
+Real
+HeatConductionTimeDerivativeRZ::computeQpResidual()
+{
+  Real r = computeRadius(_q_point[_qp]);
+  return 2. * libMesh::pi * r * HeatConductionTimeDerivative::computeQpResidual();
+}
+
+Real
+HeatConductionTimeDerivativeRZ::computeQpJacobian()
+{
+  Real r = computeRadius(_q_point[_qp]);
+  return 2. * libMesh::pi * r * HeatConductionTimeDerivative::computeQpJacobian();
+}
