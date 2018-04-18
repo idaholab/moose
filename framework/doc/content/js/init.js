81a58bb2dd3588b7fca09ea7248b11658bd5100d
Daniel Schwen <daniel.schwen@inl.gov>
2018-04-18 13:22:17 -0600

    Tweak search, reduce index size (#11265)

--- framework/doc/content/js/init.js
+++ framework/doc/content/js/init.js
@@ -30,8 +30,8 @@
 
 var options = {
   shouldSort: true,
-  threshold: 0.5,
-  minMatchCharLength: 2,
+  threshold: 0.3,
+  minMatchCharLength: 3,
   keys: [
     {
       name: "name",
@@ -51,7 +51,7 @@ function mooseSearch() {
     var fuse = new Fuse(index_data, options);
     var results = fuse.search(box.value);
 
-    console.log(results.length);
+    console.log(results);
     if (results.length > 0)
     {
         element.innerHTML = '';
@@ -59,15 +59,24 @@ function mooseSearch() {
         {
             var div = document.createElement("div");
             div.className = 'moose-search-result';
-            element.appendChild(div);
 
             var title = document.createElement("div");
             title.className = 'moose-search-result-title';
             var a = document.createElement("a");
             a.innerHTML = item.name;
             a.setAttribute("href", item.location);
+
+            if (item.name != item.text) {
+              section = document.createElement("span");
+              section.innerHTML = ' &mdash; ' + item.text;
+              a.appendChild(section);
+            } else {
+              a.setAttribute("style", "font-weight: bold");
+            }
+
             title.appendChild(a);
             div.appendChild(title);
+            element.appendChild(div);
         }
     }
     else
