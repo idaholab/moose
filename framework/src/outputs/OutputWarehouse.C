a4ddd6bd3732a8e63370f0f8fccbea9907dd8f29
Andrew E Slaughter <andrew.slaughter@inl.gov>
2018-04-26 16:25:27 -0600

    If no Console objects exist, don't do any screen output
    (closes #5178)

--- framework/src/outputs/OutputWarehouse.C
+++ framework/src/outputs/OutputWarehouse.C
@@ -182,9 +182,7 @@ OutputWarehouse::mooseConsole()
     // Reset
     _console_buffer.clear();
     _console_buffer.str("");
-  }
-  else
-  {
+
     if (!_buffer_action_console_outputs)
     {
       // this will cause messages to console before its construction immediately flushed and
