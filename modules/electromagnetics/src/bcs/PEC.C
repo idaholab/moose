27ebd936d10391987458cfdf3693873dcf1d2d18
Casey Icenhour <cticenho@ncsu.edu>
2018-05-09 16:41:57 -0600

    PEC testing part 2
    Almost reproducing MMS, but not quite. Hard to strongly enforce on
    certain special boundaries. Placing this on the back burner for now,
    getting better results from scalar potential solves.

--- modules/electromagnetics/src/bcs/PEC.C
+++ modules/electromagnetics/src/bcs/PEC.C
@@ -6,7 +6,7 @@ template <>
 InputParameters
 validParams<PEC>()
 {
-  InputParameters params = validParams<NodalNormalBC>();
+  InputParameters params = validParams<IntegratedBC>();
   params.addClassDescription("NodalNormals system description of the Perfect Electrical Conductor "
                              "(PEC) Boundary Condition, where ${\\hat{n} \\times \\vec{E} = 0}.");
   MooseEnum component("x=0 y=1 z=2");
@@ -15,16 +15,18 @@ validParams<PEC>()
   params.addCoupledVar("coupled_0", 0.0, "Coupled field variable, 0 component.");
   params.addCoupledVar("coupled_1", 0.0, "Coupled field variable, 1 component.");
   params.addCoupledVar("coupled_2", 0.0, "Coupled field variable, 2 component.");
+  params.addRequiredParam<Real>("penalty", "The penalty coefficient.");
   return params;
 }
 
 PEC::PEC(const InputParameters & parameters)
-  : NodalNormalBC(parameters),
+  : IntegratedBC(parameters),
 
     _coupled_val_0(coupledValue("coupled_0")),
     _coupled_val_1(coupledValue("coupled_1")),
     _coupled_val_2(coupledValue("coupled_2")),
-    _component(getParam<MooseEnum>("component"))
+    _component(getParam<MooseEnum>("component")),
+    _penalty(getParam<Real>("penalty"))
 {
 }
 
@@ -32,13 +34,15 @@ Real
 PEC::computeQpResidual()
 {
   RealVectorValue field(_coupled_val_0[_qp], _coupled_val_1[_qp], _coupled_val_2[_qp]);
+  RealVectorValue test(_test[_i][_qp], _test[_i][_qp], _test[_i][_qp]);
 
-  RealVectorValue NcF = _normal.cross(field);
+  RealVectorValue NcF = _normals[_qp].cross(field);
+  RealVectorValue NcT = _normals[_qp].cross(test);
 
   int comp = _component - 1;
   if (_component == 0)
     comp = 2;
 
   // return single component of calculated cross product to be used in the residual calculation.
-  return NcF(comp);
+  return -test * NcF;
 }
