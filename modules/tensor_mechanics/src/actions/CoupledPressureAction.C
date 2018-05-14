e2dbc193e02b72e6357fdc0d07c0a7fa60595e37
David Andrs <david.andrs@inl.gov>
2018-05-14 09:54:51 -0600

    Adding Pressure BC for multi-app runs
    In coupled runs, the external app computes pressure which is then
    transferred into an auxiliary variable. In order to use this externally
    computed variable this BC use the variable to impose pressure BC.
    
    Closes #11558

---
+++ modules/tensor_mechanics/src/actions/CoupledPressureAction.C
@@ -0,0 +1,98 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "CoupledPressureAction.h"
+#include "Factory.h"
+#include "FEProblem.h"
+#include "Conversion.h"
+
+registerMooseAction("TensorMechanicsApp", CoupledPressureAction, "add_bc");
+
+template <>
+InputParameters
+validParams<CoupledPressureAction>()
+{
+  InputParameters params = validParams<Action>();
+  params.addClassDescription("Set up Coupled Pressure boundary conditions");
+
+  params.addRequiredParam<std::vector<BoundaryName>>(
+      "boundary", "The list of boundary IDs from the mesh where the pressure will be applied");
+
+  params.addParam<NonlinearVariableName>("disp_x", "The x displacement");
+  params.addParam<NonlinearVariableName>("disp_y", "The y displacement");
+  params.addParam<NonlinearVariableName>("disp_z", "The z displacement");
+
+  params.addParam<std::vector<NonlinearVariableName>>(
+      "displacements",
+      "The displacements appropriate for the simulation geometry and coordinate system");
+
+  params.addParam<std::vector<AuxVariableName>>("save_in_disp_x",
+                                                "The save_in variables for x displacement");
+  params.addParam<std::vector<AuxVariableName>>("save_in_disp_y",
+                                                "The save_in variables for y displacement");
+  params.addParam<std::vector<AuxVariableName>>("save_in_disp_z",
+                                                "The save_in variables for z displacement");
+
+  params.addParam<VariableName>("pressure", "The variable that contains the pressure");
+  return params;
+}
+
+CoupledPressureAction::CoupledPressureAction(const InputParameters & params) : Action(params)
+{
+  _save_in_vars.push_back(getParam<std::vector<AuxVariableName>>("save_in_disp_x"));
+  _save_in_vars.push_back(getParam<std::vector<AuxVariableName>>("save_in_disp_y"));
+  _save_in_vars.push_back(getParam<std::vector<AuxVariableName>>("save_in_disp_z"));
+
+  _has_save_in_vars.push_back(params.isParamValid("save_in_disp_x"));
+  _has_save_in_vars.push_back(params.isParamValid("save_in_disp_y"));
+  _has_save_in_vars.push_back(params.isParamValid("save_in_disp_z"));
+}
+
+void
+CoupledPressureAction::act()
+{
+  const std::string kernel_name = "CoupledPressureBC";
+
+  std::vector<NonlinearVariableName> displacements;
+  if (isParamValid("displacements"))
+    displacements = getParam<std::vector<NonlinearVariableName>>("displacements");
+  else
+  {
+    // Legacy parameter scheme for displacements
+    if (!isParamValid("disp_x"))
+      mooseError("Specify displacement variables using the `displacements` parameter.");
+    displacements.push_back(getParam<NonlinearVariableName>("disp_x"));
+
+    if (isParamValid("disp_y"))
+    {
+      displacements.push_back(getParam<NonlinearVariableName>("disp_y"));
+      if (isParamValid("disp_z"))
+        displacements.push_back(getParam<NonlinearVariableName>("disp_z"));
+    }
+  }
+
+  // Create pressure BCs
+  for (unsigned int i = 0; i < displacements.size(); ++i)
+  {
+    // Create unique kernel name for each of the components
+    std::string unique_kernel_name = kernel_name + "_" + _name + "_" + Moose::stringify(i);
+
+    InputParameters params = _factory.getValidParams(kernel_name);
+    params.applySpecificParameters(parameters(), {"boundary"});
+    params.set<std::vector<VariableName>>("pressure") = {getParam<VariableName>("pressure")};
+    params.set<bool>("use_displaced_mesh") = true;
+    params.set<unsigned int>("component") = i;
+    params.set<NonlinearVariableName>("variable") = displacements[i];
+
+    if (_has_save_in_vars[i])
+      params.set<std::vector<AuxVariableName>>("save_in") = _save_in_vars[i];
+
+    _problem->addBoundaryCondition(kernel_name, unique_kernel_name, params);
+  }
+}
