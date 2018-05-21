7b126299b9635a743b4171b605c23ab1b29b2159
Derek Gaston <derek.gaston@inl.gov>
2018-05-20 22:40:03 -0600

    cleanup test refs #11551

--- unit/src/PerfGraphTest.C
+++ unit/src/PerfGraphTest.C
@@ -16,8 +16,6 @@ TEST(PerfGraphTest, test)
 {
   PerfGraph graph;
 
-  //  graph.setActive(false);
-
   auto a_id = graph.registerSection("a");
   auto b_id = graph.registerSection("b");
   auto c_id = graph.registerSection("c");
@@ -26,46 +24,31 @@ TEST(PerfGraphTest, test)
   unsigned int b = 0;
   unsigned int c = 0;
 
-  std::vector<double> a_vec(1000000, 2);
-  std::vector<double> b_vec(1000000, 3);
-  std::vector<double> c_vec(1000000, 2);
+  std::vector<double> a_vec(100000, 2);
+  std::vector<double> b_vec(100000, 3);
+  std::vector<double> c_vec(100000, 2);
 
-  auto start = std::chrono::steady_clock::now();
-
-  for (unsigned int i = 0; i < 1000; i++)
   {
-    {
-      PerfGuard guard(graph, a_id);
-      for (auto & aval : a_vec)
-        aval += a++;
-
-      {
-        PerfGuard guard(graph, c_id);
-        for (auto & cval : c_vec)
-          cval += c++;
-      }
-    }
+    PerfGuard guard(graph, a_id);
+    for (auto & aval : a_vec)
+      aval += a++;
 
     {
-      PerfGuard guard(graph, b_id);
-      for (auto & bval : b_vec)
-        bval += b++;
-
-      {
-        PerfGuard guard(graph, c_id);
-        for (auto & cval : c_vec)
-          cval += c++;
-      }
+      PerfGuard guard(graph, c_id);
+      for (auto & cval : c_vec)
+        cval += c++;
     }
   }
 
-  auto end = std::chrono::steady_clock::now();
+  {
+    PerfGuard guard(graph, b_id);
+    for (auto & bval : b_vec)
+      bval += b++;
 
-  auto duration = std::chrono::duration<double, std::milli>(end - start);
-
-  std::cout << "Time: " << duration.count() << std::endl;
-
-  std::cout << a << b << c << std::endl;
-
-  graph.print();
+    {
+      PerfGuard guard(graph, c_id);
+      for (auto & cval : c_vec)
+        cval += c++;
+    }
+  }
 }
