9e3c9e1e055c7dcacc53c182a681574598fd253e
Joshua Hansel <joshua.hansel@inl.gov>
2018-06-13 16:08:50 -0600

    Added gravity data to Component
    Refs idaholab/relap-7#555

--- modules/thm/include/kernels/OneDEnergyGravity.h
+++ modules/thm/include/kernels/OneDEnergyGravity.h
@@ -40,7 +40,7 @@ protected:
   /// The direction of the pipe
   const MaterialProperty<RealVectorValue> & _dir;
   /// The global gravity vector
-  RealVectorValue _gravity_vector;
+  const RealVectorValue & _gravity_vector;
 
   const unsigned int _beta_var_number;
   const unsigned int _arhoA_var_number;
