idaholab/moose

commit 5b39a40f2b07d0e9e72afd828c7ffce161f190ce

add ifdefs around objects we don't want sometimes
and update special case registrations

framework/src/outputs/VTKOutput.C
@@ -7,10 +7,14 @@
 //* Licensed under LGPL 2.1, please see LICENSE for details
 //* https://www.gnu.org/licenses/lgpl-2.1.html
 
+#ifdef LIBMESH_HAVE_VTK
+
 #include "VTKOutput.h"
 
 #include "libmesh/vtk_io.h"
 #include "libmesh/equation_systems.h"
+
+registerMooseObjectAliased("MooseApp", VTKOutput, "VTK");
 
 template <>
 InputParameters
@@ -36,7 +40,6 @@ VTKOutput::VTKOutput(const InputParameters & parameters)
 void
 VTKOutput::output(const ExecFlagType & /*type*/)
 {
-#ifdef LIBMESH_HAVE_VTK
 
   /// Create VTKIO object
   VTKIO vtk(_es_ptr->get_mesh());
@@ -47,10 +50,6 @@ VTKOutput::output(const ExecFlagType & /*type*/)
   // Write the data
   vtk.write_equation_systems(filename(), *_es_ptr);
   _file_num++;
-
-#else
-  mooseError("libMesh not configured with VTK");
-#endif
 }
 
 std::string
@@ -68,3 +67,5 @@ VTKOutput::filename()
   // Return the filename
   return output.str();
 }
+
+#endif

framework/include/userobject/Terminator.h
@@ -10,6 +10,10 @@
 #ifndef TERMINATOR_H
 #define TERMINATOR_H
 
+#include "libmesh/libmesh_config.h"
+
+#ifdef LIBMESH_HAVE_FPARSER
+
 #include "GeneralUserObject.h"
 #include "libmesh/fparser.hh"
 
@@ -67,5 +71,7 @@ protected:
   /// Fparser parameter buffer
   Real * _params;
 };
+
+#endif // LIBMESH_HAVE_FPARSER
 
 #endif // TERMINATOR_H

framework/src/base/Moose.C
@@ -65,7 +65,6 @@
 #include "Diffusion.h"
 #include "AnisotropicDiffusion.h"
 #include "CoupledForce.h"
-#include "BodyForce.h"
 #include "Reaction.h"
 #include "MassEigenKernel.h"
 #include "NullKernel.h"
@@ -155,9 +154,6 @@
 #include "Axisymmetric2D3DSolutionFunction.h"
 #include "ConstantFunction.h"
 #include "CompositeFunction.h"
-#include "MooseParsedFunction.h"
-#include "MooseParsedVectorFunction.h"
-#include "MooseParsedGradFunction.h"
 #include "PiecewiseConstant.h"
 #include "PiecewiseLinear.h"
 #include "SolutionFunction.h"
@@ -283,13 +279,7 @@
 #endif
 
 // preconditioners
-#include "PhysicsBasedPreconditioner.h"
-#include "FiniteDifferencePreconditioner.h"
-#include "SingleMatrixPreconditioner.h"
-
-#include "FieldSplitPreconditioner.h"
 #include "Split.h"
-#include "AddFieldSplitAction.h"
 
 // dampers
 #include "ConstantDamper.h"
@@ -450,6 +440,7 @@
 #include "AddRelationshipManager.h"
 #include "MeshOnlyAction.h"
 #include "SplitMeshAction.h"
+#include "AddFieldSplitAction.h"
 
 // Outputs
 #ifdef LIBMESH_HAVE_EXODUS_API
@@ -458,17 +449,13 @@
 #include "Nemesis.h"
 #include "Console.h"
 #include "CSV.h"
-#include "VTKOutput.h"
 #include "Checkpoint.h"
-#include "XDA.h"
-#include "GMVOutput.h"
 #include "Tecplot.h"
 #include "Gnuplot.h"
 #include "SolutionHistory.h"
 #include "MaterialPropertyDebugOutput.h"
 #include "VariableResidualNormsDebugOutput.h"
 #include "TopResidualDebugOutput.h"
-#include "DOFMapOutput.h"
 #include "ControlOutput.h"
 
 // Controls
@@ -566,7 +553,6 @@ registerObjects(Factory & factory, const std::set<std::string> & obj_labels)
   registerKernel(Diffusion);
   registerKernel(AnisotropicDiffusion);
   registerKernel(CoupledForce);
-  registerRenamedObject("UserForcingFunction", BodyForce, "04/01/2018 00:00");
   registerKernel(Reaction);
   registerKernel(MassEigenKernel);
   registerKernel(NullKernel);
@@ -653,9 +639,6 @@ registerObjects(Factory & factory, const std::set<std::string> & obj_labels)
   registerFunction(Axisymmetric2D3DSolutionFunction);
   registerFunction(ConstantFunction);
   registerFunction(CompositeFunction);
-  registerNamedFunction(MooseParsedFunction, "ParsedFunction");
-  registerNamedFunction(MooseParsedGradFunction, "ParsedGradFunction");
-  registerNamedFunction(MooseParsedVectorFunction, "ParsedVectorFunction");
   registerFunction(PiecewiseConstant);
   registerFunction(PiecewiseLinear);
   registerFunction(SolutionFunction);
@@ -780,13 +763,6 @@ registerObjects(Factory & factory, const std::set<std::string> & obj_labels)
   registerUserObject(Terminator);
 #endif
 
-  // preconditioners
-  registerNamedPreconditioner(PhysicsBasedPreconditioner, "PBP");
-  registerNamedPreconditioner(FiniteDifferencePreconditioner, "FDP");
-  registerNamedPreconditioner(SingleMatrixPreconditioner, "SMP");
-#if defined(LIBMESH_HAVE_PETSC) && !PETSC_VERSION_LESS_THAN(3, 3, 0)
-  registerNamedPreconditioner(FieldSplitPreconditioner, "FSP");
-#endif
   // dampers
   registerDamper(ConstantDamper);
   registerDamper(MaxIncrement);
@@ -895,20 +871,13 @@ registerObjects(Factory & factory, const std::set<std::string> & obj_labels)
 #endif
   registerOutput(Console);
   registerOutput(CSV);
-#ifdef LIBMESH_HAVE_VTK
-  registerNamedOutput(VTKOutput, "VTK");
-#endif
   registerOutput(Checkpoint);
-  registerNamedOutput(XDA, "XDR");
-  registerOutput(XDA);
-  registerNamedOutput(GMVOutput, "GMV");
   registerOutput(Tecplot);
   registerOutput(Gnuplot);
   registerOutput(SolutionHistory);
   registerOutput(MaterialPropertyDebugOutput);
   registerOutput(VariableResidualNormsDebugOutput);
   registerOutput(TopResidualDebugOutput);
-  registerNamedOutput(DOFMapOutput, "DOFMap");
   registerOutput(ControlOutput);
 
   // Controls

framework/src/functions/MooseParsedFunction.C
@@ -14,6 +14,8 @@
 #include "MooseParsedFunction.h"
 #include "MooseParsedFunctionWrapper.h"
 
+registerMooseObjectAliased("MooseApp", MooseParsedFunction, "ParsedFunction");
+
 template <>
 InputParameters
 validParams<MooseParsedFunction>()

framework/src/functions/MooseParsedGradFunction.C
@@ -10,6 +10,8 @@
 #include "MooseParsedGradFunction.h"
 #include "MooseParsedFunctionWrapper.h"
 
+registerMooseObjectAliased("MooseApp", MooseParsedGradFunction, "ParsedGradFunction");
+
 template <>
 InputParameters
 validParams<MooseParsedGradFunction>()

framework/src/functions/MooseParsedVectorFunction.C
@@ -10,6 +10,8 @@
 #include "MooseParsedVectorFunction.h"
 #include "MooseParsedFunctionWrapper.h"
 
+registerMooseObjectAliased("MooseApp", MooseParsedVectorFunction, "ParsedVectorFunction");
+
 template <>
 InputParameters
 validParams<MooseParsedVectorFunction>()

framework/src/kernels/BodyForce.C
@@ -12,6 +12,8 @@
 // MOOSE
 #include "Function.h"
 
+registerMooseObject("MooseApp", BodyForce);
+
 template <>
 InputParameters
 validParams<BodyForce>()

framework/src/kernels/UserForcingFunction.C
@@ -10,6 +10,8 @@
 #include "UserForcingFunction.h"
 #include "Function.h"
 
+registerMooseObjectRenamed("MooseApp", UserForcingFunction, "04/01/2018 00:00", BodyForce);
+
 template <>
 InputParameters
 validParams<UserForcingFunction>()

commit 02d8c8e9e3f860edc812d56dda60a49f3fb0a4f0

Fix incorrect use of LIBMESH_HAVE_VTK ifdef.
This was broken in 5b39a40 but we never noticed it because Unity
builds covered up the issue. You can't use the LIBMESH_HAVE_VTK
preprocessor macro unless you have included the header that
(potentially) defines it first!

framework/src/outputs/VTKOutput.C
@@ -7,14 +7,14 @@
 //* Licensed under LGPL 2.1, please see LICENSE for details
 //* https://www.gnu.org/licenses/lgpl-2.1.html
 
-#ifdef LIBMESH_HAVE_VTK
-
 #include "VTKOutput.h"
 
 #include "libmesh/vtk_io.h"
 #include "libmesh/equation_systems.h"
 
+#ifdef LIBMESH_HAVE_VTK
 registerMooseObjectAliased("MooseApp", VTKOutput, "VTK");
+#endif
 
 template <>
 InputParameters
@@ -40,7 +40,7 @@ VTKOutput::VTKOutput(const InputParameters & parameters)
 void
 VTKOutput::output(const ExecFlagType & /*type*/)
 {
-
+#ifdef LIBMESH_HAVE_VTK
   /// Create VTKIO object
   VTKIO vtk(_es_ptr->get_mesh());
 
@@ -50,6 +50,9 @@ VTKOutput::output(const ExecFlagType & /*type*/)
   // Write the data
   vtk.write_equation_systems(filename(), *_es_ptr);
   _file_num++;
+#else
+  mooseError("libMesh not configured with VTK");
+#endif
 }
 
 std::string
@@ -67,5 +70,3 @@ VTKOutput::filename()
   // Return the filename
   return output.str();
 }
-
-#endif