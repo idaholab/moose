d2d8d9aed809d3c67085df11c79d1f395265d62d
Joshua Hansel <joshua.hansel@inl.gov>
2018-07-11 08:31:33 -0600

    Renamed RhoEFromPressureDensityVelocityIC to RhoEFromPressureTemperatureVelocityIC. Refs idaholab/relap-7#10.

---
+++ modules/thm/src/ics/RhoEFromPressureTemperatureVelocityIC.C
@@ -0,0 +1,34 @@
+#include "RhoEFromPressureTemperatureVelocityIC.h"
+#include "SinglePhaseFluidProperties.h"
+
+registerMooseObject("RELAP7App", RhoEFromPressureTemperatureVelocityIC);
+
+template <>
+InputParameters
+validParams<RhoEFromPressureTemperatureVelocityIC>()
+{
+  InputParameters params = validParams<InitialCondition>();
+  params.addRequiredParam<UserObjectName>("fp", "The name of fluid properties object to use.");
+  params.addRequiredCoupledVar("p", "The pressure");
+  params.addRequiredCoupledVar("T", "The temperature");
+  params.addRequiredCoupledVar("vel", "The velocity");
+  return params;
+}
+
+RhoEFromPressureTemperatureVelocityIC::RhoEFromPressureTemperatureVelocityIC(
+    const InputParameters & parameters)
+  : InitialCondition(parameters),
+    _fp(getUserObject<SinglePhaseFluidProperties>("fp")),
+    _p(coupledValue("p")),
+    _T(coupledValue("T")),
+    _vel(coupledValue("vel"))
+{
+}
+
+Real
+RhoEFromPressureTemperatureVelocityIC::value(const Point & /*p*/)
+{
+  const Real rho = _fp.rho_from_p_T(_p[_qp], _T[_qp]);
+  const Real e = _fp.e_from_p_rho(_p[_qp], rho);
+  return rho * (e + 0.5 * _vel[_qp] * _vel[_qp]);
+}
