8efc3bc2a4e512d251d5cb1f4ecb57c29d5444c5
Alex Lindsay <alexlindsay239@gmail.com>
2018-07-16 14:30:11 -0600

    Put 1s on the diagonal for LMs that don't project onto the master face

--- framework/src/systems/NonlinearSystemBase.C
+++ framework/src/systems/NonlinearSystemBase.C
@@ -1829,6 +1829,20 @@ NonlinearSystemBase::constraintJacobians(bool displaced)
               }
             }
           }
+          else
+          {
+            _fe_problem.reinitNodeFace(&slave_node, slave_boundary, 0);
+            _fe_problem.prepareAssembly(0);
+            for (const auto & nfc : constraints)
+            {
+              std::vector<dof_id_type> slave_dofs(1, nfc->variable().nodalDofIndex());
+              nfc->_Kee.resize(1, 1);
+              nfc->_Kee(0, 0) = 1.;
+              // Cache the jacobian block for the slave side
+              _fe_problem.assembly(0).cacheJacobianBlock(
+                  nfc->_Kee, slave_dofs, slave_dofs, nfc->variable().scalingFactor());
+            }
+          }
         }
       }
     }
