1d85cd2174546835dd193000b552db7d11d08bb8
John Peterson <jwpeterson@gmail.com>
2018-07-24 14:23:27 -0600

    Avoid deprecation error when calling prepare_for_use().
    This is a bit awkward because we added a second "skip_find_neighbors"
    flag at some point, but you are not supposed to use a non-default value
    for the skip_renumber_nodes_and_elements parameter to
    prepare_for_use() any more, the correct thing to do is set the
    allow_renumbering flag to false.

--- framework/src/mesh/DistributedGeneratedMesh.C
+++ framework/src/mesh/DistributedGeneratedMesh.C
@@ -1303,7 +1303,14 @@ build_cube(UnstructuredMesh & mesh,
   if (verbose)
     Moose::out << "Getting ready to prepare for use" << std::endl;
 
-  mesh.prepare_for_use(true, true); // No need to renumber or find neighbors - done did it.
+  // No need to renumber or find neighbors - done did it.
+  // Avoid deprecation message/error by _also_ setting
+  // allow_renumbering(false). This is a bit silly, but we want to
+  // catch cases where people are purely using the old "skip"
+  // interface and not the new flag setting one.
+  mesh.allow_renumbering(false);
+  mesh.prepare_for_use(/*skip_renumber (ignored!) = */ false,
+                       /*skip_find_neighbors = */ true);
 
   if (verbose)
     for (auto & elem_ptr : mesh.element_ptr_range())
