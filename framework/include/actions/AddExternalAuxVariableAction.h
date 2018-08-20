2adbcd9d95457835e0d872bbf21078079c1a422e
Cody Permann <codypermann@gmail.com>
2018-08-20 15:00:11 -0600

    Adding Action to add ExternalVariables
    refs #12024

---
+++ framework/include/actions/AddExternalAuxVariableAction.h
@@ -0,0 +1,36 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifndef ADDEXTERNALAUXVARIABLEACTION_H
+#define ADDEXTERNALAUXVARIABLEACTION_H
+
+// MOOSE includes
+#include "Action.h"
+
+// Forward declarations
+class AddExternalAuxVariableAction;
+
+template <>
+InputParameters validParams<AddExternalAuxVariableAction>();
+
+/**
+ * Action for creating Auxiliary variables
+ */
+class AddExternalAuxVariableAction : public Action
+{
+public:
+  /**
+   * Class constructor
+   */
+  AddExternalAuxVariableAction(InputParameters params);
+
+  virtual void act() override;
+};
+
+#endif // ADDEXTERNALAUXVARIABLEACTION_H
---
+++ framework/src/actions/AddExternalAuxVariableAction.C
@@ -0,0 +1,29 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "AddExternalAuxVariableAction.h"
+
+registerMooseAction("MooseApp", AddExternalAuxVariableAction, "add_external_aux_variables");
+
+template <>
+InputParameters
+validParams<AddExternalAuxVariableAction>()
+{
+  return validParams<Action>();
+}
+
+AddExternalAuxVariableAction::AddExternalAuxVariableAction(InputParameters params) : Action(params)
+{
+}
+
+void
+AddExternalAuxVariableAction::act()
+{
+  //  _problem->addExternalVariables();
+}
