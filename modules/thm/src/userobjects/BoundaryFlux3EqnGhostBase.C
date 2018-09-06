a6bcb29051e5a693dcdf41a463060c204dd49506
Joshua Hansel <joshua.hansel@inl.gov>
2018-09-06 09:50:38 -0600

    Renamed Euler1DVarAreaBoundaryFluxGhost to BoundaryFlux3EqnGhostBase
    Refs idaholab/relap-7#19688

---
+++ modules/thm/src/userobjects/BoundaryFlux3EqnGhostBase.C
@@ -0,0 +1,50 @@
+#include "BoundaryFlux3EqnGhostBase.h"
+
+template <>
+InputParameters
+validParams<BoundaryFlux3EqnGhostBase>()
+{
+  InputParameters params = validParams<BoundaryFluxBase>();
+
+  params.addClassDescription("Computes boundary fluxes for the 1-D, variable-area Euler equations "
+                             "using an RDG flux user object and a ghost cell solution");
+
+  params.addRequiredParam<UserObjectName>("rdg_flux", "Name of RDG flux user object");
+
+  return params;
+}
+
+BoundaryFlux3EqnGhostBase::BoundaryFlux3EqnGhostBase(const InputParameters & parameters)
+  : BoundaryFluxBase(parameters), _rdg_flux(getUserObject<RDGFluxBase>("rdg_flux"))
+{
+}
+
+void
+BoundaryFlux3EqnGhostBase::calcFlux(unsigned int iside,
+                                    dof_id_type ielem,
+                                    const std::vector<Real> & U1,
+                                    const RealVectorValue & normal,
+                                    std::vector<Real> & flux) const
+{
+  const std::vector<Real> U2 = getGhostCellSolution(U1);
+
+  flux = _rdg_flux.getFlux(iside, ielem, U1, U2, normal, _tid);
+}
+
+void
+BoundaryFlux3EqnGhostBase::calcJacobian(unsigned int iside,
+                                        dof_id_type ielem,
+                                        const std::vector<Real> & U1,
+                                        const RealVectorValue & normal,
+                                        DenseMatrix<Real> & J) const
+{
+  const std::vector<Real> U2 = getGhostCellSolution(U1);
+
+  const auto & pdF_pdU1 = _rdg_flux.getJacobian(true, iside, ielem, U1, U2, normal, _tid);
+  const auto & dF_dU2 = _rdg_flux.getJacobian(false, iside, ielem, U1, U2, normal, _tid);
+
+  // compute dF/dU1 = pdF/pdU1 + dF/dU2 * dU2/dU1
+  J = getGhostCellSolutionJacobian(U1);
+  J.left_multiply(dF_dU2);
+  J += pdF_pdU1;
+}
