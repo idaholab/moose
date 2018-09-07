69b6d8bcf6fdcbeddd4e349b20e40ecf219dc095
Robert Carlsen <rwcarlsen@gmail.com>
2018-09-07 14:25:22 -0600

    make script run in cwd instead of moose repo

--- scripts/format_inputs.sh
+++ scripts/format_inputs.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 MOOSE_DIR=$(realpath "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"/..)
 (cd $MOOSE_DIR/framework/contrib/hit && make &>/dev/null)
-find ${MOOSE_DIR} -name '*.i' | xargs ${MOOSE_DIR}/framework/contrib/hit/hit format -i $@ $files
-
+find . -name '*.i' | xargs ${MOOSE_DIR}/framework/contrib/hit/hit format -i $@ $files
 
