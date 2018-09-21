f8be8f462474acd04a6bc82cdf100ebf11726587
Daniel Schwen <daniel.schwen@inl.gov>
2018-09-21 11:17:01 -0600

    Better bisection search (#12168)

--- modules/phase_field/src/postprocessors/DiscreteNucleationTimeStep.C
+++ modules/phase_field/src/postprocessors/DiscreteNucleationTimeStep.C
@@ -39,9 +39,9 @@ DiscreteNucleationTimeStep::DiscreteNucleationTimeStep(const InputParameters & p
     _inserter(getUserObject<DiscreteNucleationInserter>("inserter")),
     _dt_nucleation(getParam<Real>("dt_max"))
 {
-
   //
-  // we do a bisection search because math is hard
+  // We do a bisection search because math is hard
+  // (i.e. probability function is not analytically invertible)
   //
 
   // this is the target value
@@ -49,26 +49,44 @@ DiscreteNucleationTimeStep::DiscreteNucleationTimeStep(const InputParameters & p
 
   // initial guess
   _max_lambda = 0.1;
-  Real step = _max_lambda;
-  Real multiplier = 1.0;
-  for (unsigned int i = 0; i < 60; ++i)
+  Real upper_bound = _max_lambda;
+  Real lower_bound = 0.0;
+
+  // At this point we do not know a proper upper bound for a search interval
+  // so we grow our initial guess until we find it (we only allow for a finite
+  // number of iterations)
+  for (unsigned int i = 0; i < 100; ++i)
   {
+    const Real p_upper = 1.0 - (1.0 + upper_bound) * std::exp(-upper_bound);
+
+    // we have found a lambda value that results in a p > p2n, this is the upper end of the interval
+    if (p_upper > p2n)
+      break;
+
+    // upper_bound is actually below our target lambda, set it as the new lower
+    // bound and double the upper_bound value
+    lower_bound = upper_bound;
+    upper_bound *= 2.0;
+  }
+
+  // now that we have an upper and a lower interval bounds we can do a proper bisection
+  for (unsigned int i = 0; i < 100; ++i)
+  {
+    // pick the middle of the current interval
+    _max_lambda = (upper_bound - lower_bound) / 2.0 + lower_bound;
+
+    // calculate new probability for 2 or more nuclei to appear
     const Real p = 1.0 - (1.0 + _max_lambda) * std::exp(-_max_lambda);
+
+    // quit if we zeroed in on the target
     if (MooseUtils::absoluteFuzzyEqual(p, p2n))
       break;
+
+    // otherwise adjust interval bounds
     else if (p > p2n)
-    {
-      // the interval has no upper end, so we can only start bisecting when we have
-      // exceeded the target value at least once
-      multiplier = 0.5;
-      step *= multiplier;
-      _max_lambda -= step;
-    }
+      upper_bound = _max_lambda;
     else
-    {
-      step *= multiplier;
-      _max_lambda += step;
-    }
+      lower_bound = _max_lambda;
   }
 }
 
