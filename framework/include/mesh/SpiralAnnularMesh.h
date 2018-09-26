7324b7f74a2a49fc40ced4bc8f09aec53f28cf4f
Derek Gaston <derek.gaston@inl.gov>
2018-09-26 14:08:15 -0600

    Create stub SpiralAnnularMesh to get started refs #12205

---
+++ framework/include/mesh/SpiralAnnularMesh.h
@@ -0,0 +1,73 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifndef SPIRALANNULARMESH_H
+#define SPIRALANNULARMESH_H
+
+#include "MooseMesh.h"
+
+class SpiralAnnularMesh;
+
+template <>
+InputParameters validParams<SpiralAnnularMesh>();
+
+/**
+ * Mesh generated from parameters
+ */
+class SpiralAnnularMesh : public MooseMesh
+{
+public:
+  SpiralAnnularMesh(const InputParameters & parameters);
+  SpiralAnnularMesh(const SpiralAnnularMesh & /* other_mesh */) = default;
+
+  // No copy
+  SpiralAnnularMesh & operator=(const SpiralAnnularMesh & other_mesh) = delete;
+
+  virtual std::unique_ptr<MooseMesh> safeClone() const override;
+
+  virtual void buildMesh() override;
+  virtual Real getMinInDimension(unsigned int component) const override;
+  virtual Real getMaxInDimension(unsigned int component) const override;
+
+protected:
+  /// Number of elements in radial direction
+  const unsigned _nr;
+
+  /// Number of elements in angular direction
+  const unsigned _nt;
+
+  /// Minimum radius
+  const Real _rmin;
+
+  /// Maximum radius
+  const Real _rmax;
+
+  /// Minimum angle
+  const Real _tmin;
+
+  /// Maximum angle
+  const Real _tmax;
+
+  /// Bias on radial meshing
+  const Real _growth_r;
+
+  /// rmax = rmin + len + len*g + len*g^2 + len*g^3 + ... + len*g^(nr-1) = rmin + len*(1 - g^nr)/(1 - g)
+  const Real _len;
+
+  /// Whether a full annulus (as opposed to a sector) will needs to generate
+  const bool _full_annulus;
+
+  /// Subdomain ID of created quad elements
+  const SubdomainID _quad_subdomain_id;
+
+  /// Subdomain ID of created tri elements (that only exist if rmin=0)
+  const SubdomainID _tri_subdomain_id;
+};
+
+#endif /* SPIRALANNULARMESH_H */
