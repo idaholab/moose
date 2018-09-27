81759f92aeffc00cae4715c4a40a72711f4ea095
Cody Permann <codypermann@gmail.com>
2018-09-27 14:39:08 -0600

    Add new Distributed Polycrstal test and document remaining GrainTracker tests
    closes #12216

---
+++ modules/phase_field/test/include/auxkernels/VoronoiICAux.h
@@ -0,0 +1,38 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifndef VORONOIICAUX_H
+#define VORONOIICAUX_H
+
+#include "AuxKernel.h"
+#include "PolycrystalUserObjectBase.h"
+
+// Forward Declarations
+class VoronoiICAux;
+
+template <>
+InputParameters validParams<VoronoiICAux>();
+
+/**
+ * Visualize the location of grain boundaries in a polycrystalline simulation.
+ */
+class VoronoiICAux : public AuxKernel
+{
+public:
+  VoronoiICAux(const InputParameters & parameters);
+
+protected:
+  virtual Real computeValue();
+
+  const PolycrystalUserObjectBase & _poly_ic_uo;
+
+  std::vector<unsigned int> _grain_ids;
+};
+
+#endif // VORONOIICAUX_H
