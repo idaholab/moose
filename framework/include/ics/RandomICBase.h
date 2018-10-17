87e139f07a8e6274a5e07732826faeb05ccdf401
Benjamin Spencer <benjamin.spencer@inl.gov>
2018-10-17 12:19:46 -0600

    RandomIC refactor ref #9710
    Create RandomICBase, which contains common code used by any random IC class
    Add option to RandomIC to use a user-provided Distribution object

---
+++ framework/include/ics/RandomICBase.h
@@ -0,0 +1,79 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifndef RANDOMICBASE_H
+#define RANDOMICBASE_H
+
+#include "InitialCondition.h"
+#include "RandomData.h"
+#include "MooseRandom.h"
+
+// Forward Declarations
+class InputParameters;
+class RandomICBase;
+namespace libMesh
+{
+class Point;
+}
+
+template <typename T>
+InputParameters validParams();
+
+template <>
+InputParameters validParams<RandomICBase>();
+
+/**
+ * Base class for randomly generated initial conditions
+ */
+class RandomICBase : public InitialCondition
+{
+public:
+  /**
+   * Constructor
+   *
+   * @param parameters The parameters object holding data for the class to use.
+   */
+  RandomICBase(const InputParameters & parameters);
+
+  void initialSetup() override;
+
+protected:
+  /**
+   * Generate a uniformly distributed random number on the interval from 0 to 1
+   * @return random number
+   */
+  Real generateRandom();
+
+  /// Determines whether a variable basis is elemental or nodal
+  const bool _is_nodal;
+
+  /// Boolean to indicate whether we want to use the old (deprecated) generation pattern
+  const bool _use_legacy;
+
+private:
+  /// RandomData element object, we cannot inherit from RandomInterface in an InitialCondition
+  std::unique_ptr<RandomData> _elem_random_data;
+
+  /// RandomData node object, we cannot inherit from RandomInterface in an InitialCondition
+  std::unique_ptr<RandomData> _node_random_data;
+
+  /// Elemental random number generator
+  MooseRandom * _elem_random_generator;
+
+  /// Nodal random number generator
+  MooseRandom * _node_random_generator;
+
+  /// Random numbers per element (currently limited to a single value at a time)
+  std::map<dof_id_type, Real> _elem_numbers;
+
+  /// Random numbers per node (currently limited to a single value at a time)
+  std::map<dof_id_type, Real> _node_numbers;
+};
+
+#endif // RANDOMICBASE_H
---
+++ modules/stochastic_tools/test/tests/ics/random_ic_distribution_test/random_ic_distribution_test.i
@@ -0,0 +1,88 @@
+[Mesh]
+  type = GeneratedMesh
+  dim = 2
+  nx = 50
+  ny = 50
+[]
+
+[Variables]
+  [u]
+    order = FIRST
+    family = LAGRANGE
+  []
+[]
+
+[AuxVariables]
+  [u_aux]
+    order = CONSTANT
+    family = MONOMIAL
+  []
+[]
+
+[Distributions]
+  [./uniform]
+    type = UniformDistribution
+    lower_bound = 1.0
+    upper_bound = 3.0
+  [../]
+[]
+
+[ICs]
+  [u_aux]
+    type = RandomIC
+    legacy_generator = false
+    variable = u_aux
+    distribution = uniform
+  []
+[]
+
+[Kernels]
+  [diff]
+    type = Diffusion
+    variable = u
+  []
+[]
+
+[BCs]
+  [left]
+    type = DirichletBC
+    variable = u
+    boundary = 3
+    value = 0
+  []
+
+  [right]
+    type = DirichletBC
+    variable = u
+    boundary = 1
+    value = 1
+  []
+[]
+
+[VectorPostprocessors]
+  [./histo]
+    type = VolumeHistogram
+    variable = u_aux
+    min_value = 0
+    max_value = 4
+    bin_number = 80
+    execute_on = initial
+    outputs = initial
+  [../]
+[]
+
+[Executioner]
+  type = Steady
+  solve_type = 'PJFNK'
+
+  petsc_options_iname = '-pc_type -pc_hypre_type'
+  petsc_options_value = 'hypre boomeramg'
+[]
+
+[Outputs]
+  exodus = true
+  [./initial]
+    type = CSV
+    execute_on = initial
+  [../]
+[]
