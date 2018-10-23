0eb911097206f7f9a76990197e5ace920e9190d0
Leora Chapuis <Leora.Chapuis@inl.gov>
2018-10-23 11:28:00 -0600

    Create BlockDeletionGenerator. Also add a basic test for this class. Refs #11640.

---
+++ framework/include/mesh_generators/BlockDeletionGenerator.h
@@ -0,0 +1,37 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifndef BLOCKDELETIONGENERATOR_H
+#define BLOCKDELETIONGENERATOR_H
+
+#include "ElementDeletionBaseGenerator.h"
+
+// Forward declarations
+class BlockDeletionGenerator;
+
+template <>
+InputParameters validParams<BlockDeletionGenerator>();
+
+/**
+ * MeshGenerator for 
+ */
+class BlockDeletionGenerator : public ElementDeletionBaseGenerator
+{
+public:
+  BlockDeletionGenerator(const InputParameters & parameters);
+
+protected:
+  virtual bool shouldDelete(const Elem * elem) override;
+
+private:
+  ///Defines the block to be removed
+  const SubdomainID _block_id;  
+};
+
+#endif // BLOCKDELETIONGENERATOR_H
---
+++ framework/src/mesh_generators/BlockDeletionGenerator.C
@@ -0,0 +1,37 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "BlockDeletionGenerator.h"
+
+registerMooseObject("MooseApp", BlockDeletionGenerator);
+
+template <>
+InputParameters
+validParams<BlockDeletionGenerator>()
+{
+  InputParameters params = validParams<ElementDeletionBaseGenerator>();
+
+  params.addClassDescription(
+      "Mesh modifier which removes elements with the specified subdomain ID");
+  params.addRequiredParam<SubdomainID>("block_id", "The block to be deleted");
+  
+  return params;
+}
+
+BlockDeletionGenerator::BlockDeletionGenerator(const InputParameters & parameters)
+: ElementDeletionBaseGenerator(parameters),
+  _block_id(getParam<SubdomainID>("block_id"))
+{
+}
+
+bool
+BlockDeletionGenerator::shouldDelete(const Elem * elem)
+{
+  return elem->subdomain_id() == _block_id;
+}
