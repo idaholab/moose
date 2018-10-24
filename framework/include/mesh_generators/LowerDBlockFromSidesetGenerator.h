9f8ac97b8254d6e7afe5b43f58995c77a8584b4d
Leora Chapuis <Leora.Chapuis@inl.gov>
2018-10-24 11:23:45 -0600

    Create LowerDBlockFromSidesetGenerator. Also create a mesh test. Refs #11640.

---
+++ framework/include/mesh_generators/LowerDBlockFromSidesetGenerator.h
@@ -0,0 +1,41 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifndef LOWERDBLOCKFROMSIDESETGENERATOR_H
+#define LOWERDBLOCKFROMSIDESETGENERATOR_H
+
+#include "MeshGenerator.h"
+
+// Forward declarations
+class LowerDBlockFromSidesetGenerator;
+
+template <>
+InputParameters validParams<LowerDBlockFromSidesetGenerator>();
+
+/**
+ * Creates lower-dimensional elements on the specified sidesets
+ */
+class LowerDBlockFromSidesetGenerator : public MeshGenerator
+{
+public:
+  LowerDBlockFromSidesetGenerator(const InputParameters & parameters);
+
+  std::unique_ptr<MeshBase> generate();
+
+protected:
+  std::unique_ptr<MeshBase> & _input;
+
+  /// The subdomain ID of the new lower dimensional block
+  const subdomain_id_type _new_block_id;
+  /// The sidesets on which to create the lower dimensional elements
+  std::vector<boundary_id_type> _sidesets;
+
+};
+
+#endif // LOWERDBLOCKFROMSIDESETGENERATOR_H
