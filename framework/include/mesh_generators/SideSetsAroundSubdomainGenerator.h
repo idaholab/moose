19606a46f294c88838dafd13e10f061b94d7df4e
Leora Chapuis <Leora.Chapuis@inl.gov>
2018-10-25 11:53:05 -0600

    Create SideSetsAroundSubdomainGenerator. Also add 2 mesh tests for this class. Refs #11640.

---
+++ framework/include/mesh_generators/SideSetsAroundSubdomainGenerator.h
@@ -0,0 +1,54 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifndef SIDESETSAROUNDSUBDOMAINGENERATOR_H
+#define SIDESETSAROUNDSUBDOMAINGENERATOR_H
+
+#include "GenerateSideSetsBase.h"
+
+// Forward declarations
+class SideSetsAroundSubdomainGenerator;
+
+template <>
+InputParameters validParams<SideSetsAroundSubdomainGenerator>();
+
+/**
+ * Adds the faces on the boundary of given block
+ * to the sidesets specified by "boundary"
+ * Optionally, only adds faces that have a normal
+ * equal to specified normal up to a tolerance
+ */
+class SideSetsAroundSubdomainGenerator : public GenerateSideSetsBase
+{
+public:
+  SideSetsAroundSubdomainGenerator(const InputParameters & parameters);
+
+  std::unique_ptr<MeshBase> generate();
+
+protected:
+  std::unique_ptr<MeshBase> & _input;
+
+  /// names of the sidesets to which the faces will be added
+  std::vector<BoundaryName> _boundary_names;
+
+  /// true if only faces close to "normal" will be added
+  bool _using_normal;
+
+  /**
+   * if normal is specified, then faces are only added
+   * if face_normal.normal_hat <= 1 - normal_tol
+   * where normal_hat = _normal/|_normal|
+   */
+  Real _normal_tol;
+
+  /// if specified, then faces are only added if their normal is close to this
+  Point _normal;
+};
+
+#endif // SIDESETSAROUNDSUBDOMAINGENERATOR_H
