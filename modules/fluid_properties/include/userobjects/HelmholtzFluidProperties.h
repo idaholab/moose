88698cd5c8cfb377b15b9030cf3c5b48058d53ba
Chris Green <chris.green@csiro.au>
2018-10-31 21:37:29 +1100

    Add Helmholtz fluid properties base class
    Base class for fluid EOS based on a Helmholtz free energy. Properties
    such as density, enthalpy etc can all be calculated using the free
    energy.
    
    Refs #12410

---
+++ modules/fluid_properties/include/userobjects/HelmholtzFluidProperties.h
@@ -0,0 +1,143 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifndef HELMHOLTZFLUIDPROPERTIES_H
+#define HELMHOLTZFLUIDPROPERTIES_H
+
+#include "SinglePhaseFluidProperties.h"
+
+class HelmholtzFluidProperties;
+
+template <>
+InputParameters validParams<HelmholtzFluidProperties>();
+
+/**
+ * Base class equation of state for fluids that use a Helmholtz free energy
+ * alpha(delta, tau), where delta is a scaled density and tau is a scaled temperature.
+ *
+ * To implement a fluid using the Helmholtz EOS, inherit from this base class and
+ * override the method alpha(delta, tau) and its first and second derivatives wrt delta
+ * and tau. Thermophysical properties such as enthalpy, internal energy etc, will be
+ * calculated from the Helmholtz free energy.
+ *
+ * Transport properties such as viscosity and thermal conductivity will need to be implemented
+ * in any derived class as required.
+ */
+class HelmholtzFluidProperties : public SinglePhaseFluidProperties
+{
+public:
+  HelmholtzFluidProperties(const InputParameters & parameters);
+  virtual ~HelmholtzFluidProperties();
+
+  virtual Real rho_from_p_T(Real pressure, Real temperature) const override;
+
+  virtual void rho_from_p_T(
+      Real pressure, Real temperature, Real & rho, Real & drho_dp, Real & drho_dT) const override;
+
+  virtual Real e_from_p_T(Real pressure, Real temperature) const override;
+  virtual void e_from_p_T(Real p, Real T, Real & e, Real & de_dp, Real & de_dT) const override;
+
+  virtual void rho_e_dpT(Real pressure,
+                         Real temperature,
+                         Real & rho,
+                         Real & drho_dp,
+                         Real & drho_dT,
+                         Real & e,
+                         Real & de_dp,
+                         Real & de_dT) const override;
+
+  virtual Real c_from_p_T(Real pressure, Real temperature) const override;
+
+  virtual Real cp_from_p_T(Real pressure, Real temperature) const override;
+
+  virtual Real cv_from_p_T(Real pressure, Real temperature) const override;
+
+  virtual void rho_mu(Real pressure, Real temperature, Real & rho, Real & mu) const override;
+
+  virtual void rho_mu_dpT(Real pressure,
+                          Real temperature,
+                          Real & rho,
+                          Real & drho_dp,
+                          Real & drho_dT,
+                          Real & mu,
+                          Real & dmu_dp,
+                          Real & dmu_dT) const override;
+
+  virtual Real s_from_p_T(Real pressure, Real temperature) const override;
+  virtual void s_from_p_T(Real p, Real T, Real & s, Real & ds_dp, Real & ds_dT) const override;
+
+  virtual Real h_from_p_T(Real pressure, Real temperature) const override;
+  virtual void h_from_p_T(Real p, Real T, Real & h, Real & dh_dp, Real & dh_dT) const override;
+
+  /**
+   * Pressure as a function of density and temperature
+   *
+   * @param rho density (kg/m^3)
+   * @param T temperature (K)
+   * @return pressure (Pa)
+   */
+  virtual Real p_from_rho_T(Real rho, Real T) const;
+
+protected:
+  /**
+   * Helmholtz free energy
+   *
+   * @param delta scaled density (-)
+   * @param tau scaled temperature (-)
+   * @return alpha Helmholtz free energy
+   */
+  virtual Real alpha(Real delta, Real tau) const = 0;
+
+  /**
+   * Derivative of Helmholtz free energy wrt delta
+   *
+   * @param delta scaled density (-)
+   * @param tau scaled temperature (-)
+   * @return derivative of Helmholtz free energy wrt delta
+   */
+  virtual Real dalpha_ddelta(Real delta, Real tau) const = 0;
+
+  /**
+   * Derivative of Helmholtz free energy wrt tau
+   *
+   * @param delta scaled density (-)
+   * @param tau scaled temperature (-)
+   * @return derivative of Helmholtz free energy wrt tau
+   */
+  virtual Real dalpha_dtau(Real delta, Real tau) const = 0;
+
+  /**
+   * Second derivative of Helmholtz free energy wrt delta
+   *
+   * @param delta scaled density (-)
+   * @param tau scaled temperature (-)
+   * @return second derivative of Helmholtz free energy wrt delta
+   */
+  virtual Real d2alpha_ddelta2(Real delta, Real tau) const = 0;
+
+  /**
+   * Second derivative of Helmholtz free energy wrt tau
+   *
+   * @param delta scaled density (-)
+   * @param tau scaled temperature (-)
+   * @return second derivative of Helmholtz free energy wrt tau
+   */
+  virtual Real d2alpha_dtau2(Real delta, Real tau) const = 0;
+
+  /**
+   * Second derivative of Helmholtz free energy wrt delta and tau
+   *
+   * @param delta scaled density (-)
+   * @param tau scaled temperature (-)
+   * @return second derivative of Helmholtz free energy wrt delta and tau
+   */
+  virtual Real d2alpha_ddeltatau(Real delta, Real tau) const = 0;
+};
+
+#endif /* HELMHOLTZFLUIDPROPERTIES_H */
---
+++ modules/fluid_properties/src/userobjects/HelmholtzFluidProperties.C
@@ -0,0 +1,275 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "HelmholtzFluidProperties.h"
+#include "BrentsMethod.h"
+#include "libmesh/utility.h"
+
+template <>
+InputParameters
+validParams<HelmholtzFluidProperties>()
+{
+  InputParameters params = validParams<SinglePhaseFluidProperties>();
+  params.addClassDescription("Base class for Helmholtz free energy fluid EOS");
+  return params;
+}
+
+HelmholtzFluidProperties::HelmholtzFluidProperties(const InputParameters & parameters)
+  : SinglePhaseFluidProperties(parameters)
+{
+}
+
+HelmholtzFluidProperties::~HelmholtzFluidProperties() {}
+
+Real
+HelmholtzFluidProperties::rho_from_p_T(Real pressure, Real temperature) const
+{
+  Real density;
+  // Initial estimate of a bracketing interval for the density
+  Real lower_density = 1.0e-2;
+  Real upper_density = 100.0;
+
+  // The density is found by finding the zero of the pressure
+  auto pressure_diff = [&pressure, &temperature, this](Real x) {
+    return this->p_from_rho_T(x, temperature) - pressure;
+  };
+
+  BrentsMethod::bracket(pressure_diff, lower_density, upper_density);
+  density = BrentsMethod::root(pressure_diff, lower_density, upper_density);
+
+  return density;
+}
+
+void
+HelmholtzFluidProperties::rho_from_p_T(
+    Real pressure, Real temperature, Real & rho, Real & drho_dp, Real & drho_dT) const
+{
+  rho = this->rho_from_p_T(pressure, temperature);
+
+  // Scale the density and temperature
+  const Real delta = rho / criticalDensity();
+  const Real tau = criticalTemperature() / temperature;
+  const Real da_dd = dalpha_ddelta(delta, tau);
+  const Real d2a_dd2 = d2alpha_ddelta2(delta, tau);
+
+  drho_dp = molarMass() / (_R * temperature * delta * (2.0 * da_dd + delta * d2a_dd2));
+  drho_dT = rho * (tau * d2alpha_ddeltatau(delta, tau) - da_dd) / temperature /
+            (2.0 * da_dd + delta * d2a_dd2);
+}
+
+Real
+HelmholtzFluidProperties::e_from_p_T(Real pressure, Real temperature) const
+{
+  // Require density first
+  const Real density = rho_from_p_T(pressure, temperature);
+  // Scale the input density and temperature
+  const Real delta = density / criticalDensity();
+  const Real tau = criticalTemperature() / temperature;
+
+  return _R * temperature * tau * dalpha_dtau(delta, tau) / molarMass();
+}
+
+void
+HelmholtzFluidProperties::e_from_p_T(
+    Real pressure, Real temperature, Real & e, Real & de_dp, Real & de_dT) const
+{
+  e = this->e_from_p_T(pressure, temperature);
+
+  // Require density first
+  const Real density = rho_from_p_T(pressure, temperature);
+  // Scale the input density and temperature
+  const Real delta = density / criticalDensity();
+  const Real tau = criticalTemperature() / temperature;
+
+  const Real da_dd = dalpha_ddelta(delta, tau);
+  const Real d2a_dd2 = d2alpha_ddelta2(delta, tau);
+  const Real d2a_ddt = d2alpha_ddeltatau(delta, tau);
+
+  de_dp = tau * d2a_ddt / (density * (2.0 * da_dd + delta * d2a_dd2));
+  de_dT = -_R *
+          (delta * tau * d2a_ddt * (da_dd - tau * d2a_ddt) / (2.0 * da_dd + delta * d2a_dd2) +
+           tau * tau * d2alpha_dtau2(delta, tau)) /
+          molarMass();
+}
+
+void
+HelmholtzFluidProperties::rho_e_dpT(Real pressure,
+                                    Real temperature,
+                                    Real & rho,
+                                    Real & drho_dp,
+                                    Real & drho_dT,
+                                    Real & e,
+                                    Real & de_dp,
+                                    Real & de_dT) const
+{
+  Real density, ddensity_dp, ddensity_dT;
+  rho_from_p_T(pressure, temperature, density, ddensity_dp, ddensity_dT);
+  rho = density;
+  drho_dp = ddensity_dp;
+  drho_dT = ddensity_dT;
+
+  Real energy, denergy_dp, denergy_dT;
+  e_from_p_T(pressure, temperature, energy, denergy_dp, denergy_dT);
+  e = energy;
+  de_dp = denergy_dp;
+  de_dT = denergy_dT;
+}
+
+Real
+HelmholtzFluidProperties::c_from_p_T(Real pressure, Real temperature) const
+{
+  // Require density first
+  const Real density = rho_from_p_T(pressure, temperature);
+  // Scale the input density and temperature
+  const Real delta = density / criticalDensity();
+  const Real tau = criticalTemperature() / temperature;
+
+  const Real da_dd = dalpha_ddelta(delta, tau);
+
+  Real w = 2.0 * delta * da_dd + delta * delta * d2alpha_ddelta2(delta, tau);
+  w -= Utility::pow<2>(delta * da_dd - delta * tau * d2alpha_ddeltatau(delta, tau)) /
+       (tau * tau * d2alpha_dtau2(delta, tau));
+
+  return std::sqrt(_R * temperature * w / molarMass());
+}
+
+Real
+HelmholtzFluidProperties::cp_from_p_T(Real pressure, Real temperature) const
+{
+  // Require density first
+  const Real density = rho_from_p_T(pressure, temperature);
+  // Scale the input density and temperature
+  const Real delta = density / criticalDensity();
+  const Real tau = criticalTemperature() / temperature;
+
+  const Real da_dd = dalpha_ddelta(delta, tau);
+
+  const Real cp = _R *
+                  (-tau * tau * d2alpha_dtau2(delta, tau) +
+                   Utility::pow<2>(delta * da_dd - delta * tau * d2alpha_ddeltatau(delta, tau)) /
+                       (2.0 * delta * da_dd + delta * delta * d2alpha_ddelta2(delta, tau))) /
+                  molarMass();
+
+  return cp;
+}
+
+Real
+HelmholtzFluidProperties::cv_from_p_T(Real pressure, Real temperature) const
+{
+  // Require density first
+  const Real density = rho_from_p_T(pressure, temperature);
+  // Scale the input density and temperature
+  const Real delta = density / criticalDensity();
+  const Real tau = criticalTemperature() / temperature;
+
+  return -_R * tau * tau * d2alpha_dtau2(delta, tau) / molarMass();
+}
+
+void
+HelmholtzFluidProperties::rho_mu(Real pressure, Real temperature, Real & rho, Real & mu) const
+{
+  rho = rho_from_p_T(pressure, temperature);
+  mu = mu_from_p_T(pressure, temperature);
+}
+
+void
+HelmholtzFluidProperties::rho_mu_dpT(Real pressure,
+                                     Real temperature,
+                                     Real & rho,
+                                     Real & drho_dp,
+                                     Real & drho_dT,
+                                     Real & mu,
+                                     Real & dmu_dp,
+                                     Real & dmu_dT) const
+{
+  rho_from_p_T(pressure, temperature, rho, drho_dp, drho_dT);
+  mu_from_p_T(pressure, temperature, mu, dmu_dp, dmu_dT);
+}
+
+Real
+HelmholtzFluidProperties::s_from_p_T(Real pressure, Real temperature) const
+{
+  // Require density first
+  const Real density = rho_from_p_T(pressure, temperature);
+  // Scale the input density and temperature
+  const Real delta = density / criticalDensity();
+  const Real tau = criticalTemperature() / temperature;
+
+  return _R * (tau * dalpha_dtau(delta, tau) - alpha(delta, tau)) / molarMass();
+}
+
+void
+HelmholtzFluidProperties::s_from_p_T(
+    Real pressure, Real temperature, Real & s, Real & ds_dp, Real & ds_dT) const
+{
+  s = this->s_from_p_T(pressure, temperature);
+
+  // Require density first
+  const Real density = rho_from_p_T(pressure, temperature);
+  // Scale the input density and temperature
+  const Real delta = density / criticalDensity();
+  const Real tau = criticalTemperature() / temperature;
+
+  const Real da_dd = dalpha_ddelta(delta, tau);
+  const Real da_dt = dalpha_dtau(delta, tau);
+  const Real d2a_dd2 = d2alpha_ddelta2(delta, tau);
+  const Real d2a_dt2 = d2alpha_dtau2(delta, tau);
+  const Real d2a_ddt = d2alpha_ddeltatau(delta, tau);
+
+  ds_dp = tau * (d2a_ddt - da_dd) / (density * temperature * (2.0 * da_dd + delta * d2a_dd2));
+  ds_dT = -_R * tau * (da_dt - alpha(delta, tau) + tau * (d2a_dt2 - da_dt)) /
+          (molarMass() * temperature);
+}
+
+Real
+HelmholtzFluidProperties::h_from_p_T(Real pressure, Real temperature) const
+{
+  // Require density first
+  const Real density = rho_from_p_T(pressure, temperature);
+  // Scale the input density and temperature
+  const Real delta = density / criticalDensity();
+  const Real tau = criticalTemperature() / temperature;
+
+  return _R * temperature * (tau * dalpha_dtau(delta, tau) + delta * dalpha_ddelta(delta, tau)) /
+         molarMass();
+}
+
+void
+HelmholtzFluidProperties::h_from_p_T(
+    Real pressure, Real temperature, Real & h, Real & dh_dp, Real & dh_dT) const
+{
+  h = this->h_from_p_T(pressure, temperature);
+
+  // Require density first
+  const Real density = rho_from_p_T(pressure, temperature);
+  // Scale the input density and temperature
+  const Real delta = density / criticalDensity();
+  const Real tau = criticalTemperature() / temperature;
+
+  const Real da_dd = dalpha_ddelta(delta, tau);
+  const Real d2a_dd2 = d2alpha_ddelta2(delta, tau);
+  const Real d2a_ddt = d2alpha_ddeltatau(delta, tau);
+
+  dh_dp = (da_dd + delta * d2a_dd2 + tau * d2a_ddt) / (density * (2.0 * da_dd + delta * d2a_dd2));
+  dh_dT = _R *
+          (delta * da_dd * (1.0 - tau * d2a_ddt / da_dd) * (1.0 - tau * d2a_ddt / da_dd) /
+               (2.0 + delta * d2a_dd2 / da_dd) -
+           tau * tau * d2alpha_dtau2(delta, tau)) /
+          molarMass();
+}
+
+Real
+HelmholtzFluidProperties::p_from_rho_T(Real density, Real temperature) const
+{
+  // Scale the input density and temperature
+  const Real delta = density / criticalDensity();
+  const Real tau = criticalTemperature() / temperature;
+
+  return _R * density * temperature * delta * dalpha_ddelta(delta, tau) / molarMass();
+}
