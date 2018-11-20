adb4d9b9037cd0d1cdf4bfc2437c87d0de789f57
Derek Gaston <derek.gaston@inl.gov>
2018-11-20 13:32:46 -0700

    Fixes for RenameBoundaryGenerator refs #11640

--- framework/include/meshgenerators/RenameBoundaryGenerator.h
+++ framework/include/meshgenerators/RenameBoundaryGenerator.h
@@ -38,56 +38,6 @@ protected:
   std::vector<boundary_id_type> _new_boundary_id;
 
   std::vector<BoundaryName> _new_boundary_name;
-
-  /**
-   * Given a new_boundary_id, provide a boundary name, based
-   * on the old boundary names provided (or deduced from
-   * the old_boundary_ids provided).
-   *
-   * Eg, if
-   * old_boundary_name = 'boundary1 boundary2'
-   * new_boundary_id   = '4      5'
-   * Then
-   * newBoundaryName(4) = boundary1
-   * newBoundaryName(5) = boundary2
-   *
-   * In the case of merging boundaries, the *first encountered*
-   * boundary's name is used.
-   * Eg, if
-   * old_boundary_name = 'asdf boundary4 boundary1'
-   * new_boundary_id   = '3    1      1'
-   * then
-   * newBoundaryName(1) = boundary4, because we look along
-   * new_boundary_id until we first encounter 1, and then get the corresponding name
-   * @param new_boundary_id the new boundary's ID number
-   * @return the name that will be given to that boundary
-   */
-  BoundaryName newBoundaryName(const boundary_id_type & new_boundary_id);
-
-  /**
-   * Given a new_boundary_name, provide a boundary ID, based
-   * on the old boundary IDs provided (or deduced from
-   * the old_boundary_names provided).
-   *
-   * Eg, if
-   * old_boundary_id =   '4      5'
-   * new_boundary_name = 'boundary1 boundary2'
-   * Then
-   * newBoundaryID(boundary1) = 4
-   * newBoundaryID(boundary2) = 5
-   *
-   * In the case of merging boundaries, the *first encountered*
-   * boundary's ID is used.
-   * Eg, if
-   * old_boundary_id =   '3    1      1'
-   * new_boundary_name = 'asdf boundary4 boundary4'
-   * then
-   * newBoundaryID(boundary4) = 1, because we look along
-   * new_boundary_name until we first encounter boundary4, and then get the corresponding ID
-   * @param new_boundary_name the new boundary's name
-   * @return the ID number that will be given to that boundary
-   */
-  BoundaryID newBoundaryID(const BoundaryName & new_boundary_name);
 };
 
 #endif // RENAMEBOUNDARYGENERATOR_H
