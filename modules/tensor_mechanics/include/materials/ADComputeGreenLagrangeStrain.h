f0b033617dc8a2bafb027412f07f1d39970660ce
Daniel Schwen <daniel.schwen@inl.gov>
2019-01-29 15:54:19 -0700

    Add Green-Lagrange strain (#12787)

---
+++ modules/tensor_mechanics/include/materials/ADComputeGreenLagrangeStrain.h
@@ -0,0 +1,35 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifndef ADCOMPUTEGREENLAGRANGESTRAIN_H
+#define ADCOMPUTEGREENLAGRANGESTRAIN_H
+
+#include "ADComputeStrainBase.h"
+
+template <ComputeStage>
+class ADComputeGreenLagrangeStrain;
+
+declareADValidParams(ADComputeGreenLagrangeStrain);
+
+/**
+ * ADComputeGreenLagrangeStrain defines a non-linear Green-Lagrange strain tensor
+ */
+template <ComputeStage compute_stage>
+class ADComputeGreenLagrangeStrain : public ADComputeStrainBase<compute_stage>
+{
+public:
+  ADComputeGreenLagrangeStrain(const InputParameters & parameters);
+
+protected:
+  virtual void computeProperties() override;
+
+  usingComputeStrainBaseMembers;
+};
+
+#endif // ADCOMPUTEGREENLAGRANGESTRAIN_H
