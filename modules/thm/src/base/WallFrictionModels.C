5e26cf022547326653f6db01e71a8c54f04de260
hansje <joshua.hansel@inl.gov>
2019-02-05 07:21:12 -0700

    Fixed Churchill correlation constant
    Also, removed references to TRACE theory manual; if the model is
    in this submodule, it should be independent of TRACE.
    
    Closes #19680

--- modules/thm/src/base/WallFrictionModels.C
+++ modules/thm/src/base/WallFrictionModels.C
@@ -9,12 +9,9 @@ Churchill(Real Re, Real roughness, Real Dh)
 {
   Real Re_limit = std::max(Re, 10.0);
 
-  // Equation (4-167), page 171
   Real a =
-      std::pow(2.475 * std::log(1.0 / (std::pow(7.0 / Re_limit, 0.9) + 0.27 * roughness / Dh)), 16);
-  // Equation (4-168), page 171
+      std::pow(2.457 * std::log(1.0 / (std::pow(7.0 / Re_limit, 0.9) + 0.27 * roughness / Dh)), 16);
   Real b = std::pow(3.753e4 / Re_limit, 16);
-  // Equation (4-166), page 171
   return 2.0 * std::pow(std::pow(8.0 / Re_limit, 12) + 1.0 / std::pow(a + b, 1.5), 1.0 / 12.0);
 }
 }
