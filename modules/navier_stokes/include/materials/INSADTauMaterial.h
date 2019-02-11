75b1f861a1ce72acf384f42d862789d62c114c67
Alex Lindsay <alexlindsay239@gmail.com>
2019-02-11 16:25:34 -0700

    Create INSADTauMaterial

---
+++ modules/navier_stokes/include/materials/INSADTauMaterial.h
@@ -0,0 +1,40 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifndef INSADTAUMATERIAL_H
+#define INSADTAUMATERIAL_H
+
+#include "ADMaterial.h"
+
+template <ComputeStage>
+class INSADTauMaterial;
+
+declareADValidParams(INSADTauMaterial);
+
+template <ComputeStage compute_stage>
+class INSADTauMaterial : public ADMaterial<compute_stage>
+{
+public:
+  INSADTauMaterial(const InputParameters & parameters);
+
+protected:
+  virtual void computeProperties() override;
+  virtual void computeQpProperties() override;
+
+  const ADVectorVariableValue & _velocity;
+  const ADMaterialProperty(Real) & _mu;
+  const ADMaterialProperty(Real) & _rho;
+  const bool _transient_term;
+  const Real _alpha;
+  ADMaterialProperty(Real) & _tau;
+
+  ADReal _hmax;
+};
+
+#endif // INSADTAUMATERIAL_H
---
+++ modules/navier_stokes/src/materials/INSADTauMaterial.C
@@ -0,0 +1,91 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+// Navier-Stokes includes
+#include "INSADTauMaterial.h"
+
+defineADValidParams(
+    INSADTauMaterial,
+    ADMaterial,
+    params.addClassDescription(
+        "This is the material class used to compute the stabilization parameter tau.");
+    params.addRequiredCoupledVar("velocity", "The velocity");
+    params.addParam<MaterialPropertyName>("mu_name", "mu", "The name of the dynamic viscosity");
+    params.addParam<MaterialPropertyName>("rho_name", "rho", "The name of the density");
+
+    params.addParam<Real>("alpha", 1., "Multiplicative factor on the stabilization parameter tau.");
+    params.addParam<bool>("transient_term",
+                          false,
+                          "Whether there should be a transient term in the momentum residuals."););
+
+template <ComputeStage compute_stage>
+INSADTauMaterial<compute_stage>::INSADTauMaterial(const InputParameters & parameters)
+  : ADMaterial<compute_stage>(parameters),
+    _velocity(adCoupledVectorValue("velocity")),
+    _mu(adGetADMaterialProperty<Real>("mu_name")),
+    _rho(adGetADMaterialProperty<Real>("rho_name")),
+    _transient_term(adGetParam<bool>("transient_term")),
+    _alpha(adGetParam<Real>("alpha")),
+    _tau(adDeclareADMaterialProperty<Real>("tau"))
+{
+}
+
+template <ComputeStage compute_stage>
+void
+INSADTauMaterial<compute_stage>::computeHMax()
+{
+  _hmax = _current_elem->hmax();
+}
+
+template <>
+void
+INSADTauMaterial<JACOBIAN>::computeHMax()
+{
+  if (!_displacements.size())
+  {
+    _hmax = _current_elem->hmax();
+    return;
+  }
+
+  _hmax = 0;
+
+  for (unsigned int n_outer = 0; n_outer < _current_elem->n_vertices(); n_outer++)
+    for (unsigned int n_inner = n_outer + 1; n_inner < _current_elem->n_vertices(); n_inner++)
+    {
+      VectorValue<ADReal> diff = (_current_elem->point(n_outer) - _current_elem->point(n_inner));
+      unsigned dimension = 0;
+      for (const auto & disp_num : _displacements)
+      {
+        diff(dimension).derivatives()[disp_num * _sys.getMaxVarNDofsPerElem() + n_outer] = 1.;
+        diff(dimension++).derivatives()[disp_num * _sys.getMaxVarNDofsPerElem() + n_inner] = -1.;
+      }
+
+      _hmax = std::max(_hmax, diff.norm_sq());
+    }
+
+  _hmax = std::sqrt(_hmax);
+}
+
+void
+INSADTauMaterial::computeProperties()
+{
+  computeHMax();
+
+  Material::computeProperties();
+}
+
+void
+INSADTauMaterial::computeQpProperties()
+{
+  auto && nu = _mu[_qp] / _rho[_qp];
+  auto && transient_part = _transient_term ? 4. / (_dt * _dt) : 0.;
+  _tau[_qp] = _alpha / std::sqrt(transient_part +
+                                 (2. * _velocity.norm() / _hmax) * (2. * _velocity.norm() / _hmax) +
+                                 9. * (4. * nu / (_hmax * _hmax)) * (4. * nu / (_hmax * _hmax)));
+}
