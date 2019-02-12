8a725571d4fe165d2f46fd8e2d33bf0ebfb09c9a
Casey Icenhour <cticenho@ncsu.edu>
2019-02-12 13:53:24 -0700

    Declare _curl_test, _curl_phi and _curl_u in CurlCurlField. refs #5

--- modules/electromagnetics/include/kernels/CurlCurlField.h
+++ modules/electromagnetics/include/kernels/CurlCurlField.h
@@ -17,6 +17,15 @@ protected:
   virtual Real computeQpResidual() override;
   virtual Real computeQpJacobian() override;
 
+  /// curl of the test function
+  const VectorVariableTestCurl & _curl_test;
+
+  /// curl of the shape function
+  const VectorVariablePhiCurl & _curl_phi;
+
+  /// Holds the solution curl at the current quadrature points
+  const VectorVariableCurl & _curl_u;
+
   Real _sign;
 };
 
