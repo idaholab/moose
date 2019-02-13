dad4ca85c8ecddc7df72892f4d92ddca8b42900e
David Andrs <david.andrs@inl.gov>
2019-02-13 11:18:34 -0700

    Adding test

---
+++ test/include/materials/CoupledValuesMaterial.h
@@ -0,0 +1,46 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifndef COUPLEDVALUESMATERIAL_H
+#define COUPLEDVALUESMATERIAL_H
+
+#include "Material.h"
+
+class CoupledValuesMaterial;
+
+template <>
+InputParameters validParams<CoupledValuesMaterial>();
+
+/**
+ * A material that couples variables values and stores them into material property
+ * This makes sure that everything is properly resized and can be indexed into.
+ */
+class CoupledValuesMaterial : public Material
+{
+public:
+  CoupledValuesMaterial(const InputParameters & parameters);
+
+protected:
+  virtual void computeQpProperties() override;
+
+  const VariableValue & _value;
+  const VariableValue & _dot;
+  const VariableValue & _dot_dot;
+  const VariableValue & _dot_du;
+  const VariableValue & _dot_dot_du;
+
+  const std::string & _var_name;
+  MaterialProperty<Real> & _value_prop;
+  MaterialProperty<Real> & _dot_prop;
+  MaterialProperty<Real> & _dot_dot_prop;
+  MaterialProperty<Real> & _dot_du_prop;
+  MaterialProperty<Real> & _dot_dot_du_prop;
+};
+
+#endif // COUPLEDVALUESMATERIAL_H
---
+++ test/src/materials/CoupledValuesMaterial.C
@@ -0,0 +1,48 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "CoupledValuesMaterial.h"
+
+registerMooseObject("MooseTestApp", CoupledValuesMaterial);
+
+template <>
+InputParameters
+validParams<CoupledValuesMaterial>()
+{
+  InputParameters params = validParams<Material>();
+  params.addRequiredCoupledVar("variable", "Coupled variable");
+  return params;
+}
+
+CoupledValuesMaterial::CoupledValuesMaterial(const InputParameters & parameters)
+  : Material(parameters),
+    _value(coupledValue("variable")),
+    _dot(coupledDot("variable")),
+    _dot_dot(coupledDotDot("variable")),
+    _dot_du(coupledDotDu("variable")),
+    _dot_dot_du(coupledDotDotDu("variable")),
+
+    _var_name(getVar("variable", 0)->name()),
+    _value_prop(declareProperty<Real>(_var_name + "_value")),
+    _dot_prop(declareProperty<Real>(_var_name + "_dot")),
+    _dot_dot_prop(declareProperty<Real>(_var_name + "_dot_dot")),
+    _dot_du_prop(declareProperty<Real>(_var_name + "_dot_du")),
+    _dot_dot_du_prop(declareProperty<Real>(_var_name + "_dot_dot_du"))
+{
+}
+
+void
+CoupledValuesMaterial::computeQpProperties()
+{
+  _value_prop[_qp] = _value[_qp];
+  _dot_prop[_qp] = _dot[_qp];
+  _dot_dot_prop[_qp] = _dot_dot[_qp];
+  _dot_du_prop[_qp] = _dot_du[_qp];
+  _dot_dot_du_prop[_qp] = _dot_dot_du[_qp];
+}
