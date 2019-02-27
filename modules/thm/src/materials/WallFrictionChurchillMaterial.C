13aaba528a88fe3dca61481e316deadb064f2607
David Andrs <david.andrs@inl.gov>
2019-02-27 13:25:43 -0700

    Cleanup 1-phase wall friction closure setup
    Refs idaholab/relap-7#547

--- modules/thm/src/materials/WallFrictionChurchillMaterial.C
+++ modules/thm/src/materials/WallFrictionChurchillMaterial.C
@@ -8,7 +8,7 @@ template <>
 InputParameters
 validParams<WallFrictionChurchillMaterial>()
 {
-  InputParameters params = validParams<WallFriction3EqnBaseMaterial>();
+  InputParameters params = validParams<Material>();
   params.addRequiredCoupledVar("rhoA", "Mass equation variable: rho*A");
   params.addRequiredCoupledVar("rhouA", "Momentum equation variable: rho*u*A");
   params.addRequiredCoupledVar("rhoEA", "Total energy equation variable: rho*E*A");
@@ -25,7 +25,18 @@ validParams<WallFrictionChurchillMaterial>()
 }
 
 WallFrictionChurchillMaterial::WallFrictionChurchillMaterial(const InputParameters & parameters)
-  : WallFriction3EqnBaseMaterial(parameters)
+  : DerivativeMaterialInterfaceTHM<Material>(parameters),
+    _f_D_name(getParam<MaterialPropertyName>("f_D")),
+    _f_D(declareProperty<Real>(_f_D_name)),
+    _df_D_drhoA(declarePropertyDerivativeTHM<Real>(_f_D_name, "rhoA")),
+    _df_D_drhouA(declarePropertyDerivativeTHM<Real>(_f_D_name, "rhouA")),
+    _df_D_drhoEA(declarePropertyDerivativeTHM<Real>(_f_D_name, "rhoEA")),
+
+    _mu(getMaterialProperty<Real>("mu")),
+    _rho(coupledValue("rho")),
+    _vel(coupledValue("vel")),
+    _D_h(coupledValue("D_h")),
+    _roughness(getParam<Real>("roughness"))
 {
 }
 
