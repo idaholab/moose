c95f18a270bf919753c5366972a845bdbbee7ec6
Daniel Schwen <daniel.schwen@inl.gov>
2019-03-07 13:30:16 -0700

    Add vector time derivative (#13017)

---
+++ framework/include/kernels/ADVectorTimeDerivative.h
@@ -0,0 +1,33 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifndef ADVECTORTIMEDERIVATIVE_H
+#define ADVECTORTIMEDERIVATIVE_H
+
+#include "ADTimeKernel.h"
+
+// Forward Declaration
+template <ComputeStage>
+class ADVectorTimeDerivative;
+
+declareADValidParams(ADVectorTimeDerivative);
+
+template <ComputeStage compute_stage>
+class ADVectorTimeDerivative : public ADVectorTimeKernel<compute_stage>
+{
+public:
+  ADVectorTimeDerivative(const InputParameters & parameters);
+
+protected:
+  virtual ADResidual computeQpResidual() override;
+
+  usingVectorTimeKernelMembers;
+};
+
+#endif // ADVECTORTIMEDERIVATIVE_H
---
+++ framework/src/kernels/ADVectorTimeDerivative.C
@@ -0,0 +1,33 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "ADVectorTimeDerivative.h"
+
+registerADMooseObject("MooseApp", ADVectorTimeDerivative);
+
+defineADValidParams(
+    ADVectorTimeDerivative,
+    ADVectorTimeKernel,
+    params.addClassDescription("The time derivative operator with the weak form of $(\\psi_i, "
+                               "\\frac{\\partial u_h}{\\partial t})$."););
+
+template <ComputeStage compute_stage>
+ADVectorTimeDerivative<compute_stage>::ADVectorTimeDerivative(const InputParameters & parameters)
+  : ADVectorTimeKernel<compute_stage>(parameters)
+{
+}
+
+template <ComputeStage compute_stage>
+ADResidual
+ADVectorTimeDerivative<compute_stage>::computeQpResidual()
+{
+  return _test[_i][_qp] * _u_dot[_qp];
+}
+
+adBaseClass(ADVectorTimeDerivative);
