0d29ca0a1f6707a3a60f1724ece2e0e7e743e575
Chris Green <chris.green@csiro.au>
2019-03-26 14:28:14 +1100

    Add base class for single component fluid states
    Refs #13108

---
+++ modules/porous_flow/src/userobjects/PorousFlowFluidStateSingleComponentBase.C
@@ -0,0 +1,25 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "PorousFlowFluidStateSingleComponentBase.h"
+
+template <>
+InputParameters
+validParams<PorousFlowFluidStateSingleComponentBase>()
+{
+  InputParameters params = validParams<PorousFlowFluidStateBase>();
+  params.addClassDescription("Base class for single component fluid state classes");
+  return params;
+}
+
+PorousFlowFluidStateSingleComponentBase::PorousFlowFluidStateSingleComponentBase(
+    const InputParameters & parameters)
+  : PorousFlowFluidStateBase(parameters), _pidx(0), _hidx(1), dT(1.0e-6)
+{
+}
