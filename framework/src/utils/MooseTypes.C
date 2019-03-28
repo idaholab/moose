9620cea0faf64bb9b17a7042e65bca4396a4d468
Daniel Schwen <daniel.schwen@inl.gov>
2019-03-28 17:12:12 -0600

    Explicit instantiation of DualReal DenseMatrix (#13146)

--- framework/src/utils/MooseTypes.C
+++ framework/src/utils/MooseTypes.C
@@ -9,6 +9,10 @@
 
 #include "MooseTypes.h"
 #include "libmesh/elem.h"
+#include "libmesh/dense_matrix_base_impl.h"
+#include "libmesh/dense_matrix_impl.h"
+#include "metaphysicl/numberarray.h"
+#include "metaphysicl/dualnumber.h"
 
 namespace Moose
 {
@@ -16,4 +20,14 @@ const SubdomainID ANY_BLOCK_ID = libMesh::Elem::invalid_subdomain_id - 1;
 const SubdomainID INVALID_BLOCK_ID = libMesh::Elem::invalid_subdomain_id;
 const BoundaryID ANY_BOUNDARY_ID = static_cast<BoundaryID>(-1);
 const BoundaryID INVALID_BOUNDARY_ID = libMesh::BoundaryInfo::invalid_id;
+}
+
+// bare bones explicit instantiations for DenseMatrix<DualReal>
+namespace libMesh
+{
+template class DenseMatrixBase<DualReal>;
+template class DenseMatrix<DualReal>;
+template void
+DenseMatrix<Real>::vector_mult(DenseVector<CompareTypes<Real, DualReal>::supertype> & dest,
+                               const DenseVector<DualReal> & arg) const;
 }
