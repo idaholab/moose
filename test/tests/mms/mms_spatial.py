df8b76789d7ad554390bbafc433512e09c2dd904
Andrew E Slaughter <andrew.slaughter@inl.gov>
2019-04-10 09:14:07 -0600

    Convert mms functions to be DataFrame based

--- test/tests/mms/mms_spatial.py
+++ test/tests/mms/mms_spatial.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 import mms
-x, y = mms.run_spatial('mms_spatial.i', 4, console=False)
-x2, y2 = mms.run_spatial('mms_spatial.i', 4, 'Mesh/second_order=true', 'Variables/u/order=SECOND',
+df1 = mms.run_spatial('mms_spatial.i', 4, console=False)
+df2 = mms.run_spatial('mms_spatial.i', 4, 'Mesh/second_order=true', 'Variables/u/order=SECOND',
                           console=False)
 
 fig = mms.ConvergencePlot(xlabel='Element Size ($h$)', ylabel='$L_2$ Error')
-fig.plot(x, y, label='1st Order', marker='o', markersize=8)
-fig.plot(x2, y2, label='2nd Order', marker='o', markersize=8)
+fig.plot(df1, label='1st Order', marker='o', markersize=8)
+fig.plot(df2, label='2nd Order', marker='o', markersize=8)
 fig.save('mms_spatial.pdf')
+
+#TESTING (leave this comment, it is used in doco to remove the following from a demo)
+df1.to_csv('mms_spatial_first.csv')
+df2.to_csv('mms_spatial_second.csv')
--- test/tests/mms/mms_temporal.py
+++ test/tests/mms/mms_temporal.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 import mms
-x, y = mms.run_temporal('mms_temporal.i', 4, console=False)
-x2, y2 = mms.run_temporal('mms_temporal.i', 4, 'Executioner/scheme=bdf2', console=False)
+df1 = mms.run_temporal('mms_temporal.i', 4, console=False)
+df2 = mms.run_temporal('mms_temporal.i', 4, 'Executioner/scheme=bdf2', console=False)
 
 fig = mms.ConvergencePlot(xlabel='$\Delta t$', ylabel='$L_2$ Error')
-fig.plot(x, y, label='Implicit Euler', marker='o', markersize=8)
-fig.plot(x2, y2, label='2nd Order Backward Difference', marker='o', markersize=8)
+fig.plot(df1, label='1st Order (Implicit Euler)', marker='o', markersize=8)
+fig.plot(df2, label='2nd Order (Backward Difference)', marker='o', markersize=8)
 fig.save('mms_temporal.pdf')
+
+#TESTING (leave this comment, it is used in doco to remove the following from a demo)
+df1.to_csv('mms_temporal_first.csv')
+df2.to_csv('mms_temporal_second.csv')
