39cde754b481685681f5eb4175773eadafd95980
Andrew E Slaughter <andrew.slaughter@inl.gov>
2019-04-16 11:28:46 -0600

    Add support for functions in mms utils
    (closes #13181)

--- python/mms/evaluate.py
+++ python/mms/evaluate.py
@@ -1,9 +1,9 @@
-from sympy import *
+from sympy import * # use star so all functions are available to supplied strings
 from sympy.vector import divergence, gradient, Vector, CoordSys3D
 from fparser import print_fparser
 from moosefunction import print_moose
 
-def evaluate(pde, soln, variable='u', scalars=set(), vectors=set()):
+def evaluate(pde, soln, variable='u', scalars=set(), vectors=set(), functions=set(), vectorfunctions=set()):
     """
     Function to evaluate PDEs for the method of manufactured solutions forcing functions.
 
@@ -11,9 +11,11 @@ def evaluate(pde, soln, variable='u', scalars=set(), vectors=set()):
         pde[str]: A string representation of your PDE, eg. 'diff(T,t) + div(grad(T))'
         soln[str]: The desired solution, e.g., 'cos(u*t*x)'
         variable[str]: The solution variable in the PDE (default: 'u')
-        scalars[list]: A list of strings of extra scalar variables, the symbols 'x', 'y', 'z', and
-                       't' are automatically defined
-        vectors
+        scalars[list]: A list of strings of constant extra scalar variables
+        vectors[list]: A list of strings of extra constant vector variables
+        functions[list]: A list of strings of scalar variables that are a function of x,y,z, and t.
+        vector_functions[list]: A list of strings of generic vector variables that are a function of
+                                x,y,z, and t.
 
     Example:
         import mms
@@ -29,17 +31,30 @@ def evaluate(pde, soln, variable='u', scalars=set(), vectors=set()):
     z = R.z
     t = Symbol('t')
 
-    for v in vectors:
-        for c in 'xyz':
-            s = '{}_{}'.format(v,c)
-            locals()[s] = Symbol(s)
-        locals()[v] = locals()['{}_x'.format(v)]*R.i + \
-                      locals()['{}_y'.format(v)]*R.j + \
-                      locals()['{}_z'.format(v)]*R.k
+    # Define extra vectors, use _v_ in order to not collide with vector=['v']
+    for _v_ in vectors:
+        for _c_ in 'xyz':
+            _s_ = '{}_{}'.format(_v_, _c_)
+            locals()[_s_] = Symbol(_s_)
+        locals()[_v_] = locals()['{}_x'.format(_v_)]*R.i + \
+                        locals()['{}_y'.format(_v_)]*R.j + \
+                        locals()['{}_z'.format(_v_)]*R.k
 
-    # Define extra symbols
-    for s in scalars:
-        locals()[s] = Symbol(s)
+    # Define extra scalars
+    for _s_ in scalars:
+        locals()[_s_] = Symbol(_s_)
+
+    # Define extra functions
+    for _f_ in functions:
+        locals()[_f_] = Function(_f_)(x, y, z, t)
+
+    for _vf_ in vectorfunctions:
+        for _c_ in 'xyz':
+            _s_ = '{}_{}'.format(_vf_, _c_)
+            locals()[_s_] = Function(_s_)(x, y, z, t)
+        locals()[_vf_] = locals()['{}_x'.format(_vf_)]*R.i + \
+                         locals()['{}_y'.format(_vf_)]*R.j + \
+                         locals()['{}_z'.format(_vf_)]*R.k
 
     # Evaluate the solution
     locals()[variable] = eval(soln)
@@ -48,9 +63,3 @@ def evaluate(pde, soln, variable='u', scalars=set(), vectors=set()):
     pde = pde.replace('grad', 'gradient')
     pde = pde.replace('div', 'divergence')
     return eval(pde), locals()[variable]
-
-
-if __name__ == '__main__':
-    f = evaluate('diff(h, t) + div(u*h)', 'cos(x*y*t)', variable='h', vectors='u')
-    #print_moose(f)
-    print_fparser(f)
--- python/mms/test/test_mms.py
+++ python/mms/test/test_mms.py
@@ -26,6 +26,22 @@ class TestMMS(unittest.TestCase):
                             '- std::pow(p(1), 2)*_r*std::pow(t, 2)*std::cos(p(0)*p(1)*t) - ' \
                             'p(1)*t*_u(0)*std::sin(p(0)*p(1)*t)')
 
+    def testEvaluateWithScalarFunction(self):
+        f, _ = mms.evaluate('diff(h*u, t)', 'cos(x*t)', functions=['h'])
+        s = mms.fparser(f)
+
+        self.assertEqual(s, '-x*h(R.x, R.y, R.z, t)*sin(x*t) + ' \
+                            'cos(x*t)*Derivative(h(R.x, R.y, R.z, t), t)')
+
+    def testEvaluateWithVectorFunction(self):
+        f, _ = mms.evaluate('div(h*u)', 'cos(x*t)', vectorfunctions=['h'])
+        s = mms.fparser(f)
+
+        self.assertEqual(s, '-t*h_x(R.x, R.y, R.z, t)*sin(x*t) + ' \
+                            'cos(x*t)*Derivative(h_x(R.x, R.y, R.z, t), R.x) + ' \
+                            'cos(x*t)*Derivative(h_y(R.x, R.y, R.z, t), R.y) + ' \
+                            'cos(x*t)*Derivative(h_z(R.x, R.y, R.z, t), R.z)')
+
     def testHit(self):
         f,s = mms.evaluate('a*div(k*grad(u))', 'x**3', scalars=['k', 'a'])
         n = str(mms.build_hit(f, 'force', a=42))
