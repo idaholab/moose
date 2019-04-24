bd600bfe55eef54b78b8dc163b7926c7fbc7485a
Joshua Hansel <joshua.hansel@inl.gov>
2019-04-24 09:48:27 -0600

    Added a control to enable/disable objects based on a function value
    Closes #13295

---
+++ framework/doc/content/source/controls/ConditionalFunctionEnableControl.md
@@ -0,0 +1,14 @@
+# ConditionalFunctionEnableControl
+
+!syntax description /Controls/ConditionalFunctionEnableControl
+
+`ConditionalFunctionEnableControl` objects derive from [ConditionalEnableControl](/ConditionalEnableControl.md).
+
+`ConditionalFunctionEnableControl` objects allow MOOSE objects to be enabled or
+disabled according to the value of a [Function](syntax/Functions/index.md).
+
+!syntax parameters /Controls/ConditionalFunctionEnableControl
+
+!syntax inputs /Controls/ConditionalFunctionEnableControl
+
+!syntax children /Controls/ConditionalFunctionEnableControl
---
+++ framework/include/controls/ConditionalFunctionEnableControl.h
@@ -0,0 +1,37 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifndef CONDITIONALFUNCTIONENABLECONTROL_H
+#define CONDITIONALFUNCTIONENABLECONTROL_H
+
+#include "ConditionalEnableControl.h"
+
+class ConditionalFunctionEnableControl;
+class Function;
+
+template <>
+InputParameters validParams<ConditionalFunctionEnableControl>();
+
+/**
+ * Control for enabling/disabling objects when a function value is true
+ */
+class ConditionalFunctionEnableControl : public ConditionalEnableControl
+{
+public:
+  ConditionalFunctionEnableControl(const InputParameters & parameters);
+
+protected:
+  virtual bool conditionMet(const unsigned int & i) override;
+
+private:
+  /// The function to give a true or false value
+  Function & _function;
+};
+
+#endif // CONDITIONALFUNCTIONENABLECONTROL_H
