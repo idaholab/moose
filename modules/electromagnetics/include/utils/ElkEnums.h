cf5b91468c4f50de04ff5f0b19fcfa84fed11733
Casey Icenhour <cticenho@ncsu.edu>
2019-04-25 14:16:11 -0600

    Create ElkEnums header file

---
+++ modules/electromagnetics/include/utils/ElkEnums.h
@@ -0,0 +1,17 @@
+#ifndef ELKENUMS_H
+#define ELKENUMS_H
+
+/** ElkEnums contains various enumerations useful in ELK, such as real/imag component definitions in
+ * Kernels, BCs, etc.
+ */
+
+namespace elk
+{
+enum ComponentEnum
+{
+  REAL,
+  IMAGINARY
+};
+} // namespace elk
+
+#endif // ELKENUMS_H
