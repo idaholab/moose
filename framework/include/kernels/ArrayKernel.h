427680fc899604e39012c75bea966b308dffd6ce
Yaqi Wang <Yaqi.Wang@inl.gov>
2019-05-04 18:14:45 -0600

    add array kernel

---
+++ framework/include/kernels/ArrayKernel.h
@@ -0,0 +1,117 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "KernelBase.h"
+#include "MooseVariableInterface.h"
+
+class ArrayKernel;
+
+template <>
+InputParameters validParams<ArrayKernel>();
+
+class ArrayKernel : public KernelBase, public MooseVariableInterface<RealArrayValue>
+{
+public:
+  ArrayKernel(const InputParameters & parameters);
+
+  /// Compute this ArrayKernel's contribution to the residual
+  virtual void computeResidual() override;
+
+  /// Compute this ArrayKernel's contribution to the diagonal Jacobian entries
+  virtual void computeJacobian() override;
+
+  /// Computes full Jacobian of jvar and the array variable this kernel operates on
+  virtual void computeOffDiagJacobian(MooseVariableFEBase & jvar) override;
+
+  /**
+   * Computes jacobian block with respect to a scalar variable
+   * @param jvar The number of the scalar variable
+   */
+  virtual void computeOffDiagJacobianScalar(unsigned int jvar) override;
+
+  virtual ArrayMooseVariable & variable() override { return _var; }
+
+protected:
+  /**
+   * Compute this Kernel's contribution to the residual at the current quadrature point
+   */
+  virtual RealArrayValue computeQpResidual() = 0;
+
+  /**
+   * Compute this Kernel's contribution to the diagonal Jacobian at the current quadrature point
+   */
+  virtual RealArrayValue computeQpJacobian() { return RealArrayValue(_var.count()); }
+
+  /**
+   * This is the virtual that derived classes should override for computing a full Jacobian component
+   */
+  virtual RealArray computeQpOffDiagJacobian(MooseVariableFEBase & jvar);
+
+  /**
+   * This is the virtual that derived classes should override for computing a full Jacobian component
+   */
+  virtual RealArray computeQpOffDiagJacobianScalar(MooseVariableScalar & jvar);
+
+  /// for array kernel
+  void saveLocalArrayResidual(DenseVector<Number> & re,
+                              unsigned int i,
+                              unsigned int ntest,
+                              const RealArrayValue & v)
+  {
+    for (unsigned int j = 0; j < v.size(); ++j, i += ntest)
+      re(i) += v(j);
+  }
+  void saveDiagLocalArrayJacobian(DenseMatrix<Number> & ke,
+                                  unsigned int i,
+                                  unsigned int ntest,
+                                  unsigned int j,
+                                  unsigned int nphi,
+                                  const RealArrayValue & v)
+  {
+    for (unsigned int k = 0; k < v.size(); ++k, i += ntest, j += nphi)
+      ke(i, j) += v(k);
+  }
+  void saveFullLocalArrayJacobian(DenseMatrix<Number> & ke,
+                                  unsigned int i,
+                                  unsigned int ntest,
+                                  unsigned int j,
+                                  unsigned int nphi,
+                                  const RealArray & v)
+  {
+    for (unsigned int k = 0; k < v.rows(); ++k, i += ntest)
+      for (unsigned int l = 0; l < v.cols(); ++l, j += nphi)
+        ke(i, j) += v(k, l);
+  }
+
+  /// This is an array kernel so we cast to a ArrayMooseVariable
+  ArrayMooseVariable & _var;
+
+  /// the current test function
+  const ArrayVariableTestValue & _test;
+
+  /// gradient of the test function
+  const ArrayVariableTestGradient & _grad_test;
+
+  /// the current shape functions
+  const ArrayVariablePhiValue & _phi;
+
+  /// gradient of the shape function
+  const ArrayVariablePhiGradient & _grad_phi;
+
+  /// Holds the solution at current quadrature points
+  const ArrayVariableValue & _u;
+
+  /// Holds the solution gradient at current quadrature points
+  const ArrayVariableGradient & _grad_u;
+
+  /// Number of components of the array variable
+  const unsigned int _count;
+};
---
+++ framework/src/kernels/ArrayKernel.C
@@ -0,0 +1,176 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "ArrayKernel.h"
+
+#include "Assembly.h"
+#include "MooseVariableFE.h"
+#include "MooseVariableScalar.h"
+#include "SubProblem.h"
+#include "NonlinearSystem.h"
+
+#include "libmesh/threads.h"
+#include "libmesh/quadrature.h"
+
+template <>
+InputParameters
+validParams<ArrayKernel>()
+{
+  InputParameters params = validParams<KernelBase>();
+  params.registerBase("ArrayKernel");
+  return params;
+}
+
+ArrayKernel::ArrayKernel(const InputParameters & parameters)
+  : KernelBase(parameters),
+    MooseVariableInterface<RealArrayValue>(this,
+                                           false,
+                                           "variable",
+                                           Moose::VarKindType::VAR_NONLINEAR,
+                                           Moose::VarFieldType::VAR_FIELD_ARRAY),
+    _var(*mooseVariable()),
+    _test(_var.phi()),
+    _grad_test(_var.gradPhi()),
+    _phi(_assembly.phi(_var)),
+    _grad_phi(_assembly.gradPhi(_var)),
+    _u(_is_implicit ? _var.sln() : _var.slnOld()),
+    _grad_u(_is_implicit ? _var.gradSln() : _var.gradSlnOld()),
+    _count(_var.count())
+{
+  addMooseVariableDependency(mooseVariable());
+}
+
+void
+ArrayKernel::computeResidual()
+{
+  prepareVectorTag(_assembly, _var.number());
+
+  precalculateResidual();
+  for (_i = 0; _i < _test.size(); _i++)
+    for (_qp = 0; _qp < _qrule->n_points(); _qp++)
+    {
+      RealArrayValue residual = _JxW[_qp] * _coord[_qp] * computeQpResidual();
+      mooseAssert(residual.size() == _count,
+                  "Size of local residual is not equal to the number of array variable compoments");
+      saveLocalArrayResidual(_local_re, _i, _test.size(), residual);
+    }
+
+  accumulateTaggedLocalResidual();
+
+  if (_has_save_in)
+  {
+    Threads::spin_mutex::scoped_lock lock(Threads::spin_mtx);
+    for (const auto & var : _save_in)
+    {
+      auto * avar = dynamic_cast<ArrayMooseVariable *>(var);
+      if (avar)
+        avar->saveDoFValues(_local_re);
+      else
+        mooseError("");
+    }
+  }
+}
+
+void
+ArrayKernel::computeJacobian()
+{
+  prepareMatrixTag(_assembly, _var.number(), _var.number());
+
+  precalculateJacobian();
+  for (_i = 0; _i < _test.size(); _i++)
+    for (_j = 0; _j < _phi.size(); _j++)
+      for (_qp = 0; _qp < _qrule->n_points(); _qp++)
+      {
+        RealArrayValue v = _JxW[_qp] * _coord[_qp] * computeQpJacobian();
+        saveDiagLocalArrayJacobian(_local_ke, _i, _test.size(), _j, _phi.size(), v);
+      }
+
+  accumulateTaggedLocalMatrix();
+
+  if (_has_diag_save_in)
+  {
+    unsigned int rows = _local_ke.m();
+    DenseVector<Real> diag(rows);
+    for (unsigned int i = 0; i < rows; i++)
+      diag(i) = _local_ke(i, i);
+
+    Threads::spin_mutex::scoped_lock lock(Threads::spin_mtx);
+    for (const auto & var : _diag_save_in)
+    {
+      auto * avar = dynamic_cast<ArrayMooseVariable *>(var);
+      if (avar)
+        avar->saveDoFValues(diag);
+      else
+        mooseError("");
+    }
+  }
+}
+
+void
+ArrayKernel::computeOffDiagJacobian(MooseVariableFEBase & jvar)
+{
+  prepareMatrixTag(_assembly, _var.number(), jvar.number());
+
+  precalculateOffDiagJacobian(jvar.number());
+  for (_i = 0; _i < _test.size(); _i++)
+    for (_j = 0; _j < jvar.phiSize(); _j++)
+      for (_qp = 0; _qp < _qrule->n_points(); _qp++)
+      {
+        RealArray v = _JxW[_qp] * _coord[_qp] * computeQpOffDiagJacobian(jvar);
+        saveFullLocalArrayJacobian(_local_ke, _i, _test.size(), _j, jvar.phiSize(), v);
+      }
+
+  accumulateTaggedLocalMatrix();
+
+  if (_has_diag_save_in && jvar.number() == _var.number())
+  {
+    unsigned int rows = _local_ke.m();
+    DenseVector<Real> diag(rows);
+    for (unsigned int i = 0; i < rows; i++)
+      diag(i) = _local_ke(i, i);
+
+    Threads::spin_mutex::scoped_lock lock(Threads::spin_mtx);
+    for (const auto & var : _diag_save_in)
+    {
+      auto * avar = dynamic_cast<ArrayMooseVariable *>(var);
+      if (avar)
+        avar->saveDoFValues(diag);
+      else
+        mooseError("");
+    }
+  }
+}
+
+void
+ArrayKernel::computeOffDiagJacobianScalar(unsigned int jvar)
+{
+  MooseVariableScalar & jv = _sys.getScalarVariable(_tid, jvar);
+  prepareMatrixTag(_assembly, _var.number(), jvar);
+
+  for (_i = 0; _i < _test.size(); _i++)
+    for (_qp = 0; _qp < _qrule->n_points(); _qp++)
+    {
+      RealArray v = _JxW[_qp] * _coord[_qp] * computeQpOffDiagJacobianScalar(jv);
+      saveFullLocalArrayJacobian(_local_ke, _i, _test.size(), 0, 1, v);
+    }
+
+  accumulateTaggedLocalMatrix();
+}
+
+RealArray
+ArrayKernel::computeQpOffDiagJacobian(MooseVariableFEBase & jvar)
+{
+  return RealArray(_var.count(), jvar.count());
+}
+
+RealArray
+ArrayKernel::computeQpOffDiagJacobianScalar(MooseVariableScalar & jvar)
+{
+  return RealArray(_var.count(), (unsigned int) jvar.order() + 1);
+}
