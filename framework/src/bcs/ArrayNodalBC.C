9bd3a0e01e0594f8ea91239f63825a6e707ad802
Yaqi Wang <Yaqi.Wang@inl.gov>
2019-05-06 10:52:48 -0600

    add array diffusion kernel, array dirichlet bc and array material

---
+++ framework/src/bcs/ArrayNodalBC.C
@@ -0,0 +1,102 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "ArrayNodalBC.h"
+
+#include "Assembly.h"
+#include "MooseVariableFE.h"
+#include "SystemBase.h"
+#include "NonlinearSystemBase.h"
+
+template <>
+InputParameters
+validParams<ArrayNodalBC>()
+{
+  return validParams<NodalBCBase>();
+}
+
+ArrayNodalBC::ArrayNodalBC(const InputParameters & parameters)
+  : NodalBCBase(parameters),
+    MooseVariableInterface<RealArrayValue>(this,
+                                           true,
+                                           "variable",
+                                           Moose::VarKindType::VAR_NONLINEAR,
+                                           Moose::VarFieldType::VAR_FIELD_ARRAY),
+    _var(*mooseVariable()),
+    _current_node(_var.node()),
+    _u(_var.nodalValue())
+{
+  addMooseVariableDependency(mooseVariable());
+}
+
+void
+ArrayNodalBC::computeResidual()
+{
+  if (_var.isNodalDefined())
+  {
+    RealArrayValue res = computeQpResidual();
+
+    for (auto tag_id : _vector_tags)
+      if (_sys.hasVector(tag_id))
+        _var.insertNodalValue(_sys.getVector(tag_id), res);
+  }
+}
+
+void
+ArrayNodalBC::computeJacobian()
+{
+  if (_var.isNodalDefined())
+  {
+    RealArrayValue cached_val = computeQpJacobian();
+
+    dof_id_type cached_row = _var.nodalDofIndex();
+
+    // Cache the user's computeQpJacobian() value for later use.
+    for (auto tag : _matrix_tags)
+      if (_sys.hasMatrix(tag))
+        for (unsigned int i = 0; i < _var.count(); ++i)
+          _fe_problem.assembly(0).cacheJacobianContribution(cached_row + i, cached_row + i,
+                                                            cached_val(i), tag);
+  }
+}
+
+void
+ArrayNodalBC::computeOffDiagJacobian(unsigned int jvar)
+{
+  MooseVariableFEBase & jv = _sys.getVariable(0, jvar);
+
+  RealArray cached_val = computeQpOffDiagJacobian(jv);
+
+  dof_id_type cached_row = _var.nodalDofIndex();
+  // Note: this only works for Lagrange variables...
+  dof_id_type cached_col = _current_node->dof_number(_sys.number(), jvar, 0);
+
+  // Cache the user's computeQpJacobian() value for later use.
+  for (auto tag : _matrix_tags)
+    if (_sys.hasMatrix(tag))
+      for (unsigned int i = 0; i < _var.count(); ++i)
+        for (unsigned int j = 0; j < jv.count(); ++j)
+          _fe_problem.assembly(0).cacheJacobianContribution(
+            cached_row + i, cached_col + j, cached_val(i, j), tag);
+}
+
+RealArrayValue
+ArrayNodalBC::computeQpJacobian()
+{
+  return RealArrayValue::Ones(_var.count());;
+}
+
+RealArray
+ArrayNodalBC::computeQpOffDiagJacobian(MooseVariableFEBase & jvar)
+{
+  if (jvar.number() == _var.number())
+    return RealArray::Identity(_var.count(), jvar.count());
+  else
+    return RealArray::Zero(_var.count(), jvar.count());
+}
