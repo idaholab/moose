6d29b236f32b52c4c9fdba95fb2f9c13af6d116b
Yaqi Wang <Yaqi.Wang@inl.gov>
2019-05-07 13:07:02 -0600

    create an aux kernel to extract a component of an array variable

---
+++ framework/include/auxkernels/ArrayVariableComponent.h
@@ -0,0 +1,31 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "AuxKernel.h"
+
+class ArrayVariableComponent;
+
+template <>
+InputParameters validParams<ArrayVariableComponent>();
+
+class ArrayVariableComponent : public AuxKernel
+{
+public:
+  ArrayVariableComponent(const InputParameters & parameters);
+
+protected:
+  virtual Real computeValue() override;
+
+  /// Holds the solution at current quadrature points
+  const ArrayVariableValue & _u;
+  /// The component
+  const unsigned int _component;
+};
