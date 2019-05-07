839bf8254c1f7a613bce11feeef38ee85a2f7f29
Yaqi Wang <Yaqi.Wang@inl.gov>
2019-05-07 12:36:04 -0600

    add a material for evaluating constant property in type of Eigen matrix

---
+++ framework/doc/content/source/materials/GenericConstant2DArray.md
@@ -0,0 +1,16 @@
+# GenericConstant2DArray
+
+!alert construction title=Undocumented Class
+The GenericConstant2DArray has not been documented. The content contained on this page includes the
+typical automatic documentation associated with a MooseObject; however, what is contained is
+ultimately determined by what is necessary to make the documentation clear for users.
+
+!syntax description /Materials/GenericConstant2DArray
+
+!syntax parameters /Materials/GenericConstant2DArray
+
+!syntax inputs /Materials/GenericConstant2DArray
+
+!syntax children /Materials/GenericConstant2DArray
+
+!bibtex bibliography
---
+++ framework/include/materials/GenericConstant2DArray.h
@@ -0,0 +1,33 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "Material.h"
+
+// Forward Declarations
+class GenericConstant2DArray;
+
+template <>
+InputParameters validParams<GenericConstant2DArray>();
+
+class GenericConstant2DArray : public Material
+{
+public:
+  GenericConstant2DArray(const InputParameters & parameters);
+
+protected:
+  virtual void initQpStatefulProperties() override;
+  virtual void computeQpProperties() override;
+
+  std::string _prop_name;
+  const RealArray & _prop_value;
+
+  MaterialProperty<RealArray> & _property;
+};
