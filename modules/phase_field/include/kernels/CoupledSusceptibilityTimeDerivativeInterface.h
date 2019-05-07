b2ea609f53f89376c80005054f8474cc84b6aa62
Sudipta Biswas <biswas7@purdue.edu>
2019-05-07 15:37:50 -0600

    Add CoupledSusceptibilityTimeDerivativeInterface kernel (#13373)

---
+++ modules/phase_field/include/kernels/CoupledSusceptibilityTimeDerivativeInterface.h
@@ -0,0 +1,35 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "CoupledSusceptibilityTimeDerivative.h"
+
+// Forward Declaration
+class CoupledSusceptibilityTimeDerivativeInterface;
+
+template <>
+InputParameters validParams<CoupledSusceptibilityTimeDerivativeInterface>();
+
+/**
+ * This calculates a modified coupled time derivative that multiplies the time derivative of a
+ * coupled variable by a function of the variables and interface normal
+ */
+class CoupledSusceptibilityTimeDerivativeInterface : public CoupledSusceptibilityTimeDerivative
+{
+public:
+  CoupledSusceptibilityTimeDerivativeInterface(const InputParameters & parameters);
+
+protected:
+  virtual Real computeQpResidual();
+  virtual Real computeQpJacobian();
+  virtual Real computeQpOffDiagJacobian(unsigned int jvar);
+
+  const VariableGradient & _grad_v;
+};
---
+++ modules/phase_field/src/kernels/CoupledSusceptibilityTimeDerivativeInterface.C
@@ -0,0 +1,74 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "CoupledSusceptibilityTimeDerivativeInterface.h"
+
+registerMooseObject("PhaseFieldApp", CoupledSusceptibilityTimeDerivativeInterface);
+
+template <>
+InputParameters
+validParams<CoupledSusceptibilityTimeDerivativeInterface>()
+{
+  InputParameters params = validParams<CoupledSusceptibilityTimeDerivative>();
+  params.addClassDescription(
+      "A modified coupled time derivative Kernel that multiplies the time "
+      "derivative of a coupled variable by a generalized susceptibility and interface normal");
+  return params;
+}
+
+CoupledSusceptibilityTimeDerivativeInterface::CoupledSusceptibilityTimeDerivativeInterface(
+    const InputParameters & parameters)
+  : CoupledSusceptibilityTimeDerivative(parameters), _grad_v(coupledGradient("v"))
+{
+}
+
+Real
+CoupledSusceptibilityTimeDerivativeInterface::computeQpResidual()
+{
+  if (_grad_v[_qp].norm() > 1.0e-6)
+    return _F[_qp] * _v_dot[_qp] * _grad_v[_qp] * _grad_test[_i][_qp] / _grad_v[_qp].norm();
+  else
+    return 0.0;
+}
+
+Real
+CoupledSusceptibilityTimeDerivativeInterface::computeQpJacobian()
+{
+  if (_grad_v[_qp].norm() > 1.0e-6)
+    return _dFdu[_qp] * _v_dot[_qp] * _grad_v[_qp] * _grad_test[_i][_qp] * _phi[_j][_qp] /
+           _grad_v[_qp].norm();
+  else
+    return 0.0;
+}
+
+Real
+CoupledSusceptibilityTimeDerivativeInterface::computeQpOffDiagJacobian(unsigned int jvar)
+{
+  // get the coupled variable jvar is referring to
+  const unsigned int cvar = mapJvarToCvar(jvar);
+
+  if (_grad_v[_qp].norm() > 1.0e-6)
+  {
+    if (jvar == _v_var)
+      return _F[_qp] * _dv_dot[_qp] * _grad_v[_qp] * _grad_test[_i][_qp] * _phi[_j][_qp] /
+                 _grad_v[_qp].norm() +
+             _F[_qp] * _v_dot[_qp] * _grad_test[_i][_qp] * _grad_phi[_j][_qp] /
+                 _grad_v[_qp].norm() -
+             _F[_qp] * _v_dot[_qp] * _grad_v[_qp] * _grad_test[_i][_qp] * _grad_v[_qp] *
+                 _grad_phi[_j][_qp] /
+                 (_grad_v[_qp].norm() * _grad_v[_qp].norm() * _grad_v[_qp].norm()) +
+             _v_dot[_qp] * _grad_v[_qp] * _grad_test[_i][_qp] * _phi[_j][_qp] *
+                 (*_dFdarg[cvar])[_qp] / _grad_v[_qp].norm();
+    else
+      return _v_dot[_qp] * _grad_v[_qp] * _grad_test[_i][_qp] * _phi[_j][_qp] *
+             (*_dFdarg[cvar])[_qp] / _grad_v[_qp].norm();
+  }
+  else
+    return 0.0;
+}
