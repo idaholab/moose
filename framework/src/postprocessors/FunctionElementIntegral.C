37c4341a7f11523b325d00a988847eda0bf807ec
Joshua Hansel <joshua.hansel@inl.gov>
2019-05-15 09:27:49 -0600

    Added post-processor to compute element integral of function
    Closes #13418

---
+++ framework/src/postprocessors/FunctionElementIntegral.C
@@ -0,0 +1,37 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "FunctionElementIntegral.h"
+#include "Function.h"
+
+registerMooseObject("MooseApp", FunctionElementIntegral);
+
+template <>
+InputParameters
+validParams<FunctionElementIntegral>()
+{
+  InputParameters params = validParams<ElementIntegralPostprocessor>();
+
+  params.addRequiredParam<FunctionName>("function", "Name of function to integrate");
+
+  params.addClassDescription("Integrates a function over elements");
+
+  return params;
+}
+
+FunctionElementIntegral::FunctionElementIntegral(const InputParameters & parameters)
+  : ElementIntegralPostprocessor(parameters), _function(getFunction("function"))
+{
+}
+
+Real
+FunctionElementIntegral::computeQpIntegral()
+{
+  return _function.value(_t, _q_point[_qp]);
+}
