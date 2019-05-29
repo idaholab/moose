61009238ef308de56b042dc10e9e92dc19d11b12
Andrew E Slaughter <andrew.slaughter@inl.gov>
2019-05-29 15:33:07 -0600

    Remove const Function from unit test

--- modules/thm/unit/include/CircularAreaHydraulicDiameterFunctionTest.h
+++ modules/thm/unit/include/CircularAreaHydraulicDiameterFunctionTest.h
@@ -23,7 +23,7 @@ protected:
       _fe_problem->addFunction(class_name, A_name, params);
     }
 
-    const Function & A_fn = _fe_problem->getFunction(A_name);
+    Function & A_fn = _fe_problem->getFunction(A_name);
     A_fn.initialSetup();
 
     // hydraulic diameter
