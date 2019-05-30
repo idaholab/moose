7e38b7e259ef5f18028f351da31d88bcccf0609d
Md Mohaiminul Islam <mishkat1096@gmail.com>
2019-05-30 15:13:59 -0600

    add block based rotated elasticity tensor (#13496)

---
+++ modules/tensor_mechanics/include/materials/ComputeBlockRotatedElasticityTensor.h
@@ -0,0 +1,39 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "ComputeElasticityTensorBase.h"
+#include "GrainDataTracker.h"
+#include "RankFourTensor.h"
+
+class ComputeBlockRotatedElasticityTensor;
+class EulerAngleProvider;
+
+template <>
+InputParameters validParams<ComputeBlockRotatedElasticityTensor>();
+
+/**
+ * ComputeBlockRotatedElasticityTensor defines an elasticity tensor material object with a given
+ * base name.
+ */
+class ComputeBlockRotatedElasticityTensor : public ComputeElasticityTensorBase
+{
+public:
+  ComputeBlockRotatedElasticityTensor(const InputParameters & parameters);
+
+protected:
+  virtual void computeQpElasticityTensor();
+
+  /// unrotated elasticity tensor
+  RankFourTensor _C_ijkl;
+
+  /// object providing the Euler angles
+  const EulerAngleProvider & _euler;
+};
