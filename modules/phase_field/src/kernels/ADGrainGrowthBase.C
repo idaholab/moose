1a64b14811e94174b75ad22fb97b59ff701b94bc
Daniel Schwen <daniel.schwen@inl.gov>
2019-06-06 11:25:48 -0600

    Grain growth with AD (#13539)

---
+++ modules/phase_field/src/kernels/ADGrainGrowthBase.C
@@ -0,0 +1,29 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "ADGrainGrowthBase.h"
+
+defineADValidParams(ADGrainGrowthBase,
+                    ADAllenCahnBase,
+                    params.addRequiredCoupledVar(
+                        "v", "Array of coupled order parameter names for other order parameters"););
+
+template <ComputeStage compute_stage>
+ADGrainGrowthBase<compute_stage>::ADGrainGrowthBase(const InputParameters & parameters)
+  : ADAllenCahnBase<compute_stage, Real>(parameters),
+    _op_num(coupledComponents("v")),
+    _vals(_op_num),
+    _mu(adGetADMaterialProperty<Real>("mu"))
+{
+  // Loop through grains and load coupled variables into the arrays
+  for (unsigned int i = 0; i < _op_num; ++i)
+    _vals[i] = &adCoupledValue("v", i);
+}
+
+adBaseClass(ADGrainGrowthBase);
