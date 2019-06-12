8da0c356fdb4b3d9656816a615e92075fee20a8e
Andrew E Slaughter <andrew.slaughter@inl.gov>
2019-06-12 13:44:28 -0600

    Remove symlinks
    (refs #4522)

--- tutorials/darcy_thermo_mech/step07_adaptivity/src/bcs/HeatConductionOutflow.C
+++ tutorials/darcy_thermo_mech/step07_adaptivity/src/bcs/HeatConductionOutflow.C
@@ -1,1 +1,30 @@
-../../../step06_coupled_darcy_heat_conduction/src/bcs/HeatConductionOutflow.C
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "HeatConductionOutflow.h"
+
+registerADMooseObject("DarcyThermoMechApp", HeatConductionOutflow);
+
+defineADValidParams(HeatConductionOutflow,
+                    ADIntegratedBC,
+                    params.addClassDescription("Compute the outflow boundary condition."););
+
+template <ComputeStage compute_stage>
+HeatConductionOutflow<compute_stage>::HeatConductionOutflow(const InputParameters & parameters)
+  : ADIntegratedBC<compute_stage>(parameters),
+    _thermal_conductivity(adGetMaterialProperty<Real>("thermal_conductivity"))
+{
+}
+
+template <ComputeStage compute_stage>
+ADResidual
+HeatConductionOutflow<compute_stage>::computeQpResidual()
+{
+  return -_test[_i][_qp] * _thermal_conductivity[_qp] * _grad_u[_qp] * _normals[_qp];
+}
