f5a781ab28de36538e1a564c780a532a4da1ee90
Yaqi Wang <Yaqi.Wang@inl.gov>
2019-06-18 09:54:54 -0600

    protect component param from being set to zero accidentally

--- framework/src/actions/AddVariableAction.C
+++ framework/src/actions/AddVariableAction.C
@@ -50,7 +50,8 @@ validParams<AddVariableAction>()
                              "Specifies the order of the FE shape function to use "
                              "for this variable (additional orders not listed are "
                              "allowed)");
-  params.addParam<unsigned int>("component", 1, "Number of component for an array variable");
+  params.addRangeCheckedParam<unsigned int>(
+      "component", 1, "component>0", "Number of component for an array variable");
   params.addParam<std::vector<Real>>("initial_condition",
                                      "Specifies the initial condition for this variable");
   params.addParam<std::vector<SubdomainName>>("block", "The block id where this variable lives");
