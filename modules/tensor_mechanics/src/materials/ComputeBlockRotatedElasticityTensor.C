fd8f57dc4fa54b391e8b41860c881fe46e4737c3
mishkat1096 <48925117+mishkat1096@users.noreply.github.com>
2019-06-24 17:12:25 -0600

    Update ComputeBlockRotatedElasticityTensor.C

--- modules/tensor_mechanics/src/materials/ComputeBlockRotatedElasticityTensor.C
+++ modules/tensor_mechanics/src/materials/ComputeBlockRotatedElasticityTensor.C
@@ -43,16 +43,15 @@ void
 ComputeBlockRotatedElasticityTensor::computeQpElasticityTensor()
 {
   EulerAngles angles;
-  auto grain_id = _current_elem->subdomain_id();
+  int grain_id = _current_elem->subdomain_id();
   grain_id = grain_id - _offset;
   if (grain_id < 0 || grain_id > _euler.getGrainNum())
-    mooseError("Please set correct offset value ");
-
-  if (grain_id < _euler.getGrainNum())
-    angles = _euler.getEulerAngles(grain_id);
-  else
-    mooseError(
-        "invalid block id ", grain_id, " (only have ", _euler.getGrainNum(), " Eulerangles)");
+    mooseError("Please set correct offset value, offseting block id starts from ..",
+               grain_id,
+               "..should start from 0 and have..",
+               _euler.getGrainNum(),
+               "..Eulerangles");
+  angles = _euler.getEulerAngles(grain_id);
 
   RankFourTensor C_ijkl = _C_ijkl;
   C_ijkl.rotate(RotationTensor(RealVectorValue(angles)));
