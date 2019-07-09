2c0c64fa25de2b017ab49ab756b989e7143cd76e
David Andrs <david.andrs@inl.gov>
2019-07-09 12:20:39 -0600

    Refactoring HeatFluxBCs
    Closes #19713

--- modules/thm/include/bcs/HeatFlux3EqnBC.h
+++ modules/thm/include/bcs/HeatFlux3EqnBC.h
@@ -1,48 +1,29 @@
 #pragma once
 
-#include "IntegratedBC.h"
+#include "HeatFluxBaseBC.h"
 
 class HeatFlux3EqnBC;
-class HeatFluxFromHeatStructure3EqnUserObject;
 
 template <>
 InputParameters validParams<HeatFlux3EqnBC>();
 
-class HeatFlux3EqnBC : public IntegratedBC
+class HeatFlux3EqnBC : public HeatFluxBaseBC
 {
 public:
   HeatFlux3EqnBC(const InputParameters & parameters);
 
-  virtual void computeJacobian() override;
-  virtual void computeJacobianBlock(unsigned jvar) override;
-  virtual void computeJacobianBlock(MooseVariableFEBase & jvar) override;
-
 protected:
+  virtual std::vector<unsigned int> getOffDiagVariableNumbers() override;
+
   virtual Real computeQpResidual() override;
   virtual Real computeQpJacobian() override;
-  virtual Real computeQpOffDiagJacobianNeighbor(unsigned int jvar);
+  virtual Real computeQpOffDiagJacobianNeighbor(unsigned int jvar) override;
 
   /**
    * Creates the mapping of coupled variable index to local equation system
    * index
    */
   std::map<unsigned int, unsigned int> getVariableIndexMapping() const;
-
-  /// shape function values (in QPs)
-  const VariablePhiValue & _phi_neighbor;
-
-  /// User object that computes the heat flux
-  const HeatFluxFromHeatStructure3EqnUserObject & _q_uo;
-  /// Perimeter of a single unit of heat structure
-  const Real _P_hs_unit;
-  /// Number of units of heat structure
-  const unsigned int _n_unit;
-  /// Is the heat structure coordinate system cylindrical?
-  const bool _hs_coord_system_is_cylindrical;
-  /// Coordinate transformation
-  const Real _hs_coord;
-  /// Factor by which to scale term on the flow channel side for the heat structure side
-  const Real _hs_scale;
 
   /// Flow channel rho*A coupled variable index
   const unsigned int _rhoA_jvar;
---
+++ modules/thm/src/bcs/HeatFluxBaseBC.C
@@ -0,0 +1,86 @@
+#include "HeatFluxBaseBC.h"
+#include "HeatFluxFromHeatStructureBaseUserObject.h"
+#include "THMIndices3Eqn.h"
+#include "Assembly.h"
+#include "NonlinearSystemBase.h"
+
+template <>
+InputParameters
+validParams<HeatFluxBaseBC>()
+{
+  InputParameters params = validParams<IntegratedBC>();
+  params.addRequiredParam<UserObjectName>(
+      "q_uo", "The name of the user object that computes the heat flux");
+  params.addRequiredParam<Real>("P_hs_unit", "Perimeter of a single unit of heat structure");
+  params.addRequiredParam<unsigned int>("n_unit", "Number of units of heat structure");
+  params.addRequiredParam<bool>("hs_coord_system_is_cylindrical",
+                                "Is the heat structure coordinate system cylindrical?");
+  return params;
+}
+
+HeatFluxBaseBC::HeatFluxBaseBC(const InputParameters & parameters)
+  : IntegratedBC(parameters),
+    _phi_neighbor(_assembly.phiNeighbor(_var)),
+    _q_uo(getUserObject<HeatFluxFromHeatStructureBaseUserObject>("q_uo")),
+    _P_hs_unit(getParam<Real>("P_hs_unit")),
+    _n_unit(getParam<unsigned int>("n_unit")),
+    _hs_coord_system_is_cylindrical(getParam<bool>("hs_coord_system_is_cylindrical")),
+    _hs_coord(_hs_coord_system_is_cylindrical ? _P_hs_unit : 1.0),
+    _hs_scale(-_hs_coord / (_n_unit * _P_hs_unit))
+{
+}
+
+void
+HeatFluxBaseBC::initialSetup()
+{
+  _off_diag_var_nums = getOffDiagVariableNumbers();
+}
+
+void
+HeatFluxBaseBC::computeJacobian()
+{
+  IntegratedBC::computeJacobian();
+}
+
+void
+HeatFluxBaseBC::computeJacobianBlock(unsigned jvar)
+{
+  IntegratedBC::computeJacobianBlock(jvar);
+}
+
+void
+HeatFluxBaseBC::computeJacobianBlock(MooseVariableFEBase & jvar)
+{
+  IntegratedBC::computeJacobianBlock(jvar);
+
+  if (jvar.number() == _var.number())
+  {
+    // when doing the diagonal part, also take care of the off-diag jacobian
+    // wrt the heat structure side
+    std::vector<dof_id_type> idofs = _var.dofIndices();
+
+    const dof_id_type & pipe_elem_id = _q_uo.getNearestElem(_current_elem->id());
+    const Elem * neighbor = _mesh.elemPtr(pipe_elem_id);
+
+    _assembly.setCurrentNeighborSubdomainID(neighbor->subdomain_id());
+    _assembly.reinitNeighborAtPhysical(neighbor, _q_point.stdVector());
+
+    for (std::size_t i = 0; i < _off_diag_var_nums.size(); i++)
+    {
+      unsigned int jvar_num = _off_diag_var_nums[i];
+      MooseVariableFEBase & jvar = _fe_problem.getNonlinearSystemBase().getVariable(_tid, jvar_num);
+      jvar.prepareNeighbor();
+      _assembly.copyNeighborShapes(jvar_num);
+
+      auto & jdofs = jvar.dofIndicesNeighbor();
+
+      DenseMatrix<Number> Ke(_test.size(), jvar.phiNeighborSize());
+      for (_qp = 0; _qp < _qrule->n_points(); _qp++)
+        for (_i = 0; _i < _test.size(); _i++)
+          for (_j = 0; _j < jvar.phiNeighborSize(); _j++)
+            Ke(_i, _j) += _JxW[_qp] * _coord[_qp] * computeQpOffDiagJacobianNeighbor(jvar_num);
+
+      _assembly.cacheJacobianBlock(Ke, idofs, jdofs, _var.scalingFactor());
+    }
+  }
+}
