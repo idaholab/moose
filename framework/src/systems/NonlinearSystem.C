b5ef2ee1aa97f61db11095056f6fc87f0a93e3eb
Alex Lindsay <alexlindsay239@gmail.com>
2019-07-17 15:17:40 -0700

    Really remove libMesh default ghosting/coupling
    We are now fully explicit in the ghosting and sparsity coupling
    pattern that we want. We add a coupling functor for kernels and
    bcs that does not do any ghosting because it has _n_levels set
    to 0, but it does specify intra-element dof coupling so that the
    sparsity pattern is correct.
    
    Now we can really re-gold the geometric edge test case because
    there is really no evaluable ghosting now

--- framework/src/systems/NonlinearSystem.C
+++ framework/src/systems/NonlinearSystem.C
@@ -94,14 +94,6 @@ NonlinearSystem::NonlinearSystem(FEProblemBase & fe_problem, const std::string &
     _use_coloring_finite_difference(false),
     _computed_scaling(false)
 {
-  // The default coupling functor does not actually ghost any elements because it has _n_levels set
-  // to zero. What it DOES do is actually adds sparsity coupling between dofs within an element,
-  // which you might say is pretty important. Without this, even kernels would cause mallocs in
-  // MatSetValues
-  _sys.get_dof_map().add_coupling_functor(
-      _sys.get_dof_map().default_coupling(),
-      false); // The false keeps it from getting added to the mesh
-
   nonlinearSolver()->residual_object = &_nl_residual_functor;
   nonlinearSolver()->jacobian = Moose::compute_jacobian;
   nonlinearSolver()->bounds = Moose::compute_bounds;
