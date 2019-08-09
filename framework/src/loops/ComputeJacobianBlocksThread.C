4a6eb923da45b0ea194de387a938d64ad0060b3d
Fande Kong <fdkong.jd@gmail.com>
2019-08-09 12:49:58 -0600

    ComputeJacobianBlocksThread: add face contributions
    Closes #13886

--- framework/src/loops/ComputeJacobianBlocksThread.C
+++ framework/src/loops/ComputeJacobianBlocksThread.C
@@ -37,17 +37,50 @@ ComputeJacobianBlocksThread::~ComputeJacobianBlocksThread() {}
 void
 ComputeJacobianBlocksThread::postElement(const Elem * elem)
 {
-  std::vector<dof_id_type>
-      dof_indices; // Do this out here to avoid creating and destroying it thousands of times
+  _dof_indices.clear();
 
   Threads::spin_mutex::scoped_lock lock(Threads::spin_mtx);
 
   for (const auto & block : _blocks)
   {
     const DofMap & dof_map = block->_precond_system.get_dof_map();
-    dof_map.dof_indices(elem, dof_indices);
+    dof_map.dof_indices(elem, _dof_indices);
 
     _fe_problem.addJacobianBlock(
-        block->_jacobian, block->_ivar, block->_jvar, dof_map, dof_indices, _tid);
+        block->_jacobian, block->_ivar, block->_jvar, dof_map, _dof_indices, _tid);
+  }
+}
+
+void
+ComputeJacobianBlocksThread::postInternalSide(const Elem * elem, unsigned int side)
+{
+  if (_dg_warehouse->hasActiveBlockObjects(_subdomain, _tid))
+  {
+    _dof_indices.clear();
+    _dof_neighbor_indices.clear();
+
+    // Pointer to the neighbor we are currently working on.
+    const Elem * neighbor = elem->neighbor_ptr(side);
+
+    Threads::spin_mutex::scoped_lock lock(Threads::spin_mtx);
+    // Get the global id of the element and the neighbor
+    const dof_id_type elem_id = elem->id(), neighbor_id = neighbor->id();
+
+    if ((neighbor->active() && (neighbor->level() == elem->level()) && (elem_id < neighbor_id)) ||
+        (neighbor->level() < elem->level()))
+      for (const auto & block : _blocks)
+      {
+        const DofMap & dof_map = block->_precond_system.get_dof_map();
+        dof_map.dof_indices(elem, _dof_indices);
+        dof_map.dof_indices(neighbor, _dof_neighbor_indices);
+
+        _fe_problem.addJacobianNeighbor(block->_jacobian,
+                                        block->_ivar,
+                                        block->_jvar,
+                                        dof_map,
+                                        _dof_indices,
+                                        _dof_neighbor_indices,
+                                        _tid);
+      }
   }
 }
