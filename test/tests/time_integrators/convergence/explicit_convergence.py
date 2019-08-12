de0aedc15a1eabc2ab3f1593f24208196685285e
Andrew E Slaughter <andrew.slaughter@inl.gov>
2019-08-12 09:06:41 -0600

    Update convergence tests to use MMS tools
    Update gold files to be consistent and add convergence error results
    
    (refs #13661)

---
+++ test/tests/time_integrators/convergence/explicit_convergence.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2
+#* This file is part of the MOOSE framework
+#* https://www.mooseframework.org
+#*
+#* All rights reserved, see COPYRIGHT for full restrictions
+#* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+#*
+#* Licensed under LGPL 2.1, please see LICENSE for details
+#* https://www.gnu.org/licenses/lgpl-2.1.html
+import mms
+
+N = 3
+EXE = '../../../../test'
+
+df1 = mms.run_temporal('explicit_convergence.i', N,
+                       'Executioner/TimeIntegrator/type=Heun', file_base='heun_{}',
+                       console=False, dt=0.00390625, y_pp='l2_err', executable=EXE)
+
+df2 = mms.run_temporal('explicit_convergence.i', N,
+                       'Executioner/TimeIntegrator/type=Ralston', file_base='ralston_{}',
+                       console=False, dt=0.00390625, y_pp='l2_err', executable=EXE)
+
+df3 = mms.run_temporal('explicit_convergence.i', N,
+                       'Executioner/TimeIntegrator/type=ExplicitMidpoint', file_base='explicitmidpoint_{}',
+                       console=False, dt=0.00390625, y_pp='l2_err', executable=EXE)
+
+df4 = mms.run_temporal('explicit_convergence.i', N,
+                       'Executioner/TimeIntegrator/type=ExplicitEuler', file_base='expliciteuler_{}',
+                       console=False, dt=0.00390625, y_pp='l2_err', executable=EXE)
+
+
+fig = mms.ConvergencePlot(xlabel='$\Delta t$', ylabel='$L_2$ Error')
+fig.plot(df1, label='Heun (2nd Order)', marker='o', markersize=8)
+fig.plot(df2, label='Ralston (2nd Order)', marker='s', markersize=8)
+fig.plot(df3, label='Midpoint (2nd Order)', marker='v', markersize=8)
+fig.plot(df4, label='Euler (1st Order)', marker='h', markersize=8)
+fig.save('explicit_convergence.pdf')
+
+df1.to_csv('explicit_convergence_heun.csv')
+df2.to_csv('explicit_convergence_ralston.csv')
+df3.to_csv('explicit_convergence_midpoint.csv')
+df4.to_csv('explicit_convergence_euler.csv')
