b723841b3d7f18cba82ad13405d26917798a75a2
Alex Lindsay <alexlindsay239@gmail.com>
2019-08-21 09:12:29 -0700

    Add quadrature locator plus constraint test
    This test can be used to demonstrate that a
    quadrature geometric locator combined with a constraint
    results in a failed assertion in getting dof indices.

---
+++ test/src/materials/QuadratureLocatorTestMaterial.C
@@ -0,0 +1,44 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "QuadratureLocatorTestMaterial.h"
+
+#include "MooseMesh.h"
+#include "SystemBase.h"
+#include "MooseEnum.h"
+#include "PenetrationLocator.h"
+
+#include "libmesh/string_to_enum.h"
+
+registerMooseObject("MooseApp", QuadratureLocatorTestMaterial);
+
+template <>
+InputParameters
+validParams<QuadratureLocatorTestMaterial>()
+{
+  MooseEnum orders("FIRST SECOND THIRD FOURTH", "FIRST");
+
+  InputParameters params = validParams<Material>();
+  params.set<bool>("_dual_restrictable") = true;
+  params.addRequiredParam<BoundaryName>("paired_boundary",
+                                        "The boundary on the other side of a gap.");
+  params.set<bool>("use_displaced_mesh") = true;
+  params.addParam<MooseEnum>("order", orders, "The finite element order");
+  return params;
+}
+
+QuadratureLocatorTestMaterial::QuadratureLocatorTestMaterial(const InputParameters & parameters)
+  : Material(parameters),
+    GeometricSearchInterface(this),
+    _penetration_locator(getQuadraturePenetrationLocator(
+        parameters.get<BoundaryName>("paired_boundary"),
+        boundaryNames()[0],
+        Utility::string_to_enum<Order>(parameters.get<MooseEnum>("order"))))
+{
+}
