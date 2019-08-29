e37de8272b61cb1926cd161e8ebc0bda55136fe9
Alex Lindsay <alexlindsay239@gmail.com>
2019-08-29 17:03:25 -0600

    Add auto-generated files

---
+++ framework/include/base/MooseDefaultConfig.h
@@ -0,0 +1,26 @@
+/* include/base/MooseConfig.h.  Generated from MooseConfig.h.in by configure.  */
+/* include/base/MooseConfig.h.in.  Generated from configure.ac by autoheader.  */
+
+/* The size of the derivative backing array */
+#define AD_MAX_DOFS_PER_ELEM 50
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "moose-users@googlegroups.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "moose"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "moose 1.0.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "moose"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "https://mooseframework.org"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0.0"
+
+/* Whether to use a sparse derivative type */
+/* #undef SPARSE_AD */
