61b889a977f2dd58b09e067d032128b1f3edb785
David Andrs <david.andrs@inl.gov>
2019-09-03 10:00:36 -0600

    Using VariableName type
    Variable names should use VariableName type which is provided by MOOSE
    for that purpose.
    
    Refs #0

--- modules/thm/include/components/OldJunction.h
+++ modules/thm/include/components/OldJunction.h
@@ -22,16 +22,16 @@ protected:
   virtual void check() const override;
 
   /// The name of scalar variable that represents the pressure in this junction
-  std::string _pressure_var_name;
+  const VariableName _pressure_var_name;
   /// The name of scalar variable that represents the energy in this junction
-  std::string _energy_var_name;
+  const VariableName _energy_var_name;
   /// Initial pressure
   Real _initial_P;
   /// Initial volume fraction of vapor
   Real _initial_void_fraction;
 
-  const std::string _total_mfr_in_var_name;
-  const std::string _total_int_energy_rate_in_var_name;
+  const VariableName _total_mfr_in_var_name;
+  const VariableName _total_int_energy_rate_in_var_name;
 
   /// Scaling factors for pressure and energy
   std::vector<Real> _scaling_factors;
