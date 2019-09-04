875e9963fdea83184638d88343c25a7c9f8819c4
Alex Lindsay <alexlindsay239@gmail.com>
2019-09-04 09:16:16 -0700

    Reconfigure

--- framework/include/base/MooseDefaultConfig.h
+++ framework/include/base/MooseDefaultConfig.h
@@ -1,26 +1,43 @@
-/* include/base/MooseConfig.h.  Generated from MooseConfig.h.in by configure.  */
+#pragma once
+
+/* include/base/MooseConfig.h. Generated automatically at end of configure. */
+/* include/base/MooseConfig.h.tmp.  Generated from MooseConfig.h.in by configure.  */
 /* include/base/MooseConfig.h.in.  Generated from configure.ac by autoheader.  */
 
 /* The size of the derivative backing array */
-#define AD_MAX_DOFS_PER_ELEM 50
+#ifndef MOOSE_AD_MAX_DOFS_PER_ELEM
+#define MOOSE_AD_MAX_DOFS_PER_ELEM 50
+#endif
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "moose-users@googlegroups.com"
+#ifndef MOOSE_PACKAGE_BUGREPORT
+#define MOOSE_PACKAGE_BUGREPORT "moose-users@googlegroups.com"
+#endif
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "moose"
+#ifndef MOOSE_PACKAGE_NAME
+#define MOOSE_PACKAGE_NAME "moose"
+#endif
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "moose 1.0.0"
+#ifndef MOOSE_PACKAGE_STRING
+#define MOOSE_PACKAGE_STRING "moose 0.9.0"
+#endif
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "moose"
+#ifndef MOOSE_PACKAGE_TARNAME
+#define MOOSE_PACKAGE_TARNAME "moose"
+#endif
 
 /* Define to the home page for this package. */
-#define PACKAGE_URL "https://mooseframework.org"
+#ifndef MOOSE_PACKAGE_URL
+#define MOOSE_PACKAGE_URL "https://mooseframework.org"
+#endif
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.0.0"
+#ifndef MOOSE_PACKAGE_VERSION
+#define MOOSE_PACKAGE_VERSION "0.9.0"
+#endif
 
 /* Whether to use a sparse derivative type */
 /* #undef SPARSE_AD */
