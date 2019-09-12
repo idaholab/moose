c542ef25f5714af68f923f6f22d45de6ef594209
David Andrs <david.andrs@inl.gov>
2019-09-12 10:41:24 -0600

    Adding linear fluid properties class for testing
    Refs #0

---
+++ modules/thm/test/include/fluidprops/LinearTestFluidProperties.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "SinglePhaseFluidProperties.h"
+
+class LinearTestFluidProperties;
+
+template <>
+InputParameters validParams<LinearTestFluidProperties>();
+
+/**
+ * single phase fluid properties class used for testing derivatives
+ *
+ * This class is only for supplying simple, linear functions for fluid properties
+ */
+class LinearTestFluidProperties : public SinglePhaseFluidProperties
+{
+public:
+  LinearTestFluidProperties(const InputParameters & parameters);
+
+  virtual Real rho_from_p_T(Real p, Real T) const override;
+  virtual void
+  rho_from_p_T(Real p, Real T, Real & rho, Real & drho_dp, Real & drho_dT) const override;
+  virtual Real e_from_p_rho(Real p, Real rho) const override;
+  virtual void
+  e_from_p_rho(Real p, Real rho, Real & e, Real & de_dp, Real & de_drho) const override;
+  virtual Real T_from_v_e(Real v, Real e) const override;
+  virtual void T_from_v_e(Real v, Real e, Real & T, Real & dT_dv, Real & dT_de) const override;
+  virtual Real p_from_v_e(Real v, Real e) const override;
+  virtual void p_from_v_e(Real v, Real e, Real & p, Real & dp_dv, Real & dp_de) const override;
+
+protected:
+};
