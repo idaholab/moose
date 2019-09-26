cd4b566e1fb1daf9e2f9bf4ce0fe4266f9095a9f
Joshua Hansel <joshua.hansel@inl.gov>
2019-09-26 13:17:26 -0600

    Extracted base class ExplicitTimeIntegrator from ActuallyExplicitEuler
    Refs #11855

---
+++ framework/doc/content/source/timeintegrators/ExplicitTimeIntegrator.md
@@ -0,0 +1,124 @@
+# ExplicitTimeIntegrator
+
+## Description
+
+`ExplicitTimeIntegrator` is a base class for explicit time integrators that are
+implemented without performing any nonlinear solve, which reduces runtime. Unlike
+explicit time integrators that are not derived from this class, it is not
+necessary to set `implicit` to `false` for all of the non-time residual objects.
+
+## Methods of Solution
+
+Time integrators deriving from this class have three solve options, provided via
+the `solve_type` parameter:
+
+- `consistent`: (the default) A full mass matrix is built and used in a linear solve for the update
+- `lumped`: A "lumped" mass matrix is built, inverted, and applied to the RHS, which
+  is faster but can possibly be less accurate.
+- `lump_preconditioned`: The inversion of the "lumped" mass matrix is used to
+  precondition the `consistent` solve.
+
+All three methods are solved similarly: a linear solve is performed to obtain a
+solution update $\delta u$ that is added to the existing solution.
+
+Below is some more explanation of each of these `solve_type` options:
+
+### `consistent`
+
+The `consistent` option builds a full ("consistent") "mass matrix" and uses it
+in a linear solve to get the update.  This is done by calling
+`FEProblem::computeJacobianTag()` and specifying the `TIME` tag which includes
+all of the `TimeKernel` derived Kernels and `NodalBC` derived BoundaryConditions
+to compute $\mathbf{M}$:
+
+!listing framework/src/timeintegrators/ActuallyExplicitEuler.C line=computeJacobianTag
+
+A residual computation is also completed to use as the RHS ($R$):
+
+!listing framework/src/timeintegrators/ActuallyExplicitEuler.C line=computeResidual
+
+Finally, the following linear system is solved to obtain the solution update
+$\delta u$ using the default linear solver from libMesh (usually PETSc, including
+the application of any command-line parameters specified):
+
+\begin{equation}
+\mathbf{M} \delta u = -R
+\end{equation}
+
+### `lumped`
+
+The `lumped` option creates a "lumped" mass matrix to use in the solve.  A lumped mass matrix is a diagonal matrix where the diagonal is the sum of all elements on the row from the original matrix.
+Here, to achieve the lumping, a matrix-vector product is performed between the
+consistent mass matrix and a vector of all ones.
+
+The inverse of a diagonal matrix is simply the reciprocal of each diagonal entry.
+Then the matrix-vector product of the "inverse" lumped diagonal matrix is applied by simply doing a pointwise multiplication with the RHS.
+
+Thus the `lumped` option does not actually solve a system of linear equations,
+allowing it be much faster. However, the lumping of the mass matrix may lead to
+unacceptable phase errors.
+
+### `lump_preconditioned`
+
+This option is the combination of the other two options: the consistent mass matrix
+is used in the linear system, but the linear solve is preconditioned using the
+lumped mass matrix. This compromise retains the accuracy of the `consistent`
+option while benefiting from some of the speedup offered by the `lumped` option.
+
+The lumped mass matrix preconditioner is applied with a helper class, `LumpedPreconditioner`:
+
+!listing framework/include/timeintegrators/ExplicitTimeIntegrator.h line=class LumpedPreconditioner
+
+This helper object simply applies the inverse of the diagonal, lumped
+mass-matrix as the preconditioner for the linear solve, which is very
+efficient. Note that when this option is applied you shouldn't specify any other
+preconditioners using command-line syntax or they will override this option.
+
+## Additional Details
+
+Some notes on some of the implementation details of this class follow.
+
+### Update Form
+
+Note that even though we're doing an explicit solve we are currently doing it in
+"update form" similar to a single step Newton solve.  This gives us good parity
+with the rest of MOOSE.  We may want to change this in the future to make better
+use of the fact that the mass-matrix can be constant for a wider class of
+problems if we remove `dt` from it.
+
+### `_ones`
+
+To get the sum of each row of the mass matrix for "lumping" purposes a vector consisting of all `1`s is used in a matrix-vector product:
+
+!listing framework/src/timeintegrators/ActuallyExplicitEuler.C line=mass_matrix.vector_mult
+
+This is actually the very same way `MatGetRowSum` is implemented in PETSc; however,
+doing it manually cuts down on vector creation/destruction and a few other book-keeping operations.
+
+In the future this could be changed to use `MatGetRowSum` if a specialization for `MPI_Aij` format is created.
+
+### Time
+
+Time in an explicit solve must be handled carefully. When evaluating the weak
+form (the integral parts) of the residual, time needs to be set to be the "old"
+time (the time we are solving "from"):
+
+!listing framework/src/timeintegrators/ActuallyExplicitEuler.C line=timeOld
+
+However, `DirichletBC` derived boundary conditions need to use the **final** time,
+since the strong constraints they represent use the final time and are not affected
+by the time integrator. To achieve this, time is reset to the `_current_time` after
+the weak form residual evaluation and before `NodalBC` boundary condition application,
+which makes `postResidual()` the correct place to reset time for this purpose:
+
+!listing framework/src/timeintegrators/ActuallyExplicitEuler.C line=_fe_problem.time() = _current_time;
+
+After `postResidual()` the `NodalBC` BCs are applied with the time at the final time for the step.
+
+### `meshChanged()`
+
+When the mesh changes the linear solver needs to be destroyed and recreated.
+This is done by simply building a new one and setting it up again.  This happens
+automatically just by "overwriting" the `std::unique_ptr` to the LinearSolver.
+
+!syntax children /Executioner/TimeIntegrator/ExplicitTimeIntegrator
--- framework/src/timeintegrators/ActuallyExplicitEuler.C
+++ framework/src/timeintegrators/ActuallyExplicitEuler.C
@@ -11,13 +11,9 @@
 #include "ActuallyExplicitEuler.h"
 #include "NonlinearSystem.h"
 #include "FEProblem.h"
-#include "PetscSupport.h"
 
 // libMesh includes
-#include "libmesh/sparse_matrix.h"
 #include "libmesh/nonlinear_solver.h"
-#include "libmesh/preconditioner.h"
-#include "libmesh/enum_convergence_flags.h"
 
 registerMooseObject("MooseApp", ActuallyExplicitEuler);
 
@@ -25,17 +21,7 @@ template <>
 InputParameters
 validParams<ActuallyExplicitEuler>()
 {
-  InputParameters params = validParams<TimeIntegrator>();
-
-  MooseEnum solve_type("consistent lumped lump_preconditioned", "consistent");
-
-  params.addParam<MooseEnum>(
-      "solve_type",
-      solve_type,
-      "The way to solve the system.  A 'consistent' solve uses the full mass matrix and actually "
-      "needs to use a linear solver to solve the problem.  'lumped' uses a lumped mass matrix with "
-      "a simple inversion - incredibly fast but may be less accurate.  'lump_preconditioned' uses "
-      "the lumped mass matrix as a preconditioner for the 'consistent' solve");
+  InputParameters params = validParams<ExplicitTimeIntegrator>();
 
   params.addClassDescription(
       "Implementation of Explicit/Forward Euler without invoking any of the nonlinear solver");
@@ -43,63 +29,8 @@ validParams<ActuallyExplicitEuler>()
   return params;
 }
 
-/**
- * Helper class to apply preconditioner
- */
-class LumpedPreconditioner : public Preconditioner<Real>
-{
-public:
-  LumpedPreconditioner(const NumericVector<Real> & diag_inverse)
-    : Preconditioner(diag_inverse.comm()), _diag_inverse(diag_inverse)
-  {
-  }
-
-  virtual void init() override
-  {
-    // No more initialization needed here
-    _is_initialized = true;
-  }
-
-  virtual void apply(const NumericVector<Real> & x, NumericVector<Real> & y) override
-  {
-    y.pointwise_mult(_diag_inverse, x);
-  }
-
-protected:
-  /// The inverse of the diagonal of the lumped matrix
-  const NumericVector<Real> & _diag_inverse;
-};
-
 ActuallyExplicitEuler::ActuallyExplicitEuler(const InputParameters & parameters)
-  : TimeIntegrator(parameters),
-    MeshChangedInterface(parameters),
-    _solve_type(getParam<MooseEnum>("solve_type")),
-    _explicit_residual(_nl.addVector("explicit_residual", false, PARALLEL)),
-    _explicit_euler_update(_nl.addVector("explicit_euler_update", true, PARALLEL)),
-    _mass_matrix_diag(_nl.addVector("mass_matrix_diag", false, PARALLEL))
-{
-  _Ke_time_tag = _fe_problem.getMatrixTagID("TIME");
-
-  // Try to keep MOOSE from doing any nonlinear stuff
-  _fe_problem.solverParams()._type = Moose::ST_LINEAR;
-
-  if (_solve_type == LUMPED || _solve_type == LUMP_PRECONDITIONED)
-    _ones = &_nl.addVector("ones", false, PARALLEL);
-}
-
-void
-ActuallyExplicitEuler::initialSetup()
-{
-  meshChanged();
-}
-
-void
-ActuallyExplicitEuler::init()
-{
-}
-
-void
-ActuallyExplicitEuler::preSolve()
+  : ExplicitTimeIntegrator(parameters)
 {
 }
 
@@ -127,104 +58,43 @@ ActuallyExplicitEuler::computeADTimeDerivatives(DualReal & ad_u_dot, const dof_i
 void
 ActuallyExplicitEuler::solve()
 {
-  auto & es = _fe_problem.es();
-
-  auto & nonlinear_system = _fe_problem.getNonlinearSystemBase();
-
-  auto & mass_matrix = *_nonlinear_implicit_system->matrix;
+  // Reset iteration counts
+  _n_nonlinear_iterations = 0;
+  _n_linear_iterations = 0;
 
   _current_time = _fe_problem.time();
 
-  // Set time back so that we're evaluating the interior residual at the old time
+  // Set time to the time at which to evaluate the residual
   _fe_problem.time() = _fe_problem.timeOld();
-
   _nonlinear_implicit_system->update();
 
-  // Must compute the residual first
+  // Compute the residual
   _explicit_residual.zero();
   _fe_problem.computeResidual(*_nonlinear_implicit_system->current_local_solution,
                               _explicit_residual);
 
-  // The residual is on the RHS
-  _explicit_residual *= -1.;
+  // Move the residual to the RHS
+  _explicit_residual *= -1.0;
 
   // Compute the mass matrix
+  auto & mass_matrix = *_nonlinear_implicit_system->matrix;
   _fe_problem.computeJacobianTag(
       *_nonlinear_implicit_system->current_local_solution, mass_matrix, _Ke_time_tag);
 
-  auto converged = false;
+  // Perform the linear solve
+  bool converged = performExplicitSolve(mass_matrix);
 
-  switch (_solve_type)
-  {
-    case CONSISTENT:
-    {
-      const auto num_its_and_final_tol = _linear_solver->solve(
-          mass_matrix,
-          _explicit_euler_update,
-          _explicit_residual,
-          es.parameters.get<Real>("linear solver tolerance"),
-          es.parameters.get<unsigned int>("linear solver maximum iterations"));
-
-      converged = checkLinearConvergence();
-
-      _n_linear_iterations = num_its_and_final_tol.first;
-
-      break;
-    }
-    case LUMPED:
-    {
-      // Computes the sum of each row (lumping)
-      // Note: This is actually how PETSc does it
-      // It's not "perfectly optimal" - but it will be fast (and universal)
-      mass_matrix.vector_mult(_mass_matrix_diag, *_ones);
-
-      // "Invert" the diagonal mass matrix
-      _mass_matrix_diag.reciprocal();
-
-      // Multiply the inversion by the RHS
-      _explicit_euler_update.pointwise_mult(_mass_matrix_diag, _explicit_residual);
-
-      // Check for convergence by seeing if there is a nan or inf
-      auto sum = _explicit_euler_update.sum();
-      converged = std::isfinite(sum);
-
-      _n_linear_iterations = 0;
-
-      break;
-    }
-    case LUMP_PRECONDITIONED:
-    {
-      mass_matrix.vector_mult(_mass_matrix_diag, *_ones);
-      _mass_matrix_diag.reciprocal();
-
-      const auto num_its_and_final_tol = _linear_solver->solve(
-          mass_matrix,
-          _explicit_euler_update,
-          _explicit_residual,
-          es.parameters.get<Real>("linear solver tolerance"),
-          es.parameters.get<unsigned int>("linear solver maximum iterations"));
-
-      converged = checkLinearConvergence();
-
-      _n_linear_iterations = num_its_and_final_tol.first;
-
-      break;
-    }
-    default:
-      mooseError("Unknown solve_type in ActuallyExplicitEuler ");
-  }
-
-  *_nonlinear_implicit_system->solution = nonlinear_system.solutionOld();
-  *_nonlinear_implicit_system->solution += _explicit_euler_update;
+  // Update the solution
+  *_nonlinear_implicit_system->solution = _nl.solutionOld();
+  *_nonlinear_implicit_system->solution += _solution_update;
 
   // Enforce contraints on the solution
   DofMap & dof_map = _nonlinear_implicit_system->get_dof_map();
   dof_map.enforce_constraints_exactly(*_nonlinear_implicit_system,
                                       _nonlinear_implicit_system->solution.get());
-
   _nonlinear_implicit_system->update();
 
-  nonlinear_system.setSolution(*_nonlinear_implicit_system->current_local_solution);
+  _nl.setSolution(*_nonlinear_implicit_system->current_local_solution);
 
   _nonlinear_implicit_system->nonlinear_solver->converged = converged;
 }
@@ -236,61 +106,6 @@ ActuallyExplicitEuler::postResidual(NumericVector<Number> & residual)
   residual += _Re_non_time;
   residual.close();
 
-  // Reset time - the boundary conditions (which is what comes next) are applied at the final time
+  // Reset time to the time at which to evaluate nodal BCs, which comes next
   _fe_problem.time() = _current_time;
-}
-
-void
-ActuallyExplicitEuler::meshChanged()
-{
-  // Can only be done after the system is inited
-  if (_solve_type == LUMPED || _solve_type == LUMP_PRECONDITIONED)
-    *_ones = 1.;
-
-  if (_solve_type == CONSISTENT || _solve_type == LUMP_PRECONDITIONED)
-    _linear_solver = LinearSolver<Number>::build(comm());
-
-  if (_solve_type == LUMP_PRECONDITIONED)
-  {
-    _preconditioner = libmesh_make_unique<LumpedPreconditioner>(_mass_matrix_diag);
-    _linear_solver->attach_preconditioner(_preconditioner.get());
-    _linear_solver->init();
-  }
-
-  if (_solve_type == CONSISTENT || _solve_type == LUMP_PRECONDITIONED)
-    Moose::PetscSupport::setLinearSolverDefaults(_fe_problem, *_linear_solver);
-}
-
-bool
-ActuallyExplicitEuler::checkLinearConvergence()
-{
-  auto reason = _linear_solver->get_converged_reason();
-
-  switch (reason)
-  {
-    case CONVERGED_RTOL_NORMAL:
-    case CONVERGED_ATOL_NORMAL:
-    case CONVERGED_RTOL:
-    case CONVERGED_ATOL:
-    case CONVERGED_ITS:
-    case CONVERGED_CG_NEG_CURVE:
-    case CONVERGED_CG_CONSTRAINED:
-    case CONVERGED_STEP_LENGTH:
-    case CONVERGED_HAPPY_BREAKDOWN:
-      return true;
-    case DIVERGED_NULL:
-    case DIVERGED_ITS:
-    case DIVERGED_DTOL:
-    case DIVERGED_BREAKDOWN:
-    case DIVERGED_BREAKDOWN_BICG:
-    case DIVERGED_NONSYMMETRIC:
-    case DIVERGED_INDEFINITE_PC:
-    case DIVERGED_NAN:
-    case DIVERGED_INDEFINITE_MAT:
-    case CONVERGED_ITERATING:
-    case DIVERGED_PCSETUP_FAILED:
-      return false;
-    default:
-      mooseError("Unknown convergence flat in ActuallyExplicitEuler");
-  }
 }
