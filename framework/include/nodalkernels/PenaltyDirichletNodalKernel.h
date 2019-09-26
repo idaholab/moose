572ed913ab72644aeddcb40b174becdff132f59b
Hailong Chen <hailong.chen@uky.edu>
2019-09-25 21:22:17 -0400

    Penalty Dirichlet BC for nodesets (#14092)

---
+++ framework/include/nodalkernels/PenaltyDirichletNodalKernel.h
@@ -0,0 +1,32 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "NodalKernel.h"
+
+// Forward Declarations
+class PenaltyDirichletNodalKernel;
+
+template <>
+InputParameters validParams<PenaltyDirichletNodalKernel>();
+
+class PenaltyDirichletNodalKernel : public NodalKernel
+{
+public:
+  PenaltyDirichletNodalKernel(const InputParameters & parameters);
+
+protected:
+  virtual Real computeQpResidual() override;
+  virtual Real computeQpJacobian() override;
+
+private:
+  const Real & _p;
+  const Real & _v;
+};
