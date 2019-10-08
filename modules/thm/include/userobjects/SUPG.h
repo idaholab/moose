923ec763434968250113c6e7b958b2b25189bb3e
David Andrs <david.andrs@inl.gov>
2019-10-08 09:18:48 -0600

    Removing shock capturing in SUPG
    This only worked with constant A formulation.
    It is part of SUPG, which will be removed soon.
    This code was not even tested for several year.
    
    Refs #19747

--- modules/thm/include/userobjects/SUPG.h
+++ modules/thm/include/userobjects/SUPG.h
@@ -16,9 +16,6 @@ public:
   virtual void initMooseObjects(FlowModel & fm);
   virtual void addMooseObjects(FlowModel & fm, InputParameters & pars) const;
 
-protected:
-  bool _shock_capturing;
-
 public:
   static const std::string DELTA;
   static const std::string RESIDUAL;
