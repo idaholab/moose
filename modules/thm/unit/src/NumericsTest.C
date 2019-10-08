6c7a8a034a07486a70364f59d5cef7345349290f
David Andrs <david.andrs@inl.gov>
2019-10-08 14:28:42 -0600

    Adding unit tests for functions in Numerics module
    Refs #19747

--- modules/thm/unit/src/NumericsTest.C
+++ modules/thm/unit/src/NumericsTest.C
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 #include "Numerics.h"
+#include "THMTestUtils.h"
 
 TEST(NumericsTest, test_absoluteFuzzyEqualVectors_True)
 {
@@ -43,4 +44,27 @@ TEST(NumericsTest, test_haveSameDirection_False)
   EXPECT_FALSE(THM::haveSameDirection(a, b));
 }
 
-TEST(NumericsTest, Reynolds) { ABS_TEST(THM::Reynolds(0.1, 999, 2e-2), 0.16, 1e-13); }
+TEST(NumericsTest, Reynolds)
+{
+  ABS_TEST(THM::Reynolds(0.1, 999, 0.5, 2e-2, 0.9), 1.11, 1e-13);
+  ABS_TEST(THM::Reynolds(0.1, 999, -0.5, 2e-2, 0.9), 1.11, 1e-13);
+}
+
+TEST(NumericsTest, Prandtl) { ABS_TEST(THM::Prandtl(10, 0.1, 2), 0.5, 1e-13); }
+
+TEST(NumericsTest, Grashof)
+{
+  ABS_TEST(THM::Grashof(0.1, 1, 2e-2, 999, 0.05, 9.81), 3.1329247392e3, 1e-13);
+}
+
+TEST(NumericsTest, Laplace) { ABS_TEST(THM::Laplace(0.001, 1, 9.81), 0.010096375546923, 1e-13); }
+
+TEST(NumericsTest, viscosityNumber)
+{
+  ABS_TEST(THM::viscosityNumber(0.05, 0.02, 999, 2, 9.81), 0.062602188259, 1e-13);
+}
+
+TEST(NumericsTest, wallHeatTransferCoefficient)
+{
+  ABS_TEST(THM::wallHeatTransferCoefficient(2, 6, 3), 4, 1e-13);
+}
