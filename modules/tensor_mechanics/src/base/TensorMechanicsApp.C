a3f63820a8534cc65d3309389f727d834ee23a61
Casey Icenhour <cticenho@ncsu.edu>
2019-11-06 10:37:11 -0700

    Convert TensorMechanicsApp to new static validParams
    Refs #14135

--- modules/tensor_mechanics/src/base/TensorMechanicsApp.C
+++ modules/tensor_mechanics/src/base/TensorMechanicsApp.C
@@ -12,11 +12,12 @@
 #include "AppFactory.h"
 #include "MooseSyntax.h"
 
-template <>
+defineLegacyParams(TensorMechanicsApp);
+
 InputParameters
-validParams<TensorMechanicsApp>()
+TensorMechanicsApp::validParams()
 {
-  InputParameters params = validParams<MooseApp>();
+  InputParameters params = MooseApp::validParams();
   params.set<bool>("automatic_automatic_scaling") = false;
   return params;
 }
