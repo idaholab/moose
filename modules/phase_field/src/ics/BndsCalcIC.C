c4a4037dded66d36776c6dd269b4626f5af29139
Sudipta Biswas <biswas7@purdue.edu>
2019-11-14 15:16:28 -0700

    Create BndsCalcIC #9668

---
+++ modules/phase_field/src/ics/BndsCalcIC.C
@@ -0,0 +1,38 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "BndsCalcIC.h"
+#include "BndsCalculator.h"
+
+registerMooseObject("PhaseFieldApp", BndsCalcIC);
+
+template <>
+InputParameters
+validParams<BndsCalcIC>()
+{
+  InputParameters params = validParams<InitialCondition>();
+  params.addClassDescription(
+      "Initialize the location of grain boundaries in a polycrystalline sample");
+  params.addRequiredCoupledVarWithAutoBuild(
+      "v", "var_name_base", "op_num", "Array of coupled variables");
+  return params;
+}
+
+BndsCalcIC::BndsCalcIC(const InputParameters & parameters)
+  : InitialCondition(parameters), _op_num(coupledComponents("v")), _vals(_op_num)
+{
+  for (unsigned int i = 0; i < _op_num; ++i)
+    _vals[i] = &coupledValue("v", i);
+}
+
+Real
+BndsCalcIC::value(const Point & /*p*/)
+{
+  return BndsCalculator::computeBndsVariable(_vals, _qp);
+}
