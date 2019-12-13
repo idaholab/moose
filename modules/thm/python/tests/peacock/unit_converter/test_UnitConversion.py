588fb71d5ec43e3707166395c46dfa103d122f15
David Andrs <david.andrs@inl.gov>
2019-12-13 11:01:51 -0700

    Adding path to python tests

--- modules/thm/python/tests/peacock/unit_converter/test_UnitConversion.py
+++ modules/thm/python/tests/peacock/unit_converter/test_UnitConversion.py
@@ -1,3 +1,6 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "..", 'peacock'))
 import unittest
 from UnitConversion import *
 
--- modules/thm/python/tests/peacock/unit_converter/test_UnitConverterPlugin.py
+++ modules/thm/python/tests/peacock/unit_converter/test_UnitConverterPlugin.py
@@ -1,4 +1,6 @@
+import os
 import sys
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "..", 'peacock'))
 import unittest
 import peacock
 from PyQt5 import QtWidgets, QtCore, QtGui
