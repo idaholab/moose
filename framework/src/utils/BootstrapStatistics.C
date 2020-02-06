aa5030fb46f460116238903574e404b23d661545
Andrew E Slaughter <andrew.slaughter@inl.gov>
2020-02-05 17:06:37 -0700

    Handle shuffle of distributed VPP data in parallel

--- framework/src/utils/BootstrapStatistics.C
+++ framework/src/utils/BootstrapStatistics.C
@@ -16,6 +16,9 @@
 #include "MooseRandom.h"
 #include "libmesh/auto_ptr.h"
 #include "libmesh/parallel.h"
+#include "libmesh/parallel_sync.h"
+
+#include "SerializerGuard.h"
 
 namespace Statistics
 {
@@ -77,20 +80,77 @@ BootstrapCalculator::shuffle(const std::vector<Real> & data,
                              MooseRandom & generator,
                              const bool is_distributed) const
 {
-  const std::size_t n = data.size();
-  std::vector<Real> replicate(n);
+  // Size of the local input data
+  const std::size_t n_local = data.size();
+
+  // The data to be returned
+  std::vector<Real> replicate;
+
+  // REPLICATED data
   if (!is_distributed)
   {
-    for (std::size_t j = 0; j < n; ++j)
+    replicate.resize(n_local);
+    for (std::size_t j = 0; j < n_local; ++j)
     {
-      auto index = generator.randl(0, 0, n);
+      auto index = generator.randl(0, 0, n_local);
       replicate[j] = data[index];
     }
   }
 
+  // DISTRIBUTED data
   else
   {
-    // from future import parallel magic
+    // Reserve space for storing the local
+    replicate.reserve(n_local);
+
+    // Compute the global size of the vector
+    std::size_t n_global = n_local;
+    _communicator.sum(n_global);
+
+    // Compute the vector data offsets, the scope cleans up the "n_local" vector
+    std::vector<std::size_t> offsets(n_processors());
+    {
+      std::vector<std::size_t> local_sizes;
+      _communicator.allgather(n_local, local_sizes);
+      for (std::size_t i = 1; i < local_sizes.size(); ++i)
+        offsets[i] = offsets[i - 1] + local_sizes[i];
+    }
+
+    // Advance the random number generator to the current offset
+    for (std::size_t i = 0; i < offsets[processor_id()]; ++i)
+      generator.randl(0, 0, n_global);
+
+    // Compute the needs for this processor
+    std::unordered_map<processor_id_type, std::vector<std::size_t>> needs;
+    for (std::size_t i = 0; i < n_local; ++i)
+    {
+      auto index = generator.randl(0, 0, n_global);
+      // iterator to first element in range that is GREATER THAN index
+      auto rank = std::upper_bound(offsets.begin(), offsets.end(), index);
+      auto rk = std::distance(offsets.begin(), rank) - 1;
+      needs[rk].push_back(index - offsets[rk]);
+    }
+
+    // Advance the random number generator to the end of the global vector
+    for (std::size_t i = offsets[processor_id() + 1]; i < n_global; ++i)
+      generator.randl(0, 0, n_global);
+
+    // Collect the values to be returned to the various processors
+    std::unordered_map<processor_id_type, std::vector<Real>> returns;
+    auto return_functor = [&data, &returns](processor_id_type pid,
+                                            const std::vector<std::size_t> & indices) {
+      auto & returns_pid = returns[pid];
+      returns_pid.reserve(indices.size());
+      for (auto idx : indices)
+        returns_pid.push_back(data[idx]);
+    };
+    Parallel::push_parallel_vector_data(_communicator, needs, return_functor);
+
+    // Receive shuffled values from the various processors
+    auto recv_functor = [&replicate](processor_id_type /*pid*/, const std::vector<Real> & values) {
+      replicate.insert(replicate.end(), values.begin(), values.end());
+    };
+    Parallel::push_parallel_vector_data(_communicator, returns, recv_functor);
   }
 
   return replicate;
@@ -116,7 +176,7 @@ Percentile::compute(const std::vector<Real> & data,
   }
   std::sort(values.begin(), values.end());
 
-  // Capture replicates
+  // Extract percentiles
   std::vector<Real> output;
   for (const Real & level : _levels)
   {
