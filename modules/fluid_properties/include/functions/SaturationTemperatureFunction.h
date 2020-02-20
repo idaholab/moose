6b05085f531ae14d3b934c4184a20a5697a3c0af
Joshua Hansel <joshua.hansel@inl.gov>
2020-02-20 14:02:36 -0700

    Added saturation temperature function
    Refs #14755

---
+++ modules/fluid_properties/include/functions/SaturationTemperatureFunction.h
@@ -0,0 +1,36 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "Function.h"
+#include "FunctionInterface.h"
+
+class SaturationTemperatureFunction;
+class TwoPhaseFluidProperties;
+
+/**
+ * Computes saturation temperature from pressure function and 2-phase fluid properties object
+ */
+class SaturationTemperatureFunction : public Function, public FunctionInterface
+{
+public:
+  static InputParameters validParams();
+
+  SaturationTemperatureFunction(const InputParameters & parameters);
+
+  virtual Real value(Real t, const Point & p) const override;
+  virtual RealVectorValue gradient(Real t, const Point & p) const override;
+
+protected:
+  /// Pressure function
+  const Function & _p_fn;
+  /// 2-phase fluid properties object
+  const TwoPhaseFluidProperties & _fp_2phase;
+};
---
+++ modules/fluid_properties/test/tests/functions/saturation_temperature_function/saturation_temperature_function.i
@@ -0,0 +1,57 @@
+# TestTwoPhaseFluidProperties has the following saturation temperature function:
+#   T_sat(p) = 2 p
+# Thus for p = 5, T_sat should be 10.
+
+[Mesh]
+  type = GeneratedMesh
+  dim = 1
+  nx = 1
+[]
+
+[Modules]
+  [./FluidProperties]
+    [./fp_liquid]
+      type = IdealGasFluidProperties
+    [../]
+    [./fp_vapor]
+      type = IdealGasFluidProperties
+    [../]
+    [./fp_2phase]
+      type = TestTwoPhaseFluidProperties
+      fp_liquid = fp_liquid
+      fp_vapor = fp_vapor
+    [../]
+  []
+[]
+
+[Functions]
+  [./p]
+    type = ConstantFunction
+    value = 5
+  [../]
+  [./T_sat]
+    type = SaturationTemperatureFunction
+    p = p
+    fp_2phase = fp_2phase
+  [../]
+[]
+
+[Postprocessors]
+  [./T_sat_pp]
+    type = FunctionValuePostprocessor
+    function = T_sat
+    execute_on = 'INITIAL'
+  [../]
+[]
+
+[Problem]
+  solve = false
+[]
+
+[Executioner]
+  type = Steady
+[]
+
+[Outputs]
+  csv = true
+[]
