3436332397f38416e81b65bb2c03c880e0bc5566
Akshay Dandekar <adandeka@purdue.edu>
2020-02-24 17:19:50 -0500

    Anisotropic crack propagation
    Enhancements - Considers anisotropic crack propagation along cleavage planes in crystalline materials
    - Crack path is preferred along weak cleavage plane as compared to several other cleavage planes
    - Can be used with the material anisotropic stiffness tensor
    
    Issue # 14775
    
    closes #14775

---
+++ modules/phase_field/doc/content/source/kernels/ACInterfaceBetaPenalty.md
@@ -0,0 +1,42 @@
+# ACInterfaceBetaPenalty
+
+!syntax description /Kernels/ACInterfaceBetaPenalty
+
+Based on the ACInterface, this kernel implements an added term for
+anisotropic fracture along the cleavage plane. The added term is
+\begin{equation}
+\dfrac{\beta l_0}{2} \left( \bm{I} - \bm{n} \otimes \bm{n} \right) : \left( \nabla c \otimes \nabla c \right)
+\end{equation}
+
+Its weak form is
+
+\begin{equation}
+\left( - \beta l_0 \left( \nabla^2 c - \nabla c \otimes \nabla c : \nabla \nabla c \right) \right) \psi
+\end{equation}
+
+Simplified expression would be
+
+\begin{equation}
+\beta l_0 \left( - \psi \nabla^2 c + \psi \left( \bm{n} \otimes \bm{n} : \nabla \nabla c \right)
+\end{equation}
+
+where $\beta$ (`beta_penalty`) is the penalty parameter to prefer crack
+propagation along one cleavage plane as against other cleavage planes,
+$l_0$ is the characteristic length in phase field fracture model,
+$\bm{I}$ is the identity tensor,
+$\bm{n}$ (`cleavage_plane_normal`) is the normal to the weak cleavage plane
+$c$ is the phase field variable
+and $\psi$ is the test function.
+
+The expression for Jacobian would be
+
+\begin{equation}
+\beta l_0 \left( \nable \psi \cdot \nable \phi_j - \left( \bm{n} \cdot \nabla \psi \right) \left( \bm{n} \cdot \nabla \phi_j \right)
+\end{equation}
+
+
+!syntax parameters /Kernels/ACInterfaceBetaPenalty
+
+!syntax inputs /Kernels/ACInterfaceBetaPenalty
+
+!syntax children /Kernels/ACInterfaceBetaPenalty
---
+++ modules/phase_field/include/kernels/ACInterfaceBetaPenalty.h
@@ -0,0 +1,85 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "Kernel.h"
+#include "JvarMapInterface.h"
+#include "DerivativeMaterialInterface.h"
+
+class ACInterfaceBetaPenalty;
+
+template <>
+InputParameters validParams<ACInterfaceBetaPenalty>();
+
+/**
+ * Compute the Allen-Cahn interface term with the weak form residual
+ * \f$ \left( \kappa_i \nabla\eta_i, \nabla (L_i \psi) \right) \f$
+ */
+class ACInterfaceBetaPenalty : public DerivativeMaterialInterface<JvarMapKernelInterface<Kernel>>
+{
+public:
+  ACInterfaceBetaPenalty(const InputParameters & parameters);
+  virtual void initialSetup();
+
+protected:
+  virtual Real computeQpResidual();
+  virtual Real computeQpJacobian();
+  virtual Real computeQpOffDiagJacobian(unsigned int jvar);
+
+  RealGradient gradL();
+  RealGradient gradKappa();
+
+  /// the \f$ \nabla(L\psi) \f$ term
+  RealGradient nablaLPsi();
+
+  /// the \f$ \kappa\nabla(L\psi) \f$ term
+  RealGradient kappaNablaLPsi();
+
+  /// the Mcoupling term
+  Real betaNablaPsi();
+
+  /// Mobility
+  const MaterialProperty<Real> & _L;
+  /// Interfacial parameter
+  const MaterialProperty<Real> & _kappa;
+
+  /// flag set if L is a function of non-linear variables in args
+  const bool _variable_L;
+
+  /// @{ Mobility derivatives w.r.t. order parameter
+  const MaterialProperty<Real> & _dLdop;
+  const MaterialProperty<Real> & _d2Ldop2;
+  /// @}
+
+  /// kappa derivative w.r.t. order parameter
+  const MaterialProperty<Real> & _dkappadop;
+
+  /// number of coupled variables
+  const unsigned int _nvar;
+
+  /// @{ Mobility derivative w.r.t. other coupled variables
+  std::vector<const MaterialProperty<Real> *> _dLdarg;
+  std::vector<const MaterialProperty<Real> *> _d2Ldargdop;
+  std::vector<std::vector<const MaterialProperty<Real> *>> _d2Ldarg2;
+  /// @}
+
+  /// kappa derivative w.r.t. other coupled variables
+  std::vector<const MaterialProperty<Real> *> _dkappadarg;
+
+  /// Gradients for all coupled variables
+  std::vector<const VariableGradient *> _gradarg;
+
+private:
+  // penalty for damage on planes not normal to the favoured cleavage plane normal (Nguyen, 2017)
+  const Real _beta_penalty;
+
+  // normal to the favoured cleavage plane: M in (Nguyen, 2017)
+  const std::vector<Real> _cleavage_plane_normal;
+};
