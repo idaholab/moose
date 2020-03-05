24174f701cacb8d5e5315c3c8e7c74f4bfffbd54
Daniel Schwen <daniel@schwen.de>
2020-03-05 16:45:13 -0700

    Physical unit class (#14827)

---
+++ framework/include/utils/Units.h
@@ -0,0 +1,108 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "MooseTypes.h"
+#include "MooseError.h"
+#include <vector>
+#include <iostream>
+
+class MooseUnits;
+
+// power function injected into std
+namespace std
+{
+MooseUnits pow(const MooseUnits &, int);
+}
+
+// output stream operator
+std::ostream & operator<<(std::ostream & os, const MooseUnits & u);
+
+/**
+ * Physical unit management class with runtime unit string parsing, unit checking,
+ * unit conversion, and output.
+ */
+class MooseUnits
+{
+  enum class BaseUnit
+  {
+    METER,
+    KILOGRAM,
+    SECOND,
+    AMPERE,
+    KELVIN,
+    COUNT,
+    CANDELA
+  };
+
+public:
+  MooseUnits();
+  MooseUnits(const std::string & unit_string);
+  MooseUnits(Real f, std::vector<std::pair<MooseUnits::BaseUnit, int>> b) : _factor(f), _base(b) {}
+
+  /// checks if the units are dimensionally conforming (i.e. the describe the same physical quanitity)
+  bool conformsTo(const MooseUnits &) const;
+
+  /// return the conversion factor from the current unit to unit u
+  Real to(const MooseUnits &) const;
+
+  /// parse a unit string into a MooseUnits object
+  void parse(const std::string & unit_string);
+
+  /// simplify into the canonical form that permits comparisons
+  void simplify();
+
+  ///@{ data tables with SI prefixes and known units
+  static const std::map<std::string, Real> _si_prefix;
+  static const std::vector<std::pair<std::string, MooseUnits>> _unit_table;
+  ///@}
+
+  ///@{ query the nature of the unit
+  bool isLength() const { return isBase(BaseUnit::METER); }
+  bool isTime() const { return isBase(BaseUnit::SECOND); }
+  bool isMass() const { return isBase(BaseUnit::KILOGRAM); }
+  bool isCurrent() const { return isBase(BaseUnit::AMPERE); }
+  bool isTemperature() const { return isBase(BaseUnit::KELVIN); }
+  ///@}
+
+  MooseUnits operator*(Real f) const;
+  MooseUnits operator*(const MooseUnits & rhs) const;
+  MooseUnits operator/(const MooseUnits & rhs) const;
+  bool operator==(const MooseUnits & rhs) const;
+  bool operator==(Real rhs) const;
+  explicit operator Real() const;
+
+  friend std::ostream & operator<<(std::ostream & os, const MooseUnits & dt);
+  friend MooseUnits std::pow(const MooseUnits &, int);
+
+protected:
+  /// helper function to generate a pretty mooseError
+  template <typename... Args>
+  void parseError(const std::string & unit_string, std::string::const_iterator it, Args... args);
+
+  /// conversion factor w.r.t. the base SI units
+  Real _factor;
+
+  /// check if the unit has a pure base
+  bool isBase(MooseUnits::BaseUnit) const;
+
+  /// base SI units and their exponents
+  std::vector<std::pair<BaseUnit, int>> _base;
+};
+
+template <typename... Args>
+void
+MooseUnits::parseError(const std::string & unit_string,
+                       std::string::const_iterator it,
+                       Args... args)
+{
+  auto d = std::distance(unit_string.begin(), it);
+  mooseError("At position ", d, " in ", unit_string, ": ", std::forward<Args>(args)...);
+}
---
+++ framework/src/utils/Units.C
@@ -0,0 +1,536 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "Units.h"
+#include <stack>
+#include <algorithm>
+
+const std::map<std::string, Real> MooseUnits::_si_prefix = {{"E", 1e18},
+                                                            {"P", 1e15},
+                                                            {"T", 1e12},
+                                                            {"G", 1e9},
+                                                            {"M", 1e6},
+                                                            {"k", 1e3},
+                                                            {"h", 1e2},
+                                                            {"da", 1e1},
+                                                            {"d", 1e-1},
+                                                            {"c", 1e-2},
+                                                            {"m", 1e-3},
+                                                            {"mu", 1e-6},
+                                                            {"μ", 1e-6},
+                                                            {"n", 1e-9},
+                                                            {"p", 1e-12},
+                                                            {"f", 1e-15},
+                                                            {"a", 1e-18},
+                                                            {"z", 1e-21},
+                                                            {"y", 1e-24}};
+
+const std::vector<std::pair<std::string, MooseUnits>> MooseUnits::_unit_table = {
+    // avoid matching meters
+    {"Ohm",
+     {
+         1,
+         {{MooseUnits::BaseUnit::KILOGRAM, 1},
+          {MooseUnits::BaseUnit::METER, 2},
+          {MooseUnits::BaseUnit::SECOND, -3},
+          {MooseUnits::BaseUnit::AMPERE, -2}},
+     }},
+    {"atm",
+     {101.325e3,
+      {{MooseUnits::BaseUnit::KILOGRAM, 1},
+       {MooseUnits::BaseUnit::METER, -1},
+       {MooseUnits::BaseUnit::SECOND, -2}}}}, // Standard atmosphere
+    // avoid matching volts
+    {"eV",
+     {
+         1.602176634e-19,
+         {{MooseUnits::BaseUnit::KILOGRAM, 1},
+          {MooseUnits::BaseUnit::METER, 2},
+          {MooseUnits::BaseUnit::SECOND, -2}},
+     }}, // electronvolt
+    // avoid matching grams
+    {"erg",
+     {
+         1e-7,
+         {{MooseUnits::BaseUnit::KILOGRAM, 1},
+          {MooseUnits::BaseUnit::METER, 2},
+          {MooseUnits::BaseUnit::SECOND, -2}},
+     }},                                                  // erg
+    {"Ang", {1e-10, {{MooseUnits::BaseUnit::METER, 1}}}}, // Angstrom
+
+    // Base units
+    {"m", {1, {{MooseUnits::BaseUnit::METER, 1}}}},
+    {"g", {0.001, {{MooseUnits::BaseUnit::KILOGRAM, 1}}}},
+    {"s", {1, {{MooseUnits::BaseUnit::SECOND, 1}}}},
+    {"A", {1, {{MooseUnits::BaseUnit::AMPERE, 1}}}},
+    {"K", {1, {{MooseUnits::BaseUnit::KELVIN, 1}}}},
+    {"mol", {6.02214076e23, {{MooseUnits::BaseUnit::COUNT, 1}}}},
+    {"cd", {1, {{MooseUnits::BaseUnit::CANDELA, 1}}}},
+
+    // Derived units
+    {"N",
+     {1,
+      {{MooseUnits::BaseUnit::KILOGRAM, 1},
+       {MooseUnits::BaseUnit::METER, 1},
+       {MooseUnits::BaseUnit::SECOND, -2}}}}, // Newton
+    {"Pa",
+     {1,
+      {{MooseUnits::BaseUnit::KILOGRAM, 1},
+       {MooseUnits::BaseUnit::METER, -1},
+       {MooseUnits::BaseUnit::SECOND, -2}}}}, // Pascal
+    {"J",
+     {1,
+      {{MooseUnits::BaseUnit::KILOGRAM, 1},
+       {MooseUnits::BaseUnit::METER, 2},
+       {MooseUnits::BaseUnit::SECOND, -2}}}}, // Joule
+    {"W",
+     {1,
+      {{MooseUnits::BaseUnit::KILOGRAM, 1},
+       {MooseUnits::BaseUnit::METER, 2},
+       {MooseUnits::BaseUnit::SECOND, -3}}}},                                           // Watt
+    {"C", {1, {{MooseUnits::BaseUnit::AMPERE, 1}, {MooseUnits::BaseUnit::SECOND, 1}}}}, // Coulomb
+    {"V",
+     {
+         1,
+         {{MooseUnits::BaseUnit::KILOGRAM, 1},
+          {MooseUnits::BaseUnit::METER, 2},
+          {MooseUnits::BaseUnit::SECOND, -3},
+          {MooseUnits::BaseUnit::AMPERE, -1}},
+     }},
+    {"F",
+     {1,
+      {{MooseUnits::BaseUnit::KILOGRAM, -1},
+       {MooseUnits::BaseUnit::METER, -2},
+       {MooseUnits::BaseUnit::SECOND, 4},
+       {MooseUnits::BaseUnit::AMPERE, 2}}}},
+    {"S",
+     {1,
+      {{MooseUnits::BaseUnit::KILOGRAM, -1},
+       {MooseUnits::BaseUnit::METER, -2},
+       {MooseUnits::BaseUnit::SECOND, 3},
+       {MooseUnits::BaseUnit::AMPERE, 2}}}}, // Siemens = 1/Ohm (electrical conductance)
+    {"Wb",
+     {1,
+      {{MooseUnits::BaseUnit::KILOGRAM, 1},
+       {MooseUnits::BaseUnit::METER, 2},
+       {MooseUnits::BaseUnit::SECOND, -2},
+       {MooseUnits::BaseUnit::AMPERE, -1}}}}, // Weber (magnetic flux)
+    {"T",
+     {1,
+      {{MooseUnits::BaseUnit::KILOGRAM, 1},
+       {MooseUnits::BaseUnit::SECOND, -2},
+       {MooseUnits::BaseUnit::AMPERE, -1}}}}, // Tesla (magnetic flux density)
+    {"H",
+     {1,
+      {{MooseUnits::BaseUnit::KILOGRAM, 1},
+       {MooseUnits::BaseUnit::METER, 2},
+       {MooseUnits::BaseUnit::SECOND, -2},
+       {MooseUnits::BaseUnit::AMPERE, -2}}}}, // Henry (inductance)
+
+    // cgs units
+    {"Ba",
+     {0.1,
+      {{MooseUnits::BaseUnit::KILOGRAM, 1},
+       {MooseUnits::BaseUnit::METER, -1},
+       {MooseUnits::BaseUnit::SECOND,
+        -2}}}}, // barye (unit of pressure - not to be confused with bar)
+    {"dyn",
+     {1e-5,
+      {{MooseUnits::BaseUnit::KILOGRAM, 1},
+       {MooseUnits::BaseUnit::METER, 1},
+       {MooseUnits::BaseUnit::SECOND, -2}}}}, // dyne
+
+    // Freedom units
+    {"ft", {0.3048, {{MooseUnits::BaseUnit::METER, 1}}}},        // foot
+    {"in", {25.4e-3, {{MooseUnits::BaseUnit::METER, 1}}}},       // inch
+    {"lb", {0.45359237, {{MooseUnits::BaseUnit::KILOGRAM, 1}}}}, // pound of mass
+    {"lbf",
+     {4.4482216152605,
+      {{MooseUnits::BaseUnit::KILOGRAM, 1},
+       {MooseUnits::BaseUnit::METER, 1},
+       {MooseUnits::BaseUnit::SECOND, -2}}}}, // pound of force
+    {"psi",
+     {6.894757e3,
+      {{MooseUnits::BaseUnit::KILOGRAM, 1},
+       {MooseUnits::BaseUnit::METER, -1},
+       {MooseUnits::BaseUnit::SECOND, -2}}}}, // pound-force per square inch
+
+    // misc.
+    {"bar",
+     {1e5,
+      {{MooseUnits::BaseUnit::KILOGRAM, 1},
+       {MooseUnits::BaseUnit::METER, -1},
+       {MooseUnits::BaseUnit::SECOND, -2}}}},          // bar (unit of pressure)
+    {"l", {1e-3, {{MooseUnits::BaseUnit::METER, 3}}}}, // liter
+    {"at", {1, {{MooseUnits::BaseUnit::COUNT, 1}}}}    // 1 single count (atom)
+};
+
+MooseUnits::MooseUnits(const std::string & unit_string) : _factor(1.0), _base()
+{
+  // parse the passed in unit string
+  parse(unit_string);
+}
+
+bool
+MooseUnits::conformsTo(const MooseUnits & u) const
+{
+  if (_base.size() != u._base.size())
+    return false;
+  for (std::size_t i = 0; i < _base.size(); ++i)
+    if (_base[i] != u._base[i])
+      return false;
+
+  return true;
+}
+
+Real
+MooseUnits::to(const MooseUnits & u) const
+{
+  if (!conformsTo(u))
+    mooseError("Cannot convert between non-conforming units '", *this, "' and '", u, "'.");
+  return _factor / u._factor;
+}
+
+void
+MooseUnits::parse(const std::string & unit_string)
+{
+  // we need to understand * / ^int ( ) string
+  // m^2*kg/(N*s)^2
+  // no numerical expressions are permitted (e.g. no 1/2 0.5 4*2)
+
+  // parse stack
+  std::stack<std::vector<std::pair<MooseUnits::BaseUnit, int>>> stack;
+  std::stack<char> op_stack;
+  std::stack<MooseUnits> out;
+
+  auto it = unit_string.begin();
+  const auto end = unit_string.end();
+
+  do
+  {
+    // skip whitespace
+    if (*it == ' ')
+    {
+      it++;
+      continue;
+    }
+    // opening parenthesis
+    else if (*it == '(')
+      op_stack.push(*(it++));
+    // multiply or divide
+    else if (*it == '*' || *it == '/')
+    {
+      // pop */ off
+      while (!op_stack.empty() && (op_stack.top() == '*' || op_stack.top() == '/'))
+      {
+        auto top = op_stack.top();
+        op_stack.pop();
+
+        if (out.size() < 2)
+          parseError(
+              unit_string, it, "Applying ", top, " but don't have enough items on the stack.");
+        auto rhs = out.top();
+        out.pop();
+        if (top == '*')
+          out.top() = out.top() * rhs;
+        else
+          out.top() = out.top() / rhs;
+      }
+
+      op_stack.push(*(it++));
+    }
+    // closing parenthesis
+    else if (*it == ')')
+    {
+      do
+      {
+        if (op_stack.empty())
+          parseError(unit_string, it, "Mismatching right parenthesis.");
+
+        auto top = op_stack.top();
+        op_stack.pop();
+
+        if (top == '(')
+          break;
+
+        if (top == '*' || top == '/')
+        {
+          if (out.size() < 2)
+            parseError(
+                unit_string, it, "Applying ", top, " but don't have enough items on the stack.");
+          auto rhs = out.top();
+          out.pop();
+          if (top == '*')
+            out.top() = out.top() * rhs;
+          else
+            out.top() = out.top() / rhs;
+        }
+      } while (true);
+      it++;
+    }
+    // exponent
+    else if (*it == '^')
+    {
+      // check for sign
+      int sign = 1;
+      ++it;
+      if (it != end && *it == '-')
+      {
+        sign = -1;
+        it++;
+      }
+
+      int num = 0;
+      bool valid = false;
+      while (*it >= '0' && *it <= '9')
+      {
+        num *= 10;
+        num += *(it++) - '0';
+        valid = true;
+      }
+
+      // error
+      if (!valid)
+      {
+        if (it == end)
+          parseError(unit_string, it, "Expected a number but found end of string.");
+        else
+          parseError(unit_string, it, "Expected a number but got '", (*it), "'.");
+      }
+
+      // exponents do not get pushed on the stack, but are applied immediately
+      if (out.empty())
+        parseError(unit_string, it, "Applying an exponent, but found no units in front of it.");
+      out.top() = std::pow(out.top(), sign * num);
+    }
+    else
+    {
+      // consume unit symbol
+      std::string unit;
+      while (it != end && ((*it >= 'a' && *it <= 'z') || (*it >= 'A' && *it <= 'Z'))) // no µ :(
+        unit += *(it++);
+
+      // parse the unit and a potential SI prefix
+      auto len = unit.length();
+      if (len == 0)
+      {
+        if (it == end)
+          parseError(unit_string, it, "Expeced unit but found end of string.");
+        else
+          parseError(unit_string, it, "Expeced unit but found '", *it, "'.");
+      }
+
+      unsigned int i = 0;
+      for (; i < _unit_table.size(); ++i)
+      {
+        const auto & s = _unit_table[i].first;
+        const auto slen = s.length();
+        if (slen <= len && unit.substr(len - slen) == s)
+          break;
+      }
+
+      // invalid unit
+      if (i == _unit_table.size())
+        parseError(unit_string, it, "Unknown unit '", unit, "'.");
+
+      // get prefix
+      const auto & s = _unit_table[i].first;
+      const auto slen = s.length();
+      Real si_factor = 1.0;
+      if (slen < len)
+      {
+        const auto prefix = unit.substr(0, len - slen);
+        auto jt = _si_prefix.find(prefix);
+        if (jt != _si_prefix.end())
+          si_factor = jt->second;
+        else
+          parseError(unit_string, it, "Unknown SI prefix '", prefix, "' on unit '", unit, "'.");
+      }
+
+      // construct a unit object and push to the output stack
+      MooseUnits d = _unit_table[i].second * si_factor;
+      out.push(d);
+    }
+  } while (it != end);
+
+  // unwind operator stack
+  while (!op_stack.empty())
+  {
+    auto top = op_stack.top();
+    op_stack.pop();
+
+    if (top == '(' || top == ')')
+      parseError(unit_string, it, "Unit string contains unmatched parenthesis.");
+
+    if (top == '*' || top == '/')
+    {
+      if (out.size() < 2)
+        parseError(unit_string,
+                   it,
+                   "Applying ",
+                   top,
+                   " but don't have enough items on the stack.",
+                   out.size());
+      auto rhs = out.top();
+      out.pop();
+      if (top == '*')
+        out.top() = out.top() * rhs;
+      else
+        out.top() = out.top() / rhs;
+    }
+  }
+
+  // consistency check
+  if (out.size() != 1)
+    mooseError("Internal parse error. Remaining output stach size sould be 1, but is ", out.size());
+
+  *this = out.top();
+  simplify();
+}
+
+MooseUnits MooseUnits::operator*(Real f) const
+{
+  MooseUnits u = *this;
+  u._factor *= f;
+  return u;
+}
+
+MooseUnits MooseUnits::operator*(const MooseUnits & rhs) const
+{
+  MooseUnits u = rhs;
+  u._factor *= _factor;
+  u._base.insert(u._base.end(), _base.begin(), _base.end());
+  u.simplify();
+  return u;
+}
+
+MooseUnits
+MooseUnits::operator/(const MooseUnits & rhs) const
+{
+  MooseUnits u = rhs;
+  u._factor = _factor / u._factor;
+  for (auto & b : u._base)
+    b.second *= -1;
+  u._base.insert(u._base.end(), _base.begin(), _base.end());
+  u.simplify();
+  return u;
+}
+
+bool
+MooseUnits::operator==(const MooseUnits & rhs) const
+{
+  if (_factor != rhs._factor)
+    return false;
+  return conformsTo(rhs);
+}
+
+bool
+MooseUnits::operator==(Real rhs) const
+{
+  return (_factor == rhs && _base.empty());
+}
+
+MooseUnits::operator Real() const
+{
+  if (!_base.empty())
+    mooseError("Unit '", *this, "' is not a pure number.");
+  return _factor;
+}
+
+void
+MooseUnits::simplify()
+{
+  std::map<BaseUnit, int> base_map;
+  for (auto & b : _base)
+  {
+    auto j = base_map.find(b.first);
+    if (j == base_map.end())
+      base_map.insert(b);
+    else
+      j->second += b.second;
+  }
+
+  // replace base vector with map contents
+  _base.clear();
+  for (auto & u : base_map)
+    if (u.second != 0)
+      _base.push_back(u);
+}
+
+bool
+MooseUnits::isBase(MooseUnits::BaseUnit base) const
+{
+  // must have only one base unit
+  if (_base.size() != 1)
+    return false;
+
+  // its exponent must be one
+  if (_base[0].second != 1)
+    return false;
+
+  // and it has to be the right base unit
+  return (_base[0].first == base);
+}
+
+namespace std
+{
+MooseUnits
+pow(const MooseUnits & u, int e)
+{
+  MooseUnits r = u;
+  r._factor = std::pow(u._factor, e);
+  for (auto & b : r._base)
+    b.second *= e;
+  return r;
+}
+}
+
+std::ostream &
+operator<<(std::ostream & os, const MooseUnits & u)
+{
+  if (u._factor != 1.0 || u._base.empty())
+  {
+    os << u._factor;
+    if (!u._base.empty())
+      os << ' ';
+  }
+
+  for (unsigned int i = 0; i < u._base.size(); ++i)
+  {
+    os << (i ? "*" : "");
+    switch (u._base[i].first)
+    {
+      case MooseUnits::BaseUnit::METER:
+        os << "m";
+        break;
+      case MooseUnits::BaseUnit::KILOGRAM:
+        os << "kg";
+        break;
+      case MooseUnits::BaseUnit::SECOND:
+        os << "s";
+        break;
+      case MooseUnits::BaseUnit::AMPERE:
+        os << "A";
+        break;
+      case MooseUnits::BaseUnit::KELVIN:
+        os << "K";
+        break;
+      case MooseUnits::BaseUnit::COUNT:
+        os << "at";
+        break;
+      case MooseUnits::BaseUnit::CANDELA:
+        os << "cd";
+        break;
+    }
+    if (u._base[i].second != 1)
+      os << '^' << u._base[i].second;
+  }
+
+  return os;
+}
