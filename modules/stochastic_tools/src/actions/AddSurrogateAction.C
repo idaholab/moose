49dd6a8a3e7d0acd4fcd9a8aada040919c4f20b4
Andrew E Slaughter <andrew.slaughter@inl.gov>
2020-03-16 12:53:17 -0600

    Implement save/load of surrogate model data
    (refs #14875)

--- modules/stochastic_tools/src/actions/AddSurrogateAction.C
+++ modules/stochastic_tools/src/actions/AddSurrogateAction.C
@@ -10,6 +10,7 @@
 #include "AddSurrogateAction.h"
 #include "Factory.h"
 #include "FEProblem.h"
+#include "UserObject.h"
 
 registerMooseAction("StochasticToolsApp", AddSurrogateAction, "add_surrogate");
 
@@ -27,5 +28,79 @@ AddSurrogateAction::AddSurrogateAction(InputParameters params) : MooseObjectActi
 void
 AddSurrogateAction::act()
 {
+  // If the 'filename' parameter is supplied then the model is NOT being trained:
+  //    (1) the 'execute_on' is set to NONE automatically; if the user has set
+  //        the 'execute_on' parameter produce a warning.
+  //    (2) if any training parameters are set then produce a warning that the model is being
+  //        evaluated but training parameters are set
+  //
+  // If the 'filename' parameters is not set then the model IS being trained
+  //    (1) check that all 'training' parameters are valid and error if they are not; this
+  //        allows all training parameters to be optional
+  bool warn_execute_on = false;           // use a flag so a paramWarning can be called
+  std::set<std::string> warn_no_training; // "training" parameters set, but not training
+  std::set<std::string> err_yes_training; // "training" parameters not valid, but training
+  if (_moose_object_pars.isParamValid("filename"))
+  {
+    if (!_moose_object_pars.get<ExecFlagEnum>("execute_on").contains("NONE"))
+      warn_execute_on = _moose_object_pars.isParamSetByUser("execute_on");
+    _moose_object_pars.set<ExecFlagEnum>("execute_on") = "NONE";
+
+    // Loop through all parameters in "training" group, if they are set then warning as well
+    for (const std::string & name : _moose_object_pars.getGroupParameters("training"))
+      if (_moose_object_pars.isParamSetByUser(name))
+        warn_no_training.emplace(name);
+  }
+  else
+  {
+    for (const std::string & name : _moose_object_pars.getGroupParameters("training"))
+      if (!_moose_object_pars.isParamValid(name))
+        err_yes_training.emplace(name);
+  }
+
+  // Create the actual object
   _problem->addUserObject(_type, _name, _moose_object_pars);
+
+  if (warn_execute_on)
+  {
+    const UserObject & obj = _problem->getUserObjectBase(_name);
+    obj.paramWarning("execute_on",
+                     "The '",
+                     obj.name(),
+                     "' model is setup for evaluation because the 'filename' parameter has been "
+                     "set. Howerver, the 'execute_on' parameter has also been set. When a model is "
+                     "setup for evaluation the execution of the object is automatically disabled "
+                     "as it is expected that the evaluate method is being used directly.");
+  }
+
+  if (!warn_no_training.empty())
+  {
+    const UserObject & obj = _problem->getUserObjectBase(_name);
+    for (const std::string & name : warn_no_training)
+      obj.paramWarning(name,
+                       "The '",
+                       obj.name(),
+                       "' model is setup for evaluation because the 'filename' parameter has been "
+                       "set. However, the training parameter '",
+                       name,
+                       "' is also defined. Training parameters should not be set when the model is "
+                       "setup for evaluation.");
+  }
+
+  if (!err_yes_training.empty())
+  {
+    const UserObject & obj = _problem->getUserObjectBase(_name);
+    for (const std::string & name : err_yes_training)
+      obj.paramWarning(name,
+                       "The '",
+                       obj.name(),
+                       "' model is setup for training but the training parameter '",
+                       name,
+                       "' is missing or invalid.");
+
+    ::mooseError("The '",
+                 obj.name(),
+                 "' model is setup for training but one or more training parameters are missing or "
+                 "invalid.");
+  }
 }
---
+++ modules/stochastic_tools/test/tests/surrogates/load_store/sub.i
@@ -0,0 +1,85 @@
+[Mesh]
+  type = GeneratedMesh
+  dim = 1
+  nx = 100
+  xmax = 10
+[]
+
+[Variables]
+  [u]
+  []
+[]
+
+[Kernels]
+  [diffusion]
+    type = MatDiffusion
+    variable = u
+    diffusivity = D
+  []
+  [absorption]
+    type = MaterialReaction
+    variable = u
+    coefficient = sig
+  []
+  [source]
+    type = BodyForce
+    variable = u
+    value = 1.0
+  []
+[]
+
+[Materials]
+  [diffusivity]
+    type = GenericConstantMaterial
+    prop_names = D
+    prop_values = 2.0
+  []
+  [xs]
+    type = GenericConstantMaterial
+    prop_names = sig
+    prop_values = 2.0
+  []
+[]
+
+[BCs]
+  [left]
+    type = DirichletBC
+    variable = u
+    boundary = left
+    value = 0
+  []
+  [right]
+    type = DirichletBC
+    variable = u
+    boundary = right
+    value = 0
+  []
+[]
+
+[Executioner]
+  type = Steady
+  solve_type = PJFNK
+  petsc_options_iname = '-pc_type -pc_hypre_type'
+  petsc_options_value = 'hypre boomeramg'
+[]
+
+[Controls]
+  [stochastic]
+    type = SamplerReceiver
+  []
+[]
+
+[Postprocessors]
+  [avg]
+    type = AverageNodalVariableValue
+    variable = u
+  []
+  [max]
+    type = NodalExtremeValue
+    variable = u
+    value_type = max
+  []
+[]
+
+[Outputs]
+[]
