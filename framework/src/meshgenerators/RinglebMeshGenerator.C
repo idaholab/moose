15e5121f442861b76b5a94a1720f6b218fc7489f
Alex Lindsay <alexlindsay239@gmail.com>
2020-03-19 12:26:34 -0700

    Explicitly set `skip_find_neighbors=false`
    libmesh/libmesh#2473 will hopefully add an allow_find_neighbors
    API that has the same structure as allow_renumbering. However,
    in for it to work existing users of `prepare_for_use` need to
    explicitly state whether they want to `find_neighbors` or not

--- framework/src/meshgenerators/RinglebMeshGenerator.C
+++ framework/src/meshgenerators/RinglebMeshGenerator.C
@@ -206,7 +206,7 @@ RinglebMeshGenerator::generate()
   }
 
   /// Find neighbors, etc.
-  mesh->prepare_for_use();
+  mesh->prepare_for_use(false, false);
 
   /// Create the triangular elements if required by the user
   if (_triangles)
