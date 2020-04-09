57895a67306377e2f365d13754cbf8b5a33bf6a6
Casey Icenhour <cticenho@ncsu.edu>
2020-04-08 21:44:11 -0600

    Add current continuity interface kernel, and modify test input file to use it.

---
+++ modules/electromagnetics/include/interfacekernels/ElectrostaticCurrentContinuity.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "InterfaceKernel.h"
+
+/**
+ *
+ */
+class ElectrostaticCurrentContinuity : public InterfaceKernel
+{
+public:
+  static InputParameters validParams();
+
+  ElectrostaticCurrentContinuity(const InputParameters & parameters);
+
+protected:
+  virtual Real computeQpResidual(Moose::DGResidualType type) override;
+  virtual Real computeQpJacobian(Moose::DGJacobianType type) override;
+
+  const MaterialProperty<Real> & _conductivity_master;
+  const MaterialProperty<Real> & _conductivity_neighbor;
+};
