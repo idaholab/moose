15cabb2183b0bba2d397961dbaf9079270b3290c
WilkAndy <andrew.wilkins@csiro.au>
2020-04-22 07:02:58 +1000

    GeochemistryConstants.h added
    Fixes #15117

---
+++ modules/geochemistry/include/utils/GeochemistryConstants.h
@@ -0,0 +1,15 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "MooseTypes.h"
+
+constexpr Real MOLES_PER_KG_WATER = 55.51;
+constexpr Real LOGTEN = 2.30258509299404;
