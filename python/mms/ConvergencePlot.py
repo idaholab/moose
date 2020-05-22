1791825fafb31670fd30b881f4fd5452faaecc1f
Alex Lindsay <alexlindsay239@gmail.com>
2020-05-22 11:44:11 -0700

    Make mms more flexible

--- python/mms/ConvergencePlot.py
+++ python/mms/ConvergencePlot.py
@@ -46,21 +46,46 @@ class ConvergencePlot(object):
         # Apply grid marks
         plt.grid(True, which='both', color=[0.8]*3)
 
-    def plot(self, df, label=None, **kwargs):
+    def plot(self, df, label=None, title=None, num_fitted_points=None, slope_precision=3, **kwargs):
+        num_y_columns = len(df.columns) - 1
+
+        if label:
+            if num_y_columns > 1:
+                if not isinstance(label, list):
+                    raise TypeError("For multiple y-data label must be a list")
+
+
+            if isinstance(label, list) and num_y_columns != len(label):
+                raise IOError("The length of the label and the number of y columns must be the same")
+
+            if not isinstance(label, list):
+                label = [label]
 
         x = df[df.columns[0]]
-        y = df[df.columns[1]]
+        lines = []
 
-        if label is None:
-            label = 'line-{}'.format(len(self._axes.lines))
+        for i in range(1,len(df.columns)):
+            y = df[df.columns[i]]
 
-        slope = self._fit(x, y)
-        label = '{}: {:.3f}'.format(label, slope)
+            if label is None:
+                this_label = 'line-{}'.format(len(lines))
+            else:
+                this_label = label[i-1]
 
-        line, = self._axes.plot(x, y, label=label, **kwargs)
+            if num_fitted_points is not None:
+                slope = self._fit(x[-num_fitted_points:], y[-num_fitted_points:])
+            else:
+                slope = self._fit(x, y)
+
+            this_label = '{}: {:.{precision}f}'.format(this_label, slope, precision=slope_precision)
+
+            lines.append(self._axes.plot(x, y, label=this_label, **kwargs)[0])
+
+        if title:
+            self._axes.set_title(title)
 
         self._axes.legend()
-        return line
+        return lines
 
     def _fit(self, x, y):
         """
