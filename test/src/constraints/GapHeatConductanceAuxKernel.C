576fbbef045fc83b8985c182f948f4e011007cfc
Antonio Recuero <amrecuero85@gmail.com>
2020-06-03 11:32:08 -0600

    Add test exercising coupled vars between aux kernel and mortar constraint

---
+++ test/src/constraints/GapHeatConductanceAuxKernel.C
@@ -0,0 +1,77 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "GapHeatConductanceAuxKernel.h"
+
+registerMooseObject("MooseTestApp", GapHeatConductanceAuxKernel);
+
+InputParameters
+GapHeatConductanceAuxKernel::validParams()
+{
+  InputParameters params = ADMortarConstraint::validParams();
+  params.addParam<Real>("gap_conductance_constant",
+                        0.03,
+                        "The numerator in calculation of the heat transfer coefficient");
+  params.addParam<MaterialPropertyName>(
+      "slave_gap_conductance",
+      "gap_conductance",
+      "The material property name providing the gap conductance on the slave side");
+  params.addParam<MaterialPropertyName>(
+      "master_gap_conductance",
+      "gap_conductance",
+      "The material property name providing the gap conductance on the master side");
+  params.addRequiredCoupledVar(
+      "auxkernel_variable", "The auxiliary kernel variable that uses a lower-d for compute values");
+
+  return params;
+}
+
+GapHeatConductanceAuxKernel::GapHeatConductanceAuxKernel(const InputParameters & parameters)
+  : ADMortarConstraint(parameters),
+    _slave_gap_conductance(getADMaterialProperty<Real>("slave_gap_conductance")),
+    _master_gap_conductance(getNeighborADMaterialProperty<Real>("master_gap_conductance")),
+    _aux_variable(coupledValue("auxkernel_variable")),
+    _aux_variable_old(coupledValueOld("auxkernel_variable"))
+{
+}
+
+ADReal
+GapHeatConductanceAuxKernel::computeQpResidual(Moose::MortarType type)
+{
+  switch (type)
+  {
+    case Moose::MortarType::Slave:
+      return _lambda[_qp] * _test_slave[_i][_qp];
+
+    case Moose::MortarType::Master:
+      return -_lambda[_qp] * _test_master[_i][_qp];
+
+    case Moose::MortarType::Lower:
+    {
+      ADReal heat_transfer_coeff(0);
+      if (_has_master)
+      {
+        auto gap = (_phys_points_slave[_qp] - _phys_points_master[_qp]).norm();
+        heat_transfer_coeff =
+            (0.5 * (_slave_gap_conductance[_qp] + _master_gap_conductance[_qp])) / gap;
+      }
+
+      // Modify heat transfer coefficient with auxiliary kernel variable
+      if (_aux_variable[_qp] != 0.0)
+        heat_transfer_coeff *= 0.5 * (_aux_variable[_qp] + _aux_variable_old[_qp]);
+
+      return _test[_i][_qp] *
+             (_lambda[_qp] -
+              heat_transfer_coeff * (_u_slave[_qp] - (_has_master ? _u_master[_qp] : 0)));
+    }
+
+    default:
+      return 0;
+  }
+}
---
+++ test/tests/mortar/displaced-gap-conductance-2d-auxkernel/gap-conductance.i
@@ -0,0 +1,142 @@
+[Mesh]
+  displacements = 'disp_x disp_y'
+  [file]
+    type = FileMeshGenerator
+    file = nodal_normals_test_offset_nonmatching_gap.e
+    # block 1: left
+    # block 2: right
+  []
+  [./master]
+    input = file
+    type = LowerDBlockFromSidesetGenerator
+    sidesets = '2'
+    new_block_id = '20'
+  [../]
+  [./slave]
+    input = master
+    type = LowerDBlockFromSidesetGenerator
+    sidesets = '1'
+    new_block_id = '10'
+  [../]
+[]
+
+[AuxVariables]
+  [disp_x]
+    block = '1 2'
+  []
+  [disp_y]
+    block = '1 2'
+  []
+  [aux_var]
+  []
+[]
+
+[AuxKernels]
+  [function_x]
+    type = FunctionAux
+    function = '.05 * t'
+    variable = 'disp_x'
+    block = '2'
+  []
+  [function_y]
+    type = FunctionAux
+    function = '.05 * t'
+    variable = 'disp_y'
+    block = '2'
+  []
+  [flux_modifier]
+    type = StatefulAuxLowerD
+    variable = 'aux_var'
+    coupled_variable = 'lambda'
+    boundary = '1'
+  []
+[]
+
+[Problem]
+  kernel_coverage_check = false
+[]
+
+[Variables]
+  [./T]
+    block = '1 2'
+  [../]
+  [./lambda]
+    block = '10'
+    family = LAGRANGE
+    order = FIRST
+  [../]
+[]
+
+[BCs]
+  [./left]
+    type = DirichletBC
+    variable = T
+    boundary = '5'
+    value = 0
+  [../]
+  [./right]
+    type = DirichletBC
+    variable = T
+    boundary = '8'
+    value = 1
+  [../]
+[]
+
+[Kernels]
+  [./conduction]
+    type = Diffusion
+    variable = T
+    block = '1 2'
+  [../]
+[]
+
+[Debug]
+  show_var_residual_norms = 1
+[]
+
+[Constraints]
+  [./mortar]
+    type = GapHeatConductanceAuxKernel
+    master_boundary = 2
+    slave_boundary = 1
+    master_subdomain = 20
+    slave_subdomain = 10
+    variable = lambda
+    slave_variable = T
+    use_displaced_mesh = true
+    auxkernel_variable = 'aux_var'
+  [../]
+[]
+
+[Materials]
+  [constant]
+    type = ADGenericConstantMaterial
+    prop_names = 'gap_conductance'
+    prop_values = '.03'
+    block = '1 2'
+    use_displaced_mesh = true
+  []
+[]
+
+[Preconditioning]
+  [./smp]
+    type = SMP
+    full = true
+  [../]
+[]
+
+[Executioner]
+  solve_type = NEWTON
+  type = Transient
+  num_steps = 5
+  petsc_options_iname = '-pc_type -snes_linesearch_type'
+  petsc_options_value = 'lu       basic'
+[]
+
+[Outputs]
+  exodus = true
+  [dofmap]
+    type = DOFMap
+    execute_on = 'initial'
+  []
+[]
