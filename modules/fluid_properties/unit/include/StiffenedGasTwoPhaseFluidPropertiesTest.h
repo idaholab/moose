a1ff5a91700591ccdff0d96b2f8f4e1e5dac1cd6
hansje <joshua.hansel@inl.gov>
2020-06-03 19:34:01 -0600

    Added triple-point temperature interface to TwoPhaseFluidProperties
    Closes #15396

--- modules/fluid_properties/unit/include/StiffenedGasTwoPhaseFluidPropertiesTest.h
+++ modules/fluid_properties/unit/include/StiffenedGasTwoPhaseFluidPropertiesTest.h
@@ -7,12 +7,17 @@
 class StiffenedGasTwoPhaseFluidPropertiesTest : public MooseObjectUnitTest
 {
 public:
-  StiffenedGasTwoPhaseFluidPropertiesTest() : MooseObjectUnitTest("FluidPropertiesApp") { buildObjects(); }
+  StiffenedGasTwoPhaseFluidPropertiesTest()
+    : MooseObjectUnitTest("FluidPropertiesApp"), _T_triple(300.0)
+  {
+    buildObjects();
+  }
 
 protected:
   void buildObjects()
   {
     InputParameters uo_pars = _factory.getValidParams("StiffenedGasTwoPhaseFluidProperties");
+    uo_pars.set<Real>("T_triple") = _T_triple;
     _fe_problem->addUserObject("StiffenedGasTwoPhaseFluidProperties", "fp", uo_pars);
     _fp = &_fe_problem->getUserObject<StiffenedGasTwoPhaseFluidProperties>("fp");
     _fp_liquid = &_fe_problem->getUserObject<StiffenedGasFluidProperties>(_fp->getLiquidName());
@@ -22,4 +27,6 @@ protected:
   const StiffenedGasTwoPhaseFluidProperties * _fp;
   const StiffenedGasFluidProperties * _fp_liquid;
   const StiffenedGasFluidProperties * _fp_vapor;
+
+  const Real _T_triple;
 };
