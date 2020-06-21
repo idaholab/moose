c0072f11adf1850d31aeeb5fb03c73d452483765
Casey Icenhour <cticenho@ncsu.edu>
2020-06-20 23:37:41 -0600

    Remove master/neighbor relationships from ELK-only class members and replace with primary/secondary.
    Refs #26

--- modules/electromagnetics/src/interfacekernels/PerpendicularElectricFieldInterface.C
+++ modules/electromagnetics/src/interfacekernels/PerpendicularElectricFieldInterface.C
@@ -9,17 +9,21 @@ validParams<PerpendicularElectricFieldInterface>()
   InputParameters params = validParams<VectorInterfaceKernel>();
   params.addClassDescription("VectorInterfaceKernel that implements the condtion $\\epsilon_1 \\vec{E}_{1}^{\\perp} - \\epsilon_2 \\vec{E}_{2}^{\\perp} = \\sigma_f$");
   params.addParam<Real>("free_charge", 0.0, "Free charge on the interface (default = 0).");
-  params.addParam<Real>("master_epsilon", 1.0, "Permittivity on the master side of the interface (default = 1.0).");
-  params.addParam<Real>("neighbor_epsilon", 1.0, "Permittivity on the neighbor side of the interface (default = 1.0).");
+  params.addParam<Real>(
+      "primary_epsilon", 1.0, "Permittivity on the primary side of the interface (default = 1.0).");
+  params.addParam<Real>("secondary_epsilon",
+                        1.0,
+                        "Permittivity on the secondary side of the interface (default = 1.0).");
   return params;
 }
 
-PerpendicularElectricFieldInterface::PerpendicularElectricFieldInterface(const InputParameters & parameters)
+PerpendicularElectricFieldInterface::PerpendicularElectricFieldInterface(
+    const InputParameters & parameters)
   : VectorInterfaceKernel(parameters),
 
-  _free_charge(getParam<Real>("free_charge")),
-  _master_eps(getParam<Real>("master_epsilon")),
-  _neighbor_eps(getParam<Real>("neighbor_epsilon"))
+    _free_charge(getParam<Real>("free_charge")),
+    _primary_eps(getParam<Real>("primary_epsilon")),
+    _secondary_eps(getParam<Real>("secondary_epsilon"))
 {
 }
 
@@ -27,18 +31,18 @@ Real
 PerpendicularElectricFieldInterface::computeQpResidual(Moose::DGResidualType type)
 {
   _u_perp = (_u[_qp] * _normals[_qp]) * _normals[_qp];
-  _neighbor_perp = (_neighbor_value[_qp] * _normals[_qp]) * _normals[_qp];
+  _secondary_perp = (_neighbor_value[_qp] * _normals[_qp]) * _normals[_qp];
 
   Real res = 0;
 
   switch (type)
   {
     case Moose::Element:
-      res = _test[_i][_qp] * (_master_eps * _u_perp - _neighbor_eps * _neighbor_perp);
+      res = _test[_i][_qp] * (_primary_eps * _u_perp - _secondary_eps * _secondary_perp);
       break;
 
     case Moose::Neighbor:
-      res =  _test_neighbor[_i][_qp] * -(_master_eps * _u_perp - _neighbor_eps * _neighbor_perp);
+      res = _test_neighbor[_i][_qp] * -(_primary_eps * _u_perp - _secondary_eps * _secondary_perp);
       break;
   }
 
@@ -49,26 +53,26 @@ Real
 PerpendicularElectricFieldInterface::computeQpJacobian(Moose::DGJacobianType type)
 {
   _phi_u_perp = (_phi[_j][_qp] * _normals[_qp]) * _normals[_qp];
-  _phi_neighbor_perp = (_phi_neighbor[_j][_qp] * _normals[_qp]) * _normals[_qp];
+  _phi_secondary_perp = (_phi_neighbor[_j][_qp] * _normals[_qp]) * _normals[_qp];
 
   Real jac = 0;
 
   switch (type)
   {
     case Moose::ElementElement:
-      jac = _test[_i][_qp] * _master_eps * _phi_u_perp;
+      jac = _test[_i][_qp] * _primary_eps * _phi_u_perp;
       break;
 
     case Moose::NeighborNeighbor:
-      jac = _test_neighbor[_i][_qp] * _neighbor_eps * _phi_neighbor_perp;
+      jac = _test_neighbor[_i][_qp] * _secondary_eps * _phi_secondary_perp;
       break;
 
     case Moose::NeighborElement:
-      jac = _test_neighbor[_i][_qp] * -_master_eps * _phi_u_perp;
+      jac = _test_neighbor[_i][_qp] * -_primary_eps * _phi_u_perp;
       break;
 
     case Moose::ElementNeighbor:
-      jac = _test[_i][_qp] * -_neighbor_eps * _phi_neighbor_perp;
+      jac = _test[_i][_qp] * -_secondary_eps * _phi_secondary_perp;
       break;
   }
 
