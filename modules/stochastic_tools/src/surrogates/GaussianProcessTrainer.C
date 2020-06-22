4434af9968a84f34f20f00d5ee2ac8dc5d3de105
Ian Halvic <iwhalvic@gmail.com>
2020-06-22 17:28:38 -0500

    Clang formatted #15482

--- modules/stochastic_tools/src/surrogates/GaussianProcessTrainer.C
+++ modules/stochastic_tools/src/surrogates/GaussianProcessTrainer.C
@@ -24,20 +24,23 @@ GaussianProcessTrainer::validParams()
   params.addRequiredParam<std::string>(
       "results_vector",
       "Name of vector from vectorpostprocessor with results of samples created by trainer");
-MooseEnum kernels = CovarianceFunction::makeCovarianceKernelEnum();
+  MooseEnum kernels = CovarianceFunction::makeCovarianceKernelEnum();
   params.addRequiredParam<MooseEnum>(
-      "kernel_function",
-      kernels,
-      "The kernel (covariance function) to use.");
+      "kernel_function", kernels, "The kernel (covariance function) to use.");
   params.addRequiredParam<std::vector<DistributionName>>(
       "distributions", "Names of the distributions samples were taken from.");
-  params.addRequiredParam<std::vector<Real> >("length_factor", "Length Factor to use for Covariance Kernel");
-  params.addParam<Real>("signal_variance",1, "Signal Variance (sigma_f^2) to use for kernel calculation.");
-  params.addParam<Real>("noise_variance",0, "Noise Variance (sigma_n^2) to use for kernel calculation.");
+  params.addRequiredParam<std::vector<Real>>("length_factor",
+                                             "Length Factor to use for Covariance Kernel");
+  params.addParam<Real>(
+      "signal_variance", 1, "Signal Variance (sigma_f^2) to use for kernel calculation.");
+  params.addParam<Real>(
+      "noise_variance", 0, "Noise Variance (sigma_n^2) to use for kernel calculation.");
   params.addParam<Real>("gamma", "Gamma to use for Exponential Covariance Kernel");
   params.addParam<unsigned int>("p", "Integer p to use for Matern Hald Integer Covariance Kernel");
-  params.addParam<bool>("standardize_params", true ,"Standardize (center and scale) training parameters (x values)");
-  params.addParam<bool>("standardize_data", true ,"Standardize (center and scale) training data (y values)");
+  params.addParam<bool>(
+      "standardize_params", true, "Standardize (center and scale) training parameters (x values)");
+  params.addParam<bool>(
+      "standardize_data", true, "Standardize (center and scale) training data (y values)");
 
   return params;
 }
@@ -53,7 +56,8 @@ GaussianProcessTrainer::GaussianProcessTrainer(const InputParameters & parameter
     _training_data_var(declareModelData<RealEigenVector>("_training_data_var")),
     _K(declareModelData<RealEigenMatrix>("_K")),
     _K_results_solve(declareModelData<RealEigenMatrix>("_K_results_solve")),
-    _covar_function(declareModelData<std::unique_ptr<CovarianceFunction::CovarianceKernel> >("_covar_function")),
+    _covar_function(
+        declareModelData<std::unique_ptr<CovarianceFunction::CovarianceKernel>>("_covar_function")),
     _standardize_params(getParam<bool>("standardize_params")),
     _standardize_data(getParam<bool>("standardize_data"))
 {
@@ -99,57 +103,65 @@ GaussianProcessTrainer::execute()
   // Offset for replicated/distributed result data
   dof_id_type offset = _values_distributed ? _sampler->getLocalRowBegin() : 0;
 
-  //May load a very large matrix, which could be bad.
+  // May load a very large matrix, which could be bad.
   DenseMatrix<Real> params = _sampler->getLocalSamples();
-  _training_params.resize(params.m(),params.n());
-  for (unsigned int ii=0; ii<_training_params.rows(); ii++){
-    for (unsigned int jj=0; jj<_training_params.cols(); jj++){
-      _training_params(ii,jj)=params(ii,jj);
+  _training_params.resize(params.m(), params.n());
+  for (unsigned int ii = 0; ii < _training_params.rows(); ii++)
+  {
+    for (unsigned int jj = 0; jj < _training_params.cols(); jj++)
+    {
+      _training_params(ii, jj) = params(ii, jj);
     }
   }
 
-  if (_standardize_params){
-    //comptue mean
-    _training_params_mean=_training_params.colwise().mean();
-    //center params
-    _training_params=_training_params.rowwise() - _training_params_mean.transpose();
-    //compute variance
+  if (_standardize_params)
+  {
+    // comptue mean
+    _training_params_mean = _training_params.colwise().mean();
+    // center params
+    _training_params = _training_params.rowwise() - _training_params_mean.transpose();
+    // compute variance
     _training_params_var = _training_params.colwise().squaredNorm() / _num_samples;
-    //scale params using std
-    _training_params = _training_params.array().rowwise() /  _training_params_var.transpose().array().sqrt();
+    // scale params using std
+    _training_params =
+        _training_params.array().rowwise() / _training_params_var.transpose().array().sqrt();
   }
-  else{
-    //if not standardizing data set mean=0, std=1 for use in surrogate
+  else
+  {
+    // if not standardizing data set mean=0, std=1 for use in surrogate
     _training_params_mean = RealEigenVector::Zero(_n_params);
     _training_params_var = RealEigenVector::Ones(_n_params);
   }
 
-  //Load training data into Eigen::Vector for easy manipulation
-  _training_data.resize(_num_samples,1);
-  for (unsigned int ii=0; ii<_num_samples; ii++){
-    _training_data(ii,0) =   (*_values_ptr)[ii - offset];
+  // Load training data into Eigen::Vector for easy manipulation
+  _training_data.resize(_num_samples, 1);
+  for (unsigned int ii = 0; ii < _num_samples; ii++)
+  {
+    _training_data(ii, 0) = (*_values_ptr)[ii - offset];
   }
 
-  if (_standardize_data){
-    //comptue mean
-    _training_data_mean=_training_data.colwise().mean();
-    //center data
-    _training_data=_training_data.rowwise() - _training_data_mean.transpose();
-    //compute variance
+  if (_standardize_data)
+  {
+    // comptue mean
+    _training_data_mean = _training_data.colwise().mean();
+    // center data
+    _training_data = _training_data.rowwise() - _training_data_mean.transpose();
+    // compute variance
     _training_data_var = _training_data.colwise().squaredNorm() / _num_samples;
-    //scale data using std
-    _training_data = _training_data.array().rowwise() /  _training_data_var.transpose().array().sqrt();
+    // scale data using std
+    _training_data =
+        _training_data.array().rowwise() / _training_data_var.transpose().array().sqrt();
   }
-  else{
-    //if not standardizing data set mean=0, std=1 for use in surrogate
+  else
+  {
+    // if not standardizing data set mean=0, std=1 for use in surrogate
     _training_data_mean = RealEigenVector::Zero(1);
     _training_data_var = RealEigenVector::Ones(1);
   }
 
-  _K = _covar_function->compute_K(_training_params,_training_params,true);
+  _K = _covar_function->compute_K(_training_params, _training_params, true);
   _K_cho_decomp = _K.llt();
-  _K_results_solve = _K_cho_decomp.solve( _training_data );
-
+  _K_results_solve = _K_cho_decomp.solve(_training_data);
 }
 
 void
--- modules/stochastic_tools/src/utils/CovarianceFunction.C
+++ modules/stochastic_tools/src/utils/CovarianceFunction.C
@@ -30,11 +30,12 @@ std::unique_ptr<CovarianceKernel>
 makeCovarianceKernel(const MooseEnum & kernel_type, const GaussianProcessTrainer * gp)
 {
 
-  std::vector<Real> length_factor=gp->getParam<std::vector<Real>>("length_factor");
+  std::vector<Real> length_factor = gp->getParam<std::vector<Real>>("length_factor");
   Real signal_variance = gp->getParam<Real>("signal_variance");
   Real noise_variance = gp->getParam<Real>("noise_variance");
 
-  if (kernel_type==0){
+  if (kernel_type == 0)
+  {
     std::unique_ptr<CovarianceKernel> ptr = libmesh_make_unique<SquaredExponential>();
     ptr->set_signal_variance(signal_variance);
     ptr->set_noise_variance(noise_variance);
@@ -42,9 +43,10 @@ makeCovarianceKernel(const MooseEnum & kernel_type, const GaussianProcessTrainer
     return ptr;
   }
 
-  if (kernel_type==1){
+  if (kernel_type == 1)
+  {
     std::unique_ptr<CovarianceKernel> ptr = libmesh_make_unique<Exponential>();
-    Real gamma=gp->getParam<Real>("gamma");
+    Real gamma = gp->getParam<Real>("gamma");
     ptr->set_signal_variance(signal_variance);
     ptr->set_noise_variance(noise_variance);
     ptr->set_length_factor(length_factor);
@@ -52,12 +54,15 @@ makeCovarianceKernel(const MooseEnum & kernel_type, const GaussianProcessTrainer
     return ptr;
   }
 
-  if (kernel_type==2){
-    ::mooseError("General Matern covariance function not yet implemented. Half-integer Matern implemented in MaternHalfInt if desired.");
+  if (kernel_type == 2)
+  {
+    ::mooseError("General Matern covariance function not yet implemented. Half-integer Matern "
+                 "implemented in MaternHalfInt if desired.");
     return nullptr;
   }
 
-  if (kernel_type==3){
+  if (kernel_type == 3)
+  {
     std::unique_ptr<CovarianceKernel> ptr = libmesh_make_unique<MaternHalfInt>();
     unsigned int p = gp->getParam<unsigned int>("p");
     ptr->set_signal_variance(signal_variance);
@@ -79,68 +84,83 @@ CovarianceKernel::store(std::ostream & /*stream*/, void * /*context*/) const
 }
 
 RealEigenMatrix
-CovarianceKernel::compute_K(const RealEigenMatrix /*x*/, const RealEigenMatrix /*xp*/, const bool /*is_self_covariance*/) const
+CovarianceKernel::compute_K(const RealEigenMatrix /*x*/,
+                            const RealEigenMatrix /*xp*/,
+                            const bool /*is_self_covariance*/) const
 {
   ::mooseError("Covariance function has not been implemented.");
-  RealEigenMatrix tmp(1,1);
+  RealEigenMatrix tmp(1, 1);
   return tmp;
 }
 
-void CovarianceKernel::set_signal_variance(const Real sigma_f_squared)
+void
+CovarianceKernel::set_signal_variance(const Real sigma_f_squared)
 {
   _sigma_f_squared = sigma_f_squared;
 }
 
-void CovarianceKernel::set_noise_variance(const Real sigma_n_squared)
+void
+CovarianceKernel::set_noise_variance(const Real sigma_n_squared)
 {
   _sigma_n_squared = sigma_n_squared;
 }
 
-void CovarianceKernel::set_length_factor(const std::vector<Real> length_factor)
+void
+CovarianceKernel::set_length_factor(const std::vector<Real> length_factor)
 {
   _length_factor = length_factor;
 }
 
-void CovarianceKernel::set_gamma(const Real /*gamma*/)
+void
+CovarianceKernel::set_gamma(const Real /*gamma*/)
 {
-  ::mooseError("Attempting to set gamma hyper parameter for covariance function which does not utilize.");
+  ::mooseError(
+      "Attempting to set gamma hyper parameter for covariance function which does not utilize.");
 }
 
-void CovarianceKernel::set_p(const unsigned int /*p*/)
+void
+CovarianceKernel::set_p(const unsigned int /*p*/)
 {
-  ::mooseError("Attempting to set p hyper parameter for covariance function which does not utilize.");
+  ::mooseError(
+      "Attempting to set p hyper parameter for covariance function which does not utilize.");
 }
 
-//begin squared_exponential
-SquaredExponential::SquaredExponential()
-  : CovarianceKernel()
-{
-}
+// begin squared_exponential
+SquaredExponential::SquaredExponential() : CovarianceKernel() {}
 
-RealEigenMatrix SquaredExponential::compute_K(const RealEigenMatrix x, const RealEigenMatrix xp, const bool is_self_covariance) const
+RealEigenMatrix
+SquaredExponential::compute_K(const RealEigenMatrix x,
+                              const RealEigenMatrix xp,
+                              const bool is_self_covariance) const
 {
   unsigned int num_samples_x = x.rows();
   unsigned int num_samples_xp = xp.rows();
   unsigned int num_params_x = x.cols();
   unsigned int num_params_xp = xp.cols();
 
-  if (num_params_x!=num_params_xp){
-    ::mooseError("Number of parameters do not match in covariance kernel calculation");}
+  if (num_params_x != num_params_xp)
+  {
+    ::mooseError("Number of parameters do not match in covariance kernel calculation");
+  }
 
-  RealEigenMatrix K(num_samples_x,num_samples_xp);
+  RealEigenMatrix K(num_samples_x, num_samples_xp);
 
-  for (unsigned int ii=0; ii<num_samples_x; ii++){
-    for (unsigned int jj=0; jj<num_samples_xp; jj++){
-        Real val =0;
-        for (unsigned int kk=0; kk<num_params_x; kk++){
-          //Compute distance per parameter, scaled by length factor
-          val += std::pow(( x(ii,kk)-  xp(jj,kk) ),2) / (std::pow(_length_factor.at(kk),2));
-        }
-        val = _sigma_f_squared* std::exp(-val/2.0);
-        K(ii,jj) = val;
+  for (unsigned int ii = 0; ii < num_samples_x; ii++)
+  {
+    for (unsigned int jj = 0; jj < num_samples_xp; jj++)
+    {
+      Real val = 0;
+      for (unsigned int kk = 0; kk < num_params_x; kk++)
+      {
+        // Compute distance per parameter, scaled by length factor
+        val += std::pow((x(ii, kk) - xp(jj, kk)), 2) / (std::pow(_length_factor.at(kk), 2));
       }
-    if (is_self_covariance){
-      K(ii,ii) += _sigma_n_squared;
+      val = _sigma_f_squared * std::exp(-val / 2.0);
+      K(ii, jj) = val;
+    }
+    if (is_self_covariance)
+    {
+      K(ii, ii) += _sigma_n_squared;
     }
   }
 
@@ -156,42 +176,49 @@ SquaredExponential::store(std::ostream & stream, void * context) const
   dataStore(stream, _sigma_n_squared, context);
   unsigned int n = _length_factor.size();
   dataStore(stream, n, context);
-  for (unsigned int ii = 0; ii < n; ii++){
-    dataStore(stream, _length_factor.at(ii), context);}
+  for (unsigned int ii = 0; ii < n; ii++)
+  {
+    dataStore(stream, _length_factor.at(ii), context);
+  }
 }
-//end squared_exponential
+// end squared_exponential
 
+// begin exponential
+Exponential::Exponential() : CovarianceKernel() {}
 
-//begin exponential
-Exponential::Exponential()
-  : CovarianceKernel()
-{
-}
-
-RealEigenMatrix Exponential::compute_K(const RealEigenMatrix x, const RealEigenMatrix xp, const bool is_self_covariance) const
+RealEigenMatrix
+Exponential::compute_K(const RealEigenMatrix x,
+                       const RealEigenMatrix xp,
+                       const bool is_self_covariance) const
 {
   unsigned int num_samples_x = x.rows();
   unsigned int num_samples_xp = xp.rows();
   unsigned int num_params_x = x.cols();
   unsigned int num_params_xp = xp.cols();
 
-  if (num_params_x!=num_params_xp){
-    ::mooseError("Number of parameters do not match in covariance kernel calculation");}
+  if (num_params_x != num_params_xp)
+  {
+    ::mooseError("Number of parameters do not match in covariance kernel calculation");
+  }
 
-  RealEigenMatrix K(num_samples_x,num_samples_xp);
+  RealEigenMatrix K(num_samples_x, num_samples_xp);
 
-  for (unsigned int ii=0; ii<num_samples_x; ii++){
-    for (unsigned int jj=0; jj<num_samples_xp; jj++){
-        Real r_scaled =0;
-        //Compute distance per parameter, scaled by length factor
-        for (unsigned int kk=0; kk<num_params_x; kk++){
-          r_scaled += pow( (x(ii,kk)- xp(jj,kk))/_length_factor.at(kk), 2);
-        }
-        r_scaled = sqrt(r_scaled);
-        K(ii,jj) =  _sigma_f_squared* std::exp(-pow(r_scaled,_gamma));
+  for (unsigned int ii = 0; ii < num_samples_x; ii++)
+  {
+    for (unsigned int jj = 0; jj < num_samples_xp; jj++)
+    {
+      Real r_scaled = 0;
+      // Compute distance per parameter, scaled by length factor
+      for (unsigned int kk = 0; kk < num_params_x; kk++)
+      {
+        r_scaled += pow((x(ii, kk) - xp(jj, kk)) / _length_factor.at(kk), 2);
       }
-    if (is_self_covariance){
-      K(ii,ii) += _sigma_n_squared;
+      r_scaled = sqrt(r_scaled);
+      K(ii, jj) = _sigma_f_squared * std::exp(-pow(r_scaled, _gamma));
+    }
+    if (is_self_covariance)
+    {
+      K(ii, ii) += _sigma_n_squared;
     }
   }
 
@@ -207,60 +234,70 @@ Exponential::store(std::ostream & stream, void * context) const
   dataStore(stream, _sigma_n_squared, context);
   unsigned int n = _length_factor.size();
   dataStore(stream, n, context);
-  for (unsigned int ii = 0; ii < n; ii++){
+  for (unsigned int ii = 0; ii < n; ii++)
+  {
     dataStore(stream, _length_factor.at(ii), context);
   }
   dataStore(stream, _gamma, context);
 }
 
-void Exponential::set_gamma(const Real gamma)
+void
+Exponential::set_gamma(const Real gamma)
 {
   _gamma = gamma;
 }
-//end exponential
+// end exponential
 
+// begin matern half integer
+MaternHalfInt::MaternHalfInt() : CovarianceKernel() {}
 
-//begin matern half integer
-MaternHalfInt::MaternHalfInt()
-  : CovarianceKernel()
-{
-}
-
-RealEigenMatrix MaternHalfInt::compute_K(const RealEigenMatrix x, const RealEigenMatrix xp, const bool is_self_covariance) const
+RealEigenMatrix
+MaternHalfInt::compute_K(const RealEigenMatrix x,
+                         const RealEigenMatrix xp,
+                         const bool is_self_covariance) const
 {
   unsigned int num_samples_x = x.rows();
   unsigned int num_samples_xp = xp.rows();
   unsigned int num_params_x = x.cols();
   unsigned int num_params_xp = xp.cols();
 
-  if (num_params_x!=num_params_xp){
-    ::mooseError("Number of parameters do not match in covariance kernel calculation");}
-
-  RealEigenMatrix K(num_samples_x,num_samples_xp);
-
-  //This factor is used over and over, don't calculate each time
-  Real factor = sqrt(2*_p + 1);
-
-  for (unsigned int ii=0; ii<num_samples_x; ii++){
-    for (unsigned int jj=0; jj<num_samples_xp; jj++){
-        Real r_scaled =0;
-        //Compute distance per parameter, scaled by length factor
-        for (unsigned int kk=0; kk<num_params_x; kk++){
-          r_scaled += pow( (x(ii,kk)- xp(jj,kk))/_length_factor.at(kk), 2);
-        }
-        r_scaled = sqrt(r_scaled);
-        Real summation=0;
-        //tgamma(x+1) == x! when x is a natural number, which should always be the case for MaternHalfInt
-        for (unsigned int tt=0; tt<_p+1; tt++){
-          summation += (tgamma(_p + tt + 1)/(tgamma(tt+1) * tgamma(_p - tt + 1))) * pow(2* factor * r_scaled, _p-tt);
-        }
-        K(ii,jj) =  _sigma_f_squared * std::exp(- factor * r_scaled) * (tgamma(_p + 1)/(tgamma(2*_p + 1))) * summation;
-      }
-    if (is_self_covariance){
-      K(ii,ii) += _sigma_n_squared;
-    }
+  if (num_params_x != num_params_xp)
+  {
+    ::mooseError("Number of parameters do not match in covariance kernel calculation");
   }
 
+  RealEigenMatrix K(num_samples_x, num_samples_xp);
+
+  // This factor is used over and over, don't calculate each time
+  Real factor = sqrt(2 * _p + 1);
+
+  for (unsigned int ii = 0; ii < num_samples_x; ii++)
+  {
+    for (unsigned int jj = 0; jj < num_samples_xp; jj++)
+    {
+      Real r_scaled = 0;
+      // Compute distance per parameter, scaled by length factor
+      for (unsigned int kk = 0; kk < num_params_x; kk++)
+      {
+        r_scaled += pow((x(ii, kk) - xp(jj, kk)) / _length_factor.at(kk), 2);
+      }
+      r_scaled = sqrt(r_scaled);
+      Real summation = 0;
+      // tgamma(x+1) == x! when x is a natural number, which should always be the case for
+      // MaternHalfInt
+      for (unsigned int tt = 0; tt < _p + 1; tt++)
+      {
+        summation += (tgamma(_p + tt + 1) / (tgamma(tt + 1) * tgamma(_p - tt + 1))) *
+                     pow(2 * factor * r_scaled, _p - tt);
+      }
+      K(ii, jj) = _sigma_f_squared * std::exp(-factor * r_scaled) *
+                  (tgamma(_p + 1) / (tgamma(2 * _p + 1))) * summation;
+    }
+    if (is_self_covariance)
+    {
+      K(ii, ii) += _sigma_n_squared;
+    }
+  }
   return K;
 }
 
@@ -273,19 +310,21 @@ MaternHalfInt::store(std::ostream & stream, void * context) const
   dataStore(stream, _sigma_n_squared, context);
   unsigned int n = _length_factor.size();
   dataStore(stream, n, context);
-  for (unsigned int ii = 0; ii < n; ii++){
+  for (unsigned int ii = 0; ii < n; ii++)
+  {
     dataStore(stream, _length_factor.at(ii), context);
   }
   dataStore(stream, _p, context);
 }
 
-void MaternHalfInt::set_p(const unsigned int p)
+void
+MaternHalfInt::set_p(const unsigned int p)
 {
   _p = p;
 }
-//end matern half integer
+// end matern half integer
 
-}// namespace
+} // namespace
 
 template <>
 void
@@ -302,56 +341,62 @@ dataLoad(std::istream & stream,
          std::unique_ptr<CovarianceFunction::CovarianceKernel> & ptr,
          void * context)
 {
-   std::string covar_type;
-   dataLoad(stream, covar_type, context);
-   if (covar_type == "Squared Exponential")
-   {
-     Real sigma_f_squared, sigma_n_squared;
-     dataLoad(stream, sigma_f_squared, context);
-     dataLoad(stream, sigma_n_squared, context);
-     unsigned int n;
-     dataLoad(stream, n, context);
-     std::vector<Real> length_factor(n);
-     for (unsigned int ii = 0; ii < n; ii++){
-       dataLoad(stream, length_factor.at(ii), context);}
-     ptr = libmesh_make_unique<CovarianceFunction::SquaredExponential>();
-     ptr->set_length_factor(length_factor);
-     ptr->set_signal_variance(sigma_f_squared);
-   }
-   else if (covar_type == "Exponential")
-   {
-     Real sigma_f_squared, sigma_n_squared;
-     dataLoad(stream, sigma_f_squared, context);
-     dataLoad(stream, sigma_n_squared, context);
-     unsigned int n;
-     dataLoad(stream, n, context);
-     std::vector<Real> length_factor(n);
-     for (unsigned int ii = 0; ii < n; ii++){
-       dataLoad(stream, length_factor.at(ii), context);}
-     ptr = libmesh_make_unique<CovarianceFunction::Exponential>();
-     ptr->set_length_factor(length_factor);
-     ptr->set_signal_variance(sigma_f_squared);
-     Real gamma;
-     dataLoad(stream, gamma, context);
-     ptr->set_gamma(gamma);
-   }
-   else if (covar_type == "MaternHalfInt")
-   {
-     Real sigma_f_squared, sigma_n_squared;
-     dataLoad(stream, sigma_f_squared, context);
-     dataLoad(stream, sigma_n_squared, context);
-     unsigned int n;
-     dataLoad(stream, n, context);
-     std::vector<Real> length_factor(n);
-     for (unsigned int ii = 0; ii < n; ii++){
-       dataLoad(stream, length_factor.at(ii), context);}
-     ptr = libmesh_make_unique<CovarianceFunction::MaternHalfInt>();
-     ptr->set_length_factor(length_factor);
-     ptr->set_signal_variance(sigma_f_squared);
-     unsigned int p;
-     dataLoad(stream, p, context);
-     ptr->set_p(p);
-   }
-   else
-     ::mooseError("Unknown Covariance Function: ", covar_type);
+  std::string covar_type;
+  dataLoad(stream, covar_type, context);
+  if (covar_type == "Squared Exponential")
+  {
+    Real sigma_f_squared, sigma_n_squared;
+    dataLoad(stream, sigma_f_squared, context);
+    dataLoad(stream, sigma_n_squared, context);
+    unsigned int n;
+    dataLoad(stream, n, context);
+    std::vector<Real> length_factor(n);
+    for (unsigned int ii = 0; ii < n; ii++)
+    {
+      dataLoad(stream, length_factor.at(ii), context);
+    }
+    ptr = libmesh_make_unique<CovarianceFunction::SquaredExponential>();
+    ptr->set_length_factor(length_factor);
+    ptr->set_signal_variance(sigma_f_squared);
+  }
+  else if (covar_type == "Exponential")
+  {
+    Real sigma_f_squared, sigma_n_squared;
+    dataLoad(stream, sigma_f_squared, context);
+    dataLoad(stream, sigma_n_squared, context);
+    unsigned int n;
+    dataLoad(stream, n, context);
+    std::vector<Real> length_factor(n);
+    for (unsigned int ii = 0; ii < n; ii++)
+    {
+      dataLoad(stream, length_factor.at(ii), context);
+    }
+    ptr = libmesh_make_unique<CovarianceFunction::Exponential>();
+    ptr->set_length_factor(length_factor);
+    ptr->set_signal_variance(sigma_f_squared);
+    Real gamma;
+    dataLoad(stream, gamma, context);
+    ptr->set_gamma(gamma);
+  }
+  else if (covar_type == "MaternHalfInt")
+  {
+    Real sigma_f_squared, sigma_n_squared;
+    dataLoad(stream, sigma_f_squared, context);
+    dataLoad(stream, sigma_n_squared, context);
+    unsigned int n;
+    dataLoad(stream, n, context);
+    std::vector<Real> length_factor(n);
+    for (unsigned int ii = 0; ii < n; ii++)
+    {
+      dataLoad(stream, length_factor.at(ii), context);
+    }
+    ptr = libmesh_make_unique<CovarianceFunction::MaternHalfInt>();
+    ptr->set_length_factor(length_factor);
+    ptr->set_signal_variance(sigma_f_squared);
+    unsigned int p;
+    dataLoad(stream, p, context);
+    ptr->set_p(p);
+  }
+  else
+    ::mooseError("Unknown Covariance Function: ", covar_type);
 }
