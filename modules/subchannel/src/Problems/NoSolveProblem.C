c6f3e47efe0c82e3fbf5b8a10bb3fdde2968ee44
David Andrs <david.andrs@inl.gov>
2020-06-29 10:20:37 -0600

    Adding NoSolveProblem to facilitate output on detailed mesh

---
+++ modules/subchannel/src/Problems/NoSolveProblem.C
@@ -0,0 +1,31 @@
+#include "NoSolveProblem.h"
+
+registerMooseObject("SubChannelApp", NoSolveProblem);
+
+InputParameters
+NoSolveProblem::validParams()
+{
+  InputParameters params = ExternalProblem::validParams();
+  return params;
+}
+
+NoSolveProblem::NoSolveProblem(const InputParameters & params)
+  : ExternalProblem(params)
+{
+}
+
+void
+NoSolveProblem::externalSolve()
+{
+}
+
+void
+NoSolveProblem::syncSolutions(Direction /*direction*/)
+{
+}
+
+bool
+NoSolveProblem::converged()
+{
+  return true;
+}
