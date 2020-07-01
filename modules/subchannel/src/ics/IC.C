b6ec66f697cdfcafa72df5370c5abbb1c02f8c85
v-kyr1ak <v_kyr1ak@tamu.edu>
2020-07-01 15:05:13 -0500

    removed hard coding

--- modules/subchannel/src/ics/IC.C
+++ modules/subchannel/src/ics/IC.C
@@ -1,17 +1,16 @@
 #include "IC.h"
+#include "SubChannelMesh.h"
 
 InputParameters
-IC::validParams()
-{
-  return InitialCondition::validParams();
-}
+IC::validParams(){ return InitialCondition::validParams();}
 
-IC::IC(const InputParameters & params) : InitialCondition(params) {}
+IC::IC(const InputParameters & params) : InitialCondition(params) ,
+_mesh(dynamic_cast<SubChannelMesh &> (_fe_problem.mesh())) {}
 
 std::pair<unsigned int, unsigned int>
 IC::index_point(const Point & p) const
 {
-  constexpr Real pitch{0.0126}; // in m
+  auto pitch = _mesh._pitch;
   unsigned int i = (p(0) + 0.5 * pitch) / pitch;
   unsigned int j = (p(1) + 0.5 * pitch) / pitch;
   return {i, j};
