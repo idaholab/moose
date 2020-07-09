fa4b276cc571fce4763c35d4e430b0dd8c038849
Daniel Schwen <daniel@schwen.de>
2020-07-09 12:47:12 -0600

    Add missing is_ad parameters (#15571)

--- modules/phase_field/include/materials/DerivativeMultiPhaseBase.h
+++ modules/phase_field/include/materials/DerivativeMultiPhaseBase.h
@@ -16,8 +16,6 @@
 /**
  * DerivativeMaterial child class to evaluate a parsed function for the
  * free energy and automatically provide all derivatives.
- * This requires the autodiff patch (https://github.com/libMesh/libmesh/pull/238)
- * to Function Parser in libmesh.
  */
 class DerivativeMultiPhaseBase : public DerivativeFunctionMaterialBase
 {
