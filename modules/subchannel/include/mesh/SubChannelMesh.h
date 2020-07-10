73929cad3ee41bf7f8938407e07d95ec09f18504
v-kyr1ak <v_kyr1ak@tamu.edu>
2020-07-10 08:53:48 -0500

    Doxygen comments

--- modules/subchannel/include/mesh/SubChannelMesh.h
+++ modules/subchannel/include/mesh/SubChannelMesh.h
@@ -11,7 +11,12 @@ public:
   virtual void buildMesh() override;
   unsigned int _nx, _ny, _nz, _n_channels, _n_gaps;
   Real _pitch, _rod_diameter, _gap, _heated_length;
-  std::vector<Real> _z_grid, _spacer_z, _spacer_k;
+  /// axial location of nodes
+  std::vector<Real> _z_grid;
+  /// axial locations of spacers
+  const std::vector<Real> & _spacer_z;
+  /// form loss coefficient of the spacer
+  const std::vector<Real> & _spacer_k;
   std::vector<std::vector<Node *>> _nodes;
   std::vector<std::vector<Node *>> _gapnodes;
   std::vector<std::pair<unsigned int, unsigned int>> _gap_to_chan_map;
