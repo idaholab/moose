02f37f79860ae9fced254e6e80d84523e7aa4c35
Jason Miller <jason.miller@inl.gov>
2020-07-10 11:40:56 -0600

    Adding pyhit as a conda package
    Closes #15582

---
+++ conda/pyhit/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -eu
+
+cp -R pyhit $SP_DIR/
+cd src
+make bindings
+cp hit.so $SP_DIR/pyhit/
+cat > $SP_DIR/pyhit-$PKG_VERSION.egg-info <<FAKE_EGG
+Metadata-Version: 2.1
+Name: pyhit
+Version: $PKG_VERSION
+Summary: MOOSE HIT Parser library
+Platform: UNKNOWN
+FAKE_EGG
