ec5c1f7cb786ea994601ef3feabd5abfbf84c764
v-kyr1ak <v_kyr1ak@tamu.edu>
2020-07-23 16:40:45 -0500

    made a new Class to calculate values at exit

--- modules/subchannel/src/postProcessor/exitValue.C
+++ modules/subchannel/src/postProcessor/exitValue.C
@@ -1,42 +1,56 @@
 #include "exitValue.h"
+#include "SolutionHandle.h"
 
 registerMooseObject("SubChannelApp", exitValue);
 
 InputParameters
 exitValue::validParams()
 {
-  InputParameters params = exitValue::validParams();
+  InputParameters params = GeneralUserObject::validParams();
   params.addRequiredCoupledVar("value", "variable you want the value at the exit");
-  params.addParam<std::string>("valueName", "name of the value";
-  params.addClassDescription("Prints out a user selected value in csv format to be used for post-processing");
+  params.addParam<std::string>("file_name",
+                               "name of the value used to printing in the correct file name");
+  params.addClassDescription(
+      "Prints out a user selected value in a matrix format to be used for post-processing");
   return params;
 }
 
 exitValue::exitValue(const InputParameters & parameters)
-  : InitialCondition(parameters),
+  : GeneralUserObject(parameters),
+    Coupleable(this, true),
     _mesh(dynamic_cast<SubChannelMesh &>(_fe_problem.mesh())),
     _value(coupledValue("value")),
-    _valueName(getParam<std::string>("valueName"))
+    _file_name(getParam<std::string>("file_name"))
 {
   _exitValue.resize(_mesh._ny, _mesh._nx);
 }
 
 void
-exitValue::print_exitValue()
+exitValue::initialize()
 {
+}
+void
+exitValue::finalize()
+{
+}
+
+void
+exitValue::execute()
+{
+  auto val_soln = SolutionHandle(*getFieldVar("value", 0));
+
   for (unsigned int i_ch = 0; i_ch < _mesh._n_channels; i_ch++)
   {
     auto * node_out = _mesh._nodes[i_ch][_mesh._nz];
-    unsigned int i = (i_ch / _subchannel_mesh._nx);   // row
-    unsigned int j = i_ch - i * _subchannel_mesh._nx; // column
-    _exitValue(i, j) = _value(node_out);
+    unsigned int i = (i_ch / _mesh._nx);   // row
+    unsigned int j = i_ch - i * _mesh._nx; // column
+    _exitValue(i, j) = val_soln(node_out);
   }
 
-  string fullName = _valueName + "_out " +
-                    ".txt"
+  std::string fullName = _file_name + "_out" + ".txt";
 
-                    std::ofstream myfile1;
-  myfile1.open("fullName", std::ofstream::trunc);
+  std::ofstream myfile1;
+  myfile1.open(fullName, std::ofstream::trunc);
   myfile1 << std::setprecision(3) << std::fixed << _exitValue << "\n";
   myfile1.close();
 }
