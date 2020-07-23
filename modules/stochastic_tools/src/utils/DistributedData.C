908bb148db28501f2181c5a68d8b6c6853f86d1e
grmnptr <grmnptr@gmail.com>
2020-07-23 16:27:01 -0500

    Renamed Sample to Entry in DistributedData. (#15538)

--- modules/stochastic_tools/src/utils/DistributedData.C
+++ modules/stochastic_tools/src/utils/DistributedData.C
@@ -17,104 +17,104 @@ namespace StochasticTools
 
 template <typename T>
 DistributedData<T>::DistributedData(const libMesh::Parallel::Communicator & comm_in)
-  : libMesh::ParallelObject(comm_in), _closed(false), _n_local_samples(0)
+  : libMesh::ParallelObject(comm_in), _closed(false), _n_local_entries(0)
 {
 }
 
 template <typename T>
 void
-DistributedData<T>::initializeContainer(dof_id_type n_global_samples)
+DistributedData<T>::initializeContainer(dof_id_type n_global_entries)
 {
   // This function can be used when a linear partitioning is required and the
   // number of global samples is known in advance.
-  dof_id_type local_sample_begin;
-  dof_id_type local_sample_end;
-  MooseUtils::linearPartitionItems(n_global_samples,
+  dof_id_type local_entry_begin;
+  dof_id_type local_entry_end;
+  MooseUtils::linearPartitionItems(n_global_entries,
                                    n_processors(),
                                    processor_id(),
-                                   _n_local_samples,
-                                   local_sample_begin,
-                                   local_sample_end);
+                                   _n_local_entries,
+                                   local_entry_begin,
+                                   local_entry_end);
 
-  _local_samples.resize(_n_local_samples);
-  _local_sample_ids.resize(_n_local_samples);
+  _local_entries.resize(_n_local_entries);
+  _local_entry_ids.resize(_n_local_entries);
 
   // Filling the sample ID vector, leaving the elements of the sample vector
   // with the default constructor.
-  for (dof_id_type sample_i = local_sample_begin; sample_i < local_sample_end; ++sample_i)
+  for (dof_id_type entry_i = local_entry_begin; entry_i < local_entry_end; ++entry_i)
   {
-    _local_sample_ids[sample_i] = sample_i;
+    _local_entry_ids[entry_i] = entry_i;
   }
 }
 
 template <typename T>
 void
-DistributedData<T>::addNewSample(dof_id_type glob_i, const T & sample)
+DistributedData<T>::addNewEntry(dof_id_type glob_i, const T & entry)
 {
-  auto it = std::find(_local_sample_ids.begin(), _local_sample_ids.end(), glob_i);
-  if (it != _local_sample_ids.end())
+  auto it = std::find(_local_entry_ids.begin(), _local_entry_ids.end(), glob_i);
+  if (it != _local_entry_ids.end())
     ::mooseError("Local object ID (", glob_i, ") already exists!");
   if (_closed)
     ::mooseError("DistributeData has already been closed, cannot add new elements!");
 
-  _local_samples.push_back(sample);
-  _local_sample_ids.push_back(glob_i);
-  _n_local_samples += 1;
+  _local_entries.push_back(entry);
+  _local_entry_ids.push_back(glob_i);
+  _n_local_entries += 1;
 }
 
 template <typename T>
 void
-DistributedData<T>::changeSample(dof_id_type glob_i, const T & sample)
+DistributedData<T>::changeEntry(dof_id_type glob_i, const T & entry)
 {
-  auto it = std::find(_local_sample_ids.begin(), _local_sample_ids.end(), glob_i);
-  if (it == _local_sample_ids.end())
+  auto it = std::find(_local_entry_ids.begin(), _local_entry_ids.end(), glob_i);
+  if (it == _local_entry_ids.end())
     ::mooseError("Local object ID (", glob_i, ") does not exists!");
   if (_closed)
     ::mooseError("DistributeData has already been closed, cannot change elements!");
 
-  _local_samples[std::distance(_local_sample_ids.begin(), it)] = sample;
+  _local_entries[std::distance(_local_entry_ids.begin(), it)] = entry;
 }
 
 template <typename T>
 const T &
-DistributedData<T>::getSample(dof_id_type glob_i)
+DistributedData<T>::getEntry(dof_id_type glob_i)
 {
-  auto it = std::find(_local_sample_ids.begin(), _local_sample_ids.end(), glob_i);
-  if (it == _local_sample_ids.end())
+  auto it = std::find(_local_entry_ids.begin(), _local_entry_ids.end(), glob_i);
+  if (it == _local_entry_ids.end())
     ::mooseError("Local object ID (", glob_i, ") does not exists!");
 
-  return _local_samples[std::distance(_local_sample_ids.begin(), it)];
+  return _local_entries[std::distance(_local_entry_ids.begin(), it)];
 }
 
 template <typename T>
 const T &
-DistributedData<T>::getLocalSample(dof_id_type loc_i)
+DistributedData<T>::getLocalEntry(dof_id_type loc_i)
 {
-  if (loc_i > _n_local_samples - 1)
+  if (loc_i > _n_local_entries - 1)
     ::mooseError("The requested local index (",
                  loc_i,
                  ") is greater than the size (",
-                 _n_local_samples,
+                 _n_local_entries,
                  ") of the locally stored vector!");
 
-  return _local_samples[loc_i];
+  return _local_entries[loc_i];
 }
 
 template <typename T>
 dof_id_type
-DistributedData<T>::getNumberOfGlobalSamples() const
+DistributedData<T>::getNumberOfGlobalEntries() const
 {
-  dof_id_type val = _n_local_samples;
+  dof_id_type val = _n_local_entries;
   _communicator.sum(val);
   return val;
 }
 
 template <typename T>
 bool
-DistributedData<T>::hasGlobalSample(dof_id_type glob_i)
+DistributedData<T>::hasGlobalEntry(dof_id_type glob_i)
 {
-  const auto it = std::find(_local_sample_ids.begin(), _local_sample_ids.end(), glob_i);
-  if (it != _local_sample_ids.end())
+  const auto it = std::find(_local_entry_ids.begin(), _local_entry_ids.end(), glob_i);
+  if (it != _local_entry_ids.end())
     return true;
 
   return false;
@@ -124,25 +124,25 @@ template <typename T>
 dof_id_type
 DistributedData<T>::getLocalIndex(dof_id_type glob_i)
 {
-  const auto it = std::find(_local_sample_ids.begin(), _local_sample_ids.end(), glob_i);
-  if (it == _local_sample_ids.end())
+  const auto it = std::find(_local_entry_ids.begin(), _local_entry_ids.end(), glob_i);
+  if (it == _local_entry_ids.end())
     ::mooseError("Local object ID (", glob_i, ") does not exists!");
 
-  return std::distance(_local_sample_ids.begin(), it);
+  return std::distance(_local_entry_ids.begin(), it);
 }
 
 template <typename T>
 dof_id_type
 DistributedData<T>::getGlobalIndex(dof_id_type loc_i)
 {
-  if (loc_i > _n_local_samples - 1)
+  if (loc_i > _n_local_entries - 1)
     ::mooseError("The requested local index (",
                  loc_i,
                  ") is greater than the size (",
-                 _n_local_samples,
+                 _n_local_entries,
                  ") of the locally stored vector!");
 
-  return _local_sample_ids[loc_i];
+  return _local_entry_ids[loc_i];
 }
 
 // Explicit instantiation of types that are necessary.
