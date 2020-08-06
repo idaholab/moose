4d11e68d3f0b7293982b7715e52734624cbaa3e4
Casey Icenhour <cticenho@ncsu.edu>
2020-08-06 09:28:21 -0600

    Add checks for current density object corner cases (#30)

--- modules/electromagnetics/src/auxkernels/CurrentDensity.C
+++ modules/electromagnetics/src/auxkernels/CurrentDensity.C
@@ -8,8 +8,12 @@ InputParameters
 CurrentDensityTempl<is_ad>::validParams()
 {
   InputParameters params = VectorAuxKernel::validParams();
-  params.addClassDescription("Calculates the current density vector field when given electrostatic potential (electrostatic = true, default) or electric field.");
-  params.addParam<bool>("electrostatic", true, "Whether the electric field is based on electrostatic potential or is fully electromagnetic (default = TRUE)");
+  params.addClassDescription("Calculates the current density vector field when given electrostatic "
+                             "potential (electrostatic = true, default) or electric field.");
+  params.addParam<bool>("electrostatic",
+                        true,
+                        "Whether the electric field is based on electrostatic potential or is "
+                        "fully electromagnetic (default = TRUE)");
   params.addCoupledVar("potential", "Electrostatic potential variable");
   params.addCoupledVar("electric_field", "Electric field variable (electromagnetic)");
   return params;
@@ -19,12 +23,35 @@ template <bool is_ad>
 CurrentDensityTempl<is_ad>::CurrentDensityTempl(const InputParameters & parameters)
   : VectorAuxKernel(parameters),
 
-  _is_es(getParam<bool>("electrostatic")),
-  _grad_potential(coupledGradient("potential")),
-  _electric_field(coupledVectorValue("electric_field")),
+    _is_es(getParam<bool>("electrostatic")),
+    _grad_potential(coupledGradient("potential")),
+    _electric_field(coupledVectorValue("electric_field")),
 
-  _conductivity(getGenericMaterialProperty<Real, is_ad>("electrical_conductivity"))
+    _conductivity(getGenericMaterialProperty<Real, is_ad>("electrical_conductivity"))
 {
+  if (_is_es && !isParamValid("potential") && isParamValid("electric_field"))
+  {
+    mooseError(
+        "In ",
+        name(),
+        ", an electric field vector variable has been provided when `electrostatic = TRUE`. Please "
+        "either provide an electrostatic potential variable only or set `electrostatic = FALSE`!");
+  }
+  else if (!_is_es && isParamValid("potential") && !isParamValid("electric_field"))
+  {
+    mooseError("In ",
+               name(),
+               ", an electrostatic potential variable has been provided when `electrostatic = "
+               "FALSE`. Please either provide an electric field vector variable only or set "
+               "`electrostatic = FALSE`!");
+  }
+  else if (isParamValid("potential") && isParamValid("electric_field"))
+  {
+    mooseError("In ",
+               name(),
+               ", both electrostatic potential and electric field variables have been provided. "
+               "Please only provide one or the other!");
+  }
 }
 
 template <bool is_ad>
@@ -35,7 +62,6 @@ CurrentDensityTempl<is_ad>::computeValue()
     return MetaPhysicL::raw_value(_conductivity[_qp]) * -_grad_potential[_qp];
   else
     return MetaPhysicL::raw_value(_conductivity[_qp]) * _electric_field[_qp];
-
 }
 
 template class CurrentDensityTempl<false>;
