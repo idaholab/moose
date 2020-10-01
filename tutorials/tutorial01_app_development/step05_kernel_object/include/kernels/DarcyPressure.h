87a10d2c03762f6ba8a91b0e2d3722a650481249
crswong888 <crswong888@gmail.com>
2020-09-30 22:01:53 -0600

    Clang formats in Babbler applications
    (refs #15232)

--- tutorials/tutorial01_app_development/step05_kernel_object/include/kernels/DarcyPressure.h
+++ tutorials/tutorial01_app_development/step05_kernel_object/include/kernels/DarcyPressure.h
@@ -6,18 +6,18 @@
 /**
  * Computes the residual contribution: K / mu * grad_test * grad_u.
  */
- class DarcyPressure : public ADKernelGrad
- {
- public:
-   static InputParameters validParams();
+class DarcyPressure : public ADKernelGrad
+{
+public:
+  static InputParameters validParams();
 
-   DarcyPressure(const InputParameters & parameters);
+  DarcyPressure(const InputParameters & parameters);
 
- protected:
-   /// ADKernel objects must override precomputeQpResidual
-   virtual ADRealVectorValue precomputeQpResidual() override;
+protected:
+  /// ADKernel objects must override precomputeQpResidual
+  virtual ADRealVectorValue precomputeQpResidual() override;
 
-   /// The variables which hold the value for K and mu
-   const Real _permeability;
-   const Real _viscosity;
- };
+  /// The variables which hold the value for K and mu
+  const Real _permeability;
+  const Real _viscosity;
+};
