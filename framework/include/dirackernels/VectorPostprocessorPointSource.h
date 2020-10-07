e327f830ea717f3cff92ebcb42881c33af6fc9ec
Lynn Munday <lynn.munday@inl.gov>
2020-10-06 21:03:11 -0600

    vectorPostprocessorPointSource that will read in point source from a vpp.

---
+++ framework/include/dirackernels/VectorPostprocessorPointSource.h
@@ -0,0 +1,41 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+// Moose Includes
+#include "DiracKernel.h"
+#include "VectorPostprocessorInterface.h"
+
+// Forward Declarations
+class VectorPostprocessorPointSource;
+
+template <>
+InputParameters validParams<VectorPostprocessorPointSource>();
+
+/**
+ * A VectorPostprocessorPointSource DiracKernel is used to create constant point sources.
+ * Coordinates and values for the point source can be given in the input or read from file
+ */
+class VectorPostprocessorPointSource : public DiracKernel, public VectorPostprocessorInterface
+{
+public:
+  static InputParameters validParams();
+  VectorPostprocessorPointSource(const InputParameters & parameters);
+  virtual void addPoints() override;
+
+protected:
+  virtual Real computeQpResidual() override;
+
+private:
+  const bool _use_broadcast;
+  const VectorPostprocessorValue & _vpp_values;
+  /// map to associate points with their index into the vpp value
+  std::map<Point, size_t> _point_to_index;
+};
---
+++ framework/src/dirackernels/VectorPostprocessorPointSource.C
@@ -0,0 +1,79 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "VectorPostprocessorPointSource.h"
+#include "MooseUtils.h"
+#include "DelimitedFileReader.h"
+
+registerMooseObject("MooseApp", VectorPostprocessorPointSource);
+
+defineLegacyParams(VectorPostprocessorPointSource);
+
+InputParameters
+VectorPostprocessorPointSource::validParams()
+{
+  InputParameters params = DiracKernel::validParams();
+
+  params.addClassDescription("Apply a point load defined by VectorPostprocessor.");
+
+  params.addParam<VectorPostprocessorName>(
+      "vector_postprocessor",
+      "The name of the VectorPostprocessor containing positions and corresponding");
+  params.addParam<std::string>("xcoordName", "x", "name of column containing x coordinates.");
+  params.addParam<std::string>("ycoordName", "y", "name of column containing y coordinates.");
+  params.addParam<std::string>("zcoordName", "z", "name of column containing z coordinates.");
+  params.addParam<std::string>("valueName", "value", "name of column containing values.");
+
+  // fixme lynn do I need this?
+  params.addParam<bool>(
+      "use_broadcast",
+      false,
+      "Causes this diracKernel to use a broadcasted version of the vector instead "
+      "of a scattered version of the vector (the default).  This is slower - but "
+      "is useful for debugging and testing");
+
+  return params;
+}
+
+VectorPostprocessorPointSource::VectorPostprocessorPointSource(const InputParameters & parameters)
+  : DiracKernel(parameters),
+    VectorPostprocessorInterface(this),
+    _use_broadcast(getParam<bool>("use_broadcast")),
+    _vpp_values(getVectorPostprocessorValue(
+        "vector_postprocessor", getParam<std::string>("valueName"), _use_broadcast))
+{
+}
+
+void
+VectorPostprocessorPointSource::addPoints()
+{
+  const auto xcoord(getVectorPostprocessorValue(
+      "vector_postprocessor", getParam<std::string>("xcoordName"), _use_broadcast));
+  const auto ycoord(getVectorPostprocessorValue(
+      "vector_postprocessor", getParam<std::string>("ycoordName"), _use_broadcast));
+  const auto zcoord(getVectorPostprocessorValue(
+      "vector_postprocessor", getParam<std::string>("zcoordName"), _use_broadcast));
+
+  mooseAssert(xcoord.size() != 0,
+              "VectorPostprocessorPointSource::addPoints():  Nothing read from vpp.");
+
+  for (std::size_t i = 0; i < xcoord.size(); ++i)
+  {
+    Point pt(Point(xcoord[i], ycoord[i], zcoord[i]));
+    _point_to_index[pt] = i;
+    addPoint(pt, i);
+  }
+}
+
+Real
+VectorPostprocessorPointSource::computeQpResidual()
+{
+  //  This is negative because it's a forcing function that has been brought over to the left side
+  return -_test[_i][_qp] * _vpp_values[_point_to_index[_current_point]];
+}
