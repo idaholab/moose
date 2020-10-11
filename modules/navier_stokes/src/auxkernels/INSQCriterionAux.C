dc6e85f45c1e4d4abfe5c7a263a8866dbca5dbe4
Gavin Ridley <gavin.keith.ridley@gmail.com>
2020-10-11 14:04:19 -0400

    (NavierStokes) add Q criterion aux kernel for vortex visualization

---
+++ modules/navier_stokes/src/auxkernels/INSQCriterionAux.C
@@ -0,0 +1,43 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "INSQCriterionAux.h"
+
+registerMooseObject("NavierStokesApp", INSQCriterionAux);
+
+InputParameters
+INSQCriterionAux::validParams()
+{
+  InputParameters params = AuxKernel::validParams();
+
+  params.addClassDescription("This class computes the Q criterion, a scalar which"
+                             "aids in vortex identification in turbulent flows");
+  params.addCoupledVar("velocity", "The velocity component");
+  return params;
+}
+
+INSQCriterionAux::INSQCriterionAux(const InputParameters & parameters)
+  : AuxKernel(parameters), _grad_velocity(coupledVectorGradient("velocity"))
+{
+}
+
+Real
+INSQCriterionAux::computeValue()
+{
+  const Real symm_part = 2.0 * std::pow(_grad_velocity[_qp](0, 0), 2) +
+                         2.0 * std::pow(_grad_velocity[_qp](1, 1), 2) +
+                         2.0 * std::pow(_grad_velocity[_qp](2, 2), 2) +
+                         std::pow(_grad_velocity[_qp](0, 2) + _grad_velocity[_qp](2, 0), 2) +
+                         std::pow(_grad_velocity[_qp](0, 1) + _grad_velocity[_qp](1, 0), 2) +
+                         std::pow(_grad_velocity[_qp](1, 2) + _grad_velocity[_qp](2, 1), 2);
+  const Real antisymm_part = std::pow(_grad_velocity[_qp](0, 1) - _grad_velocity[_qp](1, 0), 2) +
+                             std::pow(_grad_velocity[_qp](1, 3) - _grad_velocity[_qp](3, 1), 2) +
+                             std::pow(_grad_velocity[_qp](2, 3) - _grad_velocity[_qp](3, 2), 2);
+  return 0.5 * (antisymm_part - symm_part);
+}
