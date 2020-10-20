04bcf53c6b68f93a3cbe14f4739b16766d5ed67a
Alex Lindsay <alexlindsay239@gmail.com>
2020-10-19 18:22:27 -0700

    Update RZ RC MMS tests for correction in gradient calculation

--- modules/navier_stokes/test/tests/finite_volume/ins/mms/cylindrical/test.py
+++ modules/navier_stokes/test/tests/finite_volume/ins/mms/cylindrical/test.py
@@ -20,7 +20,7 @@ class Test1DAverage(unittest.TestCase):
 class Test1DRC(unittest.TestCase):
     def test(self):
         labels = ['L2u', 'L2p']
-        df1 = mms.run_spatial('1d-rc.i', 7, y_pp=labels)
+        df1 = mms.run_spatial('1d-rc.i', 11, y_pp=labels)
 
         fig = mms.ConvergencePlot(xlabel='Element Size ($h$)', ylabel='$L_2$ Error')
         fig.plot(df1, label=labels, marker='o', markersize=8, num_fitted_points=3, slope_precision=1)
@@ -42,8 +42,11 @@ class Test2DRC(unittest.TestCase):
         fig.save('2d-rc.png')
         for key,value in fig.label_to_slope.items():
             print("%s, %f" % (key, value))
+            # This appears to be converging asymptotically to 1, however, it
+            # takes too much CPU power for a test to go through enough mesh
+            # refinements to get there
             if key == 'L2p':
-                self.assertTrue(fuzzyAbsoluteEqual(value, 1., .05))
+                self.assertTrue(value >= .95)
             else:
                 self.assertTrue(fuzzyAbsoluteEqual(value, 2., .05))
 
