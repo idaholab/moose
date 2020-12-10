5283a56e93dfdbe5d976158d9ec95305cfd69587
Alex Lindsay <alexlindsay239@gmail.com>
2020-12-10 14:43:24 -0800

    Remove INSFVAdvectionBase and INSFV*FunctionBC
    I've determined that I can do RC cavity problems without
    `INSFV*FunctionBC` and consequently I can just use
    `FVMatAdvectionFunctionBC` for average velocity interpolation cases.
    With this realization there is also no longer a need for
    `INSFVAdvectionBase`

--- modules/navier_stokes/include/fv/kernels/INSFVMomentumAdvection.h
+++ modules/navier_stokes/include/fv/kernels/INSFVMomentumAdvection.h
@@ -10,13 +10,12 @@
 #pragma once
 
 #include "FVMatAdvection.h"
-#include "INSFVAdvectionBase.h"
 
 /**
  * An advection kernel that implements interpolation schemes specific to Navier-Stokes flow
  * physics
  */
-class INSFVMomentumAdvection : public FVMatAdvection, protected INSFVAdvectionBase
+class INSFVMomentumAdvection : public FVMatAdvection
 {
 public:
   static InputParameters validParams();
@@ -33,8 +32,8 @@ protected:
 
   ADReal computeQpResidual() override;
 
-  void residualSetup() override { clearRCCoeffs(); }
-  void jacobianSetup() override { clearRCCoeffs(); }
+  void residualSetup() override final { clearRCCoeffs(); }
+  void jacobianSetup() override final { clearRCCoeffs(); }
 
   /// The dynamic viscosity on the FaceInfo elem
   const ADMaterialProperty<Real> & _mu_elem;
@@ -42,7 +41,45 @@ protected:
   /// The dynamic viscosity on the FaceInfo neighbor
   const ADMaterialProperty<Real> & _mu_neighbor;
 
-  const VectorValue<ADReal> & rcCoeff(const Elem & elem, const ADReal & mu) const override;
+  /**
+   * Returns the Rhie-Chow 'a' coefficient for the requested element \p elem
+   * @param elem The elem to get the Rhie-Chow coefficient for
+   * @param mu The dynamic viscosity
+   */
+  const VectorValue<ADReal> & rcCoeff(const Elem & elem, const ADReal & mu) const;
 
-  VectorValue<ADReal> coeffCalculator(const Elem & elem, const ADReal & mu) const override;
+  /**
+   * method for computing the Rhie-Chow 'a' coefficients for the given elem \p elem
+   * @param elem The elem to compute the Rhie-Chow coefficient for
+   * @param mu The dynamic viscosity
+   */
+  VectorValue<ADReal> coeffCalculator(const Elem & elem, const ADReal & mu) const;
+
+  /**
+   * Clear the RC 'a' coefficient cache
+   */
+  void clearRCCoeffs();
+
+  /// pressure variable
+  const MooseVariableFV<Real> * const _p_var;
+  /// x-velocity
+  const MooseVariableFV<Real> * const _u_var;
+  /// y-velocity
+  const MooseVariableFV<Real> * const _v_var;
+  /// z-velocity
+  const MooseVariableFV<Real> * const _w_var;
+
+  /// Density
+  const Real & _rho;
+
+  /// The interpolation method to use for the velocity
+  Moose::FV::InterpMethod _velocity_interp_method;
+
+private:
+  /// A map from elements to the 'a' coefficients used in the Rhie-Chow interpolation. The size of
+  /// the vector is equal to the number of threads in the simulation. We maintain a map from
+  /// MooseApp pointer to RC coefficients in order to support MultiApp simulations
+  static std::unordered_map<const MooseApp *,
+                            std::vector<std::unordered_map<const Elem *, VectorValue<ADReal>>>>
+      _rc_a_coeffs;
 };
--- modules/navier_stokes/src/fv/kernels/INSFVMomentumAdvection.C
+++ modules/navier_stokes/src/fv/kernels/INSFVMomentumAdvection.C
@@ -22,11 +22,30 @@
 
 registerMooseObject("NavierStokesApp", INSFVMomentumAdvection);
 
+std::unordered_map<const MooseApp *,
+                   std::vector<std::unordered_map<const Elem *, VectorValue<ADReal>>>>
+    INSFVMomentumAdvection::_rc_a_coeffs;
+
 InputParameters
 INSFVMomentumAdvection::validParams()
 {
   InputParameters params = FVMatAdvection::validParams();
-  params += INSFVAdvectionBase::validParams();
+  params.addRequiredCoupledVar("pressure", "The pressure variable.");
+  params.addRequiredCoupledVar("u", "The velocity in the x direction.");
+  params.addCoupledVar("v", "The velocity in the y direction.");
+  params.addCoupledVar("w", "The velocity in the z direction.");
+
+  MooseEnum velocity_interp_method("average rc", "rc");
+
+  params.addParam<MooseEnum>(
+      "velocity_interp_method",
+      velocity_interp_method,
+      "The interpolation to use for the velocity. Options are "
+      "'average' and 'rc' which stands for Rhie-Chow. The default is Rhie-Chow.");
+
+  params.addRequiredParam<MaterialPropertyName>("mu", "The viscosity");
+  params.addRequiredParam<Real>("rho", "The value for the density");
+  params.declareControllable("rho");
 
   // We need 2 ghost layers for the Rhie-Chow interpolation
   params.set<unsigned short>("ghost_layers") = 2;
@@ -38,29 +57,70 @@ INSFVMomentumAdvection::validParams()
 
 INSFVMomentumAdvection::INSFVMomentumAdvection(const InputParameters & params)
   : FVMatAdvection(params),
-    INSFVAdvectionBase(params),
     _mu_elem(getADMaterialProperty<Real>("mu")),
-    _mu_neighbor(getNeighborADMaterialProperty<Real>("mu"))
+    _mu_neighbor(getNeighborADMaterialProperty<Real>("mu")),
+    _p_var(dynamic_cast<const MooseVariableFV<Real> *>(
+        &_subproblem.getVariable(_tid, params.get<std::vector<VariableName>>("pressure").front()))),
+    _u_var(dynamic_cast<const MooseVariableFV<Real> *>(
+        &_subproblem.getVariable(_tid, params.get<std::vector<VariableName>>("u").front()))),
+    _v_var(params.isParamValid("v")
+               ? dynamic_cast<const MooseVariableFV<Real> *>(&_subproblem.getVariable(
+                     _tid, params.get<std::vector<VariableName>>("v").front()))
+               : nullptr),
+    _w_var(params.isParamValid("w")
+               ? dynamic_cast<const MooseVariableFV<Real> *>(&_subproblem.getVariable(
+                     _tid, params.get<std::vector<VariableName>>("w").front()))
+               : nullptr),
+    _rho(params.get<Real>("rho"))
 {
 #ifndef MOOSE_GLOBAL_AD_INDEXING
   mooseError("INSFV is not supported by local AD indexing. In order to use INSFV, please run the "
              "configure script in the root MOOSE directory with the configure option "
              "'--with-ad-indexing-type=global'");
 #endif
+
+  if (!_p_var)
+    mooseError("the pressure must be a finite volume variable.");
+
+  if (!_u_var)
+    mooseError("the u velocity must be a finite volume variable.");
+
+  if (_subproblem.mesh().dimension() >= 2 && !_v_var)
+    mooseError(
+        "In two or more dimensions, the v velocity must be supplied and it must be a finite volume "
+        "variable.");
+
+  if (_subproblem.mesh().dimension() >= 3 && !params.isParamValid("w"))
+    mooseError("In three-dimensions, the w velocity must be supplied and it must be a finite "
+               "volume variable.");
+
+  const auto & velocity_interp_method = params.get<MooseEnum>("velocity_interp_method");
+  if (velocity_interp_method == "average")
+    _velocity_interp_method = Moose::FV::InterpMethod::Average;
+  else if (velocity_interp_method == "rc")
+    _velocity_interp_method = Moose::FV::InterpMethod::RhieChow;
+  else
+    mooseError("Unrecognized interpolation type ",
+               static_cast<std::string>(velocity_interp_method));
+
+  if (_tid == 0)
+  {
+    auto & vec_of_coeffs_map = _rc_a_coeffs[&_app];
+    vec_of_coeffs_map.resize(libMesh::n_threads());
+  }
 }
 
-#ifdef MOOSE_GLOBAL_AD_INDEXING
 const VectorValue<ADReal> &
 INSFVMomentumAdvection::rcCoeff(const Elem & elem, const ADReal & mu) const
 {
-  auto it = _rc_a_coeffs.find(&_nsfv_app);
+  auto it = _rc_a_coeffs.find(&_app);
   mooseAssert(it != _rc_a_coeffs.end(),
               "No RC coeffs structure exists for the given MooseApp pointer");
-  mooseAssert(_nsfv_tid < it->second.size(),
+  mooseAssert(_tid < it->second.size(),
               "The RC coeffs structure size "
                   << it->second.size() << " is greater than or equal to the provided thread ID "
-                  << _nsfv_tid);
-  auto & my_map = it->second[_nsfv_tid];
+                  << _tid);
+  auto & my_map = it->second[_tid];
 
   auto rc_map_it = my_map.find(&elem);
 
@@ -76,6 +136,7 @@ INSFVMomentumAdvection::rcCoeff(const Elem & elem, const ADReal & mu) const
   return emplace_ret.first->second;
 }
 
+#ifdef MOOSE_GLOBAL_AD_INDEXING
 VectorValue<ADReal>
 INSFVMomentumAdvection::coeffCalculator(const Elem & elem, const ADReal & mu) const
 {
@@ -164,7 +225,7 @@ INSFVMomentumAdvection::coeffCalculator(const Elem & elem, const ADReal & mu) co
     }
   };
 
-  Moose::FV::loopOverElemFaceInfo(elem, _nsfv_subproblem.mesh(), _nsfv_subproblem, action_functor);
+  Moose::FV::loopOverElemFaceInfo(elem, _subproblem.mesh(), _subproblem, action_functor);
 
   return coeff;
 }
@@ -275,6 +336,13 @@ INSFVMomentumAdvection::interpolate(Moose::FV::InterpMethod m,
     v(i) -= face_D(i) * (grad_p(i) - unc_grad_p(i));
 }
 #else
+
+VectorValue<ADReal>
+INSFVMomentumAdvection::coeffCalculator(const Elem &, const ADReal &) const
+{
+  mooseError("INSFVMomentumAdvection only works with global AD indexing");
+}
+
 void
 INSFVMomentumAdvection::interpolate(Moose::FV::InterpMethod,
                                     ADRealVectorValue &,
@@ -299,4 +367,17 @@ INSFVMomentumAdvection::computeQpResidual()
                          *_face_info,
                          true);
   return _normal * v * u_interface;
+}
+
+void
+INSFVMomentumAdvection::clearRCCoeffs()
+{
+  auto it = _rc_a_coeffs.find(&_app);
+  mooseAssert(it != _rc_a_coeffs.end(),
+              "No RC coeffs structure exists for the given MooseApp pointer");
+  mooseAssert(_tid < it->second.size(),
+              "The RC coeffs structure size "
+                  << it->second.size() << " is greater than or equal to the provided thread ID "
+                  << _tid);
+  it->second[_tid].clear();
 }
