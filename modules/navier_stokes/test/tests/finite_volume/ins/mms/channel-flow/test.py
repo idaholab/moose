baeecb713c6d5732e2f3fc07b3758f8c3f2ef215
Alex Lindsay <alexlindsay239@gmail.com>
2020-12-14 18:03:24 -0800

    Remove 1D RC tests
    I would have to reformulate them for the refactor and our 2D covers a
    superset of the 1D modeling capability

--- modules/navier_stokes/test/tests/finite_volume/ins/mms/channel-flow/test.py
+++ modules/navier_stokes/test/tests/finite_volume/ins/mms/channel-flow/test.py
@@ -14,18 +14,6 @@ class Test1DAverage(unittest.TestCase):
             print("%s, %f" % (key, value))
             self.assertTrue(fuzzyAbsoluteEqual(value, 2., .05))
 
-class Test1DRC(unittest.TestCase):
-    def test(self):
-        labels = ['L2u', 'L2p']
-        df1 = mms.run_spatial('1d-rc.i', 8, y_pp=labels)
-
-        fig = mms.ConvergencePlot(xlabel='Element Size ($h$)', ylabel='$L_2$ Error')
-        fig.plot(df1, label=labels, marker='o', markersize=8, num_fitted_points=3, slope_precision=1)
-        fig.save('1d-rc.png')
-        for key,value in fig.label_to_slope.items():
-            print("%s, %f" % (key, value))
-            self.assertTrue(fuzzyAbsoluteEqual(value, 2., .05))
-
 class Test2DAverage(unittest.TestCase):
     def test(self):
         labels = ['L2u', 'L2v', 'L2p']
--- modules/navier_stokes/test/tests/finite_volume/ins/mms/cylindrical/test.py
+++ modules/navier_stokes/test/tests/finite_volume/ins/mms/cylindrical/test.py
@@ -17,21 +17,6 @@ class Test1DAverage(unittest.TestCase):
             else:
                 self.assertTrue(fuzzyAbsoluteEqual(value, 2., .05))
 
-class Test1DRC(unittest.TestCase):
-    def test(self):
-        labels = ['L2u', 'L2p']
-        df1 = mms.run_spatial('1d-rc.i', 11, y_pp=labels)
-
-        fig = mms.ConvergencePlot(xlabel='Element Size ($h$)', ylabel='$L_2$ Error')
-        fig.plot(df1, label=labels, marker='o', markersize=8, num_fitted_points=3, slope_precision=1)
-        fig.save('1d-rc.png')
-        for key,value in fig.label_to_slope.items():
-            print("%s, %f" % (key, value))
-            if key == 'L2p':
-                self.assertTrue(fuzzyAbsoluteEqual(value, 1., .05))
-            else:
-                self.assertTrue(fuzzyAbsoluteEqual(value, 2., .05))
-
 class Test2DRC(unittest.TestCase):
     def test(self):
         labels = ['L2u', 'L2v', 'L2p']
