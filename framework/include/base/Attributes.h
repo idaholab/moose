3779da824525879f92f3012227eb551cb7fac6e3
Alex Lindsay <alexlindsay239@gmail.com>
2021-01-07 12:14:18 -0800

    Add Block and BoundaryRestrictable to AttribInterfaces

--- framework/include/base/Attributes.h
+++ framework/include/base/Attributes.h
@@ -30,6 +30,8 @@ enum class Interfaces
   Postprocessor = 1 << 10,
   VectorPostprocessor = 1 << 11,
   InterfaceUserObject = 1 << 12,
+  BlockRestrictable = 1 << 13,
+  BoundaryRestrictable = 1 << 14
 };
 
 template <>
