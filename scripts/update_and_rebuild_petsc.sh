06d60269a1a8bff782ebe2191c1171e7910e4505
Fande Kong <fdkong.jd@gmail.com>
2021-01-21 11:21:36 -0700

    Switch to use 64-bit indices by default
    There are more and more moose simulations that need to run on HPC for large problems
    
    The large problems need to use 64-bit indices
    
    Closes #16779

--- scripts/update_and_rebuild_petsc.sh
+++ scripts/update_and_rebuild_petsc.sh
@@ -118,6 +118,7 @@ if [ -z "$go_fast" ]; then
       --with-cxx-dialect=C++11 \
       --with-fortran-bindings=0 \
       --with-sowing=0 \
+      --with-64-bit-indices \
       $*
 
   exitIfExitCode $?
