6490ace9f7d581e9bacb9d034031658115ca7493
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2021-01-23 22:52:50 +0100

    Add porous flow INSFV energy diffusion kernel

---
+++ modules/navier_stokes/include/fv/kernels/PINSFVEnergyDiffusion.h
@@ -0,0 +1,34 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "FVDiffusion.h"
+
+/**
+ * A flux kernel diffusion energy in porous media across cell faces
+ */
+class PINSFVEnergyDiffusion : public FVFluxKernel
+{
+public:
+  static InputParameters validParams();
+  PINSFVEnergyDiffusion(const InputParameters & params);
+
+protected:
+  ADReal computeQpResidual() override;
+
+  /// the thermal conductivity
+  const ADMaterialProperty<Real> & _k_elem;
+  /// the neighbor element thermal conductivity
+  const ADMaterialProperty<Real> & _k_neighbor;
+  /// the porosity
+  const ADVariableValue & _eps;
+  /// the neighbor element porosity
+  const ADVariableValue & _eps_neighbor;
+};
