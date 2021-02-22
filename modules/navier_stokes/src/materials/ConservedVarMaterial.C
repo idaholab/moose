b620ed207e47a36d89b1a117de047c76c9f35fd0
Alex Lindsay <alexlindsay239@gmail.com>
2021-02-22 15:28:52 -0800

    clang-format ConservedVarMaterial

--- modules/navier_stokes/src/materials/ConservedVarMaterial.C
+++ modules/navier_stokes/src/materials/ConservedVarMaterial.C
@@ -29,10 +29,9 @@ ConservedVarMaterial::validParams()
   params.addCoupledVar(NS::momentum_y, "rhov");
   params.addCoupledVar(NS::momentum_z, "rhow");
   params.addCoupledVar(
-      NS::T_fluid,
-      "optionally use for approximating second spatial derivatives for stabilization");
+      NS::T_fluid, "optionally use for approximating second spatial derivatives for stabilization");
   params.addClassDescription("Provides access to variables for a conserved variable set "
-    "of density, total fluid energy, and momentum");
+                             "of density, total fluid energy, and momentum");
   params.addParam<MaterialPropertyName>(NS::porosity, "the porosity");
   params.addParam<bool>("velocity_is_superficial",
                         "Whether u in rho_u corresponds to a superficial velocity or a primitive "
@@ -56,34 +55,39 @@ ConservedVarMaterial::ConservedVarMaterial(const InputParameters & params)
     _var_grad_rho_w(isCoupled(NS::momentum_z) ? adCoupledGradient(NS::momentum_z) : _ad_grad_zero),
     _var_grad_grad_rho(adCoupledSecond(NS::density)),
     _var_grad_grad_rho_u(adCoupledSecond(NS::momentum_x)),
-    _var_grad_grad_rho_v(isCoupled(NS::momentum_y) ? adCoupledSecond(NS::momentum_y) : _ad_second_zero),
-    _var_grad_grad_rho_w(isCoupled(NS::momentum_z) ? adCoupledSecond(NS::momentum_z) : _ad_second_zero),
+    _var_grad_grad_rho_v(isCoupled(NS::momentum_y) ? adCoupledSecond(NS::momentum_y)
+                                                   : _ad_second_zero),
+    _var_grad_grad_rho_w(isCoupled(NS::momentum_z) ? adCoupledSecond(NS::momentum_z)
+                                                   : _ad_second_zero),
     _var_rho_dot(_is_transient ? adCoupledDot(NS::density) : _ad_zero),
     _var_rho_et_dot(_is_transient ? adCoupledDot(NS::total_energy_density) : _ad_zero),
     _var_rho_u_dot(_is_transient ? adCoupledDot(NS::momentum_x) : _ad_zero),
     _var_rho_v_dot(isCoupled(NS::momentum_y) && _is_transient ? adCoupledDot(NS::momentum_y)
-                                                          : _ad_zero),
+                                                              : _ad_zero),
     _var_rho_w_dot(isCoupled(NS::momentum_z) && _is_transient ? adCoupledDot(NS::momentum_z)
-                                                          : _ad_zero),
-    _var_grad_grad_T_fluid(isCoupled(NS::T_fluid) ? adCoupledSecond(NS::T_fluid)
-                                                   : _ad_second_zero),
+                                                              : _ad_zero),
+    _var_grad_grad_T_fluid(isCoupled(NS::T_fluid) ? adCoupledSecond(NS::T_fluid) : _ad_second_zero),
     _have_porosity(isParamValid(NS::porosity)),
     _epsilon(_have_porosity ? &getMaterialProperty<Real>(NS::porosity) : nullptr),
-    _superficial_velocity(_have_porosity ? &declareADProperty<RealVectorValue>(NS::superficial_velocity) : nullptr),
-    _velocity_is_superficial(isParamValid("velocity_is_superficial") ? getParam<bool>("velocity_is_superficial") : false)
+    _superficial_velocity(
+        _have_porosity ? &declareADProperty<RealVectorValue>(NS::superficial_velocity) : nullptr),
+    _velocity_is_superficial(
+        isParamValid("velocity_is_superficial") ? getParam<bool>("velocity_is_superficial") : false)
 {
   warnAuxiliaryVariables();
   if (_have_porosity)
     if (!isParamValid("velocity_is_superficial"))
-      mooseError("If the porosity parameter is supplied, then a value for 'velocity_is_superficial' must be supplied");
+      mooseError("If the porosity parameter is supplied, then a value for "
+                 "'velocity_is_superficial' must be supplied");
 }
 
 bool
 ConservedVarMaterial::coupledAuxiliaryVariables() const
 {
   auto & sys = _fe_problem.getAuxiliarySystem();
-  return sys.hasVariable(NS::density) || sys.hasVariable(NS::total_energy_density) || sys.hasVariable(NS::momentum_x) ||
-      sys.hasVariable(NS::momentum_y) || sys.hasVariable(NS::momentum_z);
+  return sys.hasVariable(NS::density) || sys.hasVariable(NS::total_energy_density) ||
+         sys.hasVariable(NS::momentum_x) || sys.hasVariable(NS::momentum_y) ||
+         sys.hasVariable(NS::momentum_z);
 }
 
 void
@@ -134,14 +138,25 @@ ConservedVarMaterial::computeQpProperties()
   _grad_vel_y[_qp] = _v[_qp] * _grad_rho_v[_qp] + _momentum[_qp](1) * grad_vol;
   _grad_vel_z[_qp] = _v[_qp] * _grad_rho_w[_qp] + _momentum[_qp](2) * grad_vol;
 
-  _grad_grad_vel_x[_qp] = (_var_grad_grad_rho_u[_qp] - outer_product(_grad_rho[_qp], _grad_vel_x[_qp]) -
-    outer_product(_grad_vel_x[_qp], _grad_rho[_qp]) - _velocity[_qp](0) * _var_grad_grad_rho[_qp]) / _rho[_qp];
-  _grad_grad_vel_y[_qp] = (_var_grad_grad_rho_v[_qp] - outer_product(_grad_rho[_qp], _grad_vel_y[_qp]) -
-    outer_product(_grad_vel_y[_qp], _grad_rho[_qp]) - _velocity[_qp](1) * _var_grad_grad_rho[_qp]) / _rho[_qp];
-  _grad_grad_vel_z[_qp] = (_var_grad_grad_rho_w[_qp] - outer_product(_grad_rho[_qp], _grad_vel_z[_qp]) -
-    outer_product(_grad_vel_z[_qp], _grad_rho[_qp]) - _velocity[_qp](2) * _var_grad_grad_rho[_qp]) / _rho[_qp];
+  _grad_grad_vel_x[_qp] =
+      (_var_grad_grad_rho_u[_qp] - outer_product(_grad_rho[_qp], _grad_vel_x[_qp]) -
+       outer_product(_grad_vel_x[_qp], _grad_rho[_qp]) -
+       _velocity[_qp](0) * _var_grad_grad_rho[_qp]) /
+      _rho[_qp];
+  _grad_grad_vel_y[_qp] =
+      (_var_grad_grad_rho_v[_qp] - outer_product(_grad_rho[_qp], _grad_vel_y[_qp]) -
+       outer_product(_grad_vel_y[_qp], _grad_rho[_qp]) -
+       _velocity[_qp](1) * _var_grad_grad_rho[_qp]) /
+      _rho[_qp];
+  _grad_grad_vel_z[_qp] =
+      (_var_grad_grad_rho_w[_qp] - outer_product(_grad_rho[_qp], _grad_vel_z[_qp]) -
+       outer_product(_grad_vel_z[_qp], _grad_rho[_qp]) -
+       _velocity[_qp](2) * _var_grad_grad_rho[_qp]) /
+      _rho[_qp];
 
-  const auto vel2_prefactor = (_have_porosity && _velocity_is_superficial) ? 1. / ((*_epsilon)[_qp] * (*_epsilon)[_qp]) : Real(1);
+  const auto vel2_prefactor = (_have_porosity && _velocity_is_superficial)
+                                  ? 1. / ((*_epsilon)[_qp] * (*_epsilon)[_qp])
+                                  : Real(1);
   _specific_internal_energy[_qp] = _total_energy_density[_qp] / _rho[_qp] -
                                    vel2_prefactor * (_velocity[_qp] * _velocity[_qp]) / 2;
 
@@ -155,8 +170,10 @@ ConservedVarMaterial::computeQpProperties()
   _specific_total_enthalpy[_qp] = (_total_energy_density[_qp] + _pressure[_qp]) / _rho[_qp];
   _total_enthalpy_density[_qp] = _rho[_qp] * _specific_total_enthalpy[_qp];
 
-  auto grad_e = _grad_rho_et[_qp] * _v[_qp] + _total_energy_density[_qp] * grad_vol - vel2_prefactor * (_velocity[_qp](0) * _grad_vel_x[_qp] +
-                _velocity[_qp](1) * _grad_vel_y[_qp] + _velocity[_qp](2) * _grad_vel_z[_qp]);
+  auto grad_e = _grad_rho_et[_qp] * _v[_qp] + _total_energy_density[_qp] * grad_vol -
+                vel2_prefactor *
+                    (_velocity[_qp](0) * _grad_vel_x[_qp] + _velocity[_qp](1) * _grad_vel_y[_qp] +
+                     _velocity[_qp](2) * _grad_vel_z[_qp]);
 
   _grad_T_fluid[_qp] = grad_vol * dTdvol + grad_e * dTde;
   _grad_pressure[_qp] = grad_vol * dpdvol + grad_e * dpde;
@@ -164,8 +181,10 @@ ConservedVarMaterial::computeQpProperties()
   auto dvelx_dt = (_drho_u_dt[_qp] - _velocity[_qp](0) * _drho_dt[_qp]) / _rho[_qp];
   auto dvely_dt = (_drho_v_dt[_qp] - _velocity[_qp](1) * _drho_dt[_qp]) / _rho[_qp];
   auto dvelz_dt = (_drho_w_dt[_qp] - _velocity[_qp](2) * _drho_dt[_qp]) / _rho[_qp];
-  auto de_dt = _drho_et_dt[_qp] / _rho[_qp] - _total_energy_density[_qp] / (_rho[_qp] * _rho[_qp]) * _drho_dt[_qp] -
-    vel2_prefactor * (_velocity[_qp](0) * dvelx_dt + _velocity[_qp](1) * dvely_dt + _velocity[_qp](2) * dvelz_dt);
+  auto de_dt = _drho_et_dt[_qp] / _rho[_qp] -
+               _total_energy_density[_qp] / (_rho[_qp] * _rho[_qp]) * _drho_dt[_qp] -
+               vel2_prefactor * (_velocity[_qp](0) * dvelx_dt + _velocity[_qp](1) * dvely_dt +
+                                 _velocity[_qp](2) * dvelz_dt);
   auto dvol_dt = -1 / (_rho[_qp] * _rho[_qp]) * _drho_dt[_qp];
   _dT_dt[_qp] = dTde * de_dt + dTdvol * dvol_dt;
 
