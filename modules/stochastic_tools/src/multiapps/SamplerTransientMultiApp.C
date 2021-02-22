b2dc92acc7ffd8777c6bd06c0d517bc13c983d1f
Wen Jiang <jiangwen84@gmail.com>
2021-02-22 12:26:41 -0700

    Update modules/stochastic_tools/src/multiapps/SamplerTransientMultiApp.C
    Co-authored-by: Logan Harbour <loganhharbour@gmail.com>

--- modules/stochastic_tools/src/multiapps/SamplerTransientMultiApp.C
+++ modules/stochastic_tools/src/multiapps/SamplerTransientMultiApp.C
@@ -170,7 +170,7 @@ SamplerTransientMultiApp::getActiveStochasticToolsTransfers(Transfer::DIRECTION 
   {
     std::shared_ptr<StochasticToolsTransfer> ptr =
         std::dynamic_pointer_cast<StochasticToolsTransfer>(transfer);
-    if (ptr && ptr->getMultiApp()->name() == this->name())
+    if (ptr && ptr->getMultiApp().get() == this)
       output.push_back(ptr);
   }
   return output;
