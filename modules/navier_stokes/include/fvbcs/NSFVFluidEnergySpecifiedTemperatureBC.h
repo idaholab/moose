3fb7456ca72ec4f2a714b8e4589be3f4832495b2
Alex Lindsay <alexlindsay239@gmail.com>
2021-02-25 15:07:43 -0800

    Add NSFVFluidEnergySpecifiedTemperatureBC
    This object relies on the user choosing superficial momentum and
    temperature as the physical boundary conditions at a subsonic inlet. It
    then uses implicitly computed values of rho and velocity and related
    state properties such as specific internal energy and pressure to
    compute the enthalpy flux

---
+++ modules/navier_stokes/include/fvbcs/NSFVFluidEnergySpecifiedTemperatureBC.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "FVFluxBC.h"
+
+class Function;
+class SinglePhaseFluidProperties;
+
+/**
+ * This boundary provides the advective flux of fluid energy across a boundary
+ * given a specified temperature
+ */
+class NSFVFluidEnergySpecifiedTemperatureBC : public FVFluxBC
+{
+public:
+  NSFVFluidEnergySpecifiedTemperatureBC(const InputParameters & parameters);
+  static InputParameters validParams();
+
+protected:
+  virtual ADReal computeQpResidual() override;
+
+  const Function & _temperature;
+  const Function & _superficial_rhou;
+  const Function * const _superficial_rhov;
+  const Function * const _superficial_rhow;
+  const ADMaterialProperty<Real> & _rho_elem;
+  const ADMaterialProperty<Real> & _rho_neighbor;
+  const MaterialProperty<Real> & _porosity_elem;
+  const MaterialProperty<Real> & _porosity_neighbor;
+
+  /// fluid properties
+  const SinglePhaseFluidProperties & _fluid;
+};
