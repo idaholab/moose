c7bfe9b3410df4d3a14735d87972e193e5a1c299
Alex Lindsay <alexlindsay239@gmail.com>
2021-03-15 15:53:37 -0700

    Create fv versions of InfiniteCylinderRadiativeBC and ThermalResistanceBC

---
+++ modules/heat_conduction/include/fvbcs/FVRadiativeHeatFluxBCBase.h
@@ -0,0 +1,45 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "FVFluxBC.h"
+
+/**
+ * Boundary condition for radiative heat flux where temperature and the
+ * temperature of a body in radiative heat transfer are specified.
+ */
+class FVRadiativeHeatFluxBCBase : public FVFluxBC
+{
+public:
+  static InputParameters validParams();
+
+  FVRadiativeHeatFluxBCBase(const InputParameters & parameters);
+
+protected:
+  virtual ADReal computeQpResidual();
+
+  /**
+   * qdot = sigma * coeff * (T^4 - Tinf^4 )
+   * sigma: _sigma_stefan_boltzmann
+   * coeff: coefficient()
+   * coefficientBody: cbody
+   * Tinf: temperature of the body irhs
+   */
+  virtual Real coefficient() const = 0;
+
+  /// Stefan-Boltzmann constant
+  const Real _sigma_stefan_boltzmann;
+
+  /// Function describing the temperature of the body irhs
+  const Function & _tinf;
+
+  /// Emissivity of the boundary
+  const Real _eps_boundary;
+};
