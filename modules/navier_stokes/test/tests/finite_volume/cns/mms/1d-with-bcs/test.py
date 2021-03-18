d598398d36dea53889d1505b2d91c42736e9b95f
Alex Lindsay <alexlindsay239@gmail.com>
2021-03-18 14:58:36 -0700

    Add MMS test for new generic free-flow HLLC BCs

--- modules/navier_stokes/test/tests/finite_volume/cns/mms/1d-with-bcs/test.py
+++ modules/navier_stokes/test/tests/finite_volume/cns/mms/1d-with-bcs/test.py
@@ -28,7 +28,23 @@ class Test1DAverage(unittest.TestCase):
             print("%s, %f" % (key, value))
             self.assertTrue(fuzzyAbsoluteEqual(value, 2., .05))
 
+class Test1DFreeFlowHLLC(unittest.TestCase):
+# class Test1DFreeFlowHLLC():
+    def test(self):
+        labels = ['L2rho', 'L2rho_u', 'L2rho_et']
+        df1 = mms.run_spatial('free-flow-hllc.i', 9, y_pp=labels)
+
+        fig = mms.ConvergencePlot(xlabel='Element Size ($h$)', ylabel='$L_2$ Error')
+        fig.plot(df1, label=labels, marker='o', markersize=8, num_fitted_points=3, slope_precision=1)
+        fig.save('free-flow-hllc.png')
+        for key,value in fig.label_to_slope.items():
+            print("%s, %f" % (key, value))
+            self.assertTrue(fuzzyAbsoluteEqual(value, 1., .05))
+
+
+
 if __name__ == '__main__':
     unittest.main(__name__, verbosity=2)
     # Test1DUpwind().test()
     # Test1DAverage().test()
+    # Test1DFreeFlowHLLC().test()
