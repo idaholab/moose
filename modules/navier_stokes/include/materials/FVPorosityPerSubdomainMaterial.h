f9626753f041cb1c9a6e9770bdbb5dad18dc9e89
Alex Lindsay <alexlindsay239@gmail.com>
2021-03-30 16:38:27 -0700

    Create FVPorosityPerSubdomainMaterial

---
+++ modules/navier_stokes/include/materials/FVPorosityPerSubdomainMaterial.h
@@ -0,0 +1,28 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "Material.h"
+
+#include <unordered_map>
+
+class FVPorosityPerSubdomainMaterial : public Material
+{
+public:
+  FVPorosityPerSubdomainMaterial(const InputParameters & parameters);
+  static InputParameters validParams();
+
+protected:
+  virtual void computeQpProperties() override;
+
+private:
+  MaterialProperty<Real> & _epsilon;
+  std::unordered_map<SubdomainID, Real> _sub_id_to_epsilon;
+};
