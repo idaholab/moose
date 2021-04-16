1d3ae2705627a90e61026f227fcd954e60ff04ab
Alex Lindsay <alexlindsay239@gmail.com>
2021-04-15 22:29:27 -0700

    Create grad epsilon kernels
    One is implemented using a volumetric `FVElementalKernel` which is
    useful when epsilon is continuous. Another is based on an
    `FVInterfaceKernel` which is useful when epsilon is discontinuous.
    
    - FVEpsilonJumps: discontinuous
    - PNSFVGradEpsilon: continuous

---
+++ modules/navier_stokes/include/fvkernels/PNSFVPGradEpsilon.h
@@ -0,0 +1,29 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "FVElementalKernel.h"
+
+class PNSFVPGradEpsilon : public FVElementalKernel
+{
+public:
+  static InputParameters validParams();
+  PNSFVPGradEpsilon(const InputParameters & params);
+
+protected:
+  ADReal computeQpResidual() override;
+
+  /// The gradient of the pressure
+  const ADMaterialProperty<Real> & _pressure;
+  const Function & _eps;
+
+  /// index x|y|z
+  const unsigned int _index;
+};
