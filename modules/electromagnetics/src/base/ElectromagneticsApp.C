7c6a511176927a19bf6cc433edd024143f91c967
Casey Icenhour <cticenho@ncsu.edu>
2021-04-17 12:23:21 -0600

    Init template electromagnetics module structure after transferring files from ELK (#13744)

---
+++ modules/electromagnetics/src/base/ElectromagneticsApp.C
@@ -0,0 +1,58 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "ElectromagneticsApp.h"
+#include "Moose.h"
+#include "AppFactory.h"
+#include "MooseSyntax.h"
+
+InputParameters
+ElectromagneticsApp::validParams()
+{
+  InputParameters params = MooseApp::validParams();
+
+  return params;
+}
+
+ElectromagneticsApp::ElectromagneticsApp(InputParameters parameters) : MooseApp(parameters)
+{
+  ElectromagneticsApp::registerAll(_factory, _action_factory, _syntax);
+}
+
+ElectromagneticsApp::~ElectromagneticsApp() {}
+
+void
+ElectromagneticsApp::registerAll(Factory & f, ActionFactory & af, Syntax & /*s*/)
+{
+  /* ModulesApp::registerAll(f, af, s); */
+  Registry::registerObjectsTo(f, {"ElectromagneticsApp"});
+  Registry::registerActionsTo(af, {"ElectromagneticsApp"});
+
+  /* register custom execute flags, action syntax, etc. here */
+}
+
+void
+ElectromagneticsApp::registerApps()
+{
+  registerApp(ElectromagneticsApp);
+}
+
+/***************************************************************************************************
+ *********************** Dynamic Library Entry Points - DO NOT MODIFY ******************************
+ **************************************************************************************************/
+extern "C" void
+ElectromagneticsApp__registerAll(Factory & f, ActionFactory & af, Syntax & s)
+{
+  ElectromagneticsApp::registerAll(f, af, s);
+}
+extern "C" void
+ElectromagneticsApp__registerApps()
+{
+  ElectromagneticsApp::registerApps();
+}
