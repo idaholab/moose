1c2fefb435fdbb0f4d80970c06ac6dab9578ca4e
Daniel Schwen <daniel@schwen.de>
2021-04-21 23:18:49 -0600

    Add boundary marker (#17661)

---
+++ framework/include/markers/BoundaryMarker.h
@@ -0,0 +1,29 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "Marker.h"
+
+/**
+ * Marks all elements with sides on a given boundary for refinement/coarsening
+ */
+class BoundaryMarker : public Marker
+{
+public:
+  static InputParameters validParams();
+
+  BoundaryMarker(const InputParameters & parameters);
+
+protected:
+  virtual MarkerValue computeElementMarker() override;
+
+  MarkerValue _mark;
+  BoundaryID _boundary;
+};
