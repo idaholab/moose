1903f1217c5452d5a10ec354f5b13109e450d579
Yeon Sang Jung <yjung@anl.gov>
2021-05-04 18:01:50 -0500

    added standard variable version of HFEM Diffusion kernels and BCs

---
+++ framework/src/dgkernels/HFEMDiffusion.C
@@ -0,0 +1,75 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "HFEMDiffusion.h"
+
+registerMooseObject("MooseApp", HFEMDiffusion);
+
+InputParameters
+HFEMDiffusion::validParams()
+{
+  InputParameters params = DGLowerDKernel::validParams();
+  params.addClassDescription("Imposes the constraints on internal sides with HFEM.");
+  return params;
+}
+
+HFEMDiffusion::HFEMDiffusion(const InputParameters & parameters) : DGLowerDKernel(parameters) {}
+
+Real
+HFEMDiffusion::computeQpResidual(Moose::DGResidualType type)
+{
+  switch (type)
+  {
+    case Moose::Element:
+      return -_lambda[_qp] * _test[_i][_qp];
+      break;
+
+    case Moose::Neighbor:
+      return _lambda[_qp] * _test_neighbor[_i][_qp];
+      break;
+  }
+  return 0;
+}
+
+Real
+HFEMDiffusion::computeLowerDQpResidual()
+{
+  return (_u_neighbor[_qp] - _u[_qp]) * _test_lambda[_i][_qp];
+}
+
+Real HFEMDiffusion::computeQpJacobian(Moose::DGJacobianType) { return 0; }
+
+Real
+HFEMDiffusion::computeLowerDQpJacobian(Moose::ConstraintJacobianType type)
+{
+
+  switch (type)
+  {
+    case Moose::LowerPrimary:
+      return -_test_lambda[_i][_qp] * _phi[_j][_qp];
+      break;
+
+    case Moose::LowerSecondary:
+      return _test_lambda[_i][_qp] * _phi_neighbor[_j][_qp];
+      break;
+
+    case Moose::PrimaryLower:
+      return -_phi_lambda[_j][_qp] * _test[_i][_qp];
+      break;
+
+    case Moose::SecondaryLower:
+      return _phi_lambda[_j][_qp] * _test_neighbor[_i][_qp];
+      break;
+
+    default:
+      break;
+  }
+
+  return 0;
+}
