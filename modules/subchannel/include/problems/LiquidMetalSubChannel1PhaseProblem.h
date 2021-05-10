ed33b8dc43ae531be09f2bc2f709961db49270fc
Aydin Karahan <akarahan@anl.gov>
2021-05-10 17:03:12 -0600

    The LiquidMetalSubChannel1PhaseProblem.C and h files are modified to add friction factor and external solve methods with override option. The updated friction factor method currently is for water. It will be used to develop the sodium friction factor correlations. The externalsolve method that is implemented accounts for the enforcing uniform inlet pressure distribution by an iterative method. ComputeInletMassFlowDist method assists this task by solving for the mass flow rate distribution at the inlet at each iteration to satisfy the uniform inlet pressure. Refs#29194

--- modules/subchannel/include/problems/LiquidMetalSubChannel1PhaseProblem.h
+++ modules/subchannel/include/problems/LiquidMetalSubChannel1PhaseProblem.h
@@ -13,7 +13,24 @@ public:
   LiquidMetalSubChannel1PhaseProblem(const InputParameters & params);
 
 protected:
+  /**
+   * computes the friction factor for the sodium coolant.
+   * temporarily this is set to the friction factor correlation
+   * given for water coolant.
+   */
   virtual double computeFrictionFactor(double Re) override;
+  /**
+   * computeInletMassFlowDist corrects the inlet mass flow rate distribution
+   * in order to satisfy the uniform inlet pressure condition, iteratively.
+   */
+  virtual void computeInletMassFlowDist();
+  /**
+   * solver with iterative option to enforce uniform inlet
+   * pressure distribution option
+   */
+  virtual void externalSolve() override;
+  /// average relative error in pressure drop of channels
+  Real _dpz_error;
 
 public:
   static InputParameters validParams();
