cfbdc81a8d73b232e74f05e86244726bffd813b7
Alex Lindsay <alexlindsay239@gmail.com>
2021-05-10 13:32:58 -0700

    Add FVKTLimitedAdvection and order of accuracy tests
    KT here means Kurganov-Tadmor which is a method for computing convective
    intercell fluxes on nonstaggered grids.
    
    Observed orders of accuracy:
    - Upwind: 1
    - Central Differencing: 2
    - VanLeer: 2.5
    
    I honestly don't understand where the additional half order of accuracy
    for VanLeer is coming from. I don't like things I don't understand...

---
+++ framework/src/fvkernels/FVKTLimitedAdvection.C
@@ -0,0 +1,58 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "FVKTLimitedAdvection.h"
+#include "Limiter.h"
+
+registerMooseObject("MooseApp", FVKTLimitedAdvection);
+
+using namespace Moose::FV;
+
+InputParameters
+FVKTLimitedAdvection::validParams()
+{
+  InputParameters params = FVFluxKernel::validParams();
+  params.addClassDescription(
+      "Residual contribution from advection operator for finite volume method.");
+  params.addRequiredParam<RealVectorValue>("velocity", "Constant advection velocity");
+  params.addRequiredParam<MaterialPropertyName>(
+      "max_abs_eig", "The maximum absolute value of the advection Jacobian eigenvalues.");
+  params.addParam<MooseEnum>(
+      "limiter", moose_limiter_type, "The limiter to use for the advected quantity.");
+  params.addParam<bool>("add_artificial_diff", true, "Add artificial diffusion for monotonicity.");
+  return params;
+}
+
+FVKTLimitedAdvection::FVKTLimitedAdvection(const InputParameters & params)
+  : FVFluxKernel(params),
+    _velocity(getParam<RealVectorValue>("velocity")),
+    _limiter(Limiter::build(LimiterType(int(getParam<MooseEnum>("limiter"))))),
+    _cd_limiter(Limiter::build(LimiterType::CentralDifference)),
+    _grad_u_elem(_var.adGradSln()),
+    _grad_u_neighbor(_var.adGradSlnNeighbor()),
+    _max_abs_eig(getADMaterialProperty<Real>("max_abs_eig")),
+    _add_artificial_diff(getParam<bool>("add_artificial_diff"))
+{
+}
+
+ADReal
+FVKTLimitedAdvection::computeQpResidual()
+{
+  const auto phi_avg_f = interpolate(
+      *_cd_limiter, _u_elem[_qp], _u_neighbor[_qp], _grad_u_elem[_qp], *_face_info, true);
+  const auto phi_elem =
+      interpolate(*_limiter, _u_elem[_qp], _u_neighbor[_qp], _grad_u_elem[_qp], *_face_info, true);
+  const auto phi_neighbor = interpolate(
+      *_limiter, _u_neighbor[_qp], _u_elem[_qp], _grad_u_neighbor[_qp], *_face_info, false);
+
+  auto resid = _normal * _velocity * phi_avg_f;
+  if (_add_artificial_diff)
+    resid -= 0.5 * _max_abs_eig[_qp] * (phi_neighbor - phi_elem);
+  return resid;
+}
---
+++ test/tests/fvkernels/mms/advective-outflow/kt-limited-advection.i
@@ -0,0 +1,119 @@
+a=1.1
+c=343
+max_abs_eig=${fparse c + a}
+
+[Mesh]
+  [./gen_mesh]
+    type = GeneratedMeshGenerator
+    dim = 1
+    xmin = 0.1
+    xmax = 1.1
+    nx = 2
+  [../]
+[]
+
+[Problem]
+  kernel_coverage_check = off
+  fv_bcs_integrity_check = false
+[]
+
+[ICs]
+  [u]
+    type = FunctionIC
+    variable = u
+    function = exact
+  []
+[]
+
+[Variables]
+  [./u]
+    two_term_boundary_expansion = true
+    type = MooseVariableFVReal
+  [../]
+[]
+
+[FVKernels]
+  [./advection_u]
+    type = FVKTLimitedAdvection
+    variable = u
+    velocity = '${a} 0 0'
+    limiter = 'vanLeer'
+    max_abs_eig = ${max_abs_eig}
+    add_artificial_diff = true
+  [../]
+  [body_u]
+    type = FVBodyForce
+    variable = u
+    function = 'forcing'
+  []
+[]
+
+[FVBCs]
+  [left_u]
+    type = FVFunctionNeumannBC
+    boundary = 'left'
+    function = 'advection'
+    variable = u
+  []
+  [diri_left]
+    type = FVFunctionDirichletBC
+    boundary = 'left'
+    function = 'exact'
+    variable = u
+  []
+  [right]
+    type = FVConstantScalarOutflowBC
+    variable = u
+    velocity = '${a} 0 0'
+    boundary = 'right'
+  []
+[]
+
+[Functions]
+  [exact]
+    type = ParsedFunction
+    value = 'cos(x)'
+  []
+  [advection]
+    type = ParsedFunction
+    value = '${a} * cos(x)'
+  []
+  [forcing]
+    type = ParsedFunction
+    value = '-${a} * sin(x)'
+  []
+[]
+
+[Executioner]
+  type = Steady
+  petsc_options_iname = '-snes_linesearch_minlambda'
+  petsc_options_value = '1e-3'
+  nl_abs_tol = 1e-9
+[]
+
+[Outputs]
+  file_base = 'kt-limited-advection_out'
+  [csv]
+    type = CSV
+    execute_on = 'final'
+  []
+  [exo]
+    type = Exodus
+    execute_on = 'final'
+  []
+[]
+
+[Postprocessors]
+  [./L2u]
+    type = ElementL2Error
+    variable = u
+    function = exact
+    outputs = 'console csv'
+    execute_on = 'timestep_end'
+  [../]
+  [h]
+    type = AverageElementSize
+    outputs = 'console csv'
+    execute_on = 'timestep_end'
+  []
+[]
