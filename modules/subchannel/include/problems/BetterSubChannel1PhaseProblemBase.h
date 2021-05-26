eb327e382a316411cd8ba267fbc82890936546d5
Vasileios Kyriakopulos <v_kyr1ak@tamu.edu>
2021-05-26 12:43:56 -0500

    Created context for implementing new scheme #Refs 25

--- modules/subchannel/include/problems/BetterSubChannel1PhaseProblemBase.h
+++ modules/subchannel/include/problems/BetterSubChannel1PhaseProblemBase.h
@@ -1,8 +1,7 @@
 #pragma once
-
 #include "ExternalProblem.h"
 #include "SubChannelApp.h"
-#include "SubChannelMeshBase.h"
+#include "BetterSubChannelMeshBase.h"
 #include "SolutionHandle.h"
 #include "SinglePhaseFluidProperties.h"
 
@@ -25,35 +24,47 @@ public:
 protected:
   /// Returns friction factor
   virtual double computeFrictionFactor(double Re);
-  /// Computes diversion crossflow per gap for level iz
-  virtual void computeWij(int iz);
-  /// Computes net diversion crossflow per channel for level iz
-  virtual void computeSumWij(int iz);
-  /// Computes mass flow per channel for level iz
-  virtual void computeMdot(int iz);
-  /// Computes turbulent crossflow per gap for level iz
-  virtual void computeWijPrime(int iz);
-  /// Computes Pressure Drop per channel for level iz
-  virtual void computeDP(int iz);
-  /// Computes Pressure per channel for level iz
-  virtual void computeP(int iz);
-  /// Computes Enthalpy per channel for level iz
-  virtual void computeh(int iz);
-  /// Computes Temperature per channel for level iz
-  virtual void computeT(int iz);
-  /// Computes Density per channel for level iz
-  virtual void computeRho(int iz);
+  /// Computes diversion crossflow per gap for block iblock
+  virtual void computeWij(int iblock);
+  /// Computes net diversion crossflow per channel for block iblock
+  virtual void computeSumWij(int iblock);
+  /// Computes mass flow per channel for block iblock
+  virtual void computeMdot(int iblock);
+  /// Computes turbulent crossflow per gap for block iblock
+  virtual void computeWijPrime(int iblock);
+  /// Computes Pressure Drop per channel for block iblock
+  virtual void computeDP(int iblock);
+  /// Computes Pressure per channel for block iblock
+  virtual void computeP(int iblock);
+  /// Computes Enthalpy per channel for block iblock
+  virtual void computeh(int iblock);
+  /// Computes Temperature per channel for block iblock
+  virtual void computeT(int iblock);
+  /// Computes Density per channel for block iblock
+  virtual void computeRho(int iblock);
+  /// Computes Viscosity per channel for block iblock
+  virtual void computeMu(int iblock);
+  /// Computes Residual per gap for block iblock
+  virtual void computeResidualFunction(int iblock);
 
   Eigen::MatrixXd Wij;
   Eigen::MatrixXd Wij_old;
   Eigen::MatrixXd WijPrime;
   const Real _g_grav;
+  unsigned int n_cells;
+  unsigned int n_blocks;
+  unsigned int n_gaps;
+  unsigned int block_size;
   Real _one;
   /// Flag that activates or deactivates the transient parts of the equations solved
   Real _TR;
+  /// Flag that activates or deactivates the calculation of density
+  const bool _Density;
+  /// Flag that activates or deactivates the calculation of viscosity
+  const bool _Viscosity;
   /// Time step
   const Real & _dt;
-  SubChannelMeshBase & _subchannel_mesh;
+  BetterSubChannelMeshBase & _subchannel_mesh;
   /// Thermal diffusion coefficient used in turbulent crossflow
   const Real & _beta;
   /// Turbulent modeling parameter used in axial momentum equation
@@ -68,6 +79,7 @@ protected:
   SolutionHandle * h_soln;
   SolutionHandle * T_soln;
   SolutionHandle * rho_soln;
+  SolutionHandle * Mu_soln;
   SolutionHandle * S_flow_soln;
   SolutionHandle * w_perim_soln;
   SolutionHandle * q_prime_soln;
