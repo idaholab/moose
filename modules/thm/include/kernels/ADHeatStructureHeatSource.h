281b147918ea0a95cf053c70710588053c45533d
Rachel Beall <rlbeall@ncsu.edu>
2021-06-17 09:03:41 -0400

    Converting HeatStructureHeatSource to AD
    Refs #19868

---
+++ modules/thm/include/kernels/ADHeatStructureHeatSource.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "ADKernel.h"
+#include "Function.h"
+
+/**
+ *
+ */
+class ADHeatStructureHeatSource : public ADKernel
+{
+public:
+  ADHeatStructureHeatSource(const InputParameters & parameters);
+
+protected:
+  virtual ADReal computeQpResidual();
+
+  const Real & _power_fraction;
+  const ADVariableValue & _total_power;
+  const Function & _power_shape_function;
+  const PostprocessorValue & _power_shape_integral;
+  const Real & _scale;
+  const Real & _num_units;
+
+public:
+  static InputParameters validParams();
+};
