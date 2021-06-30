9ac3b61ec21dc63551185996afb3f740a4208af9
David Andrs <david.andrs@inl.gov>
2021-06-30 12:39:41 -0600

    Converting HSBoundaryHeatFlux into AD

---
+++ modules/thm/src/bcs/ADHSHeatFluxRZBC.C
@@ -0,0 +1,28 @@
+#include "ADHSHeatFluxRZBC.h"
+#include "Function.h"
+
+registerMooseObject("THMApp", ADHSHeatFluxRZBC);
+
+InputParameters
+ADHSHeatFluxRZBC::validParams()
+{
+  InputParameters params = ADHSHeatFluxBC::validParams();
+  params += RZSymmetry::validParams();
+
+  params.addClassDescription(
+      "Applies a specified heat flux to the side of a cylindrical heat structure");
+
+  return params;
+}
+
+ADHSHeatFluxRZBC::ADHSHeatFluxRZBC(const InputParameters & parameters)
+  : ADHSHeatFluxBC(parameters), RZSymmetry(parameters)
+{
+}
+
+ADReal
+ADHSHeatFluxRZBC::computeQpResidual()
+{
+  const Real P_heat = computeCircumference(_q_point[_qp]);
+  return P_heat * ADHSHeatFluxBC::computeQpResidual();
+}
