cfc5ebcd89be772427e5fc36e5d4b181db09aa41
Rachel Beall <rlbeall@ncsu.edu>
2021-07-14 09:48:08 -0400

    Converting Flow DGKernel

---
+++ modules/thm/src/userobjects/ADNumericalFlux3EqnBase.C
@@ -0,0 +1,62 @@
+#include "ADNumericalFlux3EqnBase.h"
+
+InputParameters
+ADNumericalFlux3EqnBase::validParams()
+{
+  InputParameters params = GeneralUserObject::validParams();
+  return params;
+}
+
+ADNumericalFlux3EqnBase::ADNumericalFlux3EqnBase(const InputParameters & parameters)
+  : ThreadedGeneralUserObject(parameters),
+
+    _cached_flux_elem_id(libMesh::invalid_uint),
+    _cached_flux_side_id(libMesh::invalid_uint),
+
+    _last_region_index(0)
+{
+}
+
+void
+ADNumericalFlux3EqnBase::initialize()
+{
+  _cached_flux_elem_id = libMesh::invalid_uint;
+  _cached_flux_side_id = libMesh::invalid_uint;
+}
+
+void
+ADNumericalFlux3EqnBase::execute()
+{
+}
+
+void
+ADNumericalFlux3EqnBase::finalize()
+{
+}
+
+void
+ADNumericalFlux3EqnBase::threadJoin(const UserObject &)
+{
+}
+
+const std::vector<ADReal> &
+ADNumericalFlux3EqnBase::getFlux(const unsigned int iside,
+                                 const dof_id_type ielem,
+                                 bool res_side_is_left,
+                                 const std::vector<ADReal> & UL,
+                                 const std::vector<ADReal> & UR,
+                                 const ADReal & nLR_dot_d) const
+{
+  if (_cached_flux_elem_id != ielem || _cached_flux_side_id != iside)
+  {
+    _cached_flux_elem_id = ielem;
+    _cached_flux_side_id = iside;
+
+    calcFlux(UL, UR, nLR_dot_d, _FL, _FR);
+  }
+
+  if (res_side_is_left)
+    return _FL;
+  else
+    return _FR;
+}
