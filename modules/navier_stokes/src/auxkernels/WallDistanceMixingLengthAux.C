15624d4b3190679046570b96430700a871936104
freiler <freiler@tamu.edu>
2021-07-22 09:32:55 -0500

    Modifications

--- modules/navier_stokes/src/auxkernels/WallDistanceMixingLengthAux.C
+++ modules/navier_stokes/src/auxkernels/WallDistanceMixingLengthAux.C
@@ -62,6 +62,21 @@ WallDistanceMixingLengthAux::computeValue()
       min_sq_dist = std::min(min_sq_dist, sq_dist);
     }
   }
+<<<<<<< HEAD
   // Return the mixing length
   return _von_karman_const * std::sqrt(min_sq_dist);
+=======
+
+    // Return the mixing length
+    Real delta = 0.1; //arbitrary boundary layer thickness based on geometry
+    Real _von_karman_const_0 = 0.09;
+    if (std::sqrt(min_sq_dist)/delta <= _von_karman_const_0/_von_karman_const)
+    {
+      return _von_karman_const * std::sqrt(min_sq_dist);
+    }
+    else
+    {
+      return _von_karman_const_0 * delta;
+    }
+>>>>>>> Modifications
 }
