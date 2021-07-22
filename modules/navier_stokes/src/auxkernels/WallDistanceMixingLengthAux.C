a9b2c0f6d1b43485ceaa30601952830a53e1b652
freiler <freiler@tamu.edu>
2021-07-22 10:20:55 -0500

    Updates
    Escudier capped mixing length
    
    Tuning mixinglength

--- modules/navier_stokes/src/auxkernels/WallDistanceMixingLengthAux.C
+++ modules/navier_stokes/src/auxkernels/WallDistanceMixingLengthAux.C
@@ -69,7 +69,7 @@ WallDistanceMixingLengthAux::computeValue()
 
     // Return the mixing length
     Real delta = 0.1; //arbitrary boundary layer thickness based on geometry
-    Real _von_karman_const_0 = 0.09;
+    Real _von_karman_const_0 = 0.05;
     if (std::sqrt(min_sq_dist)/delta <= _von_karman_const_0/_von_karman_const)
     {
       return _von_karman_const * std::sqrt(min_sq_dist);
