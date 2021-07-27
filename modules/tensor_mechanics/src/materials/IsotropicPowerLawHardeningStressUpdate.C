2b456fec544cdd4495c94373b4ea0ba26537c97d
Lynn Munday <lynn.munday@inl.gov>
2021-07-27 11:53:47 -0600

    add templated version of AD powerlaw plasticity and test

--- modules/tensor_mechanics/src/materials/IsotropicPowerLawHardeningStressUpdate.C
+++ modules/tensor_mechanics/src/materials/IsotropicPowerLawHardeningStressUpdate.C
@@ -11,11 +11,13 @@
 #include "ElasticityTensorTools.h"
 
 registerMooseObject("TensorMechanicsApp", IsotropicPowerLawHardeningStressUpdate);
+registerMooseObject("TensorMechanicsApp", ADIsotropicPowerLawHardeningStressUpdate);
 
+template <bool is_ad>
 InputParameters
-IsotropicPowerLawHardeningStressUpdate::validParams()
+IsotropicPowerLawHardeningStressUpdateTempl<is_ad>::validParams()
 {
-  InputParameters params = IsotropicPlasticityStressUpdate::validParams();
+  InputParameters params = IsotropicPlasticityStressUpdateTempl<is_ad>::validParams();
   params.addClassDescription("This class uses the discrete material in a radial return isotropic "
                              "plasticity power law hardening model, solving for the yield stress "
                              "as the intersection of the power law relation curve and Hooke's law. "
@@ -39,62 +41,73 @@ IsotropicPowerLawHardeningStressUpdate::validParams()
   return params;
 }
 
-IsotropicPowerLawHardeningStressUpdate::IsotropicPowerLawHardeningStressUpdate(
+template <bool is_ad>
+IsotropicPowerLawHardeningStressUpdateTempl<is_ad>::IsotropicPowerLawHardeningStressUpdateTempl(
     const InputParameters & parameters)
-  : IsotropicPlasticityStressUpdate(parameters),
+  : IsotropicPlasticityStressUpdateTempl<is_ad>(parameters),
     _K(parameters.get<Real>("strength_coefficient")),
     _strain_hardening_exponent(parameters.get<Real>("strain_hardening_exponent"))
 {
 }
 
+template <bool is_ad>
 void
-IsotropicPowerLawHardeningStressUpdate::computeStressInitialize(
-    const Real & effective_trial_stress, const RankFourTensor & elasticity_tensor)
+IsotropicPowerLawHardeningStressUpdateTempl<is_ad>::computeStressInitialize(
+    const GenericReal<is_ad> & effective_trial_stress,
+    const GenericRankFourTensor<is_ad> & elasticity_tensor)
 {
   computeYieldStress(elasticity_tensor);
 
   _effective_trial_stress = effective_trial_stress;
-  _yield_condition = effective_trial_stress - _hardening_variable_old[_qp] - _yield_stress;
+  this->_yield_condition =
+      effective_trial_stress - this->_hardening_variable_old[_qp] - this->_yield_stress;
 
-  _hardening_variable[_qp] = _hardening_variable_old[_qp];
-  _plastic_strain[_qp] = _plastic_strain_old[_qp];
+  this->_hardening_variable[_qp] = this->_hardening_variable_old[_qp];
+  this->_plastic_strain[_qp] = this->_plastic_strain_old[_qp];
 }
 
-Real
-IsotropicPowerLawHardeningStressUpdate::computeHardeningDerivative(Real scalar)
+template <bool is_ad>
+GenericReal<is_ad>
+IsotropicPowerLawHardeningStressUpdateTempl<is_ad>::computeHardeningDerivative(
+    GenericReal<is_ad> scalar)
 {
-  const Real stress_delta = _effective_trial_stress - _three_shear_modulus * scalar;
-  Real slope = std::pow(stress_delta, (1.0 / _strain_hardening_exponent - 1.0)) /
-               _strain_hardening_exponent * 1.0 / std::pow(_K, 1.0 / _strain_hardening_exponent);
+  const GenericReal<is_ad> stress_delta = _effective_trial_stress - _three_shear_modulus * scalar;
+  GenericReal<is_ad> slope = std::pow(stress_delta, (1.0 / _strain_hardening_exponent - 1.0)) /
+                             _strain_hardening_exponent * 1.0 /
+                             std::pow(_K, 1.0 / _strain_hardening_exponent);
   slope -= 1.0 / _youngs_modulus;
 
   return 1.0 / slope;
 }
 
+template <bool is_ad>
 void
-IsotropicPowerLawHardeningStressUpdate::computeYieldStress(const RankFourTensor & elasticity_tensor)
+IsotropicPowerLawHardeningStressUpdateTempl<is_ad>::computeYieldStress(
+    const GenericRankFourTensor<is_ad> & elasticity_tensor)
 {
   // Pull in the Lam\`{e} lambda, and caculate E
-  const Real lambda = getIsotropicLameLambda(elasticity_tensor);
-  const Real shear_modulus = _three_shear_modulus / 3.0;
+  const GenericReal<is_ad> lambda = getIsotropicLameLambda(elasticity_tensor);
+  const GenericReal<is_ad> shear_modulus = _three_shear_modulus / 3.0;
 
   _youngs_modulus = shear_modulus * (3.0 * lambda + 2 * shear_modulus) / (lambda + shear_modulus);
 
   // Then solve for yield stress using equation from the header file
-  _yield_stress = std::pow(_K / std::pow(_youngs_modulus, _strain_hardening_exponent),
-                           1.0 / (1.0 - _strain_hardening_exponent));
-  if (_yield_stress <= 0.0)
+  this->_yield_stress = std::pow(_K / std::pow(_youngs_modulus, _strain_hardening_exponent),
+                                 1.0 / (1.0 - _strain_hardening_exponent));
+  if (this->_yield_stress <= 0.0)
     mooseError("The yield stress must be greater than zero, but during the simulation your yield "
                "stress became less than zero.");
 }
 
-Real
-IsotropicPowerLawHardeningStressUpdate::getIsotropicLameLambda(
-    const RankFourTensor & elasticity_tensor)
+template <bool is_ad>
+GenericReal<is_ad>
+IsotropicPowerLawHardeningStressUpdateTempl<is_ad>::getIsotropicLameLambda(
+    const GenericRankFourTensor<is_ad> & elasticity_tensor)
 {
-  const Real lame_lambda = elasticity_tensor(0, 0, 1, 1);
+  const GenericReal<is_ad> lame_lambda = elasticity_tensor(0, 0, 1, 1);
 
-  if (_mesh.dimension() == 3 && lame_lambda != elasticity_tensor(1, 1, 2, 2))
+  if (this->_mesh.dimension() == 3 &&
+      MetaPhysicL::raw_value(lame_lambda) != MetaPhysicL::raw_value(elasticity_tensor(1, 1, 2, 2)))
     mooseError(
         "Check to ensure that your Elasticity Tensor is truly Isotropic: different lambda values");
   return lame_lambda;
