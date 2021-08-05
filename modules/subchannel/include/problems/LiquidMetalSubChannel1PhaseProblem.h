ed22a97a43e77fe8903d0b0341637ebcc68ac713
Aydin Karahan <akarahan@anl.gov>
2021-08-05 06:55:42 -0600

    Cheng-Todreas's turbulen mixing model for the wire-wrapped hexagonal subassemblies is implemented. Refs #29194

--- modules/subchannel/include/problems/LiquidMetalSubChannel1PhaseProblem.h
+++ modules/subchannel/include/problems/LiquidMetalSubChannel1PhaseProblem.h
@@ -27,6 +27,8 @@ protected:
   /// are overriden to define the sodium friction factor
   virtual double computeMassFlowForDPDZ(double dpdz, int i_ch) override;
   virtual void enforceUniformDPDZAtInlet() override;
+  /// turbulent-mixing cross flow model
+  virtual void computeWijPrime(int iz) override;
   /**
    * computeInletMassFlowDist corrects the inlet mass flow rate distribution
    * in order to satisfy the uniform inlet pressure condition, iteratively.
