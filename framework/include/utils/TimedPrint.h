40230ef4f6a7d3e7187e7018274b4222795c6247
Derek Gaston <derek.gaston@inl.gov>
2021-08-11 14:15:27 -0600

    Add deprecated timed prints refs #15444
    clang format refs #15444

---
+++ framework/include/utils/TimedPrint.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "MooseError.h"
+
+#define CONTROLLED_CONSOLE_TIMED_PRINT(initial_wait, dot_interval, ...)                            \
+  mooseDeprecated("TIMED_PRINT is deprecated, use TIME_SECTION instead");
+
+#define CONSOLE_TIMED_PRINT(...) CONTROLLED_CONSOLE_TIMED_PRINT(1, 1, __VA_ARGS__)
+
+/// Dummy TimedPrint Class - use TIME_SECTION instead
+class TimedPrint final
+{
+public:
+  template <class StreamType, typename... Args>
+  TimedPrint(StreamType &, std::chrono::duration<double>, std::chrono::duration<double>, Args &&...)
+  {
+    mooseDeprecated("TimedPrint is deprecated, use TIME_SECTION instead");
+    }
+};
