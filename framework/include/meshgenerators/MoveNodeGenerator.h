cbe874949bc731042b519f89824ec4dbae0f6ee6
socratesgorilla <nathaniel.peat@inl.gov>
2021-08-12 14:17:33 -0500

    Added ModifyNodeGenerator refs #18577
    Delete testCart.i
    
    Update tests
    
    Fixed whitespace
    
    clang reformat
    
    Update tests
    
    Update ModifyNodeGenerator.C
    
    Update framework/include/meshgenerators/ModifyNodeGenerator.h
    
    Co-authored-by: Logan Harbour <loganhharbour@gmail.com>
    
    Update framework/src/meshgenerators/ModifyNodeGenerator.C
    
    Co-authored-by: Logan Harbour <loganhharbour@gmail.com>
    
    Update ModifyNodeGenerator.C
    
    reformat
    
    Fixed import issues
    
    Added gold file for test
    
    Update framework/src/meshgenerators/ModifyNodeGenerator.C
    
    Co-authored-by: Logan Harbour <loganhharbour@gmail.com>
    
    Update framework/include/meshgenerators/ModifyNodeGenerator.h
    
    Co-authored-by: Guillaume Giudicelli <guillaume.giudicelli@gmail.com>
    
    Yaqi edits and comment changes
    
    Fixed test_in exodiff, hopefully
    
    Hopefully fix the last test issue
    
    Debugging distributed mesh errors
    Make mesh new positions more visible in the diffusion test for the modified mesh
    Refs #18577
    
    rename everything
    
    Added mesh_type

---
+++ framework/include/meshgenerators/MoveNodeGenerator.h
@@ -0,0 +1,35 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "MeshGenerator.h"
+
+/**
+ * Modifies the position of one node
+ */
+class MoveNodeGenerator : public MeshGenerator
+{
+public:
+  static InputParameters validParams();
+
+  MoveNodeGenerator(const InputParameters & parameters);
+
+  std::unique_ptr<MeshBase> generate() override;
+
+protected:
+  /// Mesh that possibly comes from another generator
+  std::unique_ptr<MeshBase> & _input;
+
+  /// The id(s) of the node(s) to be moved
+  const std::vector<dof_id_type> _node_id;
+
+  /// The new position(s) of the node
+  const std::vector<Point> _new_position;
+};
