03e3c0518255918ffa9e8c0fb20b12e7a10ae12e
Logan Harbour <loganhharbour@gmail.com>
2021-08-20 11:41:08 -0600

    Add copyrights and script comments
    refs #18658

--- docker_ci/build_moose.sh
+++ docker_ci/build_moose.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
+#* This file is part of the MOOSE framework
+#* https://www.mooseframework.org
+#*
+#* All rights reserved, see COPYRIGHT for full restrictions
+#* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+#*
+#* Licensed under LGPL 2.1, please see LICENSE for details
+#* https://www.gnu.org/licenses/lgpl-2.1.html
 
+# This script is used in docker_ci/Dockerfile to build
+# framework, test, and possibly modules
+#
+# Used Environment variables:
+# METHODS: The methods to build
+# MOOSE_JOBS: The number of jobs to run make with
+# BUILD_MODULES: If set, build modules
+
+# Runs make in the current directory for all of
+# the methods in METHODS, with MOOSE_JOBS jobs
+# and any additional arguments
 function make_pwd()
 {
   for method in $METHODS; do
--- docker_ci/install_packages.sh
+++ docker_ci/install_packages.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+#* This file is part of the MOOSE framework
+#* https://www.mooseframework.org
+#*
+#* All rights reserved, see COPYRIGHT for full restrictions
+#* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+#*
+#* Licensed under LGPL 2.1, please see LICENSE for details
+#* https://www.gnu.org/licenses/lgpl-2.1.html
+
+# This script is used in docker_ci/Dockerfile to setup
+# the base container environment, and uses
+# docker_ci/*_installs.sh based on the distro
 
 # Supported package managers
 MGR_ARY=("apt" "yum")
