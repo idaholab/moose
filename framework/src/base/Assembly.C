e494367a47fa49302b2bfe06f3f4d33386623833
Alex Lindsay <alexlindsay239@gmail.com>
2021-08-30 13:00:05 -0700

    Always use single point map for both residual and Jacobian
    This can be reverted after the affine map tolerance tightening in
    libmesh/libmesh#2997

--- framework/src/base/Assembly.C
+++ framework/src/base/Assembly.C
@@ -756,11 +756,8 @@ Assembly::reinitFE(const Elem * elem)
     if (_displaced)
     {
       const auto & qw = _current_qrule->get_weights();
-      if (elem->has_affine_map() && !_subproblem.currentlyComputingJacobian())
-        computeAffineMapAD(elem, qw, n_qp, *_holder_fe_helper[dim]);
-      else
-        for (unsigned int qp = 0; qp != n_qp; qp++)
-          computeSinglePointMapAD(elem, qw, qp, *_holder_fe_helper[dim]);
+      for (unsigned int qp = 0; qp != n_qp; qp++)
+        computeSinglePointMapAD(elem, qw, qp, *_holder_fe_helper[dim]);
     }
     else
       for (unsigned qp = 0; qp < n_qp; ++qp)
@@ -2148,11 +2145,8 @@ Assembly::computeADFace(const Elem * elem, unsigned int side)
       computeFaceMap(dim, qw, side_elem.get());
       std::vector<Real> dummy_qw(n_qp, 1.);
 
-      if (elem->has_affine_map() && !_subproblem.currentlyComputingJacobian())
-        computeAffineMapAD(elem, dummy_qw, n_qp, *_holder_fe_face_helper[dim]);
-      else
-        for (unsigned int qp = 0; qp != n_qp; qp++)
-          computeSinglePointMapAD(elem, dummy_qw, qp, *_holder_fe_face_helper[dim]);
+      for (unsigned int qp = 0; qp != n_qp; qp++)
+        computeSinglePointMapAD(elem, dummy_qw, qp, *_holder_fe_face_helper[dim]);
     }
     else
       for (unsigned qp = 0; qp < n_qp; ++qp)
