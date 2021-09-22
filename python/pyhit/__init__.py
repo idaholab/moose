b8e57cf7a8fe1f7694f2e3372f4827f53426010d
Andrew E Slaughter <slaughter98@gmail.com>
2021-09-22 14:53:22 -0600

    Remove moosetools use
    The moosetools project is being archived until the need and staffing exist
    
    (refs #17006)

--- python/pyhit/__init__.py
+++ python/pyhit/__init__.py
@@ -3,37 +3,22 @@ import sys
 import subprocess
 import mooseutils
 
+"""
+First, attempt to import hit. If that fails, try adding the hit source directory to the path
+and try the import again. If that fails, try running "make hit" before importing.
+"""
+
 try:
     import hit
-
 except ImportError:
-
-    # If the repository is not a git repository, then give up otherwise try to figure out what to do
-    if not mooseutils.git_is_repo(os.path.dirname(__file__)):
-        raise
-
-    moose_dir = mooseutils.git_root_dir(os.path.dirname(__file__))
-    status = mooseutils.git_submodule_info(moose_dir)
-
-    # Use framework/contrib/hit because moosetools submodule is not available
-    if status['moosetools'][0] == '-':
-        hit_dir = os.path.join(moose_dir, 'framework', 'contrib', 'hit')
-        sys.path.append(hit_dir)
-        try:
-            import hit
-        except ImportError:
-            moose_test_dir = os.path.abspath(os.path.join(moose_dir, 'test'))
-            subprocess.run(['make', 'hit'], cwd=moose_test_dir)
-            import hit
-    # Use hit in moosetools submodule
-    else:
-        hit_dir = os.path.join(moose_dir, 'moosetools', 'contrib', 'hit')
-        sys.path.append(hit_dir)
-        try:
-            import hit
-        except ImportError:
-            subprocess.run(['make', 'hit.so'], cwd=hit_dir)
-            import hit
+    hit_dir = os.path.join(moose_dir, 'framework', 'contrib', 'hit')
+    sys.path.append(hit_dir)
+    try:
+        import hit
+    except ImportError:
+        moose_test_dir = os.path.abspath(os.path.join(moose_dir, 'test'))
+        subprocess.run(['make', 'hit'], cwd=moose_test_dir)
+        import hit
 
 from hit import TokenType, Token
 from .pyhit import Node, load, write, parse, tokenize
