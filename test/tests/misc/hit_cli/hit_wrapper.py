db9e1be5246a715b179d7bf6f32b408e14a15680
Daniel Schwen <daniel@schwen.de>
2021-10-18 12:58:38 -0600

    Add tests (#19001)

---
+++ test/tests/misc/hit_cli/hit_wrapper.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import subprocess
+
+if len(sys.argv) < 2:
+    print("Usage:\nhit_wrapper.py gold_output [hit arguments]\n")
+
+MOOSE_DIR = os.getenv('MOOSE_DIR', os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', '..', '..')))
+hit = os.path.join(MOOSE_DIR, 'framework', 'contrib', 'hit', 'hit')
+if not os.path.exists(hit):
+    print('Failed to locate hit executable.')
+    sys.exit(1)
+
+gold_file = sys.argv[1]
+hit_args = sys.argv[2:]
+
+with open(gold_file, 'rb') as f:
+    gold = f.read()
+
+out = subprocess.check_output([hit] + hit_args)
+
+if out == gold:
+    sys.exit(0)
+
+print("Output:\n", out, "\ndoes not match gold:\n", gold)
+sys.exit(1)
