e84180511e33731dc4618485d5be4b5bd4b93be7
Alex Lindsay <alexlindsay239@gmail.com>
2021-11-01 13:49:04 -0700

    Use parameters that have relationship managers in them
    I kind of think that the contact action has been giving us incorrect
    relationship managers for mortar since its inception and somehow we have
    never triggered it in the test harness until
    https://civet.inl.gov/job/870326/ ?!

--- modules/contact/src/actions/ContactAction.C
+++ modules/contact/src/actions/ContactAction.C
@@ -367,7 +367,18 @@ void
 ContactAction::addRelationshipManagers(Moose::RelationshipManagerType input_rm_type)
 {
   if (_formulation == ContactFormulation::MORTAR)
-    addRelationshipManagers(input_rm_type, _pars);
+  {
+    auto params = MortarConstraintBase::validParams();
+    params.set<bool>("use_displaced_mesh") = true;
+    std::string action_name = MooseUtils::shortName(name());
+    const std::string primary_subdomain_name = action_name + "_primary_subdomain";
+    const std::string secondary_subdomain_name = action_name + "_secondary_subdomain";
+    params.set<BoundaryName>("primary_boundary") = _boundary_pairs[0].first;
+    params.set<BoundaryName>("secondary_boundary") = _boundary_pairs[0].second;
+    params.set<SubdomainName>("primary_subdomain") = primary_subdomain_name;
+    params.set<SubdomainName>("secondary_subdomain") = secondary_subdomain_name;
+    addRelationshipManagers(input_rm_type, params);
+  }
 }
 
 void
