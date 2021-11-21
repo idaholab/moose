f2fb03c39252f992a9a21081fd70ea8b1c13c442
Peter German <grmnptr@gmail.com>
2021-11-21 11:57:59 -0700

    Added conda env variable. (#19571)

--- modules/stochastic_tools/stochastic_tools.mk
+++ modules/stochastic_tools/stochastic_tools.mk
@@ -1,6 +1,6 @@
 libmesh_CXXFLAGS += -DENABLE_TF
 
-libmesh_CXXFLAGS += -I$(HOME)/miniconda3/pkgs/tensorflow-base-2.4.1-py39h23a8cbf_0/lib/python3.9/site-packages/tensorflow/include
+libmesh_CXXFLAGS += -I$(CONDA_PREFIX_1)/pkgs/tensorflow-base-2.4.1-py39h23a8cbf_0/lib/python3.9/site-packages/tensorflow/include
 # libmesh_CXXFLAGS += -I$(HOME)/miniconda3/pkgs/libprotobuf-3.15.8-h780b84a_0/include
 # libmesh_CXXFLAGS += -Wl,-rpath-link,$(HOME)/miniconda3/pkgs/tensorflow-base-2.4.1-py39h23a8cbf_0/lib/python3.9/site-packages/tensorflow
 # libmesh_CXXFLAGS += -Wl,-rpath-link,$(HOME)/miniconda3/pkgs/libprotobuf-3.15.8-h780b84a_0/lib
