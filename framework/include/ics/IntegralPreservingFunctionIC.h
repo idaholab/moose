781b4a34955b6c990eaf10b55661b49647425c41
aprilnovak <novak@berkeley.edu>
2021-11-24 13:01:38 -0600

    Add initial condition that preserves a function integral. Refs #19476

---
+++ framework/include/ics/IntegralPreservingFunctionIC.h
@@ -0,0 +1,47 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "FunctionIC.h"
+
+class IntegralPreservingFunctionIC;
+class InputParameters;
+
+template <typename T>
+InputParameters validParams();
+
+template <>
+InputParameters validParams<IntegralPreservingFunctionIC>();
+
+/**
+ * Initial conditions specifying an initial condition as a general function
+ * while preserving a total integral magnitude.
+ */
+class IntegralPreservingFunctionIC : public FunctionIC
+{
+public:
+  static InputParameters validParams();
+
+  IntegralPreservingFunctionIC(const InputParameters & parameters);
+
+  virtual void initialSetup() override;
+
+protected:
+  virtual Real value(const Point & p) override;
+
+  /// Name of postprocessor providing the integral of the function
+  const PostprocessorName & _pp_name;
+
+  /// Integral of the function
+  const PostprocessorValue & _integral;
+
+  /// Magnitude of the initial condition upon integration
+  const Real & _magnitude;
+};
---
+++ framework/src/ics/IntegralPreservingFunctionIC.C
@@ -0,0 +1,54 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "IntegralPreservingFunctionIC.h"
+#include "Function.h"
+#include "UserObject.h"
+
+registerMooseObject("MooseApp", IntegralPreservingFunctionIC);
+
+defineLegacyParams(IntegralPreservingFunctionIC);
+
+InputParameters
+IntegralPreservingFunctionIC::validParams()
+{
+  InputParameters params = FunctionIC::validParams();
+  params.addRequiredParam<PostprocessorName>(
+      "integral", "Postprocessor providing the integral of the function, for normalization");
+  params.addRequiredParam<Real>("magnitude",
+                                "Desired magnitude of the initial condition upon integration");
+  params.addClassDescription("Function initial condition that preserves an integral");
+  return params;
+}
+
+IntegralPreservingFunctionIC::IntegralPreservingFunctionIC(const InputParameters & parameters)
+  : FunctionIC(parameters),
+    _pp_name(getParam<PostprocessorName>("integral")),
+    _integral(getPostprocessorValue("integral")),
+    _magnitude(getParam<Real>("magnitude"))
+{
+}
+
+void
+IntegralPreservingFunctionIC::initialSetup()
+{
+  const UserObject & pp = _fe_problem.getUserObject<UserObject>(_pp_name);
+  if (!pp.getExecuteOnEnum().contains(EXEC_INITIAL))
+    mooseError("The 'execute_on' parameter for the '" + _pp_name +
+               "' postprocessor must include 'initial'!");
+}
+
+Real
+IntegralPreservingFunctionIC::value(const Point & p)
+{
+  if (std::abs(_integral) < libMesh::TOLERANCE)
+    mooseError("The integral of '" + _pp_name + "' cannot be zero!");
+
+  return _magnitude * _func.value(_t, p) / _integral;
+}
