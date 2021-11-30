6b8f17a3d16f4d4c3b1e1557cf49827faf81be7b
Daniel Schwen <daniel@schwen.de>
2021-11-29 18:08:48 -0700

    Add tests (#18999)

---
+++ test/include/materials/OptionalTestMaterial.h
@@ -0,0 +1,34 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+// MOOSE includes
+#include "Material.h"
+
+/**
+ * A test object that uses optional material properties
+ */
+class OptionalTestMaterial : public Material
+{
+public:
+  static InputParameters validParams();
+
+  OptionalTestMaterial(const InputParameters & parameters);
+
+protected:
+  void computeQpProperties();
+
+private:
+  const MaterialProperty<Real> * const & _prop;
+  const ADMaterialProperty<Real> * const & _adprop;
+  const bool _expect;
+  const bool _adexpect;
+  MaterialProperty<Real> & _mirror;
+};
---
+++ test/tests/materials/optional_properties/gold/material_after.e
@@ -0,0 +1,1 @@
+material_before.e
