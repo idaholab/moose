541873a555ee0cc0b7186733aa5ac1ae37720703
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2021-12-04 11:25:58 -0700

    Add flux & dirichlet boundary conditions for SAM-Pgh coupling, closes #19543

---
+++ modules/navier_stokes/include/fvbcs/WCNSFVInletTemperatureBC.h
@@ -0,0 +1,52 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "FVDirichletBCBase.h"
+#include "INSFVFlowBC.h"
+
+/**
+ * Dirichlet boundary conditions for the temperature, set from either a temperature postprocessor
+ * or an energy flow rate divided by mass flow and specific heat capacity
+ */
+class WCNSFVInletTemperatureBC : public FVDirichletBCBase, public INSFVFlowBC
+{
+public:
+  static InputParameters validParams();
+  WCNSFVInletTemperatureBC(const InputParameters & params);
+
+protected:
+  // FIXME: this should be an ADReal
+  Real boundaryValue(const FaceInfo & fi) const override;
+
+  /// Scaling factor
+  const Real _scaling_factor;
+
+  /// Postprocessor with the inlet temperature
+  const PostprocessorValue * _temperature_pp;
+
+  /// Postprocessor with the inlet energy flow rate
+  const PostprocessorValue * _energy_pp;
+
+  /// Postprocessor with the inlet velocity
+  const PostprocessorValue * _velocity_pp;
+
+  /// Postprocessor with the inlet mass flow rate
+  const PostprocessorValue * _mdot_pp;
+
+  /// Postprocessor with the inlet area
+  const PostprocessorValue * _area_pp;
+
+  /// Fluid density functor
+  const Moose::Functor<ADReal> * _rho;
+
+  /// Fluid specific heat capacity functor
+  const Moose::Functor<ADReal> * _cp;
+};
---
+++ modules/navier_stokes/src/fvbcs/WCNSFVInletVelocityBC.C
@@ -0,0 +1,81 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "WCNSFVInletVelocityBC.h"
+#include "INSFVVelocityVariable.h"
+#include "NS.h"
+
+registerMooseObject("NavierStokesApp", WCNSFVInletVelocityBC);
+
+InputParameters
+WCNSFVInletVelocityBC::validParams()
+{
+  InputParameters params = FVDirichletBCBase::validParams();
+  params += INSFVFlowBC::validParams();
+
+  params.addParam<Real>("scaling_factor", 1, "To scale the velocity");
+
+  // Two different ways to input velocity
+  // 1) Postprocessor with the velocity value directly
+  params.addParam<PostprocessorName>("velocity_pp", "Postprocessor with the inlet velocity");
+
+  // 2) Postprocessors with an inlet mass flow rate
+  params.addParam<PostprocessorName>("mdot_pp", "Postprocessor with the inlet mass flow rate");
+  params.addParam<MooseFunctorName>(NS::density, "Density functor");
+  params.addParam<PostprocessorName>("area_pp", "Inlet area as a postprocessor");
+
+  return params;
+}
+
+WCNSFVInletVelocityBC::WCNSFVInletVelocityBC(const InputParameters & params)
+  : FVDirichletBCBase(params),
+    INSFVFlowBC(params),
+    _scaling_factor(getParam<Real>("scaling_factor")),
+    _velocity_pp(isParamValid("velocity_pp") ? &getPostprocessorValue("velocity_pp") : nullptr),
+    _mdot_pp(isParamValid("mdot_pp") ? &getPostprocessorValue("mdot_pp") : nullptr),
+    _area_pp(isParamValid("area_pp") ? &getPostprocessorValue("area_pp") : nullptr),
+    _rho(isParamValid(NS::density) ? &getFunctor<ADReal>(NS::density) : nullptr)
+{
+  if (!dynamic_cast<INSFVVelocityVariable *>(&_var))
+    paramError(
+        "variable",
+        "The variable argument to WCNSFVInletVelocityBC must be of type INSFVVelocityVariable");
+
+  // Density is often set as global parameters so it is not checked
+  if (_mdot_pp && (_velocity_pp || _area_pp))
+    mooseWarning("If setting the mass flow rate directly, no need for inlet velocity or area");
+
+  // Need enough information if trying to use a mass flow rate postprocessor
+  if (!_mdot_pp && (!_velocity_pp || !_area_pp || !_rho))
+    mooseError("Velocity, area and density should be provided if velocity is not");
+}
+
+Real
+WCNSFVInletVelocityBC::boundaryValue(const FaceInfo & fi) const
+{
+
+  if (_velocity_pp)
+    return _scaling_factor * (*_velocity_pp);
+  else
+  {
+    ADReal rho;
+
+    // TODO: Make this a utility to all FVBCs
+    const bool use_elem = fi.faceType(_var.name()) == FaceInfo::VarFaceNeighbors::ELEM;
+
+    if (use_elem)
+      rho = (*_rho)(std::make_tuple(
+          &fi, Moose::FV::LimiterType::CentralDifference, true, fi.elem().subdomain_id()));
+    else
+      rho = (*_rho)(std::make_tuple(
+          &fi, Moose::FV::LimiterType::CentralDifference, true, fi.neighborPtr()->subdomain_id()));
+
+    return _scaling_factor * (*_mdot_pp) / (*_area_pp * rho.value());
+  }
+}
