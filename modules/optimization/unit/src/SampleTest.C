387ec05f7021b95ed17d29969b63a5a4de916480
Lynn Munday <lynn.munday@inl.gov>
2022-01-15 14:12:52 -0700

    trying to fix sample test in unit tester and its warning.

--- modules/optimization/unit/src/SampleTest.C
+++ modules/optimization/unit/src/SampleTest.C
@@ -19,7 +19,8 @@ TEST(MySampleTests, descriptiveTestName)
   // ASSERT_EQ(2, 1);
 
   // this won't run if you uncomment the above test because above assert will fail
-  ASSERT_NO_THROW(1 + 1);
+  int n = 0;
+  ASSERT_NO_THROW(n += 3);
 
   // for a complete list of assertions and for more unit testing documentation see:
   // https://github.com/google/googletest/blob/master/googletest/docs/Primer.md
