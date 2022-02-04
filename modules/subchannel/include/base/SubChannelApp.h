8fa8bba3a17f51e693359ad4572e364a386ed247
David Andrs <david.andrs@inl.gov>
2022-02-04 05:28:14 -0700

    Empty dtors are no longer needed
    Refs #0

--- modules/subchannel/include/base/SubChannelApp.h
+++ modules/subchannel/include/base/SubChannelApp.h
@@ -6,7 +6,6 @@ class SubChannelApp : public MooseApp
 {
 public:
   SubChannelApp(InputParameters parameters);
-  virtual ~SubChannelApp();
 
   static void registerApps();
   static void registerAll(Factory & f, ActionFactory & af, Syntax & s);
