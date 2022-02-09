50d255bc3e5686164f54fd1c68bda0595ca7dc47
David Andrs <david.andrs@inl.gov>
2022-02-09 14:31:16 -0700

    Adding detailed pin mesh generator for quadrilateral lattice
    Closes #29246

---
+++ modules/subchannel/src/meshgenerators/DetailedQuadPinMeshGenerator.C
@@ -0,0 +1,44 @@
+#include "DetailedQuadPinMeshGenerator.h"
+#include "QuadSubChannelMesh.h"
+#include "libmesh/cell_prism6.h"
+
+registerMooseObject("SubChannelApp", DetailedQuadPinMeshGenerator);
+
+InputParameters
+DetailedQuadPinMeshGenerator::validParams()
+{
+  InputParameters params = DetailedPinMeshGeneratorBase::validParams();
+  params.addRequiredParam<MeshGeneratorName>("input", "The corresponding subchannel mesh");
+  params.addRequiredParam<unsigned int>("nx", "Number of channels in the x direction [-]");
+  params.addRequiredParam<unsigned int>("ny", "Number of channels in the y direction [-]");
+  return params;
+}
+
+DetailedQuadPinMeshGenerator::DetailedQuadPinMeshGenerator(const InputParameters & parameters)
+  : DetailedPinMeshGeneratorBase(parameters),
+    _input(getMesh("input")),
+    _nx(getParam<unsigned int>("nx")),
+    _ny(getParam<unsigned int>("ny"))
+{
+}
+
+std::unique_ptr<MeshBase>
+DetailedQuadPinMeshGenerator::generate()
+{
+  std::unique_ptr<MeshBase> mesh_base = std::move(_input);
+  if (!mesh_base)
+    mesh_base = buildMeshBaseObject();
+  mesh_base->set_mesh_dimension(3);
+
+  std::vector<Point> pin_centers;
+  QuadSubChannelMesh::generatePinCenters(_nx, _ny, _pitch, 0, pin_centers);
+
+  _elem_id = mesh_base->n_elem();
+  for (auto & ctr : pin_centers)
+    generatePin(mesh_base, ctr);
+
+  mesh_base->subdomain_name(_block_id) = name();
+  mesh_base->prepare_for_use();
+
+  return mesh_base;
+}
