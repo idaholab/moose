96fe912879ff1e52e38a95c40e3e762f1d91dcdf
hansje <joshua.hansel@inl.gov>
2022-02-10 06:45:09 -0600

    Added MOOSE headers for thermal hydraulics module
    Closes #20259

--- modules/thermal_hydraulics/python/tests/peacock/model_builder/test_FlowChannelParametersCalculator.py
+++ modules/thermal_hydraulics/python/tests/peacock/model_builder/test_FlowChannelParametersCalculator.py
@@ -1,3 +1,12 @@
+#* This file is part of the MOOSE framework
+#* https://www.mooseframework.org
+#*
+#* All rights reserved, see COPYRIGHT for full restrictions
+#* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+#*
+#* Licensed under LGPL 2.1, please see LICENSE for details
+#* https://www.gnu.org/licenses/lgpl-2.1.html
+
 import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "..", 'peacock'))
