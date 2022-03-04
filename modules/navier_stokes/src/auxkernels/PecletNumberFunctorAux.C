6caef5cc18ce1b560cd0e05082464ba35058b42c
Alex Lindsay <alexlindsay239@gmail.com>
2022-03-04 14:01:55 -0800

    Add PecletNumberFunctorAux
    Closes #20476

---
+++ modules/navier_stokes/src/auxkernels/PecletNumberFunctorAux.C
@@ -0,0 +1,59 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "PecletNumberFunctorAux.h"
+#include "MooseMesh.h"
+#include "NS.h"
+
+registerMooseObject("NavierStokesApp", PecletNumberFunctorAux);
+
+InputParameters
+PecletNumberFunctorAux::validParams()
+{
+  InputParameters params = AuxKernel::validParams();
+  params.addClassDescription("Computes u*L/alpha.");
+  params.addRequiredParam<MooseFunctorName>(NS::speed, "The fluid speed");
+  params.addRequiredParam<MooseFunctorName>(
+      NS::thermal_diffusivity,
+      "The fluid thermal diffusivity, or if using this object for evaluating mass transfer, the "
+      "mass diffusivity. Regardless of which, this should have units of length^2/time");
+  params.addParam<bool>(
+      "use_qp_arg",
+      false,
+      "Whether to use the quadrature point based functor agument to evaluate the functors. If "
+      "false, then the argument is element-based, which is more appropriate for finite volume. A "
+      "value of true is more appropriate for finite element computations.");
+  return params;
+}
+
+PecletNumberFunctorAux::PecletNumberFunctorAux(const InputParameters & parameters)
+  : AuxKernel(parameters),
+    _speed(getFunctor<ADReal>(NS::speed)),
+    _alpha(getFunctor<ADReal>(NS::thermal_diffusivity)),
+    _use_qp_arg(getParam<bool>("use_qp_arg"))
+{
+  if (isNodal())
+    mooseError("This AuxKernel only supports Elemental fields");
+}
+
+Real
+PecletNumberFunctorAux::computeValue()
+{
+  using MetaPhysicL::raw_value;
+  if (_use_qp_arg)
+  {
+    const auto qp_arg = std::make_tuple(_current_elem, _qp, _qrule);
+    return _current_elem->hmax() * raw_value(_speed(qp_arg)) / raw_value(_alpha(qp_arg));
+  }
+  else
+  {
+    const auto elem_arg = makeElemArg(_current_elem);
+    return _current_elem->hmax() * raw_value(_speed(elem_arg)) / raw_value(_alpha(elem_arg));
+  }
+}
