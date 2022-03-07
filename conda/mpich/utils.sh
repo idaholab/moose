71463780147707441d8059e704a0720b40561bbd
Jason M Miller <Jason.Miller@inl.gov>
2022-03-07 08:04:28 -0700

    Use export, unbound variable

--- conda/mpich/utils.sh
+++ conda/mpich/utils.sh
@@ -1,10 +1,10 @@
 # establish cores, or reasonable default
 if [ -z "$CPU_COUNT" ]; then
     if [ `uname` == "Darwin" ]; then
-        CPU_COUNT=$(echo "$(sysctl -n hw.ncpu) / 2" | bc)
+        export CPU_COUNT=$(echo "$(sysctl -n hw.ncpu) / 2" | bc)
     elif [ `which nproc` ]; then
-        CPU_COUNT=$(echo `nproc` / 2 | bc)
+        export CPU_COUNT=$(echo `nproc` / 2 | bc)
     else
-        CPU_COUNT=4
+        export CPU_COUNT=4
     fi
 fi
