57ee6bac64d619e1c6eea00bb8e7e47962188550
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2022-03-21 17:27:52 -0600

    Add a kernel that creates bound on FV variable values, refs #20607

---
+++ framework/src/fvkernels/FVBoundedValueConstraint.C
@@ -0,0 +1,46 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "FVBoundedValueConstraint.h"
+
+#include "MooseVariableScalar.h"
+#include "MooseVariableFV.h"
+#include "Assembly.h"
+
+registerMooseObject("MooseApp", FVBoundedValueConstraint);
+
+InputParameters
+FVBoundedValueConstraint::validParams()
+{
+  InputParameters params = FVScalarLagrangeMultiplierConstraint::validParams();
+  params.addClassDescription("This class is used to enforce a min or max value for a finite volume variable");
+  params.addRequiredParam<Real>("phi0", "The min or max bound");
+  // Define the min/max enumeration
+  MooseEnum type_options("max=0 min=1");
+  params.addRequiredParam<MooseEnum>("bound_type", type_options, "Whether a minimum or a maximum bound");
+  return params;
+}
+
+FVBoundedValueConstraint::FVBoundedValueConstraint(const InputParameters & parameters)
+  : FVScalarLagrangeMultiplierConstraint(parameters),
+    _phi0(getParam<Real>("phi0")),
+    _bound_type(getParam<MooseEnum>("bound_type"))
+{
+}
+
+ADReal
+FVBoundedValueConstraint::computeQpResidual()
+{
+  if (_bound_type == BoundType::MAX && _u[_qp] > _phi0)
+    return _u[_qp] - _phi0;
+  else if (_bound_type == BoundType::MIN && _u[_qp] < _phi0)
+    return _phi0 - _u[_qp];
+  else
+    return 0;
+}
