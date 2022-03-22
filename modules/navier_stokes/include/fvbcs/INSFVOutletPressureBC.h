7117c03888f952a578f00fcfc02872272da481dc
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2022-03-22 15:25:18 -0600

    Allow a postprocessor for the outlet pressure BC, refs #18086

--- modules/navier_stokes/include/fvbcs/INSFVOutletPressureBC.h
+++ modules/navier_stokes/include/fvbcs/INSFVOutletPressureBC.h
@@ -16,9 +16,18 @@
  * A class for setting the value of the pressure at an outlet of the system.
  * It may not be used with a mean-pressure approach
  */
-class INSFVOutletPressureBC : public FVFunctionDirichletBC, public INSFVFullyDevelopedFlowBC
+class INSFVOutletPressureBC : public FVDirichletBCBase, public INSFVFullyDevelopedFlowBC
 {
 public:
   static InputParameters validParams();
   INSFVOutletPressureBC(const InputParameters & params);
+
+  Real boundaryValue(const FaceInfo & /* fi */) const override;
+
+private:
+  /// Functor that gives the distribution of pressure on the boundary
+  const Moose::Functor<ADReal> * const _functor;
+
+  /// Postprocessor that gives the uniform value of pressure on the boundary
+  const PostprocessorValue * const _pp_value;
 };
--- modules/navier_stokes/src/fvbcs/INSFVOutletPressureBC.C
+++ modules/navier_stokes/src/fvbcs/INSFVOutletPressureBC.C
@@ -15,16 +15,48 @@ registerMooseObject("NavierStokesApp", INSFVOutletPressureBC);
 InputParameters
 INSFVOutletPressureBC::validParams()
 {
-  InputParameters params = FVFunctionDirichletBC::validParams();
+  InputParameters params = FVDirichletBCBase::validParams();
   params += INSFVFullyDevelopedFlowBC::validParams();
+
+  // Value may be specified by a functor (function) or a postprocessor
+  params.addDeprecatedParam<FunctionName>("function",
+                                          "The pressure as a function.",
+                                          "Use functor instead");
+  params.addParam<MooseFunctorName>("functor",
+                                    "The boundary pressure as a functor (most often a function)");
+  params.addParam<PostprocessorName>("postprocessor", "The boundary pressure as a postprocessor");
+
   return params;
 }
 
 INSFVOutletPressureBC::INSFVOutletPressureBC(const InputParameters & params)
-  : FVFunctionDirichletBC(params), INSFVFullyDevelopedFlowBC(params)
+  : FVDirichletBCBase(params),
+  INSFVFullyDevelopedFlowBC(params),
+  _functor(isParamValid("functor") ?
+           &getFunctor<ADReal>("functor") :
+           isParamValid("function") ?
+           &getFunctor<ADReal>("function") :
+           nullptr),
+  _pp_value(isParamValid("postprocessor") ?
+            &getPostprocessorValue("postprocessor") :
+            nullptr)
 {
   if (!dynamic_cast<INSFVPressureVariable *>(&_var))
     paramError(
         "variable",
         "The variable argument to INSFVOutletPressureBC must be of type INSFVPressureVariable");
+
+  // Check parameters
+  if ((_functor && _pp_value) || (!_functor && !_pp_value))
+    mooseError(
+        "One and only one of function/functor/postprocessor may be specified for the outlet pressure");
+}
+
+Real
+INSFVOutletPressureBC::boundaryValue(const FaceInfo & fi) const
+{
+  if (_functor)
+    return (*_functor)(singleSidedFaceArg(&fi)).value();
+  else
+    return *_pp_value;
 }
