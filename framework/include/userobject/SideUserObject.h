67c6395e44e87d1add4e92de6560d5cca945b8c1
Guillaume Giudicelli <guillaume.giudicelli@gmail.com>
2022-04-23 21:10:50 -0600

    Update framework/include/userobject/SideUserObject.h
    Expand the elem/side loop to faceInfos to capture extra faces during adaptivity
    
    Co-authored-by: Alex Lindsay <alexlindsay239@gmail.com>

--- framework/include/userobject/SideUserObject.h
+++ framework/include/userobject/SideUserObject.h
@@ -58,26 +58,31 @@ protected:
    * Note this face info could hold the element from the other side of the
    * sideset. Sidesets are oriented!
    */
-  const FaceInfo * getFaceInfo()
+void
+getFaceInfo()
+{
+  _face_infos.clear();
+
+  // Either the element or the neighbor is a valid argument to get a face info
+  const Elem * equal_level_neighbor = _current_elem->neighbor_ptr(_current_side);
+  _candidate_neighbors = {equal_level_neighbor};
+
+  mooseAssert(_current_elem, "We should have an element");
+  mooseAssert(_current_elem->active(), "The current element should be active");
+
+  // No neighbor means we are at a boundary, a FaceInfo exists in the mesh
+  // If a neighbor exists, the face info may only be defined on that side
+  if (equal_level_neighbor)
   {
+    // neighbor is not active and we cant just pick an arbitrary refined element in neighbor
+    // no FaceInfo in mesh will satisfy us, let's error out
+    if (!equal_level_neighbor->active())
+      equal_level_neighbor->active_family_tree_by_neighbor(_candidate_neighbors, _current_elem);
 
-    // Either the element or the neighbor is a valid argument to get a face info
-    const Elem * neighbor = _current_elem->neighbor_ptr(_current_side);
-    const Elem * element = _current_elem;
-    int side = _current_side;
-
-    mooseAssert(_current_elem, "We should have an element");
-    mooseAssert(_current_elem->active(), "The current element should be active");
-
-    // No neighbor means we are at a boundary, a FaceInfo exists in the mesh
-    // If a neighbor exists, the face info may only be defined on that side
-    if (neighbor)
+    for (const Elem * neighbor : _candidate_neighbors)
     {
-      // neighbor is not active and we cant just pick an arbitrary refined element in neighbor
-      // no FaceInfo in mesh will satisfy us, let's error out
-      if (!neighbor->active())
-        mooseError("Inactive neighbor found in SideUO loop. Uneven refinement across sidesets "
-                   "is not supported by the faceInfo system in user objects");
+      const Elem * element = _current_elem;
+      auto side = _current_side;
 
       // First check refinement level
       if (_current_elem->level() < neighbor->level())
@@ -91,8 +96,10 @@ protected:
         element = neighbor;
         side = neighbor->which_neighbor_am_i(_current_elem);
       }
+      _face_infos.push_back(_mesh.faceInfo(element, side));
     }
-
-    return _mesh.faceInfo(element, side);
   }
+  else
+    _face_infos.push_back(_mesh.faceInfo(_current_elem, _current_side));
+}
 };
