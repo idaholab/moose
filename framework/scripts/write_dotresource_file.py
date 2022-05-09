bf8ffcfc36c83484dba4745494fa02da94da3beb
Jason M Miller <Jason.Miller@inl.gov>
2022-05-09 12:29:53 -0600

    Do not error on failed . resource file creation
    The TestHarness will 'handle' these situations (skip that test, with
    a helpful caveat)
    
    update our .gitignore list to include .moose_test resource file.

--- framework/scripts/write_dotresource_file.py
+++ framework/scripts/write_dotresource_file.py
@@ -2,7 +2,6 @@
 import os
 import sys
 import re
-import yaml
 
 def get_registered_apps(parts_list):
     """
@@ -31,6 +30,10 @@ def getParts(parts_list):
 def main(args):
     file_name = f'.{os.path.basename(args[1])}'
     file_path = os.path.dirname(args[1])
+    try:
+        import yaml
+    except ModuleNotFoundError:
+        return f'Python Yaml module not available to build {file_name}'
     yaml_contents = getParts(args[1:])
     with open(os.path.join(file_path, file_name), 'w') as stream:
         yaml.dump(yaml_contents, stream, default_flow_style=False)
