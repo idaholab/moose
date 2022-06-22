73036e2b0b9378720b8ce9b8223b97a182b93f59
Jason M Miller <Jason.Miller@inl.gov>
2022-06-22 13:04:53 -0600

    Do the modcache cleaning properly...

--- conda/pprof/build.sh
+++ conda/pprof/build.sh
@@ -5,7 +5,7 @@ export GOPATH="$PREFIX/pprof"
 go install github.com/google/pprof@c488b8fa1db3fa467bf30beb5a1d6f4f10bb1b87
 
 # go creates read-only files. Do this so Civet can properly clean up
-chmod -R 700 ${GOPATH}/pkg
+go clean -modcache
 
 # Set GPERF_DIR path (influential environment variable in MOOSE make files)
 mkdir -p "${PREFIX}/etc/conda/activate.d" "${PREFIX}/etc/conda/deactivate.d"
