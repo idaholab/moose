9d0fb518567cdac4f328cd4fe322f7c299db3fd3
April Novak <novak@berkeley.edu>
2022-06-30 21:12:39 -0500

    Doxygen not needed for inherited methods in derived classes. Refs #12284

--- modules/solid_properties/include/userobjects/ThermalFunctionSolidProperties.h
+++ modules/solid_properties/include/userobjects/ThermalFunctionSolidProperties.h
@@ -28,57 +28,18 @@ public:
 
   ThermalFunctionSolidProperties(const InputParameters & parameters);
 
-  /**
-   * Thermal conductivity from temperature
-   *
-   * @param[in] T temperature
-   * @return thermal conductivity
-   */
   virtual Real k_from_T(const Real & T) const override;
 
-  /**
-   * Thermal conductivity and its derivatives from temperature
-   *
-   * @param[in] T   temperature
-   * @param[out]    thermal conductivity
-   * @param[out]    derivative of thermal conductivity w.r.t. temperature
-   */
   virtual void k_from_T(const Real & T, Real & k, Real & dk_dT) const override;
   virtual void k_from_T(const DualReal & T, DualReal & k, DualReal & dk_dT) const override;
 
-  /**
-   * Isobaric specific heat capacity from temperature
-   *
-   * @param[in] T temperature
-   * @return isobaric specific heat capacity
-   */
   virtual Real cp_from_T(const Real & T) const override;
 
-  /**
-   * Isobaric specific heat capacity and its derivatives from temperature
-   *
-   * @param[in] T   temperature
-   * @param[out]    isobaric specific heat capacity
-   * @param[out]    derivative of isobaric specific heat capacity w.r.t. temperature
-   */
   virtual void cp_from_T(const Real & T, Real & cp, Real & dcp_dT) const override;
   virtual void cp_from_T(const DualReal & T, DualReal & cp, DualReal & dcp_dT) const override;
 
-  /**
-   * Density from temperature
-   *
-   * @param[in] T temperature
-   * @return density
-   */
   virtual Real rho_from_T(const Real & T) const override;
 
-  /**
-   * Density and its derivatives from temperature
-   *
-   * @param[in] T   temperature
-   * @param[out]    density
-   * @param[out]    derivative of density w.r.t. temperature
-   */
   virtual void rho_from_T(const Real & T, Real & rho, Real & drho_dT) const override;
   virtual void rho_from_T(const DualReal & T, DualReal & rho, DualReal & drho_dT) const override;
 
--- modules/solid_properties/include/userobjects/ThermalMonolithicSiCProperties.h
+++ modules/solid_properties/include/userobjects/ThermalMonolithicSiCProperties.h
@@ -23,57 +23,18 @@ public:
 
   ThermalMonolithicSiCProperties(const InputParameters & parameters);
 
-  /**
-   * Thermal conductivity from temperature
-   *
-   * @param[in] T temperature (K)
-   * @return thermal conductivity (W/(m*K))
-   */
-  virtual Real k_from_T(const Real & T) const override;
+   virtual Real k_from_T(const Real & T) const override;
 
-  /**
-   * Thermal conductivity and its derivatives from temperature
-   *
-   * @param[in] T   temperature (K)
-   * @param[out]    thermal conductivity (W/(m*K))
-   * @param[out]    derivative of thermal conductivity w.r.t. temperature
-   */
   virtual void k_from_T(const Real & T, Real & k, Real & dk_dT) const override;
   virtual void k_from_T(const DualReal & T, DualReal & k, DualReal & dk_dT) const override;
 
-  /**
-   * Isobaric specific heat capacity from temperature
-   *
-   * @param[in] T temperature (K)
-   * @return isobaric specific heat capacity (J/(kg*K))
-   */
   virtual Real cp_from_T(const Real & T) const override;
 
-  /**
-   * Isobaric specific heat capacity and its derivatives from temperature
-   *
-   * @param[in] T   temperature (K)
-   * @param[out]    isobaric specific heat capacity (J/(kg*K))
-   * @param[out]    derivative of isobaric specific heat capacity w.r.t. temperature
-   */
   virtual void cp_from_T(const Real & T, Real & cp, Real & dcp_dT) const override;
   virtual void cp_from_T(const DualReal & T, DualReal & cp, DualReal & dcp_dT) const override;
 
-  /**
-   * Density from temperature
-   *
-   * @param[in] T temperature (K)
-   * @return density (kg/m^3)
-   */
   virtual Real rho_from_T(const Real & T) const override;
 
-  /**
-   * Density and its derivatives from temperature
-   *
-   * @param[in] T   temperature (K)
-   * @param[out]    density (kg/m^3)
-   * @param[out]    derivative of density w.r.t. temperature
-   */
   virtual void rho_from_T(const Real & T, Real & rho, Real & drho_dT) const override;
   virtual void rho_from_T(const DualReal & T, DualReal & rho, DualReal & drho_dT) const override;
 
