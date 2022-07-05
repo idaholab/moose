56bfe73bd961d9eb36555fc66840ade868fd87c2
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2022-07-05 14:27:26 -0600

    Silence partially the main app perf graph during the multiapp executions, closes #20967

--- framework/src/multiapps/MultiApp.C
+++ framework/src/multiapps/MultiApp.C
@@ -245,7 +245,7 @@ MultiApp::MultiApp(const InputParameters & parameters)
     _backups(declareRestartableDataWithContext<SubAppBackups>("backups", this)),
     _cli_args(getParam<std::vector<std::string>>("cli_args")),
     _keep_solution_during_restore(getParam<bool>("keep_solution_during_restore")),
-    _solve_step_timer(registerTimedSection("solveStep", 3, "Taking Step")),
+    _solve_step_timer(registerTimedSection("solveStep", 3, "Executing MultiApps", false)),
     _init_timer(registerTimedSection("init", 3, "Initializing MultiApp")),
     _backup_timer(registerTimedSection("backup", 3, "Backing Up MultiApp")),
     _restore_timer(registerTimedSection("restore", 3, "Restoring MultiApp")),
