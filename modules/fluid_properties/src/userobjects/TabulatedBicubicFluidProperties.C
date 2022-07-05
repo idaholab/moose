da33d8df856a828d21832541f84e1f2c37571377
Benjamin Spaude <benjamin.spaude@inl.gov>
2022-07-05 10:51:21 -0600

    changed fp to a pointer. This allowed for the ability to choose not to generate fluid properties from _fp. ref #20101

--- modules/fluid_properties/src/userobjects/TabulatedBicubicFluidProperties.C
+++ modules/fluid_properties/src/userobjects/TabulatedBicubicFluidProperties.C
@@ -97,16 +97,12 @@ TabulatedBicubicFluidProperties::constructInterpolation()
       for (unsigned int j = 0; j < _num_e; ++j)
       {
         Real p_ve, T_ve;
-<<<<<<< HEAD
-        _fp.p_T_from_v_e(_specific_volume[i], _internal_energy[j], _p_initial_guess, _T_initial_guess, p_ve, T_ve);
-=======
         _fp->p_T_from_v_e(_specific_volume[i],
                           _internal_energy[j],
                           _p_initial_guess,
                           _T_initial_guess,
                           p_ve,
                           T_ve);
->>>>>>> 9fde525054 (fixup! Added checkNaNs routine to TabBicubicFP and TabBilinearFP. If nans exist, set variable to be constant as min or max, depending on where nans exist. Also added a checkOutofBounds routine to set variable to be constant at min or max depending on where variable is out of bounds. Added T_from_p_h routine using Newton Method. Added s_from_p_h routine which uses T_from_p_h, then finds s_from_p_T. Added feature to TabBicubicFP and TabBilinearFP which allows user to choose to generate (p,T) from both (v,e) and (v,h) or choose one or the other. Added routine to TFP to check the initial guesses for p and T that are used in Newton's Mehotd. If these guesses are outside the range for max/min of p and T, routine produces an error and informs the user. Ref #20101)
 
         /// check for NaNs in p interpolation
         checkNaNs(_pressure_min, _pressure_max, i, p_ve, num_p_nans_ve);
