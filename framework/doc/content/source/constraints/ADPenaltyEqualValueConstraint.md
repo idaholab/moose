99314fc3092f081e520fffb360efd58716770d6c
Gary Hu <hugary1995@gmail.com>
2022-08-03 21:57:43 -0500

    Move test object to framework; template on is_ad

---
+++ framework/doc/content/source/constraints/ADPenaltyEqualValueConstraint.md
@@ -0,0 +1,11 @@
+# ADPenaltyEqualValueConstraint
+
+The `ADPenaltyEqualValueConstraint` is the automatic differentiation version of [`PenaltyEqualValueConstraint`](PenaltyEqualValueConstraint.md).
+
+!syntax description /Constraints/ADPenaltyEqualValueConstraint
+
+!syntax parameters /Constraints/ADPenaltyEqualValueConstraint
+
+!syntax inputs /Constraints/ADPenaltyEqualValueConstraint
+
+!syntax children /Constraints/ADPenaltyEqualValueConstraint
--- framework/include/constraints/PenaltyEqualValueConstraint.h
+++ framework/include/constraints/PenaltyEqualValueConstraint.h
@@ -9,22 +9,42 @@
 
 #pragma once
 
+#include "MortarConstraint.h"
 #include "ADMortarConstraint.h"
+
+// Forward declaration
+template <bool is_ad>
+using MortarConstraintTempl =
+    typename std::conditional<is_ad, ADMortarConstraint, MortarConstraint>::type;
 
 /**
  * Constrain the value of a variable to be the same on both sides of an
  * interface using a generalized force stemming from a penalty-based enforcement.
  */
-class PenaltyEqualValueConstraint : public ADMortarConstraint
+template <bool is_ad>
+class PenaltyEqualValueConstraintTempl : public MortarConstraintTempl<is_ad>
 {
 public:
   static InputParameters validParams();
 
-  PenaltyEqualValueConstraint(const InputParameters & parameters);
+  PenaltyEqualValueConstraintTempl(const InputParameters & parameters);
 
 protected:
-  ADReal computeQpResidual(Moose::MortarType mortar_type) final;
+  GenericReal<is_ad> computeQpResidual(Moose::MortarType mortar_type) final;
+  GenericReal<is_ad> computeQpJacobian(Moose::ConstraintJacobianType jacobian_type,
+                                       unsigned int jvar);
 
   /// Penalty value used to enforce the constraint
   const Real _penalty_value;
+
+  using MortarConstraintTempl<is_ad>::_u_primary;
+  using MortarConstraintTempl<is_ad>::_u_secondary;
+  using MortarConstraintTempl<is_ad>::_test_primary;
+  using MortarConstraintTempl<is_ad>::_test_secondary;
+  using MortarConstraintTempl<is_ad>::_qp;
+  using MortarConstraintTempl<is_ad>::_i;
+  using MortarConstraintTempl<is_ad>::_j;
 };
+
+typedef PenaltyEqualValueConstraintTempl<false> PenaltyEqualValueConstraint;
+typedef PenaltyEqualValueConstraintTempl<true> ADPenaltyEqualValueConstraint;
