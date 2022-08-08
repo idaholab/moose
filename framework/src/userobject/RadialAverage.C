79808782eb5b0f9ade49d5a7bbb8cd1d632f0447
Max <maxnezdyur@gmail.com>
2022-08-08 06:30:24 -0600

    Fixed header, but still trying to setup patch

--- framework/src/userobject/RadialAverage.C
+++ framework/src/userobject/RadialAverage.C
@@ -1,10 +1,11 @@
-/**********************************************************************/
-/*                     DO NOT MODIFY THIS HEADER                      */
-/* MAGPIE - Mesoscale Atomistic Glue Program for Integrated Execution */
-/*                                                                    */
-/*            Copyright 2017 Battelle Energy Alliance, LLC            */
-/*                        ALL RIGHTS RESERVED                         */
-/**********************************************************************/
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
 
 #include "RadialAverage.h"
 #include "ThreadedRadialAverageLoop.h"
