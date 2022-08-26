823653a038bfe604faea38270bc49e7289a070e2
Gary Hu <thu@anl.gov>
2022-08-26 08:14:02 -0500

    Address comment

--- framework/src/actions/ExecuteMeshGenerators.C
+++ framework/src/actions/ExecuteMeshGenerators.C
@@ -24,9 +24,10 @@ ExecuteMeshGenerators::ExecuteMeshGenerators(InputParameters params) : Action(pa
 void
 ExecuteMeshGenerators::act()
 {
-  // Don't do mesh generators when recovering as the master app or using master mesh! We do need
-  // to run MeshGenerators for sub-apps because we don't currently have checkpoint/restart
-  // information for the sub-app meshes; e.g. we just need to re-build them
+  // Don't do mesh generators when recovering as the master app (unless requested with
+  // use_checkpoint_mesh = false) or using master mesh! We do need to run MeshGenerators for
+  // sub-apps because we don't currently have checkpoint/restart information for the sub-app meshes;
+  // e.g. we just need to re-build them
   if ((_mesh->useCheckpointMesh() && _app.isUltimateMaster()) || _app.masterMesh() ||
       _app.isUseSplit())
     return;
