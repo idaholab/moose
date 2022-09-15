12c30b786e7e0c94a5a2fd653557855b005697a0
ttruster <timothy.truster@gmail.com>
2022-09-15 12:56:27 -0400

    Copy files for scalar computations inside mortar constraints

--- framework/include/constraints/MortarConstraint.h
+++ framework/include/constraints/MortarConstraint.h
@@ -27,10 +27,38 @@ public:
   using MortarConstraintBase::computeJacobian;
 
 protected:
+
+  /**
+   * compute the residual for primary/secondary/lower
+   */
+  virtual void computeResidual(Moose::MortarType mortar_type) override;
+
+  /**
+   * compute the scalar residual
+   */
+  virtual void computeResidualScalar() override;
+
+  /**
+   * compute the Jacobian for the specified element type
+   */
+  virtual void computeJacobian(Moose::MortarType mortar_type) override;
+
+  /**
+   * Method for computing the scalar Jacobian
+   */
+  virtual void computeJacobianScalar() override;
+
+
   /**
    * compute the residual at the quadrature points
    */
   virtual Real computeQpResidual(Moose::MortarType mortar_type) = 0;
+
+  /**
+   * compute the scalar residual at the quadrature points
+   */
+  virtual Real computeQpResidualScalar() {return 0;};
+  virtual Real computeQpResidualScalarScalar() {return 0;};
 
   /**
    * compute the jacobian at the quadrature points
@@ -38,15 +66,17 @@ protected:
   virtual Real computeQpJacobian(Moose::ConstraintJacobianType jacobian_type,
                                  unsigned int jvar) = 0;
 
-  void computeResidual(Moose::MortarType mortar_type) override;
-
-  void computeJacobian(Moose::MortarType mortar_type) override;
+  /**
+   * compute the scalar jacobian at the quadrature points
+   */
+  virtual Real computeQpJacobianScalarScalar() {return 0;};
 
 private:
   /// A dummy object useful for constructing _lambda when not using Lagrange multipliers
   const VariableValue _lambda_dummy;
 
 protected:
+
   /// The LM solution
   const VariableValue & _lambda;
 
