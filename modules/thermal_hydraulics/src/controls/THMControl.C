d0a216c8b935cdf1559554142440f53f8080cf11
hansje <joshua.hansel@inl.gov>
2022-09-28 13:52:52 -0500

    Make THMControl execute only on TIMESTEP_BEGIN
    Refs #22118

--- modules/thermal_hydraulics/src/controls/THMControl.C
+++ modules/thermal_hydraulics/src/controls/THMControl.C
@@ -13,7 +13,7 @@ InputParameters
 THMControl::validParams()
 {
   InputParameters params = Control::validParams();
-  params.set<ExecFlagEnum>("execute_on") = {EXEC_INITIAL, EXEC_TIMESTEP_BEGIN};
+  params.set<ExecFlagEnum>("execute_on") = {EXEC_TIMESTEP_BEGIN};
   params.addPrivateParam<THMProblem *>("_thm_problem");
   params.suppressParameter<ExecFlagEnum>("execute_on");
   return params;
