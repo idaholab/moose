0952cdc825cf0bb2b6511536d7ab0dd037633b6f
Jason M Miller <Jason.Miller@inl.gov>
2022-10-25 12:05:14 -0600

    Add empty Apptainer definition files
    Add empty Apptainer recipe/definition files so that module_hash
    functions correctly.
    
    Closes #22505

--- scripts/tests/test_modulehash.py
+++ scripts/tests/test_modulehash.py
@@ -18,11 +18,15 @@ import mooseutils
 MOOSE_DIR = mooseutils.git_root_dir()
 YAML_FILE = os.path.relpath(os.path.join(MOOSE_DIR, "scripts", "module_hash.yaml"))
 
-# TODO: Replace with an actual commit hash which contains module_hash.yaml
+# TODO: replace with a hash from master branch which contains the updated module_hash.yaml file
+# where the following keys in HASH_TABLE.keys() exist (at time of writing we are missing mpi, moose).
 COMMIT = 'FIXME'
 
-# The following represent the current output of module_hash.py petsc|libmesh
-HASH_TABLE = {'petsc': '8328006', 'libmesh' :'ae2fdf1'}
+# The following represent the current hashes for COMMIT
+HASH_TABLE = {'mpi'       : 'd385d66',
+              'petsc'     : '662b9e2',
+              'libmesh'   : 'c095106',
+              'moose'     : 'cd87adc'}
 
 try:
     with open(f'{YAML_FILE}', 'r', encoding='utf-8') as rc_file:
@@ -64,7 +68,7 @@ class Test(unittest.TestCase):
         for library in LIBRARIES:
             out = mooseutils.check_output(['./module_hash.py', library, 'abc1234'],
                                           cwd=os.path.join(MOOSE_DIR, 'scripts'))
-            self.assertIn('warning: commit abc1234 does not contain module_hash.yaml\narbitrary\n', out)
+            self.assertIn('warning: commit abc1234 is not a valid git commit\narbitrary\n', out)
 
         # Verify a non existent hash fatally errors when invoked with --influential
         for library in LIBRARIES:
@@ -72,50 +76,16 @@ class Test(unittest.TestCase):
                 out = mooseutils.check_output(['./module_hash.py', library, 'abc1234', '--influential'],
                                               cwd=os.path.join(MOOSE_DIR, 'scripts'))
             e = cm.exception
-            self.assertIn('warning: commit abc1234 does not contain module_hash.yaml\narbitrary\n', out)
-
-    def testEntitiesHEAD(self):
-        # Verify that module_hashes.py reports valid control files for HEAD
-        _ltmp = set([])
-        _unit_tmp = set([])
-        for library in LIBRARIES:
-            out = mooseutils.check_output(['./module_hash.py', library, '--influential'],
-                                          cwd=os.path.join(mooseutils.git_root_dir(), 'scripts'))
-            _ltmp.update(out.split())
-        for library in LIBRARIES:
-            _unit_tmp.update([f'{os.path.join(MOOSE_DIR, x)}' for x in ENTITIES[library]])
-
-        if _ltmp.difference(_unit_tmp) or len(_ltmp) is not len(_unit_tmp):
-            self.fail(f'Difference in control files: {_ltmp.difference(_unit_tmp)}')
+            self.assertIn('warning: commit abc1234 is not a valid git commit\narbitrary\n', out)
 
     def testExpectedOutputHASH(self):
         # TODO: When module_hash.yaml has entered git history, uncomment the following test
         #       and replace COMMIT='FIXME' with a proper hash containing module_hash.yaml
-
         # # Verify a vetted hash reports correctly for all involved libraries
         # for library in LIBRARIES:
         #     out = mooseutils.check_output(['./module_hash.py', library, COMMIT, '--quiet'],
         #                                   cwd=os.path.join(mooseutils.git_root_dir(), 'scripts'))
         #     self.assertIn(HASH_TABLE[library], out)
-        return
-
-    def testEntitiesHASH(self):
-        # TODO: When module_hash.yaml has entered git history, uncomment the following test
-        #       and replace COMMIT='FIXME' with a proper hash containing module_hash.yaml
-
-        # # Verify a vetted hash reports proper control files
-        # hashed_list = self.read_yaml()
-        # _ltmp = set([])
-        # _unit_tmp = set([])
-        # for library in LIBRARIES:
-        #     out = mooseutils.check_output(['./module_hash.py', library, COMMIT, '--influential'],
-        #                                   cwd=os.path.join(mooseutils.git_root_dir(), 'scripts'))
-        #     _ltmp.update(out.split())
-        # for library in LIBRARIES:
-        #     _unit_tmp.update([f'{os.path.join(MOOSE_DIR, x)}' for x in hashed_list['packages'][library]])
-
-        # if _ltmp.difference(_unit_tmp) or len(_ltmp) is not len(_unit_tmp):
-        #     self.fail(f'Difference in control files: {_ltmp.difference(_unit_tmp)}')
         return
 
 if __name__ == '__main__':
