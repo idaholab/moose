7a98930363bf0a7bb94cf6c7a69f83c40fd8cc5c
somu15 <som.dhulipala@inl.gov>
2022-10-31 11:37:25 -0600

    Gaussian likelihood

--- modules/stochastic_tools/src/likelihood/Gaussian.C
+++ modules/stochastic_tools/src/likelihood/Gaussian.C
@@ -8,9 +8,9 @@
 //* https://www.gnu.org/licenses/lgpl-2.1.html
 
 #include "Gaussian.h"
-#include "Normal.h"
 #include "math.h"
 #include "libmesh/utility.h"
+#include "DelimitedFileReader.h"
 
 registerMooseObject("StochasticToolsApp", Gaussian);
 
@@ -20,31 +20,62 @@ Gaussian::validParams()
   InputParameters params = Likelihood::validParams();
   params.addClassDescription("Gaussian likelihood function evaluating the model goodness against experiments.");
   params.addParam<bool>(
-      "inferred_noise", false, "Whether noise is inferred from Bayes' rule.");
-  params.addParam<bool>(
-      "experiment_file", true, "Whether a csv file containing experimental values is provided.");
-  params.addParam<bool>(
       "log_likelihood", true, "Compute log-likelihood or likelihood.");
   params.addRequiredParam<std::vector<Real>>("model_pred", "A vector of model predictions.");
-  params.addParam<Real>("noise", "User-specified noise value when not inferred from Bayes' rule.");
-  params.addParam<std::vector<Real>>("exp_values", "User-specified experimental values when csv file is not provided.");
+  params.addRequiredParam<Real>("noise", "Experimental noise plus model deviations against experiments.");
+  params.addParam<FileName>("file_name", "Name of the CSV file with experimental values.");
+  params.addParam<std::string>(
+      "file_column_name", "Name of column in CSV file to use, by default first column is used.");
+  params.addParam<std::vector<Real>>("exp_values", "User-specified experimental values when CSV file is not provided.");
   return params;
 }
 
 Gaussian::Gaussian(const InputParameters & parameters)
-  : Likelihood(parameters)
+  : Likelihood(parameters),
+    _log_likelihood(getParam<bool>("log_likelihood")),
+    _model_pred(getParam<std::vector<Real>>("model_pred")),
+    _noise(getParam<Real>("noise"))
 {
+  if (isParamValid("exp_values") && isParamValid("file_name"))
+    paramError("exp_values", "exp_values and file_name both cannot be set at the same time.");
+  else if (isParamValid("file_name"))
+  {
+    MooseUtils::DelimitedFileReader reader(getParam<FileName>("file_name"));
+    reader.read();
+    if (isParamValid("file_column_name"))
+      _exp_values = reader.getData(getParam<std::string>("file_column_name"));
+    else
+      _exp_values = reader.getData(0);
+  }
+  else if (isParamValid("exp_values"))
+    _exp_values = getParam<std::vector<Real>>("exp_values");
+  else
+    mooseError("Either 'exp_values' or 'file_name' parameters must be specified to represent experimental data.");
+  
+  if (_exp_values.size() != _model_pred.size())
+    mooseError("Model prediction and experimental value vectors should have the same size.");
 }
 
 Real
-Gaussian::densityFunction(const std::vector<Real> & x, const Real & noise)
+Gaussian::densityFunction(const std::vector<Real> & exp, const std::vector<Real> & model, const Real & noise, const bool & log_likelihood)
 {
-  return 1.0 / (std_dev * std::sqrt(2.0 * M_PI)) *
-         std::exp(-0.5 * Utility::pow<2>((x - mean) / std_dev));
+  Real result = 0.0;
+  for (unsigned i = 0; i < exp.size(); ++i)
+    result += std::log(1.0 / (noise * std::sqrt(2.0 * M_PI))) - 0.5 * Utility::pow<2>((exp[i] - model[i]) / noise);
+  if (!log_likelihood)
+    result = std::exp(result);
+  return result;
 }
 
 Real
-Gaussian::densityFunction(const std::vector<Real> & x) const
+Gaussian::densityFunction() const
 {
-  return densityFunction(x, _noise);
+  return densityFunction(_exp_values, _model_pred, _noise, _log_likelihood);
+}
+
+Real
+Gaussian::massFunction() const
+{
+  mooseError("Mass function is not defined for continuous a distribution.");
+  return 0.0;
 }
