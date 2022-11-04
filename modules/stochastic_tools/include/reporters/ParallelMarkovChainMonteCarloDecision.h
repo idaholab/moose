52f3aa6e4f8a589c6813a8faf2c569c81bec23e5
somu15 <som.dhulipala@inl.gov>
2022-11-04 13:40:55 -0600

    Initial base files

---
+++ modules/stochastic_tools/include/reporters/ParallelMarkovChainMonteCarloDecision.h
@@ -0,0 +1,75 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "GeneralReporter.h"
+#include "ParallelMarkovChainMonteCarloBase.h"
+#include "LikelihoodInterface.h"
+#include "DistributionInterface.h"
+
+/**
+ * ParallelMarkovChainMonteCarloDecision will help make sample accept/reject decisions in MCMC
+ * schemes (for e.g., when performing Bayesian inference).
+ */
+class ParallelMarkovChainMonteCarloDecision : public GeneralReporter, public LikelihoodInterface, public DistributionInterface
+{
+public:
+  static InputParameters validParams();
+  ParallelMarkovChainMonteCarloDecision(const InputParameters & parameters);
+  virtual void initialize() override {}
+  virtual void finalize() override {}
+  virtual void execute() override;
+
+  /**
+   * Compute the transition probability vector
+   */
+  virtual void computeTransitionVector(std::vector<Real> tv, std::vector<const Distribution *> priors, std::vector<const Likelihood *> likelihoods, const DenseMatrix<Real> & inputs, const std::vector<Real> & outputs) const = 0;
+
+  /**
+   * Resample inputs given weights
+   */
+  virtual void resample(const DenseMatrix<Real> & given_inputs, std::vector<std::vector<Real>> resampled_inputs, const std::vector<Real> & weights, const unsigned int & num_resamples) const = 0;
+
+protected:
+  /// Reporter value of the seed input values for proposing the next set of samples
+  const std::vector<Real> & _seed_inputs;
+
+  /// Model output value from SubApp
+  const std::vector<Real> & _output_value;
+
+  /// Model input data that is uncertain
+  std::vector<std::vector<Real>> & _inputs;
+
+private:
+  /// Track the current step of the main App
+  const int & _step;
+
+  /// The adaptive Monte Carlo sampler
+  Sampler & _sampler;
+
+  /// Adaptive Importance Sampler
+  const ParallelMarkovChainMonteCarloBase * const _pmcmc;
+
+  /// Ensure that the MCMC algorithm proceeds in a sequential fashion
+  int _check_step;
+
+  /// Storage for the likelihood objects to be utilized
+  std::vector<const Likelihood *> _likelihoods;
+
+  /// Storage for prior distribution objects to be utilized
+  std::vector<const Distribution *> _priors;
+
+  /// Communicator that was split based on samples that have rows
+  libMesh::Parallel::Communicator _local_comm;
+
+  /// Facilitate allGather of outputs
+  std::vector<Real> _output_comm;
+  
+};
---
+++ modules/stochastic_tools/include/samplers/ParallelMarkovChainMonteCarloBase.h
@@ -0,0 +1,59 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "Sampler.h"
+#include "ReporterInterface.h"
+#include "LikelihoodInterface.h"
+
+/**
+ * A base class used to perform Parallel Markov Chain Monte Carlo (MCMC) sampling
+ */
+class ParallelMarkovChainMonteCarloBase : public Sampler, public ReporterInterface, public LikelihoodInterface
+{
+public:
+  static InputParameters validParams();
+
+  ParallelMarkovChainMonteCarloBase(const InputParameters & parameters);
+
+protected:
+  virtual void sampleSetUp(const Sampler::SampleMode mode) override;
+  virtual Real computeSample(dof_id_type row_index, dof_id_type col_index) override;
+
+  /// Reporter value the seed input values for proposing the next set of samples
+  const std::vector<Real> & _seed_inputs;
+
+  /// Number of parallel proposals to be made and subApps to be executed
+  const unsigned int & _num_parallel_proposals;
+
+  /// Storage for the likelihood object to be utilized
+  // const Likelihood & _likelihood;
+
+  /// Initial values of the input params to get the MCMC scheme started
+  const std::vector<Real> & _initial_values;
+
+  /// Storage for prior distribution objects to be utilized
+  std::vector<const Distribution *> _priors;
+
+  /// Track the current step of the main App
+  const int & _step;
+
+  /// Ensure that the MCMC algorithm proceeds in a sequential fashion
+  int _check_step;
+
+  /// Initialize a certain number of random seeds. Change from the default only if you have to.
+  const unsigned int & _num_random_seeds;
+
+private:
+
+  /// Initialize a certain number of random seeds. Change from the default only if you have to.
+  std::vector<std::vector<Real>> _new_samples;
+  
+};
