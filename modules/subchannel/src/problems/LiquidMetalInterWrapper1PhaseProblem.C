f08f95835094efa2fb108922e4351ac7d43a0638
Cody Permann <codypermann@gmail.com>
2022-11-10 10:04:39 -0700

    Adding copyright headers
    closes #29264

--- modules/subchannel/src/problems/LiquidMetalInterWrapper1PhaseProblem.C
+++ modules/subchannel/src/problems/LiquidMetalInterWrapper1PhaseProblem.C
@@ -1,3 +1,17 @@
+/********************************************************************/
+/*                   DO NOT MODIFY THIS HEADER                      */
+/*          Subchannel: Thermal Hydraulics Reactor Analysis         */
+/*                                                                  */
+/*              (c) 2022 Battelle Energy Alliance, LLC              */
+/*                      ALL RIGHTS RESERVED                         */
+/*                                                                  */
+/*             Prepared by Battelle Energy Alliance, LLC            */
+/*               Under Contract No. DE-AC07-05ID14517               */
+/*               With the U. S. Department of Energy                */
+/*                                                                  */
+/*               See COPYRIGHT for full restrictions                */
+/********************************************************************/
+
 #include "LiquidMetalInterWrapper1PhaseProblem.h"
 #include "AuxiliarySystem.h"
 #include "TriInterWrapperMesh.h"
