ef0d5597afb3168e0aac5d2a2ef6f3b1e1e698aa
Joshua E. Hansel <joshua.hansel@inl.gov>
2022-12-01 12:31:24 -0600

    Modified THM tasks, syntax, and dependencies
    Refs #22354

--- modules/thermal_hydraulics/src/parser/THMSyntax.C
+++ modules/thermal_hydraulics/src/parser/THMSyntax.C
@@ -20,8 +20,11 @@ associateSyntax(Syntax & syntax)
   syntax.registerActionSyntax("AddHeatStructureMaterialAction",
                               "HeatStructureMaterials/*",
                               "THM:add_heat_structure_material");
+  syntax.registerActionSyntax("THMSetUpMeshAction", "Components");
   syntax.registerActionSyntax("CreateTHMMeshAction", "Components");
-  syntax.registerActionSyntax("THMCreateMeshAction", "Components");
+  syntax.registerActionSyntax("CreateTHMMeshGeneratorAction", "Components");
+  syntax.registerActionSyntax("THMBuildMeshAction", "Components");
+  syntax.registerActionSyntax("THMAddRelationshipManagersAction", "Components");
   syntax.registerActionSyntax("AddComponentAction", "Components/*", "THM:add_component");
   syntax.registerActionSyntax("AddComponentAction", "Components/*/*", "THM:add_component");
   syntax.registerActionSyntax("AddClosuresAction", "Closures/*", "THM:add_closures");
@@ -44,7 +47,10 @@ registerActions(Syntax & syntax)
   registerTask("THM:init_simulation", true);
   registerTask("THM:create_thm_mesh", false);
   registerTask("THM:set_up_coordinate_system", true);
-  registerTask("THM:build_mesh", true);
+  registerTask("THM:create_thm_problem", true);
+  registerTask("THM:create_thm_mesh", false);
+  registerTask("THM:create_thm_mesh_generator", false);
+  registerTask("THM:build_mesh", false);
   registerTask("THM:init_components", true);
   registerTask("THM:identify_loops", true);
   registerTask("THM:add_variables", true);
@@ -57,7 +63,7 @@ registerActions(Syntax & syntax)
   registerTask("THM:debug_action", false);
   registerTask("THM:print_component_loops", false);
   registerTask("THM:output_vector_velocity", true);
-  registerTask("THM:add_relationship_managers", true);
+  registerTask("THM:add_relationship_managers", false);
 
   registerMooseObjectTask("THM:add_component", Component, false);
   registerMooseObjectTask("THM:add_heat_structure_material", SolidMaterialProperties, false);
@@ -68,8 +74,14 @@ registerActions(Syntax & syntax)
     syntax.addDependency("THM:add_heat_structure_material", "add_function");
     syntax.addDependency("THM:output_vector_velocity", "setup_mesh");
     syntax.addDependency("THM:add_closures", "setup_mesh");
+
+    // FEProblem requires that the MeshBase pointer not be nullptr.
+    syntax.addDependency("THM:create_thm_problem", "set_mesh_base");
+
+    syntax.addDependency("THM:add_component", "THM:create_thm_mesh");
     syntax.addDependency("THM:add_component", "THM:output_vector_velocity");
     syntax.addDependency("THM:debug_action", "setup_mesh");
+    syntax.addDependency("create_problem_default", "THM:create_thm_problem");
     syntax.addDependency("THM:init_simulation", "THM:add_component");
     syntax.addDependency("add_mesh_generator", "THM:add_component");
     syntax.addDependency("THM:identify_loops", "THM:add_component");
@@ -78,7 +90,9 @@ registerActions(Syntax & syntax)
     syntax.addDependency("THM:integrity_check", "THM:identify_loops");
     syntax.addDependency("THM:integrity_check", "THM:debug_action");
     syntax.addDependency("THM:build_mesh", "THM:init_simulation");
-    syntax.addDependency("add_mesh_generator", "THM:build_mesh");
+    syntax.addDependency("THM:build_mesh", "THM:create_thm_mesh");
+    syntax.addDependency("add_mesh_generator", "THM:create_thm_mesh");
+    syntax.addDependency("execute_mesh_generators", "THM:build_mesh");
     syntax.addDependency("THM:set_up_coordinate_system", "create_problem_complete");
     syntax.addDependency("add_fluid_properties", "THM:set_up_coordinate_system");
     syntax.addDependency("add_elemental_field_variable", "add_fluid_properties");
@@ -97,6 +111,13 @@ registerActions(Syntax & syntax)
     syntax.addDependency("THM:print_component_loops", "THM:control_data_integrity_check");
     syntax.addDependency("THM:preconditioning_integrity_check", "check_integrity");
     syntax.addDependency("THM:add_relationship_managers", "add_geometric_rm");
+
+    // The THMMesh builds its MeshBase using the system MooseMesh.
+    syntax.addDependency("THM:create_thm_mesh", "setup_mesh");
+
+    // The THM mesh MeshBase is used in the construction of the relationship manager.
+    syntax.addDependency("THM:add_relationship_managers", "THM:create_thm_mesh");
+
     syntax.addDependency("THM:output_vector_velocity", "THM:add_relationship_managers");
     syntax.addDependency("THM:add_variables", "THM:integrity_check");
   }
