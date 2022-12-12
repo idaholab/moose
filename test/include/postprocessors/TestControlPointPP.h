11fc13c73b3298707f3c8fed3a4e08a20c29825f
Sebastian Schunert <sebastian.schunert@inl.gov>
2022-12-12 12:55:31 -0700

    The controllable point parameter is used all over the controls tests. Need to replace by testing object (#22937

---
+++ test/include/postprocessors/TestControlPointPP.h
@@ -0,0 +1,34 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "GeneralPostprocessor.h"
+
+class Function;
+
+/**
+ * Tests controllable parameter of type Point
+ */
+class TestControlPointPP : public GeneralPostprocessor
+{
+public:
+  static InputParameters validParams();
+
+  TestControlPointPP(const InputParameters & parameters);
+
+  virtual void initialize() override;
+  virtual void execute() override;
+  virtual PostprocessorValue getValue() override;
+
+protected:
+  const Function & _function;
+  const Point & _point;
+  const Real & _scale_factor;
+};
