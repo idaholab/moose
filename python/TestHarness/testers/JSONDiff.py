17fe7320319b8aab24aaab8312ffd9e026cf9dd3
socratesgorilla <76120892+socratesgorilla@users.noreply.github.com>
2023-01-10 11:38:59 -0700

    Fixed keep_system_information not existing

--- python/TestHarness/testers/JSONDiff.py
+++ python/TestHarness/testers/JSONDiff.py
@@ -16,14 +16,16 @@ class JSONDiff(SchemaDiff):
         params = SchemaDiff.validParams()
         params.addRequiredParam('jsondiff',   [], "A list of JSON files to compare.")
         params.addParam('skip_keys', [],"Deprecated. Items in the JSON that the differ will ignore. This is functionally identical to ignored_items inside of SchemaDiff.")
+        params.addParam('keep_system_information', False, "Whether or not to keep the system information as part of the diff.")
         return params
 
     def __init__(self, name, params):
         params['schemadiff'] = params['jsondiff']
         params['ignored_items'] += params['skip_keys']
-        # Add system information to ignored_items
-        if (not self.specs['keep_system_information']):
-            params['ignored_items'].extend(['app_name',
+
+        SchemaDiff.__init__(self, name, params)
+        if (not params['keep_system_information']):
+            self.specs['ignored_items'].extend(['app_name',
                                             'current_time',
                                             'executable',
                                             'executable_time',
@@ -31,7 +33,7 @@ class JSONDiff(SchemaDiff):
                                             'libmesh_version',
                                             'petsc_version',
                                             'slepc_version'])
-        SchemaDiff.__init__(self, name, params)
+
 
     def prepare(self, options):
         if self.specs['delete_output_before_running'] == True:
