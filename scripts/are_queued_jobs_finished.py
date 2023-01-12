e667e7cb8838c294b98463ae3d6edaef44baadd4
Jason M Miller <Jason.Miller@inl.gov>
2023-01-12 13:19:24 -0700

    Update are_queued_jobs_finished.py
    Use quicker/better methods to report if all jobs launched, have
    finished.
    
    Closes #23141

--- scripts/are_queued_jobs_finished.py
+++ scripts/are_queued_jobs_finished.py
@@ -23,18 +23,61 @@ class Jobs:
         for k, v in self.__job_data.items():
             yield k, v
 
+def runCommand(cmd, cwd=None):
+    # On Windows it is not allowed to close fds while redirecting output
+    p = subprocess.Popen(cmd,
+                         cwd=cwd,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT,
+                         close_fds=True,
+                         shell=True)
+
+    output = p.communicate()[0].decode('utf-8')
+    if (p.returncode != 0):
+        output = 'ERROR: ' + output
+    return output
 
 def isNotFinished(jobs):
+    """
+    Return True when we find a reason that not every job has completed.
+    Check for the quick stuff first, then do the expensive `qstat` command.
+    """
     for path, meta in jobs.yieldJobsResultPath():
-        if os.path.exists(path):
-            job_id = meta['RunPBS']['ID'].split('.')[0]
-            qstat_process = subprocess.Popen([ 'qstat' , '-xf', job_id], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            qstat_result = qstat_process.communicate()[0].decode('utf-8')
-            job_status = re.findall(r'job_state = (.*)', qstat_result)
-            if (job_status != ['F'] and job_status != ['X'] and job_status != ['U'] and job_status  != ['S'] and job_status != []):
-                return True
 
+        # Not a Job artifact (TestHarness argument/option, like -i, the scheduler, etc)
+        if type(meta) != type({}):
+            continue
 
+        # Results file exist (job therefore finished)
+        elif os.path.exists(os.path.join(path, '.previous_test_results.json')):
+            continue
+
+        # Job was skipped, or otherwise not launched via PBS
+        elif not meta.get('RunPBS', False):
+            continue
+
+        # Ask qstat
+        elif 'RunPBS' in meta:
+            job_id = meta['RunPBS']['ID'].replace('\n', '')
+            qstat_command_result = runCommand(f'qstat -xf -F json {job_id}')
+
+            # Catch json parsing errors
+            try:
+                json_out = json.loads(qstat_command_result)
+                job_meta = json_out['Jobs'][job_id]
+
+                # If Exit_status exists, then this job completed
+                if job_meta.get('Exit_status', False):
+                    continue
+                else:
+                    print(f'STILL RUNNING: {job_id}')
+                    return True
+
+            # JobID no longer exists (stale after 1 week)
+            except json.decoder.JSONDecodeError:
+                # Job has completed if we make it here
+                print('problem decoding data (week old)')
+                continue
 
 def usage():
     print('Supply a path to json queue file. Multiple files are supported, in which case all'
@@ -49,4 +92,6 @@ if __name__ == '__main__':
     for queue_file in args:
         jobs = Jobs(queue_file)
         if isNotFinished(jobs):
-            sys.exit(1)
+            sys.exit('NOT READY')
+    print('READY')
+    sys.exit(0)
