3f69e43cc613984306805026d7368aa73e850713
Aya Hegazy <aya.hamdy.anwar@gmail.com>
2023-01-19 19:15:15 -0600

    applying tyle changes

--- modules/solid_properties/src/userobjects/ThermalUCProperties.C
+++ modules/solid_properties/src/userobjects/ThermalUCProperties.C
@@ -29,36 +29,38 @@ void
 ThermalUCProperties::cp_from_T(const Real & T, Real & cp, Real & dcp_dT) const
 {
   cp = cp_from_T(T);
-  dcp_dT = - 5.068e-3 - 3.5208e-5 * T + 6976200 / Utility::pow<3>(T);
+  dcp_dT = -5.068e-3 - 3.5208e-5 * T + 6976200 / Utility::pow<3>(T);
 }
 
 Real
 ThermalUCProperties::k_from_T(const Real & T) const
 {
 
- if (323 < T < 923){
-                   return 21.7 - 3.04e-3 * (T-273) + 3.61e-6 * Utility::pow<2>(T-273);
-         }
- if (924 < T < 2573){
-                    return 20.2 + 1.48e-3 * (T-273);
-         }
-
+  if (323 < T < 923)
+  {
+    return 21.7 - 3.04e-3 * (T - 273) + 3.61e-6 * Utility::pow<2>(T - 273);
+  }
+  if (924 < T < 2573)
+  {
+    return 20.2 + 1.48e-3 * (T - 273);
+  }
 }
 
 void
 ThermalUCProperties::k_from_T(const Real & T, Real & k, Real & dk_dT) const
 {
 
- if (323 < T < 923){
-                   k = k_from_T(T);
-                   dk_dT = - 3.04e-3 + 7.22e-6 * (T-273);
-         }
- if (924 < T < 2573){
-                    k = k_from_T(T);
-                    dk_dT = 1.48e-3;
-         }
+  if (323 < T < 923)
+  {
+    k = k_from_T(T);
+    dk_dT = -3.04e-3 + 7.22e-6 * (T - 273);
+  }
+  if (924 < T < 2573)
+  {
+    k = k_from_T(T);
+    dk_dT = 1.48e-3;
+  }
 }
-
 
 Real
 ThermalUCProperties::rho_from_T(const Real & /* T */) const
