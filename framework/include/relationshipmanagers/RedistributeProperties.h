0f5d6b26e3fb0e9ec65bd9ccb64241bc07ebd530
Roy Stogner <Roy.Stogner@inl.gov>
2023-01-24 16:04:55 -0600

    Boilerplate RedistributeProperties GhostingFunctor

---
+++ framework/include/relationshipmanagers/RedistributeProperties.h
@@ -0,0 +1,46 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "RelationshipManager.h"
+
+class MaterialPropertyStorage;
+
+/**
+ * RedistributeProperties is used for its \p redistribute() callback,
+ * which ensures that any stateful properties are restributed to the
+ * same properties to which elements they are associated with are
+ * redistributed.
+ */
+class RedistributeProperties : public RelationshipManager
+{
+public:
+  static InputParameters validParams();
+
+  RedistributeProperties(const InputParameters & parameters);
+
+  RedistributeProperties(const RedistributeProperties & other) = default;
+
+  virtual void operator()(const MeshBase::const_element_iterator & range_begin,
+                          const MeshBase::const_element_iterator & range_end,
+                          processor_id_type p,
+                          map_type & coupled_elements) override;
+
+  virtual std::unique_ptr<GhostingFunctor> clone() const override;
+
+  virtual std::string getInfo() const override;
+
+  virtual bool operator>=(const RelationshipManager & rhs) const override;
+
+  virtual void redistribute() override;
+
+private:
+  MaterialPropertyStorage * _mat_prop_storage;
+};
---
+++ framework/src/relationshipmanagers/RedistributeProperties.C
@@ -0,0 +1,61 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "RedistributeProperties.h"
+
+registerMooseObject("MooseApp", RedistributeProperties);
+
+InputParameters
+RedistributeProperties::validParams()
+{
+  InputParameters params = RelationshipManager::validParams();
+
+  return params;
+}
+
+RedistributeProperties::RedistributeProperties(const InputParameters & parameters)
+  : RelationshipManager(parameters)
+{
+}
+
+void
+RedistributeProperties::operator()(const MeshBase::const_element_iterator &,
+                                   const MeshBase::const_element_iterator &,
+                                   processor_id_type,
+                                   map_type &)
+{
+}
+
+std::unique_ptr<GhostingFunctor>
+RedistributeProperties::clone() const
+{
+  return std::make_unique<RedistributeProperties>(*this);
+}
+
+std::string
+RedistributeProperties::getInfo() const
+{
+  return "RedistributeProperties";
+}
+
+// the LHS ("this" object) in MooseApp::addRelationshipManager is the existing RelationshipManager
+// object to which we are comparing the rhs to determine whether it should get added
+bool
+RedistributeProperties::operator>=(const RelationshipManager & rhs) const
+{
+  const auto * rm = dynamic_cast<const RedistributeProperties *>(&rhs);
+
+  // All RedistributeProperties objects are effectively equivalent
+  return rm;
+}
+
+void
+RedistributeProperties::redistribute()
+{
+}
