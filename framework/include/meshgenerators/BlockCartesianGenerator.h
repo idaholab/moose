6ca5317246376dc742c162274b1974a31359068e
MengnanLi91 <118846840+MengnanLi91@users.noreply.github.com>
2023-02-20 17:36:44 -0700

    Working progress on MeshGenerator

--- framework/include/meshgenerators/BlockCartesianGenerator.h
+++ framework/include/meshgenerators/BlockCartesianGenerator.h
@@ -27,24 +27,39 @@ public:
 protected:
   /// The dimension of the mesh
   MooseEnum _dim;
-  /// Intervals in x direction
-  std::vector<Real> _dx;
-  /// Number of grids in all intervals in x direction
-  std::vector<unsigned int> _ix;
-  /// Intervals in y direction
-  std::vector<Real> _dy;
-  /// Number of grids in all intervals in y direction
-  std::vector<unsigned int> _iy;
-  /// Intervals in z direction
-  std::vector<Real> _dz;
-  /// Number of grids in all intervals in z direction
-  std::vector<unsigned int> _iz;
-  /// Block IDs
-  std::vector<unsigned int> _subdomain_id;
+
+  /// Number of elements in x, y, z direction
+  dof_id_type &_nx, &_ny, &_nz;
+
+  /// Number of cores for partitioning the graph
+  processor_id_type _num_cores_for_partition;
+
+  /// The type of element to build
+  ElemType _elem_type;
+
+  /// The amount by which to bias the cells in the x,y,z directions.
+  /// Must be in the range 0.5 <= _bias_x <= 2.0.
+  /// _bias_x < 1 implies cells are shrinking in the x-direction.
+  /// _bias_x==1 implies no bias (original mesh unchanged).
+  /// _bias_x > 1 implies cells are growing in the x-direction.
+  Real _bias_x, _bias_y, _bias_z;
+
+  /// External partitioner
+  std::string _part_package;
+
+  /// Number of cores per compute node if hierarch partitioning is used
+  processor_id_type _num_parts_per_compute_node;
+
+  /// Which method is used to partition the mesh that is not built yet
+  std::string _partition_method;
+
+  /// Number of element side neighbor layers
+  /// While most of applications in moose require one layer of side neighbors,
+  /// phase field simulation with grain tracker needs two layers. This parameter
+  /// allow us to reserve an arbitrary number of side neighbors
+  unsigned _num_side_layers;
   /// Name of the generated Cartesian mesh
   std::unique_ptr<MeshBase> * _build_mesh;
   /// Name of the base mesh
   std::unique_ptr<MeshBase> & _mesh_input;
-
-  int _nx, _ny, _nz;
 };
--- test/tests/meshgenerators/block_cartesian_generator/block_cartesian_generator.i
+++ test/tests/meshgenerators/block_cartesian_generator/block_cartesian_generator.i
@@ -8,22 +8,17 @@
     xmax = 4
     ymin = 0
     ymax = 2.2
-    nemesis = true
-    output = true
   []
   [bcg]
     type = BlockCartesianGenerator
     input = 'gmg'
     dim = 2
-    dx = '1.5 2.4 0.1'
-    dy = '1.3 0.9'
-    ix = '2 1 1'
-    iy = '2 3'
-    subdomain_id = '0 1 1 2 2 2'
+    nx = 6
+    ny = 6
   []
 []
 
 [Outputs]
-  
+
 []
 
