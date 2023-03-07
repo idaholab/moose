6ff571c4efdd1c7c8e71f21eda9965e4178481ec
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2023-03-06 20:00:23 -0700

    Make KKS action error since it s not implemented

--- modules/phase_field/src/actions/KKSAction.C
+++ modules/phase_field/src/actions/KKSAction.C
@@ -32,17 +32,5 @@ KKSAction::KKSAction(const InputParameters & params) : Action(params)
 void
 KKSAction::act()
 {
-  /*
-    InputParameters poly_params = _factory.getValidParams("BoundingBoxIC");
-    poly_params.set<VariableName>("variable") = var_name;
-    poly_params.set<Real>("x1") = _x1;
-    poly_params.set<Real>("y1") = _y1;
-    poly_params.set<Real>("z1") = _z1;
-    poly_params.set<Real>("x2") = _x2;
-    poly_params.set<Real>("y2") = _y2;
-    poly_params.set<Real>("z2") = _z2;
-
-    // Add initial condition
-    _problem->addInitialCondition("BoundingBoxIC", "InitialCondition", poly_params);
-  */
+  mooseError("Not implemented");
 }
