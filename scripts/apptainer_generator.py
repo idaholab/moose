d8e56b9d0f222a3942937f310af00e9a5eaec368
Logan Harbour <loganhharbour@gmail.com>
2023-03-30 13:39:05 -0600

    WIP squash
    refs #0

--- scripts/apptainer_generator.py
+++ scripts/apptainer_generator.py
@@ -5,8 +5,11 @@ Manipulate Apptainer/Harbor containers based on version/hashes of MOOSE reposito
 import os
 import sys
 import argparse
+import re
 import socket
 import subprocess
+import json
+import tempfile
 
 import jinja2
 
@@ -25,18 +28,18 @@ class ApptainerGenerator:
         self.meta = Versioner().meta()
         self.args = self.parse_args(list(self.meta.keys()))
 
-        library_meta = self.meta[self.args.library]['apptainer'].copy()
-        self.project = library_meta['name_base']
-        self.name = library_meta['name']
-        self.tag = library_meta['tag']
+        self.library_meta = self.meta[self.args.library]['apptainer'].copy()
+        self.project = self.library_meta['name_base']
+        self.name = self.library_meta['name']
+        self.tag = self.library_meta['tag']
 
         if hasattr(self.args, 'modify') and self.args.modify is not None:
             self.def_path = os.path.abspath(self.args.modify)
         else:
-            self.def_path = library_meta['def']
+            self.def_path = self.library_meta['def']
 
         if self.args.suffix is not None:
-            self.name = self.add_name_suffix(library_meta, self.args.suffix)
+            self.name = self.add_name_suffix(self.library_meta, self.args.suffix)
         if self.args.tag is not None:
             self.tag = self.args.tag
         if self.args.tag_prefix is not None:
@@ -119,6 +122,10 @@ class ApptainerGenerator:
         build_parser.add_argument('--sign', type=int,
                                   help='Sign the built container with the given key')
 
+        squash_parser = action_parser.add_parser('squash', parents=[parent],
+                                                 help='Squash a container (app only)')
+        add_default_args(squash_parser)
+
         push_parser = action_parser.add_parser('push', parents=[parent],
                                                 help='Push a container')
         add_default_args(push_parser, remote=True)
@@ -126,6 +133,8 @@ class ApptainerGenerator:
                                  help='An alternate tag to push to')
         push_parser.add_argument('--to-tag-prefix', type=str,
                                  help='A prefix to add to the pushed tag')
+        push_parser.add_argument('--squashed', action='store_true',
+                                 help='Push the squashed container (app only)')
 
         uri_parser = action_parser.add_parser('uri', parents=[parent],
                                               help='Get the URI to a container')
@@ -176,11 +185,10 @@ class ApptainerGenerator:
         self.print(self.add_color(' '.join(command), 'green'))
         subprocess.run(command, check=True)
 
-    def container_path(self, name: str, tag: str, image=True):
+    def container_path(self, name: str, tag: str, ext='sif'):
         """
         Gets the local path to a container or definition file
         """
-        ext = 'sif' if image else 'def'
         return os.path.join(self.dir, f'{name}_{tag}.{ext}')
 
     def oras_uri(self, project: str, name: str, tag: str):
@@ -208,18 +216,6 @@ class ApptainerGenerator:
         self.run(command)
         return file
 
-    def apptainer_push(self, project: str, name: str, from_tag: str, to_tag=None):
-        """
-        Pushes the given image via apptainer
-        """
-        if to_tag is None:
-            to_tag = from_tag
-        oras_uri = self.oras_uri(project, name, to_tag)
-        file = self.container_path(name, from_tag)
-        self.print(f'Pushing {file}')
-        command = ['apptainer', 'push', file, oras_uri]
-        self.run(command)
-
     def apptainer_sign(self, name: str, tag: str, key_id: int):
         """
         Signs the given image vit apptainer
@@ -245,16 +241,26 @@ class ApptainerGenerator:
         command += [file, def_file]
         self.run(command)
 
-    @staticmethod
-    def oras_exists(uri: str):
+    def oras_call(self, command, loud=True, check=True, cwd=os.getcwd()):
+        config_file = os.path.join(os.environ['HOME'], '.apptainer/docker-config.json')
+        command = ['oras', '--registry-config', config_file] + command
+
+        if loud:
+            stdout = sys.stdout
+            stderr = sys.stderr
+            self.print(self.add_color(' '.join(command), 'green'))
+        else:
+            stdout = subprocess.PIPE
+            stderr = subprocess.PIPE
+
+        return subprocess.run(command, stdout=stdout, stderr=stderr, check=check, cwd=cwd)
+
+    def oras_exists(self, uri: str):
         """
         Checks whether or not the given image exists via ORAS
         """
-        config_file = os.path.join(os.environ['HOME'], '.apptainer/docker-config.json')
-        uri = uri.replace('oras://', '')
-        command = ['oras', 'manifest', 'fetch', '--registry-config', config_file, uri]
-        process = subprocess.run(command, stdout=subprocess.PIPE,
-                                 stderr=subprocess.PIPE, check=False)
+        command = ['manifest', 'fetch', uri.replace('oras://', '')]
+        process = self.oras_call(command, loud=False, check=False)
         if process.returncode == 0:
             return True
 
@@ -263,6 +269,31 @@ class ApptainerGenerator:
 
         print(process.stderr.decode("utf-8"), file=sys.stderr)
         raise Exception('Failed to check ORAS image existance')
+
+    def oras_push(self, project: str, name: str, tag: str, files: list, extra_annotations={}):
+        """
+        Pushes the given files via oras
+        """
+        oras_uri = self.oras_uri(project, name, tag).replace('oras://', '')
+        oras_command = ['push', oras_uri]
+
+        annotations = {}
+        for file in files:
+            if not file.startswith(self.dir):
+                self.error(f'File {file} does not start with generation dir {self.dir}')
+            filetype_suffix = ''
+            if file.endswith('.sif'):
+                annotations = self.build_oras_annotations(file, extra_annotations=extra_annotations)
+                filetype_suffix = ':application/vnd.sylabs.sif.layer.v1.sif'
+            oras_command.append(os.path.basename(file) + filetype_suffix)
+
+        with tempfile.NamedTemporaryFile(mode='w') as annotation_tf:
+            if annotations is not None:
+                with annotation_tf.file as tf:
+                    tf.write(json.dumps(annotations))
+                oras_command.extend(['--annotation-file', annotation_tf.name])
+
+            self.oras_call(oras_command, cwd=self.dir)
 
     @staticmethod
     def add_name_suffix(meta, suffix):
@@ -332,21 +363,46 @@ class ApptainerGenerator:
         self.print(f'Using remote dependency for {name} from {uri}')
         return 'oras', uri.replace('oras://', '')
 
+    @staticmethod
+    def submodule_sha(cwd, name):
+        """
+        Get the SHA of the given submodule
+        """
+        cmd = ['git', 'ls-tree', '--object-only', 'HEAD', name]
+        return subprocess.check_output(cmd, cwd=cwd, encoding='utf-8').rstrip()
+
     def _add_definition_labels(self, definition):
         """
         Adds common labels to the given definition content
         """
+        # Get the moose sha
+        moose_sha_command = ['git', 'rev-parse', 'HEAD']
+        moose_sha = subprocess.check_output(moose_sha_command, encoding='utf-8', cwd=MOOSE_DIR).rstrip()
+
         definition += '\n\n%labels\n'
-        name = self.name
-        if hasattr(self.args, 'modify') and self.args.modify is not None:
-            name += '.modified'
-        definition += f'    {name}.buildhost {socket.gethostname()}\n'
-        definition += f'    {name}.version {self.tag}\n'
+        def add_label(definition, key, value):
+            name = 'moose.' + self.project
+            if hasattr(self.args, 'modify') and self.args.modify is not None:
+                name += '.modified'
+            definition += f'    {name}.{key} {value}\n'
+
+        add_label(definition, 'build.host', socket.gethostname())
+        add_label(definition, 'build.user', os.getenv('USER'))
+        add_label(definition, 'moose.gitsha', moose_sha)
+        add_label(definition, 'version', self.library_meta['tag'])
+
         # If we have CIVET info, add the url
         if 'CIVET_SERVER' in os.environ and 'CIVET_JOB_ID' in os.environ:
             civet_server = os.environ.get('CIVET_SERVER')
             civet_job_id = os.environ.get('CIVET_JOB_ID')
-            definition += f'    {name}.job {civet_server}/job/{civet_job_id}\n'
+            add_label(definition, 'build.civetjob', f'{civet_server}/job/{civet_job_id}')
+
+        # For libmesh and petsc, also add the commit
+        if self.args.library in ['petsc', 'libmesh']:
+            add_label(definition,
+                      f'{self.args.library}.gitsha',
+                      self.submodule_sha(MOOSE_DIR, self.args.library))
+
         return definition
 
     @staticmethod
@@ -446,6 +502,28 @@ class ApptainerGenerator:
                 jinja_var = f'vtk_{var}'
                 jinja_data[jinja_var] = meta['source'][var]
 
+    @staticmethod
+    def get_sif_labels(sif_path):
+        inspect_cmd = ['apptainer', 'inspect', '-j', sif_path]
+        inspection = json.loads(subprocess.check_output(inspect_cmd).decode(sys.stdout.encoding))
+
+        if 'labels' in inspection['data']['attributes']:
+            return inspection['data']['attributes']['labels']
+        return {}
+
+    @staticmethod
+    def build_oras_annotations(container_path, extra_annotations={}):
+        inspect_cmd = ['apptainer', 'inspect', '-j', container_path]
+        inspection = json.loads(subprocess.check_output(inspect_cmd).decode(sys.stdout.encoding))
+
+        annotations = {'$manifest': {}}
+        if 'labels' in inspection['data']['attributes']:
+            labels = ApptainerGenerator.get_sif_labels(container_path)
+            for key, value in labels.items():
+                if key not in ['Authors:', 'Version:']:
+                    annotations['$manifest'][key] = value
+        annotations['$manifest'].update(extra_annotations)
+        return annotations
 
     def _action_exists(self):
         """
@@ -524,7 +602,7 @@ class ApptainerGenerator:
         new_definition = self._add_definition_labels(new_definition)
 
         # Definition file checks
-        container_definition_path = self.container_path(self.name, self.tag, image=False)
+        container_definition_path = self.container_path(self.name, self.tag, ext='def')
         self.print(f'Writing definition to {container_definition_path}')
         if os.path.exists(container_definition_path):
             if self.args.overwrite:
@@ -557,12 +635,38 @@ class ApptainerGenerator:
         args = []
         if self.args.build_args is not None:
             args = self.args.build_args.split(' ')
-        container_definition_path = self.container_path(self.name, self.tag, image=False)
+        container_definition_path = self.container_path(self.name, self.tag, ext='def')
         self.apptainer_build(container_definition_path, self.name, self.tag, args=args)
 
         # Sign if requested
         if self.args.sign is not None:
             self.apptainer_sign(self.name, self.tag, self.args.sign)
+
+    def _action_squash(self):
+        """
+        Performs the "squash" action
+        """
+        if self.args.library != 'app':
+            self.error('Can only squash applications')
+
+        container_path = self.container_path(self.name, self.tag)
+        if not os.path.exists(container_path):
+            self.error(f'Container {container_path} does not exist')
+        self.print(f'Squashing container {container_path}')
+
+        squashfs_path = self.container_path(self.name, self.tag, ext='squashfs')
+        if os.path.exists(squashfs_path):
+            if self.args.overwrite:
+                self.warn(f'Overwriting {squashfs_path}')
+                os.remove(squashfs_path)
+            else:
+                self.error(f'Squashed filesystem {squashfs_path} already exists')
+
+        bind_mount = os.path.dirname(squashfs_path) + ':' + os.path.dirname(squashfs_path)
+        command = ['apptainer', 'exec', '-B', bind_mount, container_path, '/opt/moose-apptainer/squash.sh', squashfs_path]
+        self.run(command)
+
+        self.print(f'Container was squashed to {squashfs_path}')
 
     def _action_push(self):
         """
@@ -577,14 +681,67 @@ class ApptainerGenerator:
         if not os.path.exists(container_path):
             self.error(f'Container {container_path} does not exist')
 
-        uri = self.oras_uri(self.project, self.name, to_tag)
-        if self.oras_exists(uri):
-            if self.args.overwrite:
-                self.warn(f'Overwriting {uri}')
-            else:
-                self.error(f'Tag {uri} already exists')
+        if self.args.squashed:
+            squashfs_path = self.container_path(self.name, from_tag, ext='squashfs')
+            if not os.path.exists(squashfs_path):
+                self.error(f'To-be squashed container {squashfs_path} does not exist')
 
-        self.apptainer_push(self.project, self.name, from_tag, to_tag)
+            # Load the def file from the container
+            dep_def_cmd = ['apptainer', 'inspect', '-d', container_path]
+            dep_def = subprocess.check_output(dep_def_cmd).decode(sys.stdout.encoding)
+
+            # Find the container that this was based on
+            dep_from_re = re.search(r'From: +(([a-zA-Z0-9_.]+)\/([a-zA-Z0-9_.-]+)\/([a-zA-Z0-9_.-]+)\:([a-zA-Z0-9_.-]+))', dep_def)
+            if not dep_from_re:
+                self.error(f'Failed to parse from-container from def for {container_path}')
+            dep_uri = 'oras://' + dep_from_re.group(1)
+            dep_host = dep_from_re.group(2)
+            dep_project = dep_from_re.group(3)
+            dep_name = dep_from_re.group(4)
+            dep_tag = dep_from_re.group(5)
+
+            # Get the container that this was based on
+            self.print(f'Fetching dependent container {dep_name}:{dep_tag}')
+            from_container_path = self.container_path(dep_name, dep_tag)
+            if os.path.exists(from_container_path):
+                if self.args.overwrite:
+                    self.warn(f'Overwriting {from_container_path}')
+                    os.remove(from_container_path)
+                else:
+                    self.error(f'From container {from_container_path} already exists')
+            self.apptainer_pull(dep_project, dep_name, dep_tag)
+
+            # Add "-squashed" to the end of the name
+            name_arch = self.name.split('-')[-1]
+            to_name = self.name.replace(name_arch, 'squashed-' + name_arch)
+
+            # Make sure it doesn't already exist
+            oras_uri = self.oras_uri(self.project, to_name, to_tag)
+            if self.oras_exists(oras_uri):
+                if self.args.overwrite:
+                    self.warn(f'Overwriting {oras_uri}')
+                else:
+                    self.error(f'Tag {oras_uri} already exists')
+
+            # Fill the extra annotations (the labels from the app container)
+            extra_annotations = {}
+            app_labels = self.get_sif_labels(container_path)
+            for key, value in app_labels.items():
+                if key.startswith(self.library_meta['name_base']):
+                    extra_annotations[key] = value
+
+            self.print(f'Pushing squashed container {to_name}:{to_tag}')
+            self.oras_push(self.project, to_name, to_tag, [squashfs_path, from_container_path],
+                           extra_annotations=extra_annotations)
+        else:
+            uri = self.oras_uri(self.project, self.name, to_tag)
+            if self.oras_exists(uri):
+                if self.args.overwrite:
+                    self.warn(f'Overwriting {uri}')
+                else:
+                    self.error(f'Tag {uri} already exists')
+
+            self.oras_push(self.project, self.name, to_tag, [container_path])
 
     def _action_path(self):
         """
