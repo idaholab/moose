9af2010eb284b85881d2f565ef1675a3295c06f3
Logan Harbour <loganhharbour@gmail.com>
2023-04-05 09:07:04 -0600

    Make std::string look normal again

--- framework/src/utils/MooseUtils.C
+++ framework/src/utils/MooseUtils.C
@@ -1202,7 +1202,7 @@ prettyCppType(const std::string & cpp_type)
   std::string s = cpp_type;
   pcrecpp::RE("std::__\\w+::").GlobalReplace("std::", &s);
   // It would be nice if std::string actually looked normal
-  pcrecpp::RE("\\s*std::basic_string<char, std::char_traits<char>, std::allocator<char> >\\s*")
+  pcrecpp::RE("\\s*std::basic_string<char, std::char_traits<char>, std::allocator<char>>\\s*")
       .GlobalReplace("std::string", &s);
   // It would be nice if std::vector looked normal
   pcrecpp::RE r("std::vector<([[:print:]]+),\\s?std::allocator<\\s?\\1\\s?>\\s?>");
