1faa8e836a148c9e24c59ba6d2d9d01eaa0d86b3
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2023-04-11 19:35:44 -0600

    Add base Position class - initial position - multi D options w/ unrolling utility

---
+++ framework/include/positions/Position.h
@@ -0,0 +1,72 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+// Moose includes
+#include "GeneralReporter.h"
+
+/**
+ * Positions objects are under the hood Reporters.
+ * But they are limited in to vectors or multi-dimensional vectors of points.
+ */
+class Positions : public GeneralReporter
+{
+public:
+  static InputParameters validParams();
+  Positions(const InputParameters & parameters);
+  virtual ~Positions() = default;
+
+  ///{
+  /// Getters for the positions vector for the desired dimension
+  /// 1D will be the only one guaranteed to succeed
+  const std::vector<Point> & getPositions(bool initial = false) const;
+  const std::vector<std::vector<Point>> & getPositionsVector2D() const;
+  const std::vector<std::vector<std::vector<Point>>> & getPositionsVector3D() const;
+  const std::vector<std::vector<std::vector<std::vector<Point>>>> & getPositionsVector4D() const;
+  ///}
+
+protected:
+  /// In charge of computing / loading the positions.
+  virtual void initialize() override = 0;
+  void execute() override {}
+  void finalize() override {}
+
+  /// By default, Positions will call initial setup on mesh changed
+  void meshChanged() override { initialSetup(); };
+
+  /// By default, Positions will not be modified very regularly
+  void timestepSetup() override {}
+  void residualSetup() override {}
+  void jacobianSetup() override {}
+
+  /// Unrolls the multi-dimensional position vectors
+  void unrollMultiDPositions();
+
+  /// For initialization of the positions, another position reporter may be used
+  const std::vector<Point> * const _initial_positions;
+
+  /// For now, only the 1D vector will be shared across all ranks. All the others only exist
+  /// locally.
+  /// The 4 dimensions could be used for xyzt, but could also be axial/assembly/pin/ring
+  /// 1D storage for all the positions
+  std::vector<Point> & _positions;
+
+  /// 2D storage for all the positions
+  std::vector<std::vector<Point>> _positions_2d;
+
+  /// 3D storage for all the positions
+  std::vector<std::vector<std::vector<Point>>> _positions_3d;
+
+  /// 4D storage for all the positions : space & time
+  std::vector<std::vector<std::vector<std::vector<Point>>>> _positions_4d;
+};
+
+void dataStore(std::ostream & stream, const std::vector<Point> & positions, void * context);
+void dataLoad(std::istream & stream, const std::vector<Point> & positions, void * context);
---
+++ framework/src/positions/Position.C
@@ -0,0 +1,127 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "Positions.h"
+
+InputParameters
+Positions::validParams()
+{
+  InputParameters params = GeneralReporter::validParams();
+
+  params.addParam<ReporterName>("initial_positions",
+                                "Positions at the beginning of the simulation");
+
+  params.registerBase("Positions");
+  return params;
+}
+
+Positions::Positions(const InputParameters & parameters)
+  : GeneralReporter(parameters),
+    _initial_positions(isParamValid("initial_positions")
+                           ? &getReporterValue<std::vector<Point>>("initialPositions")
+                           : nullptr),
+    _positions(declareValueByName<std::vector<Point>, ReporterVectorContext<Point>>(
+        "positions_1d", REPORTER_MODE_REPLICATED))
+{
+}
+
+const std::vector<Point> &
+Positions::getPositions(bool initial) const
+{
+  if (initial)
+    return *_initial_positions;
+  if (_positions.size())
+    return _positions;
+  else
+    mooseError("Positions vector has not been initialized.");
+};
+
+const std::vector<std::vector<Point>> &
+Positions::getPositionsVector2D() const
+{
+  if (_positions_2d.size())
+    return _positions_2d;
+  else
+    mooseError("2D positions vectors have not been initialized.");
+};
+
+const std::vector<std::vector<std::vector<Point>>> &
+Positions::getPositionsVector3D() const
+{
+  if (_positions_3d.size())
+    return _positions_3d;
+  else
+    mooseError("3D positions vectors have not been initialized.");
+};
+
+const std::vector<std::vector<std::vector<std::vector<Point>>>> &
+Positions::getPositionsVector4D() const
+{
+  if (_positions_4d.size())
+    return _positions_4d;
+  else
+    mooseError("4D positions vectors have not been initialized.");
+};
+
+void
+Positions::unrollMultiDPositions()
+{
+  // Unroll in every dimension available
+  std::vector<Point> temp_2d_unrolled;
+  std::vector<Point> temp_3d_unrolled;
+  std::vector<Point> temp_4d_unrolled;
+  for (auto vec : _positions_2d)
+    temp_2d_unrolled.insert(temp_2d_unrolled.end(), vec.begin(), vec.end());
+  for (auto vec_vec : _positions_3d)
+    for (auto vec : vec_vec)
+      temp_3d_unrolled.insert(temp_3d_unrolled.end(), vec.begin(), vec.end());
+  for (auto vec_vec_vec : _positions_4d)
+    for (auto vec_vec : vec_vec_vec)
+      for (auto vec : vec_vec)
+        temp_4d_unrolled.insert(temp_4d_unrolled.end(), vec.begin(), vec.end());
+
+  // for now we wont even tolerate a different ordering
+  // 2D & 1D: check for conflicts
+  if (temp_2d_unrolled.size() && _positions.size() && temp_2d_unrolled != _positions)
+    mooseError("Inconsistency between the 2D and 1D position vectors detected. "
+               "The 2D positions must unroll into the 1D positions");
+
+  // 3D vs 2D & 1D
+  if (temp_3d_unrolled.size() && temp_2d_unrolled.size() && temp_3d_unrolled != temp_3d_unrolled)
+    mooseError("Inconsistency between the 3D and 2D position vectors detected. "
+               "The 3D positions must unroll the same way as the 2D positions");
+  if (temp_3d_unrolled.size() && _positions.size() && temp_3d_unrolled != _positions)
+    mooseError("Inconsistency between the 3D and 1D position vectors detected. "
+               "The 3D positions must unroll into the 1D positions");
+
+  // 4D vs all lower dimensions
+  if (temp_4d_unrolled.size() && temp_3d_unrolled.size() && temp_4d_unrolled != temp_3d_unrolled)
+    mooseError("Inconsistency between the 4D and 3D position vectors detected. "
+               "The 4D positions must unroll the same way as the 3D positions");
+  if (temp_4d_unrolled.size() && temp_2d_unrolled.size() && temp_4d_unrolled != temp_2d_unrolled)
+    mooseError("Inconsistency between the 4D and 2D position vectors detected. "
+               "The 4D positions must unroll the same way as the 2D positions");
+  if (temp_4d_unrolled.size() && _positions.size() && temp_4d_unrolled != _positions)
+    mooseError("Inconsistency between the 4D and 1D position vectors detected. "
+               "The 4D positions must unroll into the 1D positions");
+
+  // Use any of the higher D to set the one D vector
+  if (!_positions.size())
+  {
+    if (temp_2d_unrolled.size())
+      _positions = temp_2d_unrolled;
+    else if (temp_3d_unrolled.size())
+      _positions = temp_3d_unrolled;
+    else if (temp_4d_unrolled.size())
+      _positions = temp_4d_unrolled;
+    else
+      mooseError("Positions::unrollMultiDPositions() may only be called if there is at least one "
+                 "non-empty positions vector.");
+  }
+}
