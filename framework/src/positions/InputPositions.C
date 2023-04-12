c1a36a2eccdf622adb9ce7047e6aeafda258e16d
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2023-04-12 10:01:12 -0600

    Add positions from input parameter

---
+++ framework/src/positions/InputPositions.C
@@ -0,0 +1,25 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "InputPositions.h"
+
+registerMooseObject("MooseApp", InputPositions);
+
+InputParameters
+InputPositions::validParams()
+{
+  InputParameters params = Positions::validParams();
+  params.addRequiredParam<std::vector<Point>>("positions", "Positions");
+  return params;
+}
+
+InputPositions::InputPositions(const InputParameters & parameters) : Positions(parameters)
+{
+  _positions = getParam<std::vector<Point>>("positions");
+}
