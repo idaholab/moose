3975777076df69906e7dceff6ba409788a38aaa9
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2023-04-24 15:03:45 -0600

    Use nearest positions for user object general field transfer refs #23585

--- framework/src/transfers/MultiAppGeneralFieldUserObjectTransfer.C
+++ framework/src/transfers/MultiAppGeneralFieldUserObjectTransfer.C
@@ -16,6 +16,7 @@
 #include "MooseTypes.h"
 #include "MooseVariableFE.h"
 #include "UserObject.h"
+#include "Positions.h"
 
 #include "libmesh/system.h"
 #include "libmesh/mesh_function.h"
@@ -84,7 +85,7 @@ MultiAppGeneralFieldUserObjectTransfer::evaluateInterpValuesWithUserObjects(
   for (auto & pt : incoming_points)
   {
     bool point_found = false;
-    if (_use_nearest_app)
+    if (_nearest_positions_obj)
       outgoing_vals[i_pt].second = GeneralFieldTransfer::BetterOutOfMeshValue;
 
     // Loop on all local origin problems until:
@@ -92,7 +93,7 @@ MultiAppGeneralFieldUserObjectTransfer::evaluateInterpValuesWithUserObjects(
     // - or if looking for conflicts between apps, we must check them all
     for (MooseIndex(_from_problems.size()) i_from = 0;
          i_from < _from_problems.size() &&
-         (!point_found || _search_value_conflicts || _use_nearest_app);
+         (!point_found || _search_value_conflicts || _nearest_positions_obj);
          ++i_from)
     {
       // Check spatial restrictions
@@ -107,13 +108,27 @@ MultiAppGeneralFieldUserObjectTransfer::evaluateInterpValuesWithUserObjects(
         // Use spatial value routine to compute the origin value to transfer
         auto val = user_object.spatialValue(pt - _from_positions[i_from]);
 
+        // Get nearest position (often a subapp position) for the target point
+        Point nearest_position_source;
+        if (_nearest_positions_obj)
+        {
+          Point nearest_position = _nearest_positions_obj->getNearestPosition(pt);
+          nearest_position_source =
+              _nearest_positions_obj->getNearestPosition(_from_positions[i_from]);
+
+          // Source (usually app position) is not closest to the same positions as the target, dont
+          // send values
+          if (nearest_position != nearest_position_source)
+            continue;
+        }
+
         // Look for overlaps. The check is not active outside of overlap search because in that
         // case we accept the first value from the lowest ranked process
         // NOTE: There is no guarantee this will be the final value used among all problems
         //       but we register an overlap as soon as two values are possible from this rank
         if (detectConflict(val,
                            outgoing_vals[i_pt].first,
-                           _use_nearest_app ? (pt - _from_positions[i_from]).norm() : 1,
+                           _nearest_positions_obj ? (pt - nearest_position_source).norm() : 1,
                            outgoing_vals[i_pt].second))
           registerConflict(i_from, 0, pt - _from_positions[i_from], 1, true);
 
@@ -124,15 +139,15 @@ MultiAppGeneralFieldUserObjectTransfer::evaluateInterpValuesWithUserObjects(
           point_found = true;
 
         // Assign value
-        if (!_use_nearest_app)
+        if (!_nearest_positions_obj)
         {
           outgoing_vals[i_pt].first = val;
           outgoing_vals[i_pt].second = 1;
         }
-        else if ((pt - _from_positions[i_from]).norm() < outgoing_vals[i_pt].second)
+        else if ((pt - nearest_position_source).norm() < outgoing_vals[i_pt].second)
         {
           outgoing_vals[i_pt].first = val;
-          outgoing_vals[i_pt].second = (pt - _from_positions[i_from]).norm();
+          outgoing_vals[i_pt].second = (pt - nearest_position_source).norm();
         }
       }
     }
