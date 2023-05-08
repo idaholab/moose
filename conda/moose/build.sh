e54b9a0865aced4157df4bdc5ce08f80c99197bd
Jason M Miller <Jason.Miller@inl.gov>
2023-05-08 14:56:45 -0600

    Hack to get moose, moose-opt --copy-inputs working

--- conda/moose/build.sh
+++ conda/moose/build.sh
@@ -14,6 +14,14 @@ cd ${PREFIX}/moose/bin
 ln -s combined-opt moose-opt
 ln -s combined-opt moose
 
+# Fix (hack) for moose -> moose symlink collision binary/copy inputs
+cd ${PREFIX}/share/moose
+for sdir in `ls ../combined`; do
+    if [ -d ../combined/$sdir ] && [ ! -d $sdir ] && [ ! -f $sdir ] && [ ! -L $sdir ]; then
+        ln -s ../combined/$sdir .
+    fi
+done
+
 mkdir -p "${PREFIX}/etc/conda/activate.d" "${PREFIX}/etc/conda/deactivate.d"
 cat <<EOF > "${PREFIX}/etc/conda/activate.d/activate_${PKG_NAME}.sh"
 export PATH=\${PATH}:${PREFIX}/moose/bin
