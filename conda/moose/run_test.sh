0598582cb448a837dc9a129a90ea56a52adcff4e
Jason Miller <jason.miller@inl.gov>
2023-05-08 13:37:50 -0600

    Do not test Intel Mac

--- conda/moose/run_test.sh
+++ conda/moose/run_test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# If Intel Mac, forget about it. The environment doesn't seem fit to run
+if [[ $(uname) == 'Darwin' ]] && [[ $(uname -m) == 'x86_64' ]]; then
+    exit 0
+fi
+
 # A list of module(s)/directories that we do not want to include for testing
 NOT_RUNNABLE=(doc module_load combined geochemistry misc navier_stokes tensor_mechanics framework external_petsc_solver)
 
