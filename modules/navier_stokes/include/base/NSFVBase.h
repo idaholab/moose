bc949d90f9adf2a335c85bb3b32ed3b9b56877fb
Sebastian Schunert <sebastian.schunert@inl.gov>
2023-06-13 12:45:06 -0600

    Add public interface functions  (#24674)

--- modules/navier_stokes/include/base/NSFVBase.h
+++ modules/navier_stokes/include/base/NSFVBase.h
@@ -26,6 +26,34 @@ public:
 
   NSFVBase(const InputParameters & parameters);
 
+  ///@{ general public interface functions
+  bool hasEnergyEquation() const { return _has_energy_equation; }
+  bool hasScalar() const { return _has_scalar_equation; }
+  unsigned int dim() const { return _dim; }
+  std::string rhieChowName() const
+  {
+    if (_porous_medium_treatment)
+      return "pins_rhie_chow_interpolator";
+    return "ins_rhie_chow_interpolator";
+  }
+  ///@}
+
+  ///@{ public interface for variable and property names
+  std::string pressureName() const { return _pressure_name; }
+  std::string fluidTemperatureName() const { return _fluid_temperature_name; }
+  std::string velocityName(unsigned int j) const;
+  std::vector<std::string> velocityNames() const { return _velocity_name; }
+  std::vector<NonlinearVariableName> passiveScalarNames() const { return _passive_scalar_names; }
+  MooseFunctorName densityName() const { return _density_name; }
+  ///@}
+
+  ///@{ public interface for boundaries
+  bool isInletBoundary(const BoundaryName & boundary_name) const;
+  bool isOutletBoundary(const BoundaryName & boundary_name) const;
+  const std::vector<BoundaryName> & inletBoundaries() const { return _inlet_boundaries; }
+  const std::vector<BoundaryName> & outletBoundaries() const { return _outlet_boundaries; }
+  ///@}
+
 protected:
   /// Type that we use in Actions for declaring coupling between the solutions
   /// of different physics components
@@ -3507,4 +3535,29 @@ NSFVBase<BaseType>::checkRhieChowFunctorsDefined()
   if (_dim == 3 && !getProblem().hasFunctor("az", /*thread_id=*/0))
     paramError("add_flow_equations",
                "Rhie Chow coefficient az must be provided for advection by auxiliary velocities");
+}
+
+template <class BaseType>
+std::string
+NSFVBase<BaseType>::velocityName(unsigned int j) const
+{
+  if (j >= _velocity_name.size())
+    mooseError("Argument j = ", j, " out of bounds");
+  return _velocity_name[j];
+}
+
+template <class BaseType>
+bool
+NSFVBase<BaseType>::isInletBoundary(const BoundaryName & boundary_name) const
+{
+  return std::find(_inlet_boundaries.begin(), _inlet_boundaries.end(), boundary_name) !=
+         _inlet_boundaries.end();
+}
+
+template <class BaseType>
+bool
+NSFVBase<BaseType>::isOutletBoundary(const BoundaryName & boundary_name) const
+{
+  return std::find(_outlet_boundaries.begin(), _outlet_boundaries.end(), boundary_name) !=
+         _outlet_boundaries.end();
 }
