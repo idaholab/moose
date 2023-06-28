cd86080543dadc2778746b7edd5aa470ec3418fb
Peter German <grmnptr@gmail.com>
2023-06-28 17:03:19 -0600

    Add initial implementation. (#24856)

---
+++ modules/navier_stokes/include/materials/PINSFVFrictionMaterial.h
@@ -0,0 +1,33 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "FunctorMaterial.h"
+
+class PINSFVFrictionMaterial : public FunctorMaterial
+{
+public:
+  static InputParameters validParams();
+
+  PINSFVFrictionMaterial(const InputParameters & parameters);
+
+protected:
+  const Moose::Functor<ADReal> & _u;
+  const Moose::Functor<ADReal> * _v;
+  const Moose::Functor<ADReal> * _w;
+  const Moose::Functor<ADReal> & _mu;
+  const Moose::Functor<ADReal> & _rho;
+  const Moose::Functor<ADReal> & _characteristic_length;
+  const Moose::Functor<ADReal> & _c1;
+  const Moose::Functor<ADReal> & _c2;
+  const Moose::Functor<ADReal> & _porosity;
+
+  const std::string _friction_factor_name;
+};
