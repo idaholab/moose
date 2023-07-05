db0f16dc3221567ef8d8cbc679c8b751c1438c2d
Elley-Folks <132602435+Elley-Folks@users.noreply.github.com>
2023-07-05 15:09:24 -0600

    WIP Minimum tester set up for find_hung_process.py
    They very very bare minimum for the find_hung_process.py tester.
    
    Needed to add a static method, and a "tests" input file for run_tests to work.
    
    Needed to add a simple_diffusion.i file as well for the tester to run the test.
    
    Also added a temp directory called "tester" for the "tests" file. It is in test/tests/tester/tests

--- python/TestHarness/testers/FindHungProcessTester.py
+++ python/TestHarness/testers/FindHungProcessTester.py
@@ -1,4 +1,3 @@
-import RunApp
 from RunApp import RunApp
 import os, sys, subprocess
 
@@ -6,16 +5,25 @@ class FindHungProcessTester(RunApp):
 
     print('Hello in FindHungProcessTester class')
 
-    def __init__(self):
-        self.cli_command_options =""
 
-    def command(self):
-        cmd = self.getCommand(self.cli_command_options).split(" ")
-        return cmd
+    # def command(self):
+    #     cmd = self.getCommand(self.cli_command_options).split(" ")
+    #     return cmd
+
+    @staticmethod
+    def validParams():
+        params = RunApp.validParams()
+
+        return params
+
+    def __init__(self, name, params):
+        RunApp.__init__(self, name, params)
 
 
-def main():
-   test = FindHungProcessTester
+# def main():
+#    test = FindHungProcessTester
 
-   cmd = test.getCommand("echo test")
-   print(cmd)
+#    cmd = test.getCommand("echo test")
+#    print(cmd)
+
+
