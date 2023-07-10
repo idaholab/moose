38311f534a5a0abaf9744faebe1fa65bcde5fc38
Elley-Folks <132602435+Elley-Folks@users.noreply.github.com>
2023-07-10 08:37:30 -0600

    Updated FindHungProcessTester.py to work on linux
    Test currently does run on linux. Need to now add pattern recognition of output to verify that find_hung_process.py is working correctly.
    
    As a note, sleep for 5 seconds to make sure that program is hung in a state that is expected. In the first few seconds bad_mpi.cpp does not have the expected output because of timers. There could be improvements later to make the program hang quicker, and stay in the hung state that we're looking for.
    
    It may not be ideal to use pkill, but it does work for now since the name of the binary shouldn't change in the tester.

--- python/TestHarness/testers/FindHungProcessTester.py
+++ python/TestHarness/testers/FindHungProcessTester.py
@@ -10,10 +10,9 @@ class FindHungProcessTester(RunApp):
 
     def getCommand(self, options):
         # Create the command line string to run
-        return "echo this is working right now; mpiexec -n 4 ./bad_mpi -s 2; python ../../../framework/scripts/find_hung_process.py -v test bad_mpi --test-local -qp"
+
+        return "mpiexec -n 4 ./bad_mpi -s 2 & sleep 5; python ../../../framework/scripts/find_hung_process.py -v test bad_mpi --test-local -qp && pkill -9 bad_mpi"
         #return "echo is this; sleep 1; echo working?"
 
     def __init__(self, name, params):
         RunApp.__init__(self, name, params)
-
-
