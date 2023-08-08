921aa1249a68339a6aff569e8249073bad4d89c5
Joshua E. Hansel <joshua.hansel@inl.gov>
2023-08-08 08:09:28 -0500

    Added FunctorNeumannBC
    Closes #25132

---
+++ framework/include/bcs/FunctorNeumannBC.h
@@ -0,0 +1,35 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "ADIntegratedBC.h"
+
+/**
+ * Neumann boundary condition with functor inputs.
+ */
+template <bool is_ad>
+class FunctorNeumannBCTempl : public ADIntegratedBC
+{
+public:
+  static InputParameters validParams();
+
+  FunctorNeumannBCTempl(const InputParameters & parameters);
+
+protected:
+  virtual ADReal computeQpResidual() override;
+
+  /// The functor to impose
+  const Moose::Functor<GenericReal<is_ad>> & _functor;
+  /// Coefficient
+  const Moose::Functor<GenericReal<is_ad>> & _coef;
+};
+
+typedef FunctorNeumannBCTempl<false> FunctorNeumannBC;
+typedef FunctorNeumannBCTempl<true> ADFunctorNeumannBC;
---
+++ framework/src/bcs/FunctorNeumannBC.C
@@ -0,0 +1,51 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "FunctorNeumannBC.h"
+
+registerMooseObject("MooseApp", FunctorNeumannBC);
+registerMooseObject("MooseApp", ADFunctorNeumannBC);
+
+template <bool is_ad>
+InputParameters
+FunctorNeumannBCTempl<is_ad>::validParams()
+{
+  InputParameters params = ADIntegratedBC::validParams();
+
+  params.addClassDescription("Imposes the integrated boundary condition "
+                             "$\\frac{\\partial u}{\\partial n}=h(t,\\vec{x})$, "
+                             "where $h$ is a functor.");
+
+  params.addRequiredParam<MooseFunctorName>("functor", "The functor to impose");
+  params.addParam<MooseFunctorName>(
+      "coefficient", 1.0, "An optional functor coefficient to multiply the imposed functor");
+
+  return params;
+}
+
+template <bool is_ad>
+FunctorNeumannBCTempl<is_ad>::FunctorNeumannBCTempl(const InputParameters & parameters)
+  : ADIntegratedBC(parameters),
+
+    _functor(getFunctor<GenericReal<is_ad>>("functor")),
+    _coef(getFunctor<GenericReal<is_ad>>("coefficient"))
+{
+}
+
+template <bool is_ad>
+ADReal
+FunctorNeumannBCTempl<is_ad>::computeQpResidual()
+{
+  const auto space_arg = std::make_tuple(_current_elem, _current_side, _qp, _qrule);
+  return -_coef(space_arg, Moose::currentState()) * _functor(space_arg, Moose::currentState()) *
+         _test[_i][_qp];
+}
+
+template class FunctorNeumannBCTempl<false>;
+template class FunctorNeumannBCTempl<true>;
