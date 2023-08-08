9274fec7b76543ef8b777ddb36abeefcca14bd35
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2023-08-08 11:49:42 -0600

    Avoid false positives in detection of nearest position refs #23585

--- framework/src/positions/Positions.C
+++ framework/src/positions/Positions.C
@@ -77,6 +77,7 @@ const Point &
 Positions::getNearestPosition(const Point & target, const bool initial) const
 {
   const auto & positions = (initial && _initial_positions) ? *_initial_positions : _positions;
+  std::pair<int, int> conflict_index(-1, -1);
 
   // TODO Use faster & fancier machinery such as a KNN-partition
   std::size_t nearest_index = 0;
@@ -92,10 +93,16 @@ Positions::getNearestPosition(const Point & target, const bool initial) const
     // Check that no two positions are equidistant to the target
     else if (MooseUtils::absoluteFuzzyEqual((_positions[i] - target).norm_sq(),
                                             nearest_distance_sq))
-      mooseWarning(
-          "Search for nearest position found several matches: " + Moose::stringify(_positions[i]) +
-              " and " + Moose::stringify(_positions[nearest_index]),
-          " at a distance of " + std::to_string(std::sqrt(nearest_distance_sq)));
+      conflict_index = std::make_pair(int(i), int(nearest_index));
+  }
+
+  if (nearest_index == conflict_index.first || nearest_index == conflict_index.second)
+  {
+    mooseWarning("Search for nearest position found several matches: " +
+                     Moose::stringify(_positions[conflict_index.first]) + " and " +
+                     Moose::stringify(_positions[nearest_index]),
+                 " for point " + Moose::stringify(target) + " at a distance of " +
+                     std::to_string(std::sqrt(nearest_distance_sq)));
   }
 
   return positions[nearest_index];
