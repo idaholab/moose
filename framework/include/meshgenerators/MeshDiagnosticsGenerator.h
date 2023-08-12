98eee4c4508cc9f24ac2bd098015574d6eb1cff6
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2023-08-12 12:36:23 -0600

    Const-ness of mesh diagnostics generator atrributes

--- framework/include/meshgenerators/MeshDiagnosticsGenerator.h
+++ framework/include/meshgenerators/MeshDiagnosticsGenerator.h
@@ -13,7 +13,7 @@
 #include "MooseEnum.h"
 
 /*
- *
+ * Mesh 'generator' to diagnose potentially unsupported features or miscellaneous issues
  */
 class MeshDiagnosticsGenerator : public MeshGenerator
 {
@@ -30,14 +30,14 @@ protected:
 
   /// whether to check element volumes
   const MooseEnum _check_element_volumes;
-  /// counting number of small elements
+  /// counter for the number of small elements
   unsigned int _num_tiny_elems;
-  /// counting number of big elements
+  /// counter for the number of big elements
   unsigned int _num_big_elems;
   /// minimum size for element volume to be counted as a tiny element
-  Real _min_volume;
+  const Real _min_volume;
   /// maximum size for element volume to be counted as a big element
-  Real _max_volume;
+  const Real _max_volume;
   /// whether to check different element types in the same sub-domain
   const MooseEnum _check_element_types;
   /// whether to check for intersecting elements
@@ -46,15 +46,14 @@ protected:
   unsigned int _num_elem_overlaps;
   /// whether to check for elements in different planes (non_planar)
   const MooseEnum _check_non_planar_sides;
-  /// counting number of sides that are non-planar
+  /// counter for the number of sides that are non-planar
   unsigned int _sides_non_planar;
   /// whether to check for non-conformal meshes
   const MooseEnum _check_non_conformal_mesh;
-  /// tolerance for meshes that are not comformal
-  Real _non_conformality_tol;
-  /// counting the number of non-conformal elements
+  /// tolerance for detecting when meshes are not conformal
+  const Real _non_conformality_tol;
+  /// counter for the number of non-conformal elements
   unsigned int _num_nonconformal_nodes;
-  /// whether to check
 
   /// whether to check for the adaptivity of non-conformal meshes
   const MooseEnum _check_adaptivity_non_conformality;
