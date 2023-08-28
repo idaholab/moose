975edc107def0b6dd8895105615545b4bbfedd64
Max Nezdyur <maxnezdyur@gmail.com>
2023-08-28 09:34:47 -0400

    eliminate unneeded header optApp

--- modules/optimization/src/base/OptimizationApp.C
+++ modules/optimization/src/base/OptimizationApp.C
@@ -7,7 +7,6 @@
 //* Licensed under LGPL 2.1, please see LICENSE for details
 //* https://www.gnu.org/licenses/lgpl-2.1.html
 
-#include "ActionFactory.h"
 #include "OptimizationApp.h"
 #include "Moose.h"
 #include "AppFactory.h"
