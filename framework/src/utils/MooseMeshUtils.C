a68c9043b4479216dbd886b5f1460ddc8cae9d94
Alex Lindsay <alexander.lindsay@inl.gov>
2023-09-01 14:00:50 -0700

    Speed up getBoundaryIDs when distributed
    If we are prepared, then we can use the cached data in
    `BoundaryInfo::get_global_boundary_ids`. This allows us to avoid
    a communication call that we would otherwise have to do if the
    mesh is distributed
    
    Refs #0

--- framework/src/utils/MooseMeshUtils.C
+++ framework/src/utils/MooseMeshUtils.C
@@ -90,10 +90,11 @@ getBoundaryIDs(const MeshBase & mesh,
    */
   if (generate_unknown)
   {
-    const std::set<BoundaryID> & local_bids = mesh.get_boundary_info().get_boundary_ids();
-    max_boundary_local_id = local_bids.empty() ? 0 : *(local_bids.rbegin());
+    const auto & bids = mesh.is_prepared() ? mesh.get_boundary_info().get_global_boundary_ids()
+                                           : mesh.get_boundary_info().get_boundary_ids();
+    max_boundary_local_id = bids.empty() ? 0 : *(bids.rbegin());
     /* We should not hit this often */
-    if (!mesh.is_serial())
+    if (!mesh.is_prepared() && !mesh.is_serial())
       mesh.comm().max(max_boundary_local_id);
   }
 
