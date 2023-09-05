ec06aa2a50435464d53092b7e51afbc29383093a
Logan Harbour <loganhharbour@gmail.com>
2023-09-05 07:54:24 -0600

    Add return docstrings
    refs #21011

--- framework/include/base/MooseApp.h
+++ framework/include/base/MooseApp.h
@@ -715,10 +715,14 @@ public:
 
   /**
    * Backs up the application to the folder \p folder_base
+   *
+   * @return The files that are written in the backup
    */
   std::vector<std::filesystem::path> backup(const std::filesystem::path & folder_base);
   /**
    * Backs up the application memory in a Backup.
+   *
+   * @return The backup
    */
   std::unique_ptr<Backup> backup();
 
