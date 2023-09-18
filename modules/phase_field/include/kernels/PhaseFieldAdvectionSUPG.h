addfbea2c45e88db2cf55ddf23189799fc6be244
Prithivirajan Veerappan <veerappan.prithivirajan@inl.gov>
2023-09-18 16:55:47 -0600

    Advection and SUPG Kernels added from LevelSet

---
+++ modules/phase_field/include/kernels/PhaseFieldAdvectionSUPG.h
@@ -0,0 +1,29 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "ADKernelGrad.h"
+
+/**
+ * SUPG stabilization for the advection portion of the level set equation.
+ */
+class PhaseFieldAdvectionSUPG : public ADKernelGrad
+{
+public:
+  static InputParameters validParams();
+
+  PhaseFieldAdvectionSUPG(const InputParameters & parameters);
+
+protected:
+  virtual ADRealVectorValue precomputeQpResidual() override;
+
+  /// Velocity vector variable
+  const ADVectorVariableValue & _velocity;
+};
