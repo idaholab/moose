e840b57bb3dab10c9784eaff9599e9d5419d6758
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2023-09-26 08:08:16 -0600

    Adapt app creation code to new name

--- modules/heat_transfer/include/base/HeatConductionApp.h
+++ modules/heat_transfer/include/base/HeatConductionApp.h
@@ -10,18 +10,13 @@
 #pragma once
 
 #include "MooseApp.h"
+#include "HeatTransferApp.h"
 
-class HeatConductionApp : public MooseApp
+class HeatConductionApp : public HeatTransferApp
 {
 public:
-  static InputParameters validParams();
-
-  HeatConductionApp(const InputParameters & parameters);
-  virtual ~HeatConductionApp();
-
-  static void registerApps();
-  static void registerAll(Factory & f, ActionFactory & af, Syntax & s);
-  static void registerObjects(Factory & factory);
-  static void associateSyntax(Syntax & syntax, ActionFactory & action_factory);
-  static void registerExecFlags(Factory & factory);
+  HeatConductionApp(const InputParameters & parameters) : HeatTransferApp(parameters)
+  {
+    mooseDeprecated("HeatConductionApp is deprecated. Please create a HeatTransferApp instead");
+  }
 };
