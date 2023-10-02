bf100088efa0929962680943e76e487ee49b6897
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2023-10-02 16:53:33 -0600

    Add first stab at heat conduction physics object

---
+++ modules/heat_transfer/include/physics/HeatConductionPhysics.h
@@ -0,0 +1,34 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "PhysicsBase.h"
+
+/**
+ * Creates all the objects needed to solve the heat conduction equations
+ */
+class HeatConductionPhysics : public PhysicsBase
+{
+public:
+  static InputParameters validParams();
+
+  HeatConductionPhysics(const InputParameters & parameters);
+
+  /// GeneralUO not the right base class probably
+  virtual void initialize() override{};
+  virtual void execute() override{};
+  virtual void finalize() override{};
+
+  const VariableName & _temperature_name;
+
+private:
+  void addNonlinearVariables() override;
+  void addFEKernels() override;
+};
---
+++ modules/heat_transfer/src/physics/HeatConductionPhysics.C
@@ -0,0 +1,55 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "HeatConductionPhysics.h"
+#include "ADHeatConduction.h"
+
+registerMooseObject("HeatConductionApp", HeatConductionPhysics);
+
+InputParameters
+HeatConductionPhysics::validParams()
+{
+  InputParameters params = PhysicsBase::validParams();
+  params.transferParam<MaterialPropertyName>(ADHeatConduction::validParams(), "diffusivity");
+  params.addParam<VariableName>("temperature_name", "T", "Variable name for the temperature");
+  params.addClassDescription("Add the heat conduction physics");
+
+  return params;
+}
+
+HeatConductionPhysics::HeatConductionPhysics(const InputParameters & parameters)
+  : PhysicsBase(parameters), _temperature_name(getParam<VariableName>("temperature_name"))
+{
+}
+
+void
+HeatConductionPhysics::addFEKernels()
+{
+  {
+    const std::string kernel_type = "HeatConduction";
+    InputParameters params = getFactory().getValidParams(kernel_type);
+    params.set<NonlinearVariableName>("variable") = _temperature_name;
+    getProblem().addKernel(kernel_type, name() + "_" + _temperature_name + "_conduction", params);
+  }
+  if (isTransient())
+  {
+    const std::string kernel_type = "TimeDerivative";
+    InputParameters params = getFactory().getValidParams(kernel_type);
+    params.set<NonlinearVariableName>("variable") = _temperature_name;
+    getProblem().addKernel(kernel_type, name() + "_" + _temperature_name + "_time", params);
+  }
+}
+
+void
+HeatConductionPhysics::addNonlinearVariables()
+{
+  const std::string variable_type = "MooseVariableFE";
+  InputParameters params = getFactory().getValidParams(variable_type);
+  getProblem().addVariable(variable_type, _temperature_name, params);
+}
