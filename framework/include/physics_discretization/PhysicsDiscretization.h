3cb54e5f538526668724233828f0cc4633e0bfd9
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2023-10-03 12:40:14 -0600

    Add a base class for specifying discretizations

---
+++ framework/include/physics_discretization/PhysicsDiscretization.h
@@ -0,0 +1,42 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "MooseObject.h"
+
+/**
+ * Discretization class to indicate how you plan to discretize the equation
+ */
+class PhysicsDiscretization : public MooseObject
+{
+public:
+  static InputParameters validParams();
+
+  PhysicsDiscretization(const InputParameters & parameters);
+
+  /// Getter for the variables' names in the discretization
+  std::vector<VariableName> getVariableNames() const { return _var_names; }
+  /// Getter for the variables' families in the discretization
+  std::vector<MooseEnum> getVariableFamilies() const { return _var_families; }
+  /// Getter for the variables' orders in the discretization
+  std::vector<MooseEnum> getVariableOrders() const { return _var_orders; }
+  /// Getter for the variables' scaling in the discretization
+  std::vector<Real> getVariableScalings() const { return _var_scalings; }
+
+private:
+  /// Vector of the variable names
+  std::vector<VariableName> _var_names;
+  /// Vector of the finite element families of the variables (same order as the names)
+  std::vector<MooseEnum> _var_families;
+  /// Vector of the orders of the variables (same order as the names)
+  std::vector<MooseEnum> _var_orders;
+  /// Vector of the scaling of the variables (same order as the names)
+  std::vector<Real> _var_scalings;
+};
---
+++ framework/src/physics_discretization/PhysicsDiscretization.C
@@ -0,0 +1,49 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "PhysicsDiscretization.h"
+#include "AddVariableAction.h"
+
+registerMooseObject("MooseApp", PhysicsDiscretization);
+
+InputParameters
+PhysicsDiscretization::validParams()
+{
+  InputParameters params = emptyInputParameters();
+  params.addClassDescription(
+      "Base class for indicating the discretization to use for your Physics");
+
+  // Variable parameters
+  MooseEnum families(AddVariableAction::getNonlinearVariableFamilies());
+  MooseEnum orders(AddVariableAction::getNonlinearVariableOrders());
+  params.addParam<std::vector<VariableName>>(
+      "variables",
+      "Variables used in the Physics. If unspecified, will be pulled in from the Physics");
+  params.addParam<std::vector<MooseEnum>>(
+      "family", families, "Specifies the family of FE shape functions to use for this variable");
+  params.addParam<std::vector<MooseEnum>>("order",
+                                          orders,
+                                          "Specifies the order of the FE shape function to use "
+                                          "for this variable (additional orders not listed are "
+                                          "allowed)");
+  params.addParam<std::vector<Real>>("scaling",
+                                     "Specifies a scaling factor to apply to this variable. This "
+                                     "is used for manual scaling of variables");
+  return params;
+}
+
+PhysicsDiscretization::PhysicsDiscretization(const InputParameters & parameters)
+  : MooseObject(parameters),
+    _var_names(getParam<std::vector<VariableName>>("variables")),
+    _var_families(getParam<std::vector<MooseEnum>>("scaling")),
+    _var_orders(getParam<std::vector<MooseEnum>>("order")),
+    _var_scalings(getParam<std::vector<Real>>("scaling"))
+{
+  // InputParametersUtils::chec
+}
