c920ca49d4c90ea03232a78cf6ea3bb482ec0d58
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2023-10-05 15:19:08 -0600

    Add variable and boundaries at the NS Physics base class level - they are physics concepts and are shared between all of our NS implementations

--- modules/navier_stokes/include/physics/NavierStokesFlowPhysics.h
+++ modules/navier_stokes/include/physics/NavierStokesFlowPhysics.h
@@ -26,5 +26,44 @@ public:
   virtual void execute() override{};
   virtual void finalize() override{};
 
+  /// To interface with other Physics
+  std::vector<std::string> getVelocityNames() const { return _velocity_names; }
+  NonlinearVariableName getPressureName() const { return _pressure_name; }
+  NonlinearVariableName getTemperatureName() const { return _fluid_temperature_name; }
+  MooseFunctorName getPorosityFunctorName(bool smoothed) const;
+
 protected:
+  /// To interface with components
+  void processMesh();
+
+  /// Compressibility type, can be compressible, incompressible or weakly-compressible
+  const MooseEnum _compressibility;
+
+  /// Switch to show if porous medium treatment is requested or not
+  const bool _porous_medium_treatment;
+  /// The name of the functor for the porosity field
+  const MooseFunctorName _porosity_name;
+  /// The name of the functor for the smoothed porosity field
+  const MooseFunctorName _flow_porosity_functor_name;
+  /// The number of smoothing layers if that treatment is used on porosity
+  const unsigned _porosity_smoothing_layers;
+
+  /// Velocity names
+  const std::vector<std::string> _velocity_names;
+  /// Pressure name
+  const NonlinearVariableName _pressure_name;
+  /// Fluid temperature name
+  const NonlinearVariableName _fluid_temperature_name;
+
+  /// Boundaries with a flow inlet specified on them
+  const std::vector<BoundaryName> _inlet_boundaries;
+  /// Boundaries with a flow outlet specified on them
+  const std::vector<BoundaryName> _outlet_boundaries;
+  /// Boundaries which define a wall (slip/noslip/etc.)
+  const std::vector<BoundaryName> _wall_boundaries;
+
+  /// Name of the density material property
+  const MooseFunctorName _density_name;
+  /// Name of the dynamic viscosity material property
+  const MooseFunctorName _dynamic_viscosity_name;
 };
