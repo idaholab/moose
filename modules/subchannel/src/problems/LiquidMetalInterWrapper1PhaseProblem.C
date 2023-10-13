0180623ffb534dd652de4e62d42f2ebe565c3a24
Alex Lindsay <alexander.lindsay@inl.gov>
2023-10-13 11:46:26 -0700

    Make sure to do global assembly after setting and before reading
    Refs [libmesh/libmesh#3603](https://github.com/libMesh/libmesh/pull/3603)

--- modules/subchannel/src/problems/LiquidMetalInterWrapper1PhaseProblem.C
+++ modules/subchannel/src/problems/LiquidMetalInterWrapper1PhaseProblem.C
@@ -552,6 +552,10 @@ LiquidMetalInterWrapper1PhaseProblem::externalSolve()
         if (_compute_viscosity)
           computeMu(iblock);
 
+        // We must do a global assembly to make sure data is parallel consistent before we do things
+        // like compute L2 norms
+        _aux->solution().close();
+
         auto T_L2norm_new = _T_soln->L2norm();
         T_block_error =
             std::abs((T_L2norm_new - T_L2norm_old_block) / (T_L2norm_old_block + 1E-14));
