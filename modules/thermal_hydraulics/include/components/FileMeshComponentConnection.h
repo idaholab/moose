723e585e0639fcc4396b9635576411f9744c9e98
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2023-10-16 17:44:08 -0600

    Add a file mesh component connection: keeps track of connected components keeps track of connection information (nodes, id, etc)

---
+++ modules/thermal_hydraulics/include/components/FileMeshComponentConnection.h
@@ -0,0 +1,199 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "Component.h"
+
+/**
+ * Base class for mesh-based component junctions and boundaries
+ */
+class FileMeshComponentConnection : public Component
+{
+public:
+  FileMeshComponentConnection(const InputParameters & params);
+
+  /// End type
+  enum EEndType
+  {
+    IN, ///< inlet
+    OUT ///< outlet
+  };
+  /// Map of end type string to enum
+  static const std::map<std::string, EEndType> _end_type_to_enum;
+
+  /// Structure for holding data for a connection
+  struct Connection
+  {
+    /// The name of the boundary this connection is attached to
+    const BoundaryName _boundary_name;
+
+    /// Name of the component in the connection
+    const std::string _component_name;
+
+    /// End type for the connection
+    const EEndType _end_type;
+
+    Connection(const BoundaryName & boundary_name,
+               const std::string & component_name,
+               const EEndType & end_type)
+      : _boundary_name(boundary_name), _component_name(component_name), _end_type(end_type)
+    {
+    }
+  };
+
+  /**
+   * Returns the vector of connections of this component
+   *
+   * @returns vector of connections of this component
+   */
+  const std::vector<Connection> & getConnections() const { return _connections; }
+
+  /**
+   * Returns a list of names of components that are connected to this component
+   *
+   * @returns list of names of components that are connected to this component
+   */
+  const std::vector<std::string> & getConnectedComponentNames() const
+  {
+    return _connected_component_names;
+  }
+
+  /**
+   * Gets the vector of connected subdomain names
+   */
+  const std::vector<SubdomainName> & getConnectedSubdomainNames() const
+  {
+    return _connected_subdomain_names;
+  }
+
+  /**
+   * Gets the list of boundary nodes connected to this component
+   *
+   * @return list of connected boundary nodes
+   */
+  virtual const std::vector<dof_id_type> & getNodeIDs() const;
+
+  /**
+   * Gets the boundary names for this component
+   *
+   * @return boundary names for this component
+   */
+  const std::vector<BoundaryName> & getBoundaryNames() const;
+
+protected:
+  virtual void setupMesh() override;
+  virtual void init() override;
+  virtual void check() const override;
+
+  /**
+   * Adds a connection for this component
+   *
+   * Components using this interface must call this function on all of their
+   * connection strings.
+   *
+   * @param[in] boundary_name   The name of the boundary
+   */
+  void addConnection(const BoundaryName & boundary_name);
+
+  /**
+   * Checks that the number of connections is equal to the supplied value
+   *
+   * @param[in] n_connections   enforced number of connections
+   */
+  void checkNumberOfConnections(const unsigned int & n_connections) const;
+
+  /**
+   * Checks that the size of a vector parameter equals the number of connections
+   *
+   * @tparam    T       type of element in the vector parameter
+   * @param[in] param   vector parameter name
+   */
+  template <typename T>
+  void checkSizeEqualsNumberOfConnections(const std::string & param) const;
+
+  /**
+   * Checks that all connections have the same of a certain type of object
+   *
+   * @tparam    T             type of object to check
+   * @param[in] objects       list of objects corresponding to each connection
+   * @param[in] description   description of the obect to check
+   */
+  template <typename T>
+  void checkAllConnectionsHaveSame(const std::vector<T> & objects,
+                                   const std::string & description) const;
+
+  /// Physical positions of connected components
+  std::vector<Point> _positions;
+  /// Boundary node IDs from connected components
+  std::vector<dof_id_type> _nodes;
+  /// Boundary IDs of connected components
+  std::vector<unsigned int> _boundary_ids;
+  /// Boundary names of connected components
+  std::vector<BoundaryName> _boundary_names;
+  /// Outward normals associated with connected components
+  std::vector<Real> _normals;
+  /// Directions of connected components
+  std::vector<RealVectorValue> _directions;
+
+  /// Vector of connections of this component
+  std::vector<Connection> _connections;
+  /// Vector of connected component names
+  std::vector<std::string> _connected_component_names;
+  /// Vector of subdomain names of the connected components
+  std::vector<SubdomainName> _connected_subdomain_names;
+
+public:
+  static InputParameters validParams();
+};
+
+template <typename T>
+void
+FileMeshComponentConnection::checkSizeEqualsNumberOfConnections(const std::string & param) const
+{
+  const auto & value = getParam<std::vector<T>>(param);
+  if (value.size() != _connections.size())
+    logError("The number of entries in '",
+             param,
+             "' (",
+             value.size(),
+             ") must equal the number of connections (",
+             _connections.size(),
+             ")");
+}
+
+template <typename T>
+void
+FileMeshComponentConnection::checkAllConnectionsHaveSame(const std::vector<T> & objects,
+                                                         const std::string & description) const
+{
+  for (const auto & obj : objects)
+    if (obj != objects[0])
+      logError("All connections must have the same ", description);
+}
+
+namespace THM
+{
+template <>
+FileMeshComponentConnection::EEndType stringToEnum(const std::string & s);
+}
+
+namespace Moose
+{
+std::string
+stringify(FileMeshComponentConnection::EEndType end_type)
+{
+  if (end_type == FileMeshComponentConnection::EEndType::IN)
+    return "IN";
+  else if (end_type == FileMeshComponentConnection::EEndType::OUT)
+    return "OUT";
+  else
+    return std::to_string(end_type) + " (unrecognized)";
+}
+}
---
+++ modules/thermal_hydraulics/src/components/FileMeshComponentConnection.C
@@ -0,0 +1,168 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "FileMeshComponentConnection.h"
+#include "FileMeshComponent.h"
+
+const std::map<std::string, FileMeshComponentConnection::EEndType>
+    FileMeshComponentConnection::_end_type_to_enum{{"IN", IN}, {"OUT", OUT}};
+
+template <>
+FileMeshComponentConnection::EEndType
+THM::stringToEnum(const std::string & s)
+{
+  return stringToEnum<FileMeshComponentConnection::EEndType>(
+      s, FileMeshComponentConnection::_end_type_to_enum);
+}
+
+InputParameters
+FileMeshComponentConnection::validParams()
+{
+  InputParameters params = Component::validParams();
+  return params;
+}
+
+FileMeshComponentConnection::FileMeshComponentConnection(const InputParameters & params)
+  : Component(params)
+{
+}
+
+void
+FileMeshComponentConnection::setupMesh()
+{
+  // Copy the connection data from each specified component to this connection component
+  for (const auto & connection : _connections)
+  {
+    const std::string & comp_name = connection._component_name;
+
+    if (hasComponentByName<FileMeshComponent>(comp_name))
+    {
+      _boundary_names.push_back(connection._boundary_name);
+
+      const FileMeshComponent & comp = getComponentByName<FileMeshComponent>(comp_name);
+      std::cout << Moose::stringify(connection._end_type) << std::endl;
+      for (auto && conn : comp.getConnections(connection._end_type))
+      {
+        // copy info from the connection
+        // _positions.push_back(conn._position);
+        // _nodes.push_back(conn._node->id());
+        // _normals.push_back(conn._normal);
+        _boundary_ids.push_back(conn._boundary_id);
+        // _directions.push_back(comp.getDirection()); // this will be matched to the BC direction
+      }
+    }
+    else
+      logError("Trying to connect to a component '",
+               comp_name,
+               "', but there is no such component in the simulation. Please check your spelling.");
+  }
+}
+
+void
+FileMeshComponentConnection::init()
+{
+  Component::init();
+
+  if (_connections.size() > 0)
+  {
+    std::vector<UserObjectName> fp_names;
+    std::vector<THM::FlowModelID> flow_model_ids;
+    for (const auto & connection : _connections)
+    {
+      const std::string comp_name = connection._component_name;
+      if (hasComponentByName<FileMeshComponent>(comp_name))
+      {
+        const FileMeshComponent & comp =
+            getTHMProblem().getComponentByName<FileMeshComponent>(comp_name);
+
+        // add to list of subdomain names
+        const std::vector<SubdomainName> & subdomain_names = comp.getSubdomainNames();
+        _connected_subdomain_names.insert(
+            _connected_subdomain_names.end(), subdomain_names.begin(), subdomain_names.end());
+      }
+    }
+  }
+  else
+    logError("The component is not connected.");
+}
+
+void
+FileMeshComponentConnection::check() const
+{
+  Component::check();
+
+  for (const auto & comp_name : _connected_component_names)
+    checkComponentOfTypeExistsByName<FileMeshComponent>(comp_name);
+}
+
+void
+FileMeshComponentConnection::addConnection(const BoundaryName & boundary_name)
+{
+  const size_t oparenthesis_pos = boundary_name.find('(');
+  if (oparenthesis_pos != std::string::npos)
+  {
+    logError("You are using the old connection format 'comp_name(end)'. Please update your input "
+             "file to the new one 'comp_name:end'.");
+  }
+  else
+  {
+    const size_t begin_colon_pos = boundary_name.find(':');
+    const size_t end_colon_pos = boundary_name.rfind(':');
+    if (begin_colon_pos == end_colon_pos)
+      mooseError("Boundary name ",
+                 boundary_name,
+                 " should include two semicolons to match this format: "
+                 "component_name:boundary_name:in/out");
+    // if it has a colon, assume 'component_name:end_type' format
+    if (end_colon_pos != std::string::npos)
+    {
+      const std::string connected_component_name = boundary_name.substr(0, begin_colon_pos);
+      const std::string boundary_name_cut = boundary_name.substr(0, end_colon_pos);
+      const std::string str_end =
+          boundary_name.substr(end_colon_pos + 1, boundary_name.length() - end_colon_pos - 1);
+      const EEndType end_type = THM::stringToEnum<EEndType>(str_end);
+
+      _connections.push_back(Connection(boundary_name_cut, connected_component_name, end_type));
+      _connected_component_names.push_back(connected_component_name);
+
+      // Add dependency because the connected component's setupMesh() must be called
+      // before this component's setupMesh().
+      addDependency(connected_component_name);
+    }
+    else
+    {
+      logError("Incorrect connection specified '",
+               boundary_name,
+               "'. Valid connection format is 'component_name:end_type'.");
+    }
+  }
+}
+
+void
+FileMeshComponentConnection::checkNumberOfConnections(const unsigned int & n_connections) const
+{
+  if (_connections.size() != n_connections)
+    logError("The number of connections (", _connections.size(), ") must equal ", n_connections);
+}
+
+const std::vector<dof_id_type> &
+FileMeshComponentConnection::getNodeIDs() const
+{
+  checkSetupStatus(MESH_PREPARED);
+
+  return _nodes;
+}
+
+const std::vector<BoundaryName> &
+FileMeshComponentConnection::getBoundaryNames() const
+{
+  checkSetupStatus(MESH_PREPARED);
+
+  return _boundary_names;
+}
