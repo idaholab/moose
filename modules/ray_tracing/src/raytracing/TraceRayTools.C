aed12bd4630fdf1c9849ae0b3e448a7a7c6d6a20
Nuno Nobre <nuno.nobre@stfc.ac.uk>
2023-10-17 11:14:25 +0100

    Rm unneeded includes & simplify switches (#25685)

--- modules/ray_tracing/src/raytracing/TraceRayTools.C
+++ modules/ray_tracing/src/raytracing/TraceRayTools.C
@@ -608,21 +608,17 @@ withinEdge(const Elem * elem,
   switch (elem->type())
   {
     case HEX8:
-      return withinEdgeTempl<Hex8>(elem, point, extrema, tolerance);
-    case TET4:
-      return withinEdgeTempl<Tet4>(elem, point, extrema, tolerance);
-    case PYRAMID5:
-      return withinEdgeTempl<Pyramid5>(elem, point, extrema, tolerance);
-    case PRISM6:
-      return withinEdgeTempl<Prism6>(elem, point, extrema, tolerance);
     case HEX20:
     case HEX27:
       return withinEdgeTempl<Hex8>(elem, point, extrema, tolerance);
+    case TET4:
     case TET10:
       return withinEdgeTempl<Tet4>(elem, point, extrema, tolerance);
+    case PYRAMID5:
     case PYRAMID13:
     case PYRAMID14:
       return withinEdgeTempl<Pyramid5>(elem, point, extrema, tolerance);
+    case PRISM6:
     case PRISM15:
     case PRISM18:
       return withinEdgeTempl<Prism6>(elem, point, extrema, tolerance);
@@ -641,30 +637,24 @@ atVertexOnSide(const Elem * elem, const Point & point, const unsigned short side
   switch (elem->type())
   {
     case HEX8:
-      return atVertexOnSideTempl<Hex8>(elem, point, side);
-    case TET4:
-      return atVertexOnSideTempl<Tet4>(elem, point, side);
-    case PYRAMID5:
-      return atVertexOnSideTempl<Pyramid5>(elem, point, side);
-    case PRISM6:
-      return atVertexOnSideTempl<Prism6>(elem, point, side);
-    case QUAD4:
-      return atVertexOnSideTempl<Quad4>(elem, point, side);
-    case TRI3:
-      return atVertexOnSideTempl<Tri3>(elem, point, side);
     case HEX20:
     case HEX27:
       return atVertexOnSideTempl<Hex8>(elem, point, side);
+    case QUAD4:
     case QUAD8:
     case QUAD9:
       return atVertexOnSideTempl<Quad4>(elem, point, side);
+    case TRI3:
     case TRI6:
       return atVertexOnSideTempl<Tri3>(elem, point, side);
+    case TET4:
     case TET10:
       return atVertexOnSideTempl<Tet4>(elem, point, side);
+    case PYRAMID5:
     case PYRAMID13:
     case PYRAMID14:
       return atVertexOnSideTempl<Pyramid5>(elem, point, side);
+    case PRISM6:
     case PRISM15:
     case PRISM18:
       return atVertexOnSideTempl<Prism6>(elem, point, side);
@@ -719,21 +709,17 @@ withinEdgeOnSide(const Elem * const elem,
   switch (elem->type())
   {
     case HEX8:
-      return withinEdgeOnSideTempl<Hex8>(elem, point, side, extrema);
-    case TET4:
-      return withinEdgeOnSideTempl<Tet4>(elem, point, side, extrema);
-    case PYRAMID5:
-      return withinEdgeOnSideTempl<Pyramid5>(elem, point, side, extrema);
-    case PRISM6:
-      return withinEdgeOnSideTempl<Prism6>(elem, point, side, extrema);
     case HEX20:
     case HEX27:
       return withinEdgeOnSideTempl<Hex8>(elem, point, side, extrema);
+    case TET4:
     case TET10:
       return withinEdgeOnSideTempl<Tet4>(elem, point, side, extrema);
+    case PYRAMID5:
     case PYRAMID13:
     case PYRAMID14:
       return withinEdgeOnSideTempl<Pyramid5>(elem, point, side, extrema);
+    case PRISM6:
     case PRISM15:
     case PRISM18:
       return withinEdgeOnSideTempl<Prism6>(elem, point, side, extrema);
