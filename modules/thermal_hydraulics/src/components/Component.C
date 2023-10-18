28cbc163ec98f4ea7098566e183840c4357d821d
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2023-10-18 15:43:11 -0600

    Add ability for components to query AD functors

--- modules/thermal_hydraulics/src/components/Component.C
+++ modules/thermal_hydraulics/src/components/Component.C
@@ -18,6 +18,7 @@ InputParameters
 Component::validParams()
 {
   InputParameters params = THMObject::validParams();
+  params += ADFunctorInterface::validParams();
   params.addPrivateParam<THMProblem *>("_thm_problem");
   params.addPrivateParam<Component *>("_parent", nullptr);
   params.addPrivateParam<std::string>("built_by_action", "add_component");
@@ -35,6 +36,7 @@ Component::Component(const InputParameters & parameters)
   : THMObject(parameters),
     LoggingInterface(getCheckedPointerParam<THMProblem *>("_thm_problem")->log()),
     NamingInterface(),
+    ADFunctorInterface(this),
 
     _parent(getParam<Component *>("_parent")),
     _sim(*getCheckedPointerParam<THMProblem *>("_thm_problem")),
