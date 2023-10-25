224cf6bd9a2be3a0828f371b3110f4d9d9771c2f
Jason M Miller <Jason.Miller@inl.gov>
2023-10-25 14:19:20 -0600

    Refrain from using mamba update --all
    As we continue to diverge from the latest available packages on
    conda-forge, `--all` becomes less and less effective. It is time
    to instruct folks to update the package which matters most in the
    (moose) Conda environment.
    
    Closes #25854

--- framework/include/base/Moose.h
+++ framework/include/base/Moose.h
@@ -56,7 +56,7 @@ static_assert(__cplusplus >= 201703L,
               "MOOSE requires a C++17 compatible compiler (GCC >= 7.5.0, Clang >= 5.0.2). Please "
               "update your compiler or, if compatible, add '-std=c++17' to your compiler flags "
               "and try again. If using the MOOSE conda package, please attempt a MOOSE environment "
-              "update (using `mamba update --all`). If this update is not successful, please "
+              "update (using `mamba update moose-dev`). If this update is not successful, please "
               "create a new MOOSE environment (see "
               "https://mooseframework.inl.gov/getting_started/installation/"
               "conda.html#uninstall-conda-moose-environment).");
