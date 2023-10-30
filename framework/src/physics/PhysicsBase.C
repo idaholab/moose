4d3ead3e3cc7f1708eb79f1c80cdd692be75ab92
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2023-10-30 08:37:42 -0600

    Add routine to check block restriction of variables to physics base class

--- framework/src/physics/PhysicsBase.C
+++ framework/src/physics/PhysicsBase.C
@@ -292,3 +292,39 @@ PhysicsBase::assignBlocks(InputParameters & params, const std::vector<SubdomainN
   if (std::find(blocks.begin(), blocks.end(), "ANY_BLOCK_ID") == blocks.end())
     params.set<std::vector<SubdomainName>>("block") = blocks;
 }
+
+void
+PhysicsBase::checkBlockRestrictionIdentical(const std::string & object_name,
+                                            const std::vector<SubdomainName> & blocks) const
+{
+  // If identical, we can return fast
+  if (_blocks == blocks)
+    return;
+  // Copy, sort and unique is the only way to check that they are actually the same
+  auto copy_blocks = _blocks;
+  auto copy_blocks_other = blocks;
+  std::sort(copy_blocks.begin(), copy_blocks.end());
+  copy_blocks.erase(unique(copy_blocks.begin(), copy_blocks.end()), copy_blocks.end());
+  std::sort(copy_blocks_other.begin(), copy_blocks_other.end());
+  copy_blocks_other.erase(unique(copy_blocks_other.begin(), copy_blocks_other.end()),
+                          copy_blocks_other.end());
+
+  if (copy_blocks == copy_blocks_other)
+    return;
+  std::vector<SubdomainName> diff;
+  std::set_difference(copy_blocks.begin(),
+                      copy_blocks.end(),
+                      copy_blocks_other.begin(),
+                      copy_blocks_other.end(),
+                      std::inserter(diff, diff.begin()));
+  mooseError("Physics '",
+             name(),
+             "' and object '",
+             object_name,
+             "' have different block restrictions.\n Physics: ",
+             Moose::stringify(_blocks),
+             "\nObject: ",
+             Moose::stringify(blocks),
+             "\nDifference: ",
+             Moose::stringify(diff));
+}
