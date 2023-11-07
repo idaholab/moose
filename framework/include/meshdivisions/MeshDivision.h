db0e84ef433ae363c45452ef619dc0d8dc33347c
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2023-11-07 12:59:14 -0700

    Add a base class for mesh divisions, refs #25901 - public getters for which division and number of divisions - meshChanged causes new initialize

---
+++ framework/include/meshdivisions/MeshDivision.h
@@ -0,0 +1,78 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "MooseObject.h"
+#include "SetupInterface.h"
+#include "Restartable.h"
+#include "MeshChangedInterface.h"
+
+// libMesh
+#include "libmesh/vector_value.h"
+
+// libMesh forward declarations
+namespace libMesh
+{
+class Point;
+}
+
+namespace MooseMeshDivision
+{
+/// Invalid subdomain id to return when outside the mesh division
+inline unsigned int INVALID_DIVISION_INDEX = std::numeric_limits<unsigned int>::max();
+}
+
+/**
+ * Base class for MeshDivision objects. MeshDivision objects divide the mesh into a
+ * contiguously ordered set of divisions/partitions.
+ */
+class MeshDivision : public MooseObject, public SetupInterface, public MeshChangedInterface
+{
+public:
+  /**
+   * Class constructor
+   * \param parameters The input parameters for the MeshDivision
+   */
+  static InputParameters validParams();
+
+  MeshDivision(const InputParameters & parameters);
+
+  /**
+   * MeshDivision destructor
+   */
+  virtual ~MeshDivision();
+
+  /// Return the index of the division to which the point belongs
+  virtual unsigned int divisionIndex(const Point & pt) const = 0;
+  /// Return the index of the division to which the element belongs
+  virtual unsigned int divisionIndex(const Elem & elem) const = 0;
+  /// Return the number of divisions
+  unsigned int getNumDivisions() const { return _num_divs; }
+
+  /// By default, meshChanged will cause a re-initialization of the necessary data members
+  void meshChanged() override { initialize(); }
+
+protected:
+  /// Set the number of divisions
+  void setNumDivisions(unsigned int ndivs) { _num_divs = ndivs; }
+
+  /// Set up any data members that would be necessary to obtain the division indices
+  void initialize() {}
+
+  /// Pointer to the problem, needed to retrieve pointers to various objects
+  const FEProblemBase * const _fe_problem;
+
+  /// Mesh that is being divided
+  const MooseMesh & _mesh;
+
+private:
+  /// Number of divisions in the division
+  unsigned int _num_divs;
+};
