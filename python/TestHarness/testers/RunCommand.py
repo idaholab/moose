cff5c822c0bd44544422b99c5002154c0b51aab5
Patrick Behne <patrick.behne@inl.gov>
2023-11-06 17:27:40 -0700

    Testing with shell=False.
    This commit tests all tests without launching them in a separate shell.
    A custom string splitting utility function was implemented to correctly split
    the commands into lists to be passed to subprocess.Popen. If we procees with
    this approach, this utility will need to be cleaned up for conciseness and
    clarity. Some of the tests files were changes to reflect the fact that the
    tests are no longer run in a separate shell by default.

--- python/TestHarness/testers/RunCommand.py
+++ python/TestHarness/testers/RunCommand.py
@@ -8,6 +8,7 @@
 #* https://www.gnu.org/licenses/lgpl-2.1.html
 
 from Tester import Tester
+from TestHarness import util
 
 class RunCommand(Tester):
 
--- python/TestHarness/testers/Tester.py
+++ python/TestHarness/testers/Tester.py
@@ -308,7 +308,7 @@ class Tester(MooseObject):
         """ return the executable command that will be executed by the tester """
         return ''
 
-    def spawnSubprocessFromOptions(self, timer, options, shell_linux=True, shell_windows=True):
+    def spawnSubprocessFromOptions(self, timer, options, shell_linux=False, shell_windows=False):
         """
         Spawns a subprocess based on given options, sets output and error files,
         and starts timer.
@@ -317,9 +317,10 @@ class Tester(MooseObject):
         cmd_linux = cmd
         cmd_windows = cmd
         if not shell_linux:
-            cmd_linux = cmd_linux.split(" ")
+            cmd_linux = util.split_string_with_quotes(cmd_linux)
+
         if not shell_windows:
-            cmd_windows = cmd_windows.split(" ")
+            cmd_windows = util.split_string_with_quotes(cmd_windows)
 
         cwd = self.getTestDir()
 
