a60d8992572968f46089ad10a04d9ffccbff1457
Tano Retamales <tanome@rod.hpc.inl.gov>
2023-12-09 23:53:55 -0700

    correcting wall temperature bc Refs #9007

--- modules/navier_stokes/src/fvbcs/INSFVTurbulentTemperatureWallFunction.C
+++ modules/navier_stokes/src/fvbcs/INSFVTurbulentTemperatureWallFunction.C
@@ -57,10 +57,14 @@ INSFVTurbulentTemperatureWallFunction::computeQpResidual()
 {
   // Useful parameters
   const FaceInfo & fi = *_face_info;
-  Real wall_dist = std::abs((fi.elemCentroid() - fi.faceCentroid()) * fi.normal());
+  const Real wall_dist = std::abs((fi.elemCentroid() - fi.faceCentroid()) * fi.normal());
   const Elem & _current_elem = fi.elem();
-  auto current_argument = makeElemArg(&_current_elem);
+  const auto current_argument = makeElemArg(&_current_elem);
   const auto state = determineState();
+  const auto mu = _mu(current_argument, state);
+  const auto rho = _rho(current_argument, state);
+  const auto cp =  _cp(current_argument, state);
+  const auto kappa = _kappa(current_argument, state);
 
   // Get the velocity vector
   ADRealVectorValue velocity(_u_var(current_argument, state));
@@ -70,60 +74,60 @@ INSFVTurbulentTemperatureWallFunction::computeQpResidual()
     velocity(2) = (*_w_var)(current_argument, state);
 
   // Compute the velocity and direction of the velocity component that is parallel to the wall
-  ADReal parallel_speed = (velocity - velocity * (fi.normal()) * (fi.normal())).norm();
+  const ADReal parallel_speed = (velocity - velocity * (fi.normal()) * (fi.normal())).norm();
 
-  // Coputing friction velocity
+  // Computing friction velocity
   ADReal u_tau;
   if (_linearized_yplus)
   {
     const ADReal a_c = 1 / NS::von_karman_constant;
     const ADReal b_c =
         1 / NS::von_karman_constant *
-        (std::log(NS::E_turb_constant * wall_dist / _mu(current_argument, state)) + 1.0);
+        (std::log(NS::E_turb_constant * wall_dist / mu) + 1.0);
     const ADReal c_c = parallel_speed;
     u_tau = (-b_c + std::sqrt(std::pow(b_c, 2) + 4.0 * a_c * c_c)) / (2.0 * a_c);
   }
   else
     u_tau = NS::findUStar(
-        _mu(current_argument, state), _rho(current_argument, state), parallel_speed, wall_dist);
+        mu, rho, parallel_speed, wall_dist);
 
   // Computing non-dimensional wall distance
-  ADReal y_plus = wall_dist * u_tau * _rho(current_argument, state) / _mu(current_argument, state);
+  const ADReal y_plus = wall_dist * u_tau * rho / mu;
 
   ADReal alpha;
   if (y_plus <= 5.0) // sub-laminar layer
   {
-    alpha = _kappa(current_argument, state) /
-            (_rho(current_argument, state) * _cp(current_argument, state));
+    alpha = kappa /
+            (rho * cp);
   }
   else if (y_plus >= 30.0) // log-layer
   {
-    auto Pr = _cp(current_argument, state) * _mu(current_argument, state) /
-              _kappa(current_argument, state);
-    auto Pr_ratio = Pr / _Pr_t(current_argument, state);
-    auto jayatilleke_P =
+    const auto Pr = cp * mu /
+              kappa;
+    const auto Pr_ratio = Pr / _Pr_t(current_argument, state);
+    const auto jayatilleke_P =
         9.24 * (std::pow(Pr_ratio, 0.75) - 1.0) * (1.0 + 0.28 * std::exp(-0.007 * Pr_ratio));
-    auto wall_scaling =
+    const auto wall_scaling =
         1.0 / NS::von_karman_constant * std::log(NS::E_turb_constant * y_plus) + jayatilleke_P;
     alpha = u_tau * wall_dist / (_Pr_t(current_argument, state) * wall_scaling);
   }
   else // buffer layer
   {
-    auto alpha_lam = _kappa(current_argument, state) /
-                     (_rho(current_argument, state) * _cp(current_argument, state));
-    auto Pr = _cp(current_argument, state) * _mu(current_argument, state) /
-              _kappa(current_argument, state);
-    auto Pr_ratio = Pr / _Pr_t(current_argument, state);
-    auto jayatilleke_P =
+    const auto alpha_lam = kappa /
+                     (rho * cp);
+    const auto Pr = cp * mu /
+              kappa;
+    const auto Pr_ratio = Pr / _Pr_t(current_argument, state);
+    const auto jayatilleke_P =
         9.24 * (std::pow(Pr_ratio, 0.75) - 1.0) * (1.0 + 0.28 * std::exp(-0.007 * Pr_ratio));
-    auto wall_scaling =
+    const auto wall_scaling =
         1.0 / NS::von_karman_constant * std::log(NS::E_turb_constant * y_plus) + jayatilleke_P;
-    auto alpha_turb = u_tau * wall_dist / (_Pr_t(current_argument, state) * wall_scaling);
-    auto blending_function = (y_plus - 5.0) / 25.0;
+    const auto alpha_turb = u_tau * wall_dist / (_Pr_t(current_argument, state) * wall_scaling);
+    const auto blending_function = (y_plus - 5.0) / 25.0;
     alpha = blending_function * alpha_turb + (1.0 - blending_function) * alpha_lam;
   }
 
   const auto face_arg = singleSidedFaceArg();
-  return -_rho(current_argument, state) * _cp(current_argument, state) * alpha *
+  return -rho * cp * alpha *
          (_T_w(face_arg, state) - _var(current_argument, state)) / wall_dist;
 }
