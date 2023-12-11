e8954bf293644964a70874b8f2b63c06551b3591
maxnezddyur <maxnezdyur@gmail.com>
2023-12-10 18:11:55 -0700

    Extract POD portion out of POD Mapping (#26292)

---
+++ modules/stochastic_tools/include/utils/POD.h
@@ -0,0 +1,58 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "ParallelSolutionStorage.h"
+#include <slepcsvd.h>
+#include "libmesh/petsc_vector.h"
+#include <MooseTypes.h>
+/**
+ * Class which computes a Proper Orthogonal Decomposition (POD) for snapshots
+ * stored in ParallelSolutionStorage.
+ */
+class POD
+{
+public:
+  POD(const ParallelSolutionStorage * const _parallel_storage,
+      const std::string & _extra_slepc_options,
+      const Parallel::Communicator & communicator);
+  /**
+   *
+   * @param vname Variable name to extract snapshot data
+   * @param left_basis_functions Vector for left basis function
+   * @param right_basis_functions Vector for right basis functions
+   * @param singular_values Vector for singular values
+   * @param num_modes Max number of modes to compute
+   * @param energy Energy threshold to determine the minimum number of modes
+   */
+  void computePOD(const VariableName & vname,
+                  std::vector<DenseVector<Real>> & left_basis_functions,
+                  std::vector<DenseVector<Real>> & right_basis_functions,
+                  std::vector<Real> & singular_values,
+                  const dof_id_type & num_modes,
+                  const Real & energy) const;
+
+private:
+  /**
+   * Determine the number of basis functions needed for a given variable based on the information
+   * on the singular values.
+   * @param singular_values Vector of singular values
+   * @param num_modes_compute Max number of modes to compute
+   * @param energy Energy threshold to determine the minimum number of modes
+   * @return dof_id_type Number of modes to extract
+   */
+  dof_id_type determineNumberOfModes(const std::vector<Real> & singular_values,
+                                     const dof_id_type num_modes_compute,
+                                     const Real & energy) const;
+
+  const ParallelSolutionStorage * const _parallel_storage;
+  const std::string & _extra_slepc_options;
+  const Parallel::Communicator & _communicator;
+};
