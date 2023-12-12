5327cf7e7946b97249ea8f1d65efa86ae24a8e33
Jason M Miller <Jason.Miller@inl.gov>
2023-12-12 06:52:28 -0700

    Add MOOSE_OPTIONS to configure
    Allow for MOOSE_OPTIONS to be passed as arguments which will be
    used during ./configure of MOOSE.
    
    Closes #26300

--- conda/template/build.sh
+++ conda/template/build.sh
@@ -3,6 +3,7 @@ set -eux
 export APPLICATION=<APPLICATION>
 export MOOSE_JOBS=<MOOSE_JOBS>
 export SKIP_DOCS=<SKIP_DOCS>
+export MOOSE_OPTIONS=<MOOSE_OPTIONS>
 export O_WORKDIR=`pwd`
 export INSTALL_DIR=${PREFIX}/${APPLICATION}
 
@@ -13,7 +14,7 @@ if [ "$SKIP_DOCS" == "True" ]; then
     export MOOSE_SKIP_DOCS="True"
 fi
 cd ${O_WORKDIR}<MOOSE>
-./configure --prefix=${INSTALL_DIR}
+./configure --prefix=${INSTALL_DIR} ${MOOSE_OPTIONS}
 cd ${O_WORKDIR}<IS_MOOSE>
 make -j <MOOSE_JOBS>
 make install -j <MOOSE_JOBS>
