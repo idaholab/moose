a3587f47f842e7c6694f7ec7ddfb1bc2439bd05f
maxnezddyur <maxnezdyur@gmail.com>
2023-12-13 04:21:52 -0700

    Refactor solutioncontainer (#26317)

---
+++ modules/stochastic_tools/include/reporters/SnapshotContainerBase.h
@@ -0,0 +1,62 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "GeneralReporter.h"
+#include "libmesh/petsc_vector.h"
+#include "libmesh/petsc_matrix.h"
+#include "NonlinearSystemBase.h"
+
+/**
+ * Base class for storing and managing numerical data like solutions, residuals, and Jacobians.
+ * The vectors are kept distributed with respect to the communicator of the application.
+ * The whole snapshot vector is stored. The saving frequency can be defined using the `execute_on`
+ * parameter.
+ */
+class SnapshotContainerBase : public GeneralReporter
+{
+public:
+  static InputParameters validParams();
+  SnapshotContainerBase(const InputParameters & parameters);
+  virtual void initialSetup() override;
+  virtual void initialize() override {}
+  virtual void execute() override;
+  virtual void finalize() override {}
+
+  /**
+   * Return the whole snapshot container
+   * @return const std::vector<std::unique_ptr<NumericVector<Number>>>&
+   */
+  const std::vector<std::unique_ptr<NumericVector<Number>>> & getContainer()
+  {
+    return _accumulated_data;
+  }
+
+  /**
+   * Return one of the stored solution vectors
+   * @param local_i The index of the locally stored solution vector
+   */
+  const std::unique_ptr<NumericVector<Number>> & getSnapshot(unsigned int local_i) const;
+
+protected:
+  /**
+   * Clone the current snapshot vector.
+   * @return std::unique_ptr<NumericVector<Number>>
+   */
+  virtual std::unique_ptr<NumericVector<Number>> cloneSnapshot() = 0;
+
+  /// Dynamic container for snapshot vectors. We store pointers to make sure that the change in size
+  /// comes with little overhead. This is a reference because we need it to be restartable for
+  /// stochastic runs in batch mode.
+  std::vector<std::unique_ptr<NumericVector<Number>>> & _accumulated_data;
+
+  /// The nonlinear system's number whose solution shall be collected
+  const unsigned int _nonlinear_system_number;
+};
---
+++ modules/stochastic_tools/src/reporters/SnapshotContainerBase.C
@@ -0,0 +1,54 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "SnapshotContainerBase.h"
+
+InputParameters
+SnapshotContainerBase::validParams()
+{
+  InputParameters params = GeneralReporter::validParams();
+
+  params.addParam<NonlinearSystemName>(
+      "nonlinear_system_name",
+      "nl0",
+      "Option to select which nonlinear system's solution shall be stored.");
+  return params;
+}
+
+SnapshotContainerBase::SnapshotContainerBase(const InputParameters & parameters)
+  : GeneralReporter(parameters),
+    _accumulated_data(declareRestartableData<std::vector<std::unique_ptr<NumericVector<Number>>>>(
+        "accumulated_snapshots")),
+    _nonlinear_system_number(
+        _fe_problem.nlSysNum(getParam<NonlinearSystemName>("nonlinear_system_name")))
+{
+}
+
+void
+SnapshotContainerBase::initialSetup()
+{
+  _accumulated_data.clear();
+}
+
+const std::unique_ptr<NumericVector<Number>> &
+SnapshotContainerBase::getSnapshot(unsigned int local_i) const
+{
+  mooseAssert(local_i < _accumulated_data.size(),
+              "The container only has (" + std::to_string(_accumulated_data.size()) +
+                  ") solutions so we cannot find any with index (" + std::to_string(local_i) +
+                  ")!");
+  return _accumulated_data[local_i];
+}
+
+void
+SnapshotContainerBase::execute()
+{
+  // Store the cloned snapshot. Each derived class has to implement the cloneSnapshot() method.
+  _accumulated_data.push_back(std::move(cloneSnapshot()));
+}
