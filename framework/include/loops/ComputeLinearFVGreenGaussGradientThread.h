48273e3e740b5154caaff5386b7a0240b703bb44
Peter German <grmnptr@gmail.com>
2024-01-10 15:07:21 -0700

    Add skeleton for GGG thread. (#25722)

---
+++ framework/include/loops/ComputeLinearFVGreenGaussGradientThread.h
@@ -0,0 +1,50 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "MooseTypes.h"
+#include "MooseMesh.h"
+
+// libmesh
+#include "libmesh/elem_range.h"
+#include "libmesh/threads.h"
+
+class FEProblemBase;
+class LinearFVFluxKernel;
+
+class ComputeLinearFVGreenGaussGradientThread
+{
+public:
+  ComputeLinearFVGreenGaussGradientThread(FEProblemBase & fe_problem,
+                                          const unsigned int linear_system_num,
+                                          MooseLinearVariableFV<Real> * var);
+
+  // Splitting Constructor
+  ComputeLinearFVGreenGaussGradientThread(ComputeLinearFVGreenGaussGradientThread & x,
+                                          Threads::split split);
+  using FaceInfoRange = StoredRange<MooseMesh::const_face_info_iterator, const FaceInfo *>;
+  void operator()(const FaceInfoRange & range);
+  void join(const ComputeLinearFVGreenGaussGradientThread & /*y*/);
+
+protected:
+  void onInternalFace(const FaceInfo & face_info);
+  void onBoundaryFace(const FaceInfo & face_info);
+
+  FEProblemBase & _fe_problem;
+  const unsigned int _linear_system_number;
+
+  const libMesh::LinearImplicitSystem & _linear_system;
+
+  // Thread ID
+  THREAD_ID _tid;
+
+  /// Pointer to the linear finite volume variable
+  MooseLinearVariableFV<Real> * _var;
+};
---
+++ framework/src/loops/ComputeLinearFVGreenGaussGradientThread.C
@@ -0,0 +1,84 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "ComputeLinearFVGreenGaussGradientThread.h"
+#include "LinearSystem.h"
+
+ComputeLinearFVGreenGaussGradientThread::ComputeLinearFVGreenGaussGradientThread(
+    FEProblemBase & fe_problem,
+    const unsigned int linear_system_num,
+    MooseLinearVariableFV<Real> * var)
+  : _fe_problem(fe_problem),
+    _linear_system_number(linear_system_num),
+    _linear_system(libMesh::cast_ref<libMesh::LinearImplicitSystem &>(
+        _fe_problem.getLinearSystem(_linear_system_number).system())),
+    _var(var)
+{
+}
+
+// Splitting Constructor
+ComputeLinearFVGreenGaussGradientThread::ComputeLinearFVGreenGaussGradientThread(
+    ComputeLinearFVGreenGaussGradientThread & x, Threads::split /*split*/)
+  : _fe_problem(x._fe_problem),
+    _linear_system_number(x._linear_system_number),
+    _linear_system(x._linear_system),
+    _var(x._var)
+{
+}
+
+void
+ComputeLinearFVGreenGaussGradientThread::operator()(const FaceInfoRange & range)
+{
+  ParallelUniqueId puid;
+  _tid = puid.id;
+
+  // Iterate over all the elements in the range
+  for (const auto & face_info : range)
+  {
+    const auto current_face_type =
+        face_info->faceType(std::make_pair(_var->number(), _var->sys().number()));
+
+    if (current_face_type == FaceInfo::VarFaceNeighbors::BOTH)
+      onInternalFace(*face_info);
+    else if (current_face_type == FaceInfo::VarFaceNeighbors::ELEM ||
+             current_face_type == FaceInfo::VarFaceNeighbors::NEIGHBOR)
+      onBoundaryFace(*face_info);
+  }
+}
+
+void
+ComputeLinearFVGreenGaussGradientThread::onInternalFace(const FaceInfo & face_info)
+{
+  const auto dof_id_elem = face_info.elemInfo()->dofIndices()[_var->sys().number()][_var->number()];
+  const auto dof_id_neighbor =
+      face_info.neighborInfo()->dofIndices()[_var->sys().number()][_var->number()];
+
+  const auto & solution = *_linear_system.solution;
+  const auto face_value = Moose::FV::linearInterpolation(
+      solution(dof_id_elem), solution(dof_id_neighbor), face_info, true);
+
+  const auto contribution = face_info.normal() * face_value;
+
+  const auto & grad_container = _var.gradientContainer();
+  for (const auto i : index_range(normal))
+  {
+    grad_container[i]->add(dof_id_elem, contribution(i));
+    grad_container[i]->add(dof_id_neighbor, -contribution(i));
+  }
+}
+
+void
+ComputeLinearFVGreenGaussGradientThread::onBoundaryFace(const FaceInfo & face_info)
+{
+}
+
+void
+ComputeLinearFVGreenGaussGradientThread::join(const ComputeLinearFVGreenGaussGradientThread & /*y*/)
+{
+}
