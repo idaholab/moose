63ed0ac61cb5702b7bb1158003f2d81b35f67266
Logan Harbour <loganhharbour@gmail.com>
2024-01-18 07:57:48 -0700

    Use Moose::main, update registration
    refs idaholab/moose#25994

--- modules/subchannel/src/main.C
+++ modules/subchannel/src/main.C
@@ -12,30 +12,13 @@
 /*               See COPYRIGHT for full restrictions                */
 /********************************************************************/
 
-#include "SubChannelApp.h"
-#include "MooseInit.h"
-#include "Moose.h"
-#include "MooseApp.h"
-#include "AppFactory.h"
+#include "SubChannelTestApp.h"
+#include "MooseMain.h"
 
-// Create a performance log
-PerfLog Moose::perf_log("subchannel");
-
-// Begin the main program.
 int
 main(int argc, char * argv[])
 {
-  // Initialize MPI, solvers and MOOSE
-  MooseInit init(argc, argv);
-
-  // Register this application's MooseApp and any it depends on
-  SubChannelApp::registerApps();
-
-  // Create an instance of the application and store it in a smart pointer for easy cleanup
-  std::shared_ptr<MooseApp> app = AppFactory::createAppShared("SubChannelApp", argc, argv);
-
-  // Execute the application
-  app->run();
+  Moose::main<SubChannelTestApp>(argc, argv);
 
   return 0;
 }
