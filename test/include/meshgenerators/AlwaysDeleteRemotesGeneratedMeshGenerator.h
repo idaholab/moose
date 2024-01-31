aa1fa7a41aa84da057c0f4e686bb9b24b9348f5f
Alex Lindsay <alexander.lindsay@inl.gov>
2024-01-31 15:56:44 -0800

    Have test generator inherit directly from GeneratedMeshGenerator
    This allows not re-writing the class definition, validParams, nor
    the constructor. Unfortunately we still have to re-write `generate`
    since we have to insert our remote element deletion line

--- test/include/meshgenerators/AlwaysDeleteRemotesGeneratedMeshGenerator.h
+++ test/include/meshgenerators/AlwaysDeleteRemotesGeneratedMeshGenerator.h
@@ -9,13 +9,13 @@
 
 #pragma once
 
-#include "MeshGenerator.h"
+#include "GeneratedMeshGenerator.h"
 #include "MooseEnum.h"
 
 /**
  * Generates a line, square, or cube mesh with uniformly spaced or biased elements.
  */
-class AlwaysDeleteRemotesGeneratedMeshGenerator : public MeshGenerator
+class AlwaysDeleteRemotesGeneratedMeshGenerator : public GeneratedMeshGenerator
 {
 public:
   static InputParameters validParams();
@@ -23,41 +23,4 @@ public:
   AlwaysDeleteRemotesGeneratedMeshGenerator(const InputParameters & parameters);
 
   std::unique_ptr<MeshBase> generate() override;
-
-protected:
-  /// The dimension of the mesh
-  MooseEnum _dim;
-
-  /// Number of elements in x, y, z direction
-  unsigned int &_nx, &_ny, &_nz;
-
-  /// The min/max values for x,y,z component
-  Real &_xmin, &_xmax, &_ymin, &_ymax, &_zmin, &_zmax;
-
-  /// Whether or not subdomain_ids parameter is set
-  bool _has_subdomain_ids;
-
-  /**
-   * All of the libmesh build_line/square/cube routines support an
-   * option to grade the mesh into the boundaries according to the
-   * spacing of the Gauss-Lobatto quadrature points.  Defaults to
-   * false, and cannot be used in conjunction with x, y, and z
-   * biasing.
-   */
-  const bool _gauss_lobatto_grid;
-
-  /**
-   * The amount by which to bias the cells in the x,y,z directions.
-   * Must be in the range 0.5 <= _bias_x <= 2.0.
-   * _bias_x < 1 implies cells are shrinking in the x-direction.
-   * _bias_x==1 implies no bias (original mesh unchanged).
-   * _bias_x > 1 implies cells are growing in the x-direction.
-   */
-  const Real _bias_x, _bias_y, _bias_z;
-
-  /// prefix string for the boundary names
-  const std::string _boundary_name_prefix;
-
-  /// offset that is added to the boundary IDs
-  const boundary_id_type _boundary_id_offset;
 };
--- test/src/meshgenerators/AlwaysDeleteRemotesGeneratedMeshGenerator.C
+++ test/src/meshgenerators/AlwaysDeleteRemotesGeneratedMeshGenerator.C
@@ -27,89 +27,13 @@ registerMooseObject("MooseTestApp", AlwaysDeleteRemotesGeneratedMeshGenerator);
 InputParameters
 AlwaysDeleteRemotesGeneratedMeshGenerator::validParams()
 {
-  InputParameters params = MeshGenerator::validParams();
-
-  MooseEnum elem_types(LIST_GEOM_ELEM); // no default
-
-  MooseEnum dims("1=1 2 3");
-  params.addRequiredParam<MooseEnum>("dim", dims, "The dimension of the mesh to be generated");
-
-  params.addParam<unsigned int>("nx", 1, "Number of elements in the X direction");
-  params.addParam<unsigned int>("ny", 1, "Number of elements in the Y direction");
-  params.addParam<unsigned int>("nz", 1, "Number of elements in the Z direction");
-  params.addParam<Real>("xmin", 0.0, "Lower X Coordinate of the generated mesh");
-  params.addParam<Real>("ymin", 0.0, "Lower Y Coordinate of the generated mesh");
-  params.addParam<Real>("zmin", 0.0, "Lower Z Coordinate of the generated mesh");
-  params.addParam<Real>("xmax", 1.0, "Upper X Coordinate of the generated mesh");
-  params.addParam<Real>("ymax", 1.0, "Upper Y Coordinate of the generated mesh");
-  params.addParam<Real>("zmax", 1.0, "Upper Z Coordinate of the generated mesh");
-  params.addParam<MooseEnum>("elem_type",
-                             elem_types,
-                             "The type of element from libMesh to "
-                             "generate (default: linear element for "
-                             "requested dimension)");
-  params.addParam<std::vector<SubdomainID>>("subdomain_ids",
-                                            "Subdomain IDs for each element, default to all zero");
-
-  params.addParam<bool>(
-      "gauss_lobatto_grid",
-      false,
-      "Grade mesh into boundaries according to Gauss-Lobatto quadrature spacing.");
-  params.addRangeCheckedParam<Real>(
-      "bias_x",
-      1.,
-      "bias_x>=0.5 & bias_x<=2",
-      "The amount by which to grow (or shrink) the cells in the x-direction.");
-  params.addRangeCheckedParam<Real>(
-      "bias_y",
-      1.,
-      "bias_y>=0.5 & bias_y<=2",
-      "The amount by which to grow (or shrink) the cells in the y-direction.");
-  params.addRangeCheckedParam<Real>(
-      "bias_z",
-      1.,
-      "bias_z>=0.5 & bias_z<=2",
-      "The amount by which to grow (or shrink) the cells in the z-direction.");
-
-  params.addParam<std::string>("boundary_name_prefix",
-                               "If provided, prefix the built in boundary names with this string");
-  params.addParam<boundary_id_type>(
-      "boundary_id_offset", 0, "This offset is added to the generated boundary IDs");
-
-  params.addParam<std::vector<ExtraElementIDName>>("extra_element_integers",
-                                                   "Names of extra element integers");
-
-  params.addClassDescription(
-      "Create a line, square, or cube mesh with uniformly spaced or biased elements.");
-
-  return params;
+  return GeneratedMeshGenerator::validParams();
 }
 
 AlwaysDeleteRemotesGeneratedMeshGenerator::AlwaysDeleteRemotesGeneratedMeshGenerator(
     const InputParameters & parameters)
-  : MeshGenerator(parameters),
-    _dim(getParam<MooseEnum>("dim")),
-    _nx(declareMeshProperty("num_elements_x", getParam<unsigned int>("nx"))),
-    _ny(declareMeshProperty("num_elements_y", getParam<unsigned int>("ny"))),
-    _nz(declareMeshProperty("num_elements_z", getParam<unsigned int>("nz"))),
-    _xmin(declareMeshProperty("xmin", getParam<Real>("xmin"))),
-    _xmax(declareMeshProperty("xmax", getParam<Real>("xmax"))),
-    _ymin(declareMeshProperty("ymin", getParam<Real>("ymin"))),
-    _ymax(declareMeshProperty("ymax", getParam<Real>("ymax"))),
-    _zmin(declareMeshProperty("zmin", getParam<Real>("zmin"))),
-    _zmax(declareMeshProperty("zmax", getParam<Real>("zmax"))),
-    _has_subdomain_ids(isParamValid("subdomain_ids")),
-    _gauss_lobatto_grid(getParam<bool>("gauss_lobatto_grid")),
-    _bias_x(getParam<Real>("bias_x")),
-    _bias_y(getParam<Real>("bias_y")),
-    _bias_z(getParam<Real>("bias_z")),
-    _boundary_name_prefix(isParamValid("boundary_name_prefix")
-                              ? getParam<std::string>("boundary_name_prefix") + "_"
-                              : ""),
-    _boundary_id_offset(getParam<boundary_id_type>("boundary_id_offset"))
+  : GeneratedMeshGenerator(parameters)
 {
-  if (_gauss_lobatto_grid && (_bias_x != 1.0 || _bias_y != 1.0 || _bias_z != 1.0))
-    mooseError("Cannot apply both Gauss-Lobatto mesh grading and biasing at the same time.");
 }
 
 std::unique_ptr<MeshBase>
