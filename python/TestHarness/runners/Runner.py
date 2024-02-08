3520f932fe9be1e7784bb6621947950dc57ec7ff
Logan Harbour <loganhharbour@gmail.com>
2024-02-08 11:22:25 -0700

    Separate out how commands are ran
    This is needed so that we can create a Runner for PBS/slurm
    
    refs #0

---
+++ python/TestHarness/runners/Runner.py
@@ -0,0 +1,70 @@
+#* This file is part of the MOOSE framework
+#* https://www.mooseframework.org
+#*
+#* All rights reserved, see COPYRIGHT for full restrictions
+#* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+#*
+#* Licensed under LGPL 2.1, please see LICENSE for details
+#* https://www.gnu.org/licenses/lgpl-2.1.html
+
+class Runner:
+    """
+    Base class for running a process via a command.
+
+    Used within the Tester to actually run a test. We need
+    this specialized so that we can either run things locally
+    or externally (i.e., PBS, slurm, etc on HPC)
+    """
+    def __init__(self, tester):
+        # The tester that we're going to run
+        self.tester = tester
+        # The job's exit code, should be set after wait()
+        self.exit_code = None
+
+    def spawn(self, cmd, cwd, timer):
+        """
+        Spawns the process.
+
+        Wait for the process to complete with wait().
+
+        Should be overridden.
+        """
+        pass
+
+    def wait(self, timer):
+        """
+        Waits for the process started with spawn() to complete.
+
+        Should be overridden.
+        """
+        pass
+
+    def kill(self):
+        """
+        Kills the process started with spawn()
+
+        Should be overridden.
+        """
+        pass
+
+    def getOutput(self):
+        """
+        Gets the combined output of the process.
+
+        Should be overridden.
+        """
+        return None
+
+    def getExitCode(self):
+        """
+        Gets the error code of the process.
+        """
+        return self.exit_code
+
+    def isOutputReady(self):
+        """
+        Whether or not the output is ready for reading.
+
+        Should be overridden.
+        """
+        return None
