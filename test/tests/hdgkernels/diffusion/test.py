a4e51f73225c647a98bbac49588c74cc8902f1fb
Alex Lindsay <alexander.lindsay@inl.gov>
2024-03-13 13:52:56 -0700

    Add tensor-product element MMS test

--- test/tests/hdgkernels/diffusion/test.py
+++ test/tests/hdgkernels/diffusion/test.py
@@ -2,13 +2,26 @@ import mms
 import unittest
 from mooseutils import fuzzyEqual, fuzzyAbsoluteEqual
 
-class TestLagrange(unittest.TestCase):
+class TestLagrangeTri(unittest.TestCase):
     def test(self):
         df1 = mms.run_spatial('mms-diffusion.i', 5, "--error", "--error-unused", y_pp=['L2u'])
 
         fig = mms.ConvergencePlot(xlabel='Element Size ($h$)', ylabel='$L_2$ Error')
         fig.plot(df1, label=['L2u'], marker='o', markersize=8, num_fitted_points=3, slope_precision=1)
-        fig.save('lagrange.png')
+        fig.save('lagrange-tri.png')
+        for key,value in fig.label_to_slope.items():
+            print("%s, %f" % (key, value))
+            self.assertTrue(fuzzyAbsoluteEqual(value, 2., .1))
+
+class TestLagrangeQuad(unittest.TestCase):
+    def test(self):
+        df1 = mms.run_spatial('mms-diffusion.i', 6, "--error", "--error-unused",
+                              "Mesh/gen/elem_type=QUAD9",
+                              y_pp=['L2u'])
+
+        fig = mms.ConvergencePlot(xlabel='Element Size ($h$)', ylabel='$L_2$ Error')
+        fig.plot(df1, label=['L2u'], marker='o', markersize=8, num_fitted_points=3, slope_precision=1)
+        fig.save('lagrange-quad.png')
         for key,value in fig.label_to_slope.items():
             print("%s, %f" % (key, value))
             self.assertTrue(fuzzyAbsoluteEqual(value, 2., .1))
