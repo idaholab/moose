0c0052741e1f93c4cc3ca6786caaa724e4b867f3
Jörg Meier <joerg.meier@gruner.ch>
2024-03-14 07:33:05 +0100

    initial commit for branch 'TimedSubdomainModifier' refs #26952 includes: - TimedSubdomainModifier - TimedElementSubdomainModifier - DelimitedFileReaderOfString (modified copy of DelimitedFileReader)
    Now supporting explicit naming/indexing of the columns used in the CSV file.
    
    removed tab char
    
    applied style patch
    
    exposing comment char
    
    style patch
    
    Apply some suggestions from code review
    
    Co-authored-by: Guillaume Giudicelli <guillaume.giudicelli@gmail.com>
    
    style changes and using 'real' for storing 'time'
    
    docstrings as requested
    
    style changes by job precheck
    
    style change as requested by job precheck
    
    applied suggested changes by GiudGiud
    
    switching to 'auto'; cleaning up variable renames
    
    applied style patch
    
    switching to SubdomainName for blocks_from and blocks_to
    
    code comments

---
+++ framework/src/userobjects/TimedElementSubdomainModifier.C
@@ -0,0 +1,86 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+// Base class to move elements to a specific subdomain at the given times.
+
+#include "TimedElementSubdomainModifier.h"
+#include "DelimitedFileReaderOfString.h"
+#include "MooseMesh.h"
+
+registerMooseObject("MooseApp", TimedElementSubdomainModifier);
+
+InputParameters
+TimedElementSubdomainModifier::validParams()
+{
+  InputParameters params = ElementSubdomainModifier::validParams();
+
+  params.addParam<std::vector<Real>>("times", "The times of the subdomain modifications.");
+
+  return params;
+}
+
+TimedElementSubdomainModifier::TimedElementSubdomainModifier(const InputParameters & parameters)
+  : ElementSubdomainModifier(parameters)
+{
+}
+
+void
+TimedElementSubdomainModifier::initialize()
+{
+  // state variables
+  _last_t = -1;
+  _current_step = _t_step - 1;
+
+  // ask for all times (must NOT be sorted)
+  const auto _times = onGetTimes();
+  const auto n = _times.size();
+
+  // copy data to local storage
+  _times_and_indices.resize(n);
+  for (size_t i = 0; i < n; ++i)
+  {
+    _times_and_indices[i].time = _times[i];
+    _times_and_indices[i].index = i;
+  };
+
+  // sort the times
+  std::sort(_times_and_indices.begin(), _times_and_indices.end());
+}
+
+std::vector<Real>
+TimedElementSubdomainModifier::onGetTimes()
+{
+  std::vector<Real> _times;
+  return _times;
+}
+
+SubdomainID
+TimedElementSubdomainModifier::onComputeSubdomainID(Real /* t_from_exclusive */,
+                                                    Real /* t_to_inclusive */)
+{
+  const auto _subdomain_id = _current_elem->subdomain_id();
+  return _subdomain_id;
+}
+
+SubdomainID
+TimedElementSubdomainModifier::computeSubdomainID()
+{
+  // did we advance to the next step?
+  if (_current_step != _t_step)
+  {
+    _last_t = _current_t;
+    _current_t = _t;
+    _current_step = _t_step;
+  }
+
+  // get the new subdomain-id of the current element; provide the timespan to be considered
+  const auto _subdomain_id = onComputeSubdomainID(_last_t, _current_t);
+
+  return _subdomain_id;
+}
---
+++ framework/src/userobjects/TimedSubdomainModifier.C
@@ -0,0 +1,300 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "TimedSubdomainModifier.h"
+#include "TimedElementSubdomainModifier.h"
+#include "DelimitedFileReaderOfString.h"
+#include "MooseMesh.h"
+
+registerMooseObject("MooseApp", TimedSubdomainModifier);
+
+InputParameters
+TimedSubdomainModifier::validParams()
+{
+  InputParameters params = TimedElementSubdomainModifier::validParams();
+
+  // parameters for direct input (additionally to 'times')
+  params.addParam<std::vector<SubdomainName>>("blocks_from",
+                                              "Names or ids of the 'old' block(s), to be renamed.");
+  params.addParam<std::vector<SubdomainName>>("blocks_to", "Names or ids of the 'new' block.");
+  params.addParamNamesToGroup("times blocks_from blocks_to", "Direct subdomain changes data input");
+
+  // parameters for file-based data supply
+  params.addParam<FileName>("data_file", "File holding CSV data");
+  params.addParam<bool>("header",
+                        "Indicates whether the file contains a header with the column names");
+  params.addParam<std::string>("delimiter", ",", "Delimiter used to parse the file");
+  params.addParam<std::string>("comment", "#", "Comment character used to parse the file");
+  params.addParam<size_t>(
+      "time_column_index", 0, "Zero-based index of the time column. Default is '0'.");
+  params.addParam<size_t>(
+      "blocks_from_column_index", 1, "Zero-based index of the blocks_from column. Default is '1'.");
+  params.addParam<size_t>(
+      "blocks_to_column_index", 2, "Zero-based index of the blocks_to column. Default is '2'.");
+  params.addParam<std::string>("time_column_text", "Header text of the time column.");
+  params.addParam<std::string>("blocks_from_column_text", "Header text of the blocks_from column.");
+  params.addParam<std::string>("blocks_to_column_text", "Header text of the blocks_to column.");
+  params.addParamNamesToGroup(
+      "data_file header delimiter comment time_column_index blocks_from_column_index "
+      "blocks_to_column_index time_column_text blocks_from_column_text blocks_to_column_text",
+      "Subdomain change data input from CSV file");
+
+  params.addClassDescription("Renames entire blocks at user-specified times.");
+
+  return params;
+}
+
+TimedSubdomainModifier::TimedSubdomainModifier(const InputParameters & parameters)
+  : TimedElementSubdomainModifier(parameters)
+{
+  // determine function arguments
+
+  // helper variables for parameter consistency checks
+  const auto from_data_file =
+      isParamValid("data_file") + isParamValid("header") + isParamValid("delimiter") +
+      isParamValid("comment") + isParamValid("time_column_index") +
+      isParamValid("blocks_from_column_index") + isParamValid("blocks_to_column_index") +
+      isParamValid("time_column_text") + isParamValid("blocks_from_column_text") +
+      isParamValid("blocks_to_column_text");
+
+  const auto from_data_File_needs_header = isParamValid("time_column_text") +
+                                           isParamValid("blocks_from_column_text") +
+                                           isParamValid("blocks_to_column_text");
+
+  const auto from_parameters =
+      isParamValid("times") + isParamValid("blocks_from") + isParamValid("blocks_to");
+
+  // Check parameter set for inconsistencies
+  const auto from_source_count = ((from_data_file > 0) + (from_parameters > 0));
+  if (from_source_count != 1)
+  {
+    mooseError("Data on times and blocks must be provided either via a CSV file ('data_file' and "
+               "corresponding blocks), or via direct parameter input ('times', 'blocks_from', and "
+               "'blocks_to').");
+  };
+
+  if (from_parameters > 0)
+  {
+    if (from_parameters != 3)
+    {
+      mooseError("All parameters 'times', and 'blocks_from', and 'blocks_to' must be specified.");
+    };
+    buildFromParameters();
+  }
+  else if (from_data_file > 0)
+  {
+    if ((isParamSetByUser("time_column_index") + isParamSetByUser("time_column_text")) > 1)
+      mooseError(
+          "The parameters 'time_column_index', and 'time_column_text' are mutual exclusive.");
+    if ((isParamSetByUser("blocks_from_column_index") +
+         isParamSetByUser("blocks_from_column_text")) > 1)
+      mooseError("The parameters 'blocks_from_column_index', and 'blocks_from_column_text' are "
+                 "mutual exclusive.");
+    if ((isParamSetByUser("blocks_to_column_index") + isParamSetByUser("blocks_to_column_text")) >
+        1)
+      mooseError("The parameters 'blocks_to_column_index', and 'blocks_to_column_text' are mutual "
+                 "exclusive.");
+    if ((from_data_File_needs_header > 0) && (isParamSetByUser("header") == 0))
+      mooseError("Header flag must be active if columns are to be found via header.");
+
+    buildFromFile();
+  }
+  else
+  {
+    mooseError("Unknown data source. Are you missing a parameter? Did you misspell one?");
+  };
+}
+
+void
+TimedSubdomainModifier::buildFromParameters()
+{
+
+  _times = this->template getParam<std::vector<Real>>("times");
+  size_t n = _times.size();
+
+  const auto raw_from = getParam<std::vector<SubdomainName>>("blocks_from");
+  const auto raw_to = getParam<std::vector<SubdomainName>>("blocks_to");
+
+  if (raw_from.size() != n)
+    mooseError(
+        "Parameter 'blocks_from' must contain the same number of items as parameter 'times'.");
+  if (raw_to.size() != n)
+    mooseError("Parameter 'blocks_to' must contain the same number of items as parameter 'times'.");
+
+  _blocks_from.resize(n);
+  _blocks_to.resize(n);
+
+  const std::shared_ptr<MooseMesh> _mesh = _app.actionWarehouse().mesh();
+  for (const auto i : index_range(raw_from))
+  {
+    _blocks_from[i] = _mesh->getSubdomainID(raw_from[i]);
+    _blocks_to[i] = _mesh->getSubdomainID(raw_to[i]);
+  };
+}
+
+void
+TimedSubdomainModifier::buildFromFile()
+{
+  const auto _file_name = getParam<FileName>("data_file");
+
+  /// Flag indicating if the file contains a header.
+  auto _header_flag = MooseUtils::DelimitedFileReaderOfString::HeaderFlag::OFF;
+  if (isParamValid("header"))
+  {
+    _header_flag = getParam<bool>("header")
+                       ? MooseUtils::DelimitedFileReaderOfString::HeaderFlag::ON
+                       : MooseUtils::DelimitedFileReaderOfString::HeaderFlag::OFF;
+  };
+
+  std::string _delimiter = ",";
+  if (isParamValid("delimiter"))
+  {
+    _delimiter = getParam<std::string>("delimiter");
+  };
+
+  std::string _comment = ",";
+  if (isParamValid("comment"))
+  {
+    _comment = getParam<std::string>("comment");
+  };
+
+  MooseUtils::DelimitedFileReaderOfString file(_file_name);
+
+  file.setHeaderFlag(_header_flag);
+  file.setDelimiter(_delimiter);
+  file.setComment(_comment);
+  file.read();
+
+  size_t _time_column = 0;
+  if (isParamValid("time_column_text"))
+  {
+    const auto s = getParam<std::string>("time_column_text");
+    const auto _names = file.getNames();
+    const auto it = find(_names.begin(), _names.end(), s);
+    if (it == _names.end())
+      mooseError("Could not find '", s, "' in header of file ", _file_name, ".");
+    _time_column = std::distance(_names.begin(), it);
+  }
+  else if (isParamValid("time_column_index"))
+  {
+    _time_column = getParam<size_t>("time_column_index");
+  };
+
+  size_t _blocks_from_column = 1;
+  if (isParamValid("blocks_from_column_text"))
+  {
+    const auto s = getParam<std::string>("blocks_from_column_text");
+    const auto _names = file.getNames();
+    const auto it = find(_names.begin(), _names.end(), s);
+    if (it == _names.end())
+      mooseError("Could not find '", s, "' in header of file ", _file_name, ".");
+    _blocks_from_column = std::distance(_names.begin(), it);
+  }
+  else if (isParamValid("blocks_from_column_index"))
+  {
+    _blocks_from_column = getParam<size_t>("blocks_from_column_index");
+  };
+
+  size_t _blocks_to_column = 2;
+  if (isParamValid("blocks_to_column_text"))
+  {
+    const auto s = getParam<std::string>("blocks_to_column_text");
+    const auto _names = file.getNames();
+    const auto it = find(_names.begin(), _names.end(), s);
+    if (it == _names.end())
+      mooseError("Could not find '", s, "' in header of file ", _file_name, ".");
+    _blocks_to_column = std::distance(_names.begin(), it);
+  }
+  else if (isParamValid("blocks_to_column_index"))
+  {
+    _blocks_to_column = getParam<size_t>("blocks_to_column_index");
+  };
+
+  const auto max_needed_column_index =
+      std::max({_time_column, _blocks_from_column, _blocks_to_column});
+
+  const auto data = file.getData();
+
+  if (data.size() < max_needed_column_index)
+    mooseError("data must contain at least " + std::to_string(max_needed_column_index) +
+                   " columns in file ",
+               _file_name);
+
+  const auto strTimes = data[_time_column];
+  const auto strBlockFrom = data[_blocks_from_column];
+  const auto strBlockTo = data[_blocks_to_column];
+
+  const auto n_rows = strTimes.size();
+
+  // some sanity checks
+  if (n_rows == 0)
+    mooseError("empty sequence in file ", _file_name);
+  if (n_rows != strBlockFrom.size())
+    mooseError("inconsistent data in ", _file_name);
+  if (n_rows != strBlockTo.size())
+    mooseError("inconsistent data in ", _file_name);
+
+  // resize variables to fit the data
+  _times.resize(n_rows);
+  _blocks_from.resize(n_rows);
+  _blocks_to.resize(n_rows);
+
+  // parse the data given by the user
+  const std::shared_ptr<MooseMesh> _mesh = _app.actionWarehouse().mesh();
+  std::transform(
+      strTimes.begin(), strTimes.end(), _times.begin(), [](std::string x) { return std::stod(x); });
+  std::transform(strBlockFrom.begin(),
+                 strBlockFrom.end(),
+                 _blocks_from.begin(),
+                 [_mesh](std::string x) { return _mesh->getSubdomainID(x); });
+  std::transform(strBlockTo.begin(),
+                 strBlockTo.end(),
+                 _blocks_to.begin(),
+                 [_mesh](std::string x) { return _mesh->getSubdomainID(x); });
+}
+
+void
+TimedSubdomainModifier::initialize()
+{
+  TimedElementSubdomainModifier::initialize();
+}
+
+std::vector<Real>
+TimedSubdomainModifier::onGetTimes()
+{
+  // just return our local array of times
+  return _times;
+}
+
+SubdomainID
+TimedSubdomainModifier::onComputeSubdomainID(const Real t_from_exclusive, const Real t_to_inclusive)
+{
+  // get the subdomain-id of the current element
+  SubdomainID resulting_subdomain_id = _current_elem->subdomain_id();
+
+  // iterate all block changes issued by the user and apply if appropriate
+  const auto n_rows = _times_and_indices.size();
+  for (size_t i = 0; i < n_rows; ++i)
+  {
+    // time of the data point
+    const auto t = _times_and_indices[i].time;
+
+    // original data point index
+    const auto j = _times_and_indices[i].index;
+
+    // do we have to apply?
+    if (t > t_from_exclusive && t <= t_to_inclusive && resulting_subdomain_id == _blocks_from[j])
+    {
+      // we have to change the subdomain-id using the original index (stored in 'j')
+      resulting_subdomain_id = _blocks_to[j];
+    }
+  };
+
+  return resulting_subdomain_id;
+}
