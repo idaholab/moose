85e72a5ae800733cbbf1642b95272f19d0fdaf70
Alex Lindsay <alexander.lindsay@inl.gov>
2024-03-20 08:27:55 -0700

    Implement interior penalty HDG method
    Closes #30038

---
+++ framework/include/dgkernels/ADHDGDiffusion.h
@@ -0,0 +1,38 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "ADDGKernel.h"
+
+/**
+ * DG kernel for diffusion
+ *
+ * General DG kernel that this class can handle is:
+ * \f$ { \nabla u * n_e} [v] + epsilon { \nabla v * n_e } [u] + (sigma / |e| * [u][v]) \f$
+ *
+ * \f$ [a] = [ a_1 - a_2 ] \f$
+ * \f$ {a} = 0.5 * (a_1 + a_2) \f$
+ *
+ */
+class ADHDGDiffusion : public ADDGKernel
+{
+public:
+  static InputParameters validParams();
+
+  ADHDGDiffusion(const InputParameters & parameters);
+
+protected:
+  virtual ADReal computeQpResidual(Moose::DGResidualType type) override;
+
+  const Real _alpha;
+  const ADMaterialProperty<Real> & _diff;
+  const ADMaterialProperty<Real> & _diff_neighbor;
+  const ADVariableValue & _side_u;
+};
