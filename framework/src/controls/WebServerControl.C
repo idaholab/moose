4968b2ee8820eb6bcb90c657cf1cd6926d579f26
Logan Harbour <loganhharbour@gmail.com>
2024-03-24 21:14:25 -0600

    Remove some tabs

--- framework/src/controls/WebServerControl.C
+++ framework/src/controls/WebServerControl.C
@@ -72,191 +72,187 @@ WebServerControl::~WebServerControl()
 std::unique_ptr<std::thread>
 WebServerControl::startServer()
 {
-  // Only start the server on the root node
-  if (processor_id() == 0)
+  // Only start on rank 0
+  if (processor_id() != 0)
+    return nullptr;
+
+  // Helper for returning an error response
+  const auto error = [](const std::string & error)
   {
-    // Helper for returning an error response
-    const auto error = [](const std::string & error)
-    {
-      miniJson::Json::_object response;
-      response["error"] = error;
-      return HttpResponse{400, response};
-    };
+    miniJson::Json::_object response;
+    response["error"] = error;
+    return HttpResponse{400, response};
+  };
 
-    // Helper for getting a string from a json value with error checking
-    const auto get_string =
-        [&error](const auto & msg, const std::string & name, const std::string & description)
-    {
-      using result = std::variant<std::string, HttpResponse>;
-      const auto it = msg.find(name);
-      if (it == msg.end())
-        return result(
-            error("The entry '" + name + "' is missing which should contain the " + description));
-      const auto & value = it->second;
-      if (!value.isString())
-        return result(error("The entry '" + name + "' which should contain the " + description));
-      return result(value.toString());
-    };
+  // Helper for getting a string from a json value with error checking
+  const auto get_string =
+      [&error](const auto & msg, const std::string & name, const std::string & description)
+  {
+    using result = std::variant<std::string, HttpResponse>;
+    const auto it = msg.find(name);
+    if (it == msg.end())
+      return result(
+          error("The entry '" + name + "' is missing which should contain the " + description));
+    const auto & value = it->second;
+    if (!value.isString())
+      return result(error("The entry '" + name + "' which should contain the " + description));
+    return result(value.toString());
+  };
 
-    // Helper for getting a string name from a json value with error checking
-    const auto get_name = [&get_string](const auto & msg, const std::string & description)
-    { return get_string(msg, "name", "name of the " + description); };
+  // Helper for getting a string name from a json value with error checking
+  const auto get_name = [&get_string](const auto & msg, const std::string & description)
+  { return get_string(msg, "name", "name of the " + description); };
 
-    // Helper for requiring that the control is waiting
-    // Note that this is very hard to test unless we want to add sleeps
-    const auto require_waiting = [&error](auto & control)
-    {
-      using result = std::optional<HttpResponse>;
-      if (!control.currentlyWaiting())
-        return result(error("This control is not currently waiting for data"));
-      return result{};
-    };
+  // Helper for requiring that the control is waiting
+  // Note that this is very hard to test unless we want to add sleeps
+  const auto require_waiting = [&error](auto & control)
+  {
+    using result = std::optional<HttpResponse>;
+    if (!control.currentlyWaiting())
+      return result(error("This control is not currently waiting for data"));
+    return result{};
+  };
 
-    const auto require_parameters = [&error](const auto & msg, const std::set<std::string> & params)
-    {
-      using result = std::optional<HttpResponse>;
-      for (const auto & key_value_pair : msg)
-        if (!params.count(key_value_pair.first))
-          return result(error("The key '" + key_value_pair.first + "' is unused"));
-      return result{};
-    };
+  const auto require_parameters = [&error](const auto & msg, const std::set<std::string> & params)
+  {
+    using result = std::optional<HttpResponse>;
+    for (const auto & key_value_pair : msg)
+      if (!params.count(key_value_pair.first))
+        return result(error("The key '" + key_value_pair.first + "' is unused"));
+    return result{};
+  };
 
-    // GET /waiting, returns code 200 on success and JSON:
-    //  'waiting' (bool): Whether or not the control is waiting
-    //  'execute_on_flag' (string): Only exists if waiting=true, the execute
-    //                              flag that is being waited on
-    _server.when("/waiting")
-        ->requested(
-            [this](const HttpRequest & /*req*/)
+  // GET /waiting, returns code 200 on success and JSON:
+  //  'waiting' (bool): Whether or not the control is waiting
+  //  'execute_on_flag' (string): Only exists if waiting=true, the execute
+  //                              flag that is being waited on
+  _server.when("/waiting")
+      ->requested(
+          [this](const HttpRequest & /*req*/)
+          {
+            miniJson::Json::_object res_json;
+            if (this->_currently_waiting.load())
             {
-              miniJson::Json::_object res_json;
-              if (this->_currently_waiting.load())
+              res_json["waiting"] = true;
+              res_json["execute_on_flag"] =
+                  static_cast<std::string>(this->_fe_problem.getCurrentExecuteOnFlag());
+            }
+            else
+              res_json["waiting"] = false;
+
+            return HttpResponse{200, res_json};
+          });
+
+  // POST /get/postprocessor, with data:
+  //   'name' (string): The name of the Postprocessor
+  // Returns code 200 on success and JSON:
+  //   'value' (double): The postprocessor value
+  _server.when("/get/postprocessor")
+      ->posted(
+          [this, &error, &get_name, &require_waiting, &require_parameters](const HttpRequest & req)
+          {
+            const auto & msg = req.json().toObject();
+
+            // Get the postprocessor name
+            const auto name_result = get_name(msg, "postprocessor to retrieve");
+            if (const auto response = std::get_if<HttpResponse>(&name_result))
+              return *response;
+            const auto & name = std::get<std::string>(name_result);
+
+            // Should only have a name
+            if (const auto response = require_parameters(msg, {"name"}))
+              return *response;
+            // Should be waiting for data
+            if (const auto response = require_waiting(*this))
+              return *response;
+            // Postprocessor should exist
+            if (!this->hasPostprocessorByName(name))
+              return error("The postprocessor '" + name + "' was not found");
+
+            miniJson::Json::_object res_json;
+            res_json["value"] = getPostprocessorValueByName(name);
+            return HttpResponse{200, res_json};
+          });
+
+  // POST /set/controllable, with data:
+  //   'name' (string): The path to the controllable data
+  //   'value': The data to set
+  //   'type' (string): The C++ type of the controllable data to set
+  // Returns code 201 on success and JSON:
+  //   'error' (string): The error (only set if an error occurred)
+  _server.when("/set/controllable")
+      ->posted(
+          [this, &error, &get_string, &get_name, &require_waiting, &require_parameters](
+              const HttpRequest & req)
+          {
+            const auto & msg = req.json().toObject();
+
+            // Should only have a name, type, and value
+            if (const auto response = require_parameters(msg, {"name", "type", "value"}))
+              return *response;
+            // Should be waiting for data
+            if (const auto response = require_waiting(*this))
+              return *response;
+
+            // Get the parameter type
+            const auto type_result = get_string(msg, "type", "type of the parameter");
+            if (const auto response = std::get_if<HttpResponse>(&type_result))
+              return *response;
+            const auto & type = std::get<std::string>(type_result);
+            if (!WebServerControlTypeRegistry::isRegistered(type))
+              return error("The type '" + type +
+                           "' is not registered for setting a controllable parameter");
+
+            // Get the parameter name
+            const auto name_result = get_name(msg, "name of the parameter to control");
+            if (const auto response = std::get_if<HttpResponse>(&name_result))
+              return *response;
+            const auto & name = std::get<std::string>(name_result);
+            // Parameter should exist
+            if (!this->hasControllableParameterByName(name))
+              return error("The controllable parameter '" + name + "' was not found");
+
+            // Get the parameter value
+            const auto value_it = msg.find("value");
+            if (value_it == msg.end())
+              return error(
+                  "The entry 'value' is missing which should contain the value of the parameter");
+            const auto & json_value = value_it->second;
+
+            // Build the value (also does the parsing)
+            {
+              std::unique_ptr<ValueBase> value;
+              std::lock_guard<std::mutex> lock(this->_controlled_values_mutex);
+              try
               {
-                res_json["waiting"] = true;
-                res_json["execute_on_flag"] =
-                    static_cast<std::string>(this->_fe_problem.getCurrentExecuteOnFlag());
+                value = WebServerControlTypeRegistry::build(type, name, json_value);
               }
-              else
-                res_json["waiting"] = false;
-
-              return HttpResponse{200, res_json};
-            });
-
-    // POST /get/postprocessor, with data:
-    //   'name' (string): The name of the Postprocessor
-    // Returns code 200 on success and JSON:
-    //   'value' (double): The postprocessor value
-    _server.when("/get/postprocessor")
-        ->posted(
-            [this, &error, &get_name, &require_waiting, &require_parameters](
-                const HttpRequest & req)
-            {
-              const auto & msg = req.json().toObject();
-
-              // Get the postprocessor name
-              const auto name_result = get_name(msg, "postprocessor to retrieve");
-              if (const auto response = std::get_if<HttpResponse>(&name_result))
-                return *response;
-              const auto & name = std::get<std::string>(name_result);
-
-              // Should only have a name
-              if (const auto response = require_parameters(msg, {"name"}))
-                return *response;
-              // Should be waiting for data
-              if (const auto response = require_waiting(*this))
-                return *response;
-              // Postprocessor should exist
-              if (!this->hasPostprocessorByName(name))
-                return error("The postprocessor '" + name + "' was not found");
-
-              miniJson::Json::_object res_json;
-              res_json["value"] = getPostprocessorValueByName(name);
-              return HttpResponse{200, res_json};
-            });
-
-    // POST /set/controllable, with data:
-    //   'name' (string): The path to the controllable data
-    //   'value': The data to set
-    //   'type' (string): The C++ type of the controllable data to set
-    // Returns code 201 on success and JSON:
-    //   'error' (string): The error (only set if an error occurred)
-    _server.when("/set/controllable")
-        ->posted(
-            [this, &error, &get_string, &get_name, &require_waiting, &require_parameters](
-                const HttpRequest & req)
-            {
-              const auto & msg = req.json().toObject();
-
-              // Should only have a name, type, and value
-              if (const auto response = require_parameters(msg, {"name", "type", "value"}))
-                return *response;
-              // Should be waiting for data
-              if (const auto response = require_waiting(*this))
-                return *response;
-
-              // Get the parameter type
-              const auto type_result = get_string(msg, "type", "type of the parameter");
-              if (const auto response = std::get_if<HttpResponse>(&type_result))
-                return *response;
-              const auto & type = std::get<std::string>(type_result);
-              if (!WebServerControlTypeRegistry::isRegistered(type))
-                return error("The type '" + type +
-                             "' is not registered for setting a controllable parameter");
-
-              // Get the parameter name
-              const auto name_result = get_name(msg, "name of the parameter to control");
-              if (const auto response = std::get_if<HttpResponse>(&name_result))
-                return *response;
-              const auto & name = std::get<std::string>(name_result);
-              // Parameter should exist
-              if (!this->hasControllableParameterByName(name))
-                return error("The controllable parameter '" + name + "' was not found");
-
-              // Get the parameter value
-              const auto value_it = msg.find("value");
-              if (value_it == msg.end())
-                return error(
-                    "The entry 'value' is missing which should contain the value of the parameter");
-              const auto & json_value = value_it->second;
-
-              // Build the value (also does the parsing)
+              catch (ValueBase::Exception & e)
               {
-                std::unique_ptr<ValueBase> value;
-                std::lock_guard<std::mutex> lock(this->_controlled_values_mutex);
-                try
-                {
-                  value = WebServerControlTypeRegistry::build(type, name, json_value);
-                }
-                catch (ValueBase::Exception & e)
-                {
-                  return error("While parsing 'value': " + std::string(e.what()));
-                }
-                _controlled_values.emplace_back(std::move(value));
+                return error("While parsing 'value': " + std::string(e.what()));
               }
+              _controlled_values.emplace_back(std::move(value));
+            }
 
-              return HttpResponse{201};
-            });
+            return HttpResponse{201};
+          });
 
-    // GET /continue, Returns code 200
-    _server.when("/continue")
-        ->requested(
-            [this, &error](const HttpRequest &)
+  // GET /continue, Returns code 200
+  _server.when("/continue")
+      ->requested(
+          [this, &error](const HttpRequest &)
+          {
+            if (this->_currently_waiting.load())
             {
-              if (this->_currently_waiting.load())
-              {
-                this->_currently_waiting.store(false);
-                return HttpResponse{200};
-              }
+              this->_currently_waiting.store(false);
+              return HttpResponse{200};
+            }
 
-              // Not currently waiting
-              return error("The control is not currently waiting");
-            });
+            // Not currently waiting
+            return error("The control is not currently waiting");
+          });
 
-    return std::make_unique<std::thread>([this] { this->_server.startListening(this->_port); });
-  }
-
-  // No server other than on rank zero
-  return nullptr;
+  return std::make_unique<std::thread>([this] { this->_server.startListening(this->_port); });
 }
 
 void
