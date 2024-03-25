332dbc3687b8361993548f4594c583197419abb7
Alex Lindsay <alexander.lindsay@inl.gov>
2024-03-25 12:48:02 -0700

    Working advection-diffusion mms tests

--- test/tests/dgkernels/interior_penalty_hdg/advection-diffusion-compute-functions.py
+++ test/tests/dgkernels/interior_penalty_hdg/advection-diffusion-compute-functions.py
@@ -1,1 +1,9 @@
-../../fvkernels/mms/grad-reconstruction/cartesian-compute-functions.py
+#!/usr/bin/env python3
+
+import mms
+
+f, e = mms.evaluate('div(vel*u) - div(diff*grad(u))', 'sin(x)*cos(y)', variable='u',
+                    vel='a*(e_i + 2*e_j)', scalars=['a', 'diff'])
+
+mms.print_hit(e, 'exact')
+mms.print_hit(f, 'forcing', a='${a}', diff='${diff}')
