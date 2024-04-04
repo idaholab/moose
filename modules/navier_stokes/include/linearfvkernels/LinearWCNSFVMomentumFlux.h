c07502f0c86c44bf7ed5be90005251e75f2c276d
Peter German <grmnptr@gmail.com>
2024-04-04 11:30:55 -0600

    Implement advection contribution. (#27280)

--- modules/navier_stokes/include/linearfvkernels/LinearWCNSFVMomentumFlux.h
+++ modules/navier_stokes/include/linearfvkernels/LinearWCNSFVMomentumFlux.h
@@ -41,10 +41,37 @@ public:
 
   virtual Real computeBoundaryRHSContribution(const LinearFVBoundaryCondition & bc) override;
 
+  /**
+   * Set the current FaceInfo object. We override this here to make sure the face velocity
+   * evaluation happens only once and that it can be reused for the matrix and right hand side
+   * contributions.
+   * @param face_info The face info which will be used as current face info
+   */
+  virtual void setCurrentFaceInfo(const FaceInfo * face_info) override;
+
 protected:
+  /// Computes the matrix contribution on of the advective flux on the element side of current face
+  /// when the face is an internal face (doesn't have associated boundary conditions).
+  Real computeInternalAdvectionElemMatrixContribution();
+
+  /// Computes the matrix contribution on of the advective flux on the neighbor side of current face
+  /// when the face is an internal face (doesn't have associated boundary conditions).
+  Real computeInternalAdvectionNeighborMatrixContribution();
+
   /// The Rhie-Chow user object that provides us with the face velocity
   const INSFVRhieChowInterpolatorSegregated & _vel_provider;
 
+  /// Container for the current velocity vector on the face to make sure we don't compute it
+  /// multiple times for different terms/
+  std::pair<Real, Real> _interp_coeffs;
+
+  /// Container for the mass flux on the face which will be reused in the advection term's
+  /// matrix and right hand side contribution
+  Real _face_mass_flux;
+
   /// The interpolation method to use for the advected quantity
   Moose::FV::InterpMethod _advected_interp_method;
+
+  /// The interpolation method to use for the velocity
+  Moose::FV::InterpMethod _velocity_interp_method;
 };
--- modules/navier_stokes/src/linearfvkernels/LinearWCNSFVMomentumFlux.C
+++ modules/navier_stokes/src/linearfvkernels/LinearWCNSFVMomentumFlux.C
@@ -8,8 +8,7 @@
 //* https://www.gnu.org/licenses/lgpl-2.1.html
 
 #include "LinearWCNSFVMomentumFlux.h"
-#include "Assembly.h"
-#include "SubProblem.h"
+#include "NSFVUtils.h"
 
 registerMooseObject("NavierStokesApp", LinearWCNSFVMomentumFlux);
 
@@ -20,16 +19,19 @@ LinearWCNSFVMomentumFlux::validParams()
   params.addClassDescription("Represents the matrix and right hand side contributions of the "
                              "stress and advection terms of the momentum equation.");
   params.addRequiredParam<MooseFunctorName>("advecting_velocity", "Advection velocity");
-  params += Moose::FV::advectedInterpolationParameter();
+  params += Moose::FV::interpolationParameters();
+  ;
   return params;
 }
 
 LinearWCNSFVMomentumFlux::LinearWCNSFVMomentumFlux(const InputParameters & params)
   : LinearFVFluxKernel(params),
-    _vel_provider(getUserObject<INSFVRhieChowInterpolatorSegregated>("rhie_chow_user_object"))
+    _vel_provider(getUserObject<INSFVRhieChowInterpolatorSegregated>("rhie_chow_user_object")),
+    _interp_coeffs(std::make_pair<Real, Real>(0, 0)),
+    _face_mass_flux(0.0)
 
 {
-  Moose::FV::setInterpolationMethod(*this, _advected_interp_method, "advected_interp_method");
+  Moose::FV::setInterpolationMethods(*this, _advected_interp_method, _velocity_interp_method);
 }
 
 void
@@ -40,13 +42,13 @@ LinearWCNSFVMomentumFlux::initialSetup()
 Real
 LinearWCNSFVMomentumFlux::computeElemMatrixContribution()
 {
-  return 0.0;
+  return computeInternalAdvectionElemMatrixContribution() * _current_face_area;
 }
 
 Real
 LinearWCNSFVMomentumFlux::computeNeighborMatrixContribution()
 {
-  return 0.0;
+  return computeInternalAdvectionNeighborMatrixContribution() * _current_face_area;
 }
 
 Real
@@ -72,4 +74,34 @@ Real
 LinearWCNSFVMomentumFlux::computeBoundaryRHSContribution(const LinearFVBoundaryCondition & /*bc*/)
 {
   return 0.0;
+}
+
+Real
+LinearWCNSFVMomentumFlux::computeInternalAdvectionElemMatrixContribution()
+{
+  return _interp_coeffs.first * _face_mass_flux;
+}
+
+Real
+LinearWCNSFVMomentumFlux::computeInternalAdvectionNeighborMatrixContribution()
+{
+  return _interp_coeffs.second * _face_mass_flux;
+}
+
+void
+LinearWCNSFVMomentumFlux::setCurrentFaceInfo(const FaceInfo * face_info)
+{
+  LinearFVFluxKernel::setCurrentFaceInfo(face_info);
+
+  // First, we fetch the velocity on the face
+  const auto & velocity =
+      _vel_provider.getVelocity(_velocity_interp_method, *face_info, determineState(), _tid, false);
+
+  // Caching the mass flux on the face which will be reused in the advection term's matrix and right
+  // hand side contributions
+  _face_mass_flux = raw_value(velocity * _current_face_info->normal());
+
+  // Caching the interpolation coefficients so they will be reused for the matrix and right hand
+  // side terms
+  _interp_coeffs = interpCoeffs(_advected_interp_method, *_current_face_info, true, velocity);
 }
