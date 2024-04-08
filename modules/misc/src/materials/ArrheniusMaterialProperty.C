a1b57fa4c0d0deb36ccdff05e909f49ab0698c3e
Adriaan Riet <adriaan.riet@inl.gov>
2024-04-08 11:48:28 -0600

    Removed more BISON references, changed headers to reflect MOOSE standard and added the physicalConstants dependency.

--- modules/misc/src/materials/ArrheniusMaterialProperty.C
+++ modules/misc/src/materials/ArrheniusMaterialProperty.C
@@ -1,17 +1,11 @@
-/*************************************************/
-/*           DO NOT MODIFY THIS HEADER           */
-/*                                               */
-/*                     BISON                     */
-/*                                               */
-/*    (c) 2015 Battelle Energy Alliance, LLC     */
-/*            ALL RIGHTS RESERVED                */
-/*                                               */
-/*   Prepared by Battelle Energy Alliance, LLC   */
-/*     Under Contract No. DE-AC07-05ID14517      */
-/*     With the U. S. Department of Energy       */
-/*                                               */
-/*     See COPYRIGHT for full restrictions       */
-/*************************************************/
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
 
 #include "ArrheniusMaterialProperty.h"
 
@@ -19,8 +13,8 @@
 
 #include "libmesh/utility.h"
 
-registerMooseObject("BisonApp", ArrheniusMaterialProperty);
-registerMooseObject("BisonApp", ADArrheniusMaterialProperty);
+registerMooseObject("MiscApp", ArrheniusMaterialProperty);
+registerMooseObject("MiscApp", ADArrheniusMaterialProperty);
 
 template <bool is_ad>
 InputParameters
