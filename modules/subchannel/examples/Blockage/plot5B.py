f0f25571bd75735eba205d1165628f57203783ca
Kyriv <vasileios.kyriakopoulos@inl.gov>
2024-04-23 11:51:15 -0600

    Rename code name in DOCS v2  Refs #29502

--- modules/subchannel/examples/Blockage/plot5B.py
+++ modules/subchannel/examples/Blockage/plot5B.py
@@ -11,7 +11,7 @@ EXP_low = np.genfromtxt("T_EXP_LOW.csv", skip_header=0, delimiter=',')
 SC = np.genfromtxt("FFM-5B_out.csv", skip_header=2, delimiter=',') - 273.15
 
 # plt.figure()
-# plt.plot(SC[1:], "k^", label = "MOOSE-SubChannel")
+# plt.plot(SC[1:], "k^", label = "SubChannel")
 # plt.plot(EXP_high[:, 1], "ko", markerfacecolor="r", label = "EXP")
 # plt.title(r"Temperature profile at thermocouple location" "\n" "High flow Exp, FFM Series 6, Test 12, Run 101", fontsize=13)
 # plt.xticks([i for i in range(len(EXP_high[:, 0]))],
@@ -26,7 +26,7 @@ SC = np.genfromtxt("FFM-5B_out.csv", skip_header=2, delimiter=',') - 273.15
 # plt.savefig("FFM-5B.png")
 
 plt.figure()
-plt.plot(SC[1:], "k^", label = "MOOSE-SubChannel")
+plt.plot(SC[1:], "k^", label = "SubChannel")
 plt.plot(EXP_low[:, 1], "ko", markerfacecolor="r", label = "EXP")
 plt.title(r"Temperature profile at thermocouple location" "\n" "Low flow Exp, FFM Series 6, Test 12, Run 109", fontsize=13)
 plt.xticks([i for i in range(len(EXP_low[:, 0]))],
