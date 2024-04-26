1e0d5de2ee8a224a0ccfbbe7def29ac61d45d353
Patrick Behne <patrick.behne@inl.gov>
2024-04-26 10:07:57 -0600

    Removed instances of --no-gdb-backtrace since libmesh is now compiled without gdb by default.

--- python/jacobiandebug/analyzejacobian.py
+++ python/jacobiandebug/analyzejacobian.py
@@ -351,7 +351,7 @@ if __name__ == '__main__':
     dofmapfilename = basename + '_' + dofoutname + '.json'
     if not options.noauto :
         mooseparams = moosebaseparams[:]
-        mooseparams.extend(['Problem/solve=false', 'BCs/active=', 'Outputs/' + dofoutname+ '/type=DOFMap', 'Outputs/active=' + dofoutname, 'Outputs/file_base=' + basename + '_' + dofoutname, '--no-gdb-backtrace'])
+        mooseparams.extend(['Problem/solve=false', 'BCs/active=', 'Outputs/' + dofoutname+ '/type=DOFMap', 'Outputs/active=' + dofoutname, 'Outputs/file_base=' + basename + '_' + dofoutname])
         if options.cli_args != None:
             mooseparams.extend([options.cli_args])
         if options.debug :
@@ -428,7 +428,7 @@ if __name__ == '__main__':
 
         mooseparams.extend(petsc_test_options + ['-mat_fd_type', 'ds',
                                                  'BCs/active=', 'Outputs/exodus=false', 'Outputs/csv=false',
-                                                 'Outputs/active=', 'Executioner/solve_type=NEWTON', '--no-gdb-backtrace'])
+                                                 'Outputs/active=', 'Executioner/solve_type=NEWTON'])
     if options.cli_args != None:
         mooseparams.extend([options.cli_args])
 
