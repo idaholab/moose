074aec2aa997d1e1c1e8d0f05753067bf08cc466
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2024-04-30 13:17:58 -0600

    Register the mixture physics action

--- modules/navier_stokes/src/base/NavierStokesApp.C
+++ modules/navier_stokes/src/base/NavierStokesApp.C
@@ -55,6 +55,7 @@ associateSyntaxInner(Syntax & syntax, ActionFactory & /*action_factory*/)
   registerSyntax("WCNSFVScalarTransportPhysics", "Physics/NavierStokes/ScalarTransport/*");
   registerSyntax("WCNSFVTurbulencePhysics", "Physics/NavierStokes/Turbulence/*");
   registerSyntax("PNSFVSolidHeatTransferPhysics", "Physics/NavierStokes/SolidHeatTransfer/*");
+  registerSyntax("WCNSFVTwoPhaseMixturePhysics", "Physics/NavierStokes/TwoPhaseMixture/*");
 
   // Create the Action syntax
   registerSyntax("CNSAction", "Modules/CompressibleNavierStokes");
