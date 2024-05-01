ec5a985c7a54376bdc343527219c0a15ba08f49b
Joshua E. Hansel <joshua.hansel@inl.gov>
2024-05-01 07:54:07 -0500

    Made 'scale' parameter a functor instead of a function
    Also, removed deprecated 'scale_pp' parameter.
    
    Refs #25275

--- modules/thermal_hydraulics/src/postprocessors/HeatRateConvection.C
+++ modules/thermal_hydraulics/src/postprocessors/HeatRateConvection.C
@@ -20,7 +20,7 @@ HeatRateConvection::validParams()
   params.addRequiredCoupledVar("T", "Temperature");
   params.addRequiredParam<FunctionName>("T_ambient", "Ambient temperature function");
   params.addRequiredParam<FunctionName>("htc", "Ambient heat transfer coefficient function");
-  params.addParam<FunctionName>("scale", 1.0, "Function by which to scale the heat flux");
+  params.addParam<MooseFunctorName>("scale", 1.0, "Functor by which to scale the heat flux");
 
   params.addClassDescription("Integrates a convective heat flux over a boundary.");
 
@@ -33,13 +33,14 @@ HeatRateConvection::HeatRateConvection(const InputParameters & parameters)
     _T(coupledValue("T")),
     _T_ambient_fn(getFunction("T_ambient")),
     _htc_ambient_fn(getFunction("htc")),
-    _scale_fn(getFunction("scale"))
+    _scale(getFunctor<Real>("scale"))
 {
 }
 
 Real
 HeatRateConvection::computeQpIntegral()
 {
-  return _scale_fn.value(_t, _q_point[_qp]) * _htc_ambient_fn.value(_t, _q_point[_qp]) *
+  const Moose::ElemSideQpArg space_arg = {_current_elem, _current_side, _qp, _qrule, _q_point[_qp]};
+  return _scale(space_arg, Moose::currentState()) * _htc_ambient_fn.value(_t, _q_point[_qp]) *
          (_T_ambient_fn.value(_t, _q_point[_qp]) - _T[_qp]);
 }
