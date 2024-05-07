15b7f8ffe9a2c1deca9e2399ea01e751f76c31ee
maxnezddyur <maxnezdyur@gmail.com>
2024-05-07 06:15:48 -0600

    deprecate OptReporter

--- modules/optimization/src/optimizationreporters/OptimizationReporter.C
+++ modules/optimization/src/optimizationreporters/OptimizationReporter.C
@@ -11,7 +11,7 @@
 
 #include "libmesh/int_range.h"
 
-registerMooseObject("OptimizationApp", OptimizationReporter);
+registerMooseObjectDeprecated("OptimizationApp", OptimizationReporter, "12/31/2024 24:00");
 
 InputParameters
 OptimizationReporter::validParams()
@@ -43,6 +43,10 @@ OptimizationReporter::validParams()
 OptimizationReporter::OptimizationReporter(const InputParameters & parameters)
   : OptimizationDataTempl<OptimizationReporterBase>(parameters)
 {
+  mooseDeprecated(
+      "The 'OptimizationReporter' is deprecated. Please use 'GeneralOptimization' instead. "
+      "You can achieve the same functionality by using an 'OptimizationData' object in the "
+      "forward application to calculate the objective value, similar to the method used here.");
 }
 void
 OptimizationReporter::setICsandBounds()
