96e11a2aac2452654f6c7ebb1fd5927a5f806a6b
oanaoana <oana_marin@outlook.com>
2024-05-14 13:35:21 -0500

    starting setup for ghosting for split meshes refs #27203

---
+++ framework/include/relationshipmanagers/GhostBoundary.h
@@ -0,0 +1,78 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+// App includes
+#include "AutomaticMortarGeneration.h"
+#include "RelationshipManager.h"
+
+// libMesh includes
+#include "libmesh/mesh_base.h"
+
+using libMesh::boundary_id_type;
+using libMesh::CouplingMatrix;
+using libMesh::Elem;
+using libMesh::GhostingFunctor;
+using libMesh::MeshBase;
+using libMesh::processor_id_type;
+
+class GhostBoundary : public RelationshipManager
+{
+public:
+  GhostBoundary(const InputParameters &);
+
+  GhostBoundary(const GhostBoundary & others);
+
+  static InputParameters validParams();
+
+  /**
+   * This function must be overriden by application codes to add
+   * required elements from (range_begin, range_end) to the
+   * coupled_elements map.
+   */
+  virtual void operator()(const MeshBase::const_element_iterator & range_begin,
+                          const MeshBase::const_element_iterator & range_end,
+                          processor_id_type p,
+                          map_type & coupled_elements) override;
+
+  /**
+   * A clone() is needed because GhostingFunctor can not be shared between
+   * different meshes. The operations in  GhostingFunctor are mesh dependent.
+   */
+  virtual std::unique_ptr<GhostingFunctor> clone() const override;
+
+  /**
+   * Update the cached _lower_to_upper map whenever our Mesh has been
+   * redistributed.  We'll be lazy and just recalculate from scratch.
+   */
+  virtual void redistribute() override { this->mesh_reinit(); }
+
+  std::string getInfo() const override;
+
+  virtual bool operator>=(const RelationshipManager & other) const override;
+
+protected:
+  virtual void internalInitWithMesh(const MeshBase &) override;
+
+  const BoundaryName _boundary_name;
+
+  const BoundaryName _paired_boundary_name;
+
+  /// Whether this relationship manager is called when coupling functors are called when building
+  /// the matrix sparsity pattern
+  const bool _is_coupling_functor;
+
+  /// Whether to ghost point neighbors of secondary lower subdomain elements and their
+  /// cross mortar interface counterparts for applications such as mortar nodal auxiliary kernels
+  const bool _ghost_point_neighbors;
+
+  /// null matrix for generating full variable coupling
+  const CouplingMatrix * const _null_mat = nullptr;
+};
--- modules/heat_transfer/src/bcs/GapHeatTransfer.C
+++ modules/heat_transfer/src/bcs/GapHeatTransfer.C
@@ -16,6 +16,7 @@
 #include "MooseVariable.h"
 #include "PenetrationLocator.h"
 #include "SystemBase.h"
+#include "GhostBoundary.h"
 
 #include "libmesh/string_to_enum.h"
 
@@ -77,6 +78,15 @@ GapHeatTransfer::validParams()
   // Node based options
   params.addCoupledVar("gap_distance", "Distance across the gap");
   params.addCoupledVar("gap_temp", "Temperature on the other side of the gap");
+
+  params.addRelationshipManager(
+      "GhostBoundary",
+      Moose::RelationshipManagerType::GEOMETRIC | Moose::RelationshipManagerType::ALGEBRAIC,
+      [](const InputParameters & obj_params, InputParameters & rm_params)
+      {
+        rm_params.set<BoundaryName>("boundary") = obj_params.get<BoundaryName>("primary");
+        rm_params.set<BoundaryName>("paired_boundary") = obj_params.get<BoundaryName>("secondary");
+      });
 
   return params;
 }
