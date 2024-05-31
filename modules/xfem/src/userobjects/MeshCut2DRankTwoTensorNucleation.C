533fbf62fe184062bc72684456a5b120afdcbf51
Lynn Munday <lynn.munday@inl.gov>
2024-05-30 21:40:13 -0600

    fixing error in how cracks were offset when they nucleate on the boundary.   The previous version did not move the crack close to teh boundary and therefor was not nucleating the full length of the crack provided by the user

--- modules/xfem/src/userobjects/MeshCut2DRankTwoTensorNucleation.C
+++ modules/xfem/src/userobjects/MeshCut2DRankTwoTensorNucleation.C
@@ -175,15 +175,22 @@ MeshCut2DRankTwoTensorNucleation::doesElementCrack(
     point_0 = point_0 - extend_length * crack_dir.unit();
     point_1 = point_1 + extend_length * crack_dir.unit();
 
-    // modify edges of cut should go so that they are on the edge of the domain and have a length
+    // modify edges of cut to put them on the edge of the domain and have a length
     // equal to nucleation_length
     if (is_point_0_on_external_boundary)
     {
+      // first move point back to edge
+      point_0 = point_0 + extend_length * crack_dir.unit();
+      // second move point a little bit over edge
       point_0 = point_0 - (traverse_length * _edge_extension_factor) / 2.0 * crack_dir.unit();
+      // third, move point 1 so that crack equals nucleation length
+      point_1 = point_1 + extend_length * crack_dir.unit();
     }
     else if (is_point_1_on_external_boundary)
     {
+      point_1 = point_1 - extend_length * crack_dir.unit();
       point_1 = point_1 + (traverse_length * _edge_extension_factor) / 2.0 * crack_dir.unit();
+      point_0 = point_0 - extend_length * crack_dir.unit();
     }
     cutterElemNodes = {point_0, point_1};
   }
