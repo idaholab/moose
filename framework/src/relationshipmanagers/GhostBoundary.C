3cf3e05c943386214d8b04b5e26896d27f7c7ef5
oanaoana <oana_marin@outlook.com>
2024-06-25 12:42:55 -0500

    repatch #27203

--- framework/src/relationshipmanagers/GhostBoundary.C
+++ framework/src/relationshipmanagers/GhostBoundary.C
@@ -89,7 +89,8 @@ GhostBoundary::operator()(const MeshBase::const_element_iterator & /*range_begin
     }
     else
     {
-      // We've finished generating our mesh so we can be selective and only ghost elements lying on our boundary
+      // We've finished generating our mesh so we can be selective and only ghost elements lying on
+      // our boundary
 
       const BoundaryInfo & binfo = _mesh->get_boundary_info();
       for (auto side : elem->side_index_range())
