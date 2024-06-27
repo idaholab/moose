53f178f57e0fd646676c648f5eb530441efd8cac
Edward Palmer <edward.palmer@ukaea.uk>
2024-06-27 10:44:01 +0000

    Removes commented-out headers.

--- framework/include/mfem/problem_builders/problem_builder.h
+++ framework/include/mfem/problem_builders/problem_builder.h
@@ -1,6 +1,4 @@
 #pragma once
-// #include "logging.h"
-// #include "factory.h"
 #include "steady_state_problem_builder.h"
 #include "steady_state_equation_system_problem_builder.h"
 #include "time_domain_problem_builder.h"
