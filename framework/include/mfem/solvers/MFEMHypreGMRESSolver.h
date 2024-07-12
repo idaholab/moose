b54b27ad691586c9d6b203562d67e1b4a34b57eb
Edward Palmer <edward.palmer@ukaea.uk>
2024-07-12 10:51:07 +0000

    Adds MFEMHypreGMRESSolver solver wrapper

---
+++ framework/include/mfem/solvers/MFEMHypreGMRESSolver.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "MFEMSolverBase.h"
+#include "mfem.hpp"
+#include <memory>
+
+/**
+ * Wrapper for mfem::HypreGMRES solver.
+ */
+class MFEMHypreGMRESSolver : public MFEMSolverBase
+{
+public:
+  static InputParameters validParams();
+
+  MFEMHypreGMRESSolver(const InputParameters &);
+
+  /// Returns a shared pointer to the instance of the Solver derived-class.
+  std::shared_ptr<mfem::Solver> getSolver() const override { return _solver; }
+
+protected:
+  void constructSolver(const InputParameters & parameters) override;
+
+private:
+  std::shared_ptr<mfem::HypreGMRES> _solver{nullptr};
+};
---
+++ framework/src/mfem/solvers/MFEMHypreGMRESSolver.C
@@ -0,0 +1,38 @@
+#pragma once
+#include "MFEMHypreGMRESSolver.h"
+#include "MFEMProblem.h"
+
+InputParameters
+MFEMHypreGMRESSolver::validParams()
+{
+  InputParameters params = MFEMSolverBase::validParams();
+
+  params.addParam<double>("tolerance", 1e-16, "Set the tolerance.");
+  params.addParam<double>("abs_tolerance", 1e-16, "Set the absolute tolerance.");
+  params.addParam<int>("max_iteration", 1000, "Set the maximum iteration.");
+  params.addParam<int>("kdim", 10, "Set the k-dimension.");
+
+  // NB: - will require option to add a preconditioner somewhere.
+
+  return params;
+}
+
+MFEMHypreGMRESSolver::MFEMHypreGMRESSolver(const InputParameters & parameters)
+  : MFEMSolverBase(parameters)
+{
+  constructSolver(parameters);
+}
+
+void
+MFEMHypreGMRESSolver::constructSolver(const InputParameters & parameters)
+{
+  _solver = std::make_shared<mfem::HypreGMRES>(getMFEMProblem().getProblemData()._comm);
+
+  // Set solver options.
+  _solver->SetTol(parameters.get<double>("tolerance"));
+  _solver->SetAbsTol(parameters.get<double>("abs_tolerance"));
+  _solver->SetMaxIter(parameters.get<int>("max_iteration"));
+  _solver->SetKDim(parameters.get<int>("kdim"));
+
+  // NB: - set the preconditioner here.
+}
