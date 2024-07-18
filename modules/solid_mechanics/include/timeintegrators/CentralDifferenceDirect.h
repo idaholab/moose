8f04ec85683ffc12f1a85b711d67f885bed23fc0
TheGreatCid <davidt.realmadrid@gmail.com>
2024-07-18 13:35:45 -0600

    Added central difference method to solid mechanics that calculates acceleration directly from the residual forces #27883

---
+++ modules/solid_mechanics/include/timeintegrators/CentralDifferenceDirect.h
@@ -0,0 +1,50 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "ExplicitTimeIntegrator.h"
+#include "libmesh/sparse_matrix.h"
+
+/**
+ * Implements a truly explicit (no nonlinear solve) first-order, forward Euler
+ * time integration scheme.
+ */
+class CentralDifferenceDirect : public ExplicitTimeIntegrator
+{
+public:
+  static InputParameters validParams();
+
+  CentralDifferenceDirect(const InputParameters & parameters);
+
+  virtual int order() override { return 1; }
+  virtual void computeTimeDerivatives() override;
+
+  virtual void solve() override;
+  virtual void postResidual(NumericVector<Number> & residual) override;
+
+  virtual bool performExplicitSolve(SparseMatrix<Number> & mass_matrix) override;
+
+  // Applying pre-set nodal BCs
+  void setPresetBCs();
+
+  void computeADTimeDerivatives(ADReal &, const dof_id_type &, ADReal &) const override
+  {
+    mooseError("NOT SUPPORTED");
+  }
+
+protected:
+  /**
+   * Helper function that actually does the math for computing the time derivative
+   */
+
+  const bool & _constant_mass;
+
+  const TagName & _mass_matrix;
+};
