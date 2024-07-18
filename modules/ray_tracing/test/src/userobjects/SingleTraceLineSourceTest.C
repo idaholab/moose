a602b8c7bbdb2a57d2134783fabd2f0011f1eeed
Grayson Gall <ggall713@gmail.com>
2024-07-18 10:59:25 -0600

    Adding an initial version of a single trace ray study test

---
+++ modules/ray_tracing/test/src/userobjects/SingleTraceLineSourceTest.C
@@ -0,0 +1,50 @@
+#include "SingleTraceLineSourceTest.h"
+#include "NonlinearSystemBase.h"
+#include "SystemBase.h"
+
+registerMooseObject("RayTracingTestApp", SingleTraceLineSourceTest);
+
+InputParameters
+SingleTraceLineSourceTest::validParams()
+{
+  auto params = RepeatableRayStudy::validParams();
+  params.set<ExecFlagEnum>("execute_on", "TIMESTEP_BEGIN PRE_KERNELS");
+  params.addParam<TagName>("residual_vector_tag", "the vector tag for the residual tag you will accumulate into");
+
+  return params;
+}
+
+SingleTraceLineSourceTest::SingleTraceLineSourceTest(const InputParameters & parameters)
+  : RepeatableRayStudy(parameters),
+    _residual_tag_name(getParam<TagName>("residual_vector_tag")),
+    _has_traced(false)
+{
+}
+
+void
+SingleTraceLineSourceTest::execute()
+{
+  if (_current_execute_flag == EXEC_TIMESTEP_BEGIN)
+  {
+    _has_traced = false;
+    return;
+  }
+
+  if (_fe_problem.currentlyComputingJacobian())
+    return;
+
+  const auto contribution_tag_id = _fe_problem.getVectorTagID(_residual_tag_name);
+  auto & nl = _fe_problem.getNonlinearSystemBase(_sys.number());
+  auto & contribution_vec = nl.getVector(contribution_tag_id);
+  if (!_has_traced)
+  {
+    contribution_vec.zero();
+    RayTracingStudy::execute();
+    contribution_vec.close();
+    _has_traced = true;
+  }
+
+  auto & residual_vec = nl.getVector(nl.residualVectorTag());
+  residual_vec.close();
+  residual_vec += contribution_vec;
+}
