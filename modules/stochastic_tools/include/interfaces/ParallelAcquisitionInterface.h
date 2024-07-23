0290d6cb2da1e2e0ec0aa1c5f2d02141d9d679a8
somu15 <Som.Dhulipala@inl.gov>
2024-07-23 10:58:41 -0600

    Adding acquisition functions #28196

---
+++ modules/stochastic_tools/include/interfaces/ParallelAcquisitionInterface.h
@@ -0,0 +1,31 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "InputParameters.h"
+#include "FEProblemBase.h"
+
+class ParallelAcquisitionFunctionBase;
+
+class ParallelAcquisitionInterface
+{
+public:
+  static InputParameters validParams();
+
+  ParallelAcquisitionInterface(const InputParameters & parameters);
+
+protected:
+  /// Lookup a ParallelAcquisitionFunction object by name and return pointer
+  ParallelAcquisitionFunctionBase * getParallelAcquisitionFunctionByName(const UserObjectName & name) const;
+
+private:
+  /// Reference to FEProblemBase instance
+  FEProblemBase & _parallelacquisition_feproblem;
+};
