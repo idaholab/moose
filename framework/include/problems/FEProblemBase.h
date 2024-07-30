cb5534cffeeaafa8978d975ff79280c843ca494f
Alex Lindsay <alexander.lindsay@inl.gov>
2024-07-30 13:17:57 -0700

    Remove redundant protected in FEProblemBase

--- framework/include/problems/FEProblemBase.h
+++ framework/include/problems/FEProblemBase.h
@@ -2556,7 +2556,6 @@ protected:
   ScalarInitialConditionWarehouse _scalar_ics; // use base b/c of setup methods
   ///@}
 
-protected:
   // material properties
   MaterialPropertyRegistry _material_prop_registry;
   MaterialPropertyStorage & _material_props;
