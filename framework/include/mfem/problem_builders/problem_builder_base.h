6ce8258ac03fb4a7c6c7a5df28e6368260b8f478
Henrique B. R <henrique.bergallo-rocha@ukaea.uk>
2024-08-05 17:03:24 +0100

    Fixd segfault and GPU run issue

--- framework/include/mfem/problem_builders/problem_builder_base.h
+++ framework/include/mfem/problem_builders/problem_builder_base.h
@@ -33,6 +33,7 @@ public:
   platypus::FESpaces _fespaces;
   platypus::GridFunctions _gridfunctions;
 
+  mfem::Device _device;
   MPI_Comm _comm;
   int _myid;
   int _num_procs;
@@ -63,6 +64,7 @@ public:
   void SetJacobianPreconditioner(std::shared_ptr<mfem::Solver> preconditioner);
   void SetJacobianSolver(std::shared_ptr<mfem::Solver> solver);
   void SetCoefficients(platypus::Coefficients & coefficients);
+  void SetDevice(const std::string & dev);
 
   void AddFESpace(std::string fespace_name,
                   std::string fec_name,
