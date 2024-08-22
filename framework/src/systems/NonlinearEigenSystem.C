5ada18683ce79c2dcfe40c937933a24ebafa8f6f
Alex Lindsay <alexander.lindsay@inl.gov>
2024-08-21 22:21:25 -0700

    Refactor to use new libMesh API
    Refs #27276

--- framework/src/systems/NonlinearEigenSystem.C
+++ framework/src/systems/NonlinearEigenSystem.C
@@ -113,8 +113,7 @@ NonlinearEigenSystem::NonlinearEigenSystem(EigenProblem & eigen_problem, const s
     _work_rhs_vector_AX(addVector("work_rhs_vector_Ax", false, PARALLEL)),
     _work_rhs_vector_BX(addVector("work_rhs_vector_Bx", false, PARALLEL)),
     _precond_matrix_includes_eigen(false),
-    _preconditioner(nullptr),
-    _num_constrained_dofs(0)
+    _preconditioner(nullptr)
 {
   SlepcEigenSolver<Number> * solver =
       cast_ptr<SlepcEigenSolver<Number> *>(_eigen_sys.eigen_solver.get());
@@ -192,45 +191,17 @@ NonlinearEigenSystem::postAddResidualObject(ResidualObject & object)
 }
 
 void
-NonlinearEigenSystem::initializeCondensedMatrices()
-{
-  if (!(_num_constrained_dofs = dofMap().n_constrained_dofs()))
-    return;
-
-  _eigen_sys.initialize_condensed_dofs();
-  const auto m = cast_int<numeric_index_type>(_eigen_sys.local_non_condensed_dofs_vector.size());
-  auto M = m;
-  _communicator.sum(M);
-  if (_eigen_sys.has_condensed_matrix_A())
-  {
-    _eigen_sys.get_condensed_matrix_A().init(M, M, m, m);
-    // A bit ludicrously MatCopy requires the matrix being copied to to be assembled
-    _eigen_sys.get_condensed_matrix_A().close();
-  }
-  if (_eigen_sys.has_condensed_matrix_B())
-  {
-    _eigen_sys.get_condensed_matrix_B().init(M, M, m, m);
-    _eigen_sys.get_condensed_matrix_B().close();
-  }
-  if (_eigen_sys.has_condensed_precond_matrix())
-  {
-    _eigen_sys.get_condensed_precond_matrix().init(M, M, m, m);
-    _eigen_sys.get_condensed_precond_matrix().close();
-  }
-}
-
-void
 NonlinearEigenSystem::postInit()
 {
   NonlinearSystemBase::postInit();
-  initializeCondensedMatrices();
+  _eigen_sys.initialize_condensed_matrices();
 }
 
 void
 NonlinearEigenSystem::reinit()
 {
   NonlinearSystemBase::reinit();
-  initializeCondensedMatrices();
+  _eigen_sys.initialize_condensed_matrices();
 }
 
 void
@@ -245,7 +216,7 @@ NonlinearEigenSystem::solve()
   // We apply initial guess for only nonlinear solver
   if (_eigen_problem.isNonlinearEigenvalueSolver())
   {
-    if (_num_constrained_dofs)
+    if (_eigen_sys.have_condensed_dofs())
     {
       subvec = solution().get_subvector(_eigen_sys.local_non_condensed_dofs_vector);
       _eigen_sys.set_initial_space(*subvec);
@@ -280,7 +251,7 @@ NonlinearEigenSystem::solve()
   if (n_converged_eigenvalues)
     getConvergedEigenpair(_eigen_problem.activeEigenvalueIndex());
 
-  if (_eigen_problem.isNonlinearEigenvalueSolver() && _num_constrained_dofs)
+  if (_eigen_problem.isNonlinearEigenvalueSolver() && _eigen_sys.have_condensed_dofs())
     solution().restore_subvector(std::move(subvec), _eigen_sys.local_non_condensed_dofs_vector);
 }
 
@@ -290,7 +261,7 @@ NonlinearEigenSystem::attachSLEPcCallbacks()
   // Tell libmesh not to close matrices before solve
   _eigen_sys.get_eigen_solver().set_close_matrix_before_solve(false);
 
-  if (_num_constrained_dofs)
+  if (_eigen_sys.have_condensed_dofs())
   {
     // Condensed Matrix A
     if (_eigen_sys.has_condensed_matrix_A())
