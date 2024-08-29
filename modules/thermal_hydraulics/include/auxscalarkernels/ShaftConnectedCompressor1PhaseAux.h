344b69992b17bafd593f8c9e8b896f4af4bad7cc
Joshua E. Hansel <joshua.hansel@inl.gov>
2024-08-29 15:02:33 -0500

    Consolidated ShaftConnectedCompressor1Phase aux scalar kernels
    Refs #25593

--- modules/thermal_hydraulics/include/auxscalarkernels/ShaftConnectedCompressor1PhaseAux.h
+++ modules/thermal_hydraulics/include/auxscalarkernels/ShaftConnectedCompressor1PhaseAux.h
@@ -14,18 +14,30 @@
 class ADShaftConnectedCompressor1PhaseUserObject;
 
 /**
- * Isentropic torque computed in the 1-phase shaft-connected compressor
+ * Computes various quantities for a ShaftConnectedCompressor1Phase.
  */
-class Compressor1PhaseIsentropicTorqueAux : public AuxScalarKernel
+class ShaftConnectedCompressor1PhaseAux : public AuxScalarKernel
 {
 public:
-  Compressor1PhaseIsentropicTorqueAux(const InputParameters & parameters);
+  static InputParameters validParams();
+
+  ShaftConnectedCompressor1PhaseAux(const InputParameters & parameters);
 
 protected:
-  virtual Real computeValue();
+  virtual Real computeValue() override;
+
+  /// Quantity type
+  enum class Quantity
+  {
+    DELTA_P,
+    ISENTROPIC_TORQUE,
+    DISSIPATION_TORQUE,
+    FRICTION_TORQUE,
+    MOMENT_OF_INERTIA
+  };
+  /// Which quantity to compute
+  const Quantity _quantity;
+
   /// 1-phase shaft-connected compressor user object
   const ADShaftConnectedCompressor1PhaseUserObject & _compressor_uo;
-
-public:
-  static InputParameters validParams();
 };
