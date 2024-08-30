c95751c899952458cd3ef5a5753215ece2df9f40
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2024-08-30 15:23:03 -0600

    Add a base class for THM flow model physics

---
+++ modules/thermal_hydraulics/include/physics/ThermalHydraulicsFlowPhysics.h
@@ -0,0 +1,121 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "PhysicsBase.h"
+#include "InputParameters.h"
+#include "NamingInterface.h"
+#include "THMEnums.h"
+#include "libmesh/fe_type.h"
+
+class THMProblem;
+class Factory;
+class ThermalHydraulicsApp;
+class FluidProperties;
+class FlowChannelBase;
+
+#define registerTHMFlowModelPhysicsBaseTasks(app_name, derived_name)                               \
+  registerMooseAction(app_name, derived_name, "add_ic");                                           \
+  registerMooseAction(app_name, derived_name, "add_variable")
+
+/**
+ * Provides functions to setup the flow model.  Should be used by components that has flow in them
+ */
+class ThermalHydraulicsFlowPhysics : public PhysicsBase, public NamingInterface
+{
+public:
+  ThermalHydraulicsFlowPhysics(const InputParameters & params);
+
+  static InputParameters validParams();
+
+  /**
+   * Gets a vector of the solution variables
+   * // TODO: remove
+   */
+  std::vector<VariableName> getSolutionVariables() const { return nonlinearVariableNames(); }
+
+  /**
+   * Initialize the model
+   */
+  virtual void init() = 0;
+
+  /**
+   * Add variables the model uses
+   *
+   */
+  virtual void addVariables() = 0;
+
+  /**
+   * Add initial conditions
+   */
+  virtual void addInitialConditions() = 0;
+
+  /**
+   * Add MOOSE objects this model uses
+   */
+  // virtual void addMooseObjects() = 0;
+
+protected:
+  /// The THM problem
+  THMProblem & _sim;
+
+  /// The flow channel component that built this class
+  FlowChannelBase & _flow_channel;
+
+  /// The name of the user object that defines fluid properties
+  const UserObjectName _fp_name;
+
+  /// The name of the components
+  const std::vector<std::string> _component_names;
+
+  /// Gravitational acceleration vector
+  const RealVectorValue & _gravity_vector;
+  /// Gravitational acceleration magnitude
+  const Real _gravity_magnitude;
+
+  /// The type of FE used for flow
+  const FEType & _fe_type;
+  /// Lump the mass matrix
+  const bool _lump_mass_matrix;
+
+  // Names of variables for which derivative material properties need to be created
+  std::vector<VariableName> _derivative_vars;
+  /// True if we output velocity as a vector-value field, false for outputting velocity as a scalar
+  const bool & _output_vector_velocity;
+
+  const FunctionName & getVariableFn(const FunctionName & fn_param_name);
+
+  /**
+   * Adds variables common to any flow model (A, P_hf, ...)
+   * Note: we could move these to flow components since they pertain to the geometry
+   */
+  virtual void addCommonVariables();
+
+  /**
+   * Adds initial conditions common to any flow model
+   */
+  virtual void addCommonInitialConditions();
+
+  /**
+   * Adds common MOOSE objects
+   */
+  // virtual void addCommonMooseObjects();
+
+public:
+  static const std::string AREA;
+  static const std::string AREA_LINEAR;
+  static const std::string HEAT_FLUX_WALL;
+  static const std::string HEAT_FLUX_PERIMETER;
+  static const std::string NUSSELT_NUMBER;
+  static const std::string SURFACE_TENSION;
+  static const std::string TEMPERATURE_WALL;
+  static const std::string UNITY;
+  static const std::string DIRECTION;
+};
---
+++ modules/thermal_hydraulics/src/physics/ThermalHydraulicsFlowPhysics.C
@@ -0,0 +1,118 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "THMProblem.h"
+#include "Component.h"
+#include "FlowChannelBase.h"
+#include "ConstantFunction.h"
+
+InputParameters
+ThermalHydraulicsFlowPhysics::validParams()
+{
+  InputParameters params = MooseObject::validParams();
+  params.addPrivateParam<THMProblem *>("_thm_problem");
+  params.addPrivateParam<FlowChannelBase *>("_flow_channel");
+  params.addRequiredParam<UserObjectName>(
+      "fp", "The name of the user object that defines fluid properties");
+  params.addRequiredParam<bool>("output_vector_velocity",
+                                "True if velocity is put out as a vector field.");
+  return params;
+}
+
+const std::string ThermalHydraulicsFlowPhysics::AREA = "A";
+const std::string ThermalHydraulicsFlowPhysics::AREA_LINEAR = "A_linear";
+const std::string ThermalHydraulicsFlowPhysics::HEAT_FLUX_WALL = "q_wall";
+const std::string ThermalHydraulicsFlowPhysics::HEAT_FLUX_PERIMETER = "P_hf";
+const std::string ThermalHydraulicsFlowPhysics::NUSSELT_NUMBER = "Nu";
+const std::string ThermalHydraulicsFlowPhysics::TEMPERATURE_WALL = "T_wall";
+const std::string ThermalHydraulicsFlowPhysics::UNITY = "unity";
+const std::string ThermalHydraulicsFlowPhysics::DIRECTION = "direction";
+
+ThermalHydraulicsFlowPhysics::ThermalHydraulicsFlowPhysics(const InputParameters & params)
+  : MooseObject(params),
+    _sim(*params.getCheckedPointerParam<THMProblem *>("_thm_problem")),
+    _flow_channel(*params.getCheckedPointerParam<FlowChannelBase *>("_flow_channel")),
+    _fp_name(params.get<UserObjectName>("fp")),
+    _comp_names({name()}),
+    _gravity_vector(_flow_channel.getParam<RealVectorValue>("gravity_vector")),
+    _gravity_magnitude(_gravity_vector.norm()),
+    _fe_type(_sim.getFlowFEType()),
+    _lump_mass_matrix(_flow_channel.getParam<bool>("lump_mass_matrix")),
+    _output_vector_velocity(params.get<bool>("output_vector_velocity"))
+{
+}
+
+const FunctionName &
+ThermalHydraulicsFlowPhysics::getVariableFn(const FunctionName & fn_param_name)
+{
+  const FunctionName & fn_name = _flow_channel.getParam<FunctionName>(fn_param_name);
+  const Function & fn = _sim.getFunction(fn_name);
+
+  if (dynamic_cast<const ConstantFunction *>(&fn) != nullptr)
+    _flow_channel.connectObject(fn.parameters(), fn_name, fn_param_name, "value");
+
+  return fn_name;
+}
+
+void
+ThermalHydraulicsFlowPhysics::addCommonVariables()
+{
+  const std::vector<SubdomainName> & subdomains = _flow_channel.getSubdomainNames();
+
+  _sim.addSimVariable(false, AREA, _fe_type, subdomains);
+  _sim.addSimVariable(false, HEAT_FLUX_PERIMETER, _fe_type, subdomains);
+  _sim.addSimVariable(false, AREA_LINEAR, FEType(FIRST, LAGRANGE), subdomains);
+}
+
+void
+ThermalHydraulicsFlowPhysics::addCommonInitialConditions()
+{
+  if (_flow_channel.isParamValid("A") && !_app.isRestarting())
+  {
+    const std::vector<SubdomainName> & block = _flow_channel.getSubdomainNames();
+    const FunctionName & area_function = _flow_channel.getAreaFunctionName();
+
+    if (!_sim.hasFunction(area_function))
+    {
+      const Function & fn = _sim.getFunction(area_function);
+      _sim.addConstantIC(AREA, fn.value(0, Point()), block);
+      _sim.addConstantIC(AREA_LINEAR, fn.value(0, Point()), block);
+
+      _flow_channel.makeFunctionControllableIfConstant(area_function, "Area", "value");
+    }
+    else
+    {
+      _sim.addFunctionIC(AREA_LINEAR, area_function, block);
+
+      {
+        const std::string class_name = "FunctionNodalAverageIC";
+        InputParameters params = getFactory().getValidParams(class_name);
+        params.set<VariableName>("variable") = AREA;
+        params.set<std::vector<SubdomainName>>("block") = block;
+        params.set<FunctionName>("function") = area_function;
+        _sim.addSimInitialCondition(class_name, genName(_comp_name, AREA, "ic"), params);
+      }
+    }
+  }
+}
+
+void
+ThermalHydraulicsFlowPhysics::addCommonMooseObjects()
+{
+  // add material property equal to one, useful for dummy multiplier values
+  {
+    const std::string class_name = "ConstantMaterial";
+    InputParameters params = getFactory().getValidParams(class_name);
+    params.set<std::vector<SubdomainName>>("block") = _flow_channel.getSubdomainNames();
+    params.set<std::string>("property_name") = ThermalHydraulicsFlowPhysics::UNITY;
+    params.set<Real>("value") = 1.0;
+    params.set<std::vector<VariableName>>("derivative_vars") = _derivative_vars;
+    _sim.addMaterial(class_name, genName(_comp_name, ThermalHydraulicsFlowPhysics::UNITY), params);
+  }
+}
