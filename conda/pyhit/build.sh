47eadbc4a7b084f4f816ac41f96a62e6245cad11
Jason M Miller <Jason.Miller@inl.gov>
2024-08-30 10:58:18 -0600

    testing
    DO NOT MERGE
    
    Refs: #0000

--- conda/pyhit/build.sh
+++ conda/pyhit/build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -eu
 
-git clean -xfd
-git submodule update --init moose
 cp -R pyhit $SP_DIR/
 cd src
 make bindings
-cp hit.so $SP_DIR/pyhit/
+install hit.so $SP_DIR/pyhit/
 cat > $SP_DIR/pyhit-$PKG_VERSION.egg-info <<FAKE_EGG
 Metadata-Version: 2.1
 Name: pyhit
