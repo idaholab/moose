cd65304679213351112b2deb5c114724c9a4ffa7
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2024-09-03 09:24:32 -0400

    Add first stab at a WCNSFV physics

---
+++ modules/thermal_hydraulics/include/physics/THMWCNSFVFlowPhysics.h
@@ -0,0 +1,69 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "ThermalHydraulicsFlowPhysics.h"
+#include "WCNSFVFlowPhysics.h"
+
+class PhysicsFlowBoundary;
+
+/**
+ * Sets up the single-phase flow equations using Euler's equations and a RDG discretization
+ */
+class THMWCNSFVFlowPhysics : public virtual ThermalHydraulicsFlowPhysics, public virtual WCNSFVFlowPhysics
+{
+public:
+  static InputParameters validParams();
+
+  THMWCNSFVFlowPhysics(const InputParameters & params);
+
+private:
+  virtual void actOnAdditionalTasks() override;
+
+  virtual void addNonlinearVariables() override;
+  virtual void addAuxiliaryVariables() override;
+  virtual void addTHMInitialConditions();
+  // virtual void addFEKernels() override;
+  // virtual void addDGKernels() override;
+  virtual void addAuxiliaryKernels() override;
+  virtual void addMaterials() override;
+  virtual void addUserObjects() override;
+
+  virtual void addInletBoundaries() override;
+  virtual void addOutletBoundaries() override;
+
+  // /// Adds the boundary flux boundary condition, which uses the boundary flux user object
+  // void addBoundaryFluxBC(const PhysicsFlowBoundary & comp,
+  //                        const UserObjectName & boundary_numerical_flux_name);
+
+public:
+  static const std::string DENSITY;
+  static const std::string FRICTION_FACTOR_DARCY;
+  static const std::string DYNAMIC_VISCOSITY;
+  static const std::string HEAT_TRANSFER_COEFFICIENT_WALL;
+  static const std::string HYDRAULIC_DIAMETER;
+  static const std::string PRESSURE;
+  static const std::string RHOA;
+  static const std::string RHOEA;
+  static const std::string RHOUA;
+  static const std::string SOUND_SPEED;
+  static const std::string SPECIFIC_HEAT_CONSTANT_PRESSURE;
+  static const std::string SPECIFIC_HEAT_CONSTANT_VOLUME;
+  static const std::string SPECIFIC_INTERNAL_ENERGY;
+  static const std::string SPECIFIC_TOTAL_ENTHALPY;
+  static const std::string SPECIFIC_VOLUME;
+  static const std::string TEMPERATURE;
+  static const std::string THERMAL_CONDUCTIVITY;
+  static const std::string VELOCITY;
+  static const std::string VELOCITY_X;
+  static const std::string VELOCITY_Y;
+  static const std::string VELOCITY_Z;
+  static const std::string REYNOLDS_NUMBER;
+};
---
+++ modules/thermal_hydraulics/src/physics/THMWCNSFVFlowPhysics.C
@@ -0,0 +1,158 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "THMWCNSFVFlowPhysics.h"
+#include "THMProblem.h"
+#include "FlowChannelBase.h"
+#include "SlopeReconstruction1DInterface.h"
+#include "PhysicsFlowBoundary.h"
+
+// TODO: consolidate those at the THMPhysics parent class level
+typedef THMWCNSFVFlowPhysics WCNSFV;
+const std::string WCNSFV::DENSITY = "rho";
+const std::string WCNSFV::FRICTION_FACTOR_DARCY = "f_D";
+const std::string WCNSFV::DYNAMIC_VISCOSITY = "mu";
+const std::string WCNSFV::HEAT_TRANSFER_COEFFICIENT_WALL = "Hw";
+const std::string WCNSFV::HYDRAULIC_DIAMETER = "D_h";
+const std::string WCNSFV::PRESSURE = "p";
+const std::string WCNSFV::RHOA = "rhoA";
+const std::string WCNSFV::RHOEA = "rhoEA";
+const std::string WCNSFV::RHOUA = "rhouA";
+const std::string WCNSFV::SOUND_SPEED = "c";
+const std::string WCNSFV::SPECIFIC_HEAT_CONSTANT_PRESSURE = "cp";
+const std::string WCNSFV::SPECIFIC_HEAT_CONSTANT_VOLUME = "cv";
+const std::string WCNSFV::SPECIFIC_INTERNAL_ENERGY = "e";
+const std::string WCNSFV::SPECIFIC_TOTAL_ENTHALPY = "H";
+const std::string WCNSFV::SPECIFIC_VOLUME = "v";
+const std::string WCNSFV::TEMPERATURE = "T";
+const std::string WCNSFV::THERMAL_CONDUCTIVITY = "k";
+const std::string WCNSFV::VELOCITY = "vel";
+const std::string WCNSFV::VELOCITY_X = "vel_x";
+const std::string WCNSFV::VELOCITY_Y = "vel_y";
+const std::string WCNSFV::VELOCITY_Z = "vel_z";
+const std::string WCNSFV::REYNOLDS_NUMBER = "Re";
+
+registerTHMFlowModelPhysicsBaseTasks("ThermalHydraulicsApp", THMWCNSFVFlowPhysics);
+registerNavierStokesPhysicsBaseTasks("ThermalHydraulicsApp", THMWCNSFVFlowPhysics);
+registerMooseAction("ThermalHydraulicsApp", THMWCNSFVFlowPhysics, "THMPhysics:add_ic");
+registerMooseAction("ThermalHydraulicsApp", THMWCNSFVFlowPhysics, "add_fv_kernel");
+registerMooseAction("ThermalHydraulicsApp", THMWCNSFVFlowPhysics, "add_fv_bc");
+registerMooseAction("ThermalHydraulicsApp", THMWCNSFVFlowPhysics, "add_material");
+registerMooseAction("ThermalHydraulicsApp", THMWCNSFVFlowPhysics, "add_corrector");
+registerMooseAction("ThermalHydraulicsApp", THMWCNSFVFlowPhysics, "add_postprocessor");
+registerMooseAction("ThermalHydraulicsApp", THMWCNSFVFlowPhysics, "get_turbulence_physics");
+registerMooseAction("ThermalHydraulicsApp", THMWCNSFVFlowPhysics, "add_aux_variable");
+registerMooseAction("ThermalHydraulicsApp", THMWCNSFVFlowPhysics, "add_aux_kernel");
+registerMooseAction("ThermalHydraulicsApp", THMWCNSFVFlowPhysics, "add_user_object");
+
+InputParameters
+THMWCNSFVFlowPhysics::validParams()
+{
+  InputParameters params = ThermalHydraulicsFlowPhysics::validParams();
+  params += WCNSFVFlowPhysics::validParams();
+
+  return params;
+}
+
+THMWCNSFVFlowPhysics::THMWCNSFVFlowPhysics(const InputParameters & params)
+  : ThermalHydraulicsFlowPhysics(params),
+    WCNSFVFlowPhysics(params)
+{
+}
+
+void
+THMWCNSFVFlowPhysics::actOnAdditionalTasks()
+{
+  // The THMProblem adds ICs on THM:add_variables, which happens before add_ic
+  if (_current_task == "THMPhysics:add_ic")
+    addTHMInitialConditions();
+}
+
+void
+THMWCNSFVFlowPhysics::addNonlinearVariables()
+{
+  ThermalHydraulicsFlowPhysics::addCommonVariables();
+  WCNSFVFlowPhysics::addNonlinearVariables();
+}
+
+void
+THMWCNSFVFlowPhysics::addAuxiliaryVariables()
+{
+}
+
+void
+THMWCNSFVFlowPhysics::addTHMInitialConditions()
+{
+  ThermalHydraulicsFlowPhysics::addCommonInitialConditions();
+  // TODO: check that they are valid
+  WCNSFVFlowPhysics::addInitialConditions();
+}
+
+void
+THMWCNSFVFlowPhysics::addMaterials()
+{
+  ThermalHydraulicsFlowPhysics::addCommonMaterials();
+  WCNSFVFlowPhysics::addMaterials();
+}
+
+void
+THMWCNSFVFlowPhysics::addAuxiliaryKernels()
+{
+}
+
+void
+THMWCNSFVFlowPhysics::addUserObjects()
+{
+  WCNSFVFlowPhysics::addUserObjects();
+}
+
+void
+THMWCNSFVFlowPhysics::addInletBoundaries()
+{
+  for (const auto i : index_range(_inlet_components))
+  {
+    // Get component, which has reference to the controllable parameters
+    const auto & comp_name = _inlet_components[i];
+    const auto & comp = _sim->getComponentByName<PhysicsFlowBoundary>(comp_name);
+    UserObjectName boundary_numerical_flux_name = "invalid";
+    const auto & boundary_type = _inlet_types[i];
+
+    // Boundary fluxes should be updated as often as possible
+    ExecFlagEnum userobject_execute_on(MooseUtils::getDefaultExecFlagEnum());
+    userobject_execute_on = {EXEC_INITIAL, EXEC_LINEAR, EXEC_NONLINEAR};
+
+    if (boundary_type == InletTypeEnum::MdotTemperature)
+    {
+
+    }
+  }
+}
+
+void
+THMWCNSFVFlowPhysics::addOutletBoundaries()
+{
+  for (const auto i : index_range(_outlet_components))
+  {
+    // Get component, which has reference to the controllable parameters
+    const auto & comp_name = _outlet_components[i];
+    const auto & comp = _sim->getComponentByName<PhysicsFlowBoundary>(comp_name);
+    UserObjectName boundary_numerical_flux_name = "invalid";
+    const auto & boundary_type = _outlet_types[i];
+
+    // Boundary fluxes should be updated as often as possible
+    ExecFlagEnum userobject_execute_on(MooseUtils::getDefaultExecFlagEnum());
+    userobject_execute_on = {EXEC_INITIAL, EXEC_LINEAR, EXEC_NONLINEAR};
+
+    if (boundary_type == OutletTypeEnum::FixedPressure)
+    {
+
+    }
+
+  }
+}
