bef3c1b22d9be37645c5fa975ad9f06567320b4f
Alexander Blair <alexander.blair@ukaea.uk>
2024-09-04 16:14:40 +0100

    Remove unused executioner headers

--- framework/include/mfem/base/platypus.h
+++ framework/include/mfem/base/platypus.h
@@ -1,5 +1,3 @@
 #pragma once
 #include "../common/pfem_extras.hpp"
 #include "problem_builder.h"
-#include "steady_executioner.h"
-#include "transient_executioner.h"
--- framework/include/mfem/executioners/executioner.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include "MFEMSteady.h"
-#include "MFEMTransient.h"
