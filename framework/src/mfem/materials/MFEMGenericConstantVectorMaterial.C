e24f5a112442ef3275081375b1f8643abc8aab3c
Chris MacMackin <chris.macmackin@ukaea.uk>
2024-09-06 10:10:55 +0100

    Added more material classes
    These provide the functionality of the MFEMCoefficient classes.

---
+++ framework/src/mfem/materials/MFEMGenericConstantVectorMaterial.C
@@ -0,0 +1,44 @@
+#include "MFEMGenericConstantVectorMaterial.h"
+
+registerMooseObject("PlatypusApp", MFEMGenericConstantVectorMaterial);
+
+InputParameters
+MFEMGenericConstantVectorMaterial::validParams()
+{
+  InputParameters params = MFEMMaterial::validParams();
+  params.addClassDescription(
+      "Declares material properties based on names and values prescribed by input parameters.");
+  params.addRequiredParam<std::vector<std::string>>(
+      "prop_names", "The names of the properties this material will have");
+  params.addRequiredParam<std::vector<Real>>("prop_values",
+                                             "The values associated with the named properties");
+
+  params.declareControllable("prop_values");
+  return params;
+}
+
+MFEMGenericConstantVectorMaterial::MFEMGenericConstantVectorMaterial(
+    const InputParameters & parameters)
+  : MFEMMaterial(parameters),
+    _prop_names(getParam<std::vector<std::string>>("prop_names")),
+    _prop_values(getParam<std::vector<Real>>("prop_values"))
+{
+  unsigned int num_names = _prop_names.size();
+  unsigned int num_values = _prop_values.size();
+
+  if (num_names != num_values * LIBMESH_DIM)
+    mooseError("Number of prop_values must be equal to dim * number of prop_values for a "
+               "GenericConstantMaterial!");
+
+  _num_props = num_names;
+  for (unsigned int i = 0; i < _num_props; i++)
+  {
+    const int j = i * LIBMESH_DIM;
+    _properties.declareVector(
+        _prop_names[i],
+        mfem::Vector({_prop_values[j], _prop_values[j + 1], _prop_values[j + 2]}),
+        subdomainsToStrings(_block_ids));
+  }
+}
+
+MFEMGenericConstantVectorMaterial::~MFEMGenericConstantVectorMaterial() {}
