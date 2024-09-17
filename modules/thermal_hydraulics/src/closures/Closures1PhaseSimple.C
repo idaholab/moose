cf19cdbdab0a7c9db897c29900efad071614b414
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2024-09-17 17:01:03 -0600

    Let closure work with both 1phase and physics-heat transfer

--- modules/thermal_hydraulics/src/closures/Closures1PhaseSimple.C
+++ modules/thermal_hydraulics/src/closures/Closures1PhaseSimple.C
@@ -88,47 +88,44 @@ Closures1PhaseSimple::addMooseObjectsHeatTransfer(const HeatTransferBase & heat_
                                                   const FlowChannelBase & flow_channel)
 {
   const FunctionName & Hw_fn_name = heat_transfer.getParam<FunctionName>("Hw");
+  std::string htc_name;
+  if (dynamic_cast<const HeatTransfer1PhaseBase *>(&heat_transfer))
+    htc_name = dynamic_cast<const HeatTransfer1PhaseBase &>(heat_transfer)
+                   .getWallHeatTransferCoefficient1PhaseName();
+  else if (dynamic_cast<const PhysicsHeatTransferBase *>(&heat_transfer))
+    htc_name = dynamic_cast<const PhysicsHeatTransferBase &>(heat_transfer)
+                   .getWallHeatTransferCoefficientName();
+  else
+    mooseError("Component " + heat_transfer.name() + " is not of the expected type");
 
   if (_add_regular_materials)
   {
-    if (!dynamic_cast<const HeatTransfer1PhaseBase *>(&heat_transfer))
-      mooseError("Component " + heat_transfer.name() + " is not of the expected type");
-    const HeatTransfer1PhaseBase & heat_transfer_1phase =
-        dynamic_cast<const HeatTransfer1PhaseBase &>(heat_transfer);
-    {
-      const std::string class_name = "ADGenericFunctionMaterial";
-      InputParameters params = _factory.getValidParams(class_name);
-      params.set<std::vector<SubdomainName>>("block") = flow_channel.getSubdomainNames();
-      params.set<std::vector<std::string>>("prop_names") = {
-          heat_transfer_1phase.getWallHeatTransferCoefficient1PhaseName()};
-      params.set<std::vector<FunctionName>>("prop_values") = {Hw_fn_name};
-      params.applyParameter(parameters(), "outputs");
-      _sim.addMaterial(
-          class_name, genName(heat_transfer.name(), "Hw_material", flow_channel.name()), params);
-    }
+    const std::string class_name = "ADGenericFunctionMaterial";
+    InputParameters params = _factory.getValidParams(class_name);
+    params.set<std::vector<SubdomainName>>("block") = flow_channel.getSubdomainNames();
+    params.set<std::vector<std::string>>("prop_names") = {htc_name};
+    params.set<std::vector<FunctionName>>("prop_values") = {Hw_fn_name};
+    params.applyParameter(parameters(), "outputs");
+    _sim.addMaterial(
+        class_name, genName(heat_transfer.name(), "Hw_material", flow_channel.name()), params);
 
     heat_transfer.makeFunctionControllableIfConstant(Hw_fn_name, "Hw");
   }
   if (_add_functor_materials)
   {
-    if (!dynamic_cast<const PhysicsHeatTransferBase *>(&heat_transfer))
-      mooseError("Component " + heat_transfer.name() + " is not of the expected type");
-    const PhysicsHeatTransferBase & phys_heat_transfer =
-        dynamic_cast<const PhysicsHeatTransferBase &>(heat_transfer);
     if (MooseUtils::parsesToReal(Hw_fn_name))
     {
       const std::string class_name = "ADGenericFunctorMaterial";
       InputParameters params = _factory.getValidParams(class_name);
       params.set<std::vector<SubdomainName>>("block") = flow_channel.getSubdomainNames();
-      params.set<std::vector<std::string>>("prop_names") = {
-          phys_heat_transfer.getWallHeatTransferCoefficientName()};
+      params.set<std::vector<std::string>>("prop_names") = {};
       params.set<std::vector<MooseFunctorName>>("prop_values") = {Hw_fn_name};
       params.applyParameter(parameters(), "outputs");
       _sim.addFunctorMaterial(
           class_name, genName(heat_transfer.name(), "Hw_material", flow_channel.name()), params);
-    }
 
-    heat_transfer.makeFunctionControllableIfConstant(Hw_fn_name, "Hw");
+      heat_transfer.makeFunctionControllableIfConstant(Hw_fn_name, "Hw");
+    }
   }
 }
 
