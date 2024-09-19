ada62fb5fd869724a813aaaf67b3c517b358935f
Peter German <grmnptr@gmail.com>
2024-09-19 16:53:42 -0600

    Add include file for tha sampler object. (#28671)

---
+++ modules/stochastic_tools/include/samplers/DirectPerturbationSampler.h
@@ -0,0 +1,31 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "Sampler.h"
+
+/**
+ * A class used to generate samples for a direct perturbation analysis.
+ */
+class DirectPerturbationSampler : public Sampler
+{
+public:
+  static InputParameters validParams();
+
+  DirectPerturbationSampler(const InputParameters & parameters);
+
+protected:
+  /// Return the sample for the given row and column
+  virtual Real computeSample(dof_id_type row_index, dof_id_type col_index) override;
+
+private:
+  /// The nomial values of the parameters
+  std::vector<Real> _nominal_values;
+};
