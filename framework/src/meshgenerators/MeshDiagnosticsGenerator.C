33f1f5b182a179550d7459d7905bfdbd36b493ac
DanielYankura <yankdc@rod.hpc.inl.gov>
2024-09-27 10:23:40 -0600

    More style changes for Civet tests

--- framework/src/meshgenerators/MeshDiagnosticsGenerator.C
+++ framework/src/meshgenerators/MeshDiagnosticsGenerator.C
@@ -1465,20 +1465,24 @@ MeshDiagnosticsGenerator::checkNonMatchingEdges(const std::unique_ptr<MeshBase> 
             continue;
           }
 
-          //Check element/edge type 
+          // Check element/edge type
           if (edge->type() != EDGE2)
           {
-            std::string element_message = "Edge of type " + Utility::enum_to_string(edge->type()) + " was found in cell " + std::to_string(elem->id()) + 
-                                          " which is of type " + Utility::enum_to_string(elem->type()) + '\n' + 
-                                          "The edge intersection check only works for EDGE2 elements";
+            std::string element_message =
+                "Edge of type " + Utility::enum_to_string(edge->type()) + " was found in cell " +
+                std::to_string(elem->id()) + " which is of type " +
+                Utility::enum_to_string(elem->type()) + '\n' +
+                "The edge intersection check only works for EDGE2 elements";
             _console << element_message << std::endl;
             continue;
           }
           if (other_edge->type() != EDGE2)
           {
-            std::string element_message = "Edge of type " + Utility::enum_to_string(other_edge->type()) + " was found in cell " + std::to_string(other_elem->id()) + 
-                                          " which is of type " + Utility::enum_to_string(other_elem->type()) + '\n' + 
-                                          "The edge intersection check only works for EDGE2 elements";
+            std::string element_message =
+                "Edge of type " + Utility::enum_to_string(other_edge->type()) +
+                " was found in cell " + std::to_string(other_elem->id()) + " which is of type " +
+                Utility::enum_to_string(other_elem->type()) + '\n' +
+                "The edge intersection check only works for EDGE2 elements";
             _console << element_message << std::endl;
             continue;
           }
