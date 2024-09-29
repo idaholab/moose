c4dff6db64ad2aa7d2dc2f7fc5aa161c47a92d81
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2024-09-29 16:18:01 -0600

    Add base classes for scalar transfer

---
+++ modules/thermal_hydraulics/include/components/ScalarTransferBase.h
@@ -0,0 +1,80 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "ConnectorBase.h"
+
+class ClosuresBase;
+
+/**
+ * Base class for scalar transfer connections
+ */
+class ScalarTransferBase : public ConnectorBase
+{
+public:
+  static InputParameters validParams();
+
+  ScalarTransferBase(const InputParameters & parameters);
+
+  /**
+   * Returns wall scalar flux name
+   *
+   * @return The name of wall scalar flux functor
+   */
+  const MooseFunctorName & getWallScalarFluxName(unsigned int scalar_i) const;
+
+  /**
+   * Returns whether this heat transfer is specified by temperature, rather than heat flux
+   *
+   * @return true if the heat transfer is specified by temperature, false otherwise
+   */
+  virtual bool isTemperatureType() const = 0;
+
+  /**
+   * Get the list of the subdomain names associated with the flow channel
+   *
+   * @return List of subdomain names associated with the flow channel
+   */
+  const std::vector<SubdomainName> & getFlowChannelSubdomains() const
+  {
+    return _flow_channel_subdomains;
+  }
+
+  const UserObjectName & getFluidPropertiesName() const;
+
+  /**
+   * Get the name of the connected flow channel
+   */
+  const std::string & getFlowChannelName() const { return _flow_channel_name; }
+
+protected:
+  virtual void init() override;
+  virtual void initSecondary() override;
+  virtual void check() const override;
+
+  /// name of the connected flow channel
+  const std::string _flow_channel_name;
+
+  /// Names of the passive scalars being transferred
+  std::vector<VariableName> _passive_scalar_names;
+
+  /// Used closures
+  std::shared_ptr<ClosuresBase> _closures;
+
+  /// wall scalar flux name
+  std::vector<MooseFunctorName> _wall_scalar_flux_names;
+
+  /// Subdomains corresponding to the connected flow channel
+  std::vector<SubdomainName> _flow_channel_subdomains;
+  /// fluid properties object name
+  UserObjectName _fp_name;
+  /// area function name for the connected flow channel
+  FunctionName _A_fn_name;
+};
