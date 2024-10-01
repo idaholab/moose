863ad83f67e79f934299e89b7b6da80e14e35530
Alexander Blair <alexander.blair@ukaea.uk>
2024-10-01 17:26:26 +0100

    Remove Hephaestus boundary conditions

--- framework/src/mfem/bcs/MFEMVectorNormalIntegratedBC.C
+++ framework/src/mfem/bcs/MFEMVectorNormalIntegratedBC.C
@@ -5,7 +5,7 @@ registerMooseObject("PlatypusApp", MFEMVectorNormalIntegratedBC);
 InputParameters
 MFEMVectorNormalIntegratedBC::validParams()
 {
-  InputParameters params = MFEMBoundaryCondition::validParams();
+  InputParameters params = MFEMIntegratedBC::validParams();
   params.addRequiredParam<UserObjectName>(
       "vector_coefficient",
       "The vector MFEM coefficient whose normal component will be used in the integrated BC");
@@ -14,13 +14,23 @@ MFEMVectorNormalIntegratedBC::validParams()
 
 // TODO: Currently assumes the vector function coefficient is 3D
 MFEMVectorNormalIntegratedBC::MFEMVectorNormalIntegratedBC(const InputParameters & parameters)
-  : MFEMBoundaryCondition(parameters),
+  : MFEMIntegratedBC(parameters),
     _vec_coef(const_cast<MFEMVectorCoefficient *>(
         &getUserObject<MFEMVectorCoefficient>("vector_coefficient")))
 {
+}
 
-  _boundary_condition = std::make_shared<platypus::IntegratedBC>(
-      getParam<std::string>("variable"),
-      bdr_attr,
-      std::make_unique<mfem::BoundaryNormalLFIntegrator>(*_vec_coef->getVectorCoefficient()));
+// Create a new MFEM integrator to apply to the RHS of the weak form. Ownership managed by the
+// caller.
+mfem::LinearFormIntegrator *
+MFEMVectorNormalIntegratedBC::createLinearFormIntegrator()
+{
+  return new mfem::BoundaryNormalLFIntegrator(*_vec_coef->getVectorCoefficient());
+}
+
+// Create a new MFEM integrator to apply to LHS of the weak form. Ownership managed by the caller.
+mfem::BilinearFormIntegrator *
+MFEMVectorNormalIntegratedBC::createBilinearFormIntegrator()
+{
+  return nullptr;
 }
