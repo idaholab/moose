3e170a01029b1af77d90c4875597e9af7bdd9e62
Alexander Blair <alexander.blair@ukaea.uk>
2024-10-02 17:37:46 +0100

    Move mfem::Device into MFEMExecutioners to prevent segfaults on GPU during destructor calls

--- framework/src/mfem/executioners/MFEMExecutioner.C
+++ framework/src/mfem/executioners/MFEMExecutioner.C
@@ -4,7 +4,9 @@
 InputParameters
 MFEMExecutioner::validParams()
 {
-  return Executioner::validParams();
+  InputParameters params = Executioner::validParams();  
+  params.addParam<std::string>("device", "cpu", "Run app on the chosen device.");
+  return params;
 }
 
 MFEMExecutioner::MFEMExecutioner(const InputParameters & parameters)
@@ -12,4 +14,12 @@ MFEMExecutioner::MFEMExecutioner(const InputParameters & parameters)
     _mfem_problem(dynamic_cast<MFEMProblem &>(feProblem())),
     _problem_data(_mfem_problem.getProblemData())
 {
+  setDevice();
+}
+
+void
+MFEMExecutioner::setDevice()
+{
+  _device.Configure(getParam<std::string>("device"));
+  _device.Print(std::cout);
 }
