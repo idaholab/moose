6f3c18f5dee9d3e63661e18cdd6e59bed9323c42
Peter German <grmnptr@gmail.com>
2024-10-03 14:31:57 -0600

    Add include file for fv flux integral bc. (#28767)

---
+++ framework/include/postprocessors/SideFVFluxBCIntegral.h
@@ -0,0 +1,32 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+// MOOSE includes
+#include "SideIntegralPostprocessor.h"
+#include "FVFluxBC.h"
+
+/**
+ * This postprocessor computes the side integral of different
+ * finite volume flux boundary conditions.
+ */
+class SideFVFluxBCIntegral : public SideIntegralPostprocessor
+{
+public:
+  static InputParameters validParams();
+
+  SideFVFluxBCIntegral(const InputParameters & parameters);
+
+protected:
+  Real computeQpIntegral() override;
+  Real computeFaceInfoIntegral(const FaceInfo * fi) override;
+
+  const std::vector<std::string> _bc_names;
+};
