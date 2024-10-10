1841a414734af88c275562e7f0d8715295830069
Peter German <grmnptr@gmail.com>
2024-10-10 10:50:25 -0600

    Add momentum predictor and pressure corrector to SIMPLESolve. (#28819)

--- modules/navier_stokes/src/executioners/SIMPLESolve.C
+++ modules/navier_stokes/src/executioners/SIMPLESolve.C
@@ -39,3 +39,152 @@ SIMPLESolve::solve()
   // The main chunk of the code will be migrate here.
   return true;
 }
+
+std::vector<std::pair<unsigned int, Real>>
+SIMPLESolve::solveMomentumPredictor()
+{
+  // Temporary storage for the (flux-normalized) residuals form
+  // different momentum components
+  std::vector<std::pair<unsigned int, Real>> its_normalized_residuals;
+
+  LinearImplicitSystem & momentum_system_0 =
+      libMesh::cast_ref<LinearImplicitSystem &>(_momentum_systems[0]->system());
+
+  PetscLinearSolver<Real> & momentum_solver =
+      libMesh::cast_ref<PetscLinearSolver<Real> &>(*momentum_system_0.get_linear_solver());
+
+  momentum_solver.reuse_preconditioner(true);
+
+  // Solve the momentum equations.
+  // TO DO: These equations are VERY similar. If we can store the differences (things coming from
+  // BCs for example) separately, it is enough to construct one matrix.
+  for (const auto system_i : index_range(_momentum_systems))
+  {
+    _problem.setCurrentLinearSystem(_momentum_system_numbers[system_i]);
+
+    // We will need the right hand side and the solution of the next component
+    LinearImplicitSystem & momentum_system =
+        libMesh::cast_ref<LinearImplicitSystem &>(_momentum_systems[system_i]->system());
+
+    NumericVector<Number> & solution = *(momentum_system.solution);
+    NumericVector<Number> & rhs = *(momentum_system.rhs);
+    SparseMatrix<Number> & mmat = *(momentum_system.matrix);
+
+    auto diff_diagonal = solution.zero_clone();
+
+    mmat.zero();
+    rhs.zero();
+
+    // We plug zero in this to get the system matrix and the right hand side of the linear problem
+    _problem.computeLinearSystemSys(momentum_system, mmat, rhs);
+
+    // Still need to relax the right hand side with the same vector
+    relaxMatrix(mmat, _momentum_equation_relaxation, *diff_diagonal);
+    relaxRightHandSide(rhs, solution, *diff_diagonal);
+
+    // The normalization factor depends on the right hand side so we need to recompute it for this
+    // component
+    Real norm_factor = computeNormalizationFactor(solution, mmat, rhs);
+
+    // Very important, for deciding the convergence, we need the unpreconditioned
+    // norms in the linear solve
+    LIBMESH_CHKERR(KSPSetNormType(momentum_solver.ksp(), KSP_NORM_UNPRECONDITIONED));
+    // Solve this component. We don't update the ghosted solution yet, that will come at the end
+    // of the corrector step. Also setting the linear tolerances and maximum iteration counts.
+    _momentum_linear_control.real_valued_data["abs_tol"] = _momentum_l_abs_tol * norm_factor;
+    momentum_solver.set_solver_configuration(_momentum_linear_control);
+
+    // We solve the equation
+    auto its_resid_pair = momentum_solver.solve(mmat, mmat, solution, rhs);
+    momentum_system.update();
+
+    // Save the normalized residual
+    its_normalized_residuals.push_back(
+        std::make_pair(its_resid_pair.first, momentum_solver.get_initial_residual() / norm_factor));
+
+    if (_print_fields)
+    {
+      _console << " solution before solve " << std::endl;
+      solution.print();
+      _console << " matrix when we solve " << std::endl;
+      mmat.print();
+      _console << " rhs when we solve " << std::endl;
+      rhs.print();
+      _console << " velocity solution component " << system_i << std::endl;
+      solution.print();
+      _console << "Norm factor " << norm_factor << std::endl;
+      _console << Moose::stringify(momentum_solver.get_initial_residual()) << std::endl;
+    }
+  }
+
+  for (const auto system_i : index_range(_momentum_systems))
+  {
+    LinearImplicitSystem & momentum_system =
+        libMesh::cast_ref<LinearImplicitSystem &>(_momentum_systems[system_i]->system());
+    _momentum_systems[system_i]->setSolution(*(momentum_system.current_local_solution));
+    _momentum_systems[system_i]->copySolutionsBackwards();
+  }
+
+  return its_normalized_residuals;
+}
+
+std::pair<unsigned int, Real>
+SIMPLESolve::solvePressureCorrector()
+{
+  _problem.setCurrentLinearSystem(_pressure_sys_number);
+
+  // We will need some members from the linear system
+  LinearImplicitSystem & pressure_system =
+      libMesh::cast_ref<LinearImplicitSystem &>(_pressure_system.system());
+
+  // We will need the solution, the right hand side and the matrix
+  NumericVector<Number> & current_local_solution = *(pressure_system.current_local_solution);
+  NumericVector<Number> & solution = *(pressure_system.solution);
+  SparseMatrix<Number> & mmat = *(pressure_system.matrix);
+  NumericVector<Number> & rhs = *(pressure_system.rhs);
+
+  mmat.zero();
+  rhs.zero();
+
+  // Fetch the linear solver from the system
+  PetscLinearSolver<Real> & pressure_solver =
+      libMesh::cast_ref<PetscLinearSolver<Real> &>(*pressure_system.get_linear_solver());
+
+  _problem.computeLinearSystemSys(pressure_system, mmat, rhs, false);
+
+  if (_print_fields)
+  {
+    _console << "Pressure matrix" << std::endl;
+    mmat.print();
+  }
+
+  // We compute the normalization factors based on the fluxes
+  Real norm_factor = computeNormalizationFactor(solution, mmat, rhs);
+
+  // We need the non-preconditioned norm to be consistent with the norm factor
+  LIBMESH_CHKERR(KSPSetNormType(pressure_solver.ksp(), KSP_NORM_UNPRECONDITIONED));
+
+  // Setting the linear tolerances and maximum iteration counts
+  _pressure_linear_control.real_valued_data["abs_tol"] = _pressure_l_abs_tol * norm_factor;
+  pressure_solver.set_solver_configuration(_pressure_linear_control);
+
+  if (_pin_pressure)
+    constrainSystem(mmat, rhs, _pressure_pin_value, _pressure_pin_dof);
+  pressure_system.update();
+
+  auto its_res_pair = pressure_solver.solve(mmat, mmat, solution, rhs);
+  pressure_system.update();
+
+  if (_print_fields)
+  {
+    _console << " rhs when we solve pressure " << std::endl;
+    rhs.print();
+    _console << " Pressure " << std::endl;
+    solution.print();
+    _console << "Norm factor " << norm_factor << std::endl;
+  }
+
+  _pressure_system.setSolution(current_local_solution);
+
+  return std::make_pair(its_res_pair.first, pressure_solver.get_initial_residual() / norm_factor);
+}
