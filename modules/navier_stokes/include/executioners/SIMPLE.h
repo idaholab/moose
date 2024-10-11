9bef1834f1b53a4f30c968b1ec2954e3a932ab05
Peter German <grmnptr@gmail.com>
2024-10-11 12:51:07 -0600

    Use fixed point solve with SIMPLESolve within linear segregated solver. (#28819)

--- modules/navier_stokes/include/executioners/SIMPLE.h
+++ modules/navier_stokes/include/executioners/SIMPLE.h
@@ -13,6 +13,7 @@
 #include "RhieChowMassFlux.h"
 #include "PetscSupport.h"
 #include "SolverParams.h"
+#include "SIMPLESolve.h"
 
 #include "libmesh/petsc_vector.h"
 #include "libmesh/petsc_matrix.h"
@@ -39,49 +40,5 @@ public:
   virtual bool lastSolveConverged() const override { return _last_solve_converged; }
 
 protected:
-  /// Solve a momentum predictor step with a fixed pressure field
-  /// @return A vector of (number of linear iterations, normalized residual norm) pairs for
-  /// the momentum equations. The length of the vector equals the dimensionality of
-  /// the domain.
-  std::vector<std::pair<unsigned int, Real>> solveMomentumPredictor();
-
-  /// Solve a pressure corrector step.
-  /// @return The number of linear iterations and the normalized residual norm of
-  /// the pressure equation.
-  std::pair<unsigned int, Real> solvePressureCorrector();
-
-  /// Solve an equation which contains an advection term that depends
-  /// on the solution of the segregated Navier-Stokes equations.
-  /// @param system_num The number of the system which is solved
-  /// @param system Reference to the system which is solved
-  /// @param relaxation_factor The relaxation factor for matrix relaxation
-  /// @param solver_config The solver configuration object for the linear solve
-  /// @param abs_tol The scaled absolute tolerance for the linear solve
-  /// @return The normalized residual norm of the equation.
-  std::pair<unsigned int, Real> solveAdvectedSystem(const unsigned int system_num,
-                                                    LinearSystem & system,
-                                                    const Real relaxation_factor,
-                                                    SolverConfiguration & solver_config,
-                                                    const Real abs_tol);
-
-  /// The number(s) of the system(s) corresponding to the momentum equation(s)
-  std::vector<unsigned int> _momentum_system_numbers;
-
-  /// The number of the system corresponding to the pressure equation
-  const unsigned int _pressure_sys_number;
-
-  /// The number of the system corresponding to the energy equation
-  const unsigned int _energy_sys_number;
-
-  /// Pointer(s) to the system(s) corresponding to the momentum equation(s)
-  std::vector<LinearSystem *> _momentum_systems;
-
-  /// Reference to the nonlinear system corresponding to the pressure equation
-  LinearSystem & _pressure_system;
-
-  /// Pointer to the nonlinear system corresponding to the fluid energy equation
-  LinearSystem * _energy_system;
-
-  /// Pointer to the segregated RhieChow interpolation object
-  RhieChowMassFlux * _rc_uo;
+  SIMPLESolve _simple_solve;
 };
