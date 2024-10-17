a7c9f4c3dfbeb0c62bdb6693a6f077f78f494286
Jason Miller <jason.miller@inl.gov>
2024-10-17 13:05:08 -0600

    Add retry functionality
    Add a retry function for all packages that can support it.

--- conda/wasp/build_wasp.sh
+++ conda/wasp/build_wasp.sh
@@ -2,13 +2,24 @@
 set -eu
 export PATH=/bin:$PATH
 
-rm -rf build; mkdir build; cd build
-WASP_OPTIONS="-D CMAKE_INSTALL_PREFIX:STRING=${PREFIX}/wasp"
-source $SRC_DIR/configure_wasp.sh
-configure_wasp "$WASP_OPTIONS" ../
+function do_build(){
+    rm -rf "${SRC_DIR:?}/build" "${PREFIX:?}/wasp"
+    mkdir "${SRC_DIR:?}/build" || exit 1
+    cd "${SRC_DIR:?}/build" || exit 1
 
-CORES=${MOOSE_JOBS:-2}
-make -j $CORES install
+    local WASP_OPTIONS="-D CMAKE_INSTALL_PREFIX:STRING=${PREFIX}/wasp"
+    # shellcheck disable=SC1091  # made available through meta.yaml src path
+    source "${SRC_DIR}/configure_wasp.sh"
+    configure_wasp "$WASP_OPTIONS" ../ || return 1
+    make -j "${MOOSE_JOBS:-2}" install || return 1
+}
+
+# shellcheck disable=SC1091  # made available through meta.yaml src path
+source "${SRC_DIR:?}/retry_build.sh"
+
+# Sets up retry functions and calls do_build. Blocking until success
+# or 3 failed attempts, or 1 unknown/unhandled failure
+retry_build
 
 # Set WASP_DIR environment variable(s)
 mkdir -p "${PREFIX}/etc/conda/activate.d" "${PREFIX}/etc/conda/deactivate.d"
