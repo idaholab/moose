a4db563af7894e4329df6ab5f4989492b16eff0e
Jesse Carter <jesse.carter@gmail.com>
2024-10-18 09:10:32 -0600

    template JSONFileReader getter functions to support more than just Real data, ref #28850

--- framework/include/userobjects/JSONFileReader.h
+++ framework/include/userobjects/JSONFileReader.h
@@ -36,7 +36,15 @@ public:
    * @param scalar_name the name of the desired scalar
    * @param scalar reference to the scalar that will be set with the scalar in the JSON
    */
-  void getScalar(const std::string & scalar_name, Real & scalar) const;
+  template<typename T>
+  void getScalar(const std::string & scalar_name, T & scalar) const
+  {
+    if (!_root.contains(scalar_name))
+      mooseError("Attempted to get '",
+                 scalar_name,
+                 "' but the JSON file does not contain this key directly at the root level");
+    scalar = _root[scalar_name].get<T>();
+  }
 
   /**
    * Get a scalar in the JSON file/tree using the keys in the 'scalar_keys' one by one to
@@ -44,22 +52,77 @@ public:
    * @param scalar_keys the keys in descending order to access the scalar
    * @param scalar reference to the scalar that will be set with the scalar in the JSON
    */
-  void getScalar(const std::vector<std::string> & scalar_keys, Real & scalar) const;
+  template<typename T>
+  void getScalar(const std::vector<std::string> & scalar_keys, T & scalar) const
+  {
+    if (!scalar_keys.size())
+      mooseError("There should be at least one key to retrieve a value from the JSON");
 
+    // traverse the JSON tree
+    auto * current_node = &_root[scalar_keys[0]];
+    for (const auto key_index : index_range(scalar_keys))
+    {
+      if (key_index == scalar_keys.size() - 1)
+      {
+        scalar = current_node->get<T>();
+        break;
+      }
+      current_node = &(*current_node)[scalar_keys[key_index + 1]];
+    }
+  }
   /// Getter for vector values
   /**
    * Get a vector that is directly indexed at the root of the JSON file/tree
    * @param vector_name the name of the desired vector
-   * @param scalar reference to the vector that will be set with the vector in the JSON
+   * @param vector_to_fill reference to the vector that will be set with the vector in the JSON
    */
-  void getVector(const std::string & vector_name, std::vector<Real> & vector) const;
+  template<typename T>
+  void getVector(const std::string & vector_name, std::vector<T> & vector_to_fill) const
+  {
+  if (!_root.contains(vector_name))
+    mooseError("Attempted to get '",
+               vector_name,
+               "' but the JSON file does not contain this key at the root level");
+  vector_to_fill.clear();
+  for (const auto& item : _root[vector_name])
+    vector_to_fill.push_back(item.get<T>());
+  }
   /**
    * Get a vector in the JSON file/tree using the keys in the 'vector_keys' vector one by one
    * to traverse the JSON tree down to the requested vector
    * @param vector_keys the keys in descending order to access the vector
-   * @param vector reference to the vector that will be set with the vector in the JSON
+   * @param vector_to_fill reference to the vector that will be set with the vector in the JSON
    */
-  void getVector(const std::vector<std::string> & vector_keys, std::vector<Real> & vector) const;
+  template<typename T>
+  void getVector(const std::vector<std::string>& vector_keys, std::vector<T>& vector_to_fill) const
+  {
+    if (!vector_keys.size())
+      mooseError("There should be at least one key to retrieve a value from the JSON");
+
+    // traverse the JSON tree
+    auto * current_node = &_root[vector_keys[0]];
+    for (const auto key_index : index_range(vector_keys))
+    {
+      if (key_index == vector_keys.size() - 1)
+      {
+        if (!current_node->is_array())
+          mooseError("Cannot retrieve a vector from JSON node",
+                     *current_node,
+                     "obtained with last key",
+                     vector_keys[key_index]);
+        vector_to_fill.clear();
+        for (const auto& item : *current_node)
+          vector_to_fill.push_back(item.get<T>());
+        return;
+      }
+      if (current_node->is_array())
+        mooseError("Cannot obtain nested JSON item with key",
+                   vector_keys[key_index + 1],
+                   "because the current item is an array:",
+                   *current_node);
+      current_node = &(*current_node)[vector_keys[key_index + 1]];
+    }
+  }
 
 private:
   /**
@@ -67,13 +130,6 @@ private:
    * @param filename the name of the file
    */
   void read(const FileName & filename);
-
-  /**
-   * Get a real number from a node and handle potential type conversions needed
-   * @param node a node from the JSON tree
-   * @return the real number desired
-   */
-  Real getReal(const nlohmann::json & node) const;
 
   /// Database filename
   const FileName & _filename;
