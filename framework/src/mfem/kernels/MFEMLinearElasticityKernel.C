9b3d2cdbb547ba7115568077f24c0016c612da39
Kingsley Collie <kingsley.collie@ukaea.uk>
2024-10-30 13:18:27 +0000

    Match doc and comment for LinearElasticity integrator with MFEM

--- framework/src/mfem/kernels/MFEMLinearElasticityKernel.C
+++ framework/src/mfem/kernels/MFEMLinearElasticityKernel.C
@@ -7,11 +7,14 @@ MFEMLinearElasticityKernel::validParams()
 {
   InputParameters params = MFEMKernel::validParams();
   params.addClassDescription(
-      "The Cauchy stress tensor $\\sigma(u)_{ij} = \\lambda \\varepsilon_{kk} \\delta_{ij} "
-      "+ 2 \\mu \\varepsilon_{ij}$, where $\\varepsilon(u)_ij = \\frac{1}{2} \\partial_j u_i + "
-      "\\partial_i u_j$, with the weak form of "
-      "$(\\lambda \\varepsilon(\\phi_i)_{kk}, \\varepsilon(u_h)_{kk}) + 2 (\\mu "
-      "\\varepsilon(\\phi_i)_{mn}, \\varepsilon(u_h)_{mn} )$, to be added to an MFEM problem");
+      "The isotropic linear elasticity operator with weak form "
+      "$(c_{ikjl} \\nabla u_j, \\nabla v_i)$, to be added to an MFEM problem, where "
+      "$c_{ikjl}$ is the isotropic elasticity tensor, "
+      "$c_{ikjl} = \\lambda \\delta_{ik} \\delta_{jl} + \\mu \\left( \\delta_{ij} \\delta_{kl} + "
+      "\\delta_{il} \\delta_{jk} \\right)$, "
+      "$\\lambda$ is the first Lamé parameter, $\\lambda = \\frac{E\\nu}{(1-2\\nu)(1+\\nu)}$, "
+      "$\\mu$ is the second Lamé parameter, $\\mu = \\frac{E}{2(1+\\nu)}, "
+      "where $E$ is Young's modulus and $\\nu$ is Poisson's ratio.");
 
   params.addParam<std::string>(
       "lambda", "Name of MFEM Lame constant lambda to multiply the div(u)*I term by");
