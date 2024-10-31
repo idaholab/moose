1a40f3c0eca3624427badaf01b7023cf3fc16bb2
Henrique B. R. <henrique.bergallo-rocha@ukaea.uk>
2024-10-31 16:23:51 +0000

    Add MFEMConduitDataCollection

---
+++ framework/src/mfem/outputs/MFEMConduitDataCollection.C
@@ -0,0 +1,24 @@
+#include "MFEMConduitDataCollection.h"
+
+registerMooseObject("PlatypusApp", MFEMConduitDataCollection);
+
+InputParameters
+MFEMConduitDataCollection::validParams()
+{
+  InputParameters params = MFEMDataCollection::validParams();
+  params.addClassDescription("Output for controlling MFEMConduitDataCollection inherited data.");
+  params.addParam<std::string>("protocol",
+                                "hdf5",
+                                "Conduit relay I/O protocol to use. Options: hdf5 (default), json, conduit_json, conduit_bin");
+  return params;
+}
+
+MFEMConduitDataCollection::MFEMConduitDataCollection(const InputParameters & parameters)
+  : MFEMDataCollection(parameters),
+    _conduit_dc((_file_base + std::string("/Run") + std::to_string(getFileNumber())).c_str(),
+              _problem_data._pmesh.get()),
+    _protocol(getParam<std::string>("protocol"))
+{
+  _conduit_dc.SetProtocol(_protocol);
+  registerFields();
+}
