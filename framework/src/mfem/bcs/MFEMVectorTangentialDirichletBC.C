cd58564324d041a704c2285c8b45d00e15477456
Alexander Blair <alexander.blair@ukaea.uk>
2024-11-04 16:47:01 +0000

    Fix issues in MFEMVectorDirichletBC class descriptions

--- framework/src/mfem/bcs/MFEMVectorTangentialDirichletBC.C
+++ framework/src/mfem/bcs/MFEMVectorTangentialDirichletBC.C
@@ -2,6 +2,15 @@
 
 registerMooseObject("PlatypusApp", MFEMVectorTangentialDirichletBC);
 
+InputParameters
+MFEMVectorTangentialDirichletBC::validParams()
+{
+  InputParameters params = MFEMVectorDirichletBCBase::validParams();
+  params.addClassDescription(
+      "Applies a Dirichlet condition to the tangential components of a vector variable.");
+  return params;
+}
+
 MFEMVectorTangentialDirichletBC::MFEMVectorTangentialDirichletBC(const InputParameters & parameters)
   : MFEMVectorDirichletBCBase(parameters)
 {
