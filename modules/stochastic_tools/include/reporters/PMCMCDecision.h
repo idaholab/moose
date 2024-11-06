843e1a2330e2bfc017fb7356dde0cda5b3112727
somu15 <Som.Dhulipala@inl.gov>
2024-11-06 14:25:45 -0700

    Rebasing and removing unwanted files

--- modules/stochastic_tools/include/reporters/PMCMCDecision.h
+++ modules/stochastic_tools/include/reporters/PMCMCDecision.h
@@ -9,15 +9,17 @@
 
 #pragma once
 
-#include "PMCMCDecisionBase.h"
+#include "GeneralReporter.h"
+#include "PMCMCBase.h"
 #include "LikelihoodFunctionBase.h"
 #include "LikelihoodInterface.h"
+#include "Distribution.h"
 
 /**
  * PMCMCDecision will help making sample accept/reject decisions in MCMC
  * schemes (for e.g., when performing Bayesian inference).
  */
-class PMCMCDecision : public PMCMCDecisionBase, public LikelihoodInterface
+class PMCMCDecision : public GeneralReporter, public LikelihoodInterface
 {
 public:
   static InputParameters validParams();
@@ -27,16 +29,38 @@ public:
   virtual void execute() override;
 
 protected:
+  /**
+   * Compute the evidence (aka, betterness of the proposed sample vs the previous)
+   * @param evidence The evidence vector to be filled
+   * @param input_matrix The matrix of proposed inputs that are provided
+   */
   virtual void computeEvidence(std::vector<Real> & evidence,
-                               const DenseMatrix<Real> & input_matrix) override;
+                               const DenseMatrix<Real> & input_matrix);
 
-  virtual void computeTransitionVector(std::vector<Real> & tv,
-                                       const std::vector<Real> & evidence) override;
+  /**
+   * Compute the transition probability vector (after the computation of evidence)
+   * @param tv The transition probability vector to be filled
+   * @param evidence The vector of evidences provided
+   */
+  virtual void computeTransitionVector(std::vector<Real> & tv, const std::vector<Real> & evidence);
 
+  /**
+   * Resample inputs given the transition vector (after transition vector computed)
+   * @param req_inputs The vector of accepted samples to be filled
+   * @param input_matrix The matrix of proposed inputs provided
+   * @param tv The vector of transition probabilities provided
+   * @param parallel_index The current parallel proposal index provided
+   */
   virtual void nextSamples(std::vector<Real> & req_inputs,
                            DenseMatrix<Real> & input_matrix,
                            const std::vector<Real> & tv,
-                           const unsigned int & parallel_index) override;
+                           const unsigned int & parallel_index);
+
+  /**
+   * Compute the next set of seeds to facilitate proposals.
+   * Set to empty in base to permit flexibility for MCMC samplers
+   */
+  virtual void nextSeeds() {}
 
   /// Model output value from SubApp
   const std::vector<Real> & _output_value;
@@ -44,9 +68,60 @@ protected:
   /// Transfer the right outputs to the file
   std::vector<Real> & _outputs_required;
 
+  /// Model input data that is uncertain
+  std::vector<std::vector<Real>> & _inputs;
+
+  /// Transition probability matrix
+  std::vector<Real> & _tpm;
+
+  /// Model variance term
+  std::vector<Real> & _variance;
+
+  /// Model noise term to pass to Likelihoods object
+  Real & _noise;
+
   /// Storage for the likelihood objects to be utilized
   std::vector<const LikelihoodFunctionBase *> _likelihoods;
 
+  /// The MCMC sampler
+  Sampler & _sampler;
+
+  /// MCMC sampler base
+  const PMCMCBase * const _pmcmc;
+
+  /// Storage for the number of parallel proposals
+  dof_id_type _props;
+
+  /// Storage for the random numbers for decision making
+  const std::vector<Real> & _rnd_vec;
+
+  /// Storage for new proposed variance samples
+  const std::vector<Real> & _new_var_samples;
+
+  /// Storage for the priors
+  const std::vector<const Distribution *> _priors;
+
+  /// Storage for the prior over the variance
+  const Distribution * _var_prior;
+
+  /// Storage for the number of experimental configuration values
+  dof_id_type _num_confg_values;
+
+  /// Storage for the number of experimental configuration parameters
+  dof_id_type _num_confg_params;
+
+  /// Storage for previous inputs
+  DenseMatrix<Real> _data_prev;
+
+  /// Storage for previous variances
+  std::vector<Real> _var_prev;
+
   /// Storage for previous outputs
   std::vector<Real> _outputs_prev;
+
+  /// Communicator that was split based on samples that have rows
+  libMesh::Parallel::Communicator & _local_comm;
+
+  /// Ensure that the MCMC algorithm proceeds in a sequential fashion
+  int _check_step;
 };
