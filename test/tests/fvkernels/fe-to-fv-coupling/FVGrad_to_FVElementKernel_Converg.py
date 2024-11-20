12fc65b7e47969648a820a34dd7ebd423a69bf4d
csdechant <csdechan@ncsu.edu>
2024-11-19 22:42:09 -0700

    Updating FE-to-FV Coupling tests with heavy convergence testing
    Addresses #15062

--- test/tests/fvkernels/fe-to-fv-coupling/FVGrad_to_FVElementKernel_Converg.py
+++ test/tests/fvkernels/fe-to-fv-coupling/FVGrad_to_FVElementKernel_Converg.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 import mms
-df1 = mms.run_spatial('2D_Coupling_FEValue_to_FVFluxKernel.i', 4, y_pp=['u_l2Error', 'v_l2Error'])
+df1 = mms.run_spatial('2D_Coupling_FVGrad_to_FVElementKernel.i', 4, y_pp=['u_l2Error', 'v_l2Error'])
 
 fig = mms.ConvergencePlot(xlabel='Element Size ($h$)', ylabel='$L_2$ Error')
 fig.plot(df1, label=['u_l2Error', 'v_l2Error'], marker='o', markersize=8, num_fitted_points=5)
-fig.save('FEValue-to-FVFluxKernel.png')
+fig.save('FVGrad-to-FVElementKernel.png')
 
-df1.to_csv('FEValue-to-FVFluxKernel.csv')
+df1.to_csv('FVGrad-to-FVElementKernel.csv')
