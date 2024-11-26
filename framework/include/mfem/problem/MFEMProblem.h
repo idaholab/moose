e9db9a54b40630123fbccf489d1e4b06c5fae9b9
Alex Lindsay <alexander.lindsay@inl.gov>
2024-11-26 13:57:07 -0800

    Some MFEM development in support of traditional MOOSE use
    - Allow specifying initial conditions for MFEM based simulations
    - Add support for properties restricted to block names

--- framework/include/mfem/problem/MFEMProblem.h
+++ framework/include/mfem/problem/MFEMProblem.h
@@ -152,6 +152,10 @@ public:
                    const std::string & name,
                    InputParameters & parameters) override;
 
+  void addInitialCondition(const std::string & ic_name,
+                           const std::string & name,
+                           InputParameters & parameters) override;
+
   /**
    * Override of ExternalProblem::addPostprocessor. In addition to
    * creating the postprocessor object, it will create a coefficient
@@ -211,6 +215,11 @@ public:
    */
   std::optional<std::reference_wrapper<mfem::ParGridFunction const>>
   getMeshDisplacementGridFunction();
+
+  /**
+   * @returns a shared pointer to an MFEM parallel grid function
+   */
+  std::shared_ptr<mfem::ParGridFunction> getGridFunction(const std::string & name);
 
 protected:
   MFEMProblemData _problem_data;
