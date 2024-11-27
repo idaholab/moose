83982982d143b3078dbd18325db3eb7814fcebb6
Patrick Behne <patrick.behne@inl.gov>
2024-11-27 12:52:27 -0600

    Added a version of addValidName that takes another MultiMooseEnum as a parameter.

--- framework/include/utils/MultiMooseEnum.h
+++ framework/include/utils/MultiMooseEnum.h
@@ -252,6 +252,7 @@ public:
   /// Extends the range of possible values the variable can be set to
   void addValidName(const std::string & name);
   void addValidName(const std::initializer_list<std::string> & names);
+  void addValidName(const MultiMooseEnum & names);
 
 protected:
   /// Check whether any of the current values are deprecated when called
