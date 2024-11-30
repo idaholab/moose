d8780f7b8d451a900fb2ea6cc8c3bbc568ea6bb6
Logan Harbour <loganhharbour@gmail.com>
2024-11-30 16:47:56 -0700

    Copy out-of-directory symlinks during install
    refs #29171

--- modules/combined/combined_installable_dirs.mk
+++ modules/combined/combined_installable_dirs.mk
@@ -1,4 +1,4 @@
 # We need to separate out declaring INSTALLABLE_DIRS for combined because
 # it needs to be used in both modules/Makefile and modules/combined/Makefile
-INSTALLABLE_MODULE_DIRS := $(shell find $(MOOSE_DIR)/modules -maxdepth 1 -mindepth 1 -type d -not -name doc -not -name module_loader -not -name heat_conduction)
-INSTALLABLE_DIRS        := $(foreach module,$(INSTALLABLE_MODULE_DIRS),../$(shell basename $(module))/test/tests->$(shell basename $(module))) ../../test/tests->framework ../../tutorials/tutorial04_meshing/app/test/tests->reactor_tutorial
+INSTALLABLE_MODULE_DIRS := $(shell find $(MOOSE_DIR)/modules -maxdepth 1 -mindepth 1 -type d -not -name doc -not -name module_loader -not -name heat_conduction -not -name tensor_mechanics)
+INSTALLABLE_DIRS        := $(foreach module,$(INSTALLABLE_MODULE_DIRS),../$(shell basename $(module))/test/tests->$(shell basename $(module))) ../../tutorials/tutorial04_meshing/app/test/tests->reactor_tutorial
