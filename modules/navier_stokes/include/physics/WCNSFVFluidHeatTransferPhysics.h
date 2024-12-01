107df1c51d70df624a6f94e1995c5a2a14fefc9e
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2024-11-30 18:21:04 -0700

    Add a base class for fluid heat transfer physics refs #29175

--- modules/navier_stokes/include/physics/WCNSFVFluidHeatTransferPhysics.h
+++ modules/navier_stokes/include/physics/WCNSFVFluidHeatTransferPhysics.h
@@ -9,102 +9,37 @@
 
 #pragma once
 
-#include "NavierStokesPhysicsBase.h"
-#include "WCNSFVCoupledAdvectionPhysicsHelper.h"
+#include "WCNSFVFluidHeatTransferPhysicsBase.h"
 #include "NS.h"
 
 /**
  * Creates all the objects needed to solve the Navier Stokes energy equation
  */
-class WCNSFVFluidHeatTransferPhysics final : public NavierStokesPhysicsBase,
-                                             public WCNSFVCoupledAdvectionPhysicsHelper
+class WCNSFVFluidHeatTransferPhysics final : public WCNSFVFluidHeatTransferPhysicsBase
 {
 public:
   static InputParameters validParams();
 
   WCNSFVFluidHeatTransferPhysics(const InputParameters & parameters);
 
-  /// Get the name of the fluid temperature variable
-  const NonlinearVariableName & getFluidTemperatureName() const { return _fluid_temperature_name; }
-
-  /// Get the name of the specific heat material property
-  const MooseFunctorName & getSpecificHeatName() const { return _specific_heat_name; }
-  MooseFunctorName getSpecificEnthalpyName() const { return NS::specific_enthalpy; }
-  const std::vector<MooseFunctorName> & getThermalConductivityName() const
-  {
-    return _thermal_conductivity_name;
-  }
-
-  /// Get the ambient convection parameters for parameter checking
-  const std::vector<std::vector<SubdomainName>> & getAmbientConvectionBlocks() const
-  {
-    return _ambient_convection_blocks;
-  }
-  /// Name of the ambient convection heat transfer coefficients for each block-group
-  const std::vector<MooseFunctorName> & getAmbientConvectionHTCs() const
-  {
-    return _ambient_convection_alpha;
-  }
-
-  /// Whether the physics is actually creating the heat equation
-  bool hasEnergyEquation() const { return _has_energy_equation; }
-
 protected:
 private:
-  void actOnAdditionalTasks() override;
-  void addSolverVariables() override;
-  void addInitialConditions() override;
-  void addFVKernels() override;
-  void addFVBCs() override;
-  void addMaterials() override;
-
-  unsigned short getNumberAlgebraicGhostingLayersNeeded() const override;
+  virtual void addSolverVariables() override;
 
   /**
    * Functions adding kernels for the incompressible / weakly compressible energy equation
    * If the material properties are not constant, some of these can be used for
    * weakly-compressible simulations as well.
    */
-  void addINSEnergyTimeKernels();
-  void addWCNSEnergyTimeKernels();
-  void addINSEnergyHeatConductionKernels();
-  void addINSEnergyAdvectionKernels();
-  void addINSEnergyAmbientConvection();
-  void addINSEnergyExternalHeatSource();
+  void addINSEnergyTimeKernels() override;
+  void addWCNSEnergyTimeKernels() override;
+  void addINSEnergyHeatConductionKernels() override;
+  void addINSEnergyAdvectionKernels() override;
+  void addINSEnergyAmbientConvection() override;
+  void addINSEnergyExternalHeatSource() override;
 
   /// Functions adding boundary conditions for the incompressible simulation.
   /// These are used for weakly-compressible simulations as well.
-  void addINSEnergyInletBC();
-  void addINSEnergyWallBC();
-
-  /// Process thermal conductivity (multiple functor input options are available).
-  /// Return true if we have vector thermal conductivity and false if scalar
-  bool processThermalConductivity();
-
-  /// A boolean to help compatibility with the old Modules/NavierStokesFV syntax
-  const bool _has_energy_equation;
-  /// Fluid temperature name
-  NonlinearVariableName _fluid_temperature_name;
-  /// Name of the specific heat material property
-  MooseFunctorName _specific_heat_name;
-  /// Vector of subdomain groups where we want to have different thermal conduction
-  std::vector<std::vector<SubdomainName>> _thermal_conductivity_blocks;
-  /// Name of the thermal conductivity functor for each block-group
-  std::vector<MooseFunctorName> _thermal_conductivity_name;
-
-  /// Vector of subdomain groups where we want to have different ambient convection
-  std::vector<std::vector<SubdomainName>> _ambient_convection_blocks;
-  /// Name of the ambient convection heat transfer coefficients for each block-group
-  std::vector<MooseFunctorName> _ambient_convection_alpha;
-  /// Name of the solid domain temperature for each block-group
-  std::vector<MooseFunctorName> _ambient_temperature;
-
-  /// Energy inlet boundary types
-  MultiMooseEnum _energy_inlet_types;
-  /// Functors describing the inlet boundary values. See energy_inlet_types for what the functors actually represent
-  std::vector<MooseFunctorName> _energy_inlet_functors;
-  /// Energy wall boundary types
-  MultiMooseEnum _energy_wall_types;
-  /// Functors describing the wall boundary values. See energy_wall_types for what the functors actually represent
-  std::vector<MooseFunctorName> _energy_wall_functors;
+  void addINSEnergyInletBC() override;
+  void addINSEnergyWallBC() override;
 };
