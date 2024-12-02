9b1628168d7804186b51543d1b11ff8459cf6d3c
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2024-12-02 08:36:00 -0700

    Add multi-system and linear system support to DumpObjectProblem

---
+++ framework/src/systems/DumpObjectsLinearSystem.C
@@ -0,0 +1,16 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "DumpObjectsLinearSystem.h"
+#include "FEProblemBase.h"
+
+DumpObjectsLinearSystem::DumpObjectsLinearSystem(FEProblemBase & problem, const std::string & name)
+  : LinearSystem(problem, name), _dummy(nullptr)
+{
+}
