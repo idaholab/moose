12243b3d7834030ba6fb83de47ae9c72989a4d46
karthikeyan chockalingam <karthikeyan.chockalingam@stfc.ac.uk>
2024-12-02 18:26:16 +0000

    add non-linear source term

--- unit/src/TestNLDiffusionIntegrator.C
+++ unit/src/TestNLDiffusionIntegrator.C
@@ -3,6 +3,74 @@
 
 using namespace mfem;
 
+static const double alpha = -0.5;
+
+double f(double u) { return exp(alpha*u); }
+double df(double u) { return alpha*exp(alpha*u); }
+
+// Define a coefficient that, given a grid function u, returns f(u)
+class NonlinearCoefficient : public Coefficient
+{
+   GridFunction &gf;
+public:
+   NonlinearCoefficient(GridFunction &gf_) : gf(gf_) { }
+   double Eval(ElementTransformation &T, const IntegrationPoint &ip)
+   {
+      return f(gf.GetValue(T, ip));
+   }
+};
+
+// Define a coefficient that, given a grid function u, returns df(u)
+class NonlinearDerivativeCoefficient : public Coefficient
+{
+   GridFunction &gf;
+public:
+   NonlinearDerivativeCoefficient(GridFunction &gf_) : gf(gf_) { }
+   double Eval(ElementTransformation &T, const IntegrationPoint &ip)
+   {
+      return df(gf.GetValue(T, ip));
+   }
+};
+
+// Define a nonlinear integrator that computes (f(u), v) and its linearized
+// operator, (u df(u), v).
+//
+// Note that the action (f(u), v) can be computed using DomainLFIntegrator
+// and the Jacobian matrix linearized operator can be computed using
+// MassIntegrator with the appropriate coefficients.
+class NonlinearMassIntegrator : public NonlinearFormIntegrator
+{
+   FiniteElementSpace &fes;
+   GridFunction gf;
+   Array<int> dofs;
+
+public:
+   NonlinearMassIntegrator(FiniteElementSpace &fes_) : fes(fes_), gf(&fes) { }
+
+   virtual void AssembleElementVector(const FiniteElement &el,
+                                      ElementTransformation &Tr,
+                                      const Vector &elfun, Vector &elvect)
+   {
+      fes.GetElementDofs(Tr.ElementNo, dofs);
+      gf.SetSubVector(dofs, elfun);
+      NonlinearCoefficient coeff(gf);
+      DomainLFIntegrator integ(coeff);
+      integ.AssembleRHSElementVect(el, Tr, elvect);
+   }
+
+   virtual void AssembleElementGrad(const FiniteElement &el,
+                                    ElementTransformation &Tr,
+                                    const Vector &elfun, DenseMatrix &elmat)
+   {
+      fes.GetElementDofs(Tr.ElementNo, dofs);
+      gf.SetSubVector(dofs, elfun);
+      NonlinearDerivativeCoefficient coeff(gf);
+      MassIntegrator integ(coeff);
+      integ.AssembleElementMatrix(el, Tr, elmat);
+   }
+};
+
+
 
 TEST(CheckData, NLDiffusionTest)
 {
