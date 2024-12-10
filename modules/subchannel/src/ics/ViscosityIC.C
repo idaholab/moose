cac0e3f1cb246373b93bbf88a2c0c4f7f14ce1f7
Logan Harbour <loganhharbour@gmail.com>
2024-12-10 08:30:44 -0700

    Replace subchannel copyright with moose copyright
    refs #28497

--- modules/subchannel/src/ics/ViscosityIC.C
+++ modules/subchannel/src/ics/ViscosityIC.C
@@ -1,16 +1,11 @@
-/********************************************************************/
-/*                   DO NOT MODIFY THIS HEADER                      */
-/*          Subchannel: Thermal Hydraulics Reactor Analysis         */
-/*                                                                  */
-/*              (c) 2022 Battelle Energy Alliance, LLC              */
-/*                      ALL RIGHTS RESERVED                         */
-/*                                                                  */
-/*             Prepared by Battelle Energy Alliance, LLC            */
-/*               Under Contract No. DE-AC07-05ID14517               */
-/*               With the U. S. Department of Energy                */
-/*                                                                  */
-/*               See COPYRIGHT for full restrictions                */
-/********************************************************************/
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
 
 #include "ViscosityIC.h"
 #include "SinglePhaseFluidProperties.h"
