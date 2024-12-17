e40d0ba2753828311292074a66975cf2b86430d3
Yaqi Wang <yaqi.wang@inl.gov>
2024-12-16 21:14:44 -0600

    make this test executioner honor fixed point solve

--- test/include/executioners/SteadySolve2.h
+++ test/include/executioners/SteadySolve2.h
@@ -9,16 +9,15 @@
 
 #pragma once
 
-#include "Executioner.h"
+#include "SteadyBase.h"
 
 // Forward declarations
 class InputParameters;
-class FEProblemBase;
 
 /**
  * SteadySolve2 executioners call "solve()" on two different nonlinear systems in sequence
  */
-class SteadySolve2 : public Executioner
+class SteadySolve2 : public SteadyBase
 {
 public:
   static InputParameters validParams();
@@ -26,21 +25,59 @@ public:
   SteadySolve2(const InputParameters & parameters);
 
   void init() override;
-  void execute() override;
-  bool lastSolveConverged() const override { return _last_solve_converged; }
 
 protected:
-  FEProblemBase & _problem;
+  class IterativeFEProblemSolve : public FEProblemSolve
+  {
+  public:
+    IterativeFEProblemSolve(Executioner & ex,
+                            const std::vector<unsigned int> & system_numbers,
+                            const unsigned int niter)
+      : FEProblemSolve(ex), _system_numbers(system_numbers), _number_of_iterations(niter)
+    {
+    }
+    virtual bool solve() override
+    {
+      bool converged = true;
+      for (unsigned int i = 0; i < _number_of_iterations; i++)
+      {
+        for (const auto & sys_num : _system_numbers)
+        {
+          _problem.solve(sys_num);
 
-  FEProblemSolve _feproblem_solve;
+          if (_problem.shouldSolve())
+          {
+            if (_problem.converged(sys_num))
+              _console << COLOR_GREEN << " Nonlinear system " << sys_num << " solve converged!"
+                       << COLOR_DEFAULT << std::endl;
+            else
+            {
+              _console << COLOR_RED << " Nonlinear system " << sys_num << " solve did not converge!"
+                       << COLOR_DEFAULT << std::endl;
+              converged = false;
+            }
+          }
+          else
+            _console << COLOR_GREEN << " Nonlinear system " << sys_num << " solve skipped!"
+                     << COLOR_DEFAULT << std::endl;
+        }
+        if (!converged)
+        {
+          _console << "Aborting as solve did not converge" << std::endl;
+          break;
+        }
+      }
+      return converged;
+    }
 
-  Real _system_time;
-  int & _time_step;
-  Real & _time;
+  protected:
+    const std::vector<unsigned int> _system_numbers;
+    const unsigned int _number_of_iterations;
+  };
 
-private:
   const unsigned int _first_nl_sys;
   const unsigned int _second_nl_sys;
-  bool _last_solve_converged;
-  unsigned int _number_of_iterations;
+  const unsigned int _number_of_iterations;
+
+  IterativeFEProblemSolve _feproblem_solve;
 };
--- test/src/executioners/SteadySolve2.C
+++ test/src/executioners/SteadySolve2.C
@@ -21,7 +21,7 @@ registerMooseObject("MooseTestApp", SteadySolve2);
 InputParameters
 SteadySolve2::validParams()
 {
-  InputParameters params = Executioner::validParams();
+  InputParameters params = SteadyBase::validParams();
   params += FEProblemSolve::validParams();
   params.addRequiredParam<NonlinearSystemName>("first_nl_sys_to_solve",
                                                "The first nonlinear system to solve");
@@ -35,18 +35,13 @@ SteadySolve2::validParams()
 }
 
 SteadySolve2::SteadySolve2(const InputParameters & parameters)
-  : Executioner(parameters),
-    _problem(_fe_problem),
-    _feproblem_solve(*this),
-    _time_step(_problem.timeStep()),
-    _time(_problem.time()),
+  : SteadyBase(parameters),
     _first_nl_sys(_problem.nlSysNum(getParam<NonlinearSystemName>("first_nl_sys_to_solve"))),
     _second_nl_sys(_problem.nlSysNum(getParam<NonlinearSystemName>("second_nl_sys_to_solve"))),
-    _number_of_iterations(getParam<unsigned int>("number_of_iterations"))
+    _number_of_iterations(getParam<unsigned int>("number_of_iterations")),
+    _feproblem_solve(*this, {_first_nl_sys, _second_nl_sys}, _number_of_iterations)
 {
   _fixed_point_solve->setInnerSolve(_feproblem_solve);
-
-  _time = 0;
 }
 
 void
@@ -59,96 +54,4 @@ SteadySolve2::init()
   }
 
   _problem.initialSetup();
-}
-
-void
-SteadySolve2::execute()
-{
-  if (_app.isRecovering())
-    return;
-
-  _time_step = 0;
-  _problem.outputStep(EXEC_INITIAL);
-
-  preExecute();
-
-  // first step in any steady state solve is always 1 (preserving backwards compatibility)
-  _time_step = 1;
-
-#ifdef LIBMESH_ENABLE_AMR
-
-  // Define the refinement loop
-  unsigned int steps = _problem.adaptivity().getSteps();
-  for (unsigned int r_step = 0; r_step <= steps; r_step++)
-  {
-#endif // LIBMESH_ENABLE_AMR
-    _problem.timestepSetup();
-
-    preSolve();
-    _problem.execute(EXEC_TIMESTEP_BEGIN);
-    _problem.outputStep(EXEC_TIMESTEP_BEGIN);
-    _problem.updateActiveObjects();
-
-    auto solve = [this](const auto & sys_num)
-    {
-      _problem.solve(sys_num);
-
-      if (_problem.shouldSolve())
-      {
-        if (_problem.converged(sys_num))
-          _console << COLOR_GREEN << " Nonlinear system " << sys_num << " solve converged!"
-                   << COLOR_DEFAULT << std::endl;
-        else
-        {
-          _console << COLOR_RED << " Nonlinear system " << sys_num << " solve did not converge!"
-                   << COLOR_DEFAULT << std::endl;
-          return false;
-        }
-      }
-      else
-        _console << COLOR_GREEN << " Nonlinear system " << sys_num << " solve skipped!"
-                 << COLOR_DEFAULT << std::endl;
-
-      return _problem.converged(sys_num);
-    };
-
-    bool converged = true;
-
-    for (unsigned int i = 0; i < _number_of_iterations; i++)
-    {
-      converged = converged && solve(_first_nl_sys) && solve(_second_nl_sys);
-      if (!converged)
-      {
-        _console << "Aborting as solve did not converge" << std::endl;
-        break;
-      }
-    }
-    _problem.onTimestepEnd();
-    _problem.execute(EXEC_TIMESTEP_END);
-
-    _problem.computeIndicators();
-    _problem.computeMarkers();
-
-    // need to keep _time in sync with _time_step to get correct output
-    _time = _time_step;
-    _problem.outputStep(EXEC_TIMESTEP_END);
-    _time = _system_time;
-
-#ifdef LIBMESH_ENABLE_AMR
-    if (r_step != steps)
-      _problem.adaptMesh();
-
-    _time_step++;
-  }
-#endif
-
-  {
-    TIME_SECTION("final", 1, "Executing Final Objects")
-    _problem.postExecute();
-    _problem.execute(EXEC_FINAL);
-    _time = _time_step;
-    _problem.outputStep(EXEC_FINAL);
-  }
-
-  postExecute();
 }
