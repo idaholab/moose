66a2cd9eac5ee007ed976beedf5c4986ea5bb971
Chris MacMackin <chris.macmackin@ukaea.uk>
2024-12-17 10:47:55 +0000

    Update doc/content/source/bcs/MFEMScalarFunctionDirichletBC.C
    Co-authored-by: Alexander Blair <35571481+alexanderianblair@users.noreply.github.com>

--- framework/doc/content/source/mfem/bcs/MFEMScalarFunctionDirichletBC.C
+++ framework/doc/content/source/mfem/bcs/MFEMScalarFunctionDirichletBC.C
@@ -8,17 +8,12 @@
 
 Boundary condition for enforcing an essential (Dirichlet)
 condition on a scalar variable on the boundary,
-    fixing its values to the input scalar function on the boundary.
+fixing its values to the input scalar function on the boundary.
 
-        ##Example Input File Syntax
+##Example Input File Syntax
 
-        !syntax parameters /
-        BCs /
-        MFEMScalarFunctionDirichletBC
+!syntax parameters /BCs/MFEMScalarFunctionDirichletBC
 
-        !syntax inputs /
-        BCs /
-        MFEMScalarFunctionDirichletBC
+!syntax inputs /BCs/MFEMScalarFunctionDirichletBC
 
-        !syntax children /
-        BCs / MFEMScalarFunctionDirichletBC
+!syntax children /BCs/MFEMScalarFunctionDirichletBC
