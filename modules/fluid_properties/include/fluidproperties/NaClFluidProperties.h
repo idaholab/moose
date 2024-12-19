fb7cf2714b5da260998d78018741658721ed8a48
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2024-12-19 21:18:12 +0100

    Add cp derivatives to NaCl, was done for testing purposes

--- modules/fluid_properties/include/fluidproperties/NaClFluidProperties.h
+++ modules/fluid_properties/include/fluidproperties/NaClFluidProperties.h
@@ -69,7 +69,8 @@ public:
 
   virtual Real cp_from_p_T(Real pressure, Real temperature) const override;
 
-  using SinglePhaseFluidProperties::cp_from_p_T;
+  virtual void cp_from_p_T(
+      Real pressure, Real temperature, Real & cp, Real & dcp_dp, Real & dcp_dT) const override;
 
   virtual Real cv_from_p_T(Real pressure, Real temperature) const override;
 
