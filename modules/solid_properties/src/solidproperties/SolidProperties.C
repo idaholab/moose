8d98d6d8ed045070a90ef5f7fc5361142e6dc994
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2024-12-23 16:48:23 +0100

    Remove unused parameters from all fluid properties Improve parameter groups for SIMPLE refs #29609

--- modules/solid_properties/src/solidproperties/SolidProperties.C
+++ modules/solid_properties/src/solidproperties/SolidProperties.C
@@ -8,6 +8,7 @@
 //* https://www.gnu.org/licenses/lgpl-2.1.html
 
 #include "SolidProperties.h"
+#include "MooseObject.h"
 
 InputParameters
 SolidProperties::validParams()
@@ -18,6 +19,16 @@ SolidProperties::validParams()
       false,
       "true to allow unimplemented property derivative terms to be set to zero for the AD API");
   params.registerBase("SolidProperties");
+
+  // Suppress unused parameters
+  params.suppressParameter<bool>("use_displaced_mesh");
+  params.suppressParameter<ExecFlagEnum>("execute_on");
+  params.suppressParameter<bool>("allow_duplicate_execution_on_initial");
+  params.suppressParameter<bool>("force_preic");
+  params.suppressParameter<bool>("force_preaux");
+  params.suppressParameter<bool>("force_postaux");
+  params.suppressParameter<int>("execution_order_group");
+
   return params;
 }
 
