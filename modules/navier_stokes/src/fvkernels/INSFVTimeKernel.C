dd46071ae9b8e538f2c0dc5ddb287bbf6b8197d0
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2024-12-25 21:34:20 +0100

    Add an option to freeze the DT for RC coefficients to allow for perfect restarts refs #28034

--- modules/navier_stokes/src/fvkernels/INSFVTimeKernel.C
+++ modules/navier_stokes/src/fvkernels/INSFVTimeKernel.C
@@ -18,19 +18,35 @@ INSFVTimeKernel::validParams()
 {
   auto params = FVFunctorTimeKernel::validParams();
   params += INSFVMomentumResidualObject::validParams();
+
+  // These options can be set to facilitate restarts with RC coefficients that do not depend on
+  // the time steps. By default, they are not active
   params.addParam<bool>(
       "contribute_to_rc",
       true,
       "Whether the time derivative term should contribute to Rhie-Chow coefficients");
-  params.addParamNamesToGroup("contribute_to_rc", "Advanced");
+  params.addParam<Real>("start_fixed_dt_contribution_to_RC",
+                        0,
+                        "Time at which to start a fixed dt contribution to the RC coefficients");
+  params.addParam<Real>("fixed_dt_contribution_to_RC",
+                        "Fixed dt for the contribution to the Rhie Chow coefficients");
+  params.declareControllable("fixed_dt_contribution_to_RC");
+  params.addParamNamesToGroup("contribute_to_rc start_fixed_dt_contribution_to_RC", "Advanced");
   return params;
 }
 
 INSFVTimeKernel::INSFVTimeKernel(const InputParameters & params)
   : FVFunctorTimeKernel(params),
     INSFVMomentumResidualObject(*this),
-    _contribute_to_rc_coeffs(getParam<bool>("contribute_to_rc"))
+    _contribute_to_rc_coeffs(getParam<bool>("contribute_to_rc")),
+    _use_fixed_dt_rc_contrib(isParamValid("fixed_dt_contribution_to_RC")),
+    _fixed_dt_rc_contrib_start(getParam<Real>("start_fixed_dt_contribution_to_RC")),
+    _fixed_dt_rc_contrib(_use_fixed_dt_rc_contrib ? getParam<Real>("fixed_dt_contribution_to_RC")
+                                                  : _zero)
 {
+  if (!_use_fixed_dt_rc_contrib && isParamSetByUser("start_fixed_dt_contribution_to_RC"))
+    paramError("start_fixed_dt_contribution_to_RC",
+               "Should not be set unless 'start_fixed_dt_contribution_to_RC' is set");
 }
 
 void
