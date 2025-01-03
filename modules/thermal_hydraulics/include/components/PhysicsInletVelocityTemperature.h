668e4b65fbc6977c53be81a19b3352057968e077
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2025-01-03 19:56:15 +0100

    Add inlet for v-T and stagnation p-T to help: -coupling to SAM -modeling pressurizer

---
+++ modules/thermal_hydraulics/include/components/PhysicsInletVelocityTemperature.h
@@ -0,0 +1,33 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "PhysicsFlowBoundary.h"
+
+/**
+ * Boundary condition with prescribed velocity and temperature for flow channels
+ * leveraging the Physics system
+ */
+class PhysicsInletVelocityTemperature : public PhysicsFlowBoundary
+{
+public:
+  static InputParameters validParams();
+
+  PhysicsInletVelocityTemperature(const InputParameters & params);
+
+  virtual bool isReversible() const override { return _reversible; }
+
+protected:
+  virtual void check() const override;
+  virtual void init() override;
+
+  /// True to allow the flow to reverse, otherwise false
+  bool _reversible;
+};
