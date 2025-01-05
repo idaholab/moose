2lambda123/idaholab-moose

Switch to virtual inheritance for physics to allow a physics to derive both from THM & NS physics

modules/navier_stokes/src/physics/WCNSFVFlowPhysics.C
@@ -138,7 +138,8 @@ WCNSFVFlowPhysics::validParams()
 }
 
 WCNSFVFlowPhysics::WCNSFVFlowPhysics(const InputParameters & parameters)
-  : NavierStokesPhysicsBase(parameters),
+  : PhysicsBase(parameters),
+    NavierStokesPhysicsBase(parameters),
     _has_flow_equations(getParam<bool>("add_flow_equations")),
     _compressibility(getParam<MooseEnum>("compressibility")),
     _porous_medium_treatment(getParam<bool>("porous_medium_treatment")),

idaholab/moose

modules/navier_stokes/src/physics/WCNSFVFlowPhysics.C
@@ -87,10 +87,8 @@ WCNSFVFlowPhysics::validParams()
 }
 
 WCNSFVFlowPhysics::WCNSFVFlowPhysics(const InputParameters & parameters)
-  : WCNSFVFlowPhysicsBase(parameters),
-    _porosity_smoothing_layers(isParamValid("porosity_smoothing_layers")
-                                   ? getParam<unsigned short>("porosity_smoothing_layers")
-                                   : 0),
+  : PhysicsBase(parameters),
+    WCNSFVFlowPhysicsBase(parameters),
     _friction_blocks(getParam<std::vector<std::vector<SubdomainName>>>("friction_blocks")),
     _friction_types(getParam<std::vector<std::vector<std::string>>>("friction_types")),
     _friction_coeffs(getParam<std::vector<std::vector<std::string>>>("friction_coeffs"))