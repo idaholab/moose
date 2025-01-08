c6dad5e8dfea195262029af8c58f70951cfc6775
Kalin Kiesling <kkiesling@anl.gov>
2025-01-08 15:38:14 -0600

    axis aligned cylinder types

---
+++ framework/include/csg/CSGYCylinder.h
@@ -0,0 +1,52 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "CSGSurface.h"
+#include "CSGHalfspace.h"
+
+#include "libmesh/point.h"
+
+namespace CSG
+{
+
+/**
+ * CSGYCylinder creates an internal representation of a Constructive Solid Geometry (CSG)
+ * y-axis aligned cylinder, represented in the following form (x - x0)^2 + (z - z0)^2 = r^2
+ */
+class CSGYCylinder : public CSGSurface
+{
+public:
+  CSGYCylinder(const std::string name, const Real r);
+
+  CSGYCylinder(const std::string name, const Real x0, const Real z0, const Real r);
+
+  CSGYCylinder(const std::string name, const Point center, const Real r);
+
+  /**
+   * Destructor
+   */
+  virtual ~CSGYCylinder() = default;
+
+  virtual std::map<std::string, Real> getCoeffs() override;
+
+  virtual CSGSurface::Direction directionFromPoint(const Point p) override;
+
+protected:
+  /// Value of x0 in equation of an y-axis aligned cylinder
+  Real _x0;
+
+  /// Value of z0 in equation of an y-axis aligned cylinder
+  Real _z0;
+
+  /// Value of r in equation of an y-axis aligned cylinder
+  Real _r;
+};
+} // namespace CSG
---
+++ framework/src/csg/CSGXCylinder.C
@@ -0,0 +1,49 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "CSGXCylinder.h"
+
+namespace CSG
+{
+
+CSGXCylinder::CSGXCylinder(const std::string name, const Real r)
+  : CSGSurface(name, SurfaceType::xcylinder)
+{
+  _y0 = 0.0;
+  _z0 = 0.0;
+  _r = r;
+}
+
+CSGXCylinder::CSGXCylinder(const std::string name, const Real y0, const Real z0, const Real r)
+  : CSGSurface(name, SurfaceType::sphere), _y0(y0), _z0(z0), _r(r)
+{
+}
+
+CSGXCylinder::CSGXCylinder(const std::string name, const Point center, const Real r)
+  : CSGSurface(name, SurfaceType::sphere), _y0(center(1)), _z0(center(2)), _r(r)
+{
+}
+
+std::map<std::string, Real>
+CSGXCylinder::getCoeffs()
+{
+  std::map<std::string, Real> coeffs = {{"y0", _y0}, {"z0", _z0}, {"r", _r}};
+  return coeffs;
+}
+
+CSGSurface::Direction
+CSGXCylinder::directionFromPoint(const Point p)
+{
+  // Compute distance from the cylinder center to determine if inside (< r^2)
+  // or outside (> r^2) the cylinder
+  const Real dist_sq = pow((p(1) - _y0), 2) + pow((p(2) - _z0), 2);
+
+  return (dist_sq > pow(_r, 2)) ? CSGSurface::Direction::positive : CSGSurface::Direction::negative;
+}
+} // namespace CSG
