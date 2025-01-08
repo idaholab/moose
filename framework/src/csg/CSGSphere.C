383bf551737495b15c2db22788628609f6529ce8
Kalin Kiesling <kkiesling@anl.gov>
2025-01-08 16:51:00 -0600

    very messed up commit history now because apparently this one was not being tracked

---
+++ framework/src/csg/CSGSphere.C
@@ -0,0 +1,40 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "CSGSphere.h"
+
+namespace CSG
+{
+
+CSGSphere::CSGSphere(const std::string name, const Real x0, const Real y0, const Real z0, const Real r)
+  : CSGSurface(name, SurfaceType::sphere), _x0(x0), _y0(y0), _z0(z0), _r(r)
+{
+    if (r < 0.0 || r == 0.0)
+    {
+        mooseError("Radius of sphere must be postive.");
+    }
+}
+
+std::map<std::string, Real>
+CSGSphere::getCoeffs()
+{
+  std::map<std::string, Real> coeffs = {{"x0", _x0}, {"y0", _y0}, {"z0", _z0}, {"r", _r}};
+  return coeffs;
+}
+
+CSGSurface::Direction
+CSGSphere::directionFromPoint(const Point p)
+{
+  // Compute distance from the sphere center to determine if inside (< r^2)
+  // or outside (> r^2) the sphere
+  const Real dist_sq = pow((p(0) - _x0), 2) + pow((p(1) - _y0), 2) + pow((p(2) - _z0), 2);
+
+  return (dist_sq > pow(_r, 2)) ? CSGSurface::Direction::positive : CSGSurface::Direction::negative;
+}
+} // namespace CSG
