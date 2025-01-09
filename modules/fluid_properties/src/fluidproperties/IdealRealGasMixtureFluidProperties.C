fbee1c42c602af03e78a6472be9c70e1df978ad9
Joshua E. Hansel <joshua.hansel@inl.gov>
2025-01-09 14:24:45 -0600

    Added warnings to IdealRealGasMixtureFluidProperties

--- modules/fluid_properties/src/fluidproperties/IdealRealGasMixtureFluidProperties.C
+++ modules/fluid_properties/src/fluidproperties/IdealRealGasMixtureFluidProperties.C
@@ -198,6 +198,9 @@ registerMooseObject("FluidPropertiesApp", IdealRealGasMixtureFluidProperties);
     const Real yp = _fp_primary->prop##_from_v_e(vp, ep);                                          \
     y = x_primary * M_star / M_primary * yp;                                                       \
                                                                                                    \
+    mooseDoOnce(mooseWarning("The temperature and specific volume derivatives in the following "   \
+                             "function are currently neglected:\n\n",                              \
+                             __PRETTY_FUNCTION__));                                                \
     dy_dT = 0;                                                                                     \
     dy_dv = 0;                                                                                     \
                                                                                                    \
@@ -684,6 +687,9 @@ IdealRealGasMixtureFluidProperties::cp_from_T_v(Real T,
 
   // Neglect these for now. These require higher-order derivatives, so a finite
   // difference should probably be used, as for sound speed.
+  mooseDoOnce(mooseWarning("The temperature and specific volume derivatives in the following "
+                           "function are currently neglected:\n\n",
+                           __PRETTY_FUNCTION__));
   dcp_dT = 0;
   dcp_dv = 0;
 
@@ -724,6 +730,9 @@ IdealRealGasMixtureFluidProperties::cv_from_T_v(Real T,
 
   // Neglect these for now. These require higher-order derivatives, so a finite
   // difference should probably be used, as for sound speed.
+  mooseDoOnce(mooseWarning("The temperature and specific volume derivatives in the following "
+                           "function are currently neglected:\n\n",
+                           __PRETTY_FUNCTION__));
   dcv_dT = 0;
   dcv_dv = 0;
 
