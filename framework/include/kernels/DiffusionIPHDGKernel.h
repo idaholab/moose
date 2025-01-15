e54f26eda81717a498158917a19191ad40fe84f4
Alex Lindsay <alexander.lindsay@inl.gov>
2025-01-15 11:51:25 -0800

    Revamp IPHDG design to allow for modular term-by-term construction
    Also adapt IPHDGKernel to HDGKernel

---
+++ framework/include/kernels/DiffusionIPHDGKernel.h
@@ -0,0 +1,28 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "IPHDGKernel.h"
+
+/**
+ * Implements the diffusion equation for a interior penalty hybridized discretization
+ */
+class DiffusionIPHDGKernel : public IPHDGKernel
+{
+public:
+  static InputParameters validParams();
+  DiffusionIPHDGKernel(const InputParameters & params);
+
+protected:
+  virtual IPHDGAssemblyHelper & iphdgHelper() override { return *_iphdg_helper; }
+
+  /// The assembly helper providing the required IP-HDG method implementations
+  std::unique_ptr<DiffusionIPHDGAssemblyHelper> _iphdg_helper;
+};
