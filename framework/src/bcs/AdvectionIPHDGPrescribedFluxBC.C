cf5e936d3e57ba19ac87ec1d7541588ff7225ac6
Alex Lindsay <alexander.lindsay@inl.gov>
2025-01-15 11:51:25 -0800

    Revamp IPHDG design to allow for modular term-by-term construction
    Also adapt IPHDGKernel to HDGKernel

---
+++ framework/src/bcs/AdvectionIPHDGPrescribedFluxBC.C
@@ -0,0 +1,30 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "AdvectionIPHDGAssemblyHelper.h"
+#include "AdvectionIPHDGPrescribedFluxBC.h"
+
+registerMooseObject("MooseApp", AdvectionIPHDGPrescribedFluxBC);
+
+InputParameters
+AdvectionIPHDGPrescribedFluxBC::validParams()
+{
+  auto params = IPHDGPrescribedFluxBC::validParams();
+  params += AdvectionIPHDGAssemblyHelper::validParams();
+  params.addClassDescription("Implements an outflow boundary condition for use with a hybridized "
+                             "discretization of the Advection equation");
+  return params;
+}
+
+AdvectionIPHDGPrescribedFluxBC::AdvectionIPHDGPrescribedFluxBC(const InputParameters & parameters)
+  : IPHDGPrescribedFluxBC(parameters),
+    _iphdg_helper(std::make_unique<AdvectionIPHDGAssemblyHelper>(
+        this, this, this, _sys, _assembly, _tid, std::set<SubdomainID>{}, boundaryIDs()))
+{
+}
