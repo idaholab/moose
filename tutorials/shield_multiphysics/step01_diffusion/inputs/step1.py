716797c21613e6b49467d0eec6deee1185b4392e
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2025-01-14 19:08:02 -0700

    Remove legacy mode

--- tutorials/shield_multiphysics/step01_diffusion/inputs/step1.py
+++ tutorials/shield_multiphysics/step01_diffusion/inputs/step1.py
@@ -13,11 +13,11 @@ import chigger
 
 camera = vtk.vtkCamera()
 camera.SetViewUp(0.0000000000, 1.0000000000, 0.0000000000)
-camera.SetPosition(0.1520000000, 0.0128500000, 0.3251647185)
+camera.SetPosition(15.20000000, 12.8500000, 10.3251647185)
 camera.SetFocalPoint(0.1520000000, 0.0128500000, 0.0000000000)
 
 reader = chigger.exodus.ExodusReader('step1_out.e')
-pressure = chigger.exodus.ExodusResult(reader, camera=camera, variable='pressure', cmap='viridis')
+pressure = chigger.exodus.ExodusResult(reader, camera=camera, variable='T', cmap='viridis')
 cbar = chigger.exodus.ExodusColorBar(pressure,
                                      length=0.6,
                                      colorbar_origin=[0.2, 0.7],
