ca6b433f09245ec33cdc1fecc0fd89a1e129de5a
Chris MacMackin <chris.macmackin@ukaea.uk>
2025-02-03 17:58:15 +0000

    Add class needed to support adding mixed bilinear forms

---
+++ framework/src/mfem/kernels/MFEMMixedBilinearFormKernel.C
@@ -0,0 +1,30 @@
+#include "MFEMMixedBilinearFormKernel.h"
+
+InputParameters
+MFEMMixedBilinearFormKernel::validParams()
+{
+  InputParameters params = MFEMKernel::validParams();
+  params.addClassDescription(
+      "Base class for mixed bilinear form kernels, allowing different trial and test variables.");
+  params.addParam<std::string>(
+      "trial_variable",
+      "",
+      "The trial variable this kernel is acting on and which will be solved for. If empty "
+      "(default), it will be the same as the test variable.");
+  return params;
+}
+
+MFEMMixedBilinearFormKernel::MFEMMixedBilinearFormKernel(const InputParameters & parameters)
+  : MFEMKernel(parameters), _trial_var_name(getParam<std::string>("trial_variable"))
+{
+  if (_trial_var_name == "")
+  {
+    _trial_var_name = _test_var_name;
+  }
+}
+
+const std::string &
+MFEMMixedBilinearFormKernel::getTrialVariableName() const
+{
+  return _trial_var_name;
+}
