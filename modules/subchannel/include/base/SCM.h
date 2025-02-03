6b2a94bc07c2fa8dc232e6cd67a5f65bf13243f1
Vasileios Kyriakopoulos <vasileios.kyriakopoulos@inl.gov>
2025-02-03 13:35:07 -0700

    fix user object and SCM.h  #28497

--- modules/subchannel/include/base/SCM.h
+++ modules/subchannel/include/base/SCM.h
@@ -1,6 +1,17 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
 #pragma once
 
-#include "MooseMesh.h"
+#include "MooseUtils.h"
+
+class MooseMesh;
 
 namespace SCM
 {
@@ -8,13 +19,14 @@ template <typename T>
 const T &
 getMesh(const MooseMesh & mesh)
 {
+  const auto T_mesh = dynamic_cast<const T *>(&mesh);
   // Check that the mesh of the right type
-  if (!dynamic_cast<const T *>(&mesh))
+  if (!T_mesh)
     mooseError("The mesh is not of type: ",
+               MooseUtils::prettyCppType<T>(),
                ". You must use the relevant Subchannel mesh/mesh generator with this object.");
-  //  prettyCppType<T>(),
 
   // Cast it to desired type
-  return static_cast<const T &>(mesh);
+  return *T_mesh;
 }
-};
+}
