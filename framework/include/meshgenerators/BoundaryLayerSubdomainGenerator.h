56d76ab375a83aea735f485d5d1c6886a79dd62a
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2025-02-10 21:17:08 -0700

    Add a generator to create a boundary layer in a mesh refs #29747

---
+++ framework/include/meshgenerators/BoundaryLayerSubdomainGenerator.h
@@ -0,0 +1,35 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "MeshGenerator.h"
+
+/**
+ * MeshGenerator for defining a subdomain on the layer(s) next to one or more boundaries
+ */
+class BoundaryLayerSubdomainGenerator : public MeshGenerator
+{
+public:
+  static InputParameters validParams();
+
+  BoundaryLayerSubdomainGenerator(const InputParameters & parameters);
+
+  std::unique_ptr<MeshBase> generate() override;
+
+protected:
+  std::unique_ptr<MeshBase> & _input;
+
+  /// Block ID to assign to the boundary layer region
+  subdomain_id_type _new_block_id;
+  /// Block name to assign to the boundary layer region
+  const SubdomainName _new_block_name;
+  /// Whether to consider nodesets in the boundary proximity check
+  const bool _include_nodesets;
+};
