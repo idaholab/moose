323315b8ae493742b81a03a8fa08e04ad5d92129
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2025-02-12 15:15:02 -0700

    Add an interface to implement various default component behaviors on Physics refs #29856

---
+++ framework/src/physics/PhysicsComponentInterface.C
@@ -0,0 +1,74 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "PhysicsComponentInterface.h"
+#include "ComponentInitialConditionInterface.h"
+
+InputParameters
+PhysicsComponentInterface::validParams()
+{
+  InputParameters params = PhysicsBase::validParams();
+  // we likely do not need all these parameters. But developers could expect them
+  return params;
+}
+
+PhysicsComponentInterface::PhysicsComponentInterface(const InputParameters & parameters)
+  : PhysicsBase(parameters)
+{
+  addRequiredPhysicsTask("add_ic");
+}
+
+void
+PhysicsComponentInterface::actOnAdditionalTasks()
+{
+  // TODO: find a way to force this routine to be called by derived classes
+  if (_current_task == "add_ic")
+    addInitialConditionsFromComponents();
+}
+
+void
+PhysicsComponentInterface::addComponent(const ActionComponent & comp)
+{
+  PhysicsBase::addComponent(comp);
+
+  // Move initial conditions from components to the Physics
+  if (const auto comp_ic = dynamic_cast<const ComponentInitialConditionInterface *>(&comp))
+  {
+    for (const auto & var_name : solverVariableNames())
+      if (comp_ic->hasInitialCondition(var_name))
+        addInitialCondition(comp.name(), var_name, comp_ic->getInitialCondition(var_name, name()));
+    for (const auto & var_name : auxVariableNames())
+      if (comp_ic->hasInitialCondition(var_name))
+        addInitialCondition(comp.name(), var_name, comp_ic->getInitialCondition(var_name, name()));
+  }
+}
+
+void
+PhysicsComponentInterface::addInitialCondition(const ComponentName & component_name,
+                                               const VariableName & var_name,
+                                               const MooseFunctorName & ic_value)
+{
+  _components_initial_conditions[component_name][var_name] = ic_value;
+}
+
+void
+PhysicsComponentInterface::addInitialConditionsFromComponents()
+{
+  if (_components_initial_conditions.size())
+  {
+    std::vector<ComponentName> all_components(_components_initial_conditions.size());
+    for (const auto & comp : _components_initial_conditions)
+      all_components.push_back(comp.first);
+    mooseError("Component(s) '",
+               Moose::stringify(all_components),
+               "' requested to add the following initial conditions for this Physics. This Physics "
+               "however does not implement the 'addInitialConditionsFromComponents' "
+               "routine, so it cannot create these initial conditions");
+  }
+}
