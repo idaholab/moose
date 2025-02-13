ac41b0176789c43ca24f648b02709db5eca63892
Corey DeChant <37221357+csdechant@users.noreply.github.com>
2025-02-13 13:43:04 -0700

    Apply suggestions from code review regarding file comments and error/assert messages
    Co-authored-by: Guillaume Giudicelli <guillaume.giudicelli@gmail.com>

--- framework/src/base/Assembly.C
+++ framework/src/base/Assembly.C
@@ -1858,8 +1858,6 @@ Assembly::reinit(const Elem * elem, const std::vector<Point> & reference_points)
   computeCurrentElemVolume();
 }
 
-// Currently Doing: changing anything to a FV verison if an override is possible
-// during a FE reinit for FE coupling.
 void
 Assembly::reinitFVFace(const FaceInfo & fi, bool areFE)
 {
