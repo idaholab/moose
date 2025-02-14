e7d117eaf7d7300a29a4c6648a70ab1c2ed5e221
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2025-02-13 18:32:39 -0700

    Add support for adding boundary conditions from components in a Physics refs #29856

--- framework/include/physics/PhysicsComponentInterface.h
+++ framework/include/physics/PhysicsComponentInterface.h
@@ -11,6 +11,7 @@
 
 #include "PhysicsBase.h"
 #include "MooseTypes.h"
+#include "ComponentBoundaryConditionInterface.h"
 
 /**
  * Interface class to help components interact with Physics
@@ -29,7 +30,7 @@ public:
 
   /**
    * Add an initial condition from a component
-   * @param component_name the name of the component to set the variable
+   * @param component_name the name of the component to set the variable IC on
    * @param var_name the variable to provide the initial condition
    * @param ic_value the functor providing the initial condition
    */
@@ -37,11 +38,34 @@ public:
                            const VariableName & var_name,
                            const MooseFunctorName & ic_value);
 
+  /**
+   * Add a boundary condition from a component
+   * @param component_name the name of the component with the boundary condition
+   * @param var_name the variable to provide the initial condition
+   * @param boundary_name the name of the boundary condition
+   * @param bc_value the functor providing the boundary condition
+   * @param bc_type the type of the boundary condition, defined in
+   * ComponentBoundaryConditionInterface at the moment
+   */
+  void
+  addBoundaryCondition(const ComponentName & component_name,
+                       const VariableName & var_name,
+                       const BoundaryName & boundary_name,
+                       const MooseFunctorName & bc_value,
+                       const ComponentBoundaryConditionInterface::BoundaryConditionType & bc_type);
+
 protected:
   // TODO: add hash constructor to ComponentName to be able to use it as a key
   /// Map of components to variables and initial conditions
   std::unordered_map<std::string, std::map<VariableName, MooseFunctorName>>
       _components_initial_conditions;
+  /// Map of components to variables and boundary conditions
+  std::unordered_map<
+      std::string,
+      std::map<
+          std::pair<VariableName, BoundaryName>,
+          std::pair<MooseFunctorName, ComponentBoundaryConditionInterface::BoundaryConditionType>>>
+      _components_boundary_conditions;
 
 private:
   virtual void actOnAdditionalTasks() override;
@@ -49,4 +73,5 @@ private:
   // The default implementation of these routines will error if the Components have passed
   // information to the Physics, and the Physics has not implemented the expected behavior
   virtual void addInitialConditionsFromComponents();
+  virtual void addBoundaryConditionsFromComponents();
 };
--- framework/src/physics/PhysicsComponentInterface.C
+++ framework/src/physics/PhysicsComponentInterface.C
@@ -30,11 +30,14 @@ PhysicsComponentInterface::actOnAdditionalTasks()
   // TODO: find a way to force this routine to be called by derived classes
   if (_current_task == "add_ic")
     addInitialConditionsFromComponents();
+  else if (_current_task == "add_bc" || _current_task == "add_fv_bc")
+    addBoundaryConditionsFromComponents();
 }
 
 void
 PhysicsComponentInterface::addComponent(const ActionComponent & comp)
 {
+  // Adds the component's blocks to the block restriction at least
   PhysicsBase::addComponent(comp);
 
   // Move initial conditions from components to the Physics
@@ -47,6 +50,29 @@ PhysicsComponentInterface::addComponent(const ActionComponent & comp)
       if (comp_ic->hasInitialCondition(var_name))
         addInitialCondition(comp.name(), var_name, comp_ic->getInitialCondition(var_name, name()));
   }
+
+  // Move boundary conditions from components to the Physics
+  if (const auto comp_bc = dynamic_cast<const ComponentBoundaryConditionInterface *>(&comp))
+  {
+    for (const auto & var_name : solverVariableNames())
+      if (comp_bc->hasBoundaryCondition(var_name))
+        for (const auto & boundary : comp_bc->getBoundaryConditionsBoundaries(var_name))
+        {
+          ComponentBoundaryConditionInterface::BoundaryConditionType bc_type;
+          const auto boundary_functor =
+              comp_bc->getBoundaryCondition(var_name, boundary, name(), bc_type);
+          addBoundaryCondition(comp.name(), var_name, boundary, boundary_functor, bc_type);
+        }
+    for (const auto & var_name : auxVariableNames())
+      if (comp_bc->hasBoundaryCondition(var_name))
+        for (const auto & boundary : comp_bc->getBoundaryConditionsBoundaries(var_name))
+        {
+          ComponentBoundaryConditionInterface::BoundaryConditionType bc_type;
+          const auto boundary_functor =
+              comp_bc->getBoundaryCondition(var_name, boundary, name(), bc_type);
+          addBoundaryCondition(comp.name(), var_name, boundary, boundary_functor, bc_type);
+        }
+  }
 }
 
 void
@@ -55,6 +81,18 @@ PhysicsComponentInterface::addInitialCondition(const ComponentName & component_n
                                                const MooseFunctorName & ic_value)
 {
   _components_initial_conditions[component_name][var_name] = ic_value;
+}
+
+void
+PhysicsComponentInterface::addBoundaryCondition(
+    const ComponentName & component_name,
+    const VariableName & var_name,
+    const BoundaryName & boundary_name,
+    const MooseFunctorName & bc_value,
+    const ComponentBoundaryConditionInterface::BoundaryConditionType & bc_type)
+{
+  _components_boundary_conditions[component_name][std::make_pair(var_name, boundary_name)] =
+      std::make_pair(bc_value, bc_type);
 }
 
 void
@@ -70,5 +108,21 @@ PhysicsComponentInterface::addInitialConditionsFromComponents()
                "' requested to add the following initial conditions for this Physics. This Physics "
                "however does not implement the 'addInitialConditionsFromComponents' "
                "routine, so it cannot create these initial conditions");
+  }
+}
+
+void
+PhysicsComponentInterface::addBoundaryConditionsFromComponents()
+{
+  if (_components_boundary_conditions.size())
+  {
+    std::vector<ComponentName> all_components(_components_boundary_conditions.size());
+    for (const auto & comp : _components_boundary_conditions)
+      all_components.push_back(comp.first);
+    mooseError("Component(s) '",
+               Moose::stringify(all_components),
+               "' requested to add boundary conditions for the variable of this Physics. This "
+               "Physics however does not implement the 'addBoundaryConditionsFromComponents' "
+               "routine, so it cannot create these boundary conditions");
   }
 }
