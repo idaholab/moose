16ba909b5ba1003b818c2b073b6e1b3080e18299
Daniel Schwen <daniel@schwen.de>
2025-02-15 16:53:28 -0700

    parser2 wip

--- modules/solid_mechanics/src/mesh/AbaqusInputParser.C
+++ modules/solid_mechanics/src/mesh/AbaqusInputParser.C
@@ -8,49 +8,84 @@
 //* https://www.gnu.org/licenses/lgpl-2.1.html
 
 #include "AbaqusInputParser.h"
+#include "MooseError.h"
 
 namespace AbaqusInputParser
 {
 
-bool
-AbaqusUELMesh::readDataLine(std::string & s)
+HeaderMap::HeaderMap(std::vector<std::string> fields)
 {
-  s.clear();
-
-  while (true)
+  fields.erase(fields.begin());
+  for (const auto & field : fields)
   {
-    if (_in->peek() == '*' || _in->peek() == EOF)
-    {
-      if (s.empty())
-        return false;
-      mooseError("Incomplete data line.");
-    }
-
-    // read line
-    auto l = readLine();
-    strip(l);
-    s += l;
-
-    // check if line continuation is needed
-    if (s.back() != ',')
-      return true;
+    std::vector<std::string> pair;
+    MooseUtils::tokenize(field, pair, 2, "=");
+    _map[MooseUtils::toLower(MooseUtils::removeExtraWhitespace(pair[0]))] =
+        pair.size() == 1 ? "" : MooseUtils::removeExtraWhitespace(pair[1]);
   }
 }
 
-Root::Root(std::istream file) : Block(*this), _file(file)
+bool
+HeaderMap::has(const std::string & key) const
 {
-  // register syntax
-  registerBlock<Part>("part");
-  registerBlock<Assembly>("assembly");
-  registerBlock<Step>("step");
-  registerOption<Boundary>("boundary");
-  registerOption<Initial Conditions>("initial conditions");
-
-  // load and preprocess entire file
-  loadFile();
+  return _map.find(MooseUtils::toLower(key)) != _map.end();
 }
 
-Root::loadFile()
+template <>
+bool
+HeaderMap::get(const std::string & key) const
+{
+  const auto it = _map.find(MooseUtils::toLower(key));
+  return it != _map.end();
+}
+
+std::string
+HeaderMap::stringify(const std::string & indent) const
+{
+  std::string s;
+  for (const auto & [key, value] : _map)
+    if (value.empty())
+      s += indent + key + "\n";
+    else
+      s += indent + key + " = " + value + "\n";
+  return s;
+}
+
+/**
+ * These sets help the parser distinguish between blocks and options, where
+ * options are keyword lines followed by data lines, and blocks are input
+ * sections terminated by an `*end xxxx` keyword that can nest other blocks
+ * and options.
+ */
+const static std::set<std::string> abaqus_blocks = {"assembly", "part", "step", "instance"};
+const static std::set<std::string> abaqus_options = {
+    "boundary",
+    "dload",
+    "element",
+    "elset",
+    "heading",
+    "initial conditions",
+    "node",
+    "nset",
+    "output",
+    "preprint",
+    "restart",
+    "static",
+    "uel property",
+    "user element",
+};
+
+AbaqusInputParser::AbaqusInputParser(std::istream & file) : _file(file)
+{
+  // load and preprocess entire file
+  loadFile();
+
+  // build the parse tree
+  _root = parseBlock();
+}
+
+void
+AbaqusInputParser::loadFile()
 {
   _current_line = 0;
 
@@ -65,13 +100,13 @@ Root::loadFile()
     ss = s;
 
     // continuation lines
-    while (ss.back() != ',')
+    while (ss.length() > 0 && ss.back() == ',')
     {
       std::getline(_file, s);
 
       // error, expected a continuation line but got EOF
       if (_file.eof())
-        throw EndOfAbaqusInput();
+        mooseError("Expected a continuation line but got EOF in Abaqus input file");
 
       ss += s;
     }
@@ -80,17 +115,99 @@ Root::loadFile()
     if (ss.length() < 2 || ss.substr(0, 2) == "**")
       continue;
 
+    // tokenize and trim
     std::vector<std::string> items;
     MooseUtils::tokenize(ss, items, 1, ",");
+    for (auto & item : items)
+      item = MooseUtils::removeExtraWhitespace(item);
+
     _lines.push_back(items);
   }
 }
 
-Assembly::Assembly(const Root &) : Block(root)
+BlockNode
+AbaqusInputParser::parseBlock(const std::string & end, const std::vector<std::string> & head_line)
 {
-  registerBlock<Instrance>("instance");
-  registerOption<Nset>("nset");
-  registerOpt<Elset>("elset");
+  // std::cout << "*" << end << '\n';
+  BlockNode node(head_line);
+  while (_current_line < _lines.size())
+  {
+    const auto & line = _lines[_current_line];
+    // std::cout << Moose::stringify(line) << '\n';
+    if (line[0][0] != '*')
+      mooseError("Expected a keyword line in Abaqus input file");
+    const auto keyword = MooseUtils::toLower(line[0].substr(1));
+
+    // subblock end
+    if (!end.empty() && keyword == "end " + end)
+    {
+      _current_line++;
+      break;
+    }
+
+    if (abaqus_blocks.count(keyword))
+    {
+      _current_line++;
+      node._children.push_back(parseBlock(keyword, line));
+    }
+
+    else if (abaqus_options.count(keyword))
+    {
+      _current_line++;
+      node._options.push_back(parseOption(line));
+    }
+
+    else
+      mooseError("Unknown keyword '" + line[0] +
+                 "' in Abaqus input file. Add this to abaqus_blocks or abaqus_options in "
+                 "AbaqusInputParser.C to parse it.");
+  }
+  return node;
+}
+
+OptionNode
+AbaqusInputParser::parseOption(const std::vector<std::string> & head_line)
+{
+  OptionNode node(head_line);
+  while (_current_line < _lines.size())
+  {
+    const auto & line = _lines[_current_line];
+    if (line[0][0] == '*')
+      break;
+    node._data.push_back(line);
+    _current_line++;
+  }
+  return node;
+}
+
+Node::Node(std::vector<std::string> line)
+{
+  _type = line[0];
+  _header = HeaderMap(line);
+}
+
+std::string
+OptionNode::stringify(const std::string & indent) const
+{
+  std::string s = indent + "<" + _type + ">\n";
+  s += _header.stringify(indent + "  ");
+  for (const auto & line : _data)
+    s += indent + "    " + Moose::stringify(line) + "\n";
+  s += indent + "<>\n";
+  return s;
+}
+
+std::string
+BlockNode::stringify(const std::string & indent) const
+{
+  std::string s = indent + "[" + _type + "]\n";
+  s += _header.stringify(indent + "  ");
+  for (const auto & child : _children)
+    s += child.stringify(indent + "  ");
+  for (const auto & option : _options)
+    s += option.stringify(indent + "  ");
+  s += indent + "[]\n";
+  return s;
 }
 
 } // namespace AbaqusInputParser
