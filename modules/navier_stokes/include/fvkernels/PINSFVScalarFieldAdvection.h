efff8f6270553e47a908e70904f018b2ea6a7f97
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2025-02-17 14:43:44 -0700

    Add a kernel for scalar advection in porous media closes #29901

---
+++ modules/navier_stokes/include/fvkernels/PINSFVScalarFieldAdvection.h
@@ -0,0 +1,29 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "INSFVScalarAdvectionKernel.h"
+
+/**
+ * An advection kernel that implements interpolation schemes specific to Navier-Stokes flow
+ * physics and that advects arbitrary scalar quantities in porous medium
+ */
+class PINSFVScalarFieldAdvection : public INSFVScalarAdvectionKernel
+{
+public:
+  static InputParameters validParams();
+  PINSFVScalarFieldAdvection(const InputParameters & params);
+
+protected:
+  virtual ADReal computeQpResidual() override;
+
+  /// Local medium porosity as a functor
+  const Moose::Functor<ADReal> & _eps;
+};
