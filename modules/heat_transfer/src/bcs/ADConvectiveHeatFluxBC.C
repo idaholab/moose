54caf577d96671baaf6073381c6ff673a493afe0
Zachary M. Prince <zachary.prince@inl.gov>
2025-02-19 16:58:54 -0700

    Getting convective heat transfer BC to accept FV variables on the other side of the boundary #24308

--- modules/heat_transfer/src/bcs/ADConvectiveHeatFluxBC.C
+++ modules/heat_transfer/src/bcs/ADConvectiveHeatFluxBC.C
@@ -27,6 +27,17 @@ ADConvectiveHeatFluxBC::validParams()
   params.addParam<MooseFunctorName>("T_infinity_functor", "Functor for far-field temperature");
   params.addParam<MooseFunctorName>("heat_transfer_coefficient_functor",
                                     "Functor for heat transfer coefficient");
+  // In the case where we are coupling a FV variable (via functors) that are block restricted on the
+  // neighboring side of boundary, we need to have two layers of ghosting to do the face
+  // interpolation.
+  params.addRelationshipManager("ElementSideNeighborLayers",
+                                Moose::RelationshipManagerType::GEOMETRIC |
+                                    Moose::RelationshipManagerType::ALGEBRAIC,
+                                [](const InputParameters & obj_params, InputParameters & rm_params)
+                                {
+                                  rm_params.set<unsigned short>("layers") =
+                                      obj_params.isParamValid("T_infinity_functor") ? 2 : 1;
+                                });
   return params;
 }
 
@@ -71,6 +82,44 @@ ADConvectiveHeatFluxBC::ADConvectiveHeatFluxBC(const InputParameters & parameter
                "Far field temperature and heat transfer coefficients must be specified");
 }
 
+void
+ADConvectiveHeatFluxBC::initialSetup()
+{
+  if (_T_infinity)
+    return;
+
+  bool T_inf_can_use_neighbor = true;
+  bool htc_can_use_neighbor = true;
+  // Loop over elements on the primary side of all boundaries
+  for (const auto & bnd_elem : *_mesh.getBoundaryElementRange())
+  {
+    const auto & [elem, side, bid] = *bnd_elem;
+    // Skip if this boundary is not part of the restriction
+    if (!hasBoundary(bid))
+      continue;
+
+    // Use neighbors if the functor is not defined on all primary blocks
+    _T_infinity_use_neighbor =
+        _T_infinity_use_neighbor || !_T_infinity_functor->hasBlocks(elem->subdomain_id());
+    _htc_use_neighbor = _htc_use_neighbor || !_htc_functor->hasBlocks(elem->subdomain_id());
+
+    // Determine if neighbor can be used, just in case
+    const auto neighbor = elem->neighbor_ptr(side);
+    T_inf_can_use_neighbor = T_inf_can_use_neighbor && neighbor &&
+                             _T_infinity_functor->hasBlocks(neighbor->subdomain_id());
+    htc_can_use_neighbor =
+        htc_can_use_neighbor && neighbor && _htc_functor->hasBlocks(neighbor->subdomain_id());
+  }
+
+  const std::string error_msg =
+      "Functor must either be defined on all of the primary side of the boundary or on all "
+      "of the neighboring side.";
+  if (_T_infinity_use_neighbor && !T_inf_can_use_neighbor)
+    paramError("T_infinity_functor", error_msg);
+  if (_htc_use_neighbor && !htc_can_use_neighbor)
+    paramError("heat_transfer_coefficient_functor", error_msg);
+}
+
 ADReal
 ADConvectiveHeatFluxBC::computeQpResidual()
 {
@@ -78,10 +127,27 @@ ADConvectiveHeatFluxBC::computeQpResidual()
     return -_test[_i][_qp] * (*_htc)[_qp] * ((*_T_infinity)[_qp] - _u[_qp]);
   else
   {
+    // Populate neighbor information, if necessary (optimization on first qp)
+    if ((_T_infinity_use_neighbor || _htc_use_neighbor) && _qp == 0)
+    {
+      _current_neighbor_elem = _current_elem->neighbor_ptr(_current_side);
+      mooseAssert(_current_neighbor_elem, "Neighbor element should exist at this point.");
+      _current_neighbor_side = _current_neighbor_elem->which_neighbor_am_i(_current_elem);
+    }
+
+    // Convenience lambda for getting space argument on either element or neighbor
+    const auto get_space_arg = [this](bool use_neighbor) -> Moose::ElemSideQpArg
+    {
+      if (!use_neighbor)
+        return {_current_elem, _current_side, _qp, _qrule, _q_point[_qp]};
+      else
+        return {_current_neighbor_elem, _current_neighbor_side, _qp, _qrule, _q_point[_qp]};
+    };
+
+    const auto Tinf_space_arg = get_space_arg(_T_infinity_use_neighbor);
+    const auto htc_space_arg = get_space_arg(_htc_use_neighbor);
     const auto time_arg = Moose::currentState();
-    const Moose::ElemSideQpArg space_arg = {
-        _current_elem, _current_side, _qp, _qrule, _q_point[_qp]};
-    return -_test[_i][_qp] * (*_htc_functor)(space_arg, time_arg) *
-           ((*_T_infinity_functor)(space_arg, time_arg) - _u[_qp]);
+    return -_test[_i][_qp] * (*_htc_functor)(htc_space_arg, time_arg) *
+           ((*_T_infinity_functor)(Tinf_space_arg, time_arg) - _u[_qp]);
   }
 }
