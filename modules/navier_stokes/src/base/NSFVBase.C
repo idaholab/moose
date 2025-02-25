cdd4b2582ea896d4b29d4677b11384324377bdb7
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2025-02-25 13:31:02 -0700

    Add support for not setting energy wall BCs on every momentum wall boundary refs #29955

--- modules/navier_stokes/src/base/NSFVBase.C
+++ modules/navier_stokes/src/base/NSFVBase.C
@@ -228,6 +228,11 @@ NSFVBase::commonFluidEnergyEquationParams()
                                  "generally computed using a correlation");
   params.addParam<MultiMooseEnum>(
       "energy_wall_types", en_wall_types, "Types for the wall boundaries for the energy equation.");
+  params.addParam<std::vector<BoundaryName>>(
+      "energy_wall_boundaries",
+      {},
+      "Wall boundaries to apply energy boundary conditions on. If not specified, the flow equation "
+      "Physics wall boundaries will be used");
 
   params.addParam<std::vector<MooseFunctorName>>(
       "energy_wall_function",
@@ -612,9 +617,9 @@ NSFVBase::validParams()
 
   params.addParamNamesToGroup(
       "inlet_boundaries momentum_inlet_types momentum_inlet_function energy_inlet_types "
-      "energy_inlet_function wall_boundaries momentum_wall_types energy_wall_types "
-      "energy_wall_function outlet_boundaries momentum_outlet_types pressure_function "
-      "passive_scalar_inlet_types passive_scalar_inlet_function flux_inlet_pps "
+      "energy_inlet_function wall_boundaries momentum_wall_types energy_wall_boundaries "
+      "energy_wall_types energy_wall_function outlet_boundaries momentum_outlet_types "
+      "pressure_function passive_scalar_inlet_types passive_scalar_inlet_function flux_inlet_pps "
       "flux_inlet_directions",
       "Boundary condition");
 
