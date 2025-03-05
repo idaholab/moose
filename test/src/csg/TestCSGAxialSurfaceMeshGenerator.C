6b8bcb056dcdc1f6cc9cfa88f002888dd0d7c59f
Kalin Kiesling <kkiesling@anl.gov>
2025-03-05 10:26:02 -0600

    update the axial surface test to generate plane from coefficients for better test coverage

--- test/src/csg/TestCSGAxialSurfaceMeshGenerator.C
+++ test/src/csg/TestCSGAxialSurfaceMeshGenerator.C
@@ -51,16 +51,15 @@ TestCSGAxialSurfaceMeshGenerator::generateCSG()
   const auto centroid = Point(0, 0, 0);
 
   // Add surfaces and halfspaces corresponding to top and bottom axial planes
-  std::vector<std::vector<Point>> points_on_planes {
-    {Point(0., 0.,  1. * _axial_height / 2.), Point(1., 0.,  1. * _axial_height / 2.), Point(0., 1.,  1. * _axial_height / 2.)},
-    {Point(0., 0., -1. * _axial_height / 2.), Point(1., 0., -1. * _axial_height / 2.), Point(0., 1., -1. * _axial_height / 2.)}
-  };
   std::vector<std::string> surf_names {"plus_z", "minus_z"};
+  std::vector<Real> coeffs{0.5 * _axial_height, -0.5 * _axial_height};
 
-  for (unsigned int i = 0; i < points_on_planes.size(); ++i)
+  for (unsigned int i = 0; i < coeffs.size(); ++i)
   {
     const auto surf_name = "surf_" + surf_names[i];
-    auto plane_ptr = input_mesh->createPlaneFromPoints(surf_name, points_on_planes[i][0], points_on_planes[i][1], points_on_planes[i][2]);
+    // z plane equation: 0.0*x + 0.0*y + 1.0*z = (+/-)0.5 * axial_height
+    auto plane_ptr =
+        input_mesh->createPlaneFromCoefficients(surf_name, 0.0, 0.0, 1.0, coeffs[i]);
     const auto elem_direction = plane_ptr->directionFromPoint(centroid);
     auto elem_halfspace = CSG::CSGHalfspace(plane_ptr, elem_direction);
     elem_cell_ptr->addRegionHalfspace(elem_halfspace);
