55b7155a19ba4de72abf33eb8d6b9ac684466727
Topher Matthews <cmatthews@lanl.gov>
2025-03-06 15:47:18 -0700

    remove FV check

--- framework/src/materials/CoupledGradientMaterial.C
+++ framework/src/materials/CoupledGradientMaterial.C
@@ -41,9 +41,6 @@ CoupledGradientMaterialTempl<is_ad>::CoupledGradientMaterialTempl(
     _scalar_property_factor(getGenericMaterialProperty<Real, is_ad>("scalar_property_factor")),
     _grad_u(coupledGenericGradient<is_ad>("u"))
 {
-  if (getFieldVar("u", 0)->isFV())
-    mooseError("Gradient of finite volume variables are zero, thus this material property is "
-               "irrelevant.");
 }
 
 template <bool is_ad>
