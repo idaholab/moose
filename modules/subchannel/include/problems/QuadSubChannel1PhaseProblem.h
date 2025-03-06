ca5ead9b37cfa08728c04fad07c103c3e2c8083a
Vasileios Kyriakopoulos <vasileios.kyriakopoulos@inl.gov>
2025-03-06 16:34:15 -0700

    Miscalleneous v9 #28497

--- modules/subchannel/include/problems/QuadSubChannel1PhaseProblem.h
+++ modules/subchannel/include/problems/QuadSubChannel1PhaseProblem.h
@@ -22,7 +22,7 @@ public:
 
 protected:
   virtual void initializeSolution() override;
-  virtual Real computeFrictionFactor(_FrictionStruct friction_args) override;
+  virtual Real computeFrictionFactor(FrictionStruct friction_args) override;
   virtual Real computeAddedHeatPin(unsigned int i_ch, unsigned int iz) override;
   virtual void computeWijPrime(int iblock) override;
   virtual void computeh(int iblock) override;
