d1fcae6fdfddeb653f891f6543de7866543dc3fd
Alexander Blair <alexander.blair@ukaea.uk>
2025-03-07 14:44:26 +0000

    Move MFEMKernel constructor and validParams implementation into MFEMKernel.C

--- framework/include/mfem/kernels/MFEMKernel.h
+++ framework/include/mfem/kernels/MFEMKernel.h
@@ -11,31 +11,9 @@ Class to construct an MFEM integrator to apply to the equation system.
 class MFEMKernel : public MFEMGeneralUserObject
 {
 public:
-  static InputParameters validParams()
-  {
-    InputParameters params = MFEMGeneralUserObject::validParams();
-    params.registerBase("Kernel");
-    params.addParam<VariableName>("variable",
-                                  "Variable labelling the weak form this kernel is added to");
-    params.addParam<std::vector<SubdomainName>>("block",
-                                                {},
-                                                "The list of blocks (ids) that this "
-                                                "object will be applied to. Leave empty to apply "
-                                                "to all blocks.");
-    return params;
-  }
+  static InputParameters validParams();
 
-  MFEMKernel(const InputParameters & parameters)
-    : MFEMGeneralUserObject(parameters),
-      _test_var_name(getParam<VariableName>("variable")),
-      _subdomain_names(getParam<std::vector<SubdomainName>>("block")),
-      _subdomain_attributes(_subdomain_names.size())
-  {
-    for (unsigned int i = 0; i < _subdomain_names.size(); ++i)
-    {
-      _subdomain_attributes[i] = std::stoi(_subdomain_names[i]);
-    }
-  }
+  MFEMKernel(const InputParameters & parameters);
 
   virtual ~MFEMKernel() = default;
 
---
+++ framework/src/mfem/kernels/MFEMKernel.C
@@ -0,0 +1,28 @@
+#include "MFEMKernel.h"
+
+InputParameters
+MFEMKernel::validParams()
+{
+  InputParameters params = MFEMGeneralUserObject::validParams();
+  params.registerBase("Kernel");
+  params.addParam<std::string>("variable",
+                               "Variable labelling the weak form this kernel is added to");
+  params.addParam<std::vector<SubdomainName>>("block",
+                                              {},
+                                              "The list of blocks (ids) that this "
+                                              "object will be applied to. Leave empty to apply "
+                                              "to all blocks.");
+  return params;
+}
+
+MFEMKernel::MFEMKernel(const InputParameters & parameters)
+  : MFEMGeneralUserObject(parameters),
+    _test_var_name(getParam<std::string>("variable")),
+    _subdomain_names(getParam<std::vector<SubdomainName>>("block")),
+    _subdomain_attributes(_subdomain_names.size())
+{
+  for (unsigned int i = 0; i < _subdomain_names.size(); ++i)
+  {
+    _subdomain_attributes[i] = std::stoi(_subdomain_names[i]);
+  }
+}
