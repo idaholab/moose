7f119c6e699d70d855f5539803e74bbacc963e59
Alex Lindsay <alexander.lindsay@inl.gov>
2025-03-13 16:53:21 -0600

    Reset preallocation as well for debugging different convergence histories

--- framework/src/problems/FEProblemBase.C
+++ framework/src/problems/FEProblemBase.C
@@ -7227,10 +7227,7 @@ FEProblemBase::computeJacobianTags(const std::set<TagID> & tags)
           if (_current_nl_sys->hasMatrix(tag))
           {
             auto & matrix = _current_nl_sys->getMatrix(tag);
-            if (matrix.use_hash_table())
-              matrix.reset_memory();
-            else
-              matrix.zero();
+            matrix.reset_memory();
             if (haveADObjects())
               // PETSc algorithms require diagonal allocations regardless of whether there is
               // non-zero diagonal dependence. With global AD indexing we only add non-zero
