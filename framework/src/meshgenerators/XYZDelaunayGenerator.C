b3bf3f221b295143f80c3e30a13d59b11fb71d21
Yinbin Miao <ymiao@anl.gov>
2025-03-13 23:02:13 -0500

    Enable External and Hole Boundaries Assignment in XYZDelaunay
    closes #30154

--- framework/src/meshgenerators/XYZDelaunayGenerator.C
+++ framework/src/meshgenerators/XYZDelaunayGenerator.C
@@ -119,6 +119,11 @@ XYZDelaunayGenerator::generate()
   std::unique_ptr<UnstructuredMesh> mesh =
       dynamic_pointer_cast<UnstructuredMesh>(std::move(_bdy_ptr));
 
+  // The libMesh Netgen removes all the sideset info
+  // But it keeps some nodeset info for the retained nodes
+  // We need to clear these nodeset info as they could be annoying
+  mesh->get_boundary_info().clear_boundary_node_ids();
+
   // Get ready to triangulate its boundary
   libMesh::NetGenMeshInterface ngint(*mesh);
 
@@ -201,16 +206,22 @@ XYZDelaunayGenerator::generate()
   // want to pick a new ID on hole N that doesn't conflict with any
   // IDs on hole M < N (or with the IDs on the new triangulation)
 
-  // The new triangulation by default assigns BCID i+1 to hole i ...
-  // but we can't even use this for mesh stitching, because we can't
-  // be sure it isn't also already in use on the hole's mesh and so we
-  // won't be able to safely clear it afterwards.
+  // The Netgen generated mesh does not have hole and output boundary ids,
+  // which is different from its 2D counterpart.
+  // So, we need to assign boundary ids to the hole boundaries and output boundary
+  // By default, we assign the hole boundary ids to be 1,2,..., N
+  // and the output boundary id to be 0
+  // We will need a different set of boundary ids if we are stitching holes
+
+  // end_bcid is one more than the maximum of default output boundary id and hole boundary ids
   const boundary_id_type end_bcid = _hole_ptrs.size() + 1;
 
   // For the hole meshes that need to be stitched, we would like to make sure the hole boundary ids
   // and output boundary id are not conflicting with the existing boundary ids of the hole meshes to
   // be stitched.
   BoundaryID free_boundary_id = 0;
+  // We need to get a free boundary id that is larger than the existing boundary ids in any hole
+  // meshes
   if (_stitch_holes.size())
   {
     for (auto hole_i : index_range(_hole_ptrs))
@@ -222,48 +233,44 @@ XYZDelaunayGenerator::generate()
         (*_hole_ptrs[hole_i])->comm().max(free_boundary_id);
       }
     }
-    for (auto h : index_range(_hole_ptrs))
-      libMesh::MeshTools::Modification::change_boundary_id(*mesh, h + 1, h + 1 + free_boundary_id);
   }
+  // new_hole_bcid is used to ensure the boundary ids used for stitching have no conflicts
+  // If we shift the default hole boundary ids and output boundary id by free_boundary_id, we need
+  // to have new_hole_bcid that is larger than any existing boundary ids in the hole meshes and the
+  // Netgen generated mesh
   boundary_id_type new_hole_bcid = end_bcid + free_boundary_id;
 
-  // We might be overriding the default bcid numbers.  We have to be
-  // careful about how we renumber, though.  We pick unused temporary
-  // numbers because e.g. "0->2, 2->0" is impossible to do
-  // sequentially, but "0->N, 2->N+2, N->2, N+2->0" works.
-  libMesh::MeshTools::Modification::change_boundary_id(
-      *mesh, 0, (isParamValid("output_boundary") ? end_bcid : 0) + free_boundary_id);
+  auto hole_boundaries = isParamValid("hole_boundaries")
+                             ? getParam<std::vector<BoundaryName>>("hole_boundaries")
+                             : std::vector<BoundaryName>();
+  const BoundaryName output_boundary =
+      isParamValid("output_boundary") ? getParam<BoundaryName>("output_boundary") : BoundaryName();
 
+  // if outer boundary id is not specified by a numeric output_boundary, we just assign
+  // free_boundary_id to the output boundary
+  const std::vector<BoundaryID> output_boundary_id =
+      isParamValid("output_boundary")
+          ? (MooseUtils::isDigits(output_boundary)
+                 ? std::vector<BoundaryID>(
+                       1, MooseMeshUtils::getIDFromName<BoundaryName, BoundaryID>(output_boundary))
+                 : std::vector<BoundaryID>(1, free_boundary_id))
+          : std::vector<BoundaryID>();
+  // Similarly, set the hole boundary ids one by one
+  std::vector<BoundaryID> hole_boundary_ids;
   if (isParamValid("hole_boundaries"))
-  {
-    auto hole_boundaries = getParam<std::vector<BoundaryName>>("hole_boundaries");
-    auto hole_boundary_ids = MooseMeshUtils::getBoundaryIDs(*mesh, hole_boundaries, true);
+    for (auto h : index_range(hole_boundaries))
+      hole_boundary_ids.push_back(
+          MooseUtils::isDigits(hole_boundaries[h])
+              ? MooseMeshUtils::getIDFromName<BoundaryName, BoundaryID>(hole_boundaries[h])
+              : h + 1 + free_boundary_id);
 
+  // if large ids are used in the hole boundaries or the output boundary,
+  // we need to make sure the new_hole_bcid is larger than them
+  if (hole_boundary_ids.size())
     for (auto h : index_range(_hole_ptrs))
-      libMesh::MeshTools::Modification::change_boundary_id(
-          *mesh, h + 1 + free_boundary_id, h + 1 + free_boundary_id + end_bcid);
-
-    for (auto h : index_range(_hole_ptrs))
-    {
-      libMesh::MeshTools::Modification::change_boundary_id(
-          *mesh, h + 1 + free_boundary_id + end_bcid, hole_boundary_ids[h]);
-      mesh->get_boundary_info().sideset_name(hole_boundary_ids[h]) = hole_boundaries[h];
       new_hole_bcid = std::max(new_hole_bcid, boundary_id_type(hole_boundary_ids[h] + 1));
-    }
-  }
-
-  if (isParamValid("output_boundary"))
-  {
-    const BoundaryName output_boundary = getParam<BoundaryName>("output_boundary");
-    const std::vector<BoundaryID> output_boundary_id =
-        MooseMeshUtils::getBoundaryIDs(*mesh, {output_boundary}, true);
-
-    libMesh::MeshTools::Modification::change_boundary_id(
-        *mesh, end_bcid + free_boundary_id, output_boundary_id[0]);
-    mesh->get_boundary_info().sideset_name(output_boundary_id[0]) = output_boundary;
-
+  if (output_boundary_id.size())
     new_hole_bcid = std::max(new_hole_bcid, boundary_id_type(output_boundary_id[0] + 1));
-  }
 
   bool doing_stitching = false;
 
@@ -280,7 +287,7 @@ XYZDelaunayGenerator::generate()
     if (hole_i < _stitch_holes.size() && _stitch_holes[hole_i])
       doing_stitching = true;
   }
-
+  // Now we can define boundaries to assist with stitching
   const boundary_id_type inner_bcid = new_hole_bcid + 1;
 
   // libMesh mesh stitching still requires a serialized mesh, and it's
@@ -295,7 +302,10 @@ XYZDelaunayGenerator::generate()
   // that can differ from mesh to mesh.  The node locations themselves
   // should always match up exactly, though, so let's use (sorted!)
   // tuples of those to map from nodes to elements and side numbers.
-  std::unordered_map<std::tuple<Point, Point, Point>, std::pair<Elem *, unsigned int>> mesh_faces;
+  // Also added a Boolean to check if the face is already used
+  std::unordered_map<std::tuple<Point, Point, Point>,
+                     std::pair<std::pair<Elem *, unsigned int>, bool>>
+      mesh_faces;
 
   auto sorted_point_tuple = [](Elem & elem, unsigned int side)
   {
@@ -331,7 +341,7 @@ XYZDelaunayGenerator::generate()
         {
           auto points = sorted_point_tuple(*elem, s);
           libmesh_assert(!mesh_faces.count(points));
-          mesh_faces.emplace(points, std::make_pair(elem, s));
+          mesh_faces.emplace(points, std::make_pair(std::make_pair(elem, s), false));
         }
 
   auto & mesh_boundary_info = mesh->get_boundary_info();
@@ -340,36 +350,95 @@ XYZDelaunayGenerator::generate()
   auto & main_subdomain_map = mesh->set_subdomain_name_map();
   for (auto hole_i : index_range(_hole_ptrs))
   {
-    if (hole_i < _stitch_holes.size() && _stitch_holes[hole_i])
-    {
-      UnstructuredMesh & hole_mesh = dynamic_cast<UnstructuredMesh &>(**_hole_ptrs[hole_i]);
-      auto & hole_boundary_info = hole_mesh.get_boundary_info();
+    UnstructuredMesh & hole_mesh = dynamic_cast<UnstructuredMesh &>(**_hole_ptrs[hole_i]);
+    auto & hole_boundary_info = hole_mesh.get_boundary_info();
 
-      // Our algorithm here requires a serialized Mesh.  To avoid
-      // redundant serialization and deserialization (libMesh
-      // MeshedHole and stitch_meshes still also require
-      // serialization) we'll do the serialization up front.
-      libMesh::MeshSerializer serial_hole(hole_mesh);
+    // Our algorithm here requires a serialized Mesh.  To avoid
+    // redundant serialization and deserialization (libMesh
+    // MeshedHole and stitch_meshes still also require
+    // serialization) we'll do the serialization up front.
+    libMesh::MeshSerializer serial_hole(hole_mesh);
 
-      // We'll look for any sides that match between the hole mesh and
-      // the newly triangulated mesh, and apply bcids accordingly.
-      for (auto elem : hole_mesh.element_ptr_range())
-        for (auto s : make_range(elem->n_sides()))
-          if (!elem->neighbor_ptr(s))
+    // We'll look for any sides that match between the hole mesh and
+    // the newly triangulated mesh, and apply bcids accordingly.
+    for (auto elem : hole_mesh.element_ptr_range())
+      for (auto s : make_range(elem->n_sides()))
+        if (!elem->neighbor_ptr(s))
+        {
+          auto points = sorted_point_tuple(*elem, s);
+          auto it = mesh_faces.find(points);
+
+          // I'd love to assert that we don't have any missing
+          // matches, but our holes might themselves have holes
+          if (it != mesh_faces.end())
           {
-            auto points = sorted_point_tuple(*elem, s);
-            auto it = mesh_faces.find(points);
-
-            // I'd love to assert that we don't have any missing
-            // matches, but our holes might themselves have holes
-            if (it != mesh_faces.end())
+            auto [main_elem, main_side] = it->second.first;
+            if (hole_i < _stitch_holes.size() && _stitch_holes[hole_i])
             {
-              auto [main_elem, main_side] = it->second;
               hole_boundary_info.add_side(elem, s, new_hole_bcid);
               mesh_boundary_info.add_side(main_elem, main_side, inner_bcid);
             }
+            // We would like to take this opportunity to identify the hole boundary
+            // We set the boundary id to hole_i + 1 at this stage (the default)
+            mesh_boundary_info.add_side(main_elem, main_side, hole_i + 1);
+            it->second.second = true;
           }
+        }
+  }
 
+  // Any sides that do not match the hole surfaces belong to the external boundary
+  // We set the boundary id to 0 at this stage (the default)
+  for (auto & [points, elem_side] : mesh_faces)
+    if (!elem_side.second)
+    {
+      auto [main_elem, main_side] = elem_side.first;
+      mesh_boundary_info.add_side(main_elem, main_side, 0);
+    }
+
+  // Now hole boundary ids are 1,2,..., N
+  // Now external boundary id is 0
+  // We will find the upper bound of the boundary ids of the mesh so that we would not overwrite
+  // things during renumbering (because of the bcids used for stitching, this is different from
+  // end_bcid)
+  const auto temp_bcid_shift = MooseMeshUtils::getNextFreeBoundaryID(*mesh);
+  // If we stitch holes, we want to make sure the netgen mesh has no conflicting boundary ids
+  // with the hole meshes.
+  // If we assign custom boundary ids, we want to make sure no overwriting occurs.
+  // So we shift all these boundary ids by temp_bcid_shift + free_boundary_id
+  // before we assign the new boundary ids
+  if (_stitch_holes.size() || output_boundary_id.size())
+    libMesh::MeshTools::Modification::change_boundary_id(
+        *mesh, 0, temp_bcid_shift + free_boundary_id);
+  if (_stitch_holes.size() || hole_boundary_ids.size())
+    for (auto hole_i : index_range(_hole_ptrs))
+      libMesh::MeshTools::Modification::change_boundary_id(
+          *mesh, hole_i + 1, hole_i + 1 + temp_bcid_shift + free_boundary_id);
+
+  // Now we can reassign the boundary ids
+  // If these ids are specified, we will use them
+  // Otherwise, we will use the default ones
+  if (output_boundary_id.size())
+    libMesh::MeshTools::Modification::change_boundary_id(
+        *mesh, temp_bcid_shift + free_boundary_id, output_boundary_id[0]);
+  else
+    libMesh::MeshTools::Modification::change_boundary_id(
+        *mesh, temp_bcid_shift + free_boundary_id, free_boundary_id);
+
+  if (hole_boundary_ids.size())
+    for (auto hole_i : index_range(_hole_ptrs))
+      libMesh::MeshTools::Modification::change_boundary_id(
+          *mesh, hole_i + 1 + temp_bcid_shift + free_boundary_id, hole_boundary_ids[hole_i]);
+  else
+    for (auto hole_i : index_range(_hole_ptrs))
+      libMesh::MeshTools::Modification::change_boundary_id(
+          *mesh, hole_i + 1 + temp_bcid_shift + free_boundary_id, hole_i + 1 + free_boundary_id);
+
+  for (auto hole_i : index_range(_hole_ptrs))
+  {
+    UnstructuredMesh & hole_mesh = dynamic_cast<UnstructuredMesh &>(**_hole_ptrs[hole_i]);
+
+    if (hole_i < _stitch_holes.size() && _stitch_holes[hole_i])
+    {
       // Retrieve subdomain name map from the mesh to be stitched and insert it into the main
       // subdomain map
       const auto & increment_subdomain_map = hole_mesh.get_subdomain_name_map();
@@ -387,6 +456,13 @@ XYZDelaunayGenerator::generate()
         mooseError("Failed to stitch hole mesh ", hole_i, " to new tetrahedralization.");
     }
   }
+  // Boundary names
+  if (hole_boundary_ids.size())
+    for (auto h : index_range(_hole_ptrs))
+      mesh->get_boundary_info().sideset_name(hole_boundary_ids[h]) = hole_boundaries[h];
+  if (output_boundary_id.size())
+    mesh->get_boundary_info().sideset_name(output_boundary_id[0]) = output_boundary;
+
   // Check if one SubdomainName is shared by more than one subdomain ids
   std::set<SubdomainName> main_subdomain_map_name_list;
   for (auto const & id_name_pair : main_subdomain_map)
