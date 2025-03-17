e751f9e31f9000e680f688f167c41bce3deb0824
Peter German <grmnptr@gmail.com>
2025-03-17 14:48:29 -0600

    Apply format changes. (#30107)

--- framework/src/libtorch/materials/TorchScriptMaterial.C
+++ framework/src/libtorch/materials/TorchScriptMaterial.C
@@ -17,24 +17,29 @@ InputParameters
 TorchScriptMaterial::validParams()
 {
   InputParameters params = Material::validParams();
-  params.addClassDescription("Material object which relies on the evaluation of a TorchScript module.");
-  params.addRequiredParam<std::vector<std::string>>("prop_names",
-                                            "The names of the properties this material will generate.");
-  params.addRequiredParam<std::vector<PostprocessorName>>("input_names", "The input parameters for the neural network.");
-  params.addRequiredParam<UserObjectName>("torch_script_userobject", "The name of the user object which contains the torch script module.");
+  params.addClassDescription(
+      "Material object which relies on the evaluation of a TorchScript module.");
+  params.addRequiredParam<std::vector<std::string>>(
+      "prop_names", "The names of the properties this material will generate.");
+  params.addRequiredParam<std::vector<PostprocessorName>>(
+      "input_names", "The input parameters for the neural network.");
+  params.addRequiredParam<UserObjectName>(
+      "torch_script_userobject",
+      "The name of the user object which contains the torch script module.");
 
   return params;
 }
 
-TorchScriptMaterial::TorchScriptMaterial(
-    const InputParameters & parameters)
+TorchScriptMaterial::TorchScriptMaterial(const InputParameters & parameters)
   : Material(parameters),
     _prop_names(getParam<std::vector<std::string>>("prop_names")),
     _num_props(_prop_names.size()),
     _input_names(getParam<std::vector<PostprocessorName>>("input_names")),
     _num_inputs(_input_names.size()),
     _torch_script_userobject(getUserObject<TorchScriptUserObject>("torch_script_userobject")),
-    _input_tensor(torch::zeros({1, _num_inputs}, torch::TensorOptions().dtype(torch::kFloat64).device(_app.getLibtorchDevice())))
+    _input_tensor(torch::zeros(
+        {1, _num_inputs},
+        torch::TensorOptions().dtype(torch::kFloat64).device(_app.getLibtorchDevice())))
 {
   if (!_num_props)
     paramError("prop_names", "Must declare at least one property!");
@@ -64,16 +69,19 @@ TorchScriptMaterial::computeQpProperties()
 void
 TorchScriptMaterial::computeQpValues()
 {
-  auto input_accessor = _input_tensor.accessor<Real,2>();
+  auto input_accessor = _input_tensor.accessor<Real, 2>();
   for (unsigned int input_i = 0; input_i < _num_inputs; ++input_i)
     input_accessor[0][input_i] = (*_module_inputs[input_i]);
 
   const auto output = _torch_script_userobject.evaluate(_input_tensor);
   if (_num_props != output.numel())
-    mooseError("The tensor needs to be the same length (right now ",output.numel(),
-             ") as the number of properties (right now ",_num_props,")!");
+    mooseError("The tensor needs to be the same length (right now ",
+               output.numel(),
+               ") as the number of properties (right now ",
+               _num_props,
+               ")!");
 
-  const auto output_accessor = output.accessor<Real,2>();
+  const auto output_accessor = output.accessor<Real, 2>();
   for (unsigned int prop_i = 0; prop_i < _num_props; ++prop_i)
     (*_properties[prop_i])[_qp] = output_accessor[0][prop_i];
 }
--- framework/src/libtorch/userobjects/TorchScriptUserObject.C
+++ framework/src/libtorch/userobjects/TorchScriptUserObject.C
@@ -7,6 +7,8 @@
 //* Licensed under LGPL 2.1, please see LICENSE for details
 //* https://www.gnu.org/licenses/lgpl-2.1.html
 
+#ifdef LIBTORCH_ENABLED
+
 #include "TorchScriptUserObject.h"
 
 registerMooseObject("MooseApp", TorchScriptUserObject);
@@ -16,32 +18,40 @@ TorchScriptUserObject::validParams()
 {
   InputParameters params = GeneralUserObject::validParams();
   params.addClassDescription("User-facing object which loads a torch script module.");
-  params.addRequiredParam<std::string>("filename", "The file name which contains the torch script module.");
+  params.addRequiredParam<std::string>("filename",
+                                       "The file name which contains the torch script module.");
   params.declareControllable("filename");
-  params.addParam<bool>("load_during_construction", false, "If we want to load this neural network while we are constructing this object.");
+  params.addParam<bool>(
+      "load_during_construction",
+      false,
+      "If we want to load this neural network while we are constructing this object.");
   return params;
 }
 
 TorchScriptUserObject::TorchScriptUserObject(const InputParameters & parameters)
   : GeneralUserObject(parameters),
-  _filename(getParam<std::string>("filename")),
-  _torchscript_module(getParam<bool>("load_during_construction") ? std::make_shared<Moose::TorchScriptModule>(_filename) : nullptr)
+    _filename(getParam<std::string>("filename")),
+    _torchscript_module(getParam<bool>("load_during_construction")
+                            ? std::make_shared<Moose::TorchScriptModule>(_filename)
+                            : nullptr)
 {
 }
 
-void TorchScriptUserObject::execute()
+void
+TorchScriptUserObject::execute()
 {
   // We load when the user executes this user object
   _torchscript_module = std::make_shared<Moose::TorchScriptModule>(_filename);
 }
 
-torch::Tensor TorchScriptUserObject::evaluate(torch::Tensor & input) const
+torch::Tensor
+TorchScriptUserObject::evaluate(torch::Tensor & input) const
 {
   if (!_torchscript_module)
-    mooseError("The torch script module has not been loaded yet, so it can't be evaluated! Make sure that the torch script module is loaded (execute_on flags) before calling this function!");
+    mooseError(
+        "The torch script module has not been loaded yet, so it can't be evaluated! Make sure that "
+        "the torch script module is loaded (execute_on flags) before calling this function!");
   return _torchscript_module->forward(input);
 }
 
-
-
-
+#endif
