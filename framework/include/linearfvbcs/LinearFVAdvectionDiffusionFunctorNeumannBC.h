0a52b85b95a40501401bda61e48057980606e0c6
Ramiro Freile <ramiro.freile@inl.gov>
2025-03-20 11:15:54 -0600

    Add boolean to identify whether the BC requires a value extrapolation or a gradient extrapolation. Fixes issues with LinearFVDiffusion::computeBoundaryRHSContribution(), where for non-orthogonal meshes with Neumann BC an extra contribution was added that resulted in wrong results.

--- framework/include/linearfvbcs/LinearFVAdvectionDiffusionFunctorNeumannBC.h
+++ framework/include/linearfvbcs/LinearFVAdvectionDiffusionFunctorNeumannBC.h
@@ -1,5 +1,5 @@
 //* This file is part of the MOOSE framework
-//* https://mooseframework.inl.gov
+//* https://www.mooseframework.org
 //*
 //* All rights reserved, see COPYRIGHT for full restrictions
 //* https://github.com/idaholab/moose/blob/master/COPYRIGHT
@@ -15,14 +15,14 @@
  * Class implementing a Dirichlet boundary condition for linear finite
  * volume variables. This is only applicable for advection-diffusion problems.
  */
-class LinearFVAdvectionDiffusionFunctorDirichletBC : public LinearFVAdvectionDiffusionBC
+class LinearFVAdvectionDiffusionFunctorNeumannBC : public LinearFVAdvectionDiffusionBC
 {
 public:
   /**
    * Class constructor.
    * @param parameters The InputParameters for the object
    */
-  LinearFVAdvectionDiffusionFunctorDirichletBC(const InputParameters & parameters);
+  LinearFVAdvectionDiffusionFunctorNeumannBC(const InputParameters & parameters);
 
   static InputParameters validParams();
 
@@ -38,9 +38,12 @@ public:
 
   virtual Real computeBoundaryGradientRHSContribution() const override;
 
-  virtual bool use_boundary_value_extrapolation() const override { return false; }
+  virtual bool includesMaterialPropertyMultiplier() const override { return true; }
 
-  virtual bool use_boundary_gradient_extrapolation() const override { return true; }
+  virtual bool use_boundary_value_extrapolation() const override { return true; }
+
+  virtual bool use_boundary_gradient_extrapolation() const override { return false; }
+
 
 protected:
   /// The functor for this BC (can be variable, function, etc)
