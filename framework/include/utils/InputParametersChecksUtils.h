4c5d39592c37bccb84ab9d41e4f57398ade937c1
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2025-03-21 23:10:49 -0600

    Improve error message when looking to prevent overlaps within a single vector parameter refs #30168

--- framework/include/utils/InputParametersChecksUtils.h
+++ framework/include/utils/InputParametersChecksUtils.h
@@ -371,9 +371,13 @@ InputParametersChecksUtils<C>::checkVectorParamsNoOverlap(
       {
         auto copy_params = param_vec;
         copy_params.erase(std::find(copy_params.begin(), copy_params.end(), param));
-        forwardMooseError("Item '" + value + "' specified in vector parameter '" + param +
-                          "' is also present in one or more of the parameters '" +
-                          Moose::stringify(copy_params) + "', which is not allowed.");
+        if (copy_params.size())
+          forwardMooseError("Item '" + value + "' specified in vector parameter '" + param +
+                            "' is also present in one or more of the parameters '" +
+                            Moose::stringify(copy_params) + "', which is not allowed.");
+        else
+          forwardMooseError("Item '" + value + "' specified in vector parameter '" + param +
+                            "' is repeated, which is not allowed.");
       }
   }
 }
