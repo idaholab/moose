914a67ca5c3ce320c3b7aa42cc69a3252db9b53a
Peter German <grmnptr@gmail.com>
2025-03-28 09:24:22 -0600

    Add separator BC for velocity (#30185)

---
+++ modules/navier_stokes/src/fvbcs/INSFVVelocityHydraulicSeparatorBC.C
@@ -0,0 +1,19 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "INSFVVelocityHydraulicSeparatorBC.h"
+#include "InputParameters.h"
+
+InputParameters
+INSFVVelocityHydraulicSeparatorBC::validParams()
+{
+  return INSFVFluxBC::validParams();
+}
+
+INSFVVelocityHydraulicSeparatorBC::INSFVVelocityHydraulicSeparatorBC(const InputParameters & params) : INSFVFluxBC(params) {}
