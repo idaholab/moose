573d73f3756531c208ab419a7a5acdc5fc692eb1
Chris MacMackin <chris.macmackin@ukaea.uk>
2025-04-04 14:24:35 +0100

    getProperties() -> GetCoefficients()

--- framework/src/mfem/materials/MFEMMaterial.C
+++ framework/src/mfem/materials/MFEMMaterial.C
@@ -46,7 +46,7 @@ MFEMMaterial::pointFromMFEMVector(const mfem::Vector & vec)
 MFEMMaterial::MFEMMaterial(const InputParameters & parameters)
   : MFEMGeneralUserObject(parameters),
     _block_ids(getParam<std::vector<SubdomainName>>("block")),
-    _properties(getMFEMProblem().getProperties())
+    _properties(getMFEMProblem().getCoefficients())
 {
 }
 
