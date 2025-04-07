23d21ae2b8d6b7637c42a84ee9b2375d303a27e4
tanome <mauriciotano@gmail.com>
2025-04-07 15:53:19 -0600

    adding guillaume review Refs #29846

--- modules/navier_stokes/src/linearfvkernels/LinearFVTKEDSourceSink.C
+++ modules/navier_stokes/src/linearfvkernels/LinearFVTKEDSourceSink.C
@@ -23,8 +23,7 @@ LinearFVTKEDSourceSink::validParams()
   params.addRequiredParam<MooseFunctorName>("u", "The velocity in the x direction.");
   params.addParam<MooseFunctorName>("v", "The velocity in the y direction.");
   params.addParam<MooseFunctorName>("w", "The velocity in the z direction.");
-  params.addRequiredParam<MooseFunctorName>("k", "Coupled turbulent kinetic energy.");
-  params.deprecateParam("k", NS::TKE, "01/01/2025");
+  params.addRequiredParam<MooseFunctorName>(NS::TKE, "Coupled turbulent kinetic energy.");
   params.addRequiredParam<MooseFunctorName>(NS::density, "fluid density");
   params.addRequiredParam<MooseFunctorName>(NS::mu, "Dynamic viscosity.");
   params.addRequiredParam<MooseFunctorName>(NS::mu_t, "Turbulent viscosity.");
