7a6def4ddb4ba1bfd45b8a0ad815cc995a1b6d2e
Alex Lindsay <alexander.lindsay@inl.gov>
2025-04-16 22:00:44 -0600

    Allow doing field split on statically condensed systems
    Closes #30798

--- modules/navier_stokes/src/auxkernels/WallDistanceMixingLengthAux.C
+++ modules/navier_stokes/src/auxkernels/WallDistanceMixingLengthAux.C
@@ -19,8 +19,8 @@ WallDistanceMixingLengthAux::validParams()
       "Computes the turbulent mixing length by assuming that it is "
       "proportional to the distance from the nearest wall. The mixing"
       "length is capped at a distance proportional to inputted parameter delta.");
-  params.addParam<std::vector<BoundaryName>>(
-      "walls", {}, "Boundaries that correspond to solid walls.");
+  params.addRequiredParam<std::vector<BoundaryName>>("walls",
+                                                     "Boundaries that correspond to solid walls.");
   params.addParam<MooseFunctorName>("von_karman_const", 0.41, "");   // Von Karman constant
   params.addParam<MooseFunctorName>("von_karman_const_0", 0.09, ""); // Escudier' model parameter
   params.addParam<MooseFunctorName>(
@@ -41,13 +41,6 @@ WallDistanceMixingLengthAux::WallDistanceMixingLengthAux(const InputParameters &
   const MeshBase & mesh = _subproblem.mesh().getMesh();
   if (!mesh.is_replicated())
     mooseError("WallDistanceMixingLengthAux only supports replicated meshes");
-  if (!dynamic_cast<MooseVariableFV<Real> *>(&_var))
-    paramError("variable",
-               "'",
-               name(),
-               "' is currently programmed to use finite volume machinery, so make sure that '",
-               _var.name(),
-               "' is a finite volume variable.");
 }
 
 Real
@@ -73,22 +66,9 @@ WallDistanceMixingLengthAux::computeValue()
     // Loop over all boundary elements and find the distance to the closest one
     for (dof_id_type elem_id : bnd_elems)
     {
-      const Elem & elem{l_mesh.elem_ref(elem_id)};
+      const Elem & elem = l_mesh.elem_ref(elem_id);
       const auto side = _mesh.sideWithBoundaryID(&elem, bid);
-      const FaceInfo * fi = _mesh.faceInfo(&elem, side);
-      // It's possible that we are on an internal boundary
-      if (!fi)
-      {
-        const Elem * const neigh = elem.neighbor_ptr(side);
-        mooseAssert(
-            neigh,
-            "In WallDistanceMixingLengthAux, we could not find a face information object with elem "
-            "and side, and we are on an external boundary. This shouldn't happen.");
-        const auto neigh_side = neigh->which_neighbor_am_i(&elem);
-        fi = _mesh.faceInfo(neigh, neigh_side);
-        mooseAssert(fi, "We should have a face info for either the elem or neigh side");
-      }
-      Point bnd_pos = fi->faceCentroid();
+      const auto bnd_pos = elem.side_ptr(side)->vertex_average();
       const auto distance = bnd_pos - _q_point[_qp];
       const auto dist2 = distance * distance;
       mooseAssert(dist2 != 0, "This distance should never be 0");
