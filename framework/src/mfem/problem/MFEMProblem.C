aurora-multiphysics/platypus

Move MFEMEssentialBC storage into EquationSystem and remove BCMap

src/problem/MFEMProblem.C
@@ -112,7 +112,7 @@ MFEMProblem::addBoundaryCondition(const std::string & bc_name,
     mfem_bc->getBoundaries();
     if (getProblemData()._eqn_system)
     {
-      getProblemData()._eqn_system->AddBC(name, std::move(mfem_bc));
+      getProblemData()._eqn_system->AddEssentialBC(std::move(mfem_bc));
     }
     else
     {

idaholab/moose

framework/src/mfem/problem/MFEMProblem.C
@@ -114,7 +114,7 @@ MFEMProblem::addBoundaryCondition(const std::string & bc_name,
     mfem_bc->getBoundaries();
     if (getProblemData().eqn_system)
     {
-      getProblemData().eqn_system->AddBC(name, std::move(mfem_bc));
+      getProblemData().eqn_system->AddEssentialBC(std::move(mfem_bc));
     }
     else
     {