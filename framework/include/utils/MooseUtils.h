be609c8217f547c6d8ee9beb09334a7ba0586c3c
Logan Harbour <loganhharbour@gmail.com>
2025-04-22 11:50:46 -0600

    Separate out string utilities for independent compiles (#26470)
    Co-authored-by: Daniel Schwen <daniel.schwen@inl.gov>

--- framework/include/utils/MooseUtils.h
+++ framework/include/utils/MooseUtils.h
@@ -18,6 +18,7 @@
 #include "Moose.h"
 #include "ExecutablePath.h"
 #include "ConsoleUtils.h"
+#include "MooseStringUtils.h"
 
 #include "libmesh/compare_types.h"
 #include "libmesh/bounding_box.h"
@@ -112,11 +113,6 @@ int levenshteinDist(const std::string & s1, const std::string & s2);
  * passed in parameter is modified in place
  */
 void escape(std::string & str);
-
-/**
- * Standard scripting language trim function
- */
-std::string trim(const std::string & str, const std::string & white_space = " \t\n\v\f\r");
 
 /**
  * Removes additional whitespace from a string
@@ -802,59 +798,6 @@ expandAllMatches(const std::vector<T> & candidates, std::vector<T> & patterns)
 }
 
 /**
- * This function will split the passed in string on a set of delimiters appending the substrings
- * to the passed in vector.  The delimiters default to "/" but may be supplied as well.  In
- * addition if min_len is supplied, the minimum token length will be >= than the supplied
- * value. T should be std::string or a MOOSE derived string class.
- */
-template <typename T>
-void
-tokenize(const std::string & str,
-         std::vector<T> & elements,
-         unsigned int min_len = 1,
-         const std::string & delims = "/")
-{
-  elements.clear();
-
-  std::string::size_type last_pos = str.find_first_not_of(delims, 0);
-  std::string::size_type pos = str.find_first_of(delims, std::min(last_pos + min_len, str.size()));
-
-  while (last_pos != std::string::npos)
-  {
-    elements.push_back(str.substr(last_pos, pos - last_pos));
-    // skip delims between tokens
-    last_pos = str.find_first_not_of(delims, pos);
-    if (last_pos == std::string::npos)
-      break;
-    pos = str.find_first_of(delims, std::min(last_pos + min_len, str.size()));
-  }
-}
-
-/**
- *  tokenizeAndConvert splits a string using delimiter and then converts to type T.
- *  If the conversion fails tokenizeAndConvert returns false, otherwise true.
- */
-template <typename T>
-bool
-tokenizeAndConvert(const std::string & str,
-                   std::vector<T> & tokenized_vector,
-                   const std::string & delimiter = " \t\n\v\f\r")
-{
-  std::vector<std::string> tokens;
-  MooseUtils::tokenize(str, tokens, 1, delimiter);
-  tokenized_vector.resize(tokens.size());
-  for (unsigned int j = 0; j < tokens.size(); ++j)
-  {
-    std::stringstream ss(trim(tokens[j]));
-    // we have to make sure that the conversion succeeded _and_ that the string
-    // was fully read to avoid situations like [conversion to Real] 3.0abc to work
-    if ((ss >> tokenized_vector[j]).fail() || !ss.eof())
-      return false;
-  }
-  return true;
-}
-
-/**
  * convert takes a string representation of a number type and converts it to the number.
  * This method is here to get around deficiencies in the STL stoi and stod methods where they
  * might successfully convert part of a string to a number when we'd like to throw an error.
@@ -913,18 +856,6 @@ void createSymlink(const std::string & target, const std::string & link);
  * Remove a symbolic link, if the given filename is a link.
  */
 void clearSymlink(const std::string & link);
-
-/**
- * Convert supplied string to upper case.
- * @params name The string to convert upper case.
- */
-std::string toUpper(const std::string & name);
-
-/**
- * Convert supplied string to lower case.
- * @params name The string to convert upper case.
- */
-std::string toLower(const std::string & name);
 
 /**
  * Returns a container that contains the content of second passed in container
