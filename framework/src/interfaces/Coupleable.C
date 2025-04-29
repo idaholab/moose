407344dd23adfeeb677f4e53c137b3394f20de6e
Matthew Falcone <mapfalcone@gmail.com>
2025-04-29 09:15:14 +0100

    Return nodalValueOldArray in coupledVectorValueOld

--- framework/src/interfaces/Coupleable.C
+++ framework/src/interfaces/Coupleable.C
@@ -1070,6 +1070,8 @@ Coupleable::coupledVectorValueOld(const std::string & var_name, unsigned int com
     return *getDefaultVectorValue(var_name);
   checkFuncType(var_name, VarType::Ignore, FuncAge::Old);
 
+  if (_c_nodal)
+    return (_c_is_implicit) ? var->nodalValueOldArray() : var->nodalValueOlderArray();
   if (!_coupleable_neighbor)
     return (_c_is_implicit) ? var->slnOld() : var->slnOlder();
   return (_c_is_implicit) ? var->slnOldNeighbor() : var->slnOlderNeighbor();
