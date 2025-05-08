92a3b23e727d0a423057b0130c0597b10932cf9c
Alex Lindsay <alexlindsay239@gmail.com>
2025-05-08 16:30:23 -0600

    Allow attaching a constant nullspace

--- framework/src/systems/NonlinearSystemBase.C
+++ framework/src/systems/NonlinearSystemBase.C
@@ -1899,6 +1899,9 @@ NonlinearSystemBase::computeResidualAndJacobianInternal(const std::set<TagID> & 
                                       PETSC_TRUE));
     }
   }
+  if (auto nullsp = _fe_problem.constantNullspace())
+    LibmeshPetscCall(MatSetNullSpace(
+        cast_ref<PetscMatrixBase<Number> &>(getMatrix(systemMatrixTag())).mat(), nullsp));
 
   residualSetup();
 
@@ -2746,6 +2749,9 @@ NonlinearSystemBase::computeJacobianInternal(const std::set<TagID> & tags)
                                       PETSC_TRUE));
     }
   }
+  if (auto nullsp = _fe_problem.constantNullspace())
+    LibmeshPetscCall(MatSetNullSpace(
+        cast_ref<PetscMatrixBase<Number> &>(getMatrix(systemMatrixTag())).mat(), nullsp));
 
   jacobianSetup();
 
