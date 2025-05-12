aurora-multiphysics/platypus

Rename MFEMScalarBoundaryIntegratedBC to MFEMScalarFunctorBoundaryIntegratedBC

src/bcs/MFEMScalarFunctorBoundaryIntegratedBC.C
@@ -1,37 +1,38 @@
-#include "MFEMScalarBoundaryIntegratedBC.h"
+#include "MFEMScalarFunctorBoundaryIntegratedBC.h"
 
-registerMooseObject("PlatypusApp", MFEMScalarBoundaryIntegratedBC);
+registerMooseObject("PlatypusApp", MFEMScalarFunctorBoundaryIntegratedBC);
 
 InputParameters
-MFEMScalarBoundaryIntegratedBC::validParams()
+MFEMScalarFunctorBoundaryIntegratedBC::validParams()
 {
   InputParameters params = MFEMIntegratedBC::validParams();
   params.addClassDescription("Adds the domain integrator to an MFEM problem for the linear form "
                              "$(f, v)_\\Omega$ "
                              "arising from the weak form of the forcing term $f$.");
-  params.addRequiredParam<std::string>(
+  params.addRequiredParam<platypus::MFEMScalarCoefficientName>(
       "coefficient", "The scalar MFEM coefficient which will be used in the integrated BC.");
   return params;
 }
 
-MFEMScalarBoundaryIntegratedBC::MFEMScalarBoundaryIntegratedBC(const InputParameters & parameters)
+MFEMScalarFunctorBoundaryIntegratedBC::MFEMScalarFunctorBoundaryIntegratedBC(
+    const InputParameters & parameters)
   : MFEMIntegratedBC(parameters),
-    _coef_name(getParam<std::string>("coefficient")),
-    _coef(getMFEMProblem().getProperties().getScalarProperty(_coef_name))
+    _coef_name(getParam<platypus::MFEMScalarCoefficientName>("coefficient")),
+    _coef(getScalarProperty(_coef_name))
 {
 }
 
 // Create a new MFEM integrator to apply to the RHS of the weak form. Ownership managed by the
 // caller.
 mfem::LinearFormIntegrator *
-MFEMScalarBoundaryIntegratedBC::createLFIntegrator()
+MFEMScalarFunctorBoundaryIntegratedBC::createLFIntegrator()
 {
   return new mfem::BoundaryLFIntegrator(_coef);
 }
 
 // Create a new MFEM integrator to apply to LHS of the weak form. Ownership managed by the caller.
 mfem::BilinearFormIntegrator *
-MFEMScalarBoundaryIntegratedBC::createBFIntegrator()
+MFEMScalarFunctorBoundaryIntegratedBC::createBFIntegrator()
 {
   return nullptr;
 }

idaholab/moose

framework/src/mfem/bcs/MFEMScalarFunctorBoundaryIntegratedBC.C
@@ -1,40 +1,40 @@
 #ifdef MFEM_ENABLED
 
-#include "MFEMScalarBoundaryIntegratedBC.h"
-#include "MFEMProblem.h"
+#include "MFEMScalarFunctorBoundaryIntegratedBC.h"
 
-registerMooseObject("MooseApp", MFEMScalarBoundaryIntegratedBC);
-
+registerMooseObject("MooseApp", MFEMScalarFunctorBoundaryIntegratedBC);
+=======
 InputParameters
-MFEMScalarBoundaryIntegratedBC::validParams()
+MFEMScalarFunctorBoundaryIntegratedBC::validParams()
 {
   InputParameters params = MFEMIntegratedBC::validParams();
   params.addClassDescription("Adds the domain integrator to an MFEM problem for the linear form "
                              "$(f, v)_\\Omega$ "
                              "arising from the weak form of the forcing term $f$.");
-  params.addRequiredParam<std::string>(
+  params.addRequiredParam<MFEMScalarCoefficientName>(
       "coefficient", "The scalar MFEM coefficient which will be used in the integrated BC.");
   return params;
 }
 
-MFEMScalarBoundaryIntegratedBC::MFEMScalarBoundaryIntegratedBC(const InputParameters & parameters)
+MFEMScalarFunctorBoundaryIntegratedBC::MFEMScalarFunctorBoundaryIntegratedBC(
+    const InputParameters & parameters)
   : MFEMIntegratedBC(parameters),
-    _coef_name(getParam<std::string>("coefficient")),
-    _coef(getMFEMProblem().getProperties().getScalarProperty(_coef_name))
+    _coef_name(getParam<MFEMScalarCoefficientName>("coefficient")),
+    _coef(getScalarProperty(_coef_name))
 {
 }
 
 // Create a new MFEM integrator to apply to the RHS of the weak form. Ownership managed by the
 // caller.
 mfem::LinearFormIntegrator *
-MFEMScalarBoundaryIntegratedBC::createLFIntegrator()
+MFEMScalarFunctorBoundaryIntegratedBC::createLFIntegrator()
 {
   return new mfem::BoundaryLFIntegrator(_coef);
 }
 
 // Create a new MFEM integrator to apply to LHS of the weak form. Ownership managed by the caller.
 mfem::BilinearFormIntegrator *
-MFEMScalarBoundaryIntegratedBC::createBFIntegrator()
+MFEMScalarFunctorBoundaryIntegratedBC::createBFIntegrator()
 {
   return nullptr;
 }