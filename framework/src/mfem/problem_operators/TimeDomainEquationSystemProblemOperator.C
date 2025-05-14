d186505298c9be1fe3951bda3c7ce6168074fb7c
“Henrique <henrique.bergallo-rocha@ukaea.uk>
2025-05-14 11:40:15 +0100

    Add error check for LOR and single-variable systems

--- framework/src/mfem/problem_operators/TimeDomainEquationSystemProblemOperator.C
+++ framework/src/mfem/problem_operators/TimeDomainEquationSystemProblemOperator.C
@@ -38,6 +38,9 @@ TimeDomainEquationSystemProblemOperator::ImplicitSolve(const double dt,
   _problem.coefficients.setTime(GetTime());
   BuildEquationSystemOperator(dt);
 
+  if (_problem.jacobian_solver->getParam<bool>("low_order_refined") && _equation_system->_test_var_names.size() > 1)
+    mooseError("LOR solve is only supported for single-variable systems");
+
   _problem.jacobian_solver->updateSolver(
       *_equation_system->_blfs.Get(_equation_system->_test_var_names.at(0)),
       _equation_system->_ess_tdof_lists.at(0));
