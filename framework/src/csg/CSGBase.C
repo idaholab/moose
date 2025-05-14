2ef3a4b6dcb7cfdf5b75c958478d511a907c12c1
Kalin Kiesling <kkiesling@anl.gov>
2025-05-14 10:28:07 -0500

    more robust handling of joining CSGBases - various options for how to join (or not) root universes

--- framework/src/csg/CSGBase.C
+++ framework/src/csg/CSGBase.C
@@ -20,26 +20,47 @@ CSGBase::CSGBase()
 CSGBase::~CSGBase() {}
 
 std::shared_ptr<CSGCell>
-CSGBase::createCell(const std::string name, const std::string mat_name, const CSGRegion & region)
+CSGBase::createCell(const std::string name,
+                    const std::string mat_name,
+                    const CSGRegion & region,
+                    std::shared_ptr<CSGUniverse> add_to_univ)
 {
   auto cell = _cell_list.addMaterialCell(name, mat_name, region);
-  getRootUniverse()->addCell(cell);
+  if (add_to_univ)
+    add_to_univ->addCell(cell);
+  else
+    getRootUniverse()->addCell(cell);
   return cell;
 }
 
 std::shared_ptr<CSGCell>
-CSGBase::createCell(const std::string name, const CSGRegion & region)
+CSGBase::createCell(const std::string name,
+                    const CSGRegion & region,
+                    const std::shared_ptr<CSGUniverse> add_to_univ)
 {
   auto cell = _cell_list.addVoidCell(name, region);
-  getRootUniverse()->addCell(cell);
+  if (add_to_univ)
+    add_to_univ->addCell(cell);
+  else
+    getRootUniverse()->addCell(cell);
   return cell;
 }
 
 std::shared_ptr<CSGCell>
-CSGBase::createCell(const std::string name, const CSGUniverse & univ, const CSGRegion & region)
+CSGBase::createCell(const std::string name,
+                    const std::shared_ptr<CSGUniverse> fill_univ,
+                    const CSGRegion & region,
+                    const std::shared_ptr<CSGUniverse> add_to_univ)
 {
-  auto cell = _cell_list.addUniverseCell(name, univ, region);
-  getRootUniverse()->addCell(cell);
+  if (fill_univ == add_to_univ)
+    mooseError("Cell " + name +
+               " cannot be filled with the same universe to which it is being added.");
+
+  auto cell = _cell_list.addUniverseCell(name, fill_univ, region);
+  if (add_to_univ)
+    add_to_univ->addCell(cell);
+  else
+    getRootUniverse()->addCell(cell);
   return cell;
 }
 
@@ -64,21 +85,66 @@ CSGBase::joinCellList(CSGCellList & cell_list)
 
 void
 CSGBase::joinUniverseList(CSGUniverseList & univ_list)
-{
+{ // case 1: incoming root is joined into existing root; no new universes are created
   auto all_univs = univ_list.getAllUniverses();
+  auto root = getRootUniverse(); // this root universe
   for (auto u : all_univs)
   {
     if (u.second->isRoot())
     {
       // add existing cells to current root instead of creating new universe
-      auto root = getRootUniverse();
       auto all_cells = u.second->getAllCells();
       for (auto cell : all_cells)
-      {
         root->addCell(cell);
-      }
     }
-    else
+    else // unique non-root universe to add to list
+      _universe_list.addUniverse(u);
+  }
+}
+
+void
+CSGBase::joinUniverseList(CSGUniverseList & univ_list, std::string new_root_name_incoming)
+{ // case 2: incoming root is turned into new universe and existing root remains root
+
+  // add incoming universes to current Base
+  auto all_univs = univ_list.getAllUniverses();
+  for (auto u : all_univs)
+  {
+    if (u.second->isRoot())
+    {
+      // create new universe from incoming root universe
+      auto all_cells = u.second->getAllCells();
+      auto new_incoming_univ = createUniverse(new_root_name_incoming, all_cells);
+    }
+    else // unique non-root universe to add to list
+      _universe_list.addUniverse(u);
+  }
+}
+
+void
+CSGBase::joinUniverseList(CSGUniverseList & univ_list,
+                          std::string new_root_name_base,
+                          std::string new_root_name_incoming)
+{ // case 3: each root universe becomes a new universe and a new root is created
+
+  // make a new universe from the existing root universe
+  auto root = getRootUniverse();
+  auto root_cells = root->getAllCells();
+  auto new_existing_univ = createUniverse(new_root_name_base, root_cells);
+  for (auto c : root_cells)
+    root->removeCell(c);
+
+  // add incoming universes to current Base
+  auto all_univs = univ_list.getAllUniverses();
+  for (auto u : all_univs)
+  {
+    if (u.second->isRoot())
+    {
+      // create new universe from incoming root universe
+      auto all_cells = u.second->getAllCells();
+      auto new_incoming_univ = createUniverse(new_root_name_incoming, all_cells);
+    }
+    else // unique non-root universe to add to list
       _universe_list.addUniverse(u);
   }
 }
