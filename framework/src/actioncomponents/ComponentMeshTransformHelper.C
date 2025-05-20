c107df0b5e87e90287d9f8501d82b5c1a0cbac54
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2025-05-20 07:45:48 -0600

    Add a helper for orienting and placing components Make the Cylinder component inherit it Adjust test for new, better, order of operations closes #30690

---
+++ framework/src/actioncomponents/ComponentMeshTransformHelper.C
@@ -0,0 +1,87 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "ComponentMeshTransformHelper.h"
+#include "RotationMatrix.h"
+#include "Factory.h"
+#include "MooseApp.h"
+
+InputParameters
+ComponentMeshTransformHelper::validParams()
+{
+  auto params = ActionComponent::validParams();
+
+  // If your component is 0D, you should make these two parameters private!
+  params.addParam<RealVectorValue>(
+      "rotation",
+      "Rotation angles (XZX convention, in degrees NOT radians) to rotate the "
+      "mesh of this component with. Note that this rotation "
+      "is applied before the translation.");
+  params.addParam<RealVectorValue>(
+      "direction",
+      "Direction to orient the component mesh with, assuming it is initially oriented along the "
+      "X-axis (1, 0, 0). Note that this rotation is applied before the translation.");
+  // Position is widely applicable to components
+  params.addParam<Point>(
+      "position", Point(0., 0., 0.), "Vector to translate the mesh of this component by.");
+
+  params.addParamNamesToGroup("rotation direction position",
+                              "Position and orientation of the component");
+  return params;
+}
+
+ComponentMeshTransformHelper::ComponentMeshTransformHelper(const InputParameters & params)
+  : ActionComponent(params),
+    _rotation(isParamValid("rotation") ? getParam<RealVectorValue>("rotation")
+                                       : RealVectorValue(0, 0, 0)),
+    _direction(isParamValid("direction") ? getParam<RealVectorValue>("direction")
+                                         : RealVectorValue(0, 0, 1)),
+    _translation(getParam<Point>("position"))
+{
+  addRequiredTask("add_mesh_generator");
+  checkSecondParamNotSetIfFirstOneSet("rotation", "direction");
+}
+
+void
+ComponentMeshTransformHelper::addMeshGenerators()
+{
+  // Rotate the mesh as desired
+  if (isParamValid("rotation") || isParamValid("direction"))
+  {
+    InputParameters params = _factory.getValidParams("TransformGenerator");
+    params.set<MeshGeneratorName>("input") = _mg_names.back();
+    params.set<MooseEnum>("transform") = "ROTATE";
+    if (isParamValid("rotation"))
+      params.set<RealVectorValue>("vector_value") = _rotation;
+    else
+    {
+      const auto rotation_matrix =
+          RotationMatrix::rotVec1ToVec2<false>(RealVectorValue(1, 0, 0), _direction);
+      RealVectorValue angles;
+      angles(0) = std::atan2(rotation_matrix(1, 0), rotation_matrix(0, 0));
+      angles(1) = std::asin(-rotation_matrix(2, 0));
+      angles(2) = std::atan2(rotation_matrix(2, 1), rotation_matrix(2, 2));
+      params.set<RealVectorValue>("vector_value") = angles / M_PI_2 * 90;
+    }
+    _app.getMeshGeneratorSystem().addMeshGenerator(
+        "TransformGenerator", name() + "_rotated", params);
+    _mg_names.push_back(name() + "_rotated");
+  }
+
+  // Add a translation
+  {
+    InputParameters params = _factory.getValidParams("TransformGenerator");
+    params.set<MeshGeneratorName>("input") = _mg_names.back();
+    params.set<MooseEnum>("transform") = "TRANSLATE";
+    params.set<RealVectorValue>("vector_value") = getParam<Point>("position");
+    _app.getMeshGeneratorSystem().addMeshGenerator(
+        "TransformGenerator", name() + "_translated", params);
+    _mg_names.push_back(name() + "_translated");
+  }
+}
