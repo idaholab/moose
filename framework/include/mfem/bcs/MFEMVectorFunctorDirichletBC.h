18f851f26c27029f7e6c5631e7106754becbcfff
Nuno Nobre <nuno.nobre@stfc.ac.uk>
2025-06-03 12:32:51 +0100

    Fix missing BC class descriptions

--- framework/include/mfem/bcs/MFEMVectorFunctorDirichletBC.h
+++ framework/include/mfem/bcs/MFEMVectorFunctorDirichletBC.h
@@ -7,6 +7,7 @@ class MFEMVectorFunctorDirichletBC : public MFEMVectorFunctorDirichletBCBase
 {
 
 public:
+  static InputParameters validParams();
   MFEMVectorFunctorDirichletBC(const InputParameters & parameters);
   ~MFEMVectorFunctorDirichletBC() override = default;
   void ApplyBC(mfem::GridFunction & gridfunc, mfem::Mesh & mesh) override;
