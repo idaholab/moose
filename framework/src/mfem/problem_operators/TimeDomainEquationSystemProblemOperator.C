ef5faad0ce0f8448dcf1dd4343fff96450152d7e
Sean Baccas <sean.baccas@ukaea.uk>
2025-06-04 13:17:02 +0100

    Copy over changes from platypus working branch

--- framework/src/mfem/problem_operators/TimeDomainEquationSystemProblemOperator.C
+++ framework/src/mfem/problem_operators/TimeDomainEquationSystemProblemOperator.C
@@ -76,7 +76,7 @@ void
 TimeDomainEquationSystemProblemOperator::BuildEquationSystemOperator(double dt)
 {
   GetEquationSystem()->SetTimeStep(dt);
-  GetEquationSystem()->UpdateEquationSystem();
+  GetEquationSystem()->RebuildEquationSystem();
   GetEquationSystem()->BuildJacobian(_true_x, _true_rhs);
 }
 
