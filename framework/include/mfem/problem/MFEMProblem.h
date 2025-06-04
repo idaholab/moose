e9a7541bb603f66c0409bf7a92d0b4733920b3f2
Sean Baccas <sean.baccas@ukaea.uk>
2025-06-04 13:17:02 +0100

    Copy over changes from platypus working branch

--- framework/include/mfem/problem/MFEMProblem.h
+++ framework/include/mfem/problem/MFEMProblem.h
@@ -152,6 +152,10 @@ public:
                              const std::string & name,
                              InputParameters & parameters);
 
+  void addIndicator(const std::string & type,
+                    const std::string & name,
+                    InputParameters & parameters) override;
+
   /**
    * Method called in AddMFEMSolverAction which will create the solver.
    */
