5cf43ec13f0625662a250e9414325b86990b0ed3
Alex Lindsay <alexlindsay239@gmail.com>
2025-06-12 11:02:55 -0600

    Add copyright headers to MOOSE-MFEM sources
    Closes #30726

--- framework/src/mfem/problem/MFEMProblem.C
+++ framework/src/mfem/problem/MFEMProblem.C
@@ -1,3 +1,12 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
 #ifdef MFEM_ENABLED
 
 #include "MFEMProblem.h"
