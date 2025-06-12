c24295e156aef7cb3a9c657da1caa9a48e483f79
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2025-06-12 13:42:09 -0600

    Add missing header for unity build  ref #30729

--- framework/src/userobjects/SolutionUserObjectBase.C
+++ framework/src/userobjects/SolutionUserObjectBase.C
@@ -29,6 +29,7 @@
 #include "libmesh/exodusII_io.h"
 #include "libmesh/exodusII_io_helper.h"
 #include "libmesh/enum_xdr_mode.h"
+#include "libmesh/string_to_enum.h"
 
 InputParameters
 SolutionUserObjectBase::validParams()
