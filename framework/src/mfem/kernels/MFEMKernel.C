0b3159c185910853e8a653ce7005f09d9d3969fd
Alexander Blair <alexander.blair@ukaea.uk>
2025-06-13 17:12:20 +0100

    Revert change to fetched mesh in MFEMKernel, since domain attributes are always transferred from parent to submesh. Refs #30735

--- framework/src/mfem/kernels/MFEMKernel.C
+++ framework/src/mfem/kernels/MFEMKernel.C
@@ -31,11 +31,7 @@ MFEMKernel::MFEMKernel(const InputParameters & parameters)
   {
     _subdomain_attributes[i] = std::stoi(_subdomain_names[i]);
   }
-  mfem::ParMesh & mesh(*getMFEMProblem()
-                            .getProblemData()
-                            .gridfunctions.GetRef(_test_var_name)
-                            .ParFESpace()
-                            ->GetParMesh());
+  mfem::ParMesh & mesh(getMFEMProblem().mesh().getMFEMParMesh());
   mfem::common::AttrToMarker(mesh.attributes.Max(), _subdomain_attributes, _subdomain_markers);
 }
 
