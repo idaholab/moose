889b969f8250ccf5cd4c316a4c0fea0389bdec0d
Chris MacMackin <chris.macmackin@ukaea.uk>
2025-06-13 10:17:35 +0100

    Fix oddities in vector Dirichlet BC

--- framework/include/mfem/bcs/MFEMVectorDirichletBC.h
+++ framework/include/mfem/bcs/MFEMVectorDirichletBC.h
@@ -1,12 +1,13 @@
 #ifdef MFEM_ENABLED
 
 #pragma once
+
 #include "MFEMVectorDirichletBCBase.h"
 
 class MFEMVectorDirichletBC : public MFEMVectorDirichletBCBase
 {
-
 public:
+  static InputParameters validParams();
   MFEMVectorDirichletBC(const InputParameters & parameters);
   ~MFEMVectorDirichletBC() override = default;
   void ApplyBC(mfem::GridFunction & gridfunc, mfem::Mesh & mesh) override;
