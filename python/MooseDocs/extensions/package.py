e2896df303a74f8db4a46d9a67392d481372a25e
Jason M Miller <Jason.Miller@inl.gov>
2025-06-16 07:01:17 -0600

    Covert all package_config.yml to strings
    Due to: https://github.com/idaholab/moose/pull/28114, bump several
    packages version strings.
    
    Turns out when you use yaml.safe_load(file), it will perform type
    detection and set that type, thus dropping 3.10 to 3.1. Therefore
    convert all entries in package_config.yml to strings, as thats all
    we need them to be for documentation.
    
    Closes #30753

--- python/MooseDocs/extensions/package.py
+++ python/MooseDocs/extensions/package.py
@@ -13,7 +13,6 @@ import re
 from mooseutils.yaml_load import yaml_load
 
 import MooseDocs
-from ..common import exceptions
 from ..tree import tokens
 from . import core, command
 
@@ -76,7 +75,7 @@ class PackageCodeReplace(command.CommandComponent):
         content = info['inline'] if 'inline' in info else info['block']
         content = re.sub(r'__(?P<package>[A-Z][A-Z_]+)__', self._subFunction, content,
                          flags=re.UNICODE)
-        core.Code(parent, style="max-height:{};".format(settings['max-height']),
+        core.Code(parent, style=f'max-height:{settings["max-height"]}',
                   language=settings['language'], content=content)
         return parent
 
