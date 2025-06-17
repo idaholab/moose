ff33578f2bc777e7a8c7cb8d03c3352af97cdb18
Chris MacMackin <chris.macmackin@ukaea.uk>
2025-06-17 17:38:07 +0100

    Added class description

--- framework/include/mfem/bcs/MFEMVectorTangentialDirichletBC.h
+++ framework/include/mfem/bcs/MFEMVectorTangentialDirichletBC.h
@@ -7,6 +7,7 @@
 class MFEMVectorTangentialDirichletBC : public MFEMVectorDirichletBCBase
 {
 public:
+  static InputParameters validParams();
   MFEMVectorTangentialDirichletBC(const InputParameters & parameters);
   ~MFEMVectorTangentialDirichletBC() override = default;
   void ApplyBC(mfem::GridFunction & gridfunc, mfem::Mesh & mesh) override;
