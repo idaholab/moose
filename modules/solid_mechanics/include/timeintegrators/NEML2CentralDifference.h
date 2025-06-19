db604af3b7a6ecb482deb39c869e66f894272409
Gary Hu <thu@anl.gov>
2025-06-19 08:20:11 -0500

    NEML2-MOOSE kernels
    - NEML2Assembly: gather JxW, coord, etc.
    - NEML2FEInterpolation: gather dofmap, shape function values/gradiantes for each coupled variable
    - NEML2Kernel: define batched operations
    - NEML2PreKernel: inherited from NEML2Kernel, for operations _before_ the constitutive update
    - NEML2PostKernel: inherited from NEML2Kernel, for operations _after_ the constitutive update
    
    Examples:
    
    - NEML2SmallStrain: inherited from NEML2PreKernel, coupled to displacement gradients, calculates the small strain tensor
    - NEML2StressDivergence: inherited from NEML2PostKernel, retrieves stress from the NEML2ModelExecutor and uses it to calculate the residual
    
    Also add corresponding documentation and tests. Retire BatchMaterial.
    
    close #30850

---
+++ modules/solid_mechanics/include/timeintegrators/NEML2CentralDifference.h
@@ -0,0 +1,48 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifdef NEML2_ENABLED
+
+#pragma once
+
+#include "ExplicitMixedOrder.h"
+#include "NEML2Assembly.h"
+#include "NEML2FEInterpolation.h"
+
+class NEML2CentralDifference : public ExplicitMixedOrder
+{
+public:
+  static InputParameters validParams();
+
+  NEML2CentralDifference(const InputParameters & parameters);
+
+  void initialSetup() override;
+  void postSolve() override;
+
+protected:
+  void evaluateRHSResidual() override;
+
+  /// The assembly object with cached assembly information
+  const NEML2Assembly * _neml2_assembly;
+
+  /// The FE interface for getting variable values/gradients interpolated onto the finite element space
+  const NEML2FEInterpolation * _fe;
+
+private:
+  /// Empty element vector to help zero out the algebraic range
+  std::vector<const Elem *> _no_elem = {};
+
+  /// Empty node vector to help zero out the algebraic range
+  std::vector<const Node *> _no_node = {};
+
+  /// Empty boundary node vector to help zero out the algebraic range
+  std::vector<const BndNode *> _no_bnd_node = {};
+};
+
+#endif // NEML2_ENABLED
