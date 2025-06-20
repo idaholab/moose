6cd60f3715ef82fbc27f9161da39059297294e52
“Henrique <henrique.bergallo-rocha@ukaea.uk>
2025-06-20 11:21:20 +0100

    Change moose warning for info
    Linting

--- framework/src/mfem/fespaces/MFEMScalarFESpace.C
+++ framework/src/mfem/fespaces/MFEMScalarFESpace.C
@@ -42,10 +42,10 @@ MFEMScalarFESpace::getFECName() const
   else if (_fec_type == "L2")
   {
     if (basis != "g")
-      mooseWarning("L2 finite element space only supports GaussLegendre basis. "
-                   "Ignoring " +
-                   getParam<std::string>("basis") +
-                   " basis choice and using GaussLegendre instead.\n");
+      mooseInfo("L2 finite element space only supports GaussLegendre basis. "
+                "Ignoring " +
+                getParam<std::string>("basis") +
+                " basis choice and using GaussLegendre instead.\n");
     basis = "";
   }
 
