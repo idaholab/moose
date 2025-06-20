4bde09b3b55b3cd65a88622ec0af8b5a18562991
Alexander Blair <alexander.blair@ukaea.uk>
2025-06-20 21:20:18 +0100

    Add MFEMBoundaryRestrictable interface for fetching boundary markers from user provided boundary names or ids consistently. Refs #30807

--- framework/src/mfem/bcs/MFEMBoundaryCondition.C
+++ framework/src/mfem/bcs/MFEMBoundaryCondition.C
@@ -19,50 +19,24 @@ InputParameters
 MFEMBoundaryCondition::validParams()
 {
   InputParameters params = MFEMGeneralUserObject::validParams();
+  params += MFEMBoundaryRestrictable::validParams();
+
   params.addClassDescription("Base class for applying boundary conditions to MFEM problems.");
   params.registerBase("BoundaryCondition");
-  // Create user-facing 'boundary' input for restricting inheriting object to boundaries
-  // MFEM uses the boundary -1 to signify every sideset
-  params.addParam<std::vector<BoundaryName>>(
-      "boundary",
-      {"-1"},
-      "The list of boundaries (ids) from the mesh where this boundary condition applies. "
-      "Defaults to applying BC on all boundaries.");
   params.addParam<VariableName>("variable", "Variable on which to apply the boundary condition");
   return params;
 }
 
 MFEMBoundaryCondition::MFEMBoundaryCondition(const InputParameters & parameters)
   : MFEMGeneralUserObject(parameters),
-    _test_var_name(getParam<VariableName>("variable")),
-    _boundary_names(getParam<std::vector<BoundaryName>>("boundary")),
-    _bdr_attributes(_boundary_names.size())
+    MFEMBoundaryRestrictable(parameters,
+                             *getMFEMProblem()
+                                  .getProblemData()
+                                  .gridfunctions.GetRef(getParam<VariableName>("variable"))
+                                  .ParFESpace()
+                                  ->GetParMesh()),
+    _test_var_name(getParam<VariableName>("variable"))
 {
-  mfem::ParMesh & mesh(*getMFEMProblem()
-                            .getProblemData()
-                            .gridfunctions.GetRef(_test_var_name)
-                            .ParFESpace()
-                            ->GetParMesh());
-
-  for (const auto i : index_range(_boundary_names))
-  {
-    const auto & boundary_name = _boundary_names[i];
-    try
-    {
-      // Is this a boundary ID
-      _bdr_attributes[i] = std::stoi(boundary_name);
-    }
-    catch (...)
-    {
-      // It was not
-      auto & bnd_ids = mesh.bdr_attribute_sets.GetAttributeSet(boundary_name);
-      if (bnd_ids.Size() != 1)
-        this->mooseError(
-            "There should be a 1-to-1 correspondence between boundary name and boundary ID");
-      _bdr_attributes[i] = bnd_ids[0];
-    }
-  }
-  mfem::common::AttrToMarker(mesh.bdr_attributes.Max(), _bdr_attributes, _bdr_markers);
 }
 
 #endif
