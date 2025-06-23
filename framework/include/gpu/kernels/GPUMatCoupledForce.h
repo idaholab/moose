26ddb17da7749d1e928041eb4f0ee20f567edc83
Namjae Choi <Namjae.Choi@inl.gov>
2025-06-23 14:05:39 -0600

    Framework-level GPU kernels and boundary conditions #30655

---
+++ framework/include/gpu/kernels/GPUMatCoupledForce.h
@@ -0,0 +1,66 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "GPUKernel.h"
+
+/**
+ * Represents a right hand side force term of the form
+ * Sum_j c_j * m_j * v_j, where c is a vector of real numbers,
+ * m_j is a vector of material properties, and v_j is a vector
+ * of variables
+ */
+class GPUMatCoupledForce final : public GPUKernel<GPUMatCoupledForce>
+{
+public:
+  static InputParameters validParams();
+
+  GPUMatCoupledForce(const InputParameters & parameters);
+
+  KOKKOS_FUNCTION Real computeQpResidual(const unsigned int i,
+                                         const unsigned int qp,
+                                         ResidualDatum & datum) const
+  {
+    Real r = 0;
+    if (_coupled_props)
+      for (unsigned int j = 0; j < _n_coupled; ++j)
+        r += -_coef[j] * _mat_props[j](datum, qp) * _v(datum, qp, j);
+    else
+      for (unsigned int j = 0; j < _n_coupled; ++j)
+        r += -_coef[j] * _v(datum, qp, j);
+    return r * _test(datum, i, qp);
+  }
+
+  KOKKOS_FUNCTION Real computeQpOffDiagJacobian(const unsigned int i,
+                                                const unsigned int j,
+                                                const unsigned int jvar,
+                                                const unsigned int qp,
+                                                ResidualDatum & datum) const
+  {
+    auto idx = _v_var_to_index.find(jvar);
+    if (idx == _v_var_to_index.size())
+      return 0;
+
+    unsigned int p = _v_var_to_index[idx];
+
+    if (_coupled_props)
+      return -_coef[p] * _mat_props[p](datum, qp) * _phi(datum, j, qp) * _test(datum, i, qp);
+    return -_coef[p] * _phi(datum, j, qp) * _test(datum, i, qp);
+  }
+
+private:
+  const unsigned int _n_coupled;
+  const bool _coupled_props;
+  std::vector<unsigned int> _v_var;
+  GPUVariableValue _v;
+  GPUArray<Real> _coef;
+  GPUMap<unsigned int, unsigned int> _v_var_to_index;
+  GPUArray<GPUMaterialProperty<Real>> _mat_props;
+};
---
+++ framework/include/gpu/kernels/GPUReaction.h
@@ -0,0 +1,41 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "GPUKernel.h"
+
+/**
+ *  Implements a simple consuming reaction term with weak form $(\\psi_i, \\lambda u_h)$.
+ */
+class GPUReaction final : public GPUKernel<GPUReaction>
+{
+public:
+  static InputParameters validParams();
+
+  GPUReaction(const InputParameters & parameters);
+
+  KOKKOS_FUNCTION Real computeQpResidual(const unsigned int i,
+                                         const unsigned int qp,
+                                         ResidualDatum & datum) const
+  {
+    return _test(datum, i, qp) * _rate * _u(datum, qp);
+  }
+  KOKKOS_FUNCTION Real computeQpJacobian(const unsigned int i,
+                                         const unsigned int j,
+                                         const unsigned int qp,
+                                         ResidualDatum & datum) const
+  {
+    return _test(datum, i, qp) * _rate * _phi(datum, j, qp);
+  }
+
+protected:
+  /// Scalar coefficient representing the relative amount consumed per unit time
+  GPUScalar<const Real> _rate;
+};
