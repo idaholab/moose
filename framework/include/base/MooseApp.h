c92ed010d631e3c1d3c9227f7c3a314216fbf651
Namjae Choi <Namjae.Choi@inl.gov>
2025-06-23 13:14:02 -0600

    GPU base header and initialization #30655

--- framework/include/base/MooseApp.h
+++ framework/include/base/MooseApp.h
@@ -1579,6 +1579,21 @@ private:
   friend class FEProblemBase;
   friend class Restartable;
   friend class SubProblem;
+
+  /**
+   * GPU-related variables and methods
+   */
+public:
+  /// Return whether GPUs exist
+  bool hasGPUs() { return _has_GPUs > 0; }
+
+private:
+  bool _has_GPUs = false;
+
+#ifdef MOOSE_HAVE_GPU
+  /// Query whether GPUs exist
+  void queryGPUs();
+#endif
 };
 
 template <class T>
