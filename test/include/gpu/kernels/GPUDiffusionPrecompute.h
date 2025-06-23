9f6f362e9039ae4a5eab08c2b4c941ea35d4c23d
Namjae Choi <Namjae.Choi@inl.gov>
2025-06-23 13:57:16 -0600

    GPU tests #30655

---
+++ test/include/gpu/kernels/GPUDiffusionPrecompute.h
@@ -0,0 +1,35 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "GPUKernelGrad.h"
+
+class GPUDiffusionPrecompute final : public GPUKernelGrad<GPUDiffusionPrecompute>
+{
+public:
+  static InputParameters validParams();
+
+  GPUDiffusionPrecompute(const InputParameters & parameters);
+
+  KOKKOS_FUNCTION Real3 precomputeQpResidual(const unsigned int qp, ResidualDatum & datum) const
+  {
+    // Note we do not multiply by the gradient of the test function. That is done in the parent
+    // class
+    return _grad_u(datum, qp);
+  }
+  KOKKOS_FUNCTION Real3 precomputeQpJacobian(const unsigned int j,
+                                             const unsigned int qp,
+                                             ResidualDatum & datum) const
+  {
+    // Note we do not multiply by the gradient of the test function. That is done in the parent
+    // class
+    return _grad_phi(datum, j, qp);
+  }
+};
