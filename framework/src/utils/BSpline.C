76d4baba9e06d73147080c35486a41f53d0ccff8
Liam Pohlmann <liam.pohlmann@inl.gov>
2025-06-24 14:22:57 -0600

    Create BSpline utility. Code takes in a vector of libMesh Points as control points and an interpolating polynomial degree and returns a BSpline object, which can be evaluated for values of t on the standard interval [0,1]. Test cases were created to verify degree-varying capabilities and user feedback when the number of control points is insufficient.
    closes #30820

--- framework/src/utils/BSpline.C
+++ framework/src/utils/BSpline.C
@@ -9,26 +9,110 @@
 
 #include "BSpline.h"
 #include "libmesh/point.h"
+#include "libMeshReducedNamespace.h"
+
+using namespace libMesh;
 
 namespace Moose
 {
 BSpline::BSpline(const unsigned int degree, const std::vector<libMesh::Point> & control_points)
   : _degree(degree), _control_points(control_points), _knot_vector(buildKnotVector())
+
 {
 }
 
+/**
+ * Evaluate the BSpline interpolation at given value of t.
+ *
+ * @param t the parameter value, must lie in [0,1]
+ * @return libMesh::Point type with format (x,y,z)
+ */
 libMesh::Point
-BSpline::getPoint(const double t) const
+BSpline::getPoint(const Real t) const
 {
-  return {0, 0, 0};
+  libMesh::Point returnPoint(0, 0, 0);
+  for (const auto i : index_range(_control_points))
+    returnPoint += BSpline::CdBBasis(t, i, _degree) * _control_points[i];
+  return returnPoint;
 }
 
-std::vector<double>
+/**
+ * Creates normalized knot vector.
+ */
+std::vector<Real>
 BSpline::buildKnotVector() const
 {
-  if (_degree == 1)
-    return {1, 0, 0};
+  const unsigned int num_points_left = _control_points.size() - _degree;
+
+  if (num_points_left <= 0)
+    mooseError("Number of control points must be equal to degree + 1!");
+
+  std::vector<Real> knot_vector(_degree, 0); // initialize bottom to zeros
+  for (const auto i : make_range(num_points_left))
+    knot_vector.push_back(i); // count up
+
+  for (const auto i : make_range(_degree))
+  {
+    libmesh_ignore(i);
+    knot_vector.push_back(num_points_left); // append values to create the open knot vector
+  }
+
+  // Normalize values s.t. the the max value is 1.
+  // This must be done to have a constant domain of t to be between 0 and 1 (inclusive).
+  for (Real & value : knot_vector)
+    value /= num_points_left;
+  return knot_vector;
+}
+
+/**
+ * Evaluates the the basis function for a B-Spline according to the Cox-de-Boor
+ * recursive formula.
+ *
+ * @param t,i,j parameter t in [0,1], index corresponding to which control point, and the degree of
+ * the basis funtion
+ * @return scalar quantity for the contribution of the basis function at i in the sum.
+ */
+Real
+BSpline::CdBBasis(const Real & t, const unsigned int i, const unsigned int j) const
+{
+  if (j == 0)
+  {
+    Real basis_return =
+        (t <= _knot_vector[i + 1] && _knot_vector[i] <= t && _knot_vector[i] < _knot_vector[i + 1])
+            ? 1
+            : 0;
+    return basis_return;
+  }
   else
-    return {0};
+    return BSpline::firstCoeff(t, i, j) * BSpline::CdBBasis(t, i, j - 1) +
+           BSpline::secondCoeff(t, i, j) * BSpline::CdBBasis(t, i + 1, j - 1);
+}
+
+/**
+ * Submethod to aid in computing the basis function in CdBBasis.
+ */
+Real
+BSpline::firstCoeff(const Real & t, const unsigned int i, const unsigned int j) const
+{
+  Real ti = _knot_vector[i];
+  Real tij = _knot_vector[i + j];
+  if (ti != tij)
+    return (t - ti) / (tij - ti);
+  else
+    return 0;
+}
+
+/**
+ * Submethod to aid in computing the basis function in CdBBasis.
+ */
+Real
+BSpline::secondCoeff(const Real & t, const unsigned int i, const unsigned int j) const
+{
+  Real ti1 = _knot_vector[i + 1];
+  Real tij1 = _knot_vector[i + j + 1];
+  if (ti1 != tij1)
+    return (tij1 - t) / (tij1 - ti1);
+  else
+    return 0;
 }
 }
--- unit/src/BSplineTest.C
+++ unit/src/BSplineTest.C
@@ -8,15 +8,110 @@
 //* https://www.gnu.org/licenses/lgpl-2.1.html
 
 #include "gtest/gtest.h"
-
 #include "BSpline.h"
 
-TEST(BSplineTest, buildKnotVectorTest)
+//* The following tests check that the BSpline utility and its submethods work as intended.
+//* Degrees 2, 3, and 4 interpolating polynomials will be evaluated at prescribed values of
+//* t, the parameter. In addition, a vector of control points is supplied.
+//*
+//* All expected points were independently calculated and verified.
+
+const Real t0 = 0;
+const Real t1 = 0.4;
+const Real t2 = 0.8;
+const Real t3 = 1.0;
+const std::vector<libMesh::Point> control_points = {
+      {1, 2, 0}, {1, 2, 0}, {1.5, 2.5, 0}, {3, 1, 0}, {3, 1, 0}};
+
+TEST(BSplineTest, Degree2Test)
 {
-  const unsigned int degree = 1;
-  const std::vector<libMesh::Point> control_points = {{0, 0, 0}, {1, 0, 0}};
+  const unsigned int degree = 2;
 
   Moose::BSpline b_spline(degree, control_points);
-  const std::vector<double> expected_knot_vector = {0, 0, 1, 1};
-  ASSERT_EQ(b_spline.getKnotVector(), expected_knot_vector);
+
+  const libMesh::Point point_0 = b_spline.getPoint(t0);
+  const libMesh::Point point_1 = b_spline.getPoint(t1);
+  const libMesh::Point point_2 = b_spline.getPoint(t2);
+  const libMesh::Point point_3 = b_spline.getPoint(t3);
+
+  const libMesh::Point expected_point_0(1, 2, 0);
+  const libMesh::Point expected_point_1(1.37, 2.31, 0);
+  const libMesh::Point expected_point_2(2.73, 1.2699999999999996, 0);
+  const libMesh::Point expected_point_3(3, 1, 0);
+
+  EXPECT_NEAR((point_0 - expected_point_0).norm(), 0, 1e-9);
+  EXPECT_NEAR((point_1 - expected_point_1).norm(), 0, 1e-9);
+  EXPECT_NEAR((point_2 - expected_point_2).norm(), 0, 1e-9);
+  EXPECT_NEAR((point_3 - expected_point_3).norm(), 0, 1e-9);
+
+}
+
+TEST(BSplineTest, Degree3Test)
+{
+  const unsigned int degree = 3;
+
+  Moose::BSpline b_spline(degree, control_points);
+
+  const libMesh::Point point_0 = b_spline.getPoint(t0);
+  const libMesh::Point point_1 = b_spline.getPoint(t1);
+  const libMesh::Point point_2 = b_spline.getPoint(t2);
+  const libMesh::Point point_3 = b_spline.getPoint(t3);
+
+  const libMesh::Point expected_point_0(1, 2, 0);
+  const libMesh::Point expected_point_1(1.48, 2.0959999999999996, 0);
+  const libMesh::Point expected_point_2(2.7039999999999997, 1.2799999999999998, 0);
+  const libMesh::Point expected_point_3(3, 1, 0);
+
+  EXPECT_NEAR((point_0 - expected_point_0).norm(), 0, 1e-9);
+  EXPECT_NEAR((point_1 - expected_point_1).norm(), 0, 1e-9);
+  EXPECT_NEAR((point_2 - expected_point_2).norm(), 0, 1e-9);
+  EXPECT_NEAR((point_3 - expected_point_3).norm(), 0, 1e-9);
+
+}
+
+TEST(BSplineTest, Degree4Test)
+{
+  const unsigned int degree = 4;
+  
+
+  Moose::BSpline b_spline(degree, control_points);
+
+  const libMesh::Point point_0 = b_spline.getPoint(t0);
+  const libMesh::Point point_1 = b_spline.getPoint(t1);
+  const libMesh::Point point_2 = b_spline.getPoint(t2);
+  const libMesh::Point point_3 = b_spline.getPoint(t3);
+
+  const libMesh::Point expected_point_0(1, 2, 0);
+  const libMesh::Point expected_point_1(1.5312, 1.9936, 0);
+  const libMesh::Point expected_point_2(2.7152000000000003, 1.2576, 0);
+  const libMesh::Point expected_point_3(3, 1, 0);
+
+  EXPECT_NEAR((point_0 - expected_point_0).norm(), 0, 1e-9);
+  EXPECT_NEAR((point_1 - expected_point_1).norm(), 0, 1e-9);
+  EXPECT_NEAR((point_2 - expected_point_2).norm(), 0, 1e-9);
+  EXPECT_NEAR((point_3 - expected_point_3).norm(), 0, 1e-9);
+
+}
+
+/**
+ * Test for mooseError when the number of control points supplied is
+ * insufficient.
+ */
+TEST(BSplineTest, ControlPointErrorTest)
+{
+  const unsigned int degree = 5;
+
+  EXPECT_THROW(
+      {
+        try
+        {
+          Moose::BSpline b_spline(degree, control_points);
+        }
+        catch (const std::exception & e)
+        {
+          EXPECT_STREQ(e.what(), "Number of control points must be equal to degree + 1!");
+          throw;
+        }
+      },
+      std::exception);
 }
