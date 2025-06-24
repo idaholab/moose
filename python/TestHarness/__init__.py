a239e7fc65d0c9c5c93d3b114bdcfefd3fcb700b
Logan Harbour <loganhharbour@gmail.com>
2025-06-24 11:08:51 -0600

    Remove useless error

--- python/TestHarness/__init__.py
+++ python/TestHarness/__init__.py
@@ -6,12 +6,6 @@
 #*
 #* Licensed under LGPL 2.1, please see LICENSE for details
 #* https://www.gnu.org/licenses/lgpl-2.1.html
-import sys
-if sys.version_info < (3, 6):
-    print('"TestHarness" requires python version 3.6 or greater, version {}.{} is being used.' \
-          .format(sys.version_info[0], sys.version_info[1]))
-    sys.exit(1)
-
 from .TestHarness import TestHarness
 from .OutputInterface import OutputInterface
 from .TestHarness import findDepApps
