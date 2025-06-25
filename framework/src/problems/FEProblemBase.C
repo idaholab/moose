737fee6a5075bcf5aa22870b0c062a6b6868ad93
Namjae Choi <Namjae.Choi@inl.gov>
2025-06-25 15:03:56 -0600

    Check early the existence of Kokkos objects added by actions and initialize Kokkos mesh conditionally #30655

--- framework/src/problems/FEProblemBase.C
+++ framework/src/problems/FEProblemBase.C
@@ -3121,7 +3121,7 @@ FEProblemBase::addKokkosKernel(const std::string & kernel_name,
   _nl[nl_sys_num]->addKokkosKernel(kernel_name, name, parameters);
 #endif
 
-  _have_kokkos_objects = true;
+  _has_kokkos_objects = true;
 }
 
 void
@@ -3164,7 +3164,7 @@ FEProblemBase::addKokkosNodalKernel(const std::string & kernel_name,
   _nl[nl_sys_num]->addKokkosNodalKernel(kernel_name, name, parameters);
 #endif
 
-  _have_kokkos_objects = true;
+  _has_kokkos_objects = true;
 }
 
 void
@@ -3213,7 +3213,7 @@ FEProblemBase::addKokkosBoundaryCondition(const std::string & bc_name,
   _nl[nl_sys_num]->addKokkosBoundaryCondition(bc_name, name, parameters);
 #endif
 
-  _have_kokkos_objects = true;
+  _has_kokkos_objects = true;
 }
 
 void
@@ -6622,7 +6622,7 @@ FEProblemBase::init()
     _displaced_problem->init();
 
 #ifdef MOOSE_HAVE_KOKKOS
-  if (_have_kokkos_objects)
+  if (_has_kokkos_objects)
     initKokkos();
 #endif
 
@@ -8588,7 +8588,7 @@ FEProblemBase::initElementStatefulProps(const ConstElemRange & elem_range, const
     cmt(elem_range, true);
 
 #ifdef MOOSE_HAVE_KOKKOS
-  if (_have_kokkos_objects)
+  if (_has_kokkos_objects)
     initKokkosStatefulProps();
 #endif
 }
