4f50a5b047299c672132b011cf42a5d20cfd8442
Joshua E. Hansel <joshua.hansel@inl.gov>
2025-06-25 11:37:43 -0500

    Based DefaultFixedPointConvergence on DefaultConvergenceBase
    Refs #30784

--- framework/include/convergence/DefaultFixedPointConvergence.h
+++ framework/include/convergence/DefaultFixedPointConvergence.h
@@ -9,54 +9,23 @@
 
 #pragma once
 
-#include "Convergence.h"
+#include "DefaultConvergenceBase.h"
 
 class FixedPointSolve;
 
 /**
  * Default fixed point convergence criteria.
  */
-class DefaultFixedPointConvergence : public Convergence
+class DefaultFixedPointConvergence : public DefaultConvergenceBase
 {
 public:
   static InputParameters validParams();
 
   DefaultFixedPointConvergence(const InputParameters & parameters);
 
-  virtual void initialSetup() override;
   virtual void initialize() override;
 
   virtual MooseConvergenceStatus checkConvergence(unsigned int iter) override;
-
-protected:
-  /**
-   * This method is to be used for parameters that are shared with the executioner.
-   *
-   * If the parameter is set by the user in the executioner, get that value;
-   * otherwise, get this object's value.
-   * If the parameter is set by the user in both this object and the executioner,
-   * add it to a list and report an error later.
-   */
-  template <typename T>
-  const T & getSharedExecutionerParam(const std::string & name);
-
-  /**
-   * Throws an error if any of the parameters shared with the executioner have
-   * been set by the user in both places.
-   *
-   * This should be called after all calls to \c getSharedExecutionerParam.
-   * No error is thrown if the Convergence object was added as a default, since
-   * in that case, any parameters set by the user in the executioner will also
-   * be considered set by the user in the Convergence.
-   */
-  void checkDuplicateSetSharedExecutionerParams() const;
-
-private:
-  /// True if this object was added as a default instead of by the user
-  const bool _added_as_default;
-
-  /// List of shared executioner parameters that have been set by the user in both places
-  std::vector<std::string> _duplicate_shared_executioner_params;
 
 protected:
   /// Computes and prints the user-specified postprocessor assessing convergence
@@ -87,18 +56,3 @@ protected:
   /// Fixed point solve
   FixedPointSolve & _fp_solve;
 };
-
-template <typename T>
-const T &
-DefaultFixedPointConvergence::getSharedExecutionerParam(const std::string & param)
-{
-  const auto * executioner = getMooseApp().getExecutioner();
-  if (executioner->isParamSetByUser(param))
-  {
-    if (isParamSetByUser(param))
-      _duplicate_shared_executioner_params.push_back(param);
-    return executioner->getParam<T>(param);
-  }
-  else
-    return getParam<T>(param);
-}
