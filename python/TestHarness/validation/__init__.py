dd8015e263089e985ccb83c8a3fa717c69551ca9
Logan Harbour <loganhharbour@gmail.com>
2025-06-25 20:37:54 -0600

    Move ValidationCase to cases folder
    refs #30819

--- python/TestHarness/validation/__init__.py
+++ python/TestHarness/validation/__init__.py
@@ -7,4 +7,4 @@
 #* Licensed under LGPL 2.1, please see LICENSE for details
 #* https://www.gnu.org/licenses/lgpl-2.1.html
 
-from .case import ValidationCase
+from .cases.validationcase import ValidationCase
