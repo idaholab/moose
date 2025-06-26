4542dffb165beb93f087548565ab8fcfb795a8e5
Namjae Choi <Namjae.Choi@inl.gov>
2025-06-25 18:46:47 -0600

    Only sort all objects for Kokkos material warehouse #30655

--- framework/src/problems/FEProblemBase.C
+++ framework/src/problems/FEProblemBase.C
@@ -1140,7 +1140,7 @@ FEProblemBase::initialSetup()
       }
     }
 
-    _kokkos_materials.sort();
+    _kokkos_materials.sort(0, true);
 
     {
       TIME_SECTION("computingInitialStatefulProps", 3, "Computing Initial Material Values");
