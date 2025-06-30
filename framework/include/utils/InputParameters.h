0b4a9567c00820b7f1cee50019e5be24ca94fa29
Nuno Nobre <nuno.nobre@stfc.ac.uk>
2025-06-30 12:51:30 +0100

    Use MOOSE_MFEM_ENABLED, not MFEM_ENABLED (#30916)

--- framework/include/utils/InputParameters.h
+++ framework/include/utils/InputParameters.h
@@ -2318,7 +2318,7 @@ getNullptrExample()
   return nullptr;
 }
 
-#ifdef MFEM_ENABLED
+#ifdef MOOSE_MFEM_ENABLED
 
 template <typename T>
 constexpr bool
@@ -2342,7 +2342,7 @@ constexpr bool
 isScalarFunctorNameTypeHelper(T *)
 {
   return std::is_same_v<T, MooseFunctorName>
-#ifdef MFEM_ENABLED
+#ifdef MOOSE_MFEM_ENABLED
          || std::is_same_v<T, MFEMScalarCoefficientName>
 #endif
       ;
@@ -2359,7 +2359,7 @@ template <typename T>
 constexpr bool
 isVectorFunctorNameTypeHelper(T *)
 {
-#ifdef MFEM_ENABLED
+#ifdef MOOSE_MFEM_ENABLED
   return std::is_same_v<T, MFEMVectorCoefficientName>;
 #else
   return false;
@@ -2410,7 +2410,7 @@ InputParameters::appendFunctorDescription(const std::string & doc_string) const
 
   return MooseUtils::trim(doc_string, ". ") + ". A functor is any of the following: a variable, " +
          (
-#ifdef MFEM_ENABLED
+#ifdef MOOSE_MFEM_ENABLED
              Moose::internal::isMFEMFunctorNameTypeHelper(Moose::internal::getNullptrExample<T>())
                  ? "an MFEM"
                  :
