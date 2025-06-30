0d912bd44c056caa67b776d77d2a181c42932549
Sean Baccas <sean.baccas@ukaea.uk>
2025-06-30 16:40:57 +0100

    Minor structural changes

--- framework/src/mfem/executioners/MFEMSteady.C
+++ framework/src/mfem/executioners/MFEMSteady.C
@@ -100,7 +100,7 @@ MFEMSteady::execute()
     if ( _use_amr )
     {
       // p-refine
-      _problem_operator->PRefine();
+      PRefine();
       _problem_operator->Solve(_problem_data.f);
     }
 
@@ -114,6 +114,10 @@ MFEMSteady::execute()
 
   // need to keep _time in sync with _time_step to get correct output
   _time = _time_step;
+  // Execute user objects at timestep end
+  _mfem_problem.execute(EXEC_TIMESTEP_END);
+  _mfem_problem.setMeshChanged(false);
+
   _mfem_problem.outputStep(EXEC_TIMESTEP_END);
   _time = _system_time;
 
@@ -145,6 +149,38 @@ MFEMSteady::addEstimator( std::shared_ptr<MFEMEstimator> estimator )
     return false;
   }
 }
+
+bool
+MFEMSteady::PRefine()
+{
+  // Call PRefine in the problem operator
+  bool output = _problem_operator->PRefine();
+
+  UpdateAfterRefinement();
+
+  return output;
+}
+
+bool
+MFEMSteady::HRefine()
+{
+  // Call PRefine in the problem operator
+  bool output = _problem_operator->HRefine();
+
+  UpdateAfterRefinement();
+
+  return output;
+}
+
+void 
+MFEMSteady::UpdateAfterRefinement()
+{
+  // Update in the mfem problem
+  _mfem_problem.updateAfterRefinement();
+
+  _problem_operator->SetGridFunctions();
+}
+
 
 
 #endif
