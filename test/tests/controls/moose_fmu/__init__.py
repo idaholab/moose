57eaf59152603a404e38aca1d163bdf369e06fdb
MengnanLi91 <mengnanli91@gmail.com>
2025-06-30 12:38:10 -0600

    Successfully build moose_fmu with web server control, test with fmu_diffision.i

--- test/tests/controls/moose_fmu/__init__.py
+++
@@ -1,8 +0,0 @@
-from _version import __version__
-
-from FMU4MOOSE.builder import FmuBuilder
-from FMU4MOOSE.MOOSE2FMU import MOOSE2fmu
-from pythonfmu.enums import Fmi2Causality, Fmi2Initial, Fmi2Variability
-from pythonfmu.variables import Boolean, Integer, Real, String
-
-from FMU4MOOSE.MOOSEFMUBase import *
