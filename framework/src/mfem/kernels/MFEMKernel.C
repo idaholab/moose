fa77382f6c3d3f62e0b97de4a86519aaaaa8c8b6
Alex Lindsay <alexlindsay239@gmail.com>
2025-06-30 12:36:16 -0600

    Guard against MFEM_ASSERT around size 0 _subdomain_attributes

--- framework/src/mfem/kernels/MFEMKernel.C
+++ framework/src/mfem/kernels/MFEMKernel.C
@@ -37,11 +37,10 @@ MFEMKernel::MFEMKernel(const InputParameters & parameters)
     _subdomain_attributes(_subdomain_names.size())
 {
   for (unsigned int i = 0; i < _subdomain_names.size(); ++i)
-  {
     _subdomain_attributes[i] = std::stoi(_subdomain_names[i]);
-  }
   mfem::ParMesh & mesh(getMFEMProblem().mesh().getMFEMParMesh());
-  mfem::common::AttrToMarker(mesh.attributes.Max(), _subdomain_attributes, _subdomain_markers);
+  if (_subdomain_attributes.Size())
+    mfem::common::AttrToMarker(mesh.attributes.Max(), _subdomain_attributes, _subdomain_markers);
 }
 
 #endif
