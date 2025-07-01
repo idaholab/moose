9ae7576c6a5dcb1f4a5801a2c6c7ea2219267cd1
“Henrique <henrique.bergallo-rocha@ukaea.uk>
2025-07-01 17:07:27 +0100

    Add complex gridfunctions

---
+++ framework/src/mfem/variables/MFEMComplexVariable.C
@@ -0,0 +1,41 @@
+#ifdef MFEM_ENABLED
+
+#include "MFEMComplexVariable.h"
+#include "MooseVariableBase.h"
+#include "libmesh/ignore_warnings.h"
+#include <mfem.hpp>
+#include "libmesh/restore_warnings.h"
+
+registerMooseObject("MooseApp", MFEMComplexVariable);
+
+InputParameters
+MFEMComplexVariable::validParams()
+{
+  InputParameters params = MFEMGeneralUserObject::validParams();
+  //// Create user-facing 'boundary' input for restricting inheriting object to boundaries.
+  params.addRequiredParam<UserObjectName>("fespace",
+                                          "The finite element space this variable is defined on.");
+  //// Require moose variable parameters (not used!)
+  params += MooseVariableBase::validParams();
+  params.addClassDescription(
+      "Class for adding complex MFEM variables to the problem (`mfem::ParComplexGridFunction`s).");
+  params.registerBase("MooseVariableBase");
+
+  return params;
+}
+
+MFEMComplexVariable::MFEMComplexVariable(const InputParameters & parameters)
+  : MFEMGeneralUserObject(parameters),
+    _fespace(getUserObject<MFEMFESpace>("fespace")),
+    _cmplx_gridfunction(buildComplexGridFunction())
+{
+  *_cmplx_gridfunction = 0.0;
+}
+
+const std::shared_ptr<mfem::ParComplexGridFunction>
+MFEMComplexVariable::buildComplexGridFunction()
+{
+  return std::make_shared<mfem::ParComplexGridFunction>(_fespace.getFESpace().get());
+}
+
+#endif
---
+++ test/tests/mfem/kernels/complex.i
@@ -0,0 +1,74 @@
+[Mesh]
+  type = MFEMMesh
+  file = ../mesh/mug.e
+  dim = 3
+[]
+
+[Problem]
+  type = MFEMProblem
+[]
+
+[FESpaces]
+  [H1FESpace]
+    type = MFEMScalarFESpace
+    fec_type = H1
+    fec_order = FIRST
+  []
+[]
+
+[Variables]
+  [concentration]
+    type = MFEMVariable
+    fespace = H1FESpace
+  []
+  [complex]
+    type = MFEMComplexVariable
+    fespace = H1FESpace
+  []
+[]
+
+
+
+
+
+
+[BCs]
+  [bottom]
+    type = MFEMScalarDirichletBC
+    variable = concentration
+    boundary = '1'
+    coefficient = 1.0
+  []
+  [low_terminal]
+    type = MFEMScalarDirichletBC
+    variable = concentration
+    boundary = '2'
+    coefficient = 0.0
+  []
+[]
+
+[Kernels]
+  [diff]
+    type = MFEMDiffusionKernel
+    variable = concentration
+    coefficient = 1.0
+  []
+[]
+
+[Preconditioner]
+  [jacobi]
+    type = MFEMOperatorJacobiSmoother
+  []
+[]
+
+[Solver]
+  type = MFEMGMRESSolver
+  preconditioner = jacobi
+  l_tol = 1e-16
+  l_max_its = 1000
+[]
+
+[Executioner]
+  type = MFEMSteady
+  device = cpu
+[]
