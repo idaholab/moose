d2e55f39271ed479d99eca06b1e4b24b40701966
Ioannis-UKAEA <ic@Ioanniss-MacBook-Pro-2.local>
2025-07-02 13:57:23 +0100

    Add AuxKernels to project Scalar and Vector Coefficient on to MFEM Variables

---
+++ framework/include/mfem/auxkernels/MFEMVectorProjectAux.h
@@ -0,0 +1,24 @@
+#ifdef MFEM_ENABLED
+
+#pragma once
+
+#include "MFEMAuxKernel.h"
+#include "mfem.hpp"
+
+class MFEMVectorProjectAux : public MFEMAuxKernel
+{
+public:
+  static InputParameters validParams();
+
+  MFEMVectorProjectAux(const InputParameters & parameters);
+
+  virtual ~MFEMVectorProjectAux() = default;
+
+  virtual void execute() override;
+
+protected:
+  const MFEMVectorCoefficientName _coefficient_name;
+  mfem::VectorCoefficient * _vec_coef;
+};
+
+#endif
