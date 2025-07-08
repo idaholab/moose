f5f07d89d169c49476f9e5ecde39c0081e6bd811
Namjae Choi <Namjae.Choi@inl.gov>
2025-07-08 09:39:52 -0600

    Back to caching elemental data #30655

--- framework/include/gpu/kernels/GPUKernelBase.h
+++ framework/include/gpu/kernels/GPUKernelBase.h
@@ -40,17 +40,6 @@ public:
    * Copy constructor for parallel dispatch
    */
   KernelBase(const KernelBase & object);
-
-protected:
-  /**
-   * Set the variables this object depend on to the Kokkos systems
-   */
-  void setVariableDependency();
-  /**
-   * Set the quadrature cache status flags for the variables, tags, and subdomains covered by this
-   * object to the Kokkos systems
-   */
-  void setCacheFlags();
 };
 
 } // namespace Kokkos
