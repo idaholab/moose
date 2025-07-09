be172e14bca0a0d26a83e3906f6af13d8e99d7e7
“Henrique <henrique.bergallo-rocha@ukaea.uk>
2025-07-09 15:45:08 +0100

    Revert real kernels to previous state

--- framework/src/mfem/kernels/MFEMVectorDomainLFKernel.C
+++ framework/src/mfem/kernels/MFEMVectorDomainLFKernel.C
@@ -23,31 +23,19 @@ MFEMVectorDomainLFKernel::validParams()
                              "arising from the weak form of the forcing term $\\vec f$.");
   params.addParam<MFEMVectorCoefficientName>(
       "vector_coefficient", "1. 1. 1.", "Name of body force density f.");
-  params.addParam<MFEMVectorCoefficientName>(
-      "vector_coefficient_imag", "Name of imaginary part of body force density $\\vec f$.");
-
   return params;
 }
 
 MFEMVectorDomainLFKernel::MFEMVectorDomainLFKernel(const InputParameters & parameters)
   : MFEMKernel(parameters),
-    _vec_coef(getVectorCoefficient(getParam<MFEMVectorCoefficientName>("vector_coefficient"))),
-    // If the imaginary coefficient is not provided, we pick the real one since the variable needs
-    // to be initialized, but it won't be used
-    _vec_coef_imag(
-        getVectorCoefficient(isParamValid("vector_coefficient_imag")
-                                 ? getParam<MFEMVectorCoefficientName>("vector_coefficient_imag")
-                                 : getParam<MFEMVectorCoefficientName>("vector_coefficient")))
+    _vec_coef(getVectorCoefficient(getParam<MFEMVectorCoefficientName>("vector_coefficient")))
 {
 }
 
-std::pair<mfem::LinearFormIntegrator *, mfem::LinearFormIntegrator *>
+mfem::LinearFormIntegrator *
 MFEMVectorDomainLFKernel::createLFIntegrator()
 {
-  return std::make_pair(new mfem::VectorDomainLFIntegrator(_vec_coef),
-                        isParamValid("vector_coefficient_imag")
-                            ? new mfem::VectorDomainLFIntegrator(_vec_coef_imag)
-                            : nullptr);
+  return new mfem::VectorDomainLFIntegrator(_vec_coef);
 }
 
 #endif
