f09302548f634098267d9deec52c63720f1854cc
Alexander Blair <alexander.blair@ukaea.uk>
2025-07-09 10:22:42 +0100

    Rename attributes of MFEMTransient to more closely match those in TransientBase

--- framework/src/mfem/executioners/MFEMTransient.C
+++ framework/src/mfem/executioners/MFEMTransient.C
@@ -20,9 +20,9 @@ MFEMTransient::validParams()
 {
   InputParameters params = MFEMExecutioner::validParams();
   params.addClassDescription("Executioner for transient MFEM problems.");
-  params.addParam<Real>("start_time", 0.0, "The start time of the simulation");
-  params.addParam<Real>("end_time", 1.0e30, "The end time of the simulation");
-  params.addParam<Real>("dt", 1., "The timestep size between solves");
+  params.addParam<mfem::real_t>("start_time", 0.0, "The start time of the simulation");
+  params.addParam<mfem::real_t>("end_time", 1.0e30, "The end time of the simulation");
+  params.addParam<mfem::real_t>("dt", 1., "The timestep size between solves");
   params.addParam<unsigned int>(
       "visualisation_steps", 1, "The number of timesteps in a transient run");
   return params;
@@ -30,16 +30,17 @@ MFEMTransient::validParams()
 
 MFEMTransient::MFEMTransient(const InputParameters & params)
   : MFEMExecutioner(params),
-    _t_step(getParam<Real>("dt")),
-    _t_initial(getParam<Real>("start_time")),
-    _t_final(getParam<Real>("end_time")),
-    _t(_mfem_problem.time()),
-    _it(0),
+    _dt(getParam<mfem::real_t>("dt")),
+    _dt_old(getParam<mfem::real_t>("dt")),
+    _time(_mfem_problem.time()),
+    _start_time(getParam<mfem::real_t>("start_time")),
+    _end_time(getParam<mfem::real_t>("end_time")),
+    _t_step(0),
     _vis_steps(params.get<unsigned int>("visualisation_steps")),
     _last_step(false)
 {
-  _app.setStartTime(_t_initial);
-  _t = _t_initial;
+  _app.setStartTime(_start_time);
+  _time = _start_time;
   _mfem_problem.transient(true);
 }
 
@@ -53,17 +54,63 @@ MFEMTransient::constructProblemOperator()
   _problem_operator = std::move(problem_operator);
 }
 
+// void
+// MFEMTransient::takeStep(mfem::real_t input_dt)
+// {
+//   _dt_old = _dt;
+
+//   if (input_dt == -1.0)
+//     _dt = computeConstrainedDT();
+//   else
+//     _dt = input_dt;
+
+//   _time_stepper->preSolve();
+
+//   // Increment time
+//   _time = _time_old + _dt;
+
+//   _problem.timestepSetup();
+
+//   _problem.onTimestepBegin();
+
+//   _time_stepper->step();
+//   _xfem_repeat_step = _fixed_point_solve->XFEMRepeatStep();
+
+//   _last_solve_converged = _time_stepper->converged();
+
+//   if (!lastSolveConverged())
+//   {
+//     _console << "Aborting as solve did not converge" << std::endl;
+//     return;
+//   }
+
+//   if (!(_problem.haveXFEM() && _fixed_point_solve->XFEMRepeatStep()))
+//   {
+//     if (lastSolveConverged())
+//       _time_stepper->acceptStep();
+//     else
+//       _time_stepper->rejectStep();
+//   }
+
+//   _time = _time_old;
+
+//   _time_stepper->postSolve();
+
+//   _solution_change_norm =
+//       relativeSolutionDifferenceNorm() / (_normalize_solution_diff_norm_by_dt ? _dt : Real(1));
+// }
+
 void
 MFEMTransient::step(double dt, int) const
 {
   // Check if current time step is final
-  if (_t + dt >= _t_final - dt / 2)
+  if (_time + dt >= _end_time - dt / 2)
   {
     _last_step = true;
   }
 
   // Advance time step.
-  _problem_data.ode_solver->Step(_problem_data.f, _t, dt);
+  _problem_data.ode_solver->Step(_problem_data.f, _time, dt);
 
   // Synchonise time dependent GridFunctions with updated DoF data.
   _problem_operator->SetTestVariablesFromTrueVectors();
@@ -103,8 +150,8 @@ MFEMTransient::execute()
 
   while (_last_step != true)
   {
-    _it++;
-    step(_t_step, _it);
+    _t_step++;
+    step(_dt, _t_step);
   }
 
   _mfem_problem.finishMultiAppStep(EXEC_MULTIAPP_FIXED_POINT_BEGIN,
