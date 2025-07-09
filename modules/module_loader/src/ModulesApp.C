316e34022dada1e755358920931991a9ebc61186
Logan Harbour <loganhharbour@gmail.com>
2025-07-08 20:34:12 -0600

    Re-add call to registerAll
    refs #30778

--- modules/module_loader/src/ModulesApp.C
+++ modules/module_loader/src/ModulesApp.C
@@ -219,6 +219,124 @@ ModulesApp::registerApps()
 #endif
 }
 
+void
+ModulesApp::registerAll(Factory & f, ActionFactory & af, Syntax & s)
+{
+  ::mooseDeprecated(
+      "\"registerAll\" in Modules is deprecated. Please update your *App.C file(s) to call the new "
+      "templated \"registerAllObjects\" method (e.g. ModulesApp::registerAllObjects<MyApp>(...))");
+
+#ifdef CHEMICAL_REACTIONS_ENABLED
+  ChemicalReactionsApp::registerAll(f, af, s);
+#endif
+
+#ifdef CONTACT_ENABLED
+  ContactApp::registerAll(f, af, s);
+#endif
+
+#ifdef ELECTROMAGNETICS_ENABLED
+  ElectromagneticsApp::registerAll(f, af, s);
+#endif
+
+#ifdef FLUID_PROPERTIES_ENABLED
+  FluidPropertiesApp::registerAll(f, af, s);
+#endif
+
+#ifdef FSI_ENABLED
+  FsiApp::registerAll(f, af, s);
+#endif
+
+#ifdef FUNCTIONAL_EXPANSION_TOOLS_ENABLED
+  FunctionalExpansionToolsApp::registerAll(f, af, s);
+#endif
+
+#ifdef GEOCHEMISTRY_ENABLED
+  GeochemistryApp::registerAll(f, af, s);
+#endif
+
+#ifdef HEAT_TRANSFER_ENABLED
+  HeatTransferApp::registerAll(f, af, s);
+#endif
+
+#ifdef LEVEL_SET_ENABLED
+  LevelSetApp::registerAll(f, af, s);
+#endif
+
+#ifdef MISC_ENABLED
+  MiscApp::registerAll(f, af, s);
+#endif
+
+#ifdef NAVIER_STOKES_ENABLED
+  NavierStokesApp::registerAll(f, af, s);
+#endif
+
+#ifdef OPTIMIZATION_ENABLED
+  OptimizationApp::registerAll(f, af, s);
+#endif
+
+#ifdef PERIDYNAMICS_ENABLED
+  PeridynamicsApp::registerAll(f, af, s);
+#endif
+
+#ifdef PHASE_FIELD_ENABLED
+  PhaseFieldApp::registerAll(f, af, s);
+#endif
+
+#ifdef POROUS_FLOW_ENABLED
+  PorousFlowApp::registerAll(f, af, s);
+#endif
+
+#ifdef RAY_TRACING_ENABLED
+  RayTracingApp::registerAll(f, af, s);
+#endif
+
+#ifdef RDG_ENABLED
+  RdgApp::registerAll(f, af, s);
+#endif
+
+#ifdef REACTOR_ENABLED
+  ReactorApp::registerAll(f, af, s);
+#endif
+
+#ifdef RICHARDS_ENABLED
+  RichardsApp::registerAll(f, af, s);
+#endif
+
+#ifdef SCALAR_TRANSPORT_ENABLED
+  ScalarTransportApp::registerAll(f, af, s);
+#endif
+
+#ifdef SOLID_MECHANICS_ENABLED
+  SolidMechanicsApp::registerAll(f, af, s);
+#endif
+
+#ifdef SOLID_PROPERTIES_ENABLED
+  SolidPropertiesApp::registerAll(f, af, s);
+#endif
+
+#ifdef STOCHASTIC_TOOLS_ENABLED
+  StochasticToolsApp::registerAll(f, af, s);
+#endif
+
+#ifdef SUBCHANNEL_ENABLED
+  SubChannelApp::registerAll(f, af, s);
+#endif
+
+#ifdef THERMAL_HYDRAULICS_ENABLED
+  ThermalHydraulicsApp::registerAll(f, af, s);
+#endif
+
+#ifdef XFEM_ENABLED
+  XFEMApp::registerAll(f, af, s);
+#endif
+
+#ifdef EXTERNAL_PETSC_SOLVER_ENABLED
+  ExternalPetscSolverApp::registerAll(f, af, s);
+#endif
+
+  libmesh_ignore(f, s, af);
+}
+
 extern "C" void
 ModulesApp__registerAll(Factory & f, ActionFactory & af, Syntax & s)
 {
