fafa66be44302ee1c468c619976ae0786d94b7c9
Sean Baccas <sean.baccas@ukaea.uk>
2025-07-15 13:54:33 +0100

    Add some basic docu

--- framework/include/mfem/estimators/MFEMZienkiewiczZhuEstimator.h
+++ framework/include/mfem/estimators/MFEMZienkiewiczZhuEstimator.h
@@ -1,14 +1,21 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
 #ifdef MFEM_ENABLED
 
 #pragma once
 
 #include "MFEMEstimator.h"
-#include "MFEMProblemData.h"
 
 class MFEMZienkiewiczZhuEstimator : public MFEMEstimator
 {
 public:
-  // Need reference to problem here as well!!
   MFEMZienkiewiczZhuEstimator(const InputParameters & parameters);
   virtual ~MFEMZienkiewiczZhuEstimator() = default;
 
