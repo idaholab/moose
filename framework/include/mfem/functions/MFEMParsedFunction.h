f022c9732140c4e27cd1808ebd0340294d28a930
karthikeyan chockalingam <karthikeyan.chockalingam@stfc.ac.uk>
2025-07-15 16:07:00 +0100

    add MFEMParsedFunction

---
+++ framework/include/mfem/functions/MFEMParsedFunction.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "FunctionParserUtils.h"
+#include "MFEMProblemData.h"
+#include "MFEMGeneralUserObject.h"
+/**
+ * Declares material properties based on names and values prescribed by input parameters.
+ *
+ * This is identical in function to the GenericConstantMaterial in Moose.
+ */
+class MFEMParsedFunction : public MFEMGeneralUserObject, public FunctionParserUtils<false>
+{
+public:
+  static InputParameters validParams();
+
+  MFEMParsedFunction(const InputParameters & parameters);
+  virtual ~MFEMParsedFunction();
+
+protected:
+  const std::string & _prop_name;
+  /// function expression
+  std::string _function;
+  const std::vector<std::string> & _var_names;
+  unsigned int _num_props;
+  /// import coordinates and time
+  const bool _use_xyzt;
+  /// coordinate and time variable names
+  const std::vector<std::string> _xyzt;
+  MFEMProblemData & _problem_data;
+  /// function parser object for the resudual and on-diagonal Jacobian
+  SymFunctionPtr _func_F;
+
+};
