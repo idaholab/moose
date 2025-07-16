f06f954f9d5efe78c53a91ffeaf316a461653436
Kalin Kiesling <kkiesling@anl.gov>
2025-07-16 13:42:01 -0500

    use MooseEnum for surface type

--- framework/src/csg/CSGSphere.C
+++ framework/src/csg/CSGSphere.C
@@ -16,11 +16,7 @@ CSGSphere::CSGSphere(const std::string & name,
                      const Point & center,
                      const Real r,
                      CSGSurface::BoundaryType boundary)
-  : CSGSurface(name, SurfaceType::SPHERE, boundary),
-    _x0(center(0)),
-    _y0(center(1)),
-    _z0(center(2)),
-    _r(r)
+  : CSGSurface(name, "SPHERE", boundary), _x0(center(0)), _y0(center(1)), _z0(center(2)), _r(r)
 {
   if (r < 0.0 || r == 0.0)
     mooseError("Radius of sphere must be postive.");
