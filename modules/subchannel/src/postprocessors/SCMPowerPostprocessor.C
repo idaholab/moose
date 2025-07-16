24876f6146bfe7423bd1a3e6981dec0b52c101c8
Vasileios Kyriakopoulos <vasileios.kyriakopoulos@inl.gov>
2025-07-16 09:26:03 -0600

    address reviewer comments 2, Refs #30639

--- modules/subchannel/src/postprocessors/SCMPowerPostprocessor.C
+++ modules/subchannel/src/postprocessors/SCMPowerPostprocessor.C
@@ -8,13 +8,8 @@
 //* https://www.gnu.org/licenses/lgpl-2.1.html
 
 #include "SCMPowerPostprocessor.h"
-#include "SolutionHandle.h"
 #include "FEProblemBase.h"
-#include "Function.h"
 #include "MooseMesh.h"
-#include "MooseVariable.h"
-#include "SubProblem.h"
-#include "libmesh/system.h"
 #include "SCM.h"
 #include "SubChannel1PhaseProblem.h"
 
@@ -25,7 +20,7 @@ SCMPowerPostprocessor::validParams()
 {
   InputParameters params = GeneralPostprocessor::validParams();
   params.addClassDescription(
-      "Calculates the total power of the assembly based on the variable q_prime");
+      "Calculates the total power of the assembly based on the distribution of variable q_prime");
   return params;
 }
 
@@ -40,20 +35,15 @@ void
 SCMPowerPostprocessor::execute()
 {
   const auto scm_problem = dynamic_cast<SubChannel1PhaseProblem *>(&_fe_problem);
-  auto nz = _mesh.getNumOfAxialCells();
-  auto n_channels = _mesh.getNumOfChannels();
+  const auto nz = _mesh.getNumOfAxialCells();
+  const auto n_channels = _mesh.getNumOfChannels();
   if (!scm_problem)
     mooseError("SCMPowerPostprocessor can only be used within a subchannel 1phase problem.");
-  auto Power = 0.0;
+  auto power = 0.0;
   for (unsigned int iz = 1; iz < nz + 1; iz++)
-  {
     for (unsigned int i_ch = 0; i_ch < n_channels; i_ch++)
-    {
-      Power += scm_problem->computeAddedHeatPin(i_ch, iz);
-    }
-  }
-
-  _value = Power;
+      power += scm_problem->computeAddedHeatPin(i_ch, iz);
+  _value = power;
 }
 
 Real
