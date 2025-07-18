9579362fe71685a00e76042ba751cc7f437d8acd
Sean Baccas <sean.baccas@ukaea.uk>
2025-07-18 15:57:26 +0100

    Rename MFEMThresholdMarker to MFEMRefinementMarker

--- framework/include/mfem/problem/MFEMProblemData.h
+++ framework/include/mfem/problem/MFEMProblemData.h
@@ -16,7 +16,7 @@
 #include "MFEMContainers.h"
 #include "CoefficientManager.h"
 #include "MFEMSolverBase.h"
-#include "MFEMThresholdMarker.h"
+#include "MFEMRefinementMarker.h"
 #include <fstream>
 #include <iostream>
 
@@ -45,7 +45,7 @@ public:
   Moose::MFEM::TimeDerivativeMap time_derivative_map;
   Moose::MFEM::ComplexGridFunctions cmplx_gridfunctions;
 
-  std::shared_ptr<MFEMThresholdMarker> _refiner;
+  std::shared_ptr<MFEMRefinementMarker> _refiner;
   bool _use_amr{false};
 
   MPI_Comm comm;
