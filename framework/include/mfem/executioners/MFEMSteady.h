a0590e30facc13070eb7b7ebbe7878ee8461b7c8
Alexander Blair <alexander.blair@ukaea.uk>
2025-07-18 18:00:23 +0100

    Fix missing _time_step reference in MFEMSteady. Refs #31017

--- framework/include/mfem/executioners/MFEMSteady.h
+++ framework/include/mfem/executioners/MFEMSteady.h
@@ -38,7 +38,7 @@ private:
   // Time variables used for consistency with MOOSE, needed for outputs.
   // Important for future synchronisation of solves in MultiApps
   Real _system_time;
-  int _time_step;
+  int & _time_step;
   Real & _time;
   /// Iteration number obtained from the main application
   unsigned int _output_iteration_number;
