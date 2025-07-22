146ab29cc68fddd4d89f9e07d606c987215c9bbe
Yaqi Wang <yaqi.wang@inl.gov>
2025-07-22 16:56:13 -0500

    make nearest point user objects take positions objects #31101

--- framework/src/base/Moose.C
+++ framework/src/base/Moose.C
@@ -355,6 +355,7 @@ addActionTypes(Syntax & syntax)
                            "(add_default_nonlinear_convergence,"
                            " add_default_multiapp_fixed_point_convergence,"
                            " add_default_steady_state_convergence)"
+                           "(add_positions)"
                            "(add_periodic_bc)"
                            "(add_user_object, add_corrector, add_mesh_modifier)"
                            "(add_distribution)"
@@ -373,7 +374,6 @@ addActionTypes(Syntax & syntax)
                            "(setup_dampers)"
                            "(setup_residual_debug)"
                            "(add_bounds_vectors)"
-                           "(add_positions)"
                            "(add_mesh_division)"  // NearestPositionsDivision uses a Positions
                            "(add_multi_app)"
                            "(add_transfer)"
