2f080affa87c89235e063e39c6f4592901d4b0a7
Sean Baccas <sean.baccas@ukaea.uk>
2025-07-24 12:02:50 +0100

    Docu

--- framework/include/mfem/indicators/MFEMIndicator.h
+++ framework/include/mfem/indicators/MFEMIndicator.h
@@ -14,34 +14,41 @@ class MFEMIndicator : public MFEMGeneralUserObject
 public:
   static InputParameters validParams();
 
-  // also need reference to mfem problem here
   MFEMIndicator(const InputParameters & params);
 
   virtual ~MFEMIndicator() = default;
 
-  // Get name of the test variable labelling the weak form this kernel is added to
+  /// Get name of the test variable labelling the weak form this kernel is added to
   const std::string & getTestVariableName() const { return _variable_name; }
 
-  // Create the estimator internally and return a bool to indicate if it
-  // succeeded. This base class should not be used directly, so we return
-  // false here.
+  /// Create the estimator internally and return a bool to indicate if it
+  /// succeeded. This base class should not be used directly, so we return
+  /// false here.
   virtual bool createEstimator() { return false; }
 
-  // Get shared pointer to FE Space using the name we store when setting up this class
+  /// Get shared pointer to FE Space using the name we store when setting up this class
   virtual std::shared_ptr<mfem::ParFiniteElementSpace> getFESpace() const;
 
-  // Method to fetch the error estimator after creation
-  std::shared_ptr<mfem::ErrorEstimator> getEstimator() const { return _error_estimator; }
+  /// Method to fetch the error estimator after creation
+  std::shared_ptr<mfem::ErrorEstimator> getEstimator() const; { return _error_estimator; }
 
 protected:
-  // Name of (the test variable associated with) the weak form that the kernel is applied to.
+  /// Name of (the test variable associated with) the weak form that the kernel is applied to.
   std::string _variable_name;
 
-  // reference to the above variable (for fetching the the FESpace from)
+  /// Reference to the the variable we refine on. Use this refernce for fetching a reference
+  /// to the relevant FE space
   const MFEMVariable & _variable;
 
   std::string _kernel_name;
   std::shared_ptr<mfem::ErrorEstimator> _error_estimator;
 };
+
+inline std::shared_ptr<mfem::ErrorEstimator>
+MFEMIndicator::getEstimator() const
+{
+  mooseAssert(_error_estimator, "Attempting to retrieve error estimator before it's been constructed");
+  return _error_estimator;
+}
 
 #endif
