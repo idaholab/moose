2666b7a7b575b077300e9f48e7a9bb8e4ac19cec
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2025-07-27 07:44:12 -0600

    Save relaxed and limited solution as old solution when using relaxation

--- modules/navier_stokes/include/executioners/LinearAssemblySegregatedSolve.h
+++ modules/navier_stokes/include/executioners/LinearAssemblySegregatedSolve.h
@@ -57,15 +57,17 @@ protected:
   /// @param relaxation_factor The relaxation factor for matrix relaxation
   /// @param solver_config The solver configuration object for the linear solve
   /// @param abs_tol The scaled absolute tolerance for the linear solve
-  /// @param relax_fields (optional) A boolean flag to indicate whether to relax fields during the solve. Default value is false.
   /// @param field_relaxation (optional) The relaxation factor for fields if relax_fields is true. Default value is 1.0.
+  /// @param min_value_limiter (optional) The minimum value for the solution field
   /// @return The normalized residual norm of the equation.
-  std::pair<unsigned int, Real> solveAdvectedSystem(const unsigned int system_num,
-                                                    LinearSystem & system,
-                                                    const Real relaxation_factor,
-                                                    libMesh::SolverConfiguration & solver_config,
-                                                    const Real abs_tol,
-                                                    const Real field_relaxation = 1.0);
+  std::pair<unsigned int, Real>
+  solveAdvectedSystem(const unsigned int system_num,
+                      LinearSystem & system,
+                      const Real relaxation_factor,
+                      libMesh::SolverConfiguration & solver_config,
+                      const Real abs_tol,
+                      const Real field_relaxation = 1.0,
+                      const Real min_value_limiter = std::numeric_limits<Real>::min());
 
   /// Solve an equation which contains the solid energy conservation.
   std::pair<unsigned int, Real> solveSolidEnergy();
