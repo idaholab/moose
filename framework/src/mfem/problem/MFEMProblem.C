6bf778a2c6470e93b138c4c586823a4f27c732ac
“Henrique <henrique.bergallo-rocha@ukaea.uk>
2025-07-28 15:14:18 +0100

    Remove banned words

--- framework/src/mfem/problem/MFEMProblem.C
+++ framework/src/mfem/problem/MFEMProblem.C
@@ -94,7 +94,6 @@ MFEMProblem::addBoundaryCondition(const std::string & bc_name,
   const UserObject * mfem_bc_uo = &(getUserObjectBase(name));
 
   std::string action_name = _app.actionWarehouse().getCurrentActionName();
-  std::cout << "Action name = " << action_name << std::endl;
 
   if (dynamic_cast<const MFEMComplexIntegratedBC *>(mfem_bc_uo) != nullptr)
   {
@@ -165,7 +164,6 @@ MFEMProblem::addAuxBoundaryCondition(const std::string & bc_name,
                                      const std::string & name,
                                      InputParameters & parameters)
 {
-  std::cout << "Adding object with name: " << name << std::endl;
   FEProblemBase::addUserObject(bc_name, name, parameters);
 }
 
