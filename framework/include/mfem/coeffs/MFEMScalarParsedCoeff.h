84558fd9e3abd43d41c1261920d9de2db98aded3
karthikeyan chockalingam <karthikeyan.chockalingam@stfc.ac.uk>
2025-07-29 14:50:13 +0100

    Apply clang format. Refs#31145

--- framework/include/mfem/coeffs/MFEMScalarParsedCoeff.h
+++ framework/include/mfem/coeffs/MFEMScalarParsedCoeff.h
@@ -15,22 +15,19 @@
 #include <vector>
 #include <functional>
 
-
-
 class MFEMScalarParsedCoeff : public mfem::Coefficient
 {
-  private:
-    const Moose::MFEM::GridFunctions  & _gFuncs;
-    const std::vector<std::string> & _inputs;
-    bool _use_xyzt;
-    const FunctionParserUtils<false>::SymFunctionPtr & _func;
+private:
+  const Moose::MFEM::GridFunctions & _gFuncs;
+  const std::vector<std::string> & _inputs;
+  bool _use_xyzt;
+  const FunctionParserUtils<false>::SymFunctionPtr & _func;
 
+public:
+  MFEMScalarParsedCoeff(const Moose::MFEM::GridFunctions & gFuncs,
+                        const std::vector<std::string> & inputs,
+                        bool use_xyzt,
+                        const FunctionParserUtils<false>::SymFunctionPtr & func);
 
-  public:
-    MFEMScalarParsedCoeff(const Moose::MFEM::GridFunctions  & gFuncs
-                 , const std::vector<std::string> & inputs
-                 , bool use_xyzt
-                 , const FunctionParserUtils<false>::SymFunctionPtr & func);
-
-    double Eval(mfem::ElementTransformation &T, const mfem::IntegrationPoint &ip);
+  double Eval(mfem::ElementTransformation & T, const mfem::IntegrationPoint & ip);
 };
