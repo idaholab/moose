5c45f2ad296d75a1bbbfc857f2abd332f0e71fbb
Liam Pohlmann <liam.pohlmann@inl.gov>
2025-07-29 21:16:13 -0600

    First working draft of 3D CylinderComponent.
     - Added new method for creating rotation matrix between two vectors.
     - Updated ComponentMeshTransformHelper to rotate correctly.
     - Added new capabilities for rotation in TransformGenerator using either extrinsic angles or a given rotation matrix.
     - Created simple test file for verifying everything works.
    
    closes #31157

--- framework/include/meshgenerators/TransformGenerator.h
+++ framework/include/meshgenerators/TransformGenerator.h
@@ -29,4 +29,10 @@ protected:
   std::unique_ptr<MeshBase> & _input;
   /// the transform to apply to the mesh
   const MooseEnum _transform;
+
+  /// @brief Rotate the mesh using extrinsic rotation with given angles. See https://en.wikipedia.org/wiki/Euler_angles, Proper Euler Angles 'ZXZ'.
+  void rotateExtrinsic(MeshBase & mesh, const Real alpha, const Real beta, const Real gamma);
+
+  /// @brief Rotate a mesh using a given rotation matrix. See above Wiki page for various formulae.
+  void rotateWithMatrix(MeshBase & mesh, const GenericRealTensorValue<false> & rotation_matrix);
 };
