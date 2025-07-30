bdc9c1dc2c8ec0780d65144c38f622589e74baa3
“Henrique <henrique.bergallo-rocha@ukaea.uk>
2025-07-30 16:53:52 +0100

    Add phase parameter to ComplexKernel
    Linting

--- framework/include/mfem/kernels/MFEMComplexKernel.h
+++ framework/include/mfem/kernels/MFEMComplexKernel.h
@@ -36,14 +36,8 @@ public:
   {
     return _imag_kernel->createLFIntegrator();
   }
-  virtual mfem::BilinearFormIntegrator * getRealBFIntegrator()
-  {
-    return _real_kernel->createBFIntegrator();
-  }
-  virtual mfem::BilinearFormIntegrator * getImagBFIntegrator()
-  {
-    return _imag_kernel->createBFIntegrator();
-  }
+  virtual mfem::BilinearFormIntegrator * getRealBFIntegrator();
+  virtual mfem::BilinearFormIntegrator * getImagBFIntegrator();
 
   virtual void setRealKernel(std::shared_ptr<MFEMKernel> kernel) { _real_kernel = kernel; }
   virtual void setImagKernel(std::shared_ptr<MFEMKernel> kernel) { _imag_kernel = kernel; }
