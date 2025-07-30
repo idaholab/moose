a3deb6d99981fd9b05aa920476ddd9a9c147e0a4
“Henrique <henrique.bergallo-rocha@ukaea.uk>
2025-07-30 16:53:52 +0100

    Add phase parameter to ComplexKernel
    Linting

--- framework/src/mfem/kernels/MFEMComplexKernel.C
+++ framework/src/mfem/kernels/MFEMComplexKernel.C
@@ -11,6 +11,7 @@
 
 #include "MFEMComplexKernel.h"
 #include "MFEMProblem.h"
+#include "ScaleIntegrator.h"
 
 registerMooseObject("MooseApp", MFEMComplexKernel);
 
@@ -24,6 +25,11 @@ MFEMComplexKernel::validParams()
                                 "Variable labelling the weak form this kernel is added to");
   params.addClassDescription(
       "Holds MFEMKernel objects for the real and imaginary parts of a complex kernel.");
+  params.addParam<mfem::real_t>("phase",
+                                0.,
+                                "Phase shift angle to apply to the bilinear form kernels, measured "
+                                "in radians. Both the real and imaginary parts get multiplied by a "
+                                "factor of exp(i*phase). Does not affect linear form kernels");
 
   return params;
 }
@@ -31,6 +37,34 @@ MFEMComplexKernel::validParams()
 MFEMComplexKernel::MFEMComplexKernel(const InputParameters & parameters)
   : MFEMKernel(parameters), _test_var_name(getParam<VariableName>("variable"))
 {
+}
+
+mfem::BilinearFormIntegrator *
+MFEMComplexKernel::getRealBFIntegrator()
+{
+  mfem::real_t phase = getParam<mfem::real_t>("phase");
+  Moose::MFEM::ScaleIntegrator scaled_real(_real_kernel->createBFIntegrator(), cos(phase));
+  Moose::MFEM::ScaleIntegrator scaled_imag(_imag_kernel->createBFIntegrator(), -sin(phase));
+
+  mfem::SumIntegrator * shifted_integ = new mfem::SumIntegrator();
+  shifted_integ->AddIntegrator(&scaled_real);
+  shifted_integ->AddIntegrator(&scaled_imag);
+
+  return shifted_integ;
+}
+
+mfem::BilinearFormIntegrator *
+MFEMComplexKernel::getImagBFIntegrator()
+{
+  mfem::real_t phase = getParam<mfem::real_t>("phase");
+  Moose::MFEM::ScaleIntegrator scaled_real(_real_kernel->createBFIntegrator(), sin(phase));
+  Moose::MFEM::ScaleIntegrator scaled_imag(_imag_kernel->createBFIntegrator(), cos(phase));
+
+  mfem::SumIntegrator * shifted_integ = new mfem::SumIntegrator();
+  shifted_integ->AddIntegrator(&scaled_real);
+  shifted_integ->AddIntegrator(&scaled_imag);
+
+  return shifted_integ;
 }
 
 #endif
