407ad92345c503c60ce538f604ecb4a6d002fcf4
Ansh Chaube <afc6440@psu.edu>
2025-08-01 17:07:23 -0600

    rebase

---
+++ modules/navier_stokes/include/linearfvbcs/LinearFVConjugateHeatTransferBC.h
@@ -0,0 +1,41 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "LinearFVConvectiveHeatTransferBC.h"
+
+/**
+ * Class describing a convective heat transfer between two domains.
+ * The heat flux is described  by:
+ * h * (T_solid - T_liquid),
+ * where h is the heat transfer coefficient, while T_solid and T_liquid
+ * denote the solid and liquid temperatures, respectively.
+ */
+class LinearFVConjugateHeatTransferBC : public LinearFVConvectiveHeatTransferBC
+{
+public:
+  static InputParameters validParams();
+
+  /**
+   * Class constructor.
+   * @param parameters The InputParameters for the object
+   */
+  LinearFVConjugateHeatTransferBC(const InputParameters & parameters);
+
+  virtual Real computeBoundaryGradientRHSContribution() const override;
+
+protected:
+
+  const Moose::Functor<Real> & _solid_conductivity;
+
+  const Moose::Functor<Real> & _fluid_conductivity;
+
+  const Moose::Functor<Real> * _rhs_conductivity;
+};
