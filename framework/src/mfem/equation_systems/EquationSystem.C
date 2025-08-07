2818c783362ee438ab8264cdaf96dc26542fe823
“Henrique <henrique.bergallo-rocha@ukaea.uk>
2025-08-07 16:34:00 +0100

    Simplify ComplexEquationSystem

--- framework/src/mfem/equation_systems/EquationSystem.C
+++ framework/src/mfem/equation_systems/EquationSystem.C
@@ -355,13 +355,29 @@ EquationSystem::GetGradient(const mfem::Vector &) const
 void
 EquationSystem::RecoverFEMSolution(mfem::BlockVector & trueX,
                                    Moose::MFEM::GridFunctions & gridfunctions,
-                                   Moose::MFEM::ComplexGridFunctions & /*cpx_gridfunctions*/)
+                                   Moose::MFEM::ComplexGridFunctions & cpx_gridfunctions)
 {
-  for (const auto i : index_range(_trial_var_names))
+  if (gridfunctions.size())
   {
-    auto & trial_var_name = _trial_var_names.at(i);
-    trueX.GetBlock(i).SyncAliasMemory(trueX);
-    gridfunctions.Get(trial_var_name)->Distribute(&(trueX.GetBlock(i)));
+    for (const auto i : index_range(_trial_var_names))
+    {
+      auto & trial_var_name = _trial_var_names.at(i);
+      trueX.GetBlock(i).SyncAliasMemory(trueX);
+      gridfunctions.Get(trial_var_name)->Distribute(&(trueX.GetBlock(i)));
+    }
+  }
+  else if (cpx_gridfunctions.size())
+  {
+    for (const auto i : index_range(_trial_var_names))
+    {
+      auto & trial_var_name = _trial_var_names.at(i);
+      trueX.GetBlock(i).SyncAliasMemory(trueX);
+      cpx_gridfunctions.Get(trial_var_name)->Distribute(&(trueX.GetBlock(i)));
+    }
+  }
+  else
+  {
+    mooseError("No gridfunctions provided to recover solution from the equation system.");
   }
 }
 
