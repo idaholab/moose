b7a89e2a40b717c7ff57bfcf912ada67bcbc61bd
Nuno Nobre <nuno.nobre@stfc.ac.uk>
2025-08-12 10:34:56 +0100

    Fix typos/inconsistencies/redundancies in docs/descriptions/includes

--- framework/src/mfem/auxkernels/MFEMVectorProjectAux.C
+++ framework/src/mfem/auxkernels/MFEMVectorProjectAux.C
@@ -1,7 +1,6 @@
 #ifdef MOOSE_MFEM_ENABLED
 
 #include "MFEMVectorProjectAux.h"
-#include "MFEMProblem.h"
 
 registerMooseObject("MooseApp", MFEMVectorProjectAux);
 
@@ -9,14 +8,14 @@ InputParameters
 MFEMVectorProjectAux::validParams()
 {
   InputParameters params = MFEMAuxKernel::validParams();
-  params.addClassDescription("Projects a Vector Coefficient into a vector MFEMVariable.");
-  params.addRequiredParam<MFEMVectorCoefficientName>("coefficient",
-                                                     "Name of the Vector Coefficient to project.");
+  params.addClassDescription("Projects a vector coefficient onto a vector MFEMVariable.");
+  params.addRequiredParam<MFEMVectorCoefficientName>("vector_coefficient",
+                                                     "Name of the vector coefficient to project.");
   return params;
 }
 
 MFEMVectorProjectAux::MFEMVectorProjectAux(const InputParameters & parameters)
-  : MFEMAuxKernel(parameters), _vec_coef(getVectorCoefficient("coefficient"))
+  : MFEMAuxKernel(parameters), _vec_coef(getVectorCoefficient("vector_coefficient"))
 
 {
 }
@@ -24,7 +23,6 @@ MFEMVectorProjectAux::MFEMVectorProjectAux(const InputParameters & parameters)
 void
 MFEMVectorProjectAux::execute()
 {
-
   _result_var = 0.0;
   _result_var.ProjectCoefficient(_vec_coef);
 }
