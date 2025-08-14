515bd1a58ddf07efc08940eb35d626619cd19738
Shantanu Avinash Vachhani <ShantanuAvinash.Vachhani@inl.gov>
2025-08-14 11:04:37 -0600

    Adding tests for Phase field - Navier Stokes kernels(#31300)

--- modules/phase_field/src/materials/PhaseFieldTwoPhaseMaterial.C
+++ modules/phase_field/src/materials/PhaseFieldTwoPhaseMaterial.C
@@ -1,9 +1,12 @@
-/****************************************************************/
-/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
-/*                                                              */
-/*          All contents are licensed under LGPL V2.1           */
-/*             See LICENSE for full restrictions                */
-/****************************************************************/
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
 #include "PhaseFieldTwoPhaseMaterial.h"
 
 registerADMooseObject("PhaseFieldApp", PhaseFieldTwoPhaseMaterial);
