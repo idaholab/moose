cedc9a2f768a77ed3a8d31612e078fc6266514cb
Cheng-Hau Yang <chenghau.yang@anl.gov>
2025-08-16 03:17:00 -0500

    [Patch Recovery] Switch to NodalPatchRecoveryVariable for variable-specific patch recovery
    - Use NodalPatchRecoveryVariable instead of the base class in ESMBase.
    - Add class description to fix documentation errors.
    - Assert variable name matches during setup.

--- framework/include/userobjects/NodalPatchRecoveryVariable.h
+++ framework/include/userobjects/NodalPatchRecoveryVariable.h
@@ -10,6 +10,9 @@ public:
   static InputParameters validParams();
   NodalPatchRecoveryVariable(const InputParameters & params);
 
+  /// Returns the variable name
+  const VariableName & variableName() const;
+
 protected:
   /// Returns the recovered value at the quadrature point (_qp)
   Real computeValue() override;
@@ -17,4 +20,7 @@ protected:
 private:
   /// Variable value
   const VariableValue & _v;
+
+  /// Variable name
+  const VariableName _name;
 };
