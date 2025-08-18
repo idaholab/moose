b8b80b9353474be68ffb61fe26169bd5dfb13f07
karthichockalingam <39724615+karthichockalingam@users.noreply.github.com>
2025-08-18 20:16:32 +0100

    Update framework/include/mfem/functions/MFEMScalarParsedCoeff.h
    Co-authored-by: Nuno Nobre <nuno.nobre@stfc.ac.uk>

--- framework/include/mfem/functions/MFEMScalarParsedCoeff.h
+++ framework/include/mfem/functions/MFEMScalarParsedCoeff.h
@@ -1,11 +1,11 @@
-/***********************************************\
-!        Construct a non-linear parsed          !
-!        grid function coefficient              !
-!                                               !
-\***********************************************/
-
-// Define a coefficient that, given a grid function u,
-// function func, returns func(u)
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
 
 #ifdef MOOSE_MFEM_ENABLED
 
