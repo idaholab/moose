9aaa62c0be0c1272346ebebc7e95d842c7de11f4
Ioannis-UKAEA <ic@192.168.1.198>
2025-08-18 20:22:40 +0100

    Fixed moose_mfem_enabled fences, removed mfem.hpp, use mfem:SumCoefficient to avoid the nested class and now retrieving dt() from getParams(),add the test to the preexisting kernel/heattransfer with inactive blocks, fixed typos and added the copyright and license header to .C and .h. Refs #31163
    Co-authored-by: Nuno Nobre <nuno.nobre@stfc.ac.uk>

--- framework/include/mfem/auxkernels/MFEMAverageFieldAux.h
+++ framework/include/mfem/auxkernels/MFEMAverageFieldAux.h
@@ -1,24 +1,31 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifdef MOOSE_MFEM_ENABLED
+
 #pragma once
-
-#ifdef MFEM_ENABLED
-n
 #include "MFEMAuxKernel.h"
-#include "mfem.hpp"
 
-    /**
-     * AuxKernel to compute a time-weighted running average of MFEMVariable
-     * via an mfem::Coefficient and ProjectCoefficient().
-     *
-     *   avg_new(x) = (1 - w)*avg_old(x) + w*src(x), w = dt / (t - skip)
-     */
-    class MFEMAverageFieldAux : public MFEMAuxKernel
+/**
+ * AuxKernel to compute a time-weighted running average of an MFEMVariable
+ * using a linear blend.
+ *
+ *   avg_new(x) = (1 - w)*avg_old(x) + w*src(x), w = dt / (t - skip)
+ */
+class MFEMAverageFieldAux : public MFEMAuxKernel
 {
 public:
   static InputParameters validParams();
 
   MFEMAverageFieldAux(const InputParameters & parameters);
 
-  virtual ~MFEMAverageFieldAux() = default;
+  virtual ~MFEMAverageFieldAux() override = default;
 
   virtual void execute() override;
 
@@ -27,43 +34,8 @@ protected:
   const VariableName _source_var_name;
   /// Reference to the MFEMVariable underlying GridFunction
   const mfem::ParGridFunction & _source_var;
-  /// Solver time step size
-  const mfem::real_t _dt;
   /// Time before the averaging starts
   const mfem::real_t _skip;
-
-  /**
-   * a derived class from mfem::Coefficient to inherit the mfem::Coefficient eval function ,
-   *  which reads the old average saved in the AuxVariable and new field value from the source
-   * gridfunction and returns: (1 - w)*previous_average + w * current_field with w = dt/(t - skip).
-   */
-  class AvgCoef : public mfem::Coefficient
-  {
-  public:
-    AvgCoef(mfem::ParGridFunction & avg_gf,
-            const mfem::ParGridFunction & src_gf,
-            mfem::real_t time,
-            mfem::real_t dt,
-            mfem::real_t skip)
-      : _avg_gf(avg_gf), _src_gf(src_gf), _time_ref(time), _dt(dt), _skip(skip)
-    {
-    }
-    virtual ~AvgCoef();
-    virtual double Eval(mfem::ElementTransformation & T, const mfem::IntegrationPoint & ip) override
-    {
-      const double old_v = _avg_gf.GetValue(T, ip);
-      const double new_v = _src_gf.GetValue(T, ip);
-      const double w = (_time_ref > _skip) ? (_dt / (_time_ref - _skip)) : 0.0;
-      return (1.0 - w) * old_v + w * new_v;
-    }
-
-  private:
-    mfem::ParGridFunction & _avg_gf;
-    const mfem::ParGridFunction & _src_gf;
-    mfem::real_t _time_ref;
-    const mfem::real_t _dt;
-    const mfem::real_t _skip;
-  };
 };
 
 #endif
--- framework/src/mfem/auxkernels/MFEMAverageFieldAux.C
+++ framework/src/mfem/auxkernels/MFEMAverageFieldAux.C
@@ -1,4 +1,13 @@
-#ifdef MFEM_ENABLED
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifdef MOOSE_MFEM_ENABLED
 
 #include "MFEMAverageFieldAux.h"
 #include "MFEMProblem.h"
@@ -10,9 +19,8 @@ MFEMAverageFieldAux::validParams()
 {
   InputParameters params = MFEMAuxKernel::validParams();
   params.addClassDescription(
-      "Time-weighted running average of a MFEMVariable, projected into an AuxVariable.");
+      "Time-weighted running average of an MFEMVariable, projected into an AuxVariable.");
   params.addRequiredParam<VariableName>("source", "Name of the MFEMVariable to average from.");
-  params.addRequiredParam<mfem::real_t>("time_step", "Simulation time-step size dt.");
   params.addParam<mfem::real_t>("timestep_skip", 0.0, "Time to skip before beginning the average.");
 
   return params;
@@ -22,7 +30,6 @@ MFEMAverageFieldAux::MFEMAverageFieldAux(const InputParameters & parameters)
   : MFEMAuxKernel(parameters),
     _source_var_name(getParam<VariableName>("source")),
     _source_var(*getMFEMProblem().getProblemData().gridfunctions.Get(_source_var_name)),
-    _dt(getParam<mfem::real_t>("time_step")),
     _skip(getParam<mfem::real_t>("timestep_skip"))
 //)
 {
@@ -31,16 +38,23 @@ MFEMAverageFieldAux::MFEMAverageFieldAux(const InputParameters & parameters)
 void
 MFEMAverageFieldAux::execute()
 {
-  // project AvgCoef; it will read the old average from _result_var
-  // and the current field from _source_var, compute w = dt/(t-skip), and
+  // project the average value using linear blend mfem::SumCoeffient calculated
+  // from the current _source_var, with a time-average weight w = dt/(t-skip), and
   // return (1-w)*old + w*new for each integration point.
   mfem::real_t t = getMFEMProblem().time();
+  mfem::real_t dt = getMFEMProblem().dt();
+  // weight 0 until averaging starts, then dt/(t - _skip)
+  const mfem::real_t w = (t > _skip) ? (dt / (t - _skip)) : 0.0;
+  // Snapshot the previous average to avoid read/write aliasing during projection
+  mfem::ParGridFunction old_copy(_result_var);
+  // Wrap snapshot and source as coefficients
+  mfem::GridFunctionCoefficient old_avg(&old_copy);
+  mfem::GridFunctionCoefficient src(&_source_var);
 
-  AvgCoef coef(_result_var, _source_var, t, _dt, _skip);
+  // Linear Blend (1-w)*old + w*src
+  mfem::SumCoefficient blend(old_avg, src, (1.0 - w), w);
 
-  _result_var.ProjectCoefficient(coef);
+  _result_var.ProjectCoefficient(blend);
 }
-
-MFEMAverageFieldAux::AvgCoef::~AvgCoef() {}
 
 #endif
