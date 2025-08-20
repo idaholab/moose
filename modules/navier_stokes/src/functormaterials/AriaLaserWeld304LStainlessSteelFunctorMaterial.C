22d7effa68e55eee208803f73a36690f36a0f6ef
Kamalnath <Kamal@WSL_Ubuntu_20>
2025-08-20 08:13:57 -0500

    Autogen patch applied

--- modules/navier_stokes/src/functormaterials/AriaLaserWeld304LStainlessSteelFunctorMaterial.C
+++ modules/navier_stokes/src/functormaterials/AriaLaserWeld304LStainlessSteelFunctorMaterial.C
@@ -6,7 +6,7 @@
 //*
 //* Licensed under LGPL 2.1, please see LICENSE for details
 //* https://www.gnu.org/licenses/lgpl-2.1.html
-  
+
 #include "AriaLaserWeld304LStainlessSteelFunctorMaterial.h"
 #include "NS.h"
 
