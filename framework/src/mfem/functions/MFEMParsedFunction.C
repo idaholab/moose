b65c812fe875c600e667266cfd7529362e49a0a9
karthichockalingam <39724615+karthichockalingam@users.noreply.github.com>
2025-08-24 18:57:18 +0100

    Update framework/src/mfem/functions/MFEMParsedFunction.C
    Co-authored-by: Nuno Nobre <nuno.nobre@stfc.ac.uk>

--- framework/src/mfem/functions/MFEMParsedFunction.C
+++ framework/src/mfem/functions/MFEMParsedFunction.C
@@ -42,7 +42,7 @@ MFEMParsedFunction::MFEMParsedFunction(const InputParameters & parameters)
   : MFEMGeneralUserObject(parameters),
     FunctionParserUtils(parameters),
     _function(getParam<std::string>("expression")),
-    _var_names(getParam<std::vector<std::string>>("var_names")),
+    _var_names(getParam<std::vector<VariableName>>("var_names")),
     _use_xyzt(getParam<bool>("use_xyzt")),
     _xyzt({"x", "y", "z", "t"})
 {
