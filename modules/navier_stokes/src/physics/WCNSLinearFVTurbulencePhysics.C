b0694895f0238bcdbeb846ab9d163d5befd41572
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2025-08-26 21:59:31 -0600

    Add a good default for system parameter for linearFV turbulence physics Add missing orthogonality correction parameter Remove advection kernel for momentum since it will be done in FlowPhysics Change the variable type to linear Remove now-removed parameters from source & sink kernels refs #29175

--- modules/navier_stokes/src/physics/WCNSLinearFVTurbulencePhysics.C
+++ modules/navier_stokes/src/physics/WCNSLinearFVTurbulencePhysics.C
@@ -26,18 +26,40 @@ WCNSLinearFVTurbulencePhysics::validParams()
       "Define a turbulence model for a incompressible or weakly-compressible Navier Stokes "
       "flow with a linear finite volume discretization");
 
+  params.addParam<std::vector<SolverSystemName>>(
+      "system_names",
+      {"TKE_system", "TKED_system"},
+      "Names of the linear solver systems for each equation. Default is set for K-Epsilon and "
+      "should be adapted for other models");
+
   // Not an option
   params.addParam<bool>("mu_t_as_aux_variable",
                         true,
                         "Whether to use an auxiliary variable instead of a functor material "
                         "property for the turbulent viscosity");
   params.suppressParameter<bool>("mu_t_as_aux_variable");
+  params.suppressParameter<bool>("turbulent_viscosity_two_term_bc_expansion");
+
+  // Could be implemented when boundary conditions are implemented in turbulence physics
+  params.suppressParameter<bool>("tke_two_term_bc_expansion");
+  params.suppressParameter<bool>("tked_two_term_bc_expansion");
+
+  // LinearFV-specific parameters
+  params.addParam<bool>(
+      "use_nonorthogonal_correction",
+      true,
+      "Whether to use a non-orthogonal correction. This can potentially slow down convergence "
+      ", but reduces numerical dispersion on non-orthogonal meshes. Can be safely turned off on "
+      "orthogonal meshes.");
+  params.addParamNamesToGroup("use_nonorthogonal_correction", "Numerical scheme");
   return params;
 }
 
 WCNSLinearFVTurbulencePhysics::WCNSLinearFVTurbulencePhysics(const InputParameters & parameters)
   : WCNSFVTurbulencePhysicsBase(parameters)
 {
+  if (_turbulence_model != "k-epsilon")
+    errorDependentParameter("turbulence_handling", "k-epsilon", {"use_nonorthogonal_correction"});
 }
 
 void
@@ -57,7 +79,7 @@ WCNSLinearFVTurbulencePhysics::addSolverVariables()
     // Dont add if the user already defined the variable
     // Add turbulent kinetic energy variable
     if (!shouldCreateVariable(_tke_name, _blocks, /*error if aux*/ true))
-      reportPotentiallyMissedParameters({"system_names"}, "INSFVEnergyVariable");
+      reportPotentiallyMissedParameters({"system_names"}, "MooseLinearVariableFVReal");
     else if (_define_variables)
     {
       std::string variable_type = "MooseLinearVariableFVReal";
@@ -75,7 +97,7 @@ WCNSLinearFVTurbulencePhysics::addSolverVariables()
 
     // Add turbulent kinetic energy dissipation variable
     if (!shouldCreateVariable(_tked_name, _blocks, /*error if aux*/ true))
-      reportPotentiallyMissedParameters({"system_names"}, "INSFVEnergyVariable");
+      reportPotentiallyMissedParameters({"system_names"}, "MooseLinearVariableFVReal");
     else if (_define_variables)
     {
       std::string variable_type = "MooseLinearVariableFVReal";
@@ -123,35 +145,12 @@ WCNSLinearFVTurbulencePhysics::addFlowTurbulenceKernels()
 {
   if (_turbulence_model == "k-epsilon")
   {
-    const std::string u_names[3] = {"u", "v", "w"};
-    std::string kernel_type = "LinearWCNSFVMomentumFlux";
-    std::string kernel_name = prefix() + "ins_momentum_turbulent_flux_";
+    // Simpler to add the kernel in the flow physics with mu_eff instead of two kernels
+    // one with mu, one with mu_turb, and chose one of the two to NOT have the advective term
 
-    InputParameters params = getFactory().getValidParams(kernel_type);
-    assignBlocks(params, _blocks);
-    params.set<MooseFunctorName>(NS::mu) = _flow_equations_physics->dynamicViscosityName();
-    params.set<UserObjectName>("rhie_chow_user_object") = _flow_equations_physics->rhieChowUOName();
-    params.set<MooseEnum>("advected_interp_method") =
-        _flow_equations_physics->getMomentumAdvectionFaceInterpolationMethod();
-    params.set<bool>("use_nonorthogonal_correction") =
-        _flow_equations_physics->getParam<bool>("use_nonorthogonal_correction");
-    params.set<bool>("use_deviatoric_terms") =
-        _flow_equations_physics->getParam<bool>("include_deviatoric_stress");
-    params.set<bool>("add_advective_term") = false;
-
-    for (unsigned int i = 0; i < dimension(); ++i)
-      params.set<SolverVariableName>(u_names[i]) = _velocity_names[i];
-
-    for (const auto d : make_range(dimension()))
-    {
-      params.set<LinearVariableName>("variable") = _velocity_names[d];
-      params.set<MooseEnum>("momentum_component") = NS::directions[d];
-
-      getProblem().addLinearFVKernel(kernel_type, kernel_name + NS::directions[d], params);
-    }
-    if (!_flow_equations_physics->getParam<bool>("use_deviatoric_term"))
+    if (!_flow_equations_physics->getParam<bool>("include_deviatoric_stress"))
       _flow_equations_physics->paramWarning(
-          "use_deviatoric_term", "This should be set to true when using a turbulence model");
+          "include_deviatoric_stress", "This should be set to true when using a turbulence model");
   }
 }
 
@@ -166,13 +165,12 @@ WCNSLinearFVTurbulencePhysics::addFluidEnergyTurbulenceKernels()
     assignBlocks(params, _blocks);
     if (!_fluid_energy_physics->getParam<bool>("solve_for_enthalpy"))
     {
-      params.set<NonlinearVariableName>("variable") = T_fluid_name;
+      params.set<LinearVariableName>("variable") = T_fluid_name;
       params.set<MooseFunctorName>("diffusion_coeff") = NS::k_t;
     }
     else
     {
-      params.set<NonlinearVariableName>("variable") =
-          _fluid_energy_physics->getSpecificEnthalpyName();
+      params.set<LinearVariableName>("variable") = _fluid_energy_physics->getSpecificEnthalpyName();
       params.set<MooseFunctorName>("diffusion_coeff") = NS::k_t + "_by_cp";
     }
     params.set<bool>("use_nonorthogonal_correction") =
@@ -204,7 +202,7 @@ WCNSLinearFVTurbulencePhysics::addScalarAdvectionTurbulenceKernels()
 
     for (const auto & name_i : index_range(passive_scalar_names))
     {
-      params.set<NonlinearVariableName>("variable") = passive_scalar_names[name_i];
+      params.set<LinearVariableName>("variable") = passive_scalar_names[name_i];
       params.set<MooseFunctorName>("diffusion_coeff") = NS::mu_t_passive_scalar;
       getProblem().addLinearFVKernel(
           kernel_type, prefix() + passive_scalar_names[name_i] + "_turbulent_diffusion", params);
@@ -219,10 +217,10 @@ WCNSLinearFVTurbulencePhysics::addKEpsilonTimeDerivatives()
   InputParameters params = getFactory().getValidParams(kernel_type);
   assignBlocks(params, _blocks);
 
-  params.set<NonlinearVariableName>("variable") = _tke_name;
+  params.set<LinearVariableName>("variable") = _tke_name;
   if (shouldCreateTimeDerivative(_tke_name, _blocks, /*error if already defined*/ false))
     getProblem().addLinearFVKernel(kernel_type, prefix() + "tke_time", params);
-  params.set<NonlinearVariableName>("variable") = _tked_name;
+  params.set<LinearVariableName>("variable") = _tked_name;
   if (shouldCreateTimeDerivative(_tked_name, _blocks, /*error if already defined*/ false))
     getProblem().addLinearFVKernel(kernel_type, prefix() + "tked_time", params);
 }
@@ -238,9 +236,9 @@ WCNSLinearFVTurbulencePhysics::addKEpsilonAdvection()
   params.set<UserObjectName>("rhie_chow_user_object") = _flow_equations_physics->rhieChowUOName();
   params.set<MooseEnum>("advected_interp_method") =
       getParam<MooseEnum>("tke_advection_interpolation");
-  params.set<NonlinearVariableName>("variable") = _tke_name;
+  params.set<LinearVariableName>("variable") = _tke_name;
   getProblem().addLinearFVKernel(kernel_type, prefix() + "tke_advection", params);
-  params.set<NonlinearVariableName>("variable") = _tked_name;
+  params.set<LinearVariableName>("variable") = _tked_name;
   params.set<std::vector<BoundaryName>>("walls") = _turbulence_walls;
   params.set<MooseEnum>("advected_interp_method") =
       getParam<MooseEnum>("tked_advection_interpolation");
@@ -257,13 +255,13 @@ WCNSLinearFVTurbulencePhysics::addKEpsilonDiffusion()
     params.set<bool>("use_nonorthogonal_correction") =
         getParam<bool>("use_nonorthogonal_correction");
 
-    params.set<NonlinearVariableName>("variable") = _tke_name;
+    params.set<LinearVariableName>("variable") = _tke_name;
     params.set<MooseFunctorName>("diffusion_coeff") =
         _flow_equations_physics->dynamicViscosityName();
     getProblem().addLinearFVKernel(kernel_type, prefix() + "tke_diffusion_mu", params);
 
     params.set<std::vector<BoundaryName>>("walls") = _turbulence_walls;
-    params.set<NonlinearVariableName>("variable") = _tked_name;
+    params.set<LinearVariableName>("variable") = _tked_name;
     getProblem().addLinearFVKernel(kernel_type, prefix() + "tked_diffusion_mu", params);
   }
 
@@ -274,15 +272,13 @@ WCNSLinearFVTurbulencePhysics::addKEpsilonDiffusion()
     params.set<bool>("use_nonorthogonal_correction") =
         getParam<bool>("use_nonorthogonal_correction");
 
-    params.set<NonlinearVariableName>("variable") = _tke_name;
+    params.set<LinearVariableName>("variable") = _tke_name;
     params.set<MooseFunctorName>("diffusion_coeff") = _turbulent_viscosity_name;
     params.set<MooseFunctorName>("scaling_coeff") = getParam<MooseFunctorName>("sigma_k");
-    params.set<MooseEnum>("coeff_interp_method") =
-        getParam<MooseEnum>("turbulent_viscosity_interp_method");
     getProblem().addLinearFVKernel(kernel_type, prefix() + "tke_diffusion_mu_turb", params);
 
     params.set<std::vector<BoundaryName>>("walls") = _turbulence_walls;
-    params.set<NonlinearVariableName>("variable") = _tked_name;
+    params.set<LinearVariableName>("variable") = _tked_name;
     params.set<MooseFunctorName>("scaling_coeff") = getParam<MooseFunctorName>("sigma_eps");
     getProblem().addLinearFVKernel(kernel_type, prefix() + "tked_diffusion_mu_turb", params);
   }
@@ -296,18 +292,15 @@ WCNSLinearFVTurbulencePhysics::addKEpsilonSink()
     const std::string kernel_type = "LinearFVTKESourceSink";
     InputParameters params = getFactory().getValidParams(kernel_type);
     assignBlocks(params, _blocks);
-    params.set<NonlinearVariableName>("variable") = _tke_name;
+    params.set<LinearVariableName>("variable") = _tke_name;
     params.set<MooseFunctorName>(NS::TKED) = _tked_name;
     params.set<MooseFunctorName>(NS::density) = _flow_equations_physics->densityName();
     params.set<MooseFunctorName>(NS::mu) = _flow_equations_physics->dynamicViscosityName();
     params.set<MooseFunctorName>(NS::mu_t) = _turbulent_viscosity_name;
     params.set<Real>("C_mu") = getParam<Real>("C_mu");
     params.set<Real>("C_pl") = getParam<Real>("C_pl");
-    params.set<bool>("linearized_model") = getParam<bool>("linearize_sink_sources");
     params.set<std::vector<BoundaryName>>("walls") = _turbulence_walls;
     params.set<MooseEnum>("wall_treatment") = _wall_treatment_eps;
-    // Currently only Newton method for WCNSLinearFVTurbulencePhysics
-    params.set<bool>("newton_solve") = true;
     for (const auto d : make_range(dimension()))
       params.set<MooseFunctorName>(u_names[d]) = _velocity_names[d];
     getProblem().addLinearFVKernel(kernel_type, prefix() + "tke_source_sink", params);
@@ -317,20 +310,17 @@ WCNSLinearFVTurbulencePhysics::addKEpsilonSink()
     const std::string kernel_type = "LinearFVTKEDSourceSink";
     InputParameters params = getFactory().getValidParams(kernel_type);
     assignBlocks(params, _blocks);
-    params.set<NonlinearVariableName>("variable") = _tked_name;
+    params.set<LinearVariableName>("variable") = _tked_name;
     params.set<MooseFunctorName>(NS::TKE) = _tke_name;
     params.set<MooseFunctorName>(NS::density) = _flow_equations_physics->densityName();
     params.set<MooseFunctorName>(NS::mu) = _flow_equations_physics->dynamicViscosityName();
     params.set<MooseFunctorName>(NS::mu_t) = _turbulent_viscosity_name;
-    params.set<bool>("linearized_model") = getParam<bool>("linearize_sink_sources");
     params.set<std::vector<BoundaryName>>("walls") = _turbulence_walls;
     params.set<MooseEnum>("wall_treatment") = _wall_treatment_eps;
     params.set<Real>("C1_eps") = getParam<Real>("C1_eps");
     params.set<Real>("C2_eps") = getParam<Real>("C2_eps");
     params.set<Real>("C_mu") = getParam<Real>("C_mu");
     params.set<Real>("C_pl") = getParam<Real>("C_pl");
-    // Currently only Newton method for WCNSLinearFVTurbulencePhysics
-    params.set<bool>("newton_solve") = true;
     for (const auto d : make_range(dimension()))
       params.set<MooseFunctorName>(u_names[d]) = _velocity_names[d];
     getProblem().addLinearFVKernel(kernel_type, prefix() + "tked_source_sink", params);
@@ -345,13 +335,12 @@ WCNSLinearFVTurbulencePhysics::addFVBCs()
   if (_turbulence_model == "k-epsilon" && getParam<bool>("mu_t_as_aux_variable"))
   {
     mooseAssert(_flow_equations_physics, "Should have a flow equation physics");
-    const std::string bc_type = "LinearFVTurbulentViscosityWallFunction";
+    const std::string bc_type = "LinearFVTurbulentViscosityWallFunctionBC";
     InputParameters params = getFactory().getValidParams(bc_type);
     params.set<std::vector<BoundaryName>>("boundary") = _turbulence_walls;
-    params.set<NonlinearVariableName>("variable") = _turbulent_viscosity_name;
+    params.set<LinearVariableName>("variable") = _turbulent_viscosity_name;
     params.set<MooseFunctorName>(NS::density) = _flow_equations_physics->densityName();
     params.set<MooseFunctorName>(NS::mu) = _flow_equations_physics->dynamicViscosityName();
-    params.set<MooseFunctorName>(NS::mu_t) = _turbulent_viscosity_name;
     params.set<MooseFunctorName>(NS::TKE) = _tke_name;
     params.set<Real>("C_mu") = getParam<Real>("C_mu");
     params.set<MooseEnum>("wall_treatment") = _wall_treatment_eps;
