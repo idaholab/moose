47eaeb069a21bc6a692a963bf1d089fdff8b106f
Vasileios Kyriakopoulos <vasileios.kyriakopoulos@inl.gov>
2025-08-28 16:36:33 -0600

    add pp that calculates net power using TH balance of inlet and outlet Refs #31377

---
+++ modules/subchannel/include/postprocessors/SCMTHPowerPostprocessor.h
@@ -0,0 +1,34 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "GeneralPostprocessor.h"
+#include "SubChannelMesh.h"
+
+/**
+ * Calculates the total power that goes into the coolant based on
+ * the thermal-hydraulic balance of inlet and outlet
+ */
+class SCMTHPowerPostprocessor : public GeneralPostprocessor
+{
+public:
+  static InputParameters validParams();
+  SCMTHPowerPostprocessor(const InputParameters & params);
+  virtual void initialize() override {}
+  virtual void execute() override;
+  virtual void finalize() override {}
+  virtual Real getValue() const override;
+
+protected:
+  /// geometric information
+  const SubChannelMesh & _mesh;
+  /// value we want to calculate
+  Real _value;
+};
