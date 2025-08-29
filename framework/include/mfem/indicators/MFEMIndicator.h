8a8385418965fea8973594693478b0119ea9f614
Sean Baccas <sean.baccas@ukaea.uk>
2025-08-29 10:11:54 +0100

    Remove unused parameters and change pointer to reference

--- framework/include/mfem/indicators/MFEMIndicator.h
+++ framework/include/mfem/indicators/MFEMIndicator.h
@@ -28,30 +28,25 @@ public:
 
   virtual ~MFEMIndicator() = default;
 
-  /// Get name of the test variable labelling the weak form this kernel is added to
-  const std::string & getTestVariableName() const { return _variable_name; }
-
   /// Create the estimator internally and return a bool to indicate if it
   /// succeeded. This base class should not be used directly, so we return
   /// false here.
   virtual bool createEstimator() { return false; }
 
   /// Get shared pointer to FE Space using the name we store when setting up this class
-  virtual std::shared_ptr<mfem::ParFiniteElementSpace> getFESpace() const;
+  virtual mfem::ParFiniteElementSpace & getFESpace() const;
 
   /// Method to fetch the error estimator after creation
   std::shared_ptr<mfem::ErrorEstimator> getEstimator() const;
 
 protected:
   /// Name of (the test variable associated with) the weak form that the kernel is applied to.
-  std::string _variable_name;
-
-  /// Reference to the variable we refine on. Use this refernce for fetching a reference
-  /// to the relevant FE space
-  const MFEMVariable & _variable;
+  VariableName _var_name;
 
   std::string _kernel_name;
   std::shared_ptr<mfem::ErrorEstimator> _error_estimator;
+
+  mfem::ParFiniteElementSpace & _fespace;
 };
 
 inline std::shared_ptr<mfem::ErrorEstimator>
