d5b689c187662dcab6b58acbfa6a71df9f97aa33
Yogesh macbook <Yogesh@Yogesh-MacBook.local>
2025-08-29 14:02:32 -0400

    Changed the name from VolumeStrainIncrement to VolumeFluxBasedStrainIncrement, closes #31234

---
+++ modules/solid_mechanics/include/materials/VolumeFluxBasedStrainIncrement.h
@@ -0,0 +1,32 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "FluxBasedStrainIncrement.h"
+
+/**
+ * VolumeFluxBasedStrainIncrement computes strain increment based on flux divergence and source for vacancy, which accounts for swelling (non conserved) and lattice site creation
+
+ */
+class VolumeFluxBasedStrainIncrement : public FluxBasedStrainIncrement
+{
+public:
+  static InputParameters validParams();
+
+  VolumeFluxBasedStrainIncrement(const InputParameters & parameters);
+
+protected:
+  virtual void initQpStatefulProperties();
+  virtual void computeQpProperties();
+
+  const MaterialProperty<Real> & _Lambda_prefactor_J;
+  const MaterialProperty<Real> & _Lambda_prefactor_P;
+  const MaterialProperty<Real> & _source;
+};
---
+++ modules/solid_mechanics/src/materials/VolumeFluxBasedStrainIncrement.C
@@ -0,0 +1,47 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "VolumeFluxBasedStrainIncrement.h"
+#include "libmesh/quadrature.h"
+
+registerMooseObject("SolidMechanicsApp", VolumeFluxBasedStrainIncrement);
+
+InputParameters
+VolumeFluxBasedStrainIncrement::validParams()
+{
+  InputParameters params = FluxBasedStrainIncrement::validParams();
+  params.addClassDescription("Compute volumetric strain increment based on flux");
+  params.addRequiredParam<MaterialPropertyName>("Lambda_Prefactor_J","Value of prefactor_J");
+  params.addRequiredParam<MaterialPropertyName>("Lambda_Prefactor_P","Value of prefactor_P");
+  params.addRequiredParam<MaterialPropertyName>("Source","Value of Source");
+  return params;
+}
+
+VolumeFluxBasedStrainIncrement::VolumeFluxBasedStrainIncrement(const InputParameters & parameters)
+  : FluxBasedStrainIncrement(parameters),
+     _Lambda_prefactor_J(getMaterialProperty<Real>("Lambda_Prefactor_J")),
+    _Lambda_prefactor_P(getMaterialProperty<Real>("Lambda_Prefactor_P")),
+    _source(getMaterialProperty<Real>("Source"))
+{
+}
+
+void
+VolumeFluxBasedStrainIncrement::initQpStatefulProperties()
+{
+  _strain_increment[_qp].zero();
+}
+
+void
+VolumeFluxBasedStrainIncrement::computeQpProperties()
+{
+  FluxBasedStrainIncrement::computeFluxGradTensor();
+  auto strain_rate = ((_Lambda_prefactor_J[_qp])*(_flux_grad_tensor.trace()) * _Identity_tensor) + ((_Lambda_prefactor_P[_qp]) * _source[_qp] * _Identity_tensor); //Lambda's are the relaxation factors and trace of flux grad tensor gives divergence of flux. 
+  _strain_increment[_qp] = strain_rate * _dt;
+}
+
