81bb3f75b264ffc77b30b7b7ccfdcf39e2949d29
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2025-08-29 19:46:02 -0600

    Add a util to parse the reaction network using the parsed expression grammar (PEG) - should be easier to maintain than the PCRE parser refs #31453

---
+++ modules/chemical_reactions/include/utils/ReactionNetworkUtils.h
@@ -0,0 +1,40 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include <string>
+
+namespace ReactionNetworkUtils
+{
+// Helper structs for parsing reaction network
+struct Term
+{
+  double coefficient;
+  std::string species;
+  std::optional<std::string> state;
+  std::optional<std::string> charge;
+};
+
+using TermList = std::vector<Term>;
+using Metadata = std::map<std::string, std::string>;
+
+struct Reaction
+{
+  TermList reactants;
+  TermList products;
+  Metadata metadata;
+};
+
+/// @brief  Parses the reaction network from a string form to a vector a Reaction
+/// @param reaction_network_string
+/// @return
+std::vector<Reaction> parseReactionNetwork(const std::string & reaction_network_string,
+                                           bool output_to_cout);
+}
