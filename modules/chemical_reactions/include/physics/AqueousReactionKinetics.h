42fabff15d2a757db461314fd2fb4949e5abd7ec
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2025-08-29 19:46:59 -0600

    Add a derived class for aqueous reaction kinetics refs #31454

---
+++ modules/chemical_reactions/include/physics/AqueousReactionKinetics.h
@@ -0,0 +1,33 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "ReactionKineticsPhysicsBase.h"
+
+/**
+ * Creates all the objects needed to solve a reaction network of chemical reactions in an aqueous
+ * medium with a continuous Galerkin finite element discretization
+ */
+class AqueousReactionKinetics : public ReactionKineticsPhysicsBase
+{
+public:
+  static InputParameters validParams();
+
+  AqueousReactionKinetics(const InputParameters & parameters);
+
+protected:
+  virtual void addAuxiliaryVariables() override;
+  virtual void addFEKernels() override;
+
+  /// Name of the pressure variable
+  const std::vector<VariableName> & _pressure_var;
+  /// Gravity vector
+  const RealVectorValue _gravity;
+};
