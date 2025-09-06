3e5e4b56267c47148a2c0797e2cc07a5cb3be13d
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2025-09-06 17:30:34 -0600

    Add more parsing, move over kernel creation code refs #31453

--- modules/chemical_reactions/include/physics/AqueousReactionKinetics.h
+++ modules/chemical_reactions/include/physics/AqueousReactionKinetics.h
@@ -12,7 +12,7 @@
 #include "ReactionKineticsPhysicsBase.h"
 
 /**
- * Creates all the objects needed to solve a reaction network of chemical reactions in an aqueous
+ * Creates all the objects needed to solve a reaction network of chemical reactions in a fluid
  * medium with a continuous Galerkin finite element discretization
  */
 class AqueousReactionKinetics : public ReactionKineticsPhysicsBase
@@ -26,11 +26,30 @@ protected:
   virtual void addAuxiliaryVariables() override;
   virtual void addFEKernels() override;
 
+  /// Stoichiometric coefficients for each primary species in each reaction
+  std::vector<std::vector<Real>> _stos;
+  /// Weight of each primary species in each reaction
+  std::vector<std::vector<Real>> _weights;
+  /// Equilibrium constants for each reaction
+  std::vector<Real> _eq_const;
+  /// Equilibrium species
+  std::vector<VariableName> _eq_species;
+  /// Set of auxillary species
+  std::set<std::string> _aux_species;
+  /// Vector of vectors, indexed by (i, j), of whether primary solver species 'i' is present in reaction 'j'
+  std::vector<std::vector<bool>> _primary_participation;
+  /// Stoichiometric coefficients of primary variables in each reaction
+  std::vector<std::vector<Real>> _sto_u;
+  /// Stoichiometric coefficients of coupled primary variables in each reaction
+  std::vector<std::vector<std::vector<Real>>> _sto_v;
+  /// Coupled primary species for each reaction
+  std::vector<std::vector<std::vector<VariableName>>> _coupled_v;
+  /// Primary species involved in the ith equilibrium reaction
+  std::vector<std::vector<VariableName>> _solver_species_involved;
+
   /// Name of the pressure variable
   const std::vector<VariableName> & _pressure_var;
   /// Gravity vector
   const RealVectorValue _gravity;
 
-  /// Vector of vectors, indexed by (i, j), of whether primary solver species 'i' is present in reaction 'j'
-  std::vector<std::vector<bool>> _primary_participation;
 };
