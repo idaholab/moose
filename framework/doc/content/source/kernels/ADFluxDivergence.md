6da52227a98e37cb5bbbee887ea2fb64ae177cad
V Prithivirajan <120124981+vprithiv@users.noreply.github.com>
2025-09-07 16:15:54 -0600

    Add gradient-based flux material and simplify test

--- framework/doc/content/source/kernels/ADFluxDivergence.md
+++ framework/doc/content/source/kernels/ADFluxDivergence.md
@@ -14,6 +14,9 @@ R_i = (\nabla \psi_i, \mathbf{J}),
 which corresponds to $-\nabla \cdot \mathbf{J}$ in the strong form. The flux is
 retrieved from a material property named `flux` (or `base_name + flux`).
 
+The example below employs `ADFluxFromGradientMaterial`, which computes a diffusive
+flux $\mathbf{J} = -D\nabla u$ from the gradient of the coupled variable `u`.
+
 ## Example Input File
 
 !listing test/tests/kernels/flux_divergence/ad_flux_divergence.i block=Materials/flux Kernels/flux
---
+++ test/include/materials/ADFluxFromGradientMaterial.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Material.h"
+
+/**
+ * Material computing a diffusive flux from the gradient of a coupled variable.
+ *
+ * J = -D * grad(u)
+ */
+class ADFluxFromGradientMaterial : public Material
+{
+public:
+  static InputParameters validParams();
+
+  ADFluxFromGradientMaterial(const InputParameters & parameters);
+
+protected:
+  virtual void computeQpProperties() override;
+
+  /// Gradient of the coupled variable
+  const ADVariableGradient & _grad_u;
+  /// Diffusivity multiplier
+  const Real _diffusivity;
+  /// Material property storing the flux vector
+  ADMaterialProperty<RealVectorValue> & _flux;
+};
