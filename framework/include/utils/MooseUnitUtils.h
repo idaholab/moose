594b641a0e05f6a22ce6b58c6079f6af95297e9b
Logan Harbour <loganhharbour@gmail.com>
2025-09-12 12:39:59 -0600

    Add macros for throw assertions

--- framework/include/utils/MooseUnitUtils.h
+++ framework/include/utils/MooseUnitUtils.h
@@ -14,6 +14,50 @@
 #include "MooseUtils.h"
 #include "Moose.h"
 
+/// Helper that asserts that the given "action" throws an exception of type "exception_type"
+#define MOOSE_ASSERT_THROWS(exception_type, action)                                                \
+  do                                                                                               \
+  {                                                                                                \
+    static_assert(std::is_base_of_v<std::exception, exception_type>, "Not an exception");          \
+    try                                                                                            \
+    {                                                                                              \
+      action;                                                                                      \
+      FAIL() << "Expected " << MooseUtils::prettyCppType<exception_type>() << " not thrown";       \
+    }                                                                                              \
+    catch (std::exception const & e)                                                               \
+    {                                                                                              \
+      if constexpr (!std::is_same_v<std::exception, exception_type>)                               \
+        if (!dynamic_cast<const exception_type *>(&e))                                             \
+          FAIL() << "Threw " << demangle(typeid(e).name()) << " instead of "                       \
+                 << MooseUtils::prettyCppType<exception_type>() << "\nMessage: \"" << e.what()     \
+                 << "\"";                                                                          \
+    }                                                                                              \
+  } while (0)
+
+/// Helper that asserts that the given "action" throws an exception of type "exception_type"
+/// whose message contains "contains"
+#define MOOSE_ASSERT_THROWS_CONTAINS(exception_type, action, contains)                             \
+  do                                                                                               \
+  {                                                                                                \
+    static_assert(std::is_base_of_v<std::exception, exception_type>, "Not an exception");          \
+    try                                                                                            \
+    {                                                                                              \
+      action;                                                                                      \
+      FAIL() << "Expected " << MooseUtils::prettyCppType<exception_type>() << " not thrown";       \
+    }                                                                                              \
+    catch (std::exception const & e)                                                               \
+    {                                                                                              \
+      if constexpr (!std::is_same_v<std::exception, exception_type>)                               \
+        if (!dynamic_cast<const exception_type *>(&e))                                             \
+          FAIL() << "Threw " << demangle(typeid(e).name()) << " instead of "                       \
+                 << MooseUtils::prettyCppType<exception_type>() << "\nMessage: \"" << e.what()     \
+                 << "\"";                                                                          \
+      if (std::string(e.what()).find(contains) == std::string::npos)                               \
+        FAIL() << "Exception " << MooseUtils::prettyCppType<exception_type>() << ":\n"             \
+               << "Message: \"" << e.what() << "\"\nDoes not contain: \"" << contains << "\"";     \
+    }                                                                                              \
+  } while (0)
+
 namespace Moose::UnitUtils
 {
 /**
