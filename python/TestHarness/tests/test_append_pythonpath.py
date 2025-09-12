bed228b3d09cd8293e3d0c0519b7ca0e2242c694
Zachary M. Prince <zachary.prince@inl.gov>
2025-09-12 15:20:50 -0600

    Adding all the paths to python #31457

---
+++ python/TestHarness/tests/test_append_pythonpath.py
@@ -0,0 +1,90 @@
+import os
+import sys
+import unittest
+from unittest import mock
+import tempfile
+import shutil
+
+from TestHarness.TestHarness import readTestRoot, TestHarness
+from TestHarness.tests.TestHarnessTestCase import MOOSE_DIR
+
+class TestAppendPythonPath(unittest.TestCase):
+    def setUp(self):
+        self.dir = tempfile.mkdtemp()
+        self.orig_cwd = os.getcwd()
+
+    def tearDown(self):
+        shutil.rmtree(self.dir, ignore_errors=True)
+        os.chdir(self.orig_cwd)
+
+    def test_readTestRoot(self):
+        # Create paths to set as extra pythonpaths
+        new_paths = ["fake_package", "mock_package"]
+        for path in new_paths:
+            os.mkdir(os.path.join(self.dir, path))
+
+        # Create testroot file
+        content = [
+            "app_name = test_harness_test\n",
+            "extra_pythonpath = {}\n".format(":".join(new_paths)),
+        ]
+        testroot_path = os.path.join(self.dir, "testroot")
+        with open(testroot_path, "w") as fid:
+            fid.writelines(content)
+
+        # Run method
+        app_name, args, hit_node, extra_pythonpath = readTestRoot(testroot_path)
+        self.assertEqual(app_name, "test_harness_test")
+        self.assertEqual(args, [])
+        self.assertIsNotNone(hit_node)
+        self.assertEqual(len(extra_pythonpath), len(new_paths))
+        for res, exp in zip(extra_pythonpath, new_paths):
+            self.assertEqual(res, os.path.join(self.dir, exp))
+
+        # Show that missing package with raise an error
+        shutil.rmtree(os.path.join(self.dir, new_paths[0]))
+        with self.assertRaisesRegex(FileNotFoundError, f"Could not find .*/{new_paths[0]}"):
+            readTestRoot(testroot_path)
+
+    @mock.patch("TestHarness.TestHarness.TestHarness.__init__", return_value=None)
+    def test_append_pythonpath_from_testroot(self, mock_th_init):
+        # Create paths to set as extra pythonpaths
+        new_paths = ["fake_package", "mock_package"]
+        new_paths_abs = [os.path.join(self.dir, path) for path in new_paths]
+        for path in new_paths_abs:
+            os.mkdir(path)
+
+        # Create testroot file
+        content = [
+            "app_name = test_harness_test\n",
+            "extra_pythonpath = {}\n".format(":".join(new_paths)),
+        ]
+        testroot_path = os.path.join(self.dir, "testroot")
+        with open(testroot_path, "w") as fid:
+            fid.writelines(content)
+
+        # Go into directory and build TestHarness
+        os.chdir(self.dir)
+        TestHarness.build([], "test_harness_test", MOOSE_DIR)
+        for path in new_paths_abs:
+            self.assertEqual(sys.path.count(path), 1)
+
+    @mock.patch("TestHarness.TestHarness.findDepApps", return_value="") # Avoid Resource warnings
+    def test_append_pythonpath_from_executable(self, _mock_finddepapps):
+        # Add a fake package in the directory to grab
+        fake_package = os.path.join(self.dir, "python")
+        os.mkdir(fake_package)
+
+        # Create a fake executioner
+        app_name = "test_harness_test"
+        with open(os.path.join(self.dir, f"{app_name}-opt"), "w") as fid:
+            fid.write("")
+
+        # Build the TestHarness, which should add to path
+        os.chdir(self.dir)
+        TestHarness.build(["unused", "--no-capabilities"], app_name, MOOSE_DIR, skip_testroot=True)
+        self.assertEqual(sys.path.count(os.path.join(self.dir, "python")), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
