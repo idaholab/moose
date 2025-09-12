25b312c1cf67c40ff9c22acd5ff0b333b45c78d3
Logan Harbour <loganhharbour@gmail.com>
2025-09-12 16:56:58 -0600

    Use requireExclusiveParams and test it
    refs #31442

---
+++ unit/src/MooseAppParamsTest.C
@@ -0,0 +1,34 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "gtest/gtest.h"
+
+#include "AppFactory.h"
+#include "MooseUnitUtils.h"
+
+std::unique_ptr<MooseApp>
+createApp(const std::vector<std::string> & cli_args)
+{
+  return AppFactory::create("MooseUnitApp", cli_args);
+};
+
+// TEST(MooseAppParamsTest, checkExclusiveParams)
+// {
+//   MOOSE_ASSERT_THROWS_CONTAINS(
+//       MooseRuntimeError,
+//       createApp({"--check-input", "--recover"}),
+//       "--check-input: Cannot be used with parameter '--recover'; recover files might not exist");
+//   MOOSE_ASSERT_THROWS_CONTAINS(
+//       MooseRuntimeError,
+//       createApp({"--test-restep", "--test-checkpoint-half-transient"}),
+//       "--test-restep: Cannot be used with parameter '--test-checkpoint-half-transient'");
+//   MOOSE_ASSERT_THROWS_CONTAINS(MooseRuntimeError,
+//                                createApp({"--trap-fpe", "--no-trap-fpe"}),
+//                                "--trap-fpe: Cannot be used with parameter '--no-trap-fpe'");
+// }
