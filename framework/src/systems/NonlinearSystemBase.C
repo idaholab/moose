1595f95cc57a2bf6c0163109bf1d5d7fcb3b9fde
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2025-09-12 15:43:14 -0600

    Add missing setup calls for exotic objects in NLSystem
    Avoid allocating threaded warehouses for non-threaded systems
    refs #31514

--- framework/src/systems/NonlinearSystemBase.C
+++ framework/src/systems/NonlinearSystemBase.C
@@ -134,6 +134,7 @@ NonlinearSystemBase::NonlinearSystemBase(FEProblemBase & fe_problem,
     _kokkos_preset_nodal_bcs(/*threaded=*/false),
     _kokkos_nodal_kernels(/*threaded=*/false),
 #endif
+    _general_dampers(/*threaded=*/false),
     _splits(/*threaded=*/false),
     _increment_vec(NULL),
     _use_finite_differenced_preconditioner(false),
@@ -271,6 +272,8 @@ NonlinearSystemBase::initialSetup()
     _constraints.initialSetup();
     _general_dampers.initialSetup();
     _nodal_bcs.initialSetup();
+    _preset_nodal_bcs.residualSetup();
+    _ad_preset_nodal_bcs.residualSetup();
 
 #ifdef MOOSE_KOKKOS_ENABLED
     _kokkos_kernels.initialSetup();
@@ -383,6 +386,8 @@ NonlinearSystemBase::timestepSetup()
   _constraints.timestepSetup();
   _general_dampers.timestepSetup();
   _nodal_bcs.timestepSetup();
+  _preset_nodal_bcs.timestepSetup();
+  _ad_preset_nodal_bcs.timestepSetup();
 
 #ifdef MOOSE_KOKKOS_ENABLED
   _kokkos_kernels.timestepSetup();
@@ -444,6 +449,8 @@ NonlinearSystemBase::customSetup(const ExecFlagType & exec_type)
   _constraints.customSetup(exec_type);
   _general_dampers.customSetup(exec_type);
   _nodal_bcs.customSetup(exec_type);
+  _preset_nodal_bcs.customSetup(exec_type);
+  _ad_preset_nodal_bcs.customSetup(exec_type);
 
 #ifdef MOOSE_KOKKOS_ENABLED
   _kokkos_kernels.customSetup(exec_type);
@@ -1739,6 +1746,8 @@ NonlinearSystemBase::residualSetup()
   _constraints.residualSetup();
   _general_dampers.residualSetup();
   _nodal_bcs.residualSetup();
+  _preset_nodal_bcs.residualSetup();
+  _ad_preset_nodal_bcs.residualSetup();
 
 #ifdef MOOSE_KOKKOS_ENABLED
   _kokkos_kernels.residualSetup();
@@ -2817,6 +2826,8 @@ NonlinearSystemBase::jacobianSetup()
   _constraints.jacobianSetup();
   _general_dampers.jacobianSetup();
   _nodal_bcs.jacobianSetup();
+  _preset_nodal_bcs.jacobianSetup();
+  _ad_preset_nodal_bcs.jacobianSetup();
 
 #ifdef MOOSE_KOKKOS_ENABLED
   _kokkos_kernels.jacobianSetup();
@@ -3361,6 +3372,7 @@ NonlinearSystemBase::updateActive(THREAD_ID tid)
     _nodal_bcs.updateActive();
     _preset_nodal_bcs.updateActive();
     _ad_preset_nodal_bcs.updateActive();
+    _splits.updateActive();
     _constraints.updateActive();
     _scalar_kernels.updateActive();
 
