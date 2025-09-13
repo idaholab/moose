98be23965585f12913437816491b234db490f6d1
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2025-09-13 14:53:17 -0600

    Add support for outputting all components of a vector Real matprop refs #31519

---
+++ framework/include/materials/VariableSizeMaterialPropertiesInterface.h
@@ -0,0 +1,24 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "Material.h"
+
+/**
+ * Interface class to return the size of material properties that do not have a fixed size
+ */
+class VariableSizeMaterialPropertiesInterface
+{
+public:
+  VariableSizeMaterialPropertiesInterface(const InputParameters & /*params*/) {};
+
+  /// Return the size of the variable size vector material property that the material defines
+  virtual unsigned int getVectorPropertySize(const MaterialPropertyName & /*prop_name*/) const = 0;
+};
