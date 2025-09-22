814cccba7d7a437caab5b520f60c1243733b5456
Nuno Nobre <nuno.nobre@stfc.ac.uk>
2025-09-22 16:02:27 +0100

    Allocate array of coeff and vec of position values just once

--- framework/src/mfem/functions/MFEMScalarParsedCoefficient.C
+++ framework/src/mfem/functions/MFEMScalarParsedCoefficient.C
@@ -14,9 +14,14 @@
 MFEMScalarParsedCoefficient::MFEMScalarParsedCoefficient(
     Moose::MFEM::CoefficientManager & coefficients,
     const std::vector<MFEMScalarCoefficientName> & coef_names,
-    bool use_xyzt,
+    const bool & use_xyzt,
     const FunctionParserUtils<false>::SymFunctionPtr & func)
-  : _coefficients(coefficients), _coef_names(coef_names), _use_xyzt(use_xyzt), _func(func)
+  : _coefficients(coefficients),
+    _coef_names(coef_names),
+    _use_xyzt(use_xyzt),
+    _func(func),
+    _vals(coef_names.size() + (use_xyzt ? 4 : 0)),
+    _transip(3)
 {
 }
 
@@ -24,26 +29,20 @@ mfem::real_t
 MFEMScalarParsedCoefficient::Eval(mfem::ElementTransformation & T,
                                   const mfem::IntegrationPoint & ip)
 {
-  std::vector<mfem::real_t> vals(_coef_names.size() + (_use_xyzt ? 4 : 0));
-
   for (unsigned i = 0; i < _coef_names.size(); i++)
-    vals[i] = _coefficients.getScalarCoefficient(_coef_names[i]).Eval(T, ip);
+    _vals[i] = _coefficients.getScalarCoefficient(_coef_names[i]).Eval(T, ip);
 
   if (_use_xyzt)
   {
-    mfem::Vector transip;
-    T.Transform(ip, transip);
+    T.Transform(ip, _transip);
 
-    for (int i = 0; i < transip.Size(); i++)
-      vals[_coef_names.size() + i] = transip(i);
+    for (int i = 0; i < 3; i++)
+      _vals[_coef_names.size() + i] = i < _transip.Size() ? _transip(i) : 0.;
 
-    for (int i = transip.Size(); i < 3; i++)
-      vals[_coef_names.size() + i] = 0;
-
-    vals[_coef_names.size() + 3] = GetTime();
+    _vals[_coef_names.size() + 3] = GetTime();
   }
 
-  return _func->Eval(vals.data());
+  return _func->Eval(_vals.GetData());
 }
 
 #endif
