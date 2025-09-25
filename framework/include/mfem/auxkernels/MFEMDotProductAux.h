fcc5c55a92263a6edb31eda83a10237863eabe11
Ioannis-UKAEA <ic@Ioanniss-MacBook-Pro-2.local>
2025-09-25 15:27:39 +0100

    Initial commit for the inner product aux kernel .C and .h

---
+++ framework/include/mfem/auxkernels/MFEMDotProductAux.h
@@ -0,0 +1,51 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifdef MOOSE_MFEM_ENABLED
+#pragma once
+
+#include "MFEMAuxKernel.h"
+#include "mfem.hpp"
+
+/**
+ * Project s(x) * (U · V) onto a scalar MFEM auxvariable.
+ *
+ * Parameters:
+ *   - u (VariableName): vector MFEM variable U
+ *   - v (VariableName): vector MFEM variable V
+ *   - scale_factor (mfem::real_t, default=1.0): constant multiplier
+ *   - scale_variable (VariableName, optional): scalar MFEM variable s(x)
+ */
+class MFEMDotProductAux : public MFEMAuxKernel
+{
+public:
+  static InputParameters validParams();
+
+  MFEMDotProductAux(const InputParameters & parameters);
+  ~MFEMDotProductAux() override = default;
+
+  void execute() override;
+
+protected:
+  // Names of vector sources
+  const VariableName _u_var_name;
+  const VariableName _v_var_name;
+
+  // References to the vector ParGridFunctions
+  const mfem::ParGridFunction & _u_var;
+  const mfem::ParGridFunction & _v_var;
+
+  // Optional scalar scale field s(x)
+  // const mfem::ParGridFunction * _scale_var; // nullptr if not provided
+
+  // Constant multiplier
+  const mfem::real_t _scale_factor;
+};
+
+#endif // MOOSE_MFEM_ENABLED
---
+++ framework/src/mfem/auxkernels/MFEMDotProductAux.C
@@ -0,0 +1,79 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifdef MOOSE_MFEM_ENABLED
+
+#include "MFEMDotProductAux.h"
+#include "MFEMProblem.h"
+
+registerMooseObject("MooseApp", MFEMDotProductAux);
+
+InputParameters
+MFEMDotProductAux::validParams()
+{
+  InputParameters params = MFEMAuxKernel::validParams();
+  params.addClassDescription("Projects s(x) * (U · V) onto a scalar MFEM auxvariable");
+  params.addRequiredParam<VariableName>("u", "Vector MFEMVariable U");
+  params.addRequiredParam<VariableName>("v", "Vector MFEMVariable V");
+  params.addParam<mfem::real_t>("scale_factor", 1.0, "Constant multiplier applied to the dot product");
+  // params.addParam<VariableName>("scale_variable", "Optional scalar MFEMVariable s(x) to multiply the dot product.");
+  return params;
+}
+
+MFEMDotProductAux::MFEMDotProductAux(const InputParameters & parameters)
+  : MFEMAuxKernel(parameters),
+    _u_var_name(getParam<VariableName>("u")),
+    _v_var_name(getParam<VariableName>("v")),
+    _u_var(*getMFEMProblem().getProblemData().gridfunctions.Get(_u_var_name)),
+    _v_var(*getMFEMProblem().getProblemData().gridfunctions.Get(_v_var_name)),
+    // _scale_var(parameters.isParamValid("scale_variable")
+                //  ? &(*getMFEMProblem().getProblemData().gridfunctions.Get(
+                      //  getParam<VariableName>("scale_variable")))
+                //  : nullptr),
+    _scale_factor(getParam<mfem::real_t>("scale_factor"))
+{
+  // nothing else to assemble, the coefficient expression is build in the execute()
+}
+
+void
+MFEMDotProductAux::execute()
+{
+  // Build scalar coefficient: s(x) * (U · V)
+  mfem::VectorGridFunctionCoefficient Ucoef(const_cast<mfem::ParGridFunction *>(&_u_var));
+  mfem::VectorGridFunctionCoefficient Vcoef(const_cast<mfem::ParGridFunction *>(&_v_var));
+  mfem::InnerProductCoefficient dot_uv(Ucoef, Vcoef);
+
+  // If you enable scale_variable, multiply by that GridFunctionCoefficient here.
+  mfem::ConstantCoefficient ccoef(_scale_factor);
+  mfem::ProductCoefficient final_coef(ccoef, dot_uv);
+
+  //Enforce L2 with INTEGRAL mapping and interior DOFs only
+  mfem::ParFiniteElementSpace * fes = _result_var.ParFESpace();
+
+  // Must be L2
+  if (!dynamic_cast<const mfem::L2_FECollection *>(fes->FEColl()))
+    mooseError("MFEMDotProductAux requires the target variable to use L2_FECollection.");
+
+  // Must use integral mapping (element-integrated DOFs / quadrature-point-based DOFs)
+  const int dim = fes->GetMesh()->Dimension();
+  const auto mt = static_cast<mfem::FiniteElement::MapType>(fes->FEColl()->GetMapType(dim));
+  if (mt != mfem::FiniteElement::INTEGRAL)
+    mooseError("MFEMDotProductAux requires map_type = FiniteElement::INTEGRAL for the target L2 space.");
+
+  // Must have no shared/constrained DOFs
+  if (fes->GetTrueVSize() != fes->GetVSize())
+    mooseError("MFEMDotProductAux currently supports only L2 spaces with interior DOFs (no shared/constrained DOFs).");
+
+
+  // Project into the scalar aux result variable per element projection for L2
+  _result_var = 0.0;
+  _result_var.ProjectCoefficient(final_coef);
+}
+
+#endif // MOOSE_MFEM_ENABLED
