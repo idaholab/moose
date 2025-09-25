b1825aca1b7aa1db581a76741b36f18a894d7021
Ioannis-UKAEA <ic@Ioanniss-MacBook-Pro-2.local>
2025-09-25 15:44:12 +0100

    Initiall structure for the Cross Product AuxKernel s(x) * (U x V) projection on a vector MFEM aux variable.

---
+++ framework/include/mfem/auxkernels/MFEMCrossProductAux.h
@@ -0,0 +1,44 @@
+#ifdef MOOSE_MFEM_ENABLED
+#pragma once
+
+#include "MFEMAuxKernel.h"
+#include "mfem.hpp"
+
+/**
+ * Project s(x) * (U × V) onto a vector MFEM auxvariable.
+ *
+ * Parameters:
+ *   - variable (AuxVariableName, required): AuxVariable name (inherited)
+ *   - u (VariableName, required): vector MFEM variable U (vdim = 3)
+ *   - v (VariableName, required): vector MFEM variable V (vdim = 3)
+ *   - scale_factor (mfem::real_t, default=1.0): constant multiplier s(x)=scale_factor
+ *
+ * Notes:
+ *   - Requires variable to be L2_FECollection with map_type = INTEGRAL.
+ *   - Currently supports only interior DOFs (no shared/constrained DOFs).
+ *   - Enforces 3D: mesh dimension and all involved vdim must be 3.
+ */
+class MFEMCrossProductAux : public MFEMAuxKernel
+{
+public:
+  static InputParameters validParams();
+
+  MFEMCrossProductAux(const InputParameters & parameters);
+  ~MFEMCrossProductAux() override = default;
+
+  void execute() override;
+
+protected:
+  // Names of vector sources
+  const VariableName _u_var_name;
+  const VariableName _v_var_name;
+
+  // References to the vector ParGridFunctions
+  const mfem::ParGridFunction & _u_var;
+  const mfem::ParGridFunction & _v_var;
+
+  // Constant multiplier
+  const mfem::real_t _scale_factor;
+};
+
+#endif // MOOSE_MFEM_ENABLED
