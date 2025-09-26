49b1d5e5dc92d86a8a76586d7fb20063519be5f6
Namjae Choi <Namjae.Choi@inl.gov>
2025-09-26 09:01:51 -0600

    Kokkos AuxKernel framework objects #30655

---
+++ framework/include/kokkos/auxkernels/KokkosCopyValueAux.h
@@ -0,0 +1,66 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "KokkosAuxKernel.h"
+
+/**
+ * Copies one variable onto an auxiliary variable
+ */
+class KokkosCopyValueAux : public Moose::Kokkos::AuxKernel
+{
+public:
+  static InputParameters validParams();
+
+  KokkosCopyValueAux(const InputParameters & parameters);
+
+  template <typename Derived>
+  KOKKOS_FUNCTION void computeElementInternal(const Derived & auxkernel,
+                                              ResidualDatum & datum) const;
+  template <typename Derived>
+  KOKKOS_FUNCTION void computeNodeInternal(const Derived & auxkernel, ResidualDatum & datum) const;
+
+protected:
+  /// Variable used to specify state being copied
+  unsigned short _state;
+
+  /// The variable value to copy from
+  const Moose::Kokkos::VariableValue _v;
+
+  /// A reference to the variable to copy from
+  const MooseVariable & _source_variable;
+};
+
+template <typename Derived>
+KOKKOS_FUNCTION void
+KokkosCopyValueAux::computeElementInternal(const Derived & /* auxkernel */,
+                                           ResidualDatum & datum) const
+{
+  auto & sys = kokkosSystem(_kokkos_var.sys());
+  auto var = _kokkos_var.var();
+  auto tag = _kokkos_var.tag();
+  auto elem = datum.elem().id;
+
+  for (unsigned int i = 0; i < datum.n_dofs(); ++i)
+    sys.getVectorDofValue(sys.getElemLocalDofIndex(elem, i, var), tag) = _v(datum, i);
+}
+
+template <typename Derived>
+KOKKOS_FUNCTION void
+KokkosCopyValueAux::computeNodeInternal(const Derived & /* auxkernel */,
+                                        ResidualDatum & datum) const
+{
+  auto & sys = kokkosSystem(_kokkos_var.sys());
+  auto var = _kokkos_var.var();
+  auto tag = _kokkos_var.tag();
+  auto node = datum.node();
+
+  sys.getVectorDofValue(sys.getNodeLocalDofIndex(node, var), tag) = _v(datum, 0);
+}
---
+++ framework/include/kokkos/auxkernels/KokkosVariableTimeIntegrationAux.h
@@ -0,0 +1,84 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "KokkosAuxKernel.h"
+
+/**
+ * An AuxKernel that can be used to integrate a field variable in time
+ * using a variety of different integration methods.  The result is
+ * stored in another field variable.
+ */
+class KokkosVariableTimeIntegrationAux : public Moose::Kokkos::AuxKernel
+{
+public:
+  static InputParameters validParams();
+
+  KokkosVariableTimeIntegrationAux(const InputParameters & parameters);
+
+  KOKKOS_FUNCTION Real computeValue(const unsigned int qp, ResidualDatum & datum) const;
+
+protected:
+  KOKKOS_FUNCTION Real getIntegralValue(const unsigned int qp, ResidualDatum & datum) const;
+
+  Moose::Kokkos::Array<Moose::Kokkos::VariableValue> _coupled_vars;
+  const Real _coef;
+  const unsigned int _order;
+  Moose::Kokkos::Array<Real> _integration_coef;
+
+  /// The old variable value (zero if order == 3)
+  const Moose::Kokkos::VariableValue _u_old;
+  /// The older variable value (zero if order != 3)
+  const Moose::Kokkos::VariableValue _u_older;
+};
+
+KOKKOS_FUNCTION inline Real
+KokkosVariableTimeIntegrationAux::computeValue(const unsigned int qp, ResidualDatum & datum) const
+{
+  Real integral = getIntegralValue(qp, datum);
+
+  if (_order == 3)
+    return _u_older(datum, qp) + _coef * integral;
+
+  return _u_old(datum, qp) + _coef * integral;
+}
+
+KOKKOS_FUNCTION inline Real
+KokkosVariableTimeIntegrationAux::getIntegralValue(const unsigned int qp,
+                                                   ResidualDatum & datum) const
+{
+  Real integral_value = 0.0;
+
+  for (unsigned int i = 0; i < _order; ++i)
+    integral_value += _integration_coef[i] * _coupled_vars[i](datum, qp) * _dt;
+
+  /**
+   * Subsequent timesteps may be unequal, so the standard Simpson rule
+   * cannot be used. Use a different set of coefficients here.
+   * J. McNAMEE, "A PROGRAM TO INTEGRATE A FUNCTION TABULATED AT
+   * UNEQUAL INTERVALS," Internation Journal for Numerical Methods in
+   * Engineering, Vol. 17, 217-279. (1981).
+   */
+  if (_order == 3 && _dt != _dt_old)
+  {
+    Real x0 = 0.0;
+    Real x1 = _dt_old;
+    Real x2 = _dt + _dt_old;
+    Real y0 = _coupled_vars[2](datum, qp);
+    Real y1 = _coupled_vars[1](datum, qp);
+    Real y2 = _coupled_vars[0](datum, qp);
+    Real term1 = (x2 - x0) * (y0 + (x2 - x0) * (y1 - y0) / (2.0 * (x1 - x0)));
+    Real term2 = (2.0 * x2 * x2 - x0 * x2 - x0 * x0 + 3.0 * x0 * x1 - 3.0 * x1 * x2) / 6.0;
+    Real term3 = (y2 - y1) / (x2 - x1) - (y1 - y0) / (x1 - x0);
+    integral_value = term1 + term2 * term3;
+  }
+
+  return integral_value;
+}
