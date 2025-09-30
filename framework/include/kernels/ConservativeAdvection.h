92477fb1c9e1d4b33338653662b8750759098409
Topher Matthews <cmatthews@lanl.gov>
2025-09-30 08:59:54 -0600

    Make getUNodal function. Closes #31633

--- framework/include/kernels/ConservativeAdvection.h
+++ framework/include/kernels/ConservativeAdvection.h
@@ -31,6 +31,7 @@ protected:
   virtual Real computeQpOffDiagJacobian(unsigned int jvar) override;
   virtual void computeResidual() override;
   virtual void computeJacobian() override;
+  virtual GenericReal<is_ad> getUNodal(unsigned int n) { return _u_nodal[n]; }
 
   /// Material property multiplied against the velocity to scale advection strength
   const GenericMaterialProperty<Real, is_ad> & _scalar;
