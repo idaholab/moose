7ddc239825835e1e4a492d244088951c73bc5993
Logan Harbour <loganhharbour@gmail.com>
2025-10-04 11:26:37 -0600

    Run tests cached to speed them up

--- python/TestHarness/tests/test_resultsreader_results.py
+++ python/TestHarness/tests/test_resultsreader_results.py
@@ -16,6 +16,8 @@ from datetime import datetime
 from mock import patch
 from copy import deepcopy
 from typing import Tuple
+from dataclasses import dataclass
+from TestHarness import TestHarness
 from TestHarness.tests.TestHarnessTestCase import TestHarnessTestCase
 from TestHarness.resultsreader.results import TestHarnessResults, TestHarnessTestResult, TestName, DatabaseException
 
@@ -34,12 +36,22 @@ FAKE_TEST_NAME = TestName('folder', 'name')
 FAKE_JSON_METADATA = {'foo': 'bar'}
 
 class TestResultsReaderResults(TestHarnessTestCase):
-    def setUp(self):
-        self.run_tests_result = self.runTests('-i', 'validation', '--capture-perf-graph', exit_code=132)
+    @dataclass
+    class CapturedResult:
+        """
+        Helper data class for captureResult()
+        """
+        # The test harness from the run
+        harness: TestHarness
+        # The data for building the TestHarnessResults
+        result_data: dict
+        # The data for building the tests
+        test_data: dict
 
     def captureResult(self, remove_civet_keys=[], civet_version=FAKE_CIVET_VERSION,
-                      test_in_results=True, no_tests=False, delete_test_key=None):
-        values = self.run_tests_result.results
+                      test_in_results=True, no_tests=False, delete_test_key=None) -> CapturedResult:
+        result = self.runTestsCached('-i', 'validation', '--capture-perf-graph', exit_code=132)
+        values = result.results
 
         tests = {}
 
@@ -104,44 +116,57 @@ class TestResultsReaderResults(TestHarnessTestCase):
         # Base CIVET values for indexing
         results.update(civet_values)
 
-        return results, tests
+        return self.CapturedResult(harness=result.harness, result_data=results, test_data=tests)
 
-    def buildResult(self, *args, **kwargs) -> Tuple[TestHarnessResults, dict]:
-        result_data, test_data = self.captureResult(*args, **kwargs)
+    @dataclass
+    class BuiltResult:
+        """
+        Helper data class for buildResult()
+        """
+        # The test harness from the run
+        harness: TestHarness
+        # The built TestHarnessResults object
+        results: TestHarnessResults
+        # The test data
+        test_data: dict
 
-        results = TestHarnessResults(result_data)
+    def buildResult(self, *args, **kwargs) -> BuiltResult:
+        captured_result = self.captureResult(*args, **kwargs)
+
+        results = TestHarnessResults(captured_result.result_data)
         if not kwargs.get('no_tests', False):
-            self.assertEqual(len(results._tests), len(test_data))
+            self.assertEqual(len(results._tests), len(captured_result.test_data))
 
-        return results, test_data
+        return self.BuiltResult(harness=captured_result.harness, results=results, test_data=captured_result.test_data)
 
     def testTestHarnessResult(self):
         """
         Tests a basic build of a TestHarnessResult and the underlying test data
         """
-        result, test_data = self.buildResult()
-        harness = self.run_tests_result.harness
+        built_result = self.buildResult()
+        harness = built_result.harness
+        results = built_result.results
 
-        self.assertEqual(result.data, result._data)
-        self.assertEqual(result.id, FAKE_RESULT_ID)
-        self.assertEqual(result.testharness, result.data['testharness'])
-        self.assertEqual(result.version, result.data['testharness']['version'])
-        self.assertEqual(result.validation_version, result.data['testharness']['validation_version'])
+        self.assertEqual(results.data, results._data)
+        self.assertEqual(results.id, FAKE_RESULT_ID)
+        self.assertEqual(results.testharness, results.data['testharness'])
+        self.assertEqual(results.version, results.data['testharness']['version'])
+        self.assertEqual(results.validation_version, results.data['testharness']['validation_version'])
 
         # Faked entries for civet
-        self.assertEqual(result.civet_job_url, FAKE_CIVET_JOB_URL)
-        self.assertEqual(result.civet_job_id, FAKE_CIVET_JOB_ID)
-        self.assertEqual(result.civet_version, FAKE_CIVET_VERSION)
-        self.assertEqual(result.event_sha, FAKE_EVENT_SHA)
-        self.assertEqual(result.event_cause, FAKE_EVENT_CAUSE)
-        self.assertEqual(result.event_id, FAKE_EVENT_ID)
-        self.assertEqual(result.pr_num, FAKE_PR_NUM)
-        self.assertEqual(result.base_sha, FAKE_BASE_SHA)
-        self.assertEqual(result.time, FAKE_TIME)
+        self.assertEqual(results.civet_job_url, FAKE_CIVET_JOB_URL)
+        self.assertEqual(results.civet_job_id, FAKE_CIVET_JOB_ID)
+        self.assertEqual(results.civet_version, FAKE_CIVET_VERSION)
+        self.assertEqual(results.event_sha, FAKE_EVENT_SHA)
+        self.assertEqual(results.event_cause, FAKE_EVENT_CAUSE)
+        self.assertEqual(results.event_id, FAKE_EVENT_ID)
+        self.assertEqual(results.pr_num, FAKE_PR_NUM)
+        self.assertEqual(results.base_sha, FAKE_BASE_SHA)
+        self.assertEqual(results.time, FAKE_TIME)
 
         # Has same number of tests
-        for test_result in result.tests:
-            data = test_data[test_result.name]
+        for test_result in results.tests:
+            data = built_result.test_data[test_result.name]
 
             # Get the actual Job object from the TestHarness
             jobs = [j for j in harness.finished_jobs if j.getTestName() == str(test_result.name)]
@@ -151,8 +176,8 @@ class TestResultsReaderResults(TestHarnessTestCase):
             # Base properties
             self.assertEqual(test_result.data, data)
             self.assertEqual(test_result.id, data['_id'])
-            self.assertEqual(test_result.results, result)
-            self.assertEqual(test_result.result_id, result.id)
+            self.assertEqual(test_result.results, results)
+            self.assertEqual(test_result.result_id, results.id)
             self.assertEqual(test_result.folder_name, test_result.name.folder)
             self.assertEqual(test_result.test_name, test_result.name.name)
 
@@ -197,7 +222,9 @@ class TestResultsReaderResults(TestHarnessTestCase):
         Tests when tests are stored as ObjectId (reference to another collection)
         and then later loaded from the database on request
         """
-        result_data, test_data = self.captureResult(test_in_results=False)
+        captured_result = self.captureResult(test_in_results=False)
+        test_data = captured_result.test_data
+        result_data = captured_result.result_data
 
         # Mock getting the test results from mongodb
         def get_test_data(id):
@@ -230,11 +257,12 @@ class TestResultsReaderResults(TestHarnessTestCase):
         Tests TestHarnessResult.[have_test, get_test] on a test
         that is not stored
         """
-        result, _ = self.buildResult()
+        built_result = self.buildResult()
+        results = built_result.results
 
-        self.assertFalse(result.has_test('foo', 'bar'))
+        self.assertFalse(results.has_test('foo', 'bar'))
         with self.assertRaisesRegex(KeyError, 'Test "foo.bar" does not exist'):
-            result.get_test('foo', 'bar')
+            results.get_test('foo', 'bar')
 
     @patch.object(TestHarnessResults, '_find_test_data')
     def testGetTestMissingDatabase(self, patch_find_test_data):
@@ -244,12 +272,13 @@ class TestResultsReaderResults(TestHarnessTestCase):
         """
         patch_find_test_data.side_effect = lambda id: None
 
-        result, _ = self.buildResult(test_in_results=False)
+        built_result = self.buildResult(test_in_results=False)
+        results = built_result.results
 
-        for name in result.test_names:
-            id = result._tests[name]
+        for name in results.test_names:
+            id = results._tests[name]
             with self.assertRaisesRegex(DatabaseException, f'Database missing tests._id={id}'):
-                result.get_test(name.folder, name.name)
+                results.get_test(name.folder, name.name)
 
     @patch.object(TestHarnessTestResult, '__init__')
     def testBuildTestException(self, patch_init):
@@ -267,43 +296,44 @@ class TestResultsReaderResults(TestHarnessTestCase):
         Tests TestHarnessResult.init_tests when a test entry has a bad type
         (not ObjectID or dict)
         """
-        result, _ = self.captureResult()
+        captured_result = self.captureResult()
+        result_data = captured_result.result_data
 
-        for folder_name, folder_entry in result['tests'].items():
+        for folder_name, folder_entry in result_data['tests'].items():
             for test_name in list(folder_entry['tests'].keys()):
                 folder_entry['tests'][test_name] = 1
 
         with self.assertRaisesRegex(TypeError, 'has unexpected type "int"'):
-            TestHarnessResults(result)
+            TestHarnessResults(result_data)
 
     def testDeprecatedBaseSHA(self):
         """
         Tests when base_sha didn't exist in the database (civet version < 2)
         """
         civet_version = 1
-        result, _, = self.buildResult(remove_civet_keys=['base_sha'],
-                                      civet_version=civet_version,
+        built_result = self.buildResult(remove_civet_keys=['base_sha'],
+                                       civet_version=civet_version,
                                       no_tests=True)
-        self.assertEqual(result.civet_version, civet_version)
-        self.assertIsNone(result.base_sha)
+        self.assertEqual(built_result.results.civet_version, civet_version)
+        self.assertIsNone(built_result.results.base_sha)
 
     def testNoEventID(self):
         """
         Tests when event_id didn't exist in the database (civet_version < 3)
         """
         civet_version = 2
-        result, _ = self.buildResult(remove_civet_keys=['event_id'],
-                                       civet_version=civet_version,
-                                       no_tests=True)
-        self.assertEqual(result.civet_version, civet_version)
-        self.assertIsNone(result.event_id)
+        built_result = self.buildResult(remove_civet_keys=['event_id'],
+                                        civet_version=civet_version,
+                                        no_tests=True)
+        self.assertEqual(built_result.results.civet_version, civet_version)
+        self.assertIsNone(built_result.results.event_id)
 
     def testNoTiming(self):
         """
         Tests timing not being available in a test result
         """
-        result, _ = self.buildResult(delete_test_key=['timing'])
-        for test_result in result.tests:
+        built_result = self.buildResult(delete_test_key=['timing'])
+        for test_result in built_result.results.tests:
             self.assertIsNone(test_result.timing)
             self.assertIsNone(test_result.run_time)
             self.assertIsNone(test_result.hpc_queued_time)
@@ -312,24 +342,24 @@ class TestResultsReaderResults(TestHarnessTestCase):
         """
         Tests run_time not being available in timing in a test result
         """
-        result, _ = self.buildResult(delete_test_key=['timing', 'runner_run'])
-        for test_result in result.tests:
+        built_result = self.buildResult(delete_test_key=['timing', 'runner_run'])
+        for test_result in built_result.results.tests:
             self.assertIsNone(test_result.run_time)
 
     def testNoHPCQueuedTime(self):
         """
         Tests hpc_queued_time not being available in timing in a test result
         """
-        result, _ = self.buildResult(delete_test_key=['timing', 'hpc_queued'])
-        for test_result in result.tests:
+        built_result = self.buildResult(delete_test_key=['timing', 'hpc_queued'])
+        for test_result in built_result.results.tests:
             self.assertIsNone(test_result.hpc_queued_time)
 
     def testNoTester(self):
         """
         Tests tester not being available in a test result
         """
-        result, _ = self.buildResult(delete_test_key=['tester'])
-        for test_result in result.tests:
+        built_result = self.buildResult(delete_test_key=['tester'])
+        for test_result in built_result.results.tests:
             self.assertIsNone(test_result.tester)
             self.assertIsNone(test_result.json_metadata)
 
@@ -337,8 +367,8 @@ class TestResultsReaderResults(TestHarnessTestCase):
         """
         Tests status not being available in a test result
         """
-        result, _ = self.buildResult(delete_test_key=['status'])
-        for test_result in result.tests:
+        built_result = self.buildResult(delete_test_key=['status'])
+        for test_result in built_result.results.tests:
             self.assertIsNone(test_result.status)
             self.assertIsNone(test_result.status_value)
 
@@ -347,9 +377,9 @@ class TestResultsReaderResults(TestHarnessTestCase):
         Test result_id not being within a test (when tests
         are stored directly within result data)
         """
-        result, _ = self.buildResult(delete_test_key=['result_id'])
-        for test_result in result.tests:
-            self.assertEqual(test_result.result_id, result.id)
+        built_result = self.buildResult(delete_test_key=['result_id'])
+        for test_result in built_result.results.tests:
+            self.assertEqual(test_result.result_id, built_result.results.id)
 
     def _compareSerialized(self, result: TestHarnessResults, new_result: TestHarnessResults,
                            same_tests: bool):
@@ -382,7 +412,9 @@ class TestResultsReaderResults(TestHarnessTestCase):
         Test serializing and deserializing a result when the data
         is not already loaded
         """
-        result_data, test_data = self.captureResult(test_in_results=False)
+        captured_result = self.captureResult(test_in_results=False)
+        result_data = captured_result.result_data
+        test_data = captured_result.test_data
 
         # Mock getting the test results from mongodb
         def find_tests_data(ids):
@@ -411,18 +443,19 @@ class TestResultsReaderResults(TestHarnessTestCase):
         Test serializing and deserializing a result when the test
         data is already loaded in the result data (no database loads)
         """
-        result, _ = self.buildResult()
+        built_result = self.buildResult()
+        results = built_result.results
 
         # Tests should be loaded already
-        for entry in result._tests.values():
+        for entry in results._tests.values():
             self.assertIsInstance(entry, TestHarnessTestResult)
 
-        serialized = result.serialize()
+        serialized = results.serialize()
 
         new_result = TestHarnessResults.deserialize_build(serialized)
 
         # Compare all of the data
-        self._compareSerialized(result, new_result, True)
+        self._compareSerialized(results, new_result, True)
 
     @patch.object(TestHarnessResults, '_find_tests_data')
     def testLoadAllTestsMissing(self, patch_find_tests_data):
@@ -430,12 +463,12 @@ class TestResultsReaderResults(TestHarnessTestCase):
         Tests TestHarnessResults.load_all_data when test(s)
         were not found in the database
         """
-        result_data, _ = self.captureResult(test_in_results=False)
+        captured_result = self.captureResult(test_in_results=False)
 
         patch_find_tests_data.return_value = []
 
         with self.assertRaisesRegex(KeyError, 'Failed to load test results'):
-            TestHarnessResults(result_data).load_all_tests()
+            TestHarnessResults(captured_result.result_data).load_all_tests()
 
 
 if __name__ == '__main__':
