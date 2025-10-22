bc0aa1ea166c283ee4595c8e4400ccf3deb79e55
Logan Harbour <loganhharbour@gmail.com>
2025-10-22 15:03:28 -0600

    Add moose test for /kill
    refs #31755

--- test/tests/controls/web_server_control/errors.py
+++ test/tests/controls/web_server_control/errors.py
@@ -8,8 +8,6 @@
 #* Licensed under LGPL 2.1, please see LICENSE for details
 #* https://www.gnu.org/licenses/lgpl-2.1.html
 
-# pylint: disable=protected-access
-
 import sys
 from testmoosecontrol import TestMooseControl
 
