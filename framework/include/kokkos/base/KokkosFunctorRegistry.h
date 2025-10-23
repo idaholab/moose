b1c9f5a0fac6023992523877b46083deded30292
Namjae Choi <Namjae.Choi@inl.gov>
2025-10-23 14:22:24 -0600

    Kokkos functor and function base classes #30655

---
+++ framework/include/kokkos/base/KokkosFunctorRegistry.h
@@ -0,0 +1,169 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "KokkosFunctorWrapper.h"
+#include "KokkosFunctionWrapper.h"
+
+namespace Moose
+{
+namespace Kokkos
+{
+
+class FunctorRegistryEntryBase
+{
+public:
+  virtual ~FunctorRegistryEntryBase() {}
+  /**
+   * Build a host wrapper for this functor
+   * @param object The pointer to the functor
+   */
+  virtual std::unique_ptr<FunctorWrapperHostBase> build(const void * object) const = 0;
+};
+
+template <typename Object>
+class FunctorRegistryEntry : public FunctorRegistryEntryBase
+{
+public:
+  std::unique_ptr<FunctorWrapperHostBase> build(const void * object) const override final
+  {
+    return std::make_unique<FunctorWrapperHost<Object>>(object);
+  }
+};
+
+class FunctionRegistryEntryBase
+{
+public:
+  virtual ~FunctionRegistryEntryBase() {}
+  /**
+   * Build a host wrapper for this function
+   * @param object The pointer to the function
+   */
+  virtual std::unique_ptr<FunctionWrapperHostBase> build(const void * object) const = 0;
+};
+
+template <typename Object>
+class FunctionRegistryEntry : public FunctionRegistryEntryBase
+{
+public:
+  std::unique_ptr<FunctionWrapperHostBase> build(const void * object) const override final
+  {
+    return std::make_unique<FunctionWrapperHost<Object>>(object);
+  }
+};
+
+class FunctorRegistry
+{
+public:
+  FunctorRegistry() = default;
+
+  FunctorRegistry(FunctorRegistry const &) = delete;
+  FunctorRegistry & operator=(FunctorRegistry const &) = delete;
+
+  FunctorRegistry(FunctorRegistry &&) = delete;
+  FunctorRegistry & operator=(FunctorRegistry &&) = delete;
+
+  /**
+   * Register a functor
+   * @tparam Object The functor class type
+   * @param name The registered functor type name
+   */
+  template <typename Object>
+  static char addFunctor(const std::string & name)
+  {
+    getRegistry()._functors[name] = std::make_unique<FunctorRegistryEntry<Object>>();
+
+    return 0;
+  }
+
+  /**
+   * Register a function
+   * @tparam Object The function class type
+   * @param name The registered function type name
+   */
+  template <typename Object>
+  static char addFunction(const std::string & name)
+  {
+    getRegistry()._functions[name] = std::make_unique<FunctionRegistryEntry<Object>>();
+
+    return 0;
+  }
+
+  /**
+   * Build and get a host wrapper of a functor
+   * @param object The pointer to the functor
+   * @param name The registered functor type name
+   * @returns The host wrapper
+   */
+  static std::unique_ptr<FunctorWrapperHostBase> buildFunctor(const void * object,
+                                                              const std::string & name)
+  {
+    auto it = getRegistry()._functors.find(name);
+    if (it == getRegistry()._functors.end())
+      mooseError("Kokkos functor not registered for type '",
+                 name,
+                 "'. Double check that you used Kokkos-specific registration macro.");
+
+    return it->second->build(object);
+  }
+
+  /**
+   * Build and get a host wrapper of a function
+   * @param object The pointer to the function
+   * @param name The registered function type name
+   * @returns The host wrapper
+   */
+  static std::unique_ptr<FunctionWrapperHostBase> buildFunction(const void * object,
+                                                                const std::string & name)
+  {
+    auto it = getRegistry()._functions.find(name);
+    if (it == getRegistry()._functions.end())
+      mooseError("Kokkos function not registered for type '",
+                 name,
+                 "'. Double check that you used Kokkos-specific registration macro.");
+
+    return it->second->build(object);
+  }
+
+private:
+  /**
+   * Get the registry singleton
+   * @returns The registry singleton
+   */
+  static FunctorRegistry & getRegistry();
+
+  /**
+   * Map containing the host wrapper shells of functors with the key being the registered object
+   * type name
+   */
+  std::map<std::string, std::unique_ptr<FunctorRegistryEntryBase>> _functors;
+  /**
+   * Map containing the host wrapper shells of functions with the key being the registered object
+   * type name
+   */
+  std::map<std::string, std::unique_ptr<FunctionRegistryEntryBase>> _functions;
+};
+
+} // namespace Kokkos
+} // namespace Moose
+
+#define registerKokkosFunction(app, classname)                                                     \
+  registerMooseObject(app, classname);                                                             \
+  static char combineNames(kokkos_functor_##classname, __COUNTER__) =                              \
+      Moose::Kokkos::FunctorRegistry::addFunctor<classname>(#classname);                           \
+  static char combineNames(kokkos_function_##classname, __COUNTER__) =                             \
+      Moose::Kokkos::FunctorRegistry::addFunction<classname>(#classname)
+
+#define registerKokkosFunctionAliased(app, classname, alias)                                       \
+  registerMooseObjectAliased(app, classname, alias);                                               \
+  static char combineNames(kokkos_functor_##classname, __COUNTER__) =                              \
+      Moose::Kokkos::FunctorRegistry::addFunctor<classname>(alias);                                \
+  static char combineNames(kokkos_function_##classname, __COUNTER__) =                             \
+      Moose::Kokkos::FunctorRegistry::addFunction<classname>(alias)
---
+++ framework/include/kokkos/functions/KokkosFunctionWrapper.h
@@ -0,0 +1,219 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "KokkosTypes.h"
+
+namespace Moose
+{
+namespace Kokkos
+{
+
+template <typename Object>
+class FunctionWrapperHost;
+
+/**
+ * Base class for device function wrapper
+ */
+class FunctionWrapperDeviceBase
+{
+public:
+  /**
+   * Constructor
+   */
+  KOKKOS_FUNCTION FunctionWrapperDeviceBase() {}
+  /**
+   * Virtual destructor
+   */
+  KOKKOS_FUNCTION virtual ~FunctionWrapperDeviceBase() {}
+
+  /**
+   * Virtual shims that calls the corresponding methods of the actual stored function
+   */
+  ///{@
+  KOKKOS_FUNCTION virtual Real value(Real t, Real3 p) const = 0;
+  KOKKOS_FUNCTION virtual Real3 vectorValue(Real t, Real3 p) const = 0;
+  KOKKOS_FUNCTION virtual Real3 gradient(Real t, Real3 p) const = 0;
+  KOKKOS_FUNCTION virtual Real3 curl(Real t, Real3 p) const = 0;
+  KOKKOS_FUNCTION virtual Real div(Real t, Real3 p) const = 0;
+  KOKKOS_FUNCTION virtual Real timeDerivative(Real t, Real3 p) const = 0;
+  KOKKOS_FUNCTION virtual Real timeIntegral(Real t1, Real t2, Real3 p) const = 0;
+  KOKKOS_FUNCTION virtual Real integral() const = 0;
+  KOKKOS_FUNCTION virtual Real average() const = 0;
+  ///@}
+};
+
+/**
+ * Device function wrapper class that provides polymorphic interfaces for a function. The function
+ * itself is a static object and does not have any virtual method. Instead, the device wrapper
+ * defines the virtual shims and forwards the calls to the static methods of the stored function.
+ * @tparam Object The function class type
+ */
+template <typename Object>
+class FunctionWrapperDevice : public FunctionWrapperDeviceBase
+{
+  friend class FunctionWrapperHost<Object>;
+
+public:
+  /**
+   * Constructor
+   */
+  KOKKOS_FUNCTION FunctionWrapperDevice() {}
+
+  KOKKOS_FUNCTION Real value(Real t, Real3 p) const override final
+  {
+    return _function->value(t, p);
+  }
+  KOKKOS_FUNCTION Real3 vectorValue(Real t, Real3 p) const override final
+  {
+    return _function->vectorValue(t, p);
+  }
+  KOKKOS_FUNCTION Real3 gradient(Real t, Real3 p) const override final
+  {
+    return _function->gradient(t, p);
+  }
+  KOKKOS_FUNCTION Real3 curl(Real t, Real3 p) const override final { return _function->curl(t, p); }
+  KOKKOS_FUNCTION Real div(Real t, Real3 p) const override final { return _function->div(t, p); }
+  KOKKOS_FUNCTION Real timeDerivative(Real t, Real3 p) const override final
+  {
+    return _function->timeDerivative(t, p);
+  }
+  KOKKOS_FUNCTION Real timeIntegral(Real t1, Real t2, Real3 p) const override final
+  {
+    return _function->timeIntegral(t1, t2, p);
+  }
+  KOKKOS_FUNCTION Real integral() const override final { return _function->integral(); }
+  KOKKOS_FUNCTION Real average() const override final { return _function->average(); }
+
+protected:
+  /**
+   * Pointer to the function on device
+   */
+  Object * _function = nullptr;
+};
+
+/**
+ * Base class for host function wrapper
+ */
+class FunctionWrapperHostBase
+{
+public:
+  /**
+   * Virtual destructor
+   */
+  virtual ~FunctionWrapperHostBase() {}
+
+  /**
+   * Allocate device function and wrapper
+   * @returns The pointer to the device wrapper
+   */
+  virtual FunctionWrapperDeviceBase * allocate() = 0;
+  /**
+   * Copy function to device
+   */
+  virtual void copyFunction() = 0;
+  /**
+   * Free host and device copies of function
+   */
+  virtual void freeFunction() = 0;
+};
+
+/**
+ * Host function wrapper class that allocates a function on device and creates its device wrapper.
+ * This class holds the actual device instance of the function and manages its allocation and
+ * deallocation, and the device wrapper simply keeps a pointer to it.
+ * @tparam Object The function class type
+ */
+template <typename Object>
+class FunctionWrapperHost : public FunctionWrapperHostBase
+{
+public:
+  /**
+   * Constructor
+   * @param function Pointer to the function
+   */
+  FunctionWrapperHost(const void * function)
+    : _function_host(*static_cast<const Object *>(function))
+  {
+  }
+  /**
+   * Destructor
+   */
+  ~FunctionWrapperHost();
+
+  FunctionWrapperDeviceBase * allocate() override final;
+  void copyFunction() override final;
+  void freeFunction() override final;
+
+private:
+  /**
+   * Reference of the function on host
+   */
+  const Object & _function_host;
+  /**
+   * Copy of the function on host
+   */
+  std::unique_ptr<Object> _function_copy;
+  /**
+   * Copy of the function on device
+   */
+  Object * _function_device = nullptr;
+};
+
+template <typename Object>
+FunctionWrapperDeviceBase *
+FunctionWrapperHost<Object>::allocate()
+{
+  // Allocate storage for device wrapper on device
+  auto wrapper_device = static_cast<FunctionWrapperDevice<Object> *>(
+      ::Kokkos::kokkos_malloc<ExecSpace::memory_space>(sizeof(FunctionWrapperDevice<Object>)));
+
+  // Allocate device wrapper on device using placement new to populate vtable with device pointers
+  ::Kokkos::parallel_for(
+      1, KOKKOS_LAMBDA(const int) { new (wrapper_device) FunctionWrapperDevice<Object>(); });
+
+  // Allocate storage for function on device
+  _function_device =
+      static_cast<Object *>(::Kokkos::kokkos_malloc<ExecSpace::memory_space>(sizeof(Object)));
+
+  // Let device wrapper point to the copy
+  ::Kokkos::Impl::DeepCopy<MemSpace, ::Kokkos::HostSpace>(
+      &(wrapper_device->_function), &_function_device, sizeof(Object *));
+
+  return wrapper_device;
+}
+
+template <typename Object>
+void
+FunctionWrapperHost<Object>::copyFunction()
+{
+  // Make a copy of function on host to trigger copy constructor
+  _function_copy = std::make_unique<Object>(_function_host);
+
+  // Copy function to device
+  ::Kokkos::Impl::DeepCopy<MemSpace, ::Kokkos::HostSpace>(
+      _function_device, _function_copy.get(), sizeof(Object));
+}
+
+template <typename Object>
+void
+FunctionWrapperHost<Object>::freeFunction()
+{
+  _function_copy.reset();
+}
+
+template <typename Object>
+FunctionWrapperHost<Object>::~FunctionWrapperHost()
+{
+  ::Kokkos::kokkos_free<ExecSpace::memory_space>(_function_device);
+}
+
+} // namespace Kokkos
+} // namespace Moose
