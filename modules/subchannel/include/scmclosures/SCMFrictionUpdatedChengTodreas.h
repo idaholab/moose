186cd9a23e244c20aa996a0525b31512816b30d3
Vasileios Kyriakopoulos <vasileios.kyriakopoulos@inl.gov>
2025-10-27 16:07:26 -0600

    Add the friction factor computation for Cheng Todreas

---
+++ modules/subchannel/include/scmclosures/SCMFrictionUpdatedChengTodreas.h
@@ -0,0 +1,36 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "SCMFrictionClosureBase.h"
+#include "TriSubChannelMesh.h"
+#include "QuadSubChannelMesh.h"
+
+/**
+ *
+ */
+class SCMFrictionUpdatedChengTodreas : public SCMFrictionClosureBase
+{
+public:
+  static InputParameters validParams();
+
+  SCMFrictionUpdatedChengTodreas(const InputParameters & parameters);
+
+  virtual Real computeFrictionFactor(const FrictionStruct & friction_info) const override;
+
+protected:
+  Real computeTriLatticeFrictionFactor(const FrictionStruct & friction_info) const;
+  Real computeQuadLatticeFrictionFactor(const FrictionStruct & friction_info) const;
+
+  /// Keep track of the lattice type
+  bool _is_tri_lattice;
+  /// Pointer to the tri lattice mesh
+  const TriSubChannelMesh * const _tri_sch_mesh;
+};
