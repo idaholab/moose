7889e3db684117304dad9df97d32355770ffceda
Logan Harbour <loganhharbour@gmail.com>
2025-11-03 08:46:55 -0700

    Add moosepytest module and move common pytest utilities
    refs #31834

--- python/moosepytest/__init__.py
+++ python/moosepytest/__init__.py
@@ -7,10 +7,4 @@
 # Licensed under LGPL 2.1, please see LICENSE for details
 # https://www.gnu.org/licenses/lgpl-2.1.html
 
-"""Configures pytest for the moosecontrol module."""
-
-pytest_plugins = (
-    # Configures pytest for finding and optionally
-    # skipping tests that depend on MOOSE.
-    "moosepytest.mooseexe_plugin",
-)
+"""Contains pytest plugins for testing MOOSE python packages."""
