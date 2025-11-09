30c6579ff6a99f5827aa6c056f23c2a4d748d9e8
Namjae Choi <Namjae.Choi@inl.gov>
2025-11-08 18:59:21 -0700

    Kokkos user object base classes #30655

---
+++ framework/include/kokkos/postprocessors/KokkosGeneralPostprocessor.h
@@ -0,0 +1,37 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "KokkosPostprocessor.h"
+#include "KokkosGeneralUserObject.h"
+
+namespace Moose
+{
+namespace Kokkos
+{
+
+class GeneralPostprocessor : public GeneralUserObject, public Postprocessor
+{
+public:
+  static InputParameters validParams();
+
+  GeneralPostprocessor(const InputParameters & parameters);
+
+  virtual void compute() override { execute(); }
+
+  /**
+   * Finalize is not required for Postprocessor implementations since work may be done in
+   * getValue().
+   */
+  virtual void finalize() override {}
+};
+
+} // namespace Kokkos
+} // namespace Moose
