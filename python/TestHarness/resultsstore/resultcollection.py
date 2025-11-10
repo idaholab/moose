a51fe66ac8fca9831ea27581bcc59024825a5764
Logan Harbour <loganhharbour@gmail.com>
2025-11-10 16:08:57 -0700

    Begin work on get_all_tests method
    refs #31834

--- python/TestHarness/resultsstore/resultcollection.py
+++ python/TestHarness/resultsstore/resultcollection.py
@@ -9,14 +9,16 @@
 
 """Implements the ResultCollection, storage for multiple test results."""
 
-from typing import Callable, Iterable
+from collections import defaultdict
+from typing import Callable, Iterable, Tuple
 
 from bson.objectid import ObjectId
+from pymongo import DESCENDING
 from pymongo.database import Database
 
 from TestHarness.resultsstore.storedresults import StoredResult, StoredTestResult
 from TestHarness.resultsstore.testdatafilters import ALL_TEST_KEYS, TestDataFilter
-from TestHarness.resultsstore.utils import TestName
+from TestHarness.resultsstore.utils import TestName, results_test_iterator
 
 
 class ResultCollection:
@@ -64,6 +66,97 @@ class ResultCollection:
     def get_database(self) -> Database:
         """Get the database."""
         return self._database_getter()
+
+    def get_all_tests(
+        self, filters: Iterable[TestDataFilter]
+    ) -> dict[TestName, list[StoredTestResult]]:
+        """
+        Get all test results.
+
+        This can be particularly expensive! Best to do it on a
+        per-test basis with get_tests() if possible.
+
+        Arguments:
+        ---------
+        filters : Iterable[TestDataFilter]
+            The TestDataFilter objects that represent which data
+            to obtain for the tests.
+
+        """
+        # Whether or not all data was requested
+        all_data = TestDataFilter.ALL in filters
+        # Keys within the test data to get, if not all
+        keys = [v.value for v in filters]
+
+        # Tests stored separately that we need to load later
+        separate_tests: list[Tuple[ObjectId, StoredTestResult]] = []
+        # Storage for all tests
+        tests: dict[TestName, list[StoredTestResult]] = defaultdict(list)
+
+        # Load the full documents; this is generally quicker
+        # than trying to load just the data we need
+        result_it = iter(self.results)
+        with self.get_database().results.find(
+            {"_id": {"$in": self.result_ids}},
+            {"tests": 1},
+            sort=[("_id", DESCENDING)],
+        ) as cursor:
+            for doc in cursor:
+                id = doc["_id"]
+                del doc["_id"]
+
+                result = next(result_it)
+                assert result.id == id
+
+                for test in results_test_iterator(doc):
+                    value = test.value
+                    name = test.name
+                    if not all_data:
+                        value = {k: value[k] for k in keys}
+                    test_result = None
+                    if isinstance(value, dict):
+                        test_result = StoredTestResult(
+                            test.value, name, result, filters
+                        )
+                    else:
+                        test_result = StoredTestResult({}, name, result, filters)
+                        separate_tests.append((value, test_result))
+                    tests[name].append(test_result)
+
+        # Pull the data from the tests stored separately if needed
+        if separate_tests:
+            # Sort based on ID so that we can do the same
+            # when getting the data from the pipeline,
+            # removing the need for us to sort here
+            separate_tests = sorted(separate_tests, key=lambda x: x[0], reverse=True)
+
+            # Setup the pipeline for getting the separate tests
+            pipeline = [
+                # Select only the needed tests
+                {"$match": {"_id": {"$in": [v[0] for v in separate_tests]}}},
+            ]
+            # If not getting all data, filter on just the data we need
+            if not all_data:
+                pipeline += [
+                    {
+                        "$project": {
+                            **{key: 1 for key in keys},
+                        }
+                    },
+                ]
+            # And sort on ID (which was the sort test_ids were passed)
+            pipeline += [{"$sort": {"_id": -1}}]
+
+            # Update each previously-empty StoredTestResult with
+            # the real test data
+            separate_test_it = iter(separate_tests)
+            with self.get_database().tests.aggregate(pipeline) as cursor:
+                for doc in cursor:
+                    id, test = next(separate_test_it)
+                    assert id == doc["_id"]  # should be sorted
+                    test._data = doc
+
+        return tests
 
     def get_tests(
         self, name: TestName, filters: Iterable[TestDataFilter]
