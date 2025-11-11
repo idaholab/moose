ac2d519d8b70322fa0b1d49136d0439ad29a5270
Vasileios Kyriakopoulos <vasileios.kyriakopoulos@inl.gov>
2025-11-11 11:05:19 -0700

    create base class for HTCs Refs #30049

---
+++ modules/subchannel/include/scmclosures/SCMHTCClosureBase.h
@@ -0,0 +1,50 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "SCMClosureBase.h"
+#include "SubChannel1PhaseProblem.h"
+
+struct NusseltPreInfo
+{
+  Real Re;
+  Real Pr;
+  Real poD;
+  Real ReL;
+  Real ReT;
+  Real laminar_Nu;
+  EChannelType subch_type;
+};
+
+/**
+ * Base class for heat transfer coefficients (HTC) closures used in SCM
+ */
+class SCMHTCClosureBase : public SCMClosureBase
+{
+public:
+  static InputParameters validParams();
+
+  SCMHTCClosureBase(const InputParameters & parameters);
+
+  typedef SubChannel1PhaseProblem::FrictionStruct FrictionStruct;
+  typedef SubChannel1PhaseProblem::NusseltStruct NusseltStruct;
+
+  /// @brief Computes the friction factor for the local conditions
+  /// @param friction_info geometrical information about the cell in the channel
+  /// @param nusselt_info  flow/coolant information about the cell in the channel
+  /// @return the Nusselt Number
+  virtual Real computeNusseltNumber(const FrictionStruct & friction_info,
+                                    const NusseltStruct & nusselt_info) const = 0;
+
+  virtual NusseltPreInfo computeNusseltNumberPreInfo(const NusseltStruct & nusselt_info) final;
+
+  virtual Real computeHTC(const FrictionStruct & friction_info,
+                          const NusseltStruct & nusselt_info) const = 0;
+};
---
+++ modules/subchannel/src/scmclosures/SCMHTCClosureBase.C
@@ -0,0 +1,60 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "SCMHTCClosureBase.h"
+#include "SCM.h"
+
+InputParameters
+SCMHTCClosureBase::validParams()
+{
+  InputParameters params = SCMClosureBase::validParams();
+  return params;
+}
+
+SCMHTCClosureBase::SCMHTCClosureBase(const InputParameters & parameters)
+  : SCMClosureBase(parameters)
+{
+}
+
+NusseltPreInfo
+SCMHTCClosureBase::computeNusseltNumberPreInfo(const NusseltStruct & nusselt_args)
+{
+  NusseltPreInfo info;
+  info.Re = nusselt_args.Re;
+  info.Pr = nusselt_args.Pr;
+
+  const auto pitch = _subchannel_mesh.getPitch();
+  Real D;
+  const bool is_duct = (nusselt_args.i_pin == std::numeric_limits<unsigned int>::max());
+  if (!is_duct)
+  {
+    auto Dpin_soln = SolutionHandle(_subproblem.getVariable(0, "Dpin"));
+    const auto * pin_node = _subchannel_mesh.getPinNode(nusselt_args.i_pin, nusselt_args.iz);
+    if (Dpin_soln(pin_node) > 0)
+      D = Dpin_soln(pin_node);
+    else
+      mooseError(name(),
+                 "The diameter of the pin is equal or smaller than zero, "
+                 "please initialize the auxiliary variable Dpin.");
+  }
+  else
+    D = _subchannel_mesh.getPinDiameter();
+
+  info.poD = pitch / D;
+  info.subch_type = _subchannel_mesh.getSubchannelType(nusselt_args.i_ch);
+
+  info.laminar_Nu = (info.subch_type == EChannelType::CENTER)
+                        ? 4.0
+                        : (info.subch_type == EChannelType::EDGE ? 3.7 : 3.3);
+
+  info.ReL = 300 * std::pow(10.0, 1.7 * (info.poD - 1.0));
+  info.ReT = 1e4 * std::pow(10.0, 1.7 * (info.poD - 1.0));
+
+  return info;
+}
