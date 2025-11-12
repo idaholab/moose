d4b4dba8a7e1b8add42ca117559ba868fd21317a
Namjae Choi <Namjae.Choi@inl.gov>
2025-11-11 17:12:28 -0700

    Add Kokkos vector postprocessor and reporter base classes #30655

---
+++ framework/include/kokkos/reporters/KokkosReporter.h
@@ -0,0 +1,33 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "Reporter.h"
+
+namespace Moose
+{
+namespace Kokkos
+{
+
+class Reporter : public ::Reporter
+{
+public:
+  static InputParameters validParams();
+
+  Reporter(const MooseObject * moose_object);
+
+  /**
+   * Copy constructor for parallel dispatch
+   */
+  Reporter(const Reporter & object);
+};
+
+} // namespace Kokkos
+} // namespace Moose
