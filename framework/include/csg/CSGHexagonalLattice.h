4396061e618b5a3ae91dcdb9b8b09e79478c40e2
Kalin Kiesling <kkiesling@anl.gov>
2025-11-12 13:35:17 -0600

    add methods for going between ring and row type index for hex lattices

--- framework/include/csg/CSGHexagonalLattice.h
+++ framework/include/csg/CSGHexagonalLattice.h
@@ -105,6 +105,60 @@ public:
   int getNRings() const;
 
   /**
+   * @brief Get given an index in ring-element form, get the corresponding row-column index. The
+   * ring-element form assumes the outermost ring is the 0th ring and is indexed starting from the
+   * rightmost element of the ring and proceeding counter-clockwise around the ring. The row-column
+   * form assumes the top row is the 0th row and is indexed from left to right.
+   *
+   * Example of corresponding indices for a hexagonal lattice with 3 rings (5 rows):
+   *
+   * Row-Column Form:
+   *        (0,0)  (0,1)  (0,2)
+   *     (1,0)  (1,1)  (1,2)  (1,3)
+   * (2,0)  (2,1)  (2,2)  (2,3)  (2,4)
+   *     (3,0)  (3,1)  (3,2)  (3,3)
+   *        (4,0)  (4,1)  (4,2)
+   *
+   * Ring-Element Form:
+   *       (0,8)  (0,9)  (0,10)
+   *    (0,7)  (1,4)  (1,5)  (0,11)
+   * (0,6)  (1,3)  (2,0)  (1,0)  (0,0)
+   *    (0,5)  (1,2)  (1,1)  (0,1)
+   *       (0,4)  (0,3)  (0,2)
+   *
+   * @param index in ring-element form
+   * @return index in row-column form
+   */
+  std::pair<int, int> getRowIndexFromRingIndex(const std::pair<int, int> & row_col_index) const;
+
+  /**
+   * @brief Get given an index in row-column form, get the corresponding ring-element index. The
+   * row-column form assumes the top row is the 0th row and is indexed from left to right. The
+   * ring-element form assumes the outermost ring is the 0th ring and is indexed starting from the
+   * rightmost element of the ring and proceeding counter-clockwise around the ring.
+   *
+   * Example of corresponding indices for a hexagonal lattice with 3 rings (5 rows):
+   *
+   * Row-Column Form:
+   *        (0,0)  (0,1)  (0,2)
+   *     (1,0)  (1,1)  (1,2)  (1,3)
+   * (2,0)  (2,1)  (2,2)  (2,3)  (2,4)
+   *     (3,0)  (3,1)  (3,2)  (3,3)
+   *        (4,0)  (4,1)  (4,2)
+   *
+   * Ring-Element Form:
+   *       (0,8)  (0,9)  (0,10)
+   *    (0,7)  (1,4)  (1,5)  (0,11)
+   * (0,6)  (1,3)  (2,0)  (1,0)  (0,0)
+   *    (0,5)  (1,2)  (1,1)  (0,1)
+   *       (0,4)  (0,3)  (0,2)
+   *
+   * @param index in row-column form
+   * @return index in ring-element form
+   */
+  std::pair<int, int> getRingIndexFromRowIndex(const std::pair<int, int> & row_col_index) const;
+
+  /**
    * @brief get the pitch of the lattice
    *
    * @return pitch
@@ -123,6 +177,12 @@ protected:
   virtual bool compareDimensions(const CSGLattice & other) const override;
 
   /**
+   * @brief build a mapping of row-column indices to ring-element indices for quick conversion and
+   * look-up. Sets the _row_to_ring_map variable.
+   */
+  void buildIndexMap();
+
+  /**
    * @brief set the universes that define the lattice layout
    *
    * @param universes list of list of universes to set as the lattice map
@@ -136,10 +196,17 @@ protected:
   /// number of rows in the hexagonal lattice (must be odd), should be consistent with the number of rings
   int _nrow;
 
+  /// number of rings in the hexagonal lattice, should be consistent with the number of rows
+  int _nring;
+
+  /// map of row-column indices to ring-element indices for quick conversion and look-up
+  std::map<std::pair<int, int>, std::pair<int, int>> _row_to_ring_map;
+
 #ifdef MOOSE_UNIT_TEST
   /// Friends for unit testing
   ///@{
   FRIEND_TEST(CSGLatticeTest, testHexSetUniverses);
+  FRIEND_TEST(CSGLatticeTest, testConvertRowsRings);
   ///@}
 #endif
 };
