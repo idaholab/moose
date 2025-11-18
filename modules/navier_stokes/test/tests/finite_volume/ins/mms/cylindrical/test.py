3e615012f093892bcdbcfd03006cb383c84fa0b6
Peter German <grmnptr@gmail.com>
2025-11-18 16:30:24 -0700

    Adjust test files. (#31934)

--- modules/navier_stokes/test/tests/finite_volume/ins/mms/cylindrical/test.py
+++ modules/navier_stokes/test/tests/finite_volume/ins/mms/cylindrical/test.py
@@ -22,6 +22,18 @@ class Test2DRC(unittest.TestCase):
             print("%s, %f" % (key, value))
             self.assertTrue(fuzzyAbsoluteEqual(value, 2., .05))
 
+class TestRZViscousSource(unittest.TestCase):
+    def test(self):
+        labels = ['L2u', 'L2v', 'L2p']
+        df = run_spatial('2d-rc-rz-viscous-source.i', [0, 1, 2, 3], y_pp=labels, mpi=2)
+
+        fig = mms.ConvergencePlot(xlabel='Element Size ($h$)', ylabel='$L_2$ Error')
+        fig.plot(df, label=labels, marker='s', markersize=8, num_fitted_points=3, slope_precision=1)
+        fig.save('2d-rc-rz-viscous-source.png')
+        for key, value in fig.label_to_slope.items():
+            print("%s, %f" % (key, value))
+            self.assertTrue(fuzzyAbsoluteEqual(value, 2., .05))
+
 
 if __name__ == '__main__':
     unittest.main(__name__, verbosity=2)
