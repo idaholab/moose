2e2355c05ef7e9e3b8edcc8135bf9829e0b22f1e
Vasileios Kyriakopoulos <vasileios.kyriakopoulos@inl.gov>
2025-11-18 11:43:54 -0700

    create base class and closure for mixing parameter #30049

---
+++ modules/subchannel/include/scmclosures/SCMMixingClosureBase.h
@@ -0,0 +1,32 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "SCMClosureBase.h"
+#include "SubChannel1PhaseProblem.h"
+
+/**
+ * Base class for turbulent mixing closures used in SCM
+ */
+class SCMMixingClosureBase : public SCMClosureBase
+{
+public:
+  static InputParameters validParams();
+
+  SCMMixingClosureBase(const InputParameters & parameters);
+
+  typedef SubChannel1PhaseProblem::FrictionStruct FrictionStruct;
+
+  /// @brief Computes the mixing parameters for the local conditions around gap(i_gap) and axial level(iz)
+  /// @param i_gap and @param iz
+  /// @return the mixing parameter (beta)
+  virtual Real computeMixingParameter(const unsigned int & i_gap,
+                                      const unsigned int & iz) const = 0;
+};
