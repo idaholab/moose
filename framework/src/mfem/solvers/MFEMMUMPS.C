d3d6009b40fe6f622a0f54d3da9e0c1696d7bfbb
Kingsley Collie <101559596+k-collie@users.noreply.github.com>
2025-11-21 18:17:13 +0000

    Add MFEMMumps solver
    refs #31988

---
+++ framework/src/mfem/solvers/MFEMMUMPS.C
@@ -0,0 +1,45 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifdef MOOSE_MFEM_ENABLED
+
+#include "MFEMMUMPS.h"
+#include "MFEMProblem.h"
+
+registerMooseObject("MooseApp", MFEMMUMPS);
+
+InputParameters
+MFEMMUMPS::validParams()
+{
+  InputParameters params = MFEMSolverBase::validParams();
+  params.addClassDescription("MFEM solver for performing direct solves of sparse systems in "
+                             "parallel using the MUMPS library.");
+
+  return params;
+}
+
+MFEMMUMPS::MFEMMUMPS(const InputParameters & parameters) : MFEMSolverBase(parameters)
+{
+  constructSolver(parameters);
+}
+
+void
+MFEMMUMPS::constructSolver(const InputParameters &)
+{
+  _solver = std::make_unique<mfem::MUMPSSolver>(getMFEMProblem().getComm());
+}
+
+void
+MFEMMUMPS::updateSolver(mfem::ParBilinearForm &, mfem::Array<int> &)
+{
+  if (_lor)
+    mooseError("MUMPS solver does not support LOR solve");
+}
+
+#endif
