1fb5360f1a89976e465bb3178e783df5b3ddfb57
“Henrique <henrique.bergallo-rocha@ukaea.uk>
2025-11-21 10:10:39 +0000

    Added MFEMComplexAuxKernel and MFEMComplexGradAux

---
+++ framework/include/mfem/auxkernels/MFEMComplexAuxKernel.h
@@ -0,0 +1,39 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifdef MOOSE_MFEM_ENABLED
+
+#pragma once
+
+#include "MFEMGeneralUserObject.h"
+#include "MFEMContainers.h"
+
+/**
+ * Class to construct an auxiliary solver used to update an auxvariable.
+ */
+class MFEMComplexAuxKernel : public MFEMGeneralUserObject
+{
+public:
+  static InputParameters validParams();
+
+  MFEMComplexAuxKernel(const InputParameters & parameters);
+  virtual ~MFEMComplexAuxKernel() = default;
+
+  /// Method called to update any owned objects upon a mesh update.
+  virtual void update(){};
+
+protected:
+  /// Name of auxvariable to store the result of the auxkernel in.
+  const AuxVariableName _result_var_name;
+
+  /// Reference to result gridfunction.
+  mfem::ParComplexGridFunction & _result_var;
+};
+
+#endif
---
+++ framework/src/mfem/auxkernels/MFEMComplexGradAux.C
@@ -0,0 +1,51 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifdef MOOSE_MFEM_ENABLED
+
+#include "MFEMComplexGradAux.h"
+#include "MFEMProblem.h"
+
+registerMooseObject("MooseApp", MFEMComplexGradAux);
+
+InputParameters
+MFEMComplexGradAux::validParams()
+{
+  InputParameters params = MFEMComplexAuxKernel::validParams();
+  params.addClassDescription(
+      "Calculates the gradient of an H1 conforming source variable and stores the result"
+      " on an H(curl) conforming ND result auxvariable");
+  params.addRequiredParam<VariableName>("source",
+                                        "Scalar H1 MFEMVariable to take the gradient of.");
+  params.addParam<mfem::real_t>("scale_factor", 1.0, "Factor to scale result auxvariable by.");
+  return params;
+}
+
+MFEMComplexGradAux::MFEMComplexGradAux(const InputParameters & parameters)
+  : MFEMComplexAuxKernel(parameters),
+    _source_var_name(getParam<VariableName>("source")),
+    _source_var(*getMFEMProblem().getProblemData().cmplx_gridfunctions.Get(_source_var_name)),
+    _scale_factor(getParam<mfem::real_t>("scale_factor")),
+    _grad(_source_var.real().ParFESpace(), _result_var.real().ParFESpace())
+{
+  _grad.Assemble();
+  _grad.Finalize();
+}
+
+// Computes the auxvariable.
+void
+MFEMComplexGradAux::execute()
+{
+  _result_var.real() = 0;
+  _result_var.imag() = 0;
+  _grad.AddMult(_source_var.real(), _result_var.real(), _scale_factor);
+  _grad.AddMult(_source_var.imag(), _result_var.imag(), _scale_factor);
+}
+
+#endif
