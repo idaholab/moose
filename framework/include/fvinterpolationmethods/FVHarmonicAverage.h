1a882d73d2b25b53d38487de2cfa518cdc9a5fa7
Peter German <grmnptr@gmail.com>
2025-11-24 13:06:46 -0700

    Add harmonic average interpolation. (#31965)

---
+++ framework/include/fvinterpolationmethods/FVHarmonicAverage.h
@@ -0,0 +1,25 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "FVInterpolationMethod.h"
+
+/**
+ * Harmonic-mean interpolation using the geometric weighting stored on FaceInfo.
+ */
+class FVHarmonicAverage : public FVInterpolationMethod
+{
+public:
+  static InputParameters validParams();
+
+  FVHarmonicAverage(const InputParameters & params);
+
+  Real interpolate(const FaceInfo & face, Real elem_value, Real neighbor_value) const;
+};
