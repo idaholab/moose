8a1ea52abfc61a1f4d46f7b4000e5916fbfc363f
“Henrique <henrique.bergallo-rocha@ukaea.uk>
2025-11-28 11:27:34 +0000

    Fix build issues

--- framework/src/mfem/forms/ParMixedSesquilinearForm.C
+++ framework/src/mfem/forms/ParMixedSesquilinearForm.C
@@ -16,21 +16,21 @@ bool ParMixedSesquilinearForm::ImagInteg()
    return (nint != 0);
 }
 
-ParMixedSesquilinearForm::ParMixedSesquilinearForm(ParFiniteElementSpace *pf,
-                                         ComplexOperator::Convention
+ParMixedSesquilinearForm::ParMixedSesquilinearForm(mfem::ParFiniteElementSpace *pf,
+                                         mfem::ComplexOperator::Convention
                                          convention)
    : conv(convention),
-     pblfr(new ParBilinearForm(pf)),
-     pblfi(new ParBilinearForm(pf))
+     pblfr(new mfem::ParBilinearForm(pf)),
+     pblfi(new mfem::ParBilinearForm(pf))
 {}
 
-ParMixedSesquilinearForm::ParMixedSesquilinearForm(ParFiniteElementSpace *pf,
-                                         ParBilinearForm *pbfr,
-                                         ParBilinearForm *pbfi,
-                                         ComplexOperator::Convention convention)
+ParMixedSesquilinearForm::ParMixedSesquilinearForm(mfem::ParFiniteElementSpace *pf,
+                                         mfem::ParBilinearForm *pbfr,
+                                         mfem::ParBilinearForm *pbfi,
+                                         mfem::ComplexOperator::Convention convention)
    : conv(convention),
-     pblfr(new ParBilinearForm(pf,pbfr)),
-     pblfi(new ParBilinearForm(pf,pbfi))
+     pblfr(new mfem::ParBilinearForm(pf,pbfr)),
+     pblfi(new mfem::ParBilinearForm(pf,pbfi))
 {}
 
 ParMixedSesquilinearForm::~ParMixedSesquilinearForm()
@@ -39,58 +39,58 @@ ParMixedSesquilinearForm::~ParMixedSesquilinearForm()
    delete pblfi;
 }
 
-void ParMixedSesquilinearForm::AddDomainIntegrator(BilinearFormIntegrator *bfi_real,
-                                              BilinearFormIntegrator *bfi_imag)
+void ParMixedSesquilinearForm::AddDomainIntegrator(mfem::BilinearFormIntegrator *bfi_real,
+                                              mfem::BilinearFormIntegrator *bfi_imag)
 {
    if (bfi_real) { pblfr->AddDomainIntegrator(bfi_real); }
    if (bfi_imag) { pblfi->AddDomainIntegrator(bfi_imag); }
 }
 
-void ParMixedSesquilinearForm::AddDomainIntegrator(BilinearFormIntegrator *bfi_real,
-                                              BilinearFormIntegrator *bfi_imag,
-                                              Array<int> & elem_marker)
+void ParMixedSesquilinearForm::AddDomainIntegrator(mfem::BilinearFormIntegrator *bfi_real,
+                                              mfem::BilinearFormIntegrator *bfi_imag,
+                                              mfem::Array<int> & elem_marker)
 {
    if (bfi_real) { pblfr->AddDomainIntegrator(bfi_real, elem_marker); }
    if (bfi_imag) { pblfi->AddDomainIntegrator(bfi_imag, elem_marker); }
 }
 
 void
-ParMixedSesquilinearForm::AddBoundaryIntegrator(BilinearFormIntegrator *bfi_real,
-                                           BilinearFormIntegrator *bfi_imag)
+ParMixedSesquilinearForm::AddBoundaryIntegrator(mfem::BilinearFormIntegrator *bfi_real,
+                                           mfem::BilinearFormIntegrator *bfi_imag)
 {
    if (bfi_real) { pblfr->AddBoundaryIntegrator(bfi_real); }
    if (bfi_imag) { pblfi->AddBoundaryIntegrator(bfi_imag); }
 }
 
 void
-ParMixedSesquilinearForm::AddBoundaryIntegrator(BilinearFormIntegrator *bfi_real,
-                                           BilinearFormIntegrator *bfi_imag,
-                                           Array<int> & bdr_marker)
+ParMixedSesquilinearForm::AddBoundaryIntegrator(mfem::BilinearFormIntegrator *bfi_real,
+                                           mfem::BilinearFormIntegrator *bfi_imag,
+                                           mfem::Array<int> & bdr_marker)
 {
    if (bfi_real) { pblfr->AddBoundaryIntegrator(bfi_real, bdr_marker); }
    if (bfi_imag) { pblfi->AddBoundaryIntegrator(bfi_imag, bdr_marker); }
 }
 
 void
-ParMixedSesquilinearForm::AddInteriorFaceIntegrator(BilinearFormIntegrator *bfi_real,
-                                               BilinearFormIntegrator *bfi_imag)
+ParMixedSesquilinearForm::AddInteriorFaceIntegrator(mfem::BilinearFormIntegrator *bfi_real,
+                                               mfem::BilinearFormIntegrator *bfi_imag)
 {
    if (bfi_real) { pblfr->AddInteriorFaceIntegrator(bfi_real); }
    if (bfi_imag) { pblfi->AddInteriorFaceIntegrator(bfi_imag); }
 }
 
 void
-ParMixedSesquilinearForm::AddBdrFaceIntegrator(BilinearFormIntegrator *bfi_real,
-                                          BilinearFormIntegrator *bfi_imag)
+ParMixedSesquilinearForm::AddBdrFaceIntegrator(mfem::BilinearFormIntegrator *bfi_real,
+                                          mfem::BilinearFormIntegrator *bfi_imag)
 {
    if (bfi_real) { pblfr->AddBdrFaceIntegrator(bfi_real); }
    if (bfi_imag) { pblfi->AddBdrFaceIntegrator(bfi_imag); }
 }
 
 void
-ParMixedSesquilinearForm::AddBdrFaceIntegrator(BilinearFormIntegrator *bfi_real,
-                                          BilinearFormIntegrator *bfi_imag,
-                                          Array<int> &bdr_marker)
+ParMixedSesquilinearForm::AddBdrFaceIntegrator(mfem::BilinearFormIntegrator *bfi_real,
+                                          mfem::BilinearFormIntegrator *bfi_imag,
+                                          mfem::Array<int> &bdr_marker)
 {
    if (bfi_real) { pblfr->AddBdrFaceIntegrator(bfi_real, bdr_marker); }
    if (bfi_imag) { pblfi->AddBdrFaceIntegrator(bfi_imag, bdr_marker); }
@@ -110,45 +110,45 @@ ParMixedSesquilinearForm::Finalize(int skip_zeros)
    pblfi->Finalize(skip_zeros);
 }
 
-ComplexHypreParMatrix *
+mfem::ComplexHypreParMatrix *
 ParMixedSesquilinearForm::ParallelAssemble()
 {
-   return new ComplexHypreParMatrix(pblfr->ParallelAssemble(),
+   return new mfem::ComplexHypreParMatrix(pblfr->ParallelAssemble(),
                                     pblfi->ParallelAssemble(),
                                     true, true, conv);
 }
 
 void
-ParMixedSesquilinearForm::FormLinearSystem(const Array<int> &ess_tdof_list,
-                                      Vector &x, Vector &b,
-                                      OperatorHandle &A,
-                                      Vector &X, Vector &B,
+ParMixedSesquilinearForm::FormLinearSystem(const mfem::Array<int> &ess_tdof_list,
+                                      mfem::Vector &x, mfem::Vector &b,
+                                      mfem::OperatorHandle &A,
+                                      mfem::Vector &X, mfem::Vector &B,
                                       int ci)
 {
-   ParFiniteElementSpace *pfes = pblfr->ParFESpace();
+   mfem::ParFiniteElementSpace *pfes = pblfr->ParFESpace();
    const int vsize = pfes->GetVSize();
 
-   // Allocate temporary vector
-   Vector b_0;
+   // Allocate temporary mfem::vector
+   mfem::Vector b_0;
    b_0.UseDevice(true);
    b_0.SetSize(vsize);
    b_0 = 0.0;
 
-   // Extract the real and imaginary parts of the input vectors
+   // Extract the real and imaginary parts of the input mfem::vectors
    MFEM_ASSERT(x.Size() == 2 * vsize, "Input GridFunction of incorrect size!");
    x.Read();
-   Vector x_r; x_r.MakeRef(x, 0, vsize);
-   Vector x_i; x_i.MakeRef(x, vsize, vsize);
+   mfem::Vector x_r; x_r.MakeRef(x, 0, vsize);
+   mfem::Vector x_i; x_i.MakeRef(x, vsize, vsize);
 
    MFEM_ASSERT(b.Size() == 2 * vsize, "Input LinearForm of incorrect size!");
    b.Read();
-   Vector b_r; b_r.MakeRef(b, 0, vsize);
-   Vector b_i; b_i.MakeRef(b, vsize, vsize);
+   mfem::Vector b_r; b_r.MakeRef(b, 0, vsize);
+   mfem::Vector b_i; b_i.MakeRef(b, vsize, vsize);
 
-   if (conv == ComplexOperator::BLOCK_SYMMETRIC) { b_i *= -1.0; }
+   if (conv == mfem::ComplexOperator::BLOCK_SYMMETRIC) { b_i *= -1.0; }
 
    const int tvsize = pfes->GetTrueVSize();
-   OperatorHandle A_r, A_i;
+   mfem::OperatorHandle A_r, A_i;
 
    X.UseDevice(true);
    X.SetSize(2 * tvsize);
@@ -158,12 +158,12 @@ ParMixedSesquilinearForm::FormLinearSystem(const Array<int> &ess_tdof_list,
    B.SetSize(2 * tvsize);
    B = 0.0;
 
-   Vector X_r; X_r.MakeRef(X, 0, tvsize);
-   Vector X_i; X_i.MakeRef(X, tvsize, tvsize);
-   Vector B_r; B_r.MakeRef(B, 0, tvsize);
-   Vector B_i; B_i.MakeRef(B, tvsize, tvsize);
+   mfem::Vector X_r; X_r.MakeRef(X, 0, tvsize);
+   mfem::Vector X_i; X_i.MakeRef(X, tvsize, tvsize);
+   mfem::Vector B_r; B_r.MakeRef(B, 0, tvsize);
+   mfem::Vector B_i; B_i.MakeRef(B, tvsize, tvsize);
 
-   Vector X_0, B_0;
+   mfem::Vector X_0, B_0;
 
    if (RealInteg())
    {
@@ -218,16 +218,16 @@ ParMixedSesquilinearForm::FormLinearSystem(const Array<int> &ess_tdof_list,
       });
       // Modify off-diagonal blocks (imaginary parts of the matrix) to conform
       // with standard essential BC treatment
-      if (A_i.Type() == Operator::Hypre_ParCSR)
+      if (A_i.Type() == mfem::Operator::Hypre_ParCSR)
       {
-         HypreParMatrix * Ah;
+         mfem::HypreParMatrix * Ah;
          A_i.Get(Ah);
          hypre_ParCSRMatrix *Aih = *Ah;
          Ah->HypreReadWrite();
          const int *d_ess_tdof_list =
-            ess_tdof_list.GetMemory().Read(GetHypreForallMemoryClass(), n);
+            ess_tdof_list.GetMemory().Read(mfem::GetHypreForallMemoryClass(), n);
          HYPRE_Int *d_diag_i = Aih->diag->i;
-         real_t *d_diag_data = Aih->diag->data;
+         mfem::real_t *d_diag_data = Aih->diag->data;
          mfem::hypre_forall(n, [=] MFEM_HOST_DEVICE (int k)
          {
             const int j = d_ess_tdof_list[k];
@@ -236,12 +236,12 @@ ParMixedSesquilinearForm::FormLinearSystem(const Array<int> &ess_tdof_list,
       }
       else
       {
-         A_i.As<ConstrainedOperator>()->SetDiagonalPolicy
+         A_i.As<mfem::ConstrainedOperator>()->SetDiagonalPolicy
          (mfem::Operator::DiagonalPolicy::DIAG_ZERO);
       }
    }
 
-   if (conv == ComplexOperator::BLOCK_SYMMETRIC)
+   if (conv == mfem::ComplexOperator::BLOCK_SYMMETRIC)
    {
       B_i *= -1.0;
       b_i *= -1.0;
@@ -259,36 +259,36 @@ ParMixedSesquilinearForm::FormLinearSystem(const Array<int> &ess_tdof_list,
 
    // A = A_r + i A_i
    A.Clear();
-   if ( A_r.Type() == Operator::Hypre_ParCSR ||
-        A_i.Type() == Operator::Hypre_ParCSR )
+   if ( A_r.Type() == mfem::Operator::Hypre_ParCSR ||
+        A_i.Type() == mfem::Operator::Hypre_ParCSR )
    {
-      ComplexHypreParMatrix * A_hyp =
-         new ComplexHypreParMatrix(A_r.As<HypreParMatrix>(),
-                                   A_i.As<HypreParMatrix>(),
+      mfem::ComplexHypreParMatrix * A_hyp =
+         new mfem::ComplexHypreParMatrix(A_r.As<mfem::HypreParMatrix>(),
+                                   A_i.As<mfem::HypreParMatrix>(),
                                    A_r.OwnsOperator(),
                                    A_i.OwnsOperator(),
                                    conv);
-      A.Reset<ComplexHypreParMatrix>(A_hyp, true);
+      A.Reset<mfem::ComplexHypreParMatrix>(A_hyp, true);
    }
    else
    {
-      ComplexOperator * A_op =
-         new ComplexOperator(A_r.As<Operator>(),
-                             A_i.As<Operator>(),
+      mfem::ComplexOperator * A_op =
+         new mfem::ComplexOperator(A_r.As<mfem::Operator>(),
+                             A_i.As<mfem::Operator>(),
                              A_r.OwnsOperator(),
                              A_i.OwnsOperator(),
                              conv);
-      A.Reset<ComplexOperator>(A_op, true);
+      A.Reset<mfem::ComplexOperator>(A_op, true);
    }
    A_r.SetOperatorOwner(false);
    A_i.SetOperatorOwner(false);
 }
 
 void
-ParMixedSesquilinearForm::FormSystemMatrix(const Array<int> &ess_tdof_list,
-                                      OperatorHandle &A)
+ParMixedSesquilinearForm::FormSystemMatrix(const mfem::Array<int> &ess_tdof_list,
+                                      mfem::OperatorHandle &A)
 {
-   OperatorHandle A_r, A_i;
+   mfem::OperatorHandle A_r, A_i;
    if (RealInteg())
    {
       pblfr->FormSystemMatrix(ess_tdof_list, A_r);
@@ -306,10 +306,10 @@ ParMixedSesquilinearForm::FormSystemMatrix(const Array<int> &ess_tdof_list,
    {
       // Modify off-diagonal blocks (imaginary parts of the matrix) to conform
       // with standard essential BC treatment
-      if ( A_i.Type() == Operator::Hypre_ParCSR )
+      if ( A_i.Type() == mfem::Operator::Hypre_ParCSR )
       {
          int n = ess_tdof_list.Size();
-         HypreParMatrix * Ah;
+         mfem::HypreParMatrix * Ah;
          A_i.Get(Ah);
          hypre_ParCSRMatrix * Aih = *Ah;
          for (int k = 0; k < n; k++)
@@ -320,56 +320,56 @@ ParMixedSesquilinearForm::FormSystemMatrix(const Array<int> &ess_tdof_list,
       }
       else
       {
-         A_i.As<ConstrainedOperator>()->SetDiagonalPolicy
+         A_i.As<mfem::ConstrainedOperator>()->SetDiagonalPolicy
          (mfem::Operator::DiagonalPolicy::DIAG_ZERO);
       }
    }
 
    // A = A_r + i A_i
    A.Clear();
-   if ( A_r.Type() == Operator::Hypre_ParCSR ||
-        A_i.Type() == Operator::Hypre_ParCSR )
+   if ( A_r.Type() == mfem::Operator::Hypre_ParCSR ||
+        A_i.Type() == mfem::Operator::Hypre_ParCSR )
    {
-      ComplexHypreParMatrix * A_hyp =
-         new ComplexHypreParMatrix(A_r.As<HypreParMatrix>(),
-                                   A_i.As<HypreParMatrix>(),
+      mfem::ComplexHypreParMatrix * A_hyp =
+         new mfem::ComplexHypreParMatrix(A_r.As<mfem::HypreParMatrix>(),
+                                   A_i.As<mfem::HypreParMatrix>(),
                                    A_r.OwnsOperator(),
                                    A_i.OwnsOperator(),
                                    conv);
-      A.Reset<ComplexHypreParMatrix>(A_hyp, true);
+      A.Reset<mfem::ComplexHypreParMatrix>(A_hyp, true);
    }
    else
    {
-      ComplexOperator * A_op =
-         new ComplexOperator(A_r.As<Operator>(),
-                             A_i.As<Operator>(),
+      mfem::ComplexOperator * A_op =
+         new mfem::ComplexOperator(A_r.As<mfem::Operator>(),
+                             A_i.As<mfem::Operator>(),
                              A_r.OwnsOperator(),
                              A_i.OwnsOperator(),
                              conv);
-      A.Reset<ComplexOperator>(A_op, true);
+      A.Reset<mfem::ComplexOperator>(A_op, true);
    }
    A_r.SetOperatorOwner(false);
    A_i.SetOperatorOwner(false);
 }
 
 void
-ParMixedSesquilinearForm::RecoverFEMSolution(const Vector &X, const Vector &b,
-                                        Vector &x)
+ParMixedSesquilinearForm::RecoverFEMSolution(const mfem::Vector &X, const mfem::Vector &b,
+                                        mfem::Vector &x)
 {
-   ParFiniteElementSpace *pfes = pblfr->ParFESpace();
+   mfem::ParFiniteElementSpace *pfes = pblfr->ParFESpace();
 
-   const Operator &P = *pfes->GetProlongationMatrix();
+   const mfem::Operator &P = *pfes->GetProlongationMatrix();
 
    const int vsize  = pfes->GetVSize();
    const int tvsize = X.Size() / 2;
 
    X.Read();
-   Vector X_r; X_r.MakeRef(const_cast<Vector&>(X), 0, tvsize);
-   Vector X_i; X_i.MakeRef(const_cast<Vector&>(X), tvsize, tvsize);
+   mfem::Vector X_r; X_r.MakeRef(const_cast<mfem::Vector&>(X), 0, tvsize);
+   mfem::Vector X_i; X_i.MakeRef(const_cast<mfem::Vector&>(X), tvsize, tvsize);
 
    x.Write();
-   Vector x_r; x_r.MakeRef(x, 0, vsize);
-   Vector x_i; x_i.MakeRef(x, vsize, vsize);
+   mfem::Vector x_r; x_r.MakeRef(x, 0, vsize);
+   mfem::Vector x_i; x_i.MakeRef(x, vsize, vsize);
 
    // Apply conforming prolongation
    P.Mult(X_r, x_r);
@@ -380,7 +380,7 @@ ParMixedSesquilinearForm::RecoverFEMSolution(const Vector &X, const Vector &b,
 }
 
 void
-ParMixedSesquilinearForm::Update(FiniteElementSpace *nfes)
+ParMixedSesquilinearForm::Update(mfem::FiniteElementSpace *nfes)
 {
    if ( pblfr ) { pblfr->Update(nfes); }
    if ( pblfi ) { pblfi->Update(nfes); }
