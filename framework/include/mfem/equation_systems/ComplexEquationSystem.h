129e5e4cbd7901a167a9d6110c81177e767077f0
“Henrique <henrique.bergallo-rocha@ukaea.uk>
2025-12-02 16:01:55 +0000

    Add mixed slfs to ComplexEquationSystem (closes #32019)
    Add EOF lines

--- framework/include/mfem/equation_systems/ComplexEquationSystem.h
+++ framework/include/mfem/equation_systems/ComplexEquationSystem.h
@@ -9,6 +9,7 @@
 #include "MFEMComplexKernel.h"
 #include "MFEMComplexIntegratedBC.h"
 #include "MFEMComplexEssentialBC.h"
+#include "ParMixedSesquilinearForm.h"
 
 namespace Moose::MFEM
 {
@@ -109,6 +110,7 @@ public:
 protected:
   // Complex Linear and Bilinear Forms
   NamedFieldsMap<mfem::ParSesquilinearForm> _slfs;
+  NamedFieldsMap<ParMixedSesquilinearForm> _mslfs;
   NamedFieldsMap<mfem::ParComplexLinearForm> _clfs;
 
   // Complex kernels and integrated BCs
