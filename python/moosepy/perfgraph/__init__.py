ce450acc49e7b5f4fd8c0b2510a10577f64b3b14
Logan Harbour <loganhharbour@gmail.com>
2025-12-02 22:19:43 -0700

    Add module docstring
    refs #31994

--- python/moosepy/perfgraph/__init__.py
+++ python/moosepy/perfgraph/__init__.py
@@ -7,6 +7,8 @@
 # Licensed under LGPL 2.1, please see LICENSE for details
 # https://www.gnu.org/licenses/lgpl-2.1.html
 
+"""Implements the PerfGraph, a python representation of the MOOSE PerfGraph."""
+
 from .perfgraph import PerfGraph
 from .perfgraphnode import PerfGraphNode
 from .perfgraphsection import PerfGraphSection
