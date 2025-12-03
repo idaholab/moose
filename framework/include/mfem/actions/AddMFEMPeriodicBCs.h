1b116b3d46f250f0b4d8693859236ec2892e9847
Sean Baccas <sean.baccas@ukaea.uk>
2025-12-03 15:55:04 +0000

    Committing wip

---
+++ framework/include/mfem/actions/AddMFEMPeriodicBCs.h
@@ -0,0 +1,26 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifdef MOOSE_MFEM_ENABLED
+
+#pragma once
+
+#include "Action.h"
+
+class AddMFEMPeriodicBCs : public Action
+{
+public:
+  static InputParameters validParams();
+
+  AddMFEMPeriodicBCs(const InputParameters & parameters);
+
+  virtual void act() override;
+};
+
+#endif
