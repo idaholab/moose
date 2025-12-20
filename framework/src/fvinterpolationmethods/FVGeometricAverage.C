0f2f6de512a8b643986ad6bbc4e35c4f805dc9c6
Peter German <grmnptr@gmail.com>
2025-12-19 17:18:27 -0700

    Move from general uo to mooseo. (#31965)

--- framework/src/fvinterpolationmethods/FVGeometricAverage.C
+++ framework/src/fvinterpolationmethods/FVGeometricAverage.C
@@ -7,7 +7,7 @@
 //* Licensed under LGPL 2.1, please see LICENSE for details
 //* https://www.gnu.org/licenses/lgpl-2.1.html
 
-#include "fvinterpolationmethods/FVGeometricAverage.h"
+#include "FVGeometricAverage.h"
 
 registerMooseObject("MooseApp", FVGeometricAverage);
 
--- framework/src/fvinterpolationmethods/FVInterpolationMethod.C
+++ framework/src/fvinterpolationmethods/FVInterpolationMethod.C
@@ -7,19 +7,20 @@
 //* Licensed under LGPL 2.1, please see LICENSE for details
 //* https://www.gnu.org/licenses/lgpl-2.1.html
 
-#include "fvinterpolationmethods/FVInterpolationMethod.h"
+#include "FVInterpolationMethod.h"
 
 InputParameters
 FVInterpolationMethod::validParams()
 {
-  InputParameters params = GeneralUserObject::validParams();
+  InputParameters params = MooseObject::validParams();
   params.registerBase("FVInterpolationMethod");
+  params.registerSystemAttributeName("FVInterpolationMethod");
   params.addClassDescription(
       "Base class for defining face interpolation schemes used by finite volume objects.");
   return params;
 }
 
 FVInterpolationMethod::FVInterpolationMethod(const InputParameters & params)
-  : GeneralUserObject(params)
+  : MooseObject(params)
 {
 }
