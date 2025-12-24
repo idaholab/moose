8d0037c02bb239c041894ffd80f92ffc3d36ea20
Peter German <grmnptr@gmail.com>
2025-12-23 17:36:36 -0700

    Make sure we only check if we are solving in SIMPLESolve. (#32151)

--- modules/navier_stokes/src/executioners/SIMPLESolve.C
+++ modules/navier_stokes/src/executioners/SIMPLESolve.C
@@ -37,23 +37,25 @@ void
 SIMPLESolve::checkIntegrity()
 {
   // check to make sure that we don't have any time kernels in this simulation (Steady State)
-  for (const auto system : _momentum_systems)
-    checkTimeKernels(*system);
+  if (_solve_momentum)
+    for (const auto system : _momentum_systems)
+      checkTimeKernels(*system);
 
-  checkTimeKernels(_pressure_system);
+  if (_solve_pressure)
+    checkTimeKernels(_pressure_system);
 
-  if (_has_energy_system)
+  if (_has_energy_system && _solve_energy)
     checkTimeKernels(*_energy_system);
 
-  if (_has_turbulence_systems)
+  if (_has_turbulence_systems && _solve_turbulence)
     for (const auto system : _turbulence_systems)
       checkTimeKernels(*system);
 
-  if (_has_passive_scalar_systems)
+  if (_has_passive_scalar_systems && _solve_passive_scalars)
     for (const auto system : _passive_scalar_systems)
       checkTimeKernels(*system);
 
-  if (_has_active_scalar_systems)
+  if (_has_active_scalar_systems && _solve_active_scalars)
     for (const auto system : _active_scalar_systems)
       checkTimeKernels(*system);
 }
