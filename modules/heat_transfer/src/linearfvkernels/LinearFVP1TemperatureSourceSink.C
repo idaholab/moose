42bfd188f346ae1718a6ba3093f7831bb71bcc0b
Ramiro Freile <ramiro.freile@inl.gov>
2025-12-23 19:38:02 -0700

    Removing unnecessary print statements. #31297

--- modules/heat_transfer/src/linearfvkernels/LinearFVP1TemperatureSourceSink.C
+++ modules/heat_transfer/src/linearfvkernels/LinearFVP1TemperatureSourceSink.C
@@ -39,7 +39,6 @@ LinearFVP1TemperatureSourceSink::computeMatrixContribution()
 {
   const auto elem_arg = makeElemArg(_current_elem_info->elem());
   const auto state_arg = determineState();
-  // std::cout << "TemperatureSourceSink_T" << _var.getElemValue(*_current_elem_info, state_arg) << std::endl;
 
   return  4.0 * HeatConduction::Constants::sigma * std::pow(_var.getElemValue(*_current_elem_info, state_arg),3) *
           _sigma_a(elem_arg,state_arg) * _current_elem_volume;
@@ -50,7 +49,6 @@ LinearFVP1TemperatureSourceSink::computeRightHandSideContribution()
 {
   const auto elem_arg = makeElemArg(_current_elem_info->elem());
   const auto state_arg = determineState();
-  //std::cout << "TemperatureSourceSink_G" <<  _G(elem_arg,state_arg) << std::endl;
 
   return _sigma_a(elem_arg, state_arg) * _G(elem_arg,state_arg) *
         _current_elem_volume;
