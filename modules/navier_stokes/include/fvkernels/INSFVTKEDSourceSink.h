a95b49f58d588ee2a2f05b99f39e7b33cb050342
tanome <mauriciotano@gmail.com>
2025-12-25 15:33:42 -0700

    correcting physics and extending mods to nonlinear kEpsilon too Refs #32156

--- modules/navier_stokes/include/fvkernels/INSFVTKEDSourceSink.h
+++ modules/navier_stokes/include/fvkernels/INSFVTKEDSourceSink.h
@@ -63,10 +63,10 @@ protected:
   NS::WallTreatmentEnum _wall_treatment;
 
   /// Value of the first epsilon closure coefficient
-  const Real _C1_eps;
+  const Moose::Functor<ADReal> & _C1_eps;
 
   /// Value of the second epsilon closure coefficient
-  const Real _C2_eps;
+  const Moose::Functor<ADReal> & _C2_eps;
 
   /// C_mu constant
   const Real _C_mu;
