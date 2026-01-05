ac6d18c38325f896d77ef8c22e34a713e1f93a13
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2026-01-05 22:50:16 +0100

    Add the ability to limit the vectors to include refs #32173

--- framework/src/vectorpostprocessors/CombinedVectorPostprocessor.C
+++ framework/src/vectorpostprocessors/CombinedVectorPostprocessor.C
@@ -20,13 +20,14 @@ CombinedVectorPostprocessor::validParams()
 
   params.addRequiredParam<std::vector<VectorPostprocessorName>>(
       "vectorpostprocessors", "The vectorpostprocessors whose vector values are to be combined");
-
+  params.addParam<std::vector<std::vector<std::string>>>(
+      "vectors", "Vectors to combine from each vectorpostprocessor");
   params.addClassDescription(
       "Outputs the values of an arbitrary user-specified set of "
       "vectorpostprocessors as a combined vector in the order specified by the user");
 
-  // The value from this VPP is naturally already on every processor
-  // TODO: Make this not the case!  See #11415
+  // If the vectorpostprocessors are already broadcasted, this VPP should not need to
+  // broadcast on its own
   params.set<bool>("_auto_broadcast") = false;
 
   return params;
@@ -39,25 +40,35 @@ CombinedVectorPostprocessor::CombinedVectorPostprocessor(const InputParameters &
   std::vector<VectorPostprocessorName> vpps_names(
       getParam<std::vector<VectorPostprocessorName>>("vectorpostprocessors"));
   std::vector<std::vector<std::string>> vpp_vectors(vpps_names.size());
-  for (const auto i : index_range(vpps_names))
-  {
-    const auto & vpp_name = vpps_names[i];
-    const auto & vpp = _vpp_fe_problem.getVectorPostprocessorObjectByName(vpp_name);
-    for (const auto & vec_name : vpp.getVectorNames())
+  bool vecs_from_param = isParamValid("vectors");
+  if (!vecs_from_param)
+    for (const auto i : index_range(vpps_names))
     {
-      vpp_vectors[i].push_back(vec_name);
-      // Declare vectors
-      _vpp_vecs.push_back(&this->declareVector(vpp_name + "_" + vec_name));
+      const auto & vpp_name = vpps_names[i];
+      const auto & vpp = _vpp_fe_problem.getVectorPostprocessorObjectByName(vpp_name);
+      for (const auto & vec_name : vpp.getVectorNames())
+        vpp_vectors[i].push_back(vec_name);
     }
+  if (vecs_from_param)
+  {
+    vpp_vectors = getParam<std::vector<std::vector<std::string>>>("vectors");
+    if (vpp_vectors.size() != vpps_names.size())
+      paramError(
+          "vectors",
+          "Outer vector size should be the same size as the 'vectorpostprocessors' parameter");
   }
+
+  // Declare vectors
+  for (const auto i : index_range(vpp_vectors))
+    for (const auto j : index_range(vpp_vectors[i]))
+      _vpp_vecs.push_back(&this->declareVector(vpps_names[i] + "_" + vpp_vectors[i][j]));
 
   // Grab a reference to all the values
   for (const auto i : index_range(vpps_names))
     for (const auto & vec_name : vpp_vectors[i])
-    {
-      std::cout << vpps_names[i] << " " << vec_name << std::endl;
-      _vectorpostprocessor_values.push_back(&getVectorPostprocessorValueByName(vpps_names[i], vec_name));
-    }
+      // This will error if the vectors don't exist in the VPP
+      _vectorpostprocessor_values.push_back(
+          &getVectorPostprocessorValueByName(vpps_names[i], vec_name));
 }
 
 void
