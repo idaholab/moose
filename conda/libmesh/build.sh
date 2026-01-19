e2094a930b10464a64bc7283de7ca5b28f25863f
Logan Harbour <loganhharbour@gmail.com>
2026-01-19 10:53:18 -0700

    Fix LDFLAGS and CTUNING for arm mac
    refs #31935

--- conda/libmesh/build.sh
+++ conda/libmesh/build.sh
@@ -9,7 +9,7 @@ function set_libmesh_env(){
           FORTRANFLAGS DEBUG_FFLAGS DEBUG_FORTRANFLAGS
 
     if [[ "$(uname)" == Darwin ]]; then
-        if [[ $HOST == arm64-apple-darwin20.0.0 ]]; then
+        if [[ $HOST == arm64-apple-darwin* ]]; then
             CTUNING="-march=armv8.3-a -I${PREFIX:?}/include"
             export LIBRARY_PATH="${PREFIX:?}/lib"
         else
@@ -32,10 +32,9 @@ function set_libmesh_env(){
     export CXXFLAGS="${CTUNING}"
     export HYDRA_LAUNCHER=fork
     export INSTALL_BINARY="${SRC_DIR:?}/build-aux/install-sh -C"
-    export LDFLAGS=""
 
-    if [[ $HOST == arm64-apple-darwin20.0.0 ]]; then
-        LDFLAGS="-L${PREFIX:?}/lib -rpath,${PREFIX:?}/lib"
+    if [[ $HOST == arm64-apple-darwin* ]]; then
+        export LDFLAGS="-L${PREFIX:?}/lib -Wl,-rpath,${PREFIX:?}/lib"
     fi
 }
 
