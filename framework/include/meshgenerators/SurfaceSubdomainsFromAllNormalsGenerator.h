8acdbbb26cbfda765ff499aa3c4795daf5561416
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2026-02-06 15:55:21 -0700

    Add a mesh generator to paint 2D surface subdomains

---
+++ framework/include/meshgenerators/SurfaceSubdomainsFromAllNormalsGenerator.h
@@ -0,0 +1,31 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "SubdomainsGeneratorBase.h"
+
+/**
+ * This class will add subdomains to the entire mesh based on unique normals.
+ * Note: The user will have to turn the tolerance on the normal angle to be able to
+ * "paint" curved subdomains. Tight tolerances will create one subdomain per element
+ */
+class SurfaceSubdomainsFromAllNormalsGenerator : public SubdomainsGeneratorBase
+{
+public:
+  static InputParameters validParams();
+
+  SurfaceSubdomainsFromAllNormalsGenerator(const InputParameters & parameters);
+
+  std::unique_ptr<MeshBase> generate() override;
+
+protected:
+  /// Map from subdomain IDs to the normals of the corresponding boundaries
+  std::map<SubdomainID, RealVectorValue> _subdomain_to_normal_map;
+};
---
+++ framework/src/meshgenerators/SurfaceSubdomainsFromAllNormalsGenerator.C
@@ -0,0 +1,105 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "SurfaceSubdomainsFromAllNormalsGenerator.h"
+#include "Parser.h"
+#include "InputParameters.h"
+#include "CastUniquePointer.h"
+
+#include "libmesh/fe_base.h"
+#include "libmesh/mesh_generation.h"
+#include "libmesh/mesh.h"
+#include "libmesh/string_to_enum.h"
+#include "libmesh/quadrature_gauss.h"
+#include "libmesh/point_locator_base.h"
+#include "libmesh/distributed_mesh.h"
+#include "libmesh/elem.h"
+
+#include <typeinfo>
+
+registerMooseObject("MooseApp", SurfaceSubdomainsFromAllNormalsGenerator);
+
+InputParameters
+SurfaceSubdomainsFromAllNormalsGenerator::validParams()
+{
+  InputParameters params = SubdomainsGeneratorBase::validParams();
+
+  // There can be many of them
+  params.suppressParameter<std::vector<SubdomainName>>("new_subdomain");
+  params.addPrivateParam<bool>("_using_normal", true);
+
+  params.addClassDescription(
+      "Adds subdomains to surface (2D) elements in the (3D) mesh based on unique normals.");
+  return params;
+}
+
+SurfaceSubdomainsFromAllNormalsGenerator::SurfaceSubdomainsFromAllNormalsGenerator(
+    const InputParameters & parameters)
+  : SubdomainsGeneratorBase(parameters)
+{
+}
+
+std::unique_ptr<MeshBase>
+SurfaceSubdomainsFromAllNormalsGenerator::generate()
+{
+  std::unique_ptr<MeshBase> mesh = std::move(_input);
+  if (!mesh->is_replicated())
+    mooseError(
+        "SurfaceSubdomainsFromAllNormalsGenerator is not implemented for distributed meshes");
+  setup(*mesh);
+
+  _visited.clear();
+
+  // We'll need to loop over all of the elements to find ones that match this normal.
+  // We can't rely on flood catching them all here...
+  for (auto & elem : mesh->element_ptr_range())
+  {
+    // Nothing to do with edges
+    if (elem->n_nodes() < 3)
+      continue;
+    // Nothing to do with 3D elements
+    if (elem->dim() > 2)
+      continue;
+
+    // Compute the normal
+    const auto & p1 = elem->point(0);
+    const auto & p2 = elem->point(1);
+    const auto & p3 = elem->point(2);
+    auto normal = (p1 - p2).cross(p1 - p3);
+    if (normal.norm_sq() == 0)
+    {
+      mooseWarning("Colinear nodes on elements, skipping subdomain assignment for this element");
+      continue;
+    }
+    normal = normal.unit();
+
+    // See if we've seen this normal before (linear search)
+    const std::map<SubdomainID, RealVectorValue>::value_type * item = nullptr;
+    for (const auto & id_pair : _subdomain_to_normal_map)
+      if (normalsWithinTol(id_pair.second, normal, _normal_tol))
+      {
+        item = &id_pair;
+        break;
+      }
+
+    // Flood with the previously created subdomains and normals
+    if (item)
+      flood(elem, item->second, item->first);
+    // Flood with a new subdomain and the element normal
+    else
+    {
+      subdomain_id_type id = MooseMeshUtils::getNextFreeSubdomainID(*mesh);
+      _subdomain_to_normal_map[id] = normal;
+      flood(elem, normal, id);
+    }
+  }
+
+  mesh->set_isnt_prepared();
+  return dynamic_pointer_cast<MeshBase>(mesh);
+}
