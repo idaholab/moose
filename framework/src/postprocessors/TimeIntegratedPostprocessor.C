ebec368b99f1f7fba578943b61b338df075550a9
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2026-02-16 14:19:57 -0700

    Implement implicit euler in the TimeIntegratedPP so we don't get a warning in normal use refs #2077

--- framework/src/postprocessors/TimeIntegratedPostprocessor.C
+++ framework/src/postprocessors/TimeIntegratedPostprocessor.C
@@ -8,8 +8,8 @@
 //* https://www.gnu.org/licenses/lgpl-2.1.html
 
 #include "TimeIntegratedPostprocessor.h"
-#include "ExplicitMidpoint.h"
 #include "ImplicitMidpoint.h"
+#include "ImplicitEuler.h"
 #include "TransientBase.h"
 
 registerMooseObject("MooseApp", TimeIntegratedPostprocessor);
@@ -22,8 +22,11 @@ InputParameters
 TimeIntegratedPostprocessor::validParams()
 {
   InputParameters params = GeneralPostprocessor::validParams();
-  params.addClassDescription("Integrate a Postprocessor value over time using trapezoidal rule.");
+  params.addClassDescription("Integrate a Postprocessor value over time.");
   params.addParam<PostprocessorName>("value", "The name of the postprocessor");
+  MooseEnum schemes("implicit-euler implicit-midpoint", "implicit-midpoint");
+  params.addParam<MooseEnum>(
+      "time_integration_scheme", schemes, "Time integration scheme to use for the postprocessors");
   return params;
 }
 
@@ -32,16 +35,37 @@ TimeIntegratedPostprocessor::TimeIntegratedPostprocessor(const InputParameters &
     _value(0),
     _value_old(getPostprocessorValueOldByName(name())),
     _pps_value(getPostprocessorValue("value")),
-    _pps_value_old(getPostprocessorValueOld("value"))
+    _pps_value_old(getPostprocessorValueOld("value")),
+    _time_integration_scheme(getParam<MooseEnum>("time_integration_scheme"))
 {
   if (!dynamic_cast<TransientBase *>(_app.getExecutioner()))
     mooseError(
         "Time integration of postprocessor has only been implemented with a transient executioner");
   const auto time_integrators =
       dynamic_cast<TransientBase *>(_app.getExecutioner())->getTimeIntegrators();
-  for (const auto & ti : time_integrators)
-    if (!dynamic_cast<ExplicitMidpoint *>(ti) && !dynamic_cast<ImplicitMidpoint *>(ti))
-      mooseDoOnce(mooseWarning("The time integration coded in this postprocessor uses the midpoint method. The variable time integration, notably with time integrator '" + ti->name()+"' is not using the mid point method. If the postprocessor uses variable values, even indirectly, we would recommend you code the same time integration method for the time-integrated postprocessor."));
+  // Only check if the user did not select it manually
+  if (!isParamSetByUser("time_integration_scheme"))
+  {
+    for (const auto & ti : time_integrators)
+      if (_time_integration_scheme == TimeIntegration::implicit_midpoint &&
+          !dynamic_cast<ImplicitMidpoint *>(ti))
+        mooseDoOnce(
+            mooseWarning("The time integration in this postprocessor uses the midpoint method. The "
+                         "variable time integration, notably with time integrator '" +
+                         ti->name() +
+                         "' is not using the mid point method. If the postprocessor uses variable "
+                         "values, even indirectly, we would recommend you code the same time "
+                         "integration method for the time-integrated postprocessor."));
+      else if (_time_integration_scheme == TimeIntegration::implicit_euler &&
+               !dynamic_cast<ImplicitEuler *>(ti))
+        mooseDoOnce(mooseWarning(
+            "The time integration in this postprocessor uses first order implicit Euler "
+            "method. The variable time integration, notably with time integrator '" +
+            ti->name() +
+            "' is not using the mid point method. If the postprocessor uses variable "
+            "values, even indirectly, we would recommend you code the same time "
+            "integration method for the time-integrated postprocessor."));
+  }
 }
 
 void
@@ -52,7 +76,11 @@ TimeIntegratedPostprocessor::initialize()
 void
 TimeIntegratedPostprocessor::execute()
 {
-  _value = _value_old + 0.5 * (_pps_value + _pps_value_old) * _dt;
+  if (_time_integration_scheme == TimeIntegration::implicit_euler)
+    _value = _value_old + _pps_value * _dt;
+  // 2nd order midpoint is a better default for other integrators
+  else
+    _value = _value_old + 0.5 * (_pps_value + _pps_value_old) * _dt;
 }
 
 Real
