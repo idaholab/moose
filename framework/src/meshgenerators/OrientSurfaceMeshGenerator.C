4dd59d5111d2674dce22c4c033f4537ed4885144
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2026-02-24 19:11:10 -0700

    Add documentation to OrientSurfaceMG Fixup parameter descriptions No need to restrict to replicated

--- framework/src/meshgenerators/OrientSurfaceMeshGenerator.C
+++ framework/src/meshgenerators/OrientSurfaceMeshGenerator.C
@@ -36,23 +36,26 @@ OrientSurfaceMeshGenerator::validParams()
       "Subdomain names or ids for the elements that may have their normal modified.");
 
   // How to set the normal
-  params.renameParam(
-      "normal", "normal_to_align_with", "Direction vector that element normals should align with");
+  params.renameParam("normal",
+                     "normal_to_align_with",
+                     "Direction vector that element normals should be pointing in the same "
+                     "direction as (dot product > 0)");
   params.suppressParameter<Real>("normal_tol");
   params.suppressParameter<Real>("flipped_normal_tol");
   params.suppressParameter<bool>("fixed_normal");
   params.suppressParameter<bool>("allow_normal_flips");
 
-  // Flooding / spreading algorithm parameters
-  params.suppressParameter<std::vector<Real>>("max_subdomain_size_centroids");
+  // Flooding / spreading algorithm parameters are not used
+  // as the flooding is not used for now
+  params.suppressParameter<std::vector<Real>>("max_paint_size_centroids");
   params.suppressParameter<bool>("check_painted_neighbor_normals");
 
-  // There can be many of them, and we don't control the number in this generator
+  // This mesh generator does not modify the subdomains
   params.suppressParameter<std::vector<SubdomainName>>("new_subdomain");
   // We are changing the normals, not using them to paint
   params.addPrivateParam<bool>("_using_normal", false);
 
-  params.addClassDescription("Change the orientation of the surface mesh.");
+  params.addClassDescription("Change the orientation of (part of) the surface mesh.");
   return params;
 }
 
@@ -65,8 +68,6 @@ std::unique_ptr<MeshBase>
 OrientSurfaceMeshGenerator::generate()
 {
   std::unique_ptr<MeshBase> mesh = std::move(_input);
-  if (!mesh->is_replicated())
-    mooseError("OrientSurfaceMeshGenerator is not implemented for distributed meshes");
   setup(*mesh);
 
   unsigned int num_flipped = 0;
@@ -100,8 +101,7 @@ OrientSurfaceMeshGenerator::generate()
   }
 
   if (num_flipped)
-    _console << "Flipped the normal for " << num_flipped << " surface elements." << std::endl;
+    _console << "Flipped the orientation of " << num_flipped << " surface elements." << std::endl;
 
-  mesh->set_isnt_prepared();
   return dynamic_pointer_cast<MeshBase>(mesh);
 }
