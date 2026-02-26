437ff409be5bf48784c1a87e51937d11d21bad06
“Henrique <henrique.bergallo-rocha@ukaea.uk>
2026-02-26 11:03:34 +0000

    Added complex ICs

---
+++ framework/include/mfem/ics/MFEMComplexScalarBoundaryIC.h
@@ -0,0 +1,28 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifdef MOOSE_MFEM_ENABLED
+
+#pragma once
+
+#include "MFEMInitialCondition.h"
+#include "MFEMBoundaryRestrictable.h"
+
+/**
+ * Class used to set the initial value(s) on a scalar valued MFEMVariable.
+ */
+class MFEMComplexScalarBoundaryIC : public MFEMInitialCondition, public MFEMBoundaryRestrictable
+{
+public:
+  static InputParameters validParams();
+  MFEMComplexScalarBoundaryIC(const InputParameters & params);
+  virtual void execute() override;
+};
+
+#endif
