b6a7a7c0d3718461661168db99f021eb1be56a0e
“Henrique <henrique.bergallo-rocha@ukaea.uk>
2026-02-26 11:04:02 +0000

    Added MFEMMixedVectorWeakDivergenceKernel

---
+++ framework/include/mfem/kernels/MFEMMixedVectorWeakDivergenceKernel.h
@@ -0,0 +1,34 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifdef MOOSE_MFEM_ENABLED
+
+#pragma once
+
+#include "MFEMMixedBilinearFormKernel.h"
+
+/**
+ * \f[
+ * (-k \vec u, \nabla v)
+ * \f]
+ */
+class MFEMMixedVectorWeakDivergenceKernel : public MFEMMixedBilinearFormKernel
+{
+public:
+  static InputParameters validParams();
+
+  MFEMMixedVectorWeakDivergenceKernel(const InputParameters & parameters);
+
+  virtual mfem::BilinearFormIntegrator * createMBFIntegrator() override;
+
+protected:
+  mfem::Coefficient & _coef;
+};
+
+#endif
