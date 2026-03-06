04b7bb93394112f7098f6b33ddbfbd7404115dcf
Alexander Blair <alexander.blair@ukaea.uk>
2026-03-06 16:42:03 +0000

    Add patched version of mfem::ParMixedBilinearForm to support trial variables defined on a submesh of the test space

---
+++ framework/include/mfem/equation_systems/PatchedMixedBilinearForm.h
@@ -0,0 +1,36 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifdef MOOSE_MFEM_ENABLED
+
+#pragma once
+
+#include "libmesh/ignore_warnings.h"
+#include "mfem/miniapps/common/pfem_extras.hpp"
+#include "libmesh/restore_warnings.h"
+
+namespace Moose::MFEM
+{
+
+/// Patched version of mfem::ParMixedBilinearForm to extend support to cases where MFEM trial variable is defined on a submesh of the MFEM test variable
+class ParMixedBilinearForm : public mfem::ParMixedBilinearForm
+{
+
+public:
+  ParMixedBilinearForm(mfem::ParFiniteElementSpace * tr_fes, mfem::ParFiniteElementSpace * te_fes)
+    : mfem::ParMixedBilinearForm(tr_fes, te_fes)
+  {
+  }
+
+  void Assemble(int skip_zeros = 1);
+  void SubMeshTolerantAssemble(int skip_zeros);
+};
+} // namespace Moose::MFEM
+
+#endif
