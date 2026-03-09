e0128bfda273261673c89d9916276584870518c4
Roy Stogner <Roy.Stogner@inl.gov>
2026-03-09 10:22:03 -0500

    Don't over-/under-prep after subdomain id changes
    We do need to mark the cached subdomain id sets as unprepared; we no
    longer need to mark the whole mesh as unprepared.

--- modules/reactor/src/meshgenerators/PatternedHexMeshGenerator.C
+++ modules/reactor/src/meshgenerators/PatternedHexMeshGenerator.C
@@ -1021,7 +1021,7 @@ PatternedHexMeshGenerator::generate()
     setMeshProperty("interface_boundary_ids", interface_boundary_ids);
   }
 
-  out_mesh->unset_is_prepared();
+  out_mesh->unset_has_cached_elem_data(); // We changed subdomain ids
   auto mesh = dynamic_pointer_cast<MeshBase>(out_mesh);
   return mesh;
 }
