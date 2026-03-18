f0cc2b97f13d99f4db722ed7386064793d2dc479
Lynn Munday <lynn.munday@inl.gov>
2026-03-18 10:04:13 -0600

    Cleanup command creation

--- python/TestHarness/testers/TaoGradientTester.py
+++ python/TestHarness/testers/TaoGradientTester.py
@@ -52,6 +52,7 @@ class TaoGradientTester(RunApp):
         # Get the base command from RunApp
         cmd = super().getCommand(options)
 
+        # Build PETSc options
         petsc_options = [
             ("-tao_max_it", "1"),
             ("-tao_fd_test", "true"),
@@ -59,17 +60,18 @@ class TaoGradientTester(RunApp):
             ("-tao_fd_gradient", "false"),
             ("-tao_ls_type", "unit"),
         ]
+        if (tao_fd_delta := self.specs["tao_fd_delta"]) is not None:
+            petsc_options.append(("-tao_fd_delta", f"{tao_fd_delta}"))
 
-        specs = self.specs
-        if specs["tao_fd_delta"] is not None:
-            petsc_options.append(("-tao_fd_delta", f"{specs['tao_fd_delta']}"))
-        petsc_iname = [v[0] for v in petsc_options]
-        petsc_value = [v[1] for v in petsc_options]
-        cmd += f" Executioner/tao_solver=taobncg"
-        cmd += " Executioner/petsc_options_iname='" + " ".join(petsc_iname) + "'"
-        cmd += " Executioner/petsc_options_value='" + " ".join(petsc_value) + "'"
-        cmd += " Executioner/petsc_options='-tao_test_gradient_view'"
-        cmd += " Executioner/verbose=true"
+        # Append executioner options
+        executioner_options = [
+            "tao_solver=taobncg",
+            f"petsc_options_iname='" + " ".join([v[0] for v in petsc_options]) + "'",
+            f"petsc_options_value='" + " ".join([v[1] for v in petsc_options]) + "'",
+            "petsc_options='-tao_test_gradient_view'",
+            "verbose=true",
+        ]
+        cmd += " " + " ".join(f"Executioner/{v}" for v in executioner_options)
 
         return cmd
 
