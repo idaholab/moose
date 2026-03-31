066b178939d4e3c10b3e0175e558c10822464d25
Roy Stogner <Roy.Stogner@inl.gov>
2026-03-31 15:51:19 -0500

    Weaken prepare_for_use()/unset_is_prepared() calls
    We can just mark as invalidated the limited things we've invalidated.

--- framework/src/meshgenerators/ProjectSideSetOntoLevelSetGenerator.C
+++ framework/src/meshgenerators/ProjectSideSetOntoLevelSetGenerator.C
@@ -135,7 +135,16 @@ ProjectSideSetOntoLevelSetGenerator::generate()
   }
 
   new_mesh->subdomain_name(projection_block_id) = getParam<SubdomainName>("subdomain_name");
-  new_mesh->unset_is_prepared();
+
+  // We haven't built up any caches, but we are building in
+  // serial so at least we don't have to worry about id count sync or
+  // remote elements, and we don't have any boundary info or orphaned
+  // nodes or outdated point-locator on this new mesh.
+  new_mesh->unset_has_neighbor_ptrs();
+  new_mesh->unset_has_cached_elem_data();
+  new_mesh->unset_has_interior_parent_ptrs();
+  new_mesh->unset_has_reinit_ghosting_functors();
+
   return dynamic_pointer_cast<MeshBase>(new_mesh);
 }
 
