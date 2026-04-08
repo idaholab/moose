179466e0817cb975f39665bce769126374a9a6ad
Peter German <grmnptr@gmail.com>
2026-04-07 21:43:18 -0600

    Adapt pressure flux bc. (#32719)

--- modules/navier_stokes/include/linearfvbcs/LinearFVPressureFluxBC.h
+++ modules/navier_stokes/include/linearfvbcs/LinearFVPressureFluxBC.h
@@ -14,7 +14,8 @@
 /**
  * Class implementing a flux boundary condition for linear finite
  * volume pressure variables used in the pressure corrector equation which is consistent with the
- * H/A flux. This is only applicable for advection-diffusion problems.
+ * H/A flux and a prescribed boundary velocity. This allows the pressure boundary flux to be
+ * consistent with a non-zero boundary mass flux.
  */
 class LinearFVPressureFluxBC : public LinearFVAdvectionDiffusionBC
 {
@@ -42,9 +43,26 @@ public:
   virtual bool includesMaterialPropertyMultiplier() const override { return true; }
 
 protected:
+  /// Compute the required boundary pressure flux contribution
+  Real computeRequiredPressureFlux() const;
+
+  /// Compute the scalar A^{-1} coefficient used in the pressure BC
+  Real computeBoundaryAinv() const;
+
   /// The H/A flux functor for this BC (can be variable, function, etc)
   const Moose::Functor<Real> & _HbyA_flux;
 
   /// The functor for the 1/A tensor serving as a diffusion coefficient
   const Moose::Functor<RealVectorValue> & _Ainv;
+
+  /// Spatial dimension of the mesh
+  const unsigned short _dim;
+
+  /// Velocity functors used to prescribe a boundary mass flux
+  const Moose::Functor<Real> & _u;
+  const Moose::Functor<Real> * const _v;
+  const Moose::Functor<Real> * const _w;
+
+  /// Density functor used with the prescribed boundary velocity
+  const Moose::Functor<Real> & _rho;
 };
