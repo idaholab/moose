54a8f8de0c552f93dec7a637c97d47ac3bcebe34
Alex Lindsay <alexander.lindsay@inl.gov>
2026-04-14 11:20:33 -0700

    Just use name() in MFEMVectorPostprocessor for consistency

--- framework/src/mfem/vectorpostprocessors/MFEMVectorPostprocessor.C
+++ framework/src/mfem/vectorpostprocessors/MFEMVectorPostprocessor.C
@@ -28,7 +28,7 @@ MFEMVectorPostprocessor::MFEMVectorPostprocessor(const InputParameters & paramet
 std::optional<std::string>
 MFEMVectorPostprocessor::suppliedVectorPostprocessorName() const
 {
-  return PPName();
+  return name();
 }
 
 #endif // MOOSE_MFEM_ENABLED
