710c8443967e50d683bc1ca347eda5e86cec5b45
SudiptaBiswas <sudipta.biswas@inl.gov>
2026-04-15 16:46:56 -0700

    Remove duplicate files and update app info #32766

--- modules/phase_field/src/userobjects/GrainTrackerDislocations.C
+++ modules/phase_field/src/userobjects/GrainTrackerDislocations.C
@@ -1,20 +1,15 @@
-/****************************************************************/
-/*                  DO NOT MODIFY THIS HEADER                   */
-/*                           Marmot                             */
-/*                                                              */
-/*            (c) 2017 Battelle Energy Alliance, LLC            */
-/*                     ALL RIGHTS RESERVED                      */
-/*                                                              */
-/*          Prepared by Battelle Energy Alliance, LLC           */
-/*             Under Contract No. DE-AC07-05ID14517             */
-/*             With the U. S. Department of Energy              */
-/*                                                              */
-/*             See COPYRIGHT for full restrictions              */
-/****************************************************************/
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
 
 #include "GrainTrackerDislocations.h"
 
-registerMooseObject("MarmotApp", GrainTrackerDislocations);
+registerMooseObject("PhaseFieldApp", GrainTrackerDislocations);
 
 InputParameters
 GrainTrackerDislocations::validParams()
