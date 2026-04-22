38db9600236a2c1f2a248f7ac56590794e469685
Peter German <grmnptr@gmail.com>
2026-04-22 17:15:45 -0600

    Move action distribution to STM.

---
+++ modules/stochastic_tools/include/libtorch/utils/LibtorchActionDistribution.h
@@ -0,0 +1,171 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifdef MOOSE_LIBTORCH_ENABLED
+
+#pragma once
+
+#include <torch/torch.h>
+
+#include "MooseTypes.h"
+
+#include <string>
+#include <vector>
+
+namespace Moose
+{
+
+/**
+ * Reusable continuous-action distribution interface for actor policies.
+ */
+class LibtorchActionDistribution : public torch::nn::Module
+{
+public:
+  LibtorchActionDistribution(const std::string & name,
+                             unsigned int num_inputs,
+                             unsigned int num_outputs,
+                             torch::DeviceType device_type = torch::kCPU,
+                             torch::ScalarType scalar_type = torch::kDouble,
+                             const std::vector<Real> & output_scaling_factors = {});
+
+  virtual void initialize() = 0;
+
+  virtual void reset(const torch::Tensor & input) = 0;
+
+  virtual torch::Tensor sample() const = 0;
+
+  virtual torch::Tensor deterministicAction() const = 0;
+
+  virtual torch::Tensor logProbability(const torch::Tensor & action) const = 0;
+
+  virtual torch::Tensor entropy() const = 0;
+
+  virtual bool isBounded() const = 0;
+
+  void synchronizeScalingFactorsFromBuffer();
+
+protected:
+  torch::Tensor prepareFeatures(const torch::Tensor & input) const;
+  torch::Tensor prepareAction(const torch::Tensor & action) const;
+  const torch::Tensor & actionScaleTensor() const { return _action_scale_tensor; }
+
+  const std::string _name;
+  const unsigned int _num_inputs;
+  const unsigned int _num_outputs;
+  const torch::DeviceType _device_type;
+  const torch::ScalarType _data_type;
+  std::vector<Real> _output_scaling_factors;
+
+  torch::Tensor _action_scale_tensor;
+};
+
+/**
+ * Gaussian action distribution for unbounded action spaces.
+ */
+class LibtorchGaussianActionDistribution : public LibtorchActionDistribution
+{
+public:
+  LibtorchGaussianActionDistribution(const std::string & name,
+                                     unsigned int num_inputs,
+                                     unsigned int num_outputs,
+                                     torch::DeviceType device_type = torch::kCPU,
+                                     torch::ScalarType scalar_type = torch::kDouble,
+                                     bool build_on_construct = true,
+                                     const std::vector<Real> & output_scaling_factors = {},
+                                     bool state_independent_std = true);
+
+  virtual void initialize() override;
+
+  virtual void reset(const torch::Tensor & input) override;
+
+  virtual torch::Tensor sample() const override;
+
+  virtual torch::Tensor deterministicAction() const override;
+
+  virtual torch::Tensor logProbability(const torch::Tensor & action) const override;
+
+  virtual torch::Tensor entropy() const override;
+
+  virtual bool isBounded() const override { return false; }
+
+  bool stateIndependentStd() const { return _state_independent_std; }
+  torch::nn::Linear & meanModule() { return _mean_module; }
+  const torch::nn::Linear & meanModule() const { return _mean_module; }
+  torch::nn::Linear & stdModule() { return _std_module; }
+  const torch::nn::Linear & stdModule() const { return _std_module; }
+  const torch::Tensor & stdTensor() const { return _std_tensor; }
+
+private:
+  void constructDistribution();
+
+  const bool _state_independent_std;
+  torch::nn::Linear _mean_module{nullptr};
+  torch::nn::Linear _std_module{nullptr};
+  torch::Tensor _mean;
+  torch::Tensor _std_tensor;
+  torch::Tensor _log_std_tensor;
+};
+
+/**
+ * Beta action distribution for bounded action spaces.
+ */
+class LibtorchBetaActionDistribution : public LibtorchActionDistribution
+{
+public:
+  LibtorchBetaActionDistribution(const std::string & name,
+                                 unsigned int num_inputs,
+                                 unsigned int num_outputs,
+                                 const std::vector<Real> & minimum_values,
+                                 const std::vector<Real> & maximum_values,
+                                 torch::DeviceType device_type = torch::kCPU,
+                                 torch::ScalarType scalar_type = torch::kDouble,
+                                 bool build_on_construct = true,
+                                 const std::vector<Real> & output_scaling_factors = {});
+
+  virtual void initialize() override;
+
+  virtual void reset(const torch::Tensor & input) override;
+
+  virtual torch::Tensor sample() const override;
+
+  virtual torch::Tensor deterministicAction() const override;
+
+  virtual torch::Tensor logProbability(const torch::Tensor & action) const override;
+
+  virtual torch::Tensor entropy() const override;
+
+  virtual bool isBounded() const override { return true; }
+
+  torch::nn::Linear & alphaModule() { return _alpha_module; }
+  const torch::nn::Linear & alphaModule() const { return _alpha_module; }
+  torch::nn::Linear & betaModule() { return _beta_module; }
+  const torch::nn::Linear & betaModule() const { return _beta_module; }
+  const torch::Tensor & alphaTensor() const { return _alpha_tensor; }
+  const torch::Tensor & betaTensor() const { return _beta_tensor; }
+
+private:
+  void constructDistribution();
+
+  const std::vector<Real> _minimum_values;
+  const std::vector<Real> _maximum_values;
+
+  torch::nn::Linear _alpha_module{nullptr};
+  torch::nn::Linear _beta_module{nullptr};
+  torch::Tensor _min_tensor;
+  torch::Tensor _max_tensor;
+  torch::Tensor _alpha_tensor;
+  torch::Tensor _beta_tensor;
+  torch::Tensor _alpha_beta_tensor;
+  torch::Tensor _log_norm;
+  torch::Tensor _mean;
+};
+
+} // namespace Moose
+
+#endif
---
+++ modules/stochastic_tools/include/libtorch/utils/LibtorchActorNeuralNet.h
@@ -0,0 +1,132 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifdef MOOSE_LIBTORCH_ENABLED
+
+#pragma once
+
+#include <torch/torch.h>
+#include <torch/script.h>
+#include "LibtorchActionDistribution.h"
+#include "LibtorchArtificialNeuralNet.h"
+
+namespace Moose
+{
+
+class LibtorchActorNeuralNet : public LibtorchArtificialNeuralNet
+{
+public:
+  /**
+   * Construct using input parameters
+   * @param name Name of the neural network
+   * @param num_inputs The number of input neurons/parameters
+   * @param num_neurons_per_layer Number of neurons per hidden layer
+   * @param num_outputs The number of output neurons
+   */
+  LibtorchActorNeuralNet(const std::string name,
+                         const unsigned int num_inputs,
+                         const unsigned int num_outputs,
+                         const std::vector<unsigned int> & num_neurons_per_layer,
+                         const std::vector<std::string> & activation_function = {"relu"},
+                         const std::vector<Real> & minimum_values = {},
+                         const std::vector<Real> & maximum_values = {},
+                         const torch::DeviceType device_type = torch::kCPU,
+                         const torch::ScalarType scalar_type = torch::kDouble,
+                         const bool build_on_construct = true,
+                         const std::vector<Real> & input_shift_factors = {},
+                         const std::vector<Real> & input_scaling_factors = {},
+                         const std::vector<Real> & output_scaling_factors = {},
+                         const bool state_independent_std = true);
+
+  /**
+   * Copy construct an artificial neural network
+   * @param nn The neural network which needs to be copied
+   */
+  LibtorchActorNeuralNet(const Moose::LibtorchActorNeuralNet & nn,
+                         const bool build_on_construct = true);
+
+  /**
+   * Overriding the forward substitution function for the neural network, unfortunately
+   * this cannot be const since it creates a graph in the background
+   * @param x Input tensor for the evaluation
+   */
+  virtual torch::Tensor forward(const torch::Tensor & x) override;
+
+  virtual torch::Tensor evaluate(torch::Tensor & input, bool sampled);
+
+  virtual torch::Tensor sample();
+
+  /// Construct the neural network
+  virtual void constructNeuralNetwork() override;
+
+  const LibtorchActionDistribution & actionDistribution() const { return *_action_distribution; }
+  LibtorchActionDistribution & actionDistribution() { return *_action_distribution; }
+
+  const LibtorchGaussianActionDistribution * gaussianActionDistributionPtr() const;
+  LibtorchGaussianActionDistribution * gaussianActionDistributionPtr();
+  const LibtorchGaussianActionDistribution & gaussianActionDistribution() const;
+  LibtorchGaussianActionDistribution & gaussianActionDistribution();
+
+  const LibtorchBetaActionDistribution * betaActionDistributionPtr() const;
+  LibtorchBetaActionDistribution * betaActionDistributionPtr();
+  const LibtorchBetaActionDistribution & betaActionDistribution() const;
+  LibtorchBetaActionDistribution & betaActionDistribution();
+
+  bool stateIndependentStd() const { return _state_independent_std; }
+
+  void resetDistributionParams(torch::Tensor input);
+
+  torch::Tensor logProbability(const torch::Tensor & other);
+
+  torch::Tensor entropy();
+
+  virtual void initializeNeuralNetwork() override;
+
+protected:
+  const bool _state_independent_std;
+  std::shared_ptr<LibtorchActionDistribution> _action_distribution;
+};
+
+void to_json(nlohmann::json & json, const Moose::LibtorchActorNeuralNet * const & network);
+
+void loadLibtorchActorNeuralNetState(Moose::LibtorchActorNeuralNet & nn,
+                                     const std::string & filename);
+
+bool isLegacyLibtorchActorArchive(const std::string & filename);
+
+void loadLegacyLibtorchActorNeuralNetState(Moose::LibtorchActorNeuralNet & nn,
+                                           const std::string & filename,
+                                           const std::vector<Real> & action_standard_deviations);
+
+}
+
+template <>
+void dataStore<Moose::LibtorchActorNeuralNet>(std::ostream & stream,
+                                              std::shared_ptr<Moose::LibtorchActorNeuralNet> & nn,
+                                              void * context);
+
+template <>
+void dataLoad<Moose::LibtorchActorNeuralNet>(std::istream & stream,
+                                             std::shared_ptr<Moose::LibtorchActorNeuralNet> & nn,
+                                             void * context);
+
+// This is needed because the reporter which is used to ouput the neural net parameters to JSON
+// requires a dataStore/dataLoad. However, these functions will be empty due to the fact that
+// we are only interested in the JSON output and we don't want to output everything
+template <>
+void dataStore<Moose::LibtorchActorNeuralNet const>(std::ostream & stream,
+                                                    Moose::LibtorchActorNeuralNet const *& nn,
+                                                    void * context);
+
+template <>
+void dataLoad<Moose::LibtorchActorNeuralNet const>(std::istream & stream,
+                                                   Moose::LibtorchActorNeuralNet const *& nn,
+                                                   void * context);
+
+#endif
