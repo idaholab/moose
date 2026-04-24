cf8369d69613373a7fd3d2b0f71efecd16235220
Namjae Choi <Namjae.Choi@inl.gov>
2026-04-23 20:21:41 -0600

    Add parsed object base and parsed material and aux #30655

---
+++ framework/include/kokkos/base/KokkosParsedObjectBase.h
@@ -0,0 +1,127 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "KokkosFunctionParser.h"
+
+#include "MooseObject.h"
+
+namespace Moose::Kokkos
+{
+
+class ParsedObjectBase
+{
+public:
+  static InputParameters validParams();
+
+  ParsedObjectBase(const MooseObject * object);
+
+protected:
+  /**
+   * Check if duplicate symbols were added
+   * @param symbols The list of symbols
+   * @param param The parameter name containing symbols
+   */
+  template <typename T>
+  void checkDuplicateSymbols(const std::vector<T> symbols, const std::string param);
+
+  /**
+   * Add a constant
+   * @param name The variable name
+   * @param constant The constant value
+   */
+  void addConstant(const std::string & name, const Real constant);
+  /**
+   * Add a scalar variable
+   * @param name The variable name
+   * @param scalar The pointer to the scalar variable
+   */
+  void addScalar(const std::string & name, const Real * scalar);
+  /**
+   * Add a field variable
+   * @param name The variable name
+   * @param field The coupled field variable
+   */
+  void addField(const std::string & name, const VariableValue & field);
+  /**
+   * Add a material property
+   * @param name The variable name
+   * @param property The material property
+   */
+  void addProperty(const std::string & name, const MaterialProperty<Real> & property);
+  /**
+   * Add a material property
+   * @param name The variable name
+   * @param function The function
+   */
+  void addFunction(const std::string & name, const Function & function);
+  /**
+   * Finalize parsed function
+   */
+  void finalize();
+
+  /**
+   * Parsed expression
+   */
+  const std::string & _expression;
+  /**
+   * Parsed function builder
+   */
+  std::shared_ptr<RPNBuilder> _builder;
+  /**
+   * Parsed function evaluator
+   */
+  RPNEvaluator _evaluator;
+  /**
+   * Constants used in the parsed expression
+   */
+  std::map<std::string, Real> _constants;
+  /**
+   * Scalar variables used in the parsed expression
+   */
+  std::map<std::string, const Real *> _scalars;
+  /**
+   * Field variables used in the parsed expression
+   */
+  std::map<std::string, VariableValue> _fields;
+  /**
+   * Material properties used in the parsed expression
+   */
+  std::map<std::string, MaterialProperty<Real>> _properties;
+  /**
+   * Functions used in the parsed expression
+   */
+  std::map<std::string, Function> _functions;
+
+private:
+  /**
+   * Parsed object
+   */
+  const MooseObject * _parsed_object;
+  /**
+   * All symbols added to the parsed function
+   */
+  std::unordered_set<std::string> _all_symbols;
+};
+
+template <typename T>
+void
+ParsedObjectBase::checkDuplicateSymbols(const std::vector<T> symbols, const std::string param)
+{
+  for (const auto & symbol : symbols)
+  {
+    if (_all_symbols.count(symbol))
+      _parsed_object->paramError(param, "Symbol '", symbol, "' was added multiple times.");
+
+    _all_symbols.insert(symbol);
+  }
+}
+
+} // namespace Moose::Kokkos
