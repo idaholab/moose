bb22837d2f68797df8e26dcc2767063affd5f912
SohailSTFC <133211007+SohailSTFC@users.noreply.github.com>
2026-04-25 22:20:59 +0100

    Create MFEMNLCurlCurlKernel.h
    Adding the Non-linear Curl-Curl kernel

---
+++ framework/include/mfem/kernels/MFEMNLCurlCurlKernel.h
@@ -0,0 +1,35 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifdef MOOSE_MFEM_ENABLED
+
+#pragma once
+#include "MFEMKernel.h"
+
+/**
+ * \f[
+ * (k(\nabla x \vec u) \vec \nabla x \vec u, \vec \nabla x \vec v)
+ * \f]
+ */
+class MFEMNLCurlCurlKernel : public MFEMKernel
+{
+public:
+  static InputParameters validParams();
+
+  MFEMNLCurlCurlKernel(const InputParameters & parameters);
+
+  virtual mfem::NonlinearFormIntegrator * createNLIntegrator() override;
+
+protected:
+  mfem::Coefficient & _k_coef;
+  mfem::VectorCoefficient & _dk_dcu_coeff;
+  mfem::ParGridFunction & _trial_var;
+};
+
+#endif
