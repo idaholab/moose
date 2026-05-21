f02ac81c5f50a4cc5174533f1d482caca01d7d89
Giannis95 <ic@192.168.1.115>
2026-05-21 12:03:51 +0100

    Rebased to the latest and added class and pointer descriptions. Refs ##32375

--- framework/include/mfem/coordinates/MFEMCoordinateCoefficients.h
+++ framework/include/mfem/coordinates/MFEMCoordinateCoefficients.h
@@ -16,7 +16,10 @@
 #include "mfem.hpp"
 
 #include <string>
-
+/**
+ * Abstract base class for all coordinate system coefficient providers.
+ * Implemented as a MOOSE GeneralUserObject.
+ */
 class MFEMCoordinateCoefficients : public GeneralUserObject
 {
 public:
@@ -24,21 +27,22 @@ public:
 
   MFEMCoordinateCoefficients(const InputParameters & parameters);
   virtual ~MFEMCoordinateCoefficients() = default;
-
+  ///GeneralUserObject interface
   virtual void initialize() override {}
   virtual void execute() override {}
   virtual void finalize() override {}
-
+  // Virtual methods to be implemented by the derived classes
   virtual void build() = 0;
-
+  /// Get a pointer to a built-in coefficient by name (makes built-in coefficients accessible to kernels and BCs through the '[Coordinates]' block)
   virtual const mfem::Coefficient * getBuiltinCoefficient(const std::string & name) const = 0;
-
+  ///Pointer getters for coefficients in coordinate systems with radial dependence
   virtual const mfem::Coefficient * getRadialCoefficient() const = 0;
   virtual const mfem::Coefficient * getInverseRadialCoefficient() const = 0;
   virtual const mfem::Coefficient * getTwoPiRCoefficient() const = 0;
   virtual const mfem::Coefficient * getMeasureWeightCoefficient() const = 0;
 
 protected:
+  /// Optional parameters for coordinate coefficients with radial dependence
   const Real _inv_r_eps;
 };
 
