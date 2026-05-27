1e346cc315835fc6d89ea5b20b9d7a105072c38b
bozo-vazic <bozo.vazic@inl.gov>
2026-05-27 07:52:05 -0600

    Improve early MultiApp getter diagnostics (#28131)

--- test/src/problems/GetterTooEarlyProblem.C
+++ test/src/problems/GetterTooEarlyProblem.C
@@ -12,9 +12,10 @@ GetterTooEarlyProblem::validParams()
 {
   InputParameters params = FEProblem::validParams();
   params.addRequiredParam<MooseEnum>(
-      "getter", MooseEnum("function user_object"), "The getter to call too early");
+      "getter", MooseEnum("function user_object multi_app"), "The getter to call too early");
   params.addParam<FunctionName>("function", "Function to request too early");
   params.addParam<UserObjectName>("user_object", "UserObject to request too early");
+  params.addParam<MultiAppName>("multi_app", "MultiApp to request too early");
   return params;
 }
 
@@ -27,4 +28,6 @@ GetterTooEarlyProblem::GetterTooEarlyProblem(const InputParameters & params)
     _function = &getFunction(getParam<FunctionName>("function"));
   else if (getter == "user_object")
     _user_object = &getUserObjectBase(getParam<UserObjectName>("user_object"));
+  else if (getter == "multi_app")
+    static_cast<void>(getMultiApp(getParam<MultiAppName>("multi_app")));
 }
