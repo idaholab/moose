62cb71ef112c31c2ffb8f9702240fd12d1fa0453
Chris Green <chris.green@csiro.au>
2026-06-18 10:33:05 +1000

    Make dispersive flux kernel AD capable
    Refs #33122

--- modules/porous_flow/include/kernels/PorousFlowDispersiveFlux.h
+++ modules/porous_flow/include/kernels/PorousFlowDispersiveFlux.h
@@ -9,66 +9,66 @@
 
 #pragma once
 
-#include "Kernel.h"
+#include "GenericKernel.h"
 #include "PorousFlowDictator.h"
 #include "RankTwoTensor.h"
 
 /**
  * Dispersive flux of component k in fluid phase alpha. Includes the effects
  * of both molecular diffusion and hydrodynamic dispersion.
+ *
+ * Templated on is_ad: the false instantiation uses hand-coded Jacobians;
+ * the true instantiation propagates derivatives through AD material properties.
  */
-class PorousFlowDispersiveFlux : public Kernel
+template <bool is_ad>
+class PorousFlowDispersiveFluxTempl : public GenericKernel<is_ad>
 {
 public:
   static InputParameters validParams();
 
-  PorousFlowDispersiveFlux(const InputParameters & parameters);
+  PorousFlowDispersiveFluxTempl(const InputParameters & parameters);
 
 protected:
-  virtual Real computeQpResidual() override;
-
+  virtual GenericReal<is_ad> computeQpResidual() override;
   virtual Real computeQpJacobian() override;
-
   virtual Real computeQpOffDiagJacobian(unsigned int jvar) override;
 
   /**
-   * Derivative of the residual with respect to the PorousFLow Variable
-   * with variable number jvar.
-   * This is used by both computeQpJacobian and computeQpOffDiagJacobian
-   * @param jvar take the derivative wrt this variable number
-   * @return dResidual_dVar
+   * Derivative of the residual with respect to the PorousFlow variable
+   * with variable number jvar. Only used for the non-AD path.
    */
   Real computeQpJac(unsigned int jvar) const;
 
   /// Fluid density for each phase (at the qp)
-  const MaterialProperty<std::vector<Real>> & _fluid_density_qp;
+  const GenericMaterialProperty<std::vector<Real>, is_ad> & _fluid_density_qp;
 
-  /// Derivative of the fluid density for each phase wrt PorousFlow variables (at the qp)
-  const MaterialProperty<std::vector<std::vector<Real>>> & _dfluid_density_qp_dvar;
+  /// Derivative of the fluid density for each phase wrt PorousFlow variables -- null for AD
+  const MaterialProperty<std::vector<std::vector<Real>>> * const _dfluid_density_qp_dvar;
 
   /// Gradient of mass fraction of each component in each phase
-  const MaterialProperty<std::vector<std::vector<RealGradient>>> & _grad_mass_frac;
+  const GenericMaterialProperty<std::vector<std::vector<RealGradient>>, is_ad> & _grad_mass_frac;
 
-  /// Derivative of mass fraction wrt PorousFlow variables
-  const MaterialProperty<std::vector<std::vector<std::vector<Real>>>> & _dmass_frac_dvar;
+  /// Derivative of mass fraction wrt PorousFlow variables -- null for AD
+  const MaterialProperty<std::vector<std::vector<std::vector<Real>>>> * const _dmass_frac_dvar;
 
   /// Porosity at the qps
-  const MaterialProperty<Real> & _porosity_qp;
+  const GenericMaterialProperty<Real, is_ad> & _porosity_qp;
 
-  /// Derivative of porosity wrt PorousFlow variables (at the qps)
-  const MaterialProperty<std::vector<Real>> & _dporosity_qp_dvar;
+  /// Derivative of porosity wrt PorousFlow variables -- null for AD
+  const MaterialProperty<std::vector<Real>> * const _dporosity_qp_dvar;
 
   /// Tortuosity tau_0 * tau_{alpha} for fluid phase alpha
-  const MaterialProperty<std::vector<Real>> & _tortuosity;
+  const GenericMaterialProperty<std::vector<Real>, is_ad> & _tortuosity;
 
-  /// Derivative of tortuosity wrt PorousFlow variables
-  const MaterialProperty<std::vector<std::vector<Real>>> & _dtortuosity_dvar;
+  /// Derivative of tortuosity wrt PorousFlow variables -- null for AD
+  const MaterialProperty<std::vector<std::vector<Real>>> * const _dtortuosity_dvar;
 
   /// Diffusion coefficients of component k in fluid phase alpha
   const MaterialProperty<std::vector<std::vector<Real>>> & _diffusion_coeff;
 
-  /// Derivative of the diffusion coefficients wrt PorousFlow variables
-  const MaterialProperty<std::vector<std::vector<std::vector<Real>>>> & _ddiffusion_coeff_dvar;
+  /// Derivative of the diffusion coefficients wrt PorousFlow variables -- null for AD
+  const MaterialProperty<std::vector<std::vector<std::vector<Real>>>> * const
+      _ddiffusion_coeff_dvar;
 
   /// PorousFlowDictator UserObject
   const PorousFlowDictator & _dictator;
@@ -79,38 +79,38 @@ protected:
   /// The number of fluid phases
   const unsigned int _num_phases;
 
-  /// Identity tensor
-  const RankTwoTensor _identity_tensor;
+  /// Identity tensor (generic type to support AD arithmetic)
+  const GenericRankTwoTensor<is_ad> _identity_tensor;
 
   /// Relative permeability of each phase
-  const MaterialProperty<std::vector<Real>> & _relative_permeability;
+  const GenericMaterialProperty<std::vector<Real>, is_ad> & _relative_permeability;
 
-  /// Derivative of relative permeability wrt PorousFlow variables
-  const MaterialProperty<std::vector<std::vector<Real>>> & _drelative_permeability_dvar;
+  /// Derivative of relative permeability wrt PorousFlow variables -- null for AD
+  const MaterialProperty<std::vector<std::vector<Real>>> * const _drelative_permeability_dvar;
 
   /// Viscosity of each component in each phase
-  const MaterialProperty<std::vector<Real>> & _fluid_viscosity;
+  const GenericMaterialProperty<std::vector<Real>, is_ad> & _fluid_viscosity;
 
-  /// Derivative of viscosity wrt PorousFlow variables
-  const MaterialProperty<std::vector<std::vector<Real>>> & _dfluid_viscosity_dvar;
+  /// Derivative of viscosity wrt PorousFlow variables -- null for AD
+  const MaterialProperty<std::vector<std::vector<Real>>> * const _dfluid_viscosity_dvar;
 
   /// Permeability of porous material
-  const MaterialProperty<RealTensorValue> & _permeability;
+  const GenericMaterialProperty<RealTensorValue, is_ad> & _permeability;
 
-  /// Derivative of permeability wrt PorousFlow variables
-  const MaterialProperty<std::vector<RealTensorValue>> & _dpermeability_dvar;
+  /// Derivative of permeability wrt PorousFlow variables -- null for AD
+  const MaterialProperty<std::vector<RealTensorValue>> * const _dpermeability_dvar;
 
-  /// d(permeabiity)/d(grad(PorousFlow variable))
-  const MaterialProperty<std::vector<std::vector<RealTensorValue>>> & _dpermeability_dgradvar;
+  /// d(permeability)/d(grad(PorousFlow variable)) -- null for AD
+  const MaterialProperty<std::vector<std::vector<RealTensorValue>>> * const _dpermeability_dgradvar;
 
   /// Gradient of the pore pressure in each phase
-  const MaterialProperty<std::vector<RealGradient>> & _grad_p;
+  const GenericMaterialProperty<std::vector<RealGradient>, is_ad> & _grad_p;
 
-  /// Derivative of Grad porepressure in each phase wrt grad(PorousFlow variables)
-  const MaterialProperty<std::vector<std::vector<Real>>> & _dgrad_p_dgrad_var;
+  /// Derivative of Grad porepressure in each phase wrt grad(PorousFlow variables) -- null for AD
+  const MaterialProperty<std::vector<std::vector<Real>>> * const _dgrad_p_dgrad_var;
 
-  /// Derivative of Grad porepressure in each phase wrt PorousFlow variables
-  const MaterialProperty<std::vector<std::vector<RealGradient>>> & _dgrad_p_dvar;
+  /// Derivative of Grad porepressure in each phase wrt PorousFlow variables -- null for AD
+  const MaterialProperty<std::vector<std::vector<RealGradient>>> * const _dgrad_p_dvar;
 
   /// Gravitational acceleration
   const RealVectorValue _gravity;
@@ -121,6 +121,12 @@ protected:
   /// Transverse dispersivity for each phase
   const std::vector<Real> _disp_trans;
 
-  /// Flag to check whether permeabiity derivatives are non-zero
+  /// Flag to check whether permeability derivatives are non-zero
   const bool _perm_derivs;
+
+  usingGenericKernelMembers;
+  using GenericKernel<is_ad>::_grad_phi;
 };
+
+typedef PorousFlowDispersiveFluxTempl<false> PorousFlowDispersiveFlux;
+typedef PorousFlowDispersiveFluxTempl<true> ADPorousFlowDispersiveFlux;
