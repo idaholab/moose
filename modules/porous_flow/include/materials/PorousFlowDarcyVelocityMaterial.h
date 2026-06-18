c9e70135107d659e2de6afe8fdb27e03e4604701
Chris Green <chris.green@csiro.au>
2026-06-18 10:29:24 +1000

    Make PorousFlowBasicAdvection AD capable
    Required templating the Darcy velocity material that this simple
    kernel uses.
    
    Refs #33122

--- modules/porous_flow/include/materials/PorousFlowDarcyVelocityMaterial.h
+++ modules/porous_flow/include/materials/PorousFlowDarcyVelocityMaterial.h
@@ -13,13 +13,17 @@
 
 /**
  * Material to calculate the Darcy velocity for all phases
+ *
+ * Templated on is_ad: the false instantiation also computes the hand-coded derivative
+ * properties; the true instantiation carries derivatives through the AD Darcy velocity.
  */
-class PorousFlowDarcyVelocityMaterial : public PorousFlowMaterial
+template <bool is_ad>
+class PorousFlowDarcyVelocityMaterialTempl : public PorousFlowMaterial
 {
 public:
   static InputParameters validParams();
 
-  PorousFlowDarcyVelocityMaterial(const InputParameters & parameters);
+  PorousFlowDarcyVelocityMaterialTempl(const InputParameters & parameters);
 
 protected:
   virtual void computeQpProperties() override;
@@ -31,40 +35,40 @@ protected:
   const unsigned int _num_var;
 
   /// Permeability of porous material
-  const MaterialProperty<RealTensorValue> & _permeability;
+  const GenericMaterialProperty<RealTensorValue, is_ad> & _permeability;
 
-  /// d(permeabiity)/d(PorousFlow variable)
-  const MaterialProperty<std::vector<RealTensorValue>> & _dpermeability_dvar;
+  /// d(permeabiity)/d(PorousFlow variable) -- null for AD path
+  const MaterialProperty<std::vector<RealTensorValue>> * const _dpermeability_dvar;
 
-  /// d(permeabiity)/d(grad(PorousFlow variable))
-  const MaterialProperty<std::vector<std::vector<RealTensorValue>>> & _dpermeability_dgradvar;
+  /// d(permeabiity)/d(grad(PorousFlow variable)) -- null for AD path
+  const MaterialProperty<std::vector<std::vector<RealTensorValue>>> * const _dpermeability_dgradvar;
 
   /// Fluid density for each phase
-  const MaterialProperty<std::vector<Real>> & _fluid_density;
+  const GenericMaterialProperty<std::vector<Real>, is_ad> & _fluid_density;
 
-  /// Derivative of the fluid density for each phase wrt PorousFlow variables
-  const MaterialProperty<std::vector<std::vector<Real>>> & _dfluid_density_dvar;
+  /// Derivative of the fluid density for each phase wrt PorousFlow variables -- null for AD path
+  const MaterialProperty<std::vector<std::vector<Real>>> * const _dfluid_density_dvar;
 
   /// Viscosity of each component in each phase
-  const MaterialProperty<std::vector<Real>> & _fluid_viscosity;
+  const GenericMaterialProperty<std::vector<Real>, is_ad> & _fluid_viscosity;
 
-  /// Derivative of the fluid viscosity for each phase wrt PorousFlow variables
-  const MaterialProperty<std::vector<std::vector<Real>>> & _dfluid_viscosity_dvar;
+  /// Derivative of the fluid viscosity for each phase wrt PorousFlow variables -- null for AD path
+  const MaterialProperty<std::vector<std::vector<Real>>> * const _dfluid_viscosity_dvar;
 
   /// Relative permeability of each phase
-  const MaterialProperty<std::vector<Real>> & _relative_permeability;
+  const GenericMaterialProperty<std::vector<Real>, is_ad> & _relative_permeability;
 
-  /// Derivative of relative permeability of each phase wrt PorousFlow variables
-  const MaterialProperty<std::vector<std::vector<Real>>> & _drelative_permeability_dvar;
+  /// Derivative of relative permeability of each phase wrt PorousFlow variables -- null for AD path
+  const MaterialProperty<std::vector<std::vector<Real>>> * const _drelative_permeability_dvar;
 
   /// Gradient of the pore pressure in each phase
-  const MaterialProperty<std::vector<RealGradient>> & _grad_p;
+  const GenericMaterialProperty<std::vector<RealGradient>, is_ad> & _grad_p;
 
-  /// Derivative of Grad porepressure in each phase wrt grad(PorousFlow variables)
-  const MaterialProperty<std::vector<std::vector<Real>>> & _dgrad_p_dgradvar;
+  /// Derivative of Grad porepressure in each phase wrt grad(PorousFlow variables) -- null for AD path
+  const MaterialProperty<std::vector<std::vector<Real>>> * const _dgrad_p_dgradvar;
 
-  /// Derivative of Grad porepressure in each phase wrt PorousFlow variables
-  const MaterialProperty<std::vector<std::vector<RealGradient>>> & _dgrad_p_dvar;
+  /// Derivative of Grad porepressure in each phase wrt PorousFlow variables -- null for AD path
+  const MaterialProperty<std::vector<std::vector<RealGradient>>> * const _dgrad_p_dvar;
 
   /// Gravity
   const RealVectorValue _gravity;
@@ -73,13 +77,14 @@ protected:
    * Computed Darcy velocity of each phase.
    * _darcy_velocity[_qp][ph](i) = i^th component of velocity of phase ph at quadpoint _qp
    */
-  MaterialProperty<std::vector<RealVectorValue>> & _darcy_velocity;
+  GenericMaterialProperty<std::vector<RealVectorValue>, is_ad> & _darcy_velocity;
 
   /**
    * _ddarcy_velocity_dvar[_qp][ph][v](i) =
    * d(i_th component of velocity of phase ph at quadpoint _qp)/d(PorousFlow variable v)
+   * Null for the AD path.
    */
-  MaterialProperty<std::vector<std::vector<RealVectorValue>>> & _ddarcy_velocity_dvar;
+  MaterialProperty<std::vector<std::vector<RealVectorValue>>> * const _ddarcy_velocity_dvar;
 
   /**
    * _ddarcy_velocity_dgradvar[_qp][ph][j][v](i)
@@ -87,7 +92,11 @@ protected:
    *          d(j_th component of grad(PorousFlow variable v))
    * In an advection Kernel, grad_test * _darcy_velocity[ph] * u, the contribution to the Jacobian
    * is grad_test * (_ddarcy_velocity_dgradvar[_qp][ph][j][v] * _grad_phi[_j][_qp](j)) * u
+   * Null for the AD path.
    */
-  MaterialProperty<std::vector<std::vector<std::vector<RealVectorValue>>>> &
+  MaterialProperty<std::vector<std::vector<std::vector<RealVectorValue>>>> * const
       _ddarcy_velocity_dgradvar;
 };
+
+typedef PorousFlowDarcyVelocityMaterialTempl<false> PorousFlowDarcyVelocityMaterial;
+typedef PorousFlowDarcyVelocityMaterialTempl<true> ADPorousFlowDarcyVelocityMaterial;
