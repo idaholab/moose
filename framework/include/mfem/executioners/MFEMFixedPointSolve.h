3bbe941a347ed7fe52880a17a2e0668e7a3f841a
Alexander Blair <alexander.blair@ukaea.uk>
2026-06-26 18:03:13 +0100

    Remove MFEMFixedPointSolve.C since currently no overridden methods have implementations

--- framework/include/mfem/executioners/MFEMFixedPointSolve.h
+++ framework/include/mfem/executioners/MFEMFixedPointSolve.h
@@ -17,11 +17,11 @@
 class MFEMFixedPointSolve : public FixedPointSolve
 {
 public:
-  MFEMFixedPointSolve(Executioner & ex);
+  MFEMFixedPointSolve(Executioner & ex) : FixedPointSolve(ex){};
 
   virtual ~MFEMFixedPointSolve() = default;
 
-  virtual void initialSetup() override;
+  virtual void initialSetup() override{};
 
   /**
    * Allocate storage for the fixed point algorithm.
@@ -31,18 +31,18 @@ public:
    * @param primary Whether this routine is to allocate storage for the primary transformed
    *                quantities (as main app) or the secondary ones (as a subapp)
    */
-  virtual void allocateStorage(const bool primary) override{};
+  virtual void allocateStorage(const bool /*primary*/) override{};
 
   /// Print the convergence history of the coupling, at every fixed point iteration
-  virtual void
-  printFixedPointConvergenceHistory(Real initial_norm,
-                                    const std::vector<Real> & timestep_begin_norms,
-                                    const std::vector<Real> & timestep_end_norms) const override {};
+  virtual void printFixedPointConvergenceHistory(
+      Real /*initial_norm*/,
+      const std::vector<Real> & /*timestep_begin_norms*/,
+      const std::vector<Real> & /*timestep_end_norms*/) const override{};
 
 protected:
   virtual void
-  updateVariableDoFsForTransform(const std::vector<std::string> & transformed_var_names,
-                                 const bool primary) override;
+  updateVariableDoFsForTransform(const std::vector<std::string> & /*transformed_var_names*/,
+                                 const bool /*primary*/) override{};
 
   /**
    * Saves the current values of the variables, and update the old(er) vectors.
@@ -50,7 +50,7 @@ protected:
    * @param primary Whether this routine is to save the variables for the primary transformed
    *                quantities (as main app) or the secondary ones (as a subapp)
    */
-  virtual void saveVariableValues(const bool primary) override;
+  virtual void saveVariableValues(const bool /*primary*/) override{};
 
   /**
    * Saves the current values of the postprocessors, and update the old(er) vectors.
@@ -58,10 +58,10 @@ protected:
    * @param primary Whether this routine is to save the variables for the primary transformed
    *                quantities (as main app) or the secondary ones (as a subapp)
    */
-  virtual void savePostprocessorValues(const bool primary) override;
+  virtual void savePostprocessorValues(const bool /*primary*/) override{};
 
   /// Save both the variable and postprocessor values
-  virtual void saveAllValues(const bool primary) override;
+  virtual void saveAllValues(const bool /*primary*/) override{};
 
   /**
    * Use the fixed point algorithm transform instead of simply using the Picard update
@@ -71,9 +71,12 @@ protected:
    * @param primary Whether this routine is used for the primary transformed
    *                quantities (as main app) or the secondary ones (as a subapp)
    */
-  virtual bool useFixedPointAlgorithmUpdateInsteadOfPicard(const bool primary) override {return true;}
+  virtual bool useFixedPointAlgorithmUpdateInsteadOfPicard(const bool /*primary*/) override
+  {
+    return false;
+  }
 
-  virtual void copyPreviousFixedPointSolutions() override;
+  virtual void copyPreviousFixedPointSolutions() override{};
 
   /**
    * Use the fixed point algorithm to transform the postprocessors.
@@ -83,7 +86,7 @@ protected:
    * @param primary Whether this routine is to save the variables for the primary transformed
    *                quantities (as main app) or the secondary ones (as a subapp)
    */
-  virtual void transformPostprocessors(const bool primary) override;
+  virtual void transformPostprocessors(const bool /*primary*/) override{};
 
   /**
    * Use the fixed point algorithm to transform the variables.
@@ -93,5 +96,5 @@ protected:
    * @param primary Whether this routine is to save the variables for the primary transformed
    *                quantities (as main app) or the secondary ones (as a subapp)
    */
-  virtual void transformVariables(const bool primary) override;
+  virtual void transformVariables(const bool /*primary*/) override{};
 };
