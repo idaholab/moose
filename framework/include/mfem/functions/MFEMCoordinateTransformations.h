187f585e329d800adc493ff7d1b413ed5e75f052
Giannis95 <ic@Ioanniss-MacBook-Pro-2.local>
2026-06-29 18:42:51 +0100

    Migrate ccoordinate coefficients into Functions block. Delete coordinate action and update documentation and test. Refs ##32375

---
+++ framework/include/mfem/functions/MFEMCoordinateTransformations.h
@@ -0,0 +1,27 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+#include "Function.h"
+
+class MFEMCoordinateTransformations : public Function
+{
+public:
+  static InputParameters validParams();
+  MFEMCoordinateTransformations(const InputParameters & parameters);
+  virtual Real value(Real /* t */, const Point & /* p */) const override { return 0.0; }
+
+  const MooseEnum & coordType() const { return _coord_type; }
+  Real invREps() const { return _inv_r_eps; }
+  std::string coefficientName(const std::string & base) const { return name() + "_" + base; }
+
+protected:
+  const MooseEnum _coord_type;
+  const Real _inv_r_eps;
+};
