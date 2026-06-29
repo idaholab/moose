56ce302fc6d0aeea61892439dc6031c7841e431a
Giannis95 <ic@Ioanniss-MacBook-Pro-2.local>
2026-06-29 22:12:27 +0100

    fix Function overload hiding with using Function::value . Refs #32375

--- framework/include/mfem/functions/MFEMCoordinateTransformations.h
+++ framework/include/mfem/functions/MFEMCoordinateTransformations.h
@@ -15,6 +15,7 @@ class MFEMCoordinateTransformations : public Function
 public:
   static InputParameters validParams();
   MFEMCoordinateTransformations(const InputParameters & parameters);
+  using Function::value;
   virtual Real value(Real /* t */, const Point & /* p */) const override { return 0.0; }
 
   const MooseEnum & coordType() const { return _coord_type; }
