638774ebec86fb5d2102633f5b5085e3d48cd67d
hansje <joshua.hansel@inl.gov>
2026-07-08 09:28:45 -0500

    Set nonlinear solve parameters in nonlinear system
    Closes #33271

--- framework/include/convergence/DefaultNonlinearConvergence.h
+++ framework/include/convergence/DefaultNonlinearConvergence.h
@@ -26,11 +26,15 @@ public:
 
   DefaultNonlinearConvergence(const InputParameters & parameters);
 
+  virtual void initialSetup() override;
   virtual void checkIterationType(IterationType it_type) const override;
-
+  virtual void preSolve() override;
   virtual MooseConvergenceStatus checkConvergence(unsigned int n_iter) override;
 
 protected:
+  /// Sets the nonlinear system parameters, such as tolerances
+  void setNonlinearSystemParameters();
+
   /**
    * Nonlinear system whose convergence state should be checked.
    */
