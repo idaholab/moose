00ac8f4c1ea047cb0ddd31f6aad3d0e26108a32e
SohailRUPERT <sohail.rathore@stfc.ac.uk>
2026-07-13 23:58:55 +0100

    clang formatting

--- framework/include/mfem/problem_operator_builders/ProblemOperatorBuilderBase.h
+++ framework/include/mfem/problem_operator_builders/ProblemOperatorBuilderBase.h
@@ -29,8 +29,7 @@ public:
   virtual ~ProblemOperatorBuilderBase() = default;
 
   /// Returns a pointer to the operator's equation system.
-  virtual std::shared_ptr<ProblemOperatorBase>
-  createProblemOperator(MFEMProblem &) = 0;
+  virtual std::shared_ptr<ProblemOperatorBase> createProblemOperator(MFEMProblem &) = 0;
 };
 }
 
