c0d6069a3a581d5eac9b0139a9c255732210e8d1
Logan Harbour <loganhharbour@gmail.com>
2026-07-15 17:23:20 -0600

    Add comments to utils

--- apptainer/files/mpi/.singularity.d/build_utils.sh
+++ apptainer/files/mpi/.singularity.d/build_utils.sh
@@ -5,36 +5,44 @@ if ! which grep; then
     exit 1
 fi
 
+# Whether or not the operating system is rocky-based,
 is_rocky() {
     if ! grep -q "NAME=\"Rocky" /etc/os-release; then
         return 1
     fi
 }
 
+# Whether or not the operating system is rocky 8,
 is_rocky8() {
     if is_rocky && ! grep -q 'VERSION=\"8.' /etc/os-release; then
         return 1
     fi
 }
 
+# Whether or not the operating system is rocky 9,
 is_rocky9() {
     if is_rocky && ! grep -q 'VERSION=\"9.' /etc/os-release; then
         return 1
     fi
 }
 
+# Whether or not the operating system is ubuntu,
 is_ubuntu() {
     if ! grep -q "NAME=\"Ubuntu" /etc/os-release; then
         return 1
     fi
 }
 
+# Update the system packages. Useful at the beginning of
+# a build, in which ubuntu requires an update before installing,
 update_system_packages() {
     if is_ubuntu; then
         apt-get update
     fi
 }
 
+# Cleanup the system packages. Useful at the end of a build,
+# in which the downloaded package cache should be cleaned up.
 clean_system_packages() {
     if is_rocky; then
         dnf clean all
@@ -45,6 +53,9 @@ clean_system_packages() {
     fi
 }
 
+# Determine the default value for -march for a build. Needed
+# because older compilers (GCC 9, our minimum) do not support
+# things like "x86-64-v3".
 default_march() {
     if ! command -v mpicxx >/dev/null 2>&1; then
         echo "mpicxx not found" >&2
