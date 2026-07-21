2c2c6d35b1d77eeb84963a02bfe0a3fabed5d051
SohailRUPERT <sohail.rathore@stfc.ac.uk>
2026-07-21 15:55:12 +0100

    Adding forward declarations of classes and adding necessary headers that are needed in non-unity files
    	modified:   framework/include/mfem/problem/MFEMProblem.h
    	new file:   framework/include/mfem/problem_operator_builders/ProblemOperatorBuilderBase.h
    	new file:   framework/include/mfem/problem_operator_builders/ProblemOperatorBuilderSteady.h
    	new file:   framework/src/mfem/problem_operator_builders/ProblemOperatorBuilderSteady.C
    	new file:   framework/src/mfem/problem_operator_builders/ProblemOperatorBuilderTransient.C
    
    	modified:   framework/include/mfem/problem/MFEMProblem.h
    	new file:   framework/include/mfem/problem_operator_builders/ProblemOperatorBuilderBase.h
    	new file:   framework/include/mfem/problem_operator_builders/ProblemOperatorBuilderSteady.h
    	new file:   framework/src/mfem/problem_operator_builders/ProblemOperatorBuilderSteady.C
    	new file:   framework/src/mfem/problem_operator_builders/ProblemOperatorBuilderTransient.C

---
+++ framework/src/mfem/problem_operator_builders/ProblemOperatorBuilderSteady.C
@@ -0,0 +1,76 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#ifdef MOOSE_MFEM_ENABLED
+
+#include "ProblemOperatorBuilderSteady.h"
+#include "MFEMProblem.h"
+#include "ProblemOperatorBase.h"
+#include "libmesh/ignore_warnings.h"
+#include "mfem/miniapps/common/mesh_extras.hpp"
+#include "libmesh/restore_warnings.h"
+#include "EquationSystem.h"
+#include "ComplexEquationSystem.h"
+#include "EigenproblemEquationSystem.h"
+#include "EquationSystemProblemOperator.h"
+#include "EigenproblemESProblemOperator.h"
+#include "ComplexEquationSystemProblemOperator.h"
+
+namespace Moose::MFEM
+{
+registerMooseObject("MooseApp", ProblemOperatorBuilderSteady);
+}
+
+InputParameters
+Moose::MFEM::ProblemOperatorBuilderSteady::validParams()
+{
+  InputParameters params = ProblemOperatorBuilderBase::validParams();
+  return params;
+}
+
+Moose::MFEM::ProblemOperatorBuilderSteady::ProblemOperatorBuilderSteady(
+    const InputParameters & parameters)
+  : ProblemOperatorBuilderBase(parameters)
+{
+}
+
+std::shared_ptr<Moose::MFEM::ProblemOperatorBase>
+Moose::MFEM::ProblemOperatorBuilderSteady::createProblemOperator(MFEMProblem & mfemProb)
+{
+  std::shared_ptr<Moose::MFEM::ProblemOperatorBase> probOp;
+
+  // Construct a standard problem operator
+  if (mfemProb.getNumericType() == MFEMProblem::NumericType::REAL)
+  {
+    if (dynamic_cast<MFEMEigenproblem *>(&mfemProb))
+    {
+      mfemProb.getProblemData().eqn_system =
+          std::make_shared<Moose::MFEM::EigenproblemEquationSystem>();
+      probOp = std::make_shared<Moose::MFEM::EigenproblemESProblemOperator>(mfemProb);
+    }
+    else
+    {
+      mfemProb.getProblemData().eqn_system = std::make_shared<Moose::MFEM::EquationSystem>();
+      probOp = std::make_shared<Moose::MFEM::EquationSystemProblemOperator>(mfemProb);
+    }
+  }
+  else if (mfemProb.getNumericType() == MFEMProblem::NumericType::COMPLEX)
+  {
+    mfemProb.getProblemData().eqn_system = std::make_shared<Moose::MFEM::ComplexEquationSystem>();
+    probOp = std::make_shared<Moose::MFEM::ComplexEquationSystemProblemOperator>(mfemProb);
+  }
+  else
+  {
+    mooseError("Unknown numeric type. "
+               "Please set the Problem numeric type to either 'real' or 'complex'.");
+  }
+  return probOp;
+}
+
+#endif
