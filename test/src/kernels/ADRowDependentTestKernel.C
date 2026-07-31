948b96a4a20eec140a667e653a1d5b5555a65e5e
Andreas Henrik Frederiksen <andreas.frederiksen@inl.gov>
2026-07-31 16:12:14 -0600

    Address mortar CIVET regressions
    refs #33378

--- test/src/kernels/ADRowDependentTestKernel.C
+++ test/src/kernels/ADRowDependentTestKernel.C
@@ -14,18 +14,35 @@ InputParameters
 ADRowDependentTestKernel::validParams()
 {
   InputParameters params = ADKernel::validParams();
-  params.addClassDescription(
-      "Tests Jacobian assembly when residual rows have different AD derivative support.");
+  params.addClassDescription("Tests Jacobian assembly when residual rows have different AD "
+                             "derivative support or insertion order.");
+  params.addParam<bool>("reorder_equivalent_support",
+                        false,
+                        "Insert equivalent row derivative supports in different orders.");
   return params;
 }
 
 ADRowDependentTestKernel::ADRowDependentTestKernel(const InputParameters & parameters)
-  : ADKernel(parameters), _ad_dof_values(_var.adDofValues())
+  : ADKernel(parameters),
+    _ad_dof_values(_var.adDofValues()),
+    _reorder_equivalent_support(getParam<bool>("reorder_equivalent_support"))
 {
 }
 
 ADReal
 ADRowDependentTestKernel::computeQpResidual()
 {
+  if (_reorder_equivalent_support)
+  {
+    ADReal residual = 0;
+    for (const auto j : index_range(_phi))
+    {
+      // MetaPhysicL canonicalizes the alternating operation order to the same support.
+      const auto k = _i % 2 ? _phi.size() - 1 - j : j;
+      residual += _test[_i][_qp] * _phi[k][_qp] * _ad_dof_values[k];
+    }
+    return residual;
+  }
+
   return _test[_i][_qp] * _ad_dof_values[_i];
 }
