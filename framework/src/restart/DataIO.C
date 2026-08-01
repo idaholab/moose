a6e61acbf170119d1863e1487b2dcb2ed82a3d3c
Logan Harbour <loganhharbour@gmail.com>
2026-07-31 22:33:57 -0600

    Simplify data load and store for point and bounding box

--- framework/src/restart/DataIO.C
+++ framework/src/restart/DataIO.C
@@ -301,17 +301,13 @@ void
 dataStore(std::ostream & stream, Point & p, void * context)
 {
   for (const auto i : make_range(Moose::dim))
-  {
-    Real r = p(i);
-    dataStore(stream, r, context);
-  }
+    dataStore(stream, p(i), context);
 }
 
 void
-dataStore(std::ostream & stream, libMesh::BoundingBox & p, void * context)
+dataStore(std::ostream & stream, libMesh::BoundingBox & bbox, void * context)
 {
-  dataStore(stream, p.min(), context);
-  dataStore(stream, p.max(), context);
+  storeHelper(stream, static_cast<std::pair<Point, Point> &>(bbox), context);
 }
 
 template <>
@@ -677,18 +673,13 @@ void
 dataLoad(std::istream & stream, Point & p, void * context)
 {
   for (const auto i : make_range(Moose::dim))
-  {
-    Real r = 0;
-    dataLoad(stream, r, context);
-    p(i) = r;
-  }
+    dataLoad(stream, p(i), context);
 }
 
 void
 dataLoad(std::istream & stream, libMesh::BoundingBox & bbox, void * context)
 {
-  dataLoad(stream, bbox.min(), context);
-  dataLoad(stream, bbox.max(), context);
+  loadHelper(stream, static_cast<std::pair<Point, Point> &>(bbox), context);
 }
 
 template <>
