c03bb6da78539fe7dac5f2d9120fbfb6d5be5b3f
Peter German <grmnptr@gmail.com>
2026-08-01 20:09:04 -0600

    Add anisotropic neumann bc for linearFV. (#32719)

---
+++ framework/include/linearfvbcs/LinearFVAnisotropicDiffusionFunctorNeumannBC.h
@@ -0,0 +1,41 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "LinearFVAdvectionDiffusionFunctorNeumannBC.h"
+
+/**
+ * A Neumann boundary condition for linear finite volume anisotropic diffusion problems.
+ *
+ * The prescribed functor is the complete tensor-weighted boundary flux. The diagonal diffusion
+ * tensor is used to reconstruct the boundary-normal gradient and boundary value.
+ */
+class LinearFVAnisotropicDiffusionFunctorNeumannBC
+  : public LinearFVAdvectionDiffusionFunctorNeumannBC
+{
+public:
+  static InputParameters validParams();
+
+  /**
+   * Class constructor.
+   * @param parameters The InputParameters for the object
+   */
+  LinearFVAnisotropicDiffusionFunctorNeumannBC(const InputParameters & parameters);
+
+  virtual Real computeBoundaryValue() const override;
+
+  virtual Real computeBoundaryNormalGradient() const override;
+
+  virtual Real computeBoundaryValueRHSContribution() const override;
+
+protected:
+  /// The functor for the diagonal diffusion tensor
+  const Moose::Functor<RealVectorValue> & _diffusion_tensor;
+};
