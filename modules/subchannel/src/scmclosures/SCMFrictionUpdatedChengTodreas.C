90476b2d28c9b132653c4af7c21913507b2e6bc2
Peter German <grmnptr@gmail.com>
2026-08-04 16:57:15 -0600

    Fix typo in error message. (#33441)

--- modules/subchannel/src/scmclosures/SCMFrictionUpdatedChengTodreas.C
+++ modules/subchannel/src/scmclosures/SCMFrictionUpdatedChengTodreas.C
@@ -237,11 +237,10 @@ SCMFrictionUpdatedChengTodreas::computeTriLatticeFrictionFactor(
       const Real turbulent_wire_correction =
           1 + ws_t * (ar / a_p) * Utility::pow<2>(std::tan(theta));
       if (!std::isfinite(turbulent_wire_correction) || turbulent_wire_correction < 0.0)
-        mooseError(
-            "The exponentiated term in the Cheng - Todreas turbulent wire correction must be "
-            "non-negative and finite for a corner subchannel. Computed ",
-            turbulent_wire_correction,
-            ".");
+        mooseError("The exponentiated term in the Cheng-Todreas turbulent wire correction must be "
+                   "non-negative and finite for a corner subchannel. Computed ",
+                   turbulent_wire_correction,
+                   ".");
       cT *= std::pow(turbulent_wire_correction, 1.41);
       // laminar friction factor equation constant - Corner subchannel
       cL *= (1 + ws_l * (ar / a_p) * Utility::pow<2>(std::tan(theta)));
