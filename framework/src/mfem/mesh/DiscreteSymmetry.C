3ed5f43d7ab9e03466f39f7c6bb66f7f0e2680bc
Alexander Blair <alexander.blair@ukaea.uk>
2026-08-26 17:45:53 +0100

    Build and use a rotation matrix in RotationalSymmetry that can support arbitrary axes of rotation in 3D

--- framework/src/mfem/mesh/DiscreteSymmetry.C
+++ framework/src/mfem/mesh/DiscreteSymmetry.C
@@ -24,15 +24,58 @@ TranslationalSymmetry::ApplyTransform(const mfem::Vector & coord_in, mfem::Vecto
   add(coord_in, _lattice_vector, coord_out);
 }
 
+RotationalSymmetry::RotationalSymmetry(const unsigned int rotational_symmetry_order)
+  : DiscreteSymmetry(),
+    _rotational_symmetry_order(rotational_symmetry_order),
+    _rotation_angle(2 * pi / rotational_symmetry_order),
+    _rotation_axis({0, 0, 1}),
+    _rotation_matrix(BuildRotationMatrix(_rotation_axis, _rotation_angle))
+{
+}
+
+mfem::DenseMatrix
+RotationalSymmetry::BuildRotationMatrix(const mfem::Vector & rotation_axis,
+                                        const mfem::real_t & rotation_angle)
+{
+  mfem::DenseMatrix rotation_matrix(rotation_axis.Size());
+
+  mfem::Vector unit_rot_axis = rotation_axis;
+  unit_rot_axis /= rotation_axis.Norml2();
+
+  const mfem::real_t cos_a = cos(rotation_angle);
+  const mfem::real_t sin_a = sin(rotation_angle);
+
+  const mfem::real_t ux = unit_rot_axis(0);
+  const mfem::real_t uxx = unit_rot_axis(0) * unit_rot_axis(0);
+  const mfem::real_t uxy = unit_rot_axis(0) * unit_rot_axis(1);
+  const mfem::real_t uxz = unit_rot_axis(0) * unit_rot_axis(2);
+
+  const mfem::real_t uy = unit_rot_axis(1);
+  const mfem::real_t uyy = unit_rot_axis(1) * unit_rot_axis(1);
+  const mfem::real_t uyz = unit_rot_axis(1) * unit_rot_axis(2);
+
+  const mfem::real_t uz = unit_rot_axis(2);
+  const mfem::real_t uzz = unit_rot_axis(2) * unit_rot_axis(2);
+
+  rotation_matrix(0, 0) = uxx * (1 - cos_a) + cos_a;
+  rotation_matrix(0, 1) = uxy * (1 - cos_a) - uz * sin_a;
+  rotation_matrix(0, 2) = uxz * (1 - cos_a) + uy * sin_a;
+
+  rotation_matrix(1, 0) = uxy * (1 - cos_a) + uz * sin_a;
+  rotation_matrix(1, 1) = uyy * (1 - cos_a) + cos_a;
+  rotation_matrix(1, 2) = uyz * (1 - cos_a) - ux * sin_a;
+
+  rotation_matrix(2, 0) = uxz * (1 - cos_a) - uy * sin_a;
+  rotation_matrix(2, 1) = uyz * (1 - cos_a) + ux * sin_a;
+  rotation_matrix(2, 2) = uzz * (1 - cos_a) + cos_a;
+
+  return rotation_matrix;
+}
+
 void
 RotationalSymmetry::ApplyTransform(const mfem::Vector & coord_in, mfem::Vector & coord_out)
 {
-  // x' =  x cos phi + y sin phi
-  // y' = -x sin phi + y cos phi
-  // z' = z
-  coord_out[0] = coord_in[0] * cos(_rotation_angle) + coord_in[1] * sin(_rotation_angle);
-  coord_out[1] = -coord_in[0] * sin(_rotation_angle) + coord_in[1] * cos(_rotation_angle);
-  coord_out[2] = coord_in[2];
+  _rotation_matrix.Mult(coord_in, coord_out);
 }
 
 }
