de5117cce3d15f249bc45e0a9d93eb5fe0f7b39c
Alexander Blair <alexander.i.blair@gmail.com>
2026-09-01 21:30:10 +0100

    Rename node_marker_t to node_marker_tdofs in MFEMHypreAMS

--- framework/src/mfem/solvers/MFEMHypreAMS.C
+++ framework/src/mfem/solvers/MFEMHypreAMS.C
@@ -80,7 +80,8 @@ std::unique_ptr<mfem::HypreParVector>
 MFEMHypreAMS::BuildInteriorNodes()
 {
   mfem::ParMesh & pmesh = *_mfem_fespace.getFESpace()->GetParMesh();
-  mfem::H1_FECollection vert_fec(_mfem_fespace.getFESpace()->GetTypicalFE()->GetOrder(), pmesh.Dimension());
+  mfem::H1_FECollection vert_fec(_mfem_fespace.getFESpace()->GetTypicalFE()->GetOrder(),
+                                 pmesh.Dimension());
   mfem::ParFiniteElementSpace vert_fespace(&pmesh, &vert_fec);
 
   mfem::Array<int> vertex_exterior_marker(vert_fespace.GetVSize());
@@ -105,13 +106,13 @@ MFEMHypreAMS::BuildInteriorNodes()
   for (const auto i : make_range(node_marker_gf.Size()))
     node_marker_gf(i) = 1.0 - vertex_exterior_marker[i];
 
-  mfem::Vector node_marker_t;
-  node_marker_gf.GetTrueDofs(node_marker_t);
+  mfem::Vector node_marker_tdofs;
+  node_marker_gf.GetTrueDofs(node_marker_tdofs);
 
   // hypre copies the (2-entry, assumed-partition) dof offsets into the vector, so it stays
   // valid after vert_fespace is destroyed.
   auto interior_nodes = std::make_unique<mfem::HypreParVector>(&vert_fespace);
-  interior_nodes->Set(1.0, node_marker_t);
+  interior_nodes->Set(1.0, node_marker_tdofs);
   return interior_nodes;
 }
 
