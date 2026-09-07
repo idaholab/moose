215e6dc98fe31534072dfcaf79f9fffaaade1981
Sean Baccas <sean.baccas@ukaea.uk>
2026-09-07 12:59:40 +0100

    Fix tests

--- framework/src/mfem/equation_systems/EquationSystem.C
+++ framework/src/mfem/equation_systems/EquationSystem.C
@@ -556,11 +556,17 @@ EquationSystem::BuildNonlinearForms()
     _nlfs.Register(test_var_name, std::make_shared<mfem::ParNonlinearForm>(_test_pfespaces.at(i)));
     // Apply kernels
     auto nlf = _nlfs.GetShared(test_var_name);
-    nlf->SetAssemblyLevel(_assembly_level);
     nlf->SetEssentialTrueDofs(_ess_tdof_lists.at(i));
     ApplyDomainNLFIntegrators(test_var_name, nlf, _kernels_map, std::nullopt);
     ApplyBoundaryNLFIntegrators(test_var_name, nlf, _integrated_bc_map, std::nullopt);
-    nlf->Setup();
+
+    // these two are necessary for nonstandard assembly levels, but are also unsafe
+    // if there are no integrators. So guard this part
+    if (nlf->GetDNFI()->Size() or nlf->GetBNFI()->Size())
+    {
+      nlf->SetAssemblyLevel(_assembly_level);
+      nlf->Setup();
+    }
   }
 }
 
