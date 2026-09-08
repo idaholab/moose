daf075198841d97a82b17c44c1e8e444867a152a
Nicholas Mecham <Nicholas.Mecham@inl.gov>
2026-09-08 09:03:43 -0600

    Incompressible Scalar Kernels for Momentum & Energy in THM #33691
    Adds three incompressible scalar kernels:
     - A 1D momentum integration along a flow path of N geometrically unique segments, acting on the mass flow rate variable.
     - A 1D momentum integration along a flow path of N geometrically unique segments, acting on a coupled pressure drop variable.
     - A 1D area-averaged energy kernel acting on 1 segment of a flow path, acting on the bulk fluid temperature.
    These kernels are built on the SinglePhaseFluidProperties object to automatically incorporate single phase fluid properties. Potential for a separate future implementation for multiphase fluids.
    Also includes a battery of verification tests of these kernels, including:
     - Independent verification of friction, forms/minor, and gravity losses.
     - Independent verification of pump pressure gains.
     - Integrated verification of all loss terms and pump pressure gains.
     - Implicit and explicit property integration.
     - Forward and reverse flow.
     - Multiple segments with different loss terms.
     - Heat transfer with forward, reverse flow and 1, 2, and 3 segments.
    Documentation included as well.
    
    closes #33691

---
+++ modules/thermal_hydraulics/src/scalarkernels/FlowPathMomentumSCSPScalarKernel.C
@@ -0,0 +1,268 @@
+//* This file is part of the MOOSE framework
+//* https://mooseframework.inl.gov
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#include "FlowPathMomentumSCSPScalarKernel.h"
+
+// MOOSE includes
+#include "Assembly.h"
+#include "MooseVariableScalar.h"
+#include "FunctorInterface.h"
+#include "ScalarCoupleable.h"
+#include "SinglePhaseFluidProperties.h"
+
+registerMooseObject("ThermalHydraulicsApp", FlowPathMomentumSCSPScalarKernel);
+registerMooseObject("ThermalHydraulicsApp", ADFlowPathMomentumSCSPScalarKernel);
+
+template <bool is_ad>
+InputParameters
+FlowPathMomentumSCSPScalarKernelTempl<is_ad>::validParams()
+{
+  InputParameters params =
+      is_ad ? ADScalarTimeDerivative::validParams() : ODETimeDerivative::validParams();
+  params += FunctorInterface::validParams();
+  params.addClassDescription(
+      "Implements a generic momentum solve over a 1D flow path, acting on the mass flow rate."
+      "Flow path may be represented by N segments, must provide unique geometric, pump pressure,"
+      " and epsilon information for each segment."
+      "Assumes incompressibility locally."
+      "This version utilizes the Single Component, Single Phase fluid properties base class.");
+  // Lots of inputs so we need to be clear what is what
+  // This block defines coupled state variables the kernel relies on
+  params.addCoupledVar("reference_pressure_drop",
+                       {},
+                       "Reference system pressure drop (IE lower to upper plenum). Takes a "
+                       "scalar variable name");
+  params.addCoupledVar("temperatures",
+                       {},
+                       "Fluid temperature in each segment of this component. Takes a "
+                       "list of scalar variable names");
+  // This block grabs boolean parameters that control the solve type
+  params.addParam<bool>(
+      "is_implicit",
+      false,
+      "Whether an explicit (previous value calculation) or implicit (current value) is used");
+  // This block characterizes the geometry and fluid type
+  params.addRequiredParam<MooseFunctorName>("reference_pressure", "system reference pressure [Pa]");
+  params.addRequiredParam<UserObjectName>("fp", "The name of the user object for fluid properties");
+  params.addParam<std::vector<MooseFunctorName>>(
+      "areas",
+      std::vector<MooseFunctorName>({}),
+      "Component flow areas per segment. Takes a vector of functors.");
+  params.addParam<std::vector<MooseFunctorName>>(
+      "perimeters",
+      std::vector<MooseFunctorName>({}),
+      "Component flow perimeters per segment. Takes a vector of functors.");
+  params.addParam<std::vector<MooseFunctorName>>(
+      "lengths",
+      std::vector<MooseFunctorName>({}),
+      "Component flow lengths per segment. Takes a vector of functors.");
+  params.addParam<std::vector<MooseFunctorName>>(
+      "alphas",
+      std::vector<MooseFunctorName>({}),
+      "Component flow angles per segment with respect to horizontal (-pi/2 downward to pi/2 "
+      "upward). Takes a vector of functors.");
+  params.addParam<std::vector<MooseFunctorName>>(
+      "forms_losses",
+      std::vector<MooseFunctorName>({}),
+      "Forms loss coefficients per segment. Takes a vector of functors.");
+  params.addParam<std::vector<MooseFunctorName>>(
+      "pump_pressures",
+      std::vector<MooseFunctorName>({}),
+      "Pump pressure gains per segment [Pa]. Takes a vector of functors.");
+  // This block characterizes material properties not included in fluid properties object
+  params.addParam<std::vector<MooseFunctorName>>(
+      "roughnesses",
+      std::vector<MooseFunctorName>({}),
+      "Component wall roughnesses per segment [m]. Takes a vector of functors.");
+  params.addParam<MooseFunctorName>("g", 9.81, "Gravitational acceleration [m/s]");
+
+  return params;
+}
+
+template <bool is_ad>
+FlowPathMomentumSCSPScalarKernelTempl<is_ad>::FlowPathMomentumSCSPScalarKernelTempl(
+    const InputParameters & parameters)
+  : Base(parameters),
+    FunctorInterface(this),
+    // Lots of inputs so we need to be clear what is what
+    // This block defines coupled state variables the kernel relies on
+    _dPc(ScalarCoupleable::coupledScalarValue("reference_pressure_drop")),
+    _n_temps(ScalarCoupleable::coupledScalarComponents("temperatures")),
+    _T(_n_temps),
+    // This block grabs boolean parameters that control the solve type
+    _is_implicit(this->template getParam<bool>("is_implicit")),
+    // This block characterizes the geometry and fluid type
+    _Pref(this->template getFunctor<GenericReal<is_ad>>("reference_pressure")),
+    _fp(this->template getUserObject<SinglePhaseFluidProperties>("fp")),
+    _n_segments(this->template getParam<std::vector<MooseFunctorName>>("areas").size()),
+    _areas(this->template getParam<std::vector<MooseFunctorName>>("areas").size()),
+    _perimeters(this->template getParam<std::vector<MooseFunctorName>>("perimeters").size()),
+    _lengths(this->template getParam<std::vector<MooseFunctorName>>("lengths").size()),
+    _alphas(this->template getParam<std::vector<MooseFunctorName>>("alphas").size()),
+    _forms_losses(this->template getParam<std::vector<MooseFunctorName>>("forms_losses").size()),
+    _dPps(this->template getParam<std::vector<MooseFunctorName>>("pump_pressures").size()),
+    // This block characterizes material properties not included in fluid properties object
+    _roughnesses(this->template getParam<std::vector<MooseFunctorName>>("roughnesses").size()),
+    _gravity(this->template getFunctor<GenericReal<is_ad>>("g"))
+{
+  auto & area_names = MooseBase::getParam<std::vector<MooseFunctorName>>("areas");
+  auto & perimeter_names = MooseBase::getParam<std::vector<MooseFunctorName>>("perimeters");
+  auto & length_names = MooseBase::getParam<std::vector<MooseFunctorName>>("lengths");
+  auto & alpha_names = MooseBase::getParam<std::vector<MooseFunctorName>>("alphas");
+  auto & forms_loss_names = MooseBase::getParam<std::vector<MooseFunctorName>>("forms_losses");
+  auto & dPp_names = MooseBase::getParam<std::vector<MooseFunctorName>>("pump_pressures");
+  auto & roughness_names = MooseBase::getParam<std::vector<MooseFunctorName>>("roughnesses");
+  if (_n_segments != area_names.size() || _n_segments != perimeter_names.size() ||
+      _n_segments != length_names.size() || _n_segments != alpha_names.size() ||
+      _n_segments != forms_loss_names.size() || _n_segments != dPp_names.size() ||
+      _n_segments != roughness_names.size() || _n_segments != _n_temps)
+  {
+    mooseError(
+        "Must provide consistent number of segments for each parameter! Including temperatures!");
+  }
+  for (size_t i = 0; i < _n_segments; ++i)
+  {
+    _T[i] = &(ScalarCoupleable::coupledScalarValue("temperatures", i));
+    _areas[i] = &(this->template getFunctor<GenericReal<is_ad>>(area_names[i]));
+    _perimeters[i] = &(this->template getFunctor<GenericReal<is_ad>>(perimeter_names[i]));
+    _lengths[i] = &(this->template getFunctor<GenericReal<is_ad>>(length_names[i]));
+    _alphas[i] = &(this->template getFunctor<GenericReal<is_ad>>(alpha_names[i]));
+    _forms_losses[i] = &(this->template getFunctor<GenericReal<is_ad>>(forms_loss_names[i]));
+    _dPps[i] = &(this->template getFunctor<GenericReal<is_ad>>(dPp_names[i]));
+    _roughnesses[i] = &(this->template getFunctor<GenericReal<is_ad>>(roughness_names[i]));
+  }
+}
+
+template <bool is_ad>
+GenericReal<is_ad>
+FlowPathMomentumSCSPScalarKernelTempl<is_ad>::computeQpResidual()
+{
+  GenericReal<is_ad> momentum_residual = 0;
+  const Moose::ElemArg _qp = Moose::ElemArg();
+  const int _i = 0;
+  const auto _state = _is_implicit ? Moose::currentState() : Moose::oldState();
+  // start by getting global fluid properties
+  auto _mu = _fp.mu_from_p_T(_Pref(_qp, _state), (*(_T[0]))[_i]);
+  auto _rhog = _fp.rho_from_p_T(_Pref(_qp, _state), (*(_T[0]))[_i]);
+  // loop over segments
+  for (size_t i = 0; i < _n_segments; ++i)
+  {
+    // Decide flow regime for friction factor
+    auto _Dh = 4.0 * (*(_areas[i]))(_qp, _state) / (*(_perimeters[i]))(_qp, _state);
+    auto _G = Base::_u[_i] / (*(_areas[i]))(_qp, _state);
+    auto _Re = abs(_G) * _Dh / _mu;
+    auto _lam = 64.0 / _Re;
+    auto _turb =
+        0.25 /
+        pow((log10((*(_roughnesses[i]))(_qp, _state) / (_Dh * 3.7) + 5.74 / pow(_Re, 0.9))), 2);
+    auto _fd = 64.0 / _Re;
+    auto _pfd = &_fd;
+    if (_Re < 2300.0) // laminar
+    {
+      *_pfd = _lam;
+    }
+    else if (_Re > 4000.0) // turbulent using Swamee-Jain approx. of Colebrook-White eq.
+    {
+      *_pfd = _turb;
+    }
+    else // transition, conservative interpolation between the two
+    {
+      *_pfd = (_turb - _lam) / 1700 * _Re + _lam;
+      *_pfd = std::max(*_pfd, std::max(_lam, _turb));
+    }
+    // Friction
+    momentum_residual += _fd * (*(_lengths[i]))(_qp, _state) / _Dh * _G * abs(_G) / 2.0 / _rhog;
+    // Forms
+    momentum_residual += (*(_forms_losses[i]))(_qp, _state) * _G * abs(_G) / 2.0 / _rhog;
+    // Gravity
+    // get local density for natural circulation aspect
+    auto _rhol = _fp.rho_from_p_T(_Pref(_qp, _state), (*(_T[i]))[_i]);
+    momentum_residual += _rhol * _gravity(_qp, _state) * (*(_lengths[i]))(_qp, _state) *
+                         sin((*(_alphas[i]))(_qp, _state));
+    // Pump pressure
+    momentum_residual -= (*(_dPps[i]))(_qp, _state);
+    // Transient term
+    momentum_residual +=
+        (*(_lengths[i]))(_qp, _state) / (*(_areas[i]))(_qp, _state) * Base::_u_dot[_i];
+  }
+  // Pressure drop
+  momentum_residual += _dPc[_i];
+
+  return momentum_residual;
+}
+
+template <bool is_ad>
+Real
+FlowPathMomentumSCSPScalarKernelTempl<is_ad>::computeQpJacobian()
+{
+  if constexpr (!is_ad)
+  {
+    Real momentum_residual = 0;
+    const Moose::ElemArg _qp = Moose::ElemArg();
+    const int _i = 0;
+    const auto _state = _is_implicit ? Moose::currentState() : Moose::oldState();
+    // start by getting global fluid properties
+    auto _mu = _fp.mu_from_p_T(_Pref(_qp, _state), (*(_T[0]))[_i]);
+    auto _rhog = _fp.rho_from_p_T(_Pref(_qp, _state), (*(_T[0]))[_i]);
+    // loop over segments
+    for (size_t i = 0; i < _n_segments; ++i)
+    {
+      // Decide flow regime for friction factor
+      auto _Dh = 4.0 * (*(_areas[i]))(_qp, _state) / (*(_perimeters[i]))(_qp, _state);
+      auto _G = Base::_u[_i] / (*(_areas[i]))(_qp, _state);
+      auto _Re = _G * _Dh / _mu;
+      auto _lam = 64.0 / _Re;
+      auto _turb =
+          0.25 /
+          pow((log10((*(_roughnesses[i]))(_qp, _state) / (_Dh * 3.7) + 5.74 / pow(_Re, 0.9))), 2);
+      auto _fd = 64.0 / _Re;
+      auto _pfd = &_fd;
+      if (_Re < 2300.0) // laminar
+      {
+        *_pfd = _lam;
+      }
+      else if (_Re > 4000.0) // turbulent using Swamee-Jain approx. of Colebrook-White eq.
+      {
+        *_pfd = _turb;
+      }
+      else // transition, conservative interpolation between the two
+      {
+        *_pfd = (_turb - _lam) / 1700 * _Re + _lam;
+        *_pfd = std::max(*_pfd, std::max(_lam, _turb));
+      }
+      // Friction
+      momentum_residual +=
+          _fd * (*(_lengths[i]))(_qp, _state) / _Dh * _G / _rhog / (*(_areas[i]))(_qp, _state);
+      // Forms
+      momentum_residual +=
+          (*(_forms_losses[i]))(_qp, _state) * _G / _rhog / (*(_areas[i]))(_qp, _state);
+      // Transient term
+      momentum_residual +=
+          (*(_lengths[i]))(_qp, _state) / (*(_areas[i]))(_qp, _state) * Base::_du_dot_du[_i];
+    }
+
+    return momentum_residual;
+  }
+  else
+  {
+    mooseError("computeQpJacobian() should not be called in AD mode");
+    return 0;
+  }
+}
+
+template <>
+Real
+FlowPathMomentumSCSPScalarKernelTempl<true>::computeQpJacobian()
+{
+  mooseError("Internal error, calling computeQpJacobian in AD class.");
+  return 0.0;
+}
+
+template class FlowPathMomentumSCSPScalarKernelTempl<false>;
+template class FlowPathMomentumSCSPScalarKernelTempl<true>;
---
+++ modules/thermal_hydraulics/test/tests/scalarkernels/FlowPathVerification/coupledpressure_reverse.i
@@ -0,0 +1,140 @@
+
+
+length = 1.0
+R = 0.025
+min = 1.0
+Pin = 101300
+dP = 10130
+Tin = 293.15
+rho = 998.2
+mu = 0.001002
+alpha = 0.0
+epsilon = 0.0001
+forms = 0.0
+pump = 0.0
+gravity = 0.0
+area = ${fparse 3.14159* ${R}^2}
+
+[Mesh]
+  type = GeneratedMesh
+  dim = 1
+  xmin = 0
+  xmax = ${length}
+  nx = 1
+[]
+
+[Variables]
+    [m1]
+        family = SCALAR
+        initial_condition = ${min}
+    []
+    [dPc]
+        family = SCALAR
+        initial_condition = ${dP}
+    []
+    [T1]
+        family = SCALAR
+        initial_condition = ${Tin}
+    []
+[]
+
+[FluidProperties]
+  [water]
+    type = Water97FluidProperties
+  []
+[]
+
+[ScalarKernels]
+    [pipe1_mom]
+        type = CoupledPressureFlowPathMomentumSCSPScalarKernel
+        variable = 'dPc'
+        coupled_mass_flow_rate = 'm1'
+        temperatures = 'T1'
+        reference_pressure = ${Pin}
+        fp = 'water'
+        areas = '${area}'
+        perimeters = '${fparse 2*3.14159* ${R}}'
+        lengths = '${length}'
+        alphas = '${alpha}'
+        forms_losses = '${forms}'
+        pump_pressures = '${pump}'
+        roughnesses = '${epsilon}'
+        g = ${gravity}
+        is_implicit = True
+    []
+    [pipe1_coupledDT]
+      type = CoupledODETimeDerivative
+      variable = 'dPc'
+      v = 'm1'
+    []
+    [temp]
+      type = ParsedODEKernel
+      expression = 'T1 - ${Tin}'
+      variable = T1
+    []
+    [m1k]
+      type = ParsedODEKernel
+      expression = 'm1 - ${min}'
+      variable = m1
+    []
+[]
+
+[Postprocessors]
+  [mdot]
+    type = ScalarVariable
+    variable = m1
+    execute_on = 'TIMESTEP_END'
+  []
+  [dP]
+    type = ScalarVariable
+    variable = dPc
+    execute_on = 'TIMESTEP_END'
+  []
+  [Re]
+    type = ParsedPostprocessor
+    expression = 'abs(mdot) / ${area} * 2 * ${R} / ${mu}'
+    pp_names = 'mdot'
+    execute_on = 'TIMESTEP_END'
+  []
+  [f]
+    type = ParsedPostprocessor
+    expression = '0.25 / ((log10(${epsilon} / 2 / 3.7 / ${R} + 5.74 / Re ^ 0.9 ))^2)'
+    pp_names = 'Re'
+    execute_on = 'TIMESTEP_END'
+  []
+  [analytical_dP]
+    type = ParsedPostprocessor
+    expression = '-${pump} + f * ${length} * mdot * abs(mdot) / 4 / ${R} / ${rho} / ${area}^2 + ${forms} * mdot * abs(mdot) / 2 / ${rho} / ${area}^2 + ${rho} * ${gravity} * ${length} * sin(${alpha})'
+    pp_names = 'f mdot'
+    execute_on = 'TIMESTEP_END'
+  []
+  [relative_error]
+    type = ParsedPostprocessor
+    expression = 'abs((analytical_dP - dP)/analytical_dP)'
+    pp_names = 'analytical_dP dP'
+    execute_on = 'TIMESTEP_END'
+  []
+[]
+
+[Executioner]
+  type = Transient
+  start_time = 0
+  end_time = 100.0
+  [TimeStepper]
+    type = IterationAdaptiveDT
+    growth_factor = 1.4
+    dt = 5
+  []
+  solve_type = 'PJFNK'
+  nl_abs_tol = 1e-09
+  l_tol = 1e-07
+[]
+
+[Outputs]
+  perf_graph = true
+  [out]
+    type = CSV
+    execute_on = 'FINAL'
+    file_base = 'friction_onlyCP'
+  []
+[]
