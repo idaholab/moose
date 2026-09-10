4d6865c541f92e2fa8c0cc0fed38b2c991d1e357
Alexander Blair <alexander.i.blair@gmail.com>
2026-09-10 19:01:48 +0100

    Add support for line search in MFEMNewtonNonlinearSolver. Refs #33716

--- framework/include/mfem/solvers/MFEMNewtonNonlinearSolver.h
+++ framework/include/mfem/solvers/MFEMNewtonNonlinearSolver.h
@@ -13,6 +13,55 @@
 
 #include "MFEMNonlinearSolverBase.h"
 
+namespace Moose::MFEM
+{
+/**
+ * mfem::NewtonSolver applying a constant damping factor to the Newton update, optionally
+ * shortening it further with a backtracking line search on the residual norm.
+ */
+class DampedNewtonSolver : public mfem::NewtonSolver
+{
+public:
+  DampedNewtonSolver(MPI_Comm comm) : mfem::NewtonSolver(comm) {}
+
+  void SetOperator(const mfem::Operator & op) override;
+
+  /// Set the fraction of the full Newton update applied. The backtracking line search, when
+  /// enabled, starts from this trial step.
+  void SetDampingFactor(mfem::real_t damping_factor) { _damping_factor = damping_factor; }
+
+  /// Enable a backtracking line search taking at most @a max_its trial steps, each a factor
+  /// @a contraction_factor shorter than the last, accepting the first whose residual norm
+  /// satisfies the sufficient decrease condition with coefficient @a sufficient_decrease.
+  void SetBacktracking(unsigned int max_its,
+                       mfem::real_t contraction_factor,
+                       mfem::real_t sufficient_decrease);
+
+  /**
+   * Return the fraction of the Newton update to apply at the iterate @a x. Without
+   * backtracking this is the constant damping factor. With backtracking it is the first trial
+   * step t, starting from the damping factor, satisfying
+   * ||F(x - t c) - b|| <= (1 - sufficient_decrease * t) * ||F(x) - b||,
+   * or 0 if no trial step satisfies it, which interrupts the Newton iteration.
+   */
+  mfem::real_t ComputeScalingFactor(const mfem::Vector & x, const mfem::Vector & b) const override;
+
+protected:
+  /// Fraction of the full Newton update applied, and the line search's initial trial step.
+  mfem::real_t _damping_factor = 1.0;
+  /// Maximum number of trial steps taken by the line search; 0 disables backtracking.
+  unsigned int _ls_max_its = 0;
+  /// Factor a rejected trial step is multiplied by to obtain the next one.
+  mfem::real_t _ls_contraction_factor = 0.5;
+  /// Coefficient of the sufficient decrease (Armijo) condition on the residual norm.
+  mfem::real_t _ls_sufficient_decrease = 1.0e-4;
+  /// Trial iterate x - t c evaluated by the line search.
+  mutable mfem::Vector _ls_x_trial;
+  /// Residual at the trial iterate.
+  mutable mfem::Vector _ls_r_trial;
+};
+} // namespace Moose::MFEM
+
 /**
  * MooseObject wrapper for mfem::NewtonSolver-backed nonlinear solves.
  */
