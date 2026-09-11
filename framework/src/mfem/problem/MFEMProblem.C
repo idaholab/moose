73b886153c2f427147df8290f77ccae6e2c77b79
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2026-09-11 11:58:38 -0600

    Allow subapps of an MFEM problem (or parent apps) to run with threads - MFEM-moose should get its thread counts from the mooseapp - changing a runtime setting for a parent app from a child app is not ok

--- framework/src/mfem/problem/MFEMProblem.C
+++ framework/src/mfem/problem/MFEMProblem.C
@@ -77,12 +77,7 @@ MFEMProblem::MFEMProblem(const InputParameters & params)
 {
   // Initialise Hypre for all MFEM problems.
   mfem::Hypre::Init();
-  // Disable multithreading for all MFEM problems (including any libMesh or MFEM subapps).
-  libMesh::libMeshPrivateData::_n_threads = 1;
-#ifdef LIBMESH_HAVE_OPENMP
-  omp_set_num_threads(1);
-#endif
-  // Also reduce this application's thread count so MOOSE-side consumers (which read
+  // Reduce this application's thread count so MOOSE-side consumers (which read
   // MooseApp::n_threads()) run single-threaded to match.
   getMooseApp().setNumThreads(1);
   setMesh();
