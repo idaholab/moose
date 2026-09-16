3d3aac4419337a38f2dd54d56a70d5583de9ebb2
Nicholas Mecham <Nicholas.Mecham@inl.gov>
2026-09-16 08:36:35 -0600

    Refactored controllable implementation for better efficiency.

--- framework/include/postprocessors/ParsedPostprocessor.h
+++ framework/include/postprocessors/ParsedPostprocessor.h
@@ -46,6 +46,9 @@ private:
   /// This post-processor value
   Real _value;
 
+  /// 
+  std::string _postprocessors;
+
   using Moose::FunctorBase<Real>::evaluate;
   usingFunctionParserUtilsMembers(false);
 };
